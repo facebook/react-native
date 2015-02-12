70f28332b662c7f8b4292db9129fabd4be07f0ca
Christopher Chedeau <vjeux@fb.com>
2015-02-12 14:43:41 -0800

    Initial version of the automatically generated docs

--- website/server/convert.js
+++ website/server/convert.js
@@ -3,6 +3,7 @@ var glob = require('glob');
 var mkdirp = require('mkdirp');
 var optimist = require('optimist');
 var path = require('path');
+var extractDocs = require('./extractDocs');
 var argv = optimist.argv;
 
 function splitHeader(content) {
@@ -28,89 +29,93 @@ function backtickify(str) {
 function execute() {
   var MD_DIR = '../docs/';
 
-  glob('src/react-native/docs/*.*', function(er, files) {
-    files.forEach(function(file) {
-      try {
-        fs.unlinkSync(file);
-      } catch(e) {
-        /* seriously, unlink throws when the file doesn't exist :( */
-      }
-    });
+  var files = glob.sync('src/react-native/docs/*.*')
+  files.forEach(function(file) {
+    try {
+      fs.unlinkSync(file);
+    } catch(e) {
+      /* seriously, unlink throws when the file doesn't exist :( */
+    }
   });
 
   var metadatas = {
     files: [],
   };
 
-  glob(MD_DIR + '**/*.*', function (er, files) {
-    files.forEach(function(file) {
-      var extension = path.extname(file);
-      if (extension === '.md' || extension === '.markdown') {
-        var content = fs.readFileSync(file, {encoding: 'utf8'});
-        var metadata = {};
+  function handleMarkdown(content) {
+    var metadata = {};
 
-        // Extract markdown metadata header
-        var both = splitHeader(content);
-        var lines = both.header.split('\n');
-        for (var i = 0; i < lines.length - 1; ++i) {
-          var keyvalue = lines[i].split(':');
-          var key = keyvalue[0].trim();
-          var value = keyvalue.slice(1).join(':').trim();
-          // Handle the case where you have "Community #10"
-          try { value = JSON.parse(value); } catch(e) { }
-          metadata[key] = value;
-        }
-        metadatas.files.push(metadata);
+    // Extract markdown metadata header
+    var both = splitHeader(content);
+    var lines = both.header.split('\n');
+    for (var i = 0; i < lines.length - 1; ++i) {
+      var keyvalue = lines[i].split(':');
+      var key = keyvalue[0].trim();
+      var value = keyvalue.slice(1).join(':').trim();
+      // Handle the case where you have "Community #10"
+      try { value = JSON.parse(value); } catch(e) { }
+      metadata[key] = value;
+    }
+    metadatas.files.push(metadata);
 
-        if (metadata.permalink.match(/^https?:/)) {
-          return;
-        }
+    if (metadata.permalink.match(/^https?:/)) {
+      return;
+    }
 
-        // Create a dummy .js version that just calls the associated layout
-        var layout = metadata.layout[0].toUpperCase() + metadata.layout.substr(1) + 'Layout';
+    // Create a dummy .js version that just calls the associated layout
+    var layout = metadata.layout[0].toUpperCase() + metadata.layout.substr(1) + 'Layout';
 
-        var content = (
-          '/**\n' +
-          ' * @generated\n' +
-          ' * @jsx React.DOM\n' +
-          ' */\n' +
-          'var React = require("React");\n' +
-          'var layout = require("' + layout + '");\n' +
-          'var content = ' + backtickify(both.content) + '\n' +
-          'var Post = React.createClass({\n' +
-          '  render: function() {\n' +
-          '    return layout({metadata: ' + JSON.stringify(metadata) + '}, content);\n' +
-          '  }\n' +
-          '});\n' +
-          // TODO: Use React statics after upgrading React
-          'Post.content = content;\n' +
-          'module.exports = Post;\n'
-        );
-
-        var targetFile = 'src/react-native/' + metadata.permalink.replace(/\.html$/, '.js');
-        mkdirp.sync(targetFile.replace(new RegExp('/[^/]*$'), ''));
-        fs.writeFileSync(targetFile, content);
-      }
-
-      if (extension === '.json') {
-        var content = fs.readFileSync(file, {encoding: 'utf8'});
-        metadatas[path.basename(file, '.json')] = JSON.parse(content);
-      }
-    });
-
-    fs.writeFileSync(
-      'core/metadata.js',
+    var content = (
       '/**\n' +
       ' * @generated\n' +
-      ' * @providesModule Metadata\n' +
+      ' * @jsx React.DOM\n' +
       ' */\n' +
-      'module.exports = ' + JSON.stringify(metadatas, null, 2) + ';'
+      'var React = require("React");\n' +
+      'var layout = require("' + layout + '");\n' +
+      'var content = ' + backtickify(both.content) + '\n' +
+      'var Post = React.createClass({\n' +
+      '  render: function() {\n' +
+      '    return layout({metadata: ' + JSON.stringify(metadata) + '}, content);\n' +
+      '  }\n' +
+      '});\n' +
+      // TODO: Use React statics after upgrading React
+      'Post.content = content;\n' +
+      'module.exports = Post;\n'
     );
+
+    var targetFile = 'src/react-native/' + metadata.permalink.replace(/\.html$/, '.js');
+    mkdirp.sync(targetFile.replace(new RegExp('/[^/]*$'), ''));
+    fs.writeFileSync(targetFile, content);
+  }
+
+  extractDocs().forEach(handleMarkdown);
+
+  var files = glob.sync(MD_DIR + '**/*.*');
+  files.forEach(function(file) {
+    var extension = path.extname(file);
+    if (extension === '.md' || extension === '.markdown') {
+      var content = fs.readFileSync(file, {encoding: 'utf8'});
+      handleMarkdown(content);
+    }
+
+    if (extension === '.json') {
+      var content = fs.readFileSync(file, {encoding: 'utf8'});
+      metadatas[path.basename(file, '.json')] = JSON.parse(content);
+    }
   });
+
+  fs.writeFileSync(
+    'core/metadata.js',
+    '/**\n' +
+    ' * @generated\n' +
+    ' * @providesModule Metadata\n' +
+    ' */\n' +
+    'module.exports = ' + JSON.stringify(metadatas, null, 2) + ';'
+  );
 }
 
 if (argv.convert) {
-  console.log('convert!')
+  console.log('convert!');
   execute();
 }
 
