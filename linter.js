fefcb2dfa22f6b0a7689d0e17cffe976e7740e36
James Ide <ide@jameside.com>
2015-02-13 12:35:26 -0800

    [Lint] Update linter to use babel-lint and lint JSX/React directly
    - Upgraded eslint
    - Installed babel-eslint so that we can parse JSX and ES6
    - Installed eslint-react-plugin so we can lint JSX directly w/o a transform
    - `no-comma-dangle` was replaced with `comma-dangle`
    - `space-unary-word-ops` was replaced with `space-unary-ops`.

--- linter.js
+++ linter.js
@@ -8,10 +8,5 @@
  */
 'use strict';
 
-var transformSource = require('./jestSupport/scriptPreprocess.js').transformSource;
-var linterTransform = require('./lint/linterTransform');
-
-linterTransform.setLinterTransform(transformSource);
-
 // Run the original CLI
 require('eslint/bin/eslint');
