a8aedb3e78c786f1fcba99319ef88b4a3c376692
Alex Akers <a2@fb.com>
2015-03-10 12:09:35 -0700

    [React Native][Device ID][wip] implement most basic js access

---
+++ Libraries/Picker/PickerIOS.android.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * @providesModule PickerIOS
+ *
+ * This is a controlled component version of RKPickerIOS
+ */
+'use strict';
+
+module.exports = require('UnimplementedView');
