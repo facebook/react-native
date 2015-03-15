8799f0c487eff4b37b6d357f0b8b955bb72b441c
Nick Lockwood <nicklockwood@fb.com>
2015-03-15 16:08:42 -0700

    Ported ART to new UIManager

--- ReactKit/Views/RCTTabBarItemManager.m
+++ ReactKit/Views/RCTTabBarItemManager.m
@@ -16,7 +16,7 @@ RCT_EXPORT_VIEW_PROPERTY(selected);
 RCT_EXPORT_VIEW_PROPERTY(icon);
 RCT_REMAP_VIEW_PROPERTY(selectedIcon, barItem.selectedImage);
 RCT_REMAP_VIEW_PROPERTY(badgeValue, barItem.badgeValue);
-RCT_CUSTOM_VIEW_PROPERTY(title, RCTTabBarItem *)
+RCT_CUSTOM_VIEW_PROPERTY(title, RCTTabBarItem)
 {
   view.barItem.title = json ? [RCTConvert NSString:json] : defaultView.barItem.title;
   view.barItem.imageInsets = [view.barItem.title length] ? UIEdgeInsetsZero : (UIEdgeInsets){6, 0, -6, 0};
