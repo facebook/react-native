7fb8bff66b20b14f2588542a96dab16f8a917fc7
Christopher Chedeau <vjeux@fb.com>
2015-03-14 17:50:34 -0700

    [ReactNative] Fix File Watcher test

--- packager/react-packager/src/FileWatcher/__tests__/FileWatcher-test.js
+++ packager/react-packager/src/FileWatcher/__tests__/FileWatcher-test.js
@@ -20,7 +20,7 @@ describe('FileWatcher', function() {
     });
   });
 
-  it('it should get the watcher instance when ready', function() {
+  pit('it should get the watcher instance when ready', function() {
     var fileWatcher = new FileWatcher(['rootDir']);
     return fileWatcher._loading.then(function(watchers) {
       watchers.forEach(function(watcher) {
