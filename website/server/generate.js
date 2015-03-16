ad3192c3602909041f46bac661f01a6ef1e1dc98
Christopher Chedeau <vjeux@fb.com>
2015-03-15 19:32:59 -0700

    Attempt #5

--- website/server/generate.js
+++ website/server/generate.js
@@ -5,6 +5,8 @@ var fs = require('fs.extra');
 var mkdirp = require('mkdirp');
 var server = require('./server.js');
 
+require('./convert.js')();
+
 // Sadly, our setup fatals when doing multiple concurrent requests
 // I don't have the time to dig into why, it's easier to just serialize
 // requests.
