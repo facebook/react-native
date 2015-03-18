e1143dea9bab3ba56236e6e756283bd40f8a07d2
Spencer Ahrens <sahrens@fb.com>
2015-03-17 21:12:13 -0700

    Add IntegrationTests to travis CI.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+xctool \
+  -project IntegrationTests/IntegrationTests.xcodeproj \
+  -scheme IntegrationTests \
+  -sdk iphonesimulator8.1 \
+  -destination "platform=iOS Simulator,OS=${1},name=iPhone 5" \
+  build test
+
+xctool \
+  -project Examples/UIExplorer/UIExplorer.xcodeproj \
+  -scheme UIExplorer \
+  -sdk iphonesimulator8.1 \
+  -destination "platform=iOS Simulator,OS=${1},name=iPhone 5" \
+  build test
