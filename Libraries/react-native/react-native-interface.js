929775ba0bfff69d448a2252bfde960d89924a41
Basil Hosmer <bhosmer@fb.com>
2015-03-18 17:40:49 -0700

    purge rni
    Summary:
    react-native-interface.js was largely redundant with
    react-native.js. This diff leaves only the nonredundant
    parts.
    
    Test Plan: ~/fbobjc/Libraries/FBReactKit/js/react-native-github $ flow check .

--- Libraries/react-native/react-native-interface.js
+++ Libraries/react-native/react-native-interface.js
@@ -1,35 +1,9 @@
-declare module "react-native" {
-  declare class ListViewDataSource {
-    constructor(params: Object): void;
-  }
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * @flow
+ */
 
-  declare var AppRegistry: ReactClass<any, any, any>;
-  declare var Image: ReactClass<any, any, any>;
-  declare var ListView: ReactClass<any, any, any>;
-  declare var NavigatorIOS: ReactClass<any, any, any>;
-  declare var NavigatorItemIOS: ReactClass<any, any, any>;
-  declare var PixelRatio: ReactClass<any, any, any>;
-  declare var ScrollView: ReactClass<any, any, any>;
-  declare var ActivityIndicatorIOS: ReactClass<any, any, any>;
-  declare var StyleSheet: ReactClass<any, any, any>;
-  declare var Text: ReactClass<any, any, any>;
-  declare var TextInput: ReactClass<any, any, any>;
-  declare var TimerMixin: ReactClass<any, any, any>;
-  declare var TouchableHighlight: ReactClass<any, any, any>;
-  declare var TouchableWithoutFeedback: ReactClass<any, any, any>;
-  declare var View: ReactClass<any, any, any>;
-  declare var invariant: Function;
-  declare var ix: Function;
-}
-
-declare module "addons" {
-  declare var NavigatorIOS: ReactClass<any, any, any>;
-  declare var NavigatorItemIOS: ReactClass<any, any, any>;
-  declare var StyleSheet: ReactClass<any, any, any>;
-}
+// see also react-native.js
 
 declare var __DEV__: boolean;
-
-declare module "fetch" {
-  declare function exports(url: string, options?: Object): Object;
-}
