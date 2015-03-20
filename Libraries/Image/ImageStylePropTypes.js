55fccb5c6ddbbd23fd18d3bff242313be19b055d
Felix Kling <fkling@fb.com>
2015-03-20 16:41:31 -0700

    [react-native] Use SpreadProperty to make react-docgen happy
    Test Plan: Careful code inspection (if someone can tell me how to properly test this, I'll do it).

--- Libraries/Image/ImageStylePropTypes.js
+++ Libraries/Image/ImageStylePropTypes.js
@@ -9,23 +9,19 @@ var ImageResizeMode = require('ImageResizeMode');
 var LayoutPropTypes = require('LayoutPropTypes');
 var ReactPropTypes = require('ReactPropTypes');
 
-var merge = require('merge');
+var ImageStylePropTypes = {
+  ...LayoutPropTypes,
+  resizeMode: ReactPropTypes.oneOf(Object.keys(ImageResizeMode)),
+  backgroundColor: ReactPropTypes.string,
+  borderColor: ReactPropTypes.string,
+  borderWidth: ReactPropTypes.number,
+  borderRadius: ReactPropTypes.number,
 
-var ImageStylePropTypes = merge(
-  LayoutPropTypes,
-  {
-    resizeMode: ReactPropTypes.oneOf(Object.keys(ImageResizeMode)),
-    backgroundColor: ReactPropTypes.string,
-    borderColor: ReactPropTypes.string,
-    borderWidth: ReactPropTypes.number,
-    borderRadius: ReactPropTypes.number,
-
-    // iOS-Specific style to "tint" an image.
-    // It changes the color of all the non-transparent pixels to the tintColor
-    tintColor: ReactPropTypes.string,
-    opacity: ReactPropTypes.number,
-  }
-);
+  // iOS-Specific style to "tint" an image.
+  // It changes the color of all the non-transparent pixels to the tintColor
+  tintColor: ReactPropTypes.string,
+  opacity: ReactPropTypes.number,
+};
 
 // Image doesn't support padding correctly (#4841912)
 var unsupportedProps = Object.keys({
