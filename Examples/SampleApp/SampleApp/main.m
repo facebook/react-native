d56a57c956e54b51a2f285a35b397a3efda1bfee
Alex Kotliarskyi <frantic@fb.com>
2015-03-20 14:02:39 -0700

    [ReactNative] New SampleApp

---
+++ Examples/SampleApp/SampleApp/main.m
@@ -0,0 +1,11 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
