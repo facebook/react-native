bb4a0a15e7f5607dff438f6c19a8449604384614
Marshall Roch <mroch@fb.com>
2015-03-22 23:14:06 -0700

    Add clearImmediate module
    Summary: should be a dependency of Libraries/Utilities/TimerMixin.js, which is accessing the global even though clearImmediate won't be standardized.
    
    Test Plan: flow

---
+++ Libraries/vendor/core/clearImmediate.js
@@ -0,0 +1,19 @@
+/**
+ * @generated SignedSource<<4595f3986407fd02332cf9f5fc12e70f>>
+ *
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !! This file is a check-in of a static_upstream project!      !!
+ * !!                                                            !!
+ * !! You should not modify this file directly. Instead:         !!
+ * !! 1) Use `fjs use-upstream` to temporarily replace this with !!
+ * !!    the latest version from upstream.                       !!
+ * !! 2) Make your changes, test them, etc.                      !!
+ * !! 3) Use `fjs push-upstream` to copy your changes back to    !!
+ * !!    static_upstream.                                        !!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
+ * @providesModule clearImmediate
+ */
+
+module.exports = global.clearImmediate ||
+  require('ImmediateImplementation').clearImmediate;
