f67a36733f0f291e3ef285491c791ba394662d84
Christopher Chedeau <vjeux@fb.com>
2015-03-23 11:48:02 -0700

    [ReactNative] Expanded license on js packager files

--- packager/react-packager/src/DependencyResolver/haste/polyfills/error-guard.js
+++ packager/react-packager/src/DependencyResolver/haste/polyfills/error-guard.js
@@ -1,5 +1,11 @@
-
 /**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
  * The particular require runtime that we are using looks for a global
  * `ErrorUtils` object and if it exists, then it requires modules with the
  * error handler specified via ErrorUtils.setGlobalHandler by calling the
