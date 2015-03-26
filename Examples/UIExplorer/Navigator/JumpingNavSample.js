2448946a7e438388fbdc3cc9a873c427749e0412
Eric Vicenti <evv@fb.com>
2015-03-26 06:08:03 -0700

    [ReactNative] Navigator Example Overhaul

--- Examples/UIExplorer/Navigator/JumpingNavSample.js
+++ Examples/UIExplorer/Navigator/JumpingNavSample.js
@@ -11,8 +11,10 @@
 var React = require('react-native');
 var {
   Navigator,
+  PixelRatio,
   StyleSheet,
   ScrollView,
+  TabBarIOS,
   Text,
   TouchableHighlight,
   View,
@@ -20,178 +22,186 @@ var {
 
 var _getRandomRoute = function() {
   return {
-    randNumber: Math.random(),
+    randNumber: Math.ceil(Math.random() * 1000),
   };
 };
 
-var INIT_ROUTE = _getRandomRoute();
+class NavButton extends React.Component {
+  render() {
+    return (
+      <TouchableHighlight
+        style={styles.button}
+        underlayColor="#B5B5B5"
+        onPress={this.props.onPress}>
+        <Text style={styles.buttonText}>{this.props.text}</Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
 var ROUTE_STACK = [
   _getRandomRoute(),
   _getRandomRoute(),
-  INIT_ROUTE,
-  _getRandomRoute(),
   _getRandomRoute(),
 ];
-var renderScene = function(route, navigator) {
-  return (
-    <ScrollView style={styles.scene}>
-      <View style={styles.scroll}>
-      <Text>{route.randNumber}</Text>
-      <TouchableHighlight
-        onPress={() => {
-          navigator.jumpBack();
-        }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>jumpBack</Text>
-        </View>
-      </TouchableHighlight>
-      <TouchableHighlight
-        onPress={() => {
-          navigator.jumpForward();
-        }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>jumpForward</Text>
-        </View>
-      </TouchableHighlight>
-      <TouchableHighlight
-        onPress={() => {
-          navigator.jumpTo(INIT_ROUTE);
-        }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>jumpTo initial route</Text>
-        </View>
-      </TouchableHighlight>
-      <TouchableHighlight
-        onPress={() => {
-          navigator.push(_getRandomRoute());
-        }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>destructive: push</Text>
-        </View>
-      </TouchableHighlight>
-      <TouchableHighlight
-        onPress={() => {
-          navigator.replace(_getRandomRoute());
-        }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>destructive: replace</Text>
-        </View>
-      </TouchableHighlight>
-      <TouchableHighlight
-        onPress={() => {
-          navigator.pop();
-        }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>destructive: pop</Text>
-        </View>
-      </TouchableHighlight>
-      <TouchableHighlight
-        onPress={() =>  {
-          navigator.immediatelyResetRouteStack([
-            _getRandomRoute(),
-            _getRandomRoute(),
-          ]);
-        }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>destructive: Immediate set two routes</Text>
-        </View>
-      </TouchableHighlight>
-      <TouchableHighlight
-        onPress={() => {
-          navigator.popToTop();
-        }}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>destructive: pop to top</Text>
-        </View>
-      </TouchableHighlight>
-    </View>
-    </ScrollView>
-  );
-};
+var INIT_ROUTE_INDEX = 1;
 
 class JumpingNavBar extends React.Component {
   render() {
     return (
-      <View style={styles.navBar}>
-        {this.props.routeStack.map((route, index) => (
-          <TouchableHighlight onPress={() => {
-            this.props.navigator.jumpTo(route);
-          }}>
-            <View style={styles.navButton}>
-              <Text
-                style={[
-                  styles.navButtonText,
-                  this.props.navState.toIndex === index && styles.navButtonActive
-                ]}>
-                  {index}
-                </Text>
-            </View>
-          </TouchableHighlight>
-        ))}
+      <View style={styles.tabs}>
+        <TabBarIOS
+          selectedTab={'tab_' + this.props.tabIndex}>
+          <TabBarIOS.Item
+            name="tab_0"
+            icon={require('image!tabnav_notification')}
+            selected={this.props.tabIndex === 0}
+            onPress={() => { this.props.onTabIndex(0); }}
+            children={<View />}
+          />
+          <TabBarIOS.Item
+            name="tab_1"
+            icon={require('image!tabnav_list')}
+            selected={this.props.tabIndex === 1}
+            onPress={() => { this.props.onTabIndex(1); }}
+            children={<View />}
+          />
+          <TabBarIOS.Item
+            name="tab_2"
+            icon={require('image!tabnav_settings')}
+            selected={this.props.tabIndex === 2}
+            onPress={() => { this.props.onTabIndex(2); }}
+            children={<View />}
+          />
+        </TabBarIOS>
       </View>
     );
   }
 }
 
 var JumpingNavSample = React.createClass({
+  getInitialState: function() {
+    return {
+      tabIndex: INIT_ROUTE_INDEX,
+    };
+  },
 
   render: function() {
     return (
       <Navigator
         debugOverlay={false}
-        style={[styles.appContainer]}
-        initialRoute={INIT_ROUTE}
+        style={styles.appContainer}
+        ref={(navigator) => {
+          this._navigator = navigator;
+        }}
+        initialRoute={ROUTE_STACK[INIT_ROUTE_INDEX]}
         initialRouteStack={ROUTE_STACK}
-        renderScene={renderScene}
-        navigationBar={<JumpingNavBar routeStack={ROUTE_STACK} />}
+        renderScene={this.renderScene}
+        navigationBar={
+          <JumpingNavBar
+            routeStack={ROUTE_STACK}
+            tabIndex={this.state.tabIndex}
+            onTabIndex={(index) => {
+              this.setState({ tabIndex: index }, () => {
+                this._navigator.jumpTo(ROUTE_STACK[index]);
+              });
+            }}
+          />
+        }
+        onWillFocus={(route) => {
+          this.setState({
+            tabIndex: ROUTE_STACK.indexOf(route),
+          });
+        }}
         shouldJumpOnBackstackPop={true}
       />
     );
   },
 
+  renderScene: function(route, navigator) {
+    var backBtn;
+    var forwardBtn;
+    if (ROUTE_STACK.indexOf(route) !== 0) {
+      backBtn = (
+        <NavButton
+          onPress={() => {
+            navigator.jumpBack();
+          }}
+          text="jumpBack"
+        />
+      );
+    }
+    if (ROUTE_STACK.indexOf(route) !== ROUTE_STACK.length - 1) {
+      forwardBtn = (
+        <NavButton
+          onPress={() => {
+            navigator.jumpForward();
+          }}
+          text="jumpForward"
+        />
+      );
+    }
+    return (
+      <ScrollView style={styles.scene}>
+        <Text style={styles.messageText}>#{route.randNumber}</Text>
+        {backBtn}
+        {forwardBtn}
+        <NavButton
+          onPress={() => {
+            navigator.jumpTo(ROUTE_STACK[1]);
+          }}
+          text="jumpTo middle route"
+        />
+        <NavButton
+          onPress={() => {
+            this.props.navigator.pop();
+          }}
+          text="Exit Navigation Example"
+        />
+        <NavButton
+          onPress={() => {
+            this.props.navigator.push({
+              message: 'Came from jumping example',
+            });
+          }}
+          text="Nav Menu"
+        />
+      </ScrollView>
+    );
+  },
 });
 
 var styles = StyleSheet.create({
-  scene: {
-    backgroundColor: '#eeeeee',
-  },
-  scroll: {
-    flex: 1,
-  },
   button: {
-    backgroundColor: '#cccccc',
-    margin: 50,
-    marginTop: 26,
-    padding: 10,
+    backgroundColor: 'white',
+    padding: 15,
+    borderBottomWidth: 1 / PixelRatio.get(),
+    borderBottomColor: '#CDCDCD',
   },
   buttonText: {
-    fontSize: 12,
-    textAlign: 'center',
+    fontSize: 17,
+    fontWeight: '500',
   },
   appContainer: {
     overflow: 'hidden',
     backgroundColor: '#dddddd',
     flex: 1,
   },
-  navBar: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 90,
-    flexDirection: 'row',
+  messageText: {
+    fontSize: 17,
+    fontWeight: '500',
+    padding: 15,
+    marginTop: 50,
+    marginLeft: 15,
   },
-  navButton: {
+  scene: {
     flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#EAEAEA',
   },
-  navButtonText: {
-    textAlign: 'center',
-    fontSize: 32,
-    marginTop: 25,
-  },
-  navButtonActive: {
-    color: 'green',
-  },
+  tabs: {
+    height: 50,
+  }
 });
 
 module.exports = JumpingNavSample;
--- Examples/UIExplorer/Navigator/NavigationBarSample.js
+++ Examples/UIExplorer/Navigator/NavigationBarSample.js
@@ -11,15 +11,30 @@
 
 var React = require('react-native');
 var {
+  PixelRatio,
   Navigator,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableHighlight,
+  TouchableOpacity,
   View,
 } = React;
 
 var cssVar = require('cssVar');
 
+class NavButton extends React.Component {
+  render() {
+    return (
+      <TouchableHighlight
+        style={styles.button}
+        underlayColor="#B5B5B5"
+        onPress={this.props.onPress}>
+        <Text style={styles.buttonText}>{this.props.text}</Text>
+      </TouchableHighlight>
+    );
+  }
+}
 
 var NavigationBarRouteMapper = {
 
@@ -30,26 +45,27 @@ var NavigationBarRouteMapper = {
 
     var previousRoute = navState.routeStack[index - 1];
     return (
-      <TouchableHighlight onPress={() => navigator.pop()}>
-        <View>
+      <TouchableOpacity
+        onPress={() => navigator.pop()}>
+        <View style={styles.navBarLeftButton}>
           <Text style={[styles.navBarText, styles.navBarButtonText]}>
             {previousRoute.title}
           </Text>
         </View>
-      </TouchableHighlight>
+      </TouchableOpacity>
     );
   },
 
   RightButton: function(route, navigator, index, navState) {
     return (
-      <TouchableHighlight
+      <TouchableOpacity
         onPress={() => navigator.push(newRandomRoute())}>
-        <View>
+        <View style={styles.navBarRightButton}>
           <Text style={[styles.navBarText, styles.navBarButtonText]}>
             Next
           </Text>
         </View>
-      </TouchableHighlight>
+      </TouchableOpacity>
     );
   },
 
@@ -65,8 +81,7 @@ var NavigationBarRouteMapper = {
 
 function newRandomRoute() {
   return {
-    content: 'Hello World!',
-    title: 'Random ' + Math.round(Math.random() * 100),
+    title: '#' + Math.ceil(Math.random() * 1000),
   };
 }
 
@@ -74,37 +89,63 @@ var NavigationBarSample = React.createClass({
 
   render: function() {
     return (
-      <View style={styles.appContainer}>
-        <Navigator
-          debugOverlay={false}
-          style={styles.appContainer}
-          initialRoute={newRandomRoute()}
-          renderScene={(route, navigator) => (
-            <View style={styles.scene}>
-              <Text>{route.content}</Text>
-            </View>
-          )}
-          navigationBar={
-            <Navigator.NavigationBar
-              navigationBarRouteMapper={NavigationBarRouteMapper}
+      <Navigator
+        debugOverlay={false}
+        style={styles.appContainer}
+        initialRoute={newRandomRoute()}
+        renderScene={(route, navigator) => (
+          <ScrollView style={styles.scene}>
+            <Text style={styles.messageText}>{route.content}</Text>
+            <NavButton
+              onPress={() => {
+                navigator.immediatelyResetRouteStack([
+                  newRandomRoute(),
+                  newRandomRoute(),
+                  newRandomRoute(),
+                ]);
+              }}
+              text="Reset w/ 3 scenes"
             />
-          }
-        />
-      </View>
+            <NavButton
+              onPress={() => {
+                this.props.navigator.pop();
+              }}
+              text="Exit NavigationBar Example"
+            />
+          </ScrollView>
+        )}
+        navigationBar={
+          <Navigator.NavigationBar
+            navigationBarRouteMapper={NavigationBarRouteMapper}
+            navigationBarStyles={styles.navBar}
+          />
+        }
+      />
     );
   },
 
 });
 
 var styles = StyleSheet.create({
-  appContainer: {
-    overflow: 'hidden',
-    backgroundColor: '#ffffff',
-    flex: 1,
+  messageText: {
+    fontSize: 17,
+    fontWeight: '500',
+    padding: 15,
+    marginTop: 50,
+    marginLeft: 15,
   },
-  scene: {
-    paddingTop: 50,
-    flex: 1,
+  button: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderBottomWidth: 1 / PixelRatio.get(),
+    borderBottomColor: '#CDCDCD',
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: '500',
+  },
+  navBar: {
+    backgroundColor: 'white',
   },
   navBarText: {
     fontSize: 16,
@@ -115,8 +156,19 @@ var styles = StyleSheet.create({
     fontWeight: '500',
     marginVertical: 9,
   },
+  navBarLeftButton: {
+    paddingLeft: 10,
+  },
+  navBarRightButton: {
+    paddingRight: 10,
+  },
   navBarButtonText: {
     color: cssVar('fbui-accent-blue'),
+  },
+  scene: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#EAEAEA',
   },
 });
 
