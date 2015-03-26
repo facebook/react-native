2d23c2fe1fc581fc6761e963e0ec96888438583f
Ben Alpert <balpert@fb.com>
2015-03-26 09:53:27 -0700

    Switch nav design

--- website/core/HeaderLinks.js
+++ website/core/HeaderLinks.js
@@ -12,28 +12,39 @@
 var React = require('React');
 
 var HeaderLinks = React.createClass({
-  links: [
-    {section: 'docs', href: '/react-native/docs/getting-started.html#content', text: 'docs'},
-    {section: 'support', href: '/react-native/support.html', text: 'support'},
-    {section: 'react', href: 'http://facebook.github.io/react', text: 'react'},
-    {section: 'github', href: 'http://github.com/facebook/react-native', text: 'github'},
+  linksInternal: [
+    {section: 'docs', href: '/react-native/docs/getting-started.html#content', text: 'Docs'},
+    {section: 'support', href: '/react-native/support.html', text: 'Support'},
   ],
+  linksExternal: [
+    {section: 'github', href: 'http://github.com/facebook/react-native', text: 'GitHub'},
+    {section: 'react', href: 'http://facebook.github.io/react', text: 'React'},
+  ],
+
+  makeLinks: function(links) {
+    return links.map(function(link) {
+      return (
+        <li key={link.section}>
+          <a
+            href={link.href}
+            className={link.section === this.props.section ? 'active' : ''}>
+            {link.text}
+          </a>
+        </li>
+      );
+    }, this)
+  },
 
   render: function() {
     return (
-      <ul className="nav-site">
-        {this.links.map(function(link) {
-          return (
-            <li key={link.section}>
-              <a
-                href={link.href}
-                className={link.section === this.props.section ? 'active' : ''}>
-                {link.text}
-              </a>
-            </li>
-          );
-        }, this)}
-      </ul>
+      <div className="nav-site-wrapper">
+        <ul className="nav-site nav-site-internal">
+          {this.makeLinks(this.linksInternal)}
+        </ul>
+        <ul className="nav-site nav-site-external">
+          {this.makeLinks(this.linksExternal)}
+        </ul>
+      </div>
     );
   }
 });
