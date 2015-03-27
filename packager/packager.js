1d1a844f0094a498e6b4c67d8e2b99cbfc116277
Justin Carmony <justin@justincarmony.com>
2015-03-27 10:51:43 -0600

    Changing from /packager-status to /status, formatting fixes

--- packager/packager.js
+++ packager/packager.js
@@ -153,8 +153,8 @@ function getDevToolsLauncher(options) {
 // A status page so the React/project.pbxproj build script
 // can verify that packager is running on 8081 and not 
 // another program / service.
-function statusPageMiddleware(req, res, next){
-  if(req.url == '/packager-status')
+function statusPageMiddleware (req, res, next) {
+  if (req.url == '/status')
   {
     res.end('packager-status:running');
   }
