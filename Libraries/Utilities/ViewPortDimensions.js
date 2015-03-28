9f9c9760595e76f5dc98ca3bf955f052f0a7bdfa
Peter Janak <pjanak@nhl.com>
2015-03-28 19:45:43 -0400

    expanding documentation to clarify pixel ratio all ready being accounted for

--- Libraries/Utilities/ViewPortDimensions.js
+++ Libraries/Utilities/ViewPortDimensions.js
@@ -17,7 +17,13 @@ var Dimensions = require('Dimensions');
  * ViewPortDimensions class gives access to the viewport's height and width.
  *
  * This could be used if someone wanted an image which stretched the width of
- * the device
+ * the device.
+ *
+ * Note: The values returned here are all ready in terms of the pixel ratio of
+ * the device. For example, the iPhone 6 renders 750x1334 pixels. However, it has
+ * a pixel ratio of two. Therefore the return values of these functions will
+ * all ready be modified by the pixel ratio. In the iPhone 6's case this means
+ * that the resolution that gets returned will be 375x667.
  */
 class ViewPortDimensions {
   /**
