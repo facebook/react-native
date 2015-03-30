c750e8fc91ae7182552db22a07cde04d320f3678
Pilwon Huh <pilwon@gmail.com>
2015-03-30 18:14:22 -0400

    Fixed DependencyResolver require statement regex to properly match and replace code.

--- packager/react-packager/src/DependencyResolver/haste/index.js
+++ packager/react-packager/src/DependencyResolver/haste/index.js
@@ -25,7 +25,7 @@ var DEFINE_MODULE_CODE = [
 ].join('');
 
 var DEFINE_MODULE_REPLACE_RE = /_moduleName_|_code_|_deps_/g;
-var REL_REQUIRE_STMT = /require\(['"]([\.\/0-9A-Z_$\-]*)['"]\)/gi;
+var REL_REQUIRE_STMT = /(\brequire\s*\(\s*['"])([^'"]+)(['"]\s*\))/g;
 
 var validateOpts = declareOpts({
   projectRoots: {
@@ -146,10 +146,10 @@ HasteDependencyResolver.prototype.wrapModule = function(module, code) {
   }
 
   var relativizedCode =
-    code.replace(REL_REQUIRE_STMT, function(codeMatch, depName) {
+    code.replace(REL_REQUIRE_STMT, function(codeMatch, pre, depName, post) {
       var depId = resolvedDeps[depName];
       if (depId != null) {
-        return 'require(\'' + depId + '\')';
+        return pre + depId + post;
       } else {
         return codeMatch;
       }
