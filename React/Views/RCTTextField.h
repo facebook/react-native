730abad5ed85c3846b3d67a3e6d6270d0b02a6b0
Alexey Kureev <a.kureev@xs2theworld.com>
2015-03-31 10:34:55 +0200

    Text fields now support separated side border customization

--- React/Views/RCTTextField.h
+++ React/Views/RCTTextField.h
@@ -17,6 +17,28 @@
 @property (nonatomic, assign) BOOL autoCorrect;
 @property (nonatomic, assign) UIEdgeInsets paddingEdgeInsets; // TODO: contentInset
 
+/**
+ * Border colors.
+ */
+@property (nonatomic, assign) CGColorRef borderTopColor;
+@property (nonatomic, assign) CGColorRef borderRightColor;
+@property (nonatomic, assign) CGColorRef borderBottomColor;
+@property (nonatomic, assign) CGColorRef borderLeftColor;
+@property (nonatomic, assign) CGColorRef borderColor;
+
+/**
+ * Border widths.
+ */
+@property (nonatomic, assign) CGFloat borderTopWidth;
+@property (nonatomic, assign) CGFloat borderRightWidth;
+@property (nonatomic, assign) CGFloat borderBottomWidth;
+@property (nonatomic, assign) CGFloat borderLeftWidth;
+@property (nonatomic, assign) CGFloat borderWidth;
+
+
+@property (nonatomic, assign) CGColorRef highlightedColor;
+
+
 - (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
 
 @end
