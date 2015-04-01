0ca3136371a659c5cfc9d3e33de53bda6179a647
daviskoh <koh.davis.0@gmail.com>
2015-03-31 17:54:38 -0700

    Bugfix/require module regexp
    Summary:
    Resolves https://github.com/facebook/react-native/issues/316. Also updated the spec for the Haste Dependency Resolver. Not sure if these changes are the ones desired so feedback would be welcome!
    Closes https://github.com/facebook/react-native/pull/368
    Github Author: daviskoh <koh.davis.0@gmail.com>
    
    Test Plan: ./runJestTests

--- packager/react-packager/src/DependencyResolver/haste/DependencyGraph/__tests__/DependencyGraph-test.js
+++ packager/react-packager/src/DependencyResolver/haste/DependencyGraph/__tests__/DependencyGraph-test.js
@@ -14,6 +14,7 @@ jest
   .dontMock('path')
   .dontMock('absolute-path')
   .dontMock('../docblock')
+  .dontMock('../../requirePattern')
   .setMock('../../../ModuleDescriptor', function(data) {return data;});
 
 describe('DependencyGraph', function() {
