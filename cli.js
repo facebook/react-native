d5ba36b97495c30adeb17788cfc052cd2e11eef2
Alex Kotliarskyi <alex.frantic@gmail.com>
2015-04-07 12:17:52 -0700

    Simplify cli.js

--- cli.js
+++ cli.js
@@ -1,16 +1,7 @@
-
 /**
  * Copyright 2004-present Facebook. All Rights Reserved.
  */
 
 'use strict';
 
-var cli = require('./local-cli/cli.js');
-
-function run() {
-  cli.run();
-}
-
-module.exports = {
-  run: run
-};
+module.exports = require('./local-cli/cli.js');
