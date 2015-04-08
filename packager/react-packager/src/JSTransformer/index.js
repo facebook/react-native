3d2413a38a01d49d689a1678c460b3501e05df34
Amjad Masad <amasad@fb.com>
2015-04-08 14:28:44 -0700

    [react-packager] Don't depend on error.stack being available

--- packager/react-packager/src/JSTransformer/index.js
+++ packager/react-packager/src/JSTransformer/index.js
@@ -125,7 +125,7 @@ function formatError(err, filename, source) {
 function formatGenericError(err, filename) {
   var msg = 'TransformError: ' + filename + ': ' + err.message;
   var error = new TransformError();
-  var stack = err.stack.split('\n').slice(0, -1);
+  var stack = (err.stack || '').split('\n').slice(0, -1);
   stack.push(msg);
   error.stack = stack.join('\n');
   error.message = msg;
