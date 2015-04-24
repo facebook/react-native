53244b9eb6e426a53b307b9e2c8a664e68fcb2bd
Christopher Chedeau <vjeuxx@gmail.com>
2015-04-24 11:06:44 -0700

    Update Image.ios.js

--- Libraries/Image/Image.ios.js
+++ Libraries/Image/Image.ios.js
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * @providesModule Image
+ * @providesModule Image2
  * @flow
  */
 'use strict';
