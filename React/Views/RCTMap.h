5adcf091d6b6041e09eecd6724dc536bc9ac5003
David Mohl <me@dave.cx>
2015-06-18 00:13:50 +0900

    Updated `setAnnotations` to delta update annotations rather than completely re-setting them

--- React/Views/RCTMap.h
+++ React/Views/RCTMap.h
@@ -26,6 +26,7 @@ extern const CGFloat RCTMapZoomBoundBuffer;
 @property (nonatomic, assign) CGFloat maxDelta;
 @property (nonatomic, assign) UIEdgeInsets legalLabelInsets;
 @property (nonatomic, strong) NSTimer *regionChangeObserveTimer;
+@property (nonatomic, strong) NSMutableArray *annotationIds;
 
 - (void)setAnnotations:(RCTPointAnnotationArray *)annotations;
 
