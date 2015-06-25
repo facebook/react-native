64d7933e667c77811a6715de219c76052ef817a2
Alex Kotliarskyi <frantic@fb.com>
2015-06-25 09:01:17 -0700

    [ReactNative] Ignore _UILayoutGuide in tests

--- Libraries/RCTTest/RCTTestRunner.m
+++ Libraries/RCTTest/RCTTestRunner.m
@@ -99,7 +99,14 @@ RCT_NOT_IMPLEMENTED(-init)
     error = [[RCTRedBox sharedInstance] currentErrorMessage];
   }
   [rootView removeFromSuperview];
-  RCTAssert(vc.view.subviews.count == 0, @"There shouldn't be any other views: %@", vc.view);
+
+
+  NSArray *nonLayoutSubviews = [vc.view.subviews filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(id subview, NSDictionary *bindings) {
+    return ![NSStringFromClass([subview class]) isEqualToString:@"_UILayoutGuide"];
+  }]];
+  RCTAssert(nonLayoutSubviews.count == 0, @"There shouldn't be any other views: %@", nonLayoutSubviews);
+
+
   vc.view = nil;
   [[RCTRedBox sharedInstance] dismiss];
   if (expectErrorBlock) {
