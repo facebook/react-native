81ad713f5f4353ccb803d583e08bf0520d502e93
Nick Lockwood <nicklockwood@fb.com>
2015-07-16 09:34:28 -0700

    Added Gzip support
    Summary:
    Added Gzip function to RCTUtils. This uses dlopen to load the zlib library at runtime so there's no need to link it into your project.
    
    The main reason for this feature is to support gzipping of HTTP request bodies. Now, if you add 'Content-Encoding:gzip' to your request headers when using XMLHttpRequest, your request body will be automatically gzipped on the native side before sending.
    
    (Note: Gzip decoding of *response* bodies is handled automatically by iOS, and was already available).

--- React/Base/RCTUtils.m
+++ React/Base/RCTUtils.m
@@ -10,11 +10,14 @@
 #import "RCTUtils.h"
 
 #import <mach/mach_time.h>
-#import <objc/runtime.h>
+#import <objc/message.h>
 
 #import <UIKit/UIKit.h>
 
 #import <CommonCrypto/CommonCrypto.h>
+
+#import <zlib.h>
+#import <dlfcn.h>
 
 #import "RCTLog.h"
 
@@ -305,3 +308,51 @@ NSURL *RCTDataURL(NSString *mimeType, NSData *data)
            [data base64EncodedStringWithOptions:(NSDataBase64EncodingOptions)0]]];
 }
 
+static BOOL RCTIsGzippedData(NSData *data)
+{
+  UInt8 *bytes = (UInt8 *)data.bytes;
+  return (data.length >= 2 && bytes[0] == 0x1f && bytes[1] == 0x8b);
+}
+
+NSData *RCTGzipData(NSData *input, float level)
+{
+  if (input.length == 0 || RCTIsGzippedData(input)) {
+    return input;
+  }
+
+  void *libz = dlopen("libz.dylib", RTLD_NOW);
+  int (*deflateInit2_)(z_streamp, int, int, int, int, int, const char *, int) = dlsym(libz, "deflateInit2_");
+  int (*deflate)(z_streamp, int) = dlsym(libz, "deflate");
+  int (*deflateEnd)(z_streamp) = dlsym(libz, "deflateEnd");
+
+  z_stream stream;
+  stream.zalloc = Z_NULL;
+  stream.zfree = Z_NULL;
+  stream.opaque = Z_NULL;
+  stream.avail_in = (uint)input.length;
+  stream.next_in = (Bytef *)input.bytes;
+  stream.total_out = 0;
+  stream.avail_out = 0;
+
+  static const NSUInteger RCTGZipChunkSize = 16384;
+
+  NSMutableData *output = nil;
+  int compression = (level < 0.0f)? Z_DEFAULT_COMPRESSION: (int)(roundf(level * 9));
+  if (deflateInit2(&stream, compression, Z_DEFLATED, 31, 8, Z_DEFAULT_STRATEGY) == Z_OK) {
+    output = [NSMutableData dataWithLength:RCTGZipChunkSize];
+    while (stream.avail_out == 0) {
+      if (stream.total_out >= output.length) {
+        output.length += RCTGZipChunkSize;
+      }
+      stream.next_out = (uint8_t *)output.mutableBytes + stream.total_out;
+      stream.avail_out = (uInt)(output.length - stream.total_out);
+      deflate(&stream, Z_FINISH);
+    }
+    deflateEnd(&stream);
+    output.length = stream.total_out;
+  }
+
+  dlclose(libz);
+
+  return output;
+}
