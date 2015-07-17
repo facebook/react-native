2771fc4c5294d1a88d9d1b414fadbc462421b70d
Christopher Chedeau <vjeuxx@gmail.com>
2015-07-16 19:41:22 -0700

    Add link for releases

--- website/core/HeaderLinks.js
+++ website/core/HeaderLinks.js
@@ -15,6 +15,7 @@ var HeaderLinks = React.createClass({
   linksInternal: [
     {section: 'docs', href: '/react-native/docs/getting-started.html', text: 'Docs'},
     {section: 'support', href: '/react-native/support.html', text: 'Support'},
+    {section: 'releases', href: 'https://github.com/facebook/react-native/releases', text: 'Releases'},
   ],
   linksExternal: [
     {section: 'github', href: 'https://github.com/facebook/react-native', text: 'GitHub'},
