92c32d622f13c79667a094db913df301c466aab8
Harrison Harnisch <hharnisc@gmail.com>
2015-08-05 10:31:52 -0700

    use ternary cleanup whitespace

--- React/Base/RCTProfile.m
+++ React/Base/RCTProfile.m
@@ -88,24 +88,17 @@ NSDictionary *RCTProfileGetMemoryUsage(BOOL raw)
                                  (task_info_t)&info,
                                  &size);
   if( kerr == KERN_SUCCESS ) {
-    if (raw) {
-      return @{
-               @"suspend_count": @(info.suspend_count),
-               @"virtual_size": @(info.virtual_size),
-               @"resident_size": @(info.resident_size),
-               };
-    } else {
-      return @{
-               @"suspend_count": @(info.suspend_count),
-               @"virtual_size": RCTProfileMemory(info.virtual_size),
-               @"resident_size": RCTProfileMemory(info.resident_size),
-               };
-    }
-
+    vm_size_t vs = info.virtual_size;
+    vm_size_t rs = info.resident_size;
+    return @{
+      @"suspend_count": @(info.suspend_count),
+      @"virtual_size": raw ? @(vs) : RCTProfileMemory(vs),
+      @"resident_size": raw ? @(rs) : RCTProfileMemory(rs),
+    };
   } else {
     return @{};
   }
-  
+
 }
 
 NSNumber *RCTProfileGetCPUUsage(void)
@@ -113,32 +106,32 @@ NSNumber *RCTProfileGetCPUUsage(void)
   kern_return_t kr;
   task_info_data_t tinfo;
   mach_msg_type_number_t task_info_count;
-  
+
   task_info_count = TASK_INFO_MAX;
   kr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)tinfo, &task_info_count);
   if (kr != KERN_SUCCESS) {
     return nil;
   }
-  
+
   thread_array_t thread_list;
   mach_msg_type_number_t thread_count;
-  
+
   thread_info_data_t thinfo;
   mach_msg_type_number_t thread_info_count;
-  
+
   thread_basic_info_t basic_info_th;
-  
+
   // get threads in the task
   kr = task_threads(mach_task_self(), &thread_list, &thread_count);
   if (kr != KERN_SUCCESS) {
     return nil;
   }
-  
+
   long tot_sec = 0;
   long tot_usec = 0;
   float tot_cpu = 0;
   unsigned j;
-  
+
   for (j = 0; j < thread_count; j++) {
     thread_info_count = THREAD_INFO_MAX;
     kr = thread_info(thread_list[j], THREAD_BASIC_INFO,
@@ -146,24 +139,24 @@ NSNumber *RCTProfileGetCPUUsage(void)
     if (kr != KERN_SUCCESS) {
       return nil;
     }
-    
+
     basic_info_th = (thread_basic_info_t)thinfo;
-    
+
     if (!(basic_info_th->flags & TH_FLAGS_IDLE)) {
       tot_sec = tot_sec + basic_info_th->user_time.seconds + basic_info_th->system_time.seconds;
       tot_usec = tot_usec + basic_info_th->system_time.microseconds + basic_info_th->system_time.microseconds;
       tot_cpu = tot_cpu + basic_info_th->cpu_usage / (float)TH_USAGE_SCALE * 100.0;
     }
-    
+
   } // for each thread
-  
+
   kr = vm_deallocate(mach_task_self(), (vm_offset_t)thread_list, thread_count * sizeof(thread_t));
-  
+
   if( kr == KERN_SUCCESS ) {
     return [NSNumber numberWithFloat:tot_cpu];
   } else {
     return nil;
-    
+
   }
 }
 
