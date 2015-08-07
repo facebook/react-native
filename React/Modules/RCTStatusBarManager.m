6666395254f133a086558c591b9f38b76f678849
Yusef Napora <yusef@napora.org>
2015-08-07 19:23:07 -0400

    Use RCTSharedApplication() in RCTStatusBarManager

--- React/Modules/RCTStatusBarManager.m
+++ React/Modules/RCTStatusBarManager.m
@@ -117,7 +117,7 @@ RCT_EXPORT_METHOD(setHidden:(BOOL)hidden
 
 RCT_EXPORT_METHOD(setNetworkActivityIndicatorVisible:(BOOL)visible)
 {
-  [UIApplication sharedApplication].networkActivityIndicatorVisible = visible;
+  RCTSharedApplication().networkActivityIndicatorVisible = visible;
 }
 
 @end
