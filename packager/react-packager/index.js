6debfce3744678e9331e0b497a4818966320604f
Amjad Masad <amasad@fb.com>
2015-08-20 12:57:05 -0700

    [react-native] Update graceful-fs and use it in _build_bundle.js

--- packager/react-packager/index.js
+++ packager/react-packager/index.js
@@ -58,14 +58,7 @@ exports.getDependencies = function(options, main) {
 function useGracefulFs() {
   var fs = require('fs');
   var gracefulFs = require('graceful-fs');
-
-  // A bit sneaky but it's not straightforward to update all the
-  // modules we depend on.
-  Object.keys(fs).forEach(function(method) {
-    if (typeof fs[method] === 'function' && gracefulFs[method]) {
-      fs[method] = gracefulFs[method];
-    }
-  });
+  gracefulFs.gracefulify(fs);
 }
 
 function createServer(options) {
