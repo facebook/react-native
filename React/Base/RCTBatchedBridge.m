e46ad930b61d62ccf5d0c3eb0bd09e863831515b
Param Aggarwal <paramaggarwal@gmail.com>
2015-08-22 03:37:41 +0530

    Selectively expose modules to a bridge

--- React/Base/RCTBatchedBridge.m
+++ React/Base/RCTBatchedBridge.m
@@ -238,6 +238,13 @@ id<RCTJavaScriptExecutor> RCTGetLatestExecutor(void)
      // Check if module instance has already been registered for this name
      id<RCTBridgeModule> module = modulesByName[moduleName];
 
+     if ([moduleClass respondsToSelector:@selector(allowedBridgeIDs)]) {
+       NSString *bridgeID = self.parentBridge.launchOptions[@"bridgeID"];
+       if (![[moduleClass allowedBridgeIDs] containsObject:bridgeID]) {
+         continue;
+       }
+     }
+      
      if (module) {
        // Preregistered instances takes precedence, no questions asked
        if (!preregisteredModules[moduleName]) {
