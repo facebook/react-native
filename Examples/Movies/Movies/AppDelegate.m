bb708769d4cd38752626f3b470d877024621eed3
Martin Konicek <mkonicek@fb.com>
2015-08-24 12:31:23 -0700

    [ReactNative] Fix sample apps

--- Examples/Movies/Movies/AppDelegate.m
+++ Examples/Movies/Movies/AppDelegate.m
@@ -53,6 +53,7 @@
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"MoviesApp"
+                                               initialProperties:nil
                                                    launchOptions:launchOptions];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
