88add08c2eb990171b1eec330c4f269335cdf774
Kyle Corbitt <kyle@corbt.com>
2015-09-17 12:09:19 +0100

    handle back button press in JS

--- local-cli/generator-android/templates/package/MainActivity.java
+++ local-cli/generator-android/templates/package/MainActivity.java
@@ -45,6 +45,15 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
     }
 
     @Override
+    public void onBackPressed() {
+      if (mReactInstanceManager != null) {
+        mReactInstanceManager.onBackPressed();
+      } else {
+        super.onBackPressed();
+      }
+    }
+
+    @Override
     public void invokeDefaultOnBackPressed() {
       super.onBackPressed();
     }
