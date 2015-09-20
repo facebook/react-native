8c82cef16e8f220e1ed934b3992a219c981e7e75
Sudhir Jonathan <sudhir.j@gmail.com>
2015-09-20 23:21:59 +0530

    Allow web as a valid build platform, fixes #2895
    There seems to be no reason why the packager can't be used with `web` as well.

--- packager/react-packager/src/DependencyResolver/DependencyGraph/index.js
+++ packager/react-packager/src/DependencyResolver/DependencyGraph/index.js
@@ -60,7 +60,7 @@ const validateOpts = declareOpts({
   },
   platforms: {
     type: 'array',
-    default: ['ios', 'android'],
+    default: ['ios', 'android', 'web'],
   },
   cache: {
     type: 'object',
