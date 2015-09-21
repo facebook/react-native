b600c1aa76a9c21dfdc474dec9c2dce1c699c45d
Mehdi Houshmand <med1985@gmail.com>
2015-09-21 20:06:21 +0100

    Added to docs re: creation of Android view

--- website/src/react-native/index.js
+++ website/src/react-native/index.js
@@ -355,6 +355,110 @@ public class MyCustomViewManager extends SimpleViewManager<MyCustomView> {
 }
 `}
           </Prism>
+        <Prism>
+{`// Java
+
+public class MyReactPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+        return Arrays.asList(new ViewManager[]{new MyCustomViewManager()});
+    }
+}
+`}
+        </Prism>
+        <Prism>
+{`// Java
+
+package com.react;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+
+    private ReactInstanceManager mReactInstanceManager;
+    private ReactRootView mReactRootView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    mReactRootView = new ReactRootView(this);
+    FacebookSdk.sdkInitialize(getApplicationContext());
+
+    mReactInstanceManager = ReactInstanceManager.builder()
+        .setApplication(getApplication())
+        .setBundleAssetName("index.android.bundle")
+        .setJSMainModuleName("index.android")
+        .addPackage(new MyReactPackage())
+        .addPackage(new MainReactPackage())
+        .setUseDeveloperSupport(BuildConfig.DEBUG)
+        .setInitialLifecycleState(LifecycleState.RESUMED)
+        .build();
+
+    mReactRootView.startReactApplication(mReactInstanceManager, "fomo", null);
+
+    setContentView(mReactRootView);
+}
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager.showDevOptionsDialog();
+            return true;
+        }
+        if (keyCode == KeyEvent.KEYCODE_B) {
+            String[] stringArray = getResources().getStringArray(R.array.facebook_perms);
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList(stringArray));
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onResume(this);
+        }
+    }
+}
+`}
+    </Prism>
           <Prism>
 {`// JavaScript
 
