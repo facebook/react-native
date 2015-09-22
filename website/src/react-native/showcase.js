89c11beb251a5ca0fb6f665fe8ec60b48e07fe22
Dmitry <unknownliveid@hotmail.com>
2015-09-22 19:00:51 +0500

    Add Hashley app to Showcase

--- website/src/react-native/showcase.js
+++ website/src/react-native/showcase.js
@@ -73,6 +73,12 @@ var apps = [
     author: 'Liubomyr Mykhalchenko (@liubko)',
   },
   {
+    name: 'Hashley',
+    icon: 'http://a2.mzstatic.com/us/r30/Purple4/v4/5f/19/fc/5f19fc13-e7af-cd6b-6749-cedabdaeee7d/icon350x350.png',
+    link: 'https://itunes.apple.com/us/app/hashtag-by-hashley-ironic/id1022724462?mt=8',
+    author: 'Elephant, LLC',
+  },
+  {
     name: 'HSK Level 1 Chinese Flashcards',
     icon: 'http://is2.mzstatic.com/image/pf/us/r30/Purple1/v4/b2/4f/3a/b24f3ae3-2597-cc70-1040-731b425a5904/mzl.amxdcktl.jpg',
     link: 'https://itunes.apple.com/us/app/hsk-level-1-chinese-flashcards/id936639994',
