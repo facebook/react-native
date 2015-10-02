e2cfbcb13acb312ec4acf74612f9ed7e0b0d0d91
Dave Miller <dmmiller@fb.com>
2015-10-02 12:13:48 -0700

    Introduce SnapshotView which wraps Renderable content and will verify the snapshot
    Reviewed By: @javache
    
    Differential Revision: D2493722

---
+++ Libraries/RCTTest/SnapshotView.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule SnapshotView
+ * @flow
+ */
+'use strict';
+
+var React = require('React');
+var StyleSheet = require('StyleSheet');
+var { TestModule } = require('NativeModules');
+
+var requireNativeComponent = require('requireNativeComponent');
+
+var SnapshotView = React.createClass({
+  onDefaultAction: function(event: Object) {
+    TestModule.verifySnapshot(TestModule.markTestPassed);
+  },
+
+  render: function() {
+    var testIdentifier = this.props.testIdentifier || 'test';
+    var onSnapshotReady = this.props.onSnapshotReady || this.onDefaultAction;
+    return (
+      <RCTSnapshot
+        style={style.snapshot}
+        {...this.props}
+        onSnapshotReady={onSnapshotReady}
+        testIdentifier={testIdentifier}
+      />
+    );
+  },
+
+  propTypes: {
+    // A callback when the Snapshot view is ready to be compared
+    onSnapshotReady : React.PropTypes.func,
+    // A name to identify the individual instance to the SnapshotView
+    testIdentifier : React.PropTypes.string,
+  }
+});
+
+var style = StyleSheet.create({
+  snapshot: {
+    flex: 1,
+  },
+});
+
+var RCTSnapshot = requireNativeComponent('RCTSnapshot', SnapshotView);
+
+module.exports = SnapshotView;
