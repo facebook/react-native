62dc60ca6a1b5d067dda0e330c5e777165e62f2a
Felix Oghină <foghina@fb.com>
2015-10-02 14:44:20 +0100

    [cli] make init backwards-compatible

--- local-cli/init.js
+++ local-cli/init.js
@@ -3,10 +3,11 @@
 var path = require('path');
 var yeoman = require('yeoman-environment');
 
-function init(projectDir, args) {
+function init(projectDir, argsOrName) {
   console.log('Setting up new React Native app in ' + projectDir);
   var env = yeoman.createEnv();
   env.register(require.resolve(path.join(__dirname, 'generator')), 'react:app');
+  var args = Array.isArray(argsOrName) ? argsOrName : [argsOrName].concat(process.args.slice(4));
   var generator = env.create('react:app', {args: args});
   generator.destinationRoot(projectDir);
   generator.run();
