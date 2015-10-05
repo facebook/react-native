58251cd14c92235dfb0d797411464d5c3016048c
Dral <m.c.dral@gmail.com>
2015-10-05 03:59:02 -0700

    Silently (warning) fail when <Image> source has empty uri
    Summary: fixes #3127
    Closes https://github.com/facebook/react-native/pull/3185
    
    Reviewed By: @​svcscm
    
    Differential Revision: D2507588
    
    Pulled By: @nicklockwood

--- Libraries/Image/Image.ios.js
+++ Libraries/Image/Image.ios.js
@@ -19,6 +19,7 @@ var NativeModules = require('NativeModules');
 var PropTypes = require('ReactPropTypes');
 var React = require('React');
 var ReactNativeViewAttributes = require('ReactNativeViewAttributes');
+var View = require('View');
 var StyleSheet = require('StyleSheet');
 var StyleSheetPropType = require('StyleSheetPropType');
 
@@ -164,6 +165,11 @@ var Image = React.createClass({
 
     var {width, height} = source;
     var style = flattenStyle([{width, height}, styles.base, this.props.style]) || {};
+
+    if (source.uri === '') {
+      console.warn('source.uri should not be an empty string');
+      return <View {...this.props} style={style} />;
+    }
 
     var isNetwork = source.uri && source.uri.match(/^https?:/);
     var RawImage = isNetwork ? RCTNetworkImageView : RCTImageView;
