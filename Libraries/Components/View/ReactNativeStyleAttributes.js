40c40050491101540de07689b4051a049dfed9bf
Sebastian Markbage <sema@fb.com>
2015-10-05 20:21:50 -0700

    Refactor Attribute Processing (Step 3)
    Summary: Decouple processStyle from the main reconciliation. It is now a process
    extension to the style attribute `transform`. This effectively decouples a
    large portion of special cases and helper dependencies from the reconciler.
    
    The transform attribute becomes translated into the transformMatrix attribute on
    the native side so this becomes a little weird in that I have to special case
    it. I don't think it is worth while having a general solution for this so I
    intend to rename the native attribute to `transform` and just have it accept the
    resolved transform. Then I can remove the special cases.
    
    The next step is generalizing the flattenStyle function and optimizing it.
    
    @​public
    
    Reviewed By: @vjeux
    
    Differential Revision: D2460465
    
    fb-gh-sync-id: 243e7fd77d282b401bc2c028aec8d57f24522a8e

--- Libraries/Components/View/ReactNativeStyleAttributes.js
+++ Libraries/Components/View/ReactNativeStyleAttributes.js
@@ -19,6 +19,7 @@ var ViewStylePropTypes = require('ViewStylePropTypes');
 var keyMirror = require('keyMirror');
 var matricesDiffer = require('matricesDiffer');
 var processColor = require('processColor');
+var processTransform = require('processTransform');
 var sizesDiffer = require('sizesDiffer');
 
 var ReactNativeStyleAttributes = {
@@ -27,6 +28,7 @@ var ReactNativeStyleAttributes = {
   ...keyMirror(ImageStylePropTypes),
 };
 
+ReactNativeStyleAttributes.transform = { process: processTransform };
 ReactNativeStyleAttributes.transformMatrix = { diff: matricesDiffer };
 ReactNativeStyleAttributes.shadowOffset = { diff: sizesDiffer };
 
