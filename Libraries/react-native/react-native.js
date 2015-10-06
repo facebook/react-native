569685b4d98bb2aa758a256a12db4c95fc230787
Spencer Ahrens <sahrens@fb.com>
2015-10-06 09:24:19 -0700

    introduce universal switch
    Reviewed By: @vjeux, @mkonicek
    
    Differential Revision: D2441456
    
    fb-gh-sync-id: c4050ec86f30234ea29c38205d668157bde65a35

--- Libraries/react-native/react-native.js
+++ Libraries/react-native/react-native.js
@@ -35,6 +35,7 @@ var ReactNative = Object.assign(Object.create(require('React')), {
   SegmentedControlIOS: require('SegmentedControlIOS'),
   SliderIOS: require('SliderIOS'),
   SnapshotView: require('SnapshotView'),
+  Switch: require('Switch'),
   SwitchAndroid: require('SwitchAndroid'),
   SwitchIOS: require('SwitchIOS'),
   TabBarIOS: require('TabBarIOS'),
