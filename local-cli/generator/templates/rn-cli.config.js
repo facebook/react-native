09b446d8dd2f1e9d0b298fdcd3d178991bbe1db4
Martín Bigio <martinb@fb.com>
2015-10-16 13:25:14 -0700

    Allow user to configure transformer through cl argument
    Reviewed By: frantic
    
    Differential Revision: D2547435
    
    fb-gh-sync-id: 77016ee5beba854a4cb58e9ac04b91a7dd76cf3e

--- local-cli/generator/templates/rn-cli.config.js
+++ local-cli/generator/templates/rn-cli.config.js
@@ -14,10 +14,6 @@ var config = {
 
   getBlacklistRE(platform) {
     return blacklist(platform);
-  },
-
-  getTransformModulePath() {
-    return require.resolve('./node_modules/react-native/packager/transformer');
   }
 };
 
