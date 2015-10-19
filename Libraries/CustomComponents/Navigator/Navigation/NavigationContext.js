b1f1e8aeceb9a86c600ffd137348565dad4b559a
Hedger Wang <hedger@fb.com>
2015-10-19 10:03:04 -0700

    supports event capturing and bubbling phases in Navigation context.
    Summary: Adds the API that enables the navigation events capturing and bubbling which is the feature
    that is enabled if the nested navigation contexts is created by the navigator.
    
    This would allow developer to observe or reconcile navigation events within the navigation tree.
    
    public
    
    ./Libraries/FBReactKit/jest
    
    Reviewed By: zjj010104
    
    Differential Revision: D2546451
    
    fb-gh-sync-id: dfc9d16defaa563b9e80fd751a20570f6e524b74

--- Libraries/CustomComponents/Navigator/Navigation/NavigationContext.js
+++ Libraries/CustomComponents/Navigator/Navigation/NavigationContext.js
@@ -26,29 +26,42 @@
  */
 'use strict';
 
+var NavigationEvent = require('NavigationEvent');
 var NavigationEventEmitter = require('NavigationEventEmitter');
 var NavigationTreeNode = require('NavigationTreeNode');
 
 var emptyFunction = require('emptyFunction');
 var invariant = require('invariant');
 
-import type * as NavigationEvent from 'NavigationEvent';
 import type * as EventSubscription from 'EventSubscription';
+
+var {
+  AT_TARGET,
+  BUBBLING_PHASE,
+  CAPTURING_PHASE,
+} = NavigationEvent;
 
 /**
  * Class that contains the info and methods for app navigation.
  */
 class NavigationContext {
   __node: NavigationTreeNode;
-  _eventEmitter: ?NavigationEventEmitter;
+  _bubbleEventEmitter: ?NavigationEventEmitter;
+  _captureEventEmitter: ?NavigationEventEmitter;
   _currentRoute: any;
+  _emitCounter: number;
+  _emitQueue: Array<any>;
 
   constructor() {
-    this._eventEmitter = new NavigationEventEmitter(this);
+    this._bubbleEventEmitter = new NavigationEventEmitter(this);
+    this._captureEventEmitter = new NavigationEventEmitter(this);
     this._currentRoute = null;
 
     // Sets the protected property `__node`.
     this.__node = new NavigationTreeNode(this);
+
+    this._emitCounter = 0;
+    this._emitQueue = [];
 
     this.addListener('willfocus', this._onFocus, this);
     this.addListener('didfocus', this._onFocus, this);
@@ -72,9 +85,12 @@ class NavigationContext {
   addListener(
     eventType: string,
     listener: Function,
-    context: ?Object
+    context: ?Object,
+    useCapture: ?boolean
   ): EventSubscription {
-    var emitter = this._eventEmitter;
+    var emitter = useCapture ?
+      this._captureEventEmitter :
+      this._bubbleEventEmitter;
     if (emitter) {
       return emitter.addListener(eventType, listener, context);
     } else {
@@ -83,19 +99,115 @@ class NavigationContext {
   }
 
   emit(eventType: String, data: any, didEmitCallback: ?Function): void {
-    var emitter = this._eventEmitter;
-    if (emitter) {
-      emitter.emit(eventType, data, didEmitCallback);
+    if (this._emitCounter > 0) {
+      // An event cycle that was previously created hasn't finished yet.
+      // Put this event cycle into the queue and will finish them later.
+      var args: any = Array.prototype.slice.call(arguments);
+      this._emitQueue.push(args);
+      return;
+    }
+
+    this._emitCounter++;
+
+    var targets = [this];
+    var parentTarget = this.parent;
+    while (parentTarget) {
+      targets.unshift(parentTarget);
+      parentTarget = parentTarget.parent;
+    }
+
+    var propagationStopped = false;
+    var defaultPrevented = false;
+    var callback = (event) => {
+      propagationStopped = propagationStopped || event.isPropagationStopped();
+      defaultPrevented = defaultPrevented || event.defaultPrevented;
+    };
+
+    // capture phase
+    targets.some((currentTarget) => {
+      if (propagationStopped) {
+        return true;
+      }
+
+      var extraInfo = {
+        defaultPrevented,
+        eventPhase: CAPTURING_PHASE,
+        propagationStopped,
+        target: this,
+      };
+
+      currentTarget.__emit(eventType, data, callback, extraInfo);
+    }, this);
+
+    // bubble phase
+    targets.reverse().some((currentTarget) => {
+      if (propagationStopped) {
+        return true;
+      }
+      var extraInfo = {
+        defaultPrevented,
+        eventPhase: BUBBLING_PHASE,
+        propagationStopped,
+        target: this,
+      };
+      currentTarget.__emit(eventType, data, callback, extraInfo);
+    }, this);
+
+    if (didEmitCallback) {
+      var event = NavigationEvent.pool(eventType, this, data);
+      propagationStopped && event.stopPropagation();
+      defaultPrevented && event.preventDefault();
+      didEmitCallback.call(this, event);
+      event.dispose();
+    }
+
+    this._emitCounter--;
+    while (this._emitQueue.length) {
+      var args: any = this._emitQueue.shift();
+      this.emit.apply(this, args);
     }
   }
 
   dispose(): void {
-    var emitter = this._eventEmitter;
+    // clean up everything.
+    this._bubbleEventEmitter && this._bubbleEventEmitter.removeAllListeners();
+    this._captureEventEmitter && this._captureEventEmitter.removeAllListeners();
+    this._bubbleEventEmitter = null;
+    this._captureEventEmitter = null;
+    this._currentRoute = null;
+  }
+
+  // This method `__method` is protected.
+  __emit(
+    eventType: String,
+    data: any,
+    didEmitCallback: ?Function,
+    extraInfo: Object,
+  ): void {
+    var emitter;
+    switch (extraInfo.eventPhase) {
+      case CAPTURING_PHASE: // phase = 1
+        emitter = this._captureEventEmitter;
+        break;
+      case BUBBLING_PHASE: // phase = 3
+        emitter = this._bubbleEventEmitter;
+        break;
+      default:
+        invariant(false, 'invalid event phase %s', extraInfo.eventPhase);
+    }
+
+    if (extraInfo.target === this) {
+      // phase = 2
+      extraInfo.eventPhase = AT_TARGET;
+    }
+
     if (emitter) {
-      // clean up everything.
-      emitter.removeAllListeners();
-      this._eventEmitter = null;
-      this._currentRoute = null;
+      emitter.emit(
+        eventType,
+        data,
+        didEmitCallback,
+        extraInfo
+      );
     }
   }
 
