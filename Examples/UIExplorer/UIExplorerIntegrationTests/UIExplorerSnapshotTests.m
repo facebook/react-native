2560d4134a0d57ead2d7e77fccfe83c55f4fbfcf
Pieter De Baets <pieterdb@fb.com>
2015-10-23 09:32:50 -0700

    Fix configuration for OSS UIExplorer tests
    Reviewed By: nicklockwood
    
    Differential Revision: D2575356
    
    fb-gh-sync-id: 0ab5bd29aa3c17a8cb067a8a17abed3e178c5bcf

--- Examples/UIExplorer/UIExplorerIntegrationTests/UIExplorerSnapshotTests.m
+++ Examples/UIExplorer/UIExplorerIntegrationTests/UIExplorerSnapshotTests.m
@@ -37,7 +37,7 @@
 #endif
 
   NSOperatingSystemVersion version = [NSProcessInfo processInfo].operatingSystemVersion;
-  RCTAssert(version.majorVersion == 8 || version.minorVersion >= 3, @"Snapshot tests should be run on iOS 8.3+, found %zd.%zd.%zd", version.majorVersion, version.minorVersion, version.patchVersion);
+  RCTAssert((version.majorVersion == 8 && version.minorVersion >= 3) || version.majorVersion >= 9, @"Tests should be run on iOS 8.3+, found %zd.%zd.%zd", version.majorVersion, version.minorVersion, version.patchVersion);
   _runner = RCTInitRunnerForApp(@"Examples/UIExplorer/UIExplorerApp.ios", nil);
   _runner.recordMode = NO;
 }
