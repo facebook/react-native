77c3983717f738963576d48ec064669cd28d90ca
Mike Armstrong <mikea@fb.com>
2015-10-27 07:17:19 -0700

    Move PerformanceLoggerFlag to onDraw
    Differential Revision: D2585417
    
    fb-gh-sync-id: 342f7a05dc53cb0af0871b0dc8504dc48680cde4

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/ReactChoreographer.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/ReactChoreographer.java
@@ -26,15 +26,21 @@ import com.facebook.react.common.ReactConstants;
 public class ReactChoreographer {
 
   public static enum CallbackType {
+
+    /**
+     * For use by perf markers that need to happen immediately after draw
+     */
+    PERF_MARKERS(0),
+
     /**
      * For use by {@link com.facebook.react.uimanager.UIManagerModule}
      */
-    DISPATCH_UI(0),
+    DISPATCH_UI(1),
 
     /**
      * Events that make JS do things.
      */
-    TIMERS_EVENTS(1),
+    TIMERS_EVENTS(2),
     ;
 
     private final int mOrder;
