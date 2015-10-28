c2ff5e68322ef4d076015827525142577d6b431c
Matthew Arbesfeld <arbesfeld@gmail.com>
2015-10-28 04:56:26 -0700

    Allow developers to load JavaScript bundle from any source.
    Summary: This patch adds two pieces of functionality:
    
    - Exposes `JSBundleLoader` to allow a developer to load JavaScript bundles as they choose.
    - Adds `ReactBridge.loadScripFromFile` method which loads a JavaScript bundle from an arbitrary file path.
    
    Example usage:
    
    ```
    JSBundleLoader jsBundleLoader = new JSBundleLoader() {
        Override
        public void loadScript(ReactBridge reactBridge) {
            reactBridge.loadScriptFromFile("/sdcard/Download/index.android.bundle");
        }
    };
    
    mReactInstanceManager = ReactInstanceManager.builder()
            .setApplication(getApplication())
            .setJSBundleLoader(jsBundleLoader)
            .setJSMainModuleName("") /* necessary due to TODO(6803830) */
            .addPackage(new MainReactPackage())
            .setInitialLifecycleState(LifecycleState.RESUMED)
            .build();
    ```
    
    cc ide
    Closes https://github.com/facebook/react-native/pull/3189
    
    Reviewed By: svcscm
    
    Differential Revision: D2535819
    
    Pulled By: mkonicek
    
    fb-gh-sync-id: f319299dbe29bab3b7e91f94249c14b270d9fec3

--- ReactAndroid/src/main/jni/react/jni/OnLoad.cpp
+++ ReactAndroid/src/main/jni/react/jni/OnLoad.cpp
@@ -638,7 +638,7 @@ static void loadScriptFromAssets(JNIEnv* env, jobject obj, jobject assetManager,
   auto bridge = extractRefPtr<Bridge>(env, obj);
   auto assetNameStr = fromJString(env, assetName);
 
-  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromNetworkCached_start"));
+  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromAssets_start"));
   auto script = react::loadScriptFromAssets(env, assetManager, assetNameStr);
   #ifdef WITH_FBSYSTRACE
   FbSystraceSection s(TRACE_TAG_REACT_CXX_BRIDGE, "reactbridge_jni_"
@@ -646,30 +646,27 @@ static void loadScriptFromAssets(JNIEnv* env, jobject obj, jobject assetManager,
     "assetName", assetNameStr);
   #endif
 
-  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromNetworkCached_read"));
+  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromAssets_read"));
   executeApplicationScript(bridge, script, assetNameStr);
-  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromNetworkCached_done"));
+  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromAssets_done"));
 }
 
-static void loadScriptFromNetworkCached(JNIEnv* env, jobject obj, jstring sourceURL,
-                                   jstring tempFileName) {
+static void loadScriptFromFile(JNIEnv* env, jobject obj, jstring fileName, jstring sourceURL) {
   jclass markerClass = env->FindClass("com/facebook/react/bridge/ReactMarker");
 
   auto bridge = jni::extractRefPtr<Bridge>(env, obj);
-  std::string script = "";
-  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromNetworkCached_start"));
-  if (tempFileName != NULL) {
-    script = react::loadScriptFromFile(jni::fromJString(env, tempFileName));
-  }
+  auto fileNameStr = fileName == NULL ? "" : fromJString(env, fileName);
+  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromFile_start"));
+  auto script = fileName == NULL ? "" : react::loadScriptFromFile(fileNameStr);
   #ifdef WITH_FBSYSTRACE
-  auto sourceURLStr = fromJString(env, sourceURL);
+  auto sourceURLStr = sourceURL == NULL ? fileNameStr : fromJString(env, sourceURL);
   FbSystraceSection s(TRACE_TAG_REACT_CXX_BRIDGE, "reactbridge_jni_"
     "executeApplicationScript",
     "sourceURL", sourceURLStr);
   #endif
-  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromNetworkCached_read"));
+  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromFile_read"));
   executeApplicationScript(bridge, script, jni::fromJString(env, sourceURL));
-  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromNetworkCached_exec"));
+  env->CallStaticVoidMethod(markerClass, gLogMarkerMethod, env->NewStringUTF("loadScriptFromFile_exec"));
 }
 
 static void callFunction(JNIEnv* env, jobject obj, jint moduleId, jint methodId,
@@ -821,7 +818,7 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
         makeNativeMethod(
           "loadScriptFromAssets", "(Landroid/content/res/AssetManager;Ljava/lang/String;)V",
           bridge::loadScriptFromAssets),
-        makeNativeMethod("loadScriptFromNetworkCached", bridge::loadScriptFromNetworkCached),
+        makeNativeMethod("loadScriptFromFile", bridge::loadScriptFromFile),
         makeNativeMethod("callFunction", bridge::callFunction),
         makeNativeMethod("invokeCallback", bridge::invokeCallback),
         makeNativeMethod("setGlobalVariable", bridge::setGlobalVariable),
