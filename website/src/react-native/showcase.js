365754eb192d8f299f6cfcd5fc5ff5fa7ec23c18
mattds <matthewstephanou@Matthews-iMac.local>
2015-10-28 10:41:59 +0200

    Add Fan of it to showcase

--- website/src/react-native/showcase.js
+++ website/src/react-native/showcase.js
@@ -85,6 +85,12 @@ var apps = [
     author: 'Facebook',
   },
   {
+    name: 'Fan of it',
+    icon: 'http://a4.mzstatic.com/us/r30/Purple3/v4/c9/3f/e8/c93fe8fb-9332-e744-f04a-0f4f78e42aa8/icon350x350.png',
+    link: 'https://itunes.apple.com/za/app/fan-of-it/id1017025530?mt=8',
+    author: 'Fan of it (Pty) Ltd',
+  },
+  {
     name: 'FastPaper',
     icon: 'http://a2.mzstatic.com/us/r30/Purple5/v4/72/b4/d8/72b4d866-90d2-3aad-d1dc-0315f2d9d045/icon350x350.jpeg',
     link: 'https://itunes.apple.com/us/app/fast-paper/id1001174614',
