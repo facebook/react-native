6ee66754704e7eca61e23c11e37a16e88fecb867
Adrov Igor <nucleartux@gmail.com>
2015-10-29 02:13:54 +0300

    pull out example to separate file

--- Examples/UIExplorer/XHRExample.ios.js
+++ Examples/UIExplorer/XHRExample.ios.js
@@ -30,6 +30,8 @@ var {
   View,
 } = React;
 
+var XHRExampleHeaders = require('./XHRExampleHeaders');
+
 class Downloader extends React.Component {
 
   xhr: XMLHttpRequest;
@@ -350,86 +352,6 @@ class FetchTest extends React.Component {
   }
 }
 
-class Headers extends React.Component {
-
-  xhr: XMLHttpRequest;
-  cancelled: boolean;
-
-  constructor(props) {
-    super(props);
-    this.cancelled = false;
-    this.state = {
-      status: '',
-      headers: '',
-      contentSize: 1,
-      downloaded: 0,
-    };
-  }
-
-  download() {
-    this.xhr && this.xhr.abort();
-
-    var xhr = this.xhr || new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === xhr.DONE) {
-        if (this.cancelled) {
-          this.cancelled = false;
-          return;
-        }
-        if (xhr.status === 200) {
-          this.setState({
-            status: 'Download complete!',
-            headers: xhr.getAllResponseHeaders()
-          });
-        } else if (xhr.status !== 0) {
-          this.setState({
-            status: 'Error: Server returned HTTP status of ' + xhr.status + ' ' + xhr.responseText,
-          });
-        } else {
-          this.setState({
-            status: 'Error: ' + xhr.responseText,
-          });
-        }
-      }
-    };
-    xhr.open('GET', 'https://httpbin.org/response-headers?header1=value&header2=value1&header2=value2');
-    xhr.send();
-    this.xhr = xhr;
-
-    this.setState({status: 'Downloading...'});
-  }
-
-  componentWillUnmount() {
-    this.cancelled = true;
-    this.xhr && this.xhr.abort();
-  }
-
-  render() {
-    var button = this.state.status === 'Downloading...' ? (
-      <View style={styles.wrapper}>
-        <View style={styles.button}>
-          <Text>...</Text>
-        </View>
-      </View>
-    ) : (
-      <TouchableHighlight
-        style={styles.wrapper}
-        onPress={this.download.bind(this)}>
-        <View style={styles.button}>
-         <Text>Get headers</Text>
-        </View>
-      </TouchableHighlight>
-    );
-
-    return (
-      <View>
-        {button}
-        <Text>{this.state.headers}</Text>
-      </View>
-    );
-  }
-}
-
 exports.framework = 'React';
 exports.title = 'XMLHttpRequest';
 exports.description = 'XMLHttpRequest';
@@ -451,7 +373,7 @@ exports.examples = [{
 }, {
   title: 'Headers',
   render() {
-    return <Headers/>;
+    return <XHRExampleHeaders/>;
   }
 }];
 
