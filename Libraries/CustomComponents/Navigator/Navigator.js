0a6be8f3b2e99151fc467f0ba4fb320dd2b8f5d6
Hedger Wang <hedger@fb.com>
2015-10-28 18:24:59 -0700

    Replace Navigator with AdsManagerNavigator - step 1
    Reviewed By: fkgozali
    
    Differential Revision: D2585102
    
    fb-gh-sync-id: 6f0d51d755e0926f7c6f4d411a026cebf49333a2

--- Libraries/CustomComponents/Navigator/Navigator.js
+++ Libraries/CustomComponents/Navigator/Navigator.js
@@ -273,6 +273,8 @@ var Navigator = React.createClass({
   },
 
   getInitialState: function() {
+    this._navigationBarNavigator = this.props.navigationBarNavigator || this;
+
     this._renderedSceneMap = new Map();
 
     var routeStack = this.props.initialRouteStack || [this.props.initialRoute];
@@ -1074,7 +1076,7 @@ var Navigator = React.createClass({
     }
     return React.cloneElement(this.props.navigationBar, {
       ref: (navBar) => { this._navBar = navBar; },
-      navigator: this,
+      navigator: this._navigationBarNavigator,
       navState: this.state,
     });
   },
