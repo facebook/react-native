dd5ee826fdc7bc705eff01293c3711ab4a963440
rocman <rocman@vip.qq.com>
2015-11-01 20:26:46 +0800

    Fix the issue from the root
    The memory leaks root from the NavigationController not removing itself
    when its holder view deallocs. So to fix the issue, it’s just to remove
    it from its parentViewController when the holding RCTNavigator
    deallocs. This should be safe, since the RCTNavigator seems the owner
    of the NavigationController.

--- React/Views/RCTNavigator.m
+++ React/Views/RCTNavigator.m
@@ -334,8 +334,7 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
 
 - (void)dealloc
 {
-  _navigationController.delegate = nil;
-  _navigationController.viewControllers = nil;
+  [_navigationController removeFromParentViewController];
 }
 
 - (UIViewController *)reactViewController
