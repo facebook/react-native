3b82507565d2893a064eccb9a747b7b851e2af12
Dave Sibiski <dsibiski@gmail.com>
2015-11-04 22:38:36 -0400

    Adds UIExplorer example

--- Examples/UIExplorer/TextInputExample.ios.js
+++ Examples/UIExplorer/TextInputExample.ios.js
@@ -120,6 +120,60 @@ class RewriteExample extends React.Component {
   }
 }
 
+var BlurOnSubmitExample = React.createClass({
+  focusNextField(nextField) {
+    this.refs[nextField].focus()
+  },
+
+  render: function() {
+    return (
+      <View>
+        <TextInput
+          ref='1'
+          style={styles.default}
+          placeholder='blurOnSubmit = false'
+          returnKeyType='next'
+          blurOnSubmit={false}
+          onSubmitEditing={() => this.focusNextField('2')}
+        />
+        <TextInput
+          ref='2'
+          style={styles.default}
+          keyboardType='email-address'
+          placeholder='blurOnSubmit = false'
+          returnKeyType='next'
+          blurOnSubmit={false}
+          onSubmitEditing={() => this.focusNextField('3')}
+        />
+        <TextInput
+          ref='3'
+          style={styles.default}
+          keyboardType='url'
+          placeholder='blurOnSubmit = false'
+          returnKeyType='next'
+          blurOnSubmit={false}
+          onSubmitEditing={() => this.focusNextField('4')}
+        />
+        <TextInput
+          ref='4'
+          style={styles.default}
+          keyboardType='numeric'
+          placeholder='blurOnSubmit = false'
+          blurOnSubmit={false}
+          onSubmitEditing={() => this.focusNextField('5')}
+        />
+        <TextInput
+          ref='5'
+          style={styles.default}
+          keyboardType='numbers-and-punctuation'
+          placeholder='blurOnSubmit = true'
+          returnKeyType='done'
+        />
+      </View>
+    );
+  }
+});
+
 var styles = StyleSheet.create({
   page: {
     paddingBottom: 300,
@@ -443,5 +497,9 @@ exports.examples = [
         </View>
       );
     }
-  }
+  },
+  {
+    title: 'Blur on submit',
+    render: function(): ReactElement { return <BlurOnSubmitExample />; },
+  },
 ];
