85aeb4b80eeddbfc2fee305e486b114ec39bfef5
Martin Konicek <mkonicek@fb.com>
2015-11-18 09:03:51 -0800

    Open source the Android Location module
    Reviewed By: foghina
    
    Differential Revision: D2658581
    
    fb-gh-sync-id: e95b21c5c7c06f3332d2a7c9fab8be9a2e6441cb

--- Examples/UIExplorer/UIExplorerList.android.js
+++ Examples/UIExplorer/UIExplorerList.android.js
@@ -38,6 +38,7 @@ var COMPONENTS = [
 var APIS = [
   require('./AccessibilityAndroidExample.android'),
   require('./BorderExample'),
+  require('./GeolocationExample'),
   require('./IntentAndroidExample.android'),
   require('./LayoutEventsExample'),
   require('./LayoutExample'),
