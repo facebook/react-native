dd09c88571f5397346903e0e8c73224cbf40c862
Krzysztof Magiera <krzysztof@fb.com>
2015-11-18 11:34:05 -0800

    Compose propTypes from View.propTypes where appropriate.
    Reviewed By: davidaurelio
    
    Differential Revision: D2668380
    
    fb-gh-sync-id: f34fc8df3bbf755c710e99c5421762406afaba2c

--- Libraries/Components/SwitchIOS/SwitchIOS.ios.js
+++ Libraries/Components/SwitchIOS/SwitchIOS.ios.js
@@ -17,6 +17,7 @@ var NativeMethodsMixin = require('NativeMethodsMixin');
 var PropTypes = require('ReactPropTypes');
 var React = require('React');
 var StyleSheet = require('StyleSheet');
+var View = require('View');
 
 var requireNativeComponent = require('requireNativeComponent');
 
@@ -40,6 +41,7 @@ var SwitchIOS = React.createClass({
   mixins: [NativeMethodsMixin],
 
   propTypes: {
+    ...View.propTypes,
     /**
      * The value of the switch, if true the switch will be turned on.
      * Default value is false.
