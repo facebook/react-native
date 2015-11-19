7ed930ee7344c52e98574670e585a5bc687bc86f
Pasindu Perera <perera.pasindu@gmail.com>
2015-11-19 15:54:01 +0530

    added version specific docs #3459

--- website/publish.sh
+++ website/publish.sh
@@ -9,6 +9,7 @@
 
 set -e
 
+VERSION=`git describe | sed 's/-.*//'`
 # Start in website/ even if run from root directory
 cd "$(dirname "$0")"
 
@@ -18,15 +19,16 @@ git clean -dfx
 git fetch
 git rebase
 rm -Rf *
+mkdir ${VERSION}
 cd ../react-native/website
 node server/generate.js
-cp -R build/react-native/* ../../react-native-gh-pages/
+cp -R build/react-native/* ../../react-native-gh-pages/${VERSION}
 rm -Rf build/
 cd ../../react-native-gh-pages
 git status
 git add -A .
 if ! git diff-index --quiet HEAD --; then
-  git commit -m "update website"
+  git commit -m "update ${VERSION} of the website"
   git push origin gh-pages
 fi
 cd ../react-native/website
