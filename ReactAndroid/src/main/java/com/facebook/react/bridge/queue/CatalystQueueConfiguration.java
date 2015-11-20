9a61628f133a4d3fb67190e38de6c61c017a83e3
Marc Horowitz <mhorowitz@fb.com>
2015-11-20 12:17:23 -0800

    Clean up the interface to the bridge
    Summary: Change the following classes into interfaces, with a separate
    Impl file:  CatalystInstance, ReactInstanceManager,
    CatalystQueueConfiguration, MessageQueueThread.  This is done to help
    isolate the interface between React Native and applications which use it.
    This will also help some intrusive development work on a branch
    such as porting parts of the bridge to common C++ code, without affecting
    app reliability while this work is ongoing.
    public
    
    Reviewed By: astreet
    
    Differential Revision: D2651277
    
    fb-gh-sync-id: f04dc04a6e68df7acbc2bbf8b2529287d7b5b2ae

--- ReactAndroid/src/main/java/com/facebook/react/bridge/queue/CatalystQueueConfiguration.java
+++ ReactAndroid/src/main/java/com/facebook/react/bridge/queue/CatalystQueueConfiguration.java
@@ -9,12 +9,6 @@
 
 package com.facebook.react.bridge.queue;
 
-import java.util.Map;
-
-import android.os.Looper;
-
-import com.facebook.react.common.MapBuilder;
-
 /**
  * Specifies which {@link MessageQueueThread}s must be used to run the various contexts of
  * execution within catalyst (Main UI thread, native modules, and JS). Some of these queues *may* be
@@ -24,67 +18,8 @@ import com.facebook.react.common.MapBuilder;
  * Native Modules Queue Thread: The thread and Looper that native modules are invoked on.
  * JS Queue Thread: The thread and Looper that JS is executed on.
  */
-public class CatalystQueueConfiguration {
-
-  private final MessageQueueThread mUIQueueThread;
-  private final MessageQueueThread mNativeModulesQueueThread;
-  private final MessageQueueThread mJSQueueThread;
-
-  private CatalystQueueConfiguration(
-      MessageQueueThread uiQueueThread,
-      MessageQueueThread nativeModulesQueueThread,
-      MessageQueueThread jsQueueThread) {
-    mUIQueueThread = uiQueueThread;
-    mNativeModulesQueueThread = nativeModulesQueueThread;
-    mJSQueueThread = jsQueueThread;
-  }
-
-  public MessageQueueThread getUIQueueThread() {
-    return mUIQueueThread;
-  }
-
-  public MessageQueueThread getNativeModulesQueueThread() {
-    return mNativeModulesQueueThread;
-  }
-
-  public MessageQueueThread getJSQueueThread() {
-    return mJSQueueThread;
-  }
-
-  /**
-   * Should be called when the corresponding {@link com.facebook.react.bridge.CatalystInstance}
-   * is destroyed so that we shut down the proper queue threads.
-   */
-  public void destroy() {
-    if (mNativeModulesQueueThread.getLooper() != Looper.getMainLooper()) {
-      mNativeModulesQueueThread.quitSynchronous();
-    }
-    if (mJSQueueThread.getLooper() != Looper.getMainLooper()) {
-      mJSQueueThread.quitSynchronous();
-    }
-  }
-
-  public static CatalystQueueConfiguration create(
-      CatalystQueueConfigurationSpec spec,
-      QueueThreadExceptionHandler exceptionHandler) {
-    Map<MessageQueueThreadSpec, MessageQueueThread> specsToThreads = MapBuilder.newHashMap();
-
-    MessageQueueThreadSpec uiThreadSpec = MessageQueueThreadSpec.mainThreadSpec();
-    MessageQueueThread uiThread = MessageQueueThread.create( uiThreadSpec, exceptionHandler);
-    specsToThreads.put(uiThreadSpec, uiThread);
-
-    MessageQueueThread jsThread = specsToThreads.get(spec.getJSQueueThreadSpec());
-    if (jsThread == null) {
-      jsThread = MessageQueueThread.create(spec.getJSQueueThreadSpec(), exceptionHandler);
-    }
-
-    MessageQueueThread nativeModulesThread =
-        specsToThreads.get(spec.getNativeModulesQueueThreadSpec());
-    if (nativeModulesThread == null) {
-      nativeModulesThread =
-          MessageQueueThread.create(spec.getNativeModulesQueueThreadSpec(), exceptionHandler);
-    }
-
-    return new CatalystQueueConfiguration(uiThread, nativeModulesThread, jsThread);
-  }
+public interface CatalystQueueConfiguration {
+  MessageQueueThread getUIQueueThread();
+  MessageQueueThread getNativeModulesQueueThread();
+  MessageQueueThread getJSQueueThread();
 }
