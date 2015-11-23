de19626da69903650486c248ca8b27fc9adc4375
Alexander Blom <blom@fb.com>
2015-11-23 02:16:51 -0800

    Support cookies on Android
    Summary: This adds a persistent cookie store that shares cookies with WebView.
    
    Add a `ForwardingCookieHandler` to OkHttp that uses the underlying Android webkit `CookieManager`.
    Use a `LazyCookieHandler` to defer initialization of `CookieManager` as this will in turn trigger initialization of the Chromium stack in KitKat+ which takes some time. This was we will incur this cost on a background network thread instead of during startup.
    Also add a `clearCookies()` method to the network module.
    
    Add a cookies example to the XHR example. This example should also work for iOS (except for the clear cookies part). They are for now just scoped to Android.
    
    Closes #2792.
    
    public
    
    Reviewed By: andreicoman11
    
    Differential Revision: D2615550
    
    fb-gh-sync-id: ff726a35f0fc3c7124d2f755448fe24c9d1caf21

--- ReactAndroid/src/main/java/com/facebook/react/modules/network/NetworkingModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/network/NetworkingModule.java
@@ -14,6 +14,7 @@ import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.net.CookieHandler;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.GuardedAsyncTask;
@@ -72,19 +73,19 @@ public final class NetworkingModule extends ReactContextBaseJavaModule {
   }
 
   /**
-   * @param reactContext the ReactContext of the application
+   * @param context the ReactContext of the application
    */
-  public NetworkingModule(ReactApplicationContext reactContext) {
-    this(reactContext, null, OkHttpClientProvider.getOkHttpClient());
+  public NetworkingModule(final ReactApplicationContext context) {
+    this(context, null, OkHttpClientProvider.getCookieAwareOkHttpClient(context));
   }
 
   /**
-   * @param reactContext the ReactContext of the application
+   * @param context the ReactContext of the application
    * @param defaultUserAgent the User-Agent header that will be set for all requests where the
    * caller does not provide one explicitly
    */
-  public NetworkingModule(ReactApplicationContext reactContext, String defaultUserAgent) {
-    this(reactContext, defaultUserAgent, OkHttpClientProvider.getOkHttpClient());
+  public NetworkingModule(ReactApplicationContext context, String defaultUserAgent) {
+    this(context, defaultUserAgent, OkHttpClientProvider.getCookieAwareOkHttpClient(context));
   }
 
   public NetworkingModule(ReactApplicationContext reactContext, OkHttpClient client) {
@@ -100,6 +101,11 @@ public final class NetworkingModule extends ReactContextBaseJavaModule {
   public void onCatalystInstanceDestroy() {
     mShuttingDown = true;
     mClient.cancel(null);
+
+    CookieHandler cookieHandler = mClient.getCookieHandler();
+    if (cookieHandler instanceof ForwardingCookieHandler) {
+      ((ForwardingCookieHandler) cookieHandler).destroy();
+    }
   }
 
   @ReactMethod
@@ -309,6 +315,14 @@ public final class NetworkingModule extends ReactContextBaseJavaModule {
         mClient.cancel(requestId);
       }
     }.execute();
+  }
+
+  @ReactMethod
+  public void clearCookies(com.facebook.react.bridge.Callback callback) {
+    CookieHandler cookieHandler = mClient.getCookieHandler();
+    if (cookieHandler instanceof ForwardingCookieHandler) {
+      ((ForwardingCookieHandler) cookieHandler).clearCookies(callback);
+    }
   }
 
   private @Nullable MultipartBuilder constructMultipartBody(
