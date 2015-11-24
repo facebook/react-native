37f81341a006458d84d934882485c2a0a8838497
Konstantin Raev <bestander@fb.com>
2015-11-24 08:50:39 -0800

    Open source SwipeRefreshLayoutAndroid
    Reviewed By: mkonicek
    
    Differential Revision: D2679605
    
    fb-gh-sync-id: 7f3e7384b37f29002ddd8cb7a4567fa96c76f047

---
+++ ReactAndroid/src/main/java/com/facebook/react/views/swiperefresh/RefreshEvent.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.react.views.swiperefresh;
+
+import com.facebook.react.uimanager.events.Event;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+
+public class RefreshEvent extends Event<RefreshEvent> {
+
+    protected RefreshEvent(int viewTag, long timestampMs) {
+        super(viewTag, timestampMs);
+    }
+
+    @Override
+    public String getEventName() {
+        return "topRefresh";
+    }
+
+    @Override
+    public void dispatch(RCTEventEmitter rctEventEmitter) {
+        rctEventEmitter.receiveEvent(getViewTag(), getEventName(), null);
+    }
+}
