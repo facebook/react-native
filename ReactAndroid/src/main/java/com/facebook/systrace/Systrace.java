e11fb72d452053b0c1f62587595275a6f1a60ac8
SungHyun Park <puresprout@gmail.com>
2015-11-26 03:32:57 -0800

    added TRACE_TAG_REACT_APPS constants
    Summary: added required constants
    Closes https://github.com/facebook/react-native/pull/4366
    
    Reviewed By: svcscm
    
    Differential Revision: D2699433
    
    Pulled By: astreet
    
    fb-gh-sync-id: 33c69b248f682e1b31bb2ef79cb0253ddd8d8817

--- ReactAndroid/src/main/java/com/facebook/systrace/Systrace.java
+++ ReactAndroid/src/main/java/com/facebook/systrace/Systrace.java
@@ -20,6 +20,7 @@ public class Systrace {
 
   public static final long TRACE_TAG_REACT_JAVA_BRIDGE = 0L;
   public static final long TRACE_TAG_REACT_FRESCO = 0L;
+  public static final long TRACE_TAG_REACT_APPS = 0L;
 
   public enum EventScope {
     THREAD('t'),
