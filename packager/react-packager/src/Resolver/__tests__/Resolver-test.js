ffcc3f21bf9859030a8e01399b19bda132cafd59
Sebastian Markbage <sema@fb.com>
2015-11-25 17:34:55 -0800

    Extract the Require polyfill from the default list of polyfills
    Summary: Extracts the module system from the list of dependencies and adds it back to the bundling process.
    
    Unbundling and Prepack has their own module systems. jest does as well. This is not normally part of the resolver, nor polyfills.
    
    In fact, I think we'll eventually start treating polyfills as normal modules just like core-js.
    
    The prelude is the weird part right now but I think that we'll eventually move the DEV flag to be module level instead of global and we can just get rid of this prelude.
    
    public
    
    Reviewed By: davidaurelio
    
    Differential Revision: D2693701
    
    fb-gh-sync-id: a59ccda0fa15fcfcda52897e8290805eed1b92b3

--- packager/react-packager/src/Resolver/__tests__/Resolver-test.js
+++ packager/react-packager/src/Resolver/__tests__/Resolver-test.js
@@ -79,27 +79,15 @@ describe('Resolver', function() {
           expect(result.mainModuleId).toEqual('index');
           expect(result.dependencies[result.dependencies.length - 1]).toBe(module);
           expect(_.pluck(Polyfill.mock.calls, 0)).toEqual([
-            { path: 'polyfills/prelude.js',
-              id: 'polyfills/prelude.js',
-              isPolyfill: true,
-              dependencies: []
-            },
-            { path: 'polyfills/require.js',
-              id: 'polyfills/require.js',
-              isPolyfill: true,
-              dependencies: ['polyfills/prelude.js']
-            },
             { path: 'polyfills/polyfills.js',
               id: 'polyfills/polyfills.js',
               isPolyfill: true,
-              dependencies: ['polyfills/prelude.js', 'polyfills/require.js']
+              dependencies: []
             },
             { id: 'polyfills/console.js',
               isPolyfill: true,
               path: 'polyfills/console.js',
               dependencies: [
-                'polyfills/prelude.js',
-                'polyfills/require.js',
                 'polyfills/polyfills.js'
               ],
             },
@@ -107,8 +95,6 @@ describe('Resolver', function() {
               isPolyfill: true,
               path: 'polyfills/error-guard.js',
               dependencies: [
-                'polyfills/prelude.js',
-                'polyfills/require.js',
                 'polyfills/polyfills.js',
                 'polyfills/console.js'
               ],
@@ -117,8 +103,6 @@ describe('Resolver', function() {
               isPolyfill: true,
               path: 'polyfills/String.prototype.es6.js',
               dependencies: [
-                'polyfills/prelude.js',
-                'polyfills/require.js',
                 'polyfills/polyfills.js',
                 'polyfills/console.js',
                 'polyfills/error-guard.js'
@@ -128,8 +112,6 @@ describe('Resolver', function() {
               isPolyfill: true,
               path: 'polyfills/Array.prototype.es6.js',
               dependencies: [
-                'polyfills/prelude.js',
-                'polyfills/require.js',
                 'polyfills/polyfills.js',
                 'polyfills/console.js',
                 'polyfills/error-guard.js',
@@ -140,8 +122,6 @@ describe('Resolver', function() {
               isPolyfill: true,
               path: 'polyfills/Array.es6.js',
               dependencies: [
-                'polyfills/prelude.js',
-                'polyfills/require.js',
                 'polyfills/polyfills.js',
                 'polyfills/console.js',
                 'polyfills/error-guard.js',
@@ -153,8 +133,6 @@ describe('Resolver', function() {
               isPolyfill: true,
               path: 'polyfills/babelHelpers.js',
               dependencies: [
-                'polyfills/prelude.js',
-                'polyfills/require.js',
                 'polyfills/polyfills.js',
                 'polyfills/console.js',
                 'polyfills/error-guard.js',
@@ -218,8 +196,6 @@ describe('Resolver', function() {
               id: 'some module',
               isPolyfill: true,
               dependencies: [
-                'polyfills/prelude.js',
-                'polyfills/require.js',
                 'polyfills/polyfills.js',
                 'polyfills/console.js',
                 'polyfills/error-guard.js',
