e9fbf51d2ae212400d941ba10f4874d67356137e
Mike Armstrong <mikea@fb.com>
2015-11-27 02:04:32 -0800

    add timestamps to determine string convert time
    Reviewed By: astreet
    
    Differential Revision: D2684174
    
    fb-gh-sync-id: feed759025e131fd913c6188f783d6920ad1839c

---
+++ ReactAndroid/src/main/jni/react/jni/OnLoad.h
@@ -0,0 +1,13 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include <jni.h>
+
+namespace facebook {
+namespace react {
+
+jmethodID getLogMarkerMethod();
+
+} // namespace react
+} // namespace facebook
