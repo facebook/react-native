16b9c7d6f61b49917ccdb30335d1190e3fdb8247
Perry Poon <plrthink@gmail.com>
2015-12-01 23:01:48 +0800

    change order in showcase

--- website/src/react-native/showcase.js
+++ website/src/react-native/showcase.js
@@ -249,6 +249,12 @@ var apps = [
     author: 'Peter Cottle',
   },
   {
+    name: 'MockingBot',
+    icon: 'https://s3.cn-north-1.amazonaws.com.cn/modao/downloads/images/MockingBot175.png',
+    link: 'https://itunes.apple.com/cn/app/mockingbot/id1050565468?l=en&mt=8',
+    author: 'YuanYi Zhang (@mockingbot)',
+  },
+  {
     name: 'Mr. Dapper',
     icon: 'http://is5.mzstatic.com/image/pf/us/r30/Purple4/v4/e8/3f/7c/e83f7cb3-2602-f8e8-de9a-ce0a775a4a14/mzl.hmdjhfai.png',
     link: 'https://itunes.apple.com/us/app/mr.-dapper-men-fashion-app/id989735184?ls=1&mt=8',
@@ -385,12 +391,6 @@ var apps = [
     icon: 'http://a4.mzstatic.com/us/r30/Purple6/v4/80/4f/43/804f431d-2828-05aa-2593-99cfb0475351/icon175x175.png',
     link: 'https://itunes.apple.com/us/app/yazboz-batak-esli-batak-okey/id1048620855?ls=1&mt=8',
     author: 'Melih Mucuk',
-  },
-  {
-    name: 'MockingBot',
-    icon: 'https://s3.cn-north-1.amazonaws.com.cn/modao/downloads/images/MockingBot175.png',
-    link: 'https://itunes.apple.com/cn/app/mockingbot/id1050565468?l=en&mt=8',
-    author: 'YuanYi Zhang (@mockingbot)',
   },
 ];
 
