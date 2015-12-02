2a31c7cf3cb8e16be71a1da0eaca5af8d0580878
Christopher Dro <hello@blick-labs.com>
2015-12-02 05:53:38 -0800

    Update argument passed for assetRoots.
    Summary:
    Fixes #4388
    Closes https://github.com/facebook/react-native/pull/4431
    
    Reviewed By: svcscm
    
    Differential Revision: D2704076
    
    Pulled By: mkonicek
    
    fb-gh-sync-id: 2b788eb15787334bf7309fde730566092ff63832

--- local-cli/server/server.js
+++ local-cli/server/server.js
@@ -79,7 +79,7 @@ function _server(argv, config, resolve, reject) {
   }
 
   args.assetRoots = args.assetRoots
-    ? argToArray(args.projectRoots).map(dir =>
+    ? argToArray(args.assetRoots).map(dir =>
       path.resolve(process.cwd(), dir)
     )
     : config.getAssetRoots();
