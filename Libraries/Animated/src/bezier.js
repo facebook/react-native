4373aa68228054916d55c0956ede6eeacd40241a
Huang Yu <huang@bitgo.com>
2015-12-13 11:42:53 -0800

    fix animated lint warnings
    Summary:
    fix lint warnings under 'Libraries/Animated' directory
    Closes https://github.com/facebook/react-native/pull/4448
    
    Reviewed By: svcscm
    
    Differential Revision: D2753880
    
    Pulled By: androidtrunkagent
    
    fb-gh-sync-id: c6619c636ff67a74e6f063f70526327d756271db

--- Libraries/Animated/src/bezier.js
+++ Libraries/Animated/src/bezier.js
@@ -42,7 +42,7 @@ module.exports = function(x1, y1, x2, y2, epsilon){
 
   var derivativeCurveX = function(t){
     var v = 1 - t;
-    return 3 * (2 * (t - 1) * t + v * v) * x1 + 3 * (- t * t * t + 2 * v * t) * x2;
+    return 3 * (2 * (t - 1) * t + v * v) * x1 + 3 * (-t * t * t + 2 * v * t) * x2;
   };
 
   return function(t){
@@ -52,24 +52,26 @@ module.exports = function(x1, y1, x2, y2, epsilon){
     // First try a few iterations of Newton's method -- normally very fast.
     for (t2 = x, i = 0; i < 8; i++){
       x2 = curveX(t2) - x;
-      if (Math.abs(x2) < epsilon) return curveY(t2);
+      if (Math.abs(x2) < epsilon) { return curveY(t2); }
       d2 = derivativeCurveX(t2);
-      if (Math.abs(d2) < 1e-6) break;
+      if (Math.abs(d2) < 1e-6) { break; }
       t2 = t2 - x2 / d2;
     }
 
-    t0 = 0, t1 = 1, t2 = x;
+    t0 = 0;
+    t1 = 1;
+    t2 = x;
 
-    if (t2 < t0) return curveY(t0);
-    if (t2 > t1) return curveY(t1);
+    if (t2 < t0) { return curveY(t0); }
+    if (t2 > t1) { return curveY(t1); }
 
     // Fallback to the bisection method for reliability.
     while (t0 < t1){
       x2 = curveX(t2);
-      if (Math.abs(x2 - x) < epsilon) return curveY(t2);
-      if (x > x2) t0 = t2;
-      else t1 = t2;
-      t2 = (t1 - t0) * .5 + t0;
+      if (Math.abs(x2 - x) < epsilon) { return curveY(t2); }
+      if (x > x2) { t0 = t2; }
+      else { t1 = t2; }
+      t2 = (t1 - t0) * 0.5 + t0;
     }
 
     // Failure
