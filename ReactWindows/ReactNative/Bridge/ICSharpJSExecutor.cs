0578c1d416f5448f175fbf7290f27eabcc25adf1
Erik Schlegel <erik.schlegel@gmail.com>
2015-12-15 15:52:06 -0500

    Adding JS Executor

---
+++ ReactWindows/ReactNative/Bridge/ICSharpJSExecutor.cs
@@ -0,0 +1,12 @@
+﻿namespace ReactNative.Bridge
+{
+    public interface ICSharpJSExecutor
+    {
+        void close();
+
+        void executeApplicationScript(string script, string sourceURL);
+
+        string executeJSCall(string methodName, string jsonArgsArray);
+  
+    }
+}
---
+++ ReactWindows/ReactNative/Bridge/JavaScriptExecuter.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ReactNative.Bridge
+{
+    /// <summary>
+    /// Abstract class responsible for brokering communication between native and React Naitve components 
+    /// </summary>
+    public abstract class JavaScriptExecuter
+    {
+        private ICSharpJSExecutor mJSExecutor;
+
+        /// <summary>
+        /// Sets the JS executor
+        /// </summary>
+        /// <param name="executor"></param>
+        public JavaScriptExecuter(ICSharpJSExecutor executor)
+        {
+            mJSExecutor = executor;
+        }
+
+        /// <summary>
+        /// Closes the executor
+        /// </summary>
+        public abstract void close();
+
+        /// <summary>
+        /// Instantiates the executor
+        /// </summary>
+        /// <param name="executor"></param>
+        public abstract void initialize(ICSharpJSExecutor executor);
+    }
+}
