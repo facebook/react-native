9c18fde73edbdeb122144b54faf0c338a9d31553
David Aurelio <davidaurelio@fb.com>
2015-12-15 05:46:24 -0800

    Avoid copying the JS bundle unnecessarily
    Summary:
    public
    When forwarding the unbundle code to the bridge, we’re passing the code by value.
    This changes the affected method to take it by reference.
    
    Reviewed By: astreet
    
    Differential Revision: D2759569
    
    fb-gh-sync-id: 508d4f4d56bcbdd5a7df5610cf9040121f8878ef

--- ReactAndroid/src/main/jni/react/jni/OnLoad.cpp
+++ ReactAndroid/src/main/jni/react/jni/OnLoad.cpp
@@ -623,8 +623,8 @@ static void create(JNIEnv* env, jobject obj, jobject executor, jobject callback,
 
 static void executeApplicationScript(
     const RefPtr<Bridge>& bridge,
-    const std::string script,
-    const std::string sourceUri) {
+    const std::string& script,
+    const std::string& sourceUri) {
   try {
     // Execute the application script and collect/dispatch any native calls that might have occured
     bridge->executeApplicationScript(script, sourceUri);
