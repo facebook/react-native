ff4fbfc98464d31420e721e4b1785bfb2dd4227e
Pasindu Perera <perera.pasindu@gmail.com>
2015-12-17 14:00:45 +0530

    npm progress shown on react init fixes #3771 for npm 5 and upwards

--- react-native-cli/index.js
+++ react-native-cli/index.js
@@ -180,36 +180,23 @@ function createProject(name, verbose) {
 
   console.log('Installing react-native package from npm...');
 
-  if (verbose) {
-    runVerbose(root, projectName);
-  } else {
-    run(root, projectName);
+  run(root, projectName, verbose);
+}
+
+function run(root, projectName, verbose) {
+  var args = ['install', '--save'];
+  if (verbose){
+    args.push('--verbose');
   }
-}
-
-function run(root, projectName) {
-  exec('npm install --save react-native', function(e, stdout, stderr) {
-    if (e) {
-      console.log(stdout);
-      console.error(stderr);
-      console.error('`npm install --save react-native` failed');
-      process.exit(1);
-    }
-
-    checkNodeVersion();
-
-    var cli = require(CLI_MODULE_PATH());
-    cli.init(root, projectName);
-  });
-}
-
-function runVerbose(root, projectName) {
-  var proc = spawn('npm', ['install', '--verbose', '--save', 'react-native'], {stdio: 'inherit'});
+  args.push('react-native');
+  var proc = spawn('npm', args, {stdio: 'inherit'});
   proc.on('close', function (code) {
     if (code !== 0) {
       console.error('`npm install --save react-native` failed');
       return;
     }
+
+    checkNodeVersion();
 
     cli = require(CLI_MODULE_PATH());
     cli.init(root, projectName);
