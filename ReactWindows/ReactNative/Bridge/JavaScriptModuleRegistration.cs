b229799b19128cf634f2b202325b196dc8edb8e5
Eric Rozell <erozell@outlook.com>
2015-12-17 00:18:35 -0500

    Cleans up namespaces.

--- ReactWindows/ReactNative/Bridge/JavaScriptModuleRegistration.cs
+++ ReactWindows/ReactNative/Bridge/JavaScriptModuleRegistration.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Reflection;
 
 namespace ReactNative.Bridge
--- ReactWindows/ReactNative/Bridge/ReactContext.cs
+++ ReactWindows/ReactNative/Bridge/ReactContext.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReactNative.Bridge
+﻿namespace ReactNative.Bridge
 {
     class ReactContext
     {
