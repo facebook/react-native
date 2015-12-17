a7e75b9d304d1fca245579f4ef0de17cdea0547c
Erik Schlegel <erik.schlegel@gmail.com>
2015-12-17 15:50:20 -0500

    Adding ReactContext

---
+++ ReactWindows/ReactNative/Bridge/NativeModuleCallExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactNative.Bridge
+{
+    public interface NativeModuleCallExceptionHandler
+    {
+        void handleException(Exception e);
+    }
+}
