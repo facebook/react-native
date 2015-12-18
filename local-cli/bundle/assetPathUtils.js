882533d058c4908a4ae6f4083fb54a3f629bfeae
Geoffrey Goh <kg422@cornell.edu>
2015-12-18 11:33:19 -0800

    fix nit

--- local-cli/bundle/assetPathUtils.js
+++ local-cli/bundle/assetPathUtils.js
@@ -33,12 +33,11 @@ function getAndroidDrawableFolderName(asset, scale) {
 
 function getAndroidResourceIdentifier(asset) {
   var folderPath = getBasePath(asset);
-  const resourceIdentifier = (folderPath + '/' + asset.name)
+  return (folderPath + '/' + asset.name)
     .toLowerCase()
     .replace(/\//g, '_')           // Encode folder structure in file name
     .replace(/([^a-z0-9_])/g, '')  // Remove illegal chars
     .replace(/^assets_/, '');      // Remove "assets_" prefix
-  return resourceIdentifier;
 }
 
 function getBasePath(asset) {
