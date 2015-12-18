decfb5e18cb40f1f0d5be6071277920b1e5c3234
Ryan Gomba <ryan@ryangomba.com>
2015-12-18 09:39:13 -0800

    Horizontal scroll view pagination

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/PagingHorizontalScrollView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/PagingHorizontalScrollView.java
@@ -11,25 +11,40 @@ public class PagingHorizontalScrollView extends HorizontalScrollView implements
         View.OnTouchListener, GestureDetector.OnGestureListener {
 
     private static final int SWIPE_PAGE_ON_FACTOR = 5;
-    private static final int SWIPE_PAGE_MINIMUM_VELOCITY = 1000;
+    private static final int SWIPE_PAGE_MINIMUM_VELOCITY = 500;
+    private static final float SNAP_TO_INTERVAL_UNSET = -1.0f;
 
     private GestureDetector gestureDetector;
     private float prevScrollX = 0;
     private boolean start = true;
     private float currentScrollX;
 
-    private float snapToInterval = 0;
+    private boolean pagingEnabled = false;
+    private float snapToInterval = SNAP_TO_INTERVAL_UNSET;
 
     public PagingHorizontalScrollView(Context context) {
         super(context);
         setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
     }
 
-    public void setSnapToInterval(float snapToInterval) {
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
-        this.snapToInterval = snapToInterval * metrics.density;
+    public void setPagingEnabled(boolean pagingEnabled) {
+        this.pagingEnabled = pagingEnabled;
+        setSnappingEnabled(pagingEnabled);
+    }
 
-        if (snapToInterval != 0) {
+    public void setSnapToInterval(float snapToInterval) {
+        if (snapToInterval > 0) {
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+            this.snapToInterval = snapToInterval * metrics.density;
+            setSnappingEnabled(true);
+        } else {
+            this.snapToInterval = SNAP_TO_INTERVAL_UNSET;
+            setSnappingEnabled(false);
+        }
+    }
+
+    private void setSnappingEnabled(boolean enabled) {
+        if (enabled) {
             gestureDetector = new GestureDetector(getContext(), this);
             this.setOnTouchListener(this);
         } else {
@@ -38,17 +53,27 @@ public class PagingHorizontalScrollView extends HorizontalScrollView implements
         }
     }
 
+    private float getSnapToInterval() {
+        if (snapToInterval != SNAP_TO_INTERVAL_UNSET) {
+            return snapToInterval;
+        } else if (pagingEnabled) {
+            return getMeasuredWidth();
+        } else {
+            return SNAP_TO_INTERVAL_UNSET;
+        }
+    }
+
     public int numPages() {
-        return (int) Math.ceil(1.0 * computeHorizontalScrollRange() / snapToInterval);
+        return (int) Math.ceil(1.0 * computeHorizontalScrollRange() / getSnapToInterval());
     }
 
     public int currentPage(float scrollX) {
-        return (int) Math.floor(1.0 * scrollX / snapToInterval);
+        return (int) Math.floor(1.0 * scrollX / getSnapToInterval());
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        if (snapToInterval == 0) {
+        if (getSnapToInterval() == SNAP_TO_INTERVAL_UNSET) {
             return false;
         }
 
@@ -69,7 +94,7 @@ public class PagingHorizontalScrollView extends HorizontalScrollView implements
             case MotionEvent.ACTION_UP:
                 start = true;
                 currentScrollX = x;
-                int minSwipe = (int) (snapToInterval / SWIPE_PAGE_ON_FACTOR);
+                int minSwipe = (int) (getSnapToInterval() / SWIPE_PAGE_ON_FACTOR);
                 float delta = prevScrollX - currentScrollX;
                 handleMovement(delta, minSwipe);
                 result = true;
@@ -81,7 +106,7 @@ public class PagingHorizontalScrollView extends HorizontalScrollView implements
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        if (snapToInterval == 0) {
+        if (getSnapToInterval() == SNAP_TO_INTERVAL_UNSET) {
             return false;
         }
 
@@ -89,16 +114,13 @@ public class PagingHorizontalScrollView extends HorizontalScrollView implements
             return false;
         }
 
-        if (e2 == null || e1 == null) {
-            return false;
-        }
-
-        handleMovement(e2.getRawX() - e1.getRawX(), SWIPE_PAGE_MINIMUM_VELOCITY);
+        handleMovement(-1.0f * velocityX, SWIPE_PAGE_MINIMUM_VELOCITY);
         return true;
     }
 
     private void handleMovement(float delta, float threshold) {
         int currentPage = currentPage(computeHorizontalScrollOffset());
+
 
         int targetPage;
         if (delta > threshold && currentPage < numPages() - 1) {
@@ -115,7 +137,7 @@ public class PagingHorizontalScrollView extends HorizontalScrollView implements
             targetPage = currentPage;
         }
 
-        smoothScrollTo(Math.round(targetPage * snapToInterval), 0);
+        smoothScrollTo(Math.round(targetPage * getSnapToInterval()), 0);
     }
 
     @Override
