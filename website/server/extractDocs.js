4aa44227772e92f2774a3bf6353746065304c02a
Martin Konicek <mkonicek@fb.com>
2015-12-18 14:15:22 +0000

    Add alert docs

--- website/server/extractDocs.js
+++ website/server/extractDocs.js
@@ -219,6 +219,7 @@ var components = [
 
 var apis = [
   '../Libraries/ActionSheetIOS/ActionSheetIOS.js',
+  '../Libraries/Utilities/Alert.js',
   '../Libraries/Utilities/AlertIOS.js',
   '../Libraries/Animated/src/AnimatedImplementation.js',
   '../Libraries/AppRegistry/AppRegistry.js',
