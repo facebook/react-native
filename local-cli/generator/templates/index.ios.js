c6be65296b5c99877a37947fd58b88d3210ab24d
Patrick Puritscher <blackout289@gmail.com>
2015-12-21 12:24:25 +0100

    move const declaration to top

--- local-cli/generator/templates/index.ios.js
+++ local-cli/generator/templates/index.ios.js
@@ -10,6 +10,24 @@ import React, {
   View
 } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 class <%= name %> extends Component {
   render() {
@@ -29,24 +47,5 @@ class <%= name %> extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('<%= name %>', () => <%= name %>);
