45691d8b5b2f7a49a51e348364eb3cba87c349b2
StefanT <shtefanntz@yahoo.com>
2015-12-22 15:06:01 +0200

    fixed lint errors and allowed the use of numbers for colors

--- Libraries/StyleSheet/ColorPropType.js
+++ Libraries/StyleSheet/ColorPropType.js
@@ -3,14 +3,13 @@ var ReactPropTypes = require('ReactPropTypes');
 var tinycolor = require('tinycolor');
 
 var colorValidator = function (selectedColor, propName) {
-  if( selectedColor === null || selectedColor === undefined ||
-     (typeof selectedColor !== 'string') || selectedColor.trim() === '') {
+  if (selectedColor === null || selectedColor === undefined || selectedColor.toString().trim() === '') {
     return new Error(
       `Invalid argument supplied to ${propName}.Expected a string like #123ADF or 'red'.`
-  );
+    );
   }
 
-  if(tinycolor(selectedColor.trim()).isValid()) {
+  if (tinycolor(selectedColor.toString().trim()).isValid()) {
     return null;
   }
 
