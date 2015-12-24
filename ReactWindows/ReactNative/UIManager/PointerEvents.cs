a2e7a38edcf1c7347d6f772221e0da0c3e47e7c6
Eric Rozell <erozell@outlook.com>
2015-12-24 14:15:05 -0500

    Fixes #32 - Adds UIManagerModule constants
    Some remaining TODOs include integrating the constants from the view managers, and further investigation on how to include other features available in Android.

---
+++ ReactWindows/ReactNative/UIManager/PointerEvents.cs
@@ -0,0 +1,30 @@
+﻿namespace ReactNative.UIManager
+{
+    /// <summary>
+    /// Possible values for pointer events that a view and its descendants should
+    /// receive. See https://developer.mozilla.org/en-US/docs/Web/CSS/pointer-events
+    /// for more information.
+    /// </summary>
+    public enum PointerEvents
+    {
+        /// <summary>
+        /// Neither the container nor its children receive events.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Container does not get events but all its children do.
+        /// </summary>
+        BoxNone,
+
+        /// <summary>
+        /// Container gets events but none of its children do.
+        /// </summary>
+        BoxOnly,
+
+        /// <summary>
+        /// Container and all of its children receive touch events.
+        /// </summary>
+        Auto,
+    }
+}
