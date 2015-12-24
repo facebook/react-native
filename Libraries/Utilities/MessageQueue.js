893a54d0cde592e1cd77203efd597e3d961417ba
Spencer Ahrens <sahrens@fb.com>
2015-12-23 16:10:39 -0800

    Add yieldy, chained async task support to InteractionManager
    Summary:
    Default behavior should be unchanged.
    
    If we queue up a bunch of expensive tasks during an interaction, the default
    `InteractionManager` behavior would execute them all in one synchronous loop at
    the end the JS event loop via one `setImmediate` call, blocking the JS thread
    the entire time.
    
    The `setDeadline` addition in this diff enables an option to only execute tasks
    until the `eventLoopRunningTime` is hit (added to MessageQueue/BatchedBridge),
    allowing the queue execution to be paused if an interaction starts in between
    tasks, making the app more responsive.
    
    Additionally, if a task ends up generating a bunch of additional tasks
    asynchronously, the previous implementation would execute these new tasks after
    already scheduled tasks. This is often fine, but I want it to fully resolve
    async tasks and all their dependencies before making progress in the rest of the
    queue, so I added support for `type PromiseTask = {gen: () => Promise}` to do
    just this. It works by building a stack of queues each time a `PromiseTask` is
    started, and pops them off the stack once they are resolved and the queues are
    processed.
    
    I also pulled all of the actual queue logic out of `InteractionManager` and into
    a new `TaskQueue` class to isolate concerns a bit.
    
    public
    
    Reviewed By: josephsavona
    
    Differential Revision: D2754311
    
    fb-gh-sync-id: bfd6d0c54e6410cb261aa1d2c5024dd91a3959e6

--- Libraries/Utilities/MessageQueue.js
+++ Libraries/Utilities/MessageQueue.js
@@ -53,6 +53,7 @@ class MessageQueue {
     this._callbacks = [];
     this._callbackID = 0;
     this._lastFlush = 0;
+    this._eventLoopStartTime = new Date().getTime();
 
     [
       'invokeCallbackAndReturnFlushedQueue',
@@ -109,6 +110,10 @@ class MessageQueue {
     return module;
   }
 
+  getEventLoopRunningTime() {
+    return new Date().getTime() - this._eventLoopStartTime;
+  }
+
   /**
    * "Private" methods
    */
@@ -150,6 +155,7 @@ class MessageQueue {
 
   __callFunction(module, method, args) {
     this._lastFlush = new Date().getTime();
+    this._eventLoopStartTime = this._lastFlush;
     if (isFinite(module)) {
       method = this._methodTable[module][method];
       module = this._moduleTable[module];
@@ -171,6 +177,7 @@ class MessageQueue {
   __invokeCallback(cbID, args) {
     Systrace.beginEvent(`MessageQueue.invokeCallback(${cbID})`);
     this._lastFlush = new Date().getTime();
+    this._eventLoopStartTime = this._lastFlush;
     let callback = this._callbacks[cbID];
     if (!callback || __DEV__) {
       let debug = this._debugInfo[cbID >> 1];
