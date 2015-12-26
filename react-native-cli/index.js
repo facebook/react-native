1dea58c7dfa187a63ecfd17850b2c279e6e34716
sunnylqm <sunnylqm@qq.com>
2015-12-26 22:23:08 +0800

    add react-native version output for -v/--version

--- react-native-cli/index.js
+++ react-native-cli/index.js
@@ -221,8 +221,12 @@ function checkNodeVersion() {
 
 function checkForVersionArgument() {
   if (process.argv.indexOf('-v') >= 0 || process.argv.indexOf('--version') >= 0) {
-    var pjson = require('./package.json');
-    console.log(pjson.version);
+    console.log('react-native-cli: ' + require('./package.json').version);
+    try {
+      console.log('react-native: ' + require(REACT_NATIVE_PACKAGE_JSON_PATH()).version);
+    } catch (e) {
+      console.log('react-native: n/a - not inside a React Native project directory')
+    }
     process.exit();
   }
 }
