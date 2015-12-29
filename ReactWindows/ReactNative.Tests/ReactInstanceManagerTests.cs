2334bcd1d3a1524971bfa785a6e5356c26b4a66c
Erik Schlegel <erik.schlegel@gmail.com>
2015-12-29 09:19:51 -0500

    Fixing issue with unit test and variuos bugs in ReactInstanceManager

--- ReactWindows/ReactNative.Tests/ReactInstanceManagerTests.cs
+++ ReactWindows/ReactNative.Tests/ReactInstanceManagerTests.cs
@@ -1,25 +1,20 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-using ReactNative.Bridge.Queue;
-using ReactNative.Shell;
 using ReactNative.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
 namespace ReactNative.Tests
 {
     [TestClass]
     public class ReactInstanceManagerTests
     {
         [TestMethod]
-        public void ReactInstanceManagerInitializationSuccess()
+        public async Task ReactInstanceManagerInitializationSuccess()
         {
             var jsModuleName = "index.windows";
             var bundleAssetName = "ms-appx:///Resources/index.windows.bundle.js";
-            var rootView = new ReactRootView();
-                rootView.Lift(bundleAssetName, jsModuleName);
-            Assert.IsTrue(rootView.TagId > 0);
+            var rootView = await DispatcherHelpers.CallOnDispatcherAsync(() => new ReactRootView());
+            await DispatcherHelpers.CallOnDispatcherAsync(() => rootView.Lift(bundleAssetName, jsModuleName));
+            Assert.AreEqual(rootView.TagId, 0);
         }
     }
 }
