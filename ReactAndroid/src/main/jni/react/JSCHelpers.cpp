1d1c891b78e62143151b5a51e9427cfdb737c207
Andy Street <andrews@fb.com>
2015-12-29 19:47:22 -0800

    WebWorkers: Extract evaluateScript logic to JSCHelpers
    Reviewed By: lexs
    
    Differential Revision: D2779277
    
    fb-gh-sync-id: ab9a040193f36f40a4a34229a4a90199537253aa

--- ReactAndroid/src/main/jni/react/JSCHelpers.cpp
+++ ReactAndroid/src/main/jni/react/JSCHelpers.cpp
@@ -3,6 +3,10 @@
 #include "JSCHelpers.h"
 
 #include <JavaScriptCore/JSStringRef.h>
+#include <fb/log.h>
+#include <jni/fbjni/Exceptions.h>
+
+#include "Value.h"
 
 namespace facebook {
 namespace react {
@@ -26,6 +30,26 @@ JSValueRef makeJSCException(
   JSValueRef exceptionString = JSValueMakeString(ctx, message);
   JSStringRelease(message);
   return JSValueToObject(ctx, exceptionString, NULL);
+}
+
+JSValueRef evaluateScript(JSContextRef context, JSStringRef script, JSStringRef source) {
+  JSValueRef exn;
+  auto result = JSEvaluateScript(context, script, NULL, source, 0, &exn);
+  if (result == nullptr) {
+    Value exception = Value(context, exn);
+    std::string exceptionText = exception.toString().str();
+    FBLOGE("Got JS Exception: %s", exceptionText.c_str());
+    auto line = exception.asObject().getProperty("line");
+
+    std::ostringstream lineInfo;
+    if (line != nullptr && line.isNumber()) {
+      lineInfo << " (line " << line.asInteger() << " in the generated bundle)";
+    } else {
+      lineInfo << " (no line info)";
+    }
+    throwJSExecutionException("%s%s", exceptionText.c_str(), lineInfo.str().c_str());
+  }
+  return result;
 }
 
 } }
