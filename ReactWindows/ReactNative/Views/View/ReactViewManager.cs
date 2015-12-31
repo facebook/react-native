2e083438ee2c6b45b2643b1b3682390635fc9724
Erik Schlegel <erik.schlegel@gmail.com>
2015-12-31 11:26:35 -0500

    Adding ReactviewManager Stub

--- ReactWindows/ReactNative/Views/View/ReactViewManager.cs
+++ ReactWindows/ReactNative/Views/View/ReactViewManager.cs
@@ -4,11 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace ReactNative.Views.View
 {
     public class ReactViewManager : ViewGroupManager
     {
+        public static readonly string REACT_CLASS = ViewProperties.ViewClassName;
 
+        public override string Name
+        {
+            get
+            {
+                return REACT_CLASS;
+            }
+        }
+
+        protected override Panel CreateViewInstance(ThemedReactContext reactContext)
+        {
+            return new ReactViewGroup();
+        }
     }
 }
