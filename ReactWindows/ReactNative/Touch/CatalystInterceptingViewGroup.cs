2e063fcd62dd5b92a651af18ba1284ababeac21d
Erik Schlegel <erik.schlegel@gmail.com>
2015-12-31 11:26:35 -0500

    Adding ReactviewManager Stub

---
+++ ReactWindows/ReactNative/Touch/CatalystInterceptingViewGroup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactNative.UIManager;
+using ReactNative.touch;
+
+namespace ReactNative.Touch
+{
+    /// <summary>
+    /// This interface should be implemented by all <see cref="Panel"/> subviews that can 
+    /// be instantiating by <see cref="NativeViewHierarchyManager"/>
+    /// </summary>
+    public interface CatalystInterceptingViewGroup
+    {
+        /// <summary>
+        /// A callback that <see cref="Panel"/> should delage calls for <see cref="Panel.GotTouchCaptureEvent"/>
+        /// </summary>
+        /// <param name="listener">The touch event listener</param>
+        void setOnInterceptTouchEventListener(OnInterceptTouchEventListener listener);
+    }
+}
---
+++ ReactWindows/ReactNative/Views/ReactViewManager.cs
@@ -0,0 +1,14 @@
+﻿using ReactNative.UIManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactNative.Views.View
+{
+    public class ReactViewManager : ViewGroupManager
+    {
+
+    }
+}
