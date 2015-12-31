52b8b626cebd2f4762bc931bf5976f9f958d86ca
Erik Schlegel <erik.schlegel@gmail.com>
2015-12-31 11:26:35 -0500

    Adding ReactviewManager Stub

---
+++ ReactWindows/ReactNative/Views/View/ReactViewGroup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactNative.touch;
+using Windows.UI.Xaml.Controls;
+using ReactNative.Touch;
+
+namespace ReactNative.Views.View
+{
+    /// <summary>
+    /// Backing for a React View. Has support for borders, but since borders 
+    /// aren't common, lazy initializes most of the storage needed for them.
+    /// </summary>
+    class ReactViewGroup : Panel, CatalystInterceptingViewGroup
+    {
+        public void setOnInterceptTouchEventListener(OnInterceptTouchEventListener listener)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
