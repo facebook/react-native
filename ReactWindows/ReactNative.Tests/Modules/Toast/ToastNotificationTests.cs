22dd2fd685ea93d608f4f79b4369c6fd41e500bb
Shawn Cicoria <github@cicoria.com>
2016-01-05 09:31:50 -0500

    adding ToastModule with tests

---
+++ ReactWindows/ReactNative.Tests/Modules/Toast/ToastNotificationTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using ReactNative.Bridge;
+using ReactNative.Modules.Toast;
+using System;
+
+namespace ReactNative.Tests.Modules.Toast
+{
+    [TestClass]
+    public class ToastNotificationTests
+    {
+        const string TEST_CATEGORY = "Modules";
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void ToastModule_Null_ArgumentsTest()
+        {
+            AssertEx.Throws<ArgumentNullException>(
+                () => new ToastModule(null),
+                ex => Assert.AreEqual("reactContext", ex.ParamName));
+
+
+            var context = new ReactApplicationContext();
+            var module = new ToastModule(context);
+            Assert.AreSame(context, module.Context);
+
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void Send_Toast_Invalid_Duration()
+        {
+            var context = new ReactApplicationContext();
+            var module = new ToastModule(context);
+
+            AssertEx.Throws<ArgumentException>(
+               () => module.show("Invalid Toast", -1),
+               ex => Assert.AreEqual("duration", ex.ParamName));
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void Send_Basic_Toast()
+        {
+            var context = new ReactApplicationContext();
+            var module = new ToastModule(context);
+
+            module.show("SHORT TOAST", 0);
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void Send_Long_Toast()
+        {
+            var context = new ReactApplicationContext();
+            var module = new ToastModule(context);
+
+            module.show("LONG TOAST container", 1);
+        }
+
+
+    }
+}
---
+++ ReactWindows/ReactNative/Modules/Toast/ToastHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace ReactNative.Modules.Toast
+{
+    class ToastHelper
+    {
+        internal static void SendToast(string message, string duration = "short")
+        {
+            var doc = MakeDoc(message).Result;
+            var notification = new ToastNotification(doc);
+
+            // Get the toast notification manager for the current app.
+            ToastNotificationManager.CreateToastNotifier().Show(notification);
+        }
+
+        static async Task<XmlDocument> MakeDoc(string content, Durations duration = Durations.@short)
+        {
+            /// TODO: alt templates: https://msdn.microsoft.com/en-us/library/windows/apps/hh761494.aspx
+            using (var stream = new MemoryStream())
+            using (var reader = new StreamReader(stream))
+            {
+                await BuildNotificationXml(stream, content, duration);
+                stream.Position = 0;
+                var xml = await reader.ReadToEndAsync();
+                var doc = new XmlDocument();
+                doc.LoadXml(xml);
+                return doc;
+            }
+        }
+
+        static async Task BuildNotificationXml(Stream stream, string content, Durations duration = Durations.@short)
+        {
+            System.Xml.XmlWriterSettings settings = new System.Xml.XmlWriterSettings();
+            settings.OmitXmlDeclaration = true;
+            settings.Async = true;
+
+            using (System.Xml.XmlWriter writer = System.Xml.XmlWriter.Create(stream, settings))
+            {
+                await writer.WriteStartElementAsync(null, "toast", null);
+                await writer.WriteAttributeStringAsync(null, "activationType", null, "foreground");
+                await writer.WriteAttributeStringAsync(null, "duration", null, duration.ToString());
+
+                await writer.WriteStartElementAsync(null, "visual", null);
+                await writer.WriteStartElementAsync(null, "binding", null);
+                await writer.WriteAttributeStringAsync(null, "template", null, "ToastText01");
+
+                await writer.WriteStartElementAsync(null, "text", null);
+                await writer.WriteAttributeStringAsync(null, "id", null, "1");
+                await writer.WriteStringAsync(content);
+                await writer.WriteEndElementAsync();
+
+                await writer.WriteEndElementAsync();
+                await writer.FlushAsync();
+            }
+        }
+    }
+}
