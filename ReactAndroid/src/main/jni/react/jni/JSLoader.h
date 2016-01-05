14a7d66288bb4bde746f44a73fc7bb0aa0ceceb9
Fada Chen <chenfada@fb.com>
2016-01-05 00:40:54 -0800

    revert D2779079
    Reviewed By: zjj010104
    
    Differential Revision: D2802728
    
    fb-gh-sync-id: 9e09f00d1d7facd2091fe60eee8565c4bf599b1d

--- ReactAndroid/src/main/jni/react/jni/JSLoader.h
+++ ReactAndroid/src/main/jni/react/jni/JSLoader.h
@@ -9,12 +9,6 @@ namespace facebook {
 namespace react {
 
 /**
- * Helper method for loading a JS script from Android assets without
- * a reference to an AssetManager.
- */
-std::string loadScriptFromAssets(std::string assetName);
-
-/**
  * Helper method for loading JS script from android asset
  */
 std::string loadScriptFromAssets(JNIEnv *env, jobject assetManager, std::string assetName);
@@ -23,7 +17,5 @@ std::string loadScriptFromAssets(JNIEnv *env, jobject assetManager, std::string 
  * Helper method for loading JS script from a file
  */
 std::string loadScriptFromFile(std::string fileName);
-
-void registerJSLoaderNatives();
 
 } }
