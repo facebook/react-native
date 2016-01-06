3517b46fd6e1ff9baf0cb8566b7d70be185ef2f3
Eric Rozell <erozell@outlook.com>
2016-01-06 11:45:16 -0500

    Fixes #92 - Adds basic ReactTextViewManager without inline images
    Still remaining TODOs for text measurement and inline images.

---
+++ ReactWindows/ReactNative/Views/Text/ReactVirtualTextViewManager.cs
@@ -0,0 +1,15 @@
+﻿namespace ReactNative.Views.Text
+{
+    public class ReactVirtualTextViewManager : ReactRawTextManager
+    {
+        private const string ReactClass = "RCTVirtualText";
+
+        public override string Name
+        {
+            get
+            {
+                return ReactClass;
+            }
+        }
+    }
+}
