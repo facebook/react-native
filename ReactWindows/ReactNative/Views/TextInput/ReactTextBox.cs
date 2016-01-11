395491b9f0f4948278a41ab4848d90ce16f773d9
Erik Schlegel <erik.schlegel@gmail.com>
2016-01-11 16:57:40 -0500

    Text Inbox Shadow Node Changes

---
+++ ReactWindows/ReactNative/Views/TextInput/ReactTextBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace ReactNative.Views.TextInput
+{
+    /// <summary>
+    /// A Data model which holds measurement related styling updates for <see cref="TextBox"/>.
+    /// </summary>
+    public class ReactTextBox
+    {
+        private const int UNSET = -1;
+
+        /// <summary>
+        /// Sets / Gets the padding thickness of a <see cref="TextBox"/>.
+        /// </summary>
+        public Thickness? Padding { get; set; }
+
+        /// <summary>
+        /// Sets / Gets the <see cref="FontStyle"/> of a <see cref="TextBox"/>.
+        /// </summary>
+        public FontStyle? FontStyle { get; }
+
+        /// <summary>
+        /// Sets / Gets the <see cref="FontWeight"/> of a <see cref="TextBox"/>.
+        /// </summary>
+        public FontWeight? FontWeight { get; set; }
+
+        /// <summary>
+        /// Sets / Gets the font family of a <see cref="TextBox"/>.
+        /// </summary>
+        public FontFamily FontFamily { get; set; }
+
+        /// <summary>
+        /// Sets / Gets the font size of a <see cref="TextBox"/>.
+        /// </summary>
+        public int FontSize { get; set; } = UNSET;
+
+        /// <summary>
+        /// Sets / Gets the text value of a <see cref="TextBox"/>.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Sets / Gets the line height of a <see cref="TextBox"/>.
+        /// </summary>
+        public int LineHeight { get; set; } = UNSET;
+
+
+    }
+}
--- ReactWindows/ReactNative/Views/TextInput/ReactTextInputShadowNode.cs
+++ ReactWindows/ReactNative/Views/TextInput/ReactTextInputShadowNode.cs
@@ -1,24 +1,28 @@
 ﻿using Facebook.CSSLayout;
+using ReactNative.Bridge;
+using ReactNative.UIManager;
 using ReactNative.Views.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace ReactNative.Views.TextInput
 {
-    public class ReactTextInputShadowNode : ReactTextShadowNode
+    public class ReactTextInputShadowNode : LayoutShadowNode
     {
-        
-        private Style _style;
+        private ReactTextBox _textBoxStyle;
         private readonly bool _isVirtual;
-        
-        public ReactTextInputShadowNode(bool isVirtual) : base(false)
+        public const int UNSET = -1;
+
+        public ReactTextInputShadowNode(bool isVirtual)
         {
+            _textBoxStyle = new ReactTextBox();
             _isVirtual = isVirtual;
 
             if (!isVirtual)
@@ -27,32 +31,112 @@ namespace ReactNative.Views.TextInput
             }
         }
 
+        public override bool IsVirtual
+        {
+            get
+            {
+                return _isVirtual;
+            }
+        }
+
+        protected override void MarkUpdated()
+        {
+            base.MarkUpdated();
+
+            if (!_isVirtual)
+            {
+                dirty();
+            }
+        }
+
         private static MeasureOutput MeasureText(CSSNode node, float width, float height)
         {
-            // This is not a terribly efficient way of projecting the height of
-            // the text elements. It requires that we have access to the
-            // dispatcher in order to do measurement, which, for obvious
-            // reasons, can cause perceived performance issues as it will block
-            // the UI thread from handling other work.
-            //
-            // TODO: determine another way to measure text elements.
+            var shadowNode = (ReactTextInputShadowNode)node;
+            var textBlock = ReactTextBoxToNativeTextBoxForMeasurement(shadowNode.textBoxStyle);
 
-            var shadowNode = (ReactTextShadowNode)node;
-            var textBlock = new TextBlock();
-            textBlock.Inlines.Add(shadowNode._inline);
-
-            try
-            {
-                var adjustedWidth = float.IsNaN(width) ? double.PositiveInfinity : width;
-                var adjustedHeight = float.IsNaN(height) ? double.PositiveInfinity : height;
+            var adjustedHeight = float.IsNaN(height) ? double.PositiveInfinity : height;
                 textBlock.Measure(new Size(width, adjustedHeight));
-                return new MeasureOutput(
-                    (float)textBlock.DesiredSize.Width,
-                    (float)textBlock.DesiredSize.Height);
-            }
-            finally
+
+            return new MeasureOutput((float)textBlock.DesiredSize.Width, (float)textBlock.DesiredSize.Height);
+        }
+
+        /// <summary>
+        /// This lifecycle method is called by <see cref="UIImplementation"/> to bind the CSS styling to the <see cref="ReactTextInputShadowNode"/>.
+        /// </summary>
+        public override void OnBeforeLayout()
+        {
+            DispatcherHelpers.AssertOnDispatcher();
+
+            if (_isVirtual)
             {
-                textBlock.Inlines.Clear();
+                return;
+            }
+            
+            MarkUpdated();
+        }
+
+        private static TextBox ReactTextBoxToNativeTextBoxForMeasurement(ReactTextBox textBoxStyle)
+        {
+                var textBox = new TextBox() {
+                    Text = textBoxStyle.Text != null ? textBoxStyle.Text : ""
+                };
+
+                if (textBoxStyle.FontWeight.HasValue)
+                {
+                    textBox.FontWeight = textBoxStyle.FontWeight.Value;
+                }
+
+                if (textBoxStyle.FontStyle.HasValue)
+                {
+                    textBox.FontStyle = textBoxStyle.FontStyle.Value;
+                }
+
+                if (textBoxStyle.FontSize != UNSET)
+                {
+                    textBox.FontSize = textBoxStyle.FontSize;
+                }
+
+                if (textBoxStyle.FontFamily != null)
+                {
+                    textBox.FontFamily = textBoxStyle.FontFamily;
+                }
+
+                if (textBoxStyle.Padding.HasValue)
+                {
+                    textBox.Padding = textBoxStyle.Padding.Value;
+                }
+
+            return textBox;
+        }
+        
+        [ReactPropertyGroup(
+            ViewProperties.Padding,
+            ViewProperties.PaddingVertical,
+            ViewProperties.PaddingHorizontal,
+            ViewProperties.PaddingLeft,
+            ViewProperties.PaddingRight,
+            ViewProperties.PaddingTop,
+            ViewProperties.PaddingBottom)]
+        public override void SetPaddings(int index, float padding)
+        {
+            base.SetPaddings(index, padding);
+            _textBoxStyle.Padding = PaddingThickness;
+            MarkUpdated();
+        }
+
+        [ReactProperty("text")]
+        public void SetText(string text)
+        {
+            _textBoxStyle.Text = text;
+            MarkUpdated();
+        }
+
+        private Thickness PaddingThickness
+        {
+            get
+            {
+                return new Thickness(GetPadding(CSSSpacingType.Left), GetPadding(CSSSpacingType.Top),
+                                     GetPadding(CSSSpacingType.Right), GetPadding(CSSSpacingType.Bottom));
             }
         }
     }
