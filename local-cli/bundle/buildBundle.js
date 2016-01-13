e51a94a57369d5ecb8fab5a7ca59559b30b07ff2
Adam Miskiewicz <adam.skevy@mac.com>
2016-01-13 13:31:01 -0500

    Fixing CLI to pass "sourcemap-output" option to bundler.

--- local-cli/bundle/buildBundle.js
+++ local-cli/bundle/buildBundle.js
@@ -33,6 +33,7 @@ function buildBundle(args, config, output = outputBundle) {
 
     const requestOpts = {
       entryFile: args['entry-file'],
+      sourceMapUrl: args['sourcemap-output'],
       dev: args.dev,
       minify: !args.dev,
       platform: args.platform,
