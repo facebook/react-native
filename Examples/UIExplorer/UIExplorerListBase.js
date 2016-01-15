cd89016ee7168bb6971f800779e0878e9a70206f
Kyle Corbitt <kyle@corbt.com>
2016-01-15 05:14:27 -0800

    PixelRatio.pixel()
    Summary:
    This implements #5073. It adds a static method `PixelRatio.pixel()` which returns the smallest drawable line width, primarily for use in styles.
    
    It also updates the example apps to use the new function.
    Closes https://github.com/facebook/react-native/pull/5076
    
    Reviewed By: svcscm
    
    Differential Revision: D2799849
    
    Pulled By: nicklockwood
    
    fb-gh-sync-id: b83a77790601fe882affbf65531114e7c5cf4bdf

--- Examples/UIExplorer/UIExplorerListBase.js
+++ Examples/UIExplorer/UIExplorerListBase.js
@@ -18,7 +18,6 @@
 var React = require('react-native');
 var {
   ListView,
-  PixelRatio,
   StyleSheet,
   Text,
   TextInput,
@@ -159,7 +158,7 @@ var styles = StyleSheet.create({
     paddingVertical: 8,
   },
   separator: {
-    height: 1 / PixelRatio.get(),
+    height: StyleSheet.hairlineWidth,
     backgroundColor: '#bbbbbb',
     marginLeft: 15,
   },
