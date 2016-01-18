493e56df60aed319c3e4768a3d1f66ff5c955296
Elliot Hesp <elliot.hesp@gmail.com>
2016-01-18 10:00:18 +0000

    Use arrow function over bind

--- Libraries/Components/Touchable/Touchable.js
+++ Libraries/Components/Touchable/Touchable.js
@@ -668,10 +668,9 @@ var TouchableMixin = {
       this.touchableHandleActivePressIn && this.touchableHandleActivePressIn(e);
     } else if (!newIsHighlight && curIsHighlight && this.touchableHandleActivePressOut) {
       if (this.touchableGetPressOutDelayMS && this.touchableGetPressOutDelayMS()) {
-        this.pressOutDelayTimeout = setTimeout(
-          this.touchableHandleActivePressOut.bind(this, e),
-          this.touchableGetPressOutDelayMS()
-        );
+        this.pressOutDelayTimeout = setTimeout(() => {
+          this.touchableHandleActivePressOut(e);
+        }, this.touchableGetPressOutDelayMS());
       } else {
         this.touchableHandleActivePressOut(e);
       }
