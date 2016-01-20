13a98931f61cf5d430ee9a69c6704faad3ade841
Sebastian Gronewold <sebastian@elbstack.com>
2016-01-20 21:35:51 +0100

    Navigator - velocity override
    This code change allows changing the transition velocity of the navigator
    when switching views.
    
    Whats the use case? If you have different types of
    animations your animations might also need different transition velocities.

--- Libraries/CustomComponents/Navigator/Navigator.js
+++ Libraries/CustomComponents/Navigator/Navigator.js
@@ -917,16 +917,15 @@ var Navigator = React.createClass({
     var activeAnimationConfigStack = this.state.sceneConfigStack.slice(0, activeLength);
     var nextStack = activeStack.concat([route]);
     var destIndex = nextStack.length - 1;
-    var nextAnimationConfigStack = activeAnimationConfigStack.concat([
-      this.props.configureScene(route, nextStack),
-    ]);
+    var nextSceneConfig = this.props.configureScene(route, nextStack);
+    var nextAnimationConfigStack = activeAnimationConfigStack.concat([nextSceneConfig]);
     this._emitWillFocus(nextStack[destIndex]);
     this.setState({
       routeStack: nextStack,
       sceneConfigStack: nextAnimationConfigStack,
     }, () => {
       this._enableScene(destIndex);
-      this._transitionTo(destIndex);
+      this._transitionTo(destIndex, nextSceneConfig.defaultTransitionVelocity);
     });
   },
 
