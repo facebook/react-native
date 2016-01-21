e8ad45e4f21631c56cd02684b369a4459c2076cf
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-01-21 20:40:02 +0530

    Added comments for the 'Experiment' class

--- ReactAndroid/src/main/java/com/facebook/react/Experiments.java
+++ ReactAndroid/src/main/java/com/facebook/react/Experiments.java
@@ -1,14 +1,34 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 package com.facebook.react;
 
 import java.util.HashSet;
 import java.util.Set;
 
+
+/**
+ * This class exposes a way to enable/disable exprimental features in React Native.
+ */
+
 public class Experiments {
 
+  // Enables Hot Module Replacement menu item in the developer menu
   public static String HOT_MODULE_REPLACEMENT = "HOT_MODULE_REPLACEMENT";
 
   private static Set<String> enabledExperiments;
 
+  /**
+   * Enables an experiment
+   *
+   * @param experiment - the experiment to enable
+   */
   public static void enableExperiment(String experiment) {
     if (enabledExperiments == null) {
       enabledExperiments = new HashSet<>();
@@ -16,12 +36,22 @@ public class Experiments {
     enabledExperiments.add(experiment);
   }
 
+  /**
+   * Disbales an expriment
+   *
+   * @param experiment - the experiment to disable
+   */
   public static void disableExperiment(String experiment) {
     if (enabledExperiments != null) {
       enabledExperiments.remove(experiment);
     }
   }
 
+  /**
+   * Checks if an expriment is enabled
+   *
+   * @param experiment - the experiment to check for
+   */
   public static boolean isExperimentEnabled(String experiment) {
     return enabledExperiments != null && enabledExperiments.contains(experiment);
   }
