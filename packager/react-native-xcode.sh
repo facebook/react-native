f5dfa2b6abf992bc0e76e51f51e43ec6e5046a19
Alex Kotliarskyi <alex.frantic@gmail.com>
2016-01-26 08:20:18 -0800

    Skip bundling for Simulator
    Summary:
    Following up on the conversation in https://www.prod.facebook.com/groups/reactnativeoss/permalink/1516993445263951/ I currently don't see any good reason to create an offline bundle for simulator builds.
    Closes https://github.com/facebook/react-native/pull/5519
    
    Reviewed By: svcscm
    
    Differential Revision: D2859751
    
    Pulled By: mkonicek
    
    fb-gh-sync-id: f70481e447e258f5531de773729fc31d9ebec6f7

--- packager/react-native-xcode.sh
+++ packager/react-native-xcode.sh
@@ -10,6 +10,13 @@
 # This script is supposed to be invoked as part of Xcode build process
 # and relies on envoronment variables (including PWD) set by Xcode
 
+# There is no point in creating an offline package for simulator builds
+# because the packager is supposed to be running during development anyways
+if [[ "$PLATFORM_NAME" = "iphonesimulator" ]]; then
+  echo "Skipping bundling for Simulator platform"
+  exit 0;
+fi
+
 case "$CONFIGURATION" in
   Debug)
     DEV=true
