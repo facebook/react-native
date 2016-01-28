8ef9615b81374aca0c95473a5cd5d2f4175e62d0
Christopher Chedeau <vjeuxx@gmail.com>
2016-01-27 17:05:33 -0800

    Remove min/max Width/Height in the docs
    Summary:
    An initial implementation was done on css-layout but isn't working correctly on many cases. The binding from React Native has been removed a long time ago. Let's not confuse people and remove it from the docs :)
    Closes https://github.com/facebook/react-native/pull/5522
    
    Reviewed By: svcscm
    
    Differential Revision: D2859665
    
    Pulled By: vjeux
    
    fb-gh-sync-id: 4aa008dd93a6cea6b79a7bce444c94148791eee4

--- React/Views/RCTShadowView.m
+++ React/Views/RCTShadowView.m
@@ -507,10 +507,6 @@ RCT_BORDER_PROPERTY(Right, RIGHT)
 
 RCT_DIMENSIONS_PROPERTY(Width, width, WIDTH, dimensions)
 RCT_DIMENSIONS_PROPERTY(Height, height, HEIGHT, dimensions)
-RCT_DIMENSIONS_PROPERTY(MinWidth, minWidth, WIDTH, minDimensions)
-RCT_DIMENSIONS_PROPERTY(MinHeight, minHeight, HEIGHT, minDimensions)
-RCT_DIMENSIONS_PROPERTY(MaxWidth, maxWidth, WIDTH, maxDimensions)
-RCT_DIMENSIONS_PROPERTY(MaxHeight, maxHeight, HEIGHT, maxDimensions)
 
 // Position
 
