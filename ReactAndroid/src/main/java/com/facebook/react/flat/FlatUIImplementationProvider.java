a52c89b01d851ec72bb8eeda70d95f2bf548d120
Denis Koroskin <dkoroskin@fb.com>
2016-01-28 14:39:19 -0800

    Extract UIImplementationProvider for Nodes into a helper class
    Summary: Exact same class is needed for Groups, and probably for other apps that want to work with Nodes, too, so let's extract it into a helper class to simplify coding.
    
    Reviewed By: sriramramani
    
    Differential Revision: D2873943

---
+++ ReactAndroid/src/main/java/com/facebook/react/flat/FlatUIImplementationProvider.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.react.flat;
+
+import java.util.List;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.UIImplementationProvider;
+import com.facebook.react.uimanager.ViewManager;
+
+/**
+ * UIImplementationProvider that creates instances of {@link FlatUIImplementation}.
+ */
+public final class FlatUIImplementationProvider extends UIImplementationProvider {
+  @Override
+  public FlatUIImplementation createUIImplementation(
+      ReactApplicationContext reactContext,
+      List<ViewManager> viewManagers) {
+    return FlatUIImplementation.createInstance(reactContext, viewManagers);
+  }
+}
