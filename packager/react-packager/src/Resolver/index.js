c959e10edc306655496d685560d0fd52c1612c48
Dmitry Soshnikov <dmitrys@fb.com>
2016-01-28 17:20:39 -0800

    Add Object.entries and Object.values polyfill
    Reviewed By: martinbigio
    
    Differential Revision: D2876430
    
    fb-gh-sync-id: 25c7680a71c9187beb937f6faf030a83c9c81fc5

--- packager/react-packager/src/Resolver/index.js
+++ packager/react-packager/src/Resolver/index.js
@@ -157,6 +157,7 @@ class Resolver {
       path.join(__dirname, 'polyfills/String.prototype.es6.js'),
       path.join(__dirname, 'polyfills/Array.prototype.es6.js'),
       path.join(__dirname, 'polyfills/Array.es6.js'),
+      path.join(__dirname, 'polyfills/Object.es7.js'),
       path.join(__dirname, 'polyfills/babelHelpers.js'),
     ].concat(this._polyfillModuleNames);
 
