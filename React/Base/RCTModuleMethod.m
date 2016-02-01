b9550fd5d6e9035b65fc53211ce1e09c0ecd1e47
Christopher Chedeau <vjeuxx@gmail.com>
2016-02-01 10:38:58 -0800

    Update RCTModuleMethod.m

--- React/Base/RCTModuleMethod.m
+++ React/Base/RCTModuleMethod.m
@@ -449,7 +449,7 @@ SEL RCTParseMethodSignature(NSString *methodSignature, NSArray<RCTMethodArgument
       }
 
       RCTLogError(@"%@.%@ was called with %zd arguments, but expects %zd. \
-                  If you haven\'t changed this prop yourself, this usually means that \
+                  If you haven\'t changed this method yourself, this usually means that \
                   your versions of the native code and JavaScript code are out of sync. \
                   Updating both should make this error go away.",
                   RCTBridgeModuleNameForClass(_moduleClass), _JSMethodName,
