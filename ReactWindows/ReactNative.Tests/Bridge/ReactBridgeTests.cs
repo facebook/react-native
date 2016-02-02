c46ddc30fcbee44e01c0c439e85d4e0ed1ceb1e4
Eric Rozell <erozell@outlook.com>
2016-02-02 15:27:42 -0500

    Hosting was a bad name for the Chakra namespace, calling a spade a spade.

---
+++ ReactWindows/ReactNative.Tests/Bridge/ReactBridgeTests.cs
@@ -0,0 +1,233 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ReactNative.Bridge;
+using ReactNative.Bridge.Queue;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReactNative.Tests.Bridge
+{
+    [TestClass]
+    public class ReactBridgeTests
+    {
+        [TestMethod]
+        public async Task ReactBridge_Ctor_ArgumentChecks()
+        {
+            await JavaScriptHelpers.Run((executor, jsQueueThread) =>
+            {
+                using (var nativeThread = CreateNativeModulesThread())
+                {
+                    var reactCallback = new MockReactCallback();
+
+                    AssertEx.Throws<ArgumentNullException>(
+                        () => new ReactBridge(null, reactCallback, nativeThread),
+                        ex => Assert.AreEqual("jsExecutor", ex.ParamName));
+
+                    AssertEx.Throws<ArgumentNullException>(
+                        () => new ReactBridge(executor, null, nativeThread),
+                        ex => Assert.AreEqual("reactCallback", ex.ParamName));
+
+                    AssertEx.Throws<ArgumentNullException>(
+                        () => new ReactBridge(executor, reactCallback, null),
+                        ex => Assert.AreEqual("nativeModulesQueueThread", ex.ParamName));
+                }
+            });
+        }
+
+        [TestMethod]
+        public async Task ReactBridge_Method_ArgumentChecks()
+        {
+            await JavaScriptHelpers.Run((executor, jsQueueThread) =>
+            {
+                using (var nativeThread = CreateNativeModulesThread())
+                {
+                    var reactCallback = new MockReactCallback();
+                    var bridge = new ReactBridge(executor, reactCallback, nativeThread);
+
+                    AssertEx.Throws<ArgumentNullException>(
+                        () => bridge.SetGlobalVariable(null, null),
+                        ex => Assert.AreEqual("propertyName", ex.ParamName));
+                }
+            });
+        }
+
+        [TestMethod]
+        public async Task ReactBridge_CallFunction()
+        {
+            await JavaScriptHelpers.Run(async (executor, jsQueueThread) =>
+            {
+                using (var nativeThread = CreateNativeModulesThread())
+                {
+                    var reactCallback = new MockReactCallback();
+                    var bridge = new ReactBridge(executor, new MockReactCallback(), nativeThread);
+                    var token = await jsQueueThread.CallOnQueue(() =>
+                    {
+                        bridge.CallFunction(1, 1, new JArray());
+                        return executor.GetGlobalVariable("FunctionCalls");
+                    });
+
+                    var expected = new JArray
+                    {
+                        new JArray
+                        {
+                            1,
+                            1,
+                            new JArray(),
+                        },
+                    };
+
+                    Assert.AreEqual(expected.ToString(Formatting.None), token.ToString(Formatting.None));
+                }
+            });
+        }
+
+        [TestMethod]
+        public async Task ReactBridge_InvokeCallback()
+        {
+            await JavaScriptHelpers.Run(async (executor, jsQueueThread) =>
+            {
+                using (var nativeThread = MessageQueueThread.Create(MessageQueueThreadSpec.Create("native", MessageQueueThreadKind.BackgroundAnyThread), ex => { Assert.Fail(); }))
+                {
+                    var reactCallback = new MockReactCallback();
+                    var bridge = new ReactBridge(executor, new MockReactCallback(), nativeThread);
+                    var token = await jsQueueThread.CallOnQueue(() =>
+                    {
+                        bridge.InvokeCallback(1, new JArray());
+                        return executor.GetGlobalVariable("CallbackCalls");
+                    });
+
+                    var expected = new JArray
+                    {
+                        new JArray
+                        {
+                            1,
+                            new JArray(),
+                        },
+                    };
+
+                    Assert.AreEqual(expected.ToString(Formatting.None), token.ToString(Formatting.None));
+                }
+            });
+        }
+
+        [TestMethod]
+        public void ReactBridge_ReactCallbacks()
+        {
+            using (var nativeThread = CreateNativeModulesThread())
+            {
+                var jsonResponse = JArray.Parse("[[42,17],[16,22],[[],[\"foo\"]]]");
+
+                var executor = new MockJavaScriptExecutor((module, method, args) =>
+                {
+                    return jsonResponse;
+                });
+
+                var callbacks = new List<Tuple<int, int, JArray>>();
+                var eventHandler = new AutoResetEvent(false);
+                var callback = new MockReactCallback(
+                    (moduleId, methodId, args) => callbacks.Add(Tuple.Create(moduleId, methodId, args)),
+                    () => eventHandler.Set());
+
+                var bridge = new ReactBridge(executor, callback, nativeThread);
+                bridge.CallFunction(0, 0, new JArray());
+
+                Assert.IsTrue(eventHandler.WaitOne());
+
+                Assert.AreEqual(2, callbacks.Count);
+
+                Assert.AreEqual(42, callbacks[0].Item1);
+                Assert.AreEqual(16, callbacks[0].Item2);
+                Assert.AreEqual(0, callbacks[0].Item3.Count);
+
+                Assert.AreEqual(17, callbacks[1].Item1);
+                Assert.AreEqual(22, callbacks[1].Item2);
+                Assert.AreEqual(1, callbacks[1].Item3.Count);
+                Assert.AreEqual("foo", callbacks[1].Item3[0].Value<string>());
+            }
+        }
+
+        [TestMethod]
+        public void ReactBridge_InvalidJavaScriptResponse()
+        {
+            var responses = new[]
+            {
+                JArray.Parse("[null,[],[]]"),
+                JArray.Parse("[[],null,[]]"),
+                JArray.Parse("[[],[],null]"),
+                JArray.Parse("[[42],[],[]]"),
+                JArray.Parse("[[],[42],[]]"),
+                JArray.Parse("[[],[],[42]]"),
+            };
+
+            var n = responses.Length;
+            using (var nativeThread = CreateNativeModulesThread())
+            {
+                var count = 0;
+                var executor = new MockJavaScriptExecutor((module, method, args) =>
+                {
+                    return responses[count++];
+                });
+
+                var bridge = new ReactBridge(executor, new MockReactCallback(), nativeThread);
+
+                for (var i = 0; i < n; ++i)
+                {
+                    AssertEx.Throws<InvalidOperationException>(() => bridge.CallFunction(0, 0, new JArray()));
+                }
+
+                Assert.AreEqual(n, count);
+            }
+        }
+
+        private static MessageQueueThread CreateNativeModulesThread()
+        {
+            return CreateNativeModulesThread(ex => Assert.Fail(ex.ToString()));
+        }
+
+        private static MessageQueueThread CreateNativeModulesThread(Action<Exception> exceptionHandler)
+        {
+            return MessageQueueThread.Create(
+                MessageQueueThreadSpec.Create("native", MessageQueueThreadKind.BackgroundAnyThread), exceptionHandler);
+        }
+
+        class MockReactCallback : IReactCallback
+        {
+            private readonly Action<int, int, JArray> _invoke;
+            private readonly Action _onBatchComplete;
+
+            public MockReactCallback()
+                : this(() => { })
+            {
+            }
+
+            public MockReactCallback(Action<int, int, JArray> invoke)
+                : this(invoke, () => { })
+            {
+            }
+
+            public MockReactCallback(Action onBatchComplete)
+                : this((p0, p1, p2) => { }, onBatchComplete)
+            {
+            }
+
+            public MockReactCallback(Action<int, int, JArray> invoke, Action onBatchComplete)
+            {
+                _invoke = invoke;
+                _onBatchComplete = onBatchComplete;
+            }
+
+            public void Invoke(int moduleId, int methodId, JArray parameters)
+            {
+                _invoke(moduleId, methodId, parameters);
+            }
+
+            public void OnBatchComplete()
+            {
+                _onBatchComplete();
+            }
+        }
+    }
+}
---
+++ ReactWindows/ReactNative/Bridge/ReactBridge.cs
@@ -0,0 +1,145 @@
+﻿using Newtonsoft.Json.Linq;
+using ReactNative.Bridge.Queue;
+using ReactNative.Common;
+using ReactNative.Tracing;
+using System;
+
+namespace ReactNative.Bridge
+{
+    /// <summary>
+    /// Class to the JavaScript execution environment and means of transport
+    /// for messages between JavaScript and the native environment.
+    /// </summary>
+    public class ReactBridge : IReactBridge
+    {
+        private static readonly JArray s_empty = new JArray();
+
+        private readonly IJavaScriptExecutor _jsExecutor;
+        private readonly IReactCallback _reactCallback;
+        private readonly IMessageQueueThread _nativeModulesQueueThread;
+
+        /// <summary>
+        /// Instantiates the <see cref="IReactBridge"/>.
+        /// </summary>
+        /// <param name="jsExecutor">The JavaScript executor.</param>
+        /// <param name="reactCallback">The native callback handler.</param>
+        /// <param name="nativeModulesQueueThread">
+        /// The native modules queue thread.
+        /// </param>
+        public ReactBridge(
+            IJavaScriptExecutor jsExecutor,
+            IReactCallback reactCallback,
+            IMessageQueueThread nativeModulesQueueThread)
+        {
+            if (jsExecutor == null)
+                throw new ArgumentNullException(nameof(jsExecutor));
+            if (reactCallback == null)
+                throw new ArgumentNullException(nameof(reactCallback));
+            if (nativeModulesQueueThread == null)
+                throw new ArgumentNullException(nameof(nativeModulesQueueThread));
+
+            _jsExecutor = jsExecutor;
+            _reactCallback = reactCallback;
+            _nativeModulesQueueThread = nativeModulesQueueThread;
+        }
+
+        /// <summary>
+        /// Calls a JavaScript function.
+        /// </summary>
+        /// <param name="moduleId">The module ID.</param>
+        /// <param name="methodId">The method ID.</param>
+        /// <param name="arguments">The arguments.</param>
+        public void CallFunction(int moduleId, int methodId, JArray arguments)
+        {
+            var allArgs = new JArray
+            {
+                moduleId,
+                methodId,
+                arguments,
+            };
+
+            var response = _jsExecutor.Call("__fbBatchedBridge", "callFunctionReturnFlushedQueue", allArgs);
+
+            ProcessResponse(response);
+        }
+
+        /// <summary>
+        /// Invokes a JavaScript callback.
+        /// </summary>
+        /// <param name="callbackId">The callback ID.</param>
+        /// <param name="arguments">The arguments.</param>
+        public void InvokeCallback(int callbackId, JArray arguments)
+        {
+            var allArgs = new JArray
+            {
+                callbackId,
+                arguments,
+            };
+            
+            var response = _jsExecutor.Call("__fbBatchedBridge", "invokeCallbackAndReturnFlushedQueue", allArgs);
+
+            ProcessResponse(response);
+        }
+
+        /// <summary>
+        /// Sets a global JavaScript variable.
+        /// </summary>
+        /// <param name="propertyName">The property name.</param>
+        /// <param name="jsonEncodedArgument">The JSON-encoded value.</param>
+        public void SetGlobalVariable(string propertyName, string jsonEncodedArgument)
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException(nameof(propertyName));
+
+            _jsExecutor.SetGlobalVariable(propertyName, JToken.Parse(jsonEncodedArgument));
+        }
+
+        /// <summary>
+        /// Evaluates JavaScript.
+        /// </summary>
+        /// <param name="script">The script.</param>
+        public void RunScript(string script)
+        {
+            if (script == null)
+                throw new ArgumentNullException(nameof(script));
+
+            _jsExecutor.RunScript(script);
+            var response = _jsExecutor.Call("__fbBatchedBridge", "flushedQueue", s_empty);
+
+            ProcessResponse(response);
+        }
+
+        private void ProcessResponse(JToken response)
+        {
+            var messages = response as JArray;
+            if (messages == null)
+            {
+                Tracer.Write(ReactConstants.Tag, "Empty JavaScript Queue");
+                return;
+            }
+
+            var moduleIds = messages[0].ToObject<int[]>();
+            var methodIds = messages[1].ToObject<int[]>();
+            var paramsArray = messages[2] as JArray;
+            if (moduleIds == null || methodIds == null || paramsArray == null ||
+                moduleIds.Length != methodIds.Length || moduleIds.Length != paramsArray.Count)
+            {
+                throw new InvalidOperationException("Unexpected React batch response.");
+            }
+
+            _nativeModulesQueueThread.RunOnQueue(() =>
+            {
+                for (var i = 0; i < moduleIds.Length; ++i)
+                {
+                    var moduleId = moduleIds[i];
+                    var methodId = methodIds[i];
+                    var args = (JArray)paramsArray[i];
+
+                    _reactCallback.Invoke(moduleId, methodId, args);
+                };
+
+                _reactCallback.OnBatchComplete();
+            });
+        }
+    }
+}
