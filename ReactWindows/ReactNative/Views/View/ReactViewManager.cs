ef10c58d4c0bcd97fdc8c5b7e891dc559b28f378
Eric Rozell <erozell@outlook.com>
2016-02-02 12:21:11 -0500

    Unsaved files from previous commit.

--- ReactWindows/ReactNative/Views/View/ReactViewManager.cs
+++ ReactWindows/ReactNative/Views/View/ReactViewManager.cs
@@ -150,18 +150,6 @@ namespace ReactNative.Views.View
         }
 
         /// <summary>
-        /// Sets the elevation transformation effect of the <see cref="ReactCanvas"/>. 
-        /// </summary>
-        /// <param name="view">The view panel.</param>
-        /// <param name="elevation">The 3D Z-Location index of the <see cref="ReactRootView"/>.</param>
-        [ReactProperty("elevation")]
-        public void SetElevation(BorderedContentControl view, float elevation)
-        {
-            var panel = GetPanel(view);
-            panel.SetElevationEffect(elevation);
-        }
-
-        /// <summary>
         /// Sets the border thickness of the <see cref="ReactCanvas"/>.
         /// </summary>
         /// <param name="view">The view panel.</param>
