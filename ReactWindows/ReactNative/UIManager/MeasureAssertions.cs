a09de3a7b2cf0bc4f4e459ce71adeb65306f1335
Eric Rozell <erozell@outlook.com>
2016-02-02 13:06:04 -0500

    Removing multi-colored border option from BorderContentControl

---
+++ ReactWindows/ReactNative/UIManager/MeasureAssertions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReactNative.UIManager
+{
+    static class MeasureAssertions
+    {
+        public static void AssertExplicitMeasurement(double width, double height)
+        {
+            if (IsUnspecified(width) || IsUnspecified(height))
+            {
+                throw new InvalidOperationException("A react view must have an explicit width and height.");
+            }
+        }
+
+        private static bool IsUnspecified(double value)
+        {
+            return double.IsInfinity(value) || double.IsNaN(value);
+        }
+    }
+}
