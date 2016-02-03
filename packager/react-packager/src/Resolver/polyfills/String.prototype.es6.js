91fda066dac56eeff517c588e0de6dde9fdb3c77
Corentin Smith <corentin.smith@gmail.com>
2016-02-03 17:24:07 +0100

    Add String.prototype.includes polyfill
    Add a polyfill for `String.prototype.includes` taken from MDN (https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/includes)
    See https://github.com/facebook/react-native/issues/5727

--- packager/react-packager/src/Resolver/polyfills/String.prototype.es6.js
+++ packager/react-packager/src/Resolver/polyfills/String.prototype.es6.js
@@ -83,3 +83,19 @@ if (!String.prototype.repeat) {
     return result;
   };
 }
+
+if (!String.prototype.includes) {
+  String.prototype.includes = function() {
+    'use strict';
+    if (typeof arguments[1] === "number") {
+      if (this.length < arguments[0].length + arguments[1].length) {
+        return false;
+      } else {
+        if (this.substr(arguments[1], arguments[0].length) === arguments[0]) return true;
+        else return false;
+      }
+    } else {
+      return String.prototype.indexOf.apply(this, arguments) !== -1;
+    }
+  };
+}
