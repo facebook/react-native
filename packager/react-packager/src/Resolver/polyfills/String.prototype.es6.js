ebdd16d349fcfcf75caeb4cbfe64af446b1a291a
Corentin Smith <corentin.smith@gmail.com>
2016-02-03 17:26:02 +0100

    Add brackets to if/else block

--- packager/react-packager/src/Resolver/polyfills/String.prototype.es6.js
+++ packager/react-packager/src/Resolver/polyfills/String.prototype.es6.js
@@ -91,8 +91,12 @@ if (!String.prototype.includes) {
       if (this.length < arguments[0].length + arguments[1].length) {
         return false;
       } else {
-        if (this.substr(arguments[1], arguments[0].length) === arguments[0]) return true;
-        else return false;
+        if (this.substr(arguments[1], arguments[0].length) === arguments[0]) {
+          return true;
+        }
+        else {
+          return false;
+        }
       }
     } else {
       return String.prototype.indexOf.apply(this, arguments) !== -1;
