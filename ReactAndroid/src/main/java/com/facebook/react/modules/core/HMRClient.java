c0599002521563372be4ebec25d7a4d5c73b5eac
Adam Miskiewicz <adam@sk3vy.com>
2016-02-04 11:01:01 -0500

    [Android] Document HMRClient

--- ReactAndroid/src/main/java/com/facebook/react/modules/core/HMRClient.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/core/HMRClient.java
@@ -12,8 +12,19 @@ package com.facebook.react.modules.core;
 import com.facebook.react.bridge.JavaScriptModule;
 
 /**
- * JS module interface - allows for enabling of HMRClient
+ * JS module interface for HMRClient
+ *
+ * The HMR(Hot Module Replacement)Client allows for the application to receive updates
+ * from the packager server (over a web socket), allowing for injection of JavaScript to
+ * the running application (without a refresh).
  */
 public interface HMRClient extends JavaScriptModule {
+
+  /**
+   * Enable the HMRClient so that the client will receive updates
+   * from the packager server.
+   * @param platform The platform in which HMR updates will be enabled. Should be "android".
+   * @param bundleEntry The path to the bundle entry file (e.g. index.ios.bundle)
+   */
   void enable(String platform, String bundleEntry);
 }
