3e3989e76d17fe76a6fe4721e810a92ca2df0556
Eric Rozell <erozell@outlook.com>
2016-02-08 10:38:16 -0500

    Adding enum parser that is case insensitive.

---
+++ ReactWindows/ReactNative/Reflection/EnumHelpers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ReactNative.Reflection
+{
+    static class EnumHelpers
+    {
+        private static readonly ConcurrentDictionary<Type, IReadOnlyDictionary<string, object>> s_enumCache =
+            new ConcurrentDictionary<Type, IReadOnlyDictionary<string, object>>();
+
+        public static T Parse<T>(string value)
+        {
+            var lookup = s_enumCache.GetOrAdd(
+                typeof(T),
+                type => Enum.GetValues(type)
+                    .Cast<object>()
+                    .ToDictionary(
+                        e => Normalize(e.ToString()),
+                        e => e));
+
+            var result = default(object);
+            if (!lookup.TryGetValue(Normalize(value), out result))
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(value),
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "Invalid value '{0}' for type '{1}'.",
+                        value,
+                        typeof(T)));
+            }
+
+            return (T)result;
+        }
+
+        private static string Normalize(string value)
+        {
+            return value.ToLowerInvariant().Replace("-", "");
+        }
+    }
+}
