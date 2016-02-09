edf0cb6291248ce9ebe600763d0ce229fd947337
wusuopu <admin@longchangjin.cn>
2016-02-09 13:36:08 +0800

    Fix warnings of StyleInspector

--- Libraries/Inspector/StyleInspector.js
+++ Libraries/Inspector/StyleInspector.js
@@ -25,13 +25,13 @@ class StyleInspector extends React.Component {
     return (
       <View style={styles.container}>
         <View>
-          {names.map(name => <Text style={styles.attr}>{name}:</Text>)}
+          {names.map(name => <Text key={name} style={styles.attr}>{name}:</Text>)}
         </View>
 
         <View>
           {names.map(name => {
             var value = typeof this.props.style[name] === 'object' ? JSON.stringify(this.props.style[name]) : this.props.style[name];
-            return <Text style={styles.value}>{value}</Text>;
+            return <Text key={name} style={styles.value}>{value}</Text>;
           } ) }
         </View>
       </View>
