89070ae463b49b1e010bef765f48e6bf635d5031
Eric Rozell <erozell@outlook.com>
2016-02-21 19:20:18 -0500

    Clean up tabs throughout codebase.

--- ReactWindows/ReactNative/Bridge/JavaScriptModuleBase.Generated.cs
+++ ReactWindows/ReactNative/Bridge/JavaScriptModuleBase.Generated.cs
@@ -7,7 +7,7 @@ namespace ReactNative.Bridge
     /// </summary>
     public abstract partial class JavaScriptModuleBase : IJavaScriptModule
     {
-		/// <summary>
+        /// <summary>
         /// Invoke a JavaScript method with the given arguments.
         /// </summary>
         /// <param name="caller">
@@ -22,12 +22,12 @@ namespace ReactNative.Bridge
         /// executed. This is to overcome the absense of a performant "proxy"
         /// implementation in the .NET framework.
         /// </remarks>
-		protected void Invoke([CallerMemberName]string caller = null)
-		{
-			Invoke(default(object[]), caller);
-		}
+        protected void Invoke([CallerMemberName]string caller = null)
+        {
+            Invoke(default(object[]), caller);
+        }
 
-		/// <summary>
+        /// <summary>
         /// Invoke a JavaScript method with the given arguments.
         /// </summary>
         /// <param name="arg0">The first argument.</param>
@@ -43,12 +43,12 @@ namespace ReactNative.Bridge
         /// executed. This is to overcome the absense of a performant "proxy"
         /// implementation in the .NET framework.
         /// </remarks>
-		protected void Invoke(object arg0, [CallerMemberName]string caller = null)
-		{
-			Invoke(new[] { arg0 }, caller);
-		}
+        protected void Invoke(object arg0, [CallerMemberName]string caller = null)
+        {
+            Invoke(new[] { arg0 }, caller);
+        }
 
-		/// <summary>
+        /// <summary>
         /// Invoke a JavaScript method with the given arguments.
         /// </summary>
         /// <param name="arg0">The first argument.</param>
@@ -65,12 +65,12 @@ namespace ReactNative.Bridge
         /// executed. This is to overcome the absense of a performant "proxy"
         /// implementation in the .NET framework.
         /// </remarks>
-		protected void Invoke(object arg0, object arg1, [CallerMemberName]string caller = null)
-		{
-			Invoke(new[] { arg0, arg1 }, caller);
-		}
+        protected void Invoke(object arg0, object arg1, [CallerMemberName]string caller = null)
+        {
+            Invoke(new[] { arg0, arg1 }, caller);
+        }
 
-		/// <summary>
+        /// <summary>
         /// Invoke a JavaScript method with the given arguments.
         /// </summary>
         /// <param name="arg0">The first argument.</param>
@@ -88,12 +88,12 @@ namespace ReactNative.Bridge
         /// executed. This is to overcome the absense of a performant "proxy"
         /// implementation in the .NET framework.
         /// </remarks>
-		protected void Invoke(object arg0, object arg1, object arg2, [CallerMemberName]string caller = null)
-		{
-			Invoke(new[] { arg0, arg1, arg2 }, caller);
-		}
+        protected void Invoke(object arg0, object arg1, object arg2, [CallerMemberName]string caller = null)
+        {
+            Invoke(new[] { arg0, arg1, arg2 }, caller);
+        }
 
-		/// <summary>
+        /// <summary>
         /// Invoke a JavaScript method with the given arguments.
         /// </summary>
         /// <param name="arg0">The first argument.</param>
@@ -112,10 +112,10 @@ namespace ReactNative.Bridge
         /// executed. This is to overcome the absense of a performant "proxy"
         /// implementation in the .NET framework.
         /// </remarks>
-		protected void Invoke(object arg0, object arg1, object arg2, object arg3, [CallerMemberName]string caller = null)
-		{
-			Invoke(new[] { arg0, arg1, arg2, arg3 }, caller);
-		}
+        protected void Invoke(object arg0, object arg1, object arg2, object arg3, [CallerMemberName]string caller = null)
+        {
+            Invoke(new[] { arg0, arg1, arg2, arg3 }, caller);
+        }
 
     }
 }
