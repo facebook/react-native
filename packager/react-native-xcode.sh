f74c53f637c9621dfb8c0fcf01863cb65f785094
Geoffrey Goh <kg422@cornell.edu>
2016-02-23 23:24:15 -0800

    Update react-native-xcode.sh

--- packager/react-native-xcode.sh
+++ packager/react-native-xcode.sh
@@ -12,8 +12,8 @@
 
 case "$CONFIGURATION" in
   Debug)
-    # Speed up build times by skipping creating the offline package for simulator
-    # debug builds since the packager is supposed to be running anyways.
+    # Speed up build times by skipping the creation of the offline package for debug
+    # builds on the simulator since the packager is supposed to be running anyways.
     if [[ "$PLATFORM_NAME" = "iphonesimulator" ]]; then		
       echo "Skipping bundling for Simulator platform"		
       exit 0;		
