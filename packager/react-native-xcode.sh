9ae3714f4bebdd2bcab4d7fdbf23acebdc5ed2ba
Geoffrey Goh <kg422@cornell.edu>
2016-02-24 03:11:08 -0800

    Create offline package if not running in "Debug" config
    Summary:**Problem**: As seen in https://github.com/facebook/react-native/issues/5820, many devs are confused by the fact that the offline bundle is not generated when running against the simulator, even when running in the "Release" configuration which is supposed to mimic "production" scenarios.
    
    This pull request is a small change that fixes https://github.com/facebook/react-native/issues/5820 by updating the `react-native-xcode.sh` shell script to still generate the ofline bundle during Release configuration. It also updates `AppDelegate.m` to better document this behaviour in the comments so as to avoid any surprises.
    
    **Test plan**: This is a simple change, the two tests done were
    1. In a new React Native project, verify that an offline build is not generated when running against the simulator in "Debug" configuration as per normal.
    2. Change to a "Release" configuration via Product > Scheme > Edit Scheme in XCode, then verify that the project runs with the offline build generated.
    
    ![screen shot 2016-02
    Closes https://github.com/facebook/react-native/pull/6119
    
    Differential Revision: D2970755
    
    Pulled By: javache
    
    fb-gh-sync-id: 64f658512869c73aa19286ca1e3dc6e31b5ac617
    shipit-source-id: 64f658512869c73aa19286ca1e3dc6e31b5ac617

--- packager/react-native-xcode.sh
+++ packager/react-native-xcode.sh
@@ -10,15 +10,15 @@
 # This script is supposed to be invoked as part of Xcode build process
 # and relies on environment variables (including PWD) set by Xcode
 
-# There is no point in creating an offline package for simulator builds
-# because the packager is supposed to be running during development anyways
-if [[ "$PLATFORM_NAME" = "iphonesimulator" ]]; then
-  echo "Skipping bundling for Simulator platform"
-  exit 0;
-fi
-
 case "$CONFIGURATION" in
   Debug)
+    # Speed up build times by skipping the creation of the offline package for debug
+    # builds on the simulator since the packager is supposed to be running anyways.
+    if [[ "$PLATFORM_NAME" = "iphonesimulator" ]]; then		
+      echo "Skipping bundling for Simulator platform"		
+      exit 0;		
+    fi
+    
     DEV=true
     ;;
   "")
