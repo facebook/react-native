be89b661cda06dc2ed2f8c2ab5b91c3cd1a5f02d
Eric Rozell <erozell@outlook.com>
2016-02-24 20:56:55 -0500

    Towards #143 - Replaces ListView with ScrollViewer for ReactScrollViewManager
    There's still work to be done around momentum events, horizontal scroll, etc., but this should provide a solid base.

---
+++ ReactWindows/ReactNative/Views/Scroll/ScrollEventType.cs
@@ -0,0 +1,13 @@
+﻿namespace ReactNative.Views.Scroll
+{
+    /// <summary>
+    /// Scroll event types.
+    /// </summary>
+    public enum ScrollEventType
+    {
+        /// <summary>
+        /// The scroll event.
+        /// </summary>
+        Scroll,
+    }
+}
---
+++ ReactWindows/ReactNative/Views/Scroll/ScrollEventTypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReactNative.Views.Scroll
+{
+    static class ScrollEventTypeExtensions
+    {
+        public static string GetJavaScriptEventName(this ScrollEventType type)
+        {
+            switch (type)
+            {
+                case ScrollEventType.Scroll:
+                    return "topScroll";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), $"Unknown scroll event type '{type}'.");
+            }
+        }
+    }
+}
