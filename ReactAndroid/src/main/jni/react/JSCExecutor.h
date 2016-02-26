b8e2b2366c62795efcd37bc6be5efd2848c17dbb
Andy Street <andrews@fb.com>
2016-02-26 08:42:25 -0800

    Revert D2982150: Revert D2926896 WebWorkers: Move web worker impl to JSCExecutor
    Reviewed By: lexs
    
    Differential Revision: D2982364
    
    fb-gh-sync-id: 25f099377acca75298768072d38d77dcb9c1bff3
    shipit-source-id: 25f099377acca75298768072d38d77dcb9c1bff3

--- ReactAndroid/src/main/jni/react/JSCExecutor.h
+++ ReactAndroid/src/main/jni/react/JSCExecutor.h
@@ -7,9 +7,10 @@
 #include <unordered_map>
 #include <folly/json.h>
 #include <JavaScriptCore/JSContextRef.h>
+
 #include "Executor.h"
 #include "JSCHelpers.h"
-#include "JSCWebWorker.h"
+#include "Value.h"
 
 namespace facebook {
 namespace react {
@@ -27,10 +28,26 @@ private:
   folly::dynamic m_jscConfig;
 };
 
-class JSCExecutor : public JSExecutor, public JSCWebWorkerOwner {
+class JSCExecutor;
+class WorkerRegistration : public noncopyable {
+public:
+  explicit WorkerRegistration(std::unique_ptr<JSCExecutor> executor, Object jsObj) :
+      jsObj(std::move(jsObj)),
+      executor(std::move(executor)) {}
+
+  JSCExecutor* getExecutor() {
+    return executor.get();
+  }
+
+  Object jsObj;
+private:
+  std::unique_ptr<JSCExecutor> executor;
+};
+
+class JSCExecutor : public JSExecutor {
 public:
   /**
-   * Should be invoked from the JS thread.
+   * Must be invoked from thread this Executor will run on.
    */
   explicit JSCExecutor(Bridge *bridge, const std::string& cacheDir, const folly::dynamic& jscConfig);
   ~JSCExecutor() override;
@@ -60,26 +77,43 @@ public:
   virtual void handleMemoryPressureCritical() override;
 
   void installNativeHook(const char *name, JSObjectCallAsFunctionCallback callback);
-  virtual void onMessageReceived(int workerId, const std::string& message) override;
-  virtual JSGlobalContextRef getContext() override;
-  virtual std::shared_ptr<MessageQueueThread> getMessageQueueThread() override;
 
 private:
   JSGlobalContextRef m_context;
-  std::unordered_map<int, JSCWebWorker> m_webWorkers;
-  std::unordered_map<int, Object> m_webWorkerJSObjs;
   Bridge *m_bridge;
+  int m_workerId = 0; // if this is a worker executor, this is non-zero
+  JSCExecutor *m_owner = nullptr; // if this is a worker executor, this is non-null
+  std::shared_ptr<bool> m_isDestroyed = std::shared_ptr<bool>(new bool(false));
+  std::unordered_map<int, WorkerRegistration> m_ownedWorkers;
   std::string m_deviceCacheDir;
   std::shared_ptr<MessageQueueThread> m_messageQueueThread;
   std::unique_ptr<JSModulesUnbundle> m_unbundle;
   folly::dynamic m_jscConfig;
 
-  int addWebWorker(const std::string& script, JSValueRef workerRef);
-  void postMessageToWebWorker(int worker, JSValueRef message, JSValueRef *exn);
+  /**
+   * WebWorker constructor. Must be invoked from thread this Executor will run on.
+   */
+  explicit JSCExecutor(
+      Bridge *bridge,
+      int workerId,
+      JSCExecutor *owner,
+      const std::string& script,
+      const std::unordered_map<std::string, std::string>& globalObjAsJSON,
+      const folly::dynamic& jscConfig);
+
+  void initOnJSVMThread();
+  void terminateOnJSVMThread();
   void flush();
-  void terminateWebWorker(int worker);
-  void loadModule(uint32_t moduleId);
   void flushQueueImmediate(std::string queueJSON);
+  void loadModule(uint32_t moduleId);
+
+  int addWebWorker(const std::string& script, JSValueRef workerRef, JSValueRef globalObjRef);
+  void postMessageToOwnedWebWorker(int worker, JSValueRef message, JSValueRef *exn);
+  void postMessageToOwner(JSValueRef result);
+  void receiveMessageFromOwnedWebWorker(int workerId, const std::string& message);
+  void receiveMessageFromOwner(const std::string &msgString);
+  void terminateOwnedWebWorker(int worker);
+  Object createMessageObject(const std::string& msgData);
 
   static JSValueRef nativeStartWorker(
       JSContextRef ctx,
@@ -96,6 +130,13 @@ private:
       const JSValueRef arguments[],
       JSValueRef *exception);
   static JSValueRef nativeTerminateWorker(
+      JSContextRef ctx,
+      JSObjectRef function,
+      JSObjectRef thisObject,
+      size_t argumentCount,
+      const JSValueRef arguments[],
+      JSValueRef *exception);
+  static JSValueRef nativePostMessage(
       JSContextRef ctx,
       JSObjectRef function,
       JSObjectRef thisObject,
