dfd70371ec474739532b8d62f9d5fada50d94095
Eric Rozell <erozell@outlook.com>
2016-02-29 17:01:05 -0500

    Fixes a few bugs with the Timing and AppState native modules.

--- ReactWindows/ReactNative.Tests/Modules/AppState/AppStateModuleTests.cs
+++ ReactWindows/ReactNative.Tests/Modules/AppState/AppStateModuleTests.cs
@@ -2,7 +2,9 @@
 using Newtonsoft.Json.Linq;
 using ReactNative.Bridge;
 using ReactNative.Modules.AppState;
-using ReactNative.Shell;
+using ReactNative.Modules.Core;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ReactNative.Tests.Modules.AppState
@@ -13,14 +15,20 @@ namespace ReactNative.Tests.Modules.AppState
         [TestMethod]
         public async Task AppStateModule_StateChecks()
         {
+            var uninitializedState = CreateExpectedState("uninitialized");
             var activeState = CreateExpectedState("active");
             var backgroundState = CreateExpectedState("background");
 
-            var context = await CreateReactContextAsync();
+            var context = CreateReactContext();
             var module = context.GetNativeModule<AppStateModule>();
 
             var args = default(object[]);
             var callback = new MockCallback(a => args = a);
+
+            module.getCurrentAppState(callback, new MockCallback(_ => { }));
+            Assert.AreEqual(uninitializedState.ToString(), args[0].ToString());
+
+            await DispatcherHelpers.RunOnDispatcherAsync(context.OnResume);
 
             module.getCurrentAppState(callback, new MockCallback(_ => { }));
             Assert.AreEqual(activeState.ToString(), args[0].ToString());
@@ -31,20 +39,89 @@ namespace ReactNative.Tests.Modules.AppState
             Assert.AreEqual(backgroundState.ToString(), args[0].ToString());
         }
 
-        private static async Task<ReactContext> CreateReactContextAsync()
+        [TestMethod]
+        public async Task AppStateModule_Events()
         {
-            var builder = new ReactInstanceManager.Builder
+            var activeState = CreateExpectedState("active");
+            var backgroundState = CreateExpectedState("background");
+
+            var lastState = default(JObject);
+            var waitHandle = new AutoResetEvent(false);
+            var context = CreateReactContext(new MockInvocationHandler((name, args) =>
             {
-                InitialLifecycleState = LifecycleState.Resumed,
-                JavaScriptBundleFile = "ms-appx:///Resources/test.js",
+                if (name == "emit" && args.Length == 2 && ((string)args[0]) == "appStateDidChange")
+                {
+                    lastState = args[1] as JObject;
+                    waitHandle.Set();
+                }
+            }));
+
+            await DispatcherHelpers.RunOnDispatcherAsync(context.OnResume);
+
+            waitHandle.WaitOne();
+            Assert.AreEqual(activeState.ToString(), lastState.ToString());
+            lastState = null;
+
+            await DispatcherHelpers.RunOnDispatcherAsync(context.OnSuspend);
+
+            waitHandle.WaitOne();
+            Assert.AreEqual(backgroundState.ToString(), lastState.ToString());
+        }
+
+        private static ReactContext CreateReactContext()
+        {
+            return CreateReactContext(new MockInvocationHandler());
+        }
+
+        private static ReactContext CreateReactContext(IInvocationHandler handler)
+        {
+            var context = new ReactContext();
+
+            var ids = new List<int>();
+            var appStateModule = new AppStateModule(context);
+            appStateModule.Initialize();
+
+            var eventEmitter = new RCTDeviceEventEmitter
+            {
+                InvocationHandler = handler,
             };
 
-            builder.Packages.Add(new MainReactPackage());
-            var mgr = builder.Build();
-            var tcs = new TaskCompletionSource<ReactContext>();
-            mgr.ReactContextInitialized += (sender, args) => tcs.SetResult(args.Context);
-            await DispatcherHelpers.RunOnDispatcherAsync(mgr.CreateReactContextInBackground);
-            return await tcs.Task;
+            var reactInstance = new TestReactInstance(appStateModule, eventEmitter);
+            context.InitializeWithInstance(reactInstance);
+
+            return context;
+        }
+
+        class TestReactInstance : MockReactInstance
+        {
+            private readonly object _appStateModule;
+            private readonly object _eventEmitter;
+
+            public TestReactInstance(AppStateModule appStateModule, RCTDeviceEventEmitter eventEmitter)
+            {
+                _appStateModule = appStateModule;
+                _eventEmitter = eventEmitter;
+            }
+
+            public override T GetNativeModule<T>()
+            {
+                if (typeof(T) == typeof(AppStateModule))
+                {
+                    return (T)_appStateModule;
+                }
+
+                return base.GetNativeModule<T>();
+            }
+
+            public override T GetJavaScriptModule<T>()
+            {
+                if (typeof(T) == typeof(RCTDeviceEventEmitter))
+                {
+                    return (T)_eventEmitter;
+                }
+
+                return base.GetJavaScriptModule<T>();
+            }
         }
 
         private static JObject CreateExpectedState(string state)
