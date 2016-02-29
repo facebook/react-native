cb49eb8ac55718f815edb8b31b81f8e6a0d7b03b
Sokovikov <skv-headless@yandex.ru>
2016-02-29 07:32:56 -0800

    fix props anchor links
    Summary:All anchor links are broken because of https://github.com/facebook/react-native/blob/master/website/core/Site.js#L37 base tag it means that relative urls won't work.
    closes #5450
    proof
    ![screen shot 2016-02-29 at 13 34 35](https://cloud.githubusercontent.com/assets/1488195/13392082/3d14a00e-dee9-11e5-8611-bc6bfaf6c752.png)
    Closes https://github.com/facebook/react-native/pull/6208
    
    Differential Revision: D2988892
    
    Pulled By: vjeux
    
    fb-gh-sync-id: da8b621af92d792a258812e38ae15f7cf53873ca
    shipit-source-id: da8b621af92d792a258812e38ae15f7cf53873ca

--- website/core/Header.js
+++ website/core/Header.js
@@ -13,6 +13,10 @@ var React = require('React');
 var slugify = require('slugify');
 
 var Header = React.createClass({
+  getDefaultProps: function() {
+    return {permalink: ''};
+  },
+
   render: function() {
     var slug = slugify(this.props.toSlug || this.props.children);
     var H = 'h' + this.props.level;
@@ -20,7 +24,7 @@ var Header = React.createClass({
       <H {...this.props}>
         <a className="anchor" name={slug}></a>
         {this.props.children}
-        {' '}<a className="hash-link" href={'#' + slug}>#</a>
+        {' '}<a className="hash-link" href={this.props.permalink + '#' + slug}>#</a>
       </H>
     );
   }
