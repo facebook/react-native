5a6a94b05a109a431640972726ced86e516d390f
Michał Gregorczyk <michalgr@fb.com>
2016-03-02 07:23:48 -0800

    sync fbjni
    Differential Revision: D2990551
    
    fb-gh-sync-id: 587f2b5cd09295dc1829926c12e38f2880440a5e
    shipit-source-id: 587f2b5cd09295dc1829926c12e38f2880440a5e

---
+++ ReactAndroid/src/main/jni/first-party/jni/fbjni/ByteBuffer.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#pragma once
+
+#include "CoreClasses.h"
+#include "References-forward.h"
+
+namespace facebook {
+namespace jni {
+
+// JNI's NIO support has some awkward preconditions and error reporting. This
+// class provides much more user-friendly access.
+class JByteBuffer : public JavaClass<JByteBuffer> {
+ public:
+  static constexpr const char* kJavaDescriptor = "Ljava/nio/ByteBuffer;";
+
+  static local_ref<JByteBuffer> wrapBytes(uint8_t* data, size_t size);
+
+  bool isDirect();
+
+  uint8_t* getDirectBytes();
+  size_t getDirectSize();
+};
+
+}}
