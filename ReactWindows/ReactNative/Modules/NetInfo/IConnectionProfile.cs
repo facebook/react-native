8447e8af73e6928c7dc8116506830c814c438dd3
Eric Rozell <erozell@outlook.com>
2016-03-07 12:14:00 -0500

    Addressing comments from pull request review.

--- ReactWindows/ReactNative/Modules/NetInfo/IConnectionProfile.cs
+++ ReactWindows/ReactNative/Modules/NetInfo/IConnectionProfile.cs
@@ -1,9 +1,20 @@
 ﻿namespace ReactNative.Modules.NetInfo
 {
+    /// <summary>
+    /// An interface for network connection profiles.
+    /// </summary>
     public interface IConnectionProfile
     {
+        /// <summary>
+        /// A value that indicates if connection profile is a WLAN (WiFi)
+        /// connection.
+        /// </summary>
         bool IsWlanConnectionProfile { get; }
 
+        /// <summary>
+        /// A value that indicates if connection profile is a WWAN (mobile)
+        /// connection.
+        /// </summary>
         bool IsWwanConnectionProfile { get; }
     }
 }
--- ReactWindows/ReactNative/Modules/NetInfo/INetworkInformation.cs
+++ ReactWindows/ReactNative/Modules/NetInfo/INetworkInformation.cs
@@ -2,14 +2,35 @@
 
 namespace ReactNative.Modules.NetInfo
 {
+    /// <summary>
+    /// An interface for network information status and updates.
+    /// </summary>
     public interface INetworkInformation
     {
+        /// <summary>
+        /// An event that occurs whenever the network status changes.
+        /// </summary>
+        event NetworkStatusChangedEventHandler NetworkStatusChanged;
+
+        /// <summary>
+        /// Gets the connection profile associated with the internet connection
+        /// currently used by the local machine.
+        /// </summary>
+        /// <returns>
+        /// The profile for the connection currently used to connect the machine
+        /// to the Internet, or null if there is no connection profile with a 
+        /// suitable connection.
+        /// </returns>
         IConnectionProfile GetInternetConnectionProfile();
 
+        /// <summary>
+        /// Starts observing network status changes.
+        /// </summary>
         void Start();
 
+        /// <summary>
+        /// Stops observing network status changes.
+        /// </summary>
         void Stop();
-
-        event NetworkStatusChangedEventHandler NetworkStatusChanged;
     }
 }
