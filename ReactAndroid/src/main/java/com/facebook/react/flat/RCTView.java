aa609a46ad48f44417ee18826a52d08995986907
Ahmed El-Helw <ahmedre@fb.com>
2016-03-08 12:28:25 -0800

    Revert nodes touch workaround
    Summary:
    Since we now have the correct fix in place, this patch is to revert
    the hack we put in place to not be launch blocking.
    
    Reviewed By: sriramramani
    
    Differential Revision: D3019883

--- ReactAndroid/src/main/java/com/facebook/react/flat/RCTView.java
+++ ReactAndroid/src/main/java/com/facebook/react/flat/RCTView.java
@@ -59,13 +59,6 @@ import com.facebook.react.uimanager.annotations.ReactPropGroup;
 
   @Override
   public void setBackgroundColor(int backgroundColor) {
-    // TODO t10316772 this if statement is a hack - we are mounting any transparent RCTView to an
-    // Android view, which is not necessary (and is inefficent). We're doing this to work around a
-    // bug where a non-Android view won't get touch because a View that is deeper in the hierarchy
-    // (i.e. will get onTouchEvent later) will handle it instead of the higher up view.
-    if (backgroundColor == 0) {
-      forceMountToView();
-    }
     getMutableBorder().setBackgroundColor(backgroundColor);
   }
 
