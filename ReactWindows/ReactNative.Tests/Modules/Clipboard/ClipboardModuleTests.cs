c4560ff5f005d2d861a9e18927acd3b803bbebf0
Eero Bragge <ebragge@live.com>
2016-03-09 13:53:32 +0200

    Fixes

--- ReactWindows/ReactNative.Tests/Modules/Clipboard/ClipboardModuleTests.cs
+++ ReactWindows/ReactNative.Tests/Modules/Clipboard/ClipboardModuleTests.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using ReactNative.Bridge;
-using ReactNative.Modules.Core;
 using ReactNative.Modules.Clipboard;
 using System;
-using System.Threading;
 
 namespace ReactNative.Tests.Modules.Clipboard
 {
@@ -13,7 +11,7 @@ namespace ReactNative.Tests.Modules.Clipboard
         [TestMethod]
         public void ClipboardModule_ArgumentChecks()
         {
-            var module = CreateClipboardModule(new MockInvocationHandler());
+            var module = new ClipboardModule(new ReactContext());
 
             AssertEx.Throws<ArgumentNullException>(
                 () => module.getString(null),
@@ -23,93 +21,30 @@ namespace ReactNative.Tests.Modules.Clipboard
         [TestMethod]
         public void ClipboardModule_GetString_Method()
         {
-            var module = CreateClipboardModule(new MockInvocationHandler());
+            if (!ReactNative.Bridge.DispatcherHelpers.IsInitialized) return;
 
-            var waitHandle = new AutoResetEvent(false);
-            Promise promise = new Promise(waitHandle);
-            string str = "test string";
+            var module = new ClipboardModule(new ReactContext());
+            var str = "test string";
+
+            var promise = new MockPromise(resolve => Assert.AreEqual(str, resolve.ToString()), reject => str = reject);
 
             module.setString(str);
             module.getString(promise);
+        }
 
-            waitHandle.WaitOne();
-            Assert.AreEqual(str, promise.value);
+        [TestMethod]
+        public void ClipboardModule_SetString_Null_Method()
+        {
+            if (!ReactNative.Bridge.DispatcherHelpers.IsInitialized) return;
+
+            var module = new ClipboardModule(new ReactContext());
+
+            var str = "";
+            var promise = new MockPromise(resolve => Assert.AreEqual(str, resolve.ToString()), reject => str = reject);
 
             module.setString(null);
             module.getString(promise);
-
-            waitHandle.WaitOne();
-            Assert.AreEqual("", promise.value);
-        }
-
-        private static ClipboardModule CreateClipboardModule(IInvocationHandler handler)
-        {
-            var context = new ReactContext();
-
-            var eventEmitter = new RCTDeviceEventEmitter();
-            eventEmitter.InvocationHandler = handler;
-
-            var reactInstance = new TestReactInstance(eventEmitter);
-            context.InitializeWithInstance(reactInstance);
-            return new ClipboardModule(context);
-        }
-
-        class Promise : IPromise
-        {
-            private AutoResetEvent _handler = null;
-            private string _value = null;
-            public string value
-            {
-                get
-                {
-                    return _value;
-                }
-            }
-
-            public Promise(AutoResetEvent handler)
-            {
-                _handler = handler;
-            }
-
-            void IPromise.Resolve(object value)
-            {
-                if (value.GetType() == typeof(string))
-                {
-                    _value = (string)value;
-                }
-                _handler.Set();
-            }
-
-            void IPromise.Reject(Exception exception)
-            {
-                _handler.Set();
-            }
-
-            void IPromise.Reject(string reason)
-            {
-                _handler.Set();
-            }
-        }
-
-        class TestReactInstance : MockReactInstance
-        {
-            private readonly object _eventEmitter;
-
-            public TestReactInstance(RCTDeviceEventEmitter eventEmitter)
-                : base()
-            {
-                _eventEmitter = eventEmitter;
-            }
-
-            public override T GetJavaScriptModule<T>()
-            {
-                if (typeof(RCTDeviceEventEmitter) == typeof(T))
-                {
-                    return (T)_eventEmitter;
-                }
-
-                return base.GetJavaScriptModule<T>();
-            }
         }
     }
 }
+
