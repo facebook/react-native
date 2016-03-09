0bc1bf1d2bffb9f5001c413f424df621f0c2f0df
Konstantin Raev <bestander@fb.com>
2016-03-09 19:32:24 +0000

    publish script and test are now split

--- scripts/punlish-npm.js
+++ scripts/punlish-npm.js
@@ -34,7 +34,8 @@
 require(`shelljs/global`);
 
 const CIRCLE_BRANCH = process.env.CIRCLE_BRANCH || '0.23-stable';
-const JAVA_VERSION="1.7";
+const JAVA_VERSION=`1.7`;
+const PACKAGE_NAME = `react-native`;
 
 let branchVersion;
 if (CIRCLE_BRANCH.indexOf(`-stable`) !== -1) {
@@ -51,43 +52,55 @@ if (javaVersion.indexOf(JAVA_VERSION) === -1) {
   exit(1);
 }
 
+if (exec(`sed -i.bak 's/^VERSION_NAME=[0-9\.]*-SNAPSHOT/VERSION_NAME=${branchVersion}.0/g' "ReactAndroid/gradle.properties"`).code) {
+  echo(`Couldn't update version for Gradle`);
+  exit(1);
+}
+
+// Uncomment Javadoc generation
+if (exec('sed -i.bak s://\\\ archives\\\ androidJavadocJar:archives\\\ androidJavadocJar:g "ReactAndroid/release.gradle"').code) {
+  echo(`Couldn't enable Javadoc generation`);
+  exit(1);
+}
+
+if (exec(`./gradlew :ReactAndroid:installArchives`).code) {
+  echo(`Couldn't generate artifacts`);
+  exit(1);
+}
+
+echo("Generated artifacts for Maven");
+
+if (exec(`git checkout ReactAndroid/release.gradle`).code) {
+  echo(`Couldn't revert release.gradle`);
+  exit(1);
+}
+
+let artifacts = ['-javadoc.jar', '-sources.jar', '.aar', '.pom'].map((suffix) => {
+  return `react-native-${branchVersion}${suffix}`;
+});
+
+artifacts.forEach((name) => {
+  if (!test(`-e`, `./android/com/facebook/react/react-native/${branchVersion}/${name}`)) {
+    echo(`file ${name} was not generated`);
+    exit(1);
+  }
+});
+
+// TODO increment versions
+
+echo(`Updated versions`);
+
+exec(`find . -path "*.bak" | xargs rm`);
+
+echo(`Published to npm ${branchVersion}`);
+// TODO which tag?
+// exec(`npm publish`);
+exit(0);
 
 /*
-
-sed -i.bak s/^VERSION_NAME=[0-9\.]*-SNAPSHOT/VERSION_NAME=${RELEASE}.0/g "ReactAndroid/gradle.properties" || error "Couldn't update version for Gradle"
-# Uncomment Javadoc generation
-sed -i.bak s:\/\/\ archives\ androidJavadocJar:archives\ androidJavadocJar:g "ReactAndroid/release.gradle" || error "Couldn't enable Javadoc generation"
-  ./gradlew :ReactAndroid:installArchives || error "Couldn't generate artifacts"
-# Revert Javadoc generation
-sed -i.bak s:archives\ androidJavadocJar:\/\/\ archives\ androidJavadocJar:g "ReactAndroid/release.gradle" || error "Couldn't enable Javadoc generation"
-
-artifacts_list=( -javadoc.jar -sources.jar .aar .pom )
-# ./gradlew :ReactAndroid:installArchives put the artifacts in react-native/android, ready to be published to npm
-artifacts_dir="${repo_root}/android/com/facebook/react/react-native/${RELEASE}.0"
-
-for i in "${artifacts_list[@]}"; do
-  artifact_file="${artifacts_dir}/react-native-${RELEASE}.0${i}"
-    [ -e "${artifact_file}" ] || error "Couldn't find file: ${artifact_file}"
-done
-
-success "Generated artifacts for Maven"
 
 sed -i.bak -E "s/(\"version\":[[:space:]]*\").+(\")/\"version\": \"${RELEASE}.0-rc\"/g" "package.json" || error "Couldn't update version for npm"
 sed -i.bak -E "s/(s.version[[:space:]]{13}=[[:space:]].+)/s.version             = \"${RELEASE}.0-rc\"/g" "React.podspec" || error "Couldn't update version for CocoaPods"
 
-success "Updated version numbers"
-
-npm_registry="http://localhost:4873/"
-
-npm set registry "${npm_registry}" && [[ $(npm config list | grep registry) == "registry = \"${npm_registry}\"" ]] || error "Couldn't set registry to ${npm_registry}"
-
-info "npm registry set. Run 'sinopia' in a new Terminal"
-info "   - Make sure it prints 'http address - ${npm_registry}'"
-info "   - Make sure ${npm_registry} shows no old versions"
-info ""
-info "Press any key to continue"
-read -n 1
-
-npm unpublish --force || error "Couldn't unpublish package from sinopia (${npm_registry})"
 npm publish || error "Couldn't publish package to sinopia (${npm_registry})"
 */
