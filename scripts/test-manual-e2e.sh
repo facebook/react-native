b4962dd137f75c0812301ec8d3619c93de631301
Konstantin Raev <bestander@fb.com>
2016-03-10 11:35:18 +0000

    improves

--- scripts/test-manual-e2e.sh
+++ scripts/test-manual-e2e.sh
@@ -21,7 +21,11 @@ info() {
     echo -e $BLUE"$@"$ENDCOLOR
 }
 
+[[ -z $RELEASE ]] && error "Please specify a version. Example usage: release.sh 0.18"
+
 repo_root=$(pwd)
+
+sed -i.bak s/^VERSION_NAME=[0-9\.]*-SNAPSHOT/VERSION_NAME=${RELEASE}.0/g "ReactAndroid/gradle.properties" || error "Couldn't update version for Gradle"
 
 ./gradlew :ReactAndroid:installArchives || error "Couldn't generate artifacts"
 
@@ -37,6 +41,9 @@ info "   - Make sure ${npm_registry} shows no old versions"
 info ""
 info "Press any key to continue"
 read -n 1
+
+sed -i.bak -E "s/(\"version\":[[:space:]]*\").+(\")/\"version\": \"${RELEASE}.0-rc\"/g" "package.json" || error "Couldn't update version for npm"
+sed -i.bak -E "s/(s.version[[:space:]]{13}=[[:space:]].+)/s.version             = \"${RELEASE}.0-rc\"/g" "React.podspec" || error "Couldn't update version for CocoaPods"
 
 npm unpublish --force || error "Couldn't unpublish package from sinopia (${npm_registry})"
 npm publish || error "Couldn't publish package to sinopia (${npm_registry})"
@@ -72,7 +79,10 @@ open "/tmp/${project_name}/ios/${project_name}.xcodeproj"
 
 cd "$repo_root"
 
-find . -path "*.bak" | xargs rm
+# undo changes to files
+git checkout package.json
+git checkout React.podspec
+git checkout ReactAndroid/gradle.properties
 
 npm set registry "https://registry.npmjs.org/" || error "Couldn't set registry to ${npm_registry}"
 
