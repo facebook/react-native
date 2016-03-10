ebbe655faaa7979e36418dbf15a6752e851d3c99
Konstantin Raev <bestander@fb.com>
2016-03-10 12:33:43 +0000

    some cleanup and comments how release should be done

--- scripts/punlish-npm.js
+++ scripts/punlish-npm.js
@@ -5,37 +5,45 @@
  * To reduce stress on developers it uses some logic to identify with which version to publish the package.
  *
  * To cut a branch (and release RC):
- * - Developer: `git checkout -b 0.XX-stable`
+ * - Developer: `git checkout -b 0.XY-stable`
  * - Developer: `git push` to git@github.com:facebook/react-native.git (or merge as pull request)
- * - CI: test and deploy to npm (run this script) with version 0.XX.0 with tag "next"
- * - Developer: `git tag v0.XX.0` and `git push --tags` to git@github.com:facebook/react-native.git to hook Release Notes
+ * - CI: test and deploy to npm (run this script) with version 0.XY.0-rc with tag "next"
+ * - Developer: `git tag v0.XY.0-rc` and `git push --tags` to git@github.com:facebook/react-native.git to hook Release Notes
  *
  * To update RC release:
- * - Developer: `git checkout 0.XX-stable`
+ * - Developer: `git checkout 0.XY-stable`
  * - Developer: cherry-pick whatever changes needed
  * - Developer: `git push` to git@github.com:facebook/react-native.git (or merge as pull request)
- * - CI: test and deploy to npm (run this script) with version 0.XX.1 (incremented since what was pushed to npm) with tag "next"
- * - Developer: `git tag v0.XX.0` and `git push --tags` to git@github.com:facebook/react-native.git to hook Release Notes
+ * - CI: test and deploy to npm (run this script) with version 0.XY.0-rc1 (incremented since what was pushed to npm)
+ * with tag "next"
  *
  * To publish release:
- * - Developer: `git checkout 0.XX-stable`
+ * - Developer: `git checkout 0.XY-stable`
  * - Developer: cherry-pick whatever changes needed
  * - Developer: `git tag latest`
+ * - Developer: `git tag v0.XY.0`
  * - Developer: `git push --tags` to git@github.com:facebook/react-native.git
- * - CI: test and deploy to npm (run this script) with version 0.XX.1 (incremented since what was pushed to npm) with tag "latest"
+ * - CI: test and deploy to npm (run this script) with version 0.XY.0 with and not tag (latest is implied by npm)
  *
  * To patch old release:
- * - Developer: `git checkout 0.XX-stable`
+ * - Developer: `git checkout 0.XY-stable`
  * - Developer: cherry-pick whatever changes needed
+ * - Developer: `git tag v0.XY.Z`
  * - Developer: `git push` to git@github.com:facebook/react-native.git (or merge as pull request)
- * - CI: test and deploy to npm (run this script) with version 0.XX.Y (incremented since what was pushed to npm) with tag "patch"
+ * - CI: test and deploy to npm (run this script) with version 0.XY.Z with no tag, npm will not mark it as latest if
+ * there is a version higher than XY
  *
+ * Important tags:
+ * If tag v0.XY.0-rcZ is present on the commit then publish to npm with version 0.XY.0-rcZ and tag next
+ * If tag v0.XY.Z is present on the commit then publish to npm with version 0.XY.Z and no tag (npm will consider it latest)
+ * This logic could be applied to website generation
+ * TODO: is latest tag needed if we just can track tags v0.XY.Z on a commit in CI?
  */
 require(`shelljs/global`);
 
 const CIRCLE_BRANCH = process.env.CIRCLE_BRANCH || '0.23-stable';
 const JAVA_VERSION=`1.7`;
-const PACKAGE_NAME = `react-native`;
+const PACKAGE_NAME = `react-native-evergreen`;
 
 let branchVersion;
 if (CIRCLE_BRANCH.indexOf(`-stable`) !== -1) {
@@ -44,7 +52,10 @@ if (CIRCLE_BRANCH.indexOf(`-stable`) !== -1) {
   echo(`Error: We publish only from stable branches`);
   exit(1);
 }
+// TODO get it from tag on current commit that looks like v${branchVersion}[-rc.*]
+let releaseVersion = branchVersion;
 
+// -------- Generating Android Artifacts with JavaDoc
 // Java -version outputs to stderr 0_o
 const javaVersion = exec(`java -version`).stderr;
 if (javaVersion.indexOf(JAVA_VERSION) === -1) {
@@ -70,11 +81,6 @@ if (exec(`./gradlew :ReactAndroid:installArchives`).code) {
 
 echo("Generated artifacts for Maven");
 
-if (exec(`git checkout ReactAndroid/release.gradle`).code) {
-  echo(`Couldn't revert release.gradle`);
-  exit(1);
-}
-
 let artifacts = ['-javadoc.jar', '-sources.jar', '.aar', '.pom'].map((suffix) => {
   return `react-native-${branchVersion}${suffix}`;
 });
@@ -86,11 +92,12 @@ artifacts.forEach((name) => {
   }
 });
 
-// TODO increment versions
-
-echo(`Updated versions`);
+// ----------- Reverting changes to local files
 
 exec(`find . -path "*.bak" | xargs rm`);
+exec(`git checkout package.json`);
+exec(`git checkout React.podspec`);
+exec(`git checkout ReactAndroid/gradle.properties`);
 
 echo(`Published to npm ${branchVersion}`);
 // TODO which tag?
