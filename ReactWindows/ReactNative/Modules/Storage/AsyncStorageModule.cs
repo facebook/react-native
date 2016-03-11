23754624727af1e388fa89bf3d46ced0f80cb23f
ebragge <ebragge@live.com>
2016-03-11 16:40:28 +0200

    feat(AsyncStorageModule): add AsyncStorageModule
    AsyncStorageModule provides permanent storage for React Native JavaScript applications.
    - implement PR comments #2
    - add Android tests
    
    Fixes #136

--- ReactWindows/ReactNative/Modules/Storage/AsyncStorageModule.cs
+++ ReactWindows/ReactNative/Modules/Storage/AsyncStorageModule.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using ReactNative.Bridge;
+using System;
 using System.Diagnostics;
 using Windows.Storage;
+using System.Collections.Generic;
 
 namespace ReactNative.Modules.Storage
 {
@@ -13,8 +15,13 @@ namespace ReactNative.Modules.Storage
         private enum _DataType
         {
             Null,
-            String,
             Array,
+            Constructor,
+            Date,           
+            Object,    
+            Uri,
+            String,
+            Raw,
         }
 
         private const string _invalidKey = "Invalid key";
@@ -47,18 +54,29 @@ namespace ReactNative.Modules.Storage
         }
 
         /// <summary>
-        /// Given an array of keys, this returns a map of (key, value) pairs for the keys found, and
-        /// (key, null) for the keys that haven't been found.
+        /// Given an array of keys, this returns through an <see cref="ICallback"/> a <see cref="JArray"/> 
+        /// of (key, value) pairs for the keys found, and (key, null) for the keys that haven't been found.
         /// </summary>
         /// <param name="keys">Array of key values</param>
         /// <param name="callback">Callback.</param>
         [ReactMethod]
         public void multiGet(string[] keys, ICallback callback)
         {
-            Debug.Assert(keys != null);
             Debug.Assert(callback != null);
 
             var result = new JArray();
+
+            if (keys == null)
+            {
+                result.Add(new JArray
+                {
+                    _invalidKey,
+                    JValue.CreateNull(),
+                });
+                callback.Invoke(result);
+                return;
+            }
+
             foreach (var key in keys)
             {
                 result.Add(new JArray
@@ -67,21 +85,35 @@ namespace ReactNative.Modules.Storage
                     getTokenFromContainer(key),
                 });
             }
+
             callback.Invoke(result);
         }
 
         /// <summary>
-        /// Inserts multiple (key, value) pairs. The insertion will replace conflicting (key, value) pairs.
+        /// Inserts multiple (key, value) pairs from a <see cref="JArray"/>.
+        /// The insertion will replace conflicting (key, value) pairs.
+        /// When done invokes <see cref="ICallback"/> with possible errors.
         /// </summary>
         /// <param name="keyValueArray">Array of key values</param>
         /// <param name="callback">Callback.</param>
         [ReactMethod]
         public void multiSet(JArray keyValueArray, ICallback callback)
         {
-            Debug.Assert(keyValueArray != null);
             Debug.Assert(callback != null);
 
             var result = new JArray();
+
+            if (keyValueArray == null)
+            {
+                result.Add(new JArray
+                {
+                    _invalidKey,
+                    JValue.CreateNull(),
+                });
+                callback.Invoke(result);
+                return;
+            }
+
             foreach (var keyValue in keyValueArray)
             {
                 if (keyValue.Type == JTokenType.Array && keyValue.Value<JArray>().Count == 2)
@@ -109,21 +141,34 @@ namespace ReactNative.Modules.Storage
                     });
                 }
             }
+
             callback.Invoke(result);
         }
 
         /// <summary>
         /// Removes all rows of the keys given.
+        /// When done invokes <see cref="ICallback"/>.
         /// </summary>
         /// <param name="keys">Array of key values</param>
         /// <param name="callback">Callback.</param>
         [ReactMethod]
         public void multiRemove(string[] keys, ICallback callback)
         {
-            Debug.Assert(keys != null);
             Debug.Assert(callback != null);
 
             var result = new JArray();
+
+            if (keys == null)
+            {
+                result.Add(new JArray
+                {
+                    _invalidKey,
+                    JValue.CreateNull(),
+                });
+                callback.Invoke(result);
+                return;
+            }
+
             foreach (var key in keys)
             {
                 _dataContainer.Values.Remove(key);
@@ -133,18 +178,30 @@ namespace ReactNative.Modules.Storage
         }
 
         /// <summary>
-        /// Given an array of (key, value) pairs, this will merge the given values with the stored values
+        /// Given a <see cref="JArray"/> of (key, value) pairs, this will merge the given values with the stored values
         /// of the given keys, if they exist.
+        /// When done invokes <see cref="ICallback"/> with possible errors.
         /// </summary>
         /// <param name="keyValueArray">Array of key values</param>
         /// <param name="callback">Callback.</param>
         [ReactMethod]
         public void multiMerge(JArray keyValueArray, ICallback callback)
         {
-            Debug.Assert(keyValueArray != null);
             Debug.Assert(callback != null);
 
             var result = new JArray();
+
+            if (keyValueArray == null)
+            {
+                result.Add(new JArray
+                {
+                    _invalidKey,
+                    JValue.CreateNull(),
+                });
+                callback.Invoke(result);
+                return;
+            }
+
             foreach (var keyValue in keyValueArray)
             {
                 if (keyValue.Type == JTokenType.Array && keyValue.Value<JArray>().Count == 2)
@@ -156,23 +213,25 @@ namespace ReactNative.Modules.Storage
                         var tokenOld = getTokenFromContainer(key);
                         var tokenNew = pair.Last;
 
-                        if (tokenOld.Type != JTokenType.Null)
+                        if (tokenOld.Type == JTokenType.Object && tokenNew.Type == JTokenType.Object)
                         {
-                            if (tokenOld.Type == JTokenType.Array)
-                            {
-                                ((JArray)tokenOld).Merge(tokenNew);
-                                tokenNew = tokenOld;
-                            }
-                            else if (tokenNew.Type == JTokenType.Array)
-                            {
-                                ((JArray)tokenNew).Merge(tokenOld);
-                            }
-                            else if (tokenNew.Type == JTokenType.Null)
-                            {
-                                tokenNew = tokenOld;
-                            }
+                            deepMergeInto((JObject)tokenOld, (JObject)tokenNew);
+                            addTokenToContainer(key, tokenOld);
                         }
-                        addTokenToContainer(key, tokenNew);
+                        else if (tokenOld.Type == JTokenType.Array)
+                        {
+                            (tokenOld as JArray).Merge(tokenNew);
+                            addTokenToContainer(key, tokenOld);
+                        }
+                        else if (tokenNew.Type == JTokenType.Array)
+                        {
+                            (tokenNew as JArray).Merge(tokenOld);
+                            addTokenToContainer(key, tokenNew);
+                        }
+                        else if (tokenOld.Type == JTokenType.Null)
+                        {
+                            addTokenToContainer(key, tokenNew);
+                        }
                     }
                     else
                     {
@@ -192,11 +251,13 @@ namespace ReactNative.Modules.Storage
                     });
                 }
             }
+
             callback.Invoke(result);
         }
 
         /// <summary>
-        /// Clears the database.
+        /// Clears the <see cref="ApplicationDataContainer"/>.
+        /// When done invokes <see cref="ICallback"/>.
         /// </summary>
         /// <param name="callback">Callback.</param>
         [ReactMethod]
@@ -211,7 +272,8 @@ namespace ReactNative.Modules.Storage
         }
 
         /// <summary>
-        /// Returns an array with all keys from the database.
+        /// Returns a <see cref="JArray"/> with all keys from the 
+        /// through <see cref="ICallback"/>.
         /// </summary>
         /// <param name="callback">Callback.</param>
         [ReactMethod]
@@ -219,11 +281,11 @@ namespace ReactNative.Modules.Storage
         {
             Debug.Assert(callback != null);
 
-            callback.Invoke(JToken.FromObject(_dataContainer.Values.Keys)); 
+            callback.Invoke(new JArray(_dataContainer.Values.Keys));
         }
 
         /// <summary>
-        /// Adds JToken to ApplicationDataContainer with the specified key.
+        /// Adds <see cref="JToken"/> to <see cref="ApplicationDataContainer"/> with the specified key.
         /// </summary>
         /// <param name="key">The key.</param>
         /// <param name="token">The token.</param>
@@ -244,6 +306,20 @@ namespace ReactNative.Modules.Storage
                 case JTokenType.Float:
                     _dataContainer.Values[key] = token.Value<double>();
                     break;
+                case JTokenType.TimeSpan:
+                    _dataContainer.Values[key] = token.Value<TimeSpan>();
+                    break;
+                case JTokenType.Guid:
+                    _dataContainer.Values[key] = token.Value<Guid>();
+                    break;
+                case JTokenType.Date:
+                    _dataContainer.Values[key] = token.ToString();
+                    addTokenTypeToContainer(key, _DataType.Date);
+                    break;
+                case JTokenType.Uri:
+                    _dataContainer.Values[key] = token.ToString();
+                    addTokenTypeToContainer(key, _DataType.Uri);
+                    break;                                
                 case JTokenType.String:
                     _dataContainer.Values[key] = token.Value<string>();
                     addTokenTypeToContainer(key, _DataType.String);
@@ -252,54 +328,67 @@ namespace ReactNative.Modules.Storage
                     _dataContainer.Values[key] = token.ToString();
                     addTokenTypeToContainer(key, _DataType.Array);
                     break;
+                case JTokenType.Constructor:
+                    _dataContainer.Values[key] = token.ToString();
+                    addTokenTypeToContainer(key, _DataType.Constructor);
+                    break;
+                case JTokenType.Object:
+                    _dataContainer.Values[key] = token.ToString();
+                    addTokenTypeToContainer(key, _DataType.Object);
+                    break;              
+                case JTokenType.Raw:
+                    _dataContainer.Values[key] = token.ToString();
+                    addTokenTypeToContainer(key, _DataType.Raw);
+                    break;
                 default:
+                    Debug.Assert(false); // Not supported JToken type
                     break;
             }
         }
 
         /// <summary>
-        /// Gets related value for a specific key from ApplicationDataContainer as JToken.
+        /// Gets related value for a specific key from <see cref="ApplicationDataContainer"/> as <see cref="JToken"/>.
         /// </summary>
         /// <param name="key">The key.</param>
         private JToken getTokenFromContainer(string key)
         {
-            var value = _dataContainer.Values[key];
-            if (value == null)
+            object value;
+            if (_dataContainer.Values.TryGetValue(key, out value))
             {
-                return JValue.CreateNull();
-            }
-            else
-            {
-                var t = value.GetType();
-                if (t == typeof(bool) || t == typeof(long) || t == typeof(double))
+                if (value is bool || value is long || value is double || value is Guid || value is TimeSpan)
                 {
                     return JToken.FromObject(value);
                 }
-                else if (t == typeof(string))
+                else if (value is string)
                 {
                     var type = getTokenTypeFromContainer(key);
-                    if (type == _DataType.Array)
+                    switch (type)
                     {
-                        return JToken.Parse((string)value);
+                        case _DataType.Array:
+                            return JArray.Parse(value as string);
+                        case _DataType.Constructor:
+                            return JToken.Parse(value as string);
+                        case _DataType.Object:
+                            return JObject.Parse(value as string);
+                        case _DataType.Date:
+                            return JToken.FromObject(DateTime.Parse(value as string));
+                        case _DataType.Uri:
+                            return JToken.FromObject(new Uri(value as string));
+                        case _DataType.Raw:
+                            return new JRaw(value);
+                        case _DataType.String:
+                            return JValue.CreateString(value as string); 
+                        default:
+                            break;
                     }
-                    else if (type == _DataType.String)
-                    {
-                        return JToken.FromObject((string)value);
-                    }
-                    else
-                    {
-                        return JValue.CreateNull();
-                    }
-                }
-                else
-                {
-                    return JValue.CreateNull();
                 }
             }
+
+            return JValue.CreateNull();
         }
 
-        // <summary>
-        /// Adds token's type to ApplicationDataContainer with the specified key.
+        /// <summary>
+        /// Adds token's type to <see cref="ApplicationDataContainer"/> with the specified key.
         /// </summary>
         /// <param name="key">The key.</param>
         /// <param name="type">The type.</param>
@@ -309,17 +398,64 @@ namespace ReactNative.Modules.Storage
         }
 
         /// <summary>
-        /// Gets token's type for a specific key from ApplicationDataContainer.
+        /// Gets token's type for a specific key from <see cref="ApplicationDataContainer"/>.
         /// </summary>
         /// <param name="key">The key.</param>
         private _DataType getTokenTypeFromContainer(string key)
         {
-            var value = _typeContainer.Values[key];
-            if (value == null)
+            object value;
+            if (_typeContainer.Values.TryGetValue(key, out value))
+            {
+                return (_DataType)value;
+            }
+            else
             {
                 return _DataType.Null;
             }
-            return (_DataType)value;
+        }
+
+        /// <summary>
+        /// Merge two <see cref="JObject"/>.
+        /// </summary>
+        /// <param name="oldObj">The old value.</param>
+        /// <param name="newObj">The old value.</param>
+        private static void deepMergeInto(JObject oldObj, JObject newObj)
+        {
+            IDictionary<string, JToken> dictionary = newObj; 
+            var keys = dictionary.Keys;
+
+            foreach (var key in keys)
+            {
+                JToken tokenNew, tokenOld;
+                newObj.TryGetValue(key, out tokenNew);
+                oldObj.TryGetValue(key, out tokenOld);
+                if (tokenNew?.Type == JTokenType.Object && tokenOld?.Type == JTokenType.Object)
+                {
+                    deepMergeInto(tokenOld as JObject, tokenNew as JObject);
+                    putToJObject(key, tokenOld, oldObj);
+                }
+                else
+                {
+                    var property = newObj.Property(key);
+                    foreach (var token in property)
+                    {
+                        putToJObject(key, token, oldObj);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Puts an { <see cref="string"/>, <see cref="JToken"/> } item to <see cref="JObject"/>.
+        /// Replaces existing value.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="token">The token.</param>
+        /// <param name="obj">The object.</param>
+        private static void putToJObject(string key, JToken token, JObject obj)
+        {
+            obj.Remove(key);
+            obj.Add(key, token);
         }
     }
 }
