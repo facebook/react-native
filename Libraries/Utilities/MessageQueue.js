1d4068b593cb9a3758e43015c6079d43d1ce73b7
Andre Giron <agiron123@gmail.com>
2016-03-13 01:19:37 -0500

    Fix eslint-bot issues.

--- Libraries/Utilities/MessageQueue.js
+++ Libraries/Utilities/MessageQueue.js
@@ -193,7 +193,7 @@ class MessageQueue {
     let module = debug && this._remoteModuleTable[debug[0]];
     let method = debug && this._remoteMethodTable[debug[0]][debug[1]];
     let errorMessage = `Callback with id ${cbID}: ${module}.${method}() not found`;
-    if (method != null || method != undefined) {
+    if (method !== null || method !== undefined) {
       errorMessage = `The callback ${method}() exists in module ${module}, `
       + `but only one callback may be registered to a function in a native module.`;
     }
