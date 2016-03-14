fae243f773d94dc96b15b88290850a47eac47f81
Chris Geirman <chris@geirman.com>
2016-03-14 14:32:33 -0700

    trim trailing white space

--- Libraries/Storage/AsyncStorage.js
+++ Libraries/Storage/AsyncStorage.js
@@ -111,7 +111,7 @@ var AsyncStorage = {
    * let UID123_object = {
    *  name: 'Chris',
    *  age: 30,
-   *  traits: {hair: 'brown', eyes: 'brown'},  
+   *  traits: {hair: 'brown', eyes: 'brown'},
    * };
 
    // need only define what will be added or updated
@@ -119,7 +119,7 @@ var AsyncStorage = {
    *  age: 31,
    *  traits: {eyes: 'blue', shoe_size: 10}
    * };
-   
+
    * AsyncStorage.setItem(store_key, JSON.stringify(UID123_object), () => {
    *   AsyncStorage.mergeItem('UID123', JSON.stringify(UID123_delta), () => {
    *     AsyncStorage.getItem('UID123', (err, result) => {
@@ -343,7 +343,7 @@ var AsyncStorage = {
    * let UID234_object = {
    *  name: 'Chris',
    *  age: 30,
-   *  traits: {hair: 'brown', eyes: 'brown'},  
+   *  traits: {hair: 'brown', eyes: 'brown'},
    * };
 
    * // first user, delta values
@@ -356,7 +356,7 @@ var AsyncStorage = {
    * let UID345_object = {
    *  name: 'Marge',
    *  age: 25,
-   *  traits: {hair: 'blonde', eyes: 'blue'},  
+   *  traits: {hair: 'blonde', eyes: 'blue'},
    * };
 
    * // second user, delta values
@@ -364,10 +364,10 @@ var AsyncStorage = {
    *  age: 26,
    *  traits: {eyes: 'green', shoe_size: 6},
    * };
-   
+
    * let multi_set_pairs   = [['UID234', JSON.stringify(UID234_object)], ['UID345', JSON.stringify(UID345_object)]]
    * let multi_merge_pairs = [['UID234', JSON.stringify(UID234_delta)], ['UID345', JSON.stringify(UID345_delta)]]
-   
+
    * AsyncStorage.multiSet(multi_set_pairs, (err) => {
    *   AsyncStorage.multiMerge(multi_merge_pairs, (err) => {
    *     AsyncStorage.multiGet(['UID234','UID345'], (err, stores) => {
