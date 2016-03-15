4028d1c9d4707d11b08c0c0387971730f54f601b
Alexey Lang <alexeylang@fb.com>
2016-03-15 05:40:09 -0700

    Add more performance logs and improve Systrace support
    Reviewed By: tadeuzagallo
    
    Differential Revision: D3040998
    
    fb-gh-sync-id: c5ab4e5bcc073f94a2d309bfa7698919cf7e8856
    shipit-source-id: c5ab4e5bcc073f94a2d309bfa7698919cf7e8856

--- React/Base/RCTPerformanceLogger.h
+++ React/Base/RCTPerformanceLogger.h
@@ -15,15 +15,48 @@ typedef NS_ENUM(NSUInteger, RCTPLTag) {
   RCTPLScriptDownload = 0,
   RCTPLScriptExecution,
   RCTPLNativeModuleInit,
+  RCTPLNativeModuleMainThread,
   RCTPLNativeModulePrepareConfig,
   RCTPLNativeModuleInjectConfig,
+  RCTPLJSCExecutorSetup,
   RCTPLTTI,
   RCTPLBundleSize,
   RCTPLSize
 };
 
+/**
+ * Starts measuring a metric with the given tag.
+ * Overrides previous value if the measurement has been already started.
+ * If RCTProfile is enabled it also begins appropriate async event.
+ */
 RCT_EXTERN void RCTPerformanceLoggerStart(RCTPLTag tag);
+
+/**
+ * Stops measuring a metric with given tag.
+ * Checks if RCTPerformanceLoggerStart() has been called before
+ * and doesn't do anything and log a message if it hasn't.
+ * If RCTProfile is enabled it also ends appropriate async event.
+ */
 RCT_EXTERN void RCTPerformanceLoggerEnd(RCTPLTag tag);
+
+/**
+ * Sets given value for a metric with given tag.
+ */
 RCT_EXTERN void RCTPerformanceLoggerSet(RCTPLTag tag, int64_t value);
+
+/**
+ * Starts an additional measurement for a metric with given tag.
+ * It doesn't override previous measurement, instead it'll append a new value
+ * to the old one.
+ */
+RCT_EXTERN void RCTPerformanceLoggerAppendStart(RCTPLTag tag);
+
+/**
+ * Stops measurement and appends the result to the metric with given tag.
+ * Checks if RCTPerformanceLoggerAppendStart() has been called before
+ * and doesn't do anything and log a message if it hasn't.
+ */
+RCT_EXTERN void RCTPerformanceLoggerAppendEnd(RCTPLTag tag);
+
 RCT_EXTERN NSArray<NSNumber *> *RCTPerformanceLoggerOutput(void);
 RCT_EXTERN NSArray *RCTPerformanceLoggerLabels(void);
