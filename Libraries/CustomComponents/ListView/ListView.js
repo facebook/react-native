ca7eb0da70a8f2999773a956bb39e5bfd7cf4e04
Dilan Edirisinghe <dedirisi@fb.com>
2016-03-17 12:10:35 -0700

    Add missing conditional to ListView.
    Reviewed By: jingc
    
    Differential Revision: D3062338
    
    fb-gh-sync-id: 52f9477604f488b7bc4829046f166c8e30aef868
    shipit-source-id: 52f9477604f488b7bc4829046f166c8e30aef868

--- Libraries/CustomComponents/ListView/ListView.js
+++ Libraries/CustomComponents/ListView/ListView.js
@@ -1,5 +1,10 @@
 /**
- * Copyright (c) 2015, Facebook, Inc.  All rights reserved.
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  *
  * Facebook, Inc. ("Facebook") owns all right, title and interest, including
  * all intellectual property and other proprietary rights, in and to the React
@@ -527,7 +532,7 @@ var ListView = React.createClass({
     this.setState((state, props) => {
       var rowsToRender = Math.min(
         state.curRenderedRowsCount + props.pageSize,
-        props.dataSource.getRowAndSectionCount()
+        (props.enableEmptySections ? props.dataSource.getRowAndSectionCount() : props.dataSource.getRowCount())
       );
       this._prevRenderedRowsCount = state.curRenderedRowsCount;
       return {
