d329570ac8f8dffc461129b2a40da4b327da588e
Alexander Blom <blom@fb.com>
2016-03-17 11:54:18 -0700

    Get rid of jackson (mostly)
    Reviewed By: astreet
    
    Differential Revision: D3041362
    
    fb-gh-sync-id: a7027a08a63730b98dc766b86921820fa3624953
    shipit-source-id: a7027a08a63730b98dc766b86921820fa3624953

--- ReactAndroid/src/main/java/com/facebook/react/bridge/NativeModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/bridge/NativeModule.java
@@ -12,8 +12,6 @@ package com.facebook.react.bridge;
 import java.io.IOException;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-
 /**
  * A native module whose API can be provided to JS catalyst instances.  {@link NativeModule}s whose
  * implementation is written in Java should extend {@link BaseJavaModule} or {@link
@@ -42,7 +40,7 @@ public interface NativeModule {
    * Append a field which represents the constants this module exports
    * to JS.  If no constants are exported this should do nothing.
    */
-  void writeConstantsField(JsonGenerator jg, String fieldName) throws IOException;
+  void writeConstantsField(JsonWriter writer, String fieldName) throws IOException;
 
   /**
    * This is called at the end of {@link CatalystApplicationFragment#createCatalystInstance()}
