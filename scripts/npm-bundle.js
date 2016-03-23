d9eb96a819f3f869efb7f31b069d5689ad79f822
Konstantin Raev <bestander@fb.com>
2016-03-23 11:35:19 +0000

    another try

--- scripts/npm-bundle.js
+++ scripts/npm-bundle.js
@@ -18,7 +18,7 @@ require(`shelljs/global`);
 let lodash = require(`lodash`);
 let originalPackageJson = cat(`package.json`);
 let packageJson = JSON.parse(cat(`package.json`));
-let withDeps = process.argv.indexOf('--with-deps');
+let withDeps = process.argv.indexOf('--with-deps') !== -1;
 
 // in an ideal world npm CLI would work when PC is offline
 // but it is not the case https://addyosmani.com/blog/using-npm-offline/
@@ -27,7 +27,7 @@ let withDeps = process.argv.indexOf('--with-deps');
 if(withDeps) {
   // npm needs a list of all dependencies to bundle but our node_modules can be deduped to some level
   // so we need to ask npm to give us full lust of non dev dependencies
-  let packagesToBundle = JSON.parse(exec(`npm ls --prod --json --silent`, {silent: true}).stdout);
+  let packagesToBundle = JSON.parse(exec(`npm ls --prod --json --silent`, {silent: false}).stdout);
   function getDependencies(packageTree) {
     if (!packageTree.dependencies) {
       return [];
