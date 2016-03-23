cce27aebf855b7a5cdc167fe7ff6d9edb57cf0dd
Eric Rozell <erozell@outlook.com>
2016-03-22 21:58:30 -0700

    fix(AsyncStorageModule): Eliminate size constraint for async local storage
    The app LocalSettings were not a good option for storing data, as they cap out at a relatively small storage capacity.
    
    Switch to using the local file system as the key value store. Later, we can consider options like SQLite.
    
    Fixes #282

---
+++ ReactWindows/ReactNative/Modules/Storage/AsyncStorageErrorHelpers.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace ReactNative.Modules.Storage
+{
+    static class AsyncStorageErrorHelpers
+    {
+        public static JObject GetError(string key, string errorMessage)
+        {
+            var errorMap = new JObject
+            {
+                { "message", errorMessage },
+            };
+
+            if (key != null)
+            {
+                errorMap.Add("key", key);
+            }
+
+            return errorMap;
+        }
+
+        public static JObject GetInvalidKeyError(string key)
+        {
+            return GetError(key, "Invalid key");
+        }
+
+        public static JObject GetInvalidValueError(string key)
+        {
+            return GetError(key, "Invalid Value");
+        }
+
+        public static JObject GetDatabaseError(string key)
+        {
+            return GetError(key, "Database Error");
+        }
+    }
+}
