df355ce4180cb1d2bce8f5a8358ade8642ab7e8f
Konstantin Raev <bestander@fb.com>
2016-03-24 05:30:49 -0700

    Allow react-native init <version> + removed sinopia from being required for e2e testing
    Reviewed By: mkonicek, frantic
    
    Differential Revision: D3087524
    
    fb-gh-sync-id: 048e23b55916a6be17fa9fabb6e41b0b2f3f7a16
    shipit-source-id: 048e23b55916a6be17fa9fabb6e41b0b2f3f7a16

--- scripts/test-manual-e2e.sh
+++ scripts/test-manual-e2e.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-RELEASE="$1"
 JAVA_VERSION="1.7"
 
 RED="\033[0;31m"
@@ -21,43 +20,34 @@ info() {
     echo -e $BLUE"$@"$ENDCOLOR
 }
 
-[[ -z $RELEASE ]] && error "Please specify a version. Example usage: release.sh 0.18"
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+success "Preparing version $PACKAGE_VERSION"
 
 repo_root=$(pwd)
-
-sed -i.bak s/^VERSION_NAME=[0-9\.]*-SNAPSHOT/VERSION_NAME=${RELEASE}.0/g "ReactAndroid/gradle.properties" || error "Couldn't update version for Gradle"
 
 ./gradlew :ReactAndroid:installArchives || error "Couldn't generate artifacts"
 
 success "Generated artifacts for Maven"
 
-npm_registry="http://localhost:4873/"
+npm pack
 
-npm set registry "${npm_registry}" && [[ $(npm config list | grep registry) == "registry = \"${npm_registry}\"" ]] || error "Couldn't set registry to ${npm_registry}"
-
-info "npm registry set. Run 'sinopia' in a new Terminal"
-info "   - Make sure it prints 'http address - ${npm_registry}'"
-info "   - Make sure ${npm_registry} shows no old versions"
-info ""
-info "Press any key to continue"
-read -n 1
-
-sed -i.bak -E "s/(\"version\":[[:space:]]*\").+(\")/\"version\": \"${RELEASE}.0-rc\"/g" "package.json" || error "Couldn't update version for npm"
-sed -i.bak -E "s/(s.version[[:space:]]{13}=[[:space:]].+)/s.version             = \"${RELEASE}.0-rc\"/g" "React.podspec" || error "Couldn't update version for CocoaPods"
-
-npm unpublish --force || error "Couldn't unpublish package from sinopia (${npm_registry})"
-npm publish || error "Couldn't publish package to sinopia (${npm_registry})"
-
-success "Published package to sinopia (${npm_registry})"
+PACKAGE=$(pwd)/react-native-$PACKAGE_VERSION.tgz
+success "Package bundled ($PACKAGE)"
 
 project_name="RNTestProject"
 
 cd /tmp/
 rm -rf "$project_name"
-react-native init "$project_name"
+react-native init "$project_name" --version $PACKAGE
 
-info "Double checking the versions in package.json and build.gradle are correct:"
-grep "\"react-native\": \"\^${RELEASE}.0-rc\"" "/tmp/${project_name}/package.json" || error "Incorrect version number in /tmp/${project_name}/package.json"
+info "Double checking the versions in package.json are correct:"
+grep "\"react-native\": \".*react-native-$PACKAGE_VERSION.tgz\"" "/tmp/${project_name}/package.json" || error "Incorrect version number in /tmp/${project_name}/package.json"
 grep -E "com.facebook.react:react-native:\\+" "${project_name}/android/app/build.gradle" || error "Dependency in /tmp/${project_name}/android/app/build.gradle must be com.facebook.react:react-native:+"
 
 success "New sample project generated at /tmp/${project_name}"
@@ -70,23 +60,16 @@ info "   - Test Chrome debugger by adding breakpoints. We don't have tests for C
 info ""
 
 info "Press any key to run the sample in Android emulator/device"
+info ""
 read -n 1
-cd "${project_name}" && react-native run-android
+cd "/tmp/${project_name}" && react-native run-android
 
 info "Press any key to open the project in XCode"
+info ""
 read -n 1
 open "/tmp/${project_name}/ios/${project_name}.xcodeproj"
 
 cd "$repo_root"
 
-# undo changes to files
-git checkout package.json
-git checkout React.podspec
-git checkout ReactAndroid/gradle.properties
-find . -path "*.bak" | xargs rm
-
-npm set registry "https://registry.npmjs.org/" || error "Couldn't set registry to ${npm_registry}"
-
 info "Next steps:"
-info "   - git tag v${RELEASE}.0-rc"
-info "   - git push origin ${RELEASE}-stable --tags"
+info "   - https://github.com/facebook/react-native/blob/master/Releases.md"
