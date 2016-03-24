38f13184a8ea6ddf6e2e8ecc36f2447739691e52
Christopher Chedeau <vjeux@fb.com>
2016-03-24 09:45:54 -0700

    Introduce blog
    Ported the infrastructure from jest.

--- website/core/HeaderLinks.js
+++ website/core/HeaderLinks.js
@@ -16,9 +16,8 @@ var HeaderLinks = React.createClass({
   linksInternal: [
     {section: 'docs', href: 'docs/getting-started.html', text: 'Docs'},
     {section: 'support', href: 'support.html', text: 'Support'},
-    {section: 'releases', href: 'https://github.com/facebook/react-native/releases', text: 'Releases'},
-    {section: 'newsletter', href: 'http://reactnative.cc', text: 'Newsletter'},
     {section: 'showcase', href: 'showcase.html', text: 'Showcase'},
+    {section: 'blog', href: 'blog/', text: 'Blog'},
   ],
   linksExternal: [
     {section: 'github', href: 'https://github.com/facebook/react-native', text: 'GitHub'},
