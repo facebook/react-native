de40e514013c3d844b22e7434c07a9fa95e42eca
CntChen <cntchen@gmail.com>
2016-03-27 01:16:03 +0800

    [Navigator] reuse `push` code for `resetTo`

--- Libraries/CustomComponents/Navigator/Navigator.js
+++ Libraries/CustomComponents/Navigator/Navigator.js
@@ -911,23 +911,7 @@ var Navigator = React.createClass({
    * `jumpForward` to.
    */
   push: function(route) {
-    invariant(!!route, 'Must supply route to push');
-    var activeLength = this.state.presentedIndex + 1;
-    var activeStack = this.state.routeStack.slice(0, activeLength);
-    var activeAnimationConfigStack = this.state.sceneConfigStack.slice(0, activeLength);
-    var nextStack = activeStack.concat([route]);
-    var destIndex = nextStack.length - 1;
-    var nextAnimationConfigStack = activeAnimationConfigStack.concat([
-      this.props.configureScene(route, nextStack),
-    ]);
-    this._emitWillFocus(nextStack[destIndex]);
-    this.setState({
-      routeStack: nextStack,
-      sceneConfigStack: nextAnimationConfigStack,
-    }, () => {
-      this._enableScene(destIndex);
-      this._transitionTo(destIndex);
-    });
+    this._push(route, null);
   },
 
   _popN: function(n) {
@@ -1051,11 +1035,7 @@ var Navigator = React.createClass({
     this.pop();
   },
 
-  /**
-   * Navigate to a new scene and reset route stack.
-   */
-  resetTo: function(route) {
-    // Push a new route and reset routestack inside transition callback
+  _push: function(route, transition_cb) {
     invariant(!!route, 'Must supply route to push');
     var activeLength = this.state.presentedIndex + 1;
     var activeStack = this.state.routeStack.slice(0, activeLength);
@@ -1071,18 +1051,23 @@ var Navigator = React.createClass({
       sceneConfigStack: nextAnimationConfigStack,
     }, () => {
       this._enableScene(destIndex);
-      this._transitionTo(destIndex, null, null, () => {
-          var nextStack = this.state.routeStack.slice(-1);
-          this.setState({
-          routeStack: nextStack,
-          sceneConfigStack: nextStack.map(
-            (route) => this.props.configureScene(route, nextStack)
-          ),
-          presentedIndex: 0,
-          activeGesture: null,
-          transitionFromIndex: null,
-          transitionQueue: [],
-        });
+      this._transitionTo(destIndex, null, null, transition_cb);
+    });
+  },
+
+  resetTo: function(route) {
+    // Push a new route and reset routestack inside transition callback
+    this._push(route, () => {
+      var nextStack = this.state.routeStack.slice(-1);
+      this.setState({
+        routeStack: nextStack,
+        sceneConfigStack: nextStack.map(
+          (route) => this.props.configureScene(route, nextStack)
+        ),
+        presentedIndex: 0,
+        activeGesture: null,
+        transitionFromIndex: null,
+        transitionQueue: [],
       });
     });
   },
