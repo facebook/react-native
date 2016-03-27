b140609304c4cc37b1fd04064bc786e1612b4874
Lane Rettig <lanerettig@gmail.com>
2016-03-27 11:45:52 -0400

    add support for message to UIAlertController

--- Libraries/ActionSheetIOS/RCTActionSheetManager.m
+++ Libraries/ActionSheetIOS/RCTActionSheetManager.m
@@ -61,6 +61,7 @@ RCT_EXPORT_METHOD(showActionSheetWithOptions:(NSDictionary *)options
   }
 
   NSString *title = [RCTConvert NSString:options[@"title"]];
+  NSString *message = [RCTConvert NSString:options[@"message"]];
   NSArray<NSString *> *buttons = [RCTConvert NSStringArray:options[@"options"]];
   NSInteger destructiveButtonIndex = options[@"destructiveButtonIndex"] ? [RCTConvert NSInteger:options[@"destructiveButtonIndex"]] : -1;
   NSInteger cancelButtonIndex = options[@"cancelButtonIndex"] ? [RCTConvert NSInteger:options[@"cancelButtonIndex"]] : -1;
@@ -113,7 +114,7 @@ RCT_EXPORT_METHOD(showActionSheetWithOptions:(NSDictionary *)options
   {
     UIAlertController *alertController =
     [UIAlertController alertControllerWithTitle:title
-                                        message:nil
+                                        message:message
                                  preferredStyle:UIAlertControllerStyleActionSheet];
 
     NSInteger index = 0;
