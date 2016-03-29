464a2e85c29e46a96b4a89100ab1ebbd98e84179
Martin Bigio <martinb@fb.com>
2016-03-29 06:34:01 -0700

    Use `react-native` instead of `React` on hmr config
    Summary:Check https://github.com/facebook/react-native/issues/6640 for details
    Closes https://github.com/facebook/react-native/pull/6696
    
    Differential Revision: D3109128
    
    fb-gh-sync-id: b80ff44f0dbc063870dc1dc7de9bf3c814ac9a12
    fbshipit-source-id: b80ff44f0dbc063870dc1dc7de9bf3c814ac9a12

--- babel-preset/configs/hmr.js
+++ babel-preset/configs/hmr.js
@@ -25,7 +25,7 @@ module.exports = function(options, filename) {
         {
           transforms: [{
             transform: transform,
-            imports: ['React'],
+            imports: ['react-native'],
             locals: ['module'],
           }]
         },
