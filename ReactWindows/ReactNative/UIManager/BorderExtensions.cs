9d984215f1728afe6b8dde3ef155f5c1f6602fcd
Eric Rozell <erozell@outlook.com>
2016-03-30 11:47:20 -0400

    fix(BorderedViewParentManager): Remove BorderedContentControl
    Removes BorderedContentControl and replaces with a single border around all views. This creates just at most as many XAML views as wrapping all elements in a ContentControl, and in cases where a border is present, reduces the view count.
    
    Fixes #316

--- ReactWindows/ReactNative/UIManager/BorderExtensions.cs
+++ ReactWindows/ReactNative/UIManager/BorderExtensions.cs
@@ -6,25 +6,32 @@ namespace ReactNative.UIManager
 {
     static class BorderExtensions
     {
-        public static void SetBorderWidth(this Border border, CSSSpacingType kind, double width)
+        public static void SetBorderWidth(this Border border, CSSSpacingType kind, double? width)
         {
+            if (!width.HasValue)
+            {
+                border.BorderThickness = default(Thickness);
+                return;
+            }
+
+            var widthValue = width.Value;
             var thickness = border.BorderThickness;
             switch (kind)
             {
                 case CSSSpacingType.Left:
-                    thickness.Left = width;
+                    thickness.Left = widthValue;
                     break;
                 case CSSSpacingType.Top:
-                    thickness.Top = width;
+                    thickness.Top = widthValue;
                     break;
                 case CSSSpacingType.Right:
-                    thickness.Right = width;
+                    thickness.Right = widthValue;
                     break;
                 case CSSSpacingType.Bottom:
-                    thickness.Bottom = width;
+                    thickness.Bottom = widthValue;
                     break;
                 case CSSSpacingType.All:
-                    thickness = new Thickness(width);
+                    thickness = new Thickness(widthValue);
                     break;
             }
 
