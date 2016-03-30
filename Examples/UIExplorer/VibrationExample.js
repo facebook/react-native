2c1849a94b32e13f4495d31096e91df353a3bab2
Sokovikov <skv-headless@yandex.ru>
2016-03-29 21:44:33 -0700

    Add support for vibration patterns
    Summary:and "not supported" warnings for ios
    Closes https://github.com/facebook/react-native/pull/6400
    
    Differential Revision: D3113988
    
    fb-gh-sync-id: 169623f157ec59c38b4368cbc668c85201b67ed8
    fbshipit-source-id: 169623f157ec59c38b4368cbc668c85201b67ed8

--- Examples/UIExplorer/VibrationExample.js
+++ Examples/UIExplorer/VibrationExample.js
@@ -27,20 +27,64 @@ var {
 exports.framework = 'React';
 exports.title = 'Vibration';
 exports.description = 'Vibration API';
-exports.examples = [{
-  title: 'Vibration.vibrate()',
-  render() {
-    return (
-      <TouchableHighlight
-        style={styles.wrapper}
-        onPress={() => Vibration.vibrate()}>
-        <View style={styles.button}>
-          <Text>Vibrate</Text>
-        </View>
-      </TouchableHighlight>
-    );
+exports.examples = [
+  {
+    title: 'Vibration.vibrate()',
+    render() {
+      return (
+        <TouchableHighlight
+          style={styles.wrapper}
+          onPress={() => Vibration.vibrate()}>
+          <View style={styles.button}>
+            <Text>Vibrate</Text>
+          </View>
+        </TouchableHighlight>
+      );
+    },
   },
-}];
+  {
+    title: 'Vibration.vibrate([0, 500, 200, 500])',
+    render() {
+      return (
+        <TouchableHighlight
+          style={styles.wrapper}
+          onPress={() => Vibration.vibrate([0, 500, 200, 500])}>
+          <View style={styles.button}>
+            <Text>Vibrate once</Text>
+          </View>
+        </TouchableHighlight>
+      );
+    },
+  },
+  {
+    title: 'Vibration.vibrate([0, 500, 200, 500], true)',
+    render() {
+      return (
+        <TouchableHighlight
+          style={styles.wrapper}
+          onPress={() => Vibration.vibrate([0, 500, 200, 500], true)}>
+          <View style={styles.button}>
+            <Text>Vibrate until cancel</Text>
+          </View>
+        </TouchableHighlight>
+      );
+    },
+  },
+  {
+    title: 'Vibration.cancel()',
+    render() {
+      return (
+        <TouchableHighlight
+          style={styles.wrapper}
+          onPress={() => Vibration.cancel()}>
+          <View style={styles.button}>
+            <Text>Cancel</Text>
+          </View>
+        </TouchableHighlight>
+      );
+    },
+  },
+];
 
 var styles = StyleSheet.create({
   wrapper: {
