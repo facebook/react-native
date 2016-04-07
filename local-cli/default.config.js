3c056db9b7ca1330d972f25366a4de2d8ec0bd2c
Chris Liu <guohong.liu@gmail.com>
2016-04-07 15:36:38 +0800

    react app root path can be override by env var 'react_native_app_root'

--- local-cli/default.config.js
+++ local-cli/default.config.js
@@ -34,6 +34,10 @@ var config = {
 };
 
 function getRoots() {
+  var root = process.env.react_native_app_root;
+  if (root) {
+    return [path.resolve(root)];
+  }
   if (__dirname.match(/node_modules[\/\\]react-native[\/\\]local-cli$/)) {
     // Packager is running from node_modules.
     // This is the default case for all projects created using 'react-native init'.
