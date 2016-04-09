97cdcd201f58015f8d8bbcad4669f895b17452c4
Sebastian Markbage <sema@fb.com>
2016-04-09 04:17:34 -0700

    Add deprecation warnings for split of the react and react-native packages
    Summary:This adds deprecation warnings that correspond to what React 0.14 did for the web.
    
    I.e. `React.render` -> `ReactNative.render` but also `ReactNative.createClass` -> `React.createClass`.
    
    This hopefully means that it will become easier and more idiomatic to write components that are decoupled from the react-native package.
    
    It will be clear when you take on react-native as a dependency such as when using findNodeHandle.
    
    This codemod is a little more invasive for React Native because the common stuff often used the `react-native` package. For web only the uncommon stuff needed to move.
    
    Reviewed By: spicyj
    
    Differential Revision: D3148860
    
    fb-gh-sync-id: d87628d2089a2e012ad6ad50dd0a20ccec5e6c45
    fbshipit-source-id: d87628d2089a2e012ad6ad50dd0a20ccec5e6c45

--- Libraries/react-native/react-native.js
+++ Libraries/react-native/react-native.js
@@ -115,11 +115,25 @@ var ReactNative = {
     get createFragment() { return require('ReactFragment').create; },
     get update() { return require('update'); },
   },
-
-  // Note: this must be placed last to prevent eager
-  // evaluation of the getter-wrapped submodules above
-  ...require('React'),
 };
+
+// Preserve getters with warnings on the internal ReactNative copy without
+// invoking them.
+var ReactNativeInternal = require('ReactNative');
+function applyForwarding(key) {
+  if (__DEV__) {
+    Object.defineProperty(
+      ReactNative,
+      key,
+      Object.getOwnPropertyDescriptor(ReactNativeInternal, key)
+    );
+    return;
+  }
+  ReactNative[key] = ReactNativeInternal[key];
+}
+for (var key in ReactNativeInternal) {
+  applyForwarding(key);
+}
 
 if (__DEV__) {
   Object.defineProperty(ReactNative.addons, 'Perf', {
