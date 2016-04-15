3eb5130a487584199a0b96dd15720c79e9d0e7fe
Konstantin Raev <bestander@fb.com>
2016-04-15 19:00:45 +0100

    testing build status

--- scripts/trigger-osx-build.sh
+++ scripts/trigger-osx-build.sh
@@ -2,9 +2,8 @@
 set -x
 
 # circle-token hardcoded
-# TODO safe?
 curl \
   --header "Content-Type: application/json" \
   --data "{\"build_parameters\": {\"LINUX_SHA1\": \"$CIRCLE_SHA1\", \"PULL_ID\": \"$CIRCLE_PR_NUMBER\"}}" \
   --request POST \
-  "https://circleci.com/api/v1/project/bestander/circle-ci-osx-test/tree/master?circle-token=655859fe09841c0b9b7029823d9c583d09816644"
+  "https://circleci.com/api/v1/project/facebook/react-native-circle-ci-ios-build/tree/master?circle-token=5fed40b1cae9e97335ca59ea9abb57d0a34b5fd3"
