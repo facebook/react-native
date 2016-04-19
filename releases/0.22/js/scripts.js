5a3d900f60418274bba01a60c184aca6601fe343
James Ide <ide@jameside.com>
2016-04-18 18:03:19 -0700

    Revert "Updated docs for 0.24"
    This reverts commit 6fda90c4fcfd67bdfbe2d2871824549d2b601416.

---
+++ releases/0.22/js/scripts.js
@@ -0,0 +1,52 @@
+(function(){
+  // Not on browser
+  if (typeof document === 'undefined') {
+    return;
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+
+  function init() {
+    var backdrop = document.querySelector('.modal-backdrop');
+    if (!backdrop) return;
+
+    var modalButtonOpenList = document.querySelectorAll('.modal-button-open');
+    var modalButtonClose = document.querySelector('.modal-button-close');
+
+    backdrop.addEventListener('click', hideModal);
+    modalButtonClose.addEventListener('click', hideModal);
+
+    // Bind event to NodeList items
+    for (var i = 0; i < modalButtonOpenList.length; ++i) {
+      modalButtonOpenList[i].addEventListener('click', showModal);
+    }
+  }
+
+  function showModal(e) {
+    var backdrop = document.querySelector('.modal-backdrop');
+    if (!backdrop) return;
+
+    var modal = document.querySelector('.modal');
+
+    backdrop.classList.add('modal-open');
+    modal.classList.add('modal-open');
+  }
+
+  function hideModal(e) {
+    var backdrop = document.querySelector('.modal-backdrop');
+    if (!backdrop) return;
+
+    var modal = document.querySelector('.modal');
+
+    backdrop.classList.remove('modal-open');
+    modal.classList.remove('modal-open');
+  }
+
+  // Algolia
+  docsearch({
+    apiKey: 'e3d767b736584dbe6d4c35f7cf7d4633',
+    indexName: 'react-native',
+    inputSelector: '#algolia-doc-search'
+  });
+
+}());
