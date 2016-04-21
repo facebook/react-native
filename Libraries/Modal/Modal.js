2b69ec25897565ebd31f3430daa18e21ec957f9d
Pieter De Baets <pieterdb@fb.com>
2016-04-21 08:56:46 -0700

    Provide initial size to Modal
    Reviewed By: majak
    
    Differential Revision: D3202211
    
    fb-gh-sync-id: 83a1d3d52df0dc68483639393ab64fd6f1281cb3
    fbshipit-source-id: 83a1d3d52df0dc68483639393ab64fd6f1281cb3

--- Libraries/Modal/Modal.js
+++ Libraries/Modal/Modal.js
@@ -11,14 +11,14 @@
  */
 'use strict';
 
-var Platform = require('Platform');
-var PropTypes = require('ReactPropTypes');
-var React = require('React');
-var StyleSheet = require('StyleSheet');
-var View = require('View');
+const Platform = require('Platform');
+const PropTypes = require('ReactPropTypes');
+const React = require('React');
+const StyleSheet = require('StyleSheet');
+const View = require('View');
 
-var requireNativeComponent = require('requireNativeComponent');
-var RCTModalHostView = requireNativeComponent('RCTModalHostView', null);
+const requireNativeComponent = require('requireNativeComponent');
+const RCTModalHostView = requireNativeComponent('RCTModalHostView', null);
 
 /**
  * A Modal component covers the native view (e.g. UIViewController, Activity)
@@ -39,9 +39,9 @@ class Modal extends React.Component {
       return null;
     }
 
-    if (this.props.transparent) {
-      var containerBackgroundColor = {backgroundColor: 'transparent'};
-    }
+    const containerBackgroundColor = {
+      backgroundColor: this.props.transparent ? 'transparent' : 'white',
+    };
 
     return (
       <RCTModalHostView
@@ -77,7 +77,7 @@ Modal.defaultProps = {
   visible: true,
 };
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   modal: {
     position: 'absolute',
   },
