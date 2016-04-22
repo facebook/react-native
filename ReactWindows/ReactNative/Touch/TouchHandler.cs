afb2188064d0b0945dcb41244d77b1c6bc719738
ebragge <ebragge@live.com>
2016-04-22 17:34:12 +0300

    fix(TouchHandler): Rename _activePointers to _pointerIDs
    Property is only used for creating unique identifers.
    
    Fixes #371

--- ReactWindows/ReactNative/Touch/TouchHandler.cs
+++ ReactWindows/ReactNative/Touch/TouchHandler.cs
@@ -15,7 +15,7 @@ namespace ReactNative.Touch
         private readonly FrameworkElement _view;
         private readonly List<ReactPointer> _pointers;
 
-        private uint _activePointers;
+        private uint _pointerIDs;
 
         public TouchHandler(FrameworkElement view)
         {
@@ -52,7 +52,7 @@ namespace ReactNative.Touch
                 var pointer = new ReactPointer();
                 pointer.Target = reactView.GetTag();
                 pointer.PointerId = e.Pointer.PointerId;
-                pointer.Identifier = ++_activePointers;
+                pointer.Identifier = ++_pointerIDs;
                 pointer.ReactView = reactView;
                 UpdatePointerForEvent(pointer, e);
 
@@ -97,17 +97,12 @@ namespace ReactNative.Touch
                 UpdatePointerForEvent(pointer, e);
                 DispatchTouchEvent(touchEventType, _pointers, pointerIndex);
 
-                if (_activePointers == pointerIndex + 1)
-                {
-                    --_activePointers;
-                }
-
-                if (_pointers.Count == 1)
-                {
-                    _activePointers = 0;
-                }
-
                 _pointers.RemoveAt(pointerIndex);
+
+                if (_pointers.Count == 0)
+                {
+                    _pointerIDs = 0;
+                }
 
                 _view.ReleasePointerCapture(e.Pointer);
             }
