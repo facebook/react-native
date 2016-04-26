5dc38d7a6d068c0d1a43ae552c341f6043146fb4
Howard Yeh <howard@metacircus.com>
2016-04-26 12:42:37 -0700

    change entry file

--- Examples/ReactPlay/UIExplorer/AppDelegate.m
+++ Examples/ReactPlay/UIExplorer/AppDelegate.m
@@ -38,7 +38,7 @@
   }
   
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:_bridge
-                                                   moduleName:@"UIExplorerApp"
+                                                   moduleName:@"RNPlay"
                                             initialProperties:initProps];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
@@ -67,7 +67,7 @@
      * on the same Wi-Fi network.
      */
 
-    sourceURL = [NSURL URLWithString:@"http://localhost:8081/Examples/UIExplorer/UIExplorerApp.ios.bundle?platform=ios&dev=true"];
+    sourceURL = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
     /**
      * OPTION 2
