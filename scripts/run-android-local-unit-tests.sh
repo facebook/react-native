d6e2a6499a1ff8f8d20d6e03b24b2e2f4ef66021
Martin Konicek <mkonicek@fb.com>
2016-05-04 08:57:42 -0700

    Add scripts for running tests locally
    Summary:
    I was going to add an integration test and realized it would be useful to have an easy way to run these test locally. Added scripts and documented how to use them.
    
    **Test plan**
    
    `./scripts/run-android-local-unit-tests.sh`
    
    <img width="786" alt="screen shot 2016-05-04 at 3 55 26 pm" src="https://cloud.githubusercontent.com/assets/346214/15018667/7f4981cc-1212-11e6-9fcb-12493c29015c.png">
    
    `./scripts/run-android-local-integration-tests.sh`
    
    <img width="772" alt="screen shot 2016-05-04 at 3 57 23 pm" src="https://cloud.githubusercontent.com/assets/346214/15018677/90b54810-1212-11e6-83d4-58530eb41d79.png">
    
    Buck check by replacing `which buck` by `which duck` in the scripts:
    
    <img width="805" alt="screen shot 2016-05-04 at 4 09 37 pm" src="https://cloud.githubusercontent.com/assets/346214/15018696/aa008262-1212-11e6-9a22-173507cd771f.png">
    
    Checked the website renders fine:
    
    `cd website; npm install; npm start`
    
    ![screen shot 2016-05-04 at 4 05 23 pm](https://cloud.githubusercontent.com/asse
    Closes https://github.com/facebook/react-native/pull/7386
    
    Differential Revision: D3258717
    
    fb-gh-sync-id: 023eb9fdb59b00f9507e2e0694ad0934bb564c03
    fbshipit-source-id: 023eb9fdb59b00f9507e2e0694ad0934bb564c03

---
+++ scripts/run-android-local-unit-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Runs all Android unit tests locally.
+# See http://facebook.github.io/react-native/docs/testing.html
+
+which buck > /dev/null || { 
+  echo "React Native uses the Buck build tool to run tests. Please install Buck: https://buckbuild.com/setup/install.html";
+  exit 1;
+}
+
+echo "Fetching dependencies..."
+buck fetch ReactAndroid/src/test/...
+echo "Running unit tests..."
+buck test ReactAndroid/src/test/...
