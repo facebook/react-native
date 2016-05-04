48e436bf07edddb14c4fcc3883d90b12db158d26
Konstantin Raev <bestander@fb.com>
2016-05-04 13:19:46 +0100

    now trying all tests together with android-17

--- scripts/run-android-instrumentation-tests.sh
+++ scripts/run-android-instrumentation-tests.sh
@@ -6,7 +6,7 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 adb logcat -c
 
 # run tests and check output
-python - $1 $2 << END
+python - $1 << END
 
 import re
 import subprocess as sp
@@ -17,7 +17,6 @@ import time
 done = False
 
 test_app = sys.argv[1]
-test_class = sys.argv[2]
 
 def update():
   # prevent CircleCI from killing the process for inactivity
@@ -31,8 +30,8 @@ t.start()
 
 def run():
   sp.Popen(['adb', 'wait-for-device']).communicate()
-  p = sp.Popen('adb shell am instrument -w -e class %s %s/android.support.test.runner.AndroidJUnitRunner' 
-    % (test_class, test_app), shell=True, stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
+  p = sp.Popen('adb shell am instrument -w %s/android.support.test.runner.AndroidJUnitRunner' 
+    % test_app, shell=True, stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
   return p.communicate()
 
 success = re.compile(r'OK \(\d+ tests\)')
