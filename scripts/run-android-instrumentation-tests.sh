49da4c782f0495e0f791d8cb1889dd2fde14470b
Konstantin Raev <bestander@fb.com>
2016-05-04 17:21:13 +0100

    allowed executing instrumentation tests for all classes

--- scripts/run-android-instrumentation-tests.sh
+++ scripts/run-android-instrumentation-tests.sh
@@ -20,8 +20,11 @@ import time
 done = False
 
 test_app = sys.argv[1]
-test_class = sys.argv[2]
+test_class = None
 
+if len(sys.argv) > 2:
+  test_class = sys.argv[2]
+  
 def update():
   # prevent CircleCI from killing the process for inactivity
   while not done:
@@ -34,8 +37,12 @@ t.start()
 
 def run():
   sp.Popen(['adb', 'wait-for-device']).communicate()
-  p = sp.Popen('adb shell am instrument -w -e class %s %s/android.support.test.runner.AndroidJUnitRunner' 
-    % (test_class, test_app), shell=True, stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
+  if (test_class != None):
+    p = sp.Popen('adb shell am instrument -w -e class %s %s/android.support.test.runner.AndroidJUnitRunner' 
+      % (test_class, test_app), shell=True, stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
+  else :
+    p = sp.Popen('adb shell am instrument -w %s/android.support.test.runner.AndroidJUnitRunner' 
+      % (test_app), shell=True, stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
   return p.communicate()
 
 success = re.compile(r'OK \(\d+ tests\)')
