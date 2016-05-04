d5e0ac1f41e533956050aa1f122c0b6379182ab8
Vojtech Rinik <vojto@rinik.net>
2016-05-04 02:16:23 -0700

    Passing status code and reason to WebSocketModule.close on Windows (#416)
    Passing status code and reason to WebSocketModule.close on Windows

--- Libraries/WebSocket/WebSocket.js
+++ Libraries/WebSocket/WebSocket.js
@@ -136,7 +136,7 @@ class WebSocket extends EventTarget(...WEBSOCKET_EVENTS) {
   }
 
   _close(code?: number, reason?: string): void {
-    if (Platform.OS === 'android') {
+    if (Platform.OS === 'android' || Platform.OS === 'windows') {
       // See https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
       const statusCode = typeof code === 'number' ? code : CLOSE_NORMAL;
       const closeReason = typeof reason === 'string' ? reason : '';
