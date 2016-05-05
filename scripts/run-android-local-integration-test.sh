6c2ea5cf513c887c6762647b07f7903a2ab4370c
Konstantin Raev <bestander@fb.com>
2016-05-05 08:17:02 -0700

    Open sourced SwipeRefreshLayoutRecordingModule
    Reviewed By: dmmiller
    
    Differential Revision: D3264212
    
    fb-gh-sync-id: 8790200a0d3c47050d6a91c906854b3a55640829
    fbshipit-source-id: 8790200a0d3c47050d6a91c906854b3a55640829

--- scripts/run-android-local-integration-test.sh
+++ scripts/run-android-local-integration-test.sh
@@ -3,6 +3,8 @@
 # Runs all Android integration tests locally.
 # See http://facebook.github.io/react-native/docs/testing.html
 
+set -e
+
 which buck > /dev/null || { 
   echo "React Native uses the Buck build tool to run tests. Please install Buck: https://buckbuild.com/setup/install.html";
   exit 1;
@@ -13,6 +15,7 @@ echo "Compiling native code..."
 echo "Building JS bundle..."
 node local-cli/cli.js bundle --platform android --dev true --entry-file ReactAndroid/src/androidTest/js/TestBundle.js --bundle-output ReactAndroid/src/androidTest/assets/AndroidTestBundle.js
 echo "Installing test app on the device..."
+buck fetch ReactAndroid/src/androidTest/buck-runner:instrumentation-tests
 buck install ReactAndroid/src/androidTest/buck-runner:instrumentation-tests
 echo "Running integration tests..."
-./scripts/run-android-instrumentation-tests.sh com.facebook.react.tests
+adb shell am instrument -w com.facebook.react.tests/android.support.test.runner.AndroidJUnitRunner
