a0c6b33bd66455bb9da5c1676003c885af579035
David Aurelio <davidaurelio@fb.com>
2016-05-12 17:13:12 -0700

    Use continuous module IDs for random access bundles
    Reviewed By: bestander
    
    Differential Revision: D3292980
    
    fbshipit-source-id: ab5791d31add42a26cf55a0309564330c383eaa2

--- packager/react-packager/src/Bundler/__tests__/Bundler-test.js
+++ packager/react-packager/src/Bundler/__tests__/Bundler-test.js
@@ -128,6 +128,7 @@ describe('Bundler', function() {
         mainModuleId: 'foo',
         dependencies: modules,
         transformOptions,
+        getModuleId: () => 123,
       })
     );
 
@@ -207,7 +208,8 @@ describe('Bundler', function() {
   pit('gets the list of dependencies from the resolver', function() {
     const entryFile = '/root/foo.js';
     return bundler.getDependencies({entryFile, recursive: true}).then(() =>
-      expect(getDependencies).toBeCalledWith(
+      // jest calledWith does not support jasmine.any
+      expect(getDependencies.mock.calls[0].slice(0, -2)).toEqual([
         '/root/foo.js',
         { dev: true, recursive: true },
         { minify: false,
@@ -219,8 +221,7 @@ describe('Bundler', function() {
             projectRoots,
           }
         },
-        undefined,
-      )
+      ])
     );
   });
 
