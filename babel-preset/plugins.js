cf8999867f05a6aa08509d76d4f3f3a72f1c4fb9
Robert Rose <bertra97@gmail.com>
2016-05-15 22:38:25 -0400

    Issue #7526 Finishing Touches: babel-plugin-transform-es2015-literals
    - Was able to get react-native to build from source.
    - Following advice from @janicduplessis in Issue #7526 to add the
    `babel-plugin-transform-es2015-literals` to the babel-presets.
    - Added code to babel-preset/plugins.js and babel-preset/configs/main.js
    so it works correctly.
    - Ran local-cli.
    
    Test Cases:
    - Compiled and ran UIExplorer: Worked
    - Compiled and ran UIExplorer with binary number inserted in: worked
    - Compiled and ran blank project with binary number (by using react-native
      init and changing code in node_modules): worked
    
    I'm also filling a separate bug report that release builds are failing on
    Windows. I have confirmed this by trying to build from a clean copy of the
    repo.

--- babel-preset/plugins.js
+++ babel-preset/plugins.js
@@ -26,6 +26,7 @@ module.exports = {
   'babel-plugin-transform-es2015-shorthand-properties': require('babel-plugin-transform-es2015-shorthand-properties'),
   'babel-plugin-transform-es2015-spread': require('babel-plugin-transform-es2015-spread'),
   'babel-plugin-transform-es2015-template-literals': require('babel-plugin-transform-es2015-template-literals'),
+  'babel-plugin-transform-es2015-literals' : require('babel-plugin-transform-es2015-literals'),
   'babel-plugin-transform-flow-strip-types': require('babel-plugin-transform-flow-strip-types'),
   'babel-plugin-transform-object-assign': require('babel-plugin-transform-object-assign'),
   'babel-plugin-transform-object-rest-spread': require('babel-plugin-transform-object-rest-spread'),
