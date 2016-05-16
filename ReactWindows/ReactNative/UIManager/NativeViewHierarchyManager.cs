microsoft/react-native-windows

Fixes #132 - Cosmetic changes from ViewGroup -> ViewParent

ReactWindows/ReactNative/UIManager/NativeViewHierarchyManager.cs
@@ -120,18 +120,18 @@ namespace ReactNative.UIManager
                 var viewToUpdate = ResolveView(tag);
 
                 var parentViewManager = default(ViewManager);
-                var parentViewGroupManager = default(ViewGroupManager);
+                var parentViewParentManager = default(ViewParentManager);
                 if (!_tagsToViewManagers.TryGetValue(parentTag, out parentViewManager) || 
-                    (parentViewGroupManager = parentViewManager as ViewGroupManager) == null)
+                    (parentViewParentManager = parentViewManager as ViewParentManager) == null)
                 {
                     throw new InvalidOperationException(
                         string.Format(
                             CultureInfo.InvariantCulture,
-                            "Trying to use view with tag '{0}' as a parent, but its manager doesn't extend ViewGroupManager.",
+                            "Trying to use view with tag '{0}' as a parent, but its manager doesn't extend ViewParentManager.",
                             tag));
                 }
 
-                if (!parentViewGroupManager.NeedsCustomLayoutForChildren)
+                if (!parentViewParentManager.NeedsCustomLayoutForChildren)
                 {
                     UpdateLayout(viewToUpdate, x, y, width, height);
                 }
@@ -188,10 +188,10 @@ namespace ReactNative.UIManager
                         tag));
             }
 
-            var viewGroupManager = (ViewGroupManager)viewManager;
+            var viewParentManager = (ViewParentManager)viewManager;
             var viewToManage = _tagsToViews[tag];
 
-            var lastIndexToRemove = viewGroupManager.GetChildCount(viewToManage);
+            var lastIndexToRemove = viewParentManager.GetChildCount(viewToManage);
             if (indicesToRemove != null)
             {
                 for (var i = indicesToRemove.Length - 1; i >= 0; --i)
@@ -207,7 +207,7 @@ namespace ReactNative.UIManager
                                 tag));
                     }
 
-                    if (indexToRemove >= viewGroupManager.GetChildCount(viewToManage))
+                    if (indexToRemove >= viewParentManager.GetChildCount(viewToManage))
                     {
                         throw new InvalidOperationException(
                             string.Format(
@@ -228,7 +228,7 @@ namespace ReactNative.UIManager
                                 tag));
                     }
 
-                    viewGroupManager.RemoveChildAt(viewToManage, indexToRemove);
+                    viewParentManager.RemoveChildAt(viewToManage, indexToRemove);
                     lastIndexToRemove = indexToRemove;
                 }
             }
@@ -248,7 +248,7 @@ namespace ReactNative.UIManager
                                 viewAtIndex.Tag));
                     }
 
-                    viewGroupManager.AddView(viewToManage, viewToAdd, viewAtIndex.Index);
+                    viewParentManager.AddView(viewToManage, viewToAdd, viewAtIndex.Index);
                 }
             }
 
@@ -335,7 +335,7 @@ namespace ReactNative.UIManager
         /// <param name="themedContext">The themed context.</param>
         public void AddRootView(int tag, SizeMonitoringCanvas view, ThemedReactContext themedContext)
         {
-            AddRootViewGroup(tag, view, themedContext);
+            AddRootViewParent(tag, view, themedContext);
         }
 
         /// <summary>
@@ -475,7 +475,7 @@ namespace ReactNative.UIManager
             return viewManager;
         }
 
-        private void AddRootViewGroup(int tag, FrameworkElement view, ThemedReactContext themedContext)
+        private void AddRootViewParent(int tag, FrameworkElement view, ThemedReactContext themedContext)
         {
             DispatcherHelpers.AssertOnDispatcher();
             _tagsToViews.Add(tag, view);
@@ -499,13 +499,13 @@ namespace ReactNative.UIManager
             var viewManager = default(ViewManager);
             if (_tagsToViewManagers.TryGetValue(tag, out viewManager))
             {
-                var viewGroup = view as Panel;
-                var viewGroupManager = viewManager as ViewGroupManager;
-                if (viewGroup != null && viewGroupManager != null)
+                var viewParent = view as Panel;
+                var viewParentManager = viewManager as ViewParentManager;
+                if (viewParent != null && viewParentManager != null)
                 {
-                    for (var i = viewGroupManager.GetChildCount(viewGroup) - 1; i >= 0; --i)
+                    for (var i = viewParentManager.GetChildCount(viewParent) - 1; i >= 0; --i)
                     {
-                        var child = viewGroupManager.GetChildAt(viewGroup, i);
+                        var child = viewParentManager.GetChildAt(viewParent, i);
                         var managedChild = default(FrameworkElement);
                         if (_tagsToViews.TryGetValue(child.GetTag(), out managedChild))
                         {
@@ -514,7 +514,7 @@ namespace ReactNative.UIManager
                     }
                 }
 
-                viewGroupManager.RemoveAllChildren(viewGroup);
+                viewParentManager.RemoveAllChildren(viewParent);
             }
 
             _tagsToViews.Remove(tag);

facebook/react-native

ReactWindows/ReactNative/UIManager/NativeViewHierarchyManager.cs
@@ -125,18 +125,18 @@ namespace ReactNative.UIManager
                 var viewToUpdate = ResolveView(tag);
                 
                 var parentViewManager = default(ViewManager);
-                var parentViewGroupManager = default(ViewGroupManager);
+                var parentViewParentManager = default(ViewParentManager);
                 if (!_tagsToViewManagers.TryGetValue(parentTag, out parentViewManager) || 
-                    (parentViewGroupManager = parentViewManager as ViewGroupManager) == null)
+                    (parentViewParentManager = parentViewManager as ViewParentManager) == null)
                 {
                     throw new InvalidOperationException(
                         string.Format(
                             CultureInfo.InvariantCulture,
-                            "Trying to use view with tag '{0}' as a parent, but its manager doesn't extend ViewGroupManager.",
+                            "Trying to use view with tag '{0}' as a parent, but its manager doesn't extend ViewParentManager.",
                             tag));
                 }
 
-                if (!parentViewGroupManager.NeedsCustomLayoutForChildren)
+                if (!parentViewParentManager.NeedsCustomLayoutForChildren)
                 {
                     UpdateLayout(viewToUpdate, x, y, width, height);
                 }
@@ -212,10 +212,10 @@ namespace ReactNative.UIManager
                         tag));
             }
 
-            var viewGroupManager = (ViewGroupManager)viewManager;
+            var viewParentManager = (ViewParentManager)viewManager;
             var viewToManage = _tagsToViews[tag];
 
-            var lastIndexToRemove = viewGroupManager.GetChildCount(viewToManage);
+            var lastIndexToRemove = viewParentManager.GetChildCount(viewToManage);
             if (indicesToRemove != null)
             {
                 for (var i = indicesToRemove.Length - 1; i >= 0; --i)
@@ -231,7 +231,7 @@ namespace ReactNative.UIManager
                                 tag));
                     }
 
-                    if (indexToRemove >= viewGroupManager.GetChildCount(viewToManage))
+                    if (indexToRemove >= viewParentManager.GetChildCount(viewToManage))
                     {
                         throw new InvalidOperationException(
                             string.Format(
@@ -252,7 +252,7 @@ namespace ReactNative.UIManager
                                 tag));
                     }
 
-                    viewGroupManager.RemoveChildAt(viewToManage, indexToRemove);
+                    viewParentManager.RemoveChildAt(viewToManage, indexToRemove);
                     lastIndexToRemove = indexToRemove;
                 }
             }
@@ -272,7 +272,7 @@ namespace ReactNative.UIManager
                                 viewAtIndex.Tag));
                     }
 
-                    viewGroupManager.AddView(viewToManage, viewToAdd, viewAtIndex.Index);
+                    viewParentManager.AddView(viewToManage, viewToAdd, viewAtIndex.Index);
                 }
             }
 
@@ -365,7 +365,7 @@ namespace ReactNative.UIManager
         /// <param name="themedContext">The themed context.</param>
         public void AddRootView(int tag, SizeMonitoringCanvas view, ThemedReactContext themedContext)
         {
-            AddRootViewGroup(tag, view, themedContext);
+            AddRootViewParent(tag, view, themedContext);
         }
 
         /// <summary>
@@ -515,7 +515,7 @@ namespace ReactNative.UIManager
             return viewManager;
         }
 
-        private void AddRootViewGroup(int tag, FrameworkElement view, ThemedReactContext themedContext)
+        private void AddRootViewParent(int tag, FrameworkElement view, ThemedReactContext themedContext)
         {
             DispatcherHelpers.AssertOnDispatcher();
             _tagsToViews.Add(tag, view);
@@ -539,12 +539,12 @@ namespace ReactNative.UIManager
             var viewManager = default(ViewManager);
             if (_tagsToViewManagers.TryGetValue(tag, out viewManager))
             {
-                var viewGroupManager = viewManager as ViewGroupManager;
-                if (viewGroupManager != null)
+                var viewParentManager = viewManager as ViewParentManager;
+                if (viewParentManager != null)
                 {
-                    for (var i = viewGroupManager.GetChildCount(view) - 1; i >= 0; --i)
+                    for (var i = viewParentManager.GetChildCount(view) - 1; i >= 0; --i)
                     {
-                        var child = viewGroupManager.GetChildAt(view, i);
+                        var child = viewParentManager.GetChildAt(view, i);
                         var managedChild = default(FrameworkElement);
                         if (_tagsToViews.TryGetValue(child.GetTag(), out managedChild))
                         {
@@ -552,7 +552,7 @@ namespace ReactNative.UIManager
                         }
                     }
 
-                    viewGroupManager.RemoveAllChildren(view);
+                    viewParentManager.RemoveAllChildren(view);
                 }
             }
 