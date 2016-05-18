ba31c1557b24c97ef204b458fd9d3e29a1e96207
Marc Horowitz <mhorowitz@fb.com>
2016-05-18 12:46:01 -0700

    Implement incremental module requires
    Differential Revision: D3234844
    
    fbshipit-source-id: 24615528ad6a049aad7c2dbb7ce55e8b034c79e7

--- ReactCommon/bridge/ModuleRegistry.h
+++ ReactCommon/bridge/ModuleRegistry.h
@@ -25,13 +25,18 @@ class ModuleRegistry {
   // notifyCatalystInstanceDestroy: use RAII instead
 
   ModuleRegistry(std::vector<std::unique_ptr<NativeModule>> modules);
-  folly::dynamic moduleDescriptions();
+  std::vector<std::string> moduleNames();
+  folly::dynamic getConfig(const std::string& name);
   void callNativeMethod(ExecutorToken token, unsigned int moduleId, unsigned int methodId,
                         folly::dynamic&& params, int callId);
   MethodCallResult callSerializableNativeHook(ExecutorToken token, unsigned int moduleId, unsigned int methodId, folly::dynamic&& args);
 
  private:
+  // This is always populated
   std::vector<std::unique_ptr<NativeModule>> modules_;
+
+  // This is only populated if moduleNames() is called.  Values are indices into modules_.
+  std::unordered_map<std::string, size_t> modulesByName_;
 };
 
 }
