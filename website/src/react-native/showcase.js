e723d6c6ab6c8fb68d5b0b7ab53b0a514d5c2ecc
RetroXeusPoktan <575508330@qq.com>
2016-05-18 04:15:53 -0700

    Add QQ音乐 全民K歌 QQ空间 QQ to showcase
    Summary:
    Add QQ音乐 全民K歌 QQ空间 QQ to the showcase page
    Closes https://github.com/facebook/react-native/pull/7582
    
    Differential Revision: D3316209
    
    fbshipit-source-id: 47e5ce7ec91fb5f4ff747e4ea9ea77476a49fdbc

--- website/src/react-native/showcase.js
+++ website/src/react-native/showcase.js
@@ -689,6 +689,24 @@ var apps = [
     author: 'ProjectSeptember.com',
   },
   {
+    name: 'QQ',
+    icon: 'http://pp.myapp.com/ma_icon/0/icon_6633_1461768893/96',
+    link: 'http://android.myapp.com/myapp/detail.htm?apkName=com.tencent.mobileqq',
+    author: '邱俊',
+  },
+  {
+    name: 'QQ空间',
+    icon: 'http://pp.myapp.com/ma_icon/0/icon_9959_1460036593/96',
+    link: 'http://android.myapp.com/myapp/detail.htm?apkName=com.qzone',
+    author: '薛丰',
+  },
+  {
+    name: 'QQ音乐',
+    icon: 'http://pp.myapp.com/ma_icon/0/icon_6259_1462429453/96',
+    link: 'http://android.myapp.com/myapp/detail.htm?apkName=com.tencent.qqmusic',
+    author: '石玉磊',
+  },
+  {
     name: 'Raindrop.io',
     icon: 'http://a5.mzstatic.com/us/r30/Purple3/v4/f0/a4/57/f0a4574e-4a59-033f-05ff-5c421f0a0b00/icon175x175.png',
     link: 'https://itunes.apple.com/us/app/raindrop.io-keep-your-favorites/id1021913807',
@@ -983,6 +1001,12 @@ var apps = [
     icon: 'https://xiaoxian.ele.me/download/images/share_page/ic_launcher.png',
     linkAppStore: 'https://itunes.apple.com/cn/app/e-xiao-xian/id1092025196',
     author: 'Eleme',
+  },
+  {
+    name: '全民K歌',
+    icon: 'http://pp.myapp.com/ma_icon/0/icon_10966186_1460087288/96',
+    link: 'http://android.myapp.com/myapp/detail.htm?apkName=com.tencent.karaoke',
+    author: '石玉磊',
   },
 ];
 
