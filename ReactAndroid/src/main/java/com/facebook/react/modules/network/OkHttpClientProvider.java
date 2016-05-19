dc85a8e3c1b40b4286a8c4dd82d3b55c67c23ff4
Rahul Jiresal <rahul@aerofs.com>
2016-05-19 15:23:39 -0700

    Fix for okhttp to for REFUED_STREAM on nginx with HTTP/2

--- ReactAndroid/src/main/java/com/facebook/react/modules/network/OkHttpClientProvider.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/network/OkHttpClientProvider.java
@@ -38,12 +38,23 @@ public class OkHttpClientProvider {
   }
 
   private static OkHttpClient createClient() {
+
+    // create a list of protocols the client will use to communicate
+    // by default, it will use the most efficient one, falling back to 
+    // more ubiquitous ones
+    // above as per okhttp javadocs
+    List<Protocol> protocolList = new ArrayList<>();
+    protocolList.add(Protocol.HTTP_2);
+    protocolList.add(Protocol.SPDY_3);
+    protocolList.add(Protocol.HTTP_1_1);
+
     // No timeouts by default
     return new OkHttpClient.Builder()
       .connectTimeout(0, TimeUnit.MILLISECONDS)
       .readTimeout(0, TimeUnit.MILLISECONDS)
       .writeTimeout(0, TimeUnit.MILLISECONDS)
       .cookieJar(new ReactCookieJarContainer())
+      .setProtocols(protocolList)
       .build();
   }
 }
