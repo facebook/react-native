04d88d990a4a5ee38a0ff39b0af2f75733f478b4
Kureev Alexey <kureev-mail@ya.ru>
2016-06-06 02:02:17 +0200

    Cleanup legacy tests + start iOS tests migration

--- local-cli/rnpm/link/__tests__/ios/getPlistPath.spec.js
+++ local-cli/rnpm/link/__tests__/ios/getPlistPath.spec.js
@@ -1,19 +1,22 @@
-const chai = require('chai');
-const expect = chai.expect;
+'use strict';
+
+jest.autoMockOff();
+
 const xcode = require('xcode');
+const path = require('path');
 const getPlistPath = require('../../src/ios/getPlistPath');
 
-const project = xcode.project('test/fixtures/project.pbxproj');
+const project = xcode.project(
+  path.join(__dirname, '../fixtures/project.pbxproj')
+);
 
 describe('ios::getPlistPath', () => {
-
   beforeEach(() => {
     project.parseSync();
   });
 
   it('should return path without Xcode $(SRCROOT)', () => {
     const plistPath = getPlistPath(project, '/');
-    expect(plistPath).to.equals('/Basic/Info.plist');
+    expect(plistPath).toBe('/Basic/Info.plist');
   });
-
 });
