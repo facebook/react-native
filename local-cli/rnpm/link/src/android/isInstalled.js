53b58c4b0232d1d2e7d0c168d4b7429a85fd7af0
Kureev Alexey <kureev-mail@ya.ru>
2016-06-06 02:01:57 +0200

    Make Android tests to work

--- local-cli/rnpm/link/src/android/isInstalled.js
+++ local-cli/rnpm/link/src/android/isInstalled.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const makeBuildPatch = require(`./patches/makeBuildPatch`);
+const makeBuildPatch = require('./patches/makeBuildPatch');
 
 module.exports = function isInstalled(config, name) {
   return fs
