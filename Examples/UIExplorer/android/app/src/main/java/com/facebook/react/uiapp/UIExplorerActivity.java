70fb7730baff43a1550ab55ac46c41ac8d6d1453
leeight <leeight@gmail.com>
2016-06-20 21:50:15 +0800

    Add license header and remove <p/>

--- Examples/UIExplorer/android/app/src/main/java/com/facebook/react/uiapp/UIExplorerActivity.java
+++ Examples/UIExplorer/android/app/src/main/java/com/facebook/react/uiapp/UIExplorerActivity.java
@@ -1,9 +1,9 @@
 /**
  * The examples provided by Facebook are for non-commercial testing and
  * evaluation purposes only.
- * <p/>
+ *
  * Facebook reserves all rights not expressly granted.
- * <p/>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
