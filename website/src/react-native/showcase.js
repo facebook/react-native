4c3c15fd8978f37c24237033e7e4db3a254e131f
Dale Jefferson <dale@dalejefferson.com>
2016-06-20 21:25:10 -0700

    Add app to showcase - RNF Beacon Toolkit
    Summary:
    iBeacon app built with React Native
    Closes https://github.com/facebook/react-native/pull/8245
    
    Differential Revision: D3462061
    
    fbshipit-source-id: e57437c66abb4cc25d78e24f5724dc5db2710046

--- website/src/react-native/showcase.js
+++ website/src/react-native/showcase.js
@@ -832,6 +832,12 @@ var apps = [
     author: 'Jed Tiotuico',
   },
   {
+    name: 'RNF Beacon Toolkit',
+    icon: 'http://a1.mzstatic.com/eu/r30/Purple20/v4/99/79/5e/99795e25-e1f7-28bc-efcf-fd3c87f02f4e/icon175x175.png',
+    linkAppStore: 'https://itunes.apple.com/gb/app/rnf-beacon-toolkit/id1110757307',
+    author: 'RNF Digital',
+  },
+  {
     name: 'Rota Employer - Hire On Demand',
     link: 'https://itunes.apple.com/us/app/rota-employer-hire-on-demand/id1042270305?mt=8',
     icon: 'https://avatars2.githubusercontent.com/u/15051833?v=3&s=200',
