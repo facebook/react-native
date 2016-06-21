1b2ead414f5cda99a64909ce8194186ed05cad65
Kevin Lacker <lacker@gmail.com>
2016-06-21 15:34:34 -0700

    marketing on native-when-you-need-to

--- website/src/react-native/index.js
+++ website/src/react-native/index.js
@@ -98,238 +98,27 @@ class AwkwardScrollingImageWithText extends Component {
 
           <h2>Use Native Code When You Need To</h2>
           <p>
-            React Native is focused on changing the way view code is written.  For the rest, we look to the web for universal standards and polyfill those APIs where appropriate. You can use npm to install JavaScript libraries that work on top of the functionality baked into React Native, such as XMLHttpRequest, window.requestAnimationFrame, and navigator.geolocation.  We are working on expanding the available APIs, and are excited for the Open Source community to contribute as well.
+            React Native combines smoothly with components written in Objective-C, Java, or Swift. It's simple to drop down to native code if you need to optimize a few aspects of your application. It's also easy to build part of your app in React Native, and part of your app using native code directly - that's how the Facebook app works.
           </p>
+
           <Prism>
-{`// iOS & Android
+{`import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { TheGreatestComponentInTheWorld } from './your-native-code';
 
-import React, {
-  Component,
-} from 'react';
-import {
-  Text,
-} from 'react-native';
-
-class GeoInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { position: 'unknown' };
-  },
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({position}),
-      (error) => console.error(error)
-    );
-  }
+class SomethingFast extends Component {
   render() {
     return (
-      <Text>
-        Position: {JSON.stringify(this.state.position)}
-      </Text>
+      <View>
+        <TheGreatestComponentInTheWorld />
+        <Text>
+          TheGreatestComponentInTheWorld could use native Objective-C,
+          Java, or Swift - the product development process is the same.
+        </Text>
+      </View>
     );
   }
 }`}
-          </Prism>
-
-          <h2>Extensibility</h2>
-          <p>
-            It is certainly possible to create a great app using React Native without writing a single line of native code, but React Native is also designed to be easily extended with custom native views and modules - that means you can reuse anything you{"'"}ve already built, and can import and use your favorite native libraries.
-          </p>
-          <h3>Creating iOS modules</h3>
-          <p>
-            To create a simple iOS module, create a new class that implements the RCTBridgeModule protocol, and wrap the function that you want to make available to JavaScript in RCT_EXPORT_METHOD. Additionally, the class itself must be explicitly exported with RCT_EXPORT_MODULE();.
-          </p>
-          <Prism>
-{`// Objective-C
-
-#import "RCTBridgeModule.h"
-
-@interface MyCustomModule : NSObject <RCTBridgeModule>
-@end
-
-@implementation MyCustomModule
-
-RCT_EXPORT_MODULE();
-
-// Available as NativeModules.MyCustomModule.processString
-RCT_EXPORT_METHOD(processString:(NSString *)input callback:(RCTResponseSenderBlock)callback)
-{
-  callback(@[[input stringByReplacingOccurrencesOfString:@"Goodbye" withString:@"Hello"]]);
-}
-@end`}
-          </Prism>
-          <Prism>
-{`// JavaScript
-
-import React, {
-  Component,
-} from 'react';
-import {
-  NativeModules,
-  Text,
-} from 'react-native';
-
-class Message extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: 'Goodbye World.' };
-  }
-  componentDidMount() {
-    NativeModules.MyCustomModule.processString(this.state.text, (text) => {
-      this.setState({text});
-    });
-  }
-  render() {
-    return (
-      <Text>{this.state.text}</Text>
-    );
-  }
-}`}
-          </Prism>
-
-          <h3>Creating iOS views</h3>
-          <p>
-            Custom iOS views can be exposed by subclassing RCTViewManager, implementing a -(UIView *)view method, and exporting properties with the RCT_EXPORT_VIEW_PROPERTY macro.  Then a simple JavaScript file connects the dots.
-          </p>
-          <Prism>
-{`// Objective-C
-
-#import "RCTViewManager.h"
-
-@interface MyCustomViewManager : RCTViewManager
-@end
-
-@implementation MyCustomViewManager
-
-RCT_EXPORT_MODULE()
-
-- (UIView *)view
-{
-  return [[MyCustomView alloc] init];
-}
-
-RCT_EXPORT_VIEW_PROPERTY(myCustomProperty, NSString);
-@end`}
-          </Prism>
-          <Prism>
-{`// JavaScript
-
-import React, {
-  Component,
-} from 'react';
-import {
-  requireNativeComponent,
-} from 'react-native';
-
-var NativeMyCustomView = requireNativeComponent('MyCustomView', MyCustomView);
-
-export default class MyCustomView extends Component {
-  static propTypes = {
-    myCustomProperty: React.PropTypes.oneOf(['a', 'b']),
-  };
-  render() {
-    return <NativeMyCustomView {...this.props} />;
-  }
-}
-`}
-          </Prism>
-
-          <h3>Creating Android modules</h3>
-          <p>
-            Likewise, Android also supports custom extensions, the methods are just slightly different.
-          </p>
-          <p>
-            To create a simple module in Android, create a new class that extends the ReactContextBaseJavaModule class, and annotate the function that you want to make available to JavaScript with @ReactMethod. Additionally, the class itself must be registered in the ReactPackage of your React Native application.
-          </p>
-          <Prism>
-{`// Java
-
-public class MyCustomModule extends ReactContextBaseJavaModule {
-
-// Available as NativeModules.MyCustomModule.processString
-  @ReactMethod
-  public void processString(String input, Callback callback) {
-    callback.invoke(input.replace("Goodbye", "Hello"));
-  }
-}
-`}
-</Prism>
-
-<Prism>
-{`// JavaScript
-
-import React, {
-  Component,
-} from 'react';
-import {
-  NativeModules,
-  Text,
-} from 'react-native';
-class Message extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: 'Goodbye World.' };
-  },
-  componentDidMount() {
-    NativeModules.MyCustomModule.processString(this.state.text, (text) => {
-      this.setState({text});
-    });
-  }
-  render() {
-    return (
-      <Text>{this.state.text}</Text>
-    );
-  }
-}
-`}
-          </Prism>
-
-          <h3>Creating Android views</h3>
-          <p>
-          Custom Android views can be exposed by extending SimpleViewManager, implementing a createViewInstance and getName methods, and exporting properties with the @ReactProp annotation. Then a simple JavaScript file connects the dots.
-          </p>
-          <Prism>
-{`// Java
-
-public class MyCustomViewManager extends SimpleViewManager<MyCustomView> {
-  @Override
-  public String getName() {
-    return "MyCustomView";
-  }
-
-  @Override
-  protected MyCustomView createViewInstance(ThemedReactContext reactContext) {
-    return new MyCustomView(reactContext);
-  }
-
-  @ReactProp(name = "myCustomProperty")
-  public void setMyCustomProperty(MyCustomView view, String value) {
-    view.setMyCustomProperty(value);
-  }
-}
-`}
-          </Prism>
-          <Prism>
-{`// JavaScript
-
-import React, {
-  Component,
-} from 'react';
-import {
-  requireNativeComponent,
-} from 'react-native';
-
-var NativeMyCustomView = requireNativeComponent('MyCustomView', MyCustomView);
-
-export default class MyCustomView extends Component {
-  static propTypes = {
-    myCustomProperty: React.PropTypes.oneOf(['a', 'b']),
-  };
-  render() {
-    return <NativeMyCustomView {...this.props} />;
-  }
-}
-`}
           </Prism>
           </div>
           <section className="home-bottom-section">
