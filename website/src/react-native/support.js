d662628ee3d7a42c24176344a6f9c6068523ea44
Joel Marcey <joelm@fb.com>
2016-06-21 17:21:48 -0700

    Move Videos and Newsletter to Support
    Summary:
    Simplify the sidebar. We have Twitter feed in support. These have
    a community feel as well.
    Closes https://github.com/facebook/react-native/pull/8287
    
    Differential Revision: D3467042
    
    Pulled By: lacker
    
    fbshipit-source-id: 60749d0cb31f284dae7c5402bfcde7b4d01aa32f

--- website/src/react-native/support.js
+++ website/src/react-native/support.js
@@ -61,6 +61,70 @@ var support = React.createClass({
             <p><a href="https://twitter.com/search?q=%23reactnative"><strong>#reactnative</strong> hash tag on Twitter</a> is used to keep up with the latest React Native news.</p>
 
             <p><center><a className="twitter-timeline" data-dnt="true" data-chrome="nofooter noheader transparent" href="https://twitter.com/search?q=%23reactnative" data-widget-id="565960513457098753"></a></center></p>
+
+            <h2>Newsletter</h2>
+            <p>Community member Brent Vatne runs an occasional <a href="http://reactnative.cc/">React Native newsletter</a> with news and happenings from the world of React Native.</p>
+
+            <h2>Audio and Video</h2>
+            <p>Check out various audio and video of those speaking about React Native at conferences, in podcasts, etc.</p>
+            <h3>React.js Conf 2016</h3>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/2Zthnq-hIXA" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/Xnqy_zkBAew" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/RBg2_uQE4KM" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/0MlT74erp60" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/B8J8xn3pLpk" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/f1Sj48rJE3I" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/uBYPqb83C7k" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/09ddrCaLo10" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/d3VVfA9hWjc" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/impQkQOCbMw" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/wuLKELLuwVk" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/Zoerbz5Mu5U" frameborder="0" allowfullscreen></iframe>
+
+
+            <h3>React.js Conf 2015</h3>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/KVZ-P-ZI6W4" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/7rDsRXj9-cU" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/X6YbAKiLCLU" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/oWPoW0gIzvs" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/hDviGU-57lU" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/8N4f4h6SThc" frameborder="0" allowfullscreen></iframe>
+
+            <iframe width="650" height="315" src="//www.youtube.com/embed/-XxSCi8TKuk" frameborder="0" allowfullscreen></iframe>
+
+            <h3><a href="https://thechangelog.com/149/">The Changelog #149</a></h3>
+            <p>With Christopher "vjeux" Chedeau and Spencer Ahrens</p>
+
+            <audio src="http://fdlyr.co/d/changelog/cdn.5by5.tv/audio/broadcasts/changelog/2015/changelog-149.mp3" controls="controls" preload="none"></audio>
+
+            <h3><a href="http://devchat.tv/js-jabber/146-jsj-react-with-christopher-chedeau-and-jordan-walke">JSJabber #146</a></h3>
+            <p>With Christopher "vjeux" Chedeau and Jordan Walke</p>
+
+            <audio controls>
+            <source ng-src="http://www.podtrac.com/pts/redirect.mp3/media.devchat.tv/js-jabber/JSJ146React.mp3?player=true" type="audio/mpeg" src="http://www.podtrac.com/pts/redirect.mp3/media.devchat.tv/js-jabber/JSJ146React.mp3?player=true" />
+            <p>
+            This player is only available in HTML5 enabled browsers. Please update your browser or
+            <a download="146-jsj-react-with-christopher-chedeau-and-jordan-walke.mp3" href="http://www.podtrac.com/pts/redirect.mp3/media.devchat.tv/js-jabber/JSJ146React.mp3?download=true?download=true">download the episode</a>
+            </p>
+            </audio>
           </div>
         </section>
 
