584a3d8ef1f97c3087895119066593d473b7af68
Emmanouil Konstantinidis <ekonstantinidis@users.noreply.github.com>
2016-06-23 21:13:31 +0100

    Include Gitify to Showcase

--- website/src/react-native/showcase.js
+++ website/src/react-native/showcase.js
@@ -463,6 +463,13 @@ var apps = [
     author: 'Foodstand, Inc.',
   },
   {
+    name: 'Gitify',
+    icon: 'http://a2.mzstatic.com/us/r30/Purple5/v4/42/50/5a/42505a8d-3c7a-e49a-16e3-422315f24cf1/icon350x350.png',
+    linkAppStore: 'https://itunes.apple.com/us/app/gitify/id1123311036?ls=1&mt=8',
+    linkPlayStore:  'http://play.google.com/store/apps/details?id=com.gitify',
+    author: 'Emmanouil Konstantinidis',
+  },
+  {
     name: 'Go Fire',
     icon: 'http://a2.mzstatic.com/us/r30/Purple5/v4/42/50/5a/42505a8d-3c7a-e49a-16e3-422315f24cf1/icon350x350.png',
     link: 'https://itunes.apple.com/us/app/gou-huo/id1001476888?ls=1&mt=8',
