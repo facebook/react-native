ab0934972ad77997e6ba7c4855d9de90465defdd
Joel Marcey <joelm@fb.com>
2016-06-23 15:08:13 -0700

    Move Component Embedded Simulator next to its example
    Right now the embedded simulator is always at the top right corner.
    This can be confusing as to what code is associated with the simulation.
    
    So, move the simulator next to its actual code.
    
    This has the added benefit of allowing us to use the React Native
    Web Player for the simpler examples in the components.
    
    Test Plan:
    
    http://localhost:8079/react-native/docs/switch.html

--- website/layout/AutodocsLayout.js
+++ website/layout/AutodocsLayout.js
@@ -775,7 +775,7 @@ var EmbeddedSimulator = React.createClass({
       : <img alt="Run example in simulator" width="170" height="356" src="img/uiexplorer_main_ios.png" />;
 
     return (
-      <div className="column-left">
+      <div className="embedded-simulator">
         <p><a className="modal-button-open"><strong>Run this example</strong></a></p>
         <div className="modal-button-open modal-button-open-img">
           {imagePreview}
@@ -855,9 +855,12 @@ var Autodocs = React.createClass({
           path={example.path}
           metadata={metadata}
         />
-        <Prism>
-          {example.content.replace(/^[\s\S]*?\*\//, '').trim()}
-        </Prism>
+        <div className="example-container">
+          <Prism>
+           {example.content.replace(/^[\s\S]*?\*\//, '').trim()}
+          </Prism>
+          <EmbeddedSimulator shouldRender={metadata.runnable} metadata={metadata} />
+        </div>
       </div>
     );
   },
@@ -901,9 +904,6 @@ var Autodocs = React.createClass({
               {metadata.next && <a className="docs-next" href={'docs/' + metadata.next + '.html#content'}>Next &rarr;</a>}
             </div>
           </div>
-
-          <EmbeddedSimulator shouldRender={metadata.runnable} metadata={metadata} />
-
         </section>
       </Site>
     );
