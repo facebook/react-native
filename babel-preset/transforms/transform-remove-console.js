7e7f8857cbae3e634c9ad30ddc5b4ac96de93a4b
Emilio Srougo <Emilios1995@gmail.com>
2016-06-22 19:21:37 -0500

    use function.prototype

--- babel-preset/transforms/transform-remove-console.js
+++ babel-preset/transforms/transform-remove-console.js
@@ -1,15 +1,11 @@
 var template = require('babel-template');
-module.exports = function (babel) {
-  var t = babel.types;
+module.exports = function () {
   return {
     visitor: {
       CallExpression(path) {
-        if (path.get("callee").matchesPattern("console", true)) {
-         path.node.callee = template(`
-          (function () {              
-            Array.prototype.slice.call(arguments).forEach(m => (typeof m === 'function') ? m() : m)
-          })
-          `)().expression 
+        const callee = path.get("callee")
+        if (callee.matchesPattern("console", true) && !callee.matchesPattern("console.error")) {
+          path.node.callee = template(`Function.prototype`)().expression
         }
       }
     }
