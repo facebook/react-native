7c31b0cfde2b257e3b017a0771166c83801d6aa1
Christine Abernathy <ca.abernathy@fb.com>
2016-06-23 16:16:32 -0700

    Update MapView doc

--- Libraries/Components/MapView/MapView.js
+++ Libraries/Components/MapView/MapView.js
@@ -27,11 +27,29 @@ const requireNativeComponent = require('requireNativeComponent');
 
 type Event = Object;
 
+/**
+ * State an annotation on the map.
+ */
 export type AnnotationDragState = $Enum<{
+  /**
+   * Annotation is not being touched.
+   */
   idle: string;
+  /**
+   * Annotation dragging has began.
+   */
   starting: string;
+  /**
+   * Annotation is being dragged.
+   */
   dragging: string;
+  /**
+   * Annotation dragging is being canceled.
+   */
   canceling: string;
+  /**
+   * Annotation dragging has ended.
+   */
   ending: string;
 }>;
 
@@ -44,6 +62,19 @@ export type AnnotationDragState = $Enum<{
  * by Leland Richardson.
  *
  * ```
+ * import React, { Component } from 'react';
+ * import { MapView } from 'react-native';
+ *
+ * class MapMyRide extends Component {
+ *   render() {
+ *     return (
+ *       <MapView
+ *         style={{height: 200, margin: 40}}
+ *         showsUserLocation={true}
+ *       />
+ *     );
+ *   }
+ * }
  * ```
  *
  */
@@ -55,8 +86,7 @@ const MapView = React.createClass({
   propTypes: {
     ...View.propTypes,
     /**
-     * Used to style and layout the `MapView`.  See `StyleSheet.js` and
-     * `ViewStylePropTypes.js` for more info.
+     * Used to style and layout the `MapView`.
      */
     style: View.propTypes.style,
 
@@ -64,7 +94,7 @@ const MapView = React.createClass({
      * If `true` the app will ask for the user's location and display it on
      * the map. Default value is `false`.
      *
-     * **NOTE**: on iOS, you need to add the `NSLocationWhenInUseUsageDescription`
+     * **NOTE**: You'll need to add the `NSLocationWhenInUseUsageDescription`
      * key in Info.plist to enable geolocation, otherwise it will fail silently.
      */
     showsUserLocation: React.PropTypes.bool,
@@ -73,21 +103,18 @@ const MapView = React.createClass({
      * If `true` the map will follow the user's location whenever it changes.
      * Note that this has no effect unless `showsUserLocation` is enabled.
      * Default value is `true`.
-     * @platform ios
      */
     followUserLocation: React.PropTypes.bool,
 
     /**
      * If `false` points of interest won't be displayed on the map.
      * Default value is `true`.
-     * @platform ios
      */
     showsPointsOfInterest: React.PropTypes.bool,
 
     /**
-     * If `false` compass won't be displayed on the map.
+     * If `false`, compass won't be displayed on the map.
      * Default value is `true`.
-     * @platform ios
      */
     showsCompass: React.PropTypes.bool,
 
@@ -100,7 +127,9 @@ const MapView = React.createClass({
     /**
      * When this property is set to `true` and a valid camera is associated with
      * the map, the camera’s heading angle is used to rotate the plane of the
-     * map around its center point. When this property is set to `false`, the
+     * map around its center point.
+     *
+     * When this property is set to `false`, the
      * camera’s heading angle is ignored and the map is always oriented so
      * that true north is situated at the top of the map view
      */
@@ -109,7 +138,9 @@ const MapView = React.createClass({
     /**
      * When this property is set to `true` and a valid camera is associated
      * with the map, the camera’s pitch angle is used to tilt the plane
-     * of the map. When this property is set to `false`, the camera’s pitch
+     * of the map.
+     *
+     * When this property is set to `false`, the camera’s pitch
      * angle is ignored and the map is always displayed as if the user
      * is looking straight down onto it.
      */
@@ -124,11 +155,9 @@ const MapView = React.createClass({
     /**
      * The map type to be displayed.
      *
-     * - standard: standard road map (default)
-     * - satellite: satellite view
-     * - hybrid: satellite view with roads and points of interest overlaid
-     *
-     * @platform ios
+     * - `standard`: Standard road map (default).
+     * - `satellite`: Satellite view.
+     * - `hybrid`: Satellite view with roads and points of interest overlaid.
      */
     mapType: React.PropTypes.oneOf([
       'standard',
@@ -158,8 +187,7 @@ const MapView = React.createClass({
     }),
 
     /**
-     * Map annotations with title/subtitle.
-     * @platform ios
+     * Map annotations with title and subtitle.
      */
     annotations: React.PropTypes.arrayOf(React.PropTypes.shape({
       /**
@@ -196,7 +224,7 @@ const MapView = React.createClass({
       onBlur: React.PropTypes.func,
 
       /**
-       * Annotation title/subtile.
+       * Annotation title and subtile.
        */
       title: React.PropTypes.string,
       subtitle: React.PropTypes.string,
@@ -257,7 +285,6 @@ const MapView = React.createClass({
 
     /**
      * Map overlays
-     * @platform ios
      */
     overlays: React.PropTypes.arrayOf(React.PropTypes.shape({
       /**
@@ -282,21 +309,17 @@ const MapView = React.createClass({
     })),
 
     /**
-     * Maximum size of area that can be displayed.
-     * @platform ios
+     * Maximum size of the area that can be displayed.
      */
     maxDelta: React.PropTypes.number,
 
     /**
-     * Minimum size of area that can be displayed.
-     * @platform ios
+     * Minimum size of the area that can be displayed.
      */
     minDelta: React.PropTypes.number,
 
     /**
      * Insets for the map's legal label, originally at bottom left of the map.
-     * See `EdgeInsetsPropType.js` for more information.
-     * @platform ios
      */
     legalLabelInsets: EdgeInsetsPropType,
 
@@ -311,7 +334,7 @@ const MapView = React.createClass({
     onRegionChangeComplete: React.PropTypes.func,
 
     /**
-     * Deprecated. Use annotation onFocus and onBlur instead.
+     * Deprecated. Use annotation `onFocus` and `onBlur` instead.
      */
     onAnnotationPress: React.PropTypes.func,
 
