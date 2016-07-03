d8c54b2e027b67ea05e5f7bafd24befdeaab2293
Konstantin Raev <bestander@fb.com>
2016-07-03 12:36:18 +0100

    turning off new bridge for all

--- ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
@@ -339,7 +339,7 @@ public abstract class ReactInstanceManager {
         mUIImplementationProvider = new UIImplementationProvider();
       }
 
-      if (mUseOldBridge) {
+      if (true) {
         return new ReactInstanceManagerImpl(
             Assertions.assertNotNull(
                 mApplication,
