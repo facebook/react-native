5a9aaca7357d41658d7092ca002a3c97a0783275
Tomasz Kurpios <tomasz.kurpios@gmail.com>
2016-07-04 14:53:13 +0200

    Allow to specify simulator OS version from cli

--- local-cli/runIOS/runIOS.js
+++ local-cli/runIOS/runIOS.js
@@ -65,7 +65,7 @@ function _runIOS(argv, config, resolve, reject) {
     reject(new Error(`Cound't find ${args.simulator} simulator`));
   }
 
-  const simulatorFullName = `${selectedSimulator.name} (${selectedSimulator.version})`;
+  const simulatorFullName = calcSimulatorFullName(selectedSimulator)
   console.log(`Launching ${simulatorFullName}...`);
   try {
     child_process.spawnSync('xcrun', ['instruments', '-w', simulatorFullName]);
@@ -121,10 +121,14 @@ function _runIOS(argv, config, resolve, reject) {
 
 function matchingSimulator(simulators, simulatorName) {
   for (let i = simulators.length - 1; i >= 0; i--) {
-    if (simulators[i].name === simulatorName) {
+    if (simulators[i].name === simulatorName || calcSimulatorFullName(simulators[i]) === simulatorName) {
       return simulators[i];
     }
   }
+}
+
+function calcSimulatorFullName(simulator) {
+  return `${simulator.name} (${simulator.version})`;
 }
 
 module.exports = runIOS;
