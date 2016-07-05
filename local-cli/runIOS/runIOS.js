7c14f214c5b91152e45ae6d60a7ccf3e3bb364ad
Kureev Alexey <kureev-mail@ya.ru>
2016-07-05 11:08:29 +0200

    Fix runIOS

--- local-cli/runIOS/runIOS.js
+++ local-cli/runIOS/runIOS.js
@@ -11,46 +11,14 @@
 const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
-const parseCommandLine = require('../util/parseCommandLine');
 const findXcodeProject = require('./findXcodeProject');
 const parseIOSSimulatorsList = require('./parseIOSSimulatorsList');
-const Promise = require('promise');
 
-/**
- * Starts the app on iOS simulator
- */
-function runIOS(argv, config) {
-  return new Promise((resolve, reject) => {
-    _runIOS(argv, config, resolve, reject);
-  });
-}
-
-function _runIOS(argv, config, resolve, reject) {
-  const args = parseCommandLine([
-    {
-      command: 'simulator',
-      description: 'Explicitly set simulator to use',
-      type: 'string',
-      required: false,
-      default: 'iPhone 6',
-    }, {
-      command: 'scheme',
-      description: 'Explicitly set Xcode scheme to use',
-      type: 'string',
-      required: false,
-    }, {
-      command: 'project-path',
-      description: 'Path relative to project root where the Xcode project (.xcodeproj) lives. The default is \'ios\'.',
-      type: 'string',
-      required: false,
-      default: 'ios',
-    }
-  ], argv);
-
-  process.chdir(args['project-path']);
+function runIOS(argv, config, args) {
+  process.chdir(args.projectPath);
   const xcodeProject = findXcodeProject(fs.readdirSync('.'));
   if (!xcodeProject) {
-    reject(new Error(`Could not find Xcode project files in ios folder`));
+    throw new Error('Could not find Xcode project files in ios folder');
   }
 
   const inferredSchemeName = path.basename(xcodeProject.name, path.extname(xcodeProject.name));
@@ -62,14 +30,14 @@ function _runIOS(argv, config, resolve, reject) {
   );
   const selectedSimulator = matchingSimulator(simulators, args.simulator);
   if (!selectedSimulator) {
-    reject(new Error(`Cound't find ${args.simulator} simulator`));
+    throw new Error(`Cound't find ${args.simulator} simulator`);
   }
 
   const simulatorFullName = `${selectedSimulator.name} (${selectedSimulator.version})`;
   console.log(`Launching ${simulatorFullName}...`);
   try {
     child_process.spawnSync('xcrun', ['instruments', '-w', simulatorFullName]);
-  } catch(e) {
+  } catch (e) {
     // instruments always fail with 255 because it expects more arguments,
     // but we want it to only launch the simulator
   }
@@ -81,42 +49,20 @@ function _runIOS(argv, config, resolve, reject) {
     '-derivedDataPath', 'build',
   ];
   console.log(`Building using "xcodebuild ${xcodebuildArgs.join(' ')}"`);
+  child_process.spawnSync('xcodebuild', xcodebuildArgs, {stdio: 'inherit'});
 
-  let appPath = `build/Build/Products/Debug-iphonesimulator/${inferredSchemeName}.app`;
-  const xcodeBuildProcess = child_process.spawn('xcodebuild', xcodebuildArgs, {
-    stdio: [process.stdin, 'pipe', process.stderr]
-  });
+  const appPath = `build/Build/Products/Debug-iphonesimulator/${inferredSchemeName}.app`;
+  console.log(`Installing ${appPath}`);
+  child_process.spawnSync('xcrun', ['simctl', 'install', 'booted', appPath], {stdio: 'inherit'});
 
-  xcodeBuildProcess.stdout.on('data', (data) => {
-    process.stdout.write(data);
+  const bundleID = child_process.execFileSync(
+    '/usr/libexec/PlistBuddy',
+    ['-c', 'Print:CFBundleIdentifier', path.join(appPath, 'Info.plist')],
+    {encoding: 'utf8'}
+  ).trim();
 
-    // search this part of the process output for a path to the generated app and replace default
-    const appPathFromLog = data.toString().match(/Touch (build\/Build\/Products\/.*\/.*\.app)/);
-    if (appPathFromLog) {
-      appPath = appPathFromLog[1];
-    }
-  });
-
-  xcodeBuildProcess.on('close', (code) => {
-    if (code !== 0) {
-      reject(new Error(`xcodebuild process exited with code ${code}`));
-      return;
-    }
-
-    console.log(`Installing ${appPath}`);
-    child_process.spawnSync('xcrun', ['simctl', 'install', 'booted', appPath], {stdio: 'inherit'});
-
-    const bundleID = child_process.execFileSync(
-        '/usr/libexec/PlistBuddy',
-        ['-c', 'Print:CFBundleIdentifier', path.join(appPath, 'Info.plist')],
-        {encoding: 'utf8'}
-    ).trim();
-
-    console.log(`Launching ${bundleID}`);
-    child_process.spawnSync('xcrun', ['simctl', 'launch', 'booted', bundleID], {stdio: 'inherit'});
-
-    resolve();
-  });
+  console.log(`Launching ${bundleID}`);
+  child_process.spawnSync('xcrun', ['simctl', 'launch', 'booted', bundleID], {stdio: 'inherit'});
 }
 
 function matchingSimulator(simulators, simulatorName) {
