e209f2f1e93b12d64be8197bbe8eba7e28355ded
Mengjue Wang <mengjuew@fb.com>
2016-07-06 16:26:59 -0700

    Move Bridge functions to OSS -- RTL experiment
    Summary:
    Delete the bridge functions(isRTL, allowRTL()) in internal module and move to OSS.
     Create bridge for RCTI18nUtil
    
    Reviewed By: fkgozali
    
    Differential Revision: D3519224
    
    fbshipit-source-id: 3853edcfcc78777d957874448117de72ae0700b5

---
+++ React/Modules/RCTI18nManager.h
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import "RCTBridgeModule.h"
+
+/**
+ * @experimental
+ * This is a experimental module for RTL support
+ * This module bridges the i18n utility from RCTI18nUtil
+ */
+@interface RCTI18nManager : NSObject <RCTBridgeModule>
+
+@end
