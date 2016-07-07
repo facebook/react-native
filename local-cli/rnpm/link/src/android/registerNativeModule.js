a3545f9ef9bf5f1865e8fa6d0400e80896788b1a
Mike Grabowski <grabbou@gmail.com>
2016-07-07 22:20:41 +0200

    Link against MainApplication.java

--- local-cli/rnpm/link/src/android/registerNativeModule.js
+++ local-cli/rnpm/link/src/android/registerNativeModule.js
@@ -27,12 +27,12 @@ module.exports = function registerNativeAndroidModule(
   applyPatch(projectConfig.stringsPath, makeStringsPatch(params, name));
 
   applyPatch(
-    projectConfig.mainActivityPath,
+    projectConfig.mainApplicationPath,
     makePackagePatch(androidConfig.packageInstance, params, name)
   );
 
   applyPatch(
-    projectConfig.mainActivityPath,
+    projectConfig.mainApplicationPath,
     makeImportPatch(androidConfig.packageImportPath)
   );
 };
