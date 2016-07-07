fc2d6c358c2abdc6d43c8e552aa36245b849aa5a
Mike Lambert <mlambert@gmail.com>
2016-07-07 05:50:58 -0700

    Fix timestamps on android touch events to use milliseconds, to be consistent with iOS
    Summary:
    So `PanReponder.onPanResponderRelease/onPanResponderTerminate` receive a `gestureState` object containing a `onPanResponderTerminate.vx/vy` property. On Android and iOS, they appear to be orders of magnitude different, which appear to be due to the different scale of timestamps that are used when generating touch events.
    
    This pull request fixes the timestamps to be milliseconds on both platforms (since I assume iOS is the more authoritative one, and is the one that `react-native-viewpager`'s vx thresholds written written to compare against.)
    
    As far as I can tell, the RN code doesn't use the `vx/vy` properties, so they should be okay. And looks like the RN code only cares about relative values of `startTimestamp/currentTimestamp/previousTimestamp` though, so should be fine too. it's quite possible there will be downstream android breakage with this change, particularly for those who are already compensating for the RN discrepancy.
    Closes https://github.com/facebook/react-native/pull/8199
    
    Differential Revision: D3528215
    
    Pulled By: dmmiller
    
    fbshipit-source-id: cbd25bb7e7bb87fa77b661a057643a6ea97bc3f1

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputManager.java
@@ -567,7 +567,7 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
       mEventDispatcher.dispatchEvent(
           new ReactTextChangedEvent(
               mEditText.getId(),
-              SystemClock.nanoTime(),
+              SystemClock.elapsedRealtime(),
               s.toString(),
               (int) PixelUtil.toDIPFromPixel(contentWidth),
               (int) PixelUtil.toDIPFromPixel(contentHeight),
@@ -576,7 +576,7 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
       mEventDispatcher.dispatchEvent(
           new ReactTextInputEvent(
               mEditText.getId(),
-              SystemClock.nanoTime(),
+              SystemClock.elapsedRealtime(),
               newText,
               oldText,
               start,
@@ -602,17 +602,17 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
               eventDispatcher.dispatchEvent(
                   new ReactTextInputFocusEvent(
                       editText.getId(),
-                      SystemClock.nanoTime()));
+                      SystemClock.elapsedRealtime()));
             } else {
               eventDispatcher.dispatchEvent(
                   new ReactTextInputBlurEvent(
                       editText.getId(),
-                      SystemClock.nanoTime()));
+                      SystemClock.elapsedRealtime()));
 
               eventDispatcher.dispatchEvent(
                   new ReactTextInputEndEditingEvent(
                       editText.getId(),
-                      SystemClock.nanoTime(),
+                      SystemClock.elapsedRealtime(),
                       editText.getText().toString()));
             }
           }
@@ -630,7 +630,7 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
               eventDispatcher.dispatchEvent(
                   new ReactTextInputSubmitEditingEvent(
                       editText.getId(),
-                      SystemClock.nanoTime(),
+                      SystemClock.elapsedRealtime(),
                       editText.getText().toString()));
             }
             if (actionId == EditorInfo.IME_ACTION_NEXT ||
@@ -667,7 +667,7 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
         mEventDispatcher.dispatchEvent(
             new ReactTextInputSelectionEvent(
                 mReactEditText.getId(),
-                SystemClock.nanoTime(),
+                SystemClock.elapsedRealtime(),
                 start,
                 end
             )
