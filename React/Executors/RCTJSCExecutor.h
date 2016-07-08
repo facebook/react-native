4612d398a945a3d0fa6a2463cf9f6158451aecbc
Adam Ernst <adamjernst@fb.com>
2016-07-08 12:19:27 -0700

    Add the ability to pre-create the JavaScript thread with RCTJSCExecutor
    Summary: This can be used to create a JavaScript thread and `JSContext` in advance, then supply them to the `RCTJSCExecutor` at creation time later.
    
    Reviewed By: javache
    
    Differential Revision: D3534553
    
    fbshipit-source-id: 99ccf711928cd12e84c9fbe142c6d19a7af55d07

--- React/Executors/RCTJSCExecutor.h
+++ React/Executors/RCTJSCExecutor.h
@@ -26,6 +26,17 @@ RCT_EXTERN NSString *const RCTJSCThreadName;
 RCT_EXTERN NSString *const RCTJavaScriptContextCreatedNotification;
 
 /**
+ * @experimental
+ * May be used to pre-create the JSContext to make RCTJSCExecutor creation less costly.
+ * Avoid using this; it's experimental and is not likely to be supported long-term.
+ */
+@interface RCTJSContextProvider : NSObject
+
+- (instancetype)initWithUseCustomJSCLibrary:(BOOL)useCustomJSCLibrary;
+
+@end
+
+/**
  * Uses a JavaScriptCore context as the execution engine.
  */
 @interface RCTJSCExecutor : NSObject <RCTJavaScriptExecutor>
@@ -42,6 +53,11 @@ RCT_EXTERN NSString *const RCTJavaScriptContextCreatedNotification;
  * to initialize RCTJSCWrapper.
  */
 - (instancetype)initWithUseCustomJSCLibrary:(BOOL)useCustomJSCLibrary;
+
+/**
+ * Pass a RCTJSContextProvider object to use an NSThread/JSContext pair that have already been created.
+ */
+- (instancetype)initWithJSContextProvider:(RCTJSContextProvider *)JSContextProvider;
 
 /**
  * Create a NSError from a JSError object.
