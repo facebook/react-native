a9f28342552aadeeac982a103e40a0d3b5394179
Adam Ernst <adamjernst@fb.com>
2016-07-12 05:13:32 -0700

    Expose a method to synchronously load bundle if possible
    Summary:
    This diff exposes a new experimental method `[RCTJavaScriptLoader +attemptSynchronousLoadOfBundleAtURL:sourceLength:error:]`. It may be used if you know that a specific call site must load its JavaScript synchronously, or else fail entirely.
    
    This new API will succeed for file URLs that point to a RAM bundle. It will fail for non-RAM-bundle files and for HTTP URLs.
    
    This also cleans up the error domain and codes for this class. This should be the only externally visible change from this diff if you don't use the new API: the codes and domains you receive from the API may change slightly. They were pretty sloppy and undocumented before, so I think this change is for the better.
    
    Reviewed By: bnham
    
    Differential Revision: D3545956
    
    fbshipit-source-id: 30e65f4e8330d2d68f3f50ade077fdc1db4a435e

--- React/Base/RCTJavaScriptLoader.h
+++ React/Base/RCTJavaScriptLoader.h
@@ -13,6 +13,18 @@
 
 extern uint32_t const RCTRAMBundleMagicNumber;
 
+extern NSString *const RCTJavaScriptLoaderErrorDomain;
+
+NS_ENUM(NSInteger) {
+  RCTJavaScriptLoaderErrorNoScriptURL = 1,
+  RCTJavaScriptLoaderErrorFailedOpeningFile = 2,
+  RCTJavaScriptLoaderErrorFailedReadingFile = 3,
+  RCTJavaScriptLoaderErrorFailedStatingFile = 3,
+  RCTJavaScriptLoaderErrorURLLoadFailed = 3,
+
+  RCTJavaScriptLoaderErrorCannotBeLoadedSynchronously = 1000,
+};
+
 @class RCTBridge;
 
 /**
@@ -22,6 +34,19 @@ extern uint32_t const RCTRAMBundleMagicNumber;
  */
 @interface RCTJavaScriptLoader : NSObject
 
-+ (void)loadBundleAtURL:(NSURL *)moduleURL onComplete:(RCTSourceLoadBlock)onComplete;
++ (void)loadBundleAtURL:(NSURL *)scriptURL onComplete:(RCTSourceLoadBlock)onComplete;
+
+/**
+ * @experimental
+ * Attempts to synchronously load the script at the given URL. The following two conditions must be met:
+ * 1. It must be a file URL.
+ * 2. It must point to a RAM bundle, or allowLoadingNonRAMBundles must be YES.
+ * If the URL does not meet those conditions, this method will return nil and supply an error with the domain
+ * RCTJavaScriptLoaderErrorDomain and the code RCTJavaScriptLoaderErrorCannotBeLoadedSynchronously.
+ */
++ (NSData *)attemptSynchronousLoadOfBundleAtURL:(NSURL *)scriptURL
+                                   sourceLength:(int64_t *)sourceLength
+                      allowLoadingNonRAMBundles:(BOOL)allowLoadingNonRAMBundles
+                                          error:(NSError **)error;
 
 @end
