80872ffccd51fefaad897e8920047d3a428335f8
Janic Duplessis <janicduplessis@gmail.com>
2016-07-15 13:57:19 -0700

    Remove exported constants from RCTTimers to allow lazy initialization
    Summary:
    As per javache comments in #8734.
    
    Also removes now useless feature detection check.
    
    **Test plan**
    Tested that rIC still works in UIExplorer example.
    Closes https://github.com/facebook/react-native/pull/8795
    
    Differential Revision: D3572566
    
    Pulled By: javache
    
    fbshipit-source-id: 261d13d8b03898313f8b4184d634c70f81a61b62

--- React/Modules/RCTTiming.m
+++ React/Modules/RCTTiming.m
@@ -16,6 +16,8 @@
 #import "RCTUtils.h"
 
 static const NSTimeInterval kMinimumSleepInterval = 1;
+
+// These timing contants should be kept in sync with the ones in `JSTimersExecution.js`.
 // The duration of a frame. This assumes that we want to run at 60 fps.
 static const NSTimeInterval kFrameDuration = 1.0 / 60.0;
 // The minimum time left in a frame to trigger the idle callback.
@@ -136,14 +138,6 @@ RCT_EXPORT_MODULE()
 - (dispatch_queue_t)methodQueue
 {
   return RCTJSThread;
-}
-
-- (NSDictionary *)constantsToExport
-{
-  return @{
-    @"frameDuration": @(kFrameDuration * 1000),
-    @"idleCallbackFrameDeadline": @(kIdleCallbackFrameDeadline * 1000),
-  };
 }
 
 - (void)invalidate
