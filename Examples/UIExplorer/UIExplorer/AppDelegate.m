87124383ac6c62b4ac8f46a1d31ba83bd3bd0c5d
Nathan Azaria <nazaria@fb.com>
2016-07-15 10:11:07 -0700

    Changed JS location of examples to source from RCTBundleURLProvider instead.
    Summary:
    This changes the JS location of the examples (2048, UIExplorer, Movies, and TicTacToe) to be set from RCTBundleURLProvider instead.
    This also makes the example apps run the bundle script, which makes the build time longer.
    
    Reviewed By: javache
    
    Differential Revision: D3516371
    
    fbshipit-source-id: 70e53c62feb81c067df4e2298e7d1f1458777490

--- Examples/UIExplorer/UIExplorer/AppDelegate.m
+++ Examples/UIExplorer/UIExplorer/AppDelegate.m
@@ -15,6 +15,7 @@
 #import "AppDelegate.h"
 
 #import "RCTBridge.h"
+#import "RCTBundleURLProvider.h"
 #import "RCTJavaScriptLoader.h"
 #import "RCTLinkingManager.h"
 #import "RCTRootView.h"
@@ -51,41 +52,13 @@
 
 - (NSURL *)sourceURLForBridge:(__unused RCTBridge *)bridge
 {
-    NSURL *sourceURL;
-
-    /**
-     * Loading JavaScript code - uncomment the one you want.
-     *
-     * OPTION 1
-     * Load from development server. Start the server from the repository root:
-     *
-     * $ npm start
-     *
-     * To run on device, change `localhost` to the IP address of your computer
-     * (you can get this by typing `ifconfig` into the terminal and selecting the
-     * `inet` value under `en0:`) and make sure your computer and iOS device are
-     * on the same Wi-Fi network.
-     */
-
-    sourceURL = [NSURL URLWithString:@"http://localhost:8081/Examples/UIExplorer/js/UIExplorerApp.ios.bundle?platform=ios&dev=true"];
-
-    /**
-     * OPTION 2
-     * Load from pre-bundled file on disk. To re-generate the static bundle, `cd`
-     * to your Xcode project folder and run
-     *
-     * $ curl 'http://localhost:8081/Examples/UIExplorer/js/UIExplorerApp.ios.bundle?platform=ios' -o main.jsbundle
-     *
-     * then add the `main.jsbundle` file to your project and uncomment this line:
-     */
-
-  //  sourceURL = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"Examples/UIExplorer/UIExplorerApp.ios" fallbackResource:nil];
 
   if (!getenv("CI_USE_PACKAGER")) {
-     sourceURL = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
   }
 
-  return sourceURL;
+  return jsCodeLocation;
 }
 
 
