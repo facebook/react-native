02a294f832c87a74035f125f054f69addbc4f95b
Ariel Elkin <arielelkin@gmail.com>
2016-07-15 16:12:11 +0200

    update AsyncStorage.js documentation
    Explicitly mention that storage is unencrypted.

--- Libraries/Storage/AsyncStorage.js
+++ Libraries/Storage/AsyncStorage.js
@@ -26,6 +26,7 @@ var RCTAsyncStorage = RCTAsyncRocksDBStorage || RCTAsyncSQLiteStorage || RCTAsyn
  * @description
  * `AsyncStorage` is a simple, asynchronous, persistent, key-value storage
  * system that is global to the app.  It should be used instead of LocalStorage.
+ * On iOS, storage is unencrypted. 
  *
  * It is recommended that you use an abstraction on top of `AsyncStorage`
  * instead of `AsyncStorage` directly for anything more than light usage since
