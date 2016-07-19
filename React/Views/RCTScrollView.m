3dd170a0be32f094237c6e05dc024599033d43b7
Dima <muakacho+github@gmail.com>
2016-07-19 14:59:21 +0300

    Fix #5432: prevent jumping on refreshing

--- React/Views/RCTScrollView.m
+++ React/Views/RCTScrollView.m
@@ -268,6 +268,17 @@ RCT_NOT_IMPLEMENTED(- (instancetype)init)
   }
   super.contentOffset = contentOffset;
 }
+// Workaround for https://github.com/facebook/react-native/issues/5432
+- (void)setContentInset:(UIEdgeInsets)contentInset 
+{
+  if (self.tracking) {
+    CGFloat diff = contentInset.top - self.contentInset.top;
+    CGPoint translation = [self.panGestureRecognizer translationInView:self];
+    translation.y -= diff * 3.0 / 2.0;
+    [self.panGestureRecognizer setTranslation:translation inView:self];
+  }
+  [super setContentInset:contentInset];
+}
 
 - (void)dockClosestSectionHeader
 {
