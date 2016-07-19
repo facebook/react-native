b4b7cf8d30c257fe068bbb6b08664fea48c52a31
Maarten Schumacher <maarten.schumacher@drivetribe.com>
2016-07-19 17:47:11 +0100

    Modal shouldn't be restricted to portrait only

--- React/Views/RCTModalHostViewController.m
+++ React/Views/RCTModalHostViewController.m
@@ -24,14 +24,4 @@
   }
 }
 
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations
-{
-  // Picking some defaults here, we should probably make this configurable
-  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-    return UIInterfaceOrientationMaskAll;
-  } else {
-    return UIInterfaceOrientationMaskPortrait;
-  }
-}
-
 @end
