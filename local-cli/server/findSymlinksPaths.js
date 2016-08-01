294d79a9ca304573341db546046b7b70ce5d8130
Kureev Alexey <kureev-mail@ya.ru>
2016-08-01 12:12:32 +0200

    Measure time for the whole process

--- local-cli/server/findSymlinksPaths.js
+++ local-cli/server/findSymlinksPaths.js
@@ -4,10 +4,12 @@ const fs = require('fs');
 module.exports = function findSymlinksPaths(lookupFolder) {
   const timeStart = Date.now();
   const folders = fs.readdirSync(lookupFolder);
-  const timeEnd = Date.now();
-  console.log(`Scanning ${folders.length} folders in ${lookupFolder} (${timeEnd - timeStart}ms)`);
-
-  return folders.map(folder => path.resolve(lookupFolder, folder))
+  const resolvedSymlinks = folders.map(folder => path.resolve(lookupFolder, folder))
     .filter(folderPath => fs.lstatSync(folderPath).isSymbolicLink())
     .map(symlink => path.resolve(process.cwd(), fs.readlinkSync(symlink)));
+  const timeEnd = Date.now();
+
+  console.log(`Scanning ${folders.length} folders in ${lookupFolder} (${timeEnd - timeStart}ms)`);
+
+  return resolvedSymlinks;
 };
