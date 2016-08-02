c913a330d585a61d853b91edc85c7e5b2dfe975c
Kureev Alexey <kureev-mail@ya.ru>
2016-08-02 23:01:31 +0200

    Make packager work with run-android

--- local-cli/server/server.js
+++ local-cli/server/server.js
@@ -13,6 +13,7 @@ const formatBanner = require('./formatBanner');
 const path = require('path');
 const runServer = require('./runServer');
 const findSymlinksPaths = require('./findSymlinksPaths');
+const NODE_MODULES = path.resolve(__dirname, '..', '..', '..');
 
 /**
  * Starts the React Native Packager Server.
@@ -20,7 +21,7 @@ const findSymlinksPaths = require('./findSymlinksPaths');
 function server(argv, config, args) {
   args.projectRoots = args.projectRoots.concat(
     args.root,
-    findSymlinkPaths(path.resolve(process.cwd(), 'node_modules'))
+    findSymlinksPaths(NODE_MODULES)
   );
 
   console.log(formatBanner(
