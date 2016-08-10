d7e74a2d2c037a34be54cd433464b27cc349d3c1
naqvitalha <naqvitalha@gmail.com>
2016-08-11 00:35:30 +0530

    bumping recycler view version to 23.2.1 for Android, definition of ItemAnimator interface has changed which causes a crash if integrated with an app using recycler view 23.2.1+. The interface definition hasn't changed since that version. This is critical for making integration with existing apps seamless since most apps would want to use latest version of recycler view.

--- ReactAndroid/src/main/java/com/facebook/react/views/recyclerview/NotAnimatedItemAnimator.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/recyclerview/NotAnimatedItemAnimator.java
@@ -2,6 +2,8 @@
 
 package com.facebook.react.views.recyclerview;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 
 /**
@@ -10,49 +12,38 @@ import android.support.v7.widget.RecyclerView;
 /*package*/ class NotAnimatedItemAnimator extends RecyclerView.ItemAnimator {
 
   @Override
+  public boolean animateDisappearance(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @Nullable ItemHolderInfo postLayoutInfo) {
+    dispatchAnimationStarted(viewHolder);
+    dispatchAnimationFinished(viewHolder);
+    return true;
+  }
+
+  @Override
+  public boolean animateAppearance(@NonNull RecyclerView.ViewHolder viewHolder, @Nullable ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+    dispatchAnimationStarted(viewHolder);
+    dispatchAnimationFinished(viewHolder);
+    return true;
+  }
+
+  @Override
+  public boolean animatePersistence(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+    dispatchAnimationStarted(viewHolder);
+    dispatchAnimationFinished(viewHolder);
+    return true;
+  }
+
+  @Override
+  public boolean animateChange(@NonNull RecyclerView.ViewHolder oldHolder, @NonNull RecyclerView.ViewHolder newHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+    dispatchAnimationStarted(oldHolder);
+    dispatchAnimationFinished(oldHolder);
+    dispatchAnimationStarted(newHolder);
+    dispatchAnimationFinished(newHolder);
+    return true;
+  }
+
+  @Override
   public void runPendingAnimations() {
     // nothing
-  }
-
-  @Override
-  public boolean animateRemove(RecyclerView.ViewHolder holder) {
-    dispatchRemoveStarting(holder);
-    dispatchRemoveFinished(holder);
-    return true;
-  }
-
-  @Override
-  public boolean animateAdd(RecyclerView.ViewHolder holder) {
-    dispatchAddStarting(holder);
-    dispatchAddFinished(holder);
-    return true;
-  }
-
-  @Override
-  public boolean animateMove(
-      RecyclerView.ViewHolder holder,
-      int fromX,
-      int fromY,
-      int toX,
-      int toY) {
-    dispatchMoveStarting(holder);
-    dispatchMoveFinished(holder);
-    return true;
-  }
-
-  @Override
-  public boolean animateChange(
-      RecyclerView.ViewHolder oldHolder,
-      RecyclerView.ViewHolder newHolder,
-      int fromLeft,
-      int fromTop,
-      int toLeft,
-      int toTop) {
-    dispatchChangeStarting(oldHolder, true);
-    dispatchChangeFinished(oldHolder, true);
-    dispatchChangeStarting(newHolder, false);
-    dispatchChangeFinished(newHolder, false);
-    return true;
   }
 
   @Override
