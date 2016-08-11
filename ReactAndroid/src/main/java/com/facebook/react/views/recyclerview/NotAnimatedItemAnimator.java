7aa0d7bb0996277761077682798d8b901026fad3
naqvitalha <naqvitalha@gmail.com>
2016-08-11 07:55:52 +0530

    fixed styling as per review comment, also fixed issue causing CI to fail.

--- ReactAndroid/src/main/java/com/facebook/react/views/recyclerview/NotAnimatedItemAnimator.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/recyclerview/NotAnimatedItemAnimator.java
@@ -2,8 +2,6 @@
 
 package com.facebook.react.views.recyclerview;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 
 /**
@@ -12,28 +10,41 @@ import android.support.v7.widget.RecyclerView;
 /*package*/ class NotAnimatedItemAnimator extends RecyclerView.ItemAnimator {
 
   @Override
-  public boolean animateDisappearance(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @Nullable ItemHolderInfo postLayoutInfo) {
+  public boolean animateDisappearance(
+      RecyclerView.ViewHolder viewHolder,
+      ItemHolderInfo preLayoutInfo,
+      ItemHolderInfo postLayoutInfo) {
     dispatchAnimationStarted(viewHolder);
     dispatchAnimationFinished(viewHolder);
     return true;
   }
 
   @Override
-  public boolean animateAppearance(@NonNull RecyclerView.ViewHolder viewHolder, @Nullable ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+  public boolean animateAppearance(
+      RecyclerView.ViewHolder viewHolder,
+      ItemHolderInfo preLayoutInfo,
+      ItemHolderInfo postLayoutInfo) {
     dispatchAnimationStarted(viewHolder);
     dispatchAnimationFinished(viewHolder);
     return true;
   }
 
   @Override
-  public boolean animatePersistence(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+  public boolean animatePersistence(
+      RecyclerView.ViewHolder viewHolder,
+      ItemHolderInfo preLayoutInfo,
+      ItemHolderInfo postLayoutInfo) {
     dispatchAnimationStarted(viewHolder);
     dispatchAnimationFinished(viewHolder);
     return true;
   }
 
   @Override
-  public boolean animateChange(@NonNull RecyclerView.ViewHolder oldHolder, @NonNull RecyclerView.ViewHolder newHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+  public boolean animateChange(
+      RecyclerView.ViewHolder oldHolder,
+      RecyclerView.ViewHolder newHolder,
+      ItemHolderInfo preLayoutInfo,
+      ItemHolderInfo postLayoutInfo) {
     dispatchAnimationStarted(oldHolder);
     dispatchAnimationFinished(oldHolder);
     dispatchAnimationStarted(newHolder);
@@ -43,7 +54,6 @@ import android.support.v7.widget.RecyclerView;
 
   @Override
   public void runPendingAnimations() {
-    // nothing
   }
 
   @Override
