ac8de8124ff8a51bcdd67bd296fb383893ea26c7
Gant Laborde <gantman@gmail.com>
2016-08-13 15:30:45 -0500

    Add ability to detect if location was mocked
    Since API 18, Android locations have had the `isFromMockProvider()` function, to verify the validity of a provided location.  This was one of many methods one could verify location data, but as of Marshmallow, the other ways of detecting if "Mock Locations" is on in developer settings has been deprecated or defunct.
    
    This means some devices can only detect location mocking by exposing the method on the location object.
    
    This change provides that exposure.

--- ReactAndroid/src/main/java/com/facebook/react/modules/location/LocationModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/location/LocationModule.java
@@ -204,6 +204,9 @@ public class LocationModule extends ReactContextBaseJavaModule {
     coords.putDouble("speed", location.getSpeed());
     map.putMap("coords", coords);
     map.putDouble("timestamp", location.getTime());
+    if (android.os.Build.VERSION.SDK_INT >= 18) {
+      map.putBoolean("mocked", location.isFromMockProvider());
+    }    
     return map;
   }
 
