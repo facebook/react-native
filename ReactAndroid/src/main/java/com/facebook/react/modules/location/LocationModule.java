7a2eabeda6eb8bdfebd57f58917760d0f15cf8c2
Gant <GantMan@gmail.com>
2016-08-20 16:24:59 -0500

    adding whitespace as requested

--- ReactAndroid/src/main/java/com/facebook/react/modules/location/LocationModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/location/LocationModule.java
@@ -204,9 +204,11 @@ public class LocationModule extends ReactContextBaseJavaModule {
     coords.putDouble("speed", location.getSpeed());
     map.putMap("coords", coords);
     map.putDouble("timestamp", location.getTime());
+
     if (android.os.Build.VERSION.SDK_INT >= 18) {
       map.putBoolean("mocked", location.isFromMockProvider());
-    }    
+    }
+
     return map;
   }
 
