777a9c0a0e396924283bb7b9675937c4bb4a9368
Héctor Ramos <hector.ramos@gmail.com>
2016-08-23 11:59:26 -0700

    Update Blog Layout
    Summary:
    Updated the blog's styling to make it more readable.
    
    * BlogPageLayout (blog index) - Only excerpts from each article are now shown, as opposed to the entire article.
    * BlogPost - Broken up into headers, footers. Reorder header so that the blog post title is closer to the content. Adds support for hero images (visible from the blog index). Adds Facebook, Twitter social share buttons. List items are properly spaced now.
    
    Blog index:
    ![screencapture-localhost-8079-react-native-blog-1471905976431](https://cloud.githubusercontent.com/assets/165856/17874405/4ee4fc22-6880-11e6-8344-2ed823f6000e.png)
    
    Single blog post:
    ![screencapture-localhost-8079-react-native-blog-2016-08-12-react-native-meetup-san-francisco-html-1471905997923](https://cloud.githubusercontent.com/assets/165856/17874407/52af9e7a-6880-11e6-99f0-91f90331aced.png)
    Closes https://github.com/facebook/react-native/pull/9532
    
    Differential Revision: D3758524
    
    Pulled By: bestander
    
    fbshipit-source-id: 6385a3e98a3a44343c3b1d3105a32023b748c2c6

--- website/core/Site.js
+++ website/core/Site.js
@@ -46,7 +46,8 @@ var Site = React.createClass({
           <script type="text/javascript">{'try{Typekit.load();}catch(e){}'}</script>
         </head>
         <body>
-
+          <script dangerouslySetInnerHTML={{__html: `window.fbAsyncInit = function() {FB.init({appId:'1677033832619985',xfbml:true,version:'v2.7'});};(function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s); js.id = id;js.src = '//connect.facebook.net/en_US/sdk.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script','facebook-jssdk'));`}} />
+          <script dangerouslySetInnerHTML={{__html: `window.twttr=(function(d,s, id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return t;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js, fjs);t._e = [];t.ready = function(f) {t._e.push(f);};return t;}(document, "script", "twitter-wjs"));`}} />
           <div className="container">
             <div className="nav-main">
               <div className="wrap">
