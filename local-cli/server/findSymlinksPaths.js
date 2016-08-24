1386f1e37891e0725c551e87a8b4499adf35d228
Eric Kryski <e.kryski@gmail.com>
2016-08-24 14:58:17 -0600

    use lookup folder instead of cwd for symlink path resolution. Refs #9009

--- local-cli/server/findSymlinksPaths.js
+++ local-cli/server/findSymlinksPaths.js
@@ -6,7 +6,7 @@ module.exports = function findSymlinksPaths(lookupFolder) {
   const folders = fs.readdirSync(lookupFolder);
   const resolvedSymlinks = folders.map(folder => path.resolve(lookupFolder, folder))
     .filter(folderPath => fs.lstatSync(folderPath).isSymbolicLink())
-    .map(symlink => path.resolve(process.cwd(), fs.readlinkSync(symlink)));
+    .map(symlink => path.resolve(lookupFolder, fs.readlinkSync(symlink)));
   const timeEnd = Date.now();
 
   console.log(`Scanning ${folders.length} folders for symlinks in ${lookupFolder} (${timeEnd - timeStart}ms)`);
