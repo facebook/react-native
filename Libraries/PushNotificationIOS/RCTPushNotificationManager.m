c59a013f7a8a879761789f3f5d0d005b42733ad6
Jonathan Stanton <jonathan@jastanton.com>
2016-08-29 15:34:44 -0700

    add this back in

--- Libraries/PushNotificationIOS/RCTPushNotificationManager.m
+++ Libraries/PushNotificationIOS/RCTPushNotificationManager.m
@@ -32,6 +32,7 @@ NSString *const RCTRegisterUserNotificationSettings = @"RegisterUserNotification
 NSString *const RCTErrorUnableToRequestPermissions = @"E_UNABLE_TO_REQUEST_PERMISSIONS";
 
 @interface RCTPushNotificationManager ()
+@property (nonatomic, copy) RCTPromiseResolveBlock requestPermissionsResolveBlock;
 @property (nonatomic, strong) NSMutableDictionary *remoteNotificationCallbacks;
 @end
 
