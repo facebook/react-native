9525ad191f1941934dd3cec574702c30a35e74d9
Brian Cooley-Gilliom <brian@calvium.com>
2016-09-02 14:58:30 +0100

    Set allowsBackgroundLocationUpdates as necesarry when using location as a background mode.

--- Libraries/Geolocation/RCTLocationObserver.m
+++ Libraries/Geolocation/RCTLocationObserver.m
@@ -144,6 +144,14 @@ RCT_EXPORT_MODULE()
   if ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSLocationAlwaysUsageDescription"] &&
     [_locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
     [_locationManager requestAlwaysAuthorization];
+
+    // On iOS 9+ we also need to enable background updates
+    NSArray* backgroundModes  = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"UIBackgroundModes"];
+    if(backgroundModes && [backgroundModes containsObject:@"location"]) {
+      if([_locationManager respondsToSelector:@selector(setAllowsBackgroundLocationUpdates:)]) {
+        [_locationManager setAllowsBackgroundLocationUpdates:YES];
+      }
+    }
   } else if ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSLocationWhenInUseUsageDescription"] &&
     [_locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
     [_locationManager requestWhenInUseAuthorization];
