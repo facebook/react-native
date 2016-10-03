41eaf4c1b23857d08374d0fbd55628282562c6ca
benjaminhughes <ben@thoughtmachine.net>
2016-10-03 10:32:44 +0100

    adding check for NODE_PATH env variable and adding to search queue if exists

--- packager/react-packager/src/node-haste/DependencyGraph/ResolutionRequest.js
+++ packager/react-packager/src/node-haste/DependencyGraph/ResolutionRequest.js
@@ -375,6 +375,10 @@ class ResolutionRequest {
             }
           }
 
+          if (process.env.NODE_PATH) {
+            searchQueue.push(path.join(process.env.NODE_PATH, realModuleName));
+          }
+
           let p = Promise.reject(new UnableToResolveError(fromModule, toModuleName));
           searchQueue.forEach(potentialModulePath => {
             p = this._tryResolve(
