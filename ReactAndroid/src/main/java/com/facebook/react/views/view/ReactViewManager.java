6d175f2c257fe1acb1c36b15bc97bdb77560885c
Felix Oghina <foghina@fb.com>
2016-10-03 04:27:16 -0700

    Android: enable foreground ripple
    Reviewed By: astreet
    
    Differential Revision: D3932066
    
    fbshipit-source-id: ee2f019cb9ba41e32cbbd8c1cd07c9ed5263fddc

--- ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewManager.java
@@ -14,6 +14,7 @@ import javax.annotation.Nullable;
 import java.util.Locale;
 import java.util.Map;
 
+import android.annotation.TargetApi;
 import android.graphics.Rect;
 import android.os.Build;
 import android.view.View;
@@ -99,6 +100,14 @@ public class ReactViewManager extends ViewGroupManager<ReactViewGroup> {
   public void setNativeBackground(ReactViewGroup view, @Nullable ReadableMap bg) {
     view.setTranslucentBackgroundDrawable(bg == null ?
             null : ReactDrawableHelper.createDrawableFromJSDescription(view.getContext(), bg));
+  }
+
+  @TargetApi(Build.VERSION_CODES.M)
+  @ReactProp(name = "nativeForegroundAndroid")
+  public void setNativeForeground(ReactViewGroup view, @Nullable ReadableMap fg) {
+    view.setForeground(fg == null
+        ? null
+        : ReactDrawableHelper.createDrawableFromJSDescription(view.getContext(), fg));
   }
 
   @ReactProp(name = com.facebook.react.uimanager.ReactClippingViewGroupHelper.PROP_REMOVE_CLIPPED_SUBVIEWS)
