ee363f5e16ee1cc3dcc3308c62c615c373606530
Leo Natan <leo.natan@outlook.com>
2016-10-28 14:32:57 +0300

    Changed test to check carry longer than one byte. Copyright notice and code styling as requested.

--- Examples/UIExplorer/UIExplorerUnitTests/RCTUnicodeDecodeTests.m
+++ Examples/UIExplorer/UIExplorerUnitTests/RCTUnicodeDecodeTests.m
@@ -1,10 +1,16 @@
-//
-//  RCTUnicodeDecodeTests.m
-//  UIExplorer
-//
-//  Created by Leo Natan (Wix) on 27/10/2016.
-//  Copyright © 2016 Facebook. All rights reserved.
-//
+/**
+ * The examples provided by Facebook are for non-commercial testing and
+ * evaluation purposes only.
+ *
+ * Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 @import XCTest;
 @import ObjectiveC;
@@ -13,7 +19,7 @@
 
 @interface RCTNetworking ()
 
-+ (NSString *)decodeTextData:(NSData *)data fromResponse:(NSURLResponse *)response withCarryStorage:(NSMutableData*)carryStorage;
++ (NSString *)decodeTextData:(NSData *)data fromResponse:(NSURLResponse *)response withCarryData:(NSMutableData*)inputCarryData;
 
 @end
 
@@ -23,24 +29,31 @@
 
 - (void)testUnicodeDecode
 {
-	NSString* unicodeString = @"שששששששששששששששששששששששששששששששששששששששש";
-	NSData* unicodeBytes = [unicodeString dataUsingEncoding:NSUTF8StringEncoding];
-	
-	NSURLResponse* fakeResponse = [[NSHTTPURLResponse alloc] initWithURL:[NSURL URLWithString:@"testurl://"] statusCode:200 HTTPVersion:@"1.1" headerFields:@{@"content-type": @"text/plain; charset=utf-8"}];
-	XCTAssert([fakeResponse.textEncodingName isEqualToString:@"utf-8"]);
-	
-	NSMutableData* carryStorage = [NSMutableData new];
-	NSMutableString* parsedString = [NSMutableString new];
-	
-	[parsedString appendString:[RCTNetworking decodeTextData:[unicodeBytes subdataWithRange:NSMakeRange(0, 11)] fromResponse:fakeResponse withCarryStorage:carryStorage]];
-	
-	XCTAssert(carryStorage.length == 1);
-	
-	[parsedString appendString:[RCTNetworking decodeTextData:[unicodeBytes subdataWithRange:NSMakeRange(11, unicodeBytes.length - 11)] fromResponse:fakeResponse withCarryStorage:carryStorage]];
-	
-	XCTAssert(carryStorage.length == 0);
-	
-	XCTAssert([parsedString isEqualToString:unicodeString]);
+  NSString* unicodeString = @"😀😀";
+  NSData* unicodeBytes = [unicodeString dataUsingEncoding:NSUTF8StringEncoding];
+  
+  NSURLResponse* fakeResponse = [[NSHTTPURLResponse alloc] initWithURL:[NSURL URLWithString:@"testurl://"]
+                                                            statusCode:200
+                                                           HTTPVersion:@"1.1"
+                                                          headerFields:@{@"content-type": @"text/plain; charset=utf-8"}];
+  XCTAssert([fakeResponse.textEncodingName isEqualToString:@"utf-8"]);
+  
+  NSMutableData* carryStorage = [NSMutableData new];
+  NSMutableString* parsedString = [NSMutableString new];
+  
+  [parsedString appendString:[RCTNetworking decodeTextData:[unicodeBytes subdataWithRange:NSMakeRange(0, 7)]
+                                              fromResponse:fakeResponse
+                                             withCarryData:carryStorage]];
+  
+  XCTAssert(carryStorage.length == 3);
+  
+  [parsedString appendString:[RCTNetworking decodeTextData:[unicodeBytes subdataWithRange:NSMakeRange(7, unicodeBytes.length - 7)]
+                                              fromResponse:fakeResponse
+                                             withCarryData:carryStorage]];
+  
+  XCTAssert(carryStorage.length == 0);
+  
+  XCTAssert([parsedString isEqualToString:unicodeString]);
 }
 
 @end
