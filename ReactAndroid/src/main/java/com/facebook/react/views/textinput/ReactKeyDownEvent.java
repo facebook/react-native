e4c829bfa2c8addcbdb3833f73cad69bb4521064
petergarnaes <petergarnaes@gmail.com>
2016-10-31 21:48:12 +0100

    [TextInput] Implements 'onKeyPress' for Android

---
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactKeyDownEvent.java
@@ -0,0 +1,48 @@
+package com.facebook.react.views.textinput;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.Event;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+public class ReactKeyDownEvent extends Event<ReactKeyDownEvent> {
+
+    private static final String EVENT_NAME = "topKeyDown";
+
+    private String mKey;
+
+    public ReactKeyDownEvent(
+            int viewId,
+            String key) {
+        super(viewId);
+        mKey = key;
+    }
+
+    @Override
+    public String getEventName() {
+        return EVENT_NAME;
+    }
+
+    @Override
+    public void dispatch(RCTEventEmitter rctEventEmitter) {
+        rctEventEmitter.receiveEvent(getViewTag(), getEventName(), serializeEventData());
+    }
+
+    private WritableMap serializeEventData() {
+        WritableMap eventData = Arguments.createMap();
+
+        //WritableMap selectionData = Arguments.createMap();
+        eventData.putString("key", mKey);
+
+        //eventData.putMap("selection", selectionData);
+        return eventData;
+    }
+}
