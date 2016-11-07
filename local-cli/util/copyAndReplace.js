5f9b72747238f205ecb445691485ecbc80b29385
Martin Konicek <mkonicek@fb.com>
2016-11-07 13:04:45 -0800

    Keep binary files intact

--- local-cli/util/copyAndReplace.js
+++ local-cli/util/copyAndReplace.js
@@ -9,22 +9,69 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
+// Binary files, don't process these (avoid decoding as utf8)
+const binaryExtensions = ['.png', '.jar'];
+
+/**
+ * Copy a file to given destination, replacing parts of its contents.
+ * @param srcPath Path to a file to be copied.
+ * @param destPath Destination path.
+ * @param replacements: e.g. {'TextToBeReplaced': 'Replacement'}
+ */
 function copyAndReplace(srcPath, destPath, replacements) {
   if (fs.lstatSync(srcPath).isDirectory()) {
     if (!fs.existsSync(destPath)) {
       fs.mkdirSync(destPath);
     }
   } else {
-    let content = fs.readFileSync(srcPath, 'utf8');
-    let srcPermissions = fs.statSync(srcPath).mode;
-    Object.keys(replacements).forEach(regex =>
-      content = content.replace(new RegExp(regex, 'g'), replacements[regex])
-    );
-    fs.writeFileSync(destPath, content, {
-      encoding: 'utf8',
-      mode: srcPermissions,
-    });
+    const extension = path.extname(srcPath);
+    if (binaryExtensions.indexOf(extension) !== -1) {
+      // Binary file
+      copyFile(srcPath, destPath, (err) => {
+        if (err) throw err;
+      });
+    } else {
+      // Text file
+      const srcPermissions = fs.statSync(srcPath).mode;
+      let content = fs.readFileSync(srcPath, 'utf8');
+      Object.keys(replacements).forEach(regex =>
+        content = content.replace(new RegExp(regex, 'g'), replacements[regex])
+      );
+      fs.writeFileSync(destPath, content, {
+        encoding: 'utf8',
+        mode: srcPermissions,
+      });
+    }
+  }
+}
+
+/**
+ * Same as 'cp' on Unix. Don't do any replacements.
+ */
+function copyFile(srcPath, destPath, cb) {
+  let cbCalled = false;
+  const srcPermissions = fs.statSync(srcPath).mode;
+  const readStream = fs.createReadStream(srcPath);
+  readStream.on('error', function(err) {
+    done(err);
+  });
+  const writeStream = fs.createWriteStream(destPath, {
+    mode: srcPermissions
+  });
+  writeStream.on('error', function(err) {
+    done(err);
+  });
+  writeStream.on('close', function(ex) {
+    done();
+  });
+  readStream.pipe(writeStream);
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
   }
 }
 
