e935398db71f916dedd6a17ad0a4c5f6960eaa24
Martin Konicek <mkonicek@fb.com>
2016-11-09 15:50:53 -0800

    Update template

--- local-cli/templates/HelloWorld/android/app/src/main/java/com/helloworld/MainApplication.java
+++ local-cli/templates/HelloWorld/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,6 +31,12 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
