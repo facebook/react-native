308d28bad2867dad9236df5f7bfd673bb73fceb8
Dmitry Petukhov <d.petukhov@roboforex.com>
2016-11-10 14:16:13 +0200

    Sharing a single OkHttpClient among all WebSocket connections

--- ReactAndroid/src/main/java/com/facebook/react/modules/websocket/WebSocketModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/websocket/WebSocketModule.java
@@ -29,6 +29,7 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.ReactConstants;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.modules.network.OkHttpClientProvider;
 import com.facebook.react.modules.network.ForwardingCookieHandler;
 
 import okhttp3.OkHttpClient;
@@ -81,12 +82,6 @@ public class WebSocketModule extends ReactContextBaseJavaModule {
     @Nullable final ReadableArray protocols,
     @Nullable final ReadableMap headers,
     final int id) {
-    OkHttpClient client = new OkHttpClient.Builder()
-      .connectTimeout(10, TimeUnit.SECONDS)
-      .writeTimeout(10, TimeUnit.SECONDS)
-      .readTimeout(0, TimeUnit.MINUTES) // Disable timeouts for read
-      .build();
-
     Request.Builder builder = new Request.Builder()
         .tag(id)
         .url(url);
@@ -132,7 +127,8 @@ public class WebSocketModule extends ReactContextBaseJavaModule {
       }
     }
 
-    WebSocketCall.create(client, builder.build()).enqueue(new WebSocketListener() {
+    OkHttpClient okHttpClient = OkHttpClientProvider.getOkHttpClientForWebSocket();
+    WebSocketCall.create(okHttpClient, builder.build()).enqueue(new WebSocketListener() {
 
       @Override
       public void onOpen(WebSocket webSocket, Response response) {
@@ -189,9 +185,6 @@ public class WebSocketModule extends ReactContextBaseJavaModule {
         sendEvent("websocketMessage", params);
       }
     });
-
-    // Trigger shutdown of the dispatcher's executor so this process can exit cleanly
-    client.dispatcher().executorService().shutdown();
   }
 
   @ReactMethod
