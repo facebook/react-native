74369dcd73ca3d1704f89e8753d0a4de8cf5bace
Steven Goff <stevengoff@discover.com>
2016-11-13 10:31:41 -0600

    Removing ios only platform from allowFontScaling comments/documentation

--- Libraries/Text/Text.js
+++ Libraries/Text/Text.js
@@ -155,8 +155,6 @@ const Text = React.createClass({
     /**
      * Specifies whether fonts should scale to respect Text Size accessibility setting on iOS. The
      * default is `true`.
-     *
-     * @platform ios
      */
     allowFontScaling: React.PropTypes.bool,
     /**
