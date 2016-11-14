bcc152f675d20ad7d70298c6f31aab56f421ba56
Adam Comella <adcom@microsoft.com>
2016-11-14 12:58:46 -0800

    Add imagepipeline to shell's BUCK file
    Also removed unnecessary dependency from MainReactPackage.

--- ReactAndroid/src/main/java/com/facebook/react/shell/MainReactPackage.java
+++ ReactAndroid/src/main/java/com/facebook/react/shell/MainReactPackage.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.facebook.react.LazyReactPackage;
 import com.facebook.react.animated.NativeAnimatedModule;
 import com.facebook.react.bridge.JavaScriptModule;
