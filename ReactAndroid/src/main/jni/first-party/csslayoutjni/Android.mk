c04ab921924037874e34c32bfe5a880d9819c380
Andy Street <andrews@fb.com>
2016-11-15 08:28:31 -0800

    Use -O3 to compile CSSLayout in open source
    Summary: This is how we compile internally
    
    Reviewed By: emilsjolander
    
    Differential Revision: D4182691
    
    fbshipit-source-id: 314b1a1ead7d299677ce7f71549c986e1b796b3b

--- ReactAndroid/src/main/jni/first-party/csslayoutjni/Android.mk
+++ ReactAndroid/src/main/jni/first-party/csslayoutjni/Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/jni
 
-LOCAL_CFLAGS += -Wall -Werror -fvisibility=hidden -fexceptions -frtti
+LOCAL_CFLAGS += -Wall -Werror -fvisibility=hidden -fexceptions -frtti -O3
 CXX11_FLAGS := -std=c++11
 LOCAL_CFLAGS += $(CXX11_FLAGS)
 LOCAL_EXPORT_CPPFLAGS := $(CXX11_FLAGS)
--- ReactCommon/CSSLayout/Android.mk
+++ ReactCommon/CSSLayout/Android.mk
@@ -11,6 +11,6 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
-LOCAL_CFLAGS := -Wall -Werror -fexceptions -frtti -std=c99
+LOCAL_CFLAGS := -Wall -Werror -fexceptions -frtti -std=c99 -O3
 
 include $(BUILD_STATIC_LIBRARY)
