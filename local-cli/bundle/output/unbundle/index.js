112bdc99dc41442f787107aa65dc1349b4a9f50c
David Aurelio <davidaurelio@fb.com>
2016-11-17 02:36:00 -0800

    Add `--indexed-unbundle` flag to force iOS ra-bundle format
    Summary: This adds a new flag to the `unbundle` command: `--indexed-unbundle` allows to output the indexed file format even when building for android.
    
    Reviewed By: cpojer
    
    Differential Revision: D4189485
    
    fbshipit-source-id: e56192456de764eb38c25b574ceaaf52eb8a6fca

--- local-cli/bundle/output/unbundle/index.js
+++ local-cli/bundle/output/unbundle/index.js
@@ -23,7 +23,7 @@ function saveUnbundle(bundle, options, log) {
   // we fork here depending on the platform:
   // while android is pretty good at loading individual assets, ios has a large
   // overhead when reading hundreds pf assets from disk
-  return options.platform === 'android' ?
+  return options.platform === 'android' && !options.indexedUnbundle ?
     asAssets(bundle, options, log) :
     asIndexedFile(bundle, options, log);
 }
