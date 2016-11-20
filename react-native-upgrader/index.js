f08d2f260c339b2b4eb65d8c250fa6d5cbcc2b37
Nicolas Cuillery <nicolas.cuillery@gmail.com>
2016-11-21 00:29:27 +0100

    Add missing header

--- react-native-upgrader/index.js
+++ react-native-upgrader/index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 var argv = require('minimist')(process.argv.slice(2));
 
 var cli = require('./cli');
