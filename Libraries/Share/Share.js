7b96e22782276d74afd1bb4ad03606dcdc8b9e9e
Martin Konicek <martin.konicek@gmail.com>
2016-11-23 18:32:38 +0000

    Update Share.js

--- Libraries/Share/Share.js
+++ Libraries/Share/Share.js
@@ -59,7 +59,7 @@ class Share {
   static share(content: Content, options: Options = {}): Promise<Object> {
     invariant(
       typeof content === 'object' && content != null,
-      'Content must be a valid share content object'
+      'Content to share must be a valid object'
     );
     invariant(
       typeof content.url === 'string' || typeof content.message === 'string',
