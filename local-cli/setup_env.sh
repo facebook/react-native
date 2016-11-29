92971b3297cbc9a4cd792a8a109e3dba264f4cfd
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-11-28 23:46:46 -0800

    Make the non-windows environment setup script more resilient, and give a more helpful error message.

--- local-cli/setup_env.sh
+++ local-cli/setup_env.sh
@@ -5,5 +5,27 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-# 2048 is the max for non root users on Mac
-ulimit -n 2048
+desired_soft_limit=2048
+
+try_set_soft_limit() {
+  if ! ulimit -Sn $desired_soft_limit; then
+    # This is an unlikely error case, because we already know that the hard limit is higher than
+    # the desired soft limit
+    echo "ERROR: Could not set your max file limit, which means that the packager may not work."
+    exit 1
+  fi
+}
+
+hard_limit=$(ulimit -Hn)
+
+if [ "$hard_limit" = 'unlimited' ]; then
+  try_set_soft_limit
+elif [ "$hard_limit" -ge $desired_soft_limit ]; then
+   try_set_soft_limit
+else
+  echo "WARNING: Your max file limit is too low, and was likely previously set for this shell session." \
+  "If not running as a superuser, the packager may not work." \
+  "Run ulimit -Hn to see your hard file limit. Often, it will have been set in /etc/profile or your user profile" \
+  "(~/.profile, ~/.bash_profile, ~/.zprofile, ~/.bashrc, etc)"
+  ulimit -n $desired_soft_limit
+fi
