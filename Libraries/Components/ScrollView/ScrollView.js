8acaaa5f18cc200f044452518d3d89df2adaa381
Martin Kralik <majak47@gmail.com>
2016-12-07 16:13:29 +0000

    Upselling ListView in ScrollView's docs
    Internal research at FB has shown that many devs don't know why they should use ListView instead of ScrollView, or that ListView even exists. This PR changes ScrollView's docs to upsell ListView.
    
    I could put this as a separate guide in the docs, but I think this place will have the best possible reach for target audience.
    
    Suggestions welcome. Let me know if it doesn't make sense or is too verbose.

--- Libraries/Components/ScrollView/ScrollView.js
+++ Libraries/Components/ScrollView/ScrollView.js
@@ -44,6 +44,21 @@ const requireNativeComponent = require('requireNativeComponent');
  *
  * Doesn't yet support other contained responders from blocking this scroll
  * view from becoming the responder.
+ *
+ *
+ * <ScrollView> vs <ListView> - which one to use?
+ * ScrollView simply renders all it’s react child components at once. That
+ * makes very easy to understand and use.
+ * On the other hand, this has performance downside. Imagine you have a very
+ * long list of items you want to display, worth of couple of your scollview’s
+ * heights. Creating js components and native views upfront for all its items,
+ * which may not even be shown, will contribute to slow rendering of your
+ * screen and increased memory usage.
+ *
+ * This is where ListView comes into play. ListView renders items lazily,
+ * just when they are about to appear. This lazyness comes at cost of more
+ * complicated API, which is worth it, unless you are rendering a small fixed
+ * set of items.
  */
 const ScrollView = React.createClass({
   propTypes: {
