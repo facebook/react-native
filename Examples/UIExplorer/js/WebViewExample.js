6206476a08367026392efe6efc496a9ced47f47c
Luke Miles <luke@restocks.io>
2016-12-12 18:49:48 -0800

    fix quotes

--- Examples/UIExplorer/js/WebViewExample.js
+++ Examples/UIExplorer/js/WebViewExample.js
@@ -267,7 +267,7 @@ class MessagingTest extends React.Component {
 class InjectJS extends React.Component {
   webview = null;
   injectJS = () => {
-    const script = 'document.write("Injected JS ")';
+    const script = `document.write('Injected JS ')`;
     if (this.webview) {
       this.webview.injectJavaScript(script);
     }
