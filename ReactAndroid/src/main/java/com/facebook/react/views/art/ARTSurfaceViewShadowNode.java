5ef127afe9972f4f0e066c4b7e62128e6628b8e6
Dmitry Petukhov <d.petukhov@roboforex.com>
2016-12-19 11:17:42 +0200

    Fixed ART Surface: do not cancel updates to the surfce, make them pending instead and draw once the canvas becomes available

--- ReactAndroid/src/main/java/com/facebook/react/views/art/ARTSurfaceViewShadowNode.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/art/ARTSurfaceViewShadowNode.java
@@ -28,10 +28,11 @@ import com.facebook.react.uimanager.ReactShadowNode;
 /**
  * Shadow node for ART virtual tree root - ARTSurfaceView
  */
-public class ARTSurfaceViewShadowNode extends LayoutShadowNode
+public class ARTSurfaceViewShadowNode extends LayoutShadowNode 
   implements TextureView.SurfaceTextureListener {
 
   private @Nullable Surface mSurface;
+  private @Nullable boolean mHasPendingUpdates;
 
   @Override
   public boolean isVirtual() {
@@ -52,7 +53,7 @@ public class ARTSurfaceViewShadowNode extends LayoutShadowNode
 
   private void drawOutput() {
     if (mSurface == null || !mSurface.isValid()) {
-      markChildrenUpdatesSeen(this);
+      mHasPendingUpdates = true;
       return;
     }
 
@@ -70,25 +71,20 @@ public class ARTSurfaceViewShadowNode extends LayoutShadowNode
       if (mSurface == null) {
         return;
       }
-
+    
       mSurface.unlockCanvasAndPost(canvas);
-    } catch (IllegalArgumentException | IllegalStateException e) {
+      mHasPendingUpdates = false;
+    } catch(IllegalArgumentException | IllegalStateException e) {
       FLog.e(ReactConstants.TAG, e.getClass().getSimpleName() + " in Surface.unlockCanvasAndPost");
-    }
-  }
-
-  private void markChildrenUpdatesSeen(ReactShadowNode shadowNode) {
-    for (int i = 0; i < shadowNode.getChildCount(); i++) {
-      ReactShadowNode child = shadowNode.getChildAt(i);
-      child.markUpdateSeen();
-      markChildrenUpdatesSeen(child);
     }
   }
 
   @Override
   public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
     mSurface = new Surface(surface);
-    drawOutput();
+    if (mHasPendingUpdates) {
+      drawOutput();
+    }
   }
 
   @Override
