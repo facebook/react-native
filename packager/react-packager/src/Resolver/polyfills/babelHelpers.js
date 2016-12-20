b83a84ab6dca44e1e71a12a652688d669a33ba8c
Connor McEwen <connor.mcewen@gmail.com>
2016-12-20 15:26:39 -0500

    Add obj !== Symbol.prototype to babelHelpers

--- packager/react-packager/src/Resolver/polyfills/babelHelpers.js
+++ packager/react-packager/src/Resolver/polyfills/babelHelpers.js
@@ -22,7 +22,7 @@ var babelHelpers = global.babelHelpers = {};
 babelHelpers.typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
   return typeof obj;
 } : function (obj) {
-  return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj;
+  return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
 };
 
 babelHelpers.createRawReactElement = (function () {
