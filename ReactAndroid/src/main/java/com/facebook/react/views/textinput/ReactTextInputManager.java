9e6ef89e210140694f700c48ce998114d531516e
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-01-12 08:42:41 +0200

    Add caretHidden implementation to Android side too

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputManager.java
@@ -297,6 +297,11 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
     }
   }
 
+  @ReactProp(name = "caretHidden", defaultBoolean = false)
+  public void setCaretHidden(ReactEditText view, boolean caretHidden) {
+    view.setCursorVisible(!caretHidden);
+  }
+
   @ReactProp(name = "selectTextOnFocus", defaultBoolean = false)
   public void setSelectTextOnFocus(ReactEditText view, boolean selectTextOnFocus) {
     view.setSelectAllOnFocus(selectTextOnFocus);
