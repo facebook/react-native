ae4fa4b012fca8ad62b75fc9a3de232066215a28
Maël Nison <nison.mael@gmail.com>
2017-01-15 15:16:10 -0800

    Add YGLayoutGetMargin
    Summary:
    Fix #326. I'll open another PR once this one gets accepted to add support for `YGLayoutGetBorder` 👌
    Closes https://github.com/facebook/yoga/pull/335
    
    Reviewed By: gkassabli
    
    Differential Revision: D4409399
    
    Pulled By: emilsjolander
    
    fbshipit-source-id: 8153f6701cab60b55a485f6d2e0b9f7767481090

--- ReactCommon/yoga/yoga/Yoga.c
+++ ReactCommon/yoga/yoga/Yoga.c
@@ -47,6 +47,7 @@ typedef struct YGCachedMeasurement {
 typedef struct YGLayout {
   float position[4];
   float dimensions[2];
+  float margin[6];
   float padding[6];
   YGDirection direction;
 
@@ -530,6 +531,29 @@ void YGNodeStyleSetFlex(const YGNodeRef node, const float flex) {
     return node->layout.instanceName;                          \
   }
 
+#define YG_NODE_LAYOUT_RESOLVED_PROPERTY_IMPL(type, name, instanceName)  \
+  type YGNodeLayoutGet##name(const YGNodeRef node, const YGEdge edge) {                    \
+    YG_ASSERT(edge <= YGEdgeEnd, "Cannot get layout properties of multi-edge shorthands"); \
+                                                                                           \
+    if (edge == YGEdgeLeft) {                                                              \
+      if (node->layout.direction == YGDirectionRTL) {                                      \
+        return node->layout.instanceName[YGEdgeEnd];                                       \
+      } else {                                                                             \
+        return node->layout.instanceName[YGEdgeStart];                                     \
+      }                                                                                    \
+    }                                                                                      \
+                                                                                           \
+    if (edge == YGEdgeRight) {                                                             \
+      if (node->layout.direction == YGDirectionRTL) {                                      \
+        return node->layout.instanceName[YGEdgeStart];                                     \
+      } else {                                                                             \
+        return node->layout.instanceName[YGEdgeEnd];                                       \
+      }                                                                                    \
+    }                                                                                      \
+                                                                                           \
+    return node->layout.instanceName[edge];                                                \
+  }
+
 YG_NODE_PROPERTY_IMPL(void *, Context, context, context);
 YG_NODE_PROPERTY_IMPL(YGPrintFunc, PrintFunc, printFunc, print);
 YG_NODE_PROPERTY_IMPL(bool, HasNewLayout, hasNewLayout, hasNewLayout);
@@ -571,27 +595,8 @@ YG_NODE_LAYOUT_PROPERTY_IMPL(float, Width, dimensions[YGDimensionWidth]);
 YG_NODE_LAYOUT_PROPERTY_IMPL(float, Height, dimensions[YGDimensionHeight]);
 YG_NODE_LAYOUT_PROPERTY_IMPL(YGDirection, Direction, direction);
 
-float YGNodeLayoutGetPadding(const YGNodeRef node, const YGEdge edge) {
-  YG_ASSERT(edge <= YGEdgeEnd, "Cannot get layout paddings of multi-edge shorthands");
-
-  if (edge == YGEdgeLeft) {
-    if (node->layout.direction == YGDirectionRTL) {
-      return node->layout.padding[YGEdgeEnd];
-    } else {
-      return node->layout.padding[YGEdgeStart];
-    }
-  }
-
-  if (edge == YGEdgeRight) {
-    if (node->layout.direction == YGDirectionRTL) {
-      return node->layout.padding[YGEdgeStart];
-    } else {
-      return node->layout.padding[YGEdgeEnd];
-    }
-  }
-
-  return node->layout.padding[edge];
-}
+YG_NODE_LAYOUT_RESOLVED_PROPERTY_IMPL(float, Margin, margin);
+YG_NODE_LAYOUT_RESOLVED_PROPERTY_IMPL(float, Padding, padding);
 
 uint32_t gCurrentGenerationCount = 0;
 
@@ -1741,6 +1746,23 @@ static void YGNodelayoutImpl(const YGNodeRef node,
   // Set the resolved resolution in the node's layout.
   const YGDirection direction = YGNodeResolveDirection(node, parentDirection);
   node->layout.direction = direction;
+
+  node->layout.margin[YGEdgeStart] =
+      YGNodeLeadingMargin(node,
+                           YGFlexDirectionResolve(YGFlexDirectionRow, direction),
+                           parentWidth);
+  node->layout.margin[YGEdgeEnd] =
+      YGNodeTrailingMargin(node,
+                            YGFlexDirectionResolve(YGFlexDirectionRow, direction),
+                            parentWidth);
+  node->layout.margin[YGEdgeTop] =
+      YGNodeLeadingMargin(node,
+                           YGFlexDirectionResolve(YGFlexDirectionColumn, direction),
+                           parentWidth);
+  node->layout.margin[YGEdgeBottom] =
+      YGNodeTrailingMargin(node,
+                            YGFlexDirectionResolve(YGFlexDirectionColumn, direction),
+                            parentWidth);
 
   node->layout.padding[YGEdgeStart] =
       YGNodeLeadingPadding(node,
