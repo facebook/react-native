ef601d75fac82bfb4adcae433c4284fd5f949743
Tomas Roos <ptomasroos@gmail.com>
2017-01-17 13:25:29 +0100

    Added new ChildFrame class

---
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ChildFrame.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.react.views.scroll;
+
+public class ChildFrame {
+    public int index;
+    public int x;
+    public int y;
+    public int height;
+    public int width;
+}
