124109c0c5745f17159368fe2ddbf864884ee7af
Moses Gunesch <moses.gunesch@instrument.com>
2017-01-23 17:23:41 -0800

    Adds video support to react-native link for Android

--- local-cli/link/android/copyAssets.js
+++ local-cli/link/android/copyAssets.js
@@ -6,12 +6,17 @@ const groupFilesByType = require('../groupFilesByType');
  * Copies each file from an array of assets provided to targetPath directory
  *
  * For now, the only types of files that are handled are:
- * - Fonts (otf, ttf) - copied to targetPath/fonts under original name
+ * - Fonts (otf, ttf) - copied to targetPath/fonts
+ * - Videos (mp4, 3gp, etc.) - copied to targetPath/videos
  */
 module.exports = function copyAssetsAndroid(files, targetPath) {
   const assets = groupFilesByType(files);
 
   (assets.font || []).forEach(asset =>
     fs.copySync(asset, path.join(targetPath, 'fonts', path.basename(asset)))
+  );
+
+  (assets.video || []).forEach(asset =>
+    fs.copySync(asset, path.join(targetPath, 'videos', path.basename(asset)))
   );
 };
