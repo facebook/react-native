3a9312855166a5f8be58d560cfe66bebed0f959d
Eric Vicenti <ericvicenti@users.noreply.github.com>
2017-01-26 09:08:41 -0800

    Add breathing room

--- React/Views/RCTTVView.m
+++ React/Views/RCTTVView.m
@@ -173,10 +173,10 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:unused)
   if (hasTVPreferredFocus) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
       UIView *rootview = self;
-      while(![rootview isReactRootView] && rootview != nil) {
+      while (![rootview isReactRootView] && rootview != nil) {
         rootview = [rootview superview];
       }
-      if(rootview == nil) return;
+      if (rootview == nil) return;
 
       rootview = [rootview superview];
 
