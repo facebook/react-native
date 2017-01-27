55593e4a5431a7bf48c299473adc7ee81bbb71e0
Tomas Roos <ptomasroos@gmail.com>
2017-01-27 14:57:09 +0100

    Support refreshable ListView as well

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ScrollViewManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ScrollViewManager.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2016-present, Facebook, Inc.
  * All rights reserved.
- *
+ * <p>
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
@@ -20,54 +20,61 @@ import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.PixelUtil;
 import com.facebook.react.uimanager.UIManagerModule;
+import com.facebook.react.views.swiperefresh.ReactSwipeRefreshLayout;
 import com.facebook.react.views.view.ReactViewGroup;
 
 import java.util.ArrayList;
 
 public class ScrollViewManager extends ReactContextBaseJavaModule {
-    public ScrollViewManager(ReactApplicationContext reactApplicationContext) {
-        super(reactApplicationContext);
+  public ScrollViewManager(ReactApplicationContext reactApplicationContext) {
+    super(reactApplicationContext);
+  }
+
+  @Override
+  public String getName() {
+    return "ScrollViewManager";
+  }
+
+  @ReactMethod
+  public void calculateChildFrames(int tag, Callback callback) {
+    ReactApplicationContext reactCtx = getReactApplicationContext();
+    UIManagerModule uiManager = reactCtx.getNativeModule(UIManagerModule.class);
+
+    View view = uiManager.getUIImplementation().getView(tag);
+
+    ReactScrollView scrollView = null;
+
+    if (view instanceof ReactScrollView) {
+      scrollView = ((ReactScrollView) view);
+    } else if (view instanceof ReactSwipeRefreshLayout) {
+      ReactSwipeRefreshLayout refreshLayout = ((ReactSwipeRefreshLayout) view);
+
+      scrollView = ((ReactScrollView) refreshLayout.getChildAt(0));
     }
 
-    @Override
-    public String getName() {
-        return "ScrollViewManager";
+    if (scrollView == null) {
+      return;
     }
 
-    @ReactMethod
-    public void calculateChildFrames(int tag, Callback callback) {
-        ReactApplicationContext reactCtx = getReactApplicationContext();
-        UIManagerModule uiManager = reactCtx.getNativeModule(UIManagerModule.class);
+    ArrayList<ChildFrame> mUpdatedChildFrames = scrollView.calculateChildFrames();
 
-        View view = uiManager.getUIImplementation().getView(tag);
+    WritableArray updatedFrames = Arguments.createArray();
 
-        if (view instanceof ReactScrollView) {
-            ReactScrollView scrollView = ((ReactScrollView)view);
+    if (mUpdatedChildFrames != null) {
+      for (int i = 0; i < mUpdatedChildFrames.size(); i++) {
+        ChildFrame childFrame = mUpdatedChildFrames.get(i);
 
-            if (scrollView == null) {
-                return;
-            }
+        WritableMap map = Arguments.createMap();
+        map.putInt("index", childFrame.index);
+        map.putDouble("height", PixelUtil.toDIPFromPixel(childFrame.height));
+        map.putDouble("width", PixelUtil.toDIPFromPixel(childFrame.width));
+        map.putDouble("x", PixelUtil.toDIPFromPixel(childFrame.x));
+        map.putDouble("y", PixelUtil.toDIPFromPixel(childFrame.y));
 
-            ArrayList<ChildFrame> mUpdatedChildFrames = scrollView.calculateChildFrames();
-
-            WritableArray updatedFrames = Arguments.createArray();
-
-            if (mUpdatedChildFrames != null) {
-                for (int i = 0; i < mUpdatedChildFrames.size(); i++) {
-                    ChildFrame childFrame = mUpdatedChildFrames.get(i);
-
-                    WritableMap map = Arguments.createMap();
-                    map.putInt("index", childFrame.index);
-                    map.putDouble("height", PixelUtil.toDIPFromPixel(childFrame.height));
-                    map.putDouble("width", PixelUtil.toDIPFromPixel(childFrame.width));
-                    map.putDouble("x", PixelUtil.toDIPFromPixel(childFrame.x));
-                    map.putDouble("y", PixelUtil.toDIPFromPixel(childFrame.y));
-
-                    updatedFrames.pushMap(map);
-                }
-            }
-
-            callback.invoke(updatedFrames);
-        }
+        updatedFrames.pushMap(map);
+      }
     }
+
+    callback.invoke(updatedFrames);
+  }
 }
