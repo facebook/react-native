83f5b179d448a499f07823431fbeabf33bda0541
Alex Hanson <alex.hanson@redfin.com>
2017-02-01 10:17:25 -0800

    Discover scoped dependencies (e.g., "@foo/bar") in `findSymlinksPaths` by looking inside @-prefixed folders for symlinks.

--- local-cli/util/findSymlinksPaths.js
+++ local-cli/util/findSymlinksPaths.js
@@ -6,40 +6,58 @@ const fs = require('fs');
  * Ignore any descendants of the paths in `ignoredRoots`.
  */
 module.exports = function findSymlinksPaths(lookupFolder, ignoredRoots) {
-  const timeStart = Date.now();
-  const folders = fs.readdirSync(lookupFolder);
-
   const resolvedSymlinks = [];
-  folders.forEach(folder => {
-    const visited = [];
 
-    let symlink = path.resolve(lookupFolder, folder);
-    while (fs.lstatSync(symlink).isSymbolicLink()) {
-      const index = visited.indexOf(symlink);
-      if (index !== -1) {
-        throw Error(
-          `Infinite symlink recursion detected:\n  ` +
-            visited.slice(index).join(`\n  `)
-        );
-      }
-
-      visited.push(symlink);
-      symlink = path.resolve(
-        path.dirname(symlink),
-        fs.readlinkSync(symlink)
-      );
-    }
-
-    if (visited.length && !rootExists(ignoredRoots, symlink)) {
-      resolvedSymlinks.push(symlink);
-    }
-  });
-
+  const timeStart = Date.now();
+  const scannedFolderCount = _findSymlinksPaths(lookupFolder, ignoredRoots, resolvedSymlinks);
   const timeEnd = Date.now();
-  console.log(`Scanning ${folders.length} folders for symlinks in ${lookupFolder} (${timeEnd - timeStart}ms)`);
+
+  console.log(`Scanning ${scannedFolderCount} folders for symlinks in ${lookupFolder} (${timeEnd - timeStart}ms)`);
 
   return resolvedSymlinks;
 };
+
+function _findSymlinksPaths(lookupFolder, ignoredRoots, resolvedSymlinks) {
+  const folders = fs.readdirSync(lookupFolder);
+  let scannedFolderCount = folders.length;
+
+  folders.forEach(folder => {
+    const folderPath = path.resolve(lookupFolder, folder);
+
+    // Handle scope folders (e.g., "@foo") by recursing, allowing us to
+    // discover symlinks like "@foo/bar". A valid package setup should not have
+    // nested scopes like "@foo/@bar/baz", so the depth of recursion should be
+    // limited to one level.
+    if (folder.startsWith("@")) {
+      scannedFolderCount += _findSymlinksPaths(folderPath, ignoredRoots, resolvedSymlinks);
+    } else {
+      const visited = [];
+
+      let symlink = folderPath;
+      while (fs.lstatSync(symlink).isSymbolicLink()) {
+        const index = visited.indexOf(symlink);
+        if (index !== -1) {
+          throw Error(
+            `Infinite symlink recursion detected:\n  ` +
+              visited.slice(index).join(`\n  `)
+          );
+        }
+
+        visited.push(symlink);
+        symlink = path.resolve(
+          path.dirname(symlink),
+          fs.readlinkSync(symlink)
+        );
+      }
+
+      if (visited.length && !rootExists(ignoredRoots, symlink)) {
+        resolvedSymlinks.push(symlink);
+      }
+    }
+  });
+
+  return scannedFolderCount;
+}
 
 function rootExists(roots, child) {
   return roots.some(root => isDescendant(root, child));
