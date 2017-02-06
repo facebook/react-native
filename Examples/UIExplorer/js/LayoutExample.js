c78b411a662a5958be2cb52f3a2f8d0d4ec3b451
Alberto Schiabel <jkomyno@users.noreply.github.com>
2017-02-06 09:46:50 +0100

    Update LayoutExample.js
    Added ```UIManager.setLayoutAnimationEnabledExperimental(true)``` if the user is on Android. Just for the sake of clarity

--- Examples/UIExplorer/js/LayoutExample.js
+++ Examples/UIExplorer/js/LayoutExample.js
@@ -28,7 +28,14 @@ var {
   StyleSheet,
   Text,
   View,
+  Platform,
+  UIManager,
 } = ReactNative;
+
+if (Platform.OS === 'android') {
+  UIManager.setLayoutAnimationEnabledExperimental &&
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 var UIExplorerBlock = require('./UIExplorerBlock');
 var UIExplorerPage = require('./UIExplorerPage');
