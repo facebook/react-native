b998b3a1c34bbd89c90461853338c79202521c00
Benoit VALLON <benoitvallon@gmail.com>
2017-02-12 23:34:55 -0500

    Fix badgeColor for previous iOS 10 versions

--- React/Views/RCTTabBarItem.m
+++ React/Views/RCTTabBarItem.m
@@ -107,9 +107,9 @@ RCT_ENUM_CONVERTER(UITabBarSystemItem, (@{
 
 - (void)setBadgeColor:(UIColor *)badgeColor
 {
-#if !TARGET_OS_TV && defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-  _barItem.badgeColor = badgeColor;
-#endif
+  if ([self.barItem respondsToSelector:@selector(badgeColor)]) {
+    self.barItem.badgeColor = badgeColor;
+  }
 }
 
 - (UIViewController *)reactViewController
