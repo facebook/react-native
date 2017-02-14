b217f24e9420f764ba19fcb3eb0f91d7c7e972e1
Janic Duplessis <janicduplessis@gmail.com>
2017-02-14 15:55:05 -0500

    Cleanup SO code

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputManager.java
@@ -321,27 +321,24 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
     // the cursor color programatically.
     // Based on http://stackoverflow.com/questions/25996032/how-to-change-programatically-edittext-cursor-color-in-android.
     try {
-      // Get the cursor resource id.
-      Field field = TextView.class.getDeclaredField("mCursorDrawableRes");
-      field.setAccessible(true);
-      int drawableResId = field.getInt(view);
+      // Get the original cusor drawable resource.
+      Field cursorDrawableResField = TextView.class.getDeclaredField("mCursorDrawableRes");
+      cursorDrawableResField.setAccessible(true);
+      int drawableResId = cursorDrawableResField.getInt(view);
 
-      // Get the editor.
-      field = TextView.class.getDeclaredField("mEditor");
-      field.setAccessible(true);
-      Object editor = field.get(view);
-
-      // Get the drawable and set a color filter.
       Drawable drawable = view.getContext().getDrawable(drawableResId);
       if (color != null) {
         drawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
       }
       Drawable[] drawables = {drawable, drawable};
 
-      // Set the drawables.
-      field = editor.getClass().getDeclaredField("mCursorDrawable");
-      field.setAccessible(true);
-      field.set(editor, drawables);
+      // Update the current cursor drawable with the new one.
+      Field editorField = TextView.class.getDeclaredField("mEditor");
+      editorField.setAccessible(true);
+      Object editor = editorField.get(view);
+      Field cursorDrawableField = editor.getClass().getDeclaredField("mCursorDrawable");
+      cursorDrawableField.setAccessible(true);
+      cursorDrawableField.set(editor, drawables);
     } catch (Exception ex) {
       // Ignore errors to avoid crashing if these private fields don't exist on modified
       // or future android versions.
