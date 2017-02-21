ac82bb11a6f557b50dfb24089ffe9521de4e6b5e
geweb <gewebcn@gmail.com>
2017-02-21 14:09:39 +0800

    Update RCTWebView.m
    fix play vedio

--- React/Views/RCTWebView.m
+++ React/Views/RCTWebView.m
@@ -266,7 +266,7 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
       // http://stackoverflow.com/questions/1024748/how-do-i-fix-nsurlerrordomain-error-999-in-iphone-3-0-os
       return;
     }
-    // fix paly vedio get the following error:Plug-in handled load
+    // fix play vedio get the following error:Plug-in handled load
     else if ([error.domain isEqualToString:@"WebKitErrorDomain"] && (error.code == 204)) {
         return;
     }
