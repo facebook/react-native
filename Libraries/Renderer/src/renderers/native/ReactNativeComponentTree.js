a0da13322bdde2a21b2ce05255075adb466960b7
Yi Sun <yisun@fb.com>
2017-02-21 17:24:00 -0800

    Revert D4592611: [rc|onliner] null gating for component tree states
    Differential Revision: D4592611
    
    fbshipit-source-id: 5f3836e9a3631d211c3c6ff1b036e59197418eb8

--- Libraries/Renderer/src/renderers/native/ReactNativeComponentTree.js
+++ Libraries/Renderer/src/renderers/native/ReactNativeComponentTree.js
@@ -61,7 +61,7 @@ function getInstanceFromTag(tag) {
 
 function getTagFromInstance(inst) {
   // TODO (bvaughn) Clean up once Stack is deprecated
-  var tag = inst._rootNodeID || (inst.stateNode && inst.stateNode._nativeTag);
+  var tag = inst._rootNodeID || inst.stateNode._nativeTag;
   invariant(tag, 'All native instances should have a tag.');
   return tag;
 }
