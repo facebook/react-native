a60614ceea2cfeca03c7fd2844713b74ac1087c1
Martin Konicek <mkonicek@fb.com>
2017-02-23 11:29:38 +0000

    Copy template from npm

--- local-cli/generator/templates.js
+++ local-cli/generator/templates.js
@@ -13,7 +13,11 @@ const execSync = require('child_process').execSync;
 const fs = require('fs');
 const path = require('path');
 
-const availableTemplates = {
+/**
+ * Templates released as part of react-native in local-cli/templates.
+ * It's also possible to use templates from npm.
+ */
+const builtInTemplates = {
   navigation: 'HelloNavigation',
 };
 
@@ -22,9 +26,9 @@ function listTemplatesAndExit(newProjectName, options) {
     // Just listing templates using 'react-native init --template'.
     // Not creating a new app.
     // Print available templates and exit.
-    const templateKeys = Object.keys(availableTemplates);
+    const templateKeys = Object.keys(builtInTemplates);
     if (templateKeys.length === 0) {
-      // Just a guard, should never happen as long availableTemplates
+      // Just a guard, should never happen as long builtInTemplates
       // above is defined correctly :)
       console.log(
         'There are no templates available besides ' +
@@ -73,27 +77,85 @@ function createProjectFromTemplate(destPath, newProjectName, templateKey, yarnVe
   // This way we don't have to duplicate the native files in every template.
   // If we duplicated them we'd make RN larger and risk that people would
   // forget to maintain all the copies so they would go out of sync.
-  const templateName = availableTemplates[templateKey];
-  if (templateName) {
-    copyProjectTemplateAndReplace(
-      path.resolve(
-        'node_modules', 'react-native', 'local-cli', 'templates', templateName
-      ),
-      destPath,
-      newProjectName
-    );
+  const builtInTemplateName = builtInTemplates[templateKey];
+  if (builtInTemplateName) {
+    // templateKey is e.g. 'navigation',
+    // use the built-in local-cli/templates/HelloNavigation folder
+    createFromBuiltInTemplate(builtInTemplateName, destPath, newProjectName, yarnVersion);
   } else {
-    throw new Error('Uknown template: ' + templateKey);
+    // templateKey is e.g. 'ignite',
+    // use the template react-native-template-ignite from npm
+    createFromNpmTemplate(templateKey, destPath, newProjectName, yarnVersion);
   }
-
-  installTemplateDependencies(templateName, yarnVersion);
 }
 
-function installTemplateDependencies(templateName, yarnVersion) {
+// (We might want to get rid of built-in templates in the future -
+// publish them to npm and install from there.)
+function createFromBuiltInTemplate(templateName, destPath, newProjectName, yarnVersion) {
+  const templatePath = path.resolve(
+    'node_modules', 'react-native', 'local-cli', 'templates', templateName
+  );
+  copyProjectTemplateAndReplace(
+    templatePath,
+    destPath,
+    newProjectName,
+  );
+  installTemplateDependencies(templatePath, yarnVersion);
+}
+
+function createFromNpmTemplate(templateName, destPath, newProjectName, yarnVersion) {
+  const packageName = 'react-native-template-' + templateName;
+  // Check if the template exists
+  console.log(`Fetching template ${packageName} from npm...`);
+  try {
+    if (yarnVersion) {
+      execSync(`yarn info ${packageName}`);
+    } else {
+      execSync(`npm info ${packageName}`);
+    }
+  } catch (err) {
+    throw new Error(`The template ${packageName} was not found on npm:\n` + err.message);
+  }
+  const templatePath = path.resolve(
+    'node_modules', packageName
+  );
+  try {
+    if (yarnVersion) {
+      execSync(`yarn add ${packageName} --ignore-scripts`, {stdio: 'inherit'});
+    } else {
+      execSync(`npm install ${packageName} --save --save-exact --ignore-scripts`, {stdio: 'inherit'});
+    }
+    // TODO debug this, package.json is getting overwritten
+    console.log('====== installed template in ' + templatePath);
+    console.log('====== copyProjectTemplateAndReplace ', { templatePath, destPath, newProjectName });
+    copyProjectTemplateAndReplace(
+      templatePath,
+      destPath,
+      newProjectName,
+    );
+  } finally {
+    // Clean up the temp files
+    try {
+      if (yarnVersion) {
+        execSync(`yarn remove ${packageName} --ignore-scripts`);
+      } else {
+        execSync(`npm uninstall ${packageName} --ignore-scripts`);
+      }
+    } catch (err) {
+      console.warn(
+        `Failed to clean up template temp files in node_modules/${packageName}. ` +
+        'This is not a critical error, you can work on your app.'
+      );
+    }
+  }
+  installTemplateDependencies(templatePath, yarnVersion);
+}
+
+function installTemplateDependencies(templatePath, yarnVersion) {
   // dependencies.json is a special file that lists additional dependencies
   // that are required by this template
   const dependenciesJsonPath = path.resolve(
-    'node_modules', 'react-native', 'local-cli', 'templates', templateName, 'dependencies.json'
+    templatePath, 'dependencies.json'
   );
   if (!fs.existsSync(dependenciesJsonPath)) {
     return;
