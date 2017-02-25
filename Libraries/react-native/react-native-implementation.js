cf1bc8d6443432ba24efa17cd15ba73072fad193
Bhuwan Khattar <voideanvalue@fb.com>
2017-02-25 10:21:09 -0800

    s/react-native.js/react-native-implementation.js/
    Summary: Renaming since a file with the same name as a package confuses packager
    
    Reviewed By: cpojer
    
    Differential Revision: D4613215
    
    fbshipit-source-id: 5cfae71d0bb1eef2675689b37a9b42596d25678d

--- Libraries/react-native/react-native-implementation.js
+++ Libraries/react-native/react-native-implementation.js
@@ -6,6 +6,7 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
+ * @providesModule react-native-implementation
  * @noflow - get/set properties not yet supported by flow. also `...require(x)` is broken #6560135
  */
 'use strict';
