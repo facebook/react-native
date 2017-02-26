f0083f5ed455d83309e08ac8ff1fbbccbfd0ea8f
Nick DeJesus <ndejesus@sapient.com>
2017-02-26 16:23:21 -0500

    test plan is to add an icon to the root level of a project, add the filename of the icon to app.json property called icon, run react-native eject and see if app icon has been updated

--- local-cli/eject/eject.js
+++ local-cli/eject/eject.js
@@ -10,7 +10,9 @@
 
 const copyProjectTemplateAndReplace = require('../generator/copyProjectTemplateAndReplace');
 const path = require('path');
+const sharp = require('sharp');
 const fs = require('fs');
+
 
 /**
  * The eject command re-creates the `android` and `ios` native folders. Because native code can be
@@ -64,6 +66,15 @@ function eject() {
     process.exit(1);
   }
 
+  const appIcon = appConfig.icon;
+  if(!appIcon) {
+    console.error(
+      'Please provide an icon for the app'
+    );
+    process.exit(1);
+  }
+
+
   const templateOptions = { displayName };
 
   if (!doesIOSExist) {
@@ -72,8 +83,43 @@ function eject() {
       path.resolve('node_modules', 'react-native', 'local-cli', 'templates', 'HelloWorld', 'ios'),
       path.resolve('ios'),
       appName,
+      appIcon,
       templateOptions
     );
+
+    console.log('Setting Up App Icons.')
+    const pictureSizes = [40, 60, 58, 87, 80, 120, 180];
+    let contentPath = 'ios/SomeTest/Images.xcassets/AppIcon.appiconset/Contents.json';
+
+    let fileNames = pictureSizes.reduce( (acc, size) => {
+      let filePath = 'ios/SomeTest/Images.xcassets/AppIcon.appiconset/' + size + 'pt-' + appIcon;
+      let fileName = size+'pt-'+appIcon;
+      sharp(appIcon)
+      .resize(size, size)
+      .toFile(filePath, function(err) {
+        if(err) throw err;
+      });
+      acc['SIZE_'+size] = fileName;
+      return acc
+    }, []);
+
+    fs.readFile(contentPath, 'utf8', function (err, data) {
+      if (err) throw err;
+      let obj = JSON.parse(data);
+
+      obj.images.map((image, key) => {
+        let size = parseInt(image.size.split('x')[0]);
+        let scale = parseInt(image.scale.replace('x', ''));
+        let fileName = fileNames['SIZE_'+ scale*size];
+        obj.images[key].filename = fileName;
+        return obj;
+      })
+      console.log(obj);
+      fs.writeFile(contentPath, JSON.stringify(obj), 'utf8', function(err){
+        if(err) throw err;
+      })
+    });
+
   }
 
   if (!doesAndroidExist) {
@@ -84,6 +130,30 @@ function eject() {
       appName,
       templateOptions
     );
+
+    console.log('Setting up App Icons');
+    const hdpiPath = 'android/app/src/main/res/mipmap-hdpi' + '/ic_launcher.png';
+    const mdpiPath = 'android/app/src/main/res/mipmap-mdpi' + '/ic_launcher.png';
+    const xhdpiPath = 'android/app/src/main/res/mipmap-xhdpi' + '/ic_launcher.png';
+    const xxhdpiPath = 'android/app/src/main/res/mipmap-xxhdpi' + '/ic_launcher.png';
+
+    const pictureSizes = [48, 72, 96, 144];
+    const filePaths = [hdpiPath, mdpiPath, xhdpiPath, xxhdpiPath];
+
+    filePaths.map((path) => {
+      fs.unlink(path, (err) => {
+        if(err) throw err;
+      })
+    })
+
+    pictureSizes.map((size, key) => {
+      console.log(filePaths[key], size);
+      sharp(appIcon)
+      .resize(size, size)
+      .toFile(filePaths[key], function(err) {
+        if (err) throw err;
+      });
+    });
   }
 
 }
