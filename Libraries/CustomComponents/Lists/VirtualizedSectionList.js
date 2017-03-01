c753cec9a0acdc5ba6257d2c71017d4e6af23682
Douglas Lowder <dlowder@salesforce.com>
2017-03-01 12:14:08 -0800

    Fix warning module import in VirtualizedSectionList

--- Libraries/CustomComponents/Lists/VirtualizedSectionList.js
+++ Libraries/CustomComponents/Lists/VirtualizedSectionList.js
@@ -37,7 +37,7 @@ const View = require('View');
 const VirtualizedList = require('VirtualizedList');
 
 const invariant = require('invariant');
-const warning = require('warning');
+const warning = require('fbjs/lib/warning');
 
 import type {ViewToken} from 'ViewabilityHelper';
 import type {Props as VirtualizedListProps} from 'VirtualizedList';
