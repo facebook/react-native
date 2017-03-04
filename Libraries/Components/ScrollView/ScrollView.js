94a333a2ead7ea632fe52f0439397c4f3ed11f2a
Spencer Ahrens <sahrens@fb.com>
2017-03-03 20:00:30 -0800

    Fix crash with js-based sticky headers
    Summary:
    We're seeing ` inputRange must be monotonically increasing -1,0,0,-33,-32 ` which happens when we
    have zero height headers, wherever those come from...maybe rendering null?
    
    The math was also off and didn't handle variable height headers correctly, and it was confusing
    because it was `setNextHeaderY` with the header y _minus it's height_, which only works
    if the prev height was also the same height.
    
    Reviewed By: furdei
    
    Differential Revision: D4649404
    
    fbshipit-source-id: c2c2d438fa0d0b979c2cbdfa5752eaf86c14768b

--- Libraries/Components/ScrollView/ScrollView.js
+++ Libraries/Components/ScrollView/ScrollView.js
@@ -512,9 +512,7 @@ const ScrollView = React.createClass({
     ];
     if (previousHeaderIndex != null) {
       const previousHeader = this._stickyHeaderRefs.get(previousHeaderIndex);
-      previousHeader && previousHeader.setNextHeaderY(
-        event.nativeEvent.layout.y - event.nativeEvent.layout.height,
-      );
+      previousHeader && previousHeader.setNextHeaderY(event.nativeEvent.layout.y);
     }
   },
 
