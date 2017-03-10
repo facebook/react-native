8b7a45b4d7dfa742490162fff4b8ece5d6419439
Nihar <nihar.dongara@copart.com>
2017-03-10 11:46:30 -0600

    Changed normalizePrefix function for Strict mode complaince

--- Libraries/Utilities/UIManager.js
+++ Libraries/Utilities/UIManager.js
@@ -65,7 +65,7 @@ UIManager.takeSnapshot = async function(
  */
 if (Platform.OS === 'ios') {
   // Copied from NativeModules
-  function normalizePrefix(moduleName: string): string {
+  var normalizePrefix = function(moduleName: string): string {
     return moduleName.replace(/^(RCT|RK)/, '');
   }
 
