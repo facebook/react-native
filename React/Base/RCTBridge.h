87d4e96d46cc7926f21d0c51085afb67f4c503f3
Wietse Venema <wvenema@xebia.com>
2017-03-13 14:14:09 +0100

    Changes reference to delegate in RCTBridge to strong from weak

--- React/Base/RCTBridge.h
+++ React/Base/RCTBridge.h
@@ -168,7 +168,7 @@ RCT_EXTERN NSString *RCTBridgeModuleNameForClass(Class bridgeModuleClass);
 /**
  * The delegate provided during the bridge initialization
  */
-@property (nonatomic, weak, readonly) id<RCTBridgeDelegate> delegate;
+@property (nonatomic, strong, readonly) id<RCTBridgeDelegate> delegate;
 
 /**
  * The launch options that were used to initialize the bridge.
