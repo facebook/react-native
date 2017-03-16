9b5bd89134f2196ecbb0f3d09b2ae6e42c333907
Adam Wilson <adam@codegarden.co.uk>
2017-03-16 10:06:32 +0000

    GIve more detail in prop comment

--- Libraries/Components/TextInput/TextInput.js
+++ Libraries/Components/TextInput/TextInput.js
@@ -507,7 +507,10 @@ const TextInput = React.createClass({
     inlineImagePadding: PropTypes.number,
 
     /**
-     * Allow TextInput to pass touch event to parent.
+     * If `true`, allows TextInput to pass touch events to the parent component. 
+     * This allows components such as SwipeableListView to be swipeable from the TextInput on iOS, 
+     * as is the case on Android by default.
+     * If `false`, TextInput always asks to handle the input (except when disabled).
      * @platform ios
      */
     rejectResponderTermination: PropTypes.bool,
