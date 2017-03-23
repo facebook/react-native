9ab29b83392e1e3f28c95b09c292068893273a8e
John Shelley <john.shelley.p@gmail.com>
2017-03-23 14:58:32 -0500

    Update to v4 suffix drawables

--- local-cli/bundle/assetPathUtils.js
+++ local-cli/bundle/assetPathUtils.js
@@ -10,12 +10,12 @@
 
 function getAndroidAssetSuffix(scale) {
   switch (scale) {
-    case 0.75: return 'ldpi';
-    case 1: return 'mdpi';
-    case 1.5: return 'hdpi';
-    case 2: return 'xhdpi';
-    case 3: return 'xxhdpi';
-    case 4: return 'xxxhdpi';
+    case 0.75: return 'ldpi-v4';
+    case 1: return 'mdpi-v4';
+    case 1.5: return 'hdpi-v4';
+    case 2: return 'xhdpi-v4';
+    case 3: return 'xxhdpi-v4';
+    case 4: return 'xxxhdpi-v4';
   }
 }
 
