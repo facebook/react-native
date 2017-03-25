a673f8cfd52dda003762cfe0c5d8fe56c7bc331e
yihanseattle <yihanseattle@gmail.com>
2017-03-24 17:17:24 -0700

    Update AlertDialog import to use SupportLibrary.

--- ReactAndroid/src/main/java/com/facebook/react/modules/dialog/AlertFragment.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/dialog/AlertFragment.java
@@ -11,7 +11,7 @@ package com.facebook.react.modules.dialog;
 
 import javax.annotation.Nullable;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
