9a51fa8e15acc052b5d271c369dfa4b227d657c2
Janic Duplessis <janicduplessis@gmail.com>
2017-04-05 09:07:22 -0700

    Improve z-index implementation on Android
    Summary:
    Use `getChildDrawingOrder` instead of reordering views. The old implementation didn't work properly when `removeClippedSubviews` was enabled and this one should have better performance since we don't play with the view hierarchy at all.
    
    This fixes weird bugs with sticky headers in `SectionList` and allows removing the hack that disabled `removeClippedSubviews` when using sticky section headers.
    
    **Test plan**
    Tested using the SectionList and ListViewPaging examples that use sticky headers which uses z-index.
    Closes https://github.com/facebook/react-native/pull/13105
    
    Reviewed By: sahrens
    
    Differential Revision: D4765869
    
    Pulled By: achen1
    
    fbshipit-source-id: be3c824658a3ce965b6e7324ad95c77cbd8a86ae

--- Libraries/Components/ScrollView/ScrollView.js
+++ Libraries/Components/ScrollView/ScrollView.js
@@ -650,9 +650,7 @@ const ScrollView = React.createClass({
         {...contentSizeChangeProps}
         ref={this._setInnerViewRef}
         style={contentContainerStyle}
-        removeClippedSubviews={
-          hasStickyHeaders && Platform.OS === 'android' ? false : this.props.removeClippedSubviews
-        }
+        removeClippedSubviews={this.props.removeClippedSubviews}
         collapsable={false}>
         {children}
       </ScrollContentContainerViewClass>;
