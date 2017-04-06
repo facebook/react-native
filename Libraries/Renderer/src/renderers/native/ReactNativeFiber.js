e631a6dd36d8e54fbc80e35800c77372ca4ceb97
Brian Vaughn <bvaughn@fb.com>
2017-04-06 09:17:32 -0700

    Removed takeSnapshot util function from ReactNative renderer It was added to react-native-implementation in c7387fe

--- Libraries/Renderer/src/renderers/native/ReactNativeFiber.js
+++ Libraries/Renderer/src/renderers/native/ReactNativeFiber.js
@@ -29,7 +29,6 @@ const deepFreezeAndThrowOnMutationInDev = require('deepFreezeAndThrowOnMutationI
 const emptyObject = require('fbjs/lib/emptyObject');
 const findNodeHandle = require('findNodeHandle');
 const invariant = require('fbjs/lib/invariant');
-const takeSnapshot = require('takeSnapshot');
 
 const {injectInternals} = require('ReactFiberDevToolsHook');
 
@@ -420,8 +419,6 @@ const ReactNative = {
 
     return NativeRenderer.getPublicRootInstance(root);
   },
-
-  takeSnapshot,
 
   unmountComponentAtNode(containerTag: number) {
     const root = roots.get(containerTag);
