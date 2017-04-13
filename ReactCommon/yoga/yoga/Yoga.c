62360e898120836da521e41b0547d199ec0db3d4
Lukas Wöhrl <lukas@woehrl.net>
2017-04-13 08:39:40 -0700

    Let measure behave more like on the web
    Summary:
    Nodes with a measure function needs to be measured even so it seems there is no available space. So it behaves more like on the web. Fix facebook/yoga#488
    Closes https://github.com/facebook/yoga/pull/499
    
    Differential Revision: D4850458
    
    Pulled By: emilsjolander
    
    fbshipit-source-id: be5e35a670ddcbf3cd426fc3c2a0c9b60a874cdc

--- ReactCommon/yoga/yoga/Yoga.c
+++ ReactCommon/yoga/yoga/Yoga.c
@@ -1654,13 +1654,6 @@ static void YGNodeWithMeasureFuncSetMeasuredDimensions(const YGNodeRef node,
         node, YGFlexDirectionRow, availableWidth - marginAxisRow, parentWidth, parentWidth);
     node->layout.measuredDimensions[YGDimensionHeight] = YGNodeBoundAxis(
         node, YGFlexDirectionColumn, availableHeight - marginAxisColumn, parentHeight, parentWidth);
-  } else if (innerWidth <= 0.0f || innerHeight <= 0.0f) {
-    // Don't bother sizing the text if there's no horizontal or vertical
-    // space.
-    node->layout.measuredDimensions[YGDimensionWidth] =
-        YGNodeBoundAxis(node, YGFlexDirectionRow, 0.0f, availableWidth, availableWidth);
-    node->layout.measuredDimensions[YGDimensionHeight] =
-        YGNodeBoundAxis(node, YGFlexDirectionColumn, 0.0f, availableHeight, availableWidth);
   } else {
     // Measure the text under the current constraints.
     const YGSize measuredSize =
