6ffeafab7fa320ad5a003036f1f2733496241035
Héctor Ramos <hramos@users.noreply.github.com>
2017-04-25 18:30:27 -0400

    Update Linking.js

--- Libraries/Linking/Linking.js
+++ Libraries/Linking/Linking.js
@@ -22,10 +22,10 @@ const LinkingManager = Platform.OS === 'android' ?
 
 /**
  * <div class="banner-crna-ejected">
- *   <h3>Native Builds Only</h3>
+ *   <h3>Projects with Native Code Only</h3>
  *   <p>
- *     This API only works in apps made with <code>react-native init</code>
- *     or in those made with Create React Native App which have since ejected. For
+ *     This section only applies to projects made with <code>react-native init</code>
+ *     or to those made with Create React Native App which have since ejected. For
  *     more information about ejecting, please see
  *     the <a href="https://github.com/react-community/create-react-native-app/blob/master/EJECTING.md" target="_blank">guide</a> on
  *     the Create React Native App repository.
