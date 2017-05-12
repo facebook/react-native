3e7c437f259211457e97e13507ef616d49596d2d
juanfe-google <juanfe@google.com>
2017-05-11 17:39:28 -0700

    Add license header to InimplementedViews.js

--- Libraries/Components/UnimplementedViews/UnimplementedView.js
+++ Libraries/Components/UnimplementedViews/UnimplementedView.js
@@ -1,4 +1,13 @@
 /**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/**
  * Common implementation for a simple stubbed view. Simply applies the view's styles to the inner
  * View component and renders its children.
  *
