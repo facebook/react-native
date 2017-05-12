dcb7eb857c608d9011413ae4185341c3f7e2959d
Dominic Gannaway <dg@domgan.com>
2017-05-12 16:30:11 +0100

    monkey patched in changes from pull #12421

--- Libraries/Inspector/InspectorOverlay.js
+++ Libraries/Inspector/InspectorOverlay.js
@@ -12,7 +12,6 @@
 'use strict';
 
 var Dimensions = require('Dimensions');
-var InspectorUtils = require('InspectorUtils');
 var React = require('React');
 var PropTypes = require('prop-types');
 var StyleSheet = require('StyleSheet');
@@ -31,7 +30,7 @@ class InspectorOverlay extends React.Component {
       style?: any,
     },
     inspectedViewTag?: number,
-    onTouchInstance: Function,
+    onTouchViewTag: (tag: number, frame: Object, pointerY: number) => void,
   };
 
   static propTypes = {
@@ -40,7 +39,7 @@ class InspectorOverlay extends React.Component {
       style: PropTypes.any,
     }),
     inspectedViewTag: PropTypes.number,
-    onTouchInstance: PropTypes.func.isRequired,
+    onTouchViewTag: PropTypes.func.isRequired,
   };
 
   findViewForTouchEvent = (e: EventLike) => {
@@ -49,11 +48,7 @@ class InspectorOverlay extends React.Component {
       this.props.inspectedViewTag,
       [locationX, locationY],
       (nativeViewTag, left, top, width, height) => {
-        var instance = InspectorUtils.findInstanceByNativeTag(nativeViewTag);
-        if (!instance) {
-          return;
-        }
-        this.props.onTouchInstance(instance, {left, top, width, height}, locationY);
+        this.props.onTouchViewTag(nativeViewTag, {left, top, width, height}, locationY);
       }
     );
   };
