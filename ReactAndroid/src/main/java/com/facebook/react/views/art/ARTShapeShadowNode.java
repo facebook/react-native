2a47d81cce065cb9a33eaff3092dbf7487cdc9a6
Dmitry Petukhov <petukhov.dmitry@gmail.com>
2017-05-14 17:41:53 +0300

    Apply pixel-density scale to linear gradient coordinates

--- ReactAndroid/src/main/java/com/facebook/react/views/art/ARTShapeShadowNode.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/art/ARTShapeShadowNode.java
@@ -54,7 +54,7 @@ public class ARTShapeShadowNode extends ARTVirtualNode {
 
   protected @Nullable Path mPath;
   private @Nullable float[] mStrokeColor;
-  private @Nullable float[] mFillColor;
+  private @Nullable float[] mBrushData;
   private @Nullable float[] mStrokeDash;
   private float mStrokeWidth = 1;
   private int mStrokeCap = CAP_ROUND;
@@ -81,7 +81,7 @@ public class ARTShapeShadowNode extends ARTVirtualNode {
 
   @ReactProp(name = "fill")
   public void setFill(@Nullable ReadableArray fillColors) {
-    mFillColor = PropHelper.toFloatArray(fillColors);
+    mBrushData = PropHelper.toFloatArray(fillColors);
     markUpdated();
   }
 
@@ -179,43 +179,43 @@ public class ARTShapeShadowNode extends ARTVirtualNode {
    * if the fill should be drawn, {@code false} if not.
    */
   protected boolean setupFillPaint(Paint paint, float opacity) {
-    if (mFillColor != null && mFillColor.length > 0) {
+    if (mBrushData != null && mBrushData.length > 0) {
       paint.reset();
       paint.setFlags(Paint.ANTI_ALIAS_FLAG);
       paint.setStyle(Paint.Style.FILL);
-      int colorType = (int) mFillColor[0];
+      int colorType = (int) mBrushData[0];
       switch (colorType) {
         case COLOR_TYPE_SOLID_COLOR:
           paint.setARGB(
-              (int) (mFillColor.length > 4 ? mFillColor[4] * opacity * 255 : opacity * 255),
-              (int) (mFillColor[1] * 255),
-              (int) (mFillColor[2] * 255),
-              (int) (mFillColor[3] * 255));
+              (int) (mBrushData.length > 4 ? mBrushData[4] * opacity * 255 : opacity * 255),
+              (int) (mBrushData[1] * 255),
+              (int) (mBrushData[2] * 255),
+              (int) (mBrushData[3] * 255));
           break;
         case COLOR_TYPE_LINEAR_GRADIENT:
-          // For mFillColor format refer to LinearGradient and insertColorStopsIntoArray functions in ReactNativeART.js
-          if (mFillColor.length < 5) {
+          // For mBrushData format refer to LinearGradient and insertColorStopsIntoArray functions in ReactNativeART.js
+          if (mBrushData.length < 5) {
             FLog.w(ReactConstants.TAG, 
               "[ARTShapeShadowNode setupFillPaint] expects 5 elements, received " 
-              + mFillColor.length);
+              + mBrushData.length);
             return false;
           }
-          float gradientStartX = mFillColor[1];
-          float gradientStartY = mFillColor[2];
-          float gradientEndX = mFillColor[3];
-          float gradientEndY = mFillColor[4];
-          int stops = (mFillColor.length - 5) / 5;
+          float gradientStartX = mBrushData[1] * mScale;
+          float gradientStartY = mBrushData[2] * mScale;
+          float gradientEndX = mBrushData[3] * mScale;
+          float gradientEndY = mBrushData[4] * mScale;
+          int stops = (mBrushData.length - 5) / 5;
           int[] colors = null;
           float[] positions = null;
           if (stops > 0) {
             colors = new int[stops];
             positions = new float[stops];
             for (int i=0; i<stops; i++) {
-              positions[i] = mFillColor[5 + 4*stops + i];
-              int r = (int) (255 * mFillColor[5 + 4*i + 0]);
-              int g = (int) (255 * mFillColor[5 + 4*i + 1]);
-              int b = (int) (255 * mFillColor[5 + 4*i + 2]);
-              int a = (int) (255 * mFillColor[5 + 4*i + 3]);
+              positions[i] = mBrushData[5 + 4*stops + i];
+              int r = (int) (255 * mBrushData[5 + 4*i + 0]);
+              int g = (int) (255 * mBrushData[5 + 4*i + 1]);
+              int b = (int) (255 * mBrushData[5 + 4*i + 2]);
+              int a = (int) (255 * mBrushData[5 + 4*i + 3]);
               colors[i] = Color.argb(a, r, g, b);
             }
           }
