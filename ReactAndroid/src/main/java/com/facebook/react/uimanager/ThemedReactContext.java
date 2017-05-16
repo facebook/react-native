0fe43392f2a53c87194820e0502791deedfc9ad3
Saleel Salam <saleel@boxit.co>
2017-05-16 17:08:53 +0400

    Revert "Merge branch 'webview_file_input' of github.com:farazs/react-native"
    This reverts commit 934b052c4239754385b82e332c5b1f01551595fe, reversing
    changes made to 9c1d99c7b2df728b904327a92ce238d9cc8b1f80.

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/ThemedReactContext.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/ThemedReactContext.java
@@ -14,7 +14,6 @@ import javax.annotation.Nullable;
 import android.app.Activity;
 import android.content.Context;
 
-import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.LifecycleEventListener;
@@ -47,16 +46,6 @@ public class ThemedReactContext extends ReactContext {
   @Override
   public void removeLifecycleEventListener(LifecycleEventListener listener) {
     mReactApplicationContext.removeLifecycleEventListener(listener);
-  }
-
-  @Override
-  public void addActivityEventListener(ActivityEventListener listener) {
-    mReactApplicationContext.addActivityEventListener(listener);
-  }
-
-  @Override
-  public void removeActivityEventListener(ActivityEventListener listener) {
-    mReactApplicationContext.removeActivityEventListener(listener);
   }
 
   @Override
