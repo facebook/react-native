e94b0cc9799c19ba2a9a8be3d57f4b5ded2c4238
Daniel Cestari <dcestari@gmail.com>
2017-05-19 09:51:50 -0700

    Make "nativePerformanceNow" use same clock as "SystemClock.elapsedRealtime()"
    CLOCK_MONOTONIC has an unspecified start time, whereas CLOCK_BOOTTIME
    starts at boot time, same as "SystemClock.elapsedRealtime()"
    
    By using the same start point, it's possible to record a start time in Java
    and a stop time in JavaScript (or vice-versa), to measure performance across
    the bridge
    
    Moreover, Android's SystemClock documentation says:
    
    - uptimeMillis() "[...]. This clock stops when the system enters deep sleep"
    - elapsedRealtime() "[...] include[s] deep sleep"
    
    "uptimeMillis" should never be used for measuring performance, instead
    "elapsedRealtime" will be more accurate.

--- ReactAndroid/src/main/jni/xreact/jni/OnLoad.cpp
+++ ReactAndroid/src/main/jni/xreact/jni/OnLoad.cpp
@@ -107,15 +107,15 @@ static JSValueRef nativePerformanceNow(
   static const int64_t NANOSECONDS_IN_SECOND = 1000000000LL;
   static const int64_t NANOSECONDS_IN_MILLISECOND = 1000000LL;
 
-  // Since SystemClock.uptimeMillis() is commonly used for performance measurement in Java
-  // and uptimeMillis() internally uses clock_gettime(CLOCK_MONOTONIC),
+  // Since SystemClock.elapsedRealtime() is commonly used for performance measurement in Java
+  // and elapsedRealtime() internally uses clock_gettime(CLOCK_BOOTTIME),
   // we use the same API here.
   // We need that to make sure we use the same time system on both JS and Java sides.
   // Links to the source code:
   // https://android.googlesource.com/platform/frameworks/native/+/jb-mr1-release/libs/utils/SystemClock.cpp
   // https://android.googlesource.com/platform/system/core/+/master/libutils/Timers.cpp
   struct timespec now;
-  clock_gettime(CLOCK_MONOTONIC, &now);
+  clock_gettime(CLOCK_BOOTTIME, &now);
   int64_t nano = now.tv_sec * NANOSECONDS_IN_SECOND + now.tv_nsec;
   return Value::makeNumber(ctx, (nano / (double)NANOSECONDS_IN_MILLISECOND));
 }
