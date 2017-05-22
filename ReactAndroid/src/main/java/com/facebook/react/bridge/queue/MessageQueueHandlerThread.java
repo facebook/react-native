b9a2e0e003a7a798c4cafa4abc291cc6a4f3073e
xiatian [夏天] <xiatian@xiaomi.com>
2017-05-22 17:44:21 +0800

    MessageQueueThread startNewBackgroundThread more effective

--- ReactAndroid/src/main/java/com/facebook/react/bridge/queue/MessageQueueHandlerThread.java
+++ ReactAndroid/src/main/java/com/facebook/react/bridge/queue/MessageQueueHandlerThread.java
@@ -1,19 +1,24 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 package com.facebook.react.bridge.queue;
 
 import android.os.Looper;
 
-/**
- * Created by xt on 2017/5/22.
- */
-
-public class MessageQueueHandlerThread extends Thread {
+class MessageQueueHandlerThread extends Thread {
   private Looper mLooper;
 
-  public MessageQueueHandlerThread(String name, long stackSize) {
+  protected MessageQueueHandlerThread(String name, long stackSize) {
     super(null, null, name, stackSize);
   }
 
-  protected void onLooperPrepared() {
+  private void onLooperPrepared() {
   }
 
   @Override
