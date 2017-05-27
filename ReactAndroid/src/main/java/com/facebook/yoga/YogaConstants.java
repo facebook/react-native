826e3c9eeab93f3fdb6c2a9cb827ca9555d5d44a
ransj <ranshengju@gmail.com>
2017-05-28 06:04:51 +0800

    replace Float.compare(value, UNDEFINED) == 0 with Float.isNaN to improve performance

--- ReactAndroid/src/main/java/com/facebook/yoga/YogaConstants.java
+++ ReactAndroid/src/main/java/com/facebook/yoga/YogaConstants.java
@@ -14,7 +14,7 @@ public class YogaConstants {
   public static final float UNDEFINED = Float.NaN;
 
   public static boolean isUndefined(float value) {
-    return Float.compare(value, UNDEFINED) == 0;
+    return Float.isNaN(value);
   }
 
   public static boolean isUndefined(YogaValue value) {
