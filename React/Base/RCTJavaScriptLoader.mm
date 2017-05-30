427a13f268301d14fe1f52e38dd007af42a0c8f5
Héctor Ramos <hramos@users.noreply.github.com>
2017-05-30 12:25:22 -0700

    Update RCTJavaScriptLoader.mm

--- React/Base/RCTJavaScriptLoader.mm
+++ React/Base/RCTJavaScriptLoader.mm
@@ -219,7 +219,7 @@ static void attemptAsynchronousLoadOfBundleAtURL(NSURL *scriptURL, RCTSourceLoad
                       "Ensure the following:\n"
                       "- Node server is running and available on the same network - run 'npm start' from react-native root\n"
                       "- Node server URL is correctly set in AppDelegate\n"
-                      "- WiFi is enabled\n\n"
+                      "- WiFi is enabled and connected to the same network as the Node Server\n\n"
                       "URL: " stringByAppendingString:scriptURL.absoluteString],
                    NSLocalizedFailureReasonErrorKey: error.localizedDescription,
                    NSUnderlyingErrorKey: error,
