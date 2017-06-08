625a8cfbedee2b2d1d7149f2bedec2f1625c304b
Jonathan Ballerano <jballer@fb.com>
2017-06-08 15:07:48 -0700

    Add a type cast to RCT_ENUM_CONVERTER for C++ compatibility

--- React/Base/RCTConvert.h
+++ React/Base/RCTConvert.h
@@ -218,7 +218,7 @@ RCT_CUSTOM_CONVERTER(type, type, [RCT_DEBUG ? [self NSNumber:json] : json getter
   dispatch_once(&onceToken, ^{                            \
     mapping = values;                                     \
   });                                                     \
-  return [RCTConvertEnumValue(#type, mapping, @(default), json) getter]; \
+  return (type)[RCTConvertEnumValue(#type, mapping, @(default), json) getter]; \
 }
 
 /**
@@ -233,7 +233,7 @@ RCT_CUSTOM_CONVERTER(type, type, [RCT_DEBUG ? [self NSNumber:json] : json getter
   dispatch_once(&onceToken, ^{                            \
     mapping = values;                                     \
   });                                                     \
-  return [RCTConvertMultiEnumValue(#type, mapping, @(default), json) getter]; \
+  return (type)[RCTConvertMultiEnumValue(#type, mapping, @(default), json) getter]; \
 }
 
 /**
