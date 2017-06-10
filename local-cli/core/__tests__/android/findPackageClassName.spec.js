223eab930b4e36b6d36de1db23c17d09f2603c99
Tim Yung <yungsters@fb.com>
2017-06-09 23:56:03 -0700

    RN: Cleanup `local-cli/core/__tests__`
    Reviewed By: raluca-elena
    
    Differential Revision: D5224347
    
    fbshipit-source-id: 99a729c49bec28bf89d9dc91530958beec878828

--- local-cli/core/__tests__/android/findPackageClassName.spec.js
+++ local-cli/core/__tests__/android/findPackageClassName.spec.js
@@ -1,25 +1,42 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @format
+ */
+
+'use strict';
+
 jest.autoMockOff();
 
+const mockFS = require('mock-fs');
+
 const findPackageClassName = require('../../android/findPackageClassName');
-const mockFs = require('mock-fs');
 const mocks = require('../../__fixtures__/android');
 
 describe('android::findPackageClassName', () => {
+  beforeAll(() => {
+    mockFS({
+      empty: {},
+      flat: {
+        android: mocks.valid,
+      },
+    });
+  });
 
-  beforeAll(() => mockFs({
-    empty: {},
-    flat: {
-      android: mocks.valid,
-    },
-  }));
-
-  it('should return manifest content if file exists in the folder', () => {
+  it('returns manifest content if file exists in the folder', () => {
     expect(typeof findPackageClassName('flat')).toBe('string');
   });
 
-  it('should return `null` if there\'s no matches', () => {
-    expect(findPackageClassName('empty')).toBe(null);
+  it('returns `null` if there are no matches', () => {
+    expect(findPackageClassName('empty')).toBeNull();
   });
 
-  afterAll(mockFs.restore);
+  afterAll(() => {
+    mockFS.restore();
+  });
 });
