206765acb84801f0f8174ade331da0cac0bfa7ab
Janic Duplessis <janicduplessis@gmail.com>
2017-06-21 06:37:20 -0700

    Fix configure glog script when building from xcodebuild
    Summary:
    I encountered an issue when building with fastlane gym / xcodebuild where glog would not build because of missing config.h header file. I tracked it down to the ios-configure-glog.sh script that ended up error-ing because of missing valid c compiler. I guess it didn't enter the if to set c compiler env in xcodebuild and that env doesn't have proper values set like it does in xcode so just removing this check fixed it. Also tested that it still works properly in xcode.
    Closes https://github.com/facebook/react-native/pull/14267
    
    Differential Revision: D5285691
    
    Pulled By: javache
    
    fbshipit-source-id: df5315926c2d2d78806618df3d9c9bbbb974d1ea

--- scripts/ios-configure-glog.sh
+++ scripts/ios-configure-glog.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Only set when not running in an Xcode context
-if [ -z "$ACTION" ] || [ -z "$BUILD_DIR" ]; then
-  export CC="$(xcrun -find -sdk iphoneos cc) -arch armv7 -isysroot $(xcrun -sdk iphoneos --show-sdk-path)"
-  export CXX="$CC"
-fi
+PLATFORM_NAME="${PLATFORM_NAME:-iphoneos}"
+CURRENT_ARCH="${CURRENT_ARCH:-armv7}"
+
+export CC="$(xcrun -find -sdk $PLATFORM_NAME cc) -arch $CURRENT_ARCH -isysroot $(xcrun -sdk $PLATFORM_NAME --show-sdk-path)"
+export CXX="$CC"
 
 ./configure --host arm-apple-darwin
 
