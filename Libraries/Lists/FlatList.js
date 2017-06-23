6e681b0b33b85a9029edc60513b1693bd61d40c5
Júlio César <julio@julioc.me>
2017-06-23 15:38:58 -0300

    Fixed FlatList._getItemCount returning NaN
    If FlatList data prop is set to a non-array object, this method return `NaN` causing a infinite loop down the road

--- Libraries/Lists/FlatList.js
+++ Libraries/Lists/FlatList.js
@@ -467,7 +467,7 @@ class FlatList<ItemT>
   };
 
   _getItemCount = (data: ?Array<ItemT>): number => {
-    return data ? Math.ceil(data.length / this.props.numColumns) : 0;
+    return (data && data.length) ? Math.ceil(data.length / this.props.numColumns) : 0;
   };
 
   _keyExtractor = (items: ItemT | Array<ItemT>, index: number) => {
