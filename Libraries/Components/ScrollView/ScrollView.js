25c46215cff20123aa3ec349d45e394eac6a7e23
Sean Wang <seanyusa@users.noreply.github.com>
2017-07-05 16:31:35 -0500

    Clarity in pagingEnabled description.
    Current description is misleading to think that both horizontal and vertical pagination are supported on both platforms.

--- Libraries/Components/ScrollView/ScrollView.js
+++ Libraries/Components/ScrollView/ScrollView.js
@@ -257,6 +257,8 @@ const ScrollView = React.createClass({
      * When true, the scroll view stops on multiples of the scroll view's size
      * when scrolling. This can be used for horizontal pagination. The default
      * value is false.
+     *
+     * Note: Vertical pagination is not supported on Android.
      */
     pagingEnabled: PropTypes.bool,
     /**
