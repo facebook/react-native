7049f0d9771f612199523b4922eb593b420fc857
Miron Pawlik <miron.pawlik@swmansion.com>
2017-07-10 11:22:02 +0200

    Add regex to find function header in Podfile

--- local-cli/link/pods/addPodEntry.js
+++ local-cli/link/pods/addPodEntry.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function addPodEntry(podLines, lineToAddEntry, podName) {
-  podLines.splice(lineToAddEntry, 0, `  pod '${podName}', :path => '../node_modules/${podName}'`)
+  const newEntry = `  pod '${podName}', :path => '../node_modules/${podName}'\n`;
+
+  podLines.splice(lineToAddEntry, 0, newEntry)
 };
--- local-cli/link/pods/findLineToAddPod.js
+++ local-cli/link/pods/findLineToAddPod.js
@@ -2,8 +2,12 @@
 
 module.exports = function findLineToAddPod(podLines, firstTargetLine){
 
+  const nextTarget = /target \'\w+\' do/g;
+  const endOfCurrentTarget = /^\s*end\s*$/g;
+  const functionDefinition = /^\s*[a-z_]+\s+do(\s+\|[a-z]+\|)?/g;
+
   for (let i = firstTargetLine, len = podLines.length; i < len; i++) {
-    const match = podLines[i].match(/target \'\w+\' do/g) || podLines[i].match(/^\s*end\s*$/g);
+    const match = podLines[i].match(nextTarget) || podLines[i].match(endOfCurrentTarget) || podLines[i].match(functionDefinition);
     if (match) {
       return i;
     }
