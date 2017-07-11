1a2633f73e6a04908ae511535d3b73ce2fc246b5
Taylor Kline <taylor.kline@streamingfreebies.com>
2017-07-11 17:32:26 -0500

    Fix spacing to two spaces

--- Libraries/Lists/FlatList.js
+++ Libraries/Lists/FlatList.js
@@ -236,57 +236,57 @@ type DefaultProps = typeof defaultProps;
  *
  *
  *     class MyListItem extends React.PureComponent {
- *         _onPress = () => {
- *             this.props.onPressItem(this.props.id);
- *         };
+ *       _onPress = () => {
+ *         this.props.onPressItem(this.props.id);
+ *       };
  *
- *         render() {
- *             const textColor = this.props.selected ? "red" : "black";
- *             return (
- *                 <TouchableOpacity onPress={this._onPress}>
- *                     <View>
- *                         <Text style={{ color: textColor }}>
- *                             {this.props.title}
- *                         </Text>
- *                     </View>
- *                 </TouchableOpacity>
- *             );
- *         }
+ *       render() {
+ *         const textColor = this.props.selected ? "red" : "black";
+ *         return (
+ *           <TouchableOpacity onPress={this._onPress}>
+ *             <View>
+ *               <Text style={{ color: textColor }}>
+ *                 {this.props.title}
+ *               </Text>
+ *             </View>
+ *           </TouchableOpacity>
+ *         );
+ *       }
  *     }
  *
  *     class MultiSelectList extends React.PureComponent {
- *         state = { selected: (new Map(): Map<string, boolean>) };
+ *       state = { selected: (new Map(): Map<string, boolean>) };
  *
- *         _keyExtractor = (item, index) => item.id;
+ *       _keyExtractor = (item, index) => item.id;
  *
- *         _onPressItem = (id: string) => {
- *             // updater functions are preferred for transactional updates
- *             this.setState(state => {
- *                 // copy the map rather than modifying state.
- *                 const selected = new Map(state.selected);
- *                 selected.set(id, !selected.get(id)); // toggle
- *                 return { selected };
- *             });
- *         };
+ *       _onPressItem = (id: string) => {
+ *         // updater functions are preferred for transactional updates
+ *         this.setState(state => {
+ *           // copy the map rather than modifying state.
+ *           const selected = new Map(state.selected);
+ *           selected.set(id, !selected.get(id)); // toggle
+ *           return { selected };
+ *         });
+ *       };
  *
- *         _renderItem = ({ item }) =>
- *             <MyListItem
- *                 id={item.id}
- *                 onPressItem={this._onPressItem}
- *                 selected={!!this.state.selected.get(item.id)}
- *                 title={item.title}
- *             />;
+ *       _renderItem = ({ item }) =>
+ *         <MyListItem
+ *           id={item.id}
+ *           onPressItem={this._onPressItem}
+ *           selected={!!this.state.selected.get(item.id)}
+ *           title={item.title}
+ *         />;
  *
- *         render() {
- *             return (
- *                 <FlatList
- *                     data={this.props.data}
- *                     extraData={this.state}
- *                     keyExtractor={this._keyExtractor}
- *                     renderItem={this._renderItem}
- *                 />
- *             );
- *         }
+ *       render() {
+ *         return (
+ *           <FlatList
+ *             data={this.props.data}
+ *             extraData={this.state}
+ *             keyExtractor={this._keyExtractor}
+ *             renderItem={this._renderItem}
+ *           />
+ *         );
+ *       }
  *     }
  *
  * This is a convenience wrapper around [`<VirtualizedList>`](docs/virtualizedlist.html),
