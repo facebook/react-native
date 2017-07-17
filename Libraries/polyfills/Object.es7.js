7a4eda2f70aea5bcbd46d295b873788a100c18bb
Miguel Jimenez Esun <mjimenez@fb.com>
2017-07-17 03:02:14 -0700

    Remove default polyfills from metro-bundler
    Reviewed By: davidaurelio
    
    Differential Revision: D5423673
    
    fbshipit-source-id: a66655cd72d56eb60a8a79a298ebfbc746b5ad10

--- Libraries/polyfills/Object.es7.js
+++ Libraries/polyfills/Object.es7.js
@@ -6,7 +6,9 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
+ * @providesModule Object.es7
  * @polyfill
+ * @nolint
  */
 
 (function() {
