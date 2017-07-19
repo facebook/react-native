737abe3b7603e27a1a88498b3a260d67fa747ee7
Miguel Jimenez Esun <mjimenez@fb.com>
2017-07-19 11:54:16 -0700

    Revert D5388655: BREAKING: Add regenerator-runtime on demand, based on the files
    Differential Revision: D5388655
    
    fbshipit-source-id: 2f92d6ae69f4772195aeca7493f53209388b3ad0

--- RNTester/js/ListExampleShared.js
+++ RNTester/js/ListExampleShared.js
@@ -163,11 +163,11 @@ const THUMB_URLS = [
   require('./Thumbnails/victory.png'),
 ];
 
-const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, ius ad pertinax oportere accommodare, an vix ' +
-  'civibus corrumpit referrentur. Te nam case ludus inciderint, te mea facilisi adipiscing. Sea id ' +
-  'integre luptatum. In tota sale consequuntur nec. Erat ocurreret mei ei. Eu paulo sapientem ' +
-  'vulputate est, vel an accusam intellegam interesset. Nam eu stet pericula reprimique, ea vim illud ' +
-  'modus, putant invidunt reprehendunt ne qui.';
+const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, ius ad pertinax oportere accommodare, an vix \
+civibus corrumpit referrentur. Te nam case ludus inciderint, te mea facilisi adipiscing. Sea id \
+integre luptatum. In tota sale consequuntur nec. Erat ocurreret mei ei. Eu paulo sapientem \
+vulputate est, vel an accusam intellegam interesset. Nam eu stet pericula reprimique, ea vim illud \
+modus, putant invidunt reprehendunt ne qui.';
 
 /* eslint no-bitwise: 0 */
 function hashCode(str: string): number {
