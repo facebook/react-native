7bd10920686ff01814b5ecb40fb0bb361ca71831
Simon Mitchell <simon@3sidedcube.com>
2017-07-26 10:05:12 +0100

    Fixes copy pasta

--- Libraries/PushNotificationIOS/PushNotificationIOS.js
+++ Libraries/PushNotificationIOS/PushNotificationIOS.js
@@ -507,7 +507,7 @@ class PushNotificationIOS {
           // Fallbacks for new iOS 10 notification format which returns in the same format as local notifications
           this._alert = notifVal.alert || notifVal.alertBody;
           this._sound = notifVal.sound || notifVal.soundName;
-          this._badgeCount = notifVal.badge || nativeNotif.applicationIconBadgeNumber;
+          this._badgeCount = notifVal.badge || notifVal.applicationIconBadgeNumber;
           this._title = notifVal.alertTitle;
           this._subtitle = notifVal.alertSubtitle;
           this._category = notifVal.category;
