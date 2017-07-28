8617e04893b44144443a3e224d1876adc0dfa336
Douglas Lowder <dlowder@salesforce.com>
2017-07-28 16:31:36 -0700

    Fix providesModule annotation in TVEventHandlerExample

--- RNTester/js/TVEventHandlerExample.js
+++ RNTester/js/TVEventHandlerExample.js
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  *
  * @flow
- * @providesModule AlertIOSExample
+ * @providesModule TVEventHandlerExample
  */
 'use strict';
 
