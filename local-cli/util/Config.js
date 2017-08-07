782453d35edbce4203b29270ab3c3056600a4876
David Aurelio <davidaurelio@fb.com>
2017-08-07 06:38:32 -0700

    Fix indentation
    Summary: Fixes the indentation of a line.
    
    Reviewed By: cpojer
    
    Differential Revision: D5573550
    
    fbshipit-source-id: 4ae095b3d14d1dd4e94ba4035c5624ddbd48f505

--- local-cli/util/Config.js
+++ local-cli/util/Config.js
@@ -60,7 +60,7 @@ export type ConfigT = {
    * Specify any additional polyfill modules that should be processed
    * before regular module loading.
    */
- getPolyfillModuleNames: () => Array<string>,
+  getPolyfillModuleNames: () => Array<string>,
 
   /**
    * Specify any additional platforms to be used by the packager.
