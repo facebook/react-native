527d30853d1bc4b5137272e719a3ea558188348a
Faisal Alqadi <fai@everalbum.com>
2017-08-09 14:32:44 -0700

    Add import for Rect class

--- ReactAndroid/src/main/java/com/facebook/react/modules/camera/ImageEditingManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/camera/ImageEditingManager.java
@@ -31,6 +31,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapRegionDecoder;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
