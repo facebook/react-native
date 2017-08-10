4193b81f8627815fb739b109bd8a14f61cd48d27
Chris Bianca <me@chrisbianca.com>
2017-08-10 16:57:39 +0100

    Support custom android buildPatch with react-native link

--- local-cli/link/__tests__/android/makeBuildPatch.spec.js
+++ local-cli/link/__tests__/android/makeBuildPatch.spec.js
@@ -3,15 +3,27 @@
 const makeBuildPatch = require('../../android/patches/makeBuildPatch');
 const name = 'test';
 
+const buildPatch = 'import some.example.project';
+
 describe('makeBuildPatch', () => {
-  it('should build a patch function', () => {
+  it('should build a patch function (no custom buildPatch)', () => {
     expect(Object.prototype.toString(makeBuildPatch(name)))
       .toBe('[object Object]');
   });
 
-  it('should make a correct patch', () => {
+  it('should make a correct patch (no custom buildPatch)', () => {
     const {patch} = makeBuildPatch(name);
     expect(patch).toBe(`    compile project(':${name}')\n`);
+  });
+
+  it('should build a patch function (custom buildPatch)', () => {
+    expect(Object.prototype.toString(makeBuildPatch(name, buildPatch)))
+      .toBe('[object Object]');
+  });
+
+  it('should make a correct patch (custom buildPatch)', () => {
+    const {patch} = makeBuildPatch(name, buildPatch);
+    expect(patch).toBe(buildPatch);
   });
 
   it('should make a correct install check pattern', () => {
