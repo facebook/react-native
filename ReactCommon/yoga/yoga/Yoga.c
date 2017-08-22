f0f25c57cae6301a5244a7cb6a98a46390f5718d
Scott Wolchok <swolchok@fb.com>
2017-08-22 11:54:13 -0700

    Make gYGNodeDefaults const
    Reviewed By: emilsjolander
    
    Differential Revision: D5675518
    
    fbshipit-source-id: 72a6e208263dde0b6bb46a78fedb2796d0e0a600

--- ReactCommon/yoga/yoga/Yoga.c
+++ ReactCommon/yoga/yoga/Yoga.c
@@ -156,7 +156,7 @@ static const float kDefaultFlexGrow = 0.0f;
 static const float kDefaultFlexShrink = 0.0f;
 static const float kWebDefaultFlexShrink = 1.0f;
 
-static YGNode gYGNodeDefaults = {
+static const YGNode gYGNodeDefaults = {
     .parent = NULL,
     .children = NULL,
     .hasNewLayout = true,
