f2ee74041ff1efb81ee9317c93b518bfefc08e3e
Martin Yrjölä <martin.yrjola@gmail.com>
2017-08-23 06:57:52 +0200

    Rename uri to testUri

--- jest/assetFileTransformer.js
+++ jest/assetFileTransformer.js
@@ -16,10 +16,11 @@ const createCacheKeyFunction = require('fbjs-scripts/jest/createCacheKeyFunction
 module.exports = {
   // Mocks asset requires to return the filename. Makes it possible to test that
   // the correct images are loaded for components. Essentially
-  // require('img1.png') becomes `Object { "uri": 'path/to/img1.png' }` in the
-  // Jest snapshot.
+  // require('img1.png') becomes `Object { "testUri": 'path/to/img1.png' }` in
+  // the Jest snapshot.
   process: (_, filename) =>
-    `module.exports = {uri: ${JSON.stringify(path.relative('.', filename))}};`,
-
+    `module.exports = {
+      testUri: ${JSON.stringify(path.relative('.', filename))}
+    };`,
   getCacheKey: createCacheKeyFunction([__filename]),
 };
