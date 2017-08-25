128a856d3e4ed23682014e5461431836bb80587e
Jan Kassens <jkassens@fb.com>
2017-08-25 10:47:50 -0700

    fix missing @providesModule
    Reviewed By: fkgozali
    
    Differential Revision: D5707577
    
    fbshipit-source-id: 5506d1b4a13d44a61530da824c2b0c646ee3287c

--- RNTester/js/TabBarIOSBarStyleExample.js
+++ RNTester/js/TabBarIOSBarStyleExample.js
@@ -18,8 +18,10 @@
  * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
+ * @providesModule TabBarIOSBarStyleExample
  * @flow
  */
+
 'use strict';
 
 var React = require('react');
