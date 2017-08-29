b8d347d113036e570283f1b08c014ae27a857a42
Alex <alexdar94@gmail.com>
2017-08-29 00:44:03 -0700

    Update AnimatedImplementation.js
    Summary:
    Typo at line 513. "The simplest workflow for creating an animation is TO TO create an"
    
    <!--
    Thank you for sending the PR! We appreciate you spending the time to work on these changes.
    
    Help us understand your motivation by explaining why you decided to make this change.
    
    You can learn more about contributing to React Native here: http://facebook.github.io/react-native/docs/contributing.html
    
    Happy contributing!
    
    -->
    
    (Write your motivation here.)
    
    (Write your test plan here. If you changed any code, please provide us with clear instructions on how you verified your changes work. Bonus points for screenshots and videos!)
    Closes https://github.com/facebook/react-native/pull/15676
    
    Differential Revision: D5719554
    
    Pulled By: hramos
    
    fbshipit-source-id: ccb220136dfbc4632c84ec58a13d95a03c864c2b

--- Libraries/Animated/src/AnimatedImplementation.js
+++ Libraries/Animated/src/AnimatedImplementation.js
@@ -510,7 +510,7 @@ const event = function(argMapping: Array<?Mapping>, config?: EventConfig): any {
  * between inputs and outputs, with configurable transforms in between, and
  * simple `start`/`stop` methods to control time-based animation execution.
  *
- * The simplest workflow for creating an animation is to to create an
+ * The simplest workflow for creating an animation is to create an
  * `Animated.Value`, hook it up to one or more style attributes of an animated
  * component, and then drive updates via animations using `Animated.timing()`:
  *
