61800414cbd23b97172ee13a5ff602285cd4af1b
Tikhon Botchkarev <tikhon.b@sereth.com>
2017-09-05 11:42:35 -0700

    Reference PermissionsAndroid in Geolocation doc
    Summary:
    Current Geolocation API docs do not mention using PermissionAndroid when working with API 23+. This updates the docs to reflect that requirement.
    
    This a documentation change, so no testing is required.
    Closes https://github.com/facebook/react-native/pull/14133
    
    Differential Revision: D5767852
    
    Pulled By: hramos
    
    fbshipit-source-id: 25af70db864a50cdc5e3765eccdeecd49c084d9a

--- Libraries/Geolocation/Geolocation.js
+++ Libraries/Geolocation/Geolocation.js
@@ -76,6 +76,12 @@ type GeoOptions = {
  * Android API >= 18 Positions will also contain a `mocked` boolean to indicate if position
  * was created from a mock provider.
  *
+ * <p>
+ *   Android API >= 23 Requires an additional step to check for, and request
+ *   the ACCESS_FINE_LOCATION permission using
+ *   the <a href="https://facebook.github.io/react-native/docs/permissionsandroid.html" target="_blank">PermissionsAndroid API</a>.
+ *   Failure to do so may result in a hard crash.
+ * </p>
  */
 var Geolocation = {
 
