f833f7d9baf0d66b7c1f941af6bf75cd0785f310
Neo <nihgwu@live.com>
2017-09-06 20:44:50 +0800

    remove indent

--- React/Views/RCTFont.mm
+++ React/Views/RCTFont.mm
@@ -61,7 +61,7 @@ static RCTFontWeight weightOfFont(UIFont *font)
                     @(UIFontWeightHeavy),
                     @(UIFontWeightBlack)];
   });
-  
+
   for (NSUInteger i = 0; i < fontNames.count; i++) {
     if ([font.fontName.lowercaseString hasSuffix:fontNames[i]]) {
       return [fontWeights[i] doubleValue];
