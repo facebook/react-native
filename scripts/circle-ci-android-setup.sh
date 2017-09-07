e54b3b55047aa3470c9837e042b783c84ed0729b
Hector Ramos <hramos@fb.com>
2017-09-07 13:30:35 -0700

    eslint-bot, rename step

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -29,8 +29,9 @@ function getAndroidNDK {
 
   if [ ! -e $DEPS ]; then
     cd $NDK_HOME
-    curl -o ndk.zip --silent https://dl.google.com/android/repository/android-ndk-r10e-linux-x86.zip
-    curl -o ndk_64.zip --silent https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip
+    echo "Downloading NDK..."
+    curl -o ndk.zip https://dl.google.com/android/repository/android-ndk-r10e-linux-x86.zip
+    curl -o ndk_64.zip https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip
     unzip -o -q ndk.zip
     unzip -o -q ndk_64.zip
     rm ndk.zip
