5b13f5acc511e039a861ee805e0e7d37748987d8
Hector Ramos <hramos@fb.com>
2017-09-08 10:49:55 -0700

    Explicitly install various Android packages, remove NDK 32-bit install (unnecessary)

--- scripts/circle-ci-android-setup.sh
+++ scripts/circle-ci-android-setup.sh
@@ -1,26 +1,46 @@
 # inspired by https://github.com/Originate/guide/blob/master/android/guide/Continuous%20Integration.md
 
+# SDK Built Tools revision, per http://facebook.github.io/react-native/docs/getting-started.html
+ANDROID_SDK_BUILD_TOOLS_REVISION=23.0.1
+# API Level we build with
+ANDROID_SDK_BUILD_API_LEVEL="23"
+# Minimum API Level we target, used for emulator image
+ANDROID_SDK_TARGET_API_LEVEL="19"
+# Emulator name
+AVD_NAME="testAVD"
+
 function getAndroidSDK {
   export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
   DEPS="$ANDROID_HOME/installed-dependencies"
 
   if [ ! -e $DEPS ]; then
-    echo "Updating installed packages..."
-    sdkmanager --update
-    echo "Installing SDKs..."
-    sdkmanager "system-images;android-23;google_apis;armeabi-v7a"
-    echo "Installing build tools..."
-    sdkmanager "build-tools;23.0.1"
-    echo "Installing add-ons..."
-    sdkmanager "add-ons;addon-google_apis-google-23"
+    echo "Installing Android API level $ANDROID_SDK_TARGET_API_LEVEL, Google APIs, ARM EABI v7a system image..."
+    sdkmanager "system-images;android-$ANDROID_SDK_TARGET_API_LEVEL;google_apis;armeabi-v7a"
+# x86 image requires hardware acceleration, which is not supported when running within the CircleCI Docker image
+#    echo "Installing Android API level $ANDROID_SDK_TARGET_API_LEVEL, Google APIs, Intel x86 Atom system image..."
+#    sdkmanager "system-images;android-$ANDROID_SDK_TARGET_API_LEVEL;google_apis;x86"
+    echo "Installing SDK platform, Android API level $ANDROID_SDK_BUILD_API_LEVEL..."
+    sdkmanager "platforms;android-$ANDROID_SDK_BUILD_API_LEVEL"
+    echo "Installing SDK platform, android-$ANDROID_SDK_TARGET_API_LEVEL..."
+    sdkmanager "platforms;android-$ANDROID_SDK_TARGET_API_LEVEL"
+    echo "Installing SDK build tools, revision $ANDROID_SDK_BUILD_TOOLS_REVISION..."
+    sdkmanager "build-tools;$ANDROID_SDK_BUILD_TOOLS_REVISION"
+    echo "Installing Google APIs, Android API level $ANDROID_SDK_BUILD_API_LEVEL..."
+    sdkmanager "add-ons;addon-google_apis-google-$ANDROID_SDK_BUILD_API_LEVEL"
+    echo "Installing Android Support Repository"
+    sdkmanager "extras;android;m2repository"
     touch $DEPS
   fi
 }
 
 function createAVD {
-  # The package used here should match the package installed in getAndroidSDK
-  echo no | avdmanager create avd --name testAVD --force --package "system-images;android-23;google_apis;armeabi-v7a" --tag google_apis --abi armeabi-v7a
+  echo no | avdmanager create avd --name $AVD_NAME --force --package "system-images;android-$ANDROID_SDK_TARGET_API_LEVEL;google_apis;armeabi-v7a" --tag google_apis --abi armeabi-v7a
+}
+
+function launchAVD {
+  # The AVD name here should match the one created in createAVD
+  emulator64-arm -avd $AVD_NAME -no-audio -no-window -no-boot-anim -gpu off
 }
 
 function getAndroidNDK {
@@ -30,17 +50,13 @@ function getAndroidNDK {
   if [ ! -e $DEPS ]; then
     cd $NDK_HOME
     echo "Downloading NDK..."
-    curl -o ndk.zip https://dl.google.com/android/repository/android-ndk-r10e-linux-x86.zip
-    curl -o ndk_64.zip https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip
+    curl -o ndk.zip https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip
     unzip -o -q ndk.zip
-    unzip -o -q ndk_64.zip
-    rm ndk.zip
-    rm ndk_64.zip
     echo "Installed Android NDK at $NDK_HOME"
     touch $DEPS
+    rm ndk.zip
   fi
 }
-
 
 function waitForAVD {
   echo "Waiting for Android Virtual Device to finish booting..."
