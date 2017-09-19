45a48271b15a7475124a3739e12beba66acd7985
Naman Goel <nmn@fb.com>
2017-09-19 16:50:30 -0700

    Fix Flow errors in ListViewData source.
    Differential Revision: D5839414
    
    fbshipit-source-id: 7a0c7b3d27be728c74e08d3a8209deb3ca68dd63

--- Libraries/Lists/ListView/ListViewDataSource.js
+++ Libraries/Lists/ListView/ListViewDataSource.js
@@ -18,13 +18,7 @@ var warning = require('fbjs/lib/warning');
 
 function defaultGetRowData(
   dataBlob: any,
-  /* $FlowFixMe(>=0.53.0 site=react_native_fb) This comment suppresses an error
-   * found when Flow v0.53 was deployed. To see the error delete this comment
-   * and run Flow. */
   sectionID: number | string,
-  /* $FlowFixMe(>=0.53.0 site=react_native_fb) This comment suppresses an error
-   * found when Flow v0.53 was deployed. To see the error delete this comment
-   * and run Flow. */
   rowID: number | string,
 ): any {
   return dataBlob[sectionID][rowID];
