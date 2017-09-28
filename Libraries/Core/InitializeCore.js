1af645b2fd9ba98d740df70eaf69dd58fba273d1
Janic Duplessis <janicduplessis@gmail.com>
2017-09-27 18:19:44 -0700

    Validate that JS and Native code versions match for RN releases
    Summary:
    Basic implementation of the proposal in #15271
    
    Note that this should not affect facebook internally since they are not using OSS releases.
    
    Points to consider:
    - How strict should the version match be, right now I just match exact versions.
    - Wasn't able to use haste for ReactNativeVersion because I was getting duplicate module provider caused by the template file in scripts/versiontemplates. I tried adding the scripts folder to modulePathIgnorePatterns in package.json but that didn't help.
    - Redscreen vs. warning, I think warning is useless because if the app crashes you won't have time to see the warning.
    - Should the check and native modules be __DEV__ only?
    
    **Test plan**
    Tested that it works when version match and that it redscreens when versions don't before getting other errors on Android and iOS.
    Closes https://github.com/facebook/react-native/pull/15518
    
    Differential Revision: D5813551
    
    Pulled By: hramos
    
    fbshipit-source-id: 901757e25724b0f22bf39de172b56309d0dd5a95

--- Libraries/Core/InitializeCore.js
+++ Libraries/Core/InitializeCore.js
@@ -116,6 +116,23 @@ if (!global.__fbDisableExceptionsManager) {
   ErrorUtils.setGlobalHandler(handleError);
 }
 
+const formatVersion = version =>
+  `${version.major}.${version.minor}.${version.patch}` +
+  (version.prerelease !== null ? `-${version.prerelease}` : '');
+
+const ReactNativeVersion = require('ReactNativeVersion');
+const nativeVersion = require('NativeModules').PlatformConstants.reactNativeVersion;
+if (ReactNativeVersion.version.major !== nativeVersion.major ||
+    ReactNativeVersion.version.minor !== nativeVersion.minor) {
+  throw new Error(
+    `React Native version mismatch.\n\nJavaScript version: ${formatVersion(ReactNativeVersion.version)}\n` +
+    `Native version: ${formatVersion(nativeVersion)}\n\n` +
+    'Make sure that you have rebuilt the native code. If the problem persists ' +
+    'try clearing the watchman and packager caches with `watchman watch-del-all ' +
+    '&& react-native start --reset-cache`.'
+  );
+}
+
 // Set up collections
 const _shouldPolyfillCollection = require('_shouldPolyfillES6Collection');
 if (_shouldPolyfillCollection('Map')) {
