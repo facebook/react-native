7b7b491e50c89e071eb80a74d497eaaeaf52a033
James Ide <ide@jameside.com>
2017-10-07 02:58:14 -0700

    [0.49.2] Bump version numbers

--- Libraries/Core/ReactNativeVersion.js
+++ Libraries/Core/ReactNativeVersion.js
@@ -15,6 +15,6 @@
 exports.version = {
   major: 0,
   minor: 49,
-  patch: 1,
+  patch: 2,
   prerelease: null,
 };
--- React/Base/RCTVersion.h
+++ React/Base/RCTVersion.h
@@ -12,6 +12,6 @@
 #define REACT_NATIVE_VERSION @{ \
   @"major": @(0), \
   @"minor": @(49), \
-  @"patch": @(1), \
+  @"patch": @(2), \
   @"prerelease": [NSNull null], \
 }
