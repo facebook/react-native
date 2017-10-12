f2c6877b91963878f36ec42f5f865427bc69488c
Valentin Shergin <shergin@fb.com>
2017-10-12 09:41:26 -0700

    Fixed crash on attempt to update local data of deallocated shadow node
    Summary:
    Trivial.
    That's okay that sometimes shadowNodes and views hierarchies have lack of synchonization.
    
    Reviewed By: sahrens
    
    Differential Revision: D6040022
    
    fbshipit-source-id: 6b49a82317b620b66a87441719fddcafb1f27934

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/UIImplementation.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/UIImplementation.java
@@ -243,8 +243,10 @@ public class UIImplementation {
     ReactShadowNode shadowNode = mShadowNodeRegistry.getNode(tag);
 
     if (shadowNode == null) {
-      throw new IllegalViewOperationException(
-          "Trying to set local data for view with unknown tag: " + tag);
+      FLog.w(
+        ReactConstants.TAG,
+        "Attempt to set local data for view with unknown tag: " + tag);
+      return;
     }
 
     shadowNode.setLocalData(data);
