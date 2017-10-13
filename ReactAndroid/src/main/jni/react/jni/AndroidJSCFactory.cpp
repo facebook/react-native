e60a2d0a530bc987322fd946435a4fe24e69a164
Max Sherman <jonaldislarry@fb.com>
2017-10-12 19:19:16 -0700

    Revert D5939688: [hermes][rn] Share logPerfMarker with hermes
    Differential Revision: D5939688
    
    fbshipit-source-id: 37daeeaa01144472c26383106c88dd49bb99a37b

--- ReactAndroid/src/main/jni/react/jni/AndroidJSCFactory.cpp
+++ ReactAndroid/src/main/jni/react/jni/AndroidJSCFactory.cpp
@@ -13,7 +13,6 @@
 
 #include "JSCPerfLogging.h"
 #include "JSLogging.h"
-#include "ReactMarker.h"
 
 using namespace facebook::jni;
 
@@ -21,6 +20,53 @@ namespace facebook {
 namespace react {
 
 namespace {
+
+class JReactMarker : public JavaClass<JReactMarker> {
+ public:
+  static constexpr auto kJavaDescriptor = "Lcom/facebook/react/bridge/ReactMarker;";
+
+  static void logMarker(const std::string& marker) {
+    static auto cls = javaClassStatic();
+    static auto meth = cls->getStaticMethod<void(std::string)>("logMarker");
+    meth(cls, marker);
+  }
+
+  static void logMarker(const std::string& marker, const std::string& tag) {
+    static auto cls = javaClassStatic();
+    static auto meth = cls->getStaticMethod<void(std::string, std::string)>("logMarker");
+    meth(cls, marker, tag);
+  }
+};
+
+void logPerfMarker(const ReactMarker::ReactMarkerId markerId, const char* tag) {
+  switch (markerId) {
+    case ReactMarker::RUN_JS_BUNDLE_START:
+      JReactMarker::logMarker("RUN_JS_BUNDLE_START", tag);
+      break;
+    case ReactMarker::RUN_JS_BUNDLE_STOP:
+      JReactMarker::logMarker("RUN_JS_BUNDLE_END", tag);
+      break;
+    case ReactMarker::CREATE_REACT_CONTEXT_STOP:
+      JReactMarker::logMarker("CREATE_REACT_CONTEXT_END");
+      break;
+    case ReactMarker::JS_BUNDLE_STRING_CONVERT_START:
+      JReactMarker::logMarker("loadApplicationScript_startStringConvert");
+      break;
+    case ReactMarker::JS_BUNDLE_STRING_CONVERT_STOP:
+      JReactMarker::logMarker("loadApplicationScript_endStringConvert");
+      break;
+    case ReactMarker::NATIVE_MODULE_SETUP_START:
+      JReactMarker::logMarker("NATIVE_MODULE_SETUP_START", tag);
+      break;
+    case ReactMarker::NATIVE_MODULE_SETUP_STOP:
+      JReactMarker::logMarker("NATIVE_MODULE_SETUP_END", tag);
+      break;
+    case ReactMarker::NATIVE_REQUIRE_START:
+    case ReactMarker::NATIVE_REQUIRE_STOP:
+      // These are not used on Android.
+      break;
+  }
+}
 
 ExceptionHandling::ExtractedEror extractJniError(const std::exception& ex, const char *context) {
   auto jniEx = dynamic_cast<const jni::JniException *>(&ex);
@@ -80,7 +126,7 @@ namespace detail {
 
 void injectJSCExecutorAndroidPlatform() {
   // Inject some behavior into react/
-  JReactMarker::setLogPerfMarkerIfNeeded();
+  ReactMarker::logTaggedMarker = logPerfMarker;
   ExceptionHandling::platformErrorExtractor = extractJniError;
   JSCNativeHooks::loggingHook = nativeLoggingHook;
   JSCNativeHooks::nowHook = nativePerformanceNow;
