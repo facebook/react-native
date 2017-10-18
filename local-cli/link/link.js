4c196aec902fa0b9187e11f964aed0941778a2ae
Miron Pawlik <miron.pawlik@swmansion.com>
2017-10-17 21:19:53 -0700

    Make `react-native link` play nicely with CocoaPods-based iOS projects.
    Summary:
    The core React Native codebase already has full support for CocoaPods. However, `react-native link` doesn’t play nicely with CocoaPods, so installing third-party libs from the RN ecosystem is really hard.
    
    This change will allow to link projects that contains its own `.podspec` file to CocoaPods-based projects. In case `link` detect `Podfile` in `iOS` directory, it will look for related `.podspec` file in linked project directory, and add it to `Podfile`. If `Podfile` and `.podspec` files are not present, it will fall back to previous implementation.
    
    **Test Plan**
    1. Build a React Native project where the iOS part uses CocoaPods to manage its dependencies. The most common scenario here is to have React Native be a Pod dependency, among others.
    2. Install a RN-related library, that contains `.podspec` file, with `react-native link` (as an example it could be: [react-native-maps](https://github.com/airbnb/react-native-maps)
    3. Building the resulting iOS workspace should succeed (and there should be new entry in `Podfile`)
    Closes https://github.com/facebook/react-native/pull/15460
    
    Differential Revision: D6078649
    
    Pulled By: hramos
    
    fbshipit-source-id: 9651085875892fd66299563ca0e42fb2bcc00825

--- local-cli/link/link.js
+++ local-cli/link/link.js
@@ -29,9 +29,11 @@ const promiseWaterfall = require('./promiseWaterfall');
 const registerDependencyAndroid = require('./android/registerNativeModule');
 const registerDependencyWindows = require('./windows/registerNativeModule');
 const registerDependencyIOS = require('./ios/registerNativeModule');
+const registerDependencyPods = require('./pods/registerNativeModule');
 const isInstalledAndroid = require('./android/isInstalled');
 const isInstalledWindows = require('./windows/isInstalled');
 const isInstalledIOS = require('./ios/isInstalled');
+const isInstalledPods = require('./pods/isInstalled');
 const copyAssetsAndroid = require('./android/copyAssets');
 const copyAssetsIOS = require('./ios/copyAssets');
 const getProjectDependencies = require('./getProjectDependencies');
@@ -106,17 +108,19 @@ const linkDependencyIOS = (iOSProject, dependency) => {
     return;
   }
 
-  const isInstalled = isInstalledIOS(iOSProject, dependency.config.ios);
-
+  const isInstalled = isInstalledIOS(iOSProject, dependency.config.ios) || isInstalledPods(iOSProject, dependency.config.ios);
   if (isInstalled) {
     log.info(chalk.grey(`iOS module ${dependency.name} is already linked`));
     return;
   }
 
   log.info(`Linking ${dependency.name} ios dependency`);
-
-  registerDependencyIOS(dependency.config.ios, iOSProject);
-
+  if (iOSProject.podfile && dependency.config.ios.podspec) {
+    registerDependencyPods(dependency, iOSProject);
+  }
+  else {
+    registerDependencyIOS(dependency.config.ios, iOSProject);
+  }
   log.info(`iOS module ${dependency.name} has been successfully linked`);
 };
 
