b387beececce38f024e075755fac77f1591997d2
Douglas <dlowder@salesforce.com>
2017-11-21 17:07:37 -0800

    Quick fix for react-native link tvOS issue

--- local-cli/link/ios/getProducts.js
+++ local-cli/link/ios/getProducts.js
@@ -4,9 +4,24 @@
  * project static library
  */
 module.exports = function getProducts(project) {
-  return project
-    .pbxGroupByName('Products')
-    .children
-    .map(c => c.comment)
-    .filter(c => c.indexOf('.a') > -1);
+  debugger;
+  let nativeTargetSection = project.pbxNativeTargetSection();
+  var results = [];
+  for (var key in nativeTargetSection) {
+    if(key.indexOf('_comment') === -1) {
+      let configurationListId = nativeTargetSection[key].buildConfigurationList;
+      let configurationList = project.pbxXCConfigurationList()[configurationListId];
+      let buildConfigurationId = configurationList.buildConfigurations[0].value;
+      let buildConfiguration = project.pbxXCBuildConfigurationSection()[buildConfigurationId];
+      if ( !buildConfiguration.buildSettings.SDKROOT || buildConfiguration.buildSettings.SDKROOT.indexOf('appletv') === -1) {
+        results.push(nativeTargetSection[key].productReference_comment);
+      }
+    }
+  }
+  return results;
+//  return project
+//    .pbxGroupByName('Products')
+//    .children
+//    .map(c => c.comment)
+//    .filter(c => c.indexOf('.a') > -1);
 };
