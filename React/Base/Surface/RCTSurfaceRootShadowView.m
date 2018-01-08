176a578238566ad857c0911e127669f1ee82107d
Valentin Shergin <shergin@fb.com>
2018-01-07 18:21:46 -0800

    '-[RCTSurfaceRootShadowView sizeThatFitsMinimumSize:]' was removed
    Summary: Because we already support this for all kinds of shadow views!
    
    Reviewed By: mmmulani
    
    Differential Revision: D6665636
    
    fbshipit-source-id: ceee7e4952ede4bbe54aedde5545587a1d13ea5a

--- React/Base/Surface/RCTSurfaceRootShadowView.m
+++ React/Base/Surface/RCTSurfaceRootShadowView.m
@@ -76,41 +76,6 @@
   return viewsWithNewFrame;
 }
 
-- (CGSize)sizeThatFitsMinimumSize:(CGSize)minimumSize
-                      maximumSize:(CGSize)maximumSize
-{
-  // Positive case where requested constraind are aready enforced.
-  if (CGSizeEqualToSize(minimumSize, _minimumSize) &&
-      CGSizeEqualToSize(maximumSize, _maximumSize)) {
-    // We stil need to call `calculateLayoutWithMinimumSize:maximumSize`
-    // mehtod though.
-    [self calculateLayoutWithMinimumSize:_minimumSize
-                             maximumSize:_maximumSize];
-
-    YGNodeRef yogaNode = self.yogaNode;
-    return CGSizeMake(YGNodeLayoutGetWidth(yogaNode), YGNodeLayoutGetHeight(yogaNode));
-  }
-
-  // Generic case, where requested constraind are different from enforced.
-
-  // Applying given size constraints.
-  [self calculateLayoutWithMinimumSize:minimumSize
-                           maximumSize:maximumSize];
-
-  YGNodeRef yogaNode = self.yogaNode;
-  CGSize fittingSize =
-    CGSizeMake(YGNodeLayoutGetWidth(yogaNode), YGNodeLayoutGetHeight(yogaNode));
-
-  // Reverting size constraints.
-  [self calculateLayoutWithMinimumSize:_minimumSize
-                           maximumSize:_maximumSize];
-
-  return CGSizeMake(
-    MAX(minimumSize.width, MIN(maximumSize.width, fittingSize.width)),
-    MAX(minimumSize.height, MIN(maximumSize.height, fittingSize.height))
-  );
-}
-
 - (void)setMinimumSize:(CGSize)minimumSize maximumSize:(CGSize)maximumSize
 {
   if (CGSizeEqualToSize(minimumSize, _minimumSize) &&
