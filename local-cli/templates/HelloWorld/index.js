30b21a634405a9e48c61c984d3de628e4c7e4d8a
Valentin Shergin <valentin@shergin.com>
2018-01-13 15:37:41 -0800

    Update index.js

--- local-cli/templates/HelloWorld/index.js
+++ local-cli/templates/HelloWorld/index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
