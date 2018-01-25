b9d058a05c0d940faa2e7ce3eef6f4182f54f134
Hogyun Yu　　(aka Philographer) <yoohoogun114@naver.com>
2018-01-24 22:07:17 -0800

    Remove references to Travis
    Summary:
    react-native project is migrated travis to circleCI.
    but the document has some leavings.
    
    Only Docs Edited
    
    https://github.com/facebook/react-native/pull/16354
    https://github.com/facebook/react-native/pull/16364
    
    [ DOCS] Remove references to Travis
    changed Releases.md
    changed RNTester/RNTesterIntegrationTests/RNTesterSnapshotTests.m
    changed scripts/android-e2e-test.js
    Closes https://github.com/facebook/react-native/pull/17733
    
    Differential Revision: D6806336
    
    Pulled By: shergin
    
    fbshipit-source-id: 22d21995d718a89bb831f75ed2ff6074dd22c6fe

--- scripts/android-e2e-test.js
+++ scripts/android-e2e-test.js
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * Used in run-ci-e2e-test.js and executed in Travis and Circle CI.
+ * Used in run-ci-e2e-test.js and executed in Circle CI.
  * E2e test that verifies that init app can be installed, compiled, started and Hot Module reloading and Chrome debugging work.
  * For other examples of appium refer to: https://github.com/appium/sample-code/tree/master/sample-code/examples/node and
  * https://www.npmjs.com/package/wd-android
