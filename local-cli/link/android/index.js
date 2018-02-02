2847e20e01f979106336df1173577d70bed4e950
Eric Rozell <erozell@outlook.com>
2018-02-02 07:38:54 -0500

    Uses a single code path to link and unlink all platforms
    The previous commit added logic to allow platforms to self-configure their link behavior. The commit kept a special case for iOS and Android intact. This commit removes the special cases.

---
+++ local-cli/link/android/index.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+  return {
+    isInstalled: require('./isInstalled'),
+    register: require('./registerNativeModule'),
+    unregister: require('./unregisterNativeModule'),
+    copyAssets: require('./copyAssets'),
+    unlinkAssets: require('./unlinkAssets')
+  };
+};
