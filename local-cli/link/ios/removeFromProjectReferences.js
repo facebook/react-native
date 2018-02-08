bf419ab377549165b9e4f572ad56b3851cefc9fc
Hector Ramos <hramos@fb.com>
2018-02-08 09:20:11 -0800

    Restore copyright header

--- local-cli/link/ios/removeFromProjectReferences.js
+++ local-cli/link/ios/removeFromProjectReferences.js
@@ -1,4 +1,13 @@
 /**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+/**
  * For each file (.xcodeproj), there's an entry in `projectReferences` created
  * that has two entries - `ProjectRef` - reference to a file.uuid and
  * `ProductGroup` - uuid of a Products group.
