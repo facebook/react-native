2f02dd4c8cfa3ac9f3805b2c85a71f405560d25c
Héctor Ramos <hramos@fb.com>
2018-02-08 07:25:00 -0800

    Revert PR #17312 due to buck failures in continuous integration tests
    Summary:
    Circle is currently failing on the `android` step due to a dependency issue introduced by the aforementioned PR. I am currently waiting for an internal diff to be reviewed which will restore this PR alongside the necessary dependency.
    Closes https://github.com/facebook/react-native/pull/17902
    
    Differential Revision: D6937173
    
    Pulled By: hramos
    
    fbshipit-source-id: f732a397521cc5df36f503e618318ef6d69aeaa6

--- ReactAndroid/src/main/java/com/facebook/react/modules/network/ProgressRequestBody.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/network/ProgressRequestBody.java
@@ -9,75 +9,60 @@
 
 package com.facebook.react.modules.network;
 
-import com.facebook.common.internal.CountingOutputStream;
-
 import java.io.IOException;
-
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import okio.BufferedSink;
-import okio.Okio;
+import okio.Buffer;
 import okio.Sink;
+import okio.ForwardingSink;
+import okio.Okio;
 
 public class ProgressRequestBody extends RequestBody {
 
   private final RequestBody mRequestBody;
   private final ProgressListener mProgressListener;
   private BufferedSink mBufferedSink;
-  private long mContentLength = 0L;
 
   public ProgressRequestBody(RequestBody requestBody, ProgressListener progressListener) {
-    mRequestBody = requestBody;
-    mProgressListener = progressListener;
+      mRequestBody = requestBody;
+      mProgressListener = progressListener;
   }
 
   @Override
   public MediaType contentType() {
-    return mRequestBody.contentType();
+      return mRequestBody.contentType();
   }
 
   @Override
   public long contentLength() throws IOException {
-    if (mContentLength == 0) {
-      mContentLength = mRequestBody.contentLength();
-    }
-    return mContentLength;
+      return mRequestBody.contentLength();
   }
 
   @Override
   public void writeTo(BufferedSink sink) throws IOException {
-    if (mBufferedSink == null) {
-      mBufferedSink = Okio.buffer(outputStreamSink(sink));
-    }
-
-    // contentLength changes for input streams, since we're using inputStream.available(),
-    // so get the length before writing to the sink
-    contentLength();
-
-    mRequestBody.writeTo(mBufferedSink);
-    mBufferedSink.flush();
+      if (mBufferedSink == null) {
+          mBufferedSink = Okio.buffer(sink(sink));
+      }
+      mRequestBody.writeTo(mBufferedSink);
+      mBufferedSink.flush();
   }
 
-  private Sink outputStreamSink(BufferedSink sink) {
-    return Okio.sink(new CountingOutputStream(sink.outputStream()) {
-      @Override
-      public void write(byte[] data, int offset, int byteCount) throws IOException {
-        super.write(data, offset, byteCount);
-        sendProgressUpdate();
-      }
+  private Sink sink(Sink sink) {
+      return new ForwardingSink(sink) {
+          long bytesWritten = 0L;
+          long contentLength = 0L;
 
-      @Override
-      public void write(int data) throws IOException {
-        super.write(data);
-        sendProgressUpdate();
-      }
-
-      private void sendProgressUpdate() throws IOException {
-        long bytesWritten = getCount();
-        long contentLength = contentLength();
-        mProgressListener.onProgress(
-          bytesWritten, contentLength, bytesWritten == contentLength);
-      }
-    });
+          @Override
+          public void write(Buffer source, long byteCount) throws IOException {
+              super.write(source, byteCount);
+              if (contentLength == 0) {
+                  contentLength = contentLength();
+              }
+              bytesWritten += byteCount;
+              mProgressListener.onProgress(
+                bytesWritten, contentLength, bytesWritten == contentLength);
+          }
+      };
   }
 }
