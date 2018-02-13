6893a26bfb06a2d8ad9d23a572f4d9143305d905
Peter van der Zee <kuvos@fb.com>
2018-02-13 01:22:04 -0800

    Fix duplicate var name declaration
    Reviewed By: rafeca
    
    Differential Revision: D6965150
    
    fbshipit-source-id: 332c8202f350e2014fedb9790f3d895222fb4f8d

--- Libraries/Alert/AlertIOS.js
+++ Libraries/Alert/AlertIOS.js
@@ -122,11 +122,10 @@ class AlertIOS {
         'keyboardType) and the old syntax will be removed in a future version.');
 
       var callback = type;
-      var defaultValue = message;
       RCTAlertManager.alertWithArgs({
         title: title || '',
         type: 'plain-text',
-        defaultValue,
+        defaultValue: message,
       }, (id, value) => {
         callback(value);
       });
