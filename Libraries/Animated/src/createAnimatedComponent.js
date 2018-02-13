fcd1cdcf7effc78c50b69c3c0c6657d3c471854f
Janic Duplessis <janicduplessis@gmail.com>
2018-02-13 00:52:28 -0500

    Allow string components

--- Libraries/Animated/src/createAnimatedComponent.js
+++ Libraries/Animated/src/createAnimatedComponent.js
@@ -21,7 +21,8 @@ const invariant = require('fbjs/lib/invariant');
 
 function createAnimatedComponent(Component: any): any {
   invariant(
-    Component.prototype && Component.prototype.isReactComponent,
+    typeof Component === 'string' ||
+      (Component.prototype && Component.prototype.isReactComponent),
     '`createAnimatedComponent` does not support stateless functional components; ' +
       'use a class component instead.',
   );
