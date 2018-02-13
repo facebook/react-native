54870e0c6ca8611fed775e5ba12a0d6d9b1cdbd7
Spencer Ahrens <sahrens@fb.com>
2018-02-12 18:43:17 -0800

    Add UTFSequence module for common unicode usage
    Summary:
    A collection of Unicode sequences for various characters and emoji.
    
      - More explicit than using the sequences directly in code.
      - Source code should be limitted to ASCII.
      - Less chance of typos.
    
    Reviewed By: TheSavior
    
    Differential Revision: D6969065
    
    fbshipit-source-id: c11ec96b74f5dfa7c624a3c53f3c29f6284a82b3

---
+++ Libraries/UTFSequence.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule UTFSequence
+ * @flow
+ * @format
+ */
+
+'use strict';
+
+/**
+ * A collection of Unicode sequences for various characters and emoji.
+ *
+ *  - More explicit than using the sequences directly in code.
+ *  - Source code should be limitted to ASCII.
+ *  - Less chance of typos.
+ */
+const UTFSequence = {
+  MIDDOT: '\u00B7', // normal middle dot: &middot;
+  MIDDOT_SP: '\u00A0\u00B7\u00A0', // &nbsp;&middot;&nbsp;
+  MIDDOT_KATAKANA: '\u30FB', // katakana middle dot
+  MDASH: '\u2014', // em dash: &mdash;
+  MDASH_SP: '\u00A0\u2014\u00A0', // &nbsp;&mdash;&nbsp;
+  NDASH: '\u2013', // en dash: &ndash;
+  NDASH_SP: '\u00A0\u2013\u00A0', // &nbsp;&ndash;&nbsp;
+  NBSP: '\u00A0', // non-breaking space: &nbsp;
+  PIZZA: '\uD83C\uDF55',
+};
+
+module.exports = UTFSequence;
