7d20de412b37a35951e615d98509573dc1a24bcb
Valentin Shergin <shergin@fb.com>
2018-02-13 23:25:04 -0800

    Fixed a bug with positioning of nested views inside <Text>
    Summary:
    Fixing of recent regression in layout system.
    Conseptually, a superview should define frames of child views.
    
    Reviewed By: mmmulani
    
    Differential Revision: D6980128
    
    fbshipit-source-id: e267e966fd46af28db1d3d40939110040b74e33f

--- Libraries/Text/Text/RCTTextShadowView.m
+++ Libraries/Text/Text/RCTTextShadowView.m
@@ -298,6 +298,11 @@
                             maximumSize:frame.size
                         layoutDirection:self.layoutMetrics.layoutDirection
                           layoutContext:localLayoutContext];
+
+      // Reinforcing a proper frame origin for the Shadow View.
+      RCTLayoutMetrics localLayoutMetrics = shadowView.layoutMetrics;
+      localLayoutMetrics.frame.origin = frame.origin;
+      [shadowView layoutWithMetrics:localLayoutMetrics layoutContext:localLayoutContext];
     }
   ];
 }
