f7f5dc66493ad25a85927a9503728b0491c8aab9
Alex Dvornikov <advornikov@fb.com>
2018-02-15 03:52:52 -0800

    Extract polyfillGlobal from InitializeCore
    Reviewed By: jeanlauliac
    
    Differential Revision: D6987657
    
    fbshipit-source-id: 8762732de671418520376a98bdd724bbb24e4e36

--- Libraries/Core/InitializeCore.js
+++ Libraries/Core/InitializeCore.js
@@ -27,6 +27,8 @@
  */
 'use strict';
 
+const {polyfillObjectProperty, polyfillGlobal} = require('PolyfillFunctions');
+
 if (global.GLOBAL === undefined) {
   global.GLOBAL = global;
 }
@@ -35,8 +37,6 @@ if (global.window === undefined) {
   global.window = global;
 }
 
-const defineLazyObjectProperty = require('defineLazyObjectProperty');
-
 // Set up collections
 const _shouldPolyfillCollection = require('_shouldPolyfillES6Collection');
 if (_shouldPolyfillCollection('Map')) {
@@ -44,50 +44,6 @@ if (_shouldPolyfillCollection('Map')) {
 }
 if (_shouldPolyfillCollection('Set')) {
   polyfillGlobal('Set', () => require('Set'));
-}
-
-/**
- * Sets an object's property. If a property with the same name exists, this will
- * replace it but maintain its descriptor configuration. The property will be
- * replaced with a lazy getter.
- *
- * In DEV mode the original property value will be preserved as `original[PropertyName]`
- * so that, if necessary, it can be restored. For example, if you want to route
- * network requests through DevTools (to trace them):
- *
- *   global.XMLHttpRequest = global.originalXMLHttpRequest;
- *
- * @see https://github.com/facebook/react-native/issues/934
- */
-function defineLazyProperty<T>(
-  object: Object,
-  name: string,
-  getValue: () => T,
-): void {
-  const descriptor = Object.getOwnPropertyDescriptor(object, name);
-  if (__DEV__ && descriptor) {
-    const backupName = `original${name[0].toUpperCase()}${name.substr(1)}`;
-    Object.defineProperty(object, backupName, {
-      ...descriptor,
-      value: object[name],
-    });
-  }
-
-  const {enumerable, writable, configurable} = descriptor || {};
-  if (descriptor && !configurable) {
-    console.error('Failed to set polyfill. ' + name + ' is not configurable.');
-    return;
-  }
-
-  defineLazyObjectProperty(object, name, {
-    get: getValue,
-    enumerable: enumerable !== false,
-    writable: writable !== false,
-  });
-}
-
-function polyfillGlobal<T>(name: string, getValue: () => T): void {
-  defineLazyProperty(global, name, getValue);
 }
 
 // Set up process
@@ -191,8 +147,8 @@ if (navigator === undefined) {
 }
 
 // see https://github.com/facebook/react-native/issues/10881
-defineLazyProperty(navigator, 'product', () => 'ReactNative');
-defineLazyProperty(navigator, 'geolocation', () => require('Geolocation'));
+polyfillObjectProperty(navigator, 'product', () => 'ReactNative');
+polyfillObjectProperty(navigator, 'geolocation', () => require('Geolocation'));
 
 // Just to make sure the JS gets packaged up. Wait until the JS environment has
 // been initialized before requiring them.
