fee96696a28638547bd0a08681ee6a654c7c390d
Jean Lauliac <jeanlauliac@fb.com>
2018-02-19 16:23:09 +0000

    fix Flow typing for OSS

--- local-cli/bundle/buildBundle.js
+++ local-cli/bundle/buildBundle.js
@@ -10,12 +10,16 @@
 'use strict';
 
 const log = require('../util/log').out('bundle');
+/* $FlowFixMe(site=react_native_oss) */
 const Server = require('metro/src/Server');
 const {Terminal} = require('metro-core');
+/* $FlowFixMe(site=react_native_oss) */
 const TerminalReporter = require('metro/src/lib/TerminalReporter');
+/* $FlowFixMe(site=react_native_oss) */
 const TransformCaching = require('metro/src/lib/TransformCaching');
 
 const {defaults} = require('metro');
+/* $FlowFixMe(site=react_native_oss) */
 const outputBundle = require('metro/src/shared/output/bundle');
 const path = require('path');
 const saveAssets = require('./saveAssets');
