95d01ecf80a3753570a044a5aa43fbdf28581518
Benjamin Herzog <mail@benchr.de>
2018-03-02 10:47:43 +0100

    Added nullability specifier to factory typedef

--- React/Base/Surface/SurfaceHostingView/RCTSurfaceHostingView.h
+++ React/Base/Surface/SurfaceHostingView/RCTSurfaceHostingView.h
@@ -13,7 +13,7 @@
 @class RCTBridge;
 @class RCTSurface;
 
-typedef UIView *(^RCTSurfaceHostingViewActivityIndicatorViewFactory)();
+typedef UIView *_Nullable(^RCTSurfaceHostingViewActivityIndicatorViewFactory)();
 
 NS_ASSUME_NONNULL_BEGIN
 
