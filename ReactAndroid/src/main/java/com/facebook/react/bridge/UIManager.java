4466b6fa7ce5325cac3233a23b10ffcb4ff038a7
David Vacca <dvacca@fb.com>
2018-03-05 10:55:19 -0800

    Refactor BridgeListener into JSIModulesProvider
    Reviewed By: achen1
    
    Differential Revision: D7140944
    
    fbshipit-source-id: 092ea8569af5b5f90e005d6dc2c1819c1c9cf58f

--- ReactAndroid/src/main/java/com/facebook/react/bridge/UIManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/bridge/UIManager.java
@@ -3,7 +3,7 @@ package com.facebook.react.bridge;
 import com.facebook.react.uimanager.common.MeasureSpecProvider;
 import com.facebook.react.uimanager.common.SizeMonitoringFrameLayout;
 
-public interface UIManager {
+public interface UIManager extends JSIModule {
 
   /**
    * Registers a new root view.
