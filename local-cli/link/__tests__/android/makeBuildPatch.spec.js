a586dab0d83974b55b970020088c992890077b08
Thibault Malbranche <thibault.malbranche@epitech.eu>
2018-03-08 15:06:03 +0100

    Added Tests

--- local-cli/link/__tests__/android/makeBuildPatch.spec.js
+++ local-cli/link/__tests__/android/makeBuildPatch.spec.js
@@ -12,7 +12,11 @@
 'use strict';
 
 const makeBuildPatch = require('../../android/patches/makeBuildPatch');
+const normalizeProjectName = require('../../android/patches/normalizeProjectName');
+
 const name = 'test';
+const scopedName = '@scoped/test';
+const normalizedScopedName = normalizeProjectName('@scoped/test');
 
 describe('makeBuildPatch', () => {
   it('should build a patch function', () => {
@@ -28,6 +32,20 @@ describe('makeBuildPatch', () => {
   it('should make a correct install check pattern', () => {
     const {installPattern} = makeBuildPatch(name);
     const match = `/\\s{4}(compile)(\\(|\\s)(project)\\(\\':${name}\\'\\)(\\)|\\s)/`;
+    expect(installPattern.toString()).toBe(match);
+  });
+});
+
+
+describe('makeBuildPatchWithScopedPackage', () => {
+  it('should make a correct patch', () => {
+    const {patch} = makeBuildPatch(scopedName);
+    expect(patch).toBe(`    compile project(':${normalizedScopedName}')\n`);
+  });
+
+  it('should make a correct install check pattern', () => {
+    const {installPattern} = makeBuildPatch(scopedName);
+    const match = `/\\s{4}(compile)(\\(|\\s)(project)\\(\\':${normalizedScopedName}\\'\\)(\\)|\\s)/`;
     expect(installPattern.toString()).toBe(match);
   });
 });
