ac55ffd77701b7176ca08e26101367e51410c49b
Mike Grabowski <grabbou@gmail.com>
2018-03-12 22:16:32 +0100

    Revert "Fix HmrClient path"
    This reverts commit 8bdd98ea4865b186b59d9737ffd4b0084f3c225c.

--- Libraries/Utilities/HMRClient.js
+++ Libraries/Utilities/HMRClient.js
@@ -15,7 +15,7 @@
 const Platform = require('Platform');
 const invariant = require('fbjs/lib/invariant');
 
-const MetroHMRClient = require('metro/src/lib/bundle-modules/HmrClient');
+const MetroHMRClient = require('metro/src/lib/bundle-modules/HMRClient');
 
 /**
  * HMR Client that receives from the server HMR updates and propagates them
