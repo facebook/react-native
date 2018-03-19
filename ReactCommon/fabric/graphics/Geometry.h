608d1fb5909a3c6574b47ecda77b7647ba5776a5
Valentin Shergin <shergin@fb.com>
2018-03-18 19:04:13 -0700

    `fabric/graphics` module: all about graphics
    Summary: `fabric/graphics` provides graphics primitives; the implementation should be platform-specific eventually.
    
    Reviewed By: fkgozali
    
    Differential Revision: D7230675
    
    fbshipit-source-id: ff05d2673072fad5ee6f331f0155561969e1a8b6

---
+++ ReactCommon/fabric/graphics/Geometry.h
@@ -0,0 +1,60 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+namespace facebook {
+namespace react {
+
+/*
+ * Exact type of float numbers which ideally should match a type behing
+ * platform- and chip-architecture-specific float type (something like
+ * CGFloat on iOS).
+ */
+using Float = double;
+
+/*
+ * Point
+ */
+struct Point {
+  Float x {0};
+  Float y {0};
+
+  Point& operator += (const Point& rhs) {
+    x += rhs.x;
+    y += rhs.y;
+    return *this;
+  }
+
+  friend Point operator + (Point lhs, const Point& rhs) {
+    return lhs += rhs;
+  }
+};
+
+/*
+ * Size
+ */
+struct Size {
+  Float width {0};
+  Float height {0};
+};
+
+/*
+ * Rect: Point and Size
+ */
+struct Rect {
+  Point origin {0, 0};
+  Size size {0, 0};
+};
+
+/*
+ * EdgeInsets
+ */
+struct EdgeInsets {
+  Float top {0};
+  Float left {0};
+  Float bottom {0};
+  Float right {0};
+};
+
+} // namespace react
+} // namespace facebook
