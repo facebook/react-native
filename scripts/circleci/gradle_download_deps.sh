d03f0ba4950834dd8bb7f2ac4b0bc047ccda59ac
Hector Ramos <hramos@fb.com>
2018-03-27 13:07:28 -0700

    Cache gradle downloads and reuse existing caches in forks

---
+++ scripts/circleci/gradle_download_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+./gradlew :ReactAndroid:downloadBoost :ReactAndroid:downloadDoubleConversion :ReactAndroid:downloadFolly :ReactAndroid:downloadGlog :ReactAndroid:downloadJSCHeaders
