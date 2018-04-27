370bcffba748e895ad8afa825bfef40bff859c95
Mike Grabowski <grabbou@gmail.com>
2018-04-27 18:34:36 +0200

    [0.55.4] Bump version numbers

--- Libraries/Core/ReactNativeVersion.js
+++ Libraries/Core/ReactNativeVersion.js
@@ -13,6 +13,6 @@
 exports.version = {
   major: 0,
   minor: 55,
-  patch: 3,
+  patch: 4,
   prerelease: null,
 };
--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -22,7 +22,7 @@ static void __makeVersion()
   __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(55),
-                  RCTVersionPatch: @(3),
+                  RCTVersionPatch: @(4),
                   RCTVersionPrerelease: [NSNull null],
                   };
 }
