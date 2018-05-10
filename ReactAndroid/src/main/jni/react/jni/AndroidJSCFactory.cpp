a810e6875fc5b6d1e1545c34910a109450b5aacd
Marc Horowitz <mhorowitz@fb.com>
2018-05-09 22:01:58 -0700

    move nativeLoggingHook into AndroidJSCFactory
    Reviewed By: fromcelticpark
    
    Differential Revision: D7803905
    
    fbshipit-source-id: 797f5250a4a129a8dff3fb447c01837d68bea452

--- ReactAndroid/src/main/jni/react/jni/AndroidJSCFactory.cpp
+++ ReactAndroid/src/main/jni/react/jni/AndroidJSCFactory.cpp
@@ -74,6 +74,29 @@ JSValueRef nativePerformanceNow(
   return Value::makeNumber(ctx, (nano / (double)NANOSECONDS_IN_MILLISECOND));
 }
 
+JSValueRef nativeLoggingHook(
+  JSContextRef ctx,
+  JSObjectRef function,
+  JSObjectRef thisObject,
+  size_t argumentCount,
+  const JSValueRef arguments[],
+  JSValueRef* exception) {
+  android_LogPriority logLevel = ANDROID_LOG_DEBUG;
+  if (argumentCount > 1) {
+    int level = (int)Value(ctx, arguments[1]).asNumber();
+    // The lowest log level we get from JS is 0. We shift and cap it to be
+    // in the range the Android logging method expects.
+    logLevel = std::min(
+      static_cast<android_LogPriority>(level + ANDROID_LOG_DEBUG),
+      ANDROID_LOG_FATAL);
+  }
+  if (argumentCount > 0) {
+    String message = Value(ctx, arguments[0]).toString();
+    reactAndroidLoggingHook(message.str(), logLevel);
+  }
+  return Value::makeUndefined(ctx);
+}
+
 }
 
 namespace detail {
