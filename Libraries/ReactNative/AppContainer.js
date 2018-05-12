4767f9be455cc32f89fb7ccf9030f007e7462014
Panagiotis Vekris <pvekris@fb.com>
2018-05-11 17:44:27 -0700

    v0.72.0 in xplat/js
    Reviewed By: gabelevi
    
    Differential Revision: D7956747
    
    fbshipit-source-id: b1993de405c273b3cc1ed74f994e8e75bda0a985

--- Libraries/ReactNative/AppContainer.js
+++ Libraries/ReactNative/AppContainer.js
@@ -102,9 +102,6 @@ class AppContainer extends React.Component<Props, State> {
         pointerEvents="box-none"
         style={styles.appContainer}
         ref={ref => {
-          /* $FlowFixMe(>=0.53.0 site=react_native_fb,react_native_oss) This
-           * comment suppresses an error when upgrading Flow's support for
-           * React. To see the error delete this comment and run Flow. */
           this._mainRef = ref;
         }}>
         {this.props.children}
