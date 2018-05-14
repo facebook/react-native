1f9676a1cb023f149fa7caebdf4135a1137256dd
Valentin Shergin <shergin@fb.com>
2018-05-14 15:43:57 -0700

    Fabric: Final cleanup of define-based props treatment
    Summary: Oh, my! No more `#define`s related to props conversions and debug-printing.
    
    Reviewed By: fkgozali
    
    Differential Revision: D7958250
    
    fbshipit-source-id: 86950070c55f134aa3a575b9fd68fc90d865cf44

--- ReactCommon/fabric/core/propsConversions.h
+++ ReactCommon/fabric/core/propsConversions.h
@@ -15,35 +15,6 @@
 namespace facebook {
 namespace react {
 
-inline bool boolFromDynamic(const folly::dynamic &value) { return value.getBool(); }
-inline int intFromDynamic(const folly::dynamic &value) { return value.getInt(); }
-inline Float floatFromDynamic(const folly::dynamic &value) { return value.getDouble(); }
-inline std::string stringFromDynamic(const folly::dynamic &value) { return value.getString(); }
-
-#define CONVERT_RAW_PROP_TEMPLATE(type, converter) \
-inline static type convertRawProp(const RawProps &rawProps, const std::string &name, const type &defaultValue) { \
-  auto &&iterator = rawProps.find(name); \
-  if (iterator != rawProps.end()) { \
-    return converter(iterator->second); \
-  } else { \
-    return defaultValue; \
-  } \
-} \
-\
-inline static folly::Optional<type> convertRawProp(const RawProps &rawProps, const std::string &name, const folly::Optional<type> &defaultValue) { \
-  auto &&iterator = rawProps.find(name); \
-  if (iterator != rawProps.end()) { \
-    auto &&value = iterator->second; \
-    if (value.isNull()) { \
-      return {}; \
-    } else { \
-      return converter(value); \
-    } \
-  } else { \
-    return defaultValue; \
-  } \
-}
-
 inline void fromDynamic(const folly::dynamic &value, bool &result) { result = value.getBool(); }
 inline void fromDynamic(const folly::dynamic &value, int &result) { result = value.getInt(); }
 inline void fromDynamic(const folly::dynamic &value, std::string &result) { result = value.getString(); }
@@ -51,30 +22,33 @@ inline void fromDynamic(const folly::dynamic &value, std::string &result) { resu
 template <typename T>
 inline T convertRawProp(const RawProps &rawProps, const std::string &name, const T &defaultValue) {
   auto &&iterator = rawProps.find(name);
-  if (iterator != rawProps.end()) {
-    T result = defaultValue;
-    fromDynamic(iterator->second, result);
-    return result;
-  } else {
+  if (iterator == rawProps.end()) {
     return defaultValue;
   }
+
+  auto &&value = iterator->second;
+  T result;
+  fromDynamic(value, result);
+  return result;
 }
 
 template <typename T>
 inline static folly::Optional<T> convertRawProp(const RawProps &rawProps, const std::string &name, const folly::Optional<T> &defaultValue) {
   auto &&iterator = rawProps.find(name);
-  if (iterator != rawProps.end()) {
-    auto &&value = iterator->second;
-    if (value.isNull()) {
-      return defaultValue;
-    } else {
-      T result;
-      fromDynamic(value, result);
-      return result;
-    }
-  } else {
+  if (iterator == rawProps.end()) {
     return defaultValue;
   }
+
+  auto &&value = iterator->second;
+  T result;
+
+  // Special case for optionals: `null` always means `no value`.
+  if (value.isNull()) {
+    return {};
+  }
+
+  fromDynamic(value, result);
+  return result;
 }
 
 } // namespace react
