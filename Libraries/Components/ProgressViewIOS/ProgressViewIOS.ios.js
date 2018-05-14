c87701ba05a8524756e87c089eb92c8f3c81823e
Eli White <eliwhite@fb.com>
2018-05-14 00:09:26 -0700

    Flowtype ProgressViewIOS
    Reviewed By: yungsters
    
    Differential Revision: D7985969
    
    fbshipit-source-id: d351ebc26e7be2741c93ce462ae59aa13d0c1f27

--- Libraries/Components/ProgressViewIOS/ProgressViewIOS.ios.js
+++ Libraries/Components/ProgressViewIOS/ProgressViewIOS.ios.js
@@ -13,12 +13,27 @@
 const Image = require('Image');
 const NativeMethodsMixin = require('NativeMethodsMixin');
 const React = require('React');
+const ReactNative = require('ReactNative');
 const PropTypes = require('prop-types');
 const StyleSheet = require('StyleSheet');
 const ViewPropTypes = require('ViewPropTypes');
 
 const createReactClass = require('create-react-class');
 const requireNativeComponent = require('requireNativeComponent');
+
+import type {ImageSource} from 'ImageSource';
+import type {ColorValue} from 'StyleSheetTypes';
+import type {ViewProps} from 'ViewPropTypes';
+
+type Props = $ReadOnly<{|
+  ...ViewProps,
+  progressViewStyle?: ?('default' | 'bar'),
+  progress?: ?number,
+  progressTintColor?: ?ColorValue,
+  trackTintColor?: ?string,
+  progressImage?: ?ImageSource,
+  trackImage?: ?ImageSource,
+|}>;
 
 /**
  * Use `ProgressViewIOS` to render a UIProgressView on iOS.
@@ -81,4 +96,6 @@ const RCTProgressView = requireNativeComponent(
   ProgressViewIOS,
 );
 
-module.exports = ProgressViewIOS;
+module.exports = ((ProgressViewIOS: any): Class<
+  ReactNative.NativeComponent<Props>,
+>);
