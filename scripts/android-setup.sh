0c39c88490787f91ffa4b3b39b9937e4a5e1ca1c
Hector Ramos <hramos@fb.com>
2018-05-16 07:56:26 -0700

    Remove Google APIs package

--- scripts/android-setup.sh
+++ scripts/android-setup.sh
@@ -17,8 +17,10 @@ function getAndroidPackages {
     sdkmanager "platforms;android-$ANDROID_SDK_TARGET_API_LEVEL"
     echo "Installing SDK build tools, revision $ANDROID_SDK_BUILD_TOOLS_REVISION..."
     sdkmanager "build-tools;$ANDROID_SDK_BUILD_TOOLS_REVISION"
-    echo "Installing Google APIs for Android API level $ANDROID_SDK_BUILD_API_LEVEL..."
-    sdkmanager "add-ons;addon-google_apis-google-$ANDROID_SDK_BUILD_API_LEVEL"
+    # These moved to "system-images;android-$ANDROID_SDK_BUILD_API_LEVEL;google_apis;x86" starting with API level 25, but there is no ARM version.
+    # If our tests do not use Google APIs, we can ommit these.
+    # echo "Installing Google APIs ANDROID_SDK_BUILD_API_LEVEL..."
+    # sdkmanager "add-ons;addon-google_apis-google-$ANDROID_SDK_BUILD_API_LEVEL"
     echo "Installing Android Support Repository"
     sdkmanager "extras;android;m2repository"
     $CI && touch $DEPS
