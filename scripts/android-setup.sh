274c1757e67f7d1e9ae8fe724d1f0f7755e00e06
Héctor Ramos <165856+hramos@users.noreply.github.com>
2018-05-15 23:10:39 -0700

    Make Android build tools 26.0.3 available to CI
    Summary:
    a3931e95317acaff5ceb1d2fb0f26ae67744c8a3 bumped buildToolsVersions to 26.0.3, and `test_android` started failing because that particular build tools version was not getting installed in CircleCI.
    Closes https://github.com/facebook/react-native/pull/19276
    
    Differential Revision: D8020664
    
    Pulled By: hramos
    
    fbshipit-source-id: 561aa8186ed165b3652923a9bcaf56134acfd90b

--- scripts/android-setup.sh
+++ scripts/android-setup.sh
@@ -16,7 +16,7 @@ function getAndroidPackages {
     echo "Installing target SDK for Android API level $ANDROID_SDK_TARGET_API_LEVEL..."
     sdkmanager "platforms;android-$ANDROID_SDK_TARGET_API_LEVEL"
     echo "Installing SDK build tools, revision $ANDROID_SDK_BUILD_TOOLS_REVISION..."
-    sdkmanager "build-tools;$ANDROID_SDK_BUILD_TOOLS_REVISION"
+    sdkmanager "build-tools;android-$ANDROID_SDK_BUILD_TOOLS_REVISION"
     echo "Installing Google APIs for Android API level $ANDROID_SDK_BUILD_API_LEVEL..."
     sdkmanager "add-ons;addon-google_apis-google-$ANDROID_SDK_BUILD_API_LEVEL"
     echo "Installing Android Support Repository"
