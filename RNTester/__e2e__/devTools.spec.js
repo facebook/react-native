848149545b24bbe8a32fd15c0d80133d6dd72b6c
Mike Grabowski <grabbou@gmail.com>
2018-05-21 20:39:16 +0200

    Implement device shake() and test for Chrome debugger

--- RNTester/__e2e__/devTools.spec.js
+++ RNTester/__e2e__/devTools.spec.js
@@ -9,7 +9,16 @@
 'use strict';
 
 describe('DevTools', () => {
+  /**
+   * Test whether Chrome Debugger works by turning it on
+   * and seeing whether it refreshes the app correctly
+   */
   it('should have `Debug In Chrome` working', async () => {
-
+    await device.shake();
+    await element(by.text('Debug JS Remotely')).tap();
+    await expect(element(by.text('RNTester'))).toBeVisible();
+    await device.shake();
+    await element(by.text('Stop Remote JS Debugging')).tap();
+    await waitFor(element(by.text('RNTester'))).toBeVisible().withTimeout(2000);
   });
 });
