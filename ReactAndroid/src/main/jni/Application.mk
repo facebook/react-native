c85b48d4d25f0bbf3c2b7e17c066a5b151e3326d
Muhammad Arafat <3rafat@gmail.com>
2018-05-22 17:39:31 -0400

    progress 1

--- ReactAndroid/src/main/jni/Application.mk
+++ ReactAndroid/src/main/jni/Application.mk
@@ -1,15 +1,15 @@
 APP_BUILD_SCRIPT := Android.mk
 
 APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
-APP_PLATFORM := android-9
+APP_PLATFORM := android-16
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(THIRD_PARTY_NDK_DIR)$(HOST_DIRSEP)$(REACT_COMMON_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)first-party
 
-APP_STL := gnustl_shared
+APP_STL := c++_shared
 
 # Make sure every shared lib includes a .note.gnu.build-id header
 APP_LDFLAGS := -Wl,--build-id
 
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := clang
