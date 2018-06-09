b309cd878c642328223c0f66cf87ab0e1d93d746
Valentin Shergin <shergin@fb.com>
2018-06-08 20:16:17 -0700

    Fabric: Data model of Touch Events
    Summary: The data model of Touch events and payload serialization. The implementation mimics W3C standard and current React Native implementation.
    
    Reviewed By: fkgozali
    
    Differential Revision: D8246711
    
    fbshipit-source-id: 955b2068674f290d8bdb82da1ebfb796dd32971b

--- ReactCommon/fabric/view/ViewEventHandlers.cpp
+++ ReactCommon/fabric/view/ViewEventHandlers.cpp
@@ -38,5 +38,54 @@ void ViewEventHandlers::onLayout(const LayoutMetrics &layoutMetrics) const {
   dispatchEvent("layout", payload);
 }
 
+#pragma mark - Touches
+
+static folly::dynamic touchPayload(const Touch &touch) {
+  folly::dynamic object = folly::dynamic::object();
+  object["locationX"] = touch.offsetPoint.x;
+  object["locationY"] = touch.offsetPoint.x;
+  object["pageX"] = touch.pagePoint.x;
+  object["pageY"] = touch.pagePoint.x;
+  object["screenX"] = touch.screenPoint.x;
+  object["screenY"] = touch.screenPoint.x;
+  object["identifier"] = touch.identifier;
+  object["target"] = touch.target;
+  object["timestamp"] = touch.timestamp * 1000;
+  object["force"] = touch.force;
+  return object;
+}
+
+static folly::dynamic touchesPayload(const Touches &touches) {
+  folly::dynamic array = folly::dynamic::array();
+  for (auto &&touch : touches) {
+    array.push_back(touchPayload(touch));
+  }
+  return array;
+}
+
+static folly::dynamic touchEventPayload(const TouchEvent &event) {
+  folly::dynamic object = folly::dynamic::object();
+  object["touches"] = touchesPayload(event.touches);
+  object["changedTouches"] = touchesPayload(event.changedTouches);
+  object["targetTouches"] = touchesPayload(event.targetTouches);
+  return object;
+}
+
+void ViewEventHandlers::onTouchStart(const TouchEvent &event) const {
+  dispatchEvent("touchStart", touchEventPayload(event));
+}
+
+void ViewEventHandlers::onTouchMove(const TouchEvent &event) const {
+  dispatchEvent("touchMove", touchEventPayload(event));
+}
+
+void ViewEventHandlers::onTouchEnd(const TouchEvent &event) const {
+  dispatchEvent("touchEnd", touchEventPayload(event));
+}
+
+void ViewEventHandlers::onTouchCancel(const TouchEvent &event) const {
+  dispatchEvent("touchCancel", touchEventPayload(event));
+}
+
 } // namespace react
 } // namespace facebook
