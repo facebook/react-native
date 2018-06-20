facebook/react-native

commit 6bbaff2944dafd6fa7e5b77ef46dece0ec2c9983

Upgrade to OkHttp3
Summary:
Update to [OkHttp](https://github.com/square/okhttp) to [OkHttp3](https://publicobject.com/2015/12/12/com-squareup-okhttp3/)

We must also update:
- Fresco to 0.10.0
- okio to 1.8.0

**Motivation**
Reasons for upgrading:
* Issue #4021
* "We discovered that RN Android sometimes fails to connect to the latest stable version of NGINX when HTTP/2 is enabled. We aren't seeing errors with other HTTP clients so we think it's specific to RN and OkHttp. Square has fixed several HTTP/2 bugs over the past eight months." - ide
* OkHttp3 will be maintained & improved, but OkHttp2 will only receive [security fixes](https://publicobject.com/2016/02/11/okhttp-certificate-pinning-vulnerability/)
* Cleaner APIs - "Get and Set prefixes are avoided"
* Deprecated/Removed - HttpURLConnection & Apache HTTP
* React Native apps are currently being forced to bundle two versions of OkHttp (v2 & v3), if another library uses v3
* Improved WebSocket performance - [CHANGELOG.md](https://github.com/square/okhttp/blob/master
Closes https://github.com/facebook/react-native/pull/6113

Reviewed By: andreicoman11, lexs

Differential Revision: D3292375

Pulled By: bestander

fbshipit-source-id: 7c7043eaa2ea63f95854108b401c4066098d67f7

ReactAndroid/src/main/java/com/facebook/react/devsupport/DevServerHelper.java
@@ -17,14 +17,8 @@ import com.facebook.common.logging.FLog;
 import com.facebook.infer.annotation.Assertions;
 import com.facebook.react.bridge.UiThreadUtil;
 import com.facebook.react.common.ReactConstants;
+import com.facebook.react.common.network.OkHttpCallUtil;
 import com.facebook.react.modules.systeminfo.AndroidInfoHelpers;
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.ConnectionPool;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-import com.squareup.okhttp.ResponseBody;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,6 +27,13 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nullable;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import okio.Okio;
 import okio.Sink;
 
@@ -91,12 +92,12 @@ public class DevServerHelper {
 
   public DevServerHelper(DevInternalSettings settings) {
     mSettings = settings;
-    mClient = new OkHttpClient();
-    mClient.setConnectTimeout(HTTP_CONNECT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+    mClient = new OkHttpClient.Builder()
+      .connectTimeout(HTTP_CONNECT_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+      .readTimeout(0, TimeUnit.MILLISECONDS)
+      .writeTimeout(0, TimeUnit.MILLISECONDS)
+      .build();
 
-    // No read or write timeouts by default
-    mClient.setReadTimeout(0, TimeUnit.MILLISECONDS);
-    mClient.setWriteTimeout(0, TimeUnit.MILLISECONDS);
     mRestartOnChangePollingHandler = new Handler();
   }
 
@@ -176,7 +177,7 @@ public class DevServerHelper {
     mDownloadBundleFromURLCall = Assertions.assertNotNull(mClient.newCall(request));
     mDownloadBundleFromURLCall.enqueue(new Callback() {
       @Override
-      public void onFailure(Request request, IOException e) {
+      public void onFailure(Call call, IOException e) {
         // ignore callback if call was cancelled
         if (mDownloadBundleFromURLCall == null || mDownloadBundleFromURLCall.isCanceled()) {
           mDownloadBundleFromURLCall = null;
@@ -191,12 +192,12 @@ public class DevServerHelper {
           .append("\u2022 Ensure that your device/emulator is connected to your machine and has USB debugging enabled - run 'adb devices' to see a list of connected devices\n")
           .append("\u2022 If you're on a physical device connected to the same machine, run 'adb reverse tcp:8081 tcp:8081' to forward requests from your device\n")
           .append("\u2022 If your device is on the same Wi-Fi network, set 'Debug server host & port for device' in 'Dev settings' to your machine's IP address and the port of the local dev server - e.g. 10.0.1.1:8081\n\n")
-          .append("URL: ").append(request.urlString());
+          .append("URL: ").append(call.request().url().toString());
         callback.onFailure(new DebugServerException(sb.toString()));
       }
 
       @Override
-      public void onResponse(Response response) throws IOException {
+      public void onResponse(Call call, Response response) throws IOException {
         // ignore callback if call was cancelled
         if (mDownloadBundleFromURLCall == null || mDownloadBundleFromURLCall.isCanceled()) {
           mDownloadBundleFromURLCall = null;
@@ -213,7 +214,7 @@ public class DevServerHelper {
           } else {
             StringBuilder sb = new StringBuilder();
             sb.append("The development server returned response error code: ").append(response.code()).append("\n\n")
-              .append("URL: ").append(request.urlString()).append("\n\n")
+              .append("URL: ").append(call.request().url().toString()).append("\n\n")
               .append("Body:\n")
               .append(body);
             callback.onFailure(new DebugServerException(sb.toString()));
@@ -251,7 +252,7 @@ public class DevServerHelper {
     mClient.newCall(request).enqueue(
         new Callback() {
           @Override
-          public void onFailure(Request request, IOException e) {
+          public void onFailure(Call call, IOException e) {
             FLog.w(
                 ReactConstants.TAG,
                 "The packager does not seem to be running as we got an IOException requesting " +
@@ -260,7 +261,7 @@ public class DevServerHelper {
           }
 
           @Override
-          public void onResponse(Response response) throws IOException {
+          public void onResponse(Call call, Response response) throws IOException {
             if (!response.isSuccessful()) {
               FLog.e(
                   ReactConstants.TAG,
@@ -297,7 +298,7 @@ public class DevServerHelper {
     mOnChangePollingEnabled = false;
     mRestartOnChangePollingHandler.removeCallbacksAndMessages(null);
     if (mOnChangePollingClient != null) {
-      mOnChangePollingClient.cancel(this);
+      OkHttpCallUtil.cancelTag(mOnChangePollingClient, this);
       mOnChangePollingClient = null;
     }
     mOnServerContentChangeListener = null;
@@ -311,10 +312,10 @@ public class DevServerHelper {
     }
     mOnChangePollingEnabled = true;
     mOnServerContentChangeListener = onServerContentChangeListener;
-    mOnChangePollingClient = new OkHttpClient();
-    mOnChangePollingClient
-        .setConnectionPool(new ConnectionPool(1, LONG_POLL_KEEP_ALIVE_DURATION_MS))
-        .setConnectTimeout(HTTP_CONNECT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+    mOnChangePollingClient = new OkHttpClient.Builder()
+        .connectionPool(new ConnectionPool(1, LONG_POLL_KEEP_ALIVE_DURATION_MS, TimeUnit.MINUTES))
+        .connectTimeout(HTTP_CONNECT_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+        .build();
     enqueueOnChangeEndpointLongPolling();
   }
 
@@ -338,7 +339,7 @@ public class DevServerHelper {
     Request request = new Request.Builder().url(createOnChangeEndpointUrl()).tag(this).build();
     Assertions.assertNotNull(mOnChangePollingClient).newCall(request).enqueue(new Callback() {
       @Override
-      public void onFailure(Request request, IOException e) {
+      public void onFailure(Call call, IOException e) {
         if (mOnChangePollingEnabled) {
           // this runnable is used by onchange endpoint poller to delay subsequent requests in case
           // of a failure, so that we don't flood network queue with frequent requests in case when
@@ -356,7 +357,7 @@ public class DevServerHelper {
       }
 
       @Override
-      public void onResponse(Response response) throws IOException {
+      public void onResponse(Call call, Response response) throws IOException {
         handleOnChangePollingResponse(response.code() == 205);
       }
     });
@@ -376,13 +377,13 @@ public class DevServerHelper {
         .build();
     mClient.newCall(request).enqueue(new Callback() {
       @Override
-      public void onFailure(Request request, IOException e) {
+      public void onFailure(Call call, IOException e) {
         // ignore HTTP call response, this is just to open a debugger page and there is no reason
         // to report failures from here
       }
 
       @Override
-      public void onResponse(Response response) throws IOException {
+      public void onResponse(Call call, Response response) throws IOException {
         // ignore HTTP call response - see above
       }
     });

ReactAndroid/build.gradle
@@ -259,13 +259,14 @@ dependencies {
     compile fileTree(dir: 'src/main/third-party/java/infer-annotations/', include: ['*.jar'])
     compile 'com.android.support:appcompat-v7:23.0.1'
     compile 'com.android.support:recyclerview-v7:23.0.1'
-    compile 'com.facebook.fresco:fresco:0.8.1'
-    compile 'com.facebook.fresco:imagepipeline-okhttp:0.8.1'
+    compile 'com.facebook.fresco:fresco:0.10.0'
+    compile 'com.facebook.fresco:imagepipeline-okhttp3:0.10.0'
     compile 'com.fasterxml.jackson.core:jackson-core:2.2.3'
     compile 'com.google.code.findbugs:jsr305:3.0.0'
-    compile 'com.squareup.okhttp:okhttp:2.5.0'
-    compile 'com.squareup.okhttp:okhttp-ws:2.5.0'
-    compile 'com.squareup.okio:okio:1.6.0'
+    compile 'com.squareup.okhttp3:okhttp:3.2.0'
+    compile 'com.squareup.okhttp3:okhttp-urlconnection:3.2.0'
+    compile 'com.squareup.okhttp3:okhttp-ws:3.2.0'
+    compile 'com.squareup.okio:okio:1.8.0'
     compile 'org.webkit:android-jsc:r174650'
 
     testCompile "junit:junit:${JUNIT_VERSION}"

ReactAndroid/src/main/java/com/facebook/react/bridge/BUCK
@@ -21,16 +21,16 @@ android_library(
     react_native_dep('java/com/facebook/proguard/annotations:annotations'),
   ],
   deps = [
-    react_native_target('java/com/facebook/react/common:common'),
     react_native_dep('java/com/facebook/systrace:systrace'),
     react_native_dep('libraries/fbcore/src/main/java/com/facebook/common/logging:logging'),
     react_native_dep('libraries/soloader/java/com/facebook/soloader:soloader'),
     react_native_dep('third-party/java/infer-annotations:infer-annotations'),
     react_native_dep('third-party/java/jackson:core'),
     react_native_dep('third-party/java/jsr-305:jsr-305'),
+    react_native_dep('third-party/java/okhttp:okhttp3'),
+    react_native_dep('third-party/java/okhttp:okhttp3-ws'),
     react_native_dep('third-party/java/okio:okio'),
-    react_native_dep('third-party/java/okhttp:okhttp'),
-    react_native_dep('third-party/java/okhttp:okhttp-ws'),
+    react_native_target('java/com/facebook/react/common:common'),
   ],
   visibility = [
     'PUBLIC',

ReactAndroid/src/main/java/com/facebook/react/bridge/JSDebuggerWebSocketClient.java
@@ -25,14 +25,15 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-import com.squareup.okhttp.ws.WebSocket;
-import com.squareup.okhttp.ws.WebSocketCall;
-import com.squareup.okhttp.ws.WebSocketListener;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okhttp3.ws.WebSocket;
+import okhttp3.ws.WebSocketCall;
+import okhttp3.ws.WebSocketListener;
 import okio.Buffer;
-import okio.BufferedSource;
 
 /**
  * A wrapper around WebSocketClient that recognizes RN debugging message format.
@@ -59,11 +60,11 @@ public class JSDebuggerWebSocketClient implements WebSocketListener {
       throw new IllegalStateException("JSDebuggerWebSocketClient is already initialized.");
     }
     mConnectCallback = callback;
-    mHttpClient = new OkHttpClient();
-    mHttpClient.setConnectTimeout(10, TimeUnit.SECONDS);
-    mHttpClient.setWriteTimeout(10, TimeUnit.SECONDS);
-    // Disable timeouts for read
-    mHttpClient.setReadTimeout(0, TimeUnit.MINUTES);
+    mHttpClient = new OkHttpClient.Builder()
+      .connectTimeout(10, TimeUnit.SECONDS)
+      .writeTimeout(10, TimeUnit.SECONDS)
+      .readTimeout(0, TimeUnit.MINUTES) // Disable timeouts for read
+      .build();
 
     Request request = new Request.Builder().url(url).build();
     WebSocketCall call = WebSocketCall.create(mHttpClient, request);
@@ -162,10 +163,8 @@ public class JSDebuggerWebSocketClient implements WebSocketListener {
           new IllegalStateException("WebSocket connection no longer valid"));
       return;
     }
-    Buffer messageBuffer = new Buffer();
-    messageBuffer.writeUtf8(message);
     try {
-      mWebSocket.sendMessage(WebSocket.PayloadType.TEXT, messageBuffer);
+      mWebSocket.sendMessage(RequestBody.create(WebSocket.TEXT, message));
     } catch (IOException e) {
       triggerRequestFailure(requestID, e);
     }
@@ -188,17 +187,17 @@ public class JSDebuggerWebSocketClient implements WebSocketListener {
   }
 
   @Override
-  public void onMessage(BufferedSource payload, WebSocket.PayloadType type) throws IOException {
-    if (type != WebSocket.PayloadType.TEXT) {
-      FLog.w(TAG, "Websocket received unexpected message with payload of type " + type);
+  public void onMessage(ResponseBody response) throws IOException {
+    if (response.contentType() != WebSocket.TEXT) {
+      FLog.w(TAG, "Websocket received unexpected message with payload of type " + response.contentType());
       return;
     }
 
     String message = null;
     try {
-      message = payload.readUtf8();
+      message = response.source().readUtf8();
     } finally {
-      payload.close();
+      response.close();
     }
     Integer replyID = null;
 

ReactAndroid/src/main/java/com/facebook/react/bridge/webworkers/WebWorkers.java
@@ -18,9 +18,9 @@ import com.facebook.react.bridge.queue.MessageQueueThreadImpl;
 import com.facebook.react.bridge.queue.ProxyQueueThreadExceptionHandler;
 import com.facebook.react.common.build.ReactBuildConfig;
 
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okio.Okio;
 import okio.Sink;
 

ReactAndroid/src/main/java/com/facebook/react/common/BUCK
@@ -7,6 +7,7 @@ android_library(
     ':build_config',
     react_native_dep('third-party/java/infer-annotations:infer-annotations'),
     react_native_dep('third-party/java/jsr-305:jsr-305'),
+    react_native_dep('third-party/java/okhttp:okhttp3'),
   ],
   exported_deps = [
     react_native_dep('java/com/facebook/proguard/annotations:annotations'),

ReactAndroid/src/main/java/com/facebook/react/common/network/OkHttpCallUtil.java
@@ -1,0 +1,35 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ * <p/>
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.react.common.network;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+
+/**
+ * Helper class that provides the necessary methods for canceling queued and running OkHttp calls
+ */
+public class OkHttpCallUtil {
+
+  private OkHttpCallUtil() {
+  }
+
+  public static void cancelTag(OkHttpClient client, Object tag) {
+    for (Call call : client.dispatcher().queuedCalls()) {
+      if (tag.equals(call.request().tag())) {
+        call.cancel();
+      }
+    }
+    for (Call call : client.dispatcher().runningCalls()) {
+      if (tag.equals(call.request().tag())) {
+        call.cancel();
+      }
+    }
+  }
+}

ReactAndroid/src/main/java/com/facebook/react/devsupport/BUCK
@@ -5,17 +5,16 @@ android_library(
   manifest = 'AndroidManifest.xml',
   srcs = glob(['**/*.java']),
   deps = [
-    react_native_target('res:devsupport'),
+    react_native_dep('libraries/fbcore/src/main/java/com/facebook/common/logging:logging'),
+    react_native_dep('third-party/java/infer-annotations:infer-annotations'),
+    react_native_dep('third-party/java/jsr-305:jsr-305'),
+    react_native_dep('third-party/java/okhttp:okhttp3'),
+    react_native_dep('third-party/java/okio:okio'),
     react_native_target('java/com/facebook/react/bridge:bridge'),
     react_native_target('java/com/facebook/react/common:common'),
     react_native_target('java/com/facebook/react/modules/debug:debug'),
     react_native_target('java/com/facebook/react/modules/systeminfo:systeminfo'),
-    react_native_dep('libraries/fbcore/src/main/java/com/facebook/common/logging:logging'),
-    react_native_dep('third-party/java/infer-annotations:infer-annotations'),
-    react_native_dep('third-party/java/jsr-305:jsr-305'),
-    react_native_dep('third-party/java/okhttp:okhttp'),
-    react_native_dep('third-party/java/okio:okio'),
-
+    react_native_target('res:devsupport'),
   ],
   visibility = [
     'PUBLIC',

commit f77c9ae604d346ee129f424ef635f88d8526fa6c

Fix wrong time unit in dev server OKHttp client.
The keep-alive-duration for the connection pool for mOnChangePollingClient
is a number in milliseconds, but was mistakenly given in minutes. This mistake
was introduced in 6bbaff2944d.

ReactAndroid/src/main/java/com/facebook/react/devsupport/DevServerHelper.java
@@ -518,7 +518,7 @@ public class DevServerHelper {
     mOnChangePollingEnabled = true;
     mOnServerContentChangeListener = onServerContentChangeListener;
     mOnChangePollingClient = new OkHttpClient.Builder()
-        .connectionPool(new ConnectionPool(1, LONG_POLL_KEEP_ALIVE_DURATION_MS, TimeUnit.MINUTES))
+        .connectionPool(new ConnectionPool(1, LONG_POLL_KEEP_ALIVE_DURATION_MS, TimeUnit.MILLISECONDS))
         .connectTimeout(HTTP_CONNECT_TIMEOUT_MS, TimeUnit.MILLISECONDS)
         .build();
     enqueueOnChangeEndpointLongPolling();