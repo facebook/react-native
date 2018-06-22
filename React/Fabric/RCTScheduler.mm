d8c6a49df5bebe77937a0c4c6c7ce292da362a7f
Valentin Shergin <shergin@fb.com>
2018-06-22 07:28:38 -0700

    Fabric: Introducing ImageManager
    Summary:
    @public
    ImageManager coordinates all work related to loading image bitmaps for <Image> component.
    The particular iOS implementation uses RCTImageLoader from RCTImage module under the hood.
    
    Reviewed By: fkgozali
    
    Differential Revision: D8526571
    
    fbshipit-source-id: a0d927972d30113eed6e0cd169fceee17610181d

--- React/Fabric/RCTScheduler.mm
+++ React/Fabric/RCTScheduler.mm
@@ -7,9 +7,12 @@
 
 #import "RCTScheduler.h"
 
+#import <fabric/imagemanager/ImageManager.h>
 #import <fabric/uimanager/ContextContainer.h>
 #import <fabric/uimanager/Scheduler.h>
 #import <fabric/uimanager/SchedulerDelegate.h>
+#import <React/RCTImageLoader.h>
+#import <React/RCTBridge+Private.h>
 
 #import "RCTConversions.h"
 
@@ -44,6 +47,10 @@ private:
     _delegateProxy = std::make_shared<SchedulerDelegateProxy>((__bridge void *)self);
 
     SharedContextContainer contextContainer = std::make_shared<ContextContainer>();
+
+    void *imageLoader = (__bridge void *)[[RCTBridge currentBridge] imageLoader];
+    contextContainer->registerInstance(typeid(ImageManager), std::make_shared<ImageManager>(imageLoader));
+
     _scheduler = std::make_shared<Scheduler>(contextContainer);
     _scheduler->setDelegate(_delegateProxy.get());
   }
