5786db3a26c2d28863901a354602290aa4af7f77
Valentin Shergin <shergin@fb.com>
2018-07-01 21:22:21 -0700

    Fabric: Making `fabric/graphics` compilable on Android
    Summary:
    @public
    This compiles but this does not work.
    To make it actually work we have to implement all missing functions in `Color.cpp` and co.
    
    Reviewed By: fkgozali
    
    Differential Revision: D8655537
    
    fbshipit-source-id: 564fb7131445af81cf05407239dc6ba870cf6b83

---
+++ ReactCommon/fabric/graphics/platform/android/Color.h
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <fabric/graphics/ColorComponents.h>
+
+namespace facebook {
+namespace react {
+
+using Color = float;
+using SharedColor = std::shared_ptr<Color>;
+
+SharedColor colorFromComponents(ColorComponents components);
+ColorComponents colorComponentsFromColor(SharedColor color);
+
+} // namespace react
+} // namespace facebook
