e183844df548917fc666a82d728f2fe293c388ac
Maël Nison <mael@fb.com>
2018-07-19 05:01:23 -0700

    Uniformizes how THIS_DIR is computed in xplat/js
    Summary: This form of THIS_DIR resolves symlinks, which is better.
    
    Reviewed By: davidaurelio
    
    Differential Revision: D8661886
    
    fbshipit-source-id: 90bf329e765d9623d103b03c5dd3b71fae9d9854

--- scripts/packager.sh
+++ scripts/packager.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Copyright (c) 2015-present, Facebook, Inc.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-THIS_DIR=$(dirname "$0")
+THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
 source "${THIS_DIR}/.packager.env"
 cd "$THIS_DIR/.."
 node "./local-cli/cli.js" start "$@"
