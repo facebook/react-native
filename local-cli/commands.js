ebf5aeab280f2ebc439ec39d25c48fdf1980cf73
Jean Lauliac <jeanlauliac@fb.com>
2018-07-20 09:17:21 -0700

    BREAKING: metro: rename 'unbundle' to 'ram bundle'
    Summary:
    @public
    
    Let's get rid of the "unbundle" terminology and instead use "RAM bundle", short for "Random Access Bundle" format. THIS IS A BREAKING CHANGE FOR OSS, as the command becomes `ram-bundle` instead of `unbundle`. It realy shouldn't be a command to start with (only a "format" specifier for the `bundle` command), but I don't want to do that change at this point.
    
    Reviewed By: davidaurelio
    
    Differential Revision: D8894433
    
    fbshipit-source-id: 5565f9ae94c7c2d7f6b25f95ae45b64f27f6aec8

--- local-cli/commands.js
+++ local-cli/commands.js
@@ -42,6 +42,7 @@ const documentedCommands = [
   require('./library/library'),
   require('./bundle/bundle'),
   require('./bundle/unbundle'),
+  require('./bundle/ramBundle'),
   require('./eject/eject'),
   require('./link/link'),
   require('./link/unlink'),
