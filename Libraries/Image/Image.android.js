9f8b5a9ed51ed39bc91059115d71528d825f9010
Himabindu Gadupudi <himabindug@fb.com>
2018-07-31 11:49:07 -0700

    If source is null , source uri is null or source is not an array should respect style
    Summary: If source is null , source uri is null or source is not an array should respect style like in iOS
    
    Reviewed By: yungsters
    
    Differential Revision: D9018005
    
    fbshipit-source-id: 5f695e8e3007c96e6004973e7fcbc6b57cc15249

--- Libraries/Image/Image.android.js
+++ Libraries/Image/Image.android.js
@@ -184,14 +184,11 @@ let Image = (
   props: ImagePropsType,
   forwardedRef: ?React.Ref<'RCTTextInlineImage' | 'RKImage'>,
 ) => {
-  const source = resolveAssetSource(props.source);
+  let source = resolveAssetSource(props.source);
   const defaultSource = resolveAssetSource(props.defaultSource);
   const loadingIndicatorSource = resolveAssetSource(
     props.loadingIndicatorSource,
   );
-
-  // As opposed to the ios version, here we render `null` when there is no source, source.uri
-  // or source array.
 
   if (source && source.uri === '') {
     console.warn('source.uri should not be an empty string');
@@ -215,13 +212,13 @@ let Image = (
     );
   }
 
-  if (!source || (!source.uri && !Array.isArray(source))) {
-    return null;
+  if (source && !source.uri && !Array.isArray(source)) {
+    source = null;
   }
 
   let style;
   let sources;
-  if (source.uri) {
+  if (source?.uri != null) {
     const {width, height} = source;
     style = flattenStyle([{width, height}, styles.base, props.style]);
     sources = [{uri: source.uri}];
@@ -235,7 +232,7 @@ let Image = (
     style,
     shouldNotifyLoadEvents: !!(onLoadStart || onLoad || onLoadEnd || onError),
     src: sources,
-    headers: source.headers,
+    headers: source?.headers,
     defaultSrc: defaultSource ? defaultSource.uri : null,
     loadingIndicatorSrc: loadingIndicatorSource
       ? loadingIndicatorSource.uri
