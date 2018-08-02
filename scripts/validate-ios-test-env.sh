529b19da46e83178cd88e77c6b6de604c119906f
Héctor Ramos <165856+hramos@users.noreply.github.com>
2018-08-01 19:21:40 -0700

    Update validate-ios-test-env.sh

--- scripts/validate-ios-test-env.sh
+++ scripts/validate-ios-test-env.sh
@@ -12,9 +12,8 @@ function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$
 
 # Check that node is installed.
 if [ -z "$(which node)" ]; then
-  echo "You need to install nodejs."
-  echo "Note: Node 10 is not fully supported at the time and Node 6 is no longer supported."
-  echo "See https://nodejs.org/en/download/ for instructions."
+  echo "Could not find Node binary. Please check your nodejs install."
+  echo "See http://facebook.github.io/react-native/docs/getting-started.html for instructions."
   exit 1
 fi
 
@@ -29,25 +28,24 @@ if (( $(echo "${NODE_VERSION} <= 6.0" | bc -l) )); then
 fi
 
 if (( $(echo "${NODE_VERSION} == 10.0" | bc -l) )); then
-  echo "Node ${NODE_VERSION} detected. This version of Node is not supported."
-  echo "Note: Node 10 is not fully supported at the time and Node 6 is no longer supported."  
-  echo "See https://nodejs.org/en/download/ for instructions."
+  echo "Node ${NODE_VERSION} detected. This version of Node is not fully supported at this time."
+  echo "See https://github.com/facebook/react-native/issues/19229 for more information."
   exit 1
 fi
 
 # Check that Xcode is installed.
 if [ -z "$(which xcodebuild)" ]; then
-  echo "You need to install nodejs."
-  echo "Note: Node 10 is not fully supported at the time, Node 6 is no longer supported"  
-  echo "See https://nodejs.org/en/download/ for instructions."
+  echo "Could not find Xcode build tools. Please check your Xcode install."
+  echo "See http://facebook.github.io/react-native/docs/getting-started.html for instructions."
   exit 1
 fi
 
-MIN_XCODE_VERSION=8.0
+MIN_XCODE_VERSION=9.4
 # Check that the correct version of Xcode is installed
 XCODE_VERSION="$(command xcodebuild -version | sed '$ d' | sed 's/[-/a-zA-Z]//g')"
 if (version_gt $MIN_XCODE_VERSION $XCODE_VERSION) && [ "$XCODE_VERSION" != "$MIN_XCODE_VERSION" ]; then
-  echo "Xcode ${XCODE_VERSION} detected. Please upgrade to a later version using the AppStore."
+  echo "Xcode ${XCODE_VERSION} detected. React Native requires ${MIN_XCODE_VERSION} or newer."
   echo "Older versions of Xcode may cause cryptic build errors."
+  echo "See http://facebook.github.io/react-native/docs/getting-started.html for instructions."
   exit 1
 fi
