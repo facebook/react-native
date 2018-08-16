4ca949b46ec8fd72b5305daa06fac3ef58a8fa5f
Ramanpreet Nara <ramanpreet@fb.com>
2018-08-16 13:34:18 -0700

    Implement 'allowsInlineMediaPlayback` prop
    Summary:
    For iPhones with small screen sizes (e.g: iPhone 5s), inside the `<WKWebView/>` component, videos will play in fullscreen mode. In this diff, I introduce a prop called `allowsInlineMediaPlayback` that when set to true, will allow videos to play inline.
    
    **Note:** For videos to play inline, the HTML video element must also have a `playsinline` attribute on it.
    
    Reviewed By: shergin
    
    Differential Revision: D6379770
    
    fbshipit-source-id: a0130720ffede6c24a90cad0c97a75b657d77017

--- React/Views/RCTWKWebView.h
+++ React/Views/RCTWKWebView.h
@@ -27,6 +27,8 @@ shouldStartLoadForRequest:(NSMutableDictionary<NSString *, id> *)request
 @property (nonatomic, copy) NSString *injectedJavaScript;
 @property (nonatomic, assign) BOOL scrollEnabled;
 @property (nonatomic, assign) CGFloat decelerationRate;
+@property (nonatomic, assign) BOOL allowsInlineMediaPlayback;
+@property (nonatomic, assign) BOOL bounces;
 
 - (void)postMessage:(NSString *)message;
 - (void)injectJavaScript:(NSString *)script;
