5a440ca03f32bd9c400d95c67069ff3a8fcc2a41
Salakar <mike.diarmid@gmail.com>
2018-08-28 18:31:54 +0100

    Switch to `metro-react-native-babel-preset` and remove now redundant Babel 7 devDeps.
    Additionally update metro dependencies to ^0.44.0 to keep Babel 7 versions consistent.

--- babel.config.js
+++ babel.config.js
@@ -1,42 +1,12 @@
 module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    '@babel/plugin-transform-block-scoping',
-    '@babel/plugin-transform-flow-strip-types',
     [
-      '@babel/plugin-proposal-class-properties',
-      {loose: false},
-    ],
-    ['@babel/plugin-transform-computed-properties'],
-    ['@babel/plugin-transform-destructuring'],
-    ['@babel/plugin-transform-function-name'],
-    ['@babel/plugin-transform-literals'],
-    ['@babel/plugin-transform-parameters'],
-    ['@babel/plugin-transform-shorthand-properties'],
-    ['@babel/plugin-transform-react-jsx'],
-    ['@babel/plugin-transform-regenerator'],
-    ['@babel/plugin-transform-runtime'],
-    ['@babel/plugin-transform-sticky-regex'],
-    ['@babel/plugin-transform-unicode-regex'],
-    [
-      '@babel/plugin-transform-modules-commonjs',
+      require('@babel/plugin-transform-runtime'),
       {
-        strict: false,
-        strictMode: false, // prevent "use strict" injections
-        allowTopLevelThis: true, // dont rewrite global `this` -> `undefined`
+        helpers: true,
+        regenerator: true,
       },
     ],
-    '@babel/plugin-transform-arrow-functions',
-    '@babel/plugin-transform-classes',
-    ['@babel/plugin-transform-for-of', {loose: true}],
-    '@babel/plugin-transform-spread',
-    [
-      '@babel/plugin-transform-template-literals',
-      {loose: true},
-    ],
-    '@babel/plugin-transform-exponentiation-operator',
-    '@babel/plugin-transform-object-assign',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-transform-react-display-name',
   ],
 };
