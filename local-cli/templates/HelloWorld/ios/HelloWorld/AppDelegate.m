d985b2e6615bcc49740e8c65485a6e0251eae0cc
Peter Keller <peterkeller@protonmail.com>
2018-09-05 13:04:31 -0700

    Fix orientation change issue in HelloWord template (#20945)
    Summary:
    This commit is related to issue #16060
    Tested through the iOS simulator. Solution came from https://moduscreate.com/blog/changing-the-react-native-rootview-background-color-ios-and-android/
    Pull Request resolved: https://github.com/facebook/react-native/pull/20945
    
    Differential Revision: D9661761
    
    Pulled By: hramos
    
    fbshipit-source-id: ca8dd3fd09a0b0e89e1598a8114800808efa7d21

--- local-cli/templates/HelloWorld/ios/HelloWorld/AppDelegate.m
+++ local-cli/templates/HelloWorld/ios/HelloWorld/AppDelegate.m
@@ -22,7 +22,7 @@
                                                       moduleName:@"HelloWorld"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [UIColor blackColor];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
