f000d2ca736ade5466a82a709d6189642b6b97f7
David Vacca <dvacca@fb.com>
2018-09-16 23:46:09 -0700

    Introduce toDynamic conversion for AttributedString class
    Summary: This diff introduces a way to convert AttributedString object to dynamic objects
    
    Reviewed By: shergin
    
    Differential Revision: D9801438
    
    fbshipit-source-id: b762f54917ae90bf53c7f9d07f63b876d1265ece

--- ReactCommon/fabric/attributedstring/tests/AttributedStringTest.cpp
+++ ReactCommon/fabric/attributedstring/tests/AttributedStringTest.cpp
@@ -7,8 +7,40 @@
 
 #include <memory>
 
+#include <fabric/attributedstring/TextAttributes.h>
+#include <fabric/attributedstring/conversions.h>
+#include <fabric/attributedstring/primitives.h>
+#include <fabric/graphics/conversions.h>
 #include <gtest/gtest.h>
+#include <assert.h>
+#include <string>
+#include <iostream>
+namespace facebook {
+namespace react {
 
 TEST(AttributedStringTest, testSomething) {
-  // TODO
+  auto attString = new AttributedString();
+  auto fragment = new AttributedString::Fragment();
+  fragment->string = "test";
+
+  auto text = new TextAttributes();
+  text->foregroundColor = {colorFromComponents({100/255.0, 153/255.0, 200/255.0, 1.0})};
+  text->opacity = 0.5;
+  text->fontStyle = FontStyle::Italic;
+  text->fontWeight = FontWeight::Thin;
+  text->fontVariant = FontVariant::TabularNums;
+  fragment->textAttributes = *text;
+
+  attString->prependFragment(*fragment);
+
+  auto result = toDynamic(*attString);
+  assert(result["string"] == fragment->string);
+  auto textAttribute = result["fragments"][0]["textAttributes"];
+  assert(textAttribute["foregroundColor"] == toDynamic(text->foregroundColor));
+  assert(textAttribute["opacity"] == text->opacity);
+  assert(textAttribute["fontStyle"] == toString(*text->fontStyle));
+  assert(textAttribute["fontWeight"] == toString(*text->fontWeight));
+}
+
+}
 }
