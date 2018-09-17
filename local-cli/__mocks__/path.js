139292918baff6f6d385ccd39e3613e1d38f568e
Jean Lauliac <jeanlauliac@fb.com>
2018-09-17 05:43:43 -0700

    react-native: make automated fs-based tests platform-independent
    Summary:
    @public
    
    These tests are using a mock memory FS to start with, so there is no reason at all they should depend on the host OS or filesystem details. This changeset fixes that so that we fully mock the `fs` and `path` modules dependending on the mock platform (not the host platform). I also added an example of how we can test both platforms (regardless of the host platform) in `findPackageClassName`. Follow up changeset will be to do the same for all the other affected tests.
    
    Related to https://github.com/facebook/react-native/issues/20260.
    
    Reviewed By: mjesun
    
    Differential Revision: D9771024
    
    fbshipit-source-id: b368b43e8e54292d33b6183eec9a9ea69f2e6e76

---
+++ local-cli/__mocks__/path.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+'use strict';
+
+const mockPath = {};
+
+function reset(platform) {
+  Object.assign(mockPath, jest.requireActual('path')[platform]);
+}
+
+mockPath.mock = {reset};
+
+module.exports = mockPath;
