9acad0a632a9b83012529f06bbe3c2d6f4b18008
Wayne Cheng <wcheng@fb.com>
2018-09-27 15:19:44 -0700

    Add @flow to BackHandler
    Summary:
    Add flow to BackHandler
    Fixed callsites
    
    Reviewed By: TheSavior
    
    Differential Revision: D8994015
    
    fbshipit-source-id: b3d8d53014787ef1855336e0c2d0759731e9c84e

--- Libraries/Utilities/BackHandler.ios.js
+++ Libraries/Utilities/BackHandler.ios.js
@@ -8,6 +8,7 @@
  * On iOS, this just implements a stub.
  *
  * @format
+ * @flow
  */
 
 'use strict';
@@ -17,9 +18,10 @@ const TVEventHandler = require('TVEventHandler');
 
 type BackPressEventName = $Enum<{
   backPress: string,
+  hardwareBackPress: string,
 }>;
 
-function emptyFunction() {}
+function emptyFunction(): void {}
 
 /**
  * Detect hardware button presses for back navigation.
@@ -100,12 +102,12 @@ if (Platform.isTV) {
 } else {
   BackHandler = {
     exitApp: emptyFunction,
-    addEventListener() {
+    addEventListener(_eventName: BackPressEventName, _handler: Function) {
       return {
         remove: emptyFunction,
       };
     },
-    removeEventListener: emptyFunction,
+    removeEventListener(_eventName: BackPressEventName, _handler: Function) {},
   };
 }
 
