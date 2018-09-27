396b19dde7d936859365c3fabafb4a722bd78f3a
Thomas BARRAS <thomas.barras26@gmail.com>
2018-09-27 14:17:24 -0400

    UPDATE: DeprecatedTVViewPropTypes clean + rm comments

--- Libraries/DeprecatedPropTypes/DeprecatedTVViewPropTypes.js
+++ Libraries/DeprecatedPropTypes/DeprecatedTVViewPropTypes.js
@@ -12,64 +12,13 @@
 'use strict';
 var PropTypes = require('prop-types');
 
-/**
- * Additional View properties for Apple TV
- */
-var DeprecatedTVViewPropTypes = {
-  /**
-   * *(Apple TV only)* When set to true, this view will be focusable
-   * and navigable using the Apple TV remote.
-   *
-   * @platform ios
-   */
+const DeprecatedTVViewPropTypes = {
   isTVSelectable: PropTypes.bool,
-
-  /**
-   * *(Apple TV only)* May be set to true to force the Apple TV focus engine to move focus to this view.
-   *
-   * @platform ios
-   */
   hasTVPreferredFocus: PropTypes.bool,
-
-  /**
-   * *(Apple TV only)* Object with properties to control Apple TV parallax effects.
-   *
-   * enabled: If true, parallax effects are enabled.  Defaults to true.
-   * shiftDistanceX: Defaults to 2.0.
-   * shiftDistanceY: Defaults to 2.0.
-   * tiltAngle: Defaults to 0.05.
-   * magnification: Defaults to 1.0.
-   *
-   * @platform ios
-   */
   tvParallaxProperties: PropTypes.object,
-
-  /**
-   * *(Apple TV only)* May be used to change the appearance of the Apple TV parallax effect when this view goes in or out of focus.  Defaults to 2.0.
-   *
-   * @platform ios
-   */
   tvParallaxShiftDistanceX: PropTypes.number,
-
-  /**
-   * *(Apple TV only)* May be used to change the appearance of the Apple TV parallax effect when this view goes in or out of focus.  Defaults to 2.0.
-   *
-   * @platform ios
-   */
   tvParallaxShiftDistanceY: PropTypes.number,
-
-  /**
-   * *(Apple TV only)* May be used to change the appearance of the Apple TV parallax effect when this view goes in or out of focus.  Defaults to 0.05.
-   *
-   * @platform ios
-   */
   tvParallaxTiltAngle: PropTypes.number,
-
-  /**
-   * *(Apple TV only)* May be used to change the appearance of the Apple TV parallax effect when this view goes in or out of focus.  Defaults to 1.0.
-   *
-   * @platform ios
-   */
   tvParallaxMagnification: PropTypes.number,
 };
 
