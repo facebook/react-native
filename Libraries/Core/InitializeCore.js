12f27335924aee699a89a15abcbf04bce61d2815
Emily Janzer <emily.janzer@oculus.com>
2018-09-28 17:06:57 -0700

    Fix flow in InitializeCore
    Reviewed By: yungsters
    
    Differential Revision: D10115867
    
    fbshipit-source-id: be619b9d6fc30e318cbc0e0fc6e741d80dd94a4b

--- Libraries/Core/InitializeCore.js
+++ Libraries/Core/InitializeCore.js
@@ -91,9 +91,6 @@ polyfillGlobal('regeneratorRuntime', () => {
   // The require just sets up the global, so make sure when we first
   // invoke it the global does not exist
   delete global.regeneratorRuntime;
-  /* $FlowFixMe(>=0.54.0 site=react_native_oss) This comment suppresses an
-   * error found when Flow v0.54 was deployed. To see the error delete this
-   * comment and run Flow. */
   require('regenerator-runtime/runtime');
   return global.regeneratorRuntime;
 });
@@ -214,9 +211,6 @@ if (__DEV__) {
 
     // Set up inspector
     const JSInspector = require('JSInspector');
-    /* $FlowFixMe(>=0.56.0 site=react_native_fb,react_native_oss) This comment
-     * suppresses an error found when Flow v0.56 was deployed. To see the error
-     * delete this comment and run Flow. */
     JSInspector.registerAgent(require('NetworkAgent'));
   }
 }
