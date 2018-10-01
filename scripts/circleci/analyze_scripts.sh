b030fcbbf56b6e83f9647ceff5f2e2db0e7fc245
Héctor Ramos <hramos@fb.com>
2018-10-01 15:12:39 -0700

    Update path to code analysis script (#21443)
    Summary:
    The script was moved after the original ShellCheck PR was opened, and this was lost during the rebase.
    Pull Request resolved: https://github.com/facebook/react-native/pull/21443
    
    Differential Revision: D10133498
    
    Pulled By: hramos
    
    fbshipit-source-id: da61b782362ab2d13cb6f0bca3fb1c9a0af08ae5

--- scripts/circleci/analyze_scripts.sh
+++ scripts/circleci/analyze_scripts.sh
@@ -8,7 +8,7 @@ IFS=$'\n'
 
 results=( "$(find . -type f -not -path "*node_modules*" -not -path "*third-party*" -name '*.sh' -exec sh -c  'shellcheck "$1" -f json' -- {} \;)" )
 
-cat <(echo shellcheck; printf '%s\n' "${results[@]}" | jq .,[] | jq -s . | jq --compact-output --raw-output '[ (.[] | .[] | . ) ]') | node bots/code-analysis-bot.js
+cat <(echo shellcheck; printf '%s\n' "${results[@]}" | jq .,[] | jq -s . | jq --compact-output --raw-output '[ (.[] | .[] | . ) ]') | node scripts/circleci/code-analysis-bot.js
 
 # check status
 STATUS=$?
