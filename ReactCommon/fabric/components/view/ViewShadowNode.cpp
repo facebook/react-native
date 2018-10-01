0414d4bb9981e966f23781ce2a9e2ed8c654e0c1
Valentin Shergin <shergin@fb.com>
2018-09-30 22:01:51 -0700

    Fabric: Enabling view hierarchy flattening (for <View> component only for now)
    Summary: This change implements `onLayoutOnly` for regular bare <View> component (*not* for its descendants!) After this view flattening is actually starting working for all platforms.
    
    Reviewed By: mdvacca
    
    Differential Revision: D9511001
    
    fbshipit-source-id: 3562dd1b7570a064150f100cc2e1bc4220b81290

--- ReactCommon/fabric/components/view/ViewShadowNode.cpp
+++ ReactCommon/fabric/components/view/ViewShadowNode.cpp
@@ -12,5 +12,32 @@ namespace react {
 
 const char ViewComponentName[] = "View";
 
+bool ViewShadowNode::isLayoutOnly() const {
+#ifdef ANDROID
+  // This feature is not properly tested on Android yet.
+  return false;
+#else
+  const auto &viewProps = *std::static_pointer_cast<const ViewProps>(props_);
+
+  return
+    // Event listeners
+    !viewProps.onLayout &&
+    // Generic Props
+    viewProps.nativeId.empty() &&
+    // Accessibility Props
+    !viewProps.accessible &&
+    // Style Props
+    viewProps.yogaStyle.overflow == YGOverflowVisible &&
+    viewProps.opacity == 1.0 &&
+    !viewProps.backgroundColor &&
+    !viewProps.foregroundColor &&
+    !viewProps.shadowColor &&
+    viewProps.transform == Transform {} &&
+    viewProps.zIndex == 0 &&
+    // Layout Metrics
+    getLayoutMetrics().borderWidth == EdgeInsets {};
+#endif
+}
+
 } // namespace react
 } // namespace facebook
