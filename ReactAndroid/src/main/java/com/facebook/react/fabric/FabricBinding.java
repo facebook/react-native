9e7d918365baa5bc7da769b555381d6f7f7f05a4
David Vacca <dvacca@fb.com>
2018-10-02 15:01:54 -0700

    Introduce EventBeatManager and enable events
    Summary: This diff introduces the concept of EventBeatManager, this is a class that acts as a proxy between the list of EventBeats registered in C++ and the Android side.
    
    Reviewed By: shergin
    
    Differential Revision: D10127857
    
    fbshipit-source-id: a1956ca42d4c470fbc11cc9f30336a182fe7910c

--- ReactAndroid/src/main/java/com/facebook/react/fabric/FabricBinding.java
+++ ReactAndroid/src/main/java/com/facebook/react/fabric/FabricBinding.java
@@ -1,37 +1,31 @@
 /**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
+ * directory of this source tree.
  */
-
 package com.facebook.react.fabric;
 
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.NativeMap;
-import com.facebook.react.bridge.UIManager;
 
 public interface FabricBinding {
 
-  void installFabric(JavaScriptContextHolder jsContext, FabricBinder fabricBinder);
+  // TODO: T31905686 change types of UIManager and EventBeatManager when moving to OSS
+  void installFabric(
+      JavaScriptContextHolder jsContext, FabricBinder fabricBinder, Object eventBeatManager);
 
   void releaseEventTarget(long jsContextNativePointer, long eventTargetPointer);
 
   void releaseEventHandler(long jsContextNativePointer, long eventHandlerPointer);
 
   void dispatchEventToEmptyTarget(
-    long jsContextNativePointer,
-    long eventHandlerPointer,
-    String type,
-    NativeMap payload
-  );
+      long jsContextNativePointer, long eventHandlerPointer, String type, NativeMap payload);
 
   void dispatchEventToTarget(
-    long jsContextNativePointer,
-    long eventHandlerPointer,
-    long eventTargetPointer,
-    String type,
-    NativeMap payload
-  );
-
+      long jsContextNativePointer,
+      long eventHandlerPointer,
+      long eventTargetPointer,
+      String type,
+      NativeMap payload);
 }
