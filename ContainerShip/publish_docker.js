7177dd3eb29a4a507df7885967d0de070f8f58c1
gengjiawen <technicalcute@gmail.com>
2018-10-03 23:15:51 +0800

    change to docker build

--- ContainerShip/publish_docker.js
+++ ContainerShip/publish_docker.js
@@ -3,15 +3,17 @@ const execSync = require("child_process").execSync;
 function publishDocker() {
   const now = new Date()
   const finaltag = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
-  console.log(finaltag);
   const cmd =`
-docker login -u -u ${process.env.DOCKER_USER} -p ${process.env.DOCKER_PASS}
+docker login -u ${process.env.DOCKER_USER} -p ${process.env.DOCKER_PASS}
 docker build -t gengjiawen/react-native:${finaltag} -f Dockerfile.android-base .
+docker push gengjiawen/react-native:${finaltag}
   `
-  cmd.trim().split().map(i => {
+  cmd.trim().split("\n").map(i => {
     const cmd = i.trim();
     if (cmd) {
-      console.log(cmd);
+      if (!cmd.includes('login')) {
+        console.log(cmd);
+      }
       const output = execSync(cmd)
       console.log(output.toString());
     }
