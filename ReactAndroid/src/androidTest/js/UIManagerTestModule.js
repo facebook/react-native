9d52d98f8747d53ec386e254b5c1c30d433923da
Ronaldo Lima <ronal2do@gmail.com>
2018-10-10 21:49:20 +0200

    remove inline styles

--- ReactAndroid/src/androidTest/js/UIManagerTestModule.js
+++ ReactAndroid/src/androidTest/js/UIManagerTestModule.js
@@ -9,102 +9,94 @@
 
 'use strict';
 
-var BatchedBridge = require('BatchedBridge');
-var React = require('React');
-var StyleSheet = require('StyleSheet');
-var View = require('View');
-var Text = require('Text');
+const BatchedBridge = require('BatchedBridge');
+const React = require('React');
+const StyleSheet = require('StyleSheet');
+const View = require('View');
+const Text = require('Text');
 
-var createReactClass = require('create-react-class');
-var renderApplication = require('renderApplication');
+const renderApplication = require('renderApplication');
 
 type FlexTestAppProps = $ReadOnly<{||}>;
 class FlexTestApp extends React.Component<FlexTestAppProps> {
-  _styles = StyleSheet.create({
-    container: {
-      width: 200,
-      height: 200,
-      flexDirection: 'row',
-    },
-    child: {
-      flex: 1,
-    },
-    absolute: {
-      position: 'absolute',
-      top: 15,
-      left: 10,
-      width: 50,
-      height: 60,
-    },
-  });
   render() {
     return (
       <View
-        style={this._styles.container}
+        style={FlexTestAppStyles.container}
         testID="container"
         collapsable={false}>
         <View
-          style={[this._styles.child, {backgroundColor: '#ff0000'}]}
+          style={[FlexTestAppStyles.child, FlexTestAppStyles.bgRed]}
           collapsable={false}
         />
         <View
-          style={[this._styles.child, {backgroundColor: '#0000ff'}]}
+          style={[FlexTestAppStyles.child, FlexTestAppStyles.bgBlue]}
           collapsable={false}
         />
       </View>
     );
   }
 }
+const FlexTestAppStyles = StyleSheet.create({
+  container: {
+    width: 200,
+    height: 200,
+    flexDirection: 'row',
+  },
+  child: {
+    flex: 1,
+  },
+  absolute: {
+    position: 'absolute',
+    top: 15,
+    left: 10,
+    width: 50,
+    height: 60,
+  },
+  bgRed: {backgroundColor: '#ff0000'},
+  bgBlue: {backgroundColor: '#0000ff'},
+});
 
 type FlexWithTextProps = $ReadOnly<{||}>;
 class FlexWithText extends React.Component<FlexWithTextProps> {
-  _styles = StyleSheet.create({
-    container: {
-      flexDirection: 'column',
-      margin: 20,
-    },
-    row: {
-      flexDirection: 'row',
-      alignItems: 'flex-end',
-      height: 300,
-    },
-    inner: {
-      flex: 1,
-      margin: 10,
-    },
-  });
   render() {
     return (
       <View
-        style={this._styles.container}
+        style={FlexWithTextStyles.container}
         testID="container"
         collapsable={false}>
-        <View style={this._styles.row} collapsable={false}>
-          <Text style={this._styles.inner}>Hello</Text>
-          <Text style={this._styles.inner}>World</Text>
+        <View style={FlexWithTextStyles.row} collapsable={false}>
+          <Text style={FlexWithTextStyles.inner}>Hello</Text>
+          <Text style={FlexWithTextStyles.inner}>World</Text>
         </View>
       </View>
     );
   }
 }
+const FlexWithTextStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    margin: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    height: 300,
+  },
+  inner: {
+    flex: 1,
+    margin: 10,
+  },
+});
 
 type AbsolutePositionTestAppProps = $ReadOnly<{||}>;
 class AbsolutePositionTestApp extends React.Component<
   AbsolutePositionTestAppProps,
 > {
-  _styles = StyleSheet.create({
-    absolute: {
-      position: 'absolute',
-      top: 15,
-      left: 10,
-      width: 50,
-      height: 60,
-    },
-  });
   render() {
     return (
       <View
-        style={this._styles.absolute}
+        style={AbsolutePositionTestAppStyles.absolute}
         testID="absolute"
         collapsable={false}
       />
@@ -112,59 +104,58 @@ class AbsolutePositionTestApp extends React.Component<
   }
 }
 
+AbsolutePositionTestAppStyles = StyleSheet.create({
+  absolute: {
+    position: 'absolute',
+    top: 15,
+    left: 10,
+    width: 50,
+    height: 60,
+  },
+});
+
 type AbsolutePositionBottomRightTestAppProps = $ReadOnly<{||}>;
 class AbsolutePositionBottomRightTestApp extends React.Component<
   AbsolutePositionBottomRightTestAppProps,
 > {
-  _styles = StyleSheet.create({
-    container: {
-      width: 100,
-      height: 100,
-    },
-    absolute: {
-      position: 'absolute',
-      bottom: 15,
-      right: 10,
-      width: 50,
-      height: 60,
-    },
-  });
-
   render() {
     return (
       <View
-        style={this._styles.container}
+        style={AbsolutePositionBottomRightTestAppStyles.container}
         testID="container"
         collapsable={false}>
-        <View style={this._styles.absolute} collapsable={false} />
+        <View
+          style={AbsolutePositionBottomRightTestAppStyles.absolute}
+          collapsable={false}
+        />
       </View>
     );
   }
 }
 
+const AbsolutePositionBottomRightTestAppStyles = StyleSheet.create({
+  container: {
+    width: 100,
+    height: 100,
+  },
+  absolute: {
+    position: 'absolute',
+    bottom: 15,
+    right: 10,
+    width: 50,
+    height: 60,
+  },
+});
+
 type CenteredTextViewProps = $ReadOnly<{|
   text?: ?string,
 |}>;
 class CenteredTextView extends React.Component<CenteredTextViewProps> {
-  _styles = StyleSheet.create({
-    parent: {
-      width: 200,
-      height: 100,
-      backgroundColor: '#aa3311',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    text: {
-      fontSize: 15,
-      color: '#672831',
-    },
-  });
-
   render() {
     return (
       <View collapsable={false}>
-        <View style={this._styles.parent} collapsable={false}>
-          <Text style={this._styles.text} testID="text">
+        <View style={CenteredTextViewStyles.parent} collapsable={false}>
+          <Text style={CenteredTextViewStyles.text} testID="text">
             {this.props.text}
           </Text>
         </View>
@@ -172,6 +163,20 @@ class CenteredTextView extends React.Component<CenteredTextViewProps> {
     );
   }
 }
+
+const CenteredTextViewStyles = StyleSheet.create({
+  parent: {
+    width: 200,
+    height: 100,
+    backgroundColor: '#aa3311',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 15,
+    color: '#672831',
+  },
+});
 
 let flushUpdatePositionInList = null;
 type UpdatePositionInListTestAppProps = $ReadOnly<{||}>;
@@ -184,33 +189,39 @@ class UpdatePositionInListTestApp extends React.Component<
 > {
   state = {active: false};
 
-  _styles = StyleSheet.create({
-    element: {
-      height: 10,
-    },
-    active: {
-      height: 50,
-    },
-  });
-
   flushUpdatePositionInList = () => this.setState({active: true});
 
   render() {
     return (
       <View collapsable={false} testID="container">
-        <View style={this._styles.element} collapsable={false} />
+        <View
+          style={UpdatePositionInListTestAppStyles.element}
+          collapsable={false}
+        />
         <View
           style={[
-            this._styles.element,
-            this.state.active && this._styles.active,
+            UpdatePositionInListTestAppStyles.element,
+            this.state.active && UpdatePositionInListTestAppStyles.active,
           ]}
           collapsable={false}
         />
-        <View style={this._styles.element} collapsable={false} />
+        <View
+          style={UpdatePositionInListTestAppStyles.element}
+          collapsable={false}
+        />
       </View>
     );
   }
 }
+
+const UpdatePositionInListTestAppStyles = StyleSheet.create({
+  element: {
+    height: 10,
+  },
+  active: {
+    height: 50,
+  },
+});
 
 const UIManagerTestModule = {
   renderFlexTestApplication(rootTag) {
