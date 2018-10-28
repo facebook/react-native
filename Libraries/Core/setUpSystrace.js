3db2afd1797f6de7bfb47051093373867114e2bd
Emily Janzer <emily.janzer@oculus.com>
2018-10-28 15:43:29 -0700

    Modularize InitializeCore
    Summary: Split up InitializeCore into a bunch of modules. The idea here is to make it easier for apps to just get the initialization logic they want and leave behind what they don't; for example, if you don't want the Map/Set polyfills, instead of requiring InitializeCore you can require the modules you want from it.
    
    Reviewed By: yungsters
    
    Differential Revision: D10842564
    
    fbshipit-source-id: 3b12d54fddea8c4ee75886022338c214987a015c

---
+++ Libraries/Core/setUpSystrace.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+'use strict';
+
+/**
+ * Set up Systrace profiling hooks if necessary.
+ * You can use this module directly, or just require InitializeCore.
+ */
+if (global.__RCTProfileIsProfiling) {
+  const Systrace = require('Systrace');
+  Systrace.installReactHook();
+  Systrace.setEnabled(true);
+}
