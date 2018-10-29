a70625abd7bf4fba3dafb8a969a73854b7ddcd42
Kudo Chien <ckchien@gmail.com>
2018-10-29 12:11:05 -0700

    Upgrade folly to v2018.10.22.00 for iOS (#21976)
    Summary:
    Fixes #20302 (For iOS)
    
    Note:
    ------
    
    1. Checked the changes did not break CocoaPods integration.
    2. The change for glog copying header into exported/ is to prevent build break for folly.
        `folly/detail/Demangle.h` will try to use libstdc++'s demangle.h. Unfortunately, glog also has a demangle.h in source code. So I copy exported headers and only search headers in exported/ folder during build.
    Pull Request resolved: https://github.com/facebook/react-native/pull/21976
    
    Reviewed By: hramos
    
    Differential Revision: D12818131
    
    Pulled By: fkgozali
    
    fbshipit-source-id: b3c637d09d1b3adde0ea15c82eb56e28f846885b

--- scripts/ios-install-third-party.sh
+++ scripts/ios-install-third-party.sh
@@ -68,4 +68,4 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 fetch_and_unpack glog-0.3.5.tar.gz https://github.com/google/glog/archive/v0.3.5.tar.gz 61067502c5f9769d111ea1ee3f74e6ddf0a5f9cc "\"$SCRIPTDIR/ios-configure-glog.sh\""
 fetch_and_unpack double-conversion-1.1.6.tar.gz https://github.com/google/double-conversion/archive/v1.1.6.tar.gz 1c7d88afde3aaeb97bb652776c627b49e132e8e0
 fetch_and_unpack boost_1_63_0.tar.gz https://github.com/react-native-community/boost-for-react-native/releases/download/v1.63.0-0/boost_1_63_0.tar.gz c3f57e1d22a995e608983effbb752b54b6eab741
-fetch_and_unpack folly-2016.10.31.00.tar.gz https://github.com/facebook/folly/archive/v2016.10.31.00.tar.gz fb8cdf8962d8c9d0c20a150b6ec3b75d1fa50696
+fetch_and_unpack folly-2018.10.22.00.tar.gz https://github.com/facebook/folly/archive/v2018.10.22.00.tar.gz f70a75bfeb394363d2049a846bba118ffb3b368a
