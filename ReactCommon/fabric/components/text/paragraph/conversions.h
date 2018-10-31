a689711f680bb23795dea6391627383ca6973f88
Tim Yung <yungsters@fb.com>
2018-10-31 01:35:03 -0700

    RN: Missing Copyright Headers
    Summary: Adds copyright headers to all files that are missing them.
    
    Reviewed By: hramos
    
    Differential Revision: D12837494
    
    fbshipit-source-id: 6330a18919676dec9ff2c03b7c9329ed9127d930

--- ReactCommon/fabric/components/text/paragraph/conversions.h
+++ ReactCommon/fabric/components/text/paragraph/conversions.h
@@ -1,3 +1,7 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
 
 #include <fabric/attributedstring/conversions.h>
 #include <fabric/components/text/ParagraphLocalData.h>
