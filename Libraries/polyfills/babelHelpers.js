6dfdc2eed3a25dbadbaac56bdaaf9831bd8bc7cd
nicholaslee119 <nicholaslee119@gmail.com>
2018-11-01 20:09:04 +0900

    add asyncToGenerator to the babelHelper

--- Libraries/polyfills/babelHelpers.js
+++ Libraries/polyfills/babelHelpers.js
@@ -604,3 +604,36 @@ function _iterableToArray(iter) {
 }
 
 babelHelpers.iterableToArray = _iterableToArray;
+
+// ### asyncToGenerator ###
+
+function _asyncToGenerator(fn) {
+  return function () {
+    var gen = fn.apply(this, arguments);
+    return new Promise(function (resolve, reject) {
+      function step(key, arg) {
+        try {
+          var info = gen[key](arg);
+          var value = info.value;
+        } catch (error) {
+          reject(error);
+          return;
+        }
+
+        if (info.done) {
+          resolve(value);
+        } else {
+          return Promise.resolve(value).then(function (value) {
+            step('next', value);
+          }, function (err) {
+            step('throw', err);
+          });
+        }
+      }
+
+      return step('next');
+    });
+  };
+};
+
+babelHelpers.asyncToGenerator = _asyncToGenerator;
