ba4bbb2d96d72fd56af9e3675828b19c8241a6b7
Tnarita0000 <naritatakuya0000@gmail.com>
2018-11-06 03:01:25 +0900

    Flow strict-local in TimePickerAndroid.android.ios.js

--- Libraries/Components/TimePickerAndroid/TimePickerAndroid.ios.js
+++ Libraries/Components/TimePickerAndroid/TimePickerAndroid.ios.js
@@ -5,13 +5,29 @@
  * LICENSE file in the root directory of this source tree.
  *
  * @format
- * @flow
+ * @flow strict-local
  */
 
 'use strict';
 
+import type {SyntheticEvent} from 'CoreEventTypes';
+
+type Options = {
+  hour: number,
+  minute: number,
+  is24Hour: boolean,
+  mode: 'clock' | 'spinner' | 'default',
+};
+type TimePickerAndroidEvent = SyntheticEvent<
+  $ReadOnly<{|
+    action: string,
+    hour: number,
+    minute: number,
+  |}>,
+>;
+
 const TimePickerAndroid = {
-  async open(options: Object): Promise<Object> {
+  async open(options: Options): Promise<TimePickerAndroidEvent> {
     return Promise.reject({
       message: 'TimePickerAndroid is not supported on this platform.',
     });
