d5d44268bb6a6a796f83a81eb12bf95773ddb310
mottox2 <y.mottox2@gmail.com>
2018-11-07 01:28:25 +0900

    [flow-strict] Apply flow-strict

--- Libraries/Components/Touchable/TouchableNativeFeedback.android.js
+++ Libraries/Components/Touchable/TouchableNativeFeedback.android.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
+ * @flow strict-local
  * @format
  */
 
@@ -14,6 +14,7 @@ const Platform = require('Platform');
 const React = require('React');
 const PropTypes = require('prop-types');
 const ReactNative = require('ReactNative');
+// $FlowFixMe
 const Touchable = require('Touchable');
 const TouchableWithoutFeedback = require('TouchableWithoutFeedback');
 const UIManager = require('UIManager');
