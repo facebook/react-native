d7a0c44590bcf3fb9d055aeae3391d5bcd7e21be
Kevin Gozali <fkg@fb.com>
2018-11-13 20:09:21 -0800

    iOS: add moduleForNameForcedLoad: to lookup modules by name and force load them
    Summary:
    Some module classes may not be loaded yet, so looking up via classes may not always give the correct instance. This diff added a new lookup method that delegate to the bridge delegate to force load the modules as needed.
    
    The existing moduleForName: method was left untouched because it's solely used by RCTUIManager at the moment.
    
    Reviewed By: dshahidehpour
    
    Differential Revision: D13033876
    
    fbshipit-source-id: 4082fcd68498004f678b4b95adc82b5b134fefdf

--- React/CxxBridge/RCTCxxBridge.mm
+++ React/CxxBridge/RCTCxxBridge.mm
@@ -439,6 +439,30 @@ struct RCTInstanceCallback : public InstanceCallback {
   return _moduleDataByName[moduleName].instance;
 }
 
+- (id)moduleForName:(NSString *)moduleName lazilyLoadIfNecessary:(BOOL)lazilyLoad
+{
+  if (!lazilyLoad) {
+    return [self moduleForName:moduleName];
+  }
+
+  RCTModuleData *moduleData = _moduleDataByName[moduleName];
+  if (moduleData) {
+    return moduleData.instance;
+  }
+
+  // Module may not be loaded yet, so attempt to force load it here.
+  const BOOL result = [self.delegate respondsToSelector:@selector(bridge:didNotFindModule:)] &&
+    [self.delegate bridge:self didNotFindModule:moduleName];
+  if (result) {
+    // Try again.
+    moduleData = _moduleDataByName[moduleName];
+  } else {
+    RCTLogError(@"Unable to find module for %@", moduleName);
+  }
+
+  return moduleData.instance;
+}
+
 - (BOOL)moduleIsInitialized:(Class)moduleClass
 {
   return _moduleDataByName[RCTBridgeModuleNameForClass(moduleClass)].hasInstance;
@@ -446,17 +470,7 @@ struct RCTInstanceCallback : public InstanceCallback {
 
 - (id)moduleForClass:(Class)moduleClass
 {
-  NSString *moduleName = RCTBridgeModuleNameForClass(moduleClass);
-  RCTModuleData *moduleData = _moduleDataByName[moduleName];
-  if (moduleData) {
-    return moduleData.instance;
-  }
-
-  // Module may not be loaded yet, so attempt to force load it here.
-  RCTAssert([moduleClass conformsToProtocol:@protocol(RCTBridgeModule)], @"Asking for a NativeModule that doesn't conform to RCTBridgeModule: %@", NSStringFromClass(moduleClass));
-  [self registerAdditionalModuleClasses:@[moduleClass]];
-
-  return _moduleDataByName[moduleName].instance;
+  return [self moduleForName:RCTBridgeModuleNameForClass(moduleClass) lazilyLoadIfNecessary:YES];
 }
 
 - (std::shared_ptr<ModuleRegistry>)_buildModuleRegistryUnlocked
