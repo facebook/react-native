bb3045653ebac5ab2b8e2dfa5ccd88fdea8fe5ac
gengjiawen <technicalcute@gmail.com>
2018-11-15 09:54:25 -0800

    Fix compatibility issue for android 16 device.Fixes #22294 (#22295)
    Summary:
    Fix compatibility issue for android 16 device. Related issue https://github.com/facebook/react-native/pull/22295.
    pass all current ci.
    none
     [GENERAL] [ANDROID] [Fixed] - Fix compatibility issue for android 16 device
    Pull Request resolved: https://github.com/facebook/react-native/pull/22295
    
    Differential Revision: D13084152
    
    Pulled By: hramos
    
    fbshipit-source-id: 42459ddc6f84f870affdda5ba19bcd8bb27b56d7

--- ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/AndroidInfoHelpers.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/AndroidInfoHelpers.java
@@ -7,6 +7,7 @@ package com.facebook.react.modules.systeminfo;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
@@ -84,7 +85,7 @@ public class AndroidInfoHelpers {
           Runtime.getRuntime().exec(new String[] {"/system/bin/getprop", METRO_HOST_PROP_NAME});
       reader =
           new BufferedReader(
-              new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+              new InputStreamReader(process.getInputStream(), Charset.forName("UTF-8")));
 
       String lastLine = "";
       String line;
