9d634e72a679170c65cfd577453eb5d5ce3ae627
Mike Grabowski <grabbou@gmail.com>
2018-11-18 18:30:46 +0100

    Use external CLI instead

--- local-cli/cli.js
+++ local-cli/cli.js
@@ -7,18 +7,7 @@
  * @format
  */
 
-'use strict';
-
-// gracefulify() has to be called before anything else runs
-require('graceful-fs').gracefulify(require('fs'));
-
-// This file must be able to run in node 0.12 without babel so we can show that
-// it is not supported. This is why the rest of the cli code is in `cliEntry.js`.
-require('./server/checkNodeVersion')();
-
-require('../setupBabel')();
-
-var cliEntry = require('./cliEntry');
+var cliEntry = require('react-native-local-cli');
 
 if (require.main === module) {
   cliEntry.run();
