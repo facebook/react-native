3b4d6d5ef58bd6c66306152c5f62d9e09ebe1216
David Vacca <dvacca@fb.com>
2018-11-25 17:18:12 -0800

    Add systrace to calculation of Yoga layout() in Fabric
    Summary: Simple diff that adds a systrace to start measuring the calculation of Yoga layout() in Fabric
    
    Reviewed By: shergin
    
    Differential Revision: D13124641
    
    fbshipit-source-id: 6bd03e9f56524221f5d91606ffde50253673c1bb

--- ReactCommon/fabric/components/root/RootShadowNode.cpp
+++ ReactCommon/fabric/components/root/RootShadowNode.cpp
@@ -8,6 +8,7 @@
 #include "RootShadowNode.h"
 
 #include <react/components/view/conversions.h>
+#include <react/debug/SystraceSection.h>
 
 namespace facebook {
 namespace react {
@@ -15,6 +16,7 @@ namespace react {
 const char RootComponentName[] = "RootView";
 
 void RootShadowNode::layout() {
+  SystraceSection s("RootShadowNode::layout");
   ensureUnsealed();
   layout(getProps()->layoutContext);
 
