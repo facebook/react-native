2151c30474518858a5368013f5ea024588b279b9
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2018-11-27 13:10:20 +0000

    [0.57.7] Bump version numbers

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -22,7 +22,7 @@ static void __makeVersion()
   __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(57),
-                  RCTVersionPatch: @(6),
+                  RCTVersionPatch: @(7),
                   RCTVersionPrerelease: [NSNull null],
                   };
 }
