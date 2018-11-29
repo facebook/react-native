ea734dcfbfe069b9e1e1d6556c98be2281f2744c
David Aurelio <davidaurelio@fb.com>
2018-11-29 11:35:34 -0800

    Adjust yearless format for MIT license
    Summary:
    @public
    
    Adjust license headers throughout the project
    
    Reviewed By: SidharthGuglani
    
    Differential Revision: D13255691
    
    fbshipit-source-id: 98be2aa372a94e7a54a65e3d64e5c6a436b18083

--- ReactAndroid/src/main/jni/first-party/yogajni/jni/YGJNI.cpp
+++ ReactAndroid/src/main/jni/first-party/yogajni/jni/YGJNI.cpp
@@ -1,9 +1,8 @@
-/*
- *  Copyright (c) 2018-present, Facebook, Inc.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
 #include <fb/fbjni.h>
 #include <yoga/YGNode.h>
