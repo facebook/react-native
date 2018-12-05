30c2fb814c60348b49649c1c08681333e175017a
hyochan <dooboolab@gmail.com>
2018-12-04 18:46:43 -0800

    Fix ListEmptyComponent is rendered upside down when using inverted flag. (#21496)
    Summary:
    Fix ListEmptyComponent is rendered upside down when using inverted flag.
    
    Fixes #21196
    Pull Request resolved: https://github.com/facebook/react-native/pull/21496
    
    Differential Revision: D13334437
    
    Pulled By: hramos
    
    fbshipit-source-id: b34a9a0a153862d3d5dbe410ab0a6c66a8cfcffd

--- Libraries/Lists/VirtualizedList.js
+++ Libraries/Lists/VirtualizedList.js
@@ -933,7 +933,7 @@ class VirtualizedList extends React.PureComponent<Props, State> {
           : this.props.inverted,
       stickyHeaderIndices,
     };
-    if (inversionStyle) {
+    if (inversionStyle && itemCount !== 0) {
       /* $FlowFixMe(>=0.70.0 site=react_native_fb) This comment suppresses an
        * error found when Flow v0.70 was deployed. To see the error delete
        * this comment and run Flow. */
