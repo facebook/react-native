57a38263bfd106528712c3ecc175ff218b139737
David Aurelio <davidaurelio@fb.com>
2018-12-06 07:35:07 -0800

    Roll back `-ffast-math`
    Summary:
    @public
    
    `-ffast-math` does not have measurable performance benefits.
    
    By using `NaN` for *undefined* values again, we can squeeze `YGFloatOptional` into 32 bits.
    This will also enable us to store `YGValue` (or a variant of it) in 32 bits.
    
    Reviewed By: SidharthGuglani
    
    Differential Revision: D13119110
    
    fbshipit-source-id: 4e6964240bf74ebc22d8783107b89d536a1a0842

--- ReactCommon/yoga/yoga/Yoga-internal.h
+++ ReactCommon/yoga/yoga/Yoga-internal.h
@@ -27,15 +27,7 @@ namespace facebook {
 namespace yoga {
 
 inline bool isUndefined(float value) {
-  // Value of a float in the case of it being not defined is 10.1E20. Earlier
-  // it used to be NAN, the benefit of which was that if NAN is involved in any
-  // mathematical expression the result was NAN. But since we want to have
-  // `-ffast-math` flag being used by compiler which assumes that the floating
-  // point values are not NAN and Inf, we represent YGUndefined as 10.1E20. But
-  // now if YGUndefined is involved in any mathematical operations this
-  // value(10.1E20) would change. So the following check makes sure that if the
-  // value is outside a range (-10E8, 10E8) then it is undefined.
-  return value >= 10E8 || value <= -10E8;
+  return std::isnan(value);
 }
 
 } // namespace yoga
