5d06c7495d8e58543610da66f4b2aed7ea6f1386
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2018-12-20 04:39:37 -0800

    Replace our local types by flow-typed (#20320)
    Summary:
    These comments were confusing since we don't use flow-typed.
    
    cc bvaughn.
    
    I have replaced our local types by flow-typed, removed metro custom stub and deleted `flow-github` directory.
    
    As result, createReactClass, PropTypes and Metro types won't be distributed to RN users. If you need it, you should relay on flow-typed.
    Pull Request resolved: https://github.com/facebook/react-native/pull/20320
    
    Reviewed By: TheSavior
    
    Differential Revision: D13517157
    
    Pulled By: rickhanlonii
    
    fbshipit-source-id: e52c0b6114114a706f6152b2031c600e3dece0d2

--- flow-typed/npm/create-react-class_v15.x.x.js
+++ flow-typed/npm/create-react-class_v15.x.x.js
@@ -9,7 +9,8 @@
  * @nolint
  */
 
-// TODO (acdlite) Remove this file once flowtype/flow-typed/pull/773 is merged
+// flow-typed signature: 892fd8dcd3f17e76da4fb1334b0fd49e
+// flow-typed version: da30fe6876/create-react-class_v15.x.x/flow_>=v0.41.x
 
 declare module 'create-react-class' {
   declare module.exports: React$CreateClass;
