3fae3884240790a57f1da4b71a13770147b925d5
Kudo Chien <ckchien@gmail.com>
2018-12-30 10:44:42 -0800

    Fix CircleCI build error (#22822)
    Summary:
    ----------
    
    side effect of gradle task name changed from https://github.com/facebook/react-native/commit/f3e5cce4745c0ad9a5c697be772757a03e15edc5
    
    Changelog:
    ----------
    
    [Android] [Fixed] - Fix CircleCI build error
    Pull Request resolved: https://github.com/facebook/react-native/pull/22822
    
    Differential Revision: D13562455
    
    Pulled By: RSNara
    
    fbshipit-source-id: a39a19350dcd91b2f06e064c3aec62b94d009fab

--- scripts/circleci/gradle_download_deps.sh
+++ scripts/circleci/gradle_download_deps.sh
@@ -6,4 +6,4 @@
 
 set -e
 
-./gradlew :ReactAndroid:downloadBoost :ReactAndroid:downloadDoubleConversion :ReactAndroid:downloadFolly :ReactAndroid:downloadGlog :ReactAndroid:downloadJSCHeaders
+./gradlew :ReactAndroid:downloadBoost :ReactAndroid:downloadDoubleConversion :ReactAndroid:downloadFolly :ReactAndroid:downloadGlog :ReactAndroid:downloadJSC
