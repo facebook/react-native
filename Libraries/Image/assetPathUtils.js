cd951500d60779d8e2b9da8fdba8777a05ee9a45
Bruno Nallis Villanova <bruno@digimundi.com.br>
2019-01-04 15:36:52 -0200

    Update assetPathUtils.js

--- Libraries/Image/assetPathUtils.js
+++ Libraries/Image/assetPathUtils.js
@@ -51,9 +51,10 @@ function getAndroidResourceFolderName(asset: PackagerAsset, scale: number) {
   var suffix = getAndroidAssetSuffix(scale);
   if (!suffix) {
     throw new Error(
-      'Don\'t know which android drawable suffix to use for scale: ' + scale +
-       '\nAsset: ' + JSON.stringify(asset, null, '\t') +
-       '\nPossible scales are:' +
+      "Don\'t know which android drawable suffix to use for scale: " + scale +
+       '\nAsset: ' + 
+       JSON.stringify(asset, null, '\t') + 
+       '\nPossible scales are:' + 
        JSON.stringify(androidScaleSuffix, null, '\t'),
     );
   }
