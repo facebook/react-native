19de7102916a76c4a5f570eadbe162fb0e8abccc
Matt Hargett <plaztiksyke@gmail.com>
2019-01-08 12:09:03 -0800

    Remove public symbols not part of URL w3c wpec. Alpha-sort methods, bring static methods to top of class. Change XML quoting back to original.

--- Libraries/Blob/URL.js
+++ Libraries/Blob/URL.js
@@ -30,9 +30,9 @@ if (BlobModule && typeof BlobModule.BLOB_URI_SCHEME === 'string') {
  * <manifest>
  *   <application>
  *     <provider
- *       android:name='com.facebook.react.modules.blob.BlobProvider'
- *       android:authorities='@string/blob_provider_authority'
- *       android:exported='false'
+ *       android:name="com.facebook.react.modules.blob.BlobProvider"
+ *       android:authorities="@string/blob_provider_authority"
+ *       android:exported="false"
  *     />
  *   </application>
  * </manifest>
@@ -42,7 +42,7 @@ if (BlobModule && typeof BlobModule.BLOB_URI_SCHEME === 'string') {
  *
  * ```xml
  * <resources>
- *   <string name='blob_provider_authority'>your.app.package.blobs</string>
+ *   <string name="blob_provider_authority">your.app.package.blobs</string>
  * </resources>
  * ```
  */
@@ -50,7 +50,7 @@ if (BlobModule && typeof BlobModule.BLOB_URI_SCHEME === 'string') {
 // Small subset from whatwg-url: https://github.com/jsdom/whatwg-url/tree/master/lib
 // The reference code bloat comes from Unicode issues with URLs, so those won't work here.
 export class URLSearchParams {
-  searchParams = [];
+  _searchParams = [];
 
   constructor(params: any) {
     if (typeof params === 'object') {
@@ -59,37 +59,69 @@ export class URLSearchParams {
   }
 
   append(key: string, value: string) {
-    this.searchParams.push([key, value]);
+    this._searchParams.push([key, value]);
   }
 
-  get paramString() {
-    if (this.searchParams.length === 0) {
-      return '';
-    }
-    const last = this.searchParams.length - 1;
-    return this.searchParams.reduce((acc, curr, index) => {
-      return acc + curr.join('=') + (index === last ? '' : '&');
-    }, '');
+  delete(name) {
+    throw new Error('not implemented');
+  }
+
+  get(name) {
+    throw new Error('not implemented');
+  }
+
+  getAll(name) {
+    throw new Error('not implemented');
+  }
+
+  has(name) {
+    throw new Error('not implemented');
+  }
+
+  set(name, value) {
+    throw new Error('not implemented');
+  }
+
+  sort() {
+    throw new Error('not implemented');
   }
 
   [Symbol.iterator]() {
-    return this.searchParams[Symbol.iterator]();
+    return this._searchParams[Symbol.iterator]();
   }
 
   toString() {
-    return this.paramString;
+    if (this._searchParams.length === 0) {
+      return '';
+    }
+    const last = this._searchParams.length - 1;
+    return this._searchParams.reduce((acc, curr, index) => {
+      return acc + curr.join('=') + (index === last ? '' : '&');
+    }, '');
   }
+}
+
+function validateBaseUrl(url: string) {
+  // from this MIT-licensed gist: https://gist.github.com/dperini/729294
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+    url,
+  );
 }
 
 export class URL {
   _searchParamsInstance = null;
-  static urlRegexp = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/;
 
-  static validateBaseUrl(url: string) {
-    // from this MIT-licensed gist: https://gist.github.com/dperini/729294
-    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
-      url,
-    );
+  static createObjectURL(blob: Blob) {
+    if (BLOB_URL_PREFIX === null) {
+      throw new Error('Cannot create URL for blob!');
+    }
+    return `${BLOB_URL_PREFIX}${blob.data.blobId}?offset=${
+      blob.data.offset
+    }&size=${blob.size}`;
+  }
+
+  static revokeObjectURL(url: string) {
+    // Do nothing.
   }
 
   constructor(url: string, base: string) {
@@ -97,7 +129,7 @@ export class URL {
     if (base) {
       if (typeof base === 'string') {
         baseUrl = base;
-        if (!URL.validateBaseUrl(baseUrl)) {
+        if (!validateBaseUrl(baseUrl)) {
           throw new TypeError(`Invalid base URL: ${baseUrl}`);
         }
       } else if (typeof base === 'object') {
@@ -118,11 +150,19 @@ export class URL {
     }
   }
 
-  get href(): string {
-    return this.toString();
+  get hash() {
+    throw new Error('not implemented');
   }
 
-  toJSON(): string {
+  get host() {
+    throw new Error('not implemented');
+  }
+
+  get hostname() {
+    throw new Error('not implemented');
+  }
+
+  get href(): string {
     return this.toString();
   }
 
@@ -130,15 +170,11 @@ export class URL {
     throw new Error('not implemented');
   }
 
-  get username() {
-    throw new Error('not implemented');
-  }
-
   get password() {
     throw new Error('not implemented');
   }
 
-  get hostname() {
+  get pathname() {
     throw new Error('not implemented');
   }
 
@@ -150,28 +186,8 @@ export class URL {
     throw new Error('not implemented');
   }
 
-  get host() {
-    throw new Error('not implemented');
-  }
-
-  get pathname() {
-    throw new Error('not implemented');
-  }
-
   get search() {
     throw new Error('not implemented');
-  }
-
-  get hash() {
-    throw new Error('not implemented');
-  }
-
-  toString(): string {
-    if (this._searchParamsInstance === null) {
-      return this._url;
-    }
-    const separator = this._url.indexOf('?') > -1 ? '&' : '?';
-    return this._url + separator + this._searchParamsInstance.paramString;
   }
 
   get searchParams(): URLSearchParams {
@@ -181,16 +197,19 @@ export class URL {
     return this._searchParamsInstance;
   }
 
-  static createObjectURL(blob: Blob) {
-    if (BLOB_URL_PREFIX === null) {
-      throw new Error('Cannot create URL for blob!');
-    }
-    return `${BLOB_URL_PREFIX}${blob.data.blobId}?offset=${
-      blob.data.offset
-    }&size=${blob.size}`;
+  toJSON(): string {
+    return this.toString();
   }
 
-  static revokeObjectURL(url: string) {
-    // Do nothing.
+  toString(): string {
+    if (this._searchParamsInstance === null) {
+      return this._url;
+    }
+    const separator = this._url.indexOf('?') > -1 ? '&' : '?';
+    return this._url + separator + this._searchParamsInstance.toString();
+  }
+
+  get username() {
+    throw new Error('not implemented');
   }
 }
