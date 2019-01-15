9969b17dcecfdf426beae17244dafd89c3ddbb78
Adam Comella <adcom@microsoft.com>
2019-01-14 18:30:53 -0800

    RNTester: Add `allowFontScaling` example
    iOS's `allowFontScaling` example was copied to Android. Also, I added a case illustrating that `allowFontScaling` gets inherited in nested `<Text>` scenarios. This tests the bug fix from #22917. This nested case was added to both iOS and Android.
    
    Also, moved the comment in `TextAttributes.java` from the top of the file to the top of the class definiiton as requested in #22917.
    
    Test Plan:
    ----------
    
    Used the OS settings to increase the font scale to the maximum. Opened RNTester on iOS and Android and verified that the text in the `allowFontScaling` example that has `allowFontScaling` set to `false` does not get scaled.
    
    Changelog:
    ----------
    
    [Android] [Added] - RNTester: Added demo of `Text's` `allowFontScaling` prop
    
    Adam Comella
    Microsoft Corp.

--- ReactAndroid/src/main/java/com/facebook/react/views/text/TextAttributes.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/text/TextAttributes.java
@@ -5,6 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+package com.facebook.react.views.text;
+
+import com.facebook.react.uimanager.PixelUtil;
+import com.facebook.react.uimanager.ViewDefaults;
+
 /*
  * Currently, TextAttributes consists of a subset of text props that need to be passed from parent
  * to child so inheritance can be implemented correctly. An example complexity that causes a prop
@@ -12,12 +17,6 @@
  * the rendered aka effective value. For example, to figure out the rendered/effective font size,
  * you need to take into account the fontSize and allowFontScaling props.
  */
-
-package com.facebook.react.views.text;
-
-import com.facebook.react.uimanager.PixelUtil;
-import com.facebook.react.uimanager.ViewDefaults;
-
 public class TextAttributes {
   private boolean mAllowFontScaling = true;
   private float mFontSize = Float.NaN;
