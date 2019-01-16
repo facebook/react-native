d7ae279d19cadce2131d1b3008423c05eb80566e
doniyor2109 <doniyor2109@gmail.com>
2019-01-16 19:32:21 +0500

    Created standalone js file for RCTInputAccessoryView native component

---
+++ Libraries/Components/TextInput/RCTInputAccessoryViewNativeComponent.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+'use strict';
+
+import type {NativeComponent} from 'ReactNative';
+import type {ViewStyleProp} from '../../StyleSheet/StyleSheet';
+
+const DeprecatedColorPropType = require('DeprecatedColorPropType');
+const requireNativeComponent = require('requireNativeComponent');
+
+type InputAccessoryViewProps = $ReadOnly<{|
+  +children: React.Node,
+  /**
+   * An ID which is used to associate this `InputAccessoryView` to
+   * specified TextInput(s).
+   */
+  nativeID?: ?string,
+  style?: ?ViewStyleProp,
+  backgroundColor?: ?DeprecatedColorPropType,
+|}>;
+
+type NativeInputAccessoryView = Class<NativeComponent<InputAccessoryViewProps>>;
+
+module.exports = ((requireNativeComponent(
+  'RCTInputAccessoryView',
+): any): NativeInputAccessoryView);
