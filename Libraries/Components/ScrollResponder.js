acb2957778c9322d8b71dfa39c841e03c3a06377
Michał Osadnik <micosa97@gmail.com>
2019-01-16 09:45:47 +0100

    Remove bad comment

--- Libraries/Components/ScrollResponder.js
+++ Libraries/Components/ScrollResponder.js
@@ -437,8 +437,6 @@ const ScrollResponderMixin = {
    * Note: The weird argument signature is due to the fact that, for historical reasons,
    * the function also accepts separate arguments as as alternative to the options object.
    * This is deprecated due to ambiguity (y before x), and SHOULD NOT BE USED.
-   *
-   * Also note "duration" is currently only supported for Android.
    */
   scrollResponderScrollTo: function(
     x?:
