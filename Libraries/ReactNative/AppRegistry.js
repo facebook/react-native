2c7895706ded2e40c4bae8a44dea1719c4fcc0c6
Sam Goldman <samgoldman@fb.com>
2019-01-16 21:50:43 -0800

    @allow-large-files Deploy Flow v0.90 to xplat/js
    Reviewed By: dsainati1
    
    Differential Revision: D13708161
    
    fbshipit-source-id: b83dab505f2739bf4bc5077936e71ebd1800eaf4

--- Libraries/ReactNative/AppRegistry.js
+++ Libraries/ReactNative/AppRegistry.js
@@ -21,6 +21,9 @@ const renderApplication = require('renderApplication');
 
 type Task = (taskData: any) => Promise<void>;
 type TaskProvider = () => Task;
+/* $FlowFixMe(>=0.90.0 site=react_native_fb) This comment suppresses an
+ * error found when Flow v0.90 was deployed. To see the error, delete this
+ * comment and run Flow. */
 export type ComponentProvider = () => React$ComponentType<any>;
 export type ComponentProviderInstrumentationHook = (
   component: ComponentProvider,
