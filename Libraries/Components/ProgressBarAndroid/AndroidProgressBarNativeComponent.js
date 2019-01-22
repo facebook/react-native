7093bb0c102770a0bf1086c932d8793b23387425
Alex <ajstrand8@gmail.com>
2019-01-21 23:02:50 -0500

    added missing flow type for progress bar android native component

--- Libraries/Components/ProgressBarAndroid/AndroidProgressBarNativeComponent.js
+++ Libraries/Components/ProgressBarAndroid/AndroidProgressBarNativeComponent.js
@@ -15,4 +15,50 @@ import type {ViewProps} from 'ViewPropTypes';
 import type {SyntheticEvent} from 'CoreEventTypes';
 import type {NativeComponent} from 'ReactNative';
 
-module.exports = requireNativeComponent('AndroidProgressBar');
+type ProgressBarAndroidProps = $ReadOnly<{|
+  ...ViewProps,
+
+  /**
+   * Style of the ProgressBar and whether it shows indeterminate progress (e.g. spinner).
+   *
+   * `indeterminate` can only be false if `styleAttr` is Horizontal, and requires a
+   * `progress` value.
+   */
+  ...
+    | {|
+        styleAttr: 'Horizontal',
+        indeterminate: false,
+        progress: number,
+      |}
+    | {|
+        typeAttr:
+          | 'Horizontal'
+          | 'Normal'
+          | 'Small'
+          | 'Large'
+          | 'Inverse'
+          | 'SmallInverse'
+          | 'LargeInverse',
+        indeterminate: true,
+      |},
+  /**
+   * Whether to show the ProgressBar (true, the default) or hide it (false).
+   */
+  animating?: ?boolean,
+  /**
+   * Color of the progress bar.
+   */
+  color?: ?string,
+  /**
+   * Used to locate this view in end-to-end tests.
+   */
+  testID?: ?string,
+|}>;
+
+type ProgressBarAndroidNativeType = Class<
+  NativeComponent<ProgressBarAndroidProps>,
+>;
+
+module.exports = ((requireNativeComponent(
+  'AndroidProgressBar',
+): any): ProgressBarAndroidNativeType);
