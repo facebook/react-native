9d75f32c445841b2a77ff33b23274685d3cfea86
Varun Gupta <varungupta303@gmail.com>
2019-01-23 10:47:37 +0530

    Fix SwipeableActionButton styling.

--- Libraries/Experimental/SwipeableRow/SwipeableQuickActionButton.js
+++ Libraries/Experimental/SwipeableRow/SwipeableQuickActionButton.js
@@ -37,6 +37,7 @@ class SwipeableQuickActionButton extends React.Component<{
    * found when Flow v0.82 was deployed. To see the error delete this comment
    * and run Flow. */
   style?: ?DeprecatedViewPropTypes.style,
+  containerStyle?: ?DeprecatedViewPropTypes.style,
   testID?: string,
   text?: ?(string | Object | Array<string | Object>),
   /* $FlowFixMe(>=0.82.0 site=react_native_fb) This comment suppresses an error
@@ -64,8 +65,9 @@ class SwipeableQuickActionButton extends React.Component<{
       <TouchableHighlight
         onPress={this.props.onPress}
         testID={this.props.testID}
-        underlayColor="transparent">
-        <View style={this.props.style}>{mainView}</View>
+        underlayColor="transparent"
+        style={this.props.containerStyle}>
+        {mainView}
       </TouchableHighlight>
     );
   }
