847d41765466bc8f31155504305a85144f218396
Christoph Nakazawa <cpojer@fb.com>
2019-01-25 13:34:35 +0000

    Update whatwg-fetch.js

--- Libraries/vendor/core/whatwg-fetch.js
+++ Libraries/vendor/core/whatwg-fetch.js
@@ -517,7 +517,7 @@
       }
 
       if ('responseType' in xhr && support.blob) {
-        xhr.responseType = 'blob';
+        xhr.responseType = 'blob'
       }
 
       request.headers.forEach(function(value, name) {
