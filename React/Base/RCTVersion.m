6084cf14015d31d69ec150c62ecd6c38a6fd91d7
Mike Grabowski <grabbou@gmail.com>
2019-01-28 17:08:24 +0100

    [0.58.2] Bump version numbers

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -22,7 +22,7 @@ static void __makeVersion()
   __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(58),
-                  RCTVersionPatch: @(1),
+                  RCTVersionPatch: @(2),
                   RCTVersionPrerelease: [NSNull null],
                   };
 }
