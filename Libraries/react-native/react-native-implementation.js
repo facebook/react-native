22d25312c7a739452cc079cc71f8fa060e5a65c1
Wellinton Monge <wellinton.monge@autopass.com.br>
2019-02-06 16:15:03 -0200

    fixing prettier and Cannot resolve variable

--- Libraries/react-native/react-native-implementation.js
+++ Libraries/react-native/react-native-implementation.js
@@ -185,9 +185,6 @@ module.exports = {
   get Alert() {
     return require('Alert');
   },
-  get AlertIOS() {
-    return require('AlertIOS');
-  },
   get Animated() {
     return require('Animated');
   },
--- ReactAndroid/src/main/java/com/facebook/react/modules/debug/interfaces/DeveloperSettings.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/debug/interfaces/DeveloperSettings.java
@@ -1,4 +1,3 @@
-/**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
