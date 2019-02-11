7fa4293327e0067d2c6a4ca23b3aecd34ca35816
Christoph Nakazawa <cpojer@fb.com>
2019-02-11 16:58:56 +0000

    Update react-native-implementation.js

--- Libraries/react-native/react-native-implementation.js
+++ Libraries/react-native/react-native-implementation.js
@@ -196,7 +196,7 @@ module.exports = {
       'async-storage-moved',
       'Async Storage has been extracted from react-native core and will be removed in a future release. ' +
         "It can now be installed and imported from '@react-native-community/async-storage' instead of 'react-native'. " +
-        'See https://github.com/react-native-community/react-native-async-storage for more informations.',
+        'See https://github.com/react-native-community/react-native-async-storage',
     );
     return require('AsyncStorage');
   },
