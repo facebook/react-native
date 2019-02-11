d9c0dfe353eceb91efcec774bab0f65b6792e4fa
Matt Oakes <hello@mattoakes.net>
2019-02-11 13:33:01 -0800

    Add a deprecation warning when importing NetInfo (#23383)
    Summary:
    Add a deprecation warning for the `NetInfo` module as part of #23313.
    
    [General] [Deprecated] - Deprecated NetInfo as it has now been moved to react-native-community/netinfo
    Pull Request resolved: https://github.com/facebook/react-native/pull/23383
    
    Differential Revision: D14024702
    
    Pulled By: cpojer
    
    fbshipit-source-id: 353a9fb86feba2ca7f948c618c642e40fcdbfada

--- Libraries/react-native/react-native-implementation.js
+++ Libraries/react-native/react-native-implementation.js
@@ -173,7 +173,7 @@ module.exports = {
       'webview-moved',
       'WebView has been extracted from react-native core and will be removed in a future release. ' +
         "It can now be installed and imported from 'react-native-webview' instead of 'react-native'. " +
-        'See https://github.com/react-native-community/react-native-webview for more informations.',
+        'See https://github.com/react-native-community/react-native-webview',
     );
     return require('WebView');
   },
@@ -252,6 +252,12 @@ module.exports = {
     return require('NativeEventEmitter');
   },
   get NetInfo() {
+    warnOnce(
+      'netinfo-moved',
+      'NetInfo has been extracted from react-native core and will be removed in a future release. ' +
+        "It can now be installed and imported from '@react-native-community/netinfo' instead of 'react-native'. " +
+        'See https://github.com/react-native-community/react-native-netinfo',
+    );
     return require('NetInfo');
   },
   get PanResponder() {
