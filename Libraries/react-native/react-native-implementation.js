a5b26decb277f2d4f4597ab023e6ed20f6703f0b
Matt Oakes <hello@mattoakes.net>
2019-02-11 15:51:09 +0000

    Add a deprecation warning when importing NetInfo

--- Libraries/react-native/react-native-implementation.js
+++ Libraries/react-native/react-native-implementation.js
@@ -240,6 +240,12 @@ module.exports = {
     return require('NativeEventEmitter');
   },
   get NetInfo() {
+    warnOnce(
+      'netinfo-moved',
+      'NetInfo has been extracted from react-native core and will be removed in a future release. ' +
+        "It can now be installed and imported from '@react-native-community/netinfo' instead of 'react-native'. " +
+        'See https://github.com/react-native-community/react-native-netinfo for more informations.',
+    );
     return require('NetInfo');
   },
   get PanResponder() {
