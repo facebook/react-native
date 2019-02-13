a962cb659f9676ac3c82a73cb75187523e8c4531
Mike Grabowski <grabbou@gmail.com>
2019-02-13 17:10:19 +0100

    Revert "[0.59.0-rc.0] Bump version numbers"
    This reverts commit d059e747041296d79094ed0a8ad4e721134420b7.

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -21,9 +21,9 @@ static void __makeVersion()
 {
   __rnVersion = @{
                   RCTVersionMajor: @(0),
-                  RCTVersionMinor: @(59),
+                  RCTVersionMinor: @(0),
                   RCTVersionPatch: @(0),
-                  RCTVersionPrerelease: @"rc.0",
+                  RCTVersionPrerelease: [NSNull null],
                   };
 }
 
