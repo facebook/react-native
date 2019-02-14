dcb7e5c2a12b81b50e58f18f07026fe1bcaed63d
Héctor Ramos <hector@hectorramos.com>
2019-02-14 10:15:06 -0800

    Auto-accept licenses and unblock CI

--- scripts/android-setup.sh
+++ scripts/android-setup.sh
@@ -16,6 +16,7 @@ function getAndroidPackages {
 
   # Package names can be obtained using `sdkmanager --list`
   if [ ! -e "$DEPS" ] || [ ! "$CI" ]; then
+    yes | sdkmanager --licenses
     echo "Installing Android API level $ANDROID_SDK_TARGET_API_LEVEL, Google APIs, $AVD_ABI system image..."
     sdkmanager "system-images;android-$ANDROID_SDK_TARGET_API_LEVEL;google_apis;$AVD_ABI"
     echo "Installing build SDK for Android API level $ANDROID_SDK_BUILD_API_LEVEL..."
