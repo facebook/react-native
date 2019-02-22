892835810827f367a3d3488f38f7599181db311b
David Vacca <dvacca@fb.com>
2019-02-22 13:46:38 -0800

    optimize updateRootLayoutSpecs to not call setContraints if they did not change
    Summary: This diff avoids calling mBinding.setConstraints when the measurespecs haven't changed
    
    Reviewed By: shergin
    
    Differential Revision: D14142305
    
    fbshipit-source-id: 6770c97dcf0fb23c605a90fb6c3311d2fb771d11

--- ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
@@ -114,6 +114,8 @@ public class FabricUIManager implements UIManager, LifecycleEventListener {
   private long mCommitStartTime = 0l;
   private long mLayoutTime = 0l;
   private long mFinishTransactionTime = 0l;
+  private int mLastWidthMeasureSpec = 0;
+  private int mLastHeightMeasureSpec = 0;
 
   public FabricUIManager(
       ReactApplicationContext reactContext,
@@ -363,13 +365,16 @@ public class FabricUIManager implements UIManager, LifecycleEventListener {
   public void updateRootLayoutSpecs(
       final int rootTag, final int widthMeasureSpec, final int heightMeasureSpec) {
 
-    mBinding.setConstraints(
-      rootTag,
-      getMinSize(widthMeasureSpec),
-      getMaxSize(widthMeasureSpec),
-      getMinSize(heightMeasureSpec),
-      getMaxSize(heightMeasureSpec));
-
+    if (mLastWidthMeasureSpec != widthMeasureSpec || mLastHeightMeasureSpec != heightMeasureSpec) {
+      mLastWidthMeasureSpec = widthMeasureSpec;
+      mLastHeightMeasureSpec = heightMeasureSpec;
+      mBinding.setConstraints(
+          rootTag,
+          getMinSize(widthMeasureSpec),
+          getMaxSize(widthMeasureSpec),
+          getMinSize(heightMeasureSpec),
+          getMaxSize(heightMeasureSpec));
+    }
   }
 
   public void receiveEvent(int reactTag, String eventName, @Nullable WritableMap params) {
