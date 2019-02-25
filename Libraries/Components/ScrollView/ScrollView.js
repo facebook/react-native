7c4dbd617d0af9bfe8a2d8a9b295c3c89e807f5e
Pim de Witte <pim@whitespell.com>
2019-02-25 12:10:40 -0800

    Prop to disable the default pan responder on ScrollView that blocks touches when snapToInterval is enabled (#19110)
    Summary:
    The ScrollView component seems to have been written for a default touch up / touch down scrolling behavior. However, when using snapToInterval, this default behavior of adding PanResponders while the ScrollView is animating or has an active touch causes bugs with the PanResponder hijacking touches it is not supposed to hijack.
    Pull Request resolved: https://github.com/facebook/react-native/pull/19110
    
    Differential Revision: D14071342
    
    Pulled By: hramos
    
    fbshipit-source-id: ac285d9967dd6e2a347943b2455d4f986062ef62

--- Libraries/Components/ScrollView/ScrollView.js
+++ Libraries/Components/ScrollView/ScrollView.js
@@ -96,6 +96,13 @@ type IOSProps = $ReadOnly<{|
    */
   bounces?: ?boolean,
   /**
+   * By default, ScrollView has an active pan responder that hijacks panresponders
+   * deeper in the render tree in order to prevent accidental touches while scrolling.
+   * However, in certain occasions (such as when using snapToInterval) in a vertical scrollview
+   * You may want to disable this behavior in order to prevent the ScrollView from blocking touches
+   */
+  disableScrollViewPanResponder?: ?boolean,
+  /**
    * When true, gestures can drive zoom past min/max and the zoom will animate
    * to the min/max value at gesture end, otherwise the zoom will not exceed
    * the limits.
