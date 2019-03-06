a335dd4393cb9d5b2fb989cb0dce4d91c40a6b39
Dulmandakh <dulmandakh@gmail.com>
2019-03-06 11:41:38 +0800

    use TraceCompat

--- ReactAndroid/src/main/java/com/facebook/systrace/Systrace.java
+++ ReactAndroid/src/main/java/com/facebook/systrace/Systrace.java
@@ -7,8 +7,7 @@
 
 package com.facebook.systrace;
 
-import android.os.Build;
-import android.os.Trace;
+import android.support.v4.os.TraceCompat;
 
 /**
  * Systrace stub that mostly does nothing but delegates to Trace for beginning/ending sections.
@@ -55,15 +54,11 @@ public class Systrace {
   }
 
   public static void beginSection(long tag, final String sectionName) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-      Trace.beginSection(sectionName);
-    }
+    TraceCompat.beginSection(sectionName);
   }
 
   public static void endSection(long tag) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-      Trace.endSection();
-    }
+    TraceCompat.endSection();
   }
 
   public static void beginAsyncSection(
