304a89f755843c98588297f7d32461505fa6cdad
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2019-03-07 10:16:36 +0100

    Fix formatting issues

--- Libraries/Inspector/resolveRelativeSizes.js
+++ Libraries/Inspector/resolveRelativeSizes.js
@@ -15,21 +15,21 @@ const Dimensions = require('Dimensions');
 /**
  * Resolves relative sizes (percentages and auto) in a style object.
  */
-function resolveRelativeSizes(
-  style: Object,
-) {
-  resolveSize(style, "top", "height");
-  resolveSize(style, "right", "width");
-  resolveSize(style, "bottom", "height");
-  resolveSize(style, "left", "width");
+function resolveRelativeSizes(style: Object) {
+  resolveSize(style, 'top', 'height');
+  resolveSize(style, 'right', 'width');
+  resolveSize(style, 'bottom', 'height');
+  resolveSize(style, 'left', 'width');
 }
 
 function resolveSize(style, direction, dimension) {
-  if (style[direction] !== null && typeof style[direction] === "string") {
+  if (style[direction] !== null && typeof style[direction] === 'string') {
     if (style[direction].indexOf('%') !== -1) {
-      style[direction] = (parseFloat(style[direction]) / 100.0) * Dimensions.get('window')[dimension];
+      style[direction] =
+        (parseFloat(style[direction]) / 100.0) *
+        Dimensions.get('window')[dimension];
     }
-    if (style[direction] === "auto") {
+    if (style[direction] === 'auto') {
       // Ignore auto sizing in frame drawing due to complexity of correctly rendering this
       style[direction] = 0;
     }
