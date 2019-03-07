ed14447a44a6dfd888ccd11ce52886964ee799c8
Andrew Schenk <andrewschenk@linxsystems.com>
2019-03-07 12:13:57 -0600

    adds type checking to Animated toValue and iterations key values to make sure Android does not crash from bad params when using useNativeDriver

--- ReactAndroid/src/main/java/com/facebook/react/animated/FrameBasedAnimationDriver.java
+++ ReactAndroid/src/main/java/com/facebook/react/animated/FrameBasedAnimationDriver.java
@@ -41,8 +41,17 @@ class FrameBasedAnimationDriver extends AnimationDriver {
     for (int i = 0; i < numberOfFrames; i++) {
       mFrames[i] = frames.getDouble(i);
     }
-    mToValue = config.hasKey("toValue") ? config.getDouble("toValue") : 0;
-    mIterations = config.hasKey("iterations") ? config.getInt("iterations") : 1;
+    if(config.hasKey("toValue")) {
+      mToValue = config.getType("toValue") == ReadableType.Number ? config.getDouble("toValue") : 0;
+    } else {
+      mToValue = 0;
+    }
+    if(config.hasKey("iterations")) {
+      mIterations = config.getType("iterations") == ReadableType.Number ?
+                                                    config.getInt("iterations") : 1;
+    } else {
+      mIterations = 1;
+    }
     mCurrentLoop = 1;
     mHasFinished = mIterations == 0;
     mStartFrameTimeNanos = -1;
