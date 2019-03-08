63c17c9b365481cf52db9f4f9fd146916ab4bd49
ericlewis <ericlewis777@gmail.com>
2019-03-07 22:07:00 -0500

    Fix shell lint

--- scripts/generate-rncore.sh
+++ scripts/generate-rncore.sh
@@ -1,2 +1,8 @@
-find "$PWD/Libraries" -name "*Schema.js" | xargs yarn flow-node packages/react-native-codegen/buck_tests/combine-js-to-schema-cli.js schema-rncore.json
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+find "$PWD/Libraries" -name "*Schema.js" -print | xargs yarn flow-node packages/react-native-codegen/buck_tests/combine-js-to-schema-cli.js schema-rncore.json
 yarn flow-node packages/react-native-codegen/buck_tests/generate-tests.js schema-rncore.json rncore ReactCommon/fabric/components/rncore
