1c57282333b78ea90b1cc523f5c3e1d3899024e9
ericlewis <ericlewis777@gmail.com>
2019-03-12 17:47:43 -0400

    Fix script

--- scripts/generate-rncore.sh
+++ scripts/generate-rncore.sh
@@ -4,5 +4,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-find "$PWD/Libraries" -name "*Schema.js" -print0 | xargs yarn flow-node packages/react-native-codegen/buck_tests/combine-js-to-schema-cli.js schema-rncore.json
+find "$PWD/../Libraries" -name "*Schema.js" -print | xargs yarn flow-node packages/react-native-codegen/buck_tests/combine-js-to-schema-cli.js schema-rncore.json
 yarn flow-node packages/react-native-codegen/buck_tests/generate-tests.js schema-rncore.json rncore ReactCommon/fabric/components/rncore
