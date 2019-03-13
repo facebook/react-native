d3458571bf5af1dc13dc7f90bb17148c9f2f7ec3
ericlewis <ericlewis777@gmail.com>
2019-03-13 16:18:39 -0400

    add copyright header

--- RNTester/Swizzle_RCTFabricSurface.m
+++ RNTester/Swizzle_RCTFabricSurface.m
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #ifdef FABRIC_ENABLED
 #import <objc/runtime.h>
 #import <React/RCTFabricSurface.h>
