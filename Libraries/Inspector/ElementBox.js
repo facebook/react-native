e3bafa3f7e517c4d1d4cc745adf9ef4ec40f898a
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2019-03-13 09:11:12 +0100

    Fix flow types and inline functions

--- Libraries/Inspector/ElementBox.js
+++ Libraries/Inspector/ElementBox.js
@@ -14,16 +14,16 @@ const BorderBox = require('BorderBox');
 const React = require('React');
 const StyleSheet = require('StyleSheet');
 const View = require('View');
+const Dimensions = require('Dimensions');
 
 const flattenStyle = require('flattenStyle');
 const resolveBoxStyle = require('resolveBoxStyle');
-const resolveRelativeSizes = require('resolveRelativeSizes');
 
 class ElementBox extends React.Component<$FlowFixMeProps> {
   render() {
     const style = flattenStyle(this.props.style) || {};
-    const margin = resolveBoxStyle('margin', style);
-    const padding = resolveBoxStyle('padding', style);
+    let margin = resolveBoxStyle('margin', style);
+    let padding = resolveBoxStyle('padding', style);
 
     const frameStyle = {...this.props.frame};
     const contentStyle = {
@@ -32,7 +32,7 @@ class ElementBox extends React.Component<$FlowFixMeProps> {
     };
 
     if (margin != null) {
-      resolveRelativeSizes(margin);
+      margin = resolveRelativeSizes(margin);
 
       frameStyle.top -= margin.top;
       frameStyle.left -= margin.left;
@@ -54,7 +54,7 @@ class ElementBox extends React.Component<$FlowFixMeProps> {
     }
 
     if (padding != null) {
-      resolveRelativeSizes(padding);
+      padding = resolveRelativeSizes(padding);
 
       contentStyle.width -= padding.left + padding.right;
       contentStyle.height -= padding.top + padding.bottom;
@@ -86,5 +86,52 @@ const styles = StyleSheet.create({
     borderColor: 'rgba(255, 132, 0, 0.3)', // orange
   },
 });
+
+type Style = {
+  top: number,
+  right: number,
+  bottom: number,
+  left: number,
+};
+
+/**
+ * Resolves relative sizes (percentages and auto) in a style object.
+ *
+ * @param style the style to resolve
+ * @return a modified copy
+ */
+function resolveRelativeSizes(style: Style): $ReadOnly<Style> {
+  let resolvedStyle = Object.assign({}, style);
+  resolveSizeInPlace(resolvedStyle, 'top', 'height');
+  resolveSizeInPlace(resolvedStyle, 'right', 'width');
+  resolveSizeInPlace(resolvedStyle, 'bottom', 'height');
+  resolveSizeInPlace(resolvedStyle, 'left', 'width');
+  return resolvedStyle;
+}
+
+/**
+ * Resolves the given size of a style object in place.
+ *
+ * @param style the style object to modify
+ * @param direction the direction to resolve (e.g. 'top')
+ * @param dimension the window dimension that this direction belongs to (e.g. 'height')
+ */
+function resolveSizeInPlace(
+  style: Style,
+  direction: string,
+  dimension: string,
+) {
+  if (style[direction] !== null && typeof style[direction] === 'string') {
+    if (style[direction].indexOf('%') !== -1) {
+      style[direction] =
+        (parseFloat(style[direction]) / 100.0) *
+        Dimensions.get('window')[dimension];
+    }
+    if (style[direction] === 'auto') {
+      // Ignore auto sizing in frame drawing due to complexity of correctly rendering this
+      style[direction] = 0;
+    }
+  }
+}
 
 module.exports = ElementBox;
