8270de9c2cfad721b554201ab97d645ffbfdc84b
Mike Grabowski <grabbou@gmail.com>
2019-03-12 18:30:31 -0700

    Fix DatePickerIOS e2e tests (#23861)
    Summary:
    DatePickerIOS tests stopped working after US changed their time zone on CircleCI. I decided to update the tests to be more reliable, by:
    - removing "timezoneoffset" from the examples - I don't think it's needed. We don't demonstrate other props and it was causing us some troubles when timezones actually changed
    - changing "interval" example from "time-only" to "datetime" because there's a known bug that prevented the previous example from working https://github.com/facebook/react-native/issues/9566
    - splitting the label to two: one for date and other one for time, so that we can match the date only when we test "date" mode only
    
    [IOS] [FIXED] - Improved reliability of DatePickerIOS e2e tests
    Pull Request resolved: https://github.com/facebook/react-native/pull/23861
    
    Differential Revision: D14434324
    
    Pulled By: cpojer
    
    fbshipit-source-id: 0f82b7e94bb1cb6ce75b44badd0064d1754370b9

--- RNTester/js/DatePickerIOSExample.js
+++ RNTester/js/DatePickerIOSExample.js
@@ -12,11 +12,10 @@
 
 const React = require('react');
 const ReactNative = require('react-native');
-const {DatePickerIOS, StyleSheet, Text, TextInput, View} = ReactNative;
+const {DatePickerIOS, StyleSheet, Text, View} = ReactNative;
 
 type State = {|
   date: Date,
-  timeZoneOffsetInHours: number,
 |};
 
 type Props = {|
@@ -26,43 +25,26 @@ type Props = {|
 class WithDatePickerData extends React.Component<Props, State> {
   state = {
     date: new Date(),
-    timeZoneOffsetInHours: (-1 * new Date().getTimezoneOffset()) / 60,
   };
 
   onDateChange = date => {
     this.setState({date: date});
   };
 
-  onTimezoneChange = event => {
-    const offset = parseInt(event.nativeEvent.text, 10);
-    if (isNaN(offset)) {
-      return;
-    }
-    this.setState({timeZoneOffsetInHours: offset});
-  };
-
   render() {
-    // Ideally, the timezone input would be a picker rather than a
-    // text input, but we don't have any pickers yet :(
     return (
       <View>
         <WithLabel label="Value:">
-          <Text testID="date-and-time-indicator">
-            {this.state.date.toLocaleDateString() +
-              ' ' +
-              this.state.date.toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit',
-              })}
+          <Text testID="date-indicator">
+            {this.state.date.toLocaleDateString()}
           </Text>
-        </WithLabel>
-        <WithLabel label="Timezone:">
-          <TextInput
-            onChange={this.onTimezoneChange}
-            style={styles.textinput}
-            value={this.state.timeZoneOffsetInHours.toString()}
-          />
-          <Text> hours from UTC</Text>
+          <Text>&nbsp;</Text>
+          <Text testID="time-indicator">
+            {this.state.date.toLocaleTimeString([], {
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
+          </Text>
         </WithLabel>
         {this.props.children(this.state, this.onDateChange)}
       </View>
@@ -124,7 +106,6 @@ exports.examples = [
               testID="date-and-time"
               date={state.date}
               mode="datetime"
-              timeZoneOffsetInMinutes={state.timeZoneOffsetInHours * 60}
               onDateChange={onDateChange}
             />
           )}
@@ -142,7 +123,6 @@ exports.examples = [
               testID="date-only"
               date={state.date}
               mode="date"
-              timeZoneOffsetInMinutes={state.timeZoneOffsetInHours * 60}
               onDateChange={onDateChange}
             />
           )}
@@ -151,16 +131,16 @@ exports.examples = [
     },
   },
   {
-    title: 'Time only picker, 10-minute interval',
+    title: 'Picker with 20-minute interval',
     render: function(): React.Element<any> {
       return (
         <WithDatePickerData>
           {(state, onDateChange) => (
             <DatePickerIOS
-              testID="time-only"
+              testID="date-and-time-with-interval"
               date={state.date}
-              mode="time"
-              timeZoneOffsetInMinutes={state.timeZoneOffsetInHours * 60}
+              minuteInterval={20}
+              mode="datetime"
               onDateChange={onDateChange}
             />
           )}
