03841cf08c50bc7033497b7fa9c3a788eb072ed3
Alexey Lang <alexeylang@fb.com>
2019-03-14 06:45:25 -0700

    Migrate PerformanceLoggerFlag to scoped performance logger
    Reviewed By: xyin96
    
    Differential Revision: D14385136
    
    fbshipit-source-id: beaf060866ed5b7dee80e18fcd432e7701cff5d3

--- Libraries/Utilities/createPerformanceLogger.js
+++ Libraries/Utilities/createPerformanceLogger.js
@@ -42,6 +42,7 @@ export type IPerformanceLogger = {
   markPoint(string, number | void): void,
   getPoints(): {[key: string]: number},
   logPoints(): void,
+  logEverything(): void,
 };
 
 const _cookies: {[key: string]: number} = {};
@@ -241,6 +242,12 @@ function createPerformanceLogger(): IPerformanceLogger {
       for (const key in this._points) {
         infoLog(key + ': ' + this._points[key] + 'ms');
       }
+    },
+
+    logEverything() {
+      this.logTimespans();
+      this.logExtras();
+      this.logPoints();
     },
   };
   return result;
