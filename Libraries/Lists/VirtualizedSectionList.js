facebook/react-native

commit f66e8ebf51a04f47eaa2406bce461ef8f8f34d0a

Fixes #issue18098 SectionList scrollToLocation (#21577)
Summary:
Fixes #18098
Pull Request resolved: https://github.com/facebook/react-native/pull/21577

Differential Revision: D13761884

Pulled By: cpojer

fbshipit-source-id: a096d69c589815d00754427fb575de0d8d0b595f

Libraries/Lists/VirtualizedSectionList.js
@@ -9,6 +9,7 @@
  */
 'use strict';
 
+const Platform = require('Platform');
 const React = require('React');
 const View = require('View');
 const VirtualizedList = require('VirtualizedList');
@@ -145,7 +146,7 @@ class VirtualizedSectionList<SectionT: SectionBase> extends React.PureComponent<
     sectionIndex: number,
     viewPosition?: number,
   }) {
-    let index = params.itemIndex + 1;
+    let index = Platform.OS === 'ios' ? params.itemIndex : params.itemIndex - 1;
     for (let ii = 0; ii < params.sectionIndex; ii++) {
       index += this.props.sections[ii].data.length + 2;
     }

commit 64ec33a658eee7b1c6d9c05503d0ba4bd0df18b3

Fixed regression in SectionList caused by #21577 not being able to scroll to top on android

Libraries/Lists/VirtualizedSectionList.js
@@ -146,7 +146,7 @@ class VirtualizedSectionList<SectionT: SectionBase> extends React.PureComponent<
     sectionIndex: number,
     viewPosition?: number,
   }) {
-    let index = Platform.OS === 'ios' ? params.itemIndex : params.itemIndex - 1;
+    let index = Platform.OS === 'ios' ? params.itemIndex : params.itemIndex + 1;
     for (let ii = 0; ii < params.sectionIndex; ii++) {
       index += this.props.sections[ii].data.length + 2;
     }