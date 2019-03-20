871290f33a632d26ef36076d1f3beaa32e539816
Dulmandakh <dulmandakh@gmail.com>
2019-03-20 11:21:12 -0700

    improve RTL (#24069)
    Summary:
    Google recommends to use Gravity.START and Gravity.END instead of Gravity.LEFT and Gravity.RIGHT to support RTL better.
    
    [Android] [Changed] - Improve RTL support
    Pull Request resolved: https://github.com/facebook/react-native/pull/24069
    
    Differential Revision: D14541569
    
    Pulled By: cpojer
    
    fbshipit-source-id: 5c104d8bd666e1270d5410216c7f2efa6152692a

--- ReactAndroid/src/main/java/com/facebook/react/views/text/TextAttributeProps.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/text/TextAttributeProps.java
@@ -164,10 +164,10 @@ public class TextAttributeProps {
   public int getTextAlign() {
     int textAlign = mTextAlign;
     if (getLayoutDirection() == YogaDirection.RTL) {
-      if (textAlign == Gravity.RIGHT) {
-        textAlign = Gravity.LEFT;
-      } else if (textAlign == Gravity.LEFT) {
-        textAlign = Gravity.RIGHT;
+      if (textAlign == Gravity.END) {
+        textAlign = Gravity.START;
+      } else if (textAlign == Gravity.START) {
+        textAlign = Gravity.END;
       }
     }
     return textAlign;
@@ -210,7 +210,7 @@ public class TextAttributeProps {
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         mJustificationMode = Layout.JUSTIFICATION_MODE_INTER_WORD;
       }
-      mTextAlign = Gravity.LEFT;
+      mTextAlign = Gravity.START;
     } else {
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         mJustificationMode = Layout.JUSTIFICATION_MODE_NONE;
@@ -219,9 +219,9 @@ public class TextAttributeProps {
       if (textAlign == null || "auto".equals(textAlign)) {
         mTextAlign = Gravity.NO_GRAVITY;
       } else if ("left".equals(textAlign)) {
-        mTextAlign = Gravity.LEFT;
+        mTextAlign = Gravity.START;
       } else if ("right".equals(textAlign)) {
-        mTextAlign = Gravity.RIGHT;
+        mTextAlign = Gravity.END;
       } else if ("center".equals(textAlign)) {
         mTextAlign = Gravity.CENTER_HORIZONTAL;
       } else {
