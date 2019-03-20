0b18febf5439c89e5b2f2a9163358c374e61482c
Rahul kishan M <Rahulkishanm@users.noreply.github.com>
2019-03-20 12:20:43 +0530

    Picker Items should filter NULL child
    when conditional rendering is used then picker breaks when the child is null.
    
    This conditional rendering inside Picker fails when someBooleanValue variable is false
    ```
    {
       someBooleanValue && <Picker.Item label="value" value="value" />
    }
    ```

--- Libraries/Components/Picker/PickerIOS.ios.js
+++ Libraries/Components/Picker/PickerIOS.ios.js
@@ -91,7 +91,7 @@ class PickerIOS extends React.Component<Props, State> {
   static getDerivedStateFromProps(props: Props): State {
     let selectedIndex = 0;
     const items = [];
-    React.Children.toArray(props.children).forEach(function(child, index) {
+    React.Children.toArray(props.children).filter(child => child !== null).forEach(function(child, index) {
       if (child.props.value === props.selectedValue) {
         selectedIndex = index;
       }
