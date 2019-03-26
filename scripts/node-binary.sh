d4877c6b17c241ef718eca4c264c87ac3d0f80d8
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2019-03-26 17:41:48 +0100

    Fix: use NODE_BINARY when starting packager

---
+++ scripts/node-binary.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+[ -z "$NODE_BINARY" ] && export NODE_BINARY="node"
+
+nodejs_not_found()
+{
+  echo "error: Can't find '$NODE_BINARY' binary to build React Native bundle" >&2
+  echo "If you have non-standard nodejs installation, select your project in Xcode," >&2
+  echo "find 'Build Phases' - 'Bundle React Native code and images'" >&2
+  echo "and change NODE_BINARY to absolute path to your node executable" >&2
+  echo "(you can find it by invoking 'which node' in the terminal)" >&2
+  exit 2
+}
+
+type "$NODE_BINARY" >/dev/null 2>&1 || nodejs_not_found
