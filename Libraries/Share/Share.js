ec517042706848fe01a30b1c6e067f50d4563781
ansal-exp <32477006+ansal-exp@users.noreply.github.com>
2019-04-03 12:45:05 +0530

    run prettier

--- Libraries/Share/Share.js
+++ Libraries/Share/Share.js
@@ -64,9 +64,9 @@ class Share {
   static share(content: Content, options: Options = {}): Promise<Object> {
     console.warn(
       'Warning: Share package is deprecated in favour of the lean core.' +
-      'Use https://github.com/react-native-community/react-native-simple-share ' +
-      'for the exact same feature set or use https://github.com/react-native-community/react-native-share ' +
-      'for a rich feature set.',
+        'Use https://github.com/react-native-community/react-native-simple-share ' +
+        'for the exact same feature set or use https://github.com/react-native-community/react-native-share ' +
+        'for a rich feature set.',
     );
     invariant(
       typeof content === 'object' && content !== null,
