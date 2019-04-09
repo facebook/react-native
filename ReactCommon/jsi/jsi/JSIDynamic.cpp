5e5439a92171c16a929cc70f63a9c783d425e240
Will Holen <willholen@fb.com>
2019-04-09 11:36:42 -0700

    Update JSI from upstream
    Summary: This change synchronizes the JSI API with its upstream, aiming to make this easier to automate in the future.
    
    Reviewed By: mhorowitz
    
    Differential Revision: D14783311
    
    fbshipit-source-id: c180d0f728afbeb87a3e8e7331a39c06e73b907e

--- ReactCommon/jsi/jsi/JSIDynamic.cpp
+++ ReactCommon/jsi/jsi/JSIDynamic.cpp
@@ -1,9 +1,12 @@
-//  Copyright (c) Facebook, Inc. and its affiliates.
-//
-// This source code is licensed under the MIT license found in the
- // LICENSE file in the root directory of this source tree.
-
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
 #include "JSIDynamic.h"
+
+#include <glog/logging.h>
 
 #include <folly/dynamic.h>
 #include <jsi/jsi.h>
