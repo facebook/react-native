78b3d50dd17071bfab03f76dccc3889100316bd4
Brian Zhao <rezha@microsoft.com>
2019-04-09 16:53:34 -0700

    Revert "Added stubs for some native modules"
    This reverts commit a93b7a2da0849f15708afef1ff99022d8cc55b14.

--- Libraries/Network/RCTNetworking.ios.js
+++ Libraries/Network/RCTNetworking.ios.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-const MissingNativeEventEmitterShim = require('MissingNativeEventEmitterShim');
+const FormData = require('FormData');
 const NativeEventEmitter = require('NativeEventEmitter');
 const RCTNetworkingNative = require('NativeModules').Networking;
 const convertRequestBody = require('convertRequestBody');
@@ -20,7 +20,6 @@ import type {RequestBody} from 'convertRequestBody';
 import type {NativeResponseType} from './XMLHttpRequest';
 
 class RCTNetworking extends NativeEventEmitter {
-  isAvailable: boolean = true;
 
   constructor() {
     super(RCTNetworkingNative);
@@ -63,31 +62,4 @@ class RCTNetworking extends NativeEventEmitter {
   }
 }
 
-if (__DEV__ && !RCTNetworkingNative) {
-  class MissingNativeRCTNetworkingShim extends MissingNativeEventEmitterShim {
-    constructor() {
-      super('RCTNetworking', 'Networking');
-    }
-
-    sendRequest(...args: Array<any>) {
-      this.throwMissingNativeModule();
-    }
-
-    abortRequest(...args: Array<any>) {
-      this.throwMissingNativeModule();
-    }
-
-    clearCookies(...args: Array<any>) {
-      this.throwMissingNativeModule();
-    }
-  }
-
-  // This module depends on the native `RCTNetworkingNative` module. If you don't include it,
-  // `RCTNetworking.isAvailable` will return `false`, and any method calls will throw.
-  // We reassign the class variable to keep the autodoc generator happy.
-  RCTNetworking = new MissingNativeRCTNetworkingShim();
-} else {
-  RCTNetworking = new RCTNetworking();
-}
-
-module.exports = RCTNetworking;
+module.exports = new RCTNetworking();
