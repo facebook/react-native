d7e79845c69844349c4a27fac52ed636237f999b
Héctor Ramos <hector@hectorramos.com>
2019-04-09 17:48:38 -0700

    Switch back to using CLI for test app scaffolding. Use npm tag to ensure local package isinstalled.

--- scripts/run-ci-e2e-tests.js
+++ scripts/run-ci-e2e-tests.js
@@ -77,11 +77,14 @@ try {
   if (
     tryExecNTimes(
       () => {
-        exec('sleep 10s');
-        return exec('react-native init EndToEndTest').code;
+        return exec(`react-native init EndToEndTest --version ${PACKAGE} --npm`)
+          .code;
       },
       numberOfRetries,
-      () => rm('-rf', E2E_APP_ROOT),
+      () => {
+        rm('-rf', 'EndToEndTest');
+        exec('sleep 10s');
+      },
     )
   ) {
     echo('Failed to execute react-native init');
@@ -95,11 +98,10 @@ try {
   if (
     tryExecNTimes(
       () => {
-        exec('sleep 10s');
         return exec('npm install --save-dev flow-bin').code;
       },
       numberOfRetries,
-      () => rm('-rf', E2E_APP_ROOT),
+      () => exec('sleep 10s'),
     )
   ) {
     echo('Failed to install Flow');
@@ -111,11 +113,10 @@ try {
   if (
     tryExecNTimes(
       () => {
-        exec('sleep 10s');
         return exec('npm install').code;
       },
       numberOfRetries,
-      () => rm('-rf', E2E_APP_ROOT),
+      () => exec('sleep 10s'),
     )
   ) {
     echo('Failed to execute npm install');
@@ -180,10 +181,13 @@ try {
     exec('sleep 15s');
     describe('Test: Android end-to-end test');
     if (
-      tryExecNTimes(() => {
-        exec('sleep 10s');
-        return exec('node node_modules/.bin/_mocha android-e2e-test.js').code;
-      }, numberOfRetries)
+      tryExecNTimes(
+        () => {
+          return exec('node node_modules/.bin/_mocha android-e2e-test.js').code;
+        },
+        numberOfRetries,
+        () => exec('sleep 10s'),
+      )
     ) {
       echo('Failed to run Android end-to-end tests');
       echo('Most likely the code is broken');
@@ -214,43 +218,46 @@ try {
     exec('pod install');
     describe('Test: ' + iosTestType + ' end-to-end test');
     if (
-      tryExecNTimes(() => {
-        exec('sleep 10s');
-        let destination = 'platform=iOS Simulator,name=iPhone 6s,OS=12.1';
-        let sdk = 'iphonesimulator';
-        let scheme = 'EndToEndTest';
+      tryExecNTimes(
+        () => {
+          let destination = 'platform=iOS Simulator,name=iPhone 6s,OS=12.1';
+          let sdk = 'iphonesimulator';
+          let scheme = 'EndToEndTest';
 
-        if (argv.tvos) {
-          destination = 'platform=tvOS Simulator,name=Apple TV,OS=11.4';
-          sdk = 'appletvsimulator';
-          scheme = 'EndToEndTest-tvOS';
-        }
+          if (argv.tvos) {
+            destination = 'platform=tvOS Simulator,name=Apple TV,OS=11.4';
+            sdk = 'appletvsimulator';
+            scheme = 'EndToEndTest-tvOS';
+          }
 
-        return exec(
-          [
-            'xcodebuild',
-            '-workspace',
-            '"EndToEndTest.xcworkspace"',
-            '-destination',
-            `"${destination}"`,
-            '-scheme',
-            `"${scheme}"`,
-            '-sdk',
-            sdk,
-            '-UseModernBuildSystem=NO',
-            'test',
-          ].join(' ') +
-            ' | ' +
+          return exec(
             [
-              'xcpretty',
-              '--report',
-              'junit',
-              '--output',
-              `"~/react-native/reports/junit/${iosTestType}-e2e/results.xml"`,
+              'xcodebuild',
+              '-workspace',
+              '"EndToEndTest.xcworkspace"',
+              '-destination',
+              `"${destination}"`,
+              '-scheme',
+              `"${scheme}"`,
+              '-sdk',
+              sdk,
+              '-UseModernBuildSystem=NO',
+              'test',
             ].join(' ') +
-            ' && exit ${PIPESTATUS[0]}',
-        ).code;
-      }, numberOfRetries)
+              ' | ' +
+              [
+                'xcpretty',
+                '--report',
+                'junit',
+                '--output',
+                `"~/react-native/reports/junit/${iosTestType}-e2e/results.xml"`,
+              ].join(' ') +
+              ' && exit ${PIPESTATUS[0]}',
+          ).code;
+        },
+        numberOfRetries,
+        () => exec('sleep 10s'),
+      )
     ) {
       echo('Failed to run ' + iosTestType + ' end-to-end tests');
       echo('Most likely the code is broken');
