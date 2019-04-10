fe395b2be79af4801c29cb2b9db389dafe3e1db0
acoates-ms <30809111+acoates-ms@users.noreply.github.com>
2019-04-10 10:38:16 -0700

    Remove a bunch of jsc files that should have been removed with 58 (#44)
    * Remove a bunch of jsc files that should have been removed with 58
    
    * more build fix
    
    * Put back JSI JSCRuntime build switch
    
    * Put back jni changes
    
    * fix build
    
    * Remove unneeded build logic

--- ReactCommon/cxxreact/Android.mk
+++ ReactCommon/cxxreact/Android.mk
@@ -44,27 +44,10 @@ LOCAL_V8_FILES := \
     V8NativeModules.cpp \
     V8Executor.cpp 
 
-LOCAL_JSC_FILES := \
-    JSCExecutor.cpp \
-    JSCLegacyTracing.cpp \
-    JSCMemory.cpp \
-    JSCNativeModules.cpp \
-    JSCPerfStats.cpp \
-    JSCSamplingProfiler.cpp \
-    JSCTracing.cpp \
-    JSCUtils.cpp \
-
-ifeq ($(V8_ENABLED), 1)
-  LOCAL_SRC_FILES += $(LOCAL_V8_FILES)
-  LOCAL_CFLAGS += -DV8_ENABLED=1
-  LOCAL_STATIC_LIBRARIES := v8helpers
-  LOCAL_SHARED_LIBRARIES += libv8 libv8platform libv8base
-else
-  LOCAL_SRC_FILES += $(LOCAL_JSC_FILES)
-  LOCAL_CFLAGS += -DV8_ENABLED=0
-  LOCAL_STATIC_LIBRARIES := jschelpers
-  LOCAL_SHARED_LIBRARIES += libjsc
-endif
+LOCAL_SRC_FILES += $(LOCAL_V8_FILES)
+LOCAL_CFLAGS += -DV8_ENABLED=1
+LOCAL_STATIC_LIBRARIES := v8helpers
+LOCAL_SHARED_LIBRARIES += libv8 libv8platform libv8base
 
 
 include $(BUILD_STATIC_LIBRARY)
@@ -74,12 +57,7 @@ $(call import-module,folly)
 $(call import-module,glog)
 $(call import-module,jsinspector)
 $(call import-module,privatedata)
-ifeq ($(V8_ENABLED),1)
-  $(call import-module,v8)
-  $(call import-module,v8base)
-  $(call import-module,v8helpers) 
-  $(call import-module,v8platform) 
-else
-  $(call import-module,jsc)
-  $(call import-module,jschelpers)
-endif
+$(call import-module,v8)
+$(call import-module,v8base)
+$(call import-module,v8helpers) 
+$(call import-module,v8platform) 
