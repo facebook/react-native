faaa92bb04467f1d8cef0fe7ce309643486b7a0d
Taylor123 <cjohns28@terpmail.umd.edu>
2019-04-15 14:35:55 -0700

    disable momentum scrolling for horizontal ScrollView (#24045)
    Summary:
    Would like feedback from the community as this may not be the best solution for all
    
    I would like to restrict (or paginate) the fling of a horizontal ScrollView when `snapToInterval` is set. This is not currently possible with `pagingEnabled`, since the pagination works only when items are the entire width of the ScrollView.
    
    This implementation simply restricts the predicted `targetOffset` found from the `x` velocity and replaces it with the offset when the pan gesture ended.
    
    To get pagination working, I may paginate based on the interval by calculating the offset delta from the beginning of the gesture to current offset and restricting the scrolling behavior to the `snapToInterval`. If this is preferred, I can update this PR or make a new one, but wanted to start a discussion since it seems like there are many in the community that would like this feature  #21302 .
    
    [General] [Added] - add prop `disableIntervalMomentum` to disable the predictive scrolling behavior of horizontal ScrollViews
    Pull Request resolved: https://github.com/facebook/react-native/pull/24045
    
    Differential Revision: D14939754
    
    Pulled By: sahrens
    
    fbshipit-source-id: 26be19c47dfb8eed4d7e6035df53a77451e23081

--- Libraries/Components/ScrollView/ScrollView.js
+++ Libraries/Components/ScrollView/ScrollView.js
@@ -369,6 +369,13 @@ export type Props = $ReadOnly<{|
    */
   contentContainerStyle?: ?ViewStyleProp,
   /**
+   * When true, the scroll view stops on the next index (in relation to scroll
+   * position at release) regardless of how fast the gesture is. This can be
+   * used for horizontal pagination when the page is less than the width of
+   * the ScrollView. The default value is false.
+   */
+  disableIntervalMomentum?: ?boolean,
+  /**
    * A floating-point number that determines how quickly the scroll view
    * decelerates after the user lifts their finger. You may also use string
    * shortcuts `"normal"` and `"fast"` which match the underlying iOS settings
