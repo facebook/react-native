0517abce5e61c3f28c36efac68f296265cf59623
Dulmandakh <dulmandakh@gmail.com>
2019-04-20 20:39:58 +0800

    bump gradle to 5.4

--- gradlew.bat
+++ gradlew.bat
@@ -14,7 +14,7 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+set DEFAULT_JVM_OPTS="-Xmx64m"
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
