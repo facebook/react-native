facebook/react-native

commit 1f8b46a2fc191e2899735fbdcdbb535a91b63724

Clean up EventEmitter
Summary: As suggested by yungsters in D13138187

Reviewed By: mjesun

Differential Revision: D13192765

fbshipit-source-id: 8ab3dc74b3a9aaebf043bc3b5ed4f2590f480d10

Libraries/vendor/emitter/EventEmitter.js
@@ -16,8 +16,6 @@ const EventSubscriptionVendor = require('EventSubscriptionVendor');
 
 const invariant = require('fbjs/lib/invariant');
 
-const emptyFunctionThatReturnsTrue = () => true;
-
 /**
  * @class EventEmitter
  * @description
@@ -151,13 +149,9 @@ class EventEmitter {
    * @returns {array}
    */
   listeners(eventType: string): [EmitterSubscription] {
-    const subscriptions: ?[
-      EmitterSubscription,
-    ] = (this._subscriber.getSubscriptionsForType(eventType): any);
+    const subscriptions = this._subscriber.getSubscriptionsForType(eventType);
     return subscriptions
-      ? subscriptions
-          .filter(emptyFunctionThatReturnsTrue)
-          .map(subscription => subscription.listener)
+      ? subscriptions.map(subscription => subscription.listener)
       : [];
   }
 
@@ -176,9 +170,7 @@ class EventEmitter {
    *   emitter.emit('someEvent', 'abc'); // logs 'abc'
    */
   emit(eventType: string) {
-    const subscriptions: ?[
-      EmitterSubscription,
-    ] = (this._subscriber.getSubscriptionsForType(eventType): any);
+    const subscriptions = this._subscriber.getSubscriptionsForType(eventType);
     if (subscriptions) {
       for (let i = 0, l = subscriptions.length; i < l; i++) {
         const subscription = subscriptions[i];
@@ -210,9 +202,7 @@ class EventEmitter {
    *
    */
   removeListener(eventType: String, listener) {
-    const subscriptions: ?[
-      EmitterSubscription,
-    ] = (this._subscriber.getSubscriptionsForType(eventType): any);
+    const subscriptions = this._subscriber.getSubscriptionsForType(eventType);
     if (subscriptions) {
       for (let i = 0, l = subscriptions.length; i < l; i++) {
         const subscription = subscriptions[i];

commit 7db6fa1f4f620882296bf89817b4742b2677688c

[emitter] Fix sparse array handling in `EventEmitter#listeners()`
Fixes a regression in https://github.com/facebook/react-native/commit/1f8b46a2fc191e2899735fbdcdbb535a91b63724#diff-141db70a3fb9a501015e4cb2508e3a65. The internal subscription vendor uses a sparse array to track listeners, which makes listener removal fast. When querying listeners, the sparse entries need to be removed. `Array#filter` is a built-in way to do this -> linked to the JS spec, which explains this.

Libraries/vendor/emitter/EventEmitter.js
@@ -16,6 +16,8 @@ const EventSubscriptionVendor = require('EventSubscriptionVendor');
 
 const invariant = require('invariant');
 
+const sparseFilterPredicate = () => true;
+
 /**
  * @class EventEmitter
  * @description
@@ -151,7 +153,13 @@ class EventEmitter {
   listeners(eventType: string): [EmitterSubscription] {
     const subscriptions = this._subscriber.getSubscriptionsForType(eventType);
     return subscriptions
-      ? subscriptions.map(subscription => subscription.listener)
+      ? subscriptions
+          // We filter out missing entries because the array is sparse.
+          // "callbackfn is called only for elements of the array which actually
+          // exist; it is not called for missing elements of the array."
+          // https://www.ecma-international.org/ecma-262/9.0/index.html#sec-array.prototype.filter
+          .filter(sparseFilterPredicate)
+          .map(subscription => subscription.listener)
       : [];
   }
 