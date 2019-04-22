4fe14cf8b05d4a528287f591a5a8e36d0460755a
Janic Duplessis <janicduplessis@gmail.com>
2019-04-22 09:27:11 -0400

    Fix typo

--- Libraries/NativeAnimation/RCTNativeAnimatedNodesManager.m
+++ Libraries/NativeAnimation/RCTNativeAnimatedNodesManager.m
@@ -28,7 +28,7 @@
 #import "RCTValueAnimatedNode.h"
 #import "RCTTrackingAnimatedNode.h"
 
-// We de some normalizing of the event names in RCTEventDispatcher#RCTNormalizeInputEventName.
+// We do some normalizing of the event names in RCTEventDispatcher#RCTNormalizeInputEventName.
 // To make things simpler just get rid of the parts we change in the event names we use here.
 // This is a lot easier than trying to denormalize because there would be multiple possible
 // denormalized forms for a single input.
