706f67a882310ea5f43b2e000e0ebd15ae168e25
Mike Grabowski <grabbou@gmail.com>
2019-04-24 04:44:27 -0700

    Use latest React Native CLI (#24517)
    Summary:
    Updates React Native to use latest CLI.
    
    Changes:
    - No more `--reactNativePath`, define it once in the configuration file. This reverts the previous PR that added this flag
    - Add `platforms` and `commands` - React Native now defines platform like any other package. There's no longer concept of "out-of-tree" platform. All are treated equally. If React Native works, any other platform will work too.
    - Updates `jest/hasteImpl.js` to use public CLI interface (`loadConfig`) instead of `findPlugins` and removes a weird conditional that checks for CI presence.
    
    [INTERNAL] - Update React Native CLI
    Pull Request resolved: https://github.com/facebook/react-native/pull/24517
    
    Differential Revision: D15044762
    
    Pulled By: cpojer
    
    fbshipit-source-id: 379b61e842e619312c542173219a7d326663cf24

--- jest/hasteImpl.js
+++ jest/hasteImpl.js
@@ -11,44 +11,24 @@
 'use strict';
 
 const path = require('path');
+const cli = require('@react-native-community/cli');
 
-const REACT_NATIVE_CI = process.cwd() === path.resolve(__dirname, '..');
-
-let pluginsPath;
-
-if (REACT_NATIVE_CI) {
-  pluginsPath = '..';
-} else {
-  pluginsPath = '../../../';
-}
-
-function getPlugins() {
-  try {
-    const {findPlugins} = require('@react-native-community/cli');
-    return findPlugins(path.resolve(__dirname, pluginsPath));
-  } catch (e) {
-    return {
-      haste: {
-        providesModuleNodeModules: [],
-        platforms: [],
-      },
-    };
-  }
-}
-
-const plugins = getPlugins();
+// Use duck-typing because of Facebook-internal infra that doesn't have the cli package.
+const {haste} = (cli.loadConfig && cli.loadConfig()) || {
+  haste: {
+    providesModuleNodeModules: [],
+    platforms: [],
+  },
+};
 
 // Detect out-of-tree platforms and add them to the whitelists
-const pluginRoots /*: Array<string> */ = plugins.haste.providesModuleNodeModules.map(
+const pluginRoots /*: Array<string> */ = haste.providesModuleNodeModules.map(
   name => path.resolve(__dirname, '../../', name) + path.sep,
 );
 
-const pluginNameReducers /*: Array<
-  [RegExp, string],
-> */ = plugins.haste.platforms.map(name => [
-  new RegExp(`^(.*)\.(${name})$`),
-  '$1',
-]);
+const pluginNameReducers /*: Array<[RegExp, string]> */ = haste.platforms.map(
+  name => [new RegExp(`^(.*)\.(${name})$`), '$1'],
+);
 
 const ROOTS = [path.resolve(__dirname, '..') + path.sep, ...pluginRoots];
 
@@ -71,13 +51,30 @@ const NAME_REDUCERS /*: Array<[RegExp, string]> */ = [
   [/^(?:.*[\\\/])?([a-zA-Z0-9$_.-]+)$/, '$1'],
   // strip .js/.js.flow suffix
   [/^(.*)\.js(\.flow)?$/, '$1'],
-  // strip platform suffix
-  [/^(.*)\.(android|ios|native)$/, '$1'],
+  // strip native suffix
+  [/^(.*)\.(native)$/, '$1'],
   // strip plugin platform suffixes
   ...pluginNameReducers,
 ];
 
-const haste = {
+function isHastePath(filePath /*: string */) /*: boolean */ {
+  if (!filePath.endsWith('.js') && !filePath.endsWith('.js.flow')) {
+    return false;
+  }
+
+  const root = ROOTS.find(r => filePath.startsWith(r));
+  if (!root) {
+    return false;
+  }
+
+  filePath = filePath.substr(root.length);
+  if (BLACKLISTED_PATTERNS.some(pattern => pattern.test(filePath))) {
+    return false;
+  }
+  return WHITELISTED_PREFIXES.some(prefix => filePath.startsWith(prefix));
+}
+
+module.exports = {
   /*
    * @return {string|void} hasteName for module at filePath; or undefined if
    *                       filePath is not a haste module
@@ -98,22 +95,3 @@ const haste = {
     return hasteName;
   },
 };
-
-function isHastePath(filePath /*: string */) /*: boolean */ {
-  if (!filePath.endsWith('.js') && !filePath.endsWith('.js.flow')) {
-    return false;
-  }
-
-  const root = ROOTS.find(r => filePath.startsWith(r));
-  if (!root) {
-    return false;
-  }
-
-  filePath = filePath.substr(root.length);
-  if (BLACKLISTED_PATTERNS.some(pattern => pattern.test(filePath))) {
-    return false;
-  }
-  return WHITELISTED_PREFIXES.some(prefix => filePath.startsWith(prefix));
-}
-
-module.exports = haste;
