39776a82f0c5d9b84feef79cf87f8e8da243424f
Spencer Ahrens <sahrens@fb.com>
2019-04-24 17:01:48 -0700

    don't throttle below 16ms
    Summary: For some reason the scroll events are sometimes generated with highly irregular spacing, some coming less than a millisecond apart. For interactions that must track scrolling exactly, this can cause them to glitch. With a scroll throttle of less than 17 ms, the intention is clear that the UI should be updated in sync with the scroll view so we shouldn't drop any events.
    
    Reviewed By: PeteTheHeat
    
    Differential Revision: D15068841
    
    fbshipit-source-id: 730e7cb29cc3ddae66f37cf7392e02e0cc9d7844

--- Libraries/Animated/src/components/AnimatedScrollView.js
+++ Libraries/Animated/src/components/AnimatedScrollView.js
@@ -14,4 +14,6 @@ const ScrollView = require('ScrollView');
 
 const createAnimatedComponent = require('createAnimatedComponent');
 
-module.exports = createAnimatedComponent(ScrollView);
+module.exports = createAnimatedComponent(ScrollView, {
+  scrollEventThrottle: 0.0001,
+});
