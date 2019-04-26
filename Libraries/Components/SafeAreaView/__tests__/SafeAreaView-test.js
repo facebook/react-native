3164710b5cd1f63373db0ab44b5e2be51273454d
zhongwuzw <zhongwuzw@qq.com>
2019-04-26 15:29:49 +0800

    Add SafeAreaView subview for test

--- Libraries/Components/SafeAreaView/__tests__/SafeAreaView-test.js
+++ Libraries/Components/SafeAreaView/__tests__/SafeAreaView-test.js
@@ -14,33 +14,55 @@
 const React = require('React');
 const SafeAreaView = require('SafeAreaView');
 const render = require('../../../../jest/renderer');
+const View = require('View');
+const Text = require('Text');
 
 describe('<SafeAreaView />', () => {
-  it('should set displayName to prevent <Component /> regressions', () => {
-    expect(SafeAreaView.displayName).toBe('SafeAreaView');
-  });
-
   it('should render as <SafeAreaView> when mocked', () => {
-    const instance = render.create(<SafeAreaView />);
+    const instance = render.create(
+      <SafeAreaView>
+        <View>
+          <Text>Hello World!</Text>
+        </View>
+      </SafeAreaView>,
+    );
     expect(instance).toMatchSnapshot();
   });
 
   it('should shallow render as <ForwardRef(SafeAreaView)> when mocked', () => {
-    const output = render.shallow(<SafeAreaView />);
+    const output = render.shallow(
+      <SafeAreaView>
+        <View>
+          <Text>Hello World!</Text>
+        </View>
+      </SafeAreaView>,
+    );
     expect(output).toMatchSnapshot();
   });
 
   it('should shallow render as <ForwardRef(SafeAreaView)> when not mocked', () => {
     jest.dontMock('SafeAreaView');
 
-    const output = render.shallow(<SafeAreaView />);
+    const output = render.shallow(
+      <SafeAreaView>
+        <View>
+          <Text>Hello World!</Text>
+        </View>
+      </SafeAreaView>,
+    );
     expect(output).toMatchSnapshot();
   });
 
   it('should render as <SafeAreaView> when not mocked', () => {
     jest.dontMock('SafeAreaView');
 
-    const instance = render.create(<SafeAreaView />);
+    const instance = render.create(
+      <SafeAreaView>
+        <View>
+          <Text>Hello World!</Text>
+        </View>
+      </SafeAreaView>,
+    );
     expect(instance).toMatchSnapshot();
   });
 });
