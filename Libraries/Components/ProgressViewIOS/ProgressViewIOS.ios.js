88c18fa93baed3a5e6d9565b55c704a1a27fb926
Avik Chaudhuri <avik@fb.com>
2019-04-29 17:14:35 -0700

    add typeof to resolve `Component: Component` flow error
    Summary: A Flow error suppression was hiding two errors: one is a real type incompatibility, another is a "kind" mismatch error where the type of a component was being cast to another component (rather than its type).
    
    Reviewed By: nmote
    
    Differential Revision: D15141749
    
    fbshipit-source-id: a090c02c949eb6614b46f2691ad41cee0b0f3dbd

--- Libraries/Components/ProgressViewIOS/ProgressViewIOS.ios.js
+++ Libraries/Components/ProgressViewIOS/ProgressViewIOS.ios.js
@@ -78,4 +78,4 @@ const ProgressViewIOSWithRef = React.forwardRef(ProgressViewIOS);
 /* $FlowFixMe(>=0.89.0 site=react_native_ios_fb) This comment suppresses an
  * error found when Flow v0.89 was deployed. To see the error, delete this
  * comment and run Flow. */
-module.exports = (ProgressViewIOSWithRef: RCTProgressViewNativeComponent);
+module.exports = (ProgressViewIOSWithRef: typeof RCTProgressViewNativeComponent);
