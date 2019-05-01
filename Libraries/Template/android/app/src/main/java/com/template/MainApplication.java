29dc9081cdc163a642ad3ea09bffab2c74644c43
Glauber Castro <gfc714@gmail.com>
2019-05-01 11:25:17 -0300

    Unlink unused dependency

--- Libraries/Template/android/app/src/main/java/com/template/MainApplication.java
+++ Libraries/Template/android/app/src/main/java/com/template/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.airbnb.android.react.lottie.LottiePackage;
-import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new LottiePackage(),
-            new SvgPackage()
+            new LottiePackage()
       );
     }
 
