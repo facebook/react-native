afc03c206aed17a01f75e0a3466bb0bc5317168f
Glauber Castro <gfc714@gmail.com>
2019-05-01 10:51:25 -0300

    Add Lottie animation

--- Libraries/Template/android/app/src/main/java/com/template/MainApplication.java
+++ Libraries/Template/android/app/src/main/java/com/template/MainApplication.java
@@ -3,6 +3,7 @@ package com.template;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new SvgPackage()
       );
     }
