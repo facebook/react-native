f6328736c911d01cbdfaaa82d83eab2d81d04ebc
Karan Pratap Singh <synester.gates4@gmail.com>
2019-05-02 18:57:33 +0530

    removed lottie and updated logos

--- Libraries/Template/Components/Header.js
+++ Libraries/Template/Components/Header.js
@@ -1,4 +1,4 @@
-import React from './node_modules/react'
+import React from 'react'
 import { View, Text, StyleSheet, ImageBackground } from 'react-native'
 
 export default Header = () => (
--- Libraries/Template/android/app/src/main/java/com/template/MainApplication.java
+++ Libraries/Template/android/app/src/main/java/com/template/MainApplication.java
@@ -3,7 +3,6 @@ package com.template;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.lottie.LottiePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new LottiePackage()
+          new MainReactPackage()
       );
     }
 
