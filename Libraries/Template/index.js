befe3d16e627e12df82052870091a72571bbd08b
Karan Pratap Singh <synester.gates4@gmail.com>
2019-05-04 17:10:46 +0530

    removed template files

--- Libraries/Template/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native'
-import App from './App'
-import { name as appName } from './app.json'
-
-AppRegistry.registerComponent(appName, () => App)
