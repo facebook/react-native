8586ee69f62cf0f0bc229a1f456fa232e1df9e59
Karan Pratap Singh <synester.gates4@gmail.com>
2019-05-04 17:53:47 +0530

    moved to Libraties/NewAppScreen

--- Libraries/NewAppScreen/App.js
+++ Libraries/NewAppScreen/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 import {
   StyleSheet,
   ScrollView,
--- Libraries/Template/components/colors.js
+++
@@ -1,8 +0,0 @@
-export default {
-    primary: '#1292B4',
-    white: '#FFF',
-    lighter: '#F3F3F3',
-    light: '#DAE1E7',
-    dark: '#444',
-    black: '#000',
-};
