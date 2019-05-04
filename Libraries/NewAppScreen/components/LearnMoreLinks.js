70fd9ca2c20d318a661a9a701cf301bd7741cfd5
Karan Pratap Singh <synester.gates4@gmail.com>
2019-05-04 18:06:50 +0530

    added copyright headers

--- Libraries/NewAppScreen/components/LearnMoreLinks.js
+++ Libraries/NewAppScreen/components/LearnMoreLinks.js
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+'use strict';
+
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 import Colors from './Colors';
