e7ebb17452e3796324505b2b92ceae728ca3eb64
cpojer <christoph.pojer@gmail.com>
2019-05-08 19:32:41 -0700

    Fix RNTester on iOS (#24736)
    Summary:
    See e94b116d7675399af719ac58e36d3274a23feea4 which broke RNTester in open source. Environment variables are always strings, so "0" is a truthy value. This fixes it.
    
    [iOS] [fixed] - Fixed broken RNTester
    Pull Request resolved: https://github.com/facebook/react-native/pull/24736
    
    Reviewed By: fkgozali
    
    Differential Revision: D15272951
    
    Pulled By: hramos
    
    fbshipit-source-id: aa78f229e05cb553f75cdf6fb4f926829e20a557

--- RNTester/RNTesterIntegrationTests/RCTLoggingTests.m
+++ RNTester/RNTesterIntegrationTests/RCTLoggingTests.m
@@ -30,10 +30,7 @@
 {
   NSURL *scriptURL;
   if (getenv("CI_USE_PACKAGER")) {
-    NSString *bundlePrefix = @"";
-    if (getenv("CI_USE_BUNDLE_PREFIX")) {
-      bundlePrefix = @"react-native-github/";
-    }
+    NSString *bundlePrefix = [[[NSBundle mainBundle] infoDictionary] valueForKey:@"RN_BUNDLE_PREFIX"];
     NSString *app = @"IntegrationTests/IntegrationTestsApp";
     scriptURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://localhost:8081/%@%@.bundle?platform=ios&dev=true", bundlePrefix, app]];
   } else {
