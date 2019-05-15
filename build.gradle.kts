654868da336ab34ea851cada943492932009ec5d
Dulmandakh <dulmandakh+github@gmaill.com>
2019-05-15 03:25:38 -0700

    revert gradle download plugin import (#24863)
    Summary:
    Revert Gradle download plugin import, because new way is causing some issues when building from source.
    
    ## Changelog
    
    [Android] [Changed] - revert Gradle download plugin import
    Pull Request resolved: https://github.com/facebook/react-native/pull/24863
    
    Differential Revision: D15352002
    
    Pulled By: cpojer
    
    fbshipit-source-id: 5996ce8aeeca1fdd8b43fdc9087af705cf7f682d

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("de.undercouch:gradle-download-task:3.4.3")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
