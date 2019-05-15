4001ffb7ce3dbc167de3c61196a775feb091c9c5
Valentin Shergin <shergin@fb.com>
2019-05-15 10:26:45 -0700

    Fabric: Refinements in State management infra
    Summary: A couple of new methods in ConcreteShadowNode allows us to deal with State in more LocalData-like manner.
    
    Reviewed By: mdvacca
    
    Differential Revision: D15323686
    
    fbshipit-source-id: ede4aa1f1d0ad6f876bd963e57a00a0ad470c1c0

--- ReactCommon/fabric/core/shadownode/ConcreteShadowNode.h
+++ ReactCommon/fabric/core/shadownode/ConcreteShadowNode.h
@@ -84,8 +84,21 @@ class ConcreteShadowNode : public ShadowNode {
     return std::static_pointer_cast<const PropsT>(props_);
   }
 
-  const typename ConcreteState::Shared getState() const {
-    return std::static_pointer_cast<const ConcreteState>(state_);
+  /*
+   * Returns a concrete state data associated with the node.
+   * Thread-safe after the node is sealed.
+   */
+  ConcreteStateData const &getStateData() const {
+    return std::static_pointer_cast<const ConcreteState>(state_)->getData();
+  }
+
+  /*
+   * Creates and assigns a new state object containing given state data.
+   * Can be called only before the node is sealed (usually during construction).
+   */
+  void setStateData(ConcreteStateData &&data) {
+    ensureUnsealed();
+    state_ = std::make_shared<ConcreteState const>(std::move(data), *state_);
   }
 
   /*
