bf627bb6e2e830dddc7b1ee6c06ce6ab2c2350d1
Eric Lewis <ericlewis777@gmail.com>
2019-05-16 15:59:57 -0400

    Fix callsites

--- Libraries/Utilities/JSDevSupportModule.js
+++ Libraries/Utilities/JSDevSupportModule.js
@@ -15,22 +15,24 @@ const ReactNative = require('../Renderer/shims/ReactNative');
 
 const JSDevSupportModule = {
   getJSHierarchy: function(tag: number) {
-    const constants = NativeJSDevSupport.getConstants();
-    try {
-      const {
-        computeComponentStackForErrorReporting,
-      } = ReactNative.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
-      const componentStack = computeComponentStackForErrorReporting(tag);
-      if (!componentStack) {
-        NativeJSDevSupport.onFailure(
-          constants.ERROR_CODE_VIEW_NOT_FOUND,
-          "Component stack doesn't exist for tag " + tag,
-        );
-      } else {
-        NativeJSDevSupport.onSuccess(componentStack);
+    if (NativeJSDevSupport) {
+      const constants = NativeJSDevSupport.getConstants();
+      try {
+        const {
+          computeComponentStackForErrorReporting,
+        } = ReactNative.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+        const componentStack = computeComponentStackForErrorReporting(tag);
+        if (!componentStack) {
+          NativeJSDevSupport.onFailure(
+            constants.ERROR_CODE_VIEW_NOT_FOUND,
+            "Component stack doesn't exist for tag " + tag,
+          );
+        } else {
+          NativeJSDevSupport.onSuccess(componentStack);
+        }
+      } catch (e) {
+        NativeJSDevSupport.onFailure(constants.ERROR_CODE_EXCEPTION, e.message);
       }
-    } catch (e) {
-      NativeJSDevSupport.onFailure(constants.ERROR_CODE_EXCEPTION, e.message);
     }
   },
 };
