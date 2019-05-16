21030a5e34d24e12cc76a737ade60125d13734a4
Eric Lewis <ericlewis777@gmail.com>
2019-05-16 12:29:26 -0400

    [TM] add spec for JSDevSupport

--- Libraries/Utilities/JSDevSupportModule.js
+++ Libraries/Utilities/JSDevSupportModule.js
@@ -10,26 +10,27 @@
 
 'use strict';
 
-const JSDevSupport = require('../BatchedBridge/NativeModules').JSDevSupport;
+import NativeJSDevSupport from './NativeJSDevSupport';
 const ReactNative = require('../Renderer/shims/ReactNative');
 
 const JSDevSupportModule = {
   getJSHierarchy: function(tag: number) {
+    const constants = NativeJSDevSupport.getConstants();
     try {
       const {
         computeComponentStackForErrorReporting,
       } = ReactNative.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
       const componentStack = computeComponentStackForErrorReporting(tag);
       if (!componentStack) {
-        JSDevSupport.onFailure(
-          JSDevSupport.ERROR_CODE_VIEW_NOT_FOUND,
+        NativeJSDevSupport.onFailure(
+          constants.ERROR_CODE_VIEW_NOT_FOUND,
           "Component stack doesn't exist for tag " + tag,
         );
       } else {
-        JSDevSupport.onSuccess(componentStack);
+        NativeJSDevSupport.onSuccess(componentStack);
       }
     } catch (e) {
-      JSDevSupport.onFailure(JSDevSupport.ERROR_CODE_EXCEPTION, e.message);
+      NativeJSDevSupport.onFailure(constants.ERROR_CODE_EXCEPTION, e.message);
     }
   },
 };
