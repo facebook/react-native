f27a50762b572dca8f75a8a250c217c7b3ad7f13
Eric Lewis <ericlewis777@gmail.com>
2019-05-16 16:17:16 -0400

    [TM] Add spec for AnimationsDebugModule

---
+++ Libraries/NativeModules/specs/NativeAnimationsDebugModule.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+'use strict';
+
+import type {TurboModule} from 'RCTExport';
+import * as TurboModuleRegistry from 'TurboModuleRegistry';
+import {Platform} from 'react-native';
+
+export interface Spec extends TurboModule {
+  +startRecordingFps: () => void;
+  +stopRecordingFps: (animationStopTimeMs: number) => void;
+}
+
+export default (Platform.OS === 'android'
+  ? TurboModuleRegistry.getEnforcing<Spec>('AnimationsDebugModule')
+  : null);
