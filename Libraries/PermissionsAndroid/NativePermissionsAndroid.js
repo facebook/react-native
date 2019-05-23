bc1b5eb599bbf774d6988d8a957b9f285b190bd1
Krzysztof Borowy <krizzu.dev@gmail.com>
2019-05-23 21:12:31 +0200

    use inline string types

--- Libraries/PermissionsAndroid/NativePermissionsAndroid.js
+++ Libraries/PermissionsAndroid/NativePermissionsAndroid.js
@@ -13,41 +13,34 @@
 import type {TurboModule} from 'RCTExport';
 import * as TurboModuleRegistry from 'TurboModuleRegistry';
 
-export const PERMISSION_REQUEST_RESULT = Object.freeze({
-  GRANTED: 'granted',
-  DENIED: 'denied',
-  NEVER_ASK_AGAIN: 'never_ask_again',
-});
 
-export const PERMISSIONS = Object.freeze({
-  READ_CALENDAR: 'android.permission.READ_CALENDAR',
-  WRITE_CALENDAR: 'android.permission.WRITE_CALENDAR',
-  CAMERA: 'android.permission.CAMERA',
-  READ_CONTACTS: 'android.permission.READ_CONTACTS',
-  WRITE_CONTACTS: 'android.permission.WRITE_CONTACTS',
-  GET_ACCOUNTS: 'android.permission.GET_ACCOUNTS',
-  ACCESS_FINE_LOCATION: 'android.permission.ACCESS_FINE_LOCATION',
-  ACCESS_COARSE_LOCATION: 'android.permission.ACCESS_COARSE_LOCATION',
-  RECORD_AUDIO: 'android.permission.RECORD_AUDIO',
-  READ_PHONE_STATE: 'android.permission.READ_PHONE_STATE',
-  CALL_PHONE: 'android.permission.CALL_PHONE',
-  READ_CALL_LOG: 'android.permission.READ_CALL_LOG',
-  WRITE_CALL_LOG: 'android.permission.WRITE_CALL_LOG',
-  ADD_VOICEMAIL: 'com.android.voicemail.permission.ADD_VOICEMAIL',
-  USE_SIP: 'android.permission.USE_SIP',
-  PROCESS_OUTGOING_CALLS: 'android.permission.PROCESS_OUTGOING_CALLS',
-  BODY_SENSORS: 'android.permission.BODY_SENSORS',
-  SEND_SMS: 'android.permission.SEND_SMS',
-  RECEIVE_SMS: 'android.permission.RECEIVE_SMS',
-  READ_SMS: 'android.permission.READ_SMS',
-  RECEIVE_WAP_PUSH: 'android.permission.RECEIVE_WAP_PUSH',
-  RECEIVE_MMS: 'android.permission.RECEIVE_MMS',
-  READ_EXTERNAL_STORAGE: 'android.permission.READ_EXTERNAL_STORAGE',
-  WRITE_EXTERNAL_STORAGE: 'android.permission.WRITE_EXTERNAL_STORAGE',
-});
 
-export type PermissionStatus = $Values<typeof PERMISSION_REQUEST_RESULT>;
-export type PermissionType = $Values<typeof PERMISSIONS>;
+export type PermissionStatus = 'granted' | 'denied' | 'never_ask_again';
+export type PermissionType =
+  | 'android.permission.READ_CALENDAR'
+  | 'android.permission.WRITE_CALENDAR'
+  | 'android.permission.CAMERA'
+  | 'android.permission.READ_CONTACTS'
+  | 'android.permission.WRITE_CONTACTS'
+  | 'android.permission.GET_ACCOUNTS'
+  | 'android.permission.ACCESS_FINE_LOCATION'
+  | 'android.permission.ACCESS_COARSE_LOCATION'
+  | 'android.permission.RECORD_AUDIO'
+  | 'android.permission.READ_PHONE_STATE'
+  | 'android.permission.CALL_PHONE'
+  | 'android.permission.READ_CALL_LOG'
+  | 'android.permission.WRITE_CALL_LOG'
+  | 'com.android.voicemail.permission.ADD_VOICEMAIL'
+  | 'android.permission.USE_SIP'
+  | 'android.permission.PROCESS_OUTGOING_CALLS'
+  | 'android.permission.BODY_SENSORS'
+  | 'android.permission.SEND_SMS'
+  | 'android.permission.RECEIVE_SMS'
+  | 'android.permission.READ_SMS'
+  | 'android.permission.RECEIVE_WAP_PUSH'
+  | 'android.permission.RECEIVE_MMS'
+  | 'android.permission.READ_EXTERNAL_STORAGE'
+  | 'android.permission.WRITE_EXTERNAL_STORAGE';
 
 export interface Spec extends TurboModule {
   +checkPermission: (permission: PermissionType) => Promise<boolean>;
--- Libraries/PermissionsAndroid/PermissionsAndroid.js
+++ Libraries/PermissionsAndroid/PermissionsAndroid.js
@@ -12,10 +12,7 @@
 
 const NativeModules = require('../BatchedBridge/NativeModules');
 const Platform = require('../Utilities/Platform');
-import NativePermissionsAndroid, {
-  PERMISSION_REQUEST_RESULT,
-  PERMISSIONS,
-} from './NativePermissionsAndroid';
+import NativePermissionsAndroid from './NativePermissionsAndroid';
 
 import type {
   PermissionStatus,
@@ -29,6 +26,39 @@ export type Rationale = {
   buttonNegative?: string,
   buttonNeutral?: string,
 };
+
+const PERMISSION_REQUEST_RESULT = Object.freeze({
+  GRANTED: 'granted',
+  DENIED: 'denied',
+  NEVER_ASK_AGAIN: 'never_ask_again',
+});
+
+const PERMISSIONS = Object.freeze({
+  READ_CALENDAR: 'android.permission.READ_CALENDAR',
+  WRITE_CALENDAR: 'android.permission.WRITE_CALENDAR',
+  CAMERA: 'android.permission.CAMERA',
+  READ_CONTACTS: 'android.permission.READ_CONTACTS',
+  WRITE_CONTACTS: 'android.permission.WRITE_CONTACTS',
+  GET_ACCOUNTS: 'android.permission.GET_ACCOUNTS',
+  ACCESS_FINE_LOCATION: 'android.permission.ACCESS_FINE_LOCATION',
+  ACCESS_COARSE_LOCATION: 'android.permission.ACCESS_COARSE_LOCATION',
+  RECORD_AUDIO: 'android.permission.RECORD_AUDIO',
+  READ_PHONE_STATE: 'android.permission.READ_PHONE_STATE',
+  CALL_PHONE: 'android.permission.CALL_PHONE',
+  READ_CALL_LOG: 'android.permission.READ_CALL_LOG',
+  WRITE_CALL_LOG: 'android.permission.WRITE_CALL_LOG',
+  ADD_VOICEMAIL: 'com.android.voicemail.permission.ADD_VOICEMAIL',
+  USE_SIP: 'android.permission.USE_SIP',
+  PROCESS_OUTGOING_CALLS: 'android.permission.PROCESS_OUTGOING_CALLS',
+  BODY_SENSORS: 'android.permission.BODY_SENSORS',
+  SEND_SMS: 'android.permission.SEND_SMS',
+  RECEIVE_SMS: 'android.permission.RECEIVE_SMS',
+  READ_SMS: 'android.permission.READ_SMS',
+  RECEIVE_WAP_PUSH: 'android.permission.RECEIVE_WAP_PUSH',
+  RECEIVE_MMS: 'android.permission.RECEIVE_MMS',
+  READ_EXTERNAL_STORAGE: 'android.permission.READ_EXTERNAL_STORAGE',
+  WRITE_EXTERNAL_STORAGE: 'android.permission.WRITE_EXTERNAL_STORAGE',
+});
 
 /**
  * `PermissionsAndroid` provides access to Android M's new permissions model.
