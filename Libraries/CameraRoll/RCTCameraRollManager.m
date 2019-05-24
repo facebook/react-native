cf2a2661f91d587f6e03eede6c0a77d6205daa81
work_helper <liangguohun@users.noreply.github.com>
2019-05-25 04:13:49 +0800

    debug crash the ios
    when use saveToCameraRoll，we should check Permission of photo liberary, if haved even deneid we should tell the user

--- Libraries/CameraRoll/RCTCameraRollManager.m
+++ Libraries/CameraRoll/RCTCameraRollManager.m
@@ -89,171 +89,73 @@ RCT_EXPORT_METHOD(saveToCameraRoll:(NSURLRequest *)request
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
-  if ([type isEqualToString:@"video"]) {
-    // It's unclear if writeVideoAtPathToSavedPhotosAlbum is thread-safe
-    dispatch_async(dispatch_get_main_queue(), ^{
-      [self->_bridge.assetsLibrary writeVideoAtPathToSavedPhotosAlbum:request.URL completionBlock:^(NSURL *assetURL, NSError *saveError) {
-        if (saveError) {
-          reject(kErrorUnableToSave, nil, saveError);
-        } else {
-          resolve(assetURL.absoluteString);
+  [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+    if(status == PHAuthorizationStatusDenied){
+        UIViewController *presentingController = RCTPresentedViewController();
+        if (presentingController == nil) {
+          RCTLogError(@"Tried to display alert view but there is no application window");
+          return;
         }
-      }];
-    });
-  } else {
-    [_bridge.imageLoader loadImageWithURLRequest:request
-                                        callback:^(NSError *loadError, UIImage *loadedImage) {
-      if (loadError) {
-        reject(kErrorUnableToLoad, nil, loadError);
+        UIAlertController *alertController = [UIAlertController
+                                       alertControllerWithTitle:@"Alert"
+                                       message:@"You have refused to access the album before, please go to the phone privacy Settings"
+                                       preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *sureAction = [UIAlertAction
+                                     actionWithTitle:@"Sure"
+                                     style:UIAlertActionStyleDefault
+                                     handler:^(UIAlertAction *action){
+//          [presentingController dismissViewControllerAnimated:YES completion:nil];
+          [alertController.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+        }];
+        
+        [alertController addAction:sureAction];
+        
+        [presentingController presentViewController:alertController animated:YES completion:nil];
+        
+        reject(ERROR_PICKER_UNAUTHORIZED_KEY, ERROR_PICKER_UNAUTHORIZED_MSG, nil);
         return;
-      }
-      // It's unclear if writeImageToSavedPhotosAlbum is thread-safe
+    } else if (status != PHAuthorizationStatusAuthorized) {
+      reject(ERROR_PICKER_UNAUTHORIZED_KEY, ERROR_PICKER_UNAUTHORIZED_MSG, nil);
+      return;
+    }
+    
+    if ([type isEqualToString:@"video"]) {
+      // It's unclear if writeVideoAtPathToSavedPhotosAlbum is thread-safe
       dispatch_async(dispatch_get_main_queue(), ^{
-        [self->_bridge.assetsLibrary writeImageToSavedPhotosAlbum:loadedImage.CGImage metadata:nil completionBlock:^(NSURL *assetURL, NSError *saveError) {
+        [self->_bridge.assetsLibrary writeVideoAtPathToSavedPhotosAlbum:request.URL completionBlock:^(NSURL *assetURL, NSError *saveError) {
           if (saveError) {
-            RCTLogWarn(@"Error saving cropped image: %@", saveError);
             reject(kErrorUnableToSave, nil, saveError);
           } else {
             resolve(assetURL.absoluteString);
           }
         }];
       });
-    }];
-  }
-}
-
-static void RCTResolvePromise(RCTPromiseResolveBlock resolve,
-                              NSArray<NSDictionary<NSString *, id> *> *assets,
-                              BOOL hasNextPage)
-{
-  if (!assets.count) {
-    resolve(@{
-      @"edges": assets,
-      @"page_info": @{
-        @"has_next_page": @NO,
-      }
-    });
-    return;
-  }
-  resolve(@{
-    @"edges": assets,
-    @"page_info": @{
-      @"start_cursor": assets[0][@"node"][@"image"][@"uri"],
-      @"end_cursor": assets[assets.count - 1][@"node"][@"image"][@"uri"],
-      @"has_next_page": @(hasNextPage),
-    }
-  });
-}
-
-RCT_EXPORT_METHOD(getPhotos:(NSDictionary *)params
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
-  checkPhotoLibraryConfig();
-
-  NSUInteger first = [RCTConvert NSInteger:params[@"first"]];
-  NSString *afterCursor = [RCTConvert NSString:params[@"after"]];
-  NSString *groupName = [RCTConvert NSString:params[@"groupName"]];
-  ALAssetsFilter *assetType = [RCTConvert ALAssetsFilter:params[@"assetType"]];
-  ALAssetsGroupType groupTypes = [RCTConvert ALAssetsGroupType:params[@"groupTypes"]];
-
-  BOOL __block foundAfter = NO;
-  BOOL __block hasNextPage = NO;
-  BOOL __block resolvedPromise = NO;
-  NSMutableArray<NSDictionary<NSString *, id> *> *assets = [NSMutableArray new];
-
-  [_bridge.assetsLibrary enumerateGroupsWithTypes:groupTypes usingBlock:^(ALAssetsGroup *group, BOOL *stopGroups) {
-    if (group && (groupName == nil || [groupName isEqualToString:[group valueForProperty:ALAssetsGroupPropertyName]])) {
-
-      [group setAssetsFilter:assetType];
-      [group enumerateAssetsWithOptions:NSEnumerationReverse usingBlock:^(ALAsset *result, NSUInteger index, BOOL *stopAssets) {
-        if (result) {
-          NSString *uri = ((NSURL *)[result valueForProperty:ALAssetPropertyAssetURL]).absoluteString;
-          if (afterCursor && !foundAfter) {
-            if ([afterCursor isEqualToString:uri]) {
-              foundAfter = YES;
-            }
-            return; // Skip until we get to the first one
-          }
-          if (first == assets.count) {
-            *stopAssets = YES;
-            *stopGroups = YES;
-            hasNextPage = YES;
-            RCTAssert(resolvedPromise == NO, @"Resolved the promise before we finished processing the results.");
-            RCTResolvePromise(resolve, assets, hasNextPage);
-            resolvedPromise = YES;
+    } else {
+      [self->_bridge.imageLoader loadImageWithURLRequest:request
+        callback:^(NSError *loadError, UIImage *loadedImage) {
+          if (loadError) {
+            reject(kErrorUnableToLoad, nil, loadError);
             return;
           }
-          CGSize dimensions = [result defaultRepresentation].dimensions;
-          CLLocation *loc = [result valueForProperty:ALAssetPropertyLocation];
-          NSDate *date = [result valueForProperty:ALAssetPropertyDate];
-          NSString *filename = [result defaultRepresentation].filename;
-          int64_t duration = 0;
-          if ([[result valueForProperty:ALAssetPropertyType] isEqualToString:ALAssetTypeVideo]) {
-            duration = [[result valueForProperty:ALAssetPropertyDuration] intValue];
-          }
-
-          [assets addObject:@{
-            @"node": @{
-              @"type": [result valueForProperty:ALAssetPropertyType],
-              @"group_name": [group valueForProperty:ALAssetsGroupPropertyName],
-              @"image": @{
-                @"uri": uri,
-                @"filename" : filename,
-                @"height": @(dimensions.height),
-                @"width": @(dimensions.width),
-                @"isStored": @YES,
-                @"playableDuration": @(duration),
-              },
-              @"timestamp": @(date.timeIntervalSince1970),
-              @"location": loc ? @{
-                @"latitude": @(loc.coordinate.latitude),
-                @"longitude": @(loc.coordinate.longitude),
-                @"altitude": @(loc.altitude),
-                @"heading": @(loc.course),
-                @"speed": @(loc.speed),
-              } : @{},
-            }
-          }];
-        }
-      }];
+          // It's unclear if writeImageToSavedPhotosAlbum is thread-safe
+          dispatch_async(dispatch_get_main_queue(), ^{
+            [self->_bridge.assetsLibrary writeImageToSavedPhotosAlbum:loadedImage.CGImage metadata:nil completionBlock:^(NSURL *assetURL, NSError *saveError) {
+              if (saveError) {
+                RCTLogWarn(@"Error saving cropped image: %@", saveError);
+                reject(kErrorUnableToSave, nil, saveError);
+              } else {
+                resolve(assetURL.absoluteString);
+              }
+            }];
+          });
+        }];
+      
     }
-
-    if (!group) {
-      // Sometimes the enumeration continues even if we set stop above, so we guard against resolving the promise
-      // multiple times here.
-      if (!resolvedPromise) {
-        RCTResolvePromise(resolve, assets, hasNextPage);
-        resolvedPromise = YES;
-      }
-    }
-  } failureBlock:^(NSError *error) {
-    if (error.code != ALAssetsLibraryAccessUserDeniedError) {
-      RCTLogError(@"Failure while iterating through asset groups %@", error);
-    }
-    reject(kErrorUnableToLoad, nil, error);
+    
+    
   }];
-}
-
-RCT_EXPORT_METHOD(deletePhotos:(NSArray<NSString *>*)assets
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
-{
-  NSArray<NSURL *> *assets_ = [RCTConvert NSURLArray:assets];
-  [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-      PHFetchResult<PHAsset *> *fetched =
-        [PHAsset fetchAssetsWithALAssetURLs:assets_ options:nil];
-      [PHAssetChangeRequest deleteAssets:fetched];
-    }
-  completionHandler:^(BOOL success, NSError *error) {
-      if (success == YES) {
-     	    resolve(@(success));
-      }
-      else {
-	        reject(@"Couldn't delete", @"Couldn't delete assets", error);
-      }
-    }
-    ];
+  
 }
 
 static void checkPhotoLibraryConfig()
