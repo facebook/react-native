79ce9b594cc89eeab65e097019afcf19659fc39a
BarinderGrewal <barindergrewal0104@gmail.com>
2019-06-03 12:07:13 +0530

    param requires double quotes instead of single (#82)

--- .ado/publish.js
+++ .ado/publish.js
@@ -61,8 +61,8 @@ function doPublish() {
 
   if (!onlyTagSource) {
     // -------- Generating Android Artifacts with JavaDoc
-    // -Pparam='excludeLibs' - This argument will not package the DSOs in ReactAndroid aar
-    exec("gradlew -Pparam='excludeLibs' installArchives");
+    // -Pparam="excludeLibs" - This argument will not package the DSOs in ReactAndroid aar
+    exec("gradlew -Pparam=\"excludeLibs\" installArchives");
 
     // undo uncommenting javadoc setting
     exec("git checkout ReactAndroid/gradle.properties");
