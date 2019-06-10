069f41c08c530c07ee1d9e1e37248d62f7d2d22d
Petr Bela <bela.petr@gmail.com>
2019-06-10 03:14:26 -0600

    Error when entry file is missing
    If the entry file has been renamed (e.g., to `index.ts`), the script currently fails silently and prints out that the bundle file does not exist.
    
    This change will print the correct error message instead.

--- scripts/react-native-xcode.sh
+++ scripts/react-native-xcode.sh
@@ -68,8 +68,13 @@ if [[ "$ENTRY_FILE" ]]; then
   :
 elif [[ -s "index.ios.js" ]]; then
    ENTRY_FILE=${1:-index.ios.js}
- else
+else
    ENTRY_FILE=${1:-index.js}
+fi
+
+if [[ $DEV != true && ! -f "$ENTRY_FILE" ]]; then
+  echo "error: Entry file $ENTRY_FILE does not exist. If you use another file as your entry point, pass ENTRY_FILE=myindex.js" >&2
+  exit 2
 fi
 
 if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
