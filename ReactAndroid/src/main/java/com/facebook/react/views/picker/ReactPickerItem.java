3b2a64a976b80349741f80162f3a4efa2146383b
Kudo Chien <ckchien@gmail.com>
2019-06-17 13:34:06 +0800

    Fix Android Picker ArrayOutOfBoundsException during Picker.Item update from a long list

---
+++ ReactAndroid/src/main/java/com/facebook/react/views/picker/ReactPickerItem.java
@@ -0,0 +1,39 @@
+package com.facebook.react.views.picker;
+
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+/* package */
+class ReactPickerItem {
+  public final String label;
+  @Nullable
+  public final Integer color;
+
+  public ReactPickerItem(final ReadableMap jsMapData) {
+    label = jsMapData.getString("label");
+
+    if (jsMapData.hasKey("color") && !jsMapData.isNull("color")) {
+      color = jsMapData.getInt("color");
+    } else {
+      color = null;
+    }
+  }
+
+  @Nullable
+  public static List<ReactPickerItem> createFromJsArrayMap(final ReadableArray jsArrayMap) {
+    if (jsArrayMap == null) {
+      return null;
+    }
+
+    final List<ReactPickerItem> items = new ArrayList<>(jsArrayMap.size());
+    for (int i = 0; i < jsArrayMap.size(); ++i) {
+      items.add(new ReactPickerItem(jsArrayMap.getMap(i)));
+    }
+    return items;
+  }
+}
