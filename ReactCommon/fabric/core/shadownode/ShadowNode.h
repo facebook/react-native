543c5a016436c7c17a290ba48e91ed247611ab60
Valentin Shergin <shergin@fb.com>
2019-06-26 08:01:52 -0700

    Fabric: ShadowNode::getMostRecentState()
    Summary: In some cases, we cannot retrieve the "committed" state because no one state was mounted yet. The whole concept of "confirmed" or "legit at the moment" is kinda overstatement. The actual meaning of this is "the last vision of the state to which we advanced so far"; it does not have any relation to the actual "commit" phase or "mounting" process.
    
    Reviewed By: sammy-SC
    
    Differential Revision: D16002127
    
    fbshipit-source-id: 95465e632525f873ae67f6db320a89562b62ba29

--- ReactCommon/fabric/core/shadownode/ShadowNode.h
+++ ReactCommon/fabric/core/shadownode/ShadowNode.h
@@ -112,10 +112,13 @@ class ShadowNode : public virtual Sealable,
   const State::Shared &getState() const;
 
   /*
-   * Returns a momentary value of currently committed state associated with a
-   * family of nodes which this node belongs to.
+   * Returns a momentary value of the most recently created or committed state
+   * associated with a family of nodes which this node belongs to.
+   * Sequential calls might return different values.
+   * The method may return null pointer in case if the particular `ShadowNode`
+   * does not use `State`.
    */
-  State::Shared getCommitedState() const;
+  State::Shared getMostRecentState() const;
 
   /*
    * Returns a local data associated with the node.
