43259a4bcb747a95a7e35bc615d25c2e03deba54
radex <radexpl@gmail.com>
2019-06-28 16:38:43 +0200

    Don't rely on UIAppDelegate.window

--- React/Profiler/RCTProfile.m
+++ React/Profiler/RCTProfile.m
@@ -401,9 +401,9 @@ void RCTProfileUnhookModules(RCTBridge *bridge)
       };
       RCTProfileControlsWindow.hidden = YES;
       dispatch_async(dispatch_get_main_queue(), ^{
-        [[[[RCTSharedApplication() delegate] window] rootViewController] presentViewController:activityViewController
-                                                                                                 animated:YES
-                                                                                               completion:nil];
+        [RCTPresentedViewController(nil) presentViewController:activityViewController
+                                                      animated:YES
+                                                    completion:nil];
       });
 #endif
     });
