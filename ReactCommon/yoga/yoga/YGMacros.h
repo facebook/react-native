b0656b839481fb92088666c0025004b4e720bd15
Wojtek Czekalski <me@wczekalski.com>
2019-06-30 20:08:51 +0200

    Fix conversion errors between bitfields and enums in yoga

--- ReactCommon/yoga/yoga/YGMacros.h
+++ ReactCommon/yoga/yoga/YGMacros.h
@@ -24,10 +24,11 @@
 // Cannot use NSInteger as NSInteger has a different size than int (which is the
 // default type of a enum). Therefor when linking the Yoga C library into obj-c
 // the header is a missmatch for the Yoga ABI.
-#define YG_ENUM_BEGIN(name) NS_ENUM(int, name)
+// We're using unsigned int explicitly because sign of enums and bitfields is implementation specific.
+#define YG_ENUM_BEGIN(name) NS_ENUM(unsigned int, name)
 #define YG_ENUM_END(name)
 #else
-#define YG_ENUM_BEGIN(name) enum name
+#define YG_ENUM_BEGIN(name) enum name: unsigned int
 #define YG_ENUM_END(name) name
 #endif
 
