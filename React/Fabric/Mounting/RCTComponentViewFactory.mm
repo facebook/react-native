faf913056097e4c8cc9b2e35d75fe56573a4395e
Samuel Susla <samuelsusla@fb.com>
2019-07-08 03:48:26 -0700

    fabric: Migrate ModalHostView
    Summary: Implements `ModalHostView` in Fabric.
    
    Reviewed By: shergin
    
    Differential Revision: D16090949
    
    fbshipit-source-id: fe2c87063863e7f3c6c8dcc934a2cdb0b1aedaac

--- React/Fabric/Mounting/RCTComponentViewFactory.mm
+++ React/Fabric/Mounting/RCTComponentViewFactory.mm
@@ -23,6 +23,7 @@
 #import "RCTSwitchComponentView.h"
 #import "RCTUnimplementedNativeComponentView.h"
 #import "RCTViewComponentView.h"
+#import "RNModalHostViewComponentView.h"
 #import "RNPullToRefreshViewComponentView.h"
 
 using namespace facebook::react;
@@ -47,6 +48,7 @@ using namespace facebook::react;
   [componentViewFactory registerComponentViewClass:[RCTSliderComponentView class]];
   [componentViewFactory registerComponentViewClass:[RCTSwitchComponentView class]];
   [componentViewFactory registerComponentViewClass:[RCTUnimplementedNativeComponentView class]];
+  [componentViewFactory registerComponentViewClass:[RNModalHostViewComponentView class]];
 
   return componentViewFactory;
 }
