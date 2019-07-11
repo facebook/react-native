859deb97f6cc1e03d0f2a974b14a1afb9269a7ab
Michał Osadnik <osdnk@fb.com>
2019-07-11 12:36:42 -0700

    Add generating header from schem json
    Summary:
    Add basic Native Module header generator
    
    We still need to figure out how to deal with module names, so I currently made in workable by extracting `Sample` from `SampleNativeModule` and filing into proper places, but it's a temporary solution.
    
    Reviewed By: rickhanlonii
    
    Differential Revision: D16180561
    
    fbshipit-source-id: bb27592d565ae439707d781fb2650f2bdb140146

--- packages/react-native-codegen/src/generators/GenerateModuleH.js
+++ packages/react-native-codegen/src/generators/GenerateModuleH.js
@@ -10,9 +10,23 @@
 
 'use strict';
 
-import type {SchemaType} from '../CodegenSchema';
+import type {
+  SchemaType,
+  FunctionTypeAnnotationParamTypeAnnotation,
+  FunctionTypeAnnotationReturn,
+} from '../CodegenSchema';
 
 type FilesOutput = Map<string, string>;
+
+const moduleTemplate = `
+class JSI_EXPORT Native::_MODULE_NAME_::TurboCxxModuleSpecJSI : public TurboModule {
+protected:
+  Native::_MODULE_NAME_::TurboCxxModuleSpecJSI(std::shared_ptr<JSCallInvoker> jsInvoker);
+
+public:
+::_MODULE_PROPERTIES_::
+
+};`;
 
 const template = `
 /**
@@ -28,29 +42,108 @@ const template = `
 
 namespace facebook {
 namespace react {
-
-::_CLASS_DEFINITION_::
-protected:
-::_PROPERTIES_MAP_::
-
-public:
-::_MODULE_PROPERTIES_::
+::_MODULES_::
 
 } // namespace react
 } // namespace facebook
 `;
 
+function translatePrimitiveJSTypeToCpp(
+  type:
+    | FunctionTypeAnnotationParamTypeAnnotation
+    | FunctionTypeAnnotationReturn,
+  error: string,
+) {
+  switch (type.type) {
+    case 'VoidTypeAnnotation':
+      return 'void';
+    case 'StringTypeAnnotation':
+      return 'jsi::String';
+    case 'NumberTypeAnnotation':
+    case 'FloatTypeAnnotation':
+      return 'double';
+    case 'Int32TypeAnnotation':
+      return 'int';
+    case 'BooleanTypeAnnotation':
+      return 'bool';
+    case 'GenericObjectTypeAnnotation':
+    case 'ObjectTypeAnnotation':
+      return 'jsi::Object';
+    case 'ArrayTypeAnnotation':
+      return 'jsi::Array';
+    case 'FunctionTypeAnnotation':
+      return 'jsi::Function';
+    case 'GenericPromiseTypeAnnotation':
+      return 'jsi::Value';
+
+    default:
+      throw new Error(error);
+  }
+}
+const propertyTemplate =
+  'virtual ::_RETURN_VALUE_:: ::_PROPERTY_NAME_::(jsi::Runtime &rt::_ARGS_::) = 0;';
+
 module.exports = {
   generate(libraryName: string, schema: SchemaType): FilesOutput {
-    const fileName = `${libraryName}.cpp`;
+    const nativeModules = Object.keys(schema.modules)
+      .map(moduleName => {
+        const modules = schema.modules[moduleName].nativeModules;
+        if (modules == null) {
+          return null;
+        }
 
-    const moduleProperties = '';
+        return modules;
+      })
+      .filter(Boolean)
+      .reduce((acc, components) => Object.assign(acc, components), {});
 
-    const replacedTemplate = template
-      .replace(/::_MODULE_PROPERTIES_::/g, moduleProperties)
-      .replace('::_MODULE_NAME_::', libraryName)
-      .replace('::_CLASS_DEFINITION_::', '')
-      .replace('::_PROPERTIES_MAP_::', '');
+    const modules = Object.keys(nativeModules)
+      .map(name => {
+        const {properties} = nativeModules[name];
+        const traversedProperties = properties
+          .map(prop => {
+            const traversedArgs = prop.typeAnnotation.params
+              .map(param => {
+                const translatedParam = translatePrimitiveJSTypeToCpp(
+                  param.typeAnnotation,
+                  `Unspopported type for param "${param.name}" in ${
+                    prop.name
+                  }. Found: ${param.typeAnnotation.type}`,
+                );
+                const isObject = translatedParam.startsWith('jsi::');
+                return (
+                  (isObject
+                    ? 'const ' + translatedParam + ' &'
+                    : translatedParam + ' ') + param.name
+                );
+              })
+              .join(', ');
+            return propertyTemplate
+              .replace('::_PROPERTY_NAME_::', prop.name)
+              .replace(
+                '::_RETURN_VALUE_::',
+                translatePrimitiveJSTypeToCpp(
+                  prop.typeAnnotation.returnTypeAnnotation,
+                  `Unspopported return type for ${prop.name}. Found: ${
+                    prop.typeAnnotation.returnTypeAnnotation.type
+                  }`,
+                ),
+              )
+              .replace(
+                '::_ARGS_::',
+                traversedArgs === '' ? '' : ', ' + traversedArgs,
+              );
+          })
+          .join('\n');
+        return moduleTemplate
+          .replace(/::_MODULE_PROPERTIES_::/g, traversedProperties)
+          .replace(/::_MODULE_NAME_::/g, name.slice(0, -11)) // FIXME
+          .replace('::_PROPERTIES_MAP_::', '');
+      })
+      .join('\n');
+
+    const fileName = 'NativeModules.h';
+    const replacedTemplate = template.replace(/::_MODULES_::/g, modules);
 
     return new Map([[fileName, replacedTemplate]]);
   },
