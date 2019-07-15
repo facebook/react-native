77ad650faaf4a497a893c99b696d418107855a43
Dulmandakh <dulmandakh@gmail.com>
2019-07-15 10:22:47 -0700

    bump gradle-download-task to 4.0.0 (#25654)
    Summary:
    bump gradle-download-task to 4.0.0, works best and tests with Gradle 5.x.
    
    ## Changelog
    
    [Android] [Changed] - bump gradle-download-task to 4.0.0
    Pull Request resolved: https://github.com/facebook/react-native/pull/25654
    
    Test Plan: RNTester builds and runs as expected
    
    Differential Revision: D16263755
    
    Pulled By: mdvacca
    
    fbshipit-source-id: 39f64d5ff554f99d932e0f3696a1ae5fd4528055

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.2")
-        classpath("de.undercouch:gradle-download-task:3.4.3")
+        classpath("de.undercouch:gradle-download-task:4.0.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
