a8c1696c45b8ee1a2f4b020c2c87c7618498363e
Michał Osadnik <osdnk@fb.com>
2019-07-17 01:28:35 -0700

    Follow up e2e testing mechanism with real tests
    Summary: This diff's adding real tests for existing e2e codegen tests. It verifies many cases for codegen, which should generate compilable cpp code.
    
    Differential Revision: D16221277
    
    fbshipit-source-id: ec96ba27c12988e8ff1fa8bc654df54a00f2df6d

--- packages/react-native-codegen/buck_tests/emptyFile.cpp
+++ packages/react-native-codegen/buck_tests/emptyFile.cpp
@@ -1,4 +1,6 @@
 #import <react/components/codegen_tests/ComponentDescriptors.h>
+#import <react/modules/codegen_tests/NativeModules.h>
+#import <react/modules/codegen_tests/NativeModules.cpp>
 
 // TODO: Import every prop and event to asset they're generated
 
