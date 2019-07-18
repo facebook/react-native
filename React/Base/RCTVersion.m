eec4dc6b7287c5a3536ca18b553e3153a0aba103
cpojer <christoph.pojer@gmail.com>
2019-07-18 13:52:59 +0100

    [0.60.4] Bump version numbers

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -22,7 +22,7 @@ static void __makeVersion()
   __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(60),
-                  RCTVersionPatch: @(3),
+                  RCTVersionPatch: @(4),
                   RCTVersionPrerelease: [NSNull null],
                   };
 }
