5029195d224c8342caa98787c3eb780c6266b2b1
Moti Zilberman <moti@fb.com>
2019-07-18 07:44:00 -0700

    Test ReactFiberErrorDialog in terms of NativeExceptionsManager
    Summary:
    https://github.com/facebook/react-native/pull/25671 added a shallow unit test for `ReactFiberErrorDialog`, mocking out the (JS) `ExceptionsManager` module. This rewrites that test in terms of `NativeExceptionsManager` instead, so the integration with `ExceptionsManager` is also tested.
    
    Also adds tests for the behaviour of the `framesToPop` and `jsEngine` extended error fields, and for passing a frozen error object (seeing as we potentially mutate the error).
    
    Reviewed By: rickhanlonii
    
    Differential Revision: D16330341
    
    fbshipit-source-id: 0b514d1c8f193a114748739ec31ddb4e06e4d2fd

--- Libraries/Core/__tests__/ReactFiberErrorDialog-test.js
+++ Libraries/Core/__tests__/ReactFiberErrorDialog-test.js
@@ -9,6 +9,8 @@
  */
 'use strict';
 
+const fs = require('fs');
+
 const capturedErrorDefaults = {
   componentName: 'A',
   componentStack: '\n  in A\n  in B\n  in C',
@@ -19,91 +21,184 @@ const capturedErrorDefaults = {
 };
 
 describe('ReactFiberErrorDialog', () => {
-  let ReactFiberErrorDialog, ExceptionsManager;
+  let ReactFiberErrorDialog, NativeExceptionsManager, nativeReportException;
   beforeEach(() => {
     jest.resetModules();
-    jest.mock('../ExceptionsManager', () => {
+    jest.mock('../NativeExceptionsManager', () => {
       return {
-        handleException: jest.fn(),
+        default: {
+          reportException: jest.fn(),
+          // Used to show symbolicated messages, not part of this test.
+          updateExceptionMessage: () => {},
+        },
       };
     });
+    // Make symbolication a no-op.
+    jest.mock('../Devtools/symbolicateStackTrace', () => {
+      return async function symbolicateStackTrace(stack) {
+        return stack;
+      };
+    });
+    jest.spyOn(console, 'error').mockImplementation(() => {});
     ReactFiberErrorDialog = require('../ReactFiberErrorDialog');
-    ExceptionsManager = require('../ExceptionsManager');
+    NativeExceptionsManager = require('../NativeExceptionsManager').default;
+    nativeReportException = NativeExceptionsManager.reportException;
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   describe('showErrorDialog', () => {
-    test('forwards error instance to handleException', () => {
+    test('forwards error instance to reportException', () => {
       const error = new ReferenceError('Some error happened');
-      error.someCustomProp = 42;
       // Copy all the data we care about before any possible mutation.
-      const {name, stack, message, someCustomProp} = error;
+      const {message} = error;
 
-      const logToConsole = ReactFiberErrorDialog.showErrorDialog({
+      const logToConsoleInReact = ReactFiberErrorDialog.showErrorDialog({
         ...capturedErrorDefaults,
         error,
       });
 
-      expect(ExceptionsManager.handleException.mock.calls.length).toBe(1);
-      const errorArg = ExceptionsManager.handleException.mock.calls[0][0];
-      const isFatalArg = ExceptionsManager.handleException.mock.calls[0][1];
-      // We intentionally don't test whether errorArg === error, because this
-      // implementation detail might change. Instead, we test that they are
-      // functionally equivalent.
-      expect(errorArg).toBeInstanceOf(ReferenceError);
-      expect(errorArg).toHaveProperty('name', name);
-      expect(errorArg).toHaveProperty('stack', stack);
-      expect(errorArg).toHaveProperty('someCustomProp', someCustomProp);
-      expect(errorArg).toHaveProperty(
-        'message',
+      expect(nativeReportException.mock.calls.length).toBe(1);
+      const exceptionData = nativeReportException.mock.calls[0][0];
+      const formattedMessage =
         'ReferenceError: ' +
+        message +
+        '\n\n' +
+        'This error is located at:' +
+        capturedErrorDefaults.componentStack;
+      expect(exceptionData.message).toBe(formattedMessage);
+      expect(getLineFromFrame(exceptionData.stack[0])).toBe(
+        "const error = new ReferenceError('Some error happened');",
+      );
+      expect(exceptionData.isFatal).toBe(false);
+      expect(logToConsoleInReact).toBe(false);
+      expect(console.error).toBeCalledWith(formattedMessage);
+    });
+
+    test('pops frames off the stack with framesToPop', () => {
+      function createError() {
+        const error = new Error('Some error happened');
+        error.framesToPop = 1;
+        return error;
+      }
+      const error = createError();
+
+      ReactFiberErrorDialog.showErrorDialog({
+        ...capturedErrorDefaults,
+        error,
+      });
+
+      expect(nativeReportException.mock.calls.length).toBe(1);
+      const exceptionData = nativeReportException.mock.calls[0][0];
+      expect(getLineFromFrame(exceptionData.stack[0])).toBe(
+        'const error = createError();',
+      );
+    });
+
+    test('adds the JS engine to the message', () => {
+      const error = new Error('Some error happened');
+      error.jsEngine = 'hermes';
+      // Copy all the data we care about before any possible mutation.
+      const {message, jsEngine} = error;
+
+      ReactFiberErrorDialog.showErrorDialog({
+        ...capturedErrorDefaults,
+        error,
+      });
+
+      expect(nativeReportException.mock.calls.length).toBe(1);
+      const exceptionData = nativeReportException.mock.calls[0][0];
+      expect(exceptionData.message).toBe(
+        'Error: ' +
+          message +
+          '\n\n' +
+          'This error is located at:' +
+          capturedErrorDefaults.componentStack +
+          ', js engine: ' +
+          jsEngine,
+      );
+      expect(console.error).toBeCalledWith(
+        'Error: ' +
           message +
           '\n\n' +
           'This error is located at:' +
           capturedErrorDefaults.componentStack,
+        // JS engine omitted here!
       );
-      expect(isFatalArg).toBe(false);
-      expect(logToConsole).toBe(false);
     });
 
     test('wraps string in an Error and sends to handleException', () => {
       const message = 'Some error happened';
 
-      const logToConsole = ReactFiberErrorDialog.showErrorDialog({
+      const logToConsoleInReact = ReactFiberErrorDialog.showErrorDialog({
         ...capturedErrorDefaults,
         error: message,
       });
 
-      expect(ExceptionsManager.handleException.mock.calls.length).toBe(1);
-      const errorArg = ExceptionsManager.handleException.mock.calls[0][0];
-      const isFatalArg = ExceptionsManager.handleException.mock.calls[0][1];
-      expect(errorArg).toBeInstanceOf(Error);
-      expect(errorArg).toHaveProperty(
-        'message',
+      expect(nativeReportException.mock.calls.length).toBe(1);
+      const exceptionData = nativeReportException.mock.calls[0][0];
+      const formattedMessage =
         message +
-          '\n\n' +
-          'This error is located at:' +
-          capturedErrorDefaults.componentStack,
-      );
-      expect(isFatalArg).toBe(false);
-      expect(logToConsole).toBe(false);
+        '\n\n' +
+        'This error is located at:' +
+        capturedErrorDefaults.componentStack;
+      expect(exceptionData.message).toBe(formattedMessage);
+      expect(exceptionData.stack[0].file).toMatch(/ReactFiberErrorDialog\.js$/);
+      expect(exceptionData.isFatal).toBe(false);
+      expect(logToConsoleInReact).toBe(false);
+      expect(console.error).toBeCalledWith(formattedMessage);
     });
 
     test('reports "Unspecified error" if error is null', () => {
-      const logToConsole = ReactFiberErrorDialog.showErrorDialog({
+      const logToConsoleInReact = ReactFiberErrorDialog.showErrorDialog({
         ...capturedErrorDefaults,
         error: null,
       });
 
-      expect(ExceptionsManager.handleException.mock.calls.length).toBe(1);
-      const errorArg = ExceptionsManager.handleException.mock.calls[0][0];
-      const isFatalArg = ExceptionsManager.handleException.mock.calls[0][1];
-      expect(errorArg).toBeInstanceOf(Error);
-      expect(errorArg).toHaveProperty(
-        'message',
-        'Unspecified error at:' + capturedErrorDefaults.componentStack,
+      expect(nativeReportException.mock.calls.length).toBe(1);
+      const exceptionData = nativeReportException.mock.calls[0][0];
+      const formattedMessage =
+        'Unspecified error at:' + capturedErrorDefaults.componentStack;
+      expect(exceptionData.message).toBe(formattedMessage);
+      expect(exceptionData.stack[0].file).toMatch(/ReactFiberErrorDialog\.js$/);
+      expect(exceptionData.isFatal).toBe(false);
+      expect(logToConsoleInReact).toBe(false);
+      expect(console.error).toBeCalledWith(formattedMessage);
+    });
+
+    test('works with a frozen error object', () => {
+      const error = Object.freeze(new Error('Some error happened'));
+
+      ReactFiberErrorDialog.showErrorDialog({
+        ...capturedErrorDefaults,
+        error,
+      });
+
+      expect(nativeReportException.mock.calls.length).toBe(1);
+      const exceptionData = nativeReportException.mock.calls[0][0];
+      expect(getLineFromFrame(exceptionData.stack[0])).toBe(
+        "const error = Object.freeze(new Error('Some error happened'));",
       );
-      expect(isFatalArg).toBe(false);
-      expect(logToConsole).toBe(false);
     });
   });
 });
+
+const linesByFile = new Map();
+
+function getLineFromFrame({lineNumber /* 1-based */, file}) {
+  const cleanedFile = cleanFileName(file);
+  const lines =
+    linesByFile.get(cleanedFile) ||
+    fs.readFileSync(cleanedFile, 'utf8').split('\n');
+  if (!linesByFile.has(cleanedFile)) {
+    linesByFile.set(cleanedFile, lines);
+  }
+  return (lines[lineNumber - 1] || '').trim();
+}
+
+// Works around a parseErrorStack bug involving `new X` stack frames.
+function cleanFileName(file) {
+  return file.replace(/^.+? \((?=\/)/, '');
+}
