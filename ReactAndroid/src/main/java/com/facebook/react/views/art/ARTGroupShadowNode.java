6625356b7e4e2aac8bee73ff19f0306cb17e8c27
Stepan Furdei <furdei@fb.com>
2019-07-23 14:01:50 -0700

    Migrate ARTGroupShadowNode.java to Android target API 28
    Summary: React Native is using API deprecated in Android API 28. This blocks migrating RN apps to target API 28 which is Google's requirement starting August 2019. Specifically, using the deprecated flags is causing a crash. Here's Google's suggestion for a fix https://developer.android.com/reference/android/graphics/Canvas.html#clipRect(float,%2520float,%2520float,%2520float,%2520android.graphics.Region.Op)
    
    Differential Revision: D16436758
    
    fbshipit-source-id: 92042695f8a1798464b374bff9fa0e5e83e7f461

--- ReactAndroid/src/main/java/com/facebook/react/views/art/ARTGroupShadowNode.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/art/ARTGroupShadowNode.java
@@ -9,7 +9,6 @@ package com.facebook.react.views.art;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.Region;
 import androidx.annotation.Nullable;
 import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
 import com.facebook.react.bridge.ReadableArray;
@@ -46,8 +45,7 @@ public class ARTGroupShadowNode extends ARTVirtualNode {
             mClipping.left * mScale,
             mClipping.top * mScale,
             mClipping.right * mScale,
-            mClipping.bottom * mScale,
-            Region.Op.REPLACE);
+            mClipping.bottom * mScale);
       }
 
       for (int i = 0; i < getChildCount(); i++) {
