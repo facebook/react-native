2ef676f546f0467f94e6a3057f19cfe0ef5dd0e1
Petr Bela <bela.petr@gmail.com>
2019-07-23 15:00:38 +0200

    Fix indentation

--- scripts/react-native-xcode.sh
+++ scripts/react-native-xcode.sh
@@ -67,9 +67,9 @@ if [[ "$ENTRY_FILE" ]]; then
   # Use ENTRY_FILE defined by user
   :
 elif [[ -s "index.ios.js" ]]; then
-   ENTRY_FILE=${1:-index.ios.js}
+  ENTRY_FILE=${1:-index.ios.js}
 else
-   ENTRY_FILE=${1:-index.js}
+  ENTRY_FILE=${1:-index.js}
 fi
 
 if [[ $DEV != true && ! -f "$ENTRY_FILE" ]]; then
