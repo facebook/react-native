d414abf04a7489bccd742560e032e2c610201c43
Onti Vals <44214857+ontzic@users.noreply.github.com>
2019-08-12 11:22:06 -0700

    Changing the signature of this method to match its counterpart on Fac… (#133)
    * Changing the signature of this method to match its counterpart on Facebook's side.
    
    * Adding a return value.

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
@@ -426,11 +426,12 @@ public class ReactEditText extends EditText {
     return getContext().getResources().getConfiguration().keyboard != Configuration.KEYBOARD_NOKEYS;
   }
 
-  protected void showSoftKeyboard() {
+  protected boolean showSoftKeyboard() {
     // Don't show soft keyboard when a hardware keyboard is attached
     if (!isHardwareKeyboardAvailable()) {
-      mInputMethodManager.showSoftInput(this, 0);
+      return mInputMethodManager.showSoftInput(this, 0);
     }
+    return false;
   }
 
   protected void hideSoftKeyboard() {
