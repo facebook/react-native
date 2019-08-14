9dbe5e241e3137196102fb808c181c57554fedfe
David Vacca <dvacca@fb.com>
2019-08-14 12:34:43 -0700

    Use SoundManager in Pressability and Touchable
    Summary: This diff replaces the usage of UIManagerModule.playTouchSound() in Pressability and Touchable for the SoundManager.playTouchSound()
    
    Reviewed By: yungsters
    
    Differential Revision: D16543433
    
    fbshipit-source-id: a2ba060bc480889c1e08c5c87086361e06974684

--- Libraries/Components/Touchable/Touchable.js
+++ Libraries/Components/Touchable/Touchable.js
@@ -19,6 +19,7 @@ const StyleSheet = require('../../StyleSheet/StyleSheet');
 const TVEventHandler = require('../AppleTV/TVEventHandler');
 const UIManager = require('../../ReactNative/UIManager');
 const View = require('../View/View');
+const SoundManager = require('../Sound/SoundManager');
 
 const keyMirror = require('fbjs/lib/keyMirror');
 const normalizeColor = require('../../Color/normalizeColor');
@@ -875,7 +876,7 @@ const TouchableMixin = {
           this._endHighlight(e);
         }
         if (Platform.OS === 'android' && !this.props.touchSoundDisabled) {
-          this._playTouchSound();
+          SoundManager.playTouchSound();
         }
         this.touchableHandlePress(e);
       }
@@ -883,10 +884,6 @@ const TouchableMixin = {
 
     this.touchableDelayTimeout && clearTimeout(this.touchableDelayTimeout);
     this.touchableDelayTimeout = null;
-  },
-
-  _playTouchSound: function() {
-    UIManager.playTouchSound();
   },
 
   _startHighlight: function(e: PressEvent) {
