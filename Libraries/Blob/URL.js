ea6e00815edc9fb486dd78843e2e01bfbf14cef7
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2019-08-13 21:06:57 -0400

    Implement whatwg-url-without-unicode as URL

--- Libraries/Blob/URL.js
+++ Libraries/Blob/URL.js
@@ -9,6 +9,8 @@
 
 'use strict';
 
+const {URL: whatwgUrl} = require('whatwg-url-without-unicode');
+
 const Blob = require('./Blob');
 
 import NativeBlobModule from './NativeBlobModule';
@@ -105,115 +107,17 @@ export class URLSearchParams {
   }
 }
 
-function validateBaseUrl(url: string) {
-  // from this MIT-licensed gist: https://gist.github.com/dperini/729294
-  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
-    url,
-  );
-}
-
-export class URL {
-  _searchParamsInstance = null;
-
-  static createObjectURL(blob: Blob) {
-    if (BLOB_URL_PREFIX === null) {
-      throw new Error('Cannot create URL for blob!');
-    }
-    return `${BLOB_URL_PREFIX}${blob.data.blobId}?offset=${
-      blob.data.offset
-    }&size=${blob.size}`;
+whatwgUrl.createObjectURL = function createObjectURL(blob: Blob) {
+  if (BLOB_URL_PREFIX === null) {
+    throw new Error('Cannot create URL for blob!');
   }
+  return `${BLOB_URL_PREFIX}${blob.data.blobId}?offset=${
+    blob.data.offset
+  }&size=${blob.size}`;
+};
 
-  static revokeObjectURL(url: string) {
-    // Do nothing.
-  }
+whatwgUrl.revokeObjectURL = function revokeObjectURL(url: string) {
+  // Do nothing.
+};
 
-  constructor(url: string, base: string) {
-    let baseUrl = null;
-    if (base) {
-      if (typeof base === 'string') {
-        baseUrl = base;
-        if (!validateBaseUrl(baseUrl)) {
-          throw new TypeError(`Invalid base URL: ${baseUrl}`);
-        }
-      } else if (typeof base === 'object') {
-        baseUrl = base.toString();
-      }
-      if (baseUrl.endsWith('/') && url.startsWith('/')) {
-        baseUrl = baseUrl.slice(0, baseUrl.length - 1);
-      }
-      if (baseUrl.endsWith(url)) {
-        url = '';
-      }
-      this._url = `${baseUrl}${url}`;
-    } else {
-      this._url = url;
-      if (!this._url.endsWith('/')) {
-        this._url += '/';
-      }
-    }
-  }
-
-  get hash() {
-    throw new Error('not implemented');
-  }
-
-  get host() {
-    throw new Error('not implemented');
-  }
-
-  get hostname() {
-    throw new Error('not implemented');
-  }
-
-  get href(): string {
-    return this.toString();
-  }
-
-  get origin() {
-    throw new Error('not implemented');
-  }
-
-  get password() {
-    throw new Error('not implemented');
-  }
-
-  get pathname() {
-    throw new Error('not implemented');
-  }
-
-  get port() {
-    throw new Error('not implemented');
-  }
-
-  get protocol() {
-    throw new Error('not implemented');
-  }
-
-  get search() {
-    throw new Error('not implemented');
-  }
-
-  get searchParams(): URLSearchParams {
-    if (this._searchParamsInstance == null) {
-      this._searchParamsInstance = new URLSearchParams();
-    }
-    return this._searchParamsInstance;
-  }
-
-  toJSON(): string {
-    return this.toString();
-  }
-
-  toString(): string {
-    if (this._searchParamsInstance === null) {
-      return this._url;
-    }
-    const separator = this._url.indexOf('?') > -1 ? '&' : '?';
-    return this._url + separator + this._searchParamsInstance.toString();
-  }
-
-  get username() {
-    throw new Error('not implemented');
-  }
-}
+export const URL = whatwgUrl;
