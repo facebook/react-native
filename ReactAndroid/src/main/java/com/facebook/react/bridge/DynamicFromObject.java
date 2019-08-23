a82532e5a48223835d3dd896b82b67bd8ec6d9fe
Rohit Jain (MOD) <rojain@microsoft.com>
2019-08-23 15:31:02 +0530

    Making changes to let Processor compile properly

--- ReactAndroid/src/main/java/com/facebook/react/bridge/DynamicFromObject.java
+++ ReactAndroid/src/main/java/com/facebook/react/bridge/DynamicFromObject.java
@@ -7,7 +7,8 @@
 
 package com.facebook.react.bridge;
 
-import com.facebook.common.logging.FLog;
+// Using Flog causes issue with Processor hence commenting its usage.
+// import com.facebook.common.logging.FLog;
 import com.facebook.react.common.ReactConstants;
 import javax.annotation.Nullable;
 
@@ -82,7 +83,7 @@ public class DynamicFromObject implements Dynamic {
     if (mObject instanceof ReadableArray) {
       return ReadableType.Array;
     }
-    FLog.e(ReactConstants.TAG, "Unmapped object type " + mObject.getClass().getName());
+    // FLog.e(ReactConstants.TAG, "Unmapped object type " + mObject.getClass().getName());
     return ReadableType.Null;
   }
 }
