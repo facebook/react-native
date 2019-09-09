681c71b8a508bed49339bdda541d2670f9fce23f
Petter Hesselberg <petterh@microsoft.com>
2019-09-09 08:14:04 +0200

    Revert RNTesterActivity changes

--- RNTester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterActivity.java
+++ RNTester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterActivity.java
@@ -6,13 +6,8 @@
  */
 package com.facebook.react.uiapp;
 
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import androidx.annotation.Nullable;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -21,7 +16,6 @@ import com.facebook.react.ReactInstanceManager;
 public class RNTesterActivity extends ReactActivity {
   public static class RNTesterActivityDelegate extends ReactActivityDelegate {
     private static final String PARAM_ROUTE = "route";
-    private static final String PARAM_BUNDLE_TEST = "bundle-test";
     private Bundle mInitialProps = null;
     private final @Nullable ReactActivity mActivity;
 
@@ -33,45 +27,16 @@ public class RNTesterActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
       // Get remote param before calling super which uses it
-      Intent intent = mActivity.getIntent();
-      if (Intent.ACTION_VIEW.equals(intent.getAction())) {
-        Uri data = intent.getData();
-        if (data != null) {
-          intent.setData(null); // Reset so that we only take action once.
-          mInitialProps = new Bundle();
-          String host = data.getHost();
-          if ("example".equals(host)) {
-            String routeUri =
-              "rntester://example/" +
-                data.getQueryParameter(PARAM_ROUTE) +
-                "Example";
-            mInitialProps.putString("exampleFromAppetizeParams", routeUri);
-          } else if ("bundle-test".equals(host)) {
-            Bundle[] children = new Bundle[2];
-            children[0] = new Bundle();
-            children[1] = new Bundle();
-            children[0].putString("exampleChildKey", "exampleChild1");
-            children[1].putString("exampleChildKey", "exampleChild2");
-            mInitialProps.putSerializable("bundle", children);
-            Parcel parcel = null;
-            byte[] bytes = null;
-
-            try {
-              parcel = Parcel.obtain();
-              mInitialProps.writeToParcel(parcel, 0);
-              bytes = parcel.marshall();
-              parcel.unmarshall(bytes, 0, bytes.length);
-              parcel.setDataPosition(0);
-              mInitialProps = Bundle.CREATOR.createFromParcel(parcel);
-            } finally {
-              if (parcel != null) {
-                parcel.recycle();
-              }
-            }
-          }
-        }
+      Bundle bundle = mActivity.getIntent().getExtras();
+      if (bundle != null && bundle.containsKey(PARAM_ROUTE)) {
+        String routeUri =
+            new StringBuilder("rntester://example/")
+                .append(bundle.getString(PARAM_ROUTE))
+                .append("Example")
+                .toString();
+        mInitialProps = new Bundle();
+        mInitialProps.putString("exampleFromAppetizeParams", routeUri);
       }
-
       super.onCreate(savedInstanceState);
     }
 
