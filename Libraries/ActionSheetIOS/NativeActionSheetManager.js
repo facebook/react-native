cb32e191730d231b5286b98da886f940472de938
Arjan Zuidema <arjan@myalbum.com>
2019-09-11 11:32:50 +0200

    Added missing userInterfaceStyle property

--- Libraries/ActionSheetIOS/NativeActionSheetManager.js
+++ Libraries/ActionSheetIOS/NativeActionSheetManager.js
@@ -25,6 +25,7 @@ export interface Spec extends TurboModule {
       +cancelButtonIndex?: ?number,
       +anchor?: ?number,
       +tintColor?: ?number,
+      userInterfaceStyle?: ?string,
     |},
     callback: (buttonIndex: number) => void,
   ) => void;
@@ -36,6 +37,7 @@ export interface Spec extends TurboModule {
       +anchor?: ?number,
       +tintColor?: ?number,
       +excludedActivityTypes?: ?Array<string>,
+      userInterfaceStyle?: ?string,
     |},
     failureCallback: (error: {|
       +domain: string,
