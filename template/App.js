fd0de07c5d8870ee7be758ebac032574cbd94092
Dulmandakh <dulmandakh@gmail.com>
2019-09-13 11:37:49 +0800

    fix non-flow setup

--- template/App.js
+++ template/App.js
@@ -24,7 +24,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
