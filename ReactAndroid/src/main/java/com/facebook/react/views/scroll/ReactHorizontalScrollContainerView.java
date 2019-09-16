57383f6faed2296316ce86447681d3e5be65f60a
David Biedenbach <david.biedenbach@sony.com>
2019-09-16 14:07:26 -0700

    One more getChildVisibleRect override was necessary to maintain recursion for compat. with ReactHorizontalScrollView

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollContainerView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollContainerView.java
@@ -6,10 +6,15 @@
 package com.facebook.react.views.scroll;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.HorizontalScrollView;
 import androidx.core.view.ViewCompat;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
+import com.facebook.react.uimanager.ViewProps;
 
 /** Container of Horizontal scrollViews that supports RTL scrolling. */
 public class ReactHorizontalScrollContainerView extends ViewGroup {
@@ -44,5 +49,49 @@ public class ReactHorizontalScrollContainerView extends ViewGroup {
       parent.scrollTo(offsetX, parent.getScrollY());
     }
     mCurrentWidth = getWidth();
+  }
+
+  @Override
+  public boolean getChildVisibleRect(View child, Rect r, android.graphics.Point offset) {
+    // This is based on the Android ViewGroup implementation, modified to clip child rects
+    // if overflow is set to ViewProps.HIDDEN. This effectively solves Issue #23870 which
+    // appears to have been introduced by FLAG_CLIP_CHILDREN being forced false
+    // regardless of whether clipping is desired.
+    final RectF rect = new RectF();
+    rect.set(r);
+
+    child.getMatrix().mapRect(rect);
+
+    final int dx = child.getLeft() - getScrollX();
+    int dy = child.getTop() - getScrollY();
+
+    rect.offset(dx, dy);
+
+    if (offset != null) {
+      float[] position = new float[2];
+      position[0] = offset.x;
+      position[1] = offset.y;
+      child.getMatrix().mapPoints(position);
+      offset.x = Math.round(position[0]) + dx;
+      offset.y = Math.round(position[1]) + dy;
+    }
+
+    final int width = getRight() - getLeft();
+    final int height = getBottom() - getTop();
+
+    boolean rectIsVisible = true;
+
+    ViewParent parent = getParent();
+    if (parent == null) {
+      rectIsVisible = rect.intersect(0, 0, width, height);
+    }
+
+    r.set((int) Math.floor(rect.left), (int) Math.floor(rect.top),
+      (int) Math.ceil(rect.right), (int) Math.ceil(rect.bottom));
+
+    if (rectIsVisible && parent != null) {
+      rectIsVisible = parent.getChildVisibleRect(this, r, offset);
+    }
+    return rectIsVisible;
   }
 }
