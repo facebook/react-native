97505e2323614b16a39c893934ec77d954d9014b
REDMOND\acoates <acoates@microsoft.com>
2019-09-18 15:56:32 -0700

    Test fixes

--- .ado/ado-test-setup.sh
+++ .ado/ado-test-setup.sh
@@ -8,13 +8,13 @@ THIS_DIR=$PWD
 # AppleScript can't be invoked from Azure DevOps Mojave agents until the following ticket is resolved: https://dev.azure.com/mseng/AzureDevOps/_workitems/edit/1513729
 
 # Start the packager
-# osascript -e "tell application \"Terminal\" to do script \"cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./scripts/launchPackager.command --reactNativePath ${THIS_DIR}\""
+# osascript -e "tell application \"Terminal\" to do script \"cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./scripts/launchPackager.command"
 
 # Start the WebSocket test server
 # osascript -e "tell application \"Terminal\" to do script \"cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./IntegrationTests/launchWebSocketServer.command\""
 
 COMMAND="$TMPDIR/launchPackager.command"
-echo "cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./scripts/launchPackager.command --reactNativePath ${THIS_DIR}" > "$COMMAND"
+echo "cd ${THIS_DIR}; export SERVERS_NO_WAIT=1; ./scripts/launchPackager.command > "$COMMAND"
 chmod +x "$COMMAND"
 open "$COMMAND"
 
