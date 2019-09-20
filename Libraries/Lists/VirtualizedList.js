d34bc5fa64a54dfc2e780461ee2997a4b17f8c65
Mike Vitousek <mvitousek@fb.com>
2019-09-20 13:34:48 -0700

    Upgrade to Flow v0.108.0
    Reviewed By: gabelevi
    
    Differential Revision: D17488182
    
    fbshipit-source-id: e67c5bcbd9f0bda49d52531387d92d7c83a01f21

--- Libraries/Lists/VirtualizedList.js
+++ Libraries/Lists/VirtualizedList.js
@@ -1859,6 +1859,9 @@ class CellRenderer extends React.Component<
     }
 
     if (ListItemComponent) {
+      /* $FlowFixMe(>=0.108.0 site=react_native_fb) This comment suppresses an
+       * error found when Flow v0.108 was deployed. To see the error, delete
+       * this comment and run Flow. */
       return React.createElement(ListItemComponent, {
         item,
         index,
