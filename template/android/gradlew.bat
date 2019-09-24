6830ec87ff3cf5a7cbaad9b2118e94b76bb53c3b
grabbou <grabbou@gmail.com>
2019-09-24 21:05:26 +0200

    Revert "Update Gradle wrapper to 5.6 (#26079)"
    This reverts commit afd35296f2bd0a04e3406c03ff9c023e7192161f.

--- template/android/gradlew.bat
+++ template/android/gradlew.bat
@@ -5,7 +5,7 @@
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
 @rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem      http://www.apache.org/licenses/LICENSE-2.0
 @rem
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
