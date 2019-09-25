b2a7689061d4c4165c1be2f960643ca5804df0a9
grabbou <grabbou@gmail.com>
2019-09-25 11:25:15 +0200

    Revert "[0.61.1] Bump version numbers"
    This reverts commit 2577ca534008e362a977ce7c83eb4b51e14a0b52.

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -23,7 +23,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
     __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(61),
-                  RCTVersionPatch: @(1),
+                  RCTVersionPatch: @(0),
                   RCTVersionPrerelease: [NSNull null],
                   };
   });
