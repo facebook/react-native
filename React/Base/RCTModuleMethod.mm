e0ab4c6ad1fe5d1d23df22c192dfc303bf1c5c67
Zachary E. Dahan <hello@zachary.pm>
2019-09-26 17:49:14 +0200

    Resolve signature issue on iOS 13

--- React/Base/RCTModuleMethod.mm
+++ React/Base/RCTModuleMethod.mm
@@ -91,6 +91,7 @@ static BOOL RCTParseSelectorPart(const char **input, NSMutableString *selector)
 static BOOL RCTParseUnused(const char **input)
 {
   return RCTReadString(input, "__unused") ||
+         RCTReadString(input, "__attribute__((__unused__))") ||
          RCTReadString(input, "__attribute__((unused))");
 }
 
