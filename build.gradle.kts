60afd53a4f947558bde3100d084d0e0d45e9fb42
Frieder Bluemle <frieder.bluemle@gmail.com>
2019-10-02 14:15:58 -0700

    Update Android Gradle plugin to 3.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0")
+        classpath("com.android.tools.build:gradle:3.5.1")
         classpath("de.undercouch:gradle-download-task:4.0.0")
 
         // NOTE: Do not place your application dependencies here; they belong
