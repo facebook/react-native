abf612aecd7f1ce310d611501cdb946e8903fb4f
Kevin Gozali <fkg@fb.com>
2019-10-10 21:05:12 -0700

    iOS - clean up weakproxy gating
    Summary: The proper weakproxy usage should be enabled by default from now on.
    
    Reviewed By: PeteTheHeat
    
    Differential Revision: D17866448
    
    fbshipit-source-id: da404a41fd1136d7feebfc7591fa2965a65c4c6b

--- Libraries/Image/RCTUIImageViewAnimated.h
+++ Libraries/Image/RCTUIImageViewAnimated.h
@@ -8,8 +8,6 @@
 #import <React/RCTAnimatedImage.h>
 #import <React/RCTDefines.h>
 
-RCT_EXTERN void RCTUIImageViewEnableWeakProxy(BOOL enabled);
-
 @interface RCTUIImageViewAnimated : UIImageView
 
 @end
