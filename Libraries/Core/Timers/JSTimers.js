9f71551167c211d5c2604896c2fb449b44747b18
Moti Zilberman <moti@fb.com>
2019-10-14 09:00:46 -0700

    Remove use of framesToPop from JSTimers
    Summary:
    Replaces the use of `framesToPop` in `_allocateCallback` (in `__DEV__` only) with statically accessing the second frame of the stack.
    
    Changelog: [Internal]
    
    Reviewed By: cpojer
    
    Differential Revision: D17877261
    
    fbshipit-source-id: 8e4d0eb2ed7984b66a99752fb21f7909474fda8f

--- Libraries/Core/Timers/JSTimers.js
+++ Libraries/Core/Timers/JSTimers.js
@@ -15,7 +15,6 @@ const Systrace = require('../../Performance/Systrace');
 
 const invariant = require('invariant');
 
-import type {ExtendedError} from '../Devtools/parseErrorStack';
 import NativeTiming from './NativeTiming';
 
 let _performanceNow = null;
@@ -83,11 +82,10 @@ function _allocateCallback(func: Function, type: JSTimerType): number {
   types[freeIndex] = type;
   if (__DEV__) {
     const parseErrorStack = require('../Devtools/parseErrorStack');
-    const error: ExtendedError = new Error();
-    error.framesToPop = 1;
-    const stack = parseErrorStack(error);
+    // TODO: (moti) T55685778 Use Error.captureStackTrace on Hermes
+    const stack = parseErrorStack(new Error());
     if (stack) {
-      identifiers[freeIndex] = stack.shift();
+      identifiers[freeIndex] = stack[1]; // skip _allocateCallback's own stack frame
     }
   }
   return id;
