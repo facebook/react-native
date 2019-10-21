585dfff22b4df70ce3ae195ce394fdfcf6a79d2f
David Vacca <dvacca@fb.com>
2019-10-21 11:45:43 -0700

    Disable preallocation of views in Mounting layer of fabric
    Summary:
    This diff adds an experiment to disable the preallocation of views on the mounting layer of Fabric
    
    Changelog:
    Add a ReactNativeConfig to configure the preallocation of views in the mounting layer of Fabric
    
    Reviewed By: shergin
    
    Differential Revision: D17949681
    
    fbshipit-source-id: 0af63df22aff9e94289bc8a8217c79222f1fd61c

--- ReactAndroid/src/main/java/com/facebook/react/fabric/jni/Binding.h
+++ ReactAndroid/src/main/java/com/facebook/react/fabric/jni/Binding.h
@@ -105,6 +105,7 @@ class Binding : public jni::HybridClass<Binding>, public SchedulerDelegate {
 
   std::shared_ptr<const ReactNativeConfig> reactNativeConfig_{nullptr};
   bool shouldCollateRemovesAndDeletes_{false};
+  bool disablePreallocateViews_{false};
 };
 
 } // namespace react
