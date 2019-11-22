29684501952c9c1b7577009fc7caf1449c96f434
Chunlok Lo <chunloklo@fb.com>
2019-11-22 00:45:19 -0800

    Allow extensions to set their own presentedViewController
    Summary:
    Since extensions does not have access to sharedApplication, give them an option to set the presentedViewController.
    This will allow modules such as RCTAlertsManager to function correctly in extensions.
    Changelog: [General] [Added] - Added RCTUtilsUIOverride as a way to shortcut obtaining presentedViewController from sharedApplication to a supplied view controller for extensions.
    
    Reviewed By: PeteTheHeat
    
    Differential Revision: D18620886
    
    fbshipit-source-id: c752a6e328588f388e23be5270bf7da277182cca

--- React/Base/RCTUtils.m
+++ React/Base/RCTUtils.m
@@ -19,6 +19,7 @@
 
 #import "RCTAssert.h"
 #import "RCTLog.h"
+#import <React/RCTUtilsUIOverride.h>
 
 NSString *const RCTErrorUnspecified = @"EUNSPECIFIED";
 
@@ -501,8 +502,8 @@ UIWindow *__nullable RCTKeyWindow(void)
 
 UIViewController *__nullable RCTPresentedViewController(void)
 {
-  if (RCTRunningInAppExtension()) {
-    return nil;
+  if ([RCTUtilsUIOverride hasPresentedViewController]) {
+    return [RCTUtilsUIOverride presentedViewController];
   }
 
   UIViewController *controller = RCTKeyWindow().rootViewController;
