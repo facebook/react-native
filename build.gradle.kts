2c56d37eb04883ecdd6daf94f35e47ddd9e26955
Dulmandakh <dulmandakh@gmail.com>
2019-11-25 20:24:44 +0800

    bump gradle-download-task to 4.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.2")
-        classpath("de.undercouch:gradle-download-task:4.0.0")
+        classpath("de.undercouch:gradle-download-task:4.0.2")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
