586d55d54f21e0fe6a0a0230a1b407693ba7d193
Rick Hanlon <rickhanlonii@fb.com>
2019-12-13 03:07:06 -0800

    LogBox - lazily initialize on iOS, use sync APIs
    Summary:
    Update LogBox on iOS to lazily initialize, using a synchronous RCTSurface, behind RCTSharedApplication checks.
    
    This results in faster of LogBox, without keeping around a long lived window in the background, and only used when LogBox is used.
    
    On Android, we still start the react app in the background but we create a dialog when it's shown and then destroy it when it's hidden. Once we have the sync APIs on android we can update it to use the same strategy.
    
    Changelog: [Internal]
    
    Reviewed By: fkgozali
    
    Differential Revision: D18925538
    
    fbshipit-source-id: 1a72c39aa0fc26c8ba657d36c7fa7bc0ae777eb9

--- Libraries/LogBox/LogBoxInspectorContainer.js
+++ Libraries/LogBox/LogBoxInspectorContainer.js
@@ -15,7 +15,6 @@ import {View, StyleSheet} from 'react-native';
 import * as LogBoxData from './Data/LogBoxData';
 import LogBoxInspector from './UI/LogBoxInspector';
 import type LogBoxLog from './Data/LogBoxLog';
-import NativeLogBox from '../NativeModules/specs/NativeLogBox';
 
 type Props = $ReadOnly<{|
   logs: $ReadOnlyArray<LogBoxLog>,
@@ -23,35 +22,18 @@ type Props = $ReadOnly<{|
   isDisabled?: ?boolean,
 |}>;
 
-function NativeLogBoxVisibility(props) {
-  React.useLayoutEffect(() => {
-    if (NativeLogBox) {
-      if (props.visible) {
-        // Schedule this to try and prevent flashing the old state.
-        setTimeout(() => NativeLogBox.show(), 10);
-      } else {
-        NativeLogBox.hide();
-      }
-    }
-  }, [props.visible]);
-
-  return props.children;
-}
-
 export class _LogBoxInspectorContainer extends React.Component<Props> {
   render(): React.Node {
     return (
-      <NativeLogBoxVisibility visible={this.props.selectedLogIndex >= 0}>
-        <View style={StyleSheet.absoluteFill}>
-          <LogBoxInspector
-            onDismiss={this._handleDismiss}
-            onMinimize={this._handleMinimize}
-            onChangeSelectedIndex={this._handleSetSelectedLog}
-            logs={this.props.logs}
-            selectedIndex={this.props.selectedLogIndex}
-          />
-        </View>
-      </NativeLogBoxVisibility>
+      <View style={StyleSheet.absoluteFill}>
+        <LogBoxInspector
+          onDismiss={this._handleDismiss}
+          onMinimize={this._handleMinimize}
+          onChangeSelectedIndex={this._handleSetSelectedLog}
+          logs={this.props.logs}
+          selectedIndex={this.props.selectedLogIndex}
+        />
+      </View>
     );
   }
 
