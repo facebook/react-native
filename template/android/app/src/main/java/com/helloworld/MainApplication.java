f0fbca47f398b4b468eb5cb54c5d75dcfbe2aa98
Jason Safaiyeh <safaiyeh@protonmail.com>
2019-12-19 10:20:33 -0800

    Add Flipper initialization to Android template

--- template/android/app/src/main/java/com/helloworld/MainApplication.java
+++ template/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -44,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
   /**
