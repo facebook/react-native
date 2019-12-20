0140256c89d0426fe2bfdbc08925e0e1b0a5a01c
Jakub <jakub.gonet@swmansion.com>
2019-12-20 11:10:47 +0100

    Fix typo
    paramenter -> parameter

--- Libraries/Utilities/HMRClient.js
+++ Libraries/Utilities/HMRClient.js
@@ -159,8 +159,8 @@ const HMRClient: HMRClientNativeInterface = {
     isEnabled: boolean,
   ) {
     invariant(platform, 'Missing required parameter `platform`');
-    invariant(bundleEntry, 'Missing required paramenter `bundleEntry`');
-    invariant(host, 'Missing required paramenter `host`');
+    invariant(bundleEntry, 'Missing required parameter `bundleEntry`');
+    invariant(host, 'Missing required parameter `host`');
     invariant(!hmrClient, 'Cannot initialize hmrClient twice');
 
     // Moving to top gives errors due to NativeModules not being initialized
