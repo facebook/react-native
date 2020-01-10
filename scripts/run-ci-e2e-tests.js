5083ccbc9cb7033eae7af5565fb5deb561e78af9
Rick Hanlon <rickhanlonii@fb.com>
2020-01-10 13:37:20 +0000

    Bump detox and simulator version to work with latest xcode

--- scripts/run-ci-e2e-tests.js
+++ scripts/run-ci-e2e-tests.js
@@ -212,7 +212,7 @@ try {
     if (
       tryExecNTimes(
         () => {
-          let destination = 'platform=iOS Simulator,name=iPhone 6s,OS=12.4';
+          let destination = 'platform=iOS Simulator,name=iPhone 8,OS=12.4';
           let sdk = 'iphonesimulator';
           let scheme = 'HelloWorld';
 
