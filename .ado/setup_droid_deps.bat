5fb6950a2a3a5ad27725948202dd78f9306a5d52
Anandraj <mganandraj@outlook.com>
2020-01-23 09:49:20 -0800

    Fixing the publish task (#225)

---
+++ .ado/setup_droid_deps.bat
@@ -0,0 +1,20 @@
+@if "%DEBUG%" == "" @echo off
+
+REM Assuming the script is run from the root directory of a local clone of Microsoft fork of react-native. i.e. http:\\github.com\Microsoft\react-native
+
+set BUILD_DEPS_DIR=build_deps
+
+IF EXIST %BUILD_DEPS_DIR% (
+    rmdir /s /q %BUILD_DEPS_DIR%
+    if errorlevel 1 echo "Cleaning up the build dependency directory failed !" 1>&2
+)
+
+mkdir %BUILD_DEPS_DIR%
+
+mklink /D /J %BUILD_DEPS_DIR%\boost ReactAndroid\packages\boost.1.68.0.0\lib\native\include\boost
+mklink /D /J %BUILD_DEPS_DIR%\double-conversion double-conversion\double-conversion
+mklink /D /J %BUILD_DEPS_DIR%\Folly Folly\
+mklink /D /J %BUILD_DEPS_DIR%\glog glog
+
+REM When setting up locally, set the environement variable as follows.
+REM set REACT_NATIVE_DEPENDENCIES=%CD%\%BUILD_DEPS_DIR%
