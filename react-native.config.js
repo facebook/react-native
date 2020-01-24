96ffd35640b1fcf2506329daa246ccbd9dfedf03
Tom Underhill <tomun@microsoft.com>
2020-01-24 10:41:25 -0800

    Remove react-native commands from cli when installed as 'react-native-macos' (#227)
    * Add logic to remove ios and android commands from react-native cli when installed as `react-native-macos`
    
    * Linted the js

--- react-native.config.js
+++ react-native.config.js
@@ -11,8 +11,14 @@
 const ios = require('@react-native-community/cli-platform-ios');
 const android = require('@react-native-community/cli-platform-android');
 
+// Remove commands so that react-native-macos can coexist with react-native in repos that depend on both.
+const path = require('path');
+const isReactNativeMacOS = path.basename(__dirname) === 'react-native-macos';
+const iosCommands = isReactNativeMacOS ? [] : ios.commands;
+const androidCommands = isReactNativeMacOS ? [] : android.commands;
+
 module.exports = {
-  commands: [...ios.commands, ...android.commands],
+  commands: [...iosCommands, ...androidCommands],
   platforms: {
     ios: {
       linkConfig: ios.linkConfig,
