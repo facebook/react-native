6579d8f5bcd87775227ab9ca6c968ae2e200120c
Jason Safaiyeh <safaiyeh@protonmail.com>
2020-01-29 07:48:59 -0800

    Integrate Flipper in RNTester on Android (#27631)
    Summary:
    0.62 Flipper Support items: https://github.com/facebook/react-native/issues/27565
    Made RNTester's Android Flipper implementation consistent with the template.
    ~~Added Flipper to the iOS build.~~
    <img width="1259" alt="Screen Shot 2019-12-28 at 7 06 42 PM" src="https://user-images.githubusercontent.com/8675043/71551835-37290800-29a5-11ea-9eac-b119a69a68c1.png">
    
    ## Changelog
    
    [Internal] [Added] - RNTester Android Fipper updates
    Pull Request resolved: https://github.com/facebook/react-native/pull/27631
    
    Test Plan: Run RNTester and see if it connects to Flipper.
    
    Reviewed By: rickhanlonii
    
    Differential Revision: D19345093
    
    Pulled By: passy
    
    fbshipit-source-id: 6957c1ca3f4a5bb7f0e581c5daf8ddeac5d87eea

--- RNTester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterApplication.java
+++ RNTester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterApplication.java
@@ -16,6 +16,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.views.text.ReactFontManager;
+import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
@@ -48,6 +49,7 @@ public class RNTesterApplication extends Application implements ReactApplication
   public void onCreate() {
     ReactFontManager.getInstance().addCustomFont(this, "Rubik", R.font.rubik);
     super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
