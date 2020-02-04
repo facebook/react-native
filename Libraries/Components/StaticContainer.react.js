b7bc31a3051c8840e3a7a3d712173f0379dc8434
Spencer Ahrens <sahrens@fb.com>
2020-02-04 13:51:50 -0800

    Cleanup StaticContainer a bit
    Summary:
    React is way better than it used to be...
    
    Changelog: [Internal]
    
    Reviewed By: zackargyle, lunaleaps
    
    Differential Revision: D19421772
    
    fbshipit-source-id: db82b68a0c2f31e9ef7e1875fd9e4bf27ef6295a

--- Libraries/Components/StaticContainer.react.js
+++ Libraries/Components/StaticContainer.react.js
@@ -32,7 +32,7 @@ type Props = $ReadOnly<{|
   /**
    * Whether or not this component should update.
    */
-  shouldUpdate: ?boolean,
+  shouldUpdate?: ?boolean,
   /**
    * Content short-circuited by React reconciliation process.
    */
@@ -43,11 +43,8 @@ class StaticContainer extends React.Component<Props> {
     return !!nextProps.shouldUpdate;
   }
 
-  render(): null | React$Node {
-    const child = this.props.children;
-    return child === null || child === false
-      ? null
-      : React.Children.only(child);
+  render(): React.Node {
+    return this.props.children;
   }
 }
 
