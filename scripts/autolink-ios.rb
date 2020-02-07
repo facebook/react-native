3b21983e257fd5c4a1656a27444cf93ca310ba83
Janic Duplessis <janicduplessis@gmail.com>
2020-02-06 23:16:21 -0500

    Update autolink-ios.rb

--- scripts/autolink-ios.rb
+++ scripts/autolink-ios.rb
@@ -43,7 +43,7 @@ def use_react_native! (options={})
   pod 'React-jsinspector', :path => "#{prefix}/ReactCommon/jsinspector"
   pod 'ReactCommon/callinvoker', :path => "#{prefix}/ReactCommon"
   pod 'ReactCommon/turbomodule/core', :path => "#{prefix}/ReactCommon"
-  pod 'Yoga', :path => "#{prefix}/ReactCommon/yoga"
+  pod 'Yoga', :path => "#{prefix}/ReactCommon/yoga", :modular_headers => true
 
   pod 'DoubleConversion', :podspec => "#{prefix}/third-party-podspecs/DoubleConversion.podspec"
   pod 'glog', :podspec => "#{prefix}/third-party-podspecs/glog.podspec"
