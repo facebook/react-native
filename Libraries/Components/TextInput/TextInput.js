4ffb4345fd0ce533ad610cef75beb8f162912390
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-02-09 19:42:06 +0900

    chore: remove SwipeableListView from the description

--- Libraries/Components/TextInput/TextInput.js
+++ Libraries/Components/TextInput/TextInput.js
@@ -271,7 +271,7 @@ type IOSProps = $ReadOnly<{|
 
   /*
    * If `true`, allows TextInput to pass touch events to the parent component.
-   * This allows components such as SwipeableListView to be swipeable from the TextInput on iOS,
+   * This allows components to be swipeable from the TextInput on iOS,
    * as is the case on Android by default.
    * If `false`, TextInput always asks to handle the input (except when disabled).
    * @platform ios
