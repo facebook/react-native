1562d0c543d2adf741493c83d9a1949969ad0f9c
Tom Underhill <tomun@microsoft.com>
2020-02-11 17:01:55 -0800

    Fixed lint error

--- Libraries/Share/Share.js
+++ Libraries/Share/Share.js
@@ -120,10 +120,7 @@ class Share {
               typeof content.message === 'string' ? content.message : undefined,
             url: typeof content.url === 'string' ? content.url : undefined,
             subject: options.subject,
-            tintColor:
-              typeof tintColor === 'number'
-                ? tintColor
-                : undefined,
+            tintColor: typeof tintColor === 'number' ? tintColor : undefined,
             excludedActivityTypes: options.excludedActivityTypes,
           },
           error => reject(error),
