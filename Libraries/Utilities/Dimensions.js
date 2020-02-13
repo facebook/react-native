bcc62a6d179e673754a46f9d93ab61dbe0576894
Adam Ernst <adamjernst@fb.com>
2020-02-13 14:26:15 -0800

    Clean up misc references to ComponentScript
    Summary:
    ComponentScript is defunct.
    
    Changelog: [Internal]
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: yungsters
    
    Differential Revision: D19883555
    
    fbshipit-source-id: 4459e42518468086489bb5d1470a38710423656a

--- Libraries/Utilities/Dimensions.js
+++ Libraries/Utilities/Dimensions.js
@@ -134,8 +134,6 @@ if (!initialDims) {
       Dimensions.set(update);
     },
   );
-  // Can't use NativeDeviceInfo in ComponentScript because it does not support NativeModules,
-  // but has nativeExtensions instead.
   initialDims = NativeDeviceInfo.getConstants().Dimensions;
 }
 
