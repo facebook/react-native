c55521aac0322f3a07ac1df5859b62f375b7e5d4
Haozhun Jin <hjin@fb.com>
2020-02-13 19:24:51 -0800

    Allow non-ASCII std::string argument to detail::toValue
    Summary:
    In all other areas of JSI, std::string is treated as potentially
    containing UTF-8 bytes (instead of ASCII). This fixes the inconsistency.
    
    Changelog: [Internal]
    
    Reviewed By: mhorowitz
    
    Differential Revision: D19871520
    
    fbshipit-source-id: c703f07e10bedbf2518d0bec903f85f43bbcbdf5

--- ReactCommon/jsi/jsi/test/testlib.cpp
+++ ReactCommon/jsi/jsi/test/testlib.cpp
@@ -474,7 +474,8 @@ TEST_P(JSITest, FunctionTest) {
   // This tests all the function argument converters, and all the
   // non-lvalue overloads of call().
   Function f = function(
-      "function(n, b, d, df, i, s1, s2, s3, o, a, f, v) { return "
+      "function(n, b, d, df, i, s1, s2, s3, s_sun, s_bad, o, a, f, v) { "
+      "return "
       "n === null && "
       "b === true && "
       "d === 3.14 && "
@@ -483,11 +484,12 @@ TEST_P(JSITest, FunctionTest) {
       "s1 == 's1' && "
       "s2 == 's2' && "
       "s3 == 's3' && "
+      "s_sun == 's\\u2600' && "
+      "typeof s_bad == 'string' && "
       "typeof o == 'object' && "
       "Array.isArray(a) && "
       "typeof f == 'function' && "
       "v == 42 }");
-  std::string s3 = "s3";
   EXPECT_TRUE(f.call(
                    rt,
                    nullptr,
@@ -497,7 +499,10 @@ TEST_P(JSITest, FunctionTest) {
                    17,
                    "s1",
                    String::createFromAscii(rt, "s2"),
-                   s3,
+                   std::string{"s3"},
+                   std::string{u8"s\u2600"},
+                   // invalid UTF8 sequence due to unexpected continuation byte
+                   std::string{"s\x80"},
                    Object(rt),
                    Array(rt, 1),
                    function("function(){}"),
