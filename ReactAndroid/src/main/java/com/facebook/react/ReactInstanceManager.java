bf869d9917e0bee17ace579820881e5ef499cf80
Joshua Gross <joshuagross@fb.com>
2020-02-20 17:49:28 -0800

    `ReactInstanceManager.attachRootViewToInstance` should show up in production logs for T62192299 analysis
    Summary:
    Temporary debug logs.
    
    Changelog: [Internal]
    
    Reviewed By: mdvacca
    
    Differential Revision: D20016162
    
    fbshipit-source-id: 27848a5249bd625c74dc9023c89da6ec5edd9bd5

--- ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
@@ -1121,7 +1121,8 @@ public class ReactInstanceManager {
   }
 
   private void attachRootViewToInstance(final ReactRoot reactRoot) {
-    FLog.d(ReactConstants.TAG, "ReactInstanceManager.attachRootViewToInstance()");
+    // TODO: downgrade back to FLog.d once T62192299 is resolved.
+    FLog.e(ReactConstants.TAG, "ReactInstanceManager.attachRootViewToInstance()");
     Systrace.beginSection(TRACE_TAG_REACT_JAVA_BRIDGE, "attachRootViewToInstance");
 
     @Nullable
