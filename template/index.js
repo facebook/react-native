6d1c0dab0bbd72ad229a15de8c7ec183f0578966
Han Lin Yap <codler@users.noreply.github.com>
2020-02-23 09:59:21 +0100

    Move App and test to src folder in template

--- template/index.js
+++ template/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- template/src/App.test.js
+++ template/src/App.test.js
@@ -4,7 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from './App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
