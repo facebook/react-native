f5135bddbdc90ab453951a96afd4d5a50358440f
Tommy Nguyen <tn0502@gmail.com>
2020-02-25 00:46:23 +0100

    Improvements to app bundle size reports
    - Generate table for APK sizes
    - Replace existing comment if possible

--- scripts/circleci/report-bundle-size.sh
+++ scripts/circleci/report-bundle-size.sh
@@ -15,46 +15,50 @@ function diskusage {
 
 function comment {
   local body=$1
+  local replace=$2
   GITHUB_OWNER=${CIRCLE_PROJECT_USERNAME:-facebook} \
   GITHUB_REPO=${CIRCLE_PROJECT_REPONAME:-react-native} \
   GITHUB_PR_NUMBER="$CIRCLE_PR_NUMBER" \
-  node bots/make-comment.js "$body"
+  node bots/make-comment.js "$body" "$replace"
 }
 
 case $1 in
   "android")
     # Outputs:
-    #     RNTester (Android/hermes/arm64-v8a): 9437184 bytes
-    #     RNTester (Android/hermes/armeabi-v7a): 9015296 bytes
-    #     RNTester (Android/hermes/x86): 9498624 bytes
-    #     RNTester (Android/hermes/x86_64): 9965568 bytes
-    #     RNTester (Android/jsc/arm64-v8a): 9236480 bytes
-    #     RNTester (Android/jsc/armeabi-v7a): 8814592 bytes
-    #     RNTester (Android/jsc/x86): 9297920 bytes
-    #     RNTester (Android/jsc/x86_64): 9764864 bytes
+    #     | App      | Platform | Engine | Arch        | Size (bytes) |
+    #     |:---------|:---------|:-------|:------------|-------------:|
+    #     | RNTester | Android  | hermes | arm64-v8a   |      9437184 |
+    #     | RNTester | Android  | hermes | armeabi-v7a |      9015296 |
+    #     | RNTester | Android  | hermes | x86         |      9498624 |
+    #     | RNTester | Android  | hermes | x86_64      |      9965568 |
+    #     | RNTester | Android  | jsc    | arm64-v8a   |      9236480 |
+    #     | RNTester | Android  | jsc    | armeabi-v7a |      8814592 |
+    #     | RNTester | Android  | jsc    | x86         |      9297920 |
+    #     | RNTester | Android  | jsc    | x86_64      |      9764864 |
     eol=$'\n'
-    size_report=""
+    size_report="| App      | Platform | Engine | Arch        | Size (bytes) |$eol"
+    size_report+="|:---------|:---------|:-------|:------------|-------------:|$eol"
     for engine in hermes jsc; do
       outputs="RNTester/android/app/build/outputs/apk/$engine/release"
       if [[ -d "$outputs" ]]; then
         for arch in arm64-v8a armeabi-v7a x86 x86_64; do
           apk="$outputs/app-$engine-$arch-release.apk"
           if [[ -f "$apk" ]]; then
-            size_report+="RNTester (Android/$engine/$arch): $(diskusage "$apk") bytes$eol"
+            size_report+="| RNTester | Android | $engine | $arch | $(diskusage "$apk") |$eol"
           else
-            size_report+="RNTester (Android/$engine/$arch): n/a$eol"
+            size_report+="| RNTester | Android | $engine | $arch | n/a |$eol"
           fi
         done
       fi
     done
-    comment "$size_report"
+    comment "$size_report" "RNTester \\| Android"
     ;;
   "ios")
     # Outputs:
     #     RNTester.app (iOS): 9535488 bytes
     binary='RNTester/build/Build/Products/Release-iphonesimulator/RNTester.app/RNTester'
     if [[ -f "$binary" ]]; then
-      comment "RNTester.app (iOS): $(diskusage "$binary") bytes"
+      comment "RNTester.app (iOS): $(diskusage "$binary") bytes" "^RNTester.app \\(iOS\\)"
     fi
     ;;
   *)
