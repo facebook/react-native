f4538e577734d9448deec007a156905ae3c2527b
Logan Daniels <logand@fb.com>
2020-03-05 15:55:10 -0800

    Back out "UIViewController-based status bar management"
    Summary:
    Original commit changeset: 9ae1384ee20a
    
    Changelog: [Internal]
    
    Reviewed By: hramos
    
    Differential Revision: D20289822
    
    fbshipit-source-id: 0db9d99bea458e150d33b3407c256b862dedb9c1

--- React/CoreModules/RCTStatusBarManager.mm
+++ React/CoreModules/RCTStatusBarManager.mm
@@ -13,7 +13,6 @@
 #import <React/RCTLog.h>
 #import <React/RCTUIManager.h>
 #import <React/RCTUtils.h>
-#import <React/RCTRootViewController.h>
 
 #if !TARGET_OS_TV
 #import <FBReactNativeSpec/FBReactNativeSpec.h>
@@ -145,27 +144,7 @@ RCT_EXPORT_MODULE()
   [self emitEvent:@"statusBarFrameWillChange" forNotification:notification];
 }
 
-- (UIViewController<RCTRootViewControllerProtocol>*) viewControllerForReactTag:(nonnull NSNumber *)reactTag
-{
-  if (!RCTViewControllerBasedStatusBarAppearance()) {
-    return nil;
-  }
-
-  UIView *view = [self.bridge.uiManager viewForReactTag:reactTag];
-  UIViewController *viewController = view.window.rootViewController ?: RCTKeyWindow().rootViewController;
-
-  if ([viewController conformsToProtocol:@protocol(RCTRootViewControllerProtocol)]) {
-    return (UIViewController<RCTRootViewControllerProtocol>*) viewController;
-  } else {
-    RCTLogError(@"RCTStatusBarManager could not find RCTRootViewController. \
-                If UIViewControllerBasedStatusBarAppearance key in the Info.plist is set to YES (recommended for new apps), \
-                You need to use RCTRootViewControllerProtocol-conforming view controller as app window's root view controller \
-                and must pass a node reference to `surface` argument of StatusBar methods.");
-    return nil;
-  }
-}
-
-RCT_EXPORT_METHOD(getHeight:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(getHeight : (RCTResponseSenderBlock)callback)
 {
   callback(@[ @{
     @"height" : @(RCTSharedApplication().statusBarFrame.size.height),
@@ -176,22 +155,19 @@ RCT_EXPORT_METHOD(setStyle:(NSString *)style
                   animated:(BOOL)animated
                   reactTag:(double)reactTag)
 {
-    //  NSNumber *reactTag = options.reactTag() ? @(options.reactTag()) : @-1;
-    UIStatusBarStyle statusBarStyle = [RCTConvert UIStatusBarStyle:style];
-    UIViewController<RCTRootViewControllerProtocol> *viewController = [self viewControllerForReactTag:@(reactTag)];
+  UIStatusBarStyle statusBarStyle = [RCTConvert UIStatusBarStyle:style];
+  if (RCTViewControllerBasedStatusBarAppearance()) {
+    RCTLogError(@"RCTStatusBarManager module requires that the \
+                UIViewControllerBasedStatusBarAppearance key in the Info.plist is set to NO");
+    return;
+  }
 
-    if (viewController) {
-        [viewController updateStatusBarStyle:statusBarStyle
-                                      hidden:viewController.prefersStatusBarHidden
-                                   animation:viewController.preferredStatusBarUpdateAnimation
-                                    animated:animated];
-    } else {
+  // TODO (T62270453): Add proper support for UIScenes (this requires view controller based status bar management)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        [RCTSharedApplication() setStatusBarStyle:statusBarStyle
-                                         animated:animated];
+  [RCTSharedApplication() setStatusBarStyle:statusBarStyle
+                                   animated:animated];
 #pragma clang diagnostic pop
-    }
 }
 
 RCT_EXPORT_METHOD(setHidden:(BOOL)hidden
@@ -199,20 +175,18 @@ RCT_EXPORT_METHOD(setHidden:(BOOL)hidden
                   reactTag:(double)reactTag)
 {
   UIStatusBarAnimation animation = [RCTConvert UIStatusBarAnimation:withAnimation];
-  UIViewController<RCTRootViewControllerProtocol> *viewController = [self viewControllerForReactTag:@(reactTag)];
+  if (RCTViewControllerBasedStatusBarAppearance()) {
+    RCTLogError(@"RCTStatusBarManager module requires that the \
+                UIViewControllerBasedStatusBarAppearance key in the Info.plist is set to NO");
+    return;
+  }
 
-  if (viewController) {
-    [viewController updateStatusBarStyle:viewController.preferredStatusBarStyle
-                                  hidden:hidden
-                               animation:animation
-                                animated:animation != UIStatusBarAnimationNone];
-  } else {
+  // TODO (T62270453): Add proper support for UIScenes (this requires view controller based status bar management)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    [RCTSharedApplication() setStatusBarHidden:hidden
-                                 withAnimation:animation];
+  [RCTSharedApplication() setStatusBarHidden:hidden
+                               withAnimation:animation];
 #pragma clang diagnostic pop
-  }
 }
 
 RCT_EXPORT_METHOD(setNetworkActivityIndicatorVisible : (BOOL)visible)
