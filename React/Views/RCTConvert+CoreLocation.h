35f0cf7af841a763440ab445b5c57c2abed76662
maschad <chad.nehemiah94@gmail.com>
2020-03-14 16:22:21 -0500

    Re-added RCTConvert files due to dependencies

---
+++ React/Views/RCTConvert+CoreLocation.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <CoreLocation/CoreLocation.h>
+
+#import <React/RCTConvert.h>
+
+@interface RCTConvert (CoreLocation)
+
++ (CLLocationDegrees)CLLocationDegrees:(id)json;
++ (CLLocationDistance)CLLocationDistance:(id)json;
++ (CLLocationCoordinate2D)CLLocationCoordinate2D:(id)json;
+
+@end
---
+++ React/Views/RCTConvert+CoreLocation.m
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "RCTConvert+CoreLocation.h"
+
+@implementation RCTConvert (CoreLocation)
+
+RCT_CONVERTER(CLLocationDegrees, CLLocationDegrees, doubleValue);
+RCT_CONVERTER(CLLocationDistance, CLLocationDistance, doubleValue);
+
++ (CLLocationCoordinate2D)CLLocationCoordinate2D:(id)json
+{
+  json = [self NSDictionary:json];
+  return (CLLocationCoordinate2D){[self CLLocationDegrees:json[@"latitude"]],
+                                  [self CLLocationDegrees:json[@"longitude"]]};
+}
+
+@end
