235b4ba1951a4fa0fc41240ad16d1821424249bc
Tom Underhill <tomun@microsoft.com>
2020-05-04 20:59:05 -0700

    Restore Office npm publishing (#340)
    * Update scripts to publish react-native-macos-init
    
    * Clean up merge markers
    
    * Restored ios:macos RNTester parity except for InputAccessoryView.
    
    * Revert "Restored ios:macos RNTester parity except for InputAccessoryView."
    
    This reverts commit 5a67ae06b01301ac43533e7e20dd7fed24a0937f.
    
    * Remove unnecessary android builds and tar file upload.
    
    * Restore office npm tar file creation so that Release def can publish it.
    
    Co-authored-by: React-Native Bot <53619745+rnbot@users.noreply.github.com>

---
+++ .ado/npmOfficePack.js
@@ -0,0 +1,46 @@
+// @ts-check
+// Create a tar asset for publishing to the Office feed
+
+const fs = require("fs");
+const path = require("path");
+const execSync = require("child_process").execSync;
+const {pkgJsonPath, publishBranchName, gatherVersionInfo} = require('./versionUtils');
+
+function exec(command) {
+  try {
+    console.log(`Running command: ${command}`);
+    return execSync(command, {
+      stdio: "inherit"
+    });
+  } catch (err) {
+    process.exitCode = 1;
+    console.log(`Failure running: ${command}`);
+    throw err;
+  }
+}
+
+function doPublish() {
+  console.log(`Target branch to publish to: ${publishBranchName}`);
+
+  const {releaseVersion, branchVersionSuffix} = gatherVersionInfo()
+
+  const onlyTagSource = !!branchVersionSuffix;
+  if (!onlyTagSource) {
+    // -------- Generating Android Artifacts with JavaDoc
+    exec("gradlew installArchives");
+
+    // undo uncommenting javadoc setting
+    exec("git checkout ReactAndroid/gradle.properties");
+  }
+
+  // Create tar file
+  exec(`npm pack`);
+
+  const npmTarFileName = `react-native-${releaseVersion}.tgz`;
+  const npmTarPath = path.resolve(__dirname, '..', npmTarFileName);
+  const finalTarPath = path.join(process.env.BUILD_STAGINGDIRECTORY, 'final', npmTarFileName);
+  console.log(`Copying tar file ${npmTarPath} to: ${finalTarPath}`)
+  fs.copyFileSync(npmTarPath, finalTarPath);
+}
+
+doPublish();
