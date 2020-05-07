7bb1c4e1b8715a5c9cb6f9e4e77a6df783481d3d
Eloy Durán <eloy.de.enige@gmail.com>
2020-05-07 08:34:07 -0700

    Enable with CocoaPods `:configuration` (#28796)
    Summary:
    ~~⚠️ Depends on https://github.com/facebook/flipper/pull/1086 and a new Flipper release.~~
    Fixes https://github.com/facebook/react-native/commit/17f025bc26da13da795845a3f7daee65563420c0#commitcomment-38831234
    
    Currently user’s are being told to add a definition of the `FB_SONARKIT_ENABLED` macro and examples, including those in stock React Native templates, set this for the user by making use of a `post_install` hook in the user’s `Podfile`. This leads to confusion, fragile code [when a user’s project dir structure deviates from vanilla], and is ultimately not necessary as CocoaPods already has dedicated mechanisms to:
    
    * specify build settings (through the `xcconfig` property);
    * and selectively include certain pods only in certain build configurations (e.g. debug).
    
    ## Changelog
    
    <!-- Help reviewers and the release process by writing your own changelog entry. For an example, see:
    https://github.com/facebook/react-native/wiki/Changelog
    -->
    
    [iOS] [Changed] - Entirely control Flipper being enabled through inclusion in Podfile and optionally limiting to certain build configurations using the `:configuration` directive.
    Pull Request resolved: https://github.com/facebook/react-native/pull/28796
    
    Test Plan: Tested using the changes of https://github.com/facebook/flipper/pull/1086 in a new app that uses RN `master`.
    
    Reviewed By: priteshrnandgaonkar
    
    Differential Revision: D21449754
    
    Pulled By: passy
    
    fbshipit-source-id: 9ff7c7f4ffc32b364b1edd82b94e0b80c3997625

--- template/ios/HelloWorld/AppDelegate.m
+++ template/ios/HelloWorld/AppDelegate.m
@@ -4,7 +4,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -27,7 +27,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
 
