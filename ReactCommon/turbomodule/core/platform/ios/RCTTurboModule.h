ff97662db57b0740ea29358e86db1cbab7541431
Ramanpreet Nara <ramanpreet@fb.com>
2020-05-13 19:18:56 -0700

    Switch TurboModules over to NativeModulePerfLogger
    Differential Revision: D21363243
    
    fbshipit-source-id: 8abffead99ef935dcc8a3e769a1260815fd5400d

--- ReactCommon/turbomodule/core/platform/ios/RCTTurboModule.h
+++ ReactCommon/turbomodule/core/platform/ios/RCTTurboModule.h
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#pragma once
+
 #import <memory>
 
 #import <Foundation/Foundation.h>
@@ -22,101 +24,6 @@
   ((RCTTurboModuleEnabled() && [(klass) conformsToProtocol:@protocol(RCTTurboModule)]))
 #define RCT_IS_TURBO_MODULE_INSTANCE(module) RCT_IS_TURBO_MODULE_CLASS([(module) class])
 
-typedef int MethodCallId;
-
-/**
- * This interface exists to allow the application to collect performance
- * metrics of the TurboModule system. By implementing each function, you can
- * hook into various stages of TurboModule creation and method dispatch (both async and sync).
- *
- * Note:
- *  - TurboModule async method invocations can interleave, so methodCallId should be used as a unique id for a method
- *    call.
- */
-@protocol RCTTurboModulePerformanceLogger
-// Create TurboModule JS Object
-- (void)createTurboModuleStart:(const char *)moduleName;
-- (void)createTurboModuleEnd:(const char *)moduleName;
-- (void)createTurboModuleCacheHit:(const char *)moduleName;
-- (void)getCppTurboModuleFromTMMDelegateStart:(const char *)moduleName;
-- (void)getCppTurboModuleFromTMMDelegateEnd:(const char *)moduleName;
-- (void)getTurboModuleFromRCTTurboModuleStart:(const char *)moduleName;
-- (void)getTurboModuleFromRCTTurboModuleEnd:(const char *)moduleName;
-- (void)getTurboModuleFromRCTCxxModuleStart:(const char *)moduleName;
-- (void)getTurboModuleFromRCTCxxModuleEnd:(const char *)moduleName;
-- (void)getTurboModuleFromTMMDelegateStart:(const char *)moduleName;
-- (void)getTurboModuleFromTMMDelegateEnd:(const char *)moduleName;
-
-// Create RCTTurboModule object
-- (void)createRCTTurboModuleStart:(const char *)moduleName;
-- (void)createRCTTurboModuleEnd:(const char *)moduleName;
-- (void)createRCTTurboModuleCacheHit:(const char *)moduleName;
-- (void)getRCTTurboModuleClassStart:(const char *)moduleName;
-- (void)getRCTTurboModuleClassEnd:(const char *)moduleName;
-- (void)getRCTTurboModuleInstanceStart:(const char *)moduleName;
-- (void)getRCTTurboModuleInstanceEnd:(const char *)moduleName;
-- (void)setupRCTTurboModuleDispatch:(const char *)moduleName;
-- (void)setupRCTTurboModuleStart:(const char *)moduleName;
-- (void)setupRCTTurboModuleEnd:(const char *)moduleName;
-- (void)attachRCTBridgeToRCTTurboModuleStart:(const char *)moduleName;
-- (void)attachRCTBridgeToRCTTurboModuleEnd:(const char *)moduleName;
-- (void)attachMethodQueueToRCTTurboModuleStart:(const char *)moduleName;
-- (void)attachMethodQueueToRCTTurboModuleEnd:(const char *)moduleName;
-- (void)registerRCTTurboModuleForFrameUpdatesStart:(const char *)moduleName;
-- (void)registerRCTTurboModuleForFrameUpdatesEnd:(const char *)moduleName;
-- (void)dispatchDidInitializeModuleNotificationForRCTTurboModuleStart:(const char *)moduleName;
-- (void)dispatchDidInitializeModuleNotificationForRCTTurboModuleEnd:(const char *)moduleName;
-
-// Sync method invocation
-- (void)syncMethodCallStart:(const char *)moduleName
-                 methodName:(const char *)methodName
-               methodCallId:(MethodCallId)methodCallId;
-- (void)syncMethodCallEnd:(const char *)moduleName
-               methodName:(const char *)methodName
-             methodCallId:(MethodCallId)methodCallId;
-- (void)syncMethodCallArgumentConversionStart:(const char *)moduleName
-                                   methodName:(const char *)methodName
-                                 methodCallId:(MethodCallId)methodCallId;
-- (void)syncMethodCallArgumentConversionEnd:(const char *)moduleName
-                                 methodName:(const char *)methodName
-                               methodCallId:(MethodCallId)methodCallId;
-- (void)syncRCTTurboModuleMethodCallStart:(const char *)moduleName
-                               methodName:(const char *)methodName
-                             methodCallId:(MethodCallId)methodCallId;
-- (void)syncRCTTurboModuleMethodCallEnd:(const char *)moduleName
-                             methodName:(const char *)methodName
-                           methodCallId:(MethodCallId)methodCallId;
-- (void)syncMethodCallReturnConversionStart:(const char *)moduleName
-                                 methodName:(const char *)methodName
-                               methodCallId:(MethodCallId)methodCallId;
-- (void)syncMethodCallReturnConversionEnd:(const char *)moduleName
-                               methodName:(const char *)methodName
-                             methodCallId:(MethodCallId)methodCallId;
-
-// Async method invocation
-- (void)asyncMethodCallStart:(const char *)moduleName
-                  methodName:(const char *)methodName
-                methodCallId:(MethodCallId)methodCallId;
-- (void)asyncMethodCallEnd:(const char *)moduleName
-                methodName:(const char *)methodName
-              methodCallId:(MethodCallId)methodCallId;
-- (void)asyncMethodCallArgumentConversionStart:(const char *)moduleName
-                                    methodName:(const char *)methodName
-                                  methodCallId:(MethodCallId)methodCallId;
-- (void)asyncMethodCallArgumentConversionEnd:(const char *)moduleName
-                                  methodName:(const char *)methodName
-                                methodCallId:(MethodCallId)methodCallId;
-- (void)asyncRCTTurboModuleMethodCallDispatch:(const char *)moduleName
-                                   methodName:(const char *)methodName
-                                 methodCallId:(MethodCallId)methodCallId;
-- (void)asyncRCTTurboModuleMethodCallStart:(const char *)moduleName
-                                methodName:(const char *)methodName
-                              methodCallId:(MethodCallId)methodCallId;
-- (void)asyncRCTTurboModuleMethodCallEnd:(const char *)moduleName
-                              methodName:(const char *)methodName
-                            methodCallId:(MethodCallId)methodCallId;
-@end
-
 namespace facebook {
 namespace react {
 
@@ -133,7 +40,8 @@ class JSI_EXPORT ObjCTurboModule : public TurboModule {
     id<RCTTurboModule> instance;
     std::shared_ptr<CallInvoker> jsInvoker;
     std::shared_ptr<CallInvoker> nativeInvoker;
-    id<RCTTurboModulePerformanceLogger> perfLogger;
+    // Does the NativeModule dispatch async methods to the JS thread?
+    bool isSyncModule;
   };
 
   ObjCTurboModule(const InitParams &params);
@@ -159,17 +67,9 @@ class JSI_EXPORT ObjCTurboModule : public TurboModule {
    */
   NSMutableDictionary<NSString *, NSMutableArray *> *methodArgConversionSelectors_;
   NSDictionary<NSString *, NSArray<NSString *> *> *methodArgumentTypeNames_;
+  const bool isSyncModule_;
+  bool isMethodSync(TurboModuleMethodValueKind returnType);
   NSString *getArgumentTypeName(NSString *methodName, int argIndex);
-  id<RCTTurboModulePerformanceLogger> performanceLogger_;
-
-  /**
-   * Required for performance logging async method invocations.
-   * This field is static because two nth async method calls from different
-   * TurboModules can interleave, and should therefore be treated as two distinct calls.
-   */
-  static MethodCallId methodCallId_;
-
-  static MethodCallId getNewMethodCallId();
 
   NSInvocation *getMethodInvocation(
       jsi::Runtime &runtime,
@@ -178,15 +78,13 @@ class JSI_EXPORT ObjCTurboModule : public TurboModule {
       SEL selector,
       const jsi::Value *args,
       size_t count,
-      NSMutableArray *retainedObjectsForInvocation,
-      MethodCallId methodCallId);
+      NSMutableArray *retainedObjectsForInvocation);
   jsi::Value performMethodInvocation(
       jsi::Runtime &runtime,
       TurboModuleMethodValueKind returnType,
       const char *methodName,
       NSInvocation *inv,
-      NSMutableArray *retainedObjectsForInvocation,
-      MethodCallId methodCallId);
+      NSMutableArray *retainedObjectsForInvocation);
 
   BOOL hasMethodArgConversionSelector(NSString *methodName, int argIndex);
   SEL getMethodArgConversionSelector(NSString *methodName, int argIndex);
