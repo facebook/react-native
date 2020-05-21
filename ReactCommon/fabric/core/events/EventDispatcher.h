43d65ed66cd25e75c3b3545a00af7856573c991f
Samuel Susla <samuelsusla@fb.com>
2020-05-21 11:53:44 -0700

    Separate event dispatchers to ivars instead of array
    Summary:
    Changelog: [Internal]
    
    Separates EventQueues from an array into 4 ivars.
    EventQueues are of different type, in the future we will want to call different methods on different kind of EventQueue.
    
    Reviewed By: shergin
    
    Differential Revision: D21648905
    
    fbshipit-source-id: 90ae65edb8a9276eecfea9770f554d8c56804797

--- ReactCommon/fabric/core/events/EventDispatcher.h
+++ ReactCommon/fabric/core/events/EventDispatcher.h
@@ -10,12 +10,13 @@
 #include <array>
 #include <memory>
 
+#include <react/core/BatchedEventQueue.h>
 #include <react/core/EventBeat.h>
 #include <react/core/EventPipe.h>
 #include <react/core/EventPriority.h>
-#include <react/core/EventQueue.h>
 #include <react/core/StatePipe.h>
 #include <react/core/StateUpdate.h>
+#include <react/core/UnbatchedEventQueue.h>
 
 namespace facebook {
 namespace react {
@@ -52,7 +53,10 @@ class EventDispatcher {
  private:
   EventQueue const &getEventQueue(EventPriority priority) const;
 
-  std::array<std::unique_ptr<EventQueue>, 4> eventQueues_;
+  std::unique_ptr<UnbatchedEventQueue> synchronousUnbatchedQueue_;
+  std::unique_ptr<BatchedEventQueue> synchronousBatchedQueue_;
+  std::unique_ptr<UnbatchedEventQueue> asynchronousUnbatchedQueue_;
+  std::unique_ptr<BatchedEventQueue> asynchronousBatchedQueue_;
 };
 
 } // namespace react
