bd314168dd4978f45634b61529a085f274a0f77a
Joshua Gross <joshuagross@fb.com>
2020-05-27 13:34:43 -0700

    Reset RootView ID when root view is detached from view hierarchy
    Summary:
    I have a theory that T53114059 can be caused by (1) other crashes, and/or (2) reloads. Clear out the ID of a RootView when it is detached.
    
    Changelog: [Internal]
    
    Reviewed By: mdvacca
    
    Differential Revision: D21732066
    
    fbshipit-source-id: 5e607f34cf42ca26bdf199d5d3024fd49a60dc1e

--- ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
@@ -840,6 +840,9 @@ public class ReactInstanceManager {
           detachViewFromInstance(reactRoot, currentContext.getCatalystInstance());
         }
       }
+
+      // Reset reactRoot content in case it's reused
+      clearReactRoot(reactRoot);
     }
   }
 
