9baf6f2140ffc26d043f9717a1ea49fd0e08eed8
Marc Horowitz <mhorowitz@fb.com>
2020-06-03 18:11:35 -0700

    Handle stack overflow in JSError construction gracefully
    Summary:
    JSError creation can lead to further errors.  Make sure these cases
    are handled and don't cause weird crashes or other issues.
    
    This solution has a few parts:
     * include a ScopedNativeDepthTracker in checkStatus
     * If an exception object message or stack property is already a String, don't
       call JS String ctor on it
     * Verify that a jsi::Value is a String before calling getString on it.
     * Add more tests for JSError construction
    
    Changelog: [Internal]
    
    Reviewed By: dulinriley
    
    Differential Revision: D21851645
    
    fbshipit-source-id: 2d10da0e741ad4ede93cd806320f68ad512e5138

--- ReactCommon/jsi/jsi/test/testlib.cpp
+++ ReactCommon/jsi/jsi/test/testlib.cpp
@@ -981,19 +981,6 @@ TEST_P(JSITest, JSErrorsCanBeConstructedWithStack) {
 }
 
 TEST_P(JSITest, JSErrorDoesNotInfinitelyRecurse) {
-  Value globalString = rt.global().getProperty(rt, "String");
-  rt.global().setProperty(rt, "String", Value::undefined());
-  try {
-    eval("throw Error('whoops')");
-    FAIL() << "expected exception";
-  } catch (const JSError& ex) {
-    EXPECT_EQ(
-        ex.getMessage(),
-        "[Exception while creating message string: callGlobalFunction: "
-        "JS global property 'String' is undefined, expected a Function]");
-  }
-  rt.global().setProperty(rt, "String", globalString);
-
   Value globalError = rt.global().getProperty(rt, "Error");
   rt.global().setProperty(rt, "Error", Value::undefined());
   try {
@@ -1270,6 +1257,70 @@ TEST_P(JSITest, SymbolTest) {
   EXPECT_TRUE(Value::strictEquals(
       rt, eval("Symbol.for('a')"), eval("Symbol.for('a')")));
   EXPECT_FALSE(Value::strictEquals(rt, eval("Symbol('a')"), eval("'a'")));
+}
+
+TEST_P(JSITest, JSErrorTest) {
+  // JSError creation can lead to further errors.  Make sure these
+  // cases are handled and don't cause weird crashes or other issues.
+  //
+  // Getting message property can throw
+
+  EXPECT_THROW(
+      eval("var GetMessageThrows = {get message() { throw Error('ex'); }};"
+           "throw GetMessageThrows;"),
+      JSIException);
+
+  EXPECT_THROW(
+      eval("var GetMessageThrows = {get message() { throw GetMessageThrows; }};"
+           "throw GetMessageThrows;"),
+      JSIException);
+
+  // Converting exception message to String can throw
+
+  EXPECT_THROW(
+      eval(
+          "Object.defineProperty("
+          "  globalThis, 'String', {configurable:true, get() { var e = Error(); e.message = 23; throw e; }});"
+          "var e = Error();"
+          "e.message = 17;"
+          "throw e;"),
+      JSIException);
+
+  EXPECT_THROW(
+      eval(
+          "var e = Error();"
+          "Object.defineProperty("
+          "  e, 'message', {configurable:true, get() { throw Error('getter'); }});"
+          "throw e;"),
+      JSIException);
+
+  EXPECT_THROW(
+      eval("var e = Error();"
+           "String = function() { throw Error('ctor'); };"
+           "throw e;"),
+      JSIException);
+
+  // Converting an exception message to String can return a non-String
+
+  EXPECT_THROW(
+      eval("String = function() { return 42; };"
+           "var e = Error();"
+           "e.message = 17;"
+           "throw e;"),
+      JSIException);
+
+  // Exception can be non-Object
+
+  EXPECT_THROW(eval("throw 17;"), JSIException);
+
+  EXPECT_THROW(eval("throw undefined;"), JSIException);
+
+  // Converting exception with no message or stack property to String can throw
+
+  EXPECT_THROW(
+      eval("var e = {toString() { throw new Error('errstr'); }};"
+           "throw e;"),
+      JSIException);
 }
 
 //----------------------------------------------------------------------
