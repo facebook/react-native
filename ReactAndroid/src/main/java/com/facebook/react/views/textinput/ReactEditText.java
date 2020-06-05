677a78923edaf589fe07f4a5b618d26fc40734b2
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-06-05 16:06:24 +0200

    fix capitalize characters and position cursor

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
@@ -506,7 +506,10 @@ public class ReactEditText extends AppCompatEditText {
       // When we update text, we trigger onChangeText code that will
       // try to update state if the wrapper is available. Temporarily disable
       // to prevent an infinite loop.
-      getText().replace(0, length(), spannableStringBuilder);
+      Integer currentPosition = getSelectionStart();
+      getText().clear();
+      append(spannableStringBuilder);
+      setSelection(currentPosition);
     }
     mDisableTextDiffing = false;
 
