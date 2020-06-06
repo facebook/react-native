27b948fa3aa0efc06b27f0a5204d81b8304f2ef1
Website Deployment Script <reactjs-bot@users.noreply.github.com>
2020-06-06 08:07:36 +0000

    Deploy website
    Deploy website version based on c1e141e31c0e4b8b685766f61768327d822664fc

--- js/announcement.js
+++ js/announcement.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   const container = document.querySelector('.fixedHeaderContainer');
   if (container) {
-    const div = document.createElement('div');
-    div.innerHTML =
-      '<div class="announcement"><div class="announcement-inner">Black Lives Matter. <a href="https://support.eji.org/give/153413/#!/donation/checkout">Support the Equal Justice Initiative</a>.</div></div>';
-    const content = div.childNodes[0];
-    container.insertBefore(content, container.childNodes[0].nextSibling);
+    const announcement = document.createElement('div');
+    announcement.className = 'announcement';
+    announcement.innerHTML =
+      '<div class="announcement-inner">Black Lives Matter. <a href="https://support.eji.org/give/153413/#!/donation/checkout">Support the Equal Justice Initiative.</a></div>';
+    container.insertBefore(announcement, container.firstChild);
   }
 });
