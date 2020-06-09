fa5d3fb6b8c561b749f3e86316d06543a52dcc1c
Joshua Gross <joshuagross@fb.com>
2020-06-09 16:30:56 -0700

    ScrollViewStickyHeader: update position (translateY) in Fabric ShadowTree when scrolling stops
    Summary:
    In Fabric, some uses of the ScrollViewStickyHeader don't work after scrolling because even though the UI correctly reflects the translateY that the StickyHeader should be at, the underlying C++ Fabric ShadowTree doesn't have the updated parameters.
    
    1. We add a mechanism to pass static props through to animated nodes; these get passed to the platform through the normal commit-diff process. This is to allow passing props to the platform that are also controlled by the animation. This mechanism could be reused elsewhere.
    2. In ScrollViewStickyHeader, listen to updates for the translateY value and pass them to the platform when it stops changing - for Fabric only. This noops for non-Fabric since it's not necessary.
    
    Changelog: [Internal]
    
    Reviewed By: mdvacca
    
    Differential Revision: D21948830
    
    fbshipit-source-id: b203ecde466732203dd12a86e2339e81f66b27e7

--- Libraries/Components/ScrollView/ScrollView.js
+++ Libraries/Components/ScrollView/ScrollView.js
@@ -1078,6 +1078,7 @@ class ScrollView extends React.Component<Props, State> {
           return (
             <StickyHeaderComponent
               key={key}
+              nativeID={'StickyHeader-' + key}
               ref={ref => this._setStickyHeaderRef(key, ref)}
               nextHeaderLayoutY={this._headerLayoutYs.get(
                 this._getKeyForIndex(nextIndex, childArray),
