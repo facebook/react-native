48e95cae9ed1a0cd7f72496d85c25ee36de34ee8
Brent Vatne <brentvatne@gmail.com>
2020-06-16 17:50:07 -0700

    [ReactAndroid] Respect local night mode configuration for activity to play nicely with Expo client color scheme locking mechanism. https://github.com/expo/expo/pull/8793
    This is a squashed commits with the following changes:
    
    commit 945096e54ff9f5d9f5639cea201f101efac472a9
    Author: Kudo Chien <kudo@expo.io>
    Date:   Tue Jul 27 16:49:09 2021 +0800
    
        [android] Upgrade androidx.appcompat to 1.2.0
    
        Backport from: https://github.com/expo/expo/commit/58fa52e3e3e29b7a852a814b2d5bb54f4a8e1065

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/appearance/AppearanceModule.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/appearance/AppearanceModule.java
@@ -18,6 +18,9 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 /** Module that exposes the user's preferred color scheme. */
 @ReactModule(name = NativeAppearanceSpec.NAME)
 public class AppearanceModule extends NativeAppearanceSpec {
@@ -50,9 +53,22 @@ public class AppearanceModule extends NativeAppearanceSpec {
     mColorScheme = colorSchemeForCurrentConfiguration(reactContext);
   }
 
+  // NOTE(brentvatne): this was static previously, but it wasn't necessary and we need it to not be
+  // in order to use getCurrentActivity
   private String colorSchemeForCurrentConfiguration(Context context) {
     if (mOverrideColorScheme != null) {
       return mOverrideColorScheme.getScheme();
+    }
+    // NOTE(brentvatne): Same code (roughly) that we use in ExpoAppearanceModule to get the config
+    // as set by ExperienceActivityUtils to force the dark/light mode config on the activity
+    if (getCurrentActivity() instanceof AppCompatActivity) {
+      int mode = ((AppCompatActivity) getCurrentActivity()).getDelegate().getLocalNightMode();
+      switch (mode) {
+        case AppCompatDelegate.MODE_NIGHT_YES:
+          return "dark";
+        case AppCompatDelegate.MODE_NIGHT_NO:
+          return "light";
+      }
     }
     int currentNightMode =
         context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
