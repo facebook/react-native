912f7852970ab150004d3eca9689508056ed286c
Anandraj <anandrag@microsoft.com>
2020-06-19 13:06:03 -0700

    Ensure jsinpector is loaded before react native

--- android-patches/patches-0.62.2/BasicBuild/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
+++ android-patches/patches-0.62.2/BasicBuild/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
@@ -1,6 +1,6 @@
 --- /home/mganandraj/code/react-native-0.62.2/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java	2020-06-15 22:45:58.585487954 -0700
 +++ /home/mganandraj/code/react-native-macos/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java	2020-06-15 23:32:29.375721209 -0700
-@@ -31,6 +31,13 @@
+@@ -31,6 +31,14 @@
      Systrace.beginSection(
          TRACE_TAG_REACT_JAVA_BRIDGE, "ReactBridge.staticInit::load:reactnativejni");
      ReactMarker.logMarker(ReactMarkerConstants.LOAD_REACT_NATIVE_SO_FILE_START);
@@ -10,6 +10,7 @@
 +    SoLoader.loadLibrary("fb");
 +    SoLoader.loadLibrary("fbjni");
 +    SoLoader.loadLibrary("yoga");
++    SoLoader.loadLibrary("jsinspector");
 +
      SoLoader.loadLibrary("reactnativejni");
      ReactMarker.logMarker(ReactMarkerConstants.LOAD_REACT_NATIVE_SO_FILE_END);
