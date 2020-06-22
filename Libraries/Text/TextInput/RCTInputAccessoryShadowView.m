9a0c03f235fb5a0affdcb4472cd91803d2568489
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-06-22 14:20:39 +0900

    fix: remove fixed widt for InputAccessoryView

--- Libraries/Text/TextInput/RCTInputAccessoryShadowView.m
+++ Libraries/Text/TextInput/RCTInputAccessoryShadowView.m
@@ -14,7 +14,6 @@
 - (void)insertReactSubview:(RCTShadowView *)subview atIndex:(NSInteger)atIndex
 {
   [super insertReactSubview:subview atIndex:atIndex];
-  subview.width = (YGValue) { RCTScreenSize().width, YGUnitPoint };
 }
 
 @end
