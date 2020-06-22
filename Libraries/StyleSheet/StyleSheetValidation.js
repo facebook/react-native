aaff5eb99c58f63860504c72d93f40d8284ffa12
Nick Gerleman <ngerlem@microsoft.com>
2020-06-22 07:48:24 -0700

    PR feedback

--- Libraries/StyleSheet/StyleSheetValidation.js
+++ Libraries/StyleSheet/StyleSheetValidation.js
@@ -51,14 +51,16 @@ class StyleSheetValidation {
     if (!__DEV__ || global.__RCTProfileIsProfiling) {
       return;
     }
-    for (const prop in styles[name]) {
-      if (styles[name].hasOwnProperty(prop)) {
-        StyleSheetValidation.validateStyleProp(
-          prop,
-          styles[name],
-          'StyleSheet ' + name,
-        );
-      }
+    if (!styles[name]) {
+      return;
+    }
+    const styleProps = Object.keys(styles[name]);
+    for (const prop of styleProps) {
+      StyleSheetValidation.validateStyleProp(
+        prop,
+        styles[name],
+        'StyleSheet ' + name,
+      );
     }
   }
 
