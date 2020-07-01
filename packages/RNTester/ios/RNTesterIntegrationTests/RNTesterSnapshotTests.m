df92a42c6c323d54873cca9974d72ac5723962c4
Jani Eväkallio <jani.evakallio@gmail.com>
2020-07-01 09:21:35 +0100

    Re-add RNTester native iOS code

---
+++ packages/RNTester/ios/RNTesterIntegrationTests/RNTesterSnapshotTests.m
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <RCTTest/RCTTestRunner.h>
+
+@interface RNTesterSnapshotTests : XCTestCase {
+  RCTTestRunner *_runner;
+}
+
+@end
+
+@implementation RNTesterSnapshotTests
+
+- (void)setUp
+{
+  _runner = RCTInitRunnerForApp(@"RNTester/js/RNTesterApp.ios", nil, nil);
+  if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 10) {
+    _runner.testSuffix = [NSString stringWithFormat:@"-iOS%d", UIDevice.currentDevice.systemVersion.intValue];
+  }
+  _runner.recordMode = NO;
+}
+
+#define RCT_TEST(name)                     \
+  -(void)test##name                        \
+  {                                        \
+    [_runner runTest:_cmd module:@ #name]; \
+  }
+
+RCT_TEST(ViewExample)
+RCT_TEST(LayoutExample)
+RCT_TEST(ScrollViewExample)
+RCT_TEST(TextExample)
+#if !TARGET_OS_TV
+// No switch available on tvOS
+RCT_TEST(SwitchExample)
+#endif
+
+- (void)testZZZNotInRecordMode
+{
+  XCTAssertFalse(_runner.recordMode, @"Don't forget to turn record mode back to off");
+}
+
+@end
