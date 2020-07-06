36eb58e7dadf84c605d9ceccf2f908fb60b56671
Website Deployment Script <reactjs-bot@users.noreply.github.com>
2020-07-06 17:50:04 +0000

    Deploy website
    Deploy website version based on c42f606b1aecd8fed559a83f80e30ae8908070e6

--- js/announcement.js
+++ js/announcement.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.fixedHeaderContainer');
   if (container) {
     const announcement = document.createElement('div');
@@ -8,3 +8,28 @@ document.addEventListener('DOMContentLoaded', function() {
     container.insertBefore(announcement, container.firstChild);
   }
 });
+
+const scrollStartBoundary = 160;
+
+window.onscroll = () => {
+  // Simple check for mobile devices
+  if (typeof window.orientation !== 'undefined') {
+    const fixedHeaderContainer = document.querySelector(
+      '.fixedHeaderContainer'
+    );
+    const navPusher = document.querySelector('.navPusher');
+
+    if (
+      document.body.scrollTop > scrollStartBoundary ||
+      document.documentElement.scrollTop > scrollStartBoundary
+    ) {
+      fixedHeaderContainer.style.top = '-100px';
+      navPusher.style.top = '-100px';
+      navPusher.style.marginBottom = '-100px';
+    } else {
+      fixedHeaderContainer.style.top = '0';
+      navPusher.style.top = '0';
+      navPusher.style.marginBottom = '0';
+    }
+  }
+};
