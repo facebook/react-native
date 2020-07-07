b6d966f7deea493d50aca72cbe2111ecb40756b1
Mohamed Shaban <mohamed.shaban@urbansportsclub.com>
2020-07-07 11:06:35 +0200

    add default value for animated property

--- Libraries/Components/StatusBar/StatusBar.js
+++ Libraries/Components/StatusBar/StatusBar.js
@@ -473,7 +473,7 @@ class StatusBar extends React.Component<Props> {
           );
           NativeStatusBarManagerAndroid.setColor(
             processedColor,
-            mergedProps.backgroundColor.animated,
+            mergedProps.backgroundColor.animated || false,
           );
         }
         if (!oldProps || oldProps.hidden.value !== mergedProps.hidden.value) {
