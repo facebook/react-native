b6ded7261544c703e82e8dbfa442dad4b201d428
Tom Cheung <cheungch@gmail.com>
2020-07-19 21:33:10 -0700

    Fix image cannot show in iOS 14 (#29420)
    Summary:
    This PR is to fix https://github.com/facebook/react-native/issues/29279, which image cannot show in iOS 14
    As https://github.com/facebook/react-native/issues/29279#issuecomment-658244428 mention, this issue can be fixed by calling ` [super displayLayer:layer];` it it is still image, to let `UIImageView` handle still image rendering
    
    ## Changelog
    
    [iOS] [Fixed] - Fix image cannot show in iOS 14
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/29420
    
    Test Plan:
    Image can be shown in iOS 14 build with Xcode 12 beta, using
    ```js
    <Image source={require('./images/some_local_image.jpg')}/>
    ```
    
    It may also need to test gif image is render correctly
    ```js
    <Image source={{uri: 'https://some_remote_gif_image.gif'}}/>
    ```
    
    Reviewed By: p-sun
    
    Differential Revision: D22619448
    
    Pulled By: shergin
    
    fbshipit-source-id: f4d0ad83af945a6b8099d4eaea5a5f1933c7bfd2

--- Libraries/Image/RCTUIImageViewAnimated.m
+++ Libraries/Image/RCTUIImageViewAnimated.m
@@ -275,6 +275,8 @@ static NSUInteger RCTDeviceFreeMemory() {
   if (_currentFrame) {
     layer.contentsScale = self.animatedImageScale;
     layer.contents = (__bridge id)_currentFrame.CGImage;
+  } else {
+    [super displayLayer:layer];
   }
 }
 
