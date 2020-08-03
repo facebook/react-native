811e86d6ff80dbcbca2c03fcdeaecb9001dc8ab3
Website Deployment Script <reactjs-bot@users.noreply.github.com>
2020-08-03 14:13:07 +0000

    Deploy website
    Deploy website version based on 1005afe8e08cf2699731dd7996a95f319ff85ad1

--- js/codetabs.js
+++ js/codetabs.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -7,19 +7,19 @@
 // Turn off ESLint for this file because it's sent down to users as-is.
 
 /* eslint-disable */
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   // add event listener for all tab
-  document.querySelectorAll('.nav-link').forEach(function(el) {
-    el.addEventListener('click', function(e) {
+  document.querySelectorAll('.nav-link').forEach(function (el) {
+    el.addEventListener('click', function (e) {
       var groupId = e.target.getAttribute('data-group');
       document
         .querySelectorAll('.nav-link[data-group='.concat(groupId, ']'))
-        .forEach(function(el) {
+        .forEach(function (el) {
           el.classList.remove('active');
         });
       document
         .querySelectorAll('.tab-pane[data-group='.concat(groupId, ']'))
-        .forEach(function(el) {
+        .forEach(function (el) {
           el.classList.remove('active');
         });
       e.target.classList.add('active');
--- js/scrollSpy.js
+++ js/scrollSpy.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -21,7 +21,7 @@
       return;
     }
 
-    timer = setTimeout(function() {
+    timer = setTimeout(function () {
       timer = null;
       var activeNavFound = false;
       var headings = findHeadings(); // toc nav anchors
@@ -75,7 +75,7 @@
 
   document.addEventListener('scroll', onScroll);
   document.addEventListener('resize', onScroll);
-  document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function () {
     // Cache the headings once the page has fully loaded.
     headingsCache = findHeadings();
     onScroll();
