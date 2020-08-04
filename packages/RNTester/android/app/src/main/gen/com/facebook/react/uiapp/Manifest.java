60252f5fefda009fc44f0a72cf92d8d659bbf74d
Sanskar <sansyrox@gmail.com>
2020-08-04 10:06:12 +0530

    Android migrated

---
+++ packages/RNTester/android/app/src/main/gen/com/facebook/react/uiapp/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.facebook.react.uiapp;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
