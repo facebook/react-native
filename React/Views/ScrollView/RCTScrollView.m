1b0fb9bead4d158d14df5a994423d06716b5e377
Yogev Ben David <yogev132@gmail.com>
2020-08-05 11:48:36 -0700

    iOS: Fix refreshControl layouting (#28236)
    Summary:
    In `react-native-navigation` we allow the usage of native iOS navigationBar **largeTitle** which cause the title to "jump" when pulling to refresh.
    We found that the layout calculations of the refreshControl element mess up the system behaviour.
    
    ## Changelog
    
    [iOS] [Fixed] - Fix refreshControl messes up navigationBar largeTitles
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/28236
    
    Test Plan:
    ### Before the fix:
    ![before](https://user-images.githubusercontent.com/10794586/75991307-f7c7ec00-5efe-11ea-8cd9-ab8c3fbe1dc1.gif)
    
    ### And after:
    ![after](https://user-images.githubusercontent.com/10794586/75990618-d9152580-5efd-11ea-8c72-5deb6d83a840.gif)
    
    ### How it looks like with react-native init app after the fix:
    ![ezgif com-video-to-gif (4)](https://user-images.githubusercontent.com/10794586/77253369-54970680-6c62-11ea-9ad6-3265e23044e6.gif)
    
    Reviewed By: sammy-SC
    
    Differential Revision: D22782680
    
    Pulled By: PeteTheHeat
    
    fbshipit-source-id: f86ccd0a6ad492312029a69b392cd525450fe594

--- React/Views/ScrollView/RCTScrollView.m
+++ React/Views/ScrollView/RCTScrollView.m
@@ -230,7 +230,15 @@
     [_customRefreshControl removeFromSuperview];
   }
   _customRefreshControl = refreshControl;
-  [self addSubview:_customRefreshControl];
+  // We have to set this because we can't always guarantee the
+  // `RCTCustomRefreshContolProtocol`'s superview will always be of class
+  // `UIScrollView` like we were previously
+  _customRefreshControl.scrollView = self;
+  if ([refreshControl isKindOfClass:UIRefreshControl.class]) {
+    self.refreshControl = (UIRefreshControl *)refreshControl;
+  } else {
+    [self addSubview:_customRefreshControl];
+  }
 }
 
 - (void)setPinchGestureEnabled:(BOOL)pinchGestureEnabled
@@ -421,7 +429,7 @@ static inline void RCTApplyTransformationAccordingLayoutDirection(
 #if !TARGET_OS_TV
   // Adjust the refresh control frame if the scrollview layout changes.
   UIView<RCTCustomRefreshContolProtocol> *refreshControl = _scrollView.customRefreshControl;
-  if (refreshControl && refreshControl.isRefreshing) {
+  if (refreshControl && refreshControl.isRefreshing && ![refreshControl isKindOfClass:UIRefreshControl.class]) {
     refreshControl.frame =
         (CGRect){_scrollView.contentOffset, {_scrollView.frame.size.width, refreshControl.frame.size.height}};
   }
