5c9c52275f7a6f2579dccad120fc2c1486ebebe1
David Vacca <dvacca@fb.com>
2020-08-06 11:50:03 -0700

    Extend 'attributedstring' module to compile in OSS
    Summary:
    This diff extends the 'attributedstring' module to compile in OSS
    
    changelog: [internal] internal
    
    Reviewed By: JoshuaGross
    
    Differential Revision: D22918207
    
    fbshipit-source-id: 35710789d2aa71826e10c29c27e9ac34b73e5344

--- ReactCommon/react/renderer/attributedstring/Android.mk
+++ ReactCommon/react/renderer/attributedstring/Android.mk
@@ -7,7 +7,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := react_render_mounting
+LOCAL_MODULE := react_render_attributedstring
 
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.cpp)
 
@@ -21,7 +21,7 @@ LOCAL_CFLAGS += -fexceptions -frtti -std=c++14 -Wall
 
 LOCAL_STATIC_LIBRARIES :=
 
-LOCAL_SHARED_LIBRARIES := libbetter libyoga libfolly_futures glog libfolly_json libglog_init libreact_render_core libreact_render_debug libreact_render_components_view libreact_utils
+LOCAL_SHARED_LIBRARIES := libbetter libreact_render_graphics libyoga libfolly_futures glog libfolly_json libglog_init libreact_render_core libreact_render_debug libreact_render_components_view libreact_utils
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -33,5 +33,6 @@ $(call import-module,react/renderer/components/root)
 $(call import-module,react/renderer/components/view)
 $(call import-module,react/renderer/core)
 $(call import-module,react/renderer/debug)
+$(call import-module,react/renderer/graphics)
 $(call import-module,react/utils)
 $(call import-module,yogajni)
