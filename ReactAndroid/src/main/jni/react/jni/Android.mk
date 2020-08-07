6486b8cffde14f5c2a3314b5cbd1c13901e94e8e
David Vacca <dvacca@fb.com>
2020-08-07 11:09:38 -0700

    Extend 'scheduler' module to compile in OSS
    Summary:
    This diff extends the 'scheduler' module to compile in OSS
    
    changelog: [internal] internal
    
    Reviewed By: JoshuaGross
    
    Differential Revision: D22963705
    
    fbshipit-source-id: ea2b6a4fce49c4d9552deb30e89fcba165cfe772

--- ReactAndroid/src/main/jni/react/jni/Android.mk
+++ ReactAndroid/src/main/jni/react/jni/Android.mk
@@ -22,7 +22,7 @@ LOCAL_CFLAGS += -fexceptions -frtti -Wno-unused-lambda-capture
 LOCAL_LDLIBS += -landroid
 
 # The dynamic libraries (.so files) that this module depends on.
-LOCAL_SHARED_LIBRARIES := libfolly_json libfb libfbjni libglog_init libyoga libreact_utils libreact_render_debug libreact_render_graphics libreact_render_core libreact_render_mapbuffer react_render_componentregistry libreact_render_components_view libreact_render_components_view libreact_render_components_unimplementedview libreact_render_components_root libreact_render_components_scrollview libbetter libreact_render_attributedstring libreact_render_uimanager libreact_render_templateprocessor
+LOCAL_SHARED_LIBRARIES := libfolly_json libfb libfbjni libglog_init libyoga libreact_utils libreact_render_debug libreact_render_graphics libreact_render_core libreact_render_mapbuffer react_render_componentregistry libreact_render_components_view libreact_render_components_view libreact_render_components_unimplementedview libreact_render_components_root libreact_render_components_scrollview libbetter libreact_render_attributedstring libreact_render_uimanager libreact_render_templateprocessor libreact_render_scheduler
 
 # The static libraries (.a files) that this module depends on.
 LOCAL_STATIC_LIBRARIES := libreactnative libcallinvokerholder libruntimeexecutor
@@ -87,6 +87,7 @@ $(call import-module,react/renderer/debug)
 $(call import-module,react/renderer/graphics)
 $(call import-module,react/renderer/mapbuffer)
 $(call import-module,react/renderer/mounting)
+$(call import-module,react/renderer/scheduler)
 $(call import-module,react/renderer/templateprocessor)
 $(call import-module,react/renderer/uimanager)
 
