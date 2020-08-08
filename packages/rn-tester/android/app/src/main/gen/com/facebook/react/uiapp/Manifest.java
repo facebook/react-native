fafe4c9e5e1f3789a0e6b87332b3c090e256a5b9
stealthanthrax <sansyrox@gmail.com>
2020-08-09 00:43:40 +0530

    Removed autogenerated files

--- packages/rn-tester/android/app/src/main/gen/com/facebook/react/uiapp/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.facebook.react.uiapp;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
