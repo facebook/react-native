8191023fb55c1fe740a9b783ee06ac58beb43a08
luism3861 <luism3861@gmail.com>
2020-08-14 17:16:39 -0500

    remove accessibilityState validAttributes config from ReactNativeViewViewConfig.js

--- Libraries/Components/View/ReactNativeViewViewConfig.js
+++ Libraries/Components/View/ReactNativeViewViewConfig.js
@@ -122,7 +122,6 @@ const ReactNativeViewConfig = {
     accessibilityLabel: true,
     accessibilityLiveRegion: true,
     accessibilityRole: true,
-    accessibilityStates: true, // TODO: Can be removed after next release
     accessibilityState: true,
     accessibilityValue: true,
     accessibilityViewIsModal: true,
