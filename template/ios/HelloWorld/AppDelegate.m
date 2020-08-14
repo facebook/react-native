6fa8de2b5c52051f2f4fbe372cef417e8a8f5086
Jason Safaiyeh <safaiyeh@protonmail.com>
2020-08-13 23:43:42 -0700

    Update AppDelegate.m

--- template/ios/HelloWorld/AppDelegate.m
+++ template/ios/HelloWorld/AppDelegate.m
@@ -4,6 +4,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#if DEBUG
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -22,13 +23,16 @@ static void InitializeFlipper(UIApplication *application) {
   [client start];
 }
 #endif
+#endif
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+#if DEBUG
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
+#endif
 #endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
