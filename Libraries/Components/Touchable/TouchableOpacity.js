d65e254d4b2f1005de3b86c7c47acc8cef257838
Ayush Jain <ayushjainrksh@gmail.com>
2020-08-14 23:36:03 +0530

    Update the TouchableOpacity component
    * Update the title and prop description from docs.
    * Remove asterisks from the comments.
    * Add snackplayer example.
    * Cap comments to 80 characters.
    * Add platform, default and type annotations.

--- Libraries/Components/Touchable/TouchableOpacity.js
+++ Libraries/Components/Touchable/TouchableOpacity.js
@@ -6,6 +6,7 @@
  *
  * @flow strict-local
  * @format
+ * @generate-docs
  */
 
 'use strict';
@@ -24,11 +25,42 @@ import Platform from '../../Utilities/Platform';
 import * as React from 'react';
 
 type TVProps = $ReadOnly<{|
+  /**
+    (Apple TV only) TV preferred focus (see documentation for the View
+    component).
+
+    @platform ios
+   */
   hasTVPreferredFocus?: ?boolean,
+  /**
+    TV next focus down (see documentation for the View component).
+
+    @platform android
+   */
   nextFocusDown?: ?number,
+  /**
+    TV next focus forward (see documentation for the View component).
+
+    @platform android
+   */
   nextFocusForward?: ?number,
+  /**
+    TV next focus left (see documentation for the View component).
+
+    @platform android
+   */
   nextFocusLeft?: ?number,
+  /**
+    TV next focus right (see documentation for the View component).
+
+    @platform android
+   */
   nextFocusRight?: ?number,
+  /**
+    TV next focus up (see documentation for the View component).
+
+    @platform android
+   */
   nextFocusUp?: ?number,
 |}>;
 
@@ -36,7 +68,16 @@ type Props = $ReadOnly<{|
   ...React.ElementConfig<TouchableWithoutFeedback>,
   ...TVProps,
 
+  /**
+    Determines what the opacity of the wrapped view should be when touch is
+    active.
+
+    @default 0.2
+   */
   activeOpacity?: ?number,
+  /**
+    @type View.style
+   */
   style?: ?ViewStyleProp,
 
   hostRef: React.Ref<typeof Animated.View>,
@@ -48,88 +89,111 @@ type State = $ReadOnly<{|
 |}>;
 
 /**
- * A wrapper for making views respond properly to touches.
- * On press down, the opacity of the wrapped view is decreased, dimming it.
- *
- * Opacity is controlled by wrapping the children in an Animated.View, which is
- * added to the view hierarchy.  Be aware that this can affect layout.
- *
- * Example:
- *
- * ```
- * renderButton: function() {
- *   return (
- *     <TouchableOpacity onPress={this._onPressButton}>
- *       <Image
- *         style={styles.button}
- *         source={require('./myButton.png')}
- *       />
- *     </TouchableOpacity>
- *   );
- * },
- * ```
- * ### Example
- *
- * ```ReactNativeWebPlayer
- * import React, { Component } from 'react'
- * import {
- *   AppRegistry,
- *   StyleSheet,
- *   TouchableOpacity,
- *   Text,
- *   View,
- * } from 'react-native'
- *
- * class App extends Component {
- *   state = { count: 0 }
- *
- *   onPress = () => {
- *     this.setState(state => ({
- *       count: state.count + 1
- *     }));
- *   };
- *
- *  render() {
- *    return (
- *      <View style={styles.container}>
- *        <TouchableOpacity
- *          style={styles.button}
- *          onPress={this.onPress}>
- *          <Text> Touch Here </Text>
- *        </TouchableOpacity>
- *        <View style={[styles.countContainer]}>
- *          <Text style={[styles.countText]}>
- *             { this.state.count !== 0 ? this.state.count: null}
- *           </Text>
- *         </View>
- *       </View>
- *     )
- *   }
- * }
- *
- * const styles = StyleSheet.create({
- *   container: {
- *     flex: 1,
- *     justifyContent: 'center',
- *     paddingHorizontal: 10
- *   },
- *   button: {
- *     alignItems: 'center',
- *     backgroundColor: '#DDDDDD',
- *     padding: 10
- *   },
- *   countContainer: {
- *     alignItems: 'center',
- *     padding: 10
- *   },
- *   countText: {
- *     color: '#FF00FF'
- *   }
- * })
- *
- * AppRegistry.registerComponent('App', () => App)
- * ```
- *
+  > If you're looking for a more extensive and future-proof way to handle
+  > touch-based input, check out the [Pressable](pressable.md) API.
+
+  A wrapper for making views respond properly to touches. On press down, the
+  opacity of the wrapped view is decreased, dimming it.
+
+  Opacity is controlled by wrapping the children in an `Animated.View`, which is
+  added to the view hierarchy. Be aware that this can affect layout.
+
+  ```SnackPlayer name=TouchableOpacity%20Function%20Component%20Example
+  import React, { useState } from "react";
+  import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+  const App = () => {
+    const [count, setCount] = useState(0);
+    const onPress = () => setCount(prevCount => prevCount + 1);
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.countContainer}>
+          <Text>Count: {count}</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={onPress}
+        >
+          <Text>Press Here</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      paddingHorizontal: 10
+    },
+    button: {
+      alignItems: "center",
+      backgroundColor: "#DDDDDD",
+      padding: 10
+    },
+    countContainer: {
+      alignItems: "center",
+      padding: 10
+    }
+  });
+
+  export default App;
+  ```
+
+  ```SnackPlayer name=TouchableOpacity%20Class%20Component%20Example
+  import React, { Component } from "react";
+  import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { count: 0 };
+    }
+
+    onPress = () => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    };
+
+    render() {
+      const { count } = this.state;
+      return (
+        <View style={styles.container}>
+          <View style={styles.countContainer}>
+            <Text>Count: {count}</Text>
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.onPress}
+          >
+            <Text>Press Here</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      paddingHorizontal: 10
+    },
+    button: {
+      alignItems: "center",
+      backgroundColor: "#DDDDDD",
+      padding: 10
+    },
+    countContainer: {
+      alignItems: "center",
+      padding: 10
+    }
+  });
+
+  export default App;
+  ```
  */
 class TouchableOpacity extends React.Component<Props, State> {
   _tvTouchable: ?TVTouchable;
@@ -187,7 +251,11 @@ class TouchableOpacity extends React.Component<Props, State> {
   }
 
   /**
-   * Animate the touchable to a new opacity.
+    ```jsx
+    setOpacityTo((value: number), (duration: number));
+    ```
+
+    Animate the touchable to a new opacity.
    */
   _setOpacityTo(toValue: number, duration: number): void {
     Animated.timing(this.state.anim, {
