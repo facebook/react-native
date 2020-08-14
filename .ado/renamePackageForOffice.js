70c9da999261e2cba54abd560e1f6532c0ea071c
Andrew Coates <acoates-ms@noreply.github.com>
2020-08-14 12:51:11 -0700

    renamePackageForOffice is run before install, move the function so it does require semver

--- .ado/renamePackageForOffice.js
+++ .ado/renamePackageForOffice.js
@@ -1,3 +1,12 @@
 // @ts-check
-const {updatePackageName} = require('./versionUtils');
+const {pkgJsonPath} = require('./versionUtils');
+const fs = require("fs");
+
+function updatePackageName(name) {
+    let pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, "utf8"));
+    pkgJson.name = name;
+    fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
+    console.log(`Updating package.json to name ${name}`);
+}
+
 updatePackageName('react-native');
--- .ado/versionUtils.js
+++ .ado/versionUtils.js
@@ -47,17 +47,9 @@ function updateVersionsInFiles(patchVersionPrefix) {
     return {releaseVersion, branchVersionSuffix};
 }
 
-function updatePackageName(name) {
-    let pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, "utf8"));
-    pkgJson.name = name;
-    fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
-    console.log(`Updating package.json to name ${name}`);
-}
-
 module.exports = {
     gatherVersionInfo,
     publishBranchName,
     pkgJsonPath,
-    updateVersionsInFiles,
-    updatePackageName
+    updateVersionsInFiles
 }
