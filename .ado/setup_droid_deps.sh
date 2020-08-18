3b1af6c7448979c5eb22a564fcd5af35995ef02a
Anandraj <anandrag@microsoft.com>
2020-08-17 18:38:10 -0700

    A couple of minor changes for clean build

--- .ado/setup_droid_deps.sh
+++ .ado/setup_droid_deps.sh
@@ -7,3 +7,5 @@ mkdir $BUILD_DEPS_DIR
 
 mkdir $BUILD_DEPS_DIR/boost_1_68_0
 ln -s "$PWD/ReactAndroid/packages/boost.1.68.0.0/lib/native/include/boost" "$BUILD_DEPS_DIR/boost_1_68_0/boost"
+
+# export REACT_NATIVE_BOOST_PATH=$BUILD_DEPS_DIR
---
+++ android-patches/build.sh
@@ -0,0 +1,13 @@
+node ./android-patches/bundle/bundle.js patch . BasicBuild RNHostBase V8Integration Focus --patch-store ./android-patches/patches-0.62.2 --log-folder ./android-patches/logs --confirm true
+
+mkdir ./nuget-bin/ && curl -o ./nuget-bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+
+chmod -R +r .
+
+mono ./nuget-bin/nuget.exe restore ./ReactAndroid/packages.config -PackagesDirectory ./ReactAndroid/packages/ -Verbosity Detailed -NonInteractive
+
+npm install
+
+chmod +x .ado/setup_droid_deps.sh && .ado/setup_droid_deps.sh
+
+export REACT_NATIVE_BOOST_PATH=~/code/rn-macos-fb62merge/build_deps/boost_1_68_0/boost
