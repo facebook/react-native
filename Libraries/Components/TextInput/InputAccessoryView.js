05dd6b13596acab91921c0c61623939d06ac47cb
Ayush Jain <ayushjainrksh@gmail.com>
2020-08-20 00:26:49 +0530

    Update the InputAccessoryView component
    * Update the title and prop description from docs.
    * Remove asterisks from the comments.
    * Cap comments to 80 characters.
    * Add type annotations.
    * Use reference style link.

--- Libraries/Components/TextInput/InputAccessoryView.js
+++ Libraries/Components/TextInput/InputAccessoryView.js
@@ -6,6 +6,7 @@
  *
  * @flow
  * @format
+ * @generate-docs
  */
 
 'use strict';
@@ -19,75 +20,71 @@ import RCTInputAccessoryViewNativeComponent from './RCTInputAccessoryViewNativeC
 import type {ViewStyleProp} from '../../StyleSheet/StyleSheet';
 import type {ColorValue} from '../../StyleSheet/StyleSheet';
 
-/**
- * Note: iOS only
- *
- * A component which enables customization of the keyboard input accessory view.
- * The input accessory view is displayed above the keyboard whenever a TextInput
- * has focus. This component can be used to create custom toolbars.
- *
- * To use this component wrap your custom toolbar with the
- * InputAccessoryView component, and set a nativeID. Then, pass that nativeID
- * as the inputAccessoryViewID of whatever TextInput you desire. A simple
- * example:
- *
- * ```ReactNativeWebPlayer
- * import React, { Component } from 'react';
- * import { AppRegistry, TextInput, InputAccessoryView, Button } from 'react-native';
- *
- * export default class UselessTextInput extends Component {
- *   constructor(props) {
- *     super(props);
- *     this.state = {text: 'Placeholder Text'};
- *   }
- *
- *   render() {
- *     const inputAccessoryViewID = "uniqueID";
- *     return (
- *       <View>
- *         <ScrollView keyboardDismissMode="interactive">
- *           <TextInput
- *             style={{
- *               padding: 10,
- *               paddingTop: 50,
- *             }}
- *             inputAccessoryViewID=inputAccessoryViewID
- *             onChangeText={text => this.setState({text})}
- *             value={this.state.text}
- *           />
- *         </ScrollView>
- *         <InputAccessoryView nativeID=inputAccessoryViewID>
- *           <Button
- *             onPress={() => this.setState({text: 'Placeholder Text'})}
- *             title="Reset Text"
- *           />
- *         </InputAccessoryView>
- *       </View>
- *     );
- *   }
- * }
- *
- * // skip this line if using Create React Native App
- * AppRegistry.registerComponent('AwesomeProject', () => UselessTextInput);
- * ```
- *
- * This component can also be used to create sticky text inputs (text inputs
- * which are anchored to the top of the keyboard). To do this, wrap a
- * TextInput with the InputAccessoryView component, and don't set a nativeID.
- * For an example, look at InputAccessoryViewExample.js in RNTester.
- */
-
 type Props = $ReadOnly<{|
   +children: React.Node,
   /**
-   * An ID which is used to associate this `InputAccessoryView` to
-   * specified TextInput(s).
+    An ID which is used to associate this `InputAccessoryView` to specified
+    TextInput(s).
    */
   nativeID?: ?string,
+  /**
+    @type ViewStyleProps
+   */
   style?: ?ViewStyleProp,
   backgroundColor?: ?ColorValue,
 |}>;
 
+/**
+  A component which enables customization of the keyboard input accessory view
+  on iOS. The input accessory view is displayed above the keyboard whenever a
+  `TextInput` has focus. This component can be used to create custom toolbars.
+
+  To use this component wrap your custom toolbar with the InputAccessoryView
+  component, and set a `nativeID`. Then, pass that `nativeID` as the
+  `inputAccessoryViewID` of whatever `TextInput` you desire. A basic example:
+
+  ```SnackPlayer name=InputAccessoryView&supportedPlatforms=ios
+  import React, { useState } from 'react';
+  import { Button, InputAccessoryView, ScrollView, TextInput } from 'react-native';
+
+  export default App = () => {
+    const inputAccessoryViewID = 'uniqueID';
+    const initialText = 'Placeholder Text';
+    const [text, setText] = useState(initialText);
+
+    return (
+      <>
+        <ScrollView keyboardDismissMode="interactive">
+          <TextInput
+            style={{
+              padding: 16,
+              marginTop: 50
+            }}
+            inputAccessoryViewID={inputAccessoryViewID}
+            onChangeText={text => setText(text)}
+            value={text}
+          />
+        </ScrollView>
+        <InputAccessoryView nativeID={inputAccessoryViewID}>
+          <Button
+            onPress={() => setText(initialText)}
+            title="Reset Text"
+          />
+        </InputAccessoryView>
+      </>
+    );
+  }
+  ```
+
+  This component can also be used to create sticky text inputs (text inputs
+  which are anchored to the top of the keyboard). To do this, wrap a `TextInput`
+  with the `InputAccessoryView` component, and don't set a `nativeID`. For an
+  example, look at
+  [InputAccessoryViewExample.js](react-native:InputAccessoryView).
+
+  [react-native:InputAccessoryView]:
+  https://github.com/facebook/react-native/blob/master/RNTester/js/examples/InputAccessoryView/InputAccessoryViewExample.js
+ */
 class InputAccessoryView extends React.Component<Props> {
   render(): React.Node {
     if (Platform.OS !== 'ios') {
