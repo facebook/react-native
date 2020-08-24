775d90835f03efb0436f38c885b8497b2b4b5220
Aniket Kumar <aniketkumar049@gmail.com>
2020-08-24 22:28:35 +0530

    update TouchableNativeFeedback component

--- Libraries/Components/Touchable/TouchableNativeFeedback.js
+++ Libraries/Components/Touchable/TouchableNativeFeedback.js
@@ -6,6 +6,7 @@
  *
  * @flow strict-local
  * @format
+ * @generate-docs
  */
 
 'use strict';
@@ -29,10 +30,12 @@ type Props = $ReadOnly<{|
   ...React.ElementConfig<TouchableWithoutFeedback>,
 
   /**
-   * Determines the type of background drawable that's going to be used to
-   * display feedback. It takes an object with `type` property and extra data
-   * depending on the `type`. It's recommended to use one of the static
-   * methods to generate that dictionary.
+    Determines the type of background drawable that's going to be used to
+    display feedback. It takes an object with `type` property and extra data
+    depending on the `type`. It's recommended to use one of the static
+    methods to generate that dictionary.
+
+    @type backgroundPropType
    */
   background?: ?(
     | $ReadOnly<{|
@@ -51,44 +54,56 @@ type Props = $ReadOnly<{|
   ),
 
   /**
-   * TV preferred focus (see documentation for the View component).
+    TV preferred focus (see documentation for the View component).
+
+    @platform android
    */
   hasTVPreferredFocus?: ?boolean,
 
   /**
-   * TV next focus down (see documentation for the View component).
+    TV next focus down (see documentation for the View component).
+
+    @platform android
    */
   nextFocusDown?: ?number,
 
   /**
-   * TV next focus forward (see documentation for the View component).
+    TV next focus forward (see documentation for the View component).
+
+    @platform android
    */
   nextFocusForward?: ?number,
 
   /**
-   * TV next focus left (see documentation for the View component).
+    TV next focus left (see documentation for the View component).
+
+    @platform android
    */
   nextFocusLeft?: ?number,
 
   /**
-   * TV next focus right (see documentation for the View component).
+    TV next focus right (see documentation for the View component).
+
+    @platform android
    */
   nextFocusRight?: ?number,
 
   /**
-   * TV next focus up (see documentation for the View component).
+    TV next focus up (see documentation for the View component).
+
+    @platform android
    */
   nextFocusUp?: ?number,
 
   /**
-   * Set to true to add the ripple effect to the foreground of the view, instead
-   * of the background. This is useful if one of your child views has a
-   * background of its own, or you're e.g. displaying images, and you don't want
-   * the ripple to be covered by them.
-   *
-   * Check TouchableNativeFeedback.canUseNativeForeground() first, as this is
-   * only available on Android 6.0 and above. If you try to use this on older
-   * versions, this will fallback to background.
+    Set to true to add the ripple effect to the foreground of the view, instead
+    of the background. This is useful if one of your child views has a
+    background of its own, or you're e.g. displaying images, and you don't want
+    the ripple to be covered by them.
+
+    Check TouchableNativeFeedback.canUseNativeForeground() first, as this is
+    only available on Android 6.0 and above. If you try to use this on older
+    versions you will get a warning and fallback to background.
    */
   useForeground?: ?boolean,
 |}>;
@@ -97,10 +112,74 @@ type State = $ReadOnly<{|
   pressability: Pressability,
 |}>;
 
+/**
+  > If you're looking for a more extensive and future-proof way to handle
+  > touch-based input, check out the [Pressable](pressable.md) API.
+
+  A wrapper for making views respond properly to touches (Android only). On
+  Android this component uses native state drawable to display touch feedback.
+
+  At the moment it only supports having a single View instance as a child node, as
+  it's implemented by replacing that View with another instance of RCTView node
+  with some additional properties set.
+
+  Background drawable of native feedback touchable can be customized with
+  `background` property.
+
+  ```SnackPlayer name=TouchableNativeFeedback%20Android%20Component%20Example&supportedPlatforms=android
+  import React, { useState } from "react";
+  import { Text, View, StyleSheet, TouchableNativeFeedback } from "react-native";
+  import Constants from "expo-constants";
+
+  const randomHexColor = () => {
+    return "#000000".replace(/0/g, function() {
+      return (~~(Math.random() * 16)).toString(16);
+    });
+  };
+  const App = () => {
+    const [rippleColor, setRippleColor] = useState(randomHexColor());
+    const [rippleOverflow, setRippleOverflow] = useState(false);
+    return (
+      <View style={styles.container}>
+        <TouchableNativeFeedback
+          onPress={() => {
+            setRippleColor(randomHexColor());
+            setRippleOverflow(!rippleOverflow);
+          }}
+          background={TouchableNativeFeedback.Ripple(rippleColor, rippleOverflow)}
+        >
+          <View style={styles.touchable}>
+            <Text style={styles.text}>TouchableNativeFeedback</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      paddingTop: Constants.statusBarHeight,
+      backgroundColor: "#ecf0f1",
+      padding: 8
+    },
+    touchable: { flex: 0.5, borderColor: "black", borderWidth: 1 },
+    text: { alignSelf: "center" }
+  });
+
+  export default App;
+  ```
+ */
 class TouchableNativeFeedback extends React.Component<Props, State> {
   /**
-   * Creates a value for the `background` prop that uses the Android theme's
-   * default background for selectable elements.
+    ```jsx
+    static SelectableBackground(rippleRadius: ?number)
+    ```
+
+    Creates an object that represents android theme's default background for
+    selectable elements (?android:attr/selectableItemBackground). `rippleRadius`
+    parameter controls the radius of the ripple effect.
    */
   static SelectableBackground: (
     rippleRadius: ?number,
@@ -115,8 +194,14 @@ class TouchableNativeFeedback extends React.Component<Props, State> {
   });
 
   /**
-   * Creates a value for the `background` prop that uses the Android theme's
-   * default background for borderless selectable elements. Requires API 21+.
+    ```jsx
+    static SelectableBackgroundBorderless(rippleRadius: ?number)
+    ```
+
+    Creates an object that represent android theme's default background for
+    borderless selectable elements
+    (?android:attr/selectableItemBackgroundBorderless). Available on android API
+    level 21+. `rippleRadius` parameter controls the radius of the ripple effect.
    */
   static SelectableBackgroundBorderless: (
     rippleRadius: ?number,
@@ -131,13 +216,27 @@ class TouchableNativeFeedback extends React.Component<Props, State> {
   });
 
   /**
-   * Creates a value for the `background` prop that uses the Android ripple with
-   * the supplied color. If `borderless` is true, the ripple will render outside
-   * of the view bounds. Requires API 21+.
+    ```jsx
+    static Ripple(color: string, borderless: boolean, rippleRadius: ?number)
+    ```
+
+    Creates an object that represents ripple drawable with specified color (as a
+    string). If property `borderless` evaluates to true the ripple will render
+    outside of the view bounds (see native actionbar buttons as an example of that
+    behavior). This background type is available on Android API level 21+.
    */
   static Ripple: (
+    /**
+      The ripple color
+     */
     color: string,
+    /**
+      If the ripple can render outside its bounds
+     */
     borderless: boolean,
+    /**
+      controls the radius of the ripple effect
+     */
     rippleRadius: ?number,
   ) => $ReadOnly<{|
     borderless: boolean,
@@ -159,7 +258,9 @@ class TouchableNativeFeedback extends React.Component<Props, State> {
   };
 
   /**
-   * Whether `useForeground` is supported.
+    ```jsx
+    static canUseNativeForeground()
+    ```
    */
   static canUseNativeForeground: () => boolean = () =>
     Platform.OS === 'android' && Platform.Version >= 23;
