1e56e33a82c0aadfdc0ec3d27441d9be2145881d
HeyImChris <48299693+HeyImChris@users.noreply.github.com>
2020-09-03 13:40:27 -0700

    Fix image break in iOS 14 where images won't show (#573)
    * Update RCTCxxBridge.mm
    
    * Fix images on iOS 14
    
    * update podfile
    
    * Fix images on iOS 14
    
    * update podfile

--- Libraries/Image/RCTUIImageViewAnimated.m
+++ Libraries/Image/RCTUIImageViewAnimated.m
@@ -277,6 +277,8 @@ static NSUInteger RCTDeviceFreeMemory() {
   if (_currentFrame) {
     layer.contentsScale = self.animatedImageScale;
     layer.contents = (__bridge id)_currentFrame.CGImage;
+  }  else {
+     [super displayLayer:layer];
   }
 }
 
