9ca91af4326d12e92f68979fa15831a6016c46b8
Scott Kyle <scott@appden.com>
2020-09-10 09:37:34 -0700

    Fix passthrough props on TouchableWithoutFeedback (#592)
    * Fix passthrough props on TouchableWithoutFeedback
    
    Most of the macOS-specific props should be passed through, but acceptsKeyboardFocus and enableFocusRing should default to true like the other Touchable components.
    
    * Update snapshots with TouchableWithoutFeedback fix

--- Libraries/Components/Touchable/TouchableWithoutFeedback.js
+++ Libraries/Components/Touchable/TouchableWithoutFeedback.js
@@ -105,7 +105,12 @@ const PASSTHROUGH_PROPS = [
   'onFocus',
   'onLayout',
   'onMouseEnter', // [TODO(macOS ISS#2323203)
-  'onMouseLeave', // ]TODO(macOS ISS#2323203)
+  'onMouseLeave',
+  'onDragEnter',
+  'onDragLeave',
+  'onDrop',
+  'draggedTypes',
+  'tooltip', // ]TODO(macOS ISS#2323203)
   'testID',
 ];
 
@@ -190,6 +195,10 @@ class TouchableWithoutFeedback extends React.Component<Props, State> {
       accessible: this.props.accessible !== false,
       focusable:
         this.props.focusable !== false && this.props.onPress !== undefined,
+      acceptsKeyboardFocus:
+        this.props.acceptsKeyboardFocus !== false && !this.props.disabled, // [TODO(macOS ISS#2323203)
+      enableFocusRing:
+        this.props.enableFocusRing !== false && !this.props.disabled, // ]TODO(macOS ISS#2323203)
     };
     for (const prop of PASSTHROUGH_PROPS) {
       if (this.props[prop] !== undefined) {
