d3c6116d9c89505aac4efcefa792df0bb214cc5a
Eloy Durán <eloy.de.enige@gmail.com>
2020-09-11 19:22:23 +0200

    v0.63 TODO

--- Libraries/Text/TextInput/RCTBackedTextInputViewProtocol.h
+++ Libraries/Text/TextInput/RCTBackedTextInputViewProtocol.h
@@ -50,7 +50,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, assign, getter=isEditable) BOOL editable;
 @property (nonatomic, assign) BOOL caretHidden;
 @property (nonatomic, assign) BOOL enablesReturnKeyAutomatically;
+#if !TARGET_OS_OSX // [TODO(macOS v0.63)
 @property (nonatomic, assign) UITextFieldViewMode clearButtonMode;
+#endif // ]TODO(macOS v0.63)
 @property (nonatomic, getter=isScrollEnabled) BOOL scrollEnabled;
 
 // This protocol disallows direct access to `selectedTextRange` property because
