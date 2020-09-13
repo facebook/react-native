250485a94c4ca8a022da0b7313edea3244506c24
Joshua Gross <joshuagross@fb.com>
2020-09-12 21:50:51 -0700

    Move implementation of AndroidTextInputState to cpp file
    Summary:
    Changelog: [Internal]
    
    Move implementation to cpp file
    
    Reviewed By: shergin
    
    Differential Revision: D23652329
    
    fbshipit-source-id: 1b36fb7d9523938b90ae05af35494ff10dde1582

--- ReactCommon/react/renderer/components/textinput/androidtextinput/react/renderer/components/androidtextinput/AndroidTextInputState.cpp
+++ ReactCommon/react/renderer/components/textinput/androidtextinput/react/renderer/components/androidtextinput/AndroidTextInputState.cpp
@@ -13,6 +13,64 @@
 namespace facebook {
 namespace react {
 
+AndroidTextInputState::AndroidTextInputState(
+    int64_t mostRecentEventCount,
+    AttributedString const &attributedString,
+    AttributedString const &reactTreeAttributedString,
+    ParagraphAttributes const &paragraphAttributes,
+    TextAttributes const &defaultTextAttributes,
+    ShadowView const &defaultParentShadowView,
+    SharedTextLayoutManager const &layoutManager,
+    float defaultThemePaddingStart,
+    float defaultThemePaddingEnd,
+    float defaultThemePaddingTop,
+    float defaultThemePaddingBottom)
+    : mostRecentEventCount(mostRecentEventCount),
+      cachedAttributedStringId(0),
+      attributedString(attributedString),
+      reactTreeAttributedString(reactTreeAttributedString),
+      paragraphAttributes(paragraphAttributes),
+      defaultTextAttributes(defaultTextAttributes),
+      defaultParentShadowView(defaultParentShadowView),
+      layoutManager(layoutManager),
+      defaultThemePaddingStart(defaultThemePaddingStart),
+      defaultThemePaddingEnd(defaultThemePaddingEnd),
+      defaultThemePaddingTop(defaultThemePaddingTop),
+      defaultThemePaddingBottom(defaultThemePaddingBottom) {}
+
+AndroidTextInputState::AndroidTextInputState(
+    AndroidTextInputState const &previousState,
+    folly::dynamic const &data)
+    : mostRecentEventCount(data.getDefault(
+                                   "mostRecentEventCount",
+                                   previousState.mostRecentEventCount)
+                               .getInt()),
+      cachedAttributedStringId(
+          data.getDefault("cacheId", previousState.cachedAttributedStringId)
+              .getInt()),
+      attributedString(previousState.attributedString),
+      reactTreeAttributedString(previousState.reactTreeAttributedString),
+      paragraphAttributes(previousState.paragraphAttributes),
+      defaultTextAttributes(previousState.defaultTextAttributes),
+      defaultParentShadowView(previousState.defaultParentShadowView),
+      layoutManager(previousState.layoutManager),
+      defaultThemePaddingStart(data.getDefault(
+                                       "themePaddingStart",
+                                       previousState.defaultThemePaddingStart)
+                                   .getDouble()),
+      defaultThemePaddingEnd(data.getDefault(
+                                     "themePaddingEnd",
+                                     previousState.defaultThemePaddingEnd)
+                                 .getDouble()),
+      defaultThemePaddingTop(data.getDefault(
+                                     "themePaddingTop",
+                                     previousState.defaultThemePaddingTop)
+                                 .getDouble()),
+      defaultThemePaddingBottom(data.getDefault(
+                                        "themePaddingBottom",
+                                        previousState.defaultThemePaddingBottom)
+                                    .getDouble()){};
+
 #ifdef ANDROID
 folly::dynamic AndroidTextInputState::getDynamic() const {
   // Java doesn't need all fields, so we don't pass them along.
--- ReactCommon/react/renderer/components/textinput/androidtextinput/react/renderer/components/androidtextinput/AndroidTextInputState.h
+++ ReactCommon/react/renderer/components/textinput/androidtextinput/react/renderer/components/androidtextinput/AndroidTextInputState.h
@@ -93,53 +93,12 @@ class AndroidTextInputState final {
       float defaultThemePaddingStart,
       float defaultThemePaddingEnd,
       float defaultThemePaddingTop,
-      float defaultThemePaddingBottom)
-      : mostRecentEventCount(mostRecentEventCount),
-        cachedAttributedStringId(0),
-        attributedString(attributedString),
-        reactTreeAttributedString(reactTreeAttributedString),
-        paragraphAttributes(paragraphAttributes),
-        defaultTextAttributes(defaultTextAttributes),
-        defaultParentShadowView(defaultParentShadowView),
-        layoutManager(layoutManager),
-        defaultThemePaddingStart(defaultThemePaddingStart),
-        defaultThemePaddingEnd(defaultThemePaddingEnd),
-        defaultThemePaddingTop(defaultThemePaddingTop),
-        defaultThemePaddingBottom(defaultThemePaddingBottom) {}
+      float defaultThemePaddingBottom);
+
   AndroidTextInputState() = default;
   AndroidTextInputState(
       AndroidTextInputState const &previousState,
-      folly::dynamic const &data)
-      : mostRecentEventCount(data.getDefault(
-                                     "mostRecentEventCount",
-                                     previousState.mostRecentEventCount)
-                                 .getInt()),
-        cachedAttributedStringId(
-            data.getDefault("cacheId", previousState.cachedAttributedStringId)
-                .getInt()),
-        attributedString(previousState.attributedString),
-        reactTreeAttributedString(previousState.reactTreeAttributedString),
-        paragraphAttributes(previousState.paragraphAttributes),
-        defaultTextAttributes(previousState.defaultTextAttributes),
-        defaultParentShadowView(previousState.defaultParentShadowView),
-        layoutManager(previousState.layoutManager),
-        defaultThemePaddingStart(data.getDefault(
-                                         "themePaddingStart",
-                                         previousState.defaultThemePaddingStart)
-                                     .getDouble()),
-        defaultThemePaddingEnd(data.getDefault(
-                                       "themePaddingEnd",
-                                       previousState.defaultThemePaddingEnd)
-                                   .getDouble()),
-        defaultThemePaddingTop(data.getDefault(
-                                       "themePaddingTop",
-                                       previousState.defaultThemePaddingTop)
-                                   .getDouble()),
-        defaultThemePaddingBottom(
-            data.getDefault(
-                    "themePaddingBottom",
-                    previousState.defaultThemePaddingBottom)
-                .getDouble()){};
+      folly::dynamic const &data);
   folly::dynamic getDynamic() const;
 };
 
