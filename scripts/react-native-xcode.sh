4b0920b3bc22dcae384fb1ec2b555d6515916df6
grabbou <grabbou@gmail.com>
2020-09-15 17:32:45 +0200

    chore: remove `hermes-binary.sh` file

--- scripts/react-native-xcode.sh
+++ scripts/react-native-xcode.sh
@@ -116,8 +116,17 @@ REACT_NATIVE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # shellcheck source=/dev/null
 source "$REACT_NATIVE_DIR/scripts/node-binary.sh"
 
-# check and assign HERMES_CLI env
-source "$REACT_NATIVE_DIR/scripts/hermes-binary.sh"
+[ -z "$HERMES_PATH" ] && export HERMES_PATH="$REACT_NATIVE_DIR/../hermes-engine-darwin"
+
+export HERMES_CLI = "$HERMES_PATH/destroot/bin/hermesc"
+
+if [[ $USE_HERMES == true && ! -d "$HERMES_PATH" ]]; then
+  echo "error: Can't find Hermes executable - directory `$HERMES_PATH` doesn't exist. " \
+       "If you have a non-standard project structure, select your project in Xcode, find " \
+       "'Build Phases' - 'Bundle React Native code and images' and set `HERMES_PATH` to " \
+       "a `hermes-engine-darwin` package inside your `node_modules`" >&2
+  exit 2
+fi
 
 [ -z "$NODE_ARGS" ] && export NODE_ARGS=""
 
