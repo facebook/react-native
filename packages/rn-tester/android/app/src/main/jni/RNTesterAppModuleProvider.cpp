c3ab2a2541056ec9394f4f5e95a224ce265a0f8c
Kevin Gozali <fkg@fb.com>
2020-09-23 16:50:34 -0700

    RNTester Android: Add stub C++ for TurboModule provider (#30014)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/30014
    
    This is the base setup to compile C++ as part of RNTester app. There is no dependencies on ReactAndroidNdk lib yet, just a bunch of ndkBuild setup in Gradle. Note: this is using Gradle's `nkdBuild` support instead of calling `ndk-build` manually like in ReactAndroid/build.gradle.
    
    Reference: https://google.github.io/android-gradle-dsl/current/com.android.build.gradle.internal.dsl.ExternalNativeNdkBuildOptions.html
    
    Note that `ANDROID_NDK` env var is honored to pick the right NDK version.
    
    For now, this is gated behind `USE_CODEGEN` env variable.
    
    Changelog: [Internal]
    
    Reviewed By: JoshuaGross
    
    Differential Revision: D23887058
    
    fbshipit-source-id: 7a962649461d15af46999a15b900464543e5b05c

---
+++ packages/rn-tester/android/app/src/main/jni/RNTesterAppModuleProvider.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "RNTesterAppModuleProvider.h"
+
+namespace facebook {
+namespace react {
+
+std::shared_ptr<void> RNTesterAppModuleProvider(const std::string moduleName) {
+  return nullptr;
+}
+
+} // namespace react
+} // namespace facebook
