5065f58e0fe5cacbdbbd99302220468e0a616913
Eloy Durán <eloy.de.enige@gmail.com>
2020-09-24 13:45:01 +0200

    [PlatformColor] Duplicate iOS code for macOS
    This is a naive version of this change, there should likely be some code sharing
    happening between iOS and macOS.

--- Libraries/StyleSheet/PlatformColorValueTypes.macos.js
+++ Libraries/StyleSheet/PlatformColorValueTypes.macos.js
@@ -5,13 +5,74 @@
  * LICENSE file in the root directory of this source tree.
  *
  * @format
- * @flow
+ * @flow strict-local
  */
 // [TODO(macOS ISS#2323203)
 'use strict';
 
-export type {NativeColorValue} from './PlatformColorValueTypes.ios';
+import type {ColorValue} from './StyleSheetTypes';
+import type {ProcessedColorValue} from './processColor';
 
-module.exports = require('./PlatformColorValueTypes.ios');
+export opaque type NativeColorValue = {
+  semantic?: Array<string>,
+  dynamic?: {
+    light: ?(ColorValue | ProcessedColorValue),
+    dark: ?(ColorValue | ProcessedColorValue),
+  },
+};
 
+export const PlatformColor = (...names: Array<string>): ColorValue => {
+  return {semantic: names};
+};
+
+export type DynamicColorIOSTuplePrivate = {
+  light: ColorValue,
+  dark: ColorValue,
+};
+
+export const DynamicColorIOSPrivate = (
+  tuple: DynamicColorIOSTuplePrivate,
+): ColorValue => {
+  return {dynamic: {light: tuple.light, dark: tuple.dark}};
+};
+
+export const normalizeColorObject = (
+  color: NativeColorValue,
+): ?ProcessedColorValue => {
+  if ('semantic' in color) {
+    // an ios semantic color
+    return color;
+  } else if ('dynamic' in color && color.dynamic !== undefined) {
+    const normalizeColor = require('./normalizeColor');
+
+    // a dynamic, appearance aware color
+    const dynamic = color.dynamic;
+    const dynamicColor: NativeColorValue = {
+      dynamic: {
+        light: normalizeColor(dynamic.light),
+        dark: normalizeColor(dynamic.dark),
+      },
+    };
+    return dynamicColor;
+  }
+
+  return null;
+};
+
+export const processColorObject = (
+  color: NativeColorValue,
+): ?NativeColorValue => {
+  if ('dynamic' in color && color.dynamic != null) {
+    const processColor = require('./processColor');
+    const dynamic = color.dynamic;
+    const dynamicColor: NativeColorValue = {
+      dynamic: {
+        light: processColor(dynamic.light),
+        dark: processColor(dynamic.dark),
+      },
+    };
+    return dynamicColor;
+  }
+  return color;
+};
 // ]TODO(macOS ISS#2323203)
---
+++ Libraries/StyleSheet/PlatformColorValueTypesIOS.macos.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict-local
+ */
+// [TODO(macOS ISS#2323203)
+'use strict';
+
+import type {ColorValue} from './StyleSheetTypes';
+import {DynamicColorIOSPrivate} from './PlatformColorValueTypes';
+
+export type DynamicColorIOSTuple = {
+  light: ColorValue,
+  dark: ColorValue,
+};
+
+export const DynamicColorIOS = (tuple: DynamicColorIOSTuple): ColorValue => {
+  return DynamicColorIOSPrivate({light: tuple.light, dark: tuple.dark});
+};
+// ]TODO(macOS ISS#2323203)
