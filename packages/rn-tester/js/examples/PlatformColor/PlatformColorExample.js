ab4d43edde1235aaee70e654c8596c6bfb38d68c
simek <gosimek@gmail.com>
2020-09-28 17:18:51 +0200

    add clearColor to the RNTester example

--- packages/rn-tester/js/examples/PlatformColor/PlatformColorExample.js
+++ packages/rn-tester/js/examples/PlatformColor/PlatformColorExample.js
@@ -110,6 +110,8 @@ function PlatformColorsExample() {
         {label: 'systemGray4', color: PlatformColor('systemGray4')},
         {label: 'systemGray5', color: PlatformColor('systemGray5')},
         {label: 'systemGray6', color: PlatformColor('systemGray6')},
+        // Transparent Color
+        {label: 'clear', color: PlatformColor('clear')},
       ];
     } else if (Platform.OS === 'android') {
       colors = [
