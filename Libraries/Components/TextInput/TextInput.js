8872c60ad75048126bd6d54735082183585e1840
jeswinsimon <jeswinsimon@gmail.com>
2020-10-01 13:58:02 +0530

    Removed unused accessibility props from TextInput

--- Libraries/Components/TextInput/TextInput.js
+++ Libraries/Components/TextInput/TextInput.js
@@ -1147,10 +1147,6 @@ function InternalTextInput(props: Props): React.Node {
         onPress={_onPress}
         onPressIn={props.onPressIn}
         onPressOut={props.onPressOut}
-        accessible={props.accessible}
-        accessibilityLabel={props.accessibilityLabel}
-        accessibilityRole={props.accessibilityRole}
-        accessibilityState={props.accessibilityState}
         nativeID={props.nativeID}
         testID={props.testID}
         {...additionalTouchableProps}>
