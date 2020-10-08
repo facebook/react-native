154fd429e34616dad37d08839ed7e260522ce3cf
Valentin Shergin <shergin@fb.com>
2020-10-08 16:05:44 -0700

    Back out "Align multi-line TextInput onSubmitEditing behavior"
    Summary:
    This is a revert of D22488870 (https://github.com/facebook/react-native/commit/521b16730dd07d80261086c2f33eed2a766d404e). (https://github.com/facebook/react-native/pull/29177)
    We have to revert it because we realized that it's a breaking change without a very good reason. We have to figure out a better way to unify platform behaviors.
    
    Changelog:
    [iOS][Fixed] - Reverted recent change in TextInput (#29177)
    
    Reviewed By: fkgozali
    
    Differential Revision: D24200517
    
    fbshipit-source-id: af0e561a6b8d9ade487be6b197a5d79d326442b6

--- Libraries/Text/TextInput/RCTBaseTextInputView.m
+++ Libraries/Text/TextInput/RCTBaseTextInputView.m
@@ -413,13 +413,11 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithFrame:(CGRect)frame)
   // `onSubmitEditing` is called when "Submit" button
   // (the blue key on onscreen keyboard) did pressed
   // (no connection to any specific "submitting" process).
-  if (_blurOnSubmit) {
-    [_eventDispatcher sendTextEventWithType:RCTTextEventTypeSubmit
-                                   reactTag:self.reactTag
-                                       text:[self.backedTextInputView.attributedText.string copy]
-                                        key:nil
-                                 eventCount:_nativeEventCount];
-  }
+  [_eventDispatcher sendTextEventWithType:RCTTextEventTypeSubmit
+                                 reactTag:self.reactTag
+                                     text:[self.backedTextInputView.attributedText.string copy]
+                                      key:nil
+                               eventCount:_nativeEventCount];
 
   return _blurOnSubmit;
 }
