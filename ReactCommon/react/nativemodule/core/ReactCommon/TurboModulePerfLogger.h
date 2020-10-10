9a7f2b590e3ac530755a71bc317e067d8b5af2bd
Ramanpreet Nara <ramanpreet@fb.com>
2020-10-10 02:48:39 -0700

    Log QPL Events from TM + NM, when Perf. Logging is enabled
    Summary:
    ## Previously
    - When TurboModules system was on, we'd only log events from the TurboModules system.
    - When TurboModules system was off, we'd only log events from the NativeModule system.
    
    This ultimately gives us less data to analyze both systems in production.
    
    ## Changes in This Diff
    When perf. logging is on, we'll log events from both systems. Each QPL event now include an annotation of which system the event is coming from. Concretely, this will allow us to see how much of the NativeModule system is being exercised in the TurboModule test group.
    
    Changelog: [Internal]
    
    Reviewed By: hramos
    
    Differential Revision: D24232594
    
    fbshipit-source-id: 7dff57bd74fc7ef744d3e06ff174304f25790456

--- ReactCommon/react/nativemodule/core/ReactCommon/TurboModulePerfLogger.h
+++ ReactCommon/react/nativemodule/core/ReactCommon/TurboModulePerfLogger.h
@@ -14,6 +14,7 @@ namespace facebook {
 namespace react {
 namespace TurboModulePerfLogger {
 void enableLogging(std::unique_ptr<NativeModulePerfLogger> &&logger);
+void disableLogging();
 
 void moduleDataCreateStart(const char *moduleName, int32_t id);
 void moduleDataCreateEnd(const char *moduleName, int32_t id);
