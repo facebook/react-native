44a2c19e92e20d6092dc3a2b612b7d1bcfb45bf3
Dulmandakh <dulmandakh@gmail.com>
2020-10-30 01:27:20 +0800

    add android-28

--- scripts/android-setup.sh
+++ scripts/android-setup.sh
@@ -25,10 +25,6 @@ function getAndroidPackages {
     echo "Installing SDK build tools, revision $ANDROID_SDK_BUILD_TOOLS_REVISION..."
     sdkmanager "build-tools;$ANDROID_SDK_BUILD_TOOLS_REVISION"
     # These moved to "system-images;android-$ANDROID_SDK_BUILD_API_LEVEL;google_apis;x86" starting with API level 25, but there is no ARM version.
-    echo "Installing Google APIs $ANDROID_GOOGLE_API_LEVEL..."
-    sdkmanager "add-ons;addon-google_apis-google-$ANDROID_GOOGLE_API_LEVEL"
-    echo "Installing Android Support Repository"
-    sdkmanager "extras;android;m2repository"
     $CI && touch "$DEPS"
   fi
 }
@@ -51,6 +47,7 @@ function getAndroidNDK {
 function createAVD {
   AVD_PACKAGES="system-images;android-$ANDROID_SDK_TARGET_API_LEVEL;google_apis;$AVD_ABI"
   echo "Creating AVD with packages $AVD_PACKAGES"
+  sdkmanager $AVD_PACKAGES
   echo no | avdmanager create avd --name "$AVD_NAME" --force --package "$AVD_PACKAGES" --tag google_apis
   avdmanager list avd
   emulator -help-datadir
