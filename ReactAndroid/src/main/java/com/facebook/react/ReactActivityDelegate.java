b74c6bc4747a0e86ec36e4b52f8514188d1238c8
Dulmandakh <dulmandakh@gmail.com>
2020-11-07 20:14:38 +0800

    remove reference from comments

--- ReactAndroid/src/main/java/com/facebook/react/ReactActivityDelegate.java
+++ ReactAndroid/src/main/java/com/facebook/react/ReactActivityDelegate.java
@@ -20,7 +20,7 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.modules.core.PermissionListener;
 
 /**
- * Delegate class for {@link ReactActivity} and {@link ReactFragmentActivity}. You can subclass this
+ * Delegate class for {@link ReactActivity}. You can subclass this
  * to provide custom implementations for e.g. {@link #getReactNativeHost()}, if your Application
  * class doesn't implement {@link ReactApplication}.
  */
