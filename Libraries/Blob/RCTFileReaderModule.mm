282b1a880f1c66b2c6b74ec7efe7d07cacb09fec
Ramanpreet Nara <ramanpreet@fb.com>
2020-12-12 18:58:58 -0800

    Manual: Migrate from bridge.moduleForClass to RCTModuleRegistry
    Summary:
    All NativeModules that use `[RCTBridge moduleForClass:]` to access other NativeModules are now instead using the Venice-compatible RCTModuleRegistry to access other NativeModules.
    
    Changelog: [Internal]
    
    Reviewed By: shergin
    
    Differential Revision: D25508277
    
    fbshipit-source-id: 1b415a5ad4055290940879e100ceaa84ae83feeb

--- Libraries/Blob/RCTFileReaderModule.mm
+++ Libraries/Blob/RCTFileReaderModule.mm
@@ -24,14 +24,14 @@
 RCT_EXPORT_MODULE(FileReaderModule)
 
 @synthesize bridge = _bridge;
-@synthesize turboModuleRegistry = _turboModuleRegistry;
+@synthesize moduleRegistry = _moduleRegistry;
 
 RCT_EXPORT_METHOD(readAsText:(NSDictionary<NSString *, id> *)blob
                   encoding:(NSString *)encoding
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
-  RCTBlobManager *blobManager = [_bridge moduleForClass:[RCTBlobManager class]];
+  RCTBlobManager *blobManager = [_moduleRegistry moduleForName:"BlobManager"];
   NSData *data = [blobManager resolve:blob];
 
   if (data == nil) {
@@ -56,12 +56,7 @@ RCT_EXPORT_METHOD(readAsDataURL:(NSDictionary<NSString *, id> *)blob
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
-  RCTBlobManager *blobManager = nil;
-  if (_bridge) {
-    blobManager = [_bridge moduleForClass:[RCTBlobManager class]];
-  } else {
-    blobManager = [[self turboModuleRegistry] moduleForName:[NSStringFromClass([RCTBlobManager class]) UTF8String]];
-  }
+  RCTBlobManager *blobManager = [_moduleRegistry moduleForName:"BlobManager"];
   NSData *data = [blobManager resolve:blob];
 
   if (data == nil) {
