e37e56b04244f17f7013ef246de10a5a7f1ba5cb
Valentin Shergin <shergin@fb.com>
2021-01-10 13:54:36 -0800

    Back out "Add onFocus and onBlur to Pressable."
    Summary:
    I suspect it's causing T82781515.
    
    Changelog:
    [Category][Type] - Backout of "[react-native][PR] Add onFocus and onBlur to Pressable."
    
    Reviewed By: p-sun
    
    Differential Revision: D25864414
    
    fbshipit-source-id: efba9136edba97d5bd2a0de15f9ddae7dfd24e51

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputFocusEvent.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputFocusEvent.java
@@ -5,19 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.views.common;
+package com.facebook.react.views.textinput;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.events.Event;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 
-/** Event emitted by a native view when it receives focus. */
-public class ReactViewFocusEvent extends Event<ReactViewFocusEvent> {
+/** Event emitted by EditText native view when it receives focus. */
+/* package */ class ReactTextInputFocusEvent extends Event<ReactTextInputFocusEvent> {
 
   private static final String EVENT_NAME = "topFocus";
 
-  public ReactViewFocusEvent(int viewId) {
+  public ReactTextInputFocusEvent(int viewId) {
     super(viewId);
   }
 
