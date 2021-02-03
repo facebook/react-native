1e6d2e983ac3aa037bc4465a0c2cd67f1ae25139
David Vacca <dvacca@fb.com>
2021-02-03 12:52:52 -0800

    Remove strong reference from TM infra
    Summary:
    This diff refactor strong references on TM infra to use weak references and avoid leaking objects
    
    changelog: [internal] internal
    
    Reviewed By: RSNara
    
    Differential Revision: D26229814
    
    fbshipit-source-id: d4a327dba227378d23764433d5917eb4378a4453

--- ReactCommon/react/nativemodule/core/platform/android/ReactCommon/JavaTurboModule.cpp
+++ ReactCommon/react/nativemodule/core/platform/android/ReactCommon/JavaTurboModule.cpp
@@ -682,10 +682,14 @@ jsi::Value JavaTurboModule::invokeJavaMethod(
           [jargs,
            globalRefs,
            methodID,
-           instance_ = instance_,
+           instance_ = jni::make_weak(instance_),
            moduleNameStr = name_,
            methodNameStr,
            id = getUniqueId()]() mutable -> void {
+            auto instance = instance_.lockLocal();
+            if (!instance) {
+              return;
+            }
             /**
              * TODO(ramanpreet): Why do we have to require the environment
              * again? Why does JNI crash when we use the env from the upper
@@ -696,7 +700,7 @@ jsi::Value JavaTurboModule::invokeJavaMethod(
             const char *methodName = methodNameStr.c_str();
 
             TMPL::asyncMethodCallExecutionStart(moduleName, methodName, id);
-            env->CallVoidMethodA(instance_.get(), methodID, jargs.data());
+            env->CallVoidMethodA(instance.get(), methodID, jargs.data());
             try {
               FACEBOOK_JNI_THROW_PENDING_EXCEPTION();
             } catch (...) {
@@ -777,10 +781,15 @@ jsi::Value JavaTurboModule::invokeJavaMethod(
                 [jargs,
                  globalRefs,
                  methodID,
-                 instance_ = instance_,
+                 instance_ = jni::make_weak(instance_),
                  moduleNameStr,
                  methodNameStr,
                  id = getUniqueId()]() mutable -> void {
+                  auto instance = instance_.lockLocal();
+
+                  if (!instance) {
+                    return;
+                  }
                   /**
                    * TODO(ramanpreet): Why do we have to require the
                    * environment again? Why does JNI crash when we use the env
@@ -792,7 +801,7 @@ jsi::Value JavaTurboModule::invokeJavaMethod(
 
                   TMPL::asyncMethodCallExecutionStart(
                       moduleName, methodName, id);
-                  env->CallVoidMethodA(instance_.get(), methodID, jargs.data());
+                  env->CallVoidMethodA(instance.get(), methodID, jargs.data());
                   try {
                     FACEBOOK_JNI_THROW_PENDING_EXCEPTION();
                   } catch (...) {
