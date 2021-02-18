d3bc15d494f87619223cd15552651d942811a8b3
Scott Kyle <scott@appden.com>
2021-02-18 09:52:30 -0800

    Fix missing props on secure text input (#719)

--- Libraries/Text/TextInput/Singleline/RCTSinglelineTextInputView.m
+++ Libraries/Text/TextInput/Singleline/RCTSinglelineTextInputView.m
@@ -67,10 +67,20 @@
     } else {
       _backedTextInputView = [[RCTUITextField alloc] initWithFrame:self.bounds];
     }
+    _backedTextInputView.accessibilityElement = previousTextField.accessibilityElement;
+    _backedTextInputView.accessibilityHelp = previousTextField.accessibilityHelp;
+    _backedTextInputView.accessibilityIdentifier = previousTextField.accessibilityIdentifier;
+    _backedTextInputView.accessibilityLabel = previousTextField.accessibilityLabel;
+    _backedTextInputView.accessibilityRole = previousTextField.accessibilityRole;
+    _backedTextInputView.caretHidden = previousTextField.caretHidden;
+    _backedTextInputView.contextMenuHidden = previousTextField.contextMenuHidden;
+    _backedTextInputView.defaultTextAttributes = previousTextField.defaultTextAttributes;
+    _backedTextInputView.editable = previousTextField.editable;
     _backedTextInputView.placeholder = previousTextField.placeholder;
     _backedTextInputView.placeholderColor = previousTextField.placeholderColor;
     _backedTextInputView.selectionColor = previousTextField.selectionColor;
     _backedTextInputView.textAlignment = previousTextField.textAlignment;
+    _backedTextInputView.textContainerInset = previousTextField.textContainerInset;
     _backedTextInputView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     _backedTextInputView.textInputDelegate = self;
     _backedTextInputView.text = previousTextField.text;
