ac704690dcc9ef6ac8e4e855c8542b5fc708e4d3
Andrei Shikov <ashikov@fb.com>
2021-02-24 12:23:58 -0800

    Update Android.mk to ensure the rn-tester compiles in debug mode
    Summary:
    Changelog: [Internal]
    
    Adds react_debug dependency in Android.mk where it was missing
    
    Reviewed By: mdvacca
    
    Differential Revision: D26617400
    
    fbshipit-source-id: 5ac799269b106eadd881d30490ac34bd2134a9b7

--- ReactCommon/react/renderer/components/slider/Android.mk
+++ ReactCommon/react/renderer/components/slider/Android.mk
@@ -21,7 +21,7 @@ LOCAL_CFLAGS += -fexceptions -frtti -std=c++14 -Wall
 
 LOCAL_STATIC_LIBRARIES :=
 
-LOCAL_SHARED_LIBRARIES := libfbjni libreact_codegen_rncore libreact_render_imagemanager libreactnativeutilsjni libreact_render_componentregistry libreact_render_uimanager librrc_image libyoga libfolly_futures glog libfolly_json libglog_init libreact_render_core libreact_render_debug libreact_render_graphics librrc_view
+LOCAL_SHARED_LIBRARIES := libfbjni libreact_codegen_rncore libreact_render_imagemanager libreactnativeutilsjni libreact_render_componentregistry libreact_render_uimanager librrc_image libyoga libfolly_futures glog libfolly_json libglog_init libreact_render_core libreact_render_debug libreact_render_graphics librrc_view libreact_debug
 
 include $(BUILD_SHARED_LIBRARY)
 
