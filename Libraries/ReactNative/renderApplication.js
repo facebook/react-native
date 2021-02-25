eeb36f470929c2fdd8e1ed69898a5ba9144b8715
Rubén Norte <rubennorte@fb.com>
2021-02-25 02:58:34 -0800

    Improve display names of root components in React Profiler
    Summary:
    Changelog:
    [General][Added] - Added `debugName` parameter to `renderApplication` to use as the display name for the React root tree
    
    Reviewed By: rickhanlonii
    
    Differential Revision: D26637787
    
    fbshipit-source-id: 3ddc037573f4434101a9d3dcb5592a127193481c

--- Libraries/ReactNative/renderApplication.js
+++ Libraries/ReactNative/renderApplication.js
@@ -28,12 +28,13 @@ function renderApplication<Props: Object>(
   showArchitectureIndicator?: boolean,
   scopedPerformanceLogger?: IPerformanceLogger,
   isLogBox?: boolean,
+  debugName?: string,
 ) {
   invariant(rootTag, 'Expect to have a valid rootTag, instead got ', rootTag);
 
   const performanceLogger = scopedPerformanceLogger ?? GlobalPerformanceLogger;
 
-  const renderable = (
+  let renderable = (
     <PerformanceLoggerContext.Provider value={performanceLogger}>
       <AppContainer
         rootTag={rootTag}
@@ -46,6 +47,16 @@ function renderApplication<Props: Object>(
       </AppContainer>
     </PerformanceLoggerContext.Provider>
   );
+
+  if (__DEV__ && debugName) {
+    const RootComponentWithMeaningfulName = ({children}) => children;
+    RootComponentWithMeaningfulName.displayName = `${debugName}(RootComponent)`;
+    renderable = (
+      <RootComponentWithMeaningfulName>
+        {renderable}
+      </RootComponentWithMeaningfulName>
+    );
+  }
 
   performanceLogger.startTimespan('renderApplication_React_render');
   performanceLogger.setExtra('usedReactFabric', fabric ? '1' : '0');
