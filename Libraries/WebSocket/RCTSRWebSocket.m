283b4d3b920717a4d8100f2de6eee476726db9d0
wang chenyu <cwang592@usc.edu>
2021-02-26 18:24:14 -0800

    Update RCTSRWebSocket.m
    Currently, RCTSRWebSocket does not read settings from System Proxy, so it'll bypass debugging proxy, e.g. Charles. 
    
    This patch adds a few lines after `CFStreamCreatePairWithSocketToHost` is called, to read current system SOCKS proxy and apply it to the stream, as suggested by https://github.com/facebook/react-native/issues/29408.
    
    I'm a beginner of Obj-C so correct me if there're any naive mistakes :)

--- Libraries/WebSocket/RCTSRWebSocket.m
+++ Libraries/WebSocket/RCTSRWebSocket.m
@@ -495,6 +495,14 @@ RCT_NOT_IMPLEMENTED(- (instancetype)init)
   CFWriteStreamRef writeStream = NULL;
 
   CFStreamCreatePairWithSocketToHost(NULL, (__bridge CFStringRef)host, port, &readStream, &writeStream);
+    
+  CFDictionaryRef proxySettings = CFNetworkCopySystemProxySettings();
+  
+  if (CFDictionaryContainsKey(proxySettings, kCFStreamPropertySOCKSProxyHost)) {
+    NSLog(@"SocketRocket: kCFStreamPropertySOCKSProxyHost found in proxy settings, using it as our proxy. ");
+    CFReadStreamSetProperty((CFReadStreamRef)readStream, kCFStreamPropertySOCKSProxy, (CFTypeRef)proxySettings);
+    CFReadStreamSetProperty((CFReadStreamRef)writeStream, kCFStreamPropertySOCKSProxy, (CFTypeRef)proxySettings);
+  }
 
   _outputStream = CFBridgingRelease(writeStream);
   _inputStream = CFBridgingRelease(readStream);
