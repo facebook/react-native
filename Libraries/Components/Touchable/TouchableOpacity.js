2a3a9483ce3e1df695652f9d29d5db45ad1faf42
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2021-03-06 16:15:32 +0530

    Disable TouchableOpacity when accessibility disabled is set

--- Libraries/Components/Touchable/TouchableOpacity.js
+++ Libraries/Components/Touchable/TouchableOpacity.js
@@ -135,9 +135,10 @@ class TouchableOpacity extends React.Component<Props, State> {
   };
 
   _createPressabilityConfig(): PressabilityConfig {
+    const {accessibilityState, disabled} = this.props;
     return {
       cancelable: !this.props.rejectResponderTermination,
-      disabled: this.props.disabled,
+      disabled: disabled != null ? disabled : accessibilityState?.disabled,
       hitSlop: this.props.hitSlop,
       delayLongPress: this.props.delayLongPress,
       delayPressIn: this.props.delayPressIn,
@@ -215,13 +216,20 @@ class TouchableOpacity extends React.Component<Props, State> {
       ...eventHandlersWithoutBlurAndFocus
     } = this.state.pressability.getEventHandlers();
 
+    const {disabled} = this.props;
+
+    const accessibilityState =
+      disabled != null
+        ? {...this.props.accessibilityState, disabled}
+        : disabled;
+
     return (
       <Animated.View
         accessible={this.props.accessible !== false}
         accessibilityLabel={this.props.accessibilityLabel}
         accessibilityHint={this.props.accessibilityHint}
         accessibilityRole={this.props.accessibilityRole}
-        accessibilityState={this.props.accessibilityState}
+        accessibilityState={accessibilityState}
         accessibilityActions={this.props.accessibilityActions}
         onAccessibilityAction={this.props.onAccessibilityAction}
         accessibilityValue={this.props.accessibilityValue}
