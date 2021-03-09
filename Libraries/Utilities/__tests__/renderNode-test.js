015af9238a2ee96badab3dba430e09e5bbed9ad6
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-03-09 18:56:50 -0500

    Add test for renderNode utility

---
+++ Libraries/Utilities/__tests__/renderNode-test.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @emails oncall+react_native
+ */
+
+'use strict';
+
+const React = require('react');
+const ReactTestRenderer = require('react-test-renderer');
+const renderNode = require('../renderNode');
+
+describe('renderNode', () => {
+  it('renders simple component', () => {
+    class Test extends React.Component<{||}> {
+      render() {
+        return null;
+      }
+    }
+
+    const component = ReactTestRenderer.create(
+      renderNode(Test),
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
