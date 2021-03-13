e0aaec9ef684cc18852a28f1f711882535fc810f
Muhammad Numan <muhammadnuman70@gmail.com>
2021-03-14 02:40:10 +0500

    fix: Calling synchronous methods on native modules is not supported in Chrome

--- Libraries/BatchedBridge/MessageQueue.js
+++ Libraries/BatchedBridge/MessageQueue.js
@@ -169,17 +169,11 @@ class MessageQueue {
     onFail: ?(...mixed[]) => void,
     onSucc: ?(...mixed[]) => void,
   ): mixed {
-    if (__DEV__) {
-      invariant(
-        global.nativeCallSyncHook,
-        'Calling synchronous methods on native ' +
-          'modules is not supported in Chrome.\n\n Consider providing alternative ' +
-          'methods to expose this method in debug mode, e.g. by exposing constants ' +
-          'ahead-of-time.',
-      );
-    }
+    const isDebuggingEnabled = typeof atob !== 'undefined';
     this.processCallbacks(moduleID, methodID, params, onFail, onSucc);
-    return global.nativeCallSyncHook(moduleID, methodID, params);
+    if (!isDebuggingEnabled) {
+      return global.nativeCallSyncHook(moduleID, methodID, params);
+    }
   }
 
   processCallbacks(
