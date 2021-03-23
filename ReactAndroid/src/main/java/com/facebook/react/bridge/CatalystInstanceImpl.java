dbc2d84c13041be74f74b64fd5edc654a9d71b3e
Lulu Wu <luluwu@fb.com>
2021-03-23 05:00:40 -0700

    Clean up - remove unused variables
    Summary:
    Changelog:
    [Android][Changed] - Remove unused variables
    
    Reviewed By: PeteTheHeat, mdvacca
    
    Differential Revision: D27188251
    
    fbshipit-source-id: 0914b402ed1582d7d300e9eccea06f73805107df

--- ReactAndroid/src/main/java/com/facebook/react/bridge/CatalystInstanceImpl.java
+++ ReactAndroid/src/main/java/com/facebook/react/bridge/CatalystInstanceImpl.java
@@ -85,8 +85,6 @@ public class CatalystInstanceImpl implements CatalystInstance {
   private final String mJsPendingCallsTitleForTrace =
       "pending_js_calls_instance" + sNextInstanceIdForTrace.getAndIncrement();
   private volatile boolean mDestroyed = false;
-  private volatile boolean mNativeModulesThreadDestructionComplete = false;
-  private volatile boolean mJSThreadDestructionComplete = false;
   private final TraceListener mTraceListener;
   private final JavaScriptModuleRegistry mJSModuleRegistry;
   private final JSBundleLoader mJSBundleLoader;
