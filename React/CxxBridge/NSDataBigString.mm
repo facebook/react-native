7908c7fb2d9f5f098f973d1400eee40e8375b298
蓝新 <scutryzheng@gmail.com>
2021-03-25 13:14:21 +0800

    rm useless ensureNullOperation when loading JS Bundle

--- React/CxxBridge/NSDataBigString.mm
+++ React/CxxBridge/NSDataBigString.mm
@@ -10,33 +10,10 @@
 namespace facebook {
 namespace react {
 
-static NSData *ensureNullTerminated(NSData *source)
-{
-  if (!source || source.length == 0) {
-    return nil;
-  }
-
-  NSUInteger sourceLength = source.length;
-  unsigned char lastByte;
-  [source getBytes:&lastByte range:NSMakeRange(sourceLength - 1, 1)];
-
-  // TODO: bundles from the packager should always include a NULL byte
-  // or we should we relax this requirement and only read as much from the
-  // buffer as length indicates
-  if (lastByte == '\0') {
-    return source;
-  } else {
-    NSMutableData *data = [source mutableCopy];
-    unsigned char nullByte = '\0';
-    [data appendBytes:&nullByte length:1];
-    return data;
-  }
-}
-
 NSDataBigString::NSDataBigString(NSData *data)
 {
   m_length = [data length];
-  m_data = ensureNullTerminated(data);
+  m_data = data;
 }
 
 }
