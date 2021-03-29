b2988c3762a66955cfbd92db52b7a6691ea75bfb
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2021-03-29 11:25:03 +0200

    adding basic Java Unit test

--- ReactAndroid/src/test/java/com/facebook/react/views/image/ReactImagePropertyTest.java
+++ ReactAndroid/src/test/java/com/facebook/react/views/image/ReactImagePropertyTest.java
@@ -121,6 +121,14 @@ public class ReactImagePropertyTest {
   }
 
   @Test
+  public void testRoundedCorners() {
+    ReactImageManager viewManager = new ReactImageManager();
+    ReactImageView view = viewManager.createViewInstance(mThemeContext);
+    viewManager.setAccessible(view, true);
+    assertEquals(true, view.isFocusable());
+  }
+
+  @Test
   public void testTintColor() {
     ReactImageManager viewManager = new ReactImageManager();
     ReactImageView view = viewManager.createViewInstance(mThemeContext);
