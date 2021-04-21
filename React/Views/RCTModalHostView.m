8d4d37851a916ab309303e79842158a976c9d4fb
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-22 01:14:54 +0200

    Revert "Set modalInPresentation if pageSheet or formSheet"
    This reverts commit b9b7d422cfd641c9b2aa169c75831ee75a1de182.

--- React/Views/RCTModalHostView.m
+++ React/Views/RCTModalHostView.m
@@ -132,7 +132,6 @@ RCT_NOT_IMPLEMENTED(-(instancetype)initWithCoder : coder)
     if (self.presentationStyle != UIModalPresentationNone) {
       _modalViewController.modalPresentationStyle = self.presentationStyle;
     }
-    _modalViewController.modalInPresentation = self.presentationStyle != UIModalPresentationFormSheet && self.presentationStyle != UIModalPresentationPageSheet;
     [_delegate presentModalHostView:self withViewController:_modalViewController animated:[self hasAnimationType]];
     _isPresented = YES;
   }
