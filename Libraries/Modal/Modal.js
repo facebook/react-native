40706e0546a5dccb5ef0944023dea2c32d8b46b3
Paige Sun <paigesun@fb.com>
2021-05-25 10:05:47 -0700

    iOS: 6/7 JS only. Remove logic for visible prop in JS, and pass it to native
    Summary: Changelog: [Fabric][iOS] Use non-global event emitter for onDismiss event for <Modal>
    
    Reviewed By: fkgozali
    
    Differential Revision: D28138741
    
    fbshipit-source-id: df9069e523f2e8bd1b85f3d9be55cfd300f89d66

--- Libraries/Modal/Modal.js
+++ Libraries/Modal/Modal.js
@@ -219,7 +219,7 @@ class Modal extends React.Component<Props> {
   }
 
   render(): React.Node {
-    if (this.props.visible !== true) {
+    if (Platform.OS !== 'ios' && this.props.visible !== true) {
       return null;
     }
 
