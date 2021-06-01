7599593b307b71aebe3184233bd9a5f4478b4c88
Dulmandakh <dulmandakh@gmail.com>
2021-06-01 11:26:16 -0700

    Gradle 6.9, Android Gradle Plugin 4.2.1 (#31593)
    Summary:
    Bump Gradle to 6.9 which supports Apple Silicon, also Android Gradle Plugin 4.2.1 which defaults to Java 1.8 so no additional config required.
    
    ## Changelog
    
    [Android] [Changed] - Bump Gradle to 6.9, Android Gradle Plugin to 4.2.1
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/31593
    
    Test Plan: rn-tester builds and runs as expected
    
    Reviewed By: mdvacca
    
    Differential Revision: D28711942
    
    Pulled By: ShikaSD
    
    fbshipit-source-id: 2a4616cd0f17db7616ab29dea1652717f2cd0f6d

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ buildscript {
         }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0")
-        classpath("de.undercouch:gradle-download-task:4.0.2")
+        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("de.undercouch:gradle-download-task:4.1.1")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
