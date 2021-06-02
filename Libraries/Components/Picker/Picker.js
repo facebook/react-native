f40af394634a4d76b977fa23bc39778f4545392f
Kermit Xuan <kermitlx@outlook.com>
2021-06-02 01:51:32 +0000

    remove defaultProps from picker of components

--- Libraries/Components/Picker/Picker.js
+++ Libraries/Components/Picker/Picker.js
@@ -166,24 +166,21 @@ class Picker extends React.Component<PickerProps> {
 
   static Item: typeof PickerItem = PickerItem;
 
-  static defaultProps: {|mode: $TEMPORARY$string<'dialog'>|} = {
-    mode: MODE_DIALOG,
-  };
-
   render(): React.Node {
+    const { mode = MODE_DIALOG, ...rest } = this.props;
     if (Platform.OS === 'ios') {
       /* $FlowFixMe[prop-missing] (>=0.81.0 site=react_native_ios_fb) This
        * suppression was added when renaming suppression sites. */
       /* $FlowFixMe[incompatible-type] (>=0.81.0 site=react_native_ios_fb) This
        * suppression was added when renaming suppression sites. */
-      return <PickerIOS {...this.props}>{this.props.children}</PickerIOS>;
+      return <PickerIOS {...{mode,...rest}}>{this.props.children}</PickerIOS>;
     } else if (Platform.OS === 'android') {
       return (
         /* $FlowFixMe[incompatible-type] (>=0.81.0 site=react_native_android_fb) This
          * suppression was added when renaming suppression sites. */
         /* $FlowFixMe[prop-missing] (>=0.81.0 site=react_native_android_fb) This
          * suppression was added when renaming suppression sites. */
-        <PickerAndroid {...this.props}>{this.props.children}</PickerAndroid>
+        <PickerAndroid {...{mode,...rest}}>{this.props.children}</PickerAndroid>
       );
     } else {
       return <UnimplementedView />;
