caf635176a0b7b7492698b9f3cdd5908f440a37c
Kermit <kermitlx@outlook.com>
2021-06-03 10:08:49 +0800

    chore: optimize code

--- Libraries/Components/Picker/Picker.js
+++ Libraries/Components/Picker/Picker.js
@@ -174,16 +174,14 @@ class Picker extends React.Component<PickerProps> {
        * suppression was added when renaming suppression sites. */
       /* $FlowFixMe[incompatible-type] (>=0.81.0 site=react_native_ios_fb) This
        * suppression was added when renaming suppression sites. */
-      return <PickerIOS {...{mode, ...rest}}>{this.props.children}</PickerIOS>;
+      return <PickerIOS mode={mode} {...rest} />;
     } else if (Platform.OS === 'android') {
       return (
         /* $FlowFixMe[incompatible-type] (>=0.81.0 site=react_native_android_fb) This
          * suppression was added when renaming suppression sites. */
         /* $FlowFixMe[prop-missing] (>=0.81.0 site=react_native_android_fb) This
          * suppression was added when renaming suppression sites. */
-        <PickerAndroid {...{mode, ...rest}}>
-          {this.props.children}
-        </PickerAndroid>
+        <PickerAndroid mode={mode} {...rest} />
       );
     } else {
       return <UnimplementedView />;
