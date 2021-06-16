a10a20105b190e459f53eea6cfaf41320a2c3777
Lorenzo Sciandra <notkelset@kelset.dev>
2021-06-16 16:37:46 +0100

    Revert "Gradle 6.9, Android Gradle Plugin 4.2.1 (#31593)"
    This reverts commit 7599593b307b71aebe3184233bd9a5f4478b4c88.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.1")
-        classpath("de.undercouch:gradle-download-task:4.1.1")
+        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("de.undercouch:gradle-download-task:4.0.2")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
