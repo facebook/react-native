0e08b25284390751e852f89286deb51e281fc715
Lorenzo Sciandra <notkelset@kelset.dev>
2021-06-17 12:11:03 +0100

    [0.65.0-rc.1] Bump version numbers

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -24,7 +24,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(65),
                   RCTVersionPatch: @(0),
-                  RCTVersionPrerelease: @"rc.0",
+                  RCTVersionPrerelease: @"rc.1",
                   };
   });
   return __rnVersion;
