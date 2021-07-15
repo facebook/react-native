03c568f82de37413c0e3c4280de26ea740d7e272
Hubert Gendron <hubert.gendron689@gmail.com>
2021-07-15 11:08:11 -0400

    remove unused imports

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollView.java
@@ -16,8 +16,6 @@ import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
