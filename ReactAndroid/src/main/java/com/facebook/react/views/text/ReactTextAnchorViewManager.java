facebook/react-native

commit 862136a398decff8ad5d4509ca51bd6b893ff851

Revert the suspicious text input change for now
Reviewed By: nickholub

Differential Revision: D3213682

fb-gh-sync-id: 5b3090864c4c50f307de22493c9a7a8cb4687621
fbshipit-source-id: 5b3090864c4c50f307de22493c9a7a8cb4687621

ReactAndroid/src/main/java/com/facebook/react/views/text/ReactTextView.java
@@ -143,7 +143,6 @@ public class ReactTextView extends TextView implements ReactCompoundView {
   @Override
   public void onAttachedToWindow() {
     super.onAttachedToWindow();
-    setTextIsSelectable(mTextIsSelectable);
     if (mContainsImages && getText() instanceof Spanned) {
       Spanned text = (Spanned) getText();
       TextInlineImageSpan[] spans = text.getSpans(0, text.length(), TextInlineImageSpan.class);

ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
@@ -396,7 +396,6 @@ public class ReactEditText extends EditText {
   @Override
   public void onAttachedToWindow() {
     super.onAttachedToWindow();
-    setTextIsSelectable(mTextIsSelectable);
     if (mContainsImages && getText() instanceof Spanned) {
       Spanned text = (Spanned) getText();
       TextInlineImageSpan[] spans = text.getSpans(0, text.length(), TextInlineImageSpan.class);

commit f159764a89125db3d85443d5015a800e236b5011

Reapply fix introduced with commit https://github.com/facebook/react-native/commit/89340f1620295c7ca6baecccc6b6d3befdf45cdb
The fix was reverted due to a regression with commit https://github.com/facebook/react-native/commit/862136a398decff8ad5d4509ca51bd6b893ff851.
I will investigate this change and verify that no regression are
introduced

ReactAndroid/src/main/java/com/facebook/react/views/text/ReactTextAnchorViewManager.java
@@ -87,7 +87,7 @@ public abstract class ReactTextAnchorViewManager<T extends View, C extends React
 
   @ReactProp(name = "selectable")
   public void setSelectable(ReactTextView view, boolean isSelectable) {
-    view.setSelectableText(isSelectable);
+    view.setTextIsSelectable(isSelectable);
   }
 
   @ReactProp(name = "selectionColor", customType = "Color")

ReactAndroid/src/main/java/com/facebook/react/views/text/ReactTextView.java
@@ -56,7 +56,7 @@ public class ReactTextView extends AppCompatTextView implements ReactCompoundVie
   private boolean mAdjustsFontSizeToFit = false;
   private int mLinkifyMaskType = 0;
   private boolean mNotifyOnInlineViewLayout;
-  private boolean mSelectableText = false;
+  private boolean mTextIsSelectable = false;
 
   private ReactViewBackgroundManager mReactBackgroundManager;
   private Spannable mSpanned;
@@ -434,14 +434,16 @@ public class ReactTextView extends AppCompatTextView implements ReactCompoundVie
     }
   }
 
-  public void setSelectableText(boolean value) {
-    mSelectableText = value;
+  @Override
+  public void setTextIsSelectable(boolean selectable) {
+    mTextIsSelectable = selectable;
+    super.setTextIsSelectable(selectable);
   }
 
   @Override
   public void onAttachedToWindow() {
     super.onAttachedToWindow();
-    setTextIsSelectable(mSelectableText);
+    setTextIsSelectable(mTextIsSelectable);
     if (mContainsImages && getText() instanceof Spanned) {
       Spanned text = (Spanned) getText();
       TextInlineImageSpan[] spans = text.getSpans(0, text.length(), TextInlineImageSpan.class);