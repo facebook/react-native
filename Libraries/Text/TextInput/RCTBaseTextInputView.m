defd1a9a7fd92ce07581c470ee2c4b2a1c636954
Adam Gleitman <adam.gleitman@gmail.com>
2021-08-30 13:22:51 -0700

    Bring back macOS _blurOnSubmit check for -[RCTBaseTextInputView textInputShouldReturn]

--- Libraries/Text/TextInput/RCTBaseTextInputView.m
+++ Libraries/Text/TextInput/RCTBaseTextInputView.m
@@ -413,11 +413,17 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithFrame:(CGRect)frame)
   // `onSubmitEditing` is called when "Submit" button
   // (the blue key on onscreen keyboard) did pressed
   // (no connection to any specific "submitting" process).
-  [_eventDispatcher sendTextEventWithType:RCTTextEventTypeSubmit
-                                 reactTag:self.reactTag
-                                     text:[self.backedTextInputView.attributedText.string copy]
-                                      key:nil
-                               eventCount:_nativeEventCount];
+#if TARGET_OS_OSX // [TODO(macOS Candidate ISS#2710739)
+  if (_blurOnSubmit) {
+#endif // ]TODO(macOS Candidate ISS#2710739)
+    [_eventDispatcher sendTextEventWithType:RCTTextEventTypeSubmit
+                                  reactTag:self.reactTag
+                                      text:[self.backedTextInputView.attributedText.string copy]
+                                        key:nil
+                                eventCount:_nativeEventCount];
+#if TARGET_OS_OSX // [TODO(macOS Candidate ISS#2710739)
+  }
+#endif // ]TODO(macOS Candidate ISS#2710739)
 
   return _blurOnSubmit;
 }
