fe133f626dbbaa0efc9febad51ef9e41a1c96a7d
mdaj06 <pillaymeghnath@gmail.com>
2021-09-02 01:30:13 +0530

    reinstated the prop value

--- Libraries/Components/DrawerAndroid/DrawerLayoutAndroid.android.js
+++ Libraries/Components/DrawerAndroid/DrawerLayoutAndroid.android.js
@@ -55,9 +55,9 @@ type Props = $ReadOnly<{|
    *   </DrawerLayoutAndroid>
    * );
    * ```
-   
+   */
   drawerBackgroundColor: ColorValue,
-*/
+
   /**
    * Specifies the side of the screen from which the drawer will slide in.
    */
@@ -170,7 +170,7 @@ class DrawerLayoutAndroid extends React.Component<Props, State> {
 
   render(): React.Node {
     const {
-      drawerBackgroundColor = "white",
+      drawerBackgroundColor = 'white',
       onDrawerStateChanged,
       renderNavigationView,
       onDrawerOpen,
