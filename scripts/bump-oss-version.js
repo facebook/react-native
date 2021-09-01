1594af13bcc51cdda07d38aee2bc0d273f9ff6a4
Luna Wei <luwe@fb.com>
2021-09-01 01:40:01 -0700

    Copy repo-config dependencies for bumping release version
    Summary:
    Changelog: [Internal[Fixed] - Revert, yarn workspaces only used in private packages. Copy dependencies over from repo-config instead
    
    Original commit changeset: 1dd2adc6a036
    
    Reviewed By: fkgozali
    
    Differential Revision: D30599065
    
    fbshipit-source-id: 0efffaaf38bc23bac339e6e1d917736243e1750e

--- scripts/bump-oss-version.js
+++ scripts/bump-oss-version.js
@@ -127,10 +127,12 @@ fs.writeFileSync(
 );
 
 let packageJson = JSON.parse(cat('package.json'));
-const repoConfigJson = JSON.parse(cat('repo-config/package.json'));
 packageJson.version = version;
 delete packageJson.workspaces;
 delete packageJson.private;
+
+// Copy dependencies over from repo-config/package.json
+const repoConfigJson = JSON.parse(cat('repo-config/package.json'));
 packageJson.devDependencies = {...packageJson.devDependencies, ...repoConfigJson.dependencies};
 fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2), 'utf-8');
 
