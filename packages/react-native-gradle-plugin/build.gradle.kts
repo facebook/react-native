ebcdf511a58cf4da4a16dcd5f6c545fa878ea8f6
Nicola Corti <ncor@fb.com>
2021-09-09 06:39:18 -0700

    Add tests for AndroidConfiguration
    Summary:
    Another small diff to setup some testing for our Gradle Plugin.
    Specifically this one is also setting up a test environment where we would be able to exercies
    AGP usages on top of `ProjectBuilder`. This will allow us to verify that an Android project
    is configured correctly for a React Native build.
    
    Changelog:
    [Internal] [Added] - Add tests for AndroidConfiguration
    
    Reviewed By: GijsWeterings
    
    Differential Revision: D30839600
    
    fbshipit-source-id: 10e3b3c0fcf5979c2a0eaf64320f87ae58093fbd

--- packages/react-native-gradle-plugin/build.gradle.kts
+++ packages/react-native-gradle-plugin/build.gradle.kts
@@ -5,6 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import org.gradle.api.internal.classpath.ModuleRegistry
+import org.gradle.configurationcache.extensions.serviceOf
+
 plugins {
   kotlin("jvm") version "1.4.21"
   id("java-gradle-plugin")
@@ -27,5 +30,12 @@ gradlePlugin {
 dependencies {
   implementation(gradleApi())
   implementation("com.android.tools.build:gradle:4.2.2")
+  
   testImplementation("junit:junit:4.13.2")
+
+  testRuntimeOnly(
+    files(
+      serviceOf<ModuleRegistry>().getModule("gradle-tooling-api-builders").classpath.asFiles.first()
+    )
+  )
 }
