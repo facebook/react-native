4b024ad5e1bfb605b6ef45dd1766cf27e46e5569
Cristiano Coelho <cristianocca@hotmail.com>
2021-09-12 18:37:06 -0300

    Use a larger image and bust cache so defaultSource is visible.

--- packages/rn-tester/js/examples/Image/ImageExample.js
+++ packages/rn-tester/js/examples/Image/ImageExample.js
@@ -747,7 +747,9 @@ exports.examples = [
         <Image
           defaultSource={require('../../assets/bunny.png')}
           source={{
-            uri: 'https://origami.design/public/images/bird-logo.png',
+            // Note: Use a large image and bust cache so we can in fact
+            // visualize the `defaultSource` image.
+            uri: fullImage.uri + '?cacheBust=notinCache' + Date.now(),
           }}
           style={styles.base}
         />
