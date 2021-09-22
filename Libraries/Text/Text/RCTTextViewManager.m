9a8d98f22edb15162b0d1b5ae1cf64ec00a6eaaa
Chris Pearson <chripear@microsoft.com>
2021-09-22 12:37:01 -0700

    Export property for RCTTextView also

--- Libraries/Text/Text/RCTTextViewManager.m
+++ Libraries/Text/Text/RCTTextViewManager.m
@@ -34,6 +34,7 @@ RCT_REMAP_SHADOW_PROPERTY(minimumFontScale, minimumFontScale, CGFloat)
 
 RCT_EXPORT_SHADOW_PROPERTY(onTextLayout, RCTDirectEventBlock)
 
+RCT_EXPORT_VIEW_PROPERTY(enableFocusRing, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(selectable, BOOL)
 
 - (void)setBridge:(RCTBridge *)bridge
