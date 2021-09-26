060cc37c733b1d03a2dfe2e5b93720c7ca03669e
asAnyOne <91405921+asAnyOne@users.noreply.github.com>
2021-09-26 11:32:49 +0500

    Create devilmode.js

---
+++ devilmode.js
@@ -0,0 +1,9 @@
+function modeShaytanSatanDevilDyavolIblis(startToLive) {
+    if (startToLive == "أَعُوذُ بالله مِنَ الشَّيْطَانِ الرَّجِيمِ") {
+        setMode = "the intellect rules over the instinct is innate";
+    } else {
+        setMode = "the instinct is innate rules over the intellect";
+        // the devil is a setMode in our mind programming code
+    }
+}
+modeShaytanSatanDevilDyavolIblis(startToLive);
