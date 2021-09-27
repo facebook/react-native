25573db4b9bee422031e541854a060eb3e83437d
Nicola Corti <ncor@fb.com>
2021-09-27 08:21:11 -0700

    Renamed Gradle Plugint to `React`
    Summary:
    Removing the App postfix from the plugin as that is
    a result of the merging. This aligns the plugin ID to the plugin
    class name.
    
    Changelog:
    [Internal] [Changed] - Renamed Gradle Plugint to `React`
    
    Reviewed By: ShikaSD
    
    Differential Revision: D31205318
    
    fbshipit-source-id: 23cd24c91fa8526137045e8537f03f560be939cb

--- packages/react-native-gradle-plugin/src/main/kotlin/com/facebook/react/ReactPlugin.kt
+++ packages/react-native-gradle-plugin/src/main/kotlin/com/facebook/react/ReactPlugin.kt
@@ -19,7 +19,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 
-class ReactAppPlugin : Plugin<Project> {
+class ReactPlugin : Plugin<Project> {
   override fun apply(project: Project) {
     val extension = project.extensions.create("react", ReactExtension::class.java, project)
     applyAppPlugin(project, extension)
