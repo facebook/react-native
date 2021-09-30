e1d9cfa89c06e8872f6352a2d8cac61ed8be3e86
Gustavo Sverzut Barbieri <barbieri@profusion.mobi>
2021-09-30 18:00:15 -0300

    NativeAnimatedHelper: Improve warning to use "bundle exec"
    Since we're now providing a Gemfile, use "bundle exec" so we have the
    user to get the correct versions when executing a command.

--- Libraries/Animated/NativeAnimatedHelper.js
+++ Libraries/Animated/NativeAnimatedHelper.js
@@ -355,7 +355,7 @@ function shouldUseNativeDriver(
           'animated module is missing. Falling back to JS-based animation. To ' +
           'resolve this, add `RCTAnimation` module to this app, or remove ' +
           '`useNativeDriver`. ' +
-          'Make sure to run `pod install` first. Read more about autolinking: https://github.com/react-native-community/cli/blob/master/docs/autolinking.md',
+          'Make sure to run `bundle exec pod install` first. Read more about autolinking: https://github.com/react-native-community/cli/blob/master/docs/autolinking.md',
       );
       _warnedMissingNativeAnimated = true;
     }
