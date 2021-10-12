79db4835681f5d0149620ec8e0990411cb882241
Aditya Wasan <adityawasan55@gmail.com>
2021-10-12 03:59:03 -0700

    Add `ACCESS_MEDIA_LOCATION` permission (#32282)
    Summary:
    This PR adds `ACCESS_MEDIA_LOCATION` permission to the PermissionsAndroid library. It fixes https://github.com/facebook/react-native/issues/31953.
    
    It's a `dangerous` permission added in API level 29.
    
    https://developer.android.com/reference/android/Manifest.permission#ACCESS_MEDIA_LOCATION
    
    ## Changelog
    
    <!-- Help reviewers and the release process by writing your own changelog entry. For an example, see:
    https://github.com/facebook/react-native/wiki/Changelog
    -->
    
    [Android] [Added] - Add ACCESS_MEDIA_LOCATION permission to PermisionsAndroid library.
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/32282
    
    Test Plan:
    ```
    PermissionsAndroid.ACCESS_MEDIA_LOCATION === 'android.permission.ACCESS_MEDIA_LOCATION'
    ```
    
    Reviewed By: yungsters
    
    Differential Revision: D31235523
    
    Pulled By: cortinico
    
    fbshipit-source-id: 45fa86b1cdf8bc76453e9df6198c1f7ed923db7c

--- Libraries/PermissionsAndroid/NativePermissionsAndroid.js
+++ Libraries/PermissionsAndroid/NativePermissionsAndroid.js
@@ -44,7 +44,8 @@ export type PermissionType =
   | 'android.permission.WRITE_EXTERNAL_STORAGE'
   | 'android.permission.BLUETOOTH_CONNECT'
   | 'android.permission.BLUETOOTH_SCAN'
-  | 'android.permission.BLUETOOTH_ADVERTISE';
+  | 'android.permission.BLUETOOTH_ADVERTISE'
+  | 'android.permission.ACCESS_MEDIA_LOCATION';
 */
 
 export interface Spec extends TurboModule {
