0cf2b3d6ee4e6ebb2b798883f20148853c06ec9e
Anandraj <anandrag@microsoft.com>
2021-10-13 10:22:26 -0700

    Packing RN dependencies, hermes and ignoring javadoc failure,  (#852)
    * Ignore javadoc failure
    
    * Bringing few more changes from 0.63-stable
    
    * Fixing a patch in engine selection
    
    * Fixing a patch in nuget spec
    
    * Fixing the output directory of nuget pack
    
    * Packaging dependencies in the nuget

--- android-patches/patches/V8/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java
+++ android-patches/patches/V8/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java
@@ -1,5 +1,5 @@
---- "E:\\github\\rnm-63-fresh\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2020-10-27 20:26:16.728167300 -0700
-+++ "E:\\github\\rnm-63\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2020-10-13 21:27:45.535631600 -0700
+--- "E:\\gh\\react-native-macos2\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2021-10-12 13:33:12.881732000 -0700
++++ "E:\\gh\\react-native-macos\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\ReactInstanceManagerBuilder.java"	2021-10-12 13:26:43.722189600 -0700
 @@ -26,6 +26,7 @@
  import com.facebook.react.devsupport.interfaces.DevBundleDownloadListener;
  import com.facebook.react.devsupport.interfaces.DevSupportManager;
@@ -8,21 +8,69 @@
  import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
  import com.facebook.react.packagerconnection.RequestHandler;
  import com.facebook.react.uimanager.UIImplementationProvider;
-@@ -291,7 +292,8 @@
+@@ -59,8 +60,21 @@
+   private @Nullable JSIModulePackage mJSIModulesPackage;
+   private @Nullable Map<String, RequestHandler> mCustomPackagerCommandHandlers;
+ 
++  public enum JSEngine {
++    Hermes,
++    V8
++  }
++
++  private JSEngine mJSEngine = JSEngine.V8;
++
+   /* package protected */ ReactInstanceManagerBuilder() {}
+ 
++  public ReactInstanceManagerBuilder setJSEngine(
++      JSEngine jsEngine) {
++    mJSEngine = jsEngine;
++    return this;
++  }
++
+   /** Sets a provider of {@link UIImplementation}. Uses default provider if null is passed. */
+   public ReactInstanceManagerBuilder setUIImplementationProvider(
+       @Nullable UIImplementationProvider uiImplementationProvider) {
+@@ -291,40 +305,10 @@
  
    private JavaScriptExecutorFactory getDefaultJSExecutorFactory(
        String appName, String deviceName, Context applicationContext) {
 -    try {
+-      // If JSC is included, use it as normal
+-      initializeSoLoaderIfNecessary(applicationContext);
+-      SoLoader.loadLibrary("jscexecutor");
+-      return new JSCExecutorFactory(appName, deviceName);
+-    } catch (UnsatisfiedLinkError jscE) {
+-      // https://github.com/facebook/hermes/issues/78 shows that
+-      // people who aren't trying to use Hermes are having issues.
+-      // https://github.com/facebook/react-native/issues/25923#issuecomment-554295179
+-      // includes the actual JSC error in at least one case.
+-      //
+-      // So, if "__cxa_bad_typeid" shows up in the jscE exception
+-      // message, then we will assume that's the failure and just
+-      // throw now.
+-
+-      if (jscE.getMessage().contains("__cxa_bad_typeid")) {
+-        throw jscE;
+-      }
+-
+-      // Otherwise use Hermes
+-      try {
++      if(mJSEngine == JSEngine.V8) {
 +        return new V8ExecutorFactory(appName, deviceName);
-+/*    try {
-       // If JSC is included, use it as normal
-       initializeSoLoaderIfNecessary(applicationContext);
-       SoLoader.loadLibrary("jscexecutor");
-@@ -325,6 +327,6 @@
-         hermesE.printStackTrace();
-         throw jscE;
++      } else {
+         return new HermesExecutorFactory();
+-      } catch (UnsatisfiedLinkError hermesE) {
+-        // If we get here, either this is a JSC build, and of course
+-        // Hermes failed (since it's not in the APK), or it's a Hermes
+-        // build, and Hermes had a problem.
+-
+-        // We suspect this is a JSC issue (it's the default), so we
+-        // will throw that exception, but we will print hermesE first,
+-        // since it could be a Hermes issue and we don't want to
+-        // swallow that.
+-        hermesE.printStackTrace();
+-        throw jscE;
        }
 -    }
-+    } */
    }
  }
