e18cf90d71d0bef2e2a0caf30a89e53129152965
Luna Wei <luwe@fb.com>
2021-10-16 14:08:10 -0700

    [0.67.0-rc.0] Bump version numbers

--- ReactCommon/cxxreact/ReactNativeVersion.h
+++ ReactCommon/cxxreact/ReactNativeVersion.h
@@ -16,9 +16,9 @@ namespace facebook::react {
 
 constexpr struct {
   int32_t Major = 0;
-  int32_t Minor = 0;
+  int32_t Minor = 67;
   int32_t Patch = 0;
-  std::string_view Prerelease = "";
+  std::string_view Prerelease = "rc.0";
 } ReactNativeVersion;
 
 } // namespace facebook::react
