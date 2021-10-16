fe1a87022305e64f2c7e79ff933794ba0d38fc95
Anandraj <anandrag@microsoft.com>
2021-10-16 13:53:42 -0700

    Droid Packging udpates for devmain

--- android-patches/patches/OfficeRNHost/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
+++ android-patches/patches/OfficeRNHost/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
@@ -1,6 +1,6 @@
 --- "E:\\gh\\react-native-macos2\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\bridge\\ReactBridge.java"	2021-10-12 13:35:49.124099000 -0700
 +++ "E:\\gh\\react-native-macos\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\bridge\\ReactBridge.java"	2021-10-12 13:22:46.508704400 -0700
-@@ -31,6 +31,24 @@
+@@ -31,6 +31,25 @@
      Systrace.beginSection(
          TRACE_TAG_REACT_JAVA_BRIDGE, "ReactBridge.staticInit::load:reactnativejni");
      ReactMarker.logMarker(ReactMarkerConstants.LOAD_REACT_NATIVE_SO_FILE_START);
@@ -21,6 +21,7 @@
 +    SoLoader.loadLibrary("fbjni");
 +    SoLoader.loadLibrary("yoga");
 +    SoLoader.loadLibrary("jsinspector");
++    SoLoader.loadLibrary("libreactnativeutilsjni");
 +
      SoLoader.loadLibrary("reactnativejni");
      ReactMarker.logMarker(ReactMarkerConstants.LOAD_REACT_NATIVE_SO_FILE_END);
