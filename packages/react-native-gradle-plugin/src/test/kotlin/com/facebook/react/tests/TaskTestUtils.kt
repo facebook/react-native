fdf869659475ff44a597bd68de17c62aba0001d7
Nicola Corti <ncor@fb.com>
2021-10-18 03:57:10 -0700

    Export prepareJSC to an internal task
    Summary:
    This diff refactors the `prepareJSC` task to a separate Gradle task in the `.internal` package.
    
    The reason for this change is that `prepareJSC` was just a plain `Task` and not a `Copy` task.
    It was defining a top level `doLast` action and would result in being
    invalidated whenever the `build.gradle` file would change. This means that the JSC headers/source files
    would have been extracted again, effectively invalidating the timestamps for the native build.
    
    Changelog:
    [Internal] [Changed] - Export prepareJSC to an internal task
    
    Reviewed By: ShikaSD
    
    Differential Revision: D31682293
    
    fbshipit-source-id: f1a3059413ba662851dee7c8af2da31186c248e5

--- packages/react-native-gradle-plugin/src/test/kotlin/com/facebook/react/tests/TaskTestUtils.kt
+++ packages/react-native-gradle-plugin/src/test/kotlin/com/facebook/react/tests/TaskTestUtils.kt
@@ -7,6 +7,9 @@
 
 package com.facebook.react.tests
 
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.testfixtures.ProjectBuilder
@@ -23,3 +26,18 @@ internal inline fun <reified T : Task> createTestTask(
     taskName: String = T::class.java.simpleName,
     crossinline block: (T) -> Unit = {}
 ): T = project.tasks.register(taskName, T::class.java) { block(it) }.get()
+
+/** A util function to zip a list of files from [contents] inside the zipfile at [destination]. */
+internal fun zipFiles(destination: File, contents: List<File>) {
+  ZipOutputStream(BufferedOutputStream(FileOutputStream(destination.absolutePath))).use { out ->
+    for (file in contents) {
+      FileInputStream(file).use { fi ->
+        BufferedInputStream(fi).use { origin ->
+          val entry = ZipEntry(file.name)
+          out.putNextEntry(entry)
+          origin.copyTo(out, 1024)
+        }
+      }
+    }
+  }
+}
