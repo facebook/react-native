eccbf9b5cb206f83f98a7f5721f3e5728d4390d2
Rubén Norte <rubennorte@fb.com>
2021-10-21 04:27:22 -0700

    Back out "Re-apply: [RN] Make runtime initialization from React renderers a no-op"
    Summary:
    Changelog: [internal]
    
    This change had to be reverted because it caused some issues internally at Facebook. After we solved those we re-applied the changes but this also has issues in OSS apps. Specifically, the option in Metro to inject modules to execute before the EntryPoint (in this case `InitializeCore`) doesn't work if those modules aren't part of the bundle in the first place.
    
    This reverts again so we can think about a long-term solution for this.
    
    Original commit changeset: ba145a30ead5
    
    Reviewed By: ShikaSD
    
    Differential Revision: D31825736
    
    fbshipit-source-id: 393b3d5d2a726951a9ac386dc2b1c19ef4a916a5

--- Libraries/ReactPrivate/ReactNativePrivateInitializeCore.js
+++ Libraries/ReactPrivate/ReactNativePrivateInitializeCore.js
@@ -8,13 +8,4 @@
  * @flow strict-local
  */
 
-// TODO: Remove this module when the import is removed from the React renderers.
-
-// This module is used by React to initialize the React Native runtime,
-// but it is now a no-op.
-
-// This is redundant because all React Native apps are already executing
-// `InitializeCore` before the entrypoint of the JS bundle
-// (see https://github.com/react-native-community/cli/blob/e1da64317a1178c2b262d82c2f14210cdfa3ebe1/packages/cli-plugin-metro/src/tools/loadMetroConfig.ts#L93)
-// and importing it unconditionally from React only prevents users from
-// customizing what they want to include in their apps (re: app size).
+import '../Core/InitializeCore';
