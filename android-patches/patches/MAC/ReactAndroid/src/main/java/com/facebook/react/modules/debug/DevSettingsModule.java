8960e85ae9906fd973becc96c5e392de33a0f8b3
Anandraj <anandrag@microsoft.com>
2021-10-22 11:08:06 -0700

    Some more changes for bringing up RN64 in devmain Android (#861)
    * Build & Packaging changes for bringing up RN64 in devmain
    
    * Fixing gradle clean
    
    * Disable gradle clean in PR builds

---
+++ android-patches/patches/MAC/ReactAndroid/src/main/java/com/facebook/react/modules/debug/DevSettingsModule.java
@@ -0,0 +1,12 @@
+--- "E:\\gh\\react-native-macos2\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\modules\\debug\\DevSettingsModule.java"	2021-08-31 19:52:47.502468000 -0700
++++ "E:\\gh\\react-native-macos\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\modules\\debug\\DevSettingsModule.java"	2021-10-20 19:29:11.611192200 -0700
+@@ -118,4 +118,9 @@
+   public void removeListeners(double count) {
+     // iOS only
+   }
++
++  @Override
++  public void setIsSecondaryClickToShowDevMenuEnabled(boolean enabled) {
++    // macOS only.
++  }
+ }
