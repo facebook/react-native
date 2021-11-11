4ff16c62ae0c10e9ef0c7a26e213c56ea407ef88
Sota Ogo <sota@fb.com>
2021-11-10 19:35:20 -0800

    Add a phase to build third party component provider
    Summary:
    Adds a phase to generate-artifacts.js to generate the third party components provider for fabric. The output path needs to align with where the rest of generated files go (the work is wip).
    
    Changelog:[internal]
    
    Reviewed By: hramos
    
    Differential Revision: D32128834
    
    fbshipit-source-id: 8721b8a19bcf01bb388a293ce17162b4e578a72a

--- scripts/generate-artifacts.js
+++ scripts/generate-artifacts.js
@@ -50,6 +50,11 @@ const CODEGEN_CONFIG_FILENAME = argv.f;
 const CODEGEN_CONFIG_KEY = argv.k;
 const CODEGEN_REPO_PATH = `${RN_ROOT}/packages/react-native-codegen`;
 const CODEGEN_NPM_PATH = `${RN_ROOT}/../react-native-codegen`;
+const CORE_LIBRARIES = new Set(['rncore', 'FBReactNativeSpec']);
+
+function isReactNativeCoreLibrary(libraryName) {
+  return CORE_LIBRARIES.has(libraryName);
+}
 
 function main(appRootDir, outputPath) {
   if (appRootDir == null) {
@@ -148,6 +153,8 @@ function main(appRootDir, outputPath) {
       throw "error: Could not determine react-native-codegen location. Try running 'yarn install' or 'npm install' in your project root.";
     }
 
+    const schemaPaths = {};
+
     // 5. For each codegen-enabled library, generate the native code spec files
     libraries.forEach(library => {
       const tmpDir = fs.mkdtempSync(
@@ -198,7 +205,39 @@ function main(appRootDir, outputPath) {
       fs.mkdirSync(pathToOutputDirIOS, {recursive: true});
       execSync(`cp -R ${pathToTempOutputDir}/* ${pathToOutputDirIOS}`);
       console.log(`[Codegen] Generated artifacts: ${pathToOutputDirIOS}`);
+
+      // Filter the react native core library out.
+      // In the future, core library and third party library should
+      // use the same way to generate/register the fabric components.
+      if (!isReactNativeCoreLibrary(library.config.name)) {
+        schemaPaths[library.config.name] = pathToSchema;
+      }
     });
+
+    console.log('\n\n>>>>> Creating component provider');
+    // Save the list of spec paths to a temp file.
+    const schemaListTmpPath = `${os.tmpdir()}/rn-tmp-schema-list.json`;
+    const fd = fs.openSync(schemaListTmpPath, 'w');
+    fs.writeSync(fd, JSON.stringify(schemaPaths));
+    fs.closeSync(fd);
+    console.log(`Generated schema list: ${schemaListTmpPath}`);
+
+    // Generate FabricComponentProvider.
+    // Only for iOS at this moment.
+    const providerOutputPathIOS = path.join(
+      appRootDir,
+      'build',
+      'generated',
+      'ios',
+    );
+    execSync(
+      `node ${path.join(
+        RN_ROOT,
+        'scripts',
+        'generate-provider-cli.js',
+      )} --platform ios --schemaListPath "${schemaListTmpPath}" --outputDir ${providerOutputPathIOS}`,
+    );
+    console.log(`Generated provider in: ${providerOutputPathIOS}`);
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
