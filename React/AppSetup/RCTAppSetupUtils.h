8f86c16e67445d779e7ae775292c4b11c5befdff
Sota Ogo <sota@fb.com>
2021-12-13 23:07:21 -0800

    Add turbo module support in the default app template
    Summary: Changelog: [internal] Add an optional support for Turbomodule. Define RCT_TM_FABRIC_ENABLED to enable the new architecture.
    
    Reviewed By: philIip
    
    Differential Revision: D33052777
    
    fbshipit-source-id: 139ddd6bb985fed40ef9e5ed302247d99b293824

--- React/AppSetup/RCTAppSetupUtils.h
+++ React/AppSetup/RCTAppSetupUtils.h
@@ -9,9 +9,35 @@
 #import <React/RCTBridge.h>
 #import <React/RCTRootView.h>
 
+#if RCT_TM_FABRIC_ENABLED
+
+#ifndef RCT_USE_HERMES
+#if __has_include(<reacthermes/HermesExecutorFactory.h>)
+#define RCT_USE_HERMES 1
+#else
+#define RCT_USE_HERMES 0
+#endif
+#endif
+
+#if RCT_USE_HERMES
+#import <reacthermes/HermesExecutorFactory.h>
+#else
+#import <React/JSCExecutorFactory.h>
+#endif
+
+#import <ReactCommon/RCTTurboModuleManager.h>
+#endif
+
 @interface RCTAppSetupUtils : NSObject
 + (void)prepareApp:(UIApplication *_Nonnull)application;
 + (RCTRootView *_Nonnull)defaultRootViewWithBridge:(RCTBridge *_Nonnull)bridge
                                         moduleName:(NSString *_Nonnull)moduleName
                                  initialProperties:(nullable NSDictionary *)initialProperties;
+
+#if RCT_TM_FABRIC_ENABLED
++ (id<RCTTurboModule> _Nonnull)defaultModuleInstanceFromClass:(Class _Nonnull)moduleClass;
++ (std::unique_ptr<facebook::react::JSExecutorFactory>)
+    defaultJsExecutorFactoryForBridge:(RCTBridge *_Nonnull)bridge
+               withTurboModuleManager:(RCTTurboModuleManager *_Nonnull)turboModuleManager;
+#endif
 @end
