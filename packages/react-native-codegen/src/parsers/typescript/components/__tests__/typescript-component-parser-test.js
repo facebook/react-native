f4e32ac5bfbf6d84ce840eeb87ac3aaa225867b9
Charles Dudley <charlesdudley@fb.com>
2021-12-20 14:15:57 -0800

    Copy Flow parser tests to prepare TypeScript Parser
    Summary:
    These files are directly copied from the flow parser to aid in reviewing the next two Diffs: D33080789 and D33081127.
    
    The only things that were changed during the copy are the import/require paths to ensure this diff could ship independently. (For this diff, these tests will still test the flow parser instead of importing the typescript parser).
    
    Changelog:
    [Internal][Add] - Copy Flow parser tests to aid in Diff review
    
    Reviewed By: RSNara
    
    Differential Revision: D33136296
    
    fbshipit-source-id: 007e18618c9eba13728d19e4e342fbe9642adacc

---
+++ packages/react-native-codegen/src/parsers/typescript/components/__tests__/typescript-component-parser-test.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+react_native
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+const FlowParser = require('../../../flow/index.js');
+const fixtures = require('../__test_fixtures__/fixtures.js');
+const failureFixtures = require('../__test_fixtures__/failures.js');
+jest.mock('fs', () => ({
+  readFileSync: filename => fixtures[filename] || failureFixtures[filename],
+}));
+
+describe('RN Codegen Flow Parser', () => {
+  Object.keys(fixtures)
+    .sort()
+    .forEach(fixtureName => {
+      it(`can generate fixture ${fixtureName}`, () => {
+        const schema = FlowParser.parseFile(fixtureName);
+        const serializedSchema = JSON.stringify(schema, null, 2).replace(
+          /"/g,
+          "'",
+        );
+        expect(serializedSchema).toMatchSnapshot();
+      });
+    });
+
+  Object.keys(failureFixtures)
+    .sort()
+    .forEach(fixtureName => {
+      it(`Fails with error message ${fixtureName}`, () => {
+        expect(() => {
+          FlowParser.parseFile(fixtureName);
+        }).toThrowErrorMatchingSnapshot();
+      });
+    });
+});
