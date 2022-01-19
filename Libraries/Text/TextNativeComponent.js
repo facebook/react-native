a45ffbd538aaabc433c92dd6c3b0454ef10726b9
Saad Najmi <saadnajmi2@gmail.com>
2022-01-19 00:36:52 -0600

    Expose text tooltip (#960)

--- Libraries/Text/TextNativeComponent.js
+++ Libraries/Text/TextNativeComponent.js
@@ -42,6 +42,7 @@ export const NativeText: HostComponent<NativeTextProps> = (createReactNativeComp
       onInlineViewLayout: true,
       dataDetectorType: true,
       android_hyphenationFrequency: true,
+      tooltip: true, // [TODO(macOS GH#774)]
     },
     directEventTypes: {
       topTextLayout: {
