2c29337c8adb6c975fd38c25225cb670deceab7d
Hetan Thakkar <hetanthakkar1@gmail.com>
2022-01-23 20:19:51 +0530

    fix object compare bug

--- Libraries/Components/Touchable/TouchableOpacity.js
+++ Libraries/Components/Touchable/TouchableOpacity.js
@@ -261,53 +261,11 @@ class TouchableOpacity extends React.Component<Props, State> {
 
   componentDidUpdate(prevProps: Props, prevState: State) {
     //function to check equality of two objects
-    let object_equals = (x, y) => {
-      if (x === y) {
-        return true;
-      }
 
-      if (!(x instanceof Object) || !(y instanceof Object)) {
-        return false;
-      }
-
-      if (x.constructor !== y.constructor) {
-        return false;
-      }
-
-      for (var p in x) {
-        if (!x.hasOwnProperty(p)) {
-          continue;
-        }
-
-        if (!y.hasOwnProperty(p)) {
-          return false;
-        }
-
-        if (x[p] === y[p]) {
-          continue;
-        }
-
-        if (typeof x[p] !== 'object') {
-          return false;
-        }
-
-        if (!object_equals(x[p], y[p])) {
-          return false;
-        }
-      }
-
-      for (p in y) {
-        if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) {
-          return false;
-        }
-      }
-
-      return true;
-    };
     this.state.pressability.configure(this._createPressabilityConfig());
     if (
       this.props.disabled !== prevProps.disabled ||
-      !object_equals(this.props.style, prevProps.style)
+      JSON.stringify(this.props.style) !== JSON.stringify(prevProps.style)
     ) {
       this._opacityInactive(250);
     }
