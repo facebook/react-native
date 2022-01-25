a51a1d1e9d00173f4fe7dee345d49c563e665668
Saad Najmi <saadnajmi2@gmail.com>
2022-01-25 15:46:57 -0600

    Remove postinstall script (#942)
    * add pull yml
    
    * match handleOpenURLNotification event payload with iOS (#755) (#2)
    
    Co-authored-by: Ryan Linton <ryanlntn@gmail.com>
    
    * [pull] master from microsoft:master (#11)
    
    * Deprecated api (#853)
    
    * Remove deprecated/unused context param
    * Update a few Mac deprecated APIs
    
    * Packing RN dependencies, hermes and ignoring javadoc failure,  (#852)
    
    * Ignore javadoc failure
    
    * Bringing few more changes from 0.63-stable
    
    * Fixing a patch in engine selection
    
    * Fixing a patch in nuget spec
    
    * Fixing the output directory of nuget pack
    
    * Packaging dependencies in the nuget
    
    * Fix onMouseEnter/onMouseLeave callbacks not firing on Pressable (#855)
    
    * add pull yml
    
    * match handleOpenURLNotification event payload with iOS (#755) (#2)
    
    Co-authored-by: Ryan Linton <ryanlntn@gmail.com>
    
    * fix mouse evetns on pressable
    
    * delete extra yml from this branch
    
    * Add macOS tags
    
    * reorder props to have onMouseEnter/onMouseLeave always be before onPress
    
    Co-authored-by: pull[bot] <39814207+pull[bot]@users.noreply.github.com>
    Co-authored-by: Ryan Linton <ryanlntn@gmail.com>
    
    * Grammar fixes. (#856)
    
    Updates simple grammar issues.
    
    Co-authored-by: Nick Trescases <42704557+ntre@users.noreply.github.com>
    Co-authored-by: Anandraj <anandrag@microsoft.com>
    Co-authored-by: Saad Najmi <saadnajmi2@gmail.com>
    Co-authored-by: pull[bot] <39814207+pull[bot]@users.noreply.github.com>
    Co-authored-by: Ryan Linton <ryanlntn@gmail.com>
    Co-authored-by: Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
    
    * remove postinstall script
    
    * remove fork specific change
    
    * Update publish.yml
    
    Co-authored-by: pull[bot] <39814207+pull[bot]@users.noreply.github.com>
    Co-authored-by: Ryan Linton <ryanlntn@gmail.com>
    Co-authored-by: Nick Trescases <42704557+ntre@users.noreply.github.com>
    Co-authored-by: Anandraj <anandrag@microsoft.com>
    Co-authored-by: Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>

--- scripts/fixmacscripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# npm publish is currently performed on Windows build agents and loses the executable bit on files.
-# restore the executable bit during npm postinstall
-
-echo "Fixing the executable bit on macOS scripts"
-
-find . -iname \*.sh -exec chmod +x {} \;
-find . -iname \*.command -exec chmod +x {} \;
