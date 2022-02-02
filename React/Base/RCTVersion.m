6e1b535ac7685916b22321d6d1288ecbb21fabd9
Adam Gleitman <adam.gleitman@gmail.com>
2022-02-01 17:52:37 -0800

    Revert version number changes from c89a1497455

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -23,7 +23,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
     __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(66),
-                  RCTVersionPatch: @(8),
+                  RCTVersionPatch: @(0),
                   RCTVersionPrerelease: [NSNull null],
                   };
   });
