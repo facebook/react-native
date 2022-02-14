cadd8890f761d4e2e4a331a0db0d4ce2c9c91d85
Anandraj <anandrag@microsoft.com>
2022-02-14 09:29:13 -0800

    Fixed for Office Android apps (#1022)

---
+++ android-patches/patches/JniUtils/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/jni/Android.mk
@@ -0,0 +1,11 @@
+--- /dev/code/rnm-66-fresh/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/jni/Android.mk	2022-02-13 19:54:48.579686559 -0800
++++ /dev/code/react-native-macos/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/jni/Android.mk	2022-02-13 19:53:04.134612248 -0800
+@@ -19,7 +19,7 @@
+ 
+ LOCAL_CFLAGS += -fexceptions -frtti -std=c++17 -Wall
+ 
+-LOCAL_SHARED_LIBRARIES = libfb libfbjni libreactnativeutilsjni
++LOCAL_SHARED_LIBRARIES = libfb libfbjni libreactnativejni
+ 
+ LOCAL_STATIC_LIBRARIES = libcallinvoker libreactperfloggerjni libruntimeexecutor
+ 
