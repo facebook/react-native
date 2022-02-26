18f7772f58d5a9fe412680dc48deca9fc9c84b61
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-02-26 09:52:22 +0800

    yarn run lint --fix
    https://app.circleci.com/pipelines/github/facebook/react-native/12268/workflows/08bfaf7f-f09e-468b-810c-fd28231d72f6/jobs/237502

--- packages/rn-tester/js/examples/FlatList/FlatList-nested.js
+++ packages/rn-tester/js/examples/FlatList/FlatList-nested.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   SafeAreaView,
   View,
@@ -6,48 +6,48 @@ import {
   StyleSheet,
   Text,
   StatusBar,
-} from "react-native";
+} from 'react-native';
 
 const DATA = [
   {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
   },
   {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
   },
   {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
   },
   {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb8bbb",
-    title: "Fourth Item",
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb8bbb',
+    title: 'Fourth Item',
   },
   {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97676",
-    title: "Fifth Item",
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97676',
+    title: 'Fifth Item',
   },
   {
-    id: "58694a0f-3da1-471f-bd96-145571e27234",
-    title: "Sixth Item",
+    id: '58694a0f-3da1-471f-bd96-145571e27234',
+    title: 'Sixth Item',
   },
   {
-    id: "58694a0f-3da1-471f-bd96-145571e29234",
-    title: "Seven Item",
+    id: '58694a0f-3da1-471f-bd96-145571e29234',
+    title: 'Seven Item',
   },
   {
-    id: "58694a0f-3da1-471f-bd96-145571429234",
-    title: "Eight Item",
+    id: '58694a0f-3da1-471f-bd96-145571429234',
+    title: 'Eight Item',
   },
   {
-    id: "58694a0f-3da1-471f-bd96-115571429234",
-    title: "Nine Item",
+    id: '58694a0f-3da1-471f-bd96-115571429234',
+    title: 'Nine Item',
   },
   {
-    id: "58694a0f-3da1-471f-bd96-1155h1429234",
-    title: "Ten Item",
+    id: '58694a0f-3da1-471f-bd96-1155h1429234',
+    title: 'Ten Item',
   },
 ];
 
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: "#f9c2ff",
+    backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
@@ -94,14 +94,14 @@ const styles = StyleSheet.create({
   },
 });
 
-exports.title = "FlatList Nested";
-exports.testTitle = "Test accessibility announcement in nested flatlist";
-exports.category = "ListView";
-exports.documentationURL = "https://reactnative.dev/docs/flatlist";
-exports.description = "Nested flatlist example";
+exports.title = 'FlatList Nested';
+exports.testTitle = 'Test accessibility announcement in nested flatlist';
+exports.category = 'ListView';
+exports.documentationURL = 'https://reactnative.dev/docs/flatlist';
+exports.description = 'Nested flatlist example';
 exports.examples = [
   {
-    title: "FlatList Nested example",
+    title: 'FlatList Nested example',
     render: function (): React.Element<typeof FlatListNested> {
       return <FlatListNested />;
     },
