2e4c4a035e17b27bf6d46439e8b93cb43b4c2970
Nicola Corti <ncor@fb.com>
2022-03-11 05:12:15 -0800

    Build Hermes from Source (#33396)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/33396
    
    This commit fully unplugs the `ReactAndroid` from using hermes from the NPM package and plugs the usage of Hermes via the `packages/hermes-engine` Gradle build.
    
    I've used prefab to share the .so between the two builds, so we don't need any extra machinery to make this possible.
    
    Moreover, I've added a `buildHermesFromSource` property, which defaults to false when RN is imported, but is set to true when RN is opened for local development. This should allow us to distribute the `react-native` NPM package and users could potentially toggle which source to use (but see below).
    
    Changelog:
    [Android] [Changed] - Build Hermes from Source
    
    Reviewed By: hramos
    
    Differential Revision: D34389875
    
    fbshipit-source-id: e5912d24dc11a2cc7044e3615d2ff4c41fcedd30

--- ReactCommon/cxxreact/Android.mk
+++ ReactCommon/cxxreact/Android.mk
@@ -42,6 +42,9 @@ $(call import-module,jsc)
 $(call import-module,glog)
 $(call import-module,jsi)
 $(call import-module,jsinspector)
-$(call import-module,hermes/inspector)
 $(call import-module,hermes/executor)
 $(call import-module,logger)
+
+ifeq ($(APP_OPTIM),debug)
+  $(call import-module,hermes/inspector)
+endif
