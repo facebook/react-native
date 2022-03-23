12ad1fffe87c0c5ab2e001f318ff4f8d3eda7479
Héctor Ramos <hramos@fb.com>
2022-03-23 16:53:47 -0700

    Build Hermes from source by default on iOS
    Summary:
    Remove BUILD_HERMES_SOURCE gate and default to building Hermes from source on iOS when Hermes is enabled.
    
    Changelog:
    [iOS][Changed] - When Hermes is enabled, the Hermes Engine will be built from source instead of using the pre-built `hermes-engine` CocoaPod.
    
    Reviewed By: cortinico
    
    Differential Revision: D34911987
    
    fbshipit-source-id: 9d6d49498a23f6dae0b97c9f80c689b654db11bd

--- scripts/react_native_pods.rb
+++ scripts/react_native_pods.rb
@@ -110,13 +110,8 @@ def use_react_native! (options={})
 
   if hermes_enabled
     pod 'React-hermes', :path => "#{prefix}/ReactCommon/hermes"
-
-    if ENV['BUILD_HERMES_SOURCE'] == '1'
-      hermes_source_path = downloadAndConfigureHermesSource(prefix)
-      pod 'hermes-engine', :path => "#{hermes_source_path}/hermes-engine.podspec"
-    else
-      pod 'hermes-engine', '~> 0.11.0'
-    end
+    hermes_source_path = downloadAndConfigureHermesSource(prefix)
+    pod 'hermes-engine', :path => "#{hermes_source_path}/hermes-engine.podspec"
     pod 'libevent', '~> 2.1.12'
   end
 
@@ -675,20 +670,6 @@ def downloadAndConfigureHermesSource(react_native_path)
   end
 
   hermes_dir
-end
-
-def locatePathToHermesSource!(react_native_path)
-  return if ENV['BUILD_HERMES_SOURCE'] != '1'
-
-  hermes_source_path = "#{react_native_path}/sdks/hermes"
-
-  if !File.exist?(hermes_source_path)
-    Pod::UI.warn "[Hermes] Error: Hermes source code must be present at #{hermes_source_path} when BUILD_HERMES_SOURCE is enabled."
-    Pod::UI.warn '[Hermes] Verify that the release of React Native that you are using has the Hermes source code included.'
-    exit 1
-  end
-
-  hermes_source_path
 end
 
 # This provides a post_install workaround for build issues related Xcode 12.5 and Apple Silicon (M1) machines.
