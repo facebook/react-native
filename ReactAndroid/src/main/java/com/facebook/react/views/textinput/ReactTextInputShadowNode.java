0ee77ed7a6894c9ef82dc337314984d05c34d8bd
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-04-04 15:13:57 +0800

    adding @VisibleForTesting to PROP_ERROR_MESSAGE

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputShadowNode.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputShadowNode.java
@@ -44,7 +44,7 @@ public class ReactTextInputShadowNode extends ReactBaseTextShadowNode
   @VisibleForTesting public static final String PROP_TEXT = "text";
   @VisibleForTesting public static final String PROP_PLACEHOLDER = "placeholder";
   @VisibleForTesting public static final String PROP_SELECTION = "selection";
-  public static final String PROP_ERROR_MESSAGE = "errorMessageAndroid";
+  @VisibleForTesting public static final String PROP_ERROR_MESSAGE = "errorMessageAndroid";
 
   // Represents the {@code text} property only, not possible nested content.
   private @Nullable String mText = null;
