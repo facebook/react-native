401cdd29b04d0bd80c84e1139557ba2e884f73ce
Hetan Thakkar <hetanthakkar1@gmail.com>
2022-04-04 22:23:39 +0530

    Fixed ios textinput not handling paddingVertical and paddding in style prop

--- Libraries/Components/TextInput/TextInput.js
+++ Libraries/Components/TextInput/TextInput.js
@@ -1225,7 +1225,7 @@ function InternalTextInput(props: Props): React.Node {
 
     const style =
       props.multiline === true
-        ? [styles.multilineInput, props.style]
+        ? StyleSheet.flatten([styles.multilineInput, props.style])
         : props.style;
 
     const useOnChangeSync =
