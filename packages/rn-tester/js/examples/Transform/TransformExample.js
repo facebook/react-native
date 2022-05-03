598c2e02fd0346a7bd2d9502c78be9ddd7bbb49e
Moti Zilberman <moti@fb.com>
2022-05-03 12:04:02 -0700

    Use Flow type inference for interpolation outputRange
    Summary:
    Changelog:
    [Internal]
    
    Cleans up unnecessary type casts / suppressions throughout the codebase following D35869725.
    
    Reviewed By: javache
    
    Differential Revision: D35870027
    
    fbshipit-source-id: eefcb544b19ba93587011cdfd4046d18dddb246e

--- packages/rn-tester/js/examples/Transform/TransformExample.js
+++ packages/rn-tester/js/examples/Transform/TransformExample.js
@@ -38,9 +38,6 @@ function AnimateTansformSingleProp() {
               {
                 rotate: theta.interpolate({
                   inputRange: [0, 100],
-                  /* $FlowFixMe[speculation-ambiguous] (>=0.38.0) - Flow error
-                   * detected during the deployment of v0.38.0. To see the
-                   * error, remove this comment and run flow */
                   outputRange: ['0deg', '360deg'],
                 }),
               },
@@ -79,9 +76,6 @@ function Flip() {
               {
                 rotateX: theta.interpolate({
                   inputRange: [0, 180],
-                  /* $FlowFixMe[speculation-ambiguous] (>=0.38.0) - Flow error
-                   * detected during the deployment of v0.38.0. To see the
-                   * error, remove this comment and run flow */
                   outputRange: ['0deg', '180deg'],
                 }),
               },
@@ -100,9 +94,6 @@ function Flip() {
               {
                 rotateX: theta.interpolate({
                   inputRange: [0, 180],
-                  /* $FlowFixMe[speculation-ambiguous] (>=0.38.0) - Flow error
-                   * detected during the deployment of v0.38.0. To see the
-                   * error, remove this comment and run flow */
                   outputRange: ['180deg', '360deg'],
                 }),
               },
