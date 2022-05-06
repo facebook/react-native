2f63cbcfda9e043f33c3b59519a997f4ff79c7c5
Arinjay Sharma <arinjay@users.noreply.github.com>
2022-05-06 20:10:08 +0530

    Update React/DevSupport/RCTPackagerConnection.mm
    Co-authored-by: Tommy Nguyen <4123478+tido64@users.noreply.github.com>

--- React/DevSupport/RCTPackagerConnection.mm
+++ React/DevSupport/RCTPackagerConnection.mm
@@ -210,7 +210,7 @@ static void eraseRegistrationsWithToken(std::vector<Registration<Handler>> &regi
 
   [self
       addNotificationHandler:^(NSDictionary<NSString *, id> *notification) {
-        [handler handlePackageNotification:notification];
+        [handler handlePackagerNotification:notification];
       }
                        queue:queue
                    forMethod:method];
