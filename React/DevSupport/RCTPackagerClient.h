432e7f22dba1f8797cbadcaf3c748566e7593c97
Arinjay Sharma <arinjay@users.noreply.github.com>
2022-05-06 20:10:16 +0530

    Update React/DevSupport/RCTPackagerClient.h
    Co-authored-by: Tommy Nguyen <4123478+tido64@users.noreply.github.com>

--- React/DevSupport/RCTPackagerClient.h
+++ React/DevSupport/RCTPackagerClient.h
@@ -25,7 +25,7 @@ extern const int RCT_PACKAGER_CLIENT_PROTOCOL_VERSION;
 @protocol RCTPackagerClientMethod <NSObject>
 
 - (void)handleRequest:(NSDictionary<NSString *, id> *)params withResponder:(RCTPackagerClientResponder *)responder;
-- (void)handlePackageNotification:(NSDictionary<NSString *, id> *)params;
+- (void)handlePackagerNotification:(NSDictionary<NSString *, id> *)params;
 
 @optional
 
