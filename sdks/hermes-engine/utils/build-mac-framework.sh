e782bd12e86a7ca907b2d979548f6d3a2d728ab3
Héctor Ramos <hramos@fb.com>
2022-05-12 16:31:48 -0700

    Fix shellcheck warnings in Hermes build scripts
    Summary:
    Fixes some shellcheck warnings that arose in D36295406 (https://github.com/facebook/react-native/commit/ae28880fbc6b5c03c324be7dc0eeb80827ad2299) when the Hermes build scripts were copied over.
    
    Changelog: [Internal]
    
    Reviewed By: cortinico
    
    Differential Revision: D36321074
    
    fbshipit-source-id: 0eae36a4f793daa0e12786ba7989d4eac6b2e256

--- sdks/hermes-engine/utils/build-mac-framework.sh
+++ sdks/hermes-engine/utils/build-mac-framework.sh
@@ -4,7 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# shellcheck source=xplat/js/react-native-github/sdks/hermes-engine/utils/build-apple-framework.sh
 . ./utils/build-apple-framework.sh
+
 if [ ! -d destroot/Library/Frameworks/macosx/hermes.framework ]; then
     mac_deployment_target=$(get_mac_deployment_target)
 
