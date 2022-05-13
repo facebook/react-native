fc767f7ef197f31d1d431cebeac0ecabff5b8e17
Héctor Ramos <hramos@fb.com>
2022-05-12 20:11:59 -0700

    Hermes: Consolidate Hermes build scripts into scripts/hermes/hermes-utils.js
    Summary:
    Currently, the tasks for downloading, expanding, and preparing Hermes to be built for Apple targets are split across the Circle CI and CocoaPods configs.
    
    This diff sets out to consolidate these tasks into a single hermes-utils.js script that can be reused across CI and build systems.
    
    The release script that is used to assign the Hermes tag for a given React Native version has been moved into `scripts/hermes`.
    
    Changelog: [Internal]
    
    Reviewed By: cortinico
    
    Differential Revision: D36329356
    
    fbshipit-source-id: 0222070adf201fa533b607a183471396d45c6caf

--- scripts/hermes/bump-hermes-version.js
+++ scripts/hermes/bump-hermes-version.js
@@ -16,11 +16,7 @@
 const {exit} = require('shelljs');
 const yargs = require('yargs');
 const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
-
-const HERMES_TAG_FILE_DIR = 'sdks';
-const HERMES_TAG_FILE_PATH = `${HERMES_TAG_FILE_DIR}/.hermesversion`;
+const {setHermesTag} = require('./hermes-utils');
 
 let argv = yargs.option('t', {
   alias: 'tag',
@@ -28,32 +24,6 @@ let argv = yargs.option('t', {
     'Hermes release tag to use for this React Native release, ex. hermes-2022-02-21-RNv0.68.0',
   required: true,
 }).argv;
-
-function readHermesTag() {
-  if (fs.existsSync(path)) {
-    const data = fs.readFileSync(HERMES_TAG_FILE_PATH, {
-      encoding: 'utf8',
-      flag: 'r',
-    });
-    return data.trim();
-  } else {
-    return '';
-  }
-}
-
-function setHermesTag(hermesTag) {
-  if (readHermesTag() === hermesTag) {
-    // No need to update.
-    return;
-  }
-
-  if (!fs.existsSync(HERMES_TAG_FILE_DIR)) {
-    fs.mkdirSync(HERMES_TAG_FILE_DIR, {recursive: true});
-  }
-
-  fs.writeFileSync(HERMES_TAG_FILE_PATH, hermesTag.trim());
-  console.log('Hermes tag has been updated. Please commit your changes.');
-}
 
 async function main() {
   const hermesTag = argv.tag;
