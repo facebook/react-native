6eb337c860a5340807ee03e7a185eb095d4a0140
Franco Meloni <fmeloni@fb.com>
2022-05-19 15:13:59 +0200

    Use mocked Pod::UI

--- scripts/cocoapods/__tests__/new_architecture-test.rb
+++ scripts/cocoapods/__tests__/new_architecture-test.rb
@@ -6,8 +6,18 @@
 require "test/unit"
 require_relative "../new_architecture.rb"
 require_relative "./test_utils/InstallerMock.rb"
+require_relative "./test_utils/PodMock.rb"
 
 class NewArchitectureTests < Test::Unit::TestCase
+    def setup
+        File.enable_testing_mode!
+    end
+
+    def teardown
+        Pod::UI.reset()
+    end
+
+
     def test_setClangCxxLanguageStandardIfNeeded_whenReactCoreIsPresent
         installer = prepare_mocked_installer_with_react_core
         set_clang_cxx_language_standard_if_needed(installer)
@@ -15,6 +25,7 @@ class NewArchitectureTests < Test::Unit::TestCase
         assert_equal(installer.aggregate_targets[0].user_project.build_configurations[0].build_settings["CLANG_CXX_LANGUAGE_STANDARD"], "c++17")
         assert_equal(installer.aggregate_targets[1].user_project.build_configurations[0].build_settings["CLANG_CXX_LANGUAGE_STANDARD"], "c++17")
         assert_equal(installer.pods_project.targets[1].received_resolved_build_setting_parameters, [ReceivedCommonResolvedBuildSettings.new("CLANG_CXX_LANGUAGE_STANDARD", true)])
+        assert_equal(Pod::UI.collected_messages, ["Setting CLANG_CXX_LANGUAGE_STANDARD to c++17 on /test/path.xcproj"])
     end
 
     def test_setClangCxxLanguageStandardIfNeeded_whenReactCoreIsNotPresent
@@ -24,6 +35,7 @@ class NewArchitectureTests < Test::Unit::TestCase
         assert_equal(installer.aggregate_targets[0].user_project.build_configurations[0].build_settings["CLANG_CXX_LANGUAGE_STANDARD"], nil)
         assert_equal(installer.aggregate_targets[1].user_project.build_configurations[0].build_settings["CLANG_CXX_LANGUAGE_STANDARD"], nil)
         assert_equal(installer.pods_project.targets[0].received_resolved_build_setting_parameters, [])
+        assert_equal(Pod::UI.collected_messages, [])
     end
 
     def test_setClangCxxLanguageStandardIfNeeded_whenThereAreDifferentValuesForLanguageStandard_takesTheFirstValue
@@ -33,6 +45,7 @@ class NewArchitectureTests < Test::Unit::TestCase
         assert_equal(installer.aggregate_targets[0].user_project.build_configurations[0].build_settings["CLANG_CXX_LANGUAGE_STANDARD"], "c++17")
         assert_equal(installer.aggregate_targets[1].user_project.build_configurations[0].build_settings["CLANG_CXX_LANGUAGE_STANDARD"], "c++17")
         assert_equal(installer.pods_project.targets[1].received_resolved_build_setting_parameters, [ReceivedCommonResolvedBuildSettings.new("CLANG_CXX_LANGUAGE_STANDARD", true)])
+        assert_equal(Pod::UI.collected_messages, ["Setting CLANG_CXX_LANGUAGE_STANDARD to c++17 on /test/path.xcproj"])
     end
 end
 
--- scripts/cocoapods/new_architecture.rb
+++ scripts/cocoapods/new_architecture.rb
@@ -3,8 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-require "cocoapods"
-
 def set_clang_cxx_language_standard_if_needed(installer)
     language_standard = nil
 
