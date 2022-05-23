71692889b0d89b033c07ef87ee3dbf6d62d79235
Riccardo Cipolleschi <cipolleschi@fb.com>
2022-05-23 08:27:44 -0700

    Cleanup Folders after codegen (#33891)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/33891
    
    This diff adds a function to clean up folders and empty files (if any) after the codegen runs in the OSS.
    To align how iOS and Android behave in the codegen, we now have to preemptively create folders that we may not need later.
    Thanks to this function, we recursively the folders in the codegen and we delete them if they are empty.
    
    ## Changelog
    [iOS][Added] - Add function to cleanup codegen folders
    
    Reviewed By: cortinico
    
    Differential Revision: D36594999
    
    fbshipit-source-id: 904aa2442c0d9621b7ec77c31f7be9daa4e7b7e1

--- scripts/codegen/generate-artifacts-executor.js
+++ scripts/codegen/generate-artifacts-executor.js
@@ -355,6 +355,39 @@ function createComponentProvider(
   }
 }
 
+// It removes all the empty files and empty folders
+// it finds, starting from `filepath`, recursively.
+//
+// This function is needed since, after aligning the codegen between
+// iOS and Android, we have to create empty folders in advance and
+// we don't know wheter they will be populated up until the end of the process.
+//
+// @parameter filepath: the root path from which we want to remove the empty files and folders.
+function cleanupEmptyFilesAndFolders(filepath) {
+  const stats = fs.statSync(filepath);
+
+  if (stats.isFile() && stats.size === 0) {
+    fs.rmSync(filepath);
+    return;
+  } else if (stats.isFile()) {
+    return;
+  }
+
+  const dirContent = fs.readdirSync(filepath);
+  dirContent.forEach(contentPath =>
+    cleanupEmptyFilesAndFolders(path.join(filepath, contentPath)),
+  );
+
+  // The original folder may be filled with empty folders
+  // if that the case, we would also like to remove the parent.
+  // Hence, we need to read the folder again.
+  const newContent = fs.readdirSync(filepath);
+  if (newContent.length === 0) {
+    fs.rmdirSync(filepath);
+    return;
+  }
+}
+
 // Execute
 
 /**
@@ -428,6 +461,7 @@ function execute(
     );
 
     createComponentProvider(fabricEnabled, schemaPaths, node, iosOutputDir);
+    cleanupEmptyFilesAndFolders(iosOutputDir);
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
@@ -443,4 +477,5 @@ module.exports = {
   _extractLibrariesFromJSON: extractLibrariesFromJSON,
   _executeNodeScript: executeNodeScript,
   _generateCode: generateCode,
+  _cleanupEmptyFilesAndFolders: cleanupEmptyFilesAndFolders,
 };
