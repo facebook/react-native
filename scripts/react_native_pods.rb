9ee7e213b57b080e51ded1b677894139bb36cd91
Luis Santana <luissantana@microsoft.com>
2022-05-23 12:35:38 -0700

    updated remaining files to bump RTC-Folly pod

--- scripts/react_native_pods.rb
+++ scripts/react_native_pods.rb
@@ -172,7 +172,7 @@ def get_react_codegen_spec(options={})
   end
 
   folly_compiler_flags = '-DFOLLY_NO_CONFIG -DFOLLY_MOBILE=1 -DFOLLY_USE_LIBCPP=1 -Wno-comma -Wno-shorten-64-to-32'
-  folly_version = '2021.06.28.00-v2'
+  folly_version = '2021.07.22.00'
   boost_version = '1.76.0'
   boost_compiler_flags = '-Wno-documentation'
 
