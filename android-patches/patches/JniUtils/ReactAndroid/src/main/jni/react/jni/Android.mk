a12d869437e88b2d223b0839c68d3570f0114420
Adam Gleitman <adam.gleitman@gmail.com>
2022-06-07 15:59:00 -0700

    Fix Android patches for real this time

--- android-patches/patches/JniUtils/ReactAndroid/src/main/jni/react/jni/Android.mk
+++ android-patches/patches/JniUtils/ReactAndroid/src/main/jni/react/jni/Android.mk
@@ -1,11 +1,11 @@
 --- /dev/code/rnm-66-fresh/ReactAndroid/src/main/jni/react/jni/Android.mk	2022-02-13 19:54:48.595686727 -0800
 +++ /dev/code/react-native-macos/ReactAndroid/src/main/jni/react/jni/Android.mk	2022-02-13 19:53:07.962650850 -0800
-@@ -77,7 +77,7 @@
- LOCAL_LDLIBS += -landroid
- 
- # The dynamic libraries (.so files) that this module depends on.
--LOCAL_SHARED_LIBRARIES := libreactnativeutilsjni libfolly_json libfb libfbjni libglog_init libyoga logger libreact_render_runtimescheduler libruntimeexecutor
-+LOCAL_SHARED_LIBRARIES := libreactnativejni libfolly_json libfb libfbjni libglog_init libyoga logger libreact_render_runtimescheduler libruntimeexecutor
- 
- # The static libraries (.a files) that this module depends on.
- LOCAL_STATIC_LIBRARIES := libreactnative libcallinvokerholder
+@@ -90,7 +90,7 @@
+   libfolly_json \
+   libglog_init \
+   libreact_render_runtimescheduler \
+-  libreactnativeutilsjni \
++  libreactnativejni \
+   libruntimeexecutor \
+   libyoga \
+   logger
--- android-patches/patches/JniUtils/ReactCommon/react/renderer/components/switch/Android.mk
+++ android-patches/patches/JniUtils/ReactCommon/react/renderer/components/switch/Android.mk
@@ -1,11 +1,11 @@
 --- /home/mganandraj/code/rnm-66-fresh/ReactCommon/react/renderer/components/switch/Android.mk	2022-02-13 19:54:48.619686978 -0800
 +++ /home/mganandraj/code/react-native-macos/ReactCommon/react/renderer/components/switch/Android.mk	2022-02-13 19:53:25.274826242 -0800
-@@ -21,7 +21,7 @@
- 
- LOCAL_STATIC_LIBRARIES :=
- 
--LOCAL_SHARED_LIBRARIES := libfbjni libreact_codegen_rncore libreactnativeutilsjni libreact_render_componentregistry libreact_render_uimanager libyoga libfolly_futures glog libfolly_json libglog_init libreact_render_core libreact_render_debug libreact_render_graphics librrc_view libreact_debug
-+LOCAL_SHARED_LIBRARIES := libfbjni libreact_codegen_rncore libreactnativejni libreact_render_componentregistry libreact_render_uimanager libyoga libfolly_futures glog libfolly_json libglog_init libreact_render_core libreact_render_debug libreact_render_graphics librrc_view libreact_debug
- 
- include $(BUILD_SHARED_LIBRARY)
+@@ -34,7 +34,7 @@
+   libreact_render_debug \
+   libreact_render_graphics \
+   libreact_render_uimanager \
+-  libreactnativeutilsjni \
++  libreactnativejni \
+   librrc_view \
+   libyoga
  
