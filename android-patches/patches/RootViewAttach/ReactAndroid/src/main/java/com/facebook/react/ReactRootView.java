2fad3bb31156f41c9ea2a231fbbe4c10131b37ff
Adam Gleitman <adam.gleitman@gmail.com>
2022-06-09 15:17:59 -0700

    Fix Android patches

--- android-patches/patches/RootViewAttach/ReactAndroid/src/main/java/com/facebook/react/ReactRootView.java
+++ android-patches/patches/RootViewAttach/ReactAndroid/src/main/java/com/facebook/react/ReactRootView.java
@@ -1,14 +1,24 @@
 --- /dev/code/rnm-66-fresh/ReactAndroid/src/main/java/com/facebook/react/ReactRootView.java	2022-02-13 19:54:48.563686391 -0800
 +++ /dev/code/react-native-macos/ReactAndroid/src/main/java/com/facebook/react/ReactRootView.java	2022-02-13 22:34:45.828345952 -0800
-@@ -440,11 +440,7 @@
+@@ -441,16 +441,12 @@
        mInitialUITemplate = initialUITemplate;
  
        mReactInstanceManager.createReactContextInBackground();
 -      // if in this experiment, we initialize the root earlier in startReactApplication
 -      // instead of waiting for the initial measure
 -      if (ReactFeatureFlags.enableEagerRootViewAttachment) {
+-        if (!mWasMeasured) {
+-          // Ideally, those values will be used by default, but we only update them here to scope
+-          // this change to `enableEagerRootViewAttachment` experiment.
+-          setSurfaceConstraintsToScreenSize();
+-        }
 -        attachToReactInstanceManager();
 -      }
++      if (!mWasMeasured) {
++        // Ideally, those values will be used by default, but we only update them here to scope
++        // this change to `enableEagerRootViewAttachment` experiment.
++        setSurfaceConstraintsToScreenSize();
++      }
 +      attachToReactInstanceManager();
      } finally {
        Systrace.endSection(TRACE_TAG_REACT_JAVA_BRIDGE);
