710d8656e22da2b058ca999e6909e491f4a70a39
Adam Gleitman <adam.gleitman@gmail.com>
2022-06-09 13:03:24 -0700

    Use set-rn-version instead of bump-oss-version in ADO loops

--- scripts/publish-npm.js
+++ scripts/publish-npm.js
@@ -65,7 +65,7 @@ const rawVersion =
     : // For nightly we continue to use 0.0.0 for clarity for npm
     nightlyBuild
     ? '0.0.0'
-    : // For pre-release and stable releases, we use the git tag of the version we're releasing (set in bump-oss-version)
+    : // For pre-release and stable releases, we use the git tag of the version we're releasing (set in set-rn-version)
       buildTag;
 
 let version,
