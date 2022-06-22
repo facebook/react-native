6ddbca8826060322ba1e713ca9370b889071c4ba
Kunal Farmah <kunalfarmah98@gmail.com>
2022-06-22 19:00:41 +0530

    moved jsInterpreter to react package as a stanalone enum class

--- ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java
+++ ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java
@@ -66,16 +66,6 @@ public class ReactInstanceManagerBuilder {
   private @Nullable Map<String, RequestHandler> mCustomPackagerCommandHandlers;
   private @Nullable ReactPackageTurboModuleManagerDelegate.Builder mTMMDelegateBuilder;
   private @Nullable SurfaceDelegateFactory mSurfaceDelegateFactory;
-  /**
-   * An enum that specifies the JS Engine to be used in the app
-   * Old Logic uses the legacy code
-   * JSC/HERMES loads the respective engine using the revamped logic
-   */
-  public enum JSInterpreter {
-    OLD_LOGIC,
-    JSC,
-    HERMES
-  }
   private JSInterpreter jsInterpreter = JSInterpreter.OLD_LOGIC;
 
   /* package protected */ ReactInstanceManagerBuilder() {}
