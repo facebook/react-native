53c8fc94882893dd8c337fd29543ae11fd467267
LeoTM <1881059+leotm@users.noreply.github.com>
2022-07-12 03:57:14 -0700

    Bump AGP from 7.2.0 to 7.2.1 (#34166)
    Summary:
    - Fix: https://github.com/facebook/react-native/issues/34103
    - Follow-up: https://github.com/facebook/react-native/pull/33817
    
    ## Changelog
    
    <!-- Help reviewers and the release process by writing your own changelog entry. For an example, see:
    https://github.com/facebook/react-native/wiki/Changelog
    -->
    
    [Android] [Changed] - Bump Android Gradle Plugin to 7.2.1
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/34166
    
    Test Plan: Everything builds and runs as expected
    
    Reviewed By: javache
    
    Differential Revision: D37747754
    
    Pulled By: cortinico
    
    fbshipit-source-id: b51f26d773ddfbdaf4490f89f3b207a41b225a82

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:7.2.0")
+    classpath("com.android.tools.build:gradle:7.2.1")
     classpath("de.undercouch:gradle-download-task:5.0.1")
     // NOTE: Do not place your application dependencies here; they belong
     // in the individual module build.gradle files
