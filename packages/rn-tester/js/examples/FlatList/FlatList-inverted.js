c47a75a7168bb49e0ff2fba6834fe94c219010a2
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-07-13 14:20:12 +0800

    add/remove button for inverted flatlist example

--- packages/rn-tester/js/examples/FlatList/FlatList-inverted.js
+++ packages/rn-tester/js/examples/FlatList/FlatList-inverted.js
@@ -8,36 +8,118 @@
  * @format
  */
 
-import type {RNTesterModuleExample} from '../../types/RNTesterTypes';
-import BaseFlatListExample from './BaseFlatListExample';
 import * as React from 'react';
+import {useState} from 'react';
+import type {RNTesterModuleExample} from '../../types/RNTesterTypes';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
 
-export function FlatList_inverted(): React.Node {
-  const [output, setOutput] = React.useState('inverted false');
-  const [exampleProps, setExampleProps] = React.useState({
-    inverted: false,
-  });
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb8bbb',
+    title: 'Fourth Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97676',
+    title: 'Fifth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e27234',
+    title: 'Sixth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29234',
+    title: 'Seven Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571429234',
+    title: 'Eight Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-115571429234',
+    title: 'Nine Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-1155h1429234',
+    title: 'Ten Item',
+  },
+];
 
-  const onTest = () => {
-    setExampleProps({
-      inverted: !exampleProps.inverted,
-    });
-    setOutput(`Is inverted: ${(!exampleProps.inverted).toString()}`);
-  };
+const Item = ({title}) => (
+  <Text style={[styles.item, styles.title]}>{title}</Text>
+);
 
+const renderItem = ({item}) => <Item title={item.title} />;
+const ITEM_HEIGHT = 50;
+
+const renderFlatList = ({item}) => <NestedFlatList item={item} />;
+
+function NestedFlatList(props) {
+  const [items, setItems] = useState(DATA);
+  const [disabled, setDisabled] = useState(false);
   return (
-    <BaseFlatListExample
-      exampleProps={exampleProps}
-      testOutput={output}
-      onTest={onTest}
-      testLabel={exampleProps.inverted ? 'Toggle false' : 'Toggle true'}
-    />
+    <View>
+      <Button
+        title="add an item"
+        onPress={() => setItems([...items, {title: 'new item'}])}
+      />
+      <Button
+        title="remove an item"
+        onPress={() => {
+          const newItems = [...items];
+          newItems.splice(items.length - 1, 1);
+          setItems(newItems);
+        }}
+      />
+      <Text>Flatlist</Text>
+      <FlatList
+        inverted
+        style={{height: 400}}
+        renderItem={renderItem}
+        data={items}
+      />
+    </View>
   );
 }
+
+const FlatList_nested = (): React.Node => {
+  return <NestedFlatList />;
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 16,
+  },
+});
 
 export default ({
   title: 'Inverted',
   name: 'inverted',
   description: 'Test inverted prop on FlatList',
-  render: () => <FlatList_inverted />,
+  render: () => <FlatList_nested />,
 }: RNTesterModuleExample);
