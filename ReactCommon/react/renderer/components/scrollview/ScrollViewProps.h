bf54f1cae955df553d2c86083878621005a63af4
Samuel Susla <samuelsusla@fb.com>
2022-07-15 09:54:35 -0700

    Back out "Fix default value for scrollView.contentInsetAdjustmentBehavior"
    Summary:
    changelog: [internal]
    
    Original commit changeset: 290ae428a720
    
    Original Phabricator Diff: D37881453 (https://github.com/facebook/react-native/commit/e98a835bfc7b01bccb1b7cdee94db248a0a90607)
    
    in the original diff I made a mistake. The default value for RN is never, we override it in one of our subclasses.
    
    Reviewed By: cortinico
    
    Differential Revision: D37884715
    
    fbshipit-source-id: 3c5b5ef5550120034e33ae9047292f38159dea3e

--- ReactCommon/react/renderer/components/scrollview/ScrollViewProps.h
+++ ReactCommon/react/renderer/components/scrollview/ScrollViewProps.h
@@ -63,7 +63,7 @@ class ScrollViewProps final : public ViewProps {
   bool snapToStart{true};
   bool snapToEnd{true};
   ContentInsetAdjustmentBehavior contentInsetAdjustmentBehavior{
-      ContentInsetAdjustmentBehavior::Automatic};
+      ContentInsetAdjustmentBehavior::Never};
   bool scrollToOverflowEnabled{false};
 
 #pragma mark - DebugStringConvertible
