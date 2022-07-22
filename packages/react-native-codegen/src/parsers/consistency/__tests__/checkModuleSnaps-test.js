9554517ae758b51c72289ae5dfe24cff722bcb21
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2022-07-22 15:12:25 -0700

    ...

--- packages/react-native-codegen/src/parsers/consistency/__tests__/checkModuleSnaps-test.js
+++ packages/react-native-codegen/src/parsers/consistency/__tests__/checkModuleSnaps-test.js
@@ -17,6 +17,11 @@ const flowFixtures = require('../../flow/modules/__test_fixtures__/fixtures.js')
 const flowSnaps = require('../../flow/modules/__tests__/__snapshots__/module-parser-snapshot-test.js.snap');
 const tsFixtures = require('../../typescript/modules/__test_fixtures__/fixtures.js');
 const tsSnaps = require('../../typescript/modules/__tests__/__snapshots__/typescript-module-parser-snapshot-test.js.snap');
-const tsExtraCases = ['ARRAY2_PROP_TYPES_NO_EVENTS'];
+const tsExtraCases = [
+  'NATIVE_MODULE_WITH_ARRAY2_WITH_ALIAS',
+  'NATIVE_MODULE_WITH_ARRAY2_WITH_UNION_AND_TOUPLE',
+  'NATIVE_MODULE_WITH_BASIC_ARRAY2',
+  'NATIVE_MODULE_WITH_COMPLEX_ARRAY2',
+];
 
 compareSnaps(flowFixtures, flowSnaps, [], tsFixtures, tsSnaps, tsExtraCases);
