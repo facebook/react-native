56ccfeff92a70d75cea448c05258e51547917837
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2022-07-22 15:09:24 -0700

    ...

--- packages/react-native-codegen/src/parsers/consistency/compareSnaps.js
+++ packages/react-native-codegen/src/parsers/consistency/compareSnaps.js
@@ -12,48 +12,46 @@
 'use strict';
 
 function compareSnaps(
-    flowFixtures,
-    flowSnaps,
-    flowExtraCases,
-    tsFixtures,
-    tsSnaps,
-    tsExtraCases,
+  flowFixtures,
+  flowSnaps,
+  flowExtraCases,
+  tsFixtures,
+  tsSnaps,
+  tsExtraCases,
 ) {
-    const flowCases = Object.keys(flowFixtures).sort();
-    const tsCases = Object.keys(tsFixtures).sort();
-    const commonCases = flowCases.filter(name => tsCases.indexOf(name) != -1);
+  const flowCases = Object.keys(flowFixtures).sort();
+  const tsCases = Object.keys(tsFixtures).sort();
+  const commonCases = flowCases.filter(name => tsCases.indexOf(name) != -1);
 
-    describe('RN Codegen Parsers', () => {
-        it(
-            `should not unintentionally contains test case for Flow but not for TypeScript`,
-        )
-            .expect(flowCases.filter(name => commonCases.indexOf(name) == -1))
-            .toEqual(flowExtraCases);
-
-        it(
-            `should not unintentionally contains test case for TypeScript but not for Flow`,
-        )
-            .expect(tsCases.filter(name => commonCases.indexOf(name) == -1))
-            .toEqual(tsExtraCases);
-
-        for (const commonCase of commonCases) {
-            it(
-                `should generate the same snap from Flow and TypeScript for fixture ${commonCase}`,
-            )
-                .expect(
-                    flowSnaps[
-                    `RN Codegen Flow Parser can generate fixture ${commonCase}`
-                    ],
-                )
-                .toEqual(
-                    tsSnaps[
-                    `RN Codegen TypeScript Parser can generate fixture ${commonCase}`
-                    ],
-                );
-        }
+  describe('RN Codegen Parsers', () => {
+    it(`should not unintentionally contains test case for Flow but not for TypeScript`, () => {
+      expect(flowCases.filter(name => commonCases.indexOf(name) == -1)).toEqual(
+        flowExtraCases,
+      );
     });
+
+    it(`should not unintentionally contains test case for TypeScript but not for Flow`, () => {
+      expect(tsCases.filter(name => commonCases.indexOf(name) == -1)).toEqual(
+        tsExtraCases,
+      );
+    });
+
+    for (const commonCase of commonCases) {
+      it(`should generate the same snap from Flow and TypeScript for fixture ${commonCase}`, () => {
+        expect(
+          flowSnaps[
+            `RN Codegen Flow Parser can generate fixture ${commonCase}`
+          ],
+        ).toEqual(
+          tsSnaps[
+            `RN Codegen TypeScript Parser can generate fixture ${commonCase}`
+          ],
+        );
+      });
+    }
+  });
 }
 
 module.exports = {
-    compareSnaps
+  compareSnaps,
 };
