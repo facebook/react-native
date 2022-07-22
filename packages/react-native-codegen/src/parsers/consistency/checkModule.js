a20d7130668010b052c934247440306ab08ec138
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2022-07-22 14:48:39 -0700

    ...

---
+++ packages/react-native-codegen/src/parsers/consistency/checkModule.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+react_native
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+const {compareSnaps} = require('./compareSnaps');
+
+const flowFixtures = require('../flow/modules/__test_fixtures__/fixtures.js');
+const flowSnaps = require('../flow/modules/__test__/__snapshots__/module-parser-snapshot-test.js.snap');
+const tsFixtures = require('../typescript/modules/__test_fixtures__/fixtures.js');
+const tsSnaps = require('../typescript/modules/__test__/__snapshots__/typescript-component-parser-test.js.snap');
+const tsExtraCases = ['ARRAY2_PROP_TYPES_NO_EVENTS'];
+
+compareSnaps(flowFixtures, flowSnaps, [], tsFixtures, tsSnaps, tsExtraCases);
---
+++ packages/react-native-codegen/src/parsers/consistency/compareSnaps.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+react_native
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+export function compareSnaps(
+  flowFixtures,
+  flowSnaps,
+  flowExtraCases,
+  tsFixtures,
+  tsSnaps,
+  tsExtraCases,
+) {
+  const flowCases = Object.keys(flowFixtures).sort();
+  const tsCases = Object.keys(tsFixtures).sort();
+  const commonCases = flowCases.filter(name => tsCases.indexOf(name) != -1);
+
+  describe('RN Codegen Parsers', () => {
+    it(
+      `should not unintentionally contains test case for Flow but not for TypeScript`,
+    )
+      .expect(flowCases.filter(name => commonCases.indexOf(name) == -1))
+      .toEqual(flowExtraCases);
+
+    it(
+      `should not unintentionally contains test case for TypeScript but not for Flow`,
+    )
+      .expect(tsCases.filter(name => commonCases.indexOf(name) == -1))
+      .toEqual(tsExtraCases);
+
+    for (const commonCase of commonCases) {
+      it(
+        `should generate the same snap from Flow and TypeScript for fixture ${commonCase}`,
+      )
+        .expect(
+          flowSnaps[
+            `RN Codegen Flow Parser can generate fixture ${commonCase}`
+          ],
+        )
+        .toEqual(
+          tsSnaps[
+            `RN Codegen TypeScript Parser can generate fixture ${commonCase}`
+          ],
+        );
+    }
+  });
+}
