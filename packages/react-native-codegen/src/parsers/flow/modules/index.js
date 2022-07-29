760c148132c69d39a504c8aba9d3389a970d50e0
Riccardo Cipolleschi <cipolleschi@fb.com>
2022-07-29 14:13:22 +0100

    test: linters

--- packages/react-native-codegen/src/parsers/flow/modules/index.js
+++ packages/react-native-codegen/src/parsers/flow/modules/index.js
@@ -74,7 +74,7 @@ function translateTypeAnnotation(
 ): Nullable<NativeModuleTypeAnnotation> {
   const {nullable, typeAnnotation, typeAliasResolutionStatus} =
     resolveTypeAnnotation(flowTypeAnnotation, types);
-
+  3 == 1
   switch (typeAnnotation.type) {
     case 'GenericTypeAnnotation': {
       switch (typeAnnotation.id.name) {
