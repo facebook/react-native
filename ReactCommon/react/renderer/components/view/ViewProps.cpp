f846ecf90756ae3a9efdef51d577f91cb1e8aa33
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2022-08-02 12:37:34 -0700

    Backport Fix Macros for Windows

--- ReactCommon/react/renderer/components/view/ViewProps.cpp
+++ ReactCommon/react/renderer/components/view/ViewProps.cpp
@@ -252,13 +252,13 @@ ViewProps::ViewProps(
 #define VIEW_EVENT_CASE(eventType, eventString)     \
   case CONSTEXPR_RAW_PROPS_KEY_HASH(eventString): { \
     ViewEvents defaultViewEvents{};                 \
-    events[eventType] = ({                          \
+    events[eventType] = [defaultViewEvents, &value, &context]{    \
       bool res = defaultViewEvents[eventType];      \
       if (value.hasValue()) {                       \
         fromRawValue(context, value, res);          \
       }                                             \
-      res;                                          \
-    });                                             \
+      return res;                                   \
+    }();                                            \
     return;                                         \
   }
 
