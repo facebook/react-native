0bdff2090d694f93bd0a5edaa5837bc22a9fce4e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-08-10 05:30:28 -0700

    adding importantForAccessibility for Text, Button, ImageBackground (#34245)
    Summary:
    Previously published by [grgr-dkrk][2] as [https://github.com/facebook/react-native/issues/31296][3], fixes the following issues:
    
    1) ImportantForAccessibility="no" does not work on Text, Button
    2) ImportantForAccessibility="no-hide-descendants" does not work on Text, Button, or ImageBackground.
    
    Note: On ImageBackground, focus is prevented on the imageBackground node itself, but focus is not prevented on its descendants.
    
    Note: [Button component expected behavior for prop importantForAccessibility][4]
    >Some components like Button seem like atomic units, but they end up rendering a hierarchy of components for example a wrapper View with a Text inside them. Allowing those descendants to become focusable, breaks the model of these being a single component to consider and forcing no-hide-descendants makes sense here.
    
    >The other option is always to render any descendants of these elements with importantForAccessibility="no", so they can never be focusable on their own. This would have the same result, **BUT may potentially cause issues when the descendant content is supposed to automatically get moved up to the focusable ancestor to act as a label** (which is what Talkback does with unfocusable text elements by default).
    
    Note: [importantForAccessibility="no" does not allow screenreader focus on nested Text Components with accessibilityRole="link" or inline Images][5]
    
    fixes https://github.com/facebook/react-native/issues/30850 related https://github.com/facebook/react-native/pull/33690
    
    ## Changelog
    
    [Android] [Fixed] - adding importantForAccessibility for Text, Button, ImageBackground
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/34245
    
    Test Plan:
    1) testing ImageBackground importantForAccessiblity ([link to test][1])
    2) importantForAccessibility="no" does not allow screenreader focus on nested Text Components with accessibilityRole="link" or inline Images ([link to test][5])
    3) testing ImageBackground importantForAccessiblity ([link to test][6])
    4) Button with importantForAccessibility=no ([link to test][7])
    
    [1]: https://github.com/facebook/react-native/pull/31296#issuecomment-1192341626 ""
    [2]: https://github.com/grgr-dkrk "grgr-dkrk"
    [3]: https://github.com/facebook/react-native/pull/31296 "https://github.com/facebook/react-native/issues/31296"
    [4]: https://github.com/facebook/react-native/pull/31296#discussion_r616184584 "expected behaviour with prop importantForAccessibility in Button component"
    [5]: https://github.com/facebook/react-native/issues/30850#issuecomment-1192286477 "importantForAccessibility=no does not allow screenreader focus on nested Text Components with accessibilityRole=link or inline Images"
    [6]: https://github.com/facebook/react-native/pull/34245#issuecomment-1192446124 "testing ImageBackground importantForAccessiblity"
    [7]: https://github.com/facebook/react-native/pull/34245#issuecomment-1192443589 "Button with importantForAccessibility=no"
    
    Reviewed By: cipolleschi
    
    Differential Revision: D38121992
    
    Pulled By: dmitryrykun
    
    fbshipit-source-id: 368b4dcb47d7940274820aa2e39ed5e2ca068821

--- packages/rn-tester/js/examples/Accessibility/AccessibilityExample.js
+++ packages/rn-tester/js/examples/Accessibility/AccessibilityExample.js
@@ -14,6 +14,7 @@ import type {PressEvent} from 'react-native/Libraries/Types/CoreEventTypes';
 
 const React = require('react');
 const {
+  ImageBackground,
   AccessibilityInfo,
   TextInput,
   Button,
@@ -66,6 +67,21 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
+  },
+  container: {
+    flex: 1,
+  },
+  ImageBackground: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
   },
 });
 
@@ -1039,6 +1055,44 @@ class EnabledExamples extends React.Component<{}> {
   }
 }
 
+class ImportantForAccessibilityExamples extends React.Component<{}> {
+  render(): React.Node {
+    return (
+      <View>
+        <RNTesterBlock title="ImageBackground with importantForAccessibility=no-hide-descendants">
+          <View style={styles.container}>
+            <ImageBackground
+              importantForAccessibility="no-hide-descendants"
+              source={require('../../assets/trees.jpg')}
+              resizeMode="cover"
+              style={styles.ImageBackground}>
+              <Text style={styles.text}>not accessible</Text>
+            </ImageBackground>
+          </View>
+        </RNTesterBlock>
+        <RNTesterBlock title="ImageBackground with importantForAccessibility=no">
+          <View style={styles.container}>
+            <ImageBackground
+              importantForAccessibility="no"
+              source={require('../../assets/trees.jpg')}
+              resizeMode="cover"
+              style={styles.ImageBackground}>
+              <Text style={styles.text}>accessible</Text>
+            </ImageBackground>
+          </View>
+        </RNTesterBlock>
+        <RNTesterBlock title="Button with importantForAccessibility=no">
+          <Button
+            title="this is text"
+            importantForAccessibility="no"
+            onPress={() => console.log('pressed')}
+          />
+        </RNTesterBlock>
+      </View>
+    );
+  }
+}
+
 class EnabledExample extends React.Component<
   {
     eventListener:
@@ -1242,6 +1296,12 @@ exports.examples = [
     title: 'Check if these properties are enabled',
     render(): React.Element<typeof EnabledExamples> {
       return <EnabledExamples />;
+    },
+  },
+  {
+    title: 'Testing importantForAccessibility',
+    render(): React.Element<typeof ImportantForAccessibilityExamples> {
+      return <ImportantForAccessibilityExamples />;
     },
   },
   {
