417d3ebad2738f5d3dc7d406e4852ffc2da1aa81
Nicola Corti <ncor@fb.com>
2022-08-16 06:37:13 -0700

    Provide defaults for TurboModuleManagerDelegate and JSIModulePackage
    Summary:
    This is an attempt to relax the need of specifying a custom `TurboModuleManagerDelegate` and a `JSIModulePackage` in new apps for New Architecture.
    Users can just specify the name of the dynamic library to load and they'll default to use the `DefaultTurboModuleManagerDelegate` and `DefaultJSIModulePackage`.
    
    Users will still have to provide a C++ implementation for it for the time being, but this at least removes
    one extra file that we requested them to create and maintain.
    
    If we're fine with this approach, I'll replicate it inside the default template.
    
    Changelog:
    
    [Android] [Added] - Provide defaults for TurboModuleManagerDelegate and JSIModulePackage
    
    Reviewed By: cipolleschi
    
    Differential Revision: D38701180
    
    fbshipit-source-id: 68f2193a2a414bdde7c3b35081f7155a7983fa4a

--- packages/rn-tester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterApplication.java
+++ packages/rn-tester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterApplication.java
@@ -10,37 +10,24 @@ package com.facebook.react.uiapp;
 import android.app.Application;
 import android.content.Context;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import com.facebook.fbreact.specs.SampleTurboModule;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.ReactPackageTurboModuleManagerDelegate;
 import com.facebook.react.TurboReactPackage;
-import com.facebook.react.bridge.JSIModulePackage;
-import com.facebook.react.bridge.JSIModuleProvider;
-import com.facebook.react.bridge.JSIModuleSpec;
-import com.facebook.react.bridge.JSIModuleType;
-import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.UIManager;
 import com.facebook.react.config.ReactFeatureFlags;
-import com.facebook.react.fabric.ComponentFactory;
-import com.facebook.react.fabric.CoreComponentsRegistry;
-import com.facebook.react.fabric.FabricJSIModuleProvider;
-import com.facebook.react.fabric.ReactNativeConfig;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.uiapp.component.MyNativeViewManager;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.uimanager.ViewManagerRegistry;
 import com.facebook.react.views.text.ReactFontManager;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -50,7 +37,7 @@ import java.util.Map;
 public class RNTesterApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+      new DefaultReactNativeHost(this) {
         @Override
         public String getJSMainModuleName() {
           return "packages/rn-tester/js/RNTesterApp.android";
@@ -126,54 +113,9 @@ public class RNTesterApplication extends Application implements ReactApplication
               });
         }
 
-        @Nullable
         @Override
-        protected ReactPackageTurboModuleManagerDelegate.Builder
-            getReactPackageTurboModuleManagerDelegateBuilder() {
-          return new RNTesterTurboModuleManagerDelegate.Builder();
-        }
-
-        @Nullable
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-          return new JSIModulePackage() {
-            @Override
-            public List<JSIModuleSpec> getJSIModules(
-                final ReactApplicationContext reactApplicationContext,
-                final JavaScriptContextHolder jsContext) {
-              final List<JSIModuleSpec> specs = new ArrayList<>();
-
-              // Install the new renderer.
-              specs.add(
-                  new JSIModuleSpec() {
-                    @Override
-                    public JSIModuleType getJSIModuleType() {
-                      return JSIModuleType.UIManager;
-                    }
-
-                    @Override
-                    public JSIModuleProvider<UIManager> getJSIModuleProvider() {
-                      final ComponentFactory componentFactory = new ComponentFactory();
-                      CoreComponentsRegistry.register(componentFactory);
-                      RNTesterComponentsRegistry.register(componentFactory);
-                      final ReactInstanceManager reactInstanceManager = getReactInstanceManager();
-
-                      ViewManagerRegistry viewManagerRegistry =
-                          new ViewManagerRegistry(
-                              reactInstanceManager.getOrCreateViewManagers(
-                                  reactApplicationContext));
-
-                      return new FabricJSIModuleProvider(
-                          reactApplicationContext,
-                          componentFactory,
-                          ReactNativeConfig.DEFAULT_CONFIG,
-                          viewManagerRegistry);
-                    }
-                  });
-
-              return specs;
-            }
-          };
+        public String getDynamicLibraryName() {
+          return BuildConfig.DYNAMIC_LIBRARY_NAME;
         }
       };
 
@@ -183,6 +125,7 @@ public class RNTesterApplication extends Application implements ReactApplication
     ReactFontManager.getInstance().addCustomFont(this, "Rubik", R.font.rubik);
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    SoLoader.loadLibrary(BuildConfig.DYNAMIC_LIBRARY_NAME);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
