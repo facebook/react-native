e11de3f6715571a2a8b13fee188555bb834ba96a
Paige Sun <paigesun@fb.com>
2022-08-26 22:32:27 -0700

    Modularlize RCTBridgeModule.h 2/n - Move RCTTurboModuleRegistry.h to its own file in ReactInternal target
    Summary:
    Changelog: [Internal][iOS] Modularlize RCTBridgeModule.h 1/n -  Move RCTTurboModuleRegistry.h to its own file in ReactInternal target
    
    # Why clean up RCTBridgeModule.h?
    Clean up one unnecessary import of RCTBridgeModule.h.
    
    RCTBridgeModule includes a lot of header files, and this header is imported everywhere. The ultimate goal is that files (especially React Native infra files) should only import only what they need and not import the entirely of RCTBridgeModule.h whenever possible.
    
    This way, certain headers that are Bridge-only can be compiled out of the new architecture with a flag.
    
    Differential Revision: D38971168
    
    fbshipit-source-id: 413fc098e2201e601df739f38d006562295fc3a7

--- React/Base/RCTBridgeModule.h
+++ React/Base/RCTBridgeModule.h
@@ -13,6 +13,7 @@
 
 @class RCTBridge;
 @protocol RCTBridgeMethod;
+@protocol RCTTurboModuleRegistry;
 @class RCTModuleRegistry;
 @class RCTViewRegistry;
 @class RCTCallableJSModules;
@@ -377,29 +378,6 @@ RCT_EXTERN_C_END
  */
 - (void)partialBatchDidFlush;
 
-@end
-
-/**
- * A protocol that allows TurboModules to do lookup on other TurboModules.
- * Calling these methods may cause a module to be synchronously instantiated.
- */
-@protocol RCTTurboModuleRegistry <NSObject>
-- (id)moduleForName:(const char *)moduleName;
-
-/**
- * Rationale:
- * When TurboModules lookup other modules by name, we first check the TurboModule
- * registry to see if a TurboModule exists with the respective name. In this case,
- * we don't want a RedBox to be raised if the TurboModule isn't found.
- *
- * This method is deprecated and will be deleted after the migration from
- * TurboModules to TurboModules is complete.
- */
-- (id)moduleForName:(const char *)moduleName warnOnLookupFailure:(BOOL)warnOnLookupFailure;
-- (BOOL)moduleIsInitialized:(const char *)moduleName;
-
-- (NSArray<NSString *> *)eagerInitModuleNames;
-- (NSArray<NSString *> *)eagerInitMainQueueModuleNames;
 @end
 
 /**
