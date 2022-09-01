d52f7ff636ab6a924e5dab2a59b7d50cea507517
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-09-01 21:41:56 +0200

    Fix `Alert` not showing in an app using `UIScene`
    Summary
    
    In an app using `UIScene`, `Alert` no longer pops up because the window
    that gets created are not attached to a scene.
    
    Changelog
    
    [iOS] [Fixed] - Fix `Alert` not showing in an app using `UIScene`
    
    Test Plan
    
    Use the test plan in #29295. This should not regress that fix.

--- React/CoreModules/RCTAlertController.m
+++ React/CoreModules/RCTAlertController.m
@@ -9,23 +9,7 @@
 
 #import <React/RCTAlertController.h>
 
-@interface RCTAlertController ()
-
-@property (nonatomic, strong) UIWindow *alertWindow;
-
-@end
-
 @implementation RCTAlertController
-
-- (UIWindow *)alertWindow
-{
-  if (_alertWindow == nil) {
-    _alertWindow = [[UIWindow alloc] initWithFrame:RCTSharedApplication().keyWindow.bounds];
-    _alertWindow.rootViewController = [UIViewController new];
-    _alertWindow.windowLevel = UIWindowLevelAlert + 1;
-  }
-  return _alertWindow;
-}
 
 - (void)show:(BOOL)animated completion:(void (^)(void))completion
 {
@@ -34,19 +18,7 @@
         RCTSharedApplication().delegate.window.overrideUserInterfaceStyle ?: UIUserInterfaceStyleUnspecified;
     self.overrideUserInterfaceStyle = style;
   }
-  [self.alertWindow makeKeyAndVisible];
-  [self.alertWindow.rootViewController presentViewController:self animated:animated completion:completion];
-}
-
-- (void)hide
-{
-  [_alertWindow setHidden:YES];
-
-  if (@available(iOS 13, *)) {
-    _alertWindow.windowScene = nil;
-  }
-
-  _alertWindow = nil;
+  [[RCTKeyWindow() rootViewController] presentViewController:self animated:animated completion:completion];
 }
 
 @end
