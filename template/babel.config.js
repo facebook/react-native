4b270f65744605ea61484288d2c112f37d88fdd1
Steven Luscher <steveluscher@users.noreply.github.com>
2022-09-04 16:09:50 -0700

    Make Hermes transform the default in Babel config
    Now that Hermes is the default when you create a new React Native app, let's make the Hermes transforms the default wrt the Babel config.
    
    For instance, if you create a brand new React Native app, then try to do this in code:
    
    ```typescript
    console.log(2n ** 64n);
    ```
    
    The old transform is used by default, despite the fact that Hermes now supports `BigInt` exponentiation:
    
    ```typescript
    console.log(Math.pow(2n, 64n));  // Runtime fatal!
    ```

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      {unstable_transformProfile: 'hermes-stable'},
+    ],
+  ],
 };
