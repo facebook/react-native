e7d75631959f1ce3f50a44f13056eacaf82378ff
Pieter De Baets <pieterdb@fb.com>
2022-09-08 05:35:00 -0700

    Remove deprecated UIImplementationProvider
    Summary:
    UIImplementation(Provider) is not compatible with the new renderer (Fabric) and the relevant constructors have been marked deprecated for a while. While ReactInstanceManagerBuilder creates a default instance, it's not even used by CoreModulesPackage.
    
    Changelog: [Android][Removed] Removed deprecated UIImplementationProvider
    
    Reviewed By: NickGerleman
    
    Differential Revision: D39271916
    
    fbshipit-source-id: 20a3b02843ea9a2ba4bfc1352037239febbafc64

--- ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
@@ -99,7 +99,6 @@ import com.facebook.react.turbomodule.core.TurboModuleManagerDelegate;
 import com.facebook.react.turbomodule.core.interfaces.TurboModuleRegistry;
 import com.facebook.react.uimanager.DisplayMetricsHolder;
 import com.facebook.react.uimanager.ReactRoot;
-import com.facebook.react.uimanager.UIImplementationProvider;
 import com.facebook.react.uimanager.UIManagerHelper;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.views.imagehelper.ResourceDrawableIdHelper;
@@ -225,7 +224,6 @@ public class ReactInstanceManager {
       boolean requireActivity,
       @Nullable NotThreadSafeBridgeIdleDebugListener bridgeIdleDebugListener,
       LifecycleState initialLifecycleState,
-      @Nullable UIImplementationProvider mUIImplementationProvider,
       JSExceptionHandler jSExceptionHandler,
       @Nullable RedBoxHandler redBoxHandler,
       boolean lazyViewManagersEnabled,
@@ -282,7 +280,6 @@ public class ReactInstanceManager {
                   ReactInstanceManager.this.invokeDefaultOnBackPressed();
                 }
               },
-              mUIImplementationProvider,
               lazyViewManagersEnabled,
               minTimeLeftInFrameForNonBatchedOperationMs));
       if (mUseDeveloperSupport) {
