3120ae053f53314f86bbb3ab9bdc2985a2e93976
Nicola Corti <ncor@fb.com>
2022-09-12 04:29:33 -0700

    Update Android-prebuilt.mk to use the correct paths for turbomodule & fabricjni
    Summary:
    When I moved the C++ file around I only updated the Android-prebuilt.cmake
    I haven't update the Android.mk equivalent. I'm doing it here.
    
    Changelog:
    [Internal] [Changed] - Update Android-prebuilt.mk to use the correct paths for turbomodule & fabricjni
    
    Reviewed By: cipolleschi
    
    Differential Revision: D39397567
    
    fbshipit-source-id: f9f412ef4a9402f9528414af24eac06a0657f6f5

--- ReactAndroid/Android-prebuilt.mk
+++ ReactAndroid/Android-prebuilt.mk
@@ -88,7 +88,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := turbomodulejsijni
 LOCAL_SRC_FILES := $(REACT_NDK_EXPORT_DIR)/$(TARGET_ARCH_ABI)/libturbomodulejsijni.so
 LOCAL_EXPORT_C_INCLUDES := \
-  $(REACT_ANDROID_SRC_DIR)/java/com/facebook/react/turbomodule/core/jni
+  $(REACT_ANDROID_SRC_DIR)/jni/react/turbomodule
 include $(PREBUILT_SHARED_LIBRARY)
 
 # react_render_core
@@ -164,7 +164,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := fabricjni
 LOCAL_SRC_FILES := $(REACT_NDK_EXPORT_DIR)/$(TARGET_ARCH_ABI)/libfabricjni.so
 LOCAL_EXPORT_C_INCLUDES := \
-  $(REACT_ANDROID_SRC_DIR)/java/com/facebook/react/fabric/jni
+  $(REACT_ANDROID_SRC_DIR)/jni/react/fabricjni
 include $(PREBUILT_SHARED_LIBRARY)
 
 # react_render_componentregistry
