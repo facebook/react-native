ab5f26bf027144a1ae7cb86c814994e870c4dfdb
Luna Wei <luwe@fb.com>
2022-09-17 07:05:37 -0700

    Back out "Fix `Alert` not showing in an app using `UIScene`"
    Summary:
    Changelog: [Internal] - Revert https://github.com/facebook/react-native/pull/34562
    
    re: [iOS] [Fixed] - Fix Alert not showing in an app using UIScene
    
    Reviewed By: alsun2001
    
    Differential Revision: D39591113
    
    fbshipit-source-id: ba707c11b3fb97eb3a6fee32e57b92403aa8b3d8

--- React/CoreModules/RCTAlertController.m
+++ React/CoreModules/RCTAlertController.m
@@ -9,7 +9,23 @@
 
 #import <React/RCTAlertController.h>
 
+@interface RCTAlertController ()
+
+@property (nonatomic, strong) UIWindow *alertWindow;
+
+@end
+
 @implementation RCTAlertController
+
+- (UIWindow *)alertWindow
+{
+  if (_alertWindow == nil) {
+    _alertWindow = [[UIWindow alloc] initWithFrame:RCTSharedApplication().keyWindow.bounds];
+    _alertWindow.rootViewController = [UIViewController new];
+    _alertWindow.windowLevel = UIWindowLevelAlert + 1;
+  }
+  return _alertWindow;
+}
 
 - (void)show:(BOOL)animated completion:(void (^)(void))completion
 {
@@ -18,7 +34,19 @@
         RCTSharedApplication().delegate.window.overrideUserInterfaceStyle ?: UIUserInterfaceStyleUnspecified;
     self.overrideUserInterfaceStyle = style;
   }
-  [[RCTKeyWindow() rootViewController] presentViewController:self animated:animated completion:completion];
+  [self.alertWindow makeKeyAndVisible];
+  [self.alertWindow.rootViewController presentViewController:self animated:animated completion:completion];
+}
+
+- (void)hide
+{
+  [_alertWindow setHidden:YES];
+
+  if (@available(iOS 13, *)) {
+    _alertWindow.windowScene = nil;
+  }
+
+  _alertWindow = nil;
 }
 
 @end
