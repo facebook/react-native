777e282fd11890fc1387f2657006b681ac85ae97
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-09-21 00:42:54 +0800

    remove comments from github actions

--- packages/rn-tester/js/examples/FlatList/FlatList-inverted.js
+++ packages/rn-tester/js/examples/FlatList/FlatList-inverted.js
@@ -10,128 +10,30 @@
 
 import type {RNTesterModuleExample} from '../../types/RNTesterTypes';
 import BaseFlatListExample from './BaseFlatListExample';
-import {
-  FlatList,
-  Text,
-  View,
-  StyleSheet,
-  ScrollView,
-  Dimensions,
-  Button,
-} from 'react-native';
 import * as React from 'react';
 
-const DATA_SHORT = [
-  {title: 'first item'},
-  {title: 'second item'},
-  {title: 'third item'},
-];
-
-const DATA_LONG = [
-  {title: 'first item'},
-  {title: 'second item'},
-  {title: 'third item'},
-  {title: 'fourth item'},
-  {title: 'sixth item'},
-  {title: 'eight item'},
-  {title: 'ninth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'tenth item'},
-  {title: 'before last item'},
-  {title: 'last item'},
-];
-
-// For this example we mock this value
-// Test the example with talkback enabled, otherwise se this value to false
-const TALKBACK_ENABLED = true;
-
 export function FlatList_inverted(): React.Node {
-  const [flatlistHeight, setFlatlistHeight] = React.useState(null);
-  const [contentHeight, setContentHeight] = React.useState(null);
-  const [items, setItems] = React.useState(DATA_LONG);
-  const renderItem = ({item}) => {
-    return (
-      <View style={{backgroundColor: 'yellow', height: 50}}>
-        <Text>{item.title}</Text>
-      </View>
-    );
+  const [output, setOutput] = React.useState('inverted false');
+  const [exampleProps, setExampleProps] = React.useState({
+    inverted: false,
+  });
+
+  const onTest = () => {
+    setExampleProps({
+      inverted: !exampleProps.inverted,
+    });
+    setOutput(`Is inverted: ${(!exampleProps.inverted).toString()}`);
   };
-  let flatlistStyle = {};
-  if (flatlistHeight !== null && contentHeight !== null && TALKBACK_ENABLED) {
-    const diff = flatlistHeight - contentHeight;
-    if (diff > 0) {
-      // flatlistStyle = {position: 'relative', top: diff};
-      flatlistStyle = {flexDirection: 'column-reverse'};
-    }
-  }
+
   return (
-    <>
-      <Button
-        onPress={() => setItems(items => [...items, {title: 'new item'}])}
-        title="append an item"
-      />
-      <Button
-        onPress={() => setItems(items => items.slice(0, -1))}
-        title="remove item"
-      />
-      <Button
-        title="prepend an item"
-        onPress={() => {
-          setItems([{title: `new item`}, ...items]);
-        }}
-      />
-      <FlatList
-        onLayout={event => {
-          const height = event.nativeEvent.layout.height;
-          setFlatlistHeight(height);
-        }}
-        onContentSizeChange={(width, height) => {
-          setContentHeight(height);
-        }}
-        inverted
-        enabledTalkbackCompatibleInvertedList={true}
-        renderItem={renderItem}
-        data={items}
-        onEndReached={() => console.log('onEndReached')}
-        // style={flatlistStyle}
-        // contentContainerStyle={styles.contentContainerStyle}
-      />
-    </>
+    <BaseFlatListExample
+      exampleProps={exampleProps}
+      testOutput={output}
+      onTest={onTest}
+      testLabel={exampleProps.inverted ? 'Toggle false' : 'Toggle true'}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  contentContainerStyle: {
-    flexDirection: 'column-reverse',
-  },
-});
 
 export default ({
   title: 'Inverted',
