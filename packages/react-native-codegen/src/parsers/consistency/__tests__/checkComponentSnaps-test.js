4fe7ca8e6a1400541caf604f6533c8d44e14b0f3
Riccardo Cipolleschi <cipolleschi@fb.com>
2022-09-26 04:23:49 -0700

    Parse custom NativeState in TypeScript
    Summary:
    This diff is the TS equivalent of D39686251.
    It introduces the possibility to parse a custom Native State in Typescript.
    
    The parsing follows the exact same rules as props, as initial heuristic. This should allow enough customization for the developers who needs a custom state.
    
    Currently, we only support using `interface` for the state and the interface must contain the `NativeState` string in its name.
    
    This diff introduces also tests for the TypeScript parser and it aligns the tests between Flow and TS.
    
    ## Changelog
    
    [General][Added] - Implement custom Native State parsing in TypeScript
    
    Differential Revision: D39811476
    
    fbshipit-source-id: ce8f8862786b41dc19db19d602bee2c56cdd5b8b

--- packages/react-native-codegen/src/parsers/consistency/__tests__/checkComponentSnaps-test.js
+++ packages/react-native-codegen/src/parsers/consistency/__tests__/checkComponentSnaps-test.js
@@ -14,20 +14,11 @@ const {compareSnaps, compareTsArraySnaps} = require('../compareSnaps.js');
 
 const flowFixtures = require('../../flow/components/__test_fixtures__/fixtures.js');
 const flowSnaps = require('../../../../src/parsers/flow/components/__tests__/__snapshots__/component-parser-test.js.snap');
-const flowExtraCases = [
-  //TODO: remove these once we implement TypeScript parser for Custom State
-  'ALL_STATE_TYPES',
-  'ARRAY_STATE_TYPES',
-  'COMMANDS_EVENTS_STATE_TYPES_EXPORTED',
-  'OBJECT_STATE_TYPES',
-];
+const flowExtraCases = [];
 const tsFixtures = require('../../typescript/components/__test_fixtures__/fixtures.js');
 const tsSnaps = require('../../../../src/parsers/typescript/components/__tests__/__snapshots__/typescript-component-parser-test.js.snap');
-const tsExtraCases = ['ARRAY2_PROP_TYPES_NO_EVENTS'].concat([
-  //TODO: remove these once we implement TypeScript parser for Custom State
-  'COMMANDS_AND_EVENTS_TYPES_EXPORTED',
-]);
-const ignoredCases = ['ARRAY_PROP_TYPES_NO_EVENTS'];
+const tsExtraCases = ['ARRAY2_PROP_TYPES_NO_EVENTS', 'ARRAY2_STATE_TYPES'];
+const ignoredCases = ['ARRAY_PROP_TYPES_NO_EVENTS', 'ARRAY_STATE_TYPES'];
 
 compareSnaps(
   flowFixtures,
