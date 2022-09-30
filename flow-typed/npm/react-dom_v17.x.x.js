b846849f697446a66fe9daac28f8614f1be07240
Tim Yung <yungsters@meta.com>
2022-09-30 14:28:48 -0700

    RN: Normalize `flow-typed` Definitions
    Summary:
    Normalizes the `flow-typed` definitions to reflect what is actually intsalled.
    
    Changelog:
    [Internal]
    
    Reviewed By: rickhanlonii
    
    Differential Revision: D39868081
    
    fbshipit-source-id: 85e903bf78f49a9d21e1956ff41c425df0491305

--- flow-typed/npm/react-dom_v17.x.x.js
+++ flow-typed/npm/react-dom_v17.x.x.js
@@ -1,32 +1,31 @@
-/**
- * react-dom v16.x.x libdefs from flow-typed
- *
- * @noformat
- */
+// flow-typed signature: e556c06e721548417501c08b01fec911
+// flow-typed version: ad3adf2de8/react-dom_v17.x.x/flow_>=v0.127.x
+
 declare module 'react-dom' {
+  declare var version: string;
+
   declare function findDOMNode(
-    componentOrElement: Element | ?React$Component<any, any>,
+    componentOrElement: Element | ?React$Component<any, any>
   ): null | Element | Text;
 
   declare function render<ElementType: React$ElementType>(
     element: React$Element<ElementType>,
     container: Element,
-    callback?: () => void,
+    callback?: () => void
   ): React$ElementRef<ElementType>;
 
   declare function hydrate<ElementType: React$ElementType>(
     element: React$Element<ElementType>,
     container: Element,
-    callback?: () => void,
+    callback?: () => void
   ): React$ElementRef<ElementType>;
 
   declare function createPortal(
     node: React$Node,
-    container: Element,
+    container: Element
   ): React$Portal;
 
   declare function unmountComponentAtNode(container: any): boolean;
-  declare var version: string;
 
   declare function unstable_batchedUpdates<A, B, C, D, E>(
     callback: (a: A, b: B, c: C, d: D, e: E) => mixed,
@@ -34,79 +33,107 @@ declare module 'react-dom' {
     b: B,
     c: C,
     d: D,
-    e: E,
+    e: E
   ): void;
+
   declare function unstable_renderSubtreeIntoContainer<
-    ElementType: React$ElementType,
+    ElementType: React$ElementType
   >(
     parentComponent: React$Component<any, any>,
     nextElement: React$Element<ElementType>,
     container: any,
-    callback?: () => void,
+    callback?: () => void
   ): React$ElementRef<ElementType>;
 }
 
 declare module 'react-dom/server' {
-  declare function renderToString(element: React$Node): string;
-  declare function renderToStaticMarkup(element: React$Node): string;
-  declare function renderToNodeStream(element: React$Node): stream$Readable;
-  declare function renderToStaticNodeStream(
-    element: React$Node,
-  ): stream$Readable;
   declare var version: string;
+
+  declare function renderToString(element: React$Node): string;
+
+  declare function renderToStaticMarkup(element: React$Node): string;
+
+  declare function renderToNodeStream(element: React$Node): stream$Readable;
+
+  declare function renderToStaticNodeStream(
+    element: React$Node
+  ): stream$Readable;
 }
 
-type Thenable = { then(resolve: () => mixed, reject?: () => mixed): mixed, ... };
-
 declare module 'react-dom/test-utils' {
-  declare var Simulate: { [eventName: string]: (element: Element, eventData?: Object) => void, ... };
+  declare interface Thenable {
+    then(resolve: () => mixed, reject?: () => mixed): mixed,
+  }
+
+  declare var Simulate: {
+    [eventName: string]: (
+      element: Element,
+      eventData?: { [key: string]: mixed, ... }
+    ) => void,
+    ...
+  };
+
   declare function renderIntoDocument(
-    instance: React$Element<any>,
+    instance: React$Element<any>
   ): React$Component<any, any>;
+
   declare function mockComponent(
     componentClass: React$ElementType,
-    mockTagName?: string,
-  ): Object;
+    mockTagName?: string
+  ): { [key: string]: mixed, ... };
+
   declare function isElement(element: React$Element<any>): boolean;
+
   declare function isElementOfType(
     element: React$Element<any>,
-    componentClass: React$ElementType,
+    componentClass: React$ElementType
   ): boolean;
+
   declare function isDOMComponent(instance: any): boolean;
+
   declare function isCompositeComponent(
-    instance: React$Component<any, any>,
+    instance: React$Component<any, any>
   ): boolean;
+
   declare function isCompositeComponentWithType(
     instance: React$Component<any, any>,
-    componentClass: React$ElementType,
+    componentClass: React$ElementType
   ): boolean;
+
   declare function findAllInRenderedTree(
     tree: React$Component<any, any>,
-    test: (child: React$Component<any, any>) => boolean,
+    test: (child: React$Component<any, any>) => boolean
   ): Array<React$Component<any, any>>;
+
   declare function scryRenderedDOMComponentsWithClass(
     tree: React$Component<any, any>,
-    className: string,
+    className: string
   ): Array<Element>;
+
   declare function findRenderedDOMComponentWithClass(
     tree: React$Component<any, any>,
-    className: string,
+    className: string
   ): ?Element;
+
   declare function scryRenderedDOMComponentsWithTag(
     tree: React$Component<any, any>,
-    tagName: string,
+    tagName: string
   ): Array<Element>;
+
   declare function findRenderedDOMComponentWithTag(
     tree: React$Component<any, any>,
-    tagName: string,
+    tagName: string
   ): ?Element;
+
   declare function scryRenderedComponentsWithType(
     tree: React$Component<any, any>,
-    componentClass: React$ElementType,
+    componentClass: React$ElementType
   ): Array<React$Component<any, any>>;
+
   declare function findRenderedComponentWithType(
     tree: React$Component<any, any>,
-    componentClass: React$ElementType,
+    componentClass: React$ElementType
   ): ?React$Component<any, any>;
+
   declare function act(callback: () => void | Thenable): Thenable;
 }
