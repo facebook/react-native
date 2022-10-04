c1363984f1a418cb1be59d927895a69be7b9ed1e
Yurii Nakonechnyi <inobelar@gmail.com>
2022-10-03 22:04:06 -0700

    Added suppression for warnings about unused parameters in 'publish<Type E>()' func (#1141)
    Summary: Pull Request resolved: https://github.com/facebook/yoga/pull/1141
    
    Reviewed By: yungsters
    
    Differential Revision: D40025354
    
    Pulled By: yungsters
    
    fbshipit-source-id: 6eaaa77b71db95ab0dbc0a4f459c9d85f7e36c42

--- ReactCommon/yoga/yoga/event/event.h
+++ ReactCommon/yoga/yoga/event/event.h
@@ -91,6 +91,9 @@ struct YOGA_EXPORT Event {
   static void publish(const YGNode& node, const TypedData<E>& eventData = {}) {
 #ifdef YG_ENABLE_EVENTS
     publish(node, E, Data{eventData});
+#else
+    (void) node;
+    (void) eventData;
 #endif
   }
 
