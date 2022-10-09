3486ebd1e522789620579d1d9ccd53562ee7a470
Jesse Katsumata <jesse.katsumata@gmail.com>
2022-10-09 23:03:47 +0900

    chore: add unittest for emitInt32

---
+++ packages/react-native-codegen/src/parsers/__tests__/parser-primitive-test.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ * @oncall react_native
+ */
+
+'use-strict';
+
+const {emitInt32} = require('../parsers-primitives.js');
+
+describe('emitInt32', () => {
+  describe('when nullable is true', () => {
+    it('returns nullable type annotation', () => {
+      const result = emitInt32(true);
+      const expected = {
+        type: 'NullableTypeAnnotation',
+        typeAnnotation: {
+          type: 'Int32TypeAnnotation',
+        },
+      };
+
+      expect(result).toEqual(expected);
+    });
+  });
+  describe('when nullable is false', () => {
+    it('returns non nullable type annotation', () => {
+      const result = emitInt32(false);
+      const expected = {
+        type: 'Int32TypeAnnotation',
+      };
+
+      expect(result).toEqual(expected);
+    });
+  });
+});
