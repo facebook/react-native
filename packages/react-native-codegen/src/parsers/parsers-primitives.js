1d6da8c54d46cfc03a2adabb2e105c9a6df72d8c
Tarun Chauhan <tarunrajput1337@gmail.com>
2022-10-09 15:39:04 +0530

    refactor: add emitBoolean to use in BooleanTypeAnnotation

---
+++ packages/react-native-codegen/src/parsers/parsers-primitives.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict
+ */
+
+'use strict';
+
+import type {BooleanTypeAnnotation, Nullable} from '../CodegenSchema';
+
+const {wrapNullable} = require('./parsers-commons');
+
+function emitBoolean(nullable: boolean): Nullable<BooleanTypeAnnotation> {
+  return wrapNullable(nullable, {
+    type: 'BooleanTypeAnnotation',
+  });
+}
+
+module.exports = {
+  emitBoolean,
+};
