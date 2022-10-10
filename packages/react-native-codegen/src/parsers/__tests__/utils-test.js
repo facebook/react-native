fec3edc403dd8bcd10a880677da8433eabe9b3f2
MaeIg <larochmael@hotmail.fr>
2022-10-10 22:33:53 +0200

    chore(utils-test): activate flow

--- packages/react-native-codegen/src/parsers/__tests__/utils-test.js
+++ packages/react-native-codegen/src/parsers/__tests__/utils-test.js
@@ -4,11 +4,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict-local
  * @format
  * @oncall react_native
  */
 
 'use strict';
+
 const {extractNativeModuleName} = require('../utils.js');
 
 describe('extractnativeModuleName', () => {
