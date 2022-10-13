e57a19211d235b659fa74fdc4a5bb0a8f5ebc755
Nicola Corti <ncor@meta.com>
2022-10-13 02:00:35 -0700

    Setup publishing for Snapshot and Stable on Maven (#34967)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/34967
    
    This diff is a preparatory work for publishing artifacts on Maven Central.
    What it does is:
    1. It sets up all the 3 modules (react-native, hermes-engine, external-artifacts) for publishg
    2. Adds coordinates to publish on the Snapshot repository
    3. Adds support for appendign -SNAPSHOT version if invoked with `-PisNightly=true`
    4. Configures GPG signing of artifacts.
    
    I haven't touched the CircleCI and JS code yet. I'll do it in another diff.
    
    Changelog:
    [General] [Changed] - Setup publishing for Snapshot and Stable on Maven
    
    Reviewed By: mdvacca, cipolleschi
    
    Differential Revision: D40146212
    
    fbshipit-source-id: fdd6381896511608e138ac54cd19c7b6cdee8755

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,3 +74,34 @@ tasks.register("downloadAll") {
   dependsOn(":ReactAndroid:hermes-engine:dependencies")
   dependsOn(":ReactAndroid:hermes-engine:androidDependencies")
 }
+
+tasks.register("publishAllInsideNpmPackage") {
+  description =
+      "Publish all the artifacts to be available inside the NPM package in the `android` folder."
+  // Due to size constraints of NPM, we publish only react-native and hermes-engine inside
+  // the NPM package.
+  dependsOn(":ReactAndroid:installArchives")
+  dependsOn(":ReactAndroid:hermes-engine:installArchives")
+}
+
+tasks.register("publishAllToMavenLocal") {
+  description = "Publish all the artifacts to be available inside Maven Local."
+  dependsOn(":ReactAndroid:publishToMavenLocal")
+  dependsOn(":ReactAndroid:external-artifacts:publishToMavenLocal")
+  dependsOn(":ReactAndroid:hermes-engine:publishToMavenLocal")
+}
+
+tasks.register("publishAllToMavenSnapshots") {
+  description =
+      "Publish all the artifacts as a snapshot (used for testing/nightly) on Maven Central."
+  dependsOn(":ReactAndroid:publishAllPublicationsToMavenSnapshotsRepository")
+  dependsOn(":ReactAndroid:external-artifacts:publishAllPublicationsToMavenSnapshotsRepository")
+  dependsOn(":ReactAndroid:hermes-engine:publishAllPublicationsToMavenSnapshotsRepository")
+}
+
+tasks.register("publishAllToMavenCentral") {
+  description = "Publish all the artifacts as a release on Maven Central."
+  dependsOn(":ReactAndroid:publishAllPublicationsToMavenCentralRepository")
+  dependsOn(":ReactAndroid:external-artifacts:publishAllPublicationsToMavenCentralRepository")
+  dependsOn(":ReactAndroid:hermes-engine:publishAllPublicationsToMavenCentralRepository")
+}
