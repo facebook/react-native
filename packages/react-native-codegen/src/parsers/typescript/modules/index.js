918cc1b1d3fceb473a12fd0ff68a0377200223a0
Tarun Chauhan <tarunrajput1337@gmail.com>
2022-10-14 03:09:51 -0700

    extract emitMixedTypeAnnotation to parsers-commons (#34954)
    Summary:
    Create a function emitMixedTypeAnnotation into the parsers-commons.js file to put together this code from [Flow](https://github.com/facebook/react-native/blob/main/packages/react-native-codegen/src/parsers/flow/modules/index.js#L400-L404) and [TypeScript](https://github.com/facebook/react-native/blob/main/packages/react-native-codegen/src/parsers/typescript/modules/index.js#L435-L440).
    
    Part of https://github.com/facebook/react-native/issues/34872
    
    ## Changelog
    
    <!-- Help reviewers and the release process by writing your own changelog entry. For an example, see:
    https://reactnative.dev/contributing/changelogs-in-pull-requests
    -->
    
    [Internal] [Changed] - Extract emitMixedTypeAnnotation into the parsers-commons
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/34954
    
    Test Plan:
    run ```yarn jest react-native-codegen```
    
    <img width="781" alt="image" src="https://user-images.githubusercontent.com/34857453/195291603-f138c64c-1b49-41af-a133-ee366d02706a.png">
    
    Reviewed By: NickGerleman
    
    Differential Revision: D40297103
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: 3ee3569fbfa850ac50df18b74ecc3eefbeb267c9

--- packages/react-native-codegen/src/parsers/typescript/modules/index.js
+++ packages/react-native-codegen/src/parsers/typescript/modules/index.js
@@ -36,6 +36,7 @@ const {
   unwrapNullable,
   wrapNullable,
   assertGenericTypeAnnotationHasExactlyOneTypeParameter,
+  emitMixedTypeAnnotation,
 } = require('../../parsers-commons');
 const {
   emitBoolean,
@@ -429,9 +430,7 @@ function translateTypeAnnotation(
     }
     case 'TSUnknownKeyword': {
       if (cxxOnly) {
-        return wrapNullable(nullable, {
-          type: 'MixedTypeAnnotation',
-        });
+        return emitMixedTypeAnnotation(nullable);
       }
       // Fallthrough
     }
