a2e00393246ab23ff3a3a8bebd572917b5bfd0b2
Mohit Charkha <86604753+mohitcharkha@users.noreply.github.com>
2022-10-17 11:57:57 +0530

    Update packages/react-native-codegen/src/parsers/typescript/modules/index.js

--- packages/react-native-codegen/src/parsers/typescript/modules/index.js
+++ packages/react-native-codegen/src/parsers/typescript/modules/index.js
@@ -72,7 +72,6 @@ const {
   throwIfModuleTypeIsUnsupported,
   throwIfUnusedModuleInterfaceParserError,
   throwIfModuleInterfaceNotFound,
-  throwIfMoreThanOneModuleRegistryCalls,
   throwIfModuleInterfaceIsMisnamed,
   throwIfWrongNumberOfCallExpressionArgs,
   throwIfIncorrectModuleRegistryCallTypeParameterParserError,
