947c2eeacd6bfa1defe55c3491a11f77b2d3da52
William Sawyer <wmsawyer2609@gmail.com>
2022-10-31 14:25:18 +1000

    Corrected documentation to be more gramatically correct

--- Libraries/Utilities/Appearance.d.ts
+++ Libraries/Utilities/Appearance.d.ts
@@ -38,6 +38,6 @@ export namespace Appearance {
 
 /**
  * A new useColorScheme hook is provided as the preferred way of accessing
- * the user's preferred color scheme (aka Dark Mode).
+ * the user's preferred color scheme (e.g. Dark Mode).
  */
 export function useColorScheme(): ColorSchemeName;
