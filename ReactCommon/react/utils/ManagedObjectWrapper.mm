76c7ccaa60c4e820757c5d8cd8ccc80a2cb5f4a0
Christoph Purrer <chpurrer@meta.com>
2022-10-31 05:31:13 -0700

    Enable ManagedObjectWrapper on react-native-macOS (#35146)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/35146
    
    This class is used by Fabric - but does not compile on macOS yet.
    
    Changelog:
    [iOS][Fixed] Make ManagedObjectWrapper compile on macOS
    
    Reviewed By: javache
    
    Differential Revision: D40839241
    
    fbshipit-source-id: 73b93a9963db89af19529fbfd60a64f4e5aaf036

--- ReactCommon/react/utils/ManagedObjectWrapper.mm
+++ ReactCommon/react/utils/ManagedObjectWrapper.mm
@@ -7,8 +7,7 @@
 
 #include "ManagedObjectWrapper.h"
 
-#if defined(__OBJC__) && defined(__cplusplus)
-#if TARGET_OS_MAC && TARGET_OS_IPHONE
+#if TARGET_OS_MAC
 
 namespace facebook {
 namespace react {
@@ -34,5 +33,4 @@ void wrappedManagedObjectDeleter(void *cfPointer) noexcept
 @implementation RCTInternalGenericWeakWrapper
 @end
 
-#endif
 #endif
