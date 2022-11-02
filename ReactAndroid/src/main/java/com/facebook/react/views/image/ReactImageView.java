f3261a5230c79ecf737a9171cf7ea981ae0c8df4
Riccardo Cipolleschi <cipolleschi@meta.com>
2022-11-02 05:16:36 -0700

    Back out "Add unit test for ImageView to take null uri in source"
    Summary:
    Original commit changeset: fd468bfe7c33
    
    Original Phabricator Diff: D40732791 (https://github.com/facebook/react-native/commit/58a1cd236731a8aabf75ae6193acdf1c5bc1e44c)
    
    ## Changelog:
    [Internal]
    
    Differential Revision: D40936633
    
    fbshipit-source-id: 56c7dfbc1d17b3d6b2dc0b61f95d0f6e335dd128

--- ReactAndroid/src/main/java/com/facebook/react/views/image/ReactImageView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/image/ReactImageView.java
@@ -540,11 +540,6 @@ public class ReactImageView extends GenericDraweeView {
     maybeUpdateView();
   }
 
-  // VisibleForTesting
-  public @Nullable ImageSource getImageSource() {
-    return mImageSource;
-  }
-
   @Override
   protected void onSizeChanged(int w, int h, int oldw, int oldh) {
     super.onSizeChanged(w, h, oldw, oldh);
