68f987d4ac16b3e69732c1732312282603fb11bb
Gabriel Donadel Dall'Agnol <donadeldev@gmail.com>
2022-11-08 19:27:48 -0300

    chore: Add getKeyName to MockedParser

--- packages/react-native-codegen/src/parsers/parserMock.js
+++ packages/react-native-codegen/src/parsers/parserMock.js
@@ -13,8 +13,29 @@
 import type {Parser} from './parser';
 import type {ParserType} from './errors';
 
+const {
+  UnsupportedObjectPropertyTypeAnnotationParserError,
+} = require('./errors');
+
 export class MockedParser implements Parser {
   typeParameterInstantiation: string = 'TypeParameterInstantiation';
+
+  getKeyName(propertyOrIndex: $FlowFixMe, hasteModuleName: string): string {
+    switch (propertyOrIndex.type) {
+      case 'ObjectTypeProperty':
+        return propertyOrIndex.key.name;
+      case 'ObjectTypeIndexer':
+        // flow index name is optional
+        return propertyOrIndex.id?.name ?? 'key';
+      default:
+        throw new UnsupportedObjectPropertyTypeAnnotationParserError(
+          hasteModuleName,
+          propertyOrIndex,
+          propertyOrIndex.type,
+          this.language(),
+        );
+    }
+  }
 
   getMaybeEnumMemberType(maybeEnumDeclaration: $FlowFixMe): string {
     return maybeEnumDeclaration.body.type
