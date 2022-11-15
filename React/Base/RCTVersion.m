69ce989307d41a2db9c2f851066e52826c4bd7ad
Lorenzo Sciandra <lsciandra@microsoft.com>
2022-11-15 15:12:45 +0000

    Revert "[0.70.6] Bump version numbers"
    This reverts commit 02a388f0508548dbbf7c61b8b1a98583f82db3e4.

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -23,7 +23,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
     __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(70),
-                  RCTVersionPatch: @(6),
+                  RCTVersionPatch: @(5),
                   RCTVersionPrerelease: [NSNull null],
                   };
   });
