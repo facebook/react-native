8c378c4e542e20f0403f2a16c77f1a0007dc26c3
Saad Najmi <sanajmi@microsoft.com>
2022-11-15 19:17:41 -0800

    Update AnExSet.js

--- packages/rn-tester/js/examples/AnimatedGratuitousApp/AnExSet.js
+++ packages/rn-tester/js/examples/AnimatedGratuitousApp/AnExSet.js
@@ -139,11 +139,9 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 25,
     color: 'white',
-    // [TODO(macOS GH#1409)
     textShadowRadius: 3,
     textShadowColor: 'rgba(0, 0, 0, 1.0)',
     textShadowOffset: {height: 1, width: 0},
-    // ]TODO(macOS GH#1409)
   },
 });
 
