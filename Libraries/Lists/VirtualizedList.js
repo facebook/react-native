7018ce7e573f93f41419e6cc720bebde43280c34
LucioChavezFuentes <luciobertinchavez@gmail.com>
2022-11-18 11:34:01 -0600

    fix VirtualizedList saves items with offsetsequal or below zero and length below zero

--- Libraries/Lists/VirtualizedList.js
+++ Libraries/Lists/VirtualizedList.js
@@ -1181,6 +1181,16 @@ export default class VirtualizedList extends StateSafePureComponent<
       inLayout: true,
     };
     const curr = this._frames[cellKey];
+    // Measures on discarded Low Priority updates will return zero for dimensions in Web, even negative offsets
+    // no cell should have an offset of zero in frames, except for first cell
+    if (
+      next.offset < 0 ||
+      next.length === 0 ||
+      (next.offset === 0 && index !== 0)
+    ) {
+      this._scheduleCellsToRenderUpdate();
+      return;
+    }
     if (
       !curr ||
       next.offset !== curr.offset ||
