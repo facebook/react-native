018727e59dc246aab82d4d91e053828251478a30
Ruslan Shestopalyuk <rshest@fb.com>
2022-12-01 05:28:38 -0800

    Add ability to schedule JS functions as tasks from turbo modules
    Summary:
    [Changelog][Internal]
    
    Adds ability to invoke an async JS callback from a C++ native module, as a task with priority.
    
    This will allow native modules to schedule less important calls to JS thread with lower priority. One example use case is feeding collected perf metrics back to JS.
    
    Differential Revision: https://www.internalfb.com/diff/D41492849?entry_point=27
    
    fbshipit-source-id: fe58ae7039a5984c00c23456623f513c4f9e48da

---
+++ ReactCommon/callinvoker/ReactCommon/SchedulerPriority.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <chrono>
+
+namespace facebook {
+namespace react {
+
+enum class SchedulerPriority : int {
+  ImmediatePriority = 1,
+  UserBlockingPriority = 2,
+  NormalPriority = 3,
+  LowPriority = 4,
+  IdlePriority = 5,
+};
+
+} // namespace react
+} // namespace facebook
