1452a5584558eb9365c323865f64268396786cac
Pieter De Baets <pieterdb@meta.com>
2022-12-02 09:08:16 -0800

    Remove RCTViewConfigEventValidAttributesDisabled
    Summary:
    Changelog: [Internal]
    
    Codemod-bot already hardcoded this to false a while back, so cleaning up the code related to it.
    
    Reviewed By: RSNara
    
    Differential Revision: D41615378
    
    fbshipit-source-id: 7b62b20b8e25f0a06d207b2457d6caf9ad5c695e

--- React/Base/RCTBridge.h
+++ React/Base/RCTBridge.h
@@ -56,11 +56,6 @@ RCT_EXTERN void RCTEnableTurboModuleEagerInit(BOOL enabled);
 RCT_EXTERN BOOL RCTTurboModuleManagerDelegateLockingDisabled(void);
 RCT_EXTERN void RCTDisableTurboModuleManagerDelegateLocking(BOOL enabled);
 
-// Turn off validAttribute: entries inside ViewConfigs for events
-// TODO(109509380): Remove this gating
-RCT_EXTERN BOOL RCTViewConfigEventValidAttributesDisabled(void);
-RCT_EXTERN void RCTDisableViewConfigEventValidAttributes(BOOL disabled);
-
 typedef enum {
   kRCTGlobalScope,
   kRCTGlobalScopeUsingRetainJSCallback,
