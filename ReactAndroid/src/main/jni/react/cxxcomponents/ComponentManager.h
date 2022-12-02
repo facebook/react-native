fb84e092cfcd646fad6e940a5846e65fdfb3d97d
David Vacca <dvacca@meta.com>
2022-12-01 19:29:59 -0800

    unique -> shared pointer in Component
    Summary:
    Refactor unique -> shared pointer in Component. This will be necessary in the next diffs of the stack
    
    changelog: [intenal] internal
    
    Differential Revision: D41629759
    
    fbshipit-source-id: 335161c350692e25ac3443bd19675a89f9df60c4

--- ReactAndroid/src/main/jni/react/cxxcomponents/ComponentManager.h
+++ ReactAndroid/src/main/jni/react/cxxcomponents/ComponentManager.h
@@ -21,7 +21,7 @@ class ComponentManager {
       Tag tag,
       Props::Shared initialProps) = 0;
 
-  virtual std::unique_ptr<Component> createComponent(Tag tag) = 0;
+  virtual std::shared_ptr<Component> createComponent(Tag tag) = 0;
 
   virtual ~ComponentManager() = default;
 };
