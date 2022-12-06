5f9a4e8cb2748eaffd58eb6ecdb7f0a1cbda4c09
Ruslan Shestopalyuk <rshest@meta.com>
2022-12-06 15:33:43 -0800

    Native side implementation for Performance.measure()
    Summary:
    Changelog: [Internal]
    
    This implements the C++ side logic of handling `Performance.measure` calls.
    
    Since measures may refer to earlier logged marks by name, we need to keep track of the former. I also use a fixed size (circular) buffer to prevent the marks from piling forever.
    
    Also adds implementation of clearing marks/measures, as per standard.
    
    Reviewed By: mdvacca
    
    Differential Revision: D41756928
    
    fbshipit-source-id: 5835d71557cc6488cb8ed76f92fbd322f310a94b

--- Libraries/WebPerformance/PerformanceEntryReporter.h
+++ Libraries/WebPerformance/PerformanceEntryReporter.h
@@ -9,15 +9,43 @@
 
 #include <react/bridging/Function.h>
 #include <array>
+#include <functional>
 #include <optional>
+#include <unordered_set>
 #include "NativePerformanceObserver.h"
 
 namespace facebook::react {
 
+struct PerformanceMark {
+  std::string name;
+  double timeStamp;
+};
+
+struct PerformanceMarkHash {
+  size_t operator()(const PerformanceMark *mark) const {
+    return std::hash<std::string>()(mark->name);
+  }
+};
+
+struct PerformanceMarkEqual {
+  bool operator()(const PerformanceMark *lhs, const PerformanceMark *rhs)
+      const {
+    return lhs->name == rhs->name;
+  }
+};
+
+using PerformanceMarkRegistryType = std::
+    unordered_set<PerformanceMark *, PerformanceMarkHash, PerformanceMarkEqual>;
+
+// Only the MARKS_BUFFER_SIZE amount of the latest marks will be kept in
+// memory for the sake of the "Performance.measure" mark name lookup
+constexpr size_t MARKS_BUFFER_SIZE = 1024;
+
 enum class PerformanceEntryType {
   UNDEFINED = 0,
   MARK = 1,
-  _COUNT = 2,
+  MEASURE = 2,
+  _COUNT = 3,
 };
 
 class PerformanceEntryReporter {
@@ -45,13 +73,31 @@ class PerformanceEntryReporter {
   }
 
   void mark(const std::string &name, double startTime, double duration);
+  void clearMarks(const std::optional<std::string> &markName);
+
+  void measure(
+      const std::string &name,
+      double startTime,
+      double endTime,
+      const std::optional<double> &duration,
+      const std::optional<std::string> &startMark,
+      const std::optional<std::string> &endMark);
+  void clearMeasures(const std::optional<std::string> &measureName);
 
  private:
   PerformanceEntryReporter() {}
 
+  double getMarkTime(const std::string &markName) const;
+  void clearEntries(std::function<bool(const RawPerformanceEntry &)> predicate);
+
   std::optional<AsyncCallback<>> callback_;
   std::vector<RawPerformanceEntry> entries_;
   std::array<bool, (size_t)PerformanceEntryType::_COUNT> reportingType_{false};
+
+  // Mark registry for "measure" lookup
+  PerformanceMarkRegistryType marks_registry_;
+  std::array<PerformanceMark, MARKS_BUFFER_SIZE> marks_buffer_;
+  size_t marks_buffer_position_{0};
 };
 
 } // namespace facebook::react
