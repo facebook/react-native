514cfe6f1561c49c7bb9a4a2da3c1b60e82931ca
Shivam Shashank <31573704+ricku44@users.noreply.github.com>
2022-12-10 08:37:07 +0530

    Update RCTModalHostViewManager.m
    IOS Modal transition issue fix

--- React/Views/RCTModalHostViewManager.m
+++ React/Views/RCTModalHostViewManager.m
@@ -47,6 +47,8 @@ RCT_ENUM_CONVERTER(
 
 @interface RCTModalHostViewManager () <RCTModalHostViewInteractor>
 
+@property (nonatomic, copy) dispatch_block_t dismissWaitingBlock;
+
 @end
 
 @implementation RCTModalHostViewManager {
@@ -78,9 +80,26 @@ RCT_EXPORT_MODULE()
   if (_presentationBlock) {
     _presentationBlock([modalHostView reactViewController], viewController, animated, completionBlock);
   } else {
-    [[modalHostView reactViewController] presentViewController:viewController
-                                                      animated:animated
-                                                    completion:completionBlock];
+    dispatch_block_t presentBlock = ^{
+          __weak typeof(self) weakself = self;
+          [[modalHostView reactViewController] presentViewController:viewController
+                                                            animated:animated
+                                                          completion:^{
+              dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                  !completionBlock ?: completionBlock();
+                  __strong typeof(weakself) strongself = weakself;
+                  !strongself.dismissWaitingBlock ?: strongself.dismissWaitingBlock();
+                  strongself.dismissWaitingBlock = nil;
+              });
+          }];
+      };
+      if ([modalHostView reactViewController].presentedViewController) {
+          [[modalHostView reactViewController] dismissViewControllerAnimated:NO completion:^{
+              !presentBlock?:presentBlock();
+          }];
+      } else {
+          !presentBlock?:presentBlock();
+      }
   }
 }
 
@@ -96,7 +115,13 @@ RCT_EXPORT_MODULE()
   if (_dismissalBlock) {
     _dismissalBlock([modalHostView reactViewController], viewController, animated, completionBlock);
   } else {
-    [viewController.presentingViewController dismissViewControllerAnimated:animated completion:completionBlock];
+    self.dismissWaitingBlock = ^{
+      [viewController.presentingViewController dismissViewControllerAnimated:animated completion:nil];
+    };
+    if (viewController.presentingViewController) {
+      self.dismissWaitingBlock();
+      self.dismissWaitingBlock = nil;
+    }
   }
 }
 
