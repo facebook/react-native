8770b2724ae9783b649824249fa252ec7900471d
Nick Gerleman <ngerlem@meta.com>
2022-12-14 15:04:55 -0800

    Mark scrollToEnd animated as optional
    Summary:
    The flow type and inheriting TS types mark animated as optional, mark it here as well.
    
    Changelog:
    [General][Fixed] - Mark scrollToEnd animated as optional
    
    Reviewed By: christophpurrer
    
    Differential Revision: D42036647
    
    fbshipit-source-id: 59d408adc639ee1e0ca040301511c64d7ba55bfe

--- Libraries/Components/ScrollView/ScrollView.d.ts
+++ Libraries/Components/ScrollView/ScrollView.d.ts
@@ -841,7 +841,7 @@ export class ScrollView extends ScrollViewBase {
    * The options object has an animated prop, that enables the scrolling animation or not.
    * The animated prop defaults to true
    */
-  scrollToEnd(options?: {animated: boolean}): void;
+  scrollToEnd(options?: {animated?: boolean}): void;
 
   /**
    * Displays the scroll indicators momentarily.
