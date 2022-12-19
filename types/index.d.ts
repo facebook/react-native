ef39b9b452c420ea35b000a0dceadbcb053c38a4
Ibrahim Memon <106296670+ibrahimmemonn@users.noreply.github.com>
2022-12-19 13:04:00 +0500

    Remove Extra Spaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -150,7 +150,6 @@ export * from '../Libraries/Vibration/Vibration';
 export * from '../Libraries/YellowBox/YellowBoxDeprecated';
 export * from '../Libraries/vendor/core/ErrorUtils';
 export * from '../Libraries/vendor/emitter/EventEmitter';
-
 export * from './public/DeprecatedPropertiesAlias';
 export * from './public/Insets';
 export * from './public/ReactNativeRenderer';
