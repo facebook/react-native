0c53420a7af306d629350e1244e8e2ccae08a312
abing <abing@gcores.com>
2023-01-04 11:40:34 -0800

    Fix RCTAlertController not showing when using SceneDelegate on iOS 13.0+ (#35716)
    Summary:
    On iOS 13.0+, app may use SceneDelegate for multiple windows support or CarPlay support. RCTAlertController can't find the correct root vc in such scene based apps.
    
    ## Changelog
    
    <!-- Help reviewers and the release process by writing your own changelog entry.
    
    Pick one each for the category and type tags:
    
    [ANDROID|GENERAL|IOS|INTERNAL] [BREAKING|ADDED|CHANGED|DEPRECATED|REMOVED|FIXED|SECURITY] - Message
    
    For more details, see:
    https://reactnative.dev/contributing/changelogs-in-pull-requests
    -->
    
    [iOS] [Fixed] - Fix RCTAlertController not showing when using SceneDelegate on iOS 13.0+.
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/35716
    
    Reviewed By: cipolleschi
    
    Differential Revision: D42253653
    
    Pulled By: makovkastar
    
    fbshipit-source-id: ae4e833abca2af7af8028f3af9bd8d3f60ebd392

--- React/CoreModules/RCTAlertController.m
+++ React/CoreModules/RCTAlertController.m
@@ -20,14 +20,21 @@
 - (UIWindow *)alertWindow
 {
   if (_alertWindow == nil) {
-    UIWindow *keyWindow = RCTSharedApplication().keyWindow;
-    if (keyWindow) {
-      _alertWindow = [[UIWindow alloc] initWithFrame:keyWindow.bounds];
+    _alertWindow = [self getUIWindowFromScene];
+
+    if (_alertWindow == nil) {
+      UIWindow *keyWindow = RCTSharedApplication().keyWindow;
+      if (keyWindow) {
+        _alertWindow = [[UIWindow alloc] initWithFrame:keyWindow.bounds];
+      } else {
+        // keyWindow is nil, so we cannot create and initialize _alertWindow
+        NSLog(@"Unable to create alert window: keyWindow is nil");
+      }
+    }
+
+    if (_alertWindow) {
       _alertWindow.rootViewController = [UIViewController new];
       _alertWindow.windowLevel = UIWindowLevelAlert + 1;
-    } else {
-      // keyWindow is nil, so we cannot create and initialize _alertWindow
-      NSLog(@"Unable to create alert window: keyWindow is nil");
     }
   }
 
@@ -54,6 +61,19 @@
   }
 
   _alertWindow = nil;
+}
+
+- (UIWindow *)getUIWindowFromScene
+{
+  if (@available(iOS 13.0, *)) {
+    for (UIScene *scene in RCTSharedApplication().connectedScenes) {
+      if (scene.activationState == UISceneActivationStateForegroundActive &&
+          [scene isKindOfClass:[UIWindowScene class]]) {
+        return [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
+      }
+    }
+  }
+  return nil;
 }
 
 @end
