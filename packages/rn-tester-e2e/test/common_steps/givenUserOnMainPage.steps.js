f2ef9160403027a73ce1c43ed19a0a3b8cb1bcd3
adrianorszulik <adrian.orszulik@callstack.com>
2023-01-10 15:56:43 +0100

    Removing unnecessary files

--- packages/rn-tester-e2e/test/common_steps/givenUserOnMainPage.steps.js
+++
@@ -1,7 +0,0 @@
-import { driver } from '../../jest.setup.js';
-
-export const givenUserOnMainPage = (given) => {
-    given('User is on the main screen', async () => {
-        await driver.pause(2000);
-    });
-};
