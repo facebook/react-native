7e7c35d92270c158264f77cb199fc5622b128325
Shawn Dempsey <96719+shwanton@users.noreply.github.com>
2023-01-24 10:42:01 -0800

    [Fabric] Shim RCTFabricModalHostViewControllerDelegate (#1671)
    * [Fabric] Shim RCTFabricModalHostViewControllerDelegate
    
    * whitespace
    
    * Fix bad merge
    
    Co-authored-by: Shawn Dempsey <shawndempsey@fb.com>

--- React/Fabric/Mounting/ComponentViews/Modal/RCTFabricModalHostViewController.mm
+++ React/Fabric/Mounting/ComponentViews/Modal/RCTFabricModalHostViewController.mm
@@ -32,6 +32,7 @@
   return self;
 }
 
+#if !TARGET_OS_OSX // [macOS]
 - (void)viewDidLayoutSubviews
 {
   [super viewDidLayoutSubviews];
@@ -81,5 +82,6 @@
   return _supportedInterfaceOrientations;
 }
 #endif // RCT_DEV
+#endif // [macOS]
 
 @end
