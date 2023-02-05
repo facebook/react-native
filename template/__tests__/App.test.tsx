8ee09bafbf3b7bdff1095a085a73efbd671eb1da
Xun Sun <UNIDY2002@outlook.com>
2023-02-05 11:52:10 +0800

    Fix quotation marks

--- template/__tests__/App.test.tsx
+++ template/__tests__/App.test.tsx
@@ -4,7 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import {it} from "@jest/globals";
+import {it} from '@jest/globals';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
