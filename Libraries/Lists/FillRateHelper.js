1479b2ac26fded3840c596f53e6eb86a4b0c2c71
Ruslan Lesiutin <hoxy@meta.com>
2023-02-06 13:39:13 -0800

    refactor(arvr/xplat): update imports to resolve packaged virtualized list from react-native (#36035)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/36035
    
    Changelog: [Internal]
    
    Reviewed By: NickGerleman
    
    Differential Revision: D42805202
    
    fbshipit-source-id: 1f1d6e36ec6e19a1b46ce340be095bb60b8048f4

---
+++ Libraries/Lists/FillRateHelper.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+'use strict';
+
+import {typeof FillRateHelper as FillRateHelperType} from '@react-native/virtualized-lists';
+
+const FillRateHelper: FillRateHelperType =
+  require('@react-native/virtualized-lists').FillRateHelper;
+
+export type {FillRateInfo} from '@react-native/virtualized-lists';
+module.exports = FillRateHelper;
