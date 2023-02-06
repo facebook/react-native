8d7abcf5e26607dd28a9c8fcf7a4ff3b19586a89
bigcupcoffee <24373206+bigcupcoffee@users.noreply.github.com>
2023-02-06 12:45:44 +0200

    Fix: incorrect touchable hitSlop type

--- Libraries/Components/Touchable/TouchableWithoutFeedback.d.ts
+++ Libraries/Components/Touchable/TouchableWithoutFeedback.d.ts
@@ -69,7 +69,7 @@ export interface TouchableWithoutFeedbackProps
    * the Z-index of sibling views always takes precedence if a touch hits
    * two overlapping views.
    */
-  hitSlop?: Insets | undefined;
+  hitSlop?: null | Insets | number | undefined;
 
   /**
    * Used to reference react managed views from native code.
