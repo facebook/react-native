7753e1a79485ebf8cb2bd3a60f41a83519c4285f
Olga Zinoveva <ozinoveva@fb.com>
2023-02-07 15:54:41 -0800

    adding license headers to the script files per the linter

--- .github/workflows/verifyVersion.js
+++ .github/workflows/verifyVersion.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
 module.exports = async (github, context) => {
     const issue = context.payload.issue;
 
