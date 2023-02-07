4e4bb49cbb322a15ffac507d4fe9111004c9200b
Xun Sun <UNIDY2002@outlook.com>
2023-02-07 08:52:08 +0800

    Add some comments

--- template/__tests__/App.test.tsx
+++ template/__tests__/App.test.tsx
@@ -4,8 +4,10 @@
 
 import 'react-native';
 import React from 'react';
-import {it} from '@jest/globals';
 import App from '../App';
+
+// Note: import explicitly to use the types shiped with jest.
+import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
