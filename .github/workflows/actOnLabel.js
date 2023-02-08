dbc6cbc453755aaa219922f37821982fe0e57a60
Olga Zinoveva <ozinoveva@fb.com>
2023-02-07 16:23:50 -0800

    actually formatting the scripts this time

--- .github/workflows/actOnLabel.js
+++ .github/workflows/actOnLabel.js
@@ -8,97 +8,116 @@
  */
 
 module.exports = async (github, context, label) => {
+  const closeIssue = async () => {
+    await github.rest.issues.update({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      state: 'closed',
+    });
+  };
 
-    const closeIssue = async () => {
-        await github.rest.issues.update({
-            issue_number: context.issue.number,
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            state: "closed",
-        });
-    }
+  const addComment = async comment => {
+    await github.rest.issues.createComment({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: comment,
+    });
+  };
 
-    const addComment = async (comment) => {
-        await github.rest.issues.createComment({
-            issue_number: context.issue.number,
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            body: comment,
-        });
-    }
+  const requestAuthorFeedback = async () => {
+    // Remove the triage label if it exists (ignore the 404 if not; it's not expected to always be there)
+    try {
+      await github.rest.issues.removeLabel({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        name: 'Needs: Triage :mag:',
+      });
+    } catch {}
 
-    const requestAuthorFeedback = async () => {
-        // Remove the triage label if it exists (ignore the 404 if not; it's not expected to always be there)
-        try {
-            await github.rest.issues.removeLabel({
-                issue_number: context.issue.number,
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-                name: "Needs: Triage :mag:"
-            });
-        } catch {}
+    await github.rest.issues.addLabels({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      labels: ['Needs: Author Feedback'],
+    });
+  };
 
-        await github.rest.issues.addLabels({
-            issue_number: context.issue.number,
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            labels: ['Needs: Author Feedback']
-        });
-    }
-
-    switch(label){
-        case "Type: Invalid":
-            await closeIssue();
-            return;
-        case "Type: Question":
-            await addComment(`We are using GitHub issues exclusively to track bugs in React Native. GitHub may not be the ideal place to ask a question, but you can try asking over on [Stack Overflow](http://stackoverflow.com/questions/tagged/react-native), or on [Reactiflux](https://www.reactiflux.com/).`);
-            await closeIssue();
-            return;
-        case "Type: Docs":
-            await addComment(`Please report documentation issues in the [react-native-website](https://github.com/facebook/react-native-website/issues) repository.`);
-            await closeIssue();
-            return;
-        case "Resolution: For Stack Overflow":
-            await addComment(`We are using GitHub issues exclusively to track bugs in the core React Native library. Please try asking over on [Stack Overflow](http://stackoverflow.com/questions/tagged/react-native) as it is better suited for this type of question.`);
-            await closeIssue();
-            return;
-        case "Type: Expo":
-            await addComment(`It looks like your issue is related to Expo and not React Native core. Please open your issue in [Expo's repository](https://github.com/expo/expo/issues/new). If you are able to create a repro that showcases that this issue is also happening in React Native vanilla, we will be happy to re-open.`);
-            await closeIssue();
-            return;
-        case "Needs: Issue Template":
-            await addComment(`| :warning: | Missing Required Fields |\n`+
-              `| --- | --- |\n`+
-              `| :information_source: | It looks like your issue may be missing some necessary information. GitHub provides an example template whenever a [new issue is created](https://github.com/facebook/react-native/issues/new?template=bug_report.md). Could you go back and make sure to fill out the template? You may edit this issue, or close it and open a new one. |`);
-            await requestAuthorFeedback();
-            return;
-        case "Needs: Environment Info":
-            await addComment(`| :warning: | Missing Environment Information |\n`+
-            `| --- | --- |\n`+
-            `| :information_source: | Your issue may be missing information about your development environment. You can obtain the missing information by running <code>react-native info</code> in a console. |`);
-            await requestAuthorFeedback();
-            return;
-        case "Needs: Verify on Latest Version":
-            await addComment(`| :warning: | Using Old Version |\n`+
-            `| --- | --- |\n`+
-            `| :information_source: | It looks like you are using an older version of React Native. Please [upgrade](https://reactnative.dev/docs/upgrading) to the latest version, and verify if the issue persists. If it does not, please let us know so we can close out this issue. This helps us ensure we are looking at issues that still exist in the current release. |`);
-            await requestAuthorFeedback();
-            return;
-        case "Needs: Version Info":
-            await addComment(`| :warning: | Update Version Info |\n`+
-            `| --- | --- |\n`+
-            `| :information_source: | We could not find or parse the version number of React Native in your issue report. Please use the template, and report your version including major, minor, and patch numbers - e.g. 0.70.2 |`);
-            await requestAuthorFeedback();
-            return;
-        case "Needs: Repro":
-            await addComment(`| :warning: | Missing Reproducible Example |\n`+
-            `| --- | --- |\n`+
-            `| :information_source: | It looks like your issue is missing a reproducible example. Please provide a [Snack](https://snack.expo.dev) or a repository that demonstrates the issue you are reporting in a [minimal, complete, and reproducible](https://stackoverflow.com/help/minimal-reproducible-example) manner. |`);
-            await requestAuthorFeedback();
-            return;
-        case "Type: Unsupported Version":
-            await addComment(`It looks like your issue or the example you provided uses an [unsupported version of React Native](https://github.com/reactwg/react-native-releases/blob/main/README.md#releases-support-policy). Due to the amount of issues we receive, we're currently accepting only new issues against one of the supported versions. Please open your issue on [StackOverflow](https://stackoverflow.com/questions/tagged/react-native) to get further community support.`);
-            await requestAuthorFeedback();
-            return;
-    }
+  switch (label) {
+    case 'Type: Invalid':
+      await closeIssue();
+      return;
+    case 'Type: Question':
+      await addComment(
+        `We are using GitHub issues exclusively to track bugs in React Native. GitHub may not be the ideal place to ask a question, but you can try asking over on [Stack Overflow](http://stackoverflow.com/questions/tagged/react-native), or on [Reactiflux](https://www.reactiflux.com/).`,
+      );
+      await closeIssue();
+      return;
+    case 'Type: Docs':
+      await addComment(
+        `Please report documentation issues in the [react-native-website](https://github.com/facebook/react-native-website/issues) repository.`,
+      );
+      await closeIssue();
+      return;
+    case 'Resolution: For Stack Overflow':
+      await addComment(
+        `We are using GitHub issues exclusively to track bugs in the core React Native library. Please try asking over on [Stack Overflow](http://stackoverflow.com/questions/tagged/react-native) as it is better suited for this type of question.`,
+      );
+      await closeIssue();
+      return;
+    case 'Type: Expo':
+      await addComment(
+        `It looks like your issue is related to Expo and not React Native core. Please open your issue in [Expo's repository](https://github.com/expo/expo/issues/new). If you are able to create a repro that showcases that this issue is also happening in React Native vanilla, we will be happy to re-open.`,
+      );
+      await closeIssue();
+      return;
+    case 'Needs: Issue Template':
+      await addComment(
+        `| :warning: | Missing Required Fields |\n` +
+          `| --- | --- |\n` +
+          `| :information_source: | It looks like your issue may be missing some necessary information. GitHub provides an example template whenever a [new issue is created](https://github.com/facebook/react-native/issues/new?template=bug_report.md). Could you go back and make sure to fill out the template? You may edit this issue, or close it and open a new one. |`,
+      );
+      await requestAuthorFeedback();
+      return;
+    case 'Needs: Environment Info':
+      await addComment(
+        `| :warning: | Missing Environment Information |\n` +
+          `| --- | --- |\n` +
+          `| :information_source: | Your issue may be missing information about your development environment. You can obtain the missing information by running <code>react-native info</code> in a console. |`,
+      );
+      await requestAuthorFeedback();
+      return;
+    case 'Needs: Verify on Latest Version':
+      await addComment(
+        `| :warning: | Using Old Version |\n` +
+          `| --- | --- |\n` +
+          `| :information_source: | It looks like you are using an older version of React Native. Please [upgrade](https://reactnative.dev/docs/upgrading) to the latest version, and verify if the issue persists. If it does not, please let us know so we can close out this issue. This helps us ensure we are looking at issues that still exist in the current release. |`,
+      );
+      await requestAuthorFeedback();
+      return;
+    case 'Needs: Version Info':
+      await addComment(
+        `| :warning: | Update Version Info |\n` +
+          `| --- | --- |\n` +
+          `| :information_source: | We could not find or parse the version number of React Native in your issue report. Please use the template, and report your version including major, minor, and patch numbers - e.g. 0.70.2 |`,
+      );
+      await requestAuthorFeedback();
+      return;
+    case 'Needs: Repro':
+      await addComment(
+        `| :warning: | Missing Reproducible Example |\n` +
+          `| --- | --- |\n` +
+          `| :information_source: | It looks like your issue is missing a reproducible example. Please provide a [Snack](https://snack.expo.dev) or a repository that demonstrates the issue you are reporting in a [minimal, complete, and reproducible](https://stackoverflow.com/help/minimal-reproducible-example) manner. |`,
+      );
+      await requestAuthorFeedback();
+      return;
+    case 'Type: Unsupported Version':
+      await addComment(
+        `It looks like your issue or the example you provided uses an [unsupported version of React Native](https://github.com/reactwg/react-native-releases/blob/main/README.md#releases-support-policy). Due to the amount of issues we receive, we're currently accepting only new issues against one of the supported versions. Please open your issue on [StackOverflow](https://stackoverflow.com/questions/tagged/react-native) to get further community support.`,
+      );
+      await requestAuthorFeedback();
+      return;
+  }
 };
--- .github/workflows/verifyVersion.js
+++ .github/workflows/verifyVersion.js
@@ -8,77 +8,95 @@
  */
 
 module.exports = async (github, context) => {
-    const issue = context.payload.issue;
+  const issue = context.payload.issue;
 
-    // Ignore issues using upgrade template (they use a special label)
-    if (issue.labels.find(label => label.name == "Type: Upgrade Issue")){
-        return;
-    }
+  // Ignore issues using upgrade template (they use a special label)
+  if (issue.labels.find(label => label.name == 'Type: Upgrade Issue')) {
+    return;
+  }
 
-    // Extract RN version number from the issue body, if it exists
-    const getReactNativeVersionIfExists = (issue) => {
-        if (!issue || !issue.body) return;
-        const rnVersionRegex = /React Native Version[\r\n]+[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}/;
-        const rnVersionMatch = issue.body.match(rnVersionRegex);
-        if (!rnVersionMatch || !rnVersionMatch[0]) return;
-        return rnVersionMatch[0];
-    };
-    const issueVersion = parseVersionFromString(getReactNativeVersionIfExists(issue));
+  // Extract RN version number from the issue body, if it exists
+  const getReactNativeVersionIfExists = issue => {
+    if (!issue || !issue.body) return;
+    const rnVersionRegex =
+      /React Native Version[\r\n]+[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}/;
+    const rnVersionMatch = issue.body.match(rnVersionRegex);
+    if (!rnVersionMatch || !rnVersionMatch[0]) return;
+    return rnVersionMatch[0];
+  };
+  const issueVersion = parseVersionFromString(
+    getReactNativeVersionIfExists(issue),
+  );
 
-    if (!issueVersion) {
-        return "Needs: Version Info";
-    }
+  if (!issueVersion) {
+    return 'Needs: Version Info';
+  }
 
-    // Ensure the version matches one we support
-    const recentReleases = (await github.rest.repos.listReleases({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-    })).data.map(release => release.name);
+  // Ensure the version matches one we support
+  const recentReleases = (
+    await github.rest.repos.listReleases({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    })
+  ).data.map(release => release.name);
 
-    const latestRelease = (await github.rest.repos.getLatestRelease({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-    })).data;
-    const latestVersion = parseVersionFromString(latestRelease.name);
+  const latestRelease = (
+    await github.rest.repos.getLatestRelease({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    })
+  ).data;
+  const latestVersion = parseVersionFromString(latestRelease.name);
 
-    if (!isVersionSupported(issueVersion, latestVersion, recentReleases)) {
-        return "Needs: Verify on Latest Version";
-    }
+  if (!isVersionSupported(issueVersion, latestVersion, recentReleases)) {
+    return 'Needs: Verify on Latest Version';
+  }
 };
 
 // We support N-2 minor versions. We also want to match the highest available patch for the given minor.
 // E.g. the latest release is 0.71.2. 0.71.0 -> Please upgrade. 0.69.8 -> OK. 0.69.7 -> Please upgrade.
 // releaseNames is expected to be an array sorted in the order of release recency.
 function isVersionSupported(actualVersion, latestVersion, releaseNames) {
-    // Assumes that releases are sorted in the order of recency (i.e. most recent releases are earlier in the list)
-    // This enables us to stop looking as soon as we find the first release with a matching major/minor version, since
-    // we know it's the most recent release, therefore the highest patch available.
-    const getLatestPatchForVersion = (version, releases) => {
-        for (releaseName of releases){
-            const release = parseVersionFromString(releaseName);
-            if (release.major == version.major && release.minor == version.minor){
-                return release.patch;
-            }
-        }
+  // Assumes that releases are sorted in the order of recency (i.e. most recent releases are earlier in the list)
+  // This enables us to stop looking as soon as we find the first release with a matching major/minor version, since
+  // we know it's the most recent release, therefore the highest patch available.
+  const getLatestPatchForVersion = (version, releases) => {
+    for (releaseName of releases) {
+      const release = parseVersionFromString(releaseName);
+      if (release.major == version.major && release.minor == version.minor) {
+        return release.patch;
+      }
     }
+  };
 
-    // Very old version; user should upgrade to latest
-    if (actualVersion.major != latestVersion.major ||
-        actualVersion.minor < latestVersion.minor - 2){
-        return false;
-    }
+  // Very old version; user should upgrade to latest
+  if (
+    actualVersion.major != latestVersion.major ||
+    actualVersion.minor < latestVersion.minor - 2
+  ) {
+    return false;
+  }
 
-    // Supported minor, verify that the patch is also supported
-    // For now, we treat all prerelease versions as supported as long as they are on the same patch
-    const latestPatchForVersion = getLatestPatchForVersion(actualVersion, releaseNames);
-    return actualVersion.patch == latestPatchForVersion;
+  // Supported minor, verify that the patch is also supported
+  // For now, we treat all prerelease versions as supported as long as they are on the same patch
+  const latestPatchForVersion = getLatestPatchForVersion(
+    actualVersion,
+    releaseNames,
+  );
+  return actualVersion.patch == latestPatchForVersion;
 }
 
 function parseVersionFromString(version) {
-    if (!version) return;
-    // This will match the standard x.x.x semver format, as well as the non-standard prerelease x.x.x-rc.x
-    const semverRegex = /(?<major>[0-9]{1,2})\.(?<minor>[0-9]{1,2})\.(?<patch>[0-9]{1,2})(-[rR]{1}[cC]{1}\.(?<prerelease>[0-9]{1,2}))?/;
-    const versionMatch = version.match(semverRegex);
-    const { major, minor, patch, prerelease } = versionMatch.groups;
-    return { major: parseInt(major), minor: parseInt(minor), patch: parseInt(patch), prerelease: parseInt(prerelease) };
+  if (!version) return;
+  // This will match the standard x.x.x semver format, as well as the non-standard prerelease x.x.x-rc.x
+  const semverRegex =
+    /(?<major>[0-9]{1,2})\.(?<minor>[0-9]{1,2})\.(?<patch>[0-9]{1,2})(-[rR]{1}[cC]{1}\.(?<prerelease>[0-9]{1,2}))?/;
+  const versionMatch = version.match(semverRegex);
+  const {major, minor, patch, prerelease} = versionMatch.groups;
+  return {
+    major: parseInt(major),
+    minor: parseInt(minor),
+    patch: parseInt(patch),
+    prerelease: parseInt(prerelease),
+  };
 }
