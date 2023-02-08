8f575e992d1b629b14a50b22bd659cc5b5fa6a4d
Janic Duplessis <janicduplessis@gmail.com>
2023-02-08 12:40:17 -0500

    Add fabric support for maintainVisibleContentPosition on iOS
    This reverts commit 9d98e2cb07afd3f68945ec7d7125188f4b6367fa.

--- ReactCommon/react/renderer/components/scrollview/primitives.h
+++ ReactCommon/react/renderer/components/scrollview/primitives.h
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <optional>
+
 namespace facebook {
 namespace react {
 
@@ -21,6 +23,21 @@ enum class ContentInsetAdjustmentBehavior {
   Automatic,
   ScrollableAxes,
   Always
+};
+
+class ScrollViewMaintainVisibleContentPosition final {
+ public:
+  int minIndexForVisible{0};
+  std::optional<int> autoscrollToTopThreshold{};
+
+  bool operator==(const ScrollViewMaintainVisibleContentPosition &rhs) const {
+    return std::tie(this->minIndexForVisible, this->autoscrollToTopThreshold) ==
+        std::tie(rhs.minIndexForVisible, rhs.autoscrollToTopThreshold);
+  }
+
+  bool operator!=(const ScrollViewMaintainVisibleContentPosition &rhs) const {
+    return !(*this == rhs);
+  }
 };
 
 } // namespace react
