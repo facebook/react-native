d3eed1fedcf032964d426f0b581855f78477d197
Riccardo Cipolleschi <cipolleschi@meta.com>
2023-02-09 09:11:55 -0800

    Fix the Multiple Podfile warning
    Summary:
    This PR adds the `react-native.config.js` warning when installing an app from the template.
    
    ## Changelog:
    [iOS][Changed] - Fix multiple Podfile warning
    
    Differential Revision: D43159852
    
    fbshipit-source-id: a987d07ab9912db8da6da19b4eb4d74e630fa274

---
+++ template/react-native.config.js
@@ -0,0 +1,14 @@
+/**
+ * CLI configurations for ReactNative
+ * https://github.com/react-native-community/cli/blob/master/docs/configuration.md
+ *
+ * @format
+ */
+
+module.exports = {
+  project: {
+    ios: {
+      sourceDir: 'ios/',
+    },
+  },
+};
