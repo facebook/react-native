e35e1fc9d376119b83e9618b6dc5978494b8658e
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2023-02-09 00:42:50 -0800

    Create other *.d.ts

---
+++ packages/react-native-codegen/src/parsers/parser.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type { SchemaType } from "../CodegenSchema";
+import type { ParserType } from "./errors";
+
+// useful members only for downstream
+export interface Parser {
+    language(): ParserType;
+    parseFile(filename: string): SchemaType;
+    parseString(contents: string, filename?: string): SchemaType;
+    parseModuleFixture(filename: string): SchemaType;
+}
