4b8b9cc8389843d99a3e1659eb4834a5372d4d16
Nicola Corti <ncor@meta.com>
2023-02-28 08:39:21 -0800

    AGP to 7.4.2
    Summary:
    This is just a minor bump before 0.72 and it brings AGP up to date
    with the latest stable.
    
    allow-large-files
    
    Changelog:
    [Internal] [Changed] - AGP to 7.4.2
    
    Reviewed By: cipolleschi
    
    Differential Revision: D43659180
    
    fbshipit-source-id: d57ea8fb6ae902412b542e0125d3b15168d0e123

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@
 
 plugins {
   id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-  id("com.android.library") version "7.4.1" apply false
-  id("com.android.application") version "7.4.1" apply false
+  id("com.android.library") version "7.4.2" apply false
+  id("com.android.application") version "7.4.2" apply false
   id("de.undercouch.download") version "5.0.1" apply false
   kotlin("android") version "1.7.22" apply false
 }
