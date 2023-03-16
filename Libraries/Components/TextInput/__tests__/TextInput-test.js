c0abff11b66d9ec3a8e1d09333a3fb6c05678bed
Luna Wei <luwe@meta.com>
2023-03-16 08:26:53 -0700

    Precedent textContentType when set
    Summary: Changelog: [iOS][Changed] - Give precedence to `textContentType` property for backwards compat as mentioned in https://github.com/facebook/react-native/issues/36229#issuecomment-1470468374
    
    Reviewed By: necolas
    
    Differential Revision: D44106291
    
    fbshipit-source-id: 5702d7f171735d1abe6cfbc9ca1ad8f21751d51e

--- Libraries/Components/TextInput/__tests__/TextInput-test.js
+++ Libraries/Components/TextInput/__tests__/TextInput-test.js
@@ -169,6 +169,42 @@ describe('TextInput tests', () => {
     expect(TextInput.State.currentlyFocusedInput()).toBe(textInputRe2.current);
   });
 
+  it('should give precedence to `textContentType` when set', () => {
+    const instance = ReactTestRenderer.create(
+      <TextInput autoComplete="tel" textContentType="emailAddress" />,
+    );
+
+    expect(instance.toJSON()).toMatchInlineSnapshot(`
+      <RCTSinglelineTextInputView
+        accessible={true}
+        allowFontScaling={true}
+        focusable={true}
+        forwardedRef={null}
+        mostRecentEventCount={0}
+        onBlur={[Function]}
+        onChange={[Function]}
+        onChangeSync={null}
+        onClick={[Function]}
+        onFocus={[Function]}
+        onResponderGrant={[Function]}
+        onResponderMove={[Function]}
+        onResponderRelease={[Function]}
+        onResponderTerminate={[Function]}
+        onResponderTerminationRequest={[Function]}
+        onScroll={[Function]}
+        onSelectionChange={[Function]}
+        onSelectionChangeShouldSetResponder={[Function]}
+        onStartShouldSetResponder={[Function]}
+        rejectResponderTermination={true}
+        selection={null}
+        submitBehavior="blurAndSubmit"
+        text=""
+        textContentType="emailAddress"
+        underlineColorAndroid="transparent"
+      />
+    `);
+  });
+
   it('should render as expected', () => {
     expectRendersMatchingSnapshot(
       'TextInput',
