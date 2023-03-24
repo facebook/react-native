62d992019e238cfadb2328b14e6967e6f6d8d722
Nick Gerleman <ngerlem@meta.com>
2023-03-24 05:24:09 -0700

    Minimize EditText Spans 3/9: ReactBackgroundColorSpan (#36547)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/36547
    
    This is part of a series of changes to minimize the number of spans committed to EditText, as a mitigation for platform issues on Samsung devices. See this [GitHub thread]( https://github.com/facebook/react-native/issues/35936#issuecomment-1411437789) for greater context on the platform behavior.
    
    This adds `ReactBackgroundColorSpan` to the list of spans eligible to be stripped.
    
    Changelog:
    [Android][Fixed] - Minimize Spans 3/N: ReactBackgroundColorSpan
    
    Reviewed By: javache
    
    Differential Revision: D44240782
    
    fbshipit-source-id: 2ded1a1687a41cf6d5f83e89ffadd2d932089969

--- ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewBackgroundManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewBackgroundManager.java
@@ -19,6 +19,7 @@ public class ReactViewBackgroundManager {
 
   private @Nullable ReactViewBackgroundDrawable mReactBackgroundDrawable;
   private View mView;
+  private int mColor = Color.TRANSPARENT;
 
   public ReactViewBackgroundManager(View view) {
     this.mView = view;
@@ -54,6 +55,10 @@ public class ReactViewBackgroundManager {
     } else {
       getOrCreateReactViewBackground().setColor(color);
     }
+  }
+
+  public int getBackgroundColor() {
+    return mColor;
   }
 
   public void setBorderWidth(int position, float width) {
