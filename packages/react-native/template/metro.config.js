9ffe82edd3f0a891b2421b34327d2859d3f5abdf
Riccardo Cipolleschi <cipolleschi@meta.com>
2023-03-28 22:32:42 +0100

    Port changes from main into Stable to make the CI green (#36665)
    Co-authored-by: Riccardo Cipolleschi <cipolleschi@fb.com>
    Co-authored-by: Alex Hunt <alexeh@meta.com>
    resolved: https://github.com/facebook/react-native/pull/36502

--- packages/react-native/template/metro.config.js
+++ packages/react-native/template/metro.config.js
@@ -5,13 +5,16 @@
  * @format
  */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+const {getDefaultConfig} = require('@react-native/metro-config');
+
+const {mergeConfig} = require('metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
