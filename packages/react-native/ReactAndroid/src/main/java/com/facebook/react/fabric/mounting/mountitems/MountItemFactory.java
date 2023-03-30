dbac833e3dd5a87b61850e312d11c8e799c9d07c
David Vacca <515103+mdvacca@users.noreply.github.com>
2023-03-30 16:55:37 -0700

    Refactor creation of MountItems (#36605)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/36605
    
    This diff introduces the class MountItemFactory that will be used to create MountItems in Fabric.
    The purpose of this refactor is to reduce visibility for custom implementations of MountItems
    
    This should not be a breaking change, because nobody should be using these classes
    
    changelog: [internal] internal
    
    Reviewed By: javache
    
    Differential Revision: D44115053
    
    fbshipit-source-id: 29d843d3921a4f50e19b4521353ca8fa59f61e27

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/mountitems/MountItemFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.fabric.mounting.mountitems;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.facebook.react.bridge.ReadableArray;
+
+/** Factory class that expose creation of {@link MountItem} */
+public class MountItemFactory {
+
+  /** @return a {@link DispatchCommandMountItem} for commands identified by an int */
+  public static DispatchCommandMountItem createDispatchCommandMountItem(
+      int surfaceId, int reactTag, int commandId, @Nullable ReadableArray commandArgs) {
+    return new DispatchIntCommandMountItem(surfaceId, reactTag, commandId, commandArgs);
+  }
+
+  /** @return a {@link DispatchCommandMountItem} for commands identified by a String */
+  public static DispatchCommandMountItem createDispatchCommandMountItem(
+      int surfaceId, int reactTag, @NonNull String commandId, @Nullable ReadableArray commandArgs) {
+    return new DispatchStringCommandMountItem(surfaceId, reactTag, commandId, commandArgs);
+  }
+}
