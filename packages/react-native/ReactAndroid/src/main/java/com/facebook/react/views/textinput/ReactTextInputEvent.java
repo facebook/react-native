d2d2a8a598cb8b5e5218c3c21cef408de2142fe4
David Vacca <dvacca@meta.com>
2023-03-31 15:34:45 -0700

    Introduce NO_SURFACE constant to specify the lack of surfaces used by legacy system (#36747)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/36747
    
    Introduce NO_SURFACE constant to specify the lack of surfaces used by legacy system
    
    changelog: [internal] internal
    
    Reviewed By: sshic
    
    Differential Revision: D44563649
    
    fbshipit-source-id: 99c7028c5ee508c2982cefc9b3199a332c2346c7

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputEvent.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputEvent.java
@@ -10,6 +10,7 @@ package com.facebook.react.views.textinput;
 import androidx.annotation.Nullable;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.common.ViewUtil;
 import com.facebook.react.uimanager.events.Event;
 
 /**
@@ -28,7 +29,7 @@ public class ReactTextInputEvent extends Event<ReactTextInputEvent> {
   @Deprecated
   public ReactTextInputEvent(
       int viewId, String text, String previousText, int rangeStart, int rangeEnd) {
-    this(-1, viewId, text, previousText, rangeStart, rangeEnd);
+    this(ViewUtil.NO_SURFACE_ID, viewId, text, previousText, rangeStart, rangeEnd);
   }
 
   public ReactTextInputEvent(
