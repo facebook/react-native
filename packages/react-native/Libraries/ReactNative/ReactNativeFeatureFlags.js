ee2999c0c722ac1f71216b0329a9c27c8a9b3603
Genki Kondo <gkondo@meta.com>
2023-04-04 16:06:14 -0700

    Modify AnimatedProps and AnimatedStyle to use AnimatedObject (#36798)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/36798
    
    AnimatedObject is a more generic version of AnimatedTransform, able to handle animated values within arrays and objects. This is useful for props of native components that may need to be animated per field.
    
    This diff hooks up AnimatedObject to AnimatedProps and AnimatedStyle for values that are arrays or objects.
    
    Changelog:
    [Internal][Added] - Modify AnimatedProps and AnimatedStyle to use AnimatedObject
    
    Reviewed By: rshest
    
    Differential Revision: D44637985
    
    fbshipit-source-id: 21e0492cb66cf565720bfd92a618a26cca9f1e15

--- packages/react-native/Libraries/ReactNative/ReactNativeFeatureFlags.js
+++ packages/react-native/Libraries/ReactNative/ReactNativeFeatureFlags.js
@@ -45,6 +45,10 @@ export type FeatureFlags = {|
    * Enables access to the host tree in Fabric using DOM-compatible APIs.
    */
   enableAccessToHostTreeInFabric: () => boolean,
+  /**
+   * Enables animating object and array prop values.
+   */
+  isAnimatedObjectEnabled: () => boolean,
 |};
 
 const ReactNativeFeatureFlags: FeatureFlags = {
@@ -55,6 +59,7 @@ const ReactNativeFeatureFlags: FeatureFlags = {
   animatedShouldUseSingleOp: () => false,
   isGlobalWebPerformanceLoggerEnabled: () => false,
   enableAccessToHostTreeInFabric: () => false,
+  isAnimatedObjectEnabled: () => false,
 };
 
 module.exports = ReactNativeFeatureFlags;
