8e4355ebd797f182d4bbbe34de86983f36cdfcfc
Dmitry Rykun <dmitryrykun@meta.com>
2023-04-04 14:38:02 -0700

    Re-land "[react-native][PR] [Upstream][Security] Use `execFileSync` over `exec` for cases with uncontrolled absolute paths" (#36791)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/36791
    
    This is a re-land of D44635949. It was reverted as it was a suspected cause of CircleCI fails. It turned out the diff was unrelated, and the cause was something else.
    
    ## Changelog
    [Internal][Security] - Use execFileSync over exec for cases with uncontrolled absolute paths
    
    Reviewed By: cortinico
    
    Differential Revision: D44663132
    
    fbshipit-source-id: cdd2a612ca7efb416f7a8bc883dc5561f60ca59b

--- packages/react-native/scripts/codegen/__tests__/generate-artifacts-executor-test.js
+++ packages/react-native/scripts/codegen/__tests__/generate-artifacts-executor-test.js
@@ -34,32 +34,39 @@ describe('generateCode', () => {
     const node = 'usr/bin/node';
     const pathToSchema = 'app/build/schema.json';
     const rnRoot = path.join(__dirname, '../..');
-    const libraryType = 'all';
+    const libraryTypeArg = 'all';
 
-    const tmpOutDir = path.join(tmpDir, 'out');
+    const tmpOutputDir = path.join(tmpDir, 'out');
 
     // mock used functions
     jest.spyOn(fs, 'mkdirSync').mockImplementation();
     jest.spyOn(child_process, 'execSync').mockImplementation();
+    jest.spyOn(child_process, 'execFileSync').mockImplementation();
 
     underTest._generateCode(iosOutputDir, library, tmpDir, node, pathToSchema);
 
-    const expectedCommand = `${node} ${path.join(
-      rnRoot,
-      'generate-specs-cli.js',
-    )}         --platform ios         --schemaPath ${pathToSchema}         --outputDir ${tmpOutDir}         --libraryName ${
-      library.config.name
-    }         --libraryType ${libraryType}`;
-
-    expect(child_process.execSync).toHaveBeenCalledTimes(2);
-    expect(child_process.execSync).toHaveBeenNthCalledWith(1, expectedCommand);
+    expect(child_process.execFileSync).toHaveBeenCalledTimes(1);
+    expect(child_process.execFileSync).toHaveBeenNthCalledWith(1, node, [
+      `${path.join(rnRoot, 'generate-specs-cli.js')}`,
+      '--platform',
+      'ios',
+      '--schemaPath',
+      pathToSchema,
+      '--outputDir',
+      tmpOutputDir,
+      '--libraryName',
+      library.config.name,
+      '--libraryType',
+      libraryTypeArg,
+    ]);
+    expect(child_process.execSync).toHaveBeenCalledTimes(1);
     expect(child_process.execSync).toHaveBeenNthCalledWith(
-      2,
-      `cp -R ${tmpOutDir}/* ${iosOutputDir}`,
+      1,
+      `cp -R ${tmpOutputDir}/* ${iosOutputDir}`,
     );
 
     expect(fs.mkdirSync).toHaveBeenCalledTimes(2);
-    expect(fs.mkdirSync).toHaveBeenNthCalledWith(1, tmpOutDir, {
+    expect(fs.mkdirSync).toHaveBeenNthCalledWith(1, tmpOutputDir, {
       recursive: true,
     });
     expect(fs.mkdirSync).toHaveBeenNthCalledWith(2, iosOutputDir, {
