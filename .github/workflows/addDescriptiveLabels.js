a576d0f89b05be56718d8a1b8d5755c1f0428bf7
Olga Zinoveva <ozinoveva@fb.com>
2023-04-04 09:28:35 -0700

    running prettier on the new script file

--- .github/workflows/addDescriptiveLabels.js
+++ .github/workflows/addDescriptiveLabels.js
@@ -12,7 +12,7 @@ module.exports = async (github, context) => {
   const title = issue?.title?.toLowerCase();
   if (!title) return;
 
-  const addLabels = async (labelsToAdd) => {
+  const addLabels = async labelsToAdd => {
     await github.rest.issues.addLabels({
       issue_number: context.issue.number,
       owner: context.repo.owner,
@@ -27,7 +27,7 @@ module.exports = async (github, context) => {
       labelsToAdd.push(`Component: ${component}`);
     }
   }
-  for (const api of apis){
+  for (const api of apis) {
     if (title.includes(api.toLowerCase())) {
       labelsToAdd.push(`API: ${api}`);
     }
@@ -39,116 +39,115 @@ module.exports = async (github, context) => {
   }
 
   await addLabels(labelsToAdd);
-}
+};
 
-
-const labelAndroid = "Platform: Android"
-const labelIos = "Platform: iOS"
-const labelTvos = "Platform: tvOS"
-const labelNetworking = "🌐Networking"
-const labelBundler = "📦Bundler"
-const labelCli = "💻CLI"
-const labelRegression = "Impact: Regression"
+const labelAndroid = 'Platform: Android';
+const labelIos = 'Platform: iOS';
+const labelTvos = 'Platform: tvOS';
+const labelNetworking = '🌐Networking';
+const labelBundler = '📦Bundler';
+const labelCli = '💻CLI';
+const labelRegression = 'Impact: Regression';
 
 const components = [
-  "ActivityIndicator",
-  "Button",
-  "DatePickerIOS",
-  "DrawerLayoutAndroid",
-  "FlatList",
-  "Image",
-  "ImageBackground",
-  "InputAccessoryView",
-  "KeyboardAvoidingView",
-  "ListView",
-  "MaskedViewIOS",
-  "Modal",
-  "NavigatorIOS",
-  "Picker",
-  "PickerIOS",
-  "ProgressBarAndroid",
-  "ProgressViewIOS",
-  "RefreshControl",
-  "SafeAreaView",
-  "ScrollView",
-  "SectionList",
-  "SegmentedControlIOS",
-  "Slider",
-  "SnapshotViewIOS",
-  "StatusBar",
-  "Switch",
-  "TabBarIOS",
-  "TextInput",
-  "ToolbarAndroid",
-  "TouchableHighlight",
-  "TouchableNativeFeedback",
-  "TouchableOpacity",
-  "TouchableWithoutFeedback",
-  "ViewPagerAndroid",
-  "VirtualizedList",
-  "WebView"
-]
+  'ActivityIndicator',
+  'Button',
+  'DatePickerIOS',
+  'DrawerLayoutAndroid',
+  'FlatList',
+  'Image',
+  'ImageBackground',
+  'InputAccessoryView',
+  'KeyboardAvoidingView',
+  'ListView',
+  'MaskedViewIOS',
+  'Modal',
+  'NavigatorIOS',
+  'Picker',
+  'PickerIOS',
+  'ProgressBarAndroid',
+  'ProgressViewIOS',
+  'RefreshControl',
+  'SafeAreaView',
+  'ScrollView',
+  'SectionList',
+  'SegmentedControlIOS',
+  'Slider',
+  'SnapshotViewIOS',
+  'StatusBar',
+  'Switch',
+  'TabBarIOS',
+  'TextInput',
+  'ToolbarAndroid',
+  'TouchableHighlight',
+  'TouchableNativeFeedback',
+  'TouchableOpacity',
+  'TouchableWithoutFeedback',
+  'ViewPagerAndroid',
+  'VirtualizedList',
+  'WebView',
+];
 
 const apis = [
-  "AccessibilityInfo",
-  "ActionSheetIOS",
-  "Alert",
-  "AlertIOS",
-  "Animated",
-  "AppRegistry",
-  "AppState",
-  "AsyncStorage",
-  "BackAndroid",
-  "BackHandler",
-  "CameraRoll",
-  "Clipboard",
-  "DatePickerAndroid",
-  "Dimensions",
-  "Easing",
-  "Geolocation",
-  "ImageEditor",
-  "ImagePickerIOS",
-  "ImageStore",
-  "InteractionManager",
-  "Keyboard",
-  "LayoutAnimation",
-  "Linking",
-  "ListViewDataSource",
-  "NetInfo",
-  "PanResponder",
-  "PermissionsAndroid",
-  "PixelRatio",
-  "PushNotificationIOS",
-  "Settings",
-  "Share",
-  "StatusBarIOS",
-  "StyleSheet",
-  "Systrace",
-  "TimePickerAndroid",
-  "ToastAndroid",
-  "Transforms",
-  "Vibration",
-  "VibrationIOS"
-]
+  'AccessibilityInfo',
+  'ActionSheetIOS',
+  'Alert',
+  'AlertIOS',
+  'Animated',
+  'AppRegistry',
+  'AppState',
+  'AsyncStorage',
+  'BackAndroid',
+  'BackHandler',
+  'CameraRoll',
+  'Clipboard',
+  'DatePickerAndroid',
+  'Dimensions',
+  'Easing',
+  'Geolocation',
+  'ImageEditor',
+  'ImagePickerIOS',
+  'ImageStore',
+  'InteractionManager',
+  'Keyboard',
+  'LayoutAnimation',
+  'Linking',
+  'ListViewDataSource',
+  'NetInfo',
+  'PanResponder',
+  'PermissionsAndroid',
+  'PixelRatio',
+  'PushNotificationIOS',
+  'Settings',
+  'Share',
+  'StatusBarIOS',
+  'StyleSheet',
+  'Systrace',
+  'TimePickerAndroid',
+  'ToastAndroid',
+  'Transforms',
+  'Vibration',
+  'VibrationIOS',
+];
 
 const topics = {
-  "Flow": "Flow",
-  "Flow-Strict": "Flow",
-  "xhr": labelNetworking,
-  "netinfo": labelNetworking,
-  "fetch": labelNetworking,
-  "okhttp": labelNetworking,
-  "http": labelNetworking,
-  "bundle": labelBundler,
-  "bundling": labelBundler,
-  "packager": labelBundler,
-  "unable to resolve module": labelBundler,
-  "android": labelAndroid,
-  "ios": labelIos,
-  "tvos": labelTvos,
-  "react-native-cli": labelCli,
-  "react-native upgrade": labelCli,
-  "react-native link": labelCli,
-  "local-cli": labelCli,
-  "regression": labelRegression
-}
+  Flow: 'Flow',
+  'Flow-Strict': 'Flow',
+  xhr: labelNetworking,
+  netinfo: labelNetworking,
+  fetch: labelNetworking,
+  okhttp: labelNetworking,
+  http: labelNetworking,
+  bundle: labelBundler,
+  bundling: labelBundler,
+  packager: labelBundler,
+  'unable to resolve module': labelBundler,
+  android: labelAndroid,
+  ios: labelIos,
+  tvos: labelTvos,
+  'react-native-cli': labelCli,
+  'react-native upgrade': labelCli,
+  'react-native link': labelCli,
+  'local-cli': labelCli,
+  regression: labelRegression,
+};
