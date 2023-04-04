f4f3aa31f344cb7e1cca76ff497c8ca0292f9e42
Distiller <distiller@static.198.206.135.14.macminivault.com>
2023-04-04 10:13:00 +0000

    [0.69.9] Bump version numbers

--- React/Base/RCTVersion.m
+++ React/Base/RCTVersion.m
@@ -23,7 +23,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
     __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(69),
-                  RCTVersionPatch: @(8),
+                  RCTVersionPatch: @(9),
                   RCTVersionPrerelease: [NSNull null],
                   };
   });
