c3b91c2e8a54003a2a0fa233d08c0323a6f4829e
Nicola Corti <ncor@meta.com>
2023-04-17 14:43:59 +0100

    prettier - round 2

--- packages/rn-tester/NativeComponentExample/js/MyLegacyViewNativeComponent.js
+++ packages/rn-tester/NativeComponentExample/js/MyLegacyViewNativeComponent.js
@@ -45,7 +45,7 @@ export function callNativeMethodToChangeBackgroundColor(
   UIManager.dispatchViewManagerCommand(
     ReactNative.findNodeHandle(viewRef),
     UIManager.getViewManagerConfig(
-      'RNTMyLegacyNativeView'
+      'RNTMyLegacyNativeView',
     ).Commands.changeBackgroundColor.toString(),
     [color],
   );
