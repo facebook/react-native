f60938ede0dfb43699f69e6ecb2007ef750ff262
Alex Hunt <hello@alexhunt.io>
2023-04-19 13:34:57 +0100

    [0.72] Drop internal base config merge in metro-config
    Cherry pick https://github.com/facebook/react-native/pull/36777

--- packages/metro-config/index.js
+++ packages/metro-config/index.js
@@ -8,9 +8,9 @@
  * @noformat
  */
 
-/*:: import type {ConfigT} from 'metro-config'; */
+/*:: import type {MetroConfig} from 'metro-config'; */
 
-const {getDefaultConfig: getBaseConfig, mergeConfig} = require('metro-config');
+const {mergeConfig} = require('metro-config');
 
 const INTERNAL_CALLSITES_REGEX = new RegExp(
   [
@@ -37,8 +37,8 @@ const INTERNAL_CALLSITES_REGEX = new RegExp(
  */
 function getDefaultConfig(
   projectRoot /*: string */
-) /*: ConfigT */ {
-  const config = {
+) /*: MetroConfig */ {
+  return {
     resolver: {
       resolverMainFields: ['react-native', 'browser', 'main'],
       platforms: ['android', 'ios'],
@@ -76,11 +76,6 @@ function getDefaultConfig(
     },
     watchFolders: [],
   };
-
-  return mergeConfig(
-    getBaseConfig.getDefaultValues(projectRoot),
-    config,
-  );
 }
 
 module.exports = {getDefaultConfig, mergeConfig};
