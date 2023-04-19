93851fc2c8113bb38b4172ff1d60c00dae8dbfb8
Lulu Wu <luluwu@meta.com>
2023-04-19 13:15:47 -0700

    Move Hermes code coverage to non-OSS (#36800)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/36800
    
    Hermes code coverage was added in D43458602, move the logic to internal to avoid internal dependencies in Bridgeless.
    
    Changelog:
    [Android][Changed] - Move Hermes code coverage to non-OSS
    
    Reviewed By: cortinico
    
    Differential Revision: D44626679
    
    fbshipit-source-id: 95d71ec6fe6c75664682511efb571a8122d955c2

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridgeless/hermes/HermesInstance.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridgeless/hermes/HermesInstance.kt
@@ -7,16 +7,13 @@
 
 package com.facebook.react.bridgeless.hermes
 
-import com.facebook.common.process.ProcessName
 import com.facebook.jni.HybridData
 import com.facebook.jni.annotations.DoNotStrip
-import com.facebook.jsi.mdcd.HermesCodeCoverage
-import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridgeless.JSEngineInstance
 import com.facebook.soloader.SoLoader
 import kotlin.jvm.JvmStatic
 
-class HermesInstance(context: ReactApplicationContext) : JSEngineInstance(initHybrid()!!) {
+class HermesInstance() : JSEngineInstance(initHybrid()!!) {
 
   companion object {
     @JvmStatic @DoNotStrip protected external fun initHybrid(): HybridData?
@@ -24,11 +21,5 @@ class HermesInstance(context: ReactApplicationContext) : JSEngineInstance(initHy
     init {
       SoLoader.loadLibrary("hermesinstancejni")
     }
-  }
-
-  init {
-    // Initialize Code Coverage tracing for select usecases, like E2E coverage and Hermes MDCD
-    val processName = ProcessName.current().fullName
-    HermesCodeCoverage.initialize(context, processName.orEmpty())
   }
 }
