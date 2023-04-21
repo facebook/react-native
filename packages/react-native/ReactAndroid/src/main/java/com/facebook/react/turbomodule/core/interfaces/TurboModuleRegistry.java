ac5b31c4bb78d21b0b1d9fd47f62f642f82b9165
Ramanpreet Nara <ramanpreet@fb.com>
2023-04-21 15:54:19 -0700

    Remove getNative* TurboModuleRegistry APIs
    Summary:
    These getNative* TurboModuleRegistry APIs make the TurboModuleManager harder to understand. They should have never been introduced.
    
    Let's remove them for added clarity.
    
    Changelog: [Android][Removed] - Remove TurboModuleManager.getNativeModule,getNativeModules,hasNativeModule
    
    Differential Revision: https://internalfb.com/D45158032
    
    fbshipit-source-id: 377afc2505fa26e01270e62be91c1a349be91ab7

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/interfaces/TurboModuleRegistry.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/interfaces/TurboModuleRegistry.java
@@ -20,35 +20,18 @@ import java.util.List;
  */
 public interface TurboModuleRegistry {
   /**
-   * Return the TurboModule instance that has that name `moduleName`. If the `moduleName`
+   * Return the NativeModule instance that has that name `moduleName`. If the `moduleName`
    * TurboModule hasn't been instantiated, instantiate it. If no TurboModule is registered under
    * `moduleName`, return null.
    */
-  @Deprecated
   @Nullable
-  TurboModule getModule(String moduleName);
+  NativeModule getModule(String moduleName);
 
-  /** Get all instantiated TurboModules. */
-  @Deprecated
-  Collection<TurboModule> getModules();
-
-  /** Has the TurboModule with name `moduleName` been instantiated? */
-  @Deprecated
-  boolean hasModule(String moduleName);
-
-  /**
-   * Return the NativeModule instance that has that name `moduleName`. If the `moduleName`
-   * NativeModule hasn't been instantiated, instantiate it. If no NativeModule is registered under
-   * `moduleName`, return null.
-   */
-  @Nullable
-  NativeModule getNativeModule(String moduleName);
-
-  /** Get all instantiated NativeModule. */
-  Collection<NativeModule> getNativeModules();
+  /** Get all instantiated NativeModules. */
+  Collection<NativeModule> getModules();
 
   /** Has the NativeModule with name `moduleName` been instantiated? */
-  boolean hasNativeModule(String moduleName);
+  boolean hasModule(String moduleName);
 
   /**
    * Return the names of all the NativeModules that are supposed to be eagerly initialized. By
