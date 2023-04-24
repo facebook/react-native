ba927004c527708591d8a38eadbbd3c8bb126661
Tim Yung <yungsters@meta.com>
2023-04-24 15:01:51 -0700

    RN: Cleanup `NativeExceptionsManager` Mock (#37074)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37074
    
    I am exploring ways of cleaning up the React Native Jest `setup.js` logic so that we can enable Flow and make it easier to maintain.
    
    This does a few things:
    
    - Create a new Jest test to verify that mocking works as expected (i.e. `jest.mock(X)` and `import ... from 'X'` does the right thing).
    - Move the mock implementation for `NativeExceptionsManager` into a `__mocks__` directory that Jest can find automatically.
    - Add `flow strict` and fill out the missing implementation details of the `NativeExceptionsManager` mock.
    
    Changelog:
    [General][Added] - Added missing methods to `NativeExceptionsManager` mock in Jest
    
    Reviewed By: fkgozali
    
    Differential Revision: D45244175
    
    fbshipit-source-id: 0f4c8f873f93304509b80a13ed52e96265d10a42

--- packages/react-native/jest/setup.js
+++ packages/react-native/jest/setup.js
@@ -58,12 +58,7 @@ Object.defineProperties(global, {
 
 jest
   .mock('../Libraries/Core/InitializeCore', () => {})
-  .mock('../Libraries/Core/NativeExceptionsManager', () => ({
-    __esModule: true,
-    default: {
-      reportException: jest.fn(),
-    },
-  }))
+  .mock('../Libraries/Core/NativeExceptionsManager')
   .mock('../Libraries/ReactNative/UIManager', () => ({
     AndroidViewPager: {
       Commands: {
