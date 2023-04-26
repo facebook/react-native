893008b701cf83b88c93c2844c485b5152ce7d02
Sam Zhou <samzhou19815@meta.com>
2023-04-25 18:01:39 -0700

    Fix incorrect usage of React.ElementConfig and React.ElementRef in xplat (#37083)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37083
    
    `React.ElementConfig<a polymophic type>` will result in unpredictable behavior in Flow due to some types being unresolved. Since the type is already broken in most cases, I use $FlowFixMe as the type for those type arguments that are currently inferred as empty.
    
    Changelog: [Internal]
    
    Reviewed By: NickGerleman
    
    Differential Revision: D44774275
    
    fbshipit-source-id: 440b6c731fa28f40a7246883ab32f2a4d78f9cd1

--- packages/rn-tester/js/examples/SectionList/SectionList-scrollable.js
+++ packages/rn-tester/js/examples/SectionList/SectionList-scrollable.js
@@ -358,7 +358,7 @@ const styles = StyleSheet.create({
 export default {
   title: 'SectionList scrollable',
   name: 'SectionList-scrollable',
-  render: function (): React.Element<typeof SectionList_scrollable> {
+  render: function (): React.MixedElement {
     return <SectionList_scrollable />;
   },
 };
