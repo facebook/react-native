6159fc9513b25b60a20ce5e2ee1b13715ba98543
Nick Gerleman <ngerlem@meta.com>
2023-04-27 00:21:02 -0700

    Define Flag operators for YGPrintOptions (#37115)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37115
    
    X-link: https://github.com/facebook/yoga/pull/1267
    
    This is an existing bitset enum in the API. Use the facility added in the last diff to add flag operators to it, to avoid the need for casting, and to make it clearer in the generated YGEnums.h that it is a bitset.
    
    Reviewed By: rshest
    
    Differential Revision: D45341504
    
    fbshipit-source-id: 3c6a416a1e4ec178c2256dfa96575a7d7d1e2fca

--- packages/react-native/ReactCommon/yoga/yoga/YGEnums.h
+++ packages/react-native/ReactCommon/yoga/yoga/YGEnums.h
@@ -127,6 +127,7 @@ YG_ENUM_DECL(
     YGPrintOptionsLayout = 1,
     YGPrintOptionsStyle = 2,
     YGPrintOptionsChildren = 4)
+YG_DEFINE_ENUM_FLAG_OPERATORS(YGPrintOptions)
 
 YG_ENUM_SEQ_DECL(
     YGUnit,
