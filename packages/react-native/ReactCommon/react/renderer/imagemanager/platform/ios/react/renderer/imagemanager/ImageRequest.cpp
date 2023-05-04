9ace9639e861f2dc6b1f65b52d2f2d6c0cb65fb1
Samuel Susla <samuelsusla@meta.com>
2023-05-03 22:16:20 -0700

    Move cancelRequest_ in move ctor in ImageRequest (#37221)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37221
    
    changelog: [internal]
    
    Add missing cancelRequest_ std::move.
    
    Reviewed By: javache, cortinico
    
    Differential Revision: D45524704
    
    fbshipit-source-id: e492ba4763f66977c8407b09ad838f7f028cff4e

--- packages/react-native/ReactCommon/react/renderer/imagemanager/platform/ios/react/renderer/imagemanager/ImageRequest.cpp
+++ packages/react-native/ReactCommon/react/renderer/imagemanager/platform/ios/react/renderer/imagemanager/ImageRequest.cpp
@@ -16,16 +16,6 @@ ImageRequest::ImageRequest(
   coordinator_ = std::make_shared<ImageResponseObserverCoordinator>();
 }
 
-ImageRequest::ImageRequest(ImageRequest &&other) noexcept
-    : imageSource_(std::move(other.imageSource_)),
-      telemetry_(std::move(other.telemetry_)),
-      coordinator_(std::move(other.coordinator_)) {
-  other.coordinator_ = nullptr;
-  other.cancelRequest_ = nullptr;
-  other.telemetry_ = nullptr;
-  other.imageSource_ = {};
-}
-
 ImageRequest::~ImageRequest() {
   if (cancelRequest_) {
     cancelRequest_();
