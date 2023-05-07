301137ffa2bca35f260dcec5952a8c38385b7128
Nick Gerleman <ngerlem@meta.com>
2023-05-07 00:12:49 -0700

    Refactor MobileConfig Access for "react_fabric: treat_auto_as_undefined" (#37208)
    Summary: Pull Request resolved: https://github.com/facebook/react-native/pull/37208
    
    Reviewed By: javache
    
    Differential Revision: D45434603
    
    fbshipit-source-id: 653f0949d8eedf2317f8a229be6fd68000852539

---
+++ packages/react-native/ReactCommon/react/renderer/core/PropsParserContext.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "PropsParserContext.h"
+
+#include <react/config/ReactNativeConfig.h>
+
+namespace facebook::react {
+
+bool PropsParserContext::treatAutoAsYGValueUndefined() const {
+  if (treatAutoAsYGValueUndefined_ == std::nullopt) {
+    auto config = contextContainer.at<std::shared_ptr<const ReactNativeConfig>>(
+        "ReactNativeConfig");
+    treatAutoAsYGValueUndefined_ = config
+        ? config->getBool("react_fabric:treat_auto_as_undefined")
+        : false;
+  }
+
+  return *treatAutoAsYGValueUndefined_;
+}
+
+} // namespace facebook::react
