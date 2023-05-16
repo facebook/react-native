b83372132299376b96d54804da3b4cbb41626901
Luna Wei <luwe@meta.com>
2023-05-16 15:34:20 -0700

    Refactor set-rn-version and add tests (#37291)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37291
    
    Changelog: [Internal] Add tests for `rn-set-version` script and refactor for clarity
    
    Reviewed By: NickGerleman
    
    Differential Revision: D45248915
    
    fbshipit-source-id: dd58c714a8098e0405a59a54073849209d0ef726

--- scripts/set-rn-version.js
+++ scripts/set-rn-version.js
@@ -9,13 +9,6 @@
 
 'use strict';
 
-/**
- * This script updates relevant React Native files with supplied version:
- *   * Prepares a package.json suitable for package consumption
- *   * Updates package.json for template project
- *   * Updates the version in gradle files and makes sure they are consistent between each other
- *   * Creates a gemfile
- */
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
@@ -24,155 +17,160 @@ const yargs = require('yargs');
 const {parseVersion, validateBuildType} = require('./version-utils');
 const {saveFiles} = require('./scm-utils');
 
-let argv = yargs
-  .option('v', {
-    alias: 'to-version',
-    type: 'string',
-    required: true,
-  })
-  .option('b', {
-    alias: 'build-type',
-    type: 'string',
-    required: true,
-  }).argv;
+/**
+ * This script updates relevant React Native files with supplied version:
+ *   * Prepares a package.json suitable for package consumption
+ *   * Updates package.json for template project
+ *   * Updates the version in gradle files and makes sure they are consistent between each other
+ *   * Creates a gemfile
+ */
+if (require.main === module) {
+  let argv = yargs
+    .option('v', {
+      alias: 'to-version',
+      type: 'string',
+      required: true,
+    })
+    .option('b', {
+      alias: 'build-type',
+      type: 'string',
+      required: true,
+    }).argv;
 
-const buildType = argv.buildType;
-const version = argv.toVersion;
-
-try {
-  validateBuildType(buildType);
-} catch (e) {
-  throw e;
+  setReactNativeVersion(argv.toVersion, argv.buildType);
 }
 
-let major,
-  minor,
-  patch,
-  prerelease = -1;
-try {
-  ({major, minor, patch, prerelease} = parseVersion(version, buildType));
-} catch (e) {
-  throw e;
+function setSource({major, minor, patch, prerelease}) {
+  fs.writeFileSync(
+    'packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/ReactNativeVersion.java',
+    cat('scripts/versiontemplates/ReactNativeVersion.java.template')
+      .replace('${major}', major)
+      .replace('${minor}', minor)
+      .replace('${patch}', patch)
+      .replace(
+        '${prerelease}',
+        prerelease !== undefined ? `"${prerelease}"` : 'null',
+      ),
+    'utf-8',
+  );
+
+  fs.writeFileSync(
+    'packages/react-native/React/Base/RCTVersion.m',
+    cat('scripts/versiontemplates/RCTVersion.m.template')
+      .replace('${major}', `@(${major})`)
+      .replace('${minor}', `@(${minor})`)
+      .replace('${patch}', `@(${patch})`)
+      .replace(
+        '${prerelease}',
+        prerelease !== undefined ? `@"${prerelease}"` : '[NSNull null]',
+      ),
+    'utf-8',
+  );
+
+  fs.writeFileSync(
+    'packages/react-native/ReactCommon/cxxreact/ReactNativeVersion.h',
+    cat('scripts/versiontemplates/ReactNativeVersion.h.template')
+      .replace('${major}', major)
+      .replace('${minor}', minor)
+      .replace('${patch}', patch)
+      .replace(
+        '${prerelease}',
+        prerelease !== undefined ? `"${prerelease}"` : '""',
+      ),
+    'utf-8',
+  );
+
+  fs.writeFileSync(
+    'packages/react-native/Libraries/Core/ReactNativeVersion.js',
+    cat('scripts/versiontemplates/ReactNativeVersion.js.template')
+      .replace('${major}', major)
+      .replace('${minor}', minor)
+      .replace('${patch}', patch)
+      .replace(
+        '${prerelease}',
+        prerelease !== undefined ? `'${prerelease}'` : 'null',
+      ),
+    'utf-8',
+  );
 }
-
-const tmpVersioningFolder = fs.mkdtempSync(
-  path.join(os.tmpdir(), 'rn-set-version'),
-);
-echo(`The temp versioning folder is ${tmpVersioningFolder}`);
-
-saveFiles(
-  [
-    'packages/react-native/package.json',
-    'packages/react-native/template/package.json',
-  ],
-  tmpVersioningFolder,
-);
-
-fs.writeFileSync(
-  'packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/ReactNativeVersion.java',
-  cat('scripts/versiontemplates/ReactNativeVersion.java.template')
-    .replace('${major}', major)
-    .replace('${minor}', minor)
-    .replace('${patch}', patch)
-    .replace(
-      '${prerelease}',
-      prerelease !== undefined ? `"${prerelease}"` : 'null',
-    ),
-  'utf-8',
-);
-
-fs.writeFileSync(
-  'packages/react-native/React/Base/RCTVersion.m',
-  cat('scripts/versiontemplates/RCTVersion.m.template')
-    .replace('${major}', `@(${major})`)
-    .replace('${minor}', `@(${minor})`)
-    .replace('${patch}', `@(${patch})`)
-    .replace(
-      '${prerelease}',
-      prerelease !== undefined ? `@"${prerelease}"` : '[NSNull null]',
-    ),
-  'utf-8',
-);
-
-fs.writeFileSync(
-  'packages/react-native/ReactCommon/cxxreact/ReactNativeVersion.h',
-  cat('scripts/versiontemplates/ReactNativeVersion.h.template')
-    .replace('${major}', major)
-    .replace('${minor}', minor)
-    .replace('${patch}', patch)
-    .replace(
-      '${prerelease}',
-      prerelease !== undefined ? `"${prerelease}"` : '""',
-    ),
-  'utf-8',
-);
-
-fs.writeFileSync(
-  'packages/react-native/Libraries/Core/ReactNativeVersion.js',
-  cat('scripts/versiontemplates/ReactNativeVersion.js.template')
-    .replace('${major}', major)
-    .replace('${minor}', minor)
-    .replace('${patch}', patch)
-    .replace(
-      '${prerelease}',
-      prerelease !== undefined ? `'${prerelease}'` : 'null',
-    ),
-  'utf-8',
-);
-
-const packageJson = JSON.parse(cat('packages/react-native/package.json'));
-packageJson.version = version;
-
-fs.writeFileSync(
-  'packages/react-native/package.json',
-  JSON.stringify(packageJson, null, 2),
-  'utf-8',
-);
 
 // Change ReactAndroid/gradle.properties
-saveFiles(
-  ['packages/react-native/ReactAndroid/gradle.properties'],
-  tmpVersioningFolder,
-);
-if (
-  sed(
+function setGradle({version}) {
+  const result = sed(
     '-i',
     /^VERSION_NAME=.*/,
     `VERSION_NAME=${version}`,
     'packages/react-native/ReactAndroid/gradle.properties',
-  ).code
-) {
-  echo("Couldn't update version for Gradle");
-  exit(1);
-}
-
-// Change react-native version in the template's package.json
-exec(`node scripts/set-rn-template-version.js ${version}`);
-
-// Verify that files changed, we just do a git diff and check how many times version is added across files
-const filesToValidate = [
-  'packages/react-native/package.json',
-  'packages/react-native/ReactAndroid/gradle.properties',
-  'packages/react-native/template/package.json',
-];
-
-const numberOfChangedLinesWithNewVersion = exec(
-  `diff -r ${tmpVersioningFolder} . | grep '^[>]' | grep -c ${version} `,
-  {silent: true},
-).stdout.trim();
-
-if (+numberOfChangedLinesWithNewVersion !== filesToValidate.length) {
-  // TODO: the logic that checks whether all the changes have been applied
-  // is missing several files. For example, it is not checking Ruby version nor that
-  // the Objecive-C files, the codegen and other files are properly updated.
-  // We are going to work on this in another PR.
-  echo('WARNING:');
-  echo(
-    `Failed to update all the files: [${filesToValidate.join(
-      ', ',
-    )}] must have versions in them`,
   );
-  echo(`These files already had version ${version} set.`);
+  if (result.code) {
+    echo("Couldn't update version for Gradle");
+    throw result.stderr;
+  }
 }
 
-exit(0);
+function setPackage({version}) {
+  const packageJson = JSON.parse(cat('packages/react-native/package.json'));
+  packageJson.version = version;
+
+  fs.writeFileSync(
+    'packages/react-native/package.json',
+    JSON.stringify(packageJson, null, 2),
+    'utf-8',
+  );
+}
+
+function setTemplatePackage({version}) {
+  const result = exec(`node scripts/set-rn-template-version.js ${version}`);
+  if (result.code) {
+    echo("Failed to update React Native template's version of React Native");
+    throw result.stderr;
+  }
+}
+
+function setReactNativeVersion(argVersion, buildType) {
+  validateBuildType(buildType);
+
+  const version = parseVersion(argVersion, buildType);
+
+  // Create tmp folder for copies of files to verify files have changed
+  const filesToValidate = [
+    'packages/react-native/package.json',
+    'packages/react-native/ReactAndroid/gradle.properties',
+    'packages/react-native/template/package.json',
+  ];
+  const tmpVersioningFolder = fs.mkdtempSync(
+    path.join(os.tmpdir(), 'rn-set-version'),
+  );
+  echo(`The tmp versioning folder is ${tmpVersioningFolder}`);
+  saveFiles(tmpVersioningFolder);
+
+  setSource(version);
+  setPackage(version);
+  setTemplatePackage(version);
+  setGradle(version);
+
+  // Validate changes
+  // We just do a git diff and check how many times version is added across files
+  const numberOfChangedLinesWithNewVersion = exec(
+    `diff -r ${tmpVersioningFolder} . | grep '^[>]' | grep -c ${version.version} `,
+    {silent: true},
+  ).stdout.trim();
+
+  if (+numberOfChangedLinesWithNewVersion !== filesToValidate.length) {
+    // TODO: the logic that checks whether all the changes have been applied
+    // is missing several files. For example, it is not checking Ruby version nor that
+    // the Objecive-C files, the codegen and other files are properly updated.
+    // We are going to work on this in another PR.
+    echo('WARNING:');
+    echo(
+      `Failed to update all the files: [${filesToValidate.join(
+        ', ',
+      )}] must have versions in them`,
+    );
+    echo(`These files already had version ${version.version} set.`);
+  }
+
+  return exit(0);
+}
+
+module.exports = setReactNativeVersion;
