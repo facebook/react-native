9a5811abc973dbe451db4e4d9808ec6284c3dfb4
Nicola Corti <ncor@meta.com>
2023-05-22 09:25:41 -0700

    Make sure the Native RuntimeScheduler is initialized on Old Arch (#37517)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37517
    
    Fixes #35778
    
    We got reports of regressions on `useEffect` starting from 0.69+ when on Hermes.
    
    The issue seems to be caused by a bump of the `scheduler` package from 0.20 to 0.21.
    In scheduler@0.21, the method `setImmediate` gets called if available
    (see https://github.com/facebook/react/pull/20834). This causes React Native to use Microtasks
    which ends up in changing the semantic of useEffect.
    
    The solution is to use the Native RuntimeScheduler properly.
    On Paper specifically, we never initialized it as it's effectively initialized by the
    TurboModuleManagerDelegate. Here I trigger the initialization of it on Paper as well.
    
    Changelog:
    [Android] [Fixed] - Make sure the Native RuntimeScheduler is initialized on Old Arch
    
    Reviewed By: sammy-SC
    
    Differential Revision: D46024807
    
    fbshipit-source-id: 133d6de5d42f00718092601efc8b7862eda646c9

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/config/ReactFeatureFlags.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/config/ReactFeatureFlags.java
@@ -51,6 +51,13 @@ public class ReactFeatureFlags {
   public static volatile boolean unstable_useFabricInterop = false;
 
   /**
+   * Should this application always use the Native RuntimeScheduler? If yes, we'll be instantiating
+   * it over all the architectures (both Old and New). This is intentionally set to true as we want
+   * to use it more as a kill-switch to turn off this feature to potentially debug issues.
+   */
+  public static volatile boolean unstable_useRuntimeSchedulerAlways = true;
+
+  /**
    * Feature flag to enable the new bridgeless architecture. Note: Enabling this will force enable
    * the following flags: `useTurboModules` & `enableFabricRenderer`.
    */
