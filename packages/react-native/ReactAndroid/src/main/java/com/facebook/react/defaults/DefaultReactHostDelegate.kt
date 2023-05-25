83294df59dcad48acffc595dc9e006de09cf05ea
David Vacca <dvacca@fb.com>
2023-05-25 09:22:06 -0700

    Add documentation into DefaultReactHostDelegate
    Summary:
    Add documentation into DefaultReactHostDelegate
    
    changelog: [internal] internal
    
    Differential Revision: D45717456
    
    fbshipit-source-id: e6d7a88a7d8a255588d697c08c17909c1f06d7c7

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultReactHostDelegate.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultReactHostDelegate.kt
@@ -7,6 +7,7 @@
 
 package com.facebook.react.defaults
 
+import com.facebook.jni.annotations.DoNotStrip
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.JSBundleLoader
 import com.facebook.react.bridge.ReactApplicationContext
@@ -19,6 +20,25 @@ import com.facebook.react.common.annotations.UnstableReactNativeAPI
 import com.facebook.react.fabric.ReactNativeConfig
 import com.facebook.react.turbomodule.core.TurboModuleManagerDelegate
 
+/**
+ * A utility class that allows you to simplify the initialization of React Native by setting up a
+ * [ReactHostDelegate] that uses recommended dependencies.
+ *
+ * @param jSMainModulePath Path to your app's main module on Metro. This is used when reloading JS
+ *   during development. All paths are relative to the root folder the packager is serving files
+ *   from. Examples: `index.android` or `subdirectory/index.android`
+ * @param jSBundleLoader Bundle loader to use when setting up JS environment. <p>Example:
+ *   [JSBundleLoader.createFileLoader(application, bundleFile)]
+ * @param reactPackages list of reactPackages to expose Native Modules and View Components to JS
+ * @param jSEngineInstance Object that holds a native reference to the javascript engine
+ * @param bindingsInstaller Object that holds a native C++ references that allow host applications
+ *   to install C++ objects into jsi::Runtime during the initialization of React Native
+ * @param reactNativeConfig ReactNative Configuration that allows to customize the behavior of
+ *   key/value pairs used by the framework to enable/disable experimental capabilities
+ * @param exceptionHandler Callback that can be used by React Native host applications to react to
+ *   exceptions thrown by the internals of React Native.
+ */
+@DoNotStrip
 @UnstableReactNativeAPI
 class DefaultReactHostDelegate(
     override val jSMainModulePath: String,
