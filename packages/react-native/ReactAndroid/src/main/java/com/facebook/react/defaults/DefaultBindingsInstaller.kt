d45d15f4ae739b143bd3a460d9a4300fecc11b00
David Vacca <dvacca@meta.com>
2023-05-25 10:29:01 -0700

    Introduce DefaultReactHostDelegate (#37565)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37565
    
    Introduce DefaultReactHostDelegate class that will be used to simplify creation of ReactHostDelegate in React Native Android
    
    changelog: [internal] internal
    
    Reviewed By: cortinico, philIip
    
    Differential Revision: D45665528
    
    fbshipit-source-id: 154f4a2f0916c6f1cb846bc052a86dfc72f04dd7

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultBindingsInstaller.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.defaults
+
+import com.facebook.react.bridgeless.BindingsInstaller
+import com.facebook.react.common.annotations.UnstableReactNativeAPI
+
+@UnstableReactNativeAPI class DefaultBindingsInstaller : BindingsInstaller(null) {}
