39d4c43d7412de41c76ec34016d1f9945ab8a5c9
Nicola Corti <ncor@meta.com>
2023-06-01 02:45:35 -0700

    Configure JDK Toolchain to 11 for all the 3rd party libraries. (#37575)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37575
    
    This fixes the behavior reported in https://github.com/reactwg/react-native-releases/discussions/54#discussioncomment-5984629
    by forcing all the libraries to compile with JDK 11.
    
    This prevents build issues when on Gradle 8 and Kotlin 1.8
    
    Changelog:
    [Internal] [Changed] - Configure JDK Toolchain to 11 for all the 3rd party libraries
    
    Reviewed By: cipolleschi
    
    Differential Revision: D46190989
    
    fbshipit-source-id: b0ba805a3109d7f4816e607aae5eca79ee4d6444

--- packages/react-native-gradle-plugin/src/main/kotlin/com/facebook/react/utils/JdkConfiguratorUtils.kt
+++ packages/react-native-gradle-plugin/src/main/kotlin/com/facebook/react/utils/JdkConfiguratorUtils.kt
@@ -8,26 +8,40 @@
 package com.facebook.react.utils
 
 import com.android.build.api.variant.AndroidComponentsExtension
+import com.facebook.react.utils.PropertyUtils.INTERNAL_DISABLE_JAVA_VERSION_ALIGNMENT
+import org.gradle.api.Action
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.api.plugins.AppliedPlugin
 import org.jetbrains.kotlin.gradle.dsl.KotlinTopLevelExtension
 
 internal object JdkConfiguratorUtils {
   /**
-   * Function that takes care of configuring the JDK toolchain for Application projects. As we do
-   * decide the JDK version based on the AGP version that RNGP brings over, here we can safely
+   * Function that takes care of configuring the JDK toolchain for all the projects projects. As we
+   * do decide the JDK version based on the AGP version that RNGP brings over, here we can safely
    * configure the toolchain to 11.
    */
-  fun configureJavaToolChains(project: Project) {
-    project.extensions.getByType(AndroidComponentsExtension::class.java).finalizeDsl { ext ->
-      ext.compileOptions.sourceCompatibility = JavaVersion.VERSION_11
-      ext.compileOptions.targetCompatibility = JavaVersion.VERSION_11
+  fun configureJavaToolChains(input: Project) {
+    if (input.hasProperty(INTERNAL_DISABLE_JAVA_VERSION_ALIGNMENT)) {
+      return
     }
-    project.pluginManager.withPlugin("org.jetbrains.kotlin.android") {
-      project.extensions.getByType(KotlinTopLevelExtension::class.java).jvmToolchain(11)
-    }
-    project.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
-      project.extensions.getByType(KotlinTopLevelExtension::class.java).jvmToolchain(11)
+    input.rootProject.allprojects { project ->
+      val action =
+          Action<AppliedPlugin> {
+            project.extensions.getByType(AndroidComponentsExtension::class.java).finalizeDsl { ext
+              ->
+              ext.compileOptions.sourceCompatibility = JavaVersion.VERSION_11
+              ext.compileOptions.targetCompatibility = JavaVersion.VERSION_11
+            }
+          }
+      project.pluginManager.withPlugin("com.android.application", action)
+      project.pluginManager.withPlugin("com.android.library", action)
+      project.pluginManager.withPlugin("org.jetbrains.kotlin.android") {
+        project.extensions.getByType(KotlinTopLevelExtension::class.java).jvmToolchain(11)
+      }
+      project.pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+        project.extensions.getByType(KotlinTopLevelExtension::class.java).jvmToolchain(11)
+      }
     }
   }
 }
