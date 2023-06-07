36dafa2b677370b435f02e47d126c1ea839ee482
Dmitry Rykun <dmitryrykun@meta.com>
2023-06-07 12:25:42 -0700

    Enable @react-native/babel-plugin-codegen for RNTester (#37757)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37757
    
    This diff adds `react-native/babel-plugin-codegen` as an extra Babel plugin for Metro in RNTester to enable static view configs support.
    This diff does not provide full static view configs support for every React Native project. It only enables them for RNTester to unblock further bridgeless mode OSS rollout work.
    
    Changelog: [Internal] - Static view configs support for RNTester.
    
    Reviewed By: cortinico
    
    Differential Revision: D46523066
    
    fbshipit-source-id: 4ad3b49b826c4075b33950089da78b4798c12ea7

---
+++ packages/rn-tester/babel.config.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+ module.exports = {
+   presets: ['module:metro-react-native-babel-preset'],
+   plugins: ['@react-native/babel-plugin-codegen'],
+ };
