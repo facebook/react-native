de6bfec82a1cb4b387bfe8a87b1f597f39572764
Riccardo Cipolleschi <cipolleschi@meta.com>
2023-06-14 07:46:07 -0700

    Disable bitcode from Hermes (#37868)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37868
    
    Thanks to the help of the community, we discovered that we can safely disable the bitcode setting in Hermes.
    
    Community testing reported that the tarball size get reduced from ~500 Mb to ~25 Mb, a x20 reduction
    
    ## Changelog:
    [iOS][Changed] - Disabled bitcode for Hermes prebuilts
    
    Reviewed By: dmytrorykun
    
    Differential Revision: D46704633
    
    fbshipit-source-id: a6624110f27eb9f18e6b57ed28aa2f86804d45e7

--- packages/react-native/sdks/hermes-engine/utils/build-apple-framework.sh
+++ packages/react-native/sdks/hermes-engine/utils/build-apple-framework.sh
@@ -52,13 +52,8 @@ function build_host_hermesc {
 
 # Utility function to configure an Apple framework
 function configure_apple_framework {
-  local enable_bitcode enable_debugger cmake_build_type
+  local enable_debugger cmake_build_type
 
-  if [[ $1 == iphoneos || $1 == catalyst ]]; then
-    enable_bitcode="true"
-  else
-    enable_bitcode="false"
-  fi
   if [[ $BUILD_TYPE == "Debug" ]]; then
     enable_debugger="true"
   else
@@ -82,7 +77,7 @@ function configure_apple_framework {
       -DHERMES_ENABLE_LIBFUZZER:BOOLEAN=false \
       -DHERMES_ENABLE_FUZZILLI:BOOLEAN=false \
       -DHERMES_ENABLE_TEST_SUITE:BOOLEAN=false \
-      -DHERMES_ENABLE_BITCODE:BOOLEAN="$enable_bitcode" \
+      -DHERMES_ENABLE_BITCODE:BOOLEAN=false \
       -DHERMES_BUILD_APPLE_FRAMEWORK:BOOLEAN=true \
       -DHERMES_BUILD_APPLE_DSYM:BOOLEAN=true \
       -DIMPORT_HERMESC:PATH="$IMPORT_HERMESC_PATH" \
