e94e87fb1e3a876f2958d87f408ee3fd210cfeb7
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-06-15 16:15:18 +0200

    Fix import

--- packages/virtualized-lists/Lists/VirtualizedList.js
+++ packages/virtualized-lists/Lists/VirtualizedList.js
@@ -14,6 +14,7 @@ import type {
   LayoutEvent,
   ScrollEvent,
 } from 'react-native/Libraries/Types/CoreEventTypes';
+import Platform from 'react-native/Libraries/Utilities/Platform';
 import type {ViewToken} from './ViewabilityHelper';
 import type {
   Item,
