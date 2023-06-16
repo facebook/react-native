c5496fd93f3c09d72b249b8154f17af8a6df38fb
David Vacca <dvacca@meta.com>
2023-06-15 22:34:36 -0700

    Introduce ReactSurfaceInterface
    Summary:
    This diff introduces ReactSurfaceInterface (as an empty interface).
    The goal is to demonstrate that we will be extending interfaces iteratively, meaning we will complete this API as we advance on the Stable API of surface in H2
    
    bypass-github-export-checks
    
    changelog: [internal] internal
    
    Reviewed By: cortinico
    
    Differential Revision: D46770420
    
    fbshipit-source-id: 48b1ee302c157ae3431d61c84322d788d149f881

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/interfaces/ReactSurfaceInterface.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.interfaces
+
+/** Represents a Surface in React Native. */
+interface ReactSurfaceInterface {
+  // the API of this interface will be completed as we analyze and refactor API of ReactSurface,
+  // ReactRootView, etc.
+}
