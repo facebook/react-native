0f09703a29359209c47579efbff79c2992d0eb89
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-06-21 14:51:04 +0200

    inverted prop in ScrollViewProps.cpp

--- packages/react-native/ReactCommon/react/renderer/components/scrollview/ScrollViewProps.cpp
+++ packages/react-native/ReactCommon/react/renderer/components/scrollview/ScrollViewProps.cpp
@@ -319,6 +319,15 @@ ScrollViewProps::ScrollViewProps(
                     rawProps,
                     "scrollToOverflowEnabled",
                     sourceProps.scrollToOverflowEnabled,
+                    {})),
+      inverted(
+          CoreFeatures::enablePropIteratorSetter
+              ? sourceProps.inverted
+              : convertRawProp(
+                    context,
+                    rawProps,
+                    "inverted",
+                    sourceProps.inverted,
                     {})) {}
 
 void ScrollViewProps::setProp(
@@ -368,6 +377,7 @@ void ScrollViewProps::setProp(
     RAW_SET_PROP_SWITCH_CASE_BASIC(snapToEnd);
     RAW_SET_PROP_SWITCH_CASE_BASIC(contentInsetAdjustmentBehavior);
     RAW_SET_PROP_SWITCH_CASE_BASIC(scrollToOverflowEnabled);
+    RAW_SET_PROP_SWITCH_CASE_BASIC(inverted);
   }
 }
 
@@ -492,7 +502,9 @@ SharedDebugStringConvertibleList ScrollViewProps::getDebugProps() const {
           debugStringConvertibleItem(
               "snapToStart", snapToStart, defaultScrollViewProps.snapToStart),
           debugStringConvertibleItem(
-              "snapToEnd", snapToEnd, defaultScrollViewProps.snapToEnd)};
+              "snapToEnd", snapToEnd, defaultScrollViewProps.snapToEnd),
+          debugStringConvertibleItem(
+              "inverted", inverted, defaultScrollViewProps.inverted)};
 }
 #endif
 
