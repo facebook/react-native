890565000e1c38c7a503be01e6c11d006b89a32d
David Vacca <dvacca@meta.com>
2023-06-25 11:05:09 -0700

    Fix RNTester build in CI (#38057)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38057
    
    Fix RNTester build in CI
    
    changelog: [internal] internal
    
    Reviewed By: fkgozali
    
    Differential Revision: D47006806
    
    fbshipit-source-id: 762d2ef82af368acbfb4012879c3851f844c84ec

--- packages/rn-tester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterApplication.java
+++ packages/rn-tester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterApplication.java
@@ -154,7 +154,7 @@ public class RNTesterApplication extends Application implements ReactApplication
 
   @Override
   @UnstableReactNativeAPI
-  public ReactHostInterface getReactHostInterface() {
+  public ReactHostInterface reactHostInterface() {
     if (mReactHost == null) {
       // Create an instance of ReactHost to manager the instance of ReactInstance,
       // which is similar to how we use ReactNativeHost to manager instance of ReactInstanceManager
