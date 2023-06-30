d8f0ffcc79f710789a3759135d3af2da8de78f33
Jatin Nagar <jitu19nagar@gmail.com>
2023-07-01 02:31:47 +0530

    removed `m` prefix from class variable names

--- packages/react-native/ReactAndroid/src/test/java/com/facebook/react/fabric/interop/InteropEventEmitterTest.kt
+++ packages/react-native/ReactAndroid/src/test/java/com/facebook/react/fabric/interop/InteropEventEmitterTest.kt
@@ -20,39 +20,39 @@ import org.robolectric.RuntimeEnvironment
 
 @RunWith(RobolectricTestRunner::class)
 class InteropEventEmitterTest {
-  lateinit var mReactContext: ReactContext
-  lateinit var mEventDispatcher: FakeEventDispatcher
+  lateinit var reactContext: ReactContext
+  lateinit var eventDispatcher: FakeEventDispatcher
 
   @Before
   fun setup() {
-    mReactContext = ReactApplicationContext(RuntimeEnvironment.application)
-    mEventDispatcher = FakeEventDispatcher()
+    reactContext = ReactApplicationContext(RuntimeEnvironment.application)
+    eventDispatcher = FakeEventDispatcher()
   }
 
   @Test
   fun receiveEvent_dispatchesCorrectly() {
-    val eventEmitter = InteropEventEmitter(mReactContext)
-    eventEmitter.overrideEventDispatcher(mEventDispatcher)
+    val eventEmitter = InteropEventEmitter(reactContext)
+    eventEmitter.overrideEventDispatcher(eventDispatcher)
 
     eventEmitter.receiveEvent(42, "onTest", null)
 
-    assertEquals(1, mEventDispatcher.getRecordedDispatchedEvents().size)
-    assertEquals("onTest", mEventDispatcher.getRecordedDispatchedEvents().get(0).getEventName())
+    assertEquals(1, eventDispatcher.getRecordedDispatchedEvents().size)
+    assertEquals("onTest", eventDispatcher.getRecordedDispatchedEvents().get(0).getEventName())
     assertEquals(
       InteropEvent::class,
-      mEventDispatcher.getRecordedDispatchedEvents().get(0)::class
+      eventDispatcher.getRecordedDispatchedEvents().get(0)::class
     )
   }
 
   @Test
   fun receiveEvent_dataIsPreserved() {
-    val eventEmitter = InteropEventEmitter(mReactContext)
-    eventEmitter.overrideEventDispatcher(mEventDispatcher)
+    val eventEmitter = InteropEventEmitter(reactContext)
+    eventEmitter.overrideEventDispatcher(eventDispatcher)
     val eventData = JavaOnlyMap.of("color", "indigo")
 
     eventEmitter.receiveEvent(42, "onTest", eventData)
 
-    val event = mEventDispatcher.getRecordedDispatchedEvents().get(0) as InteropEvent
+    val event = eventDispatcher.getRecordedDispatchedEvents().get(0) as InteropEvent
     val dispatchedEventData = event.getEventData()
     assertNotNull(dispatchedEventData)
     assertEquals("indigo", dispatchedEventData!!.getString("color"))
@@ -60,7 +60,7 @@ class InteropEventEmitterTest {
 
   @Test(expected = java.lang.UnsupportedOperationException::class)
   fun receiveTouches_isNotSupported() {
-    val eventEmitter = InteropEventEmitter(mReactContext)
+    val eventEmitter = InteropEventEmitter(reactContext)
     eventEmitter.receiveTouches("a touch", null, null)
   }
 }
