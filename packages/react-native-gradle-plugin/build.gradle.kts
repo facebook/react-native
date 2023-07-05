727a2e10baf482b5bbf5ec764e9cc82a96e9760b
Nicola Corti <ncor@meta.com>
2023-07-05 18:39:22 +0200

    Update build.gradle.kts

--- packages/react-native-gradle-plugin/build.gradle.kts
+++ packages/react-native-gradle-plugin/build.gradle.kts
@@ -66,7 +66,7 @@ kotlin { jvmToolchain(17) }
 
 tasks.withType<KotlinCompile> {
   kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_1_8.majorVersion
+    jvmTarget = "1.8"
     apiVersion = "1.5"
     languageVersion = "1.5"
   }
