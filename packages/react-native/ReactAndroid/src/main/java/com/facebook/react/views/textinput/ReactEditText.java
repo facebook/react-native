762c7b3f35a5c313c3ee186e2c84c7945f967202
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-07-06 15:35:50 +0800

    update comments - keep only one link

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
@@ -190,11 +190,10 @@ public class ReactEditText extends AppCompatEditText
           public boolean onCreateActionMode(ActionMode mode, Menu menu) {
             // The method populateMenuWithItems adds to the context menu the cut, copy, paste,
             // share, autofill, and paste as plain text items.
-            // https://tinyurl.com/z9axts8t
+            // https://tinyurl.com/bdefmrv9
             menu.removeItem(android.R.id.pasteAsPlainText);
             // If set to false, the context menu does not show, and the cursor position resets to
             // the position of getSelectionEnd().
-            // https://tinyurl.com/4frwvreh
             return true;
           }
 
@@ -202,7 +201,6 @@ public class ReactEditText extends AppCompatEditText
           public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
             // Some menu items are displayed based on the current selection. For example, If all
             // text is selected, the menu item select all does not show.
-            // https://tinyurl.com/mtt7sptp
             menu.removeItem(android.R.id.pasteAsPlainText);
             return true;
           }
