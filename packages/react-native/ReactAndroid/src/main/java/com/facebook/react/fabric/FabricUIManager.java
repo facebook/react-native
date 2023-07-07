9cf0ea96b7a9a343e828662f0d5ba7b27303015c
Pieter De Baets <pieterdb@meta.com>
2023-07-07 07:21:59 -0700

    Guard against stopped surface in FabricUIManager.getColor (#38239)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38239
    
    In D46840717 I made SurfaceMountingManager reset its context on stop as the Context may be keeping various other objects alive and was causing memory leaks.
    
    This does cause a race condition if JS is still rendering at this time, as it needs to access to context to support things like PlatformColor.
    
    Changelog: [Android] Fix crash on teardown in new architecture
    
    Reviewed By: rshest
    
    Differential Revision: D47290581
    
    fbshipit-source-id: 1014810b13751b902af6dfa99538ad06b43baecf

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
@@ -505,6 +505,11 @@ public class FabricUIManager implements UIManager, LifecycleEventListener {
   public int getColor(int surfaceId, String[] resourcePaths) {
     ThemedReactContext context =
         mMountingManager.getSurfaceManagerEnforced(surfaceId, "getColor").getContext();
+    // Surface may have been stopped
+    if (context == null) {
+      return 0;
+    }
+
     for (String resourcePath : resourcePaths) {
       Integer color = ColorPropConverter.resolveResourcePath(context, resourcePath);
       if (color != null) {
@@ -598,16 +603,14 @@ public class FabricUIManager implements UIManager, LifecycleEventListener {
    * @return if theme data is available in the output parameters.
    */
   public boolean getThemeData(int surfaceId, float[] defaultTextInputPadding) {
-    SurfaceMountingManager surfaceMountingManager =
-        mMountingManager.getSurfaceManagerEnforced(surfaceId, "getThemeData");
-    ThemedReactContext themedReactContext = surfaceMountingManager.getContext();
-
-    if (themedReactContext == null) {
+    Context context =
+        mMountingManager.getSurfaceManagerEnforced(surfaceId, "getThemeData").getContext();
+    if (context == null) {
       FLog.w(TAG, "\"themedReactContext\" is null when call \"getThemeData\"");
       return false;
     }
-    float[] defaultTextInputPaddingForTheme =
-        UIManagerHelper.getDefaultTextInputPadding(themedReactContext);
+
+    float[] defaultTextInputPaddingForTheme = UIManagerHelper.getDefaultTextInputPadding(context);
     defaultTextInputPadding[0] = defaultTextInputPaddingForTheme[PADDING_START_INDEX];
     defaultTextInputPadding[1] = defaultTextInputPaddingForTheme[PADDING_END_INDEX];
     defaultTextInputPadding[2] = defaultTextInputPaddingForTheme[PADDING_TOP_INDEX];
@@ -869,12 +872,12 @@ public class FabricUIManager implements UIManager, LifecycleEventListener {
       return;
     }
 
-    ThemedReactContext reactContext = surfaceMountingManager.getContext();
+    Context context = surfaceMountingManager.getContext();
     boolean isRTL = false;
     boolean doLeftAndRightSwapInRTL = false;
-    if (reactContext != null) {
-      isRTL = I18nUtil.getInstance().isRTL(reactContext);
-      doLeftAndRightSwapInRTL = I18nUtil.getInstance().doLeftAndRightSwapInRTL(reactContext);
+    if (context != null) {
+      isRTL = I18nUtil.getInstance().isRTL(context);
+      doLeftAndRightSwapInRTL = I18nUtil.getInstance().doLeftAndRightSwapInRTL(context);
     }
 
     mBinding.setConstraints(
