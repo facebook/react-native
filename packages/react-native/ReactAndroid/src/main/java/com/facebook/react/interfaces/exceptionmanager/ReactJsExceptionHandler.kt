85602603d39f82ec944190478c840268c0863733
David Vacca <515103+mdvacca@users.noreply.github.com>
2023-07-08 14:38:54 -0700

    Migrate ReactJsExceptionHandler to Kotlin (#38249)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38249
    
    Quick migration of ReactJsExceptionHandler to Kotlin.
    
    It will documented in the next diffs of the stack (might rename it as well)
    
    changelog: [internal] internal
    
    Reviewed By: arushikesarwani94
    
    Differential Revision: D47300786
    
    fbshipit-source-id: 602871ee2ca3c0ff9251e0b821dd0a3e3f7f3638

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/interfaces/exceptionmanager/ReactJsExceptionHandler.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.interfaces.exceptionmanager
+
+import com.facebook.proguard.annotations.DoNotStripAny
+import com.facebook.react.common.mapbuffer.ReadableMapBuffer
+
+@DoNotStripAny
+fun interface ReactJsExceptionHandler {
+  fun reportJsException(errorMap: ReadableMapBuffer?)
+}
