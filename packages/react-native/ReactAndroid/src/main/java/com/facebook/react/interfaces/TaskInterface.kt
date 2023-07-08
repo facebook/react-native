a8419ae01dfddd2c9d9db39ca7e67a4701096d3e
David Vacca <dvacca@fb.com>
2023-07-08 14:36:51 -0700

    Complete TaskInterface interface
    Summary:
    In this diff I'm completing the TaskInterface interface with subset methods from Task that should be exposed
    
    changelog: [internal] internal
    
    Differential Revision: D47267970
    
    fbshipit-source-id: 8605fa400409771581bb03ca3a1920c24d22813e

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/interfaces/TaskInterface.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/interfaces/TaskInterface.kt
@@ -7,7 +7,38 @@
 
 package com.facebook.react.interfaces
 
+import java.util.concurrent.TimeUnit
+
 /**
  * This is the public interface for Task which represents the result of an asynchronous computation.
  */
-interface TaskInterface {}
+interface TaskInterface<TResult> {
+
+  /** Blocks until the task is complete. */
+  @Throws(InterruptedException::class) fun waitForCompletion()
+
+  /**
+   * Blocks until the task is complete or times out.
+   *
+   * @return true if the task completed (has a result, an error, or was cancelled). false otherwise.
+   */
+  @Throws(InterruptedException::class)
+  fun waitForCompletion(duration: Long, timeUnit: TimeUnit?): Boolean
+
+  /** @return The result of the task, if set. null otherwise. */
+  fun getResult(): TResult?
+
+  /** @return The error for the task, if set. null otherwise. */
+  fun getError(): Exception?
+
+  /**
+   * @return true if the task completed (has a result, an error, or was cancelled. false otherwise.
+   */
+  fun isCompleted(): Boolean
+
+  /** @return true if the task was cancelled, false otherwise. */
+  fun isCancelled(): Boolean
+
+  /** @return true if the task has an error, false otherwise. */
+  fun isFaulted(): Boolean
+}
