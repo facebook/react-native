88bd3b50985a73121892316541aa2ee7d5f31c4b
dmitry rykun <dmitryrykun@fb.com>
2023-07-10 08:09:08 -0700

    Move react-native-babel-transformer and react-native-babel-preset from Metro to React Native repo
    Summary:
    Changelog: [General][Changed] - Move react-native-babel-transformer and react-native-babel-preset from Metro to React Native repo.
    
    bypass-github-export-checks
    
    This diff does the following:
    - Move `metro/packages/metro-react-native-babel-preset` to `react-native/packages/react-native-babel-preset`.
    - Move `metro/packages/metro-react-native-babel-transformer` to `react-native/packages/react-native-babel-transformer`.
    
    Differential Revision: https://internalfb.com/D46977466
    
    fbshipit-source-id: 8107d50a4b9d6ef43fd6ea9315fb836adc08db64

---
+++ packages/react-native-babel-preset/src/configs/hmr.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @oncall react_native
+ */
+
+'use strict';
+
+module.exports = function () {
+  return {
+    plugins: [require('react-refresh/babel')],
+  };
+};
