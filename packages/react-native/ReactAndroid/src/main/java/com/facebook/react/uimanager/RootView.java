67e33ebd01fff822e3d8f9f79b739d7a66d50524
Pieter De Baets <pieterdb@meta.com>
2023-07-18 08:30:41 -0700

    Align usage of JSPointerDispatcher across ReactSurfaceView and ReactRootView (#38350)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38350
    
    Found subtle differences between how JSPointerDispatcher is configured across bridgeless.
    
    Changelog: [Internal]
    
    Reviewed By: rshest
    
    Differential Revision: D47468666
    
    fbshipit-source-id: 730cd93ea426e50eb67a71feacc8d3eb477c4c96

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/RootView.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/RootView.java
@@ -21,7 +21,9 @@ public interface RootView {
 
   /** @deprecated */
   @Deprecated
-  void onChildStartedNativeGesture(MotionEvent ev);
+  default void onChildStartedNativeGesture(MotionEvent ev) {
+    onChildStartedNativeGesture(null, ev);
+  }
 
   /**
    * Called when a child ends a native gesture. Should be called from the child's onTouchIntercepted
