225e63518ecd3f712d190d109e4a48ecdc90e5dc
David Vacca <515103+mdvacca@users.noreply.github.com>
2023-07-24 23:13:17 -0700

    Migrate TurboModule to kotlin (#38503)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38503
    
    In this diff I'm migrating TurboModule to kotlin
    
    changelog: [internal] internal
    
    Reviewed By: christophpurrer, cortinico
    
    Differential Revision: D47382875
    
    fbshipit-source-id: 399fa4f40007a383717d524ca2d2c3107a0134cb

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/interfaces/TurboModule.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/interfaces/TurboModule.kt
@@ -5,13 +5,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.turbomodule.core.interfaces;
-
+package com.facebook.react.turbomodule.core.interfaces
 /** All turbo modules should inherit from this interface */
-public interface TurboModule {
+interface TurboModule {
   /** Initialize the TurboModule. */
-  void initialize();
+  fun initialize()
 
   /** Called before React Native is torn down. Clean up after the TurboModule. */
-  void invalidate();
+  fun invalidate()
 }
