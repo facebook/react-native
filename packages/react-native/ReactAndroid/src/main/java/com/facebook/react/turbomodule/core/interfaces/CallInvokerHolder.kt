1419b277b7e6c6a3ec1e595f12612e173c906f65
David Vacca <515103+mdvacca@users.noreply.github.com>
2023-07-24 23:15:24 -0700

    Migrate CallInvokerHolder to kotlin (#38504)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38504
    
    In this diff I'm migrating CallInvokerHolder to kotlin
    
    changelog: [internal] internal
    
    Reviewed By: christophpurrer
    
    Differential Revision: D47382873
    
    fbshipit-source-id: 239105db7be0009ad5a1fc33040c1eef9fda315a

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/interfaces/CallInvokerHolder.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/interfaces/CallInvokerHolder.kt
@@ -5,14 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.turbomodule.core.interfaces;
-
+package com.facebook.react.turbomodule.core.interfaces
 /**
  * JS CallInvoker is created by CatalystInstance.cpp, but used by TurboModuleManager.cpp. Both C++
  * classes are instantiated at different times/places. Therefore, to pass the JS CallInvoker
  * instance from CatalystInstance to TurboModuleManager, we make it take a trip through Java.
  *
- * <p>This interface represents the opaque Java object that contains a pointer to and instance of
+ * This interface represents the opaque Java object that contains a pointer to and instance of
  * CallInvoker.
  */
-public interface CallInvokerHolder {}
+interface CallInvokerHolder
