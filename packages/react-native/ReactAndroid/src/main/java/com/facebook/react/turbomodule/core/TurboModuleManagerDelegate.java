1430ec788826178cc6f4632ae762f527d7b8aafd
David Vacca <dvacca@fb.com>
2023-07-29 08:07:17 -0700

    Extract loading of turbomodulejsijni into its own function
    Summary:
    In this diff I'm extracting the loading of turbomodulejsijni into its own function. This could have perf implications as we are trying to load multimple times the same soLoader
    
    changelog: [intenral] internal
    
    Differential Revision: D47409162
    
    fbshipit-source-id: 5c19062723f69f8d73d66d73e47b55712c491075

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/TurboModuleManagerDelegate.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/TurboModuleManagerDelegate.java
@@ -12,7 +12,6 @@ import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.turbomodule.core.interfaces.TurboModule;
-import com.facebook.soloader.SoLoader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,13 +20,14 @@ public abstract class TurboModuleManagerDelegate {
   @SuppressWarnings("unused")
   private final HybridData mHybridData;
 
-  private static volatile boolean sIsSoLibraryLoaded;
+  static {
+    NativeModuleSoLoader.maybeLoadSoLibrary();
+  }
 
   protected abstract HybridData initHybrid();
 
   protected TurboModuleManagerDelegate() {
     maybeLoadOtherSoLibraries();
-    maybeLoadSoLibrary();
     mHybridData = initHybrid();
   }
 
@@ -55,14 +55,6 @@ public abstract class TurboModuleManagerDelegate {
 
   public List<String> getEagerInitModuleNames() {
     return new ArrayList<>();
-  }
-
-  // Prevents issues with initializer interruptions. See T38996825 and D13793825 for more context.
-  private static synchronized void maybeLoadSoLibrary() {
-    if (!sIsSoLibraryLoaded) {
-      SoLoader.loadLibrary("turbomodulejsijni");
-      sIsSoLibraryLoaded = true;
-    }
   }
 
   protected synchronized void maybeLoadOtherSoLibraries() {}
