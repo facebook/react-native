e9246852dffb5e04eab617d2db9103964291208d
David Vacca <dvacca@meta.com>
2023-07-29 11:20:24 -0700

    Extract loading of turbomodulejsijni into its own function (#38320)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38320
    
    In this diff I'm extracting the loading of turbomodulejsijni into its own function. This could have perf implications as we are trying to load multimple times the same soLoader
    
    changelog: [intenral] internal
    
    Reviewed By: luluwu2032
    
    Differential Revision: D47409162
    
    fbshipit-source-id: 007e89e99ec0e7fa494d811f9c830b1363acec19

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/CompositeReactPackageTurboModuleManagerDelegate.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/CompositeReactPackageTurboModuleManagerDelegate.java
@@ -12,7 +12,6 @@ import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.turbomodule.core.TurboModuleManagerDelegate;
-import com.facebook.soloader.SoLoader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -20,8 +19,6 @@ import java.util.List;
 @DoNotStrip
 public class CompositeReactPackageTurboModuleManagerDelegate
     extends ReactPackageTurboModuleManagerDelegate {
-
-  private static volatile boolean sIsSoLibraryLoaded;
 
   protected native HybridData initHybrid();
 
@@ -51,14 +48,6 @@ public class CompositeReactPackageTurboModuleManagerDelegate
         delegates.add(delegatesBuilder.build(context, Collections.<ReactPackage>emptyList()));
       }
       return new CompositeReactPackageTurboModuleManagerDelegate(context, packages, delegates);
-    }
-  }
-
-  protected synchronized void maybeLoadOtherSoLibraries() {
-    // Prevents issues with initializer interruptions. See T38996825 and D13793825 for more context.
-    if (!sIsSoLibraryLoaded) {
-      SoLoader.loadLibrary("turbomodulejsijni");
-      sIsSoLibraryLoaded = true;
     }
   }
 }
