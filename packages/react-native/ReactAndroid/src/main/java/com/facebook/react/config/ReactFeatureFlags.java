a79cde689feee8d9e498c7d044d2af4b4b063fc9
Phillip Pan <phillippan@meta.com>
2023-08-01 06:36:26 -0700

    delete unused feature flag (#38708)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38708
    
    Changelog: [Internal]
    
    as title
    
    Reviewed By: fkgozali
    
    Differential Revision: D47924734
    
    fbshipit-source-id: e55a9c6ce80f9e1f843cc9bbb9a555b76e4e4548

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/config/ReactFeatureFlags.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/config/ReactFeatureFlags.java
@@ -81,9 +81,6 @@ public class ReactFeatureFlags {
    */
   public static volatile boolean warnOnLegacyNativeModuleSystemUse = false;
 
-  /** Should we dispatch TurboModule methods with promise returns to the NativeModules thread? */
-  public static volatile boolean enableTurboModulePromiseAsyncDispatch = false;
-
   /** This feature flag enables logs for Fabric */
   public static boolean enableFabricLogs = false;
 
