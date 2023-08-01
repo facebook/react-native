5089b928db90925da5e9e3cf63166adb6b4c7a3f
szymonrybczak <szymon.rybczak@gmail.com>
2023-08-01 11:28:12 +0200

    Add missing platform check.

--- packages/rn-tester-e2e/scripts/run-e2e-tests.sh
+++ packages/rn-tester-e2e/scripts/run-e2e-tests.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Check if an argument is provided
-if [ $# -eq 0 ]; then
-  echo "Error: No platform provided."
+# Supported platforms
+SUPPORTED_PLATFORMS=("ios" "android")
+
+# Check if an argument is provided and if it's a supported platform
+if [ $# -ne 1 ] || ! [[ " ${SUPPORTED_PLATFORMS[*]} " == *" $1 "* ]]; then
+  echo "Error: Invalid platform. Supported platforms are: ${SUPPORTED_PLATFORMS[*]}"
   exit 1
 fi
 
