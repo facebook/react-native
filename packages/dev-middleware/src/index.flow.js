d9f3a0e01d37157421de3803dde7c6819d7453cf
Alex Hunt <huntie@meta.com>
2023-08-03 02:41:52 -0700

    Add shared monorepo build setup (#38718)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38718
    
    > NOTE: Replaces https://github.com/facebook/react-native/pull/38240
    
    ## Context
    
    RFC: Decoupling Flipper from React Native core: https://github.com/react-native-community/discussions-and-proposals/pull/641
    
    ## Changes
    
    To support incoming new React Native packages around debugging (including migrating over [`react-native-community/cli-plugin-metro`](https://github.com/react-native-community/cli/tree/main/packages/cli-plugin-metro)) — which target Node.js and require a build step, this PR adds a minimal shared build setup across the `react-native` monorepo.
    
    The setup is closely inspired/based on the build scripts in Jest, Metro, and React Native CLI — and is a simple set of script wrappers around Babel. These are available as build commands at the root of the repo:
    
    - `yarn build` — Builds all configured packages. Functionally, this:
      - Outputs a `dist/` directory with built files.
      - Rewrites package.json `"exports"` to update every `./src/*` reference to `./dist/*` (source of truth).
    - `scripts/build/babel-register.js` — Allows running all Node.js entry points from source, similar to the current setup in [facebook/metro](https://github.com/facebook/metro). (Example entry point file in this PR: `packages/dev-middleware/src/index.js`)
    
    Build configuration (i.e. Babel config) is shared as a set standard across the monorepo, and **packages are opted-in to requiring a build**, configured in `scripts/build.config.js`.
    
    ```
    const buildConfig /*: BuildConfig */ = {
      // The packages to include for build and their build options
      packages: {
        'dev-middleware': {target: 'node'},
      },
    };
    ```
    
    For now, there is a single `target: 'node'` option — this is necessary as `react-native`, unlike the above other projects, is a repository with packages targeting several runtimes. We may, in future, introduce a build step for other, non-Node, packages — which may be useful for things such as auto-generated TypeScript definitions.
    
     {F1043312771}
    
    **Differences from the Metro setup**
    
    - References (and compiles out) repo-local `scripts/build/babel-register.js` — removing need for an npm-published dependency.
    
    ## Current integration points
    
    - **CircleCI** — `yarn build` is added to the `build_npm_package` and `find_and_publish_bumped_packages` jobs.
    
    **New Node.js package(s) are not load bearing quite yet**: There are not yet any built packages added to the dependencies of `packages/react-native/`, so this will be further tested in a later PR (and is actively being done in an internal commit stack).
    
    ### Alternative designs
    
    **Per-package config file**
    
    Replace `scripts/build/config.js` with a package-defined key in in `package.json`, similar to Jest's [`publishConfig`](https://github.com/jestjs/jest/blob/1f019afdcdfc54a6664908bb45f343db4e3d0848/packages/jest-cli/package.json#L87C3-L89C4).
    
    ```
    "buildConfig": {
      "type": "node"
    },
    ```
    
    This would be the only customisation required, with a single Babel config still standardised. Another option this might receive in future is `enableTypeScriptCodgeen`.
    
    **Rollup**
    
    More sophisticated build tool for Node.js, used by the React codebase (albeit within a custom script setup as well).
    
    **Lerna and Nx**
    
    - Most sophisticated setup enabling caching and optimised cloud runs.
    - Probably the most likely thing we'll move towards at a later stage.
    
    Changelog: [Internal]
    
    Reviewed By: NickGerleman
    
    Differential Revision: D47760330
    
    fbshipit-source-id: 456b506f9e50a43bae9bc902f36910253a0681e0

--- packages/dev-middleware/src/index.flow.js
+++ packages/dev-middleware/src/index.flow.js
@@ -4,9 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
+ * @flow strict
  * @format
  * @oncall react_native
  */
 
-export * from './src';
+export {default as createDevMiddleware} from './createDevMiddleware';
