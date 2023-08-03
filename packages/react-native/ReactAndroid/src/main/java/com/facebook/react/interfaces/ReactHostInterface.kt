a34ce64bbb322dbb0c45f42948b4e5cd08bebad2
Arushi Kesarwani <arushikesarwani@meta.com>
2023-08-03 16:29:02 -0700

    Fix lint warns in ReactHostInterface (#38762)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38762
    
    Fix lint warns in ReactHostInterface
    
    Reviewed By: cortinico
    
    Differential Revision: D48018980
    
    fbshipit-source-id: 51fe510ce4d7f903caae2e2fc995fec872df24ea

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/interfaces/ReactHostInterface.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/interfaces/ReactHostInterface.kt
@@ -85,8 +85,8 @@ interface ReactHostInterface {
    * Entrypoint to reload the ReactInstance. If the ReactInstance is destroying, will wait until
    * destroy is finished, before reloading.
    *
-   * @param reason [String] describing why ReactHost is being reloaded (e.g. js error, user tap on
-   *   reload button)
+   * @param reason describing why ReactHost is being reloaded (e.g. js error, user tap on reload
+   *   button)
    * @return A task that completes when React Native reloads
    */
   fun reload(reason: String): TaskInterface<Void>
@@ -95,9 +95,9 @@ interface ReactHostInterface {
    * Entrypoint to destroy the ReactInstance. If the ReactInstance is reloading, will wait until
    * reload is finished, before destroying.
    *
-   * @param reason [String] describing why ReactHost is being destroyed (e.g. memmory pressure)
-   * @param ex [Exception] exception that caused the trigger to destroy ReactHost (or null) This
-   *   exception will be used to log properly the cause of destroy operation.
+   * @param reason describing why ReactHost is being destroyed (e.g. memmory pressure)
+   * @param ex exception that caused the trigger to destroy ReactHost (or null) This exception will
+   *   be used to log properly the cause of destroy operation.
    * @return A task that completes when React Native gets destroyed.
    */
   fun destroy(reason: String, ex: Exception?): TaskInterface<Void>
