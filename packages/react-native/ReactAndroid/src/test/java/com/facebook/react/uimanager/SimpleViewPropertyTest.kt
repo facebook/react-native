21147121a928a7edd3b9ff846020782f5ac03385
Madhusudan Sapkota <sapkotamadhusudan99@gmail.com>
2023-08-09 00:17:08 +0545

    SimpleViewPropertyTest.java => SimpleViewPropertyTest.kt

---
+++ packages/react-native/ReactAndroid/src/test/java/com/facebook/react/uimanager/SimpleViewPropertyTest.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.uimanager
+
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import com.facebook.react.bridge.CatalystInstance
+import com.facebook.react.bridge.JavaOnlyMap
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactTestHelper.createMockCatalystInstance
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.touch.JSResponderHandler
+import com.facebook.react.uimanager.annotations.ReactProp
+import org.assertj.core.api.Assertions
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.core.classloader.annotations.PowerMockIgnore
+import org.powermock.modules.junit4.rule.PowerMockRule
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+/** Verify [View] view property being applied properly by [SimpleViewManager] */
+@RunWith(RobolectricTestRunner::class)
+@PowerMockIgnore("org.mockito.*", "org.robolectric.*", "androidx.*", "android.*")
+class SimpleViewPropertyTest {
+
+  @get:Rule var rule = PowerMockRule()
+
+  private class ConcreteViewManager : SimpleViewManager<View?>() {
+    @ReactProp(name = "foo") fun setFoo(view: View, foo: Boolean) {}
+
+    @ReactProp(name = "bar") fun setBar(view: View, bar: ReadableMap?) {}
+
+    override fun createViewInstance(reactContext: ThemedReactContext): View {
+      return View(reactContext)
+    }
+
+    override fun getName(): String {
+      return "View"
+    }
+  }
+
+  private lateinit var mContext: ReactApplicationContext
+  private lateinit var mCatalystInstanceMock: CatalystInstance
+  private lateinit var mThemedContext: ThemedReactContext
+  private lateinit var mManager: ConcreteViewManager
+
+  @Before
+  fun setup() {
+    mContext = ReactApplicationContext(RuntimeEnvironment.getApplication())
+    mCatalystInstanceMock = createMockCatalystInstance()
+    mContext.initializeWithInstance(mCatalystInstanceMock)
+    mThemedContext = ThemedReactContext(mContext, mContext)
+    mManager = ConcreteViewManager()
+  }
+
+  fun buildStyles(vararg keysAndValues: Any?): ReactStylesDiffMap {
+    return ReactStylesDiffMap(JavaOnlyMap.of(*keysAndValues))
+  }
+
+  @Test
+  fun testOpacity() {
+    val view =
+      mManager.createView(sViewTag, mThemedContext, buildStyles(), null, JSResponderHandler())
+    mManager.updateProperties(view, buildStyles())
+    Assertions.assertThat(view.alpha).isEqualTo(1.0f)
+    mManager.updateProperties(view, buildStyles("opacity", 0.31))
+    Assertions.assertThat(view.alpha).isEqualTo(0.31f, Assertions.offset(1e-5f))
+    mManager.updateProperties(view, buildStyles("opacity", null))
+    Assertions.assertThat(view.alpha).isEqualTo(1.0f)
+  }
+
+  @Test
+  fun testBackgroundColor() {
+    val view =
+      mManager.createView(sViewTag, mThemedContext, buildStyles(), null, JSResponderHandler())
+    mManager.updateProperties(view, buildStyles())
+    Assertions.assertThat(view.background).isEqualTo(null)
+    mManager.updateProperties(view, buildStyles("backgroundColor", 12))
+    Assertions.assertThat((view.background as ColorDrawable).color).isEqualTo(12)
+    mManager.updateProperties(view, buildStyles("backgroundColor", null))
+    Assertions.assertThat((view.background as ColorDrawable).color).isEqualTo(0)
+  }
+
+  @Test
+  fun testGetNativeProps() {
+    val nativeProps = mManager.nativeProps
+    Assertions.assertThat(nativeProps["foo"]).isEqualTo("boolean")
+    Assertions.assertThat(nativeProps["bar"]).isEqualTo("Map")
+  }
+
+  companion object {
+    private const val sViewTag = 2
+  }
+}
