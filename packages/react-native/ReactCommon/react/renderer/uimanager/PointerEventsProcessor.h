37fee666802dbd43dc948c19d0c3d894a13aeb73
Vincent Riemer <vincentriemer@meta.com>
2023-08-10 14:53:31 -0700

    Add initial Pointer Capture API implementation (#38505)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38505
    
    Changelog: [Internal] - Add initial Pointer Capture API implementation
    
    This diff introduces the first baseline implementation of pointer capturing through intercepting/modifying Pointer Events in flight through the `PointerEventsProcessor` class.
    
    Firstly: This adds and exposes the imperative methods `setPointerCapture`, `releasePointerCapture`, and `hasPointerCapture` which is added to the host `ReadOnlyElement` ref API. These methods are used to manage/query the `pendingPointerCaptureTargetOverrides_` map as [defined in the spec](https://www.w3.org/TR/pointerevents/#setting-pointer-capture).
    
    The code is fairly self-explainatory when it comes to retargeting the events in `PointerEventsProcessor::interceptPointerEvent` but when it comes to firing the `gotPointerCapture` and `lostPointerCapture` events those are handled in `PointerEventsProcessor::processPendingPointerCapture` and is a fairly direct implementation of [the spec's pseudocode](https://www.w3.org/TR/pointerevents/#process-pending-pointer-capture).
    
    Finally at the end of `interceptPointerEvent` I've included the basics of implicit pointer capture *release* as per [the spec](https://www.w3.org/TR/pointerevents/#implicit-release-of-pointer-capture) (note that implicit pointer capture is not yet implemented).
    
    Reviewed By: rozele
    
    Differential Revision: D47533366
    
    fbshipit-source-id: 1786f9703a88201bc9c7bde61af76eb4b07a20ee

--- packages/react-native/ReactCommon/react/renderer/uimanager/PointerEventsProcessor.h
+++ packages/react-native/ReactCommon/react/renderer/uimanager/PointerEventsProcessor.h
@@ -22,6 +22,16 @@ using DispatchEvent = std::function<void(
     ReactEventPriority priority,
     const EventPayload &payload)>;
 
+using PointerIdentifier = int32_t;
+using CaptureTargetOverrideRegistry =
+    std::unordered_map<PointerIdentifier, ShadowNode::Weak>;
+
+// Helper struct to package a PointerEvent and SharedEventTarget together
+struct PointerEventTarget {
+  PointerEvent event;
+  SharedEventTarget target;
+};
+
 class PointerEventsProcessor final {
  public:
   void interceptPointerEvent(
@@ -30,7 +40,28 @@ class PointerEventsProcessor final {
       std::string const &type,
       ReactEventPriority priority,
       PointerEvent const &event,
-      DispatchEvent const &eventDispatcher);
+      DispatchEvent const &eventDispatcher,
+      UIManager const &uiManager);
+
+  void setPointerCapture(
+      PointerIdentifier pointerId,
+      ShadowNode::Shared const &shadowNode);
+  void releasePointerCapture(
+      PointerIdentifier pointerId,
+      ShadowNode const *shadowNode);
+  bool hasPointerCapture(
+      PointerIdentifier pointerId,
+      ShadowNode const *shadowNode);
+
+ private:
+  void processPendingPointerCapture(
+      PointerEvent const &event,
+      jsi::Runtime &runtime,
+      DispatchEvent const &eventDispatcher,
+      UIManager const &uiManager);
+
+  CaptureTargetOverrideRegistry pendingPointerCaptureTargetOverrides_;
+  CaptureTargetOverrideRegistry activePointerCaptureTargetOverrides_;
 };
 
 } // namespace facebook::react
