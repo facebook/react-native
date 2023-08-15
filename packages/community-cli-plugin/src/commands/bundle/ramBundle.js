40f4ee8b12a7d4825c2f9a5d392b8b29d892ba8b
Alex Hunt <huntie@fb.com>
2023-08-15 02:30:03 -0700

    Convert cli-commands to Flow strict
    Summary: Changelog: [Internal]
    
    Differential Revision: https://internalfb.com/D48066180
    
    fbshipit-source-id: 305f440d7b3f1f764e3e68c1b017b0c357e99b38

--- packages/community-cli-plugin/src/commands/bundle/ramBundle.js
+++ packages/community-cli-plugin/src/commands/bundle/ramBundle.js
@@ -4,12 +4,12 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
+ * @flow strict-local
  * @format
  * @oncall react_native
  */
 
-import type {Config} from '@react-native-community/cli-types';
+import type {Command, Config} from '@react-native-community/cli-types';
 import type {CommandLineArgs} from './bundleCommandLineArgs';
 
 import outputUnbundle from 'metro/src/shared/output/RamBundle';
@@ -27,17 +27,20 @@ function ramBundle(
   return bundleWithOutput(argv, config, args, outputUnbundle);
 }
 
-export default {
+export default ({
   name: 'ram-bundle',
   description:
     'Build the RAM bundle for the provided JavaScript entry file. See https://reactnative.dev/docs/ram-bundles-inline-requires.',
   func: ramBundle,
-  options: (bundleCommandLineArgs.concat({
-    name: '--indexed-ram-bundle',
-    description:
-      'Force the "Indexed RAM" bundle file format, even when building for android',
-    default: false,
-  }): Array<Object>),
-};
+  options: [
+    ...bundleCommandLineArgs,
+    {
+      name: '--indexed-ram-bundle',
+      description:
+        'Force the "Indexed RAM" bundle file format, even when building for android',
+      default: false,
+    },
+  ],
+}: Command);
 
 export {ramBundle};
