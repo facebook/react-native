39a41fa2a0b3e9255663948223492ba16fe252e3
Alex Hunt <huntie@meta.com>
2023-08-15 02:33:22 -0700

    Convert community-cli-plugin to Flow strict (#38961)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38961
    
    Changelog: [Internal]
    
    Reviewed By: motiz88
    
    Differential Revision: D48066180
    
    fbshipit-source-id: 72b8b25a70062a71ab13aa1d62f3418582e99013

--- packages/community-cli-plugin/src/commands/start/runServer.js
+++ packages/community-cli-plugin/src/commands/start/runServer.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow
+ * @flow strict-local
  * @format
  * @oncall react_native
  */
@@ -132,7 +132,7 @@ async function runServer(_argv: Array<string>, ctx: Config, args: Args) {
     server: Server,
   ) => {
     if (customEnhanceMiddleware) {
-      metroMiddleware = customEnhanceMiddleware(metroMiddleware, server);
+      return middleware.use(customEnhanceMiddleware(metroMiddleware, server));
     }
     return middleware.use(metroMiddleware);
   };
