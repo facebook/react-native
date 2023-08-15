080c5d6e27e82ef591a87a15a37c23af843cc06b
Riccardo Cipolleschi <cipolleschi@meta.com>
2023-08-15 03:48:47 -0700

    Publish dSYM to Maven when doing a release (#38992)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38992
    
    This Diff publishes the Hermes dSYMs to Maven while doing a release.
    These were missing and so the Stack traces can't be fully symbolicated when a crash occurs.
    
    ## Changelog:
    [Internal] - Publish dSYM to Maven
    
    Reviewed By: cortinico
    
    Differential Revision: D48309198
    
    fbshipit-source-id: a5514e544587daadd0a0d7614f25a30fccd16a5b

--- packages/react-native/ReactAndroid/external-artifacts/build.gradle.kts
+++ packages/react-native/ReactAndroid/external-artifacts/build.gradle.kts
@@ -35,6 +35,24 @@ val hermesiOSReleaseArtifact: PublishArtifact =
       classifier = "hermes-ios-release"
     }
 
+// Those artifacts should be placed inside the `artifacts/hermes-*.framework.dSYM` location
+val hermesDSYMDebugArtifactFile: RegularFile =
+    layout.projectDirectory.file("artifacts/hermes-framework-dSYM-debug.tar.gz")
+val hermesDSYMDebugArtifact: PublishArtifact =
+    artifacts.add("default", hermesDSYMDebugArtifactFile) {
+      type = "tgz"
+      extension = "tar.gz"
+      classifier = "hermes-framework-dSYM-debug"
+    }
+val hermesDSYMReleaseArtifactFile: RegularFile =
+    layout.projectDirectory.file("artifacts/hermes-framework-dSYM-release.tar.gz")
+val hermesDSYMReleaseArtifact: PublishArtifact =
+    artifacts.add("default", hermesDSYMReleaseArtifactFile) {
+      type = "tgz"
+      extension = "tar.gz"
+      classifier = "hermes-framework-dSYM-release"
+    }
+
 apply(from = "../publish.gradle")
 
 publishing {
@@ -43,6 +61,8 @@ publishing {
       artifactId = "react-native-artifacts"
       artifact(hermesiOSDebugArtifact)
       artifact(hermesiOSReleaseArtifact)
+      artifact(hermesDSYMDebugArtifact)
+      artifact(hermesDSYMReleaseArtifact)
     }
   }
 }
