22ab0e66a51473f7accfdfdd6a3438b4983c6ce9
Phillip Pan <phillippan@meta.com>
2023-08-16 15:53:51 -0700

    delete unused RCTSurfaceHostingProxyRootView initializers (#38868)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/38868
    
    ## Changelog:
    
    [iOS][Breaking] - After [#38864](https://github.com/facebook/react-native/pull/38864), these are not needed anymore. there's some logic with the bridge, but since we're treating new arch as one piece moving forward, i don't really think they're that important. plus that logic is like really old.
    
    Reviewed By: mdvacca
    
    Differential Revision: D48140101
    
    fbshipit-source-id: b99b714c74af2c2ac9ddc50a18891e1eec55db8b

--- packages/react-native/React/Base/Surface/SurfaceHostingView/RCTSurfaceHostingProxyRootView.h
+++ packages/react-native/React/Base/Surface/SurfaceHostingView/RCTSurfaceHostingProxyRootView.h
@@ -42,15 +42,6 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, assign) NSTimeInterval loadingViewFadeDuration;
 @property (nonatomic, assign) CGSize minimumSize;
 
-- (instancetype)initWithBridge:(RCTBridge *)bridge
-                    moduleName:(NSString *)moduleName
-             initialProperties:(NSDictionary *)initialProperties NS_DESIGNATED_INITIALIZER;
-
-- (instancetype)initWithBundleURL:(NSURL *)bundleURL
-                       moduleName:(NSString *)moduleName
-                initialProperties:(NSDictionary *)initialProperties
-                    launchOptions:(NSDictionary *)launchOptions;
-
 /**
  * Bridgeless mode initializer
  */
