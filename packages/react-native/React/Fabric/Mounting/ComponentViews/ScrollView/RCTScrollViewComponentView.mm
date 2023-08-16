af36cc01f5aeb87945fa70f5ed331b659ea2764e
Nick Gerleman <ngerlem@meta.com>
2023-08-16 14:22:25 -0700

    Fix bad comparison in RCTScrollViewComponentView RTL (#39030)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/39030
    
    D29164056 fixed `scrollTo` coordinate space in RTL, but D38499666 regressed it by adding a comparison before the RTL conversion happens.
    
    This makes `scrollTo` no-op if we are scrolling from the beginning to the end of the list, since the end of the list is `x: 0` in cartesian coordinates, and the start of the list is `x: 0` in flow-relative coordinates.
    
    Do coordinate conversion before the early exit check.
    
    Changelog:
    [iOS][Fixed] - Fix bad comparison in RCTScrollViewComponentView RTL
    
    Reviewed By: rshest
    
    Differential Revision: D48378414
    
    fbshipit-source-id: 4ec9bde1a2f9910f35f4035db008bb8d2e2d6f89

--- packages/react-native/React/Fabric/Mounting/ComponentViews/ScrollView/RCTScrollViewComponentView.mm
+++ packages/react-native/React/Fabric/Mounting/ComponentViews/ScrollView/RCTScrollViewComponentView.mm
@@ -675,16 +675,16 @@ static void RCTSendScrollEventForNativeAnimations_DEPRECATED(UIScrollView *scrol
 
 - (void)scrollToOffset:(CGPoint)offset animated:(BOOL)animated
 {
+  if (_layoutMetrics.layoutDirection == LayoutDirection::RightToLeft) {
+    // Adjusting offset.x in right to left layout direction.
+    offset.x = self.contentSize.width - _scrollView.frame.size.width - offset.x;
+  }
+
   if (CGPointEqualToPoint(_scrollView.contentOffset, offset)) {
     return;
   }
 
   [self _forceDispatchNextScrollEvent];
-
-  if (_layoutMetrics.layoutDirection == LayoutDirection::RightToLeft) {
-    // Adjusting offset.x in right to left layout direction.
-    offset.x = self.contentSize.width - _scrollView.frame.size.width - offset.x;
-  }
 
   [_scrollView setContentOffset:offset animated:animated];
 
