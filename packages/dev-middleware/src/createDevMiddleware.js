33e3799767fd6b75ba615969c20934c61667c1f2
Alex Hunt <huntie@meta.com>
2023-08-17 14:58:50 -0700

    Move metro-inspector-proxy into dev-middleware (#39045)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/39045
    
    ## Context
    
    RFC: Decoupling Flipper from React Native core: https://github.com/react-native-community/discussions-and-proposals/pull/641
    
    ## Changes
    
    - Relocates `metro-inspector-proxy` source from the Metro repo into the React Native repo as part of the `react-native/dev-middleware` package.
        - Drops the `runInspectorProxy` entry point.
    - Attaches the Inspector Proxy to the `createDevMiddleware()` API as the new integration point for this functionality.
    - Documents migrated endpoints + usage of `createDevMiddleware()` in README.
    
    Changelog: [Internal]
    Metro changelog: None (`metro-inspector-proxy` is now an internal component of `react-native`, covered in the [release notes for 0.78.1](https://github.com/facebook/metro/releases/tag/v0.78.1))
    
    Reviewed By: motiz88, blakef
    
    Differential Revision: D48066213
    
    fbshipit-source-id: 8a4ae6b6533f1987cc479bf631ffa62ec7df1fed

--- packages/dev-middleware/src/createDevMiddleware.js
+++ packages/dev-middleware/src/createDevMiddleware.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict
+ * @flow strict-local
  * @format
  * @oncall react_native
  */
@@ -14,18 +14,36 @@ import type {Logger} from './types/Logger';
 
 import connect from 'connect';
 import openDebuggerMiddleware from './middleware/openDebuggerMiddleware';
+import InspectorProxy from './inspector-proxy/InspectorProxy';
 
 type Options = $ReadOnly<{
+  host: string,
+  port: number,
+  projectRoot: string,
   logger?: Logger,
 }>;
 
-export default function createDevMiddleware({logger}: Options = {}): {
+type DevMiddlewareAPI = $ReadOnly<{
   middleware: NextHandleFunction,
-} {
-  const middleware = connect().use(
-    '/open-debugger',
-    openDebuggerMiddleware({logger}),
-  );
+  websocketEndpoints: {[path: string]: ws$WebSocketServer},
+}>;
 
-  return {middleware};
+export default function createDevMiddleware({
+  host,
+  port,
+  projectRoot,
+  logger,
+}: Options): DevMiddlewareAPI {
+  const inspectorProxy = new InspectorProxy(projectRoot);
+
+  const middleware = connect()
+    .use('/open-debugger', openDebuggerMiddleware({logger}))
+    .use((...args) => inspectorProxy.processRequest(...args));
+
+  return {
+    middleware,
+    websocketEndpoints: inspectorProxy.createWebSocketListeners(
+      `${host}:${port}`,
+    ),
+  };
 }
