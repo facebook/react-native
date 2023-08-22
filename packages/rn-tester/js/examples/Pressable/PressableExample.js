821769ffc596fedb992262ef319a01020e981f16
Elżbieta Szulc <elzbieta.szulc@callstack.com>
2023-08-22 18:23:40 +0200

    Fix bot comment

--- packages/rn-tester/js/examples/Pressable/PressableExample.js
+++ packages/rn-tester/js/examples/Pressable/PressableExample.js
@@ -48,7 +48,9 @@ function ContentPress() {
             setTimesPressed(current => current + 1);
           }}>
           {({pressed}) => (
-            <Text testID="one_press_me_button" fistyle={styles.text}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
+            <Text testID="one_press_me_button" fistyle={styles.text}>
+              {pressed ? 'Pressed!' : 'Press Me'}
+            </Text>
           )}
         </Pressable>
       </View>
