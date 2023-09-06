89739780a6bb571345938a06dba09499327d95d3
IzuEneh <izueneh21@gmail.com>
2023-09-06 04:18:58 -0700

    Convert ReactPropForShadowNodeSpecTest to Kotlin (#39284)
    Summary:
    This PR converts ReactPropForShadowNodeSpecTest to Kotlin. https://github.com/facebook/react-native/issues/38825
    
    ## Changelog:
    
    [INTERNAL] [CHANGED] - Kotlinify ReactPropForShadowNodeSpecTest.java
    
    For more details, see:
    https://reactnative.dev/contributing/changelogs-in-pull-requests
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/39284
    
    Test Plan: `./gradlew :packages:react-native:ReactAndroid:test` must pass
    
    Reviewed By: javache
    
    Differential Revision: D48960815
    
    Pulled By: rshest
    
    fbshipit-source-id: d66ab64373e300c5fff6fa2013faac473890804c

---
+++ packages/react-native/ReactAndroid/src/test/java/com/facebook/react/uimanager/ReactPropForShadowNodeSpecTest.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.uimanager
+
+import android.view.View
+import com.facebook.react.uimanager.annotations.ReactProp
+import com.facebook.react.uimanager.annotations.ReactPropGroup
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Test that verifies that spec of methods annotated with @ReactProp in {@link ReactShadowNode} is
+ * correct
+ */
+@RunWith(RobolectricTestRunner::class)
+@Ignore
+class ReactPropForShadowNodeSpecTest {
+  @Test(expected = RuntimeException::class)
+  fun testMethodWithWrongNumberOfParams() {
+    BaseViewManager(
+            object : ReactShadowNodeImpl() {
+                  @ReactProp(name = "prop")
+                  fun setterWithIncorrectNumberOfArgs(value: Boolean, anotherValue: Int) {}
+                }
+                .javaClass)
+        .nativeProps
+  }
+
+  @Test(expected = RuntimeException::class)
+  fun testMethodWithTooFewParams() {
+    BaseViewManager(
+            object : ReactShadowNodeImpl() {
+                  @ReactProp(name = "prop") fun setterWithNoArgs() {}
+                }
+                .javaClass)
+        .nativeProps
+  }
+
+  @Test(expected = RuntimeException::class)
+  fun testUnsupportedValueType() {
+    BaseViewManager(
+            object : ReactShadowNodeImpl() {
+                  @ReactProp(name = "prop") fun setterWithMap(value: Map<*, *>) {}
+                }
+                .javaClass)
+        .nativeProps
+  }
+
+  @Test(expected = RuntimeException::class)
+  fun testGroupInvalidNumberOfParams() {
+    BaseViewManager(
+            object : ReactShadowNodeImpl() {
+                  @ReactPropGroup(names = ["prop1", "prop2"])
+                  fun setterWithTooManyParams(index: Int, value: Float, boolean: Boolean) {}
+                }
+                .javaClass)
+        .nativeProps
+  }
+
+  @Test(expected = RuntimeException::class)
+  fun testGroupTooFewParams() {
+    BaseViewManager(
+            object : ReactShadowNodeImpl() {
+                  @ReactPropGroup(names = ["props1", "prop2"])
+                  fun setterWithTooFewParams(index: Int) {}
+                }
+                .javaClass)
+        .nativeProps
+  }
+
+  @Test(expected = RuntimeException::class)
+  fun testGroupNoIndexParam() {
+    BaseViewManager(
+            object : ReactShadowNodeImpl() {
+                  @ReactPropGroup(names = ["prop1", "prop2"])
+                  fun setterWithNoIndexParam(value: Float, boolean: Boolean) {}
+                }
+                .javaClass)
+        .nativeProps
+  }
+
+  companion object {
+    private class BaseViewManager(
+        private val shadowNodeClass: Class<out ReactShadowNode<ReactShadowNodeImpl>>
+    ) : ViewManager<View, ReactShadowNode<*>>() {
+      override fun getName(): String = "IgnoredName"
+
+      override fun createShadowNodeInstance(): ReactShadowNode<*>? = null
+
+      override fun getShadowNodeClass(): Class<out ReactShadowNode<ReactShadowNodeImpl>> =
+          shadowNodeClass
+
+      override fun createViewInstance(reactContext: ThemedReactContext): View = View(null)
+
+      override fun updateExtraData(root: View, extraData: Any?) {}
+    }
+  }
+}
