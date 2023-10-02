b27fab70c52e613982f07a6a79699ff23afd4e59
Dmitry Rykun <dmitryrykun@meta.com>
2023-10-02 11:10:33 -0700

    Default to native view configs in bridged mode and to static view configs in bridgeless mode (#39704)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/39704
    
    Default to native view configs in bridged mode and to static view configs in bridgeless mode.
    Remove `setRuntimeConfigProvider` calls from RNTester and from the Template.
    Changelog: [Internal]
    
    Reviewed By: RSNara
    
    Differential Revision: D49687252
    
    fbshipit-source-id: 140e1c510ba3fbc153978b59c8bb4b4e35bc7571

--- packages/react-native/template/index.js
+++ packages/react-native/template/index.js
@@ -6,13 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-if (global.RN$Bridgeless) {
-  require('react-native/Libraries/NativeComponent/NativeComponentRegistry').setRuntimeConfigProvider(
-    name => {
-      // In bridgeless mode, never load native ViewConfig.
-      return {native: false, strict: false, verify: false};
-    },
-  );
-}
-
 AppRegistry.registerComponent(appName, () => App);
