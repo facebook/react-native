b12a87522e5acf38290b31ba1ecc51b0dd950d92
Phillip Pan <phillippan@fb.com>
2023-10-03 11:12:21 -0700

    introduce flag to determine if turbomodule void method should run sync (#39589)
    Summary:
    
    Changelog: [Internal]
    
    currently, turbomodule void methods run async by default, unless the consumer returns `RCTJSThread` in the `methodQueue` API.
    
    we're looking to update this, so i'm introducing a flag at the module level that allows us to configure this behavior.
    
    Reviewed By: RSNara
    
    Differential Revision: D49521864

--- packages/react-native/ReactCommon/react/nativemodule/core/platform/ios/ReactCommon/RCTTurboModule.h
+++ packages/react-native/ReactCommon/react/nativemodule/core/platform/ios/ReactCommon/RCTTurboModule.h
@@ -45,6 +45,7 @@ class JSI_EXPORT ObjCTurboModule : public TurboModule {
     std::shared_ptr<CallInvoker> jsInvoker;
     std::shared_ptr<NativeMethodCallInvoker> nativeMethodCallInvoker;
     bool isSyncModule;
+    bool shouldVoidMethodsExecuteSync;
   };
 
   ObjCTurboModule(const InitParams &params);
@@ -111,6 +112,9 @@ class JSI_EXPORT ObjCTurboModule : public TurboModule {
  private:
   // Does the NativeModule dispatch async methods to the JS thread?
   const bool isSyncModule_;
+
+  // Should void methods execute synchronously?
+  const bool shouldVoidMethodsExecuteSync_;
 
   /**
    * TODO(ramanpreet):
