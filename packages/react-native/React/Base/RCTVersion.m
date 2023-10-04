8eec3952689cc541e7e217914b38476ca0d7377a
Thibault Malbranche <malbranche.thibault@gmail.com>
2023-10-04 10:20:44 +0200

    Revert "[0.73.0-rc.1] Bump version numbers"
    This reverts commit 725b1c9a7ea1a8132b60e3d5ba345798e1ea0114.

--- packages/react-native/React/Base/RCTVersion.m
+++ packages/react-native/React/Base/RCTVersion.m
@@ -24,7 +24,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(73),
                   RCTVersionPatch: @(0),
-                  RCTVersionPrerelease: @"rc.1",
+                  RCTVersionPrerelease: @"rc.0",
                   };
   });
   return __rnVersion;
