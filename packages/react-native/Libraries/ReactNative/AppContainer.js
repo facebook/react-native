1f747a1750b1c9bf31f390c1ae0900a34ce6b4f4
Luis Miguel Alvarado <lalvarado@farmatrix.com.do>
2023-10-09 16:49:45 -0400

    refactor: don't include `DevtoolsOverlay` in the production bundle

--- packages/react-native/Libraries/ReactNative/AppContainer.js
+++ packages/react-native/Libraries/ReactNative/AppContainer.js
@@ -52,14 +52,16 @@ class AppContainer extends React.Component<Props, State> {
   static getDerivedStateFromError: any = undefined;
 
   mountReactDevToolsOverlays(): void {
-    const DevtoolsOverlay = require('../Inspector/DevtoolsOverlay').default;
-    const devtoolsOverlay = <DevtoolsOverlay inspectedView={this._mainRef} />;
+    if (__DEV__) {
+      const DevtoolsOverlay = require('../Inspector/DevtoolsOverlay').default;
+      const devtoolsOverlay = <DevtoolsOverlay inspectedView={this._mainRef} />;
 
-    const TraceUpdateOverlay =
-      require('../Components/TraceUpdateOverlay/TraceUpdateOverlay').default;
-    const traceUpdateOverlay = <TraceUpdateOverlay />;
+      const TraceUpdateOverlay =
+        require('../Components/TraceUpdateOverlay/TraceUpdateOverlay').default;
+      const traceUpdateOverlay = <TraceUpdateOverlay />;
 
-    this.setState({devtoolsOverlay, traceUpdateOverlay});
+      this.setState({devtoolsOverlay, traceUpdateOverlay});
+    }
   }
 
   componentDidMount(): void {
