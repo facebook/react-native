842a99797fca52274f0dd17fa0dc8b963d6c1047
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2023-10-11 19:49:44 +0200

    refactor: restore ReactNativeViewConfigRegistry to synced version

--- packages/react-native/Libraries/Renderer/shims/ReactNativeViewConfigRegistry.js
+++ packages/react-native/Libraries/Renderer/shims/ReactNativeViewConfigRegistry.js
@@ -16,7 +16,7 @@ import {type ViewConfig} from './ReactNativeTypes';
 import invariant from 'invariant';
 
 // Event configs
-export const customBubblingEventTypes: {
+const customBubblingEventTypes: {
   [eventName: string]: $ReadOnly<{
     phasedRegistrationNames: $ReadOnly<{
       captured: string,
@@ -26,12 +26,15 @@ export const customBubblingEventTypes: {
   }>,
   ...
 } = {};
-export const customDirectEventTypes: {
+const customDirectEventTypes: {
   [eventName: string]: $ReadOnly<{
     registrationName: string,
   }>,
   ...
 } = {};
+
+exports.customBubblingEventTypes = customBubblingEventTypes;
+exports.customDirectEventTypes = customDirectEventTypes;
 
 const viewConfigCallbacks = new Map<string, ?() => ViewConfig>();
 const viewConfigs = new Map<string, ViewConfig>();
@@ -74,7 +77,7 @@ function processEventTypes(viewConfig: ViewConfig): void {
  * A callback is provided to load the view config from UIManager.
  * The callback is deferred until the view is actually rendered.
  */
-export function register(name: string, callback: () => ViewConfig): string {
+exports.register = function (name: string, callback: () => ViewConfig): string {
   invariant(
     !viewConfigCallbacks.has(name),
     'Tried to register two views with the same name %s',
@@ -95,7 +98,7 @@ export function register(name: string, callback: () => ViewConfig): string {
  * If this is the first time the view has been used,
  * This configuration will be lazy-loaded from UIManager.
  */
-export function get(name: string): ViewConfig {
+exports.get = function (name: string): ViewConfig {
   let viewConfig;
   if (!viewConfigs.has(name)) {
     const callback = viewConfigCallbacks.get(name);
