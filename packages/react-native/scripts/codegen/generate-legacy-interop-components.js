95ccc49ef2d8502306a59241ae4699959c6772c3
Riccardo Cipolleschi <cipolleschi@fb.com>
2023-10-17 05:02:07 -0700

    Fix unstable RCTAppDelegate podspec (#41009)
    Summary:
    
    This change should fix [#39971](https://github.com/facebook/react-native/issues/39971), computing the relative path from the App path to the pod installation root and using that instead of the absolute path to the `react-native.config.js` file
    
    ## Changelog
    [Internal] - Stabilize RCTAppDelegate podspec
    
    Reviewed By: cortinico
    
    Differential Revision: D50323710

--- packages/react-native/scripts/codegen/generate-legacy-interop-components.js
+++ packages/react-native/scripts/codegen/generate-legacy-interop-components.js
@@ -11,6 +11,7 @@
 
 const yargs = require('yargs');
 const fs = require('fs');
+const p = require('path');
 
 const CONFIG_FILE_NAME = 'react-native.config.js';
 const PROJECT_FIELD = 'project';
@@ -92,7 +93,11 @@ function extractComponentsNames(reactNativeConfig) {
 }
 
 function generateRCTLegacyInteropComponents() {
-  const configFilePath = `${appRoot}/${CONFIG_FILE_NAME}`;
+  const cwd = process.cwd();
+  const configFilePath = p.join(cwd, appRoot, CONFIG_FILE_NAME);
+  console.log(
+    `Looking for a react-native.config.js file at ${configFilePath}...`,
+  );
   let reactNativeConfig = null;
   try {
     reactNativeConfig = require(configFilePath);
@@ -106,7 +111,7 @@ function generateRCTLegacyInteropComponents() {
     console.log('Skip LegacyInterop generation');
     return;
   }
-
+  console.log(`Components found: ${componentNames}`);
   let componentsArray = componentNames.map(name => `\t\t\t@"${name}",`);
   // Remove the last comma
   if (componentsArray.length > 0) {
@@ -117,6 +122,7 @@ function generateRCTLegacyInteropComponents() {
 
   const filePath = `${outputPath}/${OUTPUT_FILE_NAME}`;
   fs.writeFileSync(filePath, fileBody(componentsArray.join('\n')));
+  console.log(`${filePath} updated!`);
 }
 
 generateRCTLegacyInteropComponents();
