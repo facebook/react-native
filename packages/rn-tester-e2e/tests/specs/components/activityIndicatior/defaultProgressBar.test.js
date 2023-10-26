f41c4d048f211cef5f0d2986946460503efd35e5
Michal Korczynski <michal.korczynski1@gmail.com>
2023-10-26 23:11:08 +0200

    Adding it for greater visibility

--- packages/rn-tester-e2e/tests/specs/components/activityIndicatior/defaultProgressBar.test.js
+++ packages/rn-tester-e2e/tests/specs/components/activityIndicatior/defaultProgressBar.test.js
@@ -14,13 +14,21 @@
  } = require('../../../screens/components/activityIndicatorComponent.screen.js');
 
 describe('Test is checking default activity indicator component', function () {
-  it('Should view properly default progress bar', async function() {
+  
+  it('Should view properly Indicator Component', async function() {
     expect(
       await ComponentsScreen.checkActivityIndicatorComponentIsDisplayed(),
     ).toBeTruthy();
+  });
+
+  it('Click Activity Indicator', async function() {
     await ComponentsScreen.clickActivityIndicatorComponent();
+  });
+
+  it('Should view properly default progress bar', async function() {
     expect(
       await ActivityIndicatorComponentScreen.checkDefaultActivityIndicatorIsDisplayed(),
     ).toBeTruthy();
   });
+
 });
