67880391a016bcc92b8026abda5534811a5ebb94
David Vacca <dvacca@fb.com>
2023-10-25 22:32:46 -0700

    EZ cleanup of ReactInstanceManager
    Summary:
    cleanup of ReactInstanceManager to remove some lint warns
    
    changelog: [internal] internal
    
    Differential Revision: D50610673
    
    fbshipit-source-id: 10a3cfb27f528cab5470f321939b13abd8d2ab5b

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
@@ -88,7 +88,6 @@ import com.facebook.react.devsupport.interfaces.PackagerStatusCallback;
 import com.facebook.react.devsupport.interfaces.RedBoxHandler;
 import com.facebook.react.internal.turbomodule.core.TurboModuleManager;
 import com.facebook.react.internal.turbomodule.core.TurboModuleManagerDelegate;
-import com.facebook.react.internal.turbomodule.core.interfaces.TurboModuleRegistry;
 import com.facebook.react.modules.appearance.AppearanceModule;
 import com.facebook.react.modules.appregistry.AppRegistry;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
@@ -1338,7 +1337,7 @@ public class ReactInstanceManager {
         mJSExceptionHandler != null ? mJSExceptionHandler : mDevSupportManager;
     reactContext.setJSExceptionHandler(exceptionHandler);
 
-    NativeModuleRegistry nativeModuleRegistry = processPackages(reactContext, mPackages, false);
+    NativeModuleRegistry nativeModuleRegistry = processPackages(reactContext, mPackages);
 
     CatalystInstanceImpl.Builder catalystInstanceBuilder =
         new CatalystInstanceImpl.Builder()
@@ -1384,11 +1383,9 @@ public class ReactInstanceManager {
 
       catalystInstance.setTurboModuleManager(turboModuleManager);
 
-      TurboModuleRegistry registry = (TurboModuleRegistry) turboModuleManager;
-
       // Eagerly initialize TurboModules
-      for (String moduleName : registry.getEagerInitModuleNames()) {
-        registry.getModule(moduleName);
+      for (String moduleName : turboModuleManager.getEagerInitModuleNames()) {
+        turboModuleManager.getModule(moduleName);
       }
     }
 
@@ -1416,9 +1413,7 @@ public class ReactInstanceManager {
   }
 
   private NativeModuleRegistry processPackages(
-      ReactApplicationContext reactContext,
-      List<ReactPackage> packages,
-      boolean checkAndUpdatePackageMembership) {
+      ReactApplicationContext reactContext, List<ReactPackage> packages) {
     NativeModuleRegistryBuilder nativeModuleRegistryBuilder =
         new NativeModuleRegistryBuilder(reactContext, this);
 
@@ -1426,14 +1421,8 @@ public class ReactInstanceManager {
 
     synchronized (mPackages) {
       for (ReactPackage reactPackage : packages) {
-        if (checkAndUpdatePackageMembership && mPackages.contains(reactPackage)) {
-          continue;
-        }
         Systrace.beginSection(TRACE_TAG_REACT_JAVA_BRIDGE, "createAndProcessCustomReactPackage");
         try {
-          if (checkAndUpdatePackageMembership) {
-            mPackages.add(reactPackage);
-          }
           processPackage(reactPackage, nativeModuleRegistryBuilder);
         } finally {
           Systrace.endSection(TRACE_TAG_REACT_JAVA_BRIDGE);
