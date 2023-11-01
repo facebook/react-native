e24fa08595468af8d3217f1db07dd19fdf9ef5dc
ehsemloh <ehsemloh@gmail.com>
2023-11-02 11:06:05 +1300

    fixed

--- packages/react-native/Libraries/Text/Text/RCTTextShadowView.mm
+++ packages/react-native/Libraries/Text/Text/RCTTextShadowView.mm
@@ -301,7 +301,7 @@
                 localLayoutContext.absolutePosition.x += frame.origin.x;
                 localLayoutContext.absolutePosition.y += frame.origin.y;
 
-                [shadowView layoutWithMinimumSize:frame.size
+                [shadowView layoutWithMinimumSize:CGSizeZero
                                       maximumSize:frame.size
                                   layoutDirection:self.layoutMetrics.layoutDirection
                                     layoutContext:localLayoutContext];
