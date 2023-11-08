4c38f2ea901714cdbd3a1fb148d16106bd7ab365
Riccardo Cipolleschi <cipolleschi@fb.com>
2023-11-08 02:59:21 -0800

    Add helper function to add dependencies (#41354)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/41354
    
    In order to make the infra scalable and avoid a maintenance nightmare for macOS and future platform, we are introducing this function that automate adding a dependency to a podspec and it generates the required search paths.
    
    ## Context
    Last week I helped macOS to work with static framework.
    When multiple platforms are specified, frameworks are build in two variants, the iOS and macOS one.
    
    This break all the HEADER_SEARCH_PATHS as now we have to properly specify the base folder from which the search path is generated.
    See also [this PR](https://github.com/microsoft/react-native-macos/pull/1967) where I manually make MacOS work with `use_framewroks!`
    
    ## Changelog:
    [Internal] - Add helper function to create header_search_path
    
    Differential Revision: https://www.internalfb.com/diff/D51027343?entry_point=27
    
    fbshipit-source-id: dca4f3fb1fd9e15f7f3523b82870c71eb28c8e10

--- packages/react-native/scripts/cocoapods/utils.rb
+++ packages/react-native/scripts/cocoapods/utils.rb
@@ -244,6 +244,30 @@ class ReactNativePodsUtils
         return search_paths
     end
 
+    # Add a new dependency to an existing spec, configuring also the headers search paths
+    def self.add_dependency(spec, dependency_name, base_folder_for_frameworks, framework_name, additional_paths: [], version: nil, subspec_dependency: nil)
+        # Update Search Path
+        optional_current_search_path = spec.to_hash["pod_target_xcconfig"]["HEADER_SEARCH_PATHS"]
+        current_search_paths = (optional_current_search_path != nil ? optional_current_search_path : "")
+            .split(" ")
+        create_header_search_path_for_frameworks(base_folder_for_frameworks, dependency_name, framework_name, additional_paths)
+            .each { |path|
+                wrapped_path = "\"#{path}\""
+                current_search_paths << wrapped_path
+            }
+        current_pod_target_xcconfig = spec.to_hash["pod_target_xcconfig"]
+        current_pod_target_xcconfig["HEADER_SEARCH_PATHS"] = current_search_paths.join(" ")
+        spec.pod_target_xcconfig = current_pod_target_xcconfig
+
+        actual_dependency = subspec_dependency != nil ? "#{dependency_name}/#{subspec_dependency}" : dependency_name
+        # Set Dependency
+        if !version
+            spec.dependency actual_dependency
+        else
+            spec.dependency actual_dependency, version
+        end
+    end
+
     def self.update_search_paths(installer)
         return if ENV['USE_FRAMEWORKS'] == nil
 
