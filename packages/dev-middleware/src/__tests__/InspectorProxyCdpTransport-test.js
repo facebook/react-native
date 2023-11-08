b3ebdd8743586ae4c7792e2f727ad7f9ad881218
Moti Zilberman <moti@fb.com>
2023-11-08 06:02:09 -0800

    Add tests for HTTPS-specific behaviour
    Differential Revision: D51006835
    
    fbshipit-source-id: 4e5754ff6f440bb7fbedb024a6cdf79c9c708bce

--- packages/dev-middleware/src/__tests__/InspectorProxyCdpTransport-test.js
+++ packages/dev-middleware/src/__tests__/InspectorProxyCdpTransport-test.js
@@ -23,126 +23,130 @@ jest.useRealTimers();
 
 jest.setTimeout(10000);
 
-describe('inspector proxy CDP transport', () => {
-  const serverRef = withServerForEachTest({
-    logger: undefined,
-    projectRoot: '',
-  });
-  const autoCleanup = withAbortSignalForEachTest();
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+describe.each(['HTTP', 'HTTPS'])(
+  'inspector proxy CDP transport over %s',
+  protocol => {
+    const serverRef = withServerForEachTest({
+      logger: undefined,
+      projectRoot: '',
+      secure: protocol === 'HTTPS',
+    });
+    const autoCleanup = withAbortSignalForEachTest();
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
-  test('connection/disconnection and message from debugger to device', async () => {
-    const device1 = await createDeviceMock(
-      `${serverRef.serverBaseWsUrl}/inspector/device?device=device1&name=foo&app=bar`,
-      autoCleanup.signal,
-    );
-    try {
-      device1.getPages.mockImplementation(() => [
-        {
-          app: 'bar-app',
-          id: 'page1',
-          title: 'bar-title',
-          vm: 'bar-vm',
-        },
-      ]);
-
-      let pageList: Array<PageDescription> = [];
-      await until(async () => {
-        pageList = (await fetchJson(
-          `${serverRef.serverBaseUrl}/json`,
-          // $FlowIgnore[unclear-type]
-        ): any);
-        expect(pageList).toHaveLength(1);
-      });
-      const [{webSocketDebuggerUrl}] = pageList;
-      expect(webSocketDebuggerUrl).toBeDefined();
-
-      const debugger_ = await createDebuggerMock(
-        webSocketDebuggerUrl,
+    test('connection/disconnection and message from debugger to device', async () => {
+      const device1 = await createDeviceMock(
+        `${serverRef.serverBaseWsUrl}/inspector/device?device=device1&name=foo&app=bar`,
         autoCleanup.signal,
       );
       try {
-        await until(() => expect(device1.connect).toBeCalled());
+        device1.getPages.mockImplementation(() => [
+          {
+            app: 'bar-app',
+            id: 'page1',
+            title: 'bar-title',
+            vm: 'bar-vm',
+          },
+        ]);
 
-        debugger_.send({
-          method: 'Runtime.enable',
-          id: 0,
+        let pageList: Array<PageDescription> = [];
+        await until(async () => {
+          pageList = (await fetchJson(
+            `${serverRef.serverBaseUrl}/json`,
+            // $FlowIgnore[unclear-type]
+          ): any);
+          expect(pageList).toHaveLength(1);
         });
+        const [{webSocketDebuggerUrl}] = pageList;
+        expect(webSocketDebuggerUrl).toBeDefined();
 
-        await until(() => expect(device1.wrappedEvent).toBeCalled());
+        const debugger_ = await createDebuggerMock(
+          webSocketDebuggerUrl,
+          autoCleanup.signal,
+        );
+        try {
+          await until(() => expect(device1.connect).toBeCalled());
 
-        expect(device1.wrappedEventParsed).toBeCalledWith({
-          pageId: 'page1',
-          wrappedEvent: {
+          debugger_.send({
             method: 'Runtime.enable',
             id: 0,
-          },
-        });
+          });
 
-        debugger_.close();
+          await until(() => expect(device1.wrappedEvent).toBeCalled());
 
-        await until(() => expect(device1.disconnect).toBeCalled());
+          expect(device1.wrappedEventParsed).toBeCalledWith({
+            pageId: 'page1',
+            wrappedEvent: {
+              method: 'Runtime.enable',
+              id: 0,
+            },
+          });
+
+          debugger_.close();
+
+          await until(() => expect(device1.disconnect).toBeCalled());
+        } finally {
+          debugger_.close();
+        }
       } finally {
-        debugger_.close();
+        device1.close();
       }
-    } finally {
-      device1.close();
-    }
-  });
+    });
 
-  test('message and disconnection from device to debugger', async () => {
-    const device1 = await createDeviceMock(
-      `${serverRef.serverBaseWsUrl}/inspector/device?device=device1&name=foo&app=bar`,
-      autoCleanup.signal,
-    );
-    try {
-      device1.getPages.mockImplementation(() => [
-        {
-          app: 'bar-app',
-          id: 'page1',
-          title: 'bar-title',
-          vm: 'bar-vm',
-        },
-      ]);
-
-      let pageList: Array<PageDescription> = [];
-      await until(async () => {
-        pageList = (await fetchJson(
-          `${serverRef.serverBaseUrl}/json`,
-          // $FlowIgnore[unclear-type]
-        ): any);
-        expect(pageList).toHaveLength(1);
-      });
-      const [{webSocketDebuggerUrl}] = pageList;
-      expect(webSocketDebuggerUrl).toBeDefined();
-
-      const debugger_ = await createDebuggerMock(
-        webSocketDebuggerUrl,
+    test('message and disconnection from device to debugger', async () => {
+      const device1 = await createDeviceMock(
+        `${serverRef.serverBaseWsUrl}/inspector/device?device=device1&name=foo&app=bar`,
         autoCleanup.signal,
       );
-      let debuggerSocketClosed = false;
-      debugger_.socket.once('close', () => {
-        debuggerSocketClosed = true;
-      });
       try {
-        await until(() => expect(device1.connect).toBeCalled());
+        device1.getPages.mockImplementation(() => [
+          {
+            app: 'bar-app',
+            id: 'page1',
+            title: 'bar-title',
+            vm: 'bar-vm',
+          },
+        ]);
 
-        device1.sendWrappedEvent('page1', {
-          id: 0,
+        let pageList: Array<PageDescription> = [];
+        await until(async () => {
+          pageList = (await fetchJson(
+            `${serverRef.serverBaseUrl}/json`,
+            // $FlowIgnore[unclear-type]
+          ): any);
+          expect(pageList).toHaveLength(1);
         });
+        const [{webSocketDebuggerUrl}] = pageList;
+        expect(webSocketDebuggerUrl).toBeDefined();
 
-        await until(() => expect(debugger_.handle).toBeCalledWith({id: 0}));
+        const debugger_ = await createDebuggerMock(
+          webSocketDebuggerUrl,
+          autoCleanup.signal,
+        );
+        let debuggerSocketClosed = false;
+        debugger_.socket.once('close', () => {
+          debuggerSocketClosed = true;
+        });
+        try {
+          await until(() => expect(device1.connect).toBeCalled());
 
-        device1.close();
+          device1.sendWrappedEvent('page1', {
+            id: 0,
+          });
 
-        await until(() => expect(debuggerSocketClosed).toBe(true));
+          await until(() => expect(debugger_.handle).toBeCalledWith({id: 0}));
+
+          device1.close();
+
+          await until(() => expect(debuggerSocketClosed).toBe(true));
+        } finally {
+          debugger_.close();
+        }
       } finally {
-        debugger_.close();
+        device1.close();
       }
-    } finally {
-      device1.close();
-    }
-  });
-});
+    });
+  },
+);
