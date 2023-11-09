3c259240f0ea68a9105be817d71b06f3e046d6d3
Gabriel Donadel <donadeldev@gmail.com>
2023-11-09 13:23:41 -0300

    Fix export default

--- packages/dev-middleware/src/index.flow.js
+++ packages/dev-middleware/src/index.flow.js
@@ -14,5 +14,5 @@ export {default as createDevMiddleware} from './createDevMiddleware';
 export type {BrowserLauncher, LaunchedBrowser} from './types/BrowserLauncher';
 export type {EventReporter, ReportableEvent} from './types/EventReporter';
 
-export {InspectorProxy as unstable_InspectorProxy} from './inspector-proxy/InspectorProxy';
-export {Device as unstable_Device} from './inspector-proxy/Device';
+export {default as unstable_InspectorProxy} from './inspector-proxy/InspectorProxy';
+export {default as unstable_Device} from './inspector-proxy/Device';
