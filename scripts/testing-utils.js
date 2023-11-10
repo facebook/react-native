f399afc3955e31455f92101a17ee594a10d96279
Marek Fořt <marekfort@me.com>
2023-11-10 14:44:14 +0100

    Fix RNTestProject testing on Android (#41378)
    * Fix RNTestProject testing on Android (#41172)
    
    Summary:
    While releasing RN 0.73.0-RC3, we relaized that the e2e test script was bugged for Android when used to test RNTestProject with the `-c` option.
    
    There  were 2 problems:
    - The downloaded maven-local was not actually used because it doesn't work with a zip. (We were always downloading a version from Maven)
    - The versions of React Native between maven-local and the locally packaged React Native were different.
    
    This change fixes the script by:
    - Downloading maven-local
    - Unzipping maven-local and passing the new folder to the Android app
    - Downloading the React Native version that has been packaged in CI
    
    By unzipping maven-local and using the unzipped folder, we make sure that Android is actually using the local repository.
    By downloading both the packaged react native and the maven-local from the same CI workflow, we ensure that the versions are aligned.
    
    This also speeds-up further the Android testing.
    
    While running this change, we also moved the `pod install` step inside the `if (iOS)` branch, so we do not install Cocoapods if we need to test
    Android.
    
    [Internal] - Fix Android E2E test script when downloading artefacts from CI
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/41172
    
    Test Plan: Tested locally on both main and 0.73-stable, on both Android and iOS
    
    Reviewed By: cortinico
    
    Differential Revision: D50651448
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: 70a9ed19072119d19c5388e8a4309d7333a08e13
    
    * Backport e2e script changes to main (#41332)
    
    Summary:
    Last week, I modified the e2e script to make sure it was working properly with 0.73.
    This change backport those changes in main
    
    ## Changelog:
    [Internal] - Backport e2e script changes
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/41332
    
    Test Plan: Tested locally
    
    Reviewed By: dmytrorykun
    
    Differential Revision: D51025796
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: 89ecd3701eaac4ba4bdde2c640df45a158329158
    
    * Use old REACT_NATIVE_MAVEN_LOCAL_REPO env var
    
    * prettier
    
    ---------
    
    Co-authored-by: Riccardo Cipolleschi <riccardo.cipolleschi@gmail.com>
    Co-authored-by: Riccardo Cipolleschi <cipolleschi@meta.com>

--- scripts/testing-utils.js
+++ scripts/testing-utils.js
@@ -170,16 +170,21 @@ async function downloadArtifactsFromCircleCI(
 ) {
   const mavenLocalURL = await circleCIArtifacts.artifactURLForMavenLocal();
   const hermesURL = await circleCIArtifacts.artifactURLHermesDebug();
+  const reactNativeURL = await circleCIArtifacts.artifactURLForReactNative();
 
   const hermesPath = path.join(
     circleCIArtifacts.baseTmpPath(),
     'hermes-ios-debug.tar.gz',
   );
 
-  console.info('[Download] Maven Local Artifacts');
-  circleCIArtifacts.downloadArtifact(mavenLocalURL, mavenLocalPath);
+  console.info(`[Download] Maven Local Artifacts from ${mavenLocalURL}`);
+  const mavenLocalZipPath = `${mavenLocalPath}.zip`;
+  circleCIArtifacts.downloadArtifact(mavenLocalURL, mavenLocalZipPath);
+  exec(`unzip -oq ${mavenLocalZipPath} -d ${mavenLocalPath}`);
   console.info('[Download] Hermes');
   circleCIArtifacts.downloadArtifact(hermesURL, hermesPath);
+  console.info(`[Download] React Native from  ${reactNativeURL}`);
+  circleCIArtifacts.downloadArtifact(reactNativeURL, localNodeTGZPath);
 
   return hermesPath;
 }
