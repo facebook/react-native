86fb167d803324ac8a8ad35bd28b59381e51c386
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-11-28 15:13:30 +0800

    commit example

--- packages/rn-tester/js/RNTesterAppShared.js
+++ packages/rn-tester/js/RNTesterAppShared.js
@@ -5,16 +5,23 @@ const RNTesterApp = () => {
   const email =
     'From vincenzoddragon+five@gmail.com  From vincenzoddrlxagon+five@gmail.com';
   return (
-    <View style={styles.container}>
-      <View style={styles.flexBrokenStyle}>
-        <Text
-          textBreakStrategy="simple"
-          numberOfLines={1}
-          style={{backgroundColor: 'pink'}}>
-          {email}
-        </Text>
+    <>
+      <View style={styles.container}>
+        <View style={styles.flexBrokenStyle}>
+          <Text
+            textBreakStrategy="simple"
+            numberOfLines={1}
+            style={{backgroundColor: 'pink'}}>
+            {email}
+          </Text>
+        </View>
       </View>
-    </View>
+      <View style={styles.container}>
+        <View style={styles.fullWidth}>
+          <Text numberOfLines={1} style={styles.textStyle}>Example 2</Text>
+        </View>
+      </View>
+    </>
   );
 };
 
@@ -32,5 +39,21 @@ const styles = StyleSheet.create({
   },
   flexBrokenStyle2: {
     alignSelf: 'flex-start',
+  },
+  fullWidth: {
+    width: '100%',
+    backgroundColor: 'red',
+  },
+  textStyle: {
+    color: '#E7ECE9',
+    fontSize: 15,
+    textAlign: 'left',
+    flexShrink: 1,
+    marginLeft: 12,
+    fontWeight: '600',
+    writingDirection: 'ltr',
+    lineHeight: 20,
+    width: 30,
+    backgroundColor: "pink",
   },
 });
