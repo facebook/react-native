ca8bfba67a43ccc0c0faf1bd433e1ac87388bf5f
Will Schurman <wschurman@gmail.com>
2023-11-28 13:43:33 -0800

    Fix android root view group removal during instance re-creation

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManager.java
@@ -1296,14 +1296,12 @@ public class ReactInstanceManager {
           uiManager.stopSurface(surfaceId);
         } else {
           FLog.w(ReactConstants.TAG, "Failed to stop surface, UIManager has already gone away");
-          reactRoot.getRootViewGroup().removeAllViews();
         }
       } else {
         ReactSoftExceptionLogger.logSoftException(
             TAG,
             new RuntimeException(
                 "detachRootViewFromInstance called with ReactRootView with invalid id"));
-        reactRoot.getRootViewGroup().removeAllViews();
       }
     } else {
       reactContext
@@ -1312,8 +1310,7 @@ public class ReactInstanceManager {
           .unmountApplicationComponentAtRootTag(reactRoot.getRootViewTag());
     }
 
-    // The view is no longer attached, so mark it as such by resetting its ID.
-    reactRoot.getRootViewGroup().setId(View.NO_ID);
+    clearReactRoot(reactRoot);
   }
 
   @ThreadConfined(UI)
