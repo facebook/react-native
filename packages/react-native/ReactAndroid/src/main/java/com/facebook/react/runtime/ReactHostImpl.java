99ad36445c56993d450b2b647d35ba05a6843c50
Nicola Corti <ncor@meta.com>
2023-12-08 10:19:25 -0800

    Convert the last Unit Tests to Kotlin (#41852)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/41852
    
    I'm doing a pass and converting the last Java Unit Tests we had to Kotlin
    I've also re-enabled multiple tests that were disabled in the past.
    
    Changelog:
    [Internal] [Changed] - Convert the last Unit Tests to Kotlin
    
    Reviewed By: mdvacca
    
    Differential Revision: D51891716
    
    fbshipit-source-id: 7f953cf039a7b45bd773d1995253b4db262f8d22

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/runtime/ReactHostImpl.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/runtime/ReactHostImpl.java
@@ -137,7 +137,7 @@ public class ReactHostImpl implements ReactHost {
   public ReactHostImpl(
       Context context,
       ReactHostDelegate delegate,
-      ComponentFactory componentFactory,
+      @Nullable ComponentFactory componentFactory,
       boolean allowPackagerServerAccess,
       ReactJsExceptionHandler reactJsExceptionHandler,
       boolean useDevSupport) {
@@ -155,7 +155,7 @@ public class ReactHostImpl implements ReactHost {
   public ReactHostImpl(
       Context context,
       ReactHostDelegate delegate,
-      ComponentFactory componentFactory,
+      @Nullable ComponentFactory componentFactory,
       Executor bgExecutor,
       Executor uiExecutor,
       ReactJsExceptionHandler reactJsExceptionHandler,
