383d77f9b871e35c42acc984b76d81a56046d1b3
zhongwuzw <zhongwuzw@qq.com>
2023-12-08 15:23:47 +0800

    [iOS] Fabric: Fixes Modal present splash when animation type is none

--- packages/react-native/ReactCommon/react/renderer/components/modal/ModalHostViewState.h
+++ packages/react-native/ReactCommon/react/renderer/components/modal/ModalHostViewState.h
@@ -16,6 +16,10 @@
 #include <react/renderer/mapbuffer/MapBufferBuilder.h>
 #endif
 
+#if defined(__APPLE__) && TARGET_OS_IOS
+#include "ModalHostViewUtils.h"
+#endif
+
 namespace facebook::react {
 
 /*
@@ -25,7 +29,12 @@ class ModalHostViewState final {
  public:
   using Shared = std::shared_ptr<const ModalHostViewState>;
 
-  ModalHostViewState(){};
+#if defined(__APPLE__) && TARGET_OS_IOS
+  ModalHostViewState() : screenSize(RCTModalHostViewScreenSize()) {
+#else
+  ModalHostViewState(){
+#endif
+  };
   ModalHostViewState(Size screenSize_) : screenSize(screenSize_){};
 
 #ifdef ANDROID
