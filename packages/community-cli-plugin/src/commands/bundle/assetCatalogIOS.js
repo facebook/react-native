0876eebd6db9a796f53868cf10ef87b25fbb4c82
Janic Duplessis <janicduplessis@gmail.com>
2023-12-08 16:28:53 -0500

    Fix cleanAssetCatalog

--- packages/community-cli-plugin/src/commands/bundle/assetCatalogIOS.js
+++ packages/community-cli-plugin/src/commands/bundle/assetCatalogIOS.js
@@ -16,11 +16,11 @@ import fs from 'fs';
 import path from 'path';
 
 export function cleanAssetCatalog(catalogDir: string): void {
-  const files = fs
+  const imageSets = fs
     .readdirSync(catalogDir)
-    .filter(file => file.endsWith('.imageset'));
-  for (const file of files) {
-    fs.rmSync(path.join(catalogDir, file));
+    .filter(imageSet => imageSet.endsWith('.imageset'));
+  for (const imageSet of imageSets) {
+    fs.rmSync(path.join(catalogDir, imageSet), {recursive: true, force: true});
   }
 }
 
