b061293b8497a43c2732b18684ad20e81c15a4f1
Marc Rousavy <me@mrousavy.com>
2023-12-09 13:19:52 +0100

    fix: Fix adding custom `ReactPackage`s in Kotlin template
    The previous comment in the template used Java, this has now been updated to Kotlin.

--- packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -16,9 +16,10 @@ class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> {
+          val packages = PackageList(this).packages
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return PackageList(this).packages
+          // packages.add(MyReactNativePackage())
+          return packages
         }
 
         override fun getJSMainModuleName(): String = "index"
