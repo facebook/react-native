cab12a227597d2c1fc9110653eac097cca99691a
Janic Duplessis <janicduplessis@gmail.com>
2023-12-12 12:24:51 -0500

    Update packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
    Co-authored-by: Nicola Corti <corti.nico@gmail.com>

--- packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -18,7 +18,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              add(TWAppTurboReactPackage())
+              // add(MyReactNativePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
