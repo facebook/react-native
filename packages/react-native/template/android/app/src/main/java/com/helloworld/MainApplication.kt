4e5257213cc554bfaff2dd28ee6137ba1c612951
Janic Duplessis <janicduplessis@gmail.com>
2023-12-12 11:40:45 -0500

    Update MainApplication.kt

--- packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -15,12 +15,11 @@ class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> {
-          return PackageList(this).packages.apply {
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-	          // add(MyReactNativePackage())
-          }
-        }
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              add(TWAppTurboReactPackage())
+            }
 
         override fun getJSMainModuleName(): String = "index"
 
