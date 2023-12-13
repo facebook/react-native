1910ed8fdf2f28e41f862c8c47abaf17db1c6a0f
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-12-13 16:32:13 +0100

    feat: change logic to char based

--- packages/react-native/scripts/codegen/generate-legacy-interop-components.js
+++ packages/react-native/scripts/codegen/generate-legacy-interop-components.js
@@ -18,7 +18,7 @@ const PROJECT_FIELD = 'project';
 const IOS_FIELD = 'ios';
 const LEGACY_COMPONENTS_FIELD = 'unstable_reactLegacyComponentNames';
 const OUTPUT_FILE_NAME = 'RCTLegacyInteropComponents.mm';
-const COMPONENTS_IN_ONE_LINE = 3;
+const MAX_CHARS_IN_ONE_LINE = 80;
 
 const argv = yargs
   .option('p', {
@@ -35,9 +35,8 @@ const argv = yargs
 const appRoot = argv.path;
 const outputPath = argv.outputPath;
 
-function fileBody(components) {
+function fileBody(components, isOneLine) {
   // Generate components array string with proper formatting
-  const isOneLine = components.length < COMPONENTS_IN_ONE_LINE;
   const componentsString = components.join(isOneLine ? '' : '\n');
   const componentsArray = isOneLine
     ? `@[${componentsString} ];`
@@ -119,7 +118,7 @@ function generateRCTLegacyInteropComponents() {
     return;
   }
   console.log(`Components found: ${componentNames}`);
-  const isOneLine = componentNames.length < COMPONENTS_IN_ONE_LINE;
+  const isOneLine = componentNames.join().length < MAX_CHARS_IN_ONE_LINE;
   let componentsArray = componentNames.map(
     (name, index) => `${isOneLine ? ' ' : '\t\t\t'}@"${name}",`,
   );
@@ -131,7 +130,7 @@ function generateRCTLegacyInteropComponents() {
   }
 
   const filePath = `${outputPath}/${OUTPUT_FILE_NAME}`;
-  fs.writeFileSync(filePath, fileBody(componentsArray));
+  fs.writeFileSync(filePath, fileBody(componentsArray, isOneLine));
   console.log(`${filePath} updated!`);
 }
 
