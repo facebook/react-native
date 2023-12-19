eb4d8e2a8b7a2996de1a0f597ad0ca22bbbf4255
Abdullah Al Jahid <abdullah.al.jahid@gmail.com>
2023-12-19 12:35:24 +0600

    Tab fix
    changed the tab into 2 spaces.

--- packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -17,8 +17,8 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
           PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // add(MyReactNativePackage())
           }
 
         override fun getJSMainModuleName(): String = "index"
