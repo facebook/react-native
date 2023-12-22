e870264f0eb31440d967507cf2e6a50e7e60d50a
Kræn Hansen <kraen.hansen@mongodb.com>
2023-12-22 23:40:56 +0100

    Adding ccache wrapper scripts, which sets a config path env variable

---
+++ packages/react-native/scripts/xcode/ccache-clang++.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Get the absolute path of this script
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+
+REACT_NATIVE_CCACHE_CONFIGPATH=$SCRIPT_DIR/ccache.conf
+# Provide our config file if none is already provided
+export CCACHE_CONFIGPATH="${CCACHE_CONFIGPATH:-$REACT_NATIVE_CCACHE_CONFIGPATH}"
+
+exec ccache clang++ "$@"
