bc05f48ad125b398f338f861bb5abd1749bd59d7
Janic Duplessis <janicduplessis@gmail.com>
2023-12-27 08:47:02 -0800

    Fix comment about adding packages in android template (#41856)
    Summary:
    I noticed this comment is still in Java in the Kotlin template. It also doesn't really work anymore since there is no packages variable.
    
    To fix it I completed the comment with all code needed for it to work in kotlin. I think an older version of the template used to be more like:
    
    ```kotlin
    val packages = PackageList(this).packages
    // packages.add(MyReactNativePackage())
    return packages
    ```
    
    But then it requires adding a lint suppress annotation since packages variable can be simplified. I think this is simpler even if it makes the comment a few more lines.
    
    ## Changelog:
    
    [GENERAL] [FIXED] - Fix comment about adding packages in android template
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/41856
    
    Test Plan: Tested that uncommenting that code works
    
    Reviewed By: cipolleschi
    
    Differential Revision: D51987483
    
    Pulled By: cortinico
    
    fbshipit-source-id: d0135b5b536960017ccc7b25f92c75b3bd863cd9

--- packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ packages/react-native/template/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -16,11 +16,11 @@ class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return PackageList(this).packages
-        }
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
+            }
 
         override fun getJSMainModuleName(): String = "index"
 
