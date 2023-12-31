ce31cc4d0edca1913ccf014d333984251767fe6d
Andrés Leiva <aleiva1700@gmail.com>
2023-12-30 21:04:47 -0500

    chore(scripts): optimize tryExecNTimes for performance

--- scripts/try-n-times.js
+++ scripts/try-n-times.js
@@ -10,7 +10,7 @@
 'use strict';
 
 /**
- * Try executing a function n times recursively.
+ * Try executing a function n times iteratively.
  * Return 0 the first time it succeeds
  * Return code of the last failed commands if not more retries left
  * @funcToRetry - function that gets retried
@@ -18,21 +18,22 @@
  * @onEveryError - func to execute if funcToRetry returns non 0
  */
 function tryExecNTimes(funcToRetry, retriesLeft, onEveryError) {
-  const exitCode = funcToRetry();
-  if (exitCode === 0) {
-    return exitCode;
-  } else {
+  let exitCode = funcToRetry();
+
+  while (exitCode !== 0 && retriesLeft > 0) {
     if (onEveryError) {
       onEveryError();
     }
-    retriesLeft--;
+    
+    --retriesLeft;
     console.warn(`Command failed, ${retriesLeft} retries left`);
-    if (retriesLeft === 0) {
-      return exitCode;
-    } else {
-      return tryExecNTimes(funcToRetry, retriesLeft, onEveryError);
+
+    if (retriesLeft > 0) {
+      exitCode = funcToRetry();
     }
   }
+
+  return exitCode;
 }
 
 module.exports = tryExecNTimes;
