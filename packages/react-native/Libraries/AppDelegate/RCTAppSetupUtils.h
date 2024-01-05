d50c9068a927ec1fa0c574a4483dc7c03dca75f5
Kevin Gozali <fkg@meta.com>
2024-01-05 09:11:41 -0800

    iOS: Fixed up remaining RCT_USE_HERMES usage --> USE_HERMES (#42148)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/42148
    
    https://github.com/facebook/react-native/pull/41625 cleaned up some usages, but there are some remaining ones.
    
    Changelog: [iOS][Fixed] Further cleaned up RCT_USE_HERMES
    
    Reviewed By: cipolleschi
    
    Differential Revision: D52555309
    
    fbshipit-source-id: e19d10de339636eca2f4762a78cebb282d0427d9

--- packages/react-native/Libraries/AppDelegate/RCTAppSetupUtils.h
+++ packages/react-native/Libraries/AppDelegate/RCTAppSetupUtils.h
@@ -15,18 +15,15 @@
 
 #import <memory>
 
-#ifndef RCT_USE_HERMES
+#if USE_HERMES
 #if __has_include(<jsireact/HermesExecutorFactory.h>)
 #import <jsireact/HermesExecutorFactory.h>
-#define RCT_USE_HERMES 1
 #elif __has_include(<reacthermes/HermesExecutorFactory.h>)
 #import <reacthermes/HermesExecutorFactory.h>
-#define RCT_USE_HERMES 1
-#else
+#endif
+#else // USE_HERMES
 #import <React/JSCExecutorFactory.h>
-#define RCT_USE_HERMES 0
-#endif
-#endif
+#endif // USE_HERMES
 
 #import <ReactCommon/RCTTurboModuleManager.h>
 
