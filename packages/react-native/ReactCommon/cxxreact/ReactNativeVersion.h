d2ac5d6a55ab0cd3a32c6939b941b31f06ea32e8
Distiller <distiller@static.38.39.183.187.cyberlynk.net>
2024-01-08 14:55:48 +0000

    [0.73.2] Bump version numbers

--- packages/react-native/ReactCommon/cxxreact/ReactNativeVersion.h
+++ packages/react-native/ReactCommon/cxxreact/ReactNativeVersion.h
@@ -17,7 +17,7 @@ namespace facebook::react {
 constexpr struct {
   int32_t Major = 0;
   int32_t Minor = 73;
-  int32_t Patch = 1;
+  int32_t Patch = 2;
   std::string_view Prerelease = "";
 } ReactNativeVersion;
 
