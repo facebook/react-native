04fa812685fbe41010f5124c1c150e119a0453e4
David Vacca <dvacca@meta.com>
2024-01-19 13:16:19 -0800

    Mark CallInvokerHolder APIs as FrameworkAPI only (#42399)
    Summary:
    
    Mark CallInvokerHolder APIs as FrameworkAPI only, these APIs are meant to be used only for partner frameworks
    
    changelog: [internal] internal
    
    Reviewed By: cortinico
    
    Differential Revision: D52913739

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/NativeMethodCallInvokerHolderImpl.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/NativeMethodCallInvokerHolderImpl.java
@@ -9,6 +9,7 @@ package com.facebook.react.turbomodule.core;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
+import com.facebook.react.common.annotations.FrameworkAPI;
 import com.facebook.react.internal.turbomodule.core.NativeModuleSoLoader;
 import com.facebook.react.turbomodule.core.interfaces.NativeMethodCallInvokerHolder;
 
@@ -18,6 +19,7 @@ import com.facebook.react.turbomodule.core.interfaces.NativeMethodCallInvokerHol
  * class so that we may pass it from CatalystInstance, through Java, to
  * TurboModuleManager::initHybrid.
  */
+@FrameworkAPI
 public class NativeMethodCallInvokerHolderImpl implements NativeMethodCallInvokerHolder {
 
   @DoNotStrip private final HybridData mHybridData;
