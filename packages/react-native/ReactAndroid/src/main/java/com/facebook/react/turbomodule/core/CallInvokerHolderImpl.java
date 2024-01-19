6fbf042d937d9c073fd7c26aa70807ddf6c9f98a
David Vacca <dvacca@meta.com>
2024-01-19 13:21:37 -0800

    Mark CallInvokerHolder APIs as FrameworkAPI only (#42399)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/42399
    
    Mark CallInvokerHolder APIs as FrameworkAPI only, these APIs are meant to be used only for partner frameworks
    
    changelog: [internal] internal
    
    Reviewed By: cortinico
    
    Differential Revision: D52913739
    
    fbshipit-source-id: 5a2c8be629e90a33e0cfb66b28e0171c71f5940d

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/CallInvokerHolderImpl.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/CallInvokerHolderImpl.java
@@ -9,6 +9,7 @@ package com.facebook.react.turbomodule.core;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
+import com.facebook.react.common.annotations.FrameworkAPI;
 import com.facebook.react.internal.turbomodule.core.NativeModuleSoLoader;
 import com.facebook.react.turbomodule.core.interfaces.CallInvokerHolder;
 
@@ -17,6 +18,7 @@ import com.facebook.react.turbomodule.core.interfaces.CallInvokerHolder;
  * TurboModuleManager. Therefore, we need to wrap JSCallInvoker within a hybrid class so that we may
  * pass it from CatalystInstance, through Java, to TurboModuleManager::initHybrid.
  */
+@FrameworkAPI
 public class CallInvokerHolderImpl implements CallInvokerHolder {
 
   @DoNotStrip private final HybridData mHybridData;
