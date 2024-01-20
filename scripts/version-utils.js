de1ac2b68970a902a6f81eeceaa9513651254834
Luna Wei <lunaleaps@gmail.com>
2024-01-19 21:02:37 -0800

    Flow type some util files (#42372)
    Summary:
    Changelog: [Internal] Flow comment type some util scripts
    
    
    Differential Revision: D52893759
    
    Pulled By: lunaleaps

--- scripts/version-utils.js
+++ scripts/version-utils.js
@@ -4,11 +4,22 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict-local
  * @format
  */
 
 const VERSION_REGEX = /^v?((\d+)\.(\d+)\.(\d+)(?:-(.+))?)$/;
 
+/*::
+type BuildType = 'dry-run' | 'release' | 'nightly' | 'prealpha';
+type Version = {
+    version: string,
+    major: string,
+    minor: string,
+    patch: string,
+    prerelease: string
+}
+*/
 /**
  * Parses a version string and performs some checks to verify its validity.
  * A valid version is in the format vX.Y.Z[-KKK] where X, Y, Z are numbers and KKK can be something else.
@@ -22,24 +33,11 @@ const VERSION_REGEX = /^v?((\d+)\.(\d+)\.(\d+)(?:-(.+))?)$/;
  * - nightly: X.Y.Z-20221116-0bc4547fc
  * - dryrun: 1000.0.0
  * - prealpha: 0.0.0-prealpha-20221116
- *
- * Parameters:
- * - @versionStr the string representing a version
- * - @buildType the build type. It can be of values: `dry-run`, `release`, `nightly`
- *
- * Returns: an object with the shape:
- * ```
- * {
- *   version: string,
- *   major: number,
- *   minor: number,
- *   patch: number,
- *   prerelease: string
- * }
- * ```
- *
  */
-function parseVersion(versionStr, buildType) {
+function parseVersion(
+  versionStr /*: string */,
+  buildType /*: BuildType */,
+) /*: Version */ {
   validateBuildType(buildType);
 
   const match = extractMatchIfValid(versionStr);
@@ -58,7 +56,7 @@ function parseVersion(versionStr, buildType) {
   return versionObject;
 }
 
-function validateBuildType(buildType) {
+function validateBuildType(buildType /*: BuildType */) {
   const validBuildTypes = new Set([
     'release',
     'dry-run',
@@ -70,7 +68,7 @@ function validateBuildType(buildType) {
   }
 }
 
-function extractMatchIfValid(versionStr) {
+function extractMatchIfValid(versionStr /*: string */) {
   const match = versionStr.match(VERSION_REGEX);
   if (!match) {
     throw new Error(
@@ -80,7 +78,10 @@ function extractMatchIfValid(versionStr) {
   return match;
 }
 
-function validateVersion(versionObject, buildType) {
+function validateVersion(
+  versionObject /*: Version */,
+  buildType /*: BuildType */,
+) {
   const map = {
     release: validateRelease,
     'dry-run': validateDryRun,
@@ -95,14 +96,14 @@ function validateVersion(versionObject, buildType) {
 /**
  * Releases are in the form of 0.Y.Z[-RC.0]
  */
-function validateRelease(version) {
+function validateRelease(version /*: Version */) {
   const validRelease = isStableRelease(version) || isStablePrerelease(version);
   if (!validRelease) {
     throw new Error(`Version ${version.version} is not valid for Release`);
   }
 }
 
-function validateDryRun(version) {
+function validateDryRun(version /*: Version */) {
   if (
     !isMain(version) &&
     !isNightly(version) &&
@@ -113,27 +114,27 @@ function validateDryRun(version) {
   }
 }
 
-function validateNightly(version) {
+function validateNightly(version /*: Version */) {
   // a valid nightly is a prerelease
   if (!isNightly(version)) {
     throw new Error(`Version ${version.version} is not valid for nightlies`);
   }
 }
 
-function validatePrealpha(version) {
+function validatePrealpha(version /*: Version */) {
   if (!isValidPrealpha(version)) {
     throw new Error(`Version ${version.version} is not valid for prealphas`);
   }
 }
 
-function isStableRelease(version) {
+function isStableRelease(version /*: Version */) /*: boolean */ {
   return (
     version.major === '0' && version.minor !== '0' && version.prerelease == null
   );
 }
 
-function isStablePrerelease(version) {
-  return (
+function isStablePrerelease(version /*: Version */) /*: boolean */ {
+  return !!(
     version.major === '0' &&
     version.minor !== '0' &&
     version.patch.match(/^\d+$/) &&
@@ -144,7 +145,7 @@ function isStablePrerelease(version) {
   );
 }
 
-function isNightly(version) {
+function isNightly(version /*: Version */) /*: boolean */ {
   // Check if older nightly version
   if (version.major === '0' && version.minor === '0' && version.patch === '0') {
     return true;
@@ -153,18 +154,18 @@ function isNightly(version) {
   return version.version.includes('nightly');
 }
 
-function isMain(version) {
+function isMain(version /*: Version */) /*: boolean */ {
   return (
     version.major === '1000' && version.minor === '0' && version.patch === '0'
   );
 }
 
-function isReleaseBranch(branch) {
+function isReleaseBranch(branch /*:string */) /*: boolean */ {
   return branch.endsWith('-stable');
 }
 
-function isValidPrealpha(version) {
-  return (
+function isValidPrealpha(version /*: Version */) /*: boolean */ {
+  return !!(
     version.major === '0' &&
     version.minor === '0' &&
     version.patch === '0' &&
