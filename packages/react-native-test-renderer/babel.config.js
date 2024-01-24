bfccb093415744faf1ad9c9f4c46e1560fde04df
Jack Pope <jackpope@meta.com>
2024-01-24 12:15:35 -0500

    Create react-native-test-renderer package

---
+++ packages/react-native-test-renderer/babel.config.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+module.exports = {
+  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  plugins: [
+    '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-transform-react-jsx',
+  ],
+};
