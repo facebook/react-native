705c675d51823da7117f799320dba2e5f3dd38bd
Rubén Norte <rubennorte@meta.com>
2024-01-25 13:55:11 -0800

    Implement new feature flag system (#42430)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/42430
    
    This PR creates a new internal feature flags system for React Native. This is only meant to be used internally within the framework, but we might expose it externally in some form in the future to allow customizing specific feature flags in frameworks and applications.
    
    Features:
    * 2 types of flags:
      * Common: can be overridden from native and are accessible from all layers of the stack (Objective-C/Swift, Java/Kotlin, C++ and JavaScript).
      * JS-only: flags that can only be defined and accessed from JS (to allow things like hot reloading without a native build).
    * 1 source of truth for each flag.
    * Feature flags are application/process scoped (using C++ singletons).
    
    See the `README.md` file in this PR for additional information.
    
    This also adds modifies `run-ci-javascript-tests` to run a new check to make sure that the generate files are in sync with the JSON file that contains the definitions.
    
    Changelog: [internal]
    
    Reviewed By: huntie
    
    Differential Revision: D52806730
    
    fbshipit-source-id: 0ba95803f61ec2f05266ee535921321bf6d3dc6a

---
+++ packages/react-native/ReactCommon/react/featureflags/ReactNativeFeatureFlagsDefaults.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<76033e9fb87174da88e0ee922df28701>>
+ */
+
+/**
+ * IMPORTANT: Do NOT modify this file directly.
+ *
+ * To change the definition of the flags, edit
+ *   packages/react-native/scripts/featureflags/ReactNativeFeatureFlags.json.
+ *
+ * To regenerate this code, run the following script from the repo root:
+ *   yarn featureflags-update
+ */
+
+#pragma once
+
+#include <react/featureflags/ReactNativeFeatureFlagsProvider.h>
+
+namespace facebook::react {
+
+class ReactNativeFeatureFlagsDefaults : public ReactNativeFeatureFlagsProvider {
+ public:
+  ReactNativeFeatureFlagsDefaults() = default;
+
+  bool commonTestFlag() override {
+    return false;
+  }
+};
+
+} // namespace facebook::react
---
+++ packages/react-native/scripts/featureflags/templates/js/NativeReactNativeFeatureFlags.cpp-template.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+'use strict';
+
+const {
+  DO_NOT_MODIFY_COMMENT,
+  getCxxTypeFromDefaultValue,
+} = require('../../utils');
+const signedsource = require('signedsource');
+
+module.exports = config =>
+  signedsource.signFile(`/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * ${signedsource.getSigningToken()}
+ */
+
+${DO_NOT_MODIFY_COMMENT}
+
+#include "NativeReactNativeFeatureFlags.h"
+#include <react/featureflags/ReactNativeFeatureFlags.h>
+
+#include "Plugins.h"
+
+std::shared_ptr<facebook::react::TurboModule>
+NativeReactNativeFeatureFlagsModuleProvider(
+    std::shared_ptr<facebook::react::CallInvoker> jsInvoker) {
+  return std::make_shared<facebook::react::NativeReactNativeFeatureFlags>(
+      std::move(jsInvoker));
+}
+
+namespace facebook::react {
+
+NativeReactNativeFeatureFlags::NativeReactNativeFeatureFlags(
+    std::shared_ptr<CallInvoker> jsInvoker)
+    : NativeReactNativeFeatureFlagsCxxSpec(std::move(jsInvoker)) {}
+
+${Object.entries(config.common)
+  .map(
+    ([flagName, flagConfig]) =>
+      `${getCxxTypeFromDefaultValue(
+        flagConfig.defaultValue,
+      )} NativeReactNativeFeatureFlags::${flagName}(
+    jsi::Runtime& /*runtime*/) {
+  return ReactNativeFeatureFlags::${flagName}();
+}`,
+  )
+  .join('\n\n')}
+
+} // namespace facebook::react
+`);
