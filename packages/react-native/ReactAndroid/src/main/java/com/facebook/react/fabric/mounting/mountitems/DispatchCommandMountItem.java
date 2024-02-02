4e6eba7a2dedaa855af0bff5df3bec73a95f0fc4
David Vacca <dvacca@meta.com>
2024-02-02 10:22:02 -0800

    Mark classes of package fabric as @Nullsafe (#42725)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/42725
    
    All these classes are NullSafe, let's mark them as NullSafe(Local) to ensure lint detect errors in the future
    
    changelog: [internal] internal
    
    Reviewed By: NickGerleman
    
    Differential Revision: D53200097
    
    fbshipit-source-id: dad54f5bf03967b5d4126757ab0d5424534af888

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/mountitems/DispatchCommandMountItem.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/mountitems/DispatchCommandMountItem.java
@@ -8,12 +8,14 @@
 package com.facebook.react.fabric.mounting.mountitems;
 
 import androidx.annotation.UiThread;
+import com.facebook.infer.annotation.Nullsafe;
 
 /**
  * This is a common interface for View Command operations. Once we delete the deprecated {@link
  * DispatchIntCommandMountItem}, we can delete this interface too. It provides a set of common
  * operations to simplify generic operations on all types of ViewCommands.
  */
+@Nullsafe(Nullsafe.Mode.LOCAL)
 public abstract class DispatchCommandMountItem implements MountItem {
   private int mNumRetries = 0;
 
--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/mountitems/MountItemFactory.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/mountitems/MountItemFactory.java
@@ -9,11 +9,13 @@ package com.facebook.react.fabric.mounting.mountitems;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import com.facebook.infer.annotation.Nullsafe;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.fabric.events.EventEmitterWrapper;
 import com.facebook.react.uimanager.StateWrapper;
 
 /** Factory class that expose creation of {@link MountItem} */
+@Nullsafe(Nullsafe.Mode.LOCAL)
 public class MountItemFactory {
 
   /** @return a {@link DispatchCommandMountItem} for commands identified by an int */
