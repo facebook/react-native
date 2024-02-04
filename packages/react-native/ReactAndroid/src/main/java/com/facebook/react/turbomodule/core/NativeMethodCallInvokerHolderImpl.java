44e43b7b70ae7cc4860babdd1d548388a5d9dbfe
David Vacca <dvacca@fb.com>
2024-02-04 14:46:27 -0800

    Mark classes of packages turbmodules as @Nullsafe
    Summary:
    All these classes are NullSafe, let's mark them as NullSafe(Local) to ensure lint detect errors in the future
    
    changelog: [internal] internal
    
    Differential Revision: D53393135
    
    fbshipit-source-id: 571c3b03265e2a6b24cc45dc812a153c35a1aed2

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/NativeMethodCallInvokerHolderImpl.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/NativeMethodCallInvokerHolderImpl.java
@@ -7,6 +7,7 @@
 
 package com.facebook.react.turbomodule.core;
 
+import com.facebook.infer.annotation.Nullsafe;
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
 import com.facebook.react.common.annotations.FrameworkAPI;
@@ -19,6 +20,7 @@ import com.facebook.react.turbomodule.core.interfaces.NativeMethodCallInvokerHol
  * class so that we may pass it from CatalystInstance, through Java, to
  * TurboModuleManager::initHybrid.
  */
+@Nullsafe(Nullsafe.Mode.LOCAL)
 @FrameworkAPI
 public class NativeMethodCallInvokerHolderImpl implements NativeMethodCallInvokerHolder {
 
