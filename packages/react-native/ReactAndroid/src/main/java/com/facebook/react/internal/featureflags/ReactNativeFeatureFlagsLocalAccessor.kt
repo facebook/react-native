db1862bca9b83e17b2144cdd438ef2e9c0968a7b
Rubén Norte <rubennorte@fb.com>
2024-02-05 10:24:40 -0800

    Move feature flag definitions to JS file instead of JSON
    Differential Revision: D53351483
    
    fbshipit-source-id: eaa22ba078723ec21dcf30465fb23c91c8139420

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/internal/featureflags/ReactNativeFeatureFlagsLocalAccessor.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/internal/featureflags/ReactNativeFeatureFlagsLocalAccessor.kt
@@ -4,14 +4,14 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<7ba0cb94c10989838250252fa06768a0>>
+ * @generated SignedSource<<77fc4d6577fc0e2e9a0500cc762bba77>>
  */
 
 /**
  * IMPORTANT: Do NOT modify this file directly.
  *
  * To change the definition of the flags, edit
- *   packages/react-native/scripts/featureflags/ReactNativeFeatureFlags.json.
+ *   packages/react-native/scripts/featureflags/ReactNativeFeatureFlags.config.js.
  *
  * To regenerate this code, run the following script from the repo root:
  *   yarn featureflags-update
