b997cd72f4e1635c77fc0da3719b3478afca0db5
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-02-07 13:23:58 +0100

    fix: export XR library from typescript

--- packages/react-native/types/index.d.ts
+++ packages/react-native/types/index.d.ts
@@ -146,6 +146,7 @@ export * from '../Libraries/Utilities/Dimensions';
 export * from '../Libraries/Utilities/PixelRatio';
 export * from '../Libraries/Utilities/Platform';
 export * from '../Libraries/Vibration/Vibration';
+export * from '../Libraries/XR/XR';
 export * from '../Libraries/YellowBox/YellowBoxDeprecated';
 export * from '../Libraries/vendor/core/ErrorUtils';
 export {
