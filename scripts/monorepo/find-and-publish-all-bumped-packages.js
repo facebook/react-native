49c3c3412a4f23adfb641c1c3f19ffc91785c571
Alex Hunt <huntie@meta.com>
2024-02-12 04:42:27 -0800

    Refactor package discovery in publish script (#42938)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/42938
    
    Substitutes the `forEachPackage` util with a replacement async `getPackages` function. This will be used further in the next diff.
    
    The new function aims to be more erganomic/versatile than `forEachPackage` by returning a package mapping (see updated test mock). The API aligns roughly with `yarn workspaces list` and [Lerna's `detectProjects`](https://lerna.js.org/docs/api-reference/utilities#detectprojects).
    
    This also aligns with / replaces similar attempts in our existing scripts:
    - [`getPackagesToPublish`](https://github.com/facebook/react-native/blob/2ca7bec0c2a7d821ceaaf39840a6cdc5eceb8678/scripts/monorepo/get-and-update-packages.js#L56)
    - [`getPublicPackages`](https://github.com/facebook/react-native/blob/2ca7bec0c2a7d821ceaaf39840a6cdc5eceb8678/scripts/releases/set-version/index.js#L19)
    
    Changelog: [Internal]
    
    Reviewed By: cipolleschi
    
    Differential Revision: D53607806
    
    fbshipit-source-id: 00ec34edadab863dc8f2f0c7852f6e835a5dddf5

--- scripts/monorepo/find-and-publish-all-bumped-packages.js
+++ scripts/monorepo/find-and-publish-all-bumped-packages.js
@@ -10,8 +10,8 @@
  */
 
 const {publishPackage} = require('../npm-utils');
+const {getPackages} = require('../releases/utils/monorepo');
 const {PUBLISH_PACKAGES_TAG} = require('./constants');
-const forEachPackage = require('./for-each-package');
 const {execSync, spawnSync} = require('child_process');
 const path = require('path');
 
@@ -40,76 +40,72 @@ async function findAndPublishAllBumpedPackages() {
 
   console.log('Traversing all packages inside /packages...');
 
-  forEachPackage(
-    (packageAbsolutePath, packageRelativePathFromRoot, packageManifest) => {
-      if (packageManifest.private === true) {
-        console.log(`\u23ED Skipping private package ${packageManifest.name}`);
+  const packages = await getPackages({
+    includeReactNative: false,
+  });
 
-        return;
-      }
+  for (const package of Object.values(packages)) {
+    const {stdout: diff, stderr: commitDiffStderr} = spawnSync(
+      'git',
+      [
+        'log',
+        '-p',
+        '--format=""',
+        'HEAD~1..HEAD',
+        `${package.path}/package.json`,
+      ],
+      {cwd: ROOT_LOCATION, shell: true, stdio: 'pipe', encoding: 'utf-8'},
+    );
 
-      const {stdout: diff, stderr: commitDiffStderr} = spawnSync(
-        'git',
-        [
-          'log',
-          '-p',
-          '--format=""',
-          'HEAD~1..HEAD',
-          `${packageRelativePathFromRoot}/package.json`,
-        ],
-        {cwd: ROOT_LOCATION, shell: true, stdio: 'pipe', encoding: 'utf-8'},
-      );
-
-      if (commitDiffStderr) {
-        console.log(
-          `\u274c Failed to get latest committed changes for ${packageManifest.name}:`,
-        );
-        console.log(commitDiffStderr);
-
-        process.exit(1);
-      }
-
-      const previousVersionPatternMatches = diff
-        .toString()
-        .match(/- {2}"version": "([0-9]+.[0-9]+.[0-9]+)"/);
-
-      if (!previousVersionPatternMatches) {
-        console.log(`\uD83D\uDD0E No version bump for ${packageManifest.name}`);
-
-        return;
-      }
-
-      const [, previousVersion] = previousVersionPatternMatches;
-      const nextVersion = packageManifest.version;
-
+    if (commitDiffStderr) {
       console.log(
-        `\uD83D\uDCA1 ${packageManifest.name} was updated: ${previousVersion} -> ${nextVersion}`,
+        `\u274c Failed to get latest committed changes for ${package.name}:`,
       );
+      console.log(commitDiffStderr);
 
-      if (!nextVersion.startsWith('0.')) {
-        throw new Error(
-          `Package version expected to be 0.x.y, but received ${nextVersion}`,
-        );
-      }
+      process.exit(1);
+    }
 
-      const result = publishPackage(packageAbsolutePath, {
-        tags,
-        otp: NPM_CONFIG_OTP,
-      });
-      if (result.code !== 0) {
-        console.log(
-          `\u274c Failed to publish version ${nextVersion} of ${packageManifest.name}. npm publish exited with code ${result.code}:`,
-        );
-        console.log(result.stderr);
+    const previousVersionPatternMatches = diff
+      .toString()
+      .match(/- {2}"version": "([0-9]+.[0-9]+.[0-9]+)"/);
 
-        process.exit(1);
-      } else {
-        console.log(
-          `\u2705 Successfully published new version of ${packageManifest.name}`,
-        );
-      }
-    },
-  );
+    if (!previousVersionPatternMatches) {
+      console.log(`\uD83D\uDD0E No version bump for ${package.name}`);
+
+      return;
+    }
+
+    const [, previousVersion] = previousVersionPatternMatches;
+    const nextVersion = package.packageJson.version;
+
+    console.log(
+      `\uD83D\uDCA1 ${package.name} was updated: ${previousVersion} -> ${nextVersion}`,
+    );
+
+    if (!nextVersion.startsWith('0.')) {
+      throw new Error(
+        `Package version expected to be 0.x.y, but received ${nextVersion}`,
+      );
+    }
+
+    const result = publishPackage(package.path, {
+      tags,
+      otp: NPM_CONFIG_OTP,
+    });
+    if (result.code !== 0) {
+      console.log(
+        `\u274c Failed to publish version ${nextVersion} of ${package.name}. npm publish exited with code ${result.code}:`,
+      );
+      console.log(result.stderr);
+
+      process.exit(1);
+    } else {
+      console.log(
+        `\u2705 Successfully published new version of ${package.name}`,
+      );
+    }
+  }
 }
 
 function getTagsFromCommitMessage(msg /*: string */) /*: Array<string> */ {
