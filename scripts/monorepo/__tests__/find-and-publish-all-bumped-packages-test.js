b25be6687e6533b6cfe094aa704d06fe1146b4bd
Alex Hunt <huntie@meta.com>
2024-02-12 04:42:27 -0800

    Add Flow, add positive test case for monorepo publish step (#42936)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/42936
    
    Changelog: [Internal]
    
    Reviewed By: cipolleschi
    
    Differential Revision: D53607809
    
    fbshipit-source-id: 990826fda5538af9a13e3f24978295a2f3b0c8c3

--- scripts/monorepo/__tests__/find-and-publish-all-bumped-packages-test.js
+++ scripts/monorepo/__tests__/find-and-publish-all-bumped-packages-test.js
@@ -4,7 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict-local
  * @format
+ * @oncall react_native
  */
 
 const {PUBLISH_PACKAGES_TAG} = require('../constants');
@@ -12,18 +14,21 @@ const {
   findAndPublishAllBumpedPackages,
   getTagsFromCommitMessage,
 } = require('../find-and-publish-all-bumped-packages');
-const forEachPackage = require('../for-each-package');
-const {spawnSync} = require('child_process');
 
-jest.mock('child_process', () => ({spawnSync: jest.fn()}));
-jest.mock('../for-each-package', () => jest.fn());
+const spawnSync = jest.fn();
+const forEachPackage = jest.fn();
+const execMock = jest.fn();
+
+jest.mock('child_process', () => ({spawnSync}));
+jest.mock('shelljs', () => ({exec: execMock}));
+jest.mock('../for-each-package', () => forEachPackage);
 
 describe('findAndPublishAllBumpedPackages', () => {
   beforeEach(() => {
-    // Silence logs.
     jest.spyOn(console, 'log').mockImplementation(() => {});
   });
-  it('throws an error if updated version is not 0.x.y', () => {
+
+  test('should throw an error if updated version is not 0.x.y', async () => {
     const mockedPackageNewVersion = '1.0.0';
 
     forEachPackage.mockImplementationOnce(callback => {
@@ -40,9 +45,63 @@ describe('findAndPublishAllBumpedPackages', () => {
       stdout: `This is my commit message\n\n${PUBLISH_PACKAGES_TAG}`,
     }));
 
-    expect(() => findAndPublishAllBumpedPackages()).toThrow(
+    await expect(findAndPublishAllBumpedPackages()).rejects.toThrow(
       `Package version expected to be 0.x.y, but received ${mockedPackageNewVersion}`,
     );
+  });
+
+  test('should publish all changed packages', async () => {
+    forEachPackage.mockImplementationOnce(callback => {
+      callback('absolute/path/to/package-a', 'to/package-a', {
+        version: '0.72.1',
+      });
+      callback('absolute/path/to/package-b', 'to/package-b', {
+        version: '0.72.1',
+      });
+      callback('absolute/path/to/package-c', 'to/package-b', {
+        version: '0.72.0',
+      });
+    });
+
+    spawnSync.mockImplementationOnce(() => ({
+      stdout: `-  "version": "0.72.0"\n+  "version": "0.72.1"\n`,
+    }));
+    spawnSync.mockImplementationOnce(() => ({
+      stdout: `This is my commit message\n\n${PUBLISH_PACKAGES_TAG}`,
+    }));
+    spawnSync.mockImplementationOnce(() => ({
+      stdout: `-  "version": "0.72.0"\n+  "version": "0.72.1"\n`,
+    }));
+    spawnSync.mockImplementationOnce(() => ({
+      stdout: `This is my commit message\n\n${PUBLISH_PACKAGES_TAG}`,
+    }));
+    spawnSync.mockImplementationOnce(() => ({
+      stdout: '\n',
+    }));
+    spawnSync.mockImplementationOnce(() => ({
+      stdout: `This is my commit message\n\n${PUBLISH_PACKAGES_TAG}`,
+    }));
+
+    execMock.mockImplementation(() => ({code: 0}));
+
+    await findAndPublishAllBumpedPackages();
+
+    expect(execMock.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "npm publish",
+          Object {
+            "cwd": "absolute/path/to/package-a",
+          },
+        ],
+        Array [
+          "npm publish",
+          Object {
+            "cwd": "absolute/path/to/package-b",
+          },
+        ],
+      ]
+    `);
   });
 });
 
