1e49f93b19fcaa1a2a644e592f9b693df0ec6103
Nicola Corti <ncor@meta.com>
2024-02-13 11:29:13 -0800

    Fix autolinking for local app Fabric components
    Summary:
    Autolinking local app fabric component requires user to manipulate the C++ code.
    This removes this requirement by generating the code necessary to register all the discovered Fabric Components.
    
    I've updated the RN-Tester Android setup to use this mechanism also.
    
    Changelog:
    [Android] [Fixed] - Fix autolinking for local app Fabric components
    
    Reviewed By: cipolleschi
    
    Differential Revision: D53710676
    
    fbshipit-source-id: 667af4bcf7fa99563081330aa64d072faf50863b

--- packages/react-native/ReactAndroid/cmake-utils/default-app-setup/OnLoad.cpp
+++ packages/react-native/ReactAndroid/cmake-utils/default-app-setup/OnLoad.cpp
@@ -36,6 +36,9 @@
 #ifdef REACT_NATIVE_APP_CODEGEN_HEADER
 #include REACT_NATIVE_APP_CODEGEN_HEADER
 #endif
+#ifdef REACT_NATIVE_APP_COMPONENT_DESCRIPTORS_HEADER
+#include REACT_NATIVE_APP_COMPONENT_DESCRIPTORS_HEADER
+#endif
 
 namespace facebook::react {
 
@@ -47,7 +50,12 @@ void registerComponents(
   // providerRegistry->add(concreteComponentDescriptorProvider<
   //        MyComponentDescriptor>());
 
-  // By default we just use the components autolinked by RN CLI
+  // We link app local components if available
+#ifdef REACT_NATIVE_APP_COMPONENT_REGISTRATION
+  REACT_NATIVE_APP_COMPONENT_REGISTRATION(registry);
+#endif
+
+  // And we fallback to the components autolinked by RN CLI
   rncli_registerProviders(registry);
 }
 
