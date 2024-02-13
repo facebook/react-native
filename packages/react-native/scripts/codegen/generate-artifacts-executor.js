46d7f8a66925ebfad9c8474028fcb69e9735def4
Riccardo Cipolleschi <cipolleschi@meta.com>
2024-02-13 06:18:54 -0800

    Generate code to allow for linking custom Module that conforms to some protocols (#42923)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/42923
    
    This Diff implement the logic to:
    - Read some lists of class names provided by libraries that conforms to some protocols we defined as extension points.
    - Generate a provider in the React-Codegen podspec, whose code lives alongside the app code.
    - Glue the app and the generated code together, allowing to link custom protocols
    
    ## Changelog
    [iOS][Added] - Allow libraries to provide module which conforms to protocols meant to be extension points.
    
    Reviewed By: RSNara
    
    Differential Revision: D53441411
    
    fbshipit-source-id: f53bc6ea0417e6122d8918df2614bcb9937a515a

--- packages/react-native/scripts/codegen/generate-artifacts-executor.js
+++ packages/react-native/scripts/codegen/generate-artifacts-executor.js
@@ -60,6 +60,22 @@ const CORE_LIBRARIES_WITH_OUTPUT_FOLDER = {
 };
 const REACT_NATIVE = 'react-native';
 
+const MODULES_PROTOCOLS_H_TEMPLATE_PATH = path.join(
+  REACT_NATIVE_PACKAGE_ROOT_FOLDER,
+  'scripts',
+  'codegen',
+  'templates',
+  'RCTModulesConformingToProtocolsProviderH.template',
+);
+
+const MODULES_PROTOCOLS_MM_TEMPLATE_PATH = path.join(
+  REACT_NATIVE_PACKAGE_ROOT_FOLDER,
+  'scripts',
+  'codegen',
+  'templates',
+  'RCTModulesConformingToProtocolsProviderMM.template',
+);
+
 // HELPERS
 
 function pkgJsonIncludesGeneratedCode(pkgJson) {
@@ -503,6 +519,52 @@ function findCodegenEnabledLibraries(pkgJson, projectRoot) {
   }
 }
 
+function generateCustomURLHandlers(libraries, outputDir) {
+  const customImageURLLoaderClasses = libraries
+    .flatMap(
+      library =>
+        library?.config?.ios?.modulesConformingToProtocol?.RCTImageURLLoader,
+    )
+    .filter(Boolean)
+    .map(className => `@"${className}"`)
+    .join(',\n\t\t');
+
+  const customImageDataDecoderClasses = libraries
+    .flatMap(
+      library =>
+        library?.config?.ios?.modulesConformingToProtocol?.RCTImageDataDecoder,
+    )
+    .filter(Boolean)
+    .map(className => `@"${className}"`)
+    .join(',\n\t\t');
+
+  const customURLHandlerClasses = libraries
+    .flatMap(
+      library =>
+        library?.config?.ios?.modulesConformingToProtocol?.RCTURLRequestHandler,
+    )
+    .filter(Boolean)
+    .map(className => `@"${className}"`)
+    .join(',\n\t\t');
+
+  const template = fs.readFileSync(MODULES_PROTOCOLS_MM_TEMPLATE_PATH, 'utf8');
+  const finalMMFile = template
+    .replace(/{imageURLLoaderClassNames}/, customImageURLLoaderClasses)
+    .replace(/{imageDataDecoderClassNames}/, customImageDataDecoderClasses)
+    .replace(/{requestHandlersClassNames}/, customURLHandlerClasses);
+
+  fs.writeFileSync(
+    path.join(outputDir, 'RCTModulesConformingToProtocolsProvider.mm'),
+    finalMMFile,
+  );
+
+  const templateH = fs.readFileSync(MODULES_PROTOCOLS_H_TEMPLATE_PATH, 'utf8');
+  fs.writeFileSync(
+    path.join(outputDir, 'RCTModulesConformingToProtocolsProvider.h'),
+    templateH,
+  );
+}
+
 // It removes all the empty files and empty folders
 // it finds, starting from `filepath`, recursively.
 //
@@ -615,6 +677,8 @@ function execute(projectRoot, targetPlatform, baseOutputPath) {
 
         createComponentProvider(schemas, supportedApplePlatforms);
       }
+
+      generateCustomURLHandlers(libraries, outputPath);
       cleanupEmptyFilesAndFolders(outputPath);
     }
   } catch (err) {
