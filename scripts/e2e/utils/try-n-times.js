2a6a9820c8e82805d2f1fb33e7be53017ab8050d
Alex Hunt <huntie@meta.com>
2024-02-15 13:50:23 -0800

    Relocate run-ci-e2e-tests script, add Flow (#43053)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/43053
    
    - Relocate under `scripts/e2e/` (also move util used only by this cript).
    - Type as Flow (to catch trivial errors). Some cleanup of `log()` calls as errors.
    
    Changelog: [Internal]
    
    Reviewed By: lunaleaps
    
    Differential Revision: D53813023

--- scripts/e2e/utils/try-n-times.js
+++ scripts/e2e/utils/try-n-times.js
@@ -4,20 +4,20 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow
  * @format
+ * @oncall react_native
  */
-
-'use strict';
 
 /**
- * Try executing a function n times recursively.
- * Return 0 the first time it succeeds
- * Return code of the last failed commands if not more retries left
- * @funcToRetry - function that gets retried
- * @retriesLeft - number of retries to execute funcToRetry
- * @onEveryError - func to execute if funcToRetry returns non 0
+ * Try executing a function n times recursively. Logs a warning message between
+ * each retry.
  */
-function tryExecNTimes(funcToRetry, retriesLeft, onEveryError) {
+function tryExecNTimes(
+  funcToRetry /*: () => number */,
+  retriesLeft /*: number */,
+  onEveryError /*: ?(() => mixed) */,
+) /*: number */ {
   const exitCode = funcToRetry();
   if (exitCode === 0) {
     return exitCode;
