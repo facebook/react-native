65c394f33ac4a0f61e65c4825d2a823e924f20f5
Rubén Norte <rubennorte@meta.com>
2024-02-28 09:53:18 -0800

    Register ReactHostImpl with modern CDP backend
    Summary:
    Changelog: [Internal]
    
    This implements the integration of `ReactHost` with the modern CDP backend. It handles the registration of pages in CDP when we create new instances of `ReactHost` (which is the equivalent concept in React Native).
    
    The next PR will handle the registration of `ReactInstance` to complete the integration on bridgeless.
    
    Reviewed By: huntie
    
    Differential Revision: D51459049
    
    fbshipit-source-id: 9576c8d8e38ca925035d90f2871bcd7aa534f725

---
+++ packages/react-native/ReactAndroid/src/main/jni/react/runtime/jni/JReactHostInspectorTarget.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "JReactHostInspectorTarget.h"
+#include <fbjni/NativeRunnable.h>
+#include <jsinspector-modern/InspectorFlags.h>
+
+using namespace facebook::jni;
+using namespace facebook::react::jsinspector_modern;
+
+namespace facebook::react {
+JReactHostInspectorTarget::JReactHostInspectorTarget(
+    alias_ref<JReactHostImpl::javaobject> reactHostImpl,
+    alias_ref<JExecutor::javaobject> executor)
+    : javaReactHostImpl_(make_global(reactHostImpl)),
+      javaExecutor_(make_global(executor)) {
+  auto& inspectorFlags = InspectorFlags::getInstance();
+  if (inspectorFlags.getEnableModernCDPRegistry()) {
+    inspectorTarget_ = HostTarget::create(
+        *this,
+        [javaExecutor =
+             javaExecutor_](std::function<void()>&& callback) mutable {
+          auto jrunnable =
+              JNativeRunnable::newObjectCxxArgs(std::move(callback));
+          javaExecutor->execute(jrunnable);
+        });
+
+    inspectorPageId_ = getInspectorInstance().addPage(
+        "React Native Bridgeless (Experimental)",
+        /* vm */ "",
+        [inspectorTargetWeak = std::weak_ptr(inspectorTarget_)](
+            std::unique_ptr<IRemoteConnection> remote)
+            -> std::unique_ptr<ILocalConnection> {
+          if (auto inspectorTarget = inspectorTargetWeak.lock()) {
+            return inspectorTarget->connect(
+                std::move(remote),
+                {
+                    .integrationName = "Android Bridgeless (ReactHostImpl)",
+                });
+          }
+          // Reject the connection.
+          return nullptr;
+        },
+        {.nativePageReloads = true});
+  }
+}
+
+JReactHostInspectorTarget::~JReactHostInspectorTarget() {
+  if (inspectorPageId_.has_value()) {
+    getInspectorInstance().removePage(*inspectorPageId_);
+  }
+}
+
+local_ref<JReactHostInspectorTarget::jhybriddata>
+JReactHostInspectorTarget::initHybrid(
+    alias_ref<JReactHostInspectorTarget::jhybridobject> self,
+    jni::alias_ref<JReactHostImpl::javaobject> reactHostImpl,
+    jni::alias_ref<JExecutor::javaobject> executor) {
+  return makeCxxInstance(reactHostImpl, executor);
+}
+
+void JReactHostInspectorTarget::registerNatives() {
+  registerHybrid({
+      makeNativeMethod("initHybrid", JReactHostInspectorTarget::initHybrid),
+  });
+}
+
+void JReactHostInspectorTarget::onReload(const PageReloadRequest& request) {
+  javaReactHostImpl_->reload("CDP Page.reload");
+}
+
+HostTarget* JReactHostInspectorTarget::getInspectorTarget() {
+  return inspectorTarget_ ? inspectorTarget_.get() : nullptr;
+}
+} // namespace facebook::react
