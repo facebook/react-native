93a9270f14c384ab93e212f738871c710145d003
Riccardo Cipolleschi <cipolleschi@meta.com>
2024-03-04 06:25:03 -0800

    Move the generation of module registration in its own file
    Summary:
    This change is a refactor that moves a generation function in its own file, adding flow types.
    This allows to incremental typing the scripts plus simplify them, hiding implementation details in dedicated files.
    
    ## Changelog:
    [Internal] - Move generation of module registration for custom protocol in its own file + add flow types
    
    Differential Revision: D54414403

--- packages/react-native/scripts/codegen/generate-artifacts-executor.js
+++ packages/react-native/scripts/codegen/generate-artifacts-executor.js
@@ -17,6 +17,7 @@
  */
 
 const utils = require('./codegen-utils');
+const {generateCustomURLHandlers} = require('./generate-modules-protocols.js');
 const generateSpecsCLIExecutor = require('./generate-specs-cli-executor');
 const {execSync} = require('child_process');
 const fs = require('fs');
@@ -60,22 +61,6 @@ const CORE_LIBRARIES_WITH_OUTPUT_FOLDER = {
   },
 };
 const REACT_NATIVE = 'react-native';
-
-const MODULES_PROTOCOLS_H_TEMPLATE_PATH = path.join(
-  REACT_NATIVE_PACKAGE_ROOT_FOLDER,
-  'scripts',
-  'codegen',
-  'templates',
-  'RCTModulesConformingToProtocolsProviderH.template',
-);
-
-const MODULES_PROTOCOLS_MM_TEMPLATE_PATH = path.join(
-  REACT_NATIVE_PACKAGE_ROOT_FOLDER,
-  'scripts',
-  'codegen',
-  'templates',
-  'RCTModulesConformingToProtocolsProviderMM.template',
-);
 
 const COMPONENTS_MAPPING_H_TEMPLATE_PATH = path.join(
   REACT_NATIVE_PACKAGE_ROOT_FOLDER,
@@ -554,52 +539,6 @@ function findCodegenEnabledLibraries(pkgJson, projectRoot) {
       ...findLibrariesFromReactNativeConfig(projectRoot),
     ];
   }
-}
-
-function generateCustomURLHandlers(libraries, outputDir) {
-  const customImageURLLoaderClasses = libraries
-    .flatMap(
-      library =>
-        library?.config?.ios?.modulesConformingToProtocol?.RCTImageURLLoader,
-    )
-    .filter(Boolean)
-    .map(className => `@"${className}"`)
-    .join(',\n\t\t');
-
-  const customImageDataDecoderClasses = libraries
-    .flatMap(
-      library =>
-        library?.config?.ios?.modulesConformingToProtocol?.RCTImageDataDecoder,
-    )
-    .filter(Boolean)
-    .map(className => `@"${className}"`)
-    .join(',\n\t\t');
-
-  const customURLHandlerClasses = libraries
-    .flatMap(
-      library =>
-        library?.config?.ios?.modulesConformingToProtocol?.RCTURLRequestHandler,
-    )
-    .filter(Boolean)
-    .map(className => `@"${className}"`)
-    .join(',\n\t\t');
-
-  const template = fs.readFileSync(MODULES_PROTOCOLS_MM_TEMPLATE_PATH, 'utf8');
-  const finalMMFile = template
-    .replace(/{imageURLLoaderClassNames}/, customImageURLLoaderClasses)
-    .replace(/{imageDataDecoderClassNames}/, customImageDataDecoderClasses)
-    .replace(/{requestHandlersClassNames}/, customURLHandlerClasses);
-
-  fs.writeFileSync(
-    path.join(outputDir, 'RCTModulesConformingToProtocolsProvider.mm'),
-    finalMMFile,
-  );
-
-  const templateH = fs.readFileSync(MODULES_PROTOCOLS_H_TEMPLATE_PATH, 'utf8');
-  fs.writeFileSync(
-    path.join(outputDir, 'RCTModulesConformingToProtocolsProvider.h'),
-    templateH,
-  );
 }
 
 function generateLocalComponentProvider(libraries, outputDir) {
