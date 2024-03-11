b41849880722904649a91302ddedb6d10c0bb58d
Distiller <distiller@static.38.23.38.197.cyberlynk.net>
2024-03-11 15:52:02 +0000

    [0.72.12] Bump version numbers

--- packages/react-native/React/Base/RCTVersion.m
+++ packages/react-native/React/Base/RCTVersion.m
@@ -23,7 +23,7 @@ NSDictionary* RCTGetReactNativeVersion(void)
     __rnVersion = @{
                   RCTVersionMajor: @(0),
                   RCTVersionMinor: @(72),
-                  RCTVersionPatch: @(11),
+                  RCTVersionPatch: @(12),
                   RCTVersionPrerelease: [NSNull null],
                   };
   });
