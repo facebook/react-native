f2f62cdf54b5aff565466fd340685d88bb975bac
Jane Li <janeli@meta.com>
2024-03-15 13:37:22 -0700

    Back out "Add onUserLeaveHint support to ReactActivityDelegate"
    Summary:
    This is causing Twilight Android app to hang and crash on start
    
    Original commit changeset: 3f24271405f6
    
    Original Phabricator Diff: D54905564
    
    Reviewed By: sammy-SC
    
    Differential Revision: D54960053
    
    fbshipit-source-id: 5c1063f11ae1314e71288905eb6d8da32ddbfadd

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactActivity.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactActivity.java
@@ -106,12 +106,6 @@ public abstract class ReactActivity extends AppCompatActivity
   }
 
   @Override
-  public void onUserLeaveHint() {
-    super.onUserLeaveHint();
-    mDelegate.onUserLeaveHint();
-  }
-
-  @Override
   public void requestPermissions(
       String[] permissions, int requestCode, PermissionListener listener) {
     mDelegate.requestPermissions(permissions, requestCode, listener);
