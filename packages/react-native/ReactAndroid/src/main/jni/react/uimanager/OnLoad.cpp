e1742fdfb9c2e4f2612f7fb1d635f954822790de
Pieter De Baets <pieterdb@fb.com>
2024-03-26 04:00:35 -0700

    Make ComponentNameResolverManager a static method
    Differential Revision: D55241234

--- packages/react-native/ReactAndroid/src/main/jni/react/uimanager/OnLoad.cpp
+++ packages/react-native/ReactAndroid/src/main/jni/react/uimanager/OnLoad.cpp
@@ -7,12 +7,12 @@
 
 #include <fbjni/fbjni.h>
 
-#include "ComponentNameResolverManager.h"
+#include "ComponentNameResolverBinding.h"
 #include "UIConstantsProviderManager.h"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
   return facebook::jni::initialize(vm, [] {
-    facebook::react::ComponentNameResolverManager::registerNatives();
+    facebook::react::ComponentNameResolverBinding::registerNatives();
     facebook::react::UIConstantsProviderManager::registerNatives();
   });
 }
