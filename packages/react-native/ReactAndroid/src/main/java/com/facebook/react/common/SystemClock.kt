1cacf6b1fc1d078d277dc45cfd90405fe344e001
Ruslan Shestopalyuk <rshest@meta.com>
2024-03-31 03:26:38 -0700

    Kotlin conv
    Differential Revision: D55568894

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/common/SystemClock.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.common
+
+import android.os.SystemClock
+
+/**
+ * Detour for System.currentTimeMillis and System.nanoTime calls so that they can be mocked out in
+ * tests.
+ */
+public object SystemClock {
+  @JvmStatic
+  public fun currentTimeMillis(): Long {
+    return System.currentTimeMillis()
+  }
+
+  @JvmStatic
+  public fun nanoTime(): Long {
+    return System.nanoTime()
+  }
+
+  @JvmStatic
+  public fun uptimeMillis(): Long {
+    return SystemClock.uptimeMillis()
+  }
+}
