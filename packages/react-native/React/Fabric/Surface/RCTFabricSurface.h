657fbc94569a73c9345aea670a98293d52667bc3
admirsaheta <admirxsaheta@gmail.com>
2024-03-31 22:12:05 +0200

    update:fabric-surface

--- packages/react-native/React/Fabric/Surface/RCTFabricSurface.h
+++ packages/react-native/React/Fabric/Surface/RCTFabricSurface.h
@@ -34,79 +34,64 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface RCTFabricSurface : NSObject <RCTSurfaceProtocol>
 
+/**
+ * Initializes a new RCTFabricSurface instance.
+ *
+ * @param surfacePresenter The surface presenter responsible for managing this surface.
+ * @param moduleName The name of the module associated with this surface.
+ * @param initialProperties The initial properties to set for this surface.
+ * @return An initialized RCTFabricSurface instance.
+ */
 - (instancetype)initWithSurfacePresenter:(RCTSurfacePresenter *)surfacePresenter
                               moduleName:(NSString *)moduleName
                        initialProperties:(NSDictionary *)initialProperties;
 
-#pragma mark - Surface presenter
-
 /**
  * EXPERIMENTAL
- * Reset's the Surface to it's initial stage.
- * It uses the passed in surface presenter, and whatever else was passed in init.
+ * Resets the surface to its initial stage.
+ *
+ * @param surfacePresenter The surface presenter to use for the reset.
  */
 - (void)resetWithSurfacePresenter:(RCTSurfacePresenter *)surfacePresenter;
 
-#pragma mark - Dealing with UIView representation, the Main thread only access
-
 /**
- * Creates (if needed) and returns `UIView` instance which represents the Surface.
+ * Creates (if needed) and returns a UIView instance which represents the Surface.
  * The Surface will cache and *retain* this object.
  * Returning the UIView instance does not mean that the Surface is ready
  * to execute and layout. It can be just a handler which Surface will use later
  * to mount the actual views.
  * RCTSurface does not control (or influence in any way) the size or origin
  * of this view. Some superview (or another owner) must use other methods
- * of this class to setup proper layout and interop interactions with UIKit
+ * of this class to set up proper layout and interop interactions with UIKit
  * or another UI framework.
- * This method must be called only from the main queue.
+ *
+ * @return The UIView instance representing the Surface.
  */
 - (RCTSurfaceView *)view;
 
-#pragma mark - Layout: Setting the size constrains
-
 /**
- * Previously set `minimumSize` layout constraint.
- * Defaults to `{0, 0}`.
+ * Sets the size constraints for the Surface.
+ *
+ * @param minimumSize The minimum size constraint.
+ * @param maximumSize The maximum size constraint.
  */
-@property (atomic, assign, readonly) CGSize minimumSize;
-
-/**
- * Previously set `maximumSize` layout constraint.
- * Defaults to `{CGFLOAT_MAX, CGFLOAT_MAX}`.
- */
-@property (atomic, assign, readonly) CGSize maximumSize;
-
-/**
- * Previously set `viewportOffset` layout constraint.
- * Defaults to `{0, 0}`.
- */
-@property (atomic, assign, readonly) CGPoint viewportOffset;
-
-/**
- * Simple shortcut to `-[RCTSurface setMinimumSize:size maximumSize:size]`.
- */
-- (void)setSize:(CGSize)size;
-
-#pragma mark - Layout: Measuring
+- (void)setMinimumSize:(CGSize)minimumSize maximumSize:(CGSize)maximumSize;
 
 /**
  * Measures the Surface with given constraints.
- * This method does not cause any side effects on the surface object.
+ *
+ * @param minimumSize The minimum size constraint.
+ * @param maximumSize The maximum size constraint.
+ * @return The size that fits within the given constraints.
  */
 - (CGSize)sizeThatFitsMinimumSize:(CGSize)minimumSize maximumSize:(CGSize)maximumSize;
 
 /**
- * Return the current size of the root view based on (but not clamp by) current
- * size constraints.
- */
-@property (atomic, assign, readonly) CGSize intrinsicSize;
-
-#pragma mark - Synchronous waiting
-
-/**
- * Synchronously blocks the current thread up to given `timeout` until
+ * Synchronously blocks the current thread up to the given timeout until
  * the Surface is rendered.
+ *
+ * @param timeout The maximum time interval to wait for rendering.
+ * @return YES if rendering completed within the timeout, otherwise NO.
  */
 - (BOOL)synchronouslyWaitFor:(NSTimeInterval)timeout;
 
@@ -114,6 +99,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RCTFabricSurface (Internal)
 
+/**
+ * Retrieves the SurfaceHandler associated with this Surface.
+ *
+ * @return The SurfaceHandler instance.
+ */
 - (const facebook::react::SurfaceHandler &)surfaceHandler;
 
 @end
