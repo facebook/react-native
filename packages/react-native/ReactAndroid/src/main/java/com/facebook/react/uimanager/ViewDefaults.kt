e3f87ecda3137104835b07f1aee6d5b03b350ec0
Fabrizio Cucci <fabriziocucci@meta.com>
2024-04-01 03:20:26 -0700

    Kotlinify ViewDefaults.kt
    Summary: As part of the Sustainability Week (see [post](https://fb.workplace.com/groups/251759413609061/permalink/742797531171911/)).
    
    Differential Revision: D55584737

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ViewDefaults.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.uimanager
+
+/** Default property values for Views to be shared between Views and ShadowViews. */
+object ViewDefaults {
+  const val FONT_SIZE_SP = 14.0f
+  const val LINE_HEIGHT = 0
+  const val NUMBER_OF_LINES = Int.MAX_VALUE
+}
