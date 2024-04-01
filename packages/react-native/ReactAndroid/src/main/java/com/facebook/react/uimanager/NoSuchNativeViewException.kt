f3bb93ee83ce2d8ed78920600787aab1ae41291d
Fabrizio Cucci <fabriziocucci@meta.com>
2024-04-01 08:10:13 -0700

    Kotlinify NoSuchNativeViewException (#43739)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/43739
    
    Changelog: [Internal]
    
    As part of the Sustainability Week (see [post](https://fb.workplace.com/groups/251759413609061/permalink/742797531171911/)).
    
    Differential Revision: D55588272

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/NoSuchNativeViewException.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/NoSuchNativeViewException.kt
@@ -5,18 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.uimanager;
-
-import com.facebook.infer.annotation.Nullsafe;
+package com.facebook.react.uimanager
 
 /**
  * Exception thrown when a class tries to access a native view by a tag that has no native view
  * associated with it.
  */
-@Nullsafe(Nullsafe.Mode.LOCAL)
-class NoSuchNativeViewException extends IllegalViewOperationException {
-
-  public NoSuchNativeViewException(String detailMessage) {
-    super(detailMessage);
-  }
-}
+internal class NoSuchNativeViewException(detailMessage: String) :
+    IllegalViewOperationException(detailMessage)
