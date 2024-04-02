001e34ce9920696c09d27c39ce958ae7e1f3a2b2
Nick Gerleman <ngerlem@fb.com>
2024-04-02 05:25:59 -0700

    Decouple CSS background drawing from view, New Commit
    Differential Revision: D55565035

--- packages/react-native/ReactAndroid/src/test/java/com/facebook/react/views/view/ColorUtilTest.kt
+++ packages/react-native/ReactAndroid/src/test/java/com/facebook/react/views/view/ColorUtilTest.kt
@@ -7,7 +7,6 @@
 
 package com.facebook.react.views.view
 
-import android.graphics.PixelFormat
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,27 +15,6 @@ import org.robolectric.RobolectricTestRunner
 /** Based on Fresco's DrawableUtilsTest (https://github.com/facebook/fresco). */
 @RunWith(RobolectricTestRunner::class)
 class ColorUtilTest {
-  @Test
-  fun testMultiplyColorAlpha() {
-    assertEquals(0x00123456U.toInt(), ColorUtil.multiplyColorAlpha(0xC0123456U.toInt(), 0))
-    assertEquals(0x07123456U.toInt(), ColorUtil.multiplyColorAlpha(0xC0123456U.toInt(), 10))
-    assertEquals(0x96123456U.toInt(), ColorUtil.multiplyColorAlpha(0xC0123456U.toInt(), 200))
-    assertEquals(0xC0123456U.toInt(), ColorUtil.multiplyColorAlpha(0xC0123456U.toInt(), 255))
-  }
-
-  @Test
-  fun testGetOpacityFromColor() {
-    assertEquals(PixelFormat.TRANSPARENT, ColorUtil.getOpacityFromColor(0x00000000))
-    assertEquals(PixelFormat.TRANSPARENT, ColorUtil.getOpacityFromColor(0x00123456))
-    assertEquals(PixelFormat.TRANSPARENT, ColorUtil.getOpacityFromColor(0x00FFFFFF))
-    assertEquals(PixelFormat.TRANSLUCENT, ColorUtil.getOpacityFromColor(0xC0000000.toInt()))
-    assertEquals(PixelFormat.TRANSLUCENT, ColorUtil.getOpacityFromColor(0xC0123456.toInt()))
-    assertEquals(PixelFormat.TRANSLUCENT, ColorUtil.getOpacityFromColor(0xC0FFFFFF.toInt()))
-    assertEquals(PixelFormat.OPAQUE, ColorUtil.getOpacityFromColor(0xFF000000.toInt()))
-    assertEquals(PixelFormat.OPAQUE, ColorUtil.getOpacityFromColor(0xFF123456.toInt()))
-    assertEquals(PixelFormat.OPAQUE, ColorUtil.getOpacityFromColor(0xFFFFFFFF.toInt()))
-  }
-
   @Test
   fun testNormalize() {
     assertEquals(0x800B1621U.toInt(), ColorUtil.normalize(11.0, 22.0, 33.0, 0.5))
