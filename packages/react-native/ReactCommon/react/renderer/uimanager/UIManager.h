707082259ee9a9eeb84f46680057471be795b230
Eric Rozell <ericroz@meta.com>
2024-04-02 08:04:55 -0700

    Avoid shared_mutex re-entrancy for setNativeProps_DEPRECATED (#43777)
    Summary:
    
    It's best to avoid mutex reentrancy, even when using "read" locks (i.e., std::shared_lock). Since we already have a read lock on the ShadowTree in `UIManager::setNativeProps_DEPRECATED`, we can avoid the reentrancy by grabbing ancestor node from the shadow tree instead of relying on a recursive call to ShadowTreeRegistry::visit
    
    ## Changelog
    
    [GENERAL][FIXED] - Avoid ShadowTreeRegistry::mutex_ read lock reentrancy
    
    Differential Revision: D55640201

--- packages/react-native/ReactCommon/react/renderer/uimanager/UIManager.h
+++ packages/react-native/ReactCommon/react/renderer/uimanager/UIManager.h
@@ -218,6 +218,10 @@ class UIManager final : public ShadowTreeDelegate {
       const jsi::Value& successCallback,
       const jsi::Value& failureCallback) const;
 
+  ShadowNode::Shared getNewestCloneOfShadowNodeFromAncestor(
+      const ShadowNode& shadowNode,
+      const ShadowNode::Shared& ancestorShadowNode) const;
+
   SharedComponentDescriptorRegistry componentDescriptorRegistry_;
   UIManagerDelegate* delegate_{};
   UIManagerAnimationDelegate* animationDelegate_{nullptr};
