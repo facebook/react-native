10eb7b6b11a45b1e056c9032de0d829c816acd23
Nicola Corti <ncor@meta.com>
2024-04-03 06:06:14 -0700

    Convert several classes inside `com.facebook.react.views.modal` to Kotlin
    Summary:
    This moves a lot of classes from Java to Kotlin from the `package com.facebook.react.views.modal`
    
    Changelog:
    [Internal] [Changed] - Convert several classes inside `com.facebook.react.views.modal` to Kotlin
    
    Differential Revision: D55692067

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/modal/RequestCloseEvent.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.views.modal
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.uimanager.common.ViewUtil
+import com.facebook.react.uimanager.events.Event
+
+/** [Event] for dismissing a Dialog. */
+internal class RequestCloseEvent(surfaceId: Int, viewTag: Int) :
+    Event<RequestCloseEvent>(surfaceId, viewTag) {
+
+  @Deprecated(
+      "Do not use this constructor, use the one with explicit surfaceId",
+      ReplaceWith("ShowEvent(surfaceId, viewTag)"))
+  constructor(viewTag: Int) : this(ViewUtil.NO_SURFACE_ID, viewTag)
+
+  override fun getEventName(): String = EVENT_NAME
+
+  override fun getEventData(): WritableMap = Arguments.createMap()
+
+  companion object {
+    const val EVENT_NAME: String = "topRequestClose"
+  }
+}
