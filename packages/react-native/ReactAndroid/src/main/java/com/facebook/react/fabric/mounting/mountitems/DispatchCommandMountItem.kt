73cf2e26c6740718e6bc96835a10ab98d120f6d5
Fabrizio Cucci <fabriziocucci@fb.com>
2024-04-03 01:06:17 -0700

    Kotlinify DispatchCommandMountItem
    Differential Revision: D55642568

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/mountitems/DispatchCommandMountItem.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/mountitems/DispatchCommandMountItem.kt
@@ -5,27 +5,22 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.fabric.mounting.mountitems;
+package com.facebook.react.fabric.mounting.mountitems
 
-import androidx.annotation.UiThread;
-import com.facebook.infer.annotation.Nullsafe;
+import androidx.annotation.UiThread
 
 /**
  * This is a common interface for View Command operations. Once we delete the deprecated {@link
  * DispatchIntCommandMountItem}, we can delete this interface too. It provides a set of common
  * operations to simplify generic operations on all types of ViewCommands.
  */
-@Nullsafe(Nullsafe.Mode.LOCAL)
-public abstract class DispatchCommandMountItem implements MountItem {
-  private int mNumRetries = 0;
+public abstract class DispatchCommandMountItem : MountItem {
+  private var numRetries: Int = 0
 
   @UiThread
-  public void incrementRetries() {
-    mNumRetries++;
+  public fun incrementRetries() {
+    numRetries++
   }
 
-  @UiThread
-  public int getRetries() {
-    return mNumRetries;
-  }
+  @UiThread public fun getRetries(): Int = numRetries
 }
