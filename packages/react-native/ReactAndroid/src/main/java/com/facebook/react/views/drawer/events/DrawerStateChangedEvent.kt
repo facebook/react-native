1171c7364eb9c3367a035ba00534d5d889b8753a
Fabrizio Cucci <fabriziocucci@meta.com>
2024-04-03 23:26:06 -0700

    Kotlinify DrawerStateChangedEvent (#43835)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/43835
    
    Changelog: [Internal]
    
    As part of the Sustainability Week (see [post](https://fb.workplace.com/groups/251759413609061/permalink/742797531171911/)).
    
    Reviewed By: alanleedev
    
    Differential Revision: D55709997

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/drawer/events/DrawerStateChangedEvent.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.views.drawer.events
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.uimanager.common.ViewUtil
+import com.facebook.react.uimanager.events.Event
+
+public class DrawerStateChangedEvent : Event<DrawerStateChangedEvent> {
+
+  private val drawerState: Int
+
+  @Deprecated(
+      "Use constructor with surfaceId",
+      ReplaceWith("DrawerStateChangedEvent(surfaceId, viewId, drawerState)"))
+  public constructor(
+      viewId: Int,
+      drawerState: Int
+  ) : this(ViewUtil.NO_SURFACE_ID, viewId, drawerState)
+
+  public constructor(surfaceId: Int, viewId: Int, drawerState: Int) : super(surfaceId, viewId) {
+    this.drawerState = drawerState
+  }
+
+  public fun getDrawerState(): Int = drawerState
+
+  override public fun getEventName(): String = EVENT_NAME
+
+  override protected fun getEventData(): WritableMap? {
+    val eventData: WritableMap = Arguments.createMap()
+    eventData.putInt("drawerState", getDrawerState())
+    return eventData
+  }
+
+  public companion object {
+    public const val EVENT_NAME: String = "topDrawerStateChanged"
+  }
+}
