6b98134c684e023f9f2046577914667e8e32169b
Fabrizio Cucci <fabriziocucci@meta.com>
2024-04-03 23:13:45 -0700

    Kotlinify OkHttpCallUtil (#43825)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/43825
    
    Changelog: [Internal]
    
    As part of the Sustainability Week (see [post](https://fb.workplace.com/groups/251759413609061/permalink/742797531171911/)).
    
    Differential Revision: D55707704

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/common/network/OkHttpCallUtil.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.common.network
+
+import okhttp3.OkHttpClient
+
+/**
+ * Helper class that provides the necessary methods for canceling queued and running OkHttp calls
+ */
+public object OkHttpCallUtil {
+
+  @JvmStatic
+  public fun cancelTag(client: OkHttpClient, tag: Any) {
+    for (call in client.dispatcher().queuedCalls()) {
+      if (tag == call.request().tag()) {
+        call.cancel()
+        return
+      }
+    }
+    for (call in client.dispatcher().runningCalls()) {
+      if (tag == call.request().tag()) {
+        call.cancel()
+        return
+      }
+    }
+  }
+}
