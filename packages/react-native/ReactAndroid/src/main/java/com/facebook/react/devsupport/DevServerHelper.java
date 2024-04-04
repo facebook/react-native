b309af79e8bf2eb741db83b00359a73fe1b458a5
Moti Zilberman <moti@meta.com>
2024-04-04 11:19:54 -0700

    Introduce DevSupportManager.openDebugger() method (#43685)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/43685
    
    Changelog: [Changed][Android] Expose `openDebugger()` method on `DevSupportManager`
    
    Exposes the `openDebugger()` method on `DevSupportManager` for ease of integration by RN Android frameworks.
    
    Reviewed By: cortinico, arushikesarwani94
    
    Differential Revision: D55408820
    
    fbshipit-source-id: f06684de47cee23642bf893afaedb2755453f253

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/DevServerHelper.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/DevServerHelper.java
@@ -491,7 +491,7 @@ public class DevServerHelper {
   }
 
   /** Attempt to open the JS debugger on the host machine (on-device CDP debugging). */
-  public void openDebugger(final ReactContext context, final String errorMessage) {
+  public void openDebugger(@Nullable final ReactContext context, final String errorMessage) {
     // TODO(huntie): Requests to dev server should not assume 'http' URL scheme
     String requestUrl =
         String.format(
--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/ReleaseDevSupportManager.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/ReleaseDevSupportManager.java
@@ -204,4 +204,7 @@ public class ReleaseDevSupportManager implements DevSupportManager {
   public @Nullable SurfaceDelegate createSurfaceDelegate(String moduleName) {
     return null;
   }
+
+  @Override
+  public void openDebugger() {}
 }
