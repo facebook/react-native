e04a78d023fd59d3ce4b6566e17be3b30c36edf1
Fabrizio Cucci <fabriziocucci@fb.com>
2024-04-05 08:52:07 -0700

    Kotlinify WindowFocusChangeListener
    Differential Revision: D55794517

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/WindowFocusChangeListener.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/WindowFocusChangeListener.kt
@@ -5,11 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.bridge;
+package com.facebook.react.bridge
 
 /*
  * Listener for receiving window focus events.
  */
 public interface WindowFocusChangeListener {
-  void onWindowFocusChange(boolean hasFocus);
+  public fun onWindowFocusChange(hasFocus: Boolean): Unit
 }
