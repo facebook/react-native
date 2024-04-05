6d57ba091d16eb3625c4c7141a9281fa0faf79a1
Andrew Datsenko <asd@meta.com>
2024-04-05 08:29:29 -0700

    //xplat/js/react-native-github/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/reactperflogger:reactperfloggerAndroid (#43872)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/43872
    
    Changelog: [Internal]
    
    Reviewed By: cortinico
    
    Differential Revision: D55725695

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/reactperflogger/NativeModulePerfLogger.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.perflogger
+
+import com.facebook.jni.HybridData
+import com.facebook.soloader.SoLoader
+
+public abstract class NativeModulePerfLogger protected constructor() {
+
+  @Suppress("NoHungarianNotation") private val mHybridData: HybridData
+
+  init {
+    maybeLoadOtherSoLibraries()
+    maybeLoadSoLibrary()
+    mHybridData = initHybrid()
+  }
+
+  protected abstract fun initHybrid(): HybridData
+
+  public abstract fun moduleDataCreateStart(moduleName: String, id: Int)
+
+  public abstract fun moduleDataCreateEnd(moduleName: String, id: Int)
+
+  public abstract fun moduleCreateStart(moduleName: String, id: Int)
+
+  public abstract fun moduleCreateCacheHit(moduleName: String, id: Int)
+
+  public abstract fun moduleCreateConstructStart(moduleName: String, id: Int)
+
+  public abstract fun moduleCreateConstructEnd(moduleName: String, id: Int)
+
+  public abstract fun moduleCreateSetUpStart(moduleName: String, id: Int)
+
+  public abstract fun moduleCreateSetUpEnd(moduleName: String, id: Int)
+
+  public abstract fun moduleCreateEnd(moduleName: String, id: Int)
+
+  public abstract fun moduleCreateFail(moduleName: String, id: Int)
+
+  /** Subclasses will override this method to load their own SO libraries. */
+  @Synchronized
+  protected fun maybeLoadOtherSoLibraries() {
+    // No-op by default.
+  }
+
+  private companion object {
+    @Volatile private var isSoLibraryLoaded = false
+
+    // Prevents issues with initializer interruptions. See T38996825 and D13793825 for more context.
+    @Synchronized
+    private fun maybeLoadSoLibrary() {
+      if (!isSoLibraryLoaded) {
+        SoLoader.loadLibrary("reactperfloggerjni")
+        isSoLibraryLoaded = true
+      }
+    }
+  }
+}
