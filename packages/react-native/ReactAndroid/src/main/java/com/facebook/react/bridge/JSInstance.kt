a3332daf9cde1976635ba02f9690032dcea2bb37
Fabrizio Cucci <fabriziocucci@fb.com>
2024-04-05 05:16:25 -0700

    Kotlinify JSInstance
    Differential Revision: D55791330

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/JSInstance.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/JSInstance.kt
@@ -5,14 +5,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.bridge;
+package com.facebook.react.bridge
 
 /**
  * This interface includes the methods needed to use a running JS instance, without specifying any
  * of the bridge-specific initialization or lifecycle management.
  */
-interface JSInstance {
-  void invokeCallback(int callbackID, NativeArrayInterface arguments);
+internal interface JSInstance {
+  public fun invokeCallback(callbackID: Int, arguments: NativeArrayInterface)
+
   // TODO if this interface survives refactoring, think about adding
   // callFunction.
 }
