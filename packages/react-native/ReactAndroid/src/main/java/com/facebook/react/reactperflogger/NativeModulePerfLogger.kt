9f0f52be930576e66d1dc11602a72bb60b4b1bc3
Andrew Datsenko <asd@meta.com>
2024-04-08 08:06:53 -0700

    Update package com.facebook.react.perflogger -> com.facebook.react.reactperflogger (#43873)
    Summary:
    
    Changelog: [Internal]
    Addressing lint for package name com.facebook.react.perflogger
    
    Reviewed By: alanleedev
    
    Differential Revision: D55747967

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/reactperflogger/NativeModulePerfLogger.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/reactperflogger/NativeModulePerfLogger.kt
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.perflogger
+package com.facebook.react.reactperflogger
 
 import com.facebook.jni.HybridData
 import com.facebook.soloader.SoLoader
