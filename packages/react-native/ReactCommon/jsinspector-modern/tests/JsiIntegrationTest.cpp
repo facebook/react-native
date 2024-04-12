91d3bc5be63891916d67e9216b1534ddbe8f53fd
Moti Zilberman <moti@meta.com>
2024-04-12 03:10:16 -0700

    Update CDPAgent reentrancy test to cover `evaluate` ordering
    Summary:
    Changelog: [Internal]
    
    With the Hermes fix in D55250610, we're able to make stronger assertions in `CDPAgentReentrancyRegressionTest`, which is an integration test covering a class of related bugs.
    
    bypass-github-export-checks
    
    Reviewed By: mattbfb
    
    Differential Revision: D55962593
    
    fbshipit-source-id: 09d03effc51d6f1904842f1c7c2f7e4407fefc63

--- packages/react-native/ReactCommon/jsinspector-modern/tests/JsiIntegrationTest.cpp
+++ packages/react-native/ReactCommon/jsinspector-modern/tests/JsiIntegrationTest.cpp
@@ -458,8 +458,7 @@ TYPED_TEST(JsiIntegrationHermesTest, ResolveBreakpointAfterReload) {
 TYPED_TEST(JsiIntegrationHermesTest, CDPAgentReentrancyRegressionTest) {
   this->connect();
 
-  // TODO(moti): Add an InSequence guard here once Hermes processes the messages
-  // in the order they were received.
+  InSequence s;
 
   this->inspectorExecutor_([&]() {
     // Tasks scheduled on our executor here will be executed when this lambda
@@ -467,31 +466,32 @@ TYPED_TEST(JsiIntegrationHermesTest, CDPAgentReentrancyRegressionTest) {
     // place the EXPECT_* calls at the end of the lambda body to ensure the
     // test fails if we get eager (unexpected) responses.
 
-    // 1. Cause CDPAgent to schedule a task on the JS executor to process the
-    //    message. The task is simultaneously scheduled as an interrupt on the
-    //    JS VM (but will be called via the executor, since the interpreter is
-    //    idle at the moment).
+    // 1. Cause CDPAgent to schedule a task to process the message. Originally,
+    //    the task would be simultaneously scheduled on the JS executor, and as
+    //    an interrupt on the JS interpreter. It's called via the executor
+    //    regardless, since the interpreter is idle at the moment.
     this->toPage_->sendMessage(R"({
                                    "id": 1,
                                    "method": "Runtime.evaluate",
-                                   "params": {"expression": "1 + 2"}
+                                   "params": {"expression": "Math.random(); /* Interrupts processed here. */ globalThis.x = 1 + 2"}
                                  })");
 
-    // 2. Cause CDPAgent to schedule another task+interrupt.
+    // 2. Cause CDPAgent to schedule another task. If scheduled as an interrupt,
+    //    this task will run _during_ the first task.
     this->toPage_->sendMessage(R"({
                                    "id": 2,
                                    "method": "Runtime.evaluate",
-                                   "params": {"expression": "3 + 4"}
+                                   "params": {"expression": "globalThis.x = 3 + 4"}
                                  })");
 
-    // 3. When the first scheduled task runs (via the executor), it enters the
-    //    interpreter to evaluate the expression, at which point the
-    //    interpreter begins processing interrupts and enters the second task.
-    //    This used to trigger two distinct bugs in CDPAgent:
-    //      - The first task would be triggered twice due to a race condition
-    //        between the executor and the interrupt handler. (D54771697)
-    //      - The second task would deadlock due to the first task holding a
-    //        lock preventing any other CDPAgent tasks from running. (D54838179)
+    //  This setup used to trigger three distinct bugs in CDPAgent:
+    //    - The first task would be triggered twice due to a race condition
+    //      between the executor and the interrupt handler. (D54771697)
+    //    - The second task would deadlock due to the first task holding a lock
+    //      preventing any other CDPAgent tasks from running. (D54838179)
+    //    - The second task would complete first, returning `evaluate`
+    //      responses out of order and (crucially) performing any JS side
+    //      effects out of order. (D55250610)
 
     this->expectMessageFromPage(JsonEq(R"({
                                             "id": 1,
@@ -513,6 +513,9 @@ TYPED_TEST(JsiIntegrationHermesTest, CDPAgentReentrancyRegressionTest) {
                                             }
                                           })"));
   });
+
+  // Make sure the second task ran last.
+  EXPECT_EQ(this->eval("globalThis.x").getNumber(), 7);
 }
 
 TYPED_TEST(JsiIntegrationHermesTest, ScriptParsedExactlyOnce) {
