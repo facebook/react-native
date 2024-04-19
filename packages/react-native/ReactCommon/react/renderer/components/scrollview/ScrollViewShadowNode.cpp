f944ee07f409576b94d9a9eedfd872e91be24f92
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2024-04-19 13:15:05 +0200

    Fix inverted flat list

--- packages/react-native/ReactCommon/react/renderer/components/scrollview/ScrollViewShadowNode.cpp
+++ packages/react-native/ReactCommon/react/renderer/components/scrollview/ScrollViewShadowNode.cpp
@@ -68,7 +68,17 @@ Point ScrollViewShadowNode::getContentOriginOffset(
   auto stateData = getStateData();
   auto contentOffset = stateData.contentOffset;
 
-  return {-contentOffset.x, -contentOffset.y + stateData.scrollAwayPaddingTop};
+  auto props = getConcreteProps();
+
+  float productX = 1.0f;
+  float productY = 1.0f;
+
+  for (const auto& operation : props.transform.operations) {
+    productX *= operation.x;
+    productY *= operation.y;
+  }
+
+  return {-contentOffset.x * productX, (-contentOffset.y + stateData.scrollAwayPaddingTop) * productY};
 }
 
 } // namespace facebook::react
