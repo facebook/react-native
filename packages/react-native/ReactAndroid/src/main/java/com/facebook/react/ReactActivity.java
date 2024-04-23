dec2a7d6850b7f3ba0eb2db4e45180674be0f068
Arushi Kesarwani <arushikesarwani@meta.com>
2024-04-23 16:31:10 -0700

    Fixing exposing ReactDelegate through ReactActivity for reload() (#44227)
    Summary:
    
    In https://github.com/facebook/react-native/pull/44223/ kudo identified the incorrect return type.
    
    Differential Revision: D56497700

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactActivity.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactActivity.java
@@ -65,8 +65,8 @@ public abstract class ReactActivity extends AppCompatActivity
     mDelegate.onDestroy();
   }
 
-  public void getReactDelegate() {
-    mDelegate.getReactDelegate();
+  public ReactDelegate getReactDelegate() {
+    return mDelegate.getReactDelegate();
   }
 
   public ReactActivityDelegate getReactActivityDelegate() {
