c884d19a501081ffbc4b353d751ecc3e32d6f624
Blake Friedman <blakef@meta.com>
2024-04-23 09:50:17 -0700

    Moved @react-native-community script internally into react-native/scripts
    Summary:
    This decouples the listing of modules from the linking of those modules into Cocoapods. I've made this backwards compatible, but our internal template wont lean on the community config.
    
    The user can now override how they capture a list of React Native modules, providing an escape hatch for Framework authors to build on.
    
    Changelog: [General][iOS] Use our fork of the react-native-communti/cli-platform-ios use_native_modues.rb script
    
    Reviewed By: cipolleschi
    
    Differential Revision: D56242486
    
    fbshipit-source-id: 78505669ab6abd6718348388c3bfba3290f7071b

--- packages/react-native/scripts/react_native_pods.rb
+++ packages/react-native/scripts/react_native_pods.rb
@@ -16,24 +16,14 @@ require_relative './cocoapods/new_architecture.rb'
 require_relative './cocoapods/local_podspec_patch.rb'
 require_relative './cocoapods/runtime.rb'
 require_relative './cocoapods/helpers.rb'
+# Importing to expose use_native_modules!
+require_relative './cocoapods/autolinking.rb'
 
 $CODEGEN_OUTPUT_DIR = 'build/generated/ios'
 $CODEGEN_COMPONENT_DIR = 'react/renderer/components'
 $CODEGEN_MODULE_DIR = '.'
 
 $START_TIME = Time.now.to_i
-
-# `@react-native-community/cli-platform-ios/native_modules` defines
-# use_native_modules. We use node to resolve its path to allow for
-# different packager and workspace setups. This is reliant on
-# `@react-native-community/cli-platform-ios` being a direct dependency
-# of `react-native`.
-require Pod::Executable.execute_command('node', ['-p',
-  'require.resolve(
-    "@react-native-community/cli-platform-ios/native_modules.rb",
-    {paths: [process.argv[1]]},
-  )', __dir__]).strip
-
 
 def min_ios_version_supported
   return Helpers::Constants.min_ios_version_supported
