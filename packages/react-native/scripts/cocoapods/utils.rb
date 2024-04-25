794aaa52d39f2c419fcd49126c69dff3030e7bb9
Riccardo Cipolleschi <cipolleschi@meta.com>
2024-04-25 08:28:26 -0700

    Fix enable hermes debugger in Bridgeless mode
    Summary:
    Set the proper build flags for debugging in Bridgeless mode.
    
    This fixes [#44240](https://github.com/facebook/react-native/issues/44240)
    
    ## Changelog:
    [iOS][Fixed] - Add `HERMES_ENABLE_DEBUGGER=1` flag to React-RuntimeApple
    
    Reviewed By: cortinico
    
    Differential Revision: D56575647
    
    fbshipit-source-id: a0613a5d46caeb1d3e636e54ecd43428fbaf46e8

--- packages/react-native/scripts/cocoapods/utils.rb
+++ packages/react-native/scripts/cocoapods/utils.rb
@@ -44,6 +44,7 @@ class ReactNativePodsUtils
     def self.set_gcc_preprocessor_definition_for_React_hermes(installer)
         self.add_build_settings_to_pod(installer, "GCC_PREPROCESSOR_DEFINITIONS", "HERMES_ENABLE_DEBUGGER=1", "React-hermes", "Debug")
         self.add_build_settings_to_pod(installer, "GCC_PREPROCESSOR_DEFINITIONS", "HERMES_ENABLE_DEBUGGER=1", "hermes-engine", "Debug")
+        self.add_build_settings_to_pod(installer, "GCC_PREPROCESSOR_DEFINITIONS", "HERMES_ENABLE_DEBUGGER=1", "React-RuntimeHermes", "Debug")
     end
 
     def self.turn_off_resource_bundle_react_core(installer)
