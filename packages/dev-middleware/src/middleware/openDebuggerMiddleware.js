09683d5102f68d9a615b5e3ba8d1ba2631c5035a
Edmond Chui <edmondc@meta.com>
2024-04-29 09:43:31 -0700

    add fields to launch_debugger_frontend
    Differential Revision: D56610268

--- packages/dev-middleware/src/middleware/openDebuggerMiddleware.js
+++ packages/dev-middleware/src/middleware/openDebuggerMiddleware.js
@@ -153,6 +153,8 @@ export default function openDebuggerMiddleware({
           status: 'success',
           appId: appId ?? null,
           deviceId: device ?? null,
+          resolvedTargetDescription: target.description,
+          prefersFuseboxFrontend: useFuseboxEntryPoint ?? false,
         });
         return;
       } catch (e) {
