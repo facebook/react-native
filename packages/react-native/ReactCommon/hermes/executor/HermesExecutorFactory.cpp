facebook/react-native

commit 7af288e523c899d1ca8b55e21d9ae8939d114d4d

Lazy-init HermesRuntimeTargetDelegate
Summary:
Changelog: [Internal]

We are currently eagerly constructing a `HermesRuntimeTargetDelegate` regardless of whether the Fusebox feature flags are enabled. This can interfere with the legacy CDP backend. Instead, let's lazily construct the target delegate in a code path that only runs when the modern backend is in use.

bypass-github-export-checks

Reviewed By: rozele

Differential Revision: D54907887

fbshipit-source-id: 7dc13506739866ea6690ed21d03d91ad24ef68c5

packages/react-native/ReactCommon/hermes/executor/HermesExecutorFactory.cpp
@@ -253,12 +253,14 @@ HermesExecutor::HermesExecutor(
     HermesRuntime& hermesRuntime)
     : JSIExecutor(runtime, delegate, timeoutInvoker, runtimeInstaller),
       runtime_(runtime),
-      targetDelegate_{
-          std::shared_ptr<HermesRuntime>(runtime_, &hermesRuntime)} {}
+      hermesRuntime_(runtime_, &hermesRuntime) {}
 
 jsinspector_modern::RuntimeTargetDelegate&
 HermesExecutor::getRuntimeTargetDelegate() {
-  return targetDelegate_;
+  if (!targetDelegate_) {
+    targetDelegate_.emplace(hermesRuntime_);
+  }
+  return *targetDelegate_;
 }
 
 } // namespace facebook::react

packages/react-native/ReactCommon/hermes/executor/HermesExecutorFactory.h
@@ -61,7 +61,9 @@ class HermesExecutor : public JSIExecutor {
  private:
   JSIScopedTimeoutInvoker timeoutInvoker_;
   std::shared_ptr<jsi::Runtime> runtime_;
-  jsinspector_modern::HermesRuntimeTargetDelegate targetDelegate_;
+  std::shared_ptr<hermes::HermesRuntime> hermesRuntime_;
+  std::optional<jsinspector_modern::HermesRuntimeTargetDelegate>
+      targetDelegate_;
 };
 
 } // namespace facebook::react

packages/react-native/ReactCommon/react/runtime/hermes/HermesInstance.cpp
@@ -95,7 +95,7 @@ class DecoratedRuntime : public jsi::RuntimeDecorator<jsi::Runtime> {
 class HermesJSRuntime : public JSRuntime {
  public:
   HermesJSRuntime(std::unique_ptr<HermesRuntime> runtime)
-      : runtime_(std::move(runtime)), targetDelegate_{runtime_} {}
+      : runtime_(std::move(runtime)) {}
 
   jsi::Runtime& getRuntime() noexcept override {
     return *runtime_;
@@ -103,12 +103,16 @@ class HermesJSRuntime : public JSRuntime {
 
   jsinspector_modern::RuntimeTargetDelegate& getRuntimeTargetDelegate()
       override {
-    return targetDelegate_;
+    if (!targetDelegate_) {
+      targetDelegate_.emplace(runtime_);
+    }
+    return *targetDelegate_;
   }
 
  private:
   std::shared_ptr<HermesRuntime> runtime_;
-  jsinspector_modern::HermesRuntimeTargetDelegate targetDelegate_;
+  std::optional<jsinspector_modern::HermesRuntimeTargetDelegate>
+      targetDelegate_;
 };
 
 std::unique_ptr<JSRuntime> HermesInstance::createJSRuntime(

commit 2d46dbe6ce962bbdcae5d91afe557b46b6b7cfe4

Fix HermesExecutorFactory build error (#44423)
Summary:
https://github.com/facebook/react-native/commit/7af288e5 introduced a breaking change for whoever importing HermesExecutorFactory.h, because the `hermes/inspector-modern/chrome/HermesRuntimeTargetDelegate.h` is not a public header. Also the nested import is not ideal for CocoaPods or use_frameworks.
I think HermesRuntimeTargetDelegate could be an implementation detail that hide from header. This PR tries to turn the ownership declaration from std::optional to std::unique_ptr, so that we could hide the concrete type.

## Changelog:

[IOS] [FIXED] - Fixed `HermesExecutorFactory.h` build error when importing its private header

Pull Request resolved: https://github.com/facebook/react-native/pull/44423

Test Plan: should introduce no breaking change and ci passed

Reviewed By: cipolleschi

Differential Revision: D57041498

Pulled By: huntie

fbshipit-source-id: bfa10c7307458813d99c52313682dd62bea80f19

packages/react-native/ReactCommon/hermes/executor/HermesExecutorFactory.cpp
@@ -13,6 +13,7 @@
 #include <jsi/decorator.h>
 #include <jsinspector-modern/InspectorFlags.h>
 
+#include <hermes/inspector-modern/chrome/HermesRuntimeTargetDelegate.h>
 #include <hermes/inspector-modern/chrome/Registration.h>
 #include <hermes/inspector/RuntimeAdapter.h>
 
@@ -258,7 +259,9 @@ HermesExecutor::HermesExecutor(
 jsinspector_modern::RuntimeTargetDelegate&
 HermesExecutor::getRuntimeTargetDelegate() {
   if (!targetDelegate_) {
-    targetDelegate_.emplace(hermesRuntime_);
+    targetDelegate_ =
+        std::make_unique<jsinspector_modern::HermesRuntimeTargetDelegate>(
+            hermesRuntime_);
   }
   return *targetDelegate_;
 }

packages/react-native/ReactCommon/hermes/executor/HermesExecutorFactory.h
@@ -8,7 +8,6 @@
 #pragma once
 
 #include <hermes/hermes.h>
-#include <hermes/inspector-modern/chrome/HermesRuntimeTargetDelegate.h>
 #include <jsireact/JSIExecutor.h>
 #include <utility>
 
@@ -62,8 +61,7 @@ class HermesExecutor : public JSIExecutor {
   JSIScopedTimeoutInvoker timeoutInvoker_;
   std::shared_ptr<jsi::Runtime> runtime_;
   std::shared_ptr<hermes::HermesRuntime> hermesRuntime_;
-  std::optional<jsinspector_modern::HermesRuntimeTargetDelegate>
-      targetDelegate_;
+  std::unique_ptr<jsinspector_modern::RuntimeTargetDelegate> targetDelegate_;
 };
 
 } // namespace facebook::react