e40c93ea99004e1d23378e99058d389c04d30ebc
Saad Najmi <sanajmi@microsoft.com>
2024-05-07 06:49:34 -0700

    chore: Remove deprecated onTextInput callback (#44351)
    Summary:
    TextInputs' `onTextInput` callback was removed way back in React Native 0.62 with https://github.com/facebook/react-native/commit/3f7e0a2c9601fc186f25bfd794cd0008ac3983ab , but remnants of the implementation exists. Let's just remove it altogether?
    
    ## Changelog:
    
    [IOS] [REMOVED] - Remove deprecated onTextInput callback
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/44351
    
    Test Plan: CI should pass
    
    Differential Revision: D56804590
    
    Pulled By: javache

--- packages/react-native/Libraries/Components/TextInput/AndroidTextInputNativeComponent.js
+++ packages/react-native/Libraries/Components/TextInput/AndroidTextInputNativeComponent.js
@@ -385,15 +385,6 @@ export type NativeProps = $ReadOnly<{|
     |}>,
   >,
 
-  onTextInput?: ?BubblingEventHandler<
-    $ReadOnly<{|
-      target: Int32,
-      text: string,
-      previousText: string,
-      range: $ReadOnly<{|start: Double, end: Double|}>,
-    |}>,
-  >,
-
   /**
    * Callback that is called when text input ends.
    */
@@ -660,12 +651,6 @@ export const __INTERNAL_VIEW_CONFIG: PartialViewConfig = {
       phasedRegistrationNames: {
         bubbled: 'onSubmitEditing',
         captured: 'onSubmitEditingCapture',
-      },
-    },
-    topTextInput: {
-      phasedRegistrationNames: {
-        bubbled: 'onTextInput',
-        captured: 'onTextInputCapture',
       },
     },
   },
--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputManager.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputManager.java
@@ -231,11 +231,6 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
                     "phasedRegistrationNames",
                     MapBuilder.of("bubbled", "onEndEditing", "captured", "onEndEditingCapture")))
             .put(
-                "topTextInput",
-                MapBuilder.of(
-                    "phasedRegistrationNames",
-                    MapBuilder.of("bubbled", "onTextInput", "captured", "onTextInputCapture")))
-            .put(
                 "topFocus",
                 MapBuilder.of(
                     "phasedRegistrationNames",
@@ -1120,17 +1115,12 @@ public class ReactTextInputManager extends BaseViewManager<ReactEditText, Layout
       }
 
       // The event that contains the event counter and updates it must be sent first.
-      // TODO: t7936714 merge these events
       mEventDispatcher.dispatchEvent(
           new ReactTextChangedEvent(
               mSurfaceId,
               mEditText.getId(),
               s.toString(),
               mEditText.incrementAndGetEventCounter()));
-
-      mEventDispatcher.dispatchEvent(
-          new ReactTextInputEvent(
-              mSurfaceId, mEditText.getId(), newText, oldText, start, start + before));
     }
 
     @Override
