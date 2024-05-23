47a3b1b099405ab8fdbe193e7e6927f4df3c0b53
Pieter De Baets <pieterdb@meta.com>
2024-05-23 09:06:40 -0700

    Shortcut emitDeviceEvent in bridgeless (2nd attempt) (#44590)
    Summary:
    
    emitDeviceEvent is frequently used for perf-critical operations such as sending network responses from native to JS. We don't need to go through JavaScriptModule Proxy (which is missing caching in bridgeless) and instead can immediately invoke the callable JS module.
    
    Changelog: [Internal]
    
    Reviewed By: philIip
    
    Differential Revision: D57435750

--- packages/react-native/ReactAndroid/src/test/java/com/facebook/react/runtime/BridgelessReactContextTest.kt
+++ packages/react-native/ReactAndroid/src/test/java/com/facebook/react/runtime/BridgelessReactContextTest.kt
@@ -9,23 +9,31 @@ package com.facebook.react.runtime
 
 import android.app.Activity
 import android.content.Context
+import com.facebook.react.bridge.WritableNativeArray
 import com.facebook.react.fabric.FabricUIManager
 import com.facebook.react.uimanager.UIManagerModule
+import com.facebook.testutils.shadows.ShadowArguments
+import com.facebook.testutils.shadows.ShadowNativeArray
 import com.facebook.testutils.shadows.ShadowSoLoader
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers
-import org.mockito.Mockito
 import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.eq
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 /** Tests [BridgelessReactContext] */
 @RunWith(RobolectricTestRunner::class)
-@Config(shadows = [ShadowSoLoader::class])
+@Config(
+    shadows = [ShadowSoLoader::class, ShadowArguments::class, ShadowNativeArray.Writable::class])
 class BridgelessReactContextTest {
   private lateinit var context: Context
   private lateinit var reactHost: ReactHostImpl
@@ -34,30 +42,43 @@ class BridgelessReactContextTest {
   @Before
   fun setUp() {
     context = Robolectric.buildActivity(Activity::class.java).create().get()
-    reactHost = Mockito.mock(ReactHostImpl::class.java)
+    reactHost = mock(ReactHostImpl::class.java)
     bridgelessReactContext = BridgelessReactContext(context, reactHost)
   }
 
   @Test
   fun getNativeModuleTest() {
-    val mUiManagerModule = Mockito.mock(UIManagerModule::class.java)
+    val mUiManagerModule = mock(UIManagerModule::class.java)
     doReturn(mUiManagerModule)
         .`when`(reactHost)
         .getNativeModule(ArgumentMatchers.any<Class<UIManagerModule>>())
     val uiManagerModule = bridgelessReactContext.getNativeModule(UIManagerModule::class.java)
-    Assertions.assertThat(uiManagerModule).isEqualTo(mUiManagerModule)
+    assertThat(uiManagerModule).isEqualTo(mUiManagerModule)
   }
 
   @Test
   fun getFabricUIManagerTest() {
-    val fabricUiManager = Mockito.mock(FabricUIManager::class.java)
+    val fabricUiManager = mock(FabricUIManager::class.java)
     doReturn(fabricUiManager).`when`(reactHost).uiManager
-    Assertions.assertThat(bridgelessReactContext.getFabricUIManager()).isEqualTo(fabricUiManager)
+    assertThat(bridgelessReactContext.getFabricUIManager()).isEqualTo(fabricUiManager)
   }
 
   @Test
   fun getCatalystInstanceTest() {
-    Assertions.assertThat(bridgelessReactContext.getCatalystInstance())
+    assertThat(bridgelessReactContext.getCatalystInstance())
         .isInstanceOf(BridgelessCatalystInstance::class.java)
+  }
+
+  @Test
+  fun testEmitDeviceEvent() {
+    bridgelessReactContext.emitDeviceEvent("onNetworkResponseReceived", mapOf("foo" to "bar"))
+
+    val argsCapture = ArgumentCaptor.forClass(WritableNativeArray::class.java)
+    verify(reactHost, times(1))
+        .callFunctionOnModule(eq("RCTDeviceEventEmitter"), eq("emit"), argsCapture.capture())
+
+    val argsList = ShadowNativeArray.getContents(argsCapture.value)
+    assertThat(argsList[0]).isEqualTo("onNetworkResponseReceived")
+    @Suppress("UNCHECKED_CAST") assertThat((argsList[1] as Map<Any, Any>)["foo"]).isEqualTo("bar")
   }
 }
