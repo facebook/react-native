929822c87eebc29b0ae8479b0ebdcf2b9757d322
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-06-05 14:13:48 +0200

    Preserve state when cloning the tree

--- packages/react-native/ReactCommon/react/renderer/core/ShadowNode.cpp
+++ packages/react-native/ReactCommon/react/renderer/core/ShadowNode.cpp
@@ -371,7 +371,8 @@ ShadowNode::Unshared ShadowNode::cloneTree(
 
     childNode = parentNode.clone(
         {.children = std::make_shared<ShadowNode::ListOfShared>(children),
-         .traits = traits});
+         .traits = traits,
+         .state = parentNode.getState()});
   }
 
   return std::const_pointer_cast<ShadowNode>(childNode);
