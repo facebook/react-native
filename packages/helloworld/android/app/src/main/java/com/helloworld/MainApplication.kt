f13bb98783ebde8eddbabea44ec08b541e20dfc2
Nicola Corti <ncor@meta.com>
2024-06-07 05:14:37 -0700

    PackageList2 -> PackageList (#44828)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/44828
    
    I was using PackageList2 temporarily as I was migrating to Core Autolinking.
    Now we can rename everything to `PackageList` to reduce the number of changes to the template for users.
    
    Changelog:
    [Internal] [Changed] - PackageList2 -> PackageList
    
    Differential Revision: D58284661

--- packages/helloworld/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ packages/helloworld/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -8,7 +8,7 @@
 package com.helloworld
 
 import android.app.Application
-import com.facebook.react.PackageList2
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
@@ -23,7 +23,7 @@ class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
-            PackageList2(this).packages.apply {
+            PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
             }
