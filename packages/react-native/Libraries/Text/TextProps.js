c9d7774a240c55160e63b98a756819050abe9327
Pieter Vanderwerff <pieterv@meta.com>
2024-06-11 15:41:19 -0700

    Cleanup imports and types (#44862)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/44862
    
    Clean up:
    * type imports to be consistent
    * Remove long since deprecated `{| ... |}` exact object style.
    
    Changelog: [Internal]
    
    Reviewed By: NickGerleman
    
    Differential Revision: D58381905
    
    fbshipit-source-id: 4c061385a987a2bc7dd8339183533084f1efd699

--- packages/react-native/Libraries/Text/TextProps.js
+++ packages/react-native/Libraries/Text/TextProps.js
@@ -26,23 +26,23 @@ import type {
 } from '../Types/CoreEventTypes';
 import type {Node} from 'react';
 
-export type PressRetentionOffset = $ReadOnly<{|
+export type PressRetentionOffset = $ReadOnly<{
   top: number,
   left: number,
   bottom: number,
   right: number,
-|}>;
+}>;
 
-type PointerEventProps = $ReadOnly<{|
+type PointerEventProps = $ReadOnly<{
   onPointerEnter?: (event: PointerEvent) => void,
   onPointerLeave?: (event: PointerEvent) => void,
   onPointerMove?: (event: PointerEvent) => void,
-|}>;
+}>;
 
 /**
  * @see https://reactnative.dev/docs/text#reference
  */
-export type TextProps = $ReadOnly<{|
+export type TextProps = $ReadOnly<{
   ...PointerEventProps,
 
   /**
@@ -265,4 +265,4 @@ export type TextProps = $ReadOnly<{|
    * See https://reactnative.dev/docs/text.html#linebreakstrategyios
    */
   lineBreakStrategyIOS?: ?('none' | 'standard' | 'hangul-word' | 'push-out'),
-|}>;
+}>;
