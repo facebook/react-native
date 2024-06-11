a97e3d055db0ad1cc233e509e89c42a5daa36ba9
Thomas Nardone <tnardone@fb.com>
2024-06-11 07:25:20 -0700

    Convert Network Module [2]
    Differential Revision: D55817599

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/network/ReactCookieJarContainer.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.modules.network
+
+import java.util.ArrayList
+import java.util.Collections
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.Headers
+import okhttp3.Headers.Builder
+import okhttp3.HttpUrl
+
+/** Basic okhttp3 CookieJar container */
+public class ReactCookieJarContainer : CookieJarContainer {
+
+  private var cookieJar: CookieJar? = null
+
+  override fun setCookieJar(cookieJar: CookieJar?) {
+    this.cookieJar = cookieJar
+  }
+
+  override fun removeCookieJar() {
+    cookieJar = null
+  }
+
+  override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+    cookieJar?.saveFromResponse(url, cookies)
+  }
+
+  override fun loadForRequest(url: HttpUrl): List<Cookie> {
+    val cookies = cookieJar?.loadForRequest(url) ?: return Collections.emptyList()
+    val validatedCookies = ArrayList<Cookie>()
+    for (cookie in cookies) {
+      try {
+        val cookieChecker = Headers.Builder()
+        cookieChecker.add(cookie.name(), cookie.value())
+        validatedCookies.add(cookie)
+      } catch (ignored: IllegalArgumentException) {
+        // failed validation
+      }
+    }
+    return validatedCookies
+  }
+}
