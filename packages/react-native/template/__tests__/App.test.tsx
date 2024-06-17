31ba14c6fd8fe2800ed5e4f56bed6f933c57ec84
Riccardo Cipolleschi <cipolleschi@meta.com>
2024-06-17 13:59:31 +0100

    [RN][Jest] Fix tests for React 19

--- packages/react-native/template/__tests__/App.test.tsx
+++ packages/react-native/template/__tests__/App.test.tsx
@@ -10,8 +10,10 @@ import App from '../App';
 import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import ReactTestRenderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders correctly', async () => {
+  await ReactTestRenderer.act(() => {
+    ReactTestRenderer.create(<App />);
+  });
 });
