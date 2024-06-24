91077e5e8255efd6bef44c5d807695019cf2d843
szymonrybczak <szymon.rybczak@gmail.com>
2024-06-24 14:09:52 +0200

    fix(cli): replace `querystring` with `URLSearchParam`

--- packages/community-cli-plugin/src/utils/parseKeyValueParamArray.js
+++ packages/community-cli-plugin/src/utils/parseKeyValueParamArray.js
@@ -9,8 +9,6 @@
  * @oncall react_native
  */
 
-import querystring from 'querystring';
-
 export default function parseKeyValueParamArray(
   keyValueArray: $ReadOnlyArray<string>,
 ): Record<string, string> {
@@ -23,7 +21,10 @@ export default function parseKeyValueParamArray(
     if (item.indexOf('&') !== -1) {
       throw new Error('Parameter cannot include "&" but found: ' + item);
     }
-    Object.assign(result, querystring.parse(item));
+    const params = new URLSearchParam(item);
+    for (const [key, value] of params) {
+      result[key] = value;
+    }
   }
 
   return result;
