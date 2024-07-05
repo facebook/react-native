d4d7e28db8dcde8984d649cb22a17eedf63a994d
meetdhanani17 <meet.dhanani.aitce@gmail.com>
2024-07-06 01:56:29 +0530

    format code

--- packages/react-native/Libraries/StyleSheet/flattenStyle.js
+++ packages/react-native/Libraries/StyleSheet/flattenStyle.js
@@ -24,7 +24,7 @@ function flattenStyle<TStyleProp: DangerouslyImpreciseStyleProp>(
   if (!Array.isArray(style)) {
     if (Object.isFrozen(style)) {
       // $FlowFixMe[incompatible-return]
-      return {...style}
+      return {...style};
     }
     // $FlowFixMe[incompatible-return]
     return style;
