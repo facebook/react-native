65a3259f039416394d2b945ec10565d38b3cad9e
Joe Vilches <joevilches@meta.com>
2024-07-12 17:17:00 -0700

    Inset box shadow impl (#45337)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/45337
    
    tsia. Some things to consider when reviewing:
    
    * Made a new drawable for inset shadows
    * The drawable in this class is the same size as the view with some padding. The padding is needed for 2 reasons
      * Blur near edges looks good
      * Blur artifacts can appear inside the view if the clear region barely exits the bounds of the view
    * We draw the clear shape with another drawable, which solely exists so that we can get the border box path for the adjust border. We just use this path to clip out the shadow
    
    Changelog: [Internal]
    
    Reviewed By: NickGerleman
    
    Differential Revision: D59300215
    
    fbshipit-source-id: 30acc7aafd82122aa278a42d06418bb1079ca71f

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/drawable/OutsetBoxShadowDrawable.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/drawable/OutsetBoxShadowDrawable.kt
@@ -17,44 +17,47 @@ import androidx.annotation.RequiresApi
 import com.facebook.common.logging.FLog
 import com.facebook.react.common.annotations.UnstableReactNativeAPI
 import com.facebook.react.uimanager.FilterHelper
-import com.facebook.react.uimanager.LengthPercentage
-import com.facebook.react.uimanager.LengthPercentageType
 import com.facebook.react.uimanager.PixelUtil
-import com.facebook.react.uimanager.style.BorderRadiusProp
-import com.facebook.react.uimanager.style.BorderRadiusStyle
 import kotlin.math.roundToInt
 
-private const val TAG = "BoxShadowDrawable"
+private const val TAG = "OutsetBoxShadowDrawable"
 
 // "the resulting shadow must approximate {...} a Gaussian blur with a standard deviation equal
 // to half the blur radius"
 // https://www.w3.org/TR/css-backgrounds-3/#shadow-blur
 private const val BLUR_RADIUS_SIGMA_SCALE = 0.5f
 
-/** Draws an outer-box shadow https://www.w3.org/TR/css-backgrounds-3/#shadow-shape */
+/** Draws an outer box-shadow https://www.w3.org/TR/css-backgrounds-3/#shadow-shape */
 @RequiresApi(31)
 @OptIn(UnstableReactNativeAPI::class)
-internal class BoxShadowDrawable(
+internal class OutsetBoxShadowDrawable(
     context: Context,
     private val background: CSSBackgroundDrawable,
     shadowColor: Int,
     private val offsetX: Float,
     private val offsetY: Float,
-    blurRadius: Float,
+    private val blurRadius: Float,
     private val spread: Float,
 ) : Drawable() {
-
   private val shadowShapeDrawable = CSSBackgroundDrawable(context).apply { color = shadowColor }
 
   private val renderNode =
-      RenderNode("BoxShadowDrawable").apply {
+      RenderNode(TAG).apply {
         clipToBounds = false
         setRenderEffect(FilterHelper.createBlurEffect(blurRadius * BLUR_RADIUS_SIGMA_SCALE))
       }
 
+  override fun setAlpha(alpha: Int) {
+    renderNode.alpha = alpha / 255f
+  }
+
+  override fun setColorFilter(colorFilter: ColorFilter?): Unit = Unit
+
+  override fun getOpacity(): Int = (renderNode.alpha * 255).roundToInt()
+
   override fun draw(canvas: Canvas) {
     if (!canvas.isHardwareAccelerated) {
-      FLog.w(TAG, "BoxShadowDrawable requires a hardware accelerated canvas")
+      FLog.w(TAG, "OutsetBoxShadowDrawable requires a hardware accelerated canvas")
       return
     }
 
@@ -72,6 +75,7 @@ internal class BoxShadowDrawable(
         shadowShapeDrawable.layoutDirection != layoutDirection ||
         shadowShapeDrawable.borderRadius != borderRadii ||
         shadowShapeDrawable.colorFilter != colorFilter) {
+      canvas.save()
       shadowShapeDrawable.bounds = shadowShapeBounds
       shadowShapeDrawable.layoutDirection = layoutDirection
       shadowShapeDrawable.borderRadius = borderRadii
@@ -102,52 +106,7 @@ internal class BoxShadowDrawable(
 
       drawRenderNode(renderNode)
     }
-  }
 
-  override fun setAlpha(alpha: Int) {
-    renderNode.alpha = alpha / 255f
-  }
-
-  override fun setColorFilter(colorFilter: ColorFilter?): Unit = Unit
-
-  override fun getOpacity(): Int = (renderNode.alpha * 255).roundToInt()
-
-  private fun getShadowBorderRadii(
-      spread: Float,
-      backgroundBorderRadii: BorderRadiusStyle,
-      width: Float,
-      height: Float,
-  ): BorderRadiusStyle {
-    val adjustedBorderRadii = BorderRadiusStyle()
-    val borderRadiusProps = BorderRadiusProp.values()
-
-    borderRadiusProps.forEach { borderRadiusProp ->
-      val borderRadius = backgroundBorderRadii.get(borderRadiusProp)
-      adjustedBorderRadii.set(
-          borderRadiusProp,
-          if (borderRadius == null) null
-          else adjustedBorderRadius(spread, borderRadius, width, height))
-    }
-
-    return adjustedBorderRadii
-  }
-
-  // See https://drafts.csswg.org/css-backgrounds/#shadow-shape
-  private fun adjustedBorderRadius(
-      spread: Float,
-      backgroundBorderRadius: LengthPercentage,
-      width: Float,
-      height: Float,
-  ): LengthPercentage {
-    var adjustment = spread
-    val backgroundBorderRadiusValue = backgroundBorderRadius.resolve(width, height)
-
-    if (backgroundBorderRadiusValue < Math.abs(spread)) {
-      val r = backgroundBorderRadiusValue / Math.abs(spread)
-      val p = Math.pow(r - 1.0, 3.0)
-      adjustment *= 1.0f + p.toFloat()
-    }
-
-    return LengthPercentage(backgroundBorderRadiusValue + adjustment, LengthPercentageType.POINT)
+    canvas.restore()
   }
 }
