452829515930822b04915ce88efe360de7d0e0a7
Nick Lefever <lefever@meta.com>
2024-07-16 06:51:28 -0700

    Use weak ptr for runtime shadow node reference (#45463)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/45463
    
    Changelog: [Internal]
    
    State updates will clone shadow nodes for an shadow tree revision that is outdated.
    
    This can lead to accessing deallocated shadow node references because the JS renderer committed a newer revision and deallocated the one used by the pending state update.
    
    By using a weak pointer to hold a reference to the runtime shadow node reference, we can only update references for wrappers that are still valid.
    
    Differential Revision: D59804999

--- packages/react-native/ReactCommon/react/renderer/core/ShadowNode.h
+++ packages/react-native/ReactCommon/react/renderer/core/ShadowNode.h
@@ -190,11 +190,11 @@ class ShadowNode : public Sealable,
   bool progressStateIfNecessary();
 
   /*
-   * Bind the runtime reference to this `ShadowNode` with a raw pointer,
+   * Bind the runtime reference to this `ShadowNode` with a weak pointer,
    * allowing to update the reference to this `ShadowNode` when cloned.
    */
-  void setRuntimeShadowNodeReference(
-      ShadowNodeWrapper* runtimeShadowNodeReference) const;
+  void setRuntimeShadowNodeReference(const std::shared_ptr<ShadowNodeWrapper>&
+                                         runtimeShadowNodeReference) const;
 
   /*
    * Transfer the runtime reference to this `ShadowNode` to a new instance,
@@ -269,9 +269,9 @@ class ShadowNode : public Sealable,
   ShadowNodeTraits traits_;
 
   /*
-   * Pointer to the runtime reference to this `ShadowNode`.
+   * Weak pointer to the runtime reference to this `ShadowNode`.
    */
-  mutable ShadowNodeWrapper* runtimeShadowNodeReference_{};
+  mutable std::weak_ptr<ShadowNodeWrapper> runtimeShadowNodeReference_{};
 };
 
 static_assert(
