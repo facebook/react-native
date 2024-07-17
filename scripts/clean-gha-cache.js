0e38a8b16a0ffb01272ce8b4b13df238694613e4
Blake Friedman <blake.friedman@gmail.com>
2024-07-17 17:31:41 +0100

    GHA: create a reaper job to keep the cache < 10GB
    This would run every 2hrs during weekdays.  It aims for 9GB (a.k.a 90%
    of threshold). It'll order caches by most accessed and start trimming
    removing as soon as the cumulative cache is greater than the threshold.
    
    I've given it generous logging to help debugging and investigation about
    cache hogs.
    
    Changelog: [Internal]

---
+++ scripts/clean-gha-cache.js
@@ -0,0 +1,58 @@
+const {execSync} = require('node:child_process');
+
+const CACHE_LIMIT = (10 * 1024 ** 3) * 0.9;
+
+function cleanData(rawStr) {
+  const now = new Date();
+  const json = JSON.parse(rawStr);
+  return json
+    .map(raw => ({
+      ...raw,
+      msSinceLastAccessed: now - new Date(raw.lastAccessedAt),
+    }))
+    .sort((a, b) => b.msSinceLastAccessed - a.msSinceLastAccessed);
+}
+
+const mb = bytes => (bytes / 1024 ** 2).toFixed(2) + 'MB';
+const hrs = ms => (ms / (1000 * 60 * 60)).toFixed(1) + 'hrs';
+
+const dryRun = process.argv.indexOf('--dry') !== -1;
+
+function main() {
+  const cacheUsage = cleanData(execSync(
+    'gh cache list --sort last_accessed_at --json id,key,lastAccessedAt,sizeInBytes --limit 1000',
+    'utf8'
+  ));
+
+  let total = 0;
+  let remove = [];
+  let cleaned = 0;
+  for (let i = cacheUsage.length - 1; i > 0; i--) {
+    const {id, key, msSinceLastAccessed, sizeInBytes} = cacheUsage[i];
+    total += sizeInBytes;
+
+    // Are we in the danger zone?
+    if (total > CACHE_LIMIT) {
+      console.warn(`[${hrs(msSinceLastAccessed).padStart(7)}] ${mb(sizeInBytes).padStart(7)} -> ${key}`);
+      cleaned += sizeInBytes;
+      remove.push(id);
+    } else {
+      console.warn(`skip ${cacheUsage.length - i} ${mb(sizeInBytes)} ${hrs(msSinceLastAccessed)}`);
+    }
+  }
+  console.warn(`Identifed ${remove.length} cache keys for removal, reducing cache from ${mb(total)} -> ${mb(total - cleaned)}`);
+
+  const cleanup = remove.map(id => `gh cache delete ${id} --repo facebook/react-native`);
+  for (const cmd of cleanup) {
+    if (dryRun) {
+      console.warn(`DRY: ${cmd}`);
+    } else {
+      console.warn(
+        execSync(cmd, 'utf8').toString()
+      );
+    }
+  }
+}
+
+main();
+
