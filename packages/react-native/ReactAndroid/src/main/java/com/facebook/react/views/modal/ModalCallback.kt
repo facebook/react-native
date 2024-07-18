6be849e3c179984a25d721431a32c10ced8eaa45
Maciej Stosio <maciekstosio@users.noreply.github.com>
2024-07-18 16:06:29 +0200

    fix: remove unused return type

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/modal/ModalCallback.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/modal/ModalCallback.kt
@@ -3,6 +3,6 @@ package com.facebook.react.views.modal;
 import android.content.DialogInterface;
 
 public interface ModalCallback {
-  public fun onModalOpen(dialog: DialogInterface?, viewTag: Int): Void
-  public fun onModalRequestClose(dialog: DialogInterface?, viewTag: Int): Void
+  public fun onModalOpen(dialog: DialogInterface?, viewTag: Int)
+  public fun onModalRequestClose(dialog: DialogInterface?, viewTag: Int)
 }
