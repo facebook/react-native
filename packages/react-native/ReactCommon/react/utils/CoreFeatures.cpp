d8d9ee313589095e2b09e06eb26a25131e20f1de
David Vacca <515103+mdvacca@users.noreply.github.com>
2024-07-23 21:04:22 -0700

    Delete CoreFeatures::enablePropIteratorSetter
    Summary:
    Delete CoreFeatures::enablePropIteratorSetter in favor of ReactNativeFeatureFlags::enablePropIteratorSetter
    
    changelog: [internal] internal
    
    Differential Revision: D60129358

--- packages/react-native/ReactCommon/react/utils/CoreFeatures.cpp
+++ packages/react-native/ReactCommon/react/utils/CoreFeatures.cpp
@@ -9,7 +9,6 @@
 
 namespace facebook::react {
 
-bool CoreFeatures::enablePropIteratorSetter = false;
 bool CoreFeatures::enableGranularScrollViewStateUpdatesIOS = false;
 
 } // namespace facebook::react
