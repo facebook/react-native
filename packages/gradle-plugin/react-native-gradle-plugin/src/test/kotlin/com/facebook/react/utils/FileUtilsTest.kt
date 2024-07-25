88b3dbf293e339143c52beaf1accb2acbe1482c0
Henrique Cicero <henriquecicero@gmail.com>
2024-07-25 08:41:37 -0300

    test: improve assertion in subFolder

--- packages/gradle-plugin/react-native-gradle-plugin/src/test/kotlin/com/facebook/react/utils/FileUtilsTest.kt
+++ packages/gradle-plugin/react-native-gradle-plugin/src/test/kotlin/com/facebook/react/utils/FileUtilsTest.kt
@@ -39,7 +39,7 @@ class FileUtilsTest {
 
     subFolder.recreateDir()
 
-    assertThat(subFolder.exists()).isTrue()
+    assertThat(subFolder).exists();
     assertThat(subFolder.listFiles()).hasSize(0);
   }
 }
