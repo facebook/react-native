51156fd3b2f371e598b3da284a4b1dec45c93cc8
Nicola Corti <ncor@meta.com>
2024-07-29 15:40:56 +0100

    [LOCAL] Fix broken build of RNGP due to complex cherry-pick

--- packages/react-native-gradle-plugin/react-native-gradle-plugin/build.gradle.kts
+++ packages/react-native-gradle-plugin/react-native-gradle-plugin/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
   implementation(libs.javapoet)
 
   testImplementation(libs.junit)
+  testImplementation(libs.assertj)
   testImplementation(project(":shared-testutil"))
 
   testRuntimeOnly(
