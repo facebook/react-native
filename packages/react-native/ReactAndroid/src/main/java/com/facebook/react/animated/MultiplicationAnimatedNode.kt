7374cc5a3333f4cc4210691b8b3a997ff301774c
Ruslan Shestopalyuk <rshest@meta.com>
2024-07-30 03:42:48 -0700

    '[skip ci] [RN][Android] Migrate MultiplicationAnimatedNode to Kotlin (#45756)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/45756
    
    # Changelog:
    [Internal] -
    As in the title.
    
    Reviewed By: cortinico
    
    Differential Revision: D60340884

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/animated/MultiplicationAnimatedNode.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.animated
+
+import com.facebook.react.bridge.JSApplicationCausedNativeException
+import com.facebook.react.bridge.ReadableMap
+
+/**
+ * Animated node which takes two or more value node as an input and outputs a product of their
+ * values
+ */
+internal class MultiplicationAnimatedNode(
+    config: ReadableMap,
+    private val nativeAnimatedNodesManager: NativeAnimatedNodesManager
+) : ValueAnimatedNode() {
+  private var inputNodes: IntArray
+
+  init {
+    val input = config.getArray("input")
+    inputNodes =
+        if (input == null) {
+          IntArray(0)
+        } else {
+          IntArray(input.size()) { i -> input.getInt(i) }
+        }
+  }
+
+  override fun update() {
+    nodeValue = 1.0
+    for (i in inputNodes.indices) {
+      val animatedNode = nativeAnimatedNodesManager.getNodeById(inputNodes[i])
+      val multiplier =
+          if (animatedNode != null && animatedNode is ValueAnimatedNode) {
+            animatedNode.getValue()
+          } else {
+            throw JSApplicationCausedNativeException(
+                "Illegal node ID set as an input for Animated.multiply node")
+          }
+      nodeValue *= multiplier
+    }
+  }
+
+  override fun prettyPrint(): String =
+      "MultiplicationAnimatedNode[$tag]: input nodes: $inputNodes - super: ${super.prettyPrint()}"
+}
