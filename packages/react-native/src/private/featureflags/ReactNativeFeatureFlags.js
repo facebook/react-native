9ee50e5db9f4672b4f7019aa9c75d75465ac3a3e
Samuel Susla <samuelsusla@meta.com>
2024-08-05 08:30:49 -0700

    ship use of passive effects in Animated (#45893)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/45893
    
    changelog: [internal]
    
    This is mainly impact for the Event Loop where setting up of animation graph will no longer block paint.
    
    Reviewed By: rubennorte
    
    Differential Revision: D60648823
    
    fbshipit-source-id: 8efa1dac2a42b14a609adae05e9266f78a181d43

--- packages/react-native/src/private/featureflags/ReactNativeFeatureFlags.js
+++ packages/react-native/src/private/featureflags/ReactNativeFeatureFlags.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<0b57a2e853d1f2872ddf0c0b610805bb>>
+ * @generated SignedSource<<a3dcd2d00c25e85277416a43a1a27b1c>>
  * @flow strict-local
  */
 
@@ -36,7 +36,6 @@ export type ReactNativeFeatureFlagsJsOnly = {
   shouldUseRemoveClippedSubviewsAsDefaultOnIOS: Getter<boolean>,
   shouldUseSetNativePropsInFabric: Getter<boolean>,
   shouldUseSetNativePropsInNativeAnimationsInFabric: Getter<boolean>,
-  usePassiveEffectsForAnimations: Getter<boolean>,
   useRefsForTextInputState: Getter<boolean>,
 };
 
@@ -139,11 +138,6 @@ export const shouldUseSetNativePropsInFabric: Getter<boolean> = createJavaScript
  * Enables use of setNativeProps in Native driven animations in Fabric.
  */
 export const shouldUseSetNativePropsInNativeAnimationsInFabric: Getter<boolean> = createJavaScriptFlagGetter('shouldUseSetNativePropsInNativeAnimationsInFabric', false);
-
-/**
- * Enable a variant of useAnimatedPropsLifecycle hook that constructs the animation graph in passive effect instead of layout effect
- */
-export const usePassiveEffectsForAnimations: Getter<boolean> = createJavaScriptFlagGetter('usePassiveEffectsForAnimations', false);
 
 /**
  * Enable a variant of TextInput that moves some state to refs to avoid unnecessary re-renders
