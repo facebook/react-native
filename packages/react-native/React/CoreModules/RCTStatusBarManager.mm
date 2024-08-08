8a390ba9b8c1b8889dd0933fe1477083a24ff71d
Ingrid Wang <ingridwang@meta.com>
2024-08-08 16:27:49 -0700

    Mark setNetworkActivityIndicatorVisible as deprecated (#45950)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/45950
    
    Changelog:
    
    [iOS][Deprecated] Deprecated StatusBar.setNetworkActivityIndicatorVisible
    
    The status bar network activity indicator is deprecated in iOS 13. Setting its visibility has no effect in iOS 13 and later. It will be completely removed in a future release.
    
    Reviewed By: philIip
    
    Differential Revision: D60977517
    
    fbshipit-source-id: 31e79113fffd0201c4393b61236d3911e82b40d1

--- packages/react-native/React/CoreModules/RCTStatusBarManager.mm
+++ packages/react-native/React/CoreModules/RCTStatusBarManager.mm
@@ -161,7 +161,11 @@ RCT_EXPORT_METHOD(setHidden : (BOOL)hidden withAnimation : (NSString *)withAnima
 RCT_EXPORT_METHOD(setNetworkActivityIndicatorVisible : (BOOL)visible)
 {
   dispatch_async(dispatch_get_main_queue(), ^{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    // This is no longer supported in iOS 13 and later. We will remove this method in a future release.
     RCTSharedApplication().networkActivityIndicatorVisible = visible;
+#pragma clang diagnostic pop
   });
 }
 
