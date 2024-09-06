d1a5a72fb8f06ef55c833638b529c48585a2aadb
legobt <6wbvkn0j@anonaddy.me>
2024-09-06 06:10:49 +0000

    chore(react-native): accept failing glog ./configure in ios-configure-glog.sh

--- packages/react-native/scripts/ios-configure-glog.sh
+++ packages/react-native/scripts/ios-configure-glog.sh
@@ -61,7 +61,7 @@ fi
 sed -i.bak -e 's/\@ac_cv_have_libgflags\@/0/' src/glog/logging.h.in && rm src/glog/logging.h.in.bak
 sed -i.bak -e 's/HAVE_LIB_GFLAGS/HAVE_LIB_GFLAGS_DISABLED/' src/config.h.in && rm src/config.h.in.bak
 
-./configure --host arm-apple-darwin
+./configure --host arm-apple-darwin || true
 
 cat << EOF >> src/config.h
 /* Add in so we have Apple Target Conditionals */
