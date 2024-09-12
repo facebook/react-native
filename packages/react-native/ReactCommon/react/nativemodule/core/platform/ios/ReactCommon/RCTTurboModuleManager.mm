cf8d09b279c4bd667d96f9445c1c0b9acb0a0dfc
Phillip Pan <phillippan@meta.com>
2024-09-12 14:57:58 -0700

    remove RCTRuntimeExecutor (#46466)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/46466
    
    We should use [BindingsInstaller](https://github.com/facebook/react-native/pull/44486?fbclid=IwZXh0bgNhZW0CMTEAAR0noV5FT4pi2z0saQhFy4q2bVLiu0bLUn0Aq0QPAWnycksVBuizmUYXdnc_aem_bS3_Q_CgUIljILgDtrHUhA) and [RCTCallInvoker](https://github.com/facebook/react-native/pull/44378?fbclid=IwZXh0bgNhZW0CMTEAAR3rjd8_Ak2CsQfs3k4o6k0F9uivwx-22llwl6TiCFl5c0FmcoW8PKaxNgs_aem_R3cJp9eKQsMte_03lfuBdQ) instead.
    
    ## Changelog:
    [iOS][Breaking] - We deprecated RCTRuntimeExecutor in 0.76, so let's get rid of the code now.
    
    Reviewed By: cipolleschi
    
    Differential Revision: D62469005
    
    fbshipit-source-id: 3ed033b08f52823bd42f8a57d5d6ab40e35d30f1

--- packages/react-native/ReactCommon/react/nativemodule/core/platform/ios/ReactCommon/RCTTurboModuleManager.mm
+++ packages/react-native/ReactCommon/react/nativemodule/core/platform/ios/ReactCommon/RCTTurboModuleManager.mm
@@ -7,7 +7,6 @@
 
 #import "RCTTurboModuleManager.h"
 #import "RCTInteropTurboModule.h"
-#import "RCTRuntimeExecutor.h"
 
 #import <atomic>
 #import <cassert>
@@ -27,7 +26,6 @@
 #import <React/RCTLog.h>
 #import <React/RCTModuleData.h>
 #import <React/RCTPerformanceLogger.h>
-#import <React/RCTRuntimeExecutorModule.h>
 #import <React/RCTUtils.h>
 #import <ReactCommon/CxxTurboModuleUtils.h>
 #import <ReactCommon/RCTTurboModuleWithJSIBindings.h>
@@ -690,13 +688,6 @@ typedef struct {
            "or provide your own setter method.",
           RCTBridgeModuleNameForClass([module class]));
     }
-  }
-
-  // This is a more performant alternative for conformsToProtocol:@protocol(RCTRuntimeExecutorModule)
-  if ([module respondsToSelector:@selector(setRuntimeExecutor:)]) {
-    RCTRuntimeExecutor *runtimeExecutor = [[RCTRuntimeExecutor alloc]
-        initWithRuntimeExecutor:[_runtimeHandler runtimeExecutorForTurboModuleManager:self]];
-    [(id<RCTRuntimeExecutorModule>)module setRuntimeExecutor:runtimeExecutor];
   }
 
   // This is a more performant alternative for conformsToProtocol:@protocol(RCTCallInvokerModule)
