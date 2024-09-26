f775f6919f3890824a5d8357b203591ad401e6de
Thomas Nardone <tnardone@meta.com>
2024-09-26 13:11:07 -0700

    Redo "[RN][Android] Convert ReactViewManager, ReactClippingViewManager to Kotlin" (#46667)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/46667
    
    Pre-req for converting ReactViewGroup without affecting API visibility.
    Changelog: [Internal]
    
    Reviewed By: javache
    
    Differential Revision: D63129925
    
    fbshipit-source-id: 3b31df7b5450adcc961bb677114fb94d944f1ce4

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactClippingViewManager.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.views.view
+
+import android.view.View
+import com.facebook.react.bridge.UiThreadUtil
+import com.facebook.react.uimanager.ReactClippingViewGroupHelper
+import com.facebook.react.uimanager.ViewGroupManager
+import com.facebook.react.uimanager.annotations.ReactProp
+
+/**
+ * View manager which handles clipped subviews. Useful for custom views which extends from
+ * [com.facebook.react.views.view.ReactViewGroup]
+ */
+public abstract class ReactClippingViewManager<T : ReactViewGroup> : ViewGroupManager<T>() {
+
+  @ReactProp(name = ReactClippingViewGroupHelper.PROP_REMOVE_CLIPPED_SUBVIEWS)
+  public open fun setRemoveClippedSubviews(view: T, removeClippedSubviews: Boolean) {
+    UiThreadUtil.assertOnUiThread()
+    view.removeClippedSubviews = removeClippedSubviews
+  }
+
+  override fun addView(parent: T, child: View, index: Int) {
+    UiThreadUtil.assertOnUiThread()
+    if (parent.removeClippedSubviews) {
+      parent.addViewWithSubviewClippingEnabled(child, index)
+    } else {
+      parent.addView(child, index)
+    }
+  }
+
+  override fun getChildCount(parent: T): Int =
+      if (parent.removeClippedSubviews) {
+        parent.allChildrenCount
+      } else {
+        parent.childCount
+      }
+
+  override fun getChildAt(parent: T, index: Int): View? =
+      if (parent.removeClippedSubviews) {
+        parent.getChildAtWithSubviewClippingEnabled(index)
+      } else {
+        parent.getChildAt(index)
+      }
+
+  override fun removeViewAt(parent: T, index: Int) {
+    UiThreadUtil.assertOnUiThread()
+    if (parent.removeClippedSubviews) {
+      val child = getChildAt(parent, index) ?: return
+      if (child.parent != null) {
+        parent.removeView(child)
+      } else {
+        parent.removeViewWithSubviewClippingEnabled(child)
+      }
+    } else {
+      parent.removeViewAt(index)
+    }
+  }
+
+  override fun removeAllViews(parent: T) {
+    UiThreadUtil.assertOnUiThread()
+    if (parent.removeClippedSubviews) {
+      parent.removeAllViewsWithSubviewClippingEnabled()
+    } else {
+      parent.removeAllViews()
+    }
+  }
+}
