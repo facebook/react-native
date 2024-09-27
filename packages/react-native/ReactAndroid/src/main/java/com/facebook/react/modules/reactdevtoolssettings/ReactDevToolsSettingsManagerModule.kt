bec4c8cb2412dee0af208bfa2d90e1902f290581
Ruslan Lesiutin <hoxy@meta.com>
2024-09-27 02:43:09 -0700

    Libraries/DevToolsSettings -> private/reactdevtools/ReactDevToolsSettings (#46662)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/46662
    
    # Changelog: [Internal]
    
    This should not have been a public API in the first place.
    
    1. Moving this to `src/private`.
    2. Removed some unused APIs, such as profiling settings.
    
    Reviewed By: huntie
    
    Differential Revision: D62965492

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/reactdevtoolssettings/ReactDevToolsSettingsManagerModule.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.modules.reactdevtoolssettings
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.facebook.fbreact.specs.NativeReactDevToolsSettingsManagerSpec
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.annotations.ReactModule
+
+@ReactModule(name = NativeReactDevToolsSettingsManagerSpec.NAME)
+public class ReactDevToolsSettingsManagerModule(reactContext: ReactApplicationContext) :
+    NativeReactDevToolsSettingsManagerSpec(reactContext) {
+
+  private val sharedPreferences: SharedPreferences =
+      reactContext.getSharedPreferences(SHARED_PREFERENCES_PREFIX, Context.MODE_PRIVATE)
+
+  public override fun setGlobalHookSettings(settings: String): Unit =
+      sharedPreferences.edit().putString(KEY_HOOK_SETTINGS, settings).apply()
+
+  public override fun getGlobalHookSettings(): String? =
+      sharedPreferences.getString(KEY_HOOK_SETTINGS, null)
+
+  private companion object {
+    private const val SHARED_PREFERENCES_PREFIX = "ReactNative__DevToolsSettings"
+    private const val KEY_HOOK_SETTINGS = "HookSettings"
+  }
+}
