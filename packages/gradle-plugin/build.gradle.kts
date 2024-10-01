0b8408c32c4360b54b97131d533dd591230f28ed
Nicola Corti <ncor@meta.com>
2024-10-01 08:21:59 -0700

    RNGP - Do not apply `java-gradle-plugin` on root build (#46755)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/46755
    
    There is no need to apply `java-gradle-plugin` to the root build of RNGP as that does not contain any Gradle Plugins (they're in subfolders).
    
    This is actually causing the build to be a bit slower as extra tasks for compilation/bundling will be created which are definitely not needed.
    
    Changelog:
    [Internal] [Changed] - RNGP - Do not apply `java-gradle-plugin` on root build
    
    Reviewed By: cipolleschi
    
    Differential Revision: D63695934

--- packages/gradle-plugin/build.gradle.kts
+++ packages/gradle-plugin/build.gradle.kts
@@ -5,7 +5,22 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-plugins {
-  alias(libs.plugins.kotlin.jvm).apply(false)
-  id("java-gradle-plugin")
+plugins { alias(libs.plugins.kotlin.jvm).apply(false) }
+
+tasks.register("build") {
+  dependsOn(
+      ":react-native-gradle-plugin:build",
+      ":settings-plugin:build",
+      ":shared-testutil:build",
+      ":shared:build",
+  )
+}
+
+tasks.register("clean") {
+  dependsOn(
+      ":react-native-gradle-plugin:clean",
+      ":settings-plugin:clean",
+      ":shared-testutil:clean",
+      ":shared:clean",
+  )
 }
