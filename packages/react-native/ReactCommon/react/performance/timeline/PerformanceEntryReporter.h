5f394d9e7c7439e290c467e03df805cf3b5a50e1
Rubén Norte <rubennorte@fb.com>
2024-10-01 09:09:07 -0700

    Small renames in PerformanceEntryReporter
    Differential Revision: D63471856

--- packages/react-native/ReactCommon/react/performance/timeline/PerformanceEntryReporter.h
+++ packages/react-native/ReactCommon/react/performance/timeline/PerformanceEntryReporter.h
@@ -36,18 +36,19 @@ class PerformanceEntryReporter {
     return *observerRegistry_;
   }
 
-  uint32_t getDroppedEntriesCount(PerformanceEntryType type) const noexcept;
+#pragma mark - DOM Performance (High Resolution Time) (https://www.w3.org/TR/hr-time-3/#dom-performance)
 
-  /*
-   * DOM Performance (High Resolution Time)
-   * https://www.w3.org/TR/hr-time-3/#dom-performance
-   */
   // https://www.w3.org/TR/hr-time-3/#now-method
   DOMHighResTimeStamp getCurrentTimeStamp() const;
 
   void setTimeStampProvider(std::function<DOMHighResTimeStamp()> provider) {
     timeStampProvider_ = std::move(provider);
   }
+
+#pragma mark - Performance Timeline (https://w3c.github.io/performance-timeline/)
+
+  // https://www.w3.org/TR/performance-timeline/#dom-performanceobservercallbackoptions-droppedentriescount
+  uint32_t getDroppedEntriesCount(PerformanceEntryType type) const noexcept;
 
   // https://www.w3.org/TR/performance-timeline/#getentries-method
   // https://www.w3.org/TR/performance-timeline/#getentriesbytype-method
@@ -64,36 +65,15 @@ class PerformanceEntryReporter {
       std::string_view entryName,
       PerformanceEntryType entryType) const;
 
-  void logEventEntry(
-      std::string name,
-      double startTime,
-      double duration,
-      double processingStart,
-      double processingEnd,
-      uint32_t interactionId);
+#pragma mark - User Timing Level 3 functions (https://w3c.github.io/user-timing/)
 
-  void logLongTaskEntry(double startTime, double duration);
-
-  /*
-   * Event Timing API functions
-   * https://www.w3.org/TR/event-timing/
-   */
-  // https://www.w3.org/TR/event-timing/#dom-performance-eventcounts
-  const std::unordered_map<std::string, uint32_t>& getEventCounts() const {
-    return eventCounts_;
-  }
-
-  /*
-   * User Timing Level 3 functions
-   * https://w3c.github.io/user-timing/
-   */
   // https://w3c.github.io/user-timing/#mark-method
-  void mark(
+  void reportMark(
       const std::string& name,
       const std::optional<DOMHighResTimeStamp>& startTime = std::nullopt);
 
   // https://w3c.github.io/user-timing/#measure-method
-  void measure(
+  void reportMeasure(
       const std::string_view& name,
       double startTime,
       double endTime,
@@ -106,6 +86,25 @@ class PerformanceEntryReporter {
   void clearEntries(
       std::optional<PerformanceEntryType> entryType = std::nullopt,
       std::optional<std::string_view> entryName = std::nullopt);
+
+#pragma mark - Event Timing API functions (https://www.w3.org/TR/event-timing/)
+
+  void reportEvent(
+      std::string name,
+      double startTime,
+      double duration,
+      double processingStart,
+      double processingEnd,
+      uint32_t interactionId);
+
+  // https://www.w3.org/TR/event-timing/#dom-performance-eventcounts
+  const std::unordered_map<std::string, uint32_t>& getEventCounts() const {
+    return eventCounts_;
+  }
+
+#pragma mark - Long Tasks API functions (https://w3c.github.io/longtasks/)
+
+  void reportLongTask(double startTime, double duration);
 
  private:
   std::unique_ptr<PerformanceObserverRegistry> observerRegistry_;
