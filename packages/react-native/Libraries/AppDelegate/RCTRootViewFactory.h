904222e6083ab5e71ddc1864bfbe539a6a0f1c7d
Alan Hughes <alan@expo.io>
2024-10-03 07:12:43 -0700

    Allow taking control of bundle loading on new arch (#46731)
    Summary:
    On the old architecture you could take control of loading the bundle by implementing
    ```objc
    - (void)loadSourceForBridge:(RCTBridge *)bridge
                     onProgress:(RCTSourceLoadProgressBlock)onProgress
                     onComplete:(RCTSourceLoadBlock)loadCallback;
    ```
    in your `RCTBridgeDelegate`. This is not currently possible in the new architecture.
    
    I've added this using a pretty much identical api by adding a function to both the `RCTInstanceDelegate` and `RCTHostDelegate` protocols. This will be called on the `RCTRootViewFactory`. I've added two properties to the `RCTRootViewFactoryConfiguration`, `loadSourceForHost` and `loadSourceWithProgressForHost`. If one is present, we call it, otherwise we fallback to the normal loading process
    
    ## Changelog:
    
    [iOS] [Breaking] - Add ability to control bundle loading on the new architecture similar to `loadSourceForBridge`. Removed some properties from the `RCTRootViewFactory`.
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/46731
    
    Test Plan: Rn-tester works as normal and it is working for our use case in expo go.
    
    Reviewed By: blakef
    
    Differential Revision: D63755188
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: f1f26b2775b9e547ce7a23028665797c19bfdd9b

--- packages/react-native/Libraries/AppDelegate/RCTRootViewFactory.h
+++ packages/react-native/Libraries/AppDelegate/RCTRootViewFactory.h
@@ -12,6 +12,7 @@
 @protocol RCTCxxBridgeDelegate;
 @protocol RCTComponentViewFactoryComponentProvider;
 @protocol RCTTurboModuleManagerDelegate;
+@protocol RCTHostDelegate;
 @class RCTBridge;
 @class RCTHost;
 @class RCTRootView;
@@ -30,13 +31,6 @@ typedef NSURL *_Nullable (^RCTBundleURLBlock)(void);
 typedef NSArray<id<RCTBridgeModule>> *_Nonnull (^RCTExtraModulesForBridgeBlock)(RCTBridge *bridge);
 typedef NSDictionary<NSString *, Class> *_Nonnull (^RCTExtraLazyModuleClassesForBridge)(RCTBridge *bridge);
 typedef BOOL (^RCTBridgeDidNotFindModuleBlock)(RCTBridge *bridge, NSString *moduleName);
-typedef void (^RCTHostDidStartBlock)(RCTHost *host);
-typedef void (^RCTHostDidReceiveJSErrorStackBlock)(
-    RCTHost *host,
-    NSArray<NSDictionary<NSString *, id> *> *stack,
-    NSString *message,
-    NSUInteger exceptionId,
-    BOOL isFatal);
 
 #pragma mark - RCTRootViewFactory Configuration
 @interface RCTRootViewFactoryConfiguration : NSObject
@@ -147,22 +141,6 @@ typedef void (^RCTHostDidReceiveJSErrorStackBlock)(
  */
 @property (nonatomic, nullable) RCTBridgeDidNotFindModuleBlock bridgeDidNotFindModule;
 
-/**
- * Called when `RCTHost` started.
- * @parameter: host - The started `RCTHost`.
- */
-@property (nonatomic, nullable) RCTHostDidStartBlock hostDidStartBlock;
-
-/**
- * Called when `RCTHost` received JS error.
- * @parameter: host - `RCTHost` which received js error.
- * @parameter: stack - JS error stack.
- * @parameter: message - Error message.
- * @parameter: exceptionId - Exception ID.
- * @parameter: isFatal - YES if JS error is fatal.
- */
-@property (nonatomic, nullable) RCTHostDidReceiveJSErrorStackBlock hostDidReceiveJSErrorStackBlock;
-
 @end
 
 #pragma mark - RCTRootViewFactory
@@ -186,6 +164,10 @@ typedef void (^RCTHostDidReceiveJSErrorStackBlock)(
         andTurboModuleManagerDelegate:(id<RCTTurboModuleManagerDelegate>)turboModuleManagerDelegate;
 
 - (instancetype)initWithConfiguration:(RCTRootViewFactoryConfiguration *)configuration;
+
+- (instancetype)initWithTurboModuleDelegate:(id<RCTTurboModuleManagerDelegate>)turboModuleManagerDelegate
+                               hostDelegate:(id<RCTHostDelegate>)hostdelegate
+                              configuration:(RCTRootViewFactoryConfiguration *)configuration;
 
 /**
  * This method can be used to create new RCTRootViews on demand.
