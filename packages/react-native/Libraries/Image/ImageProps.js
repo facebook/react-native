0a272e5a37c035dc64fccc9399b7d2b0666f9bcf
Sam Zhou <samzhou19815@meta.com>
2024-10-09 12:53:06 -0700

    Replace last React.Ref type usage in react-native
    Summary:
    Prepare for deletion of this deprecated type that contain support for string ref in the next flow release.
    
    Changelog: [Internal]
    
    Differential Revision: D64127228

--- packages/react-native/Libraries/Image/ImageProps.js
+++ packages/react-native/Libraries/Image/ImageProps.js
@@ -20,7 +20,7 @@ import type {
 import type {LayoutEvent, SyntheticEvent} from '../Types/CoreEventTypes';
 import typeof Image from './Image';
 import type {ImageSource} from './ImageSource';
-import type {Node, Ref} from 'react';
+import type {ElementRef, Node, RefSetter} from 'react';
 
 export type ImageLoadEvent = SyntheticEvent<
   $ReadOnly<{|
@@ -291,5 +291,5 @@ export type ImageBackgroundProps = $ReadOnly<{|
    *
    * See https://reactnative.dev/docs/imagebackground#imageref
    */
-  imageRef?: Ref<Image>,
+  imageRef?: RefSetter<ElementRef<Image>>,
 |}>;
