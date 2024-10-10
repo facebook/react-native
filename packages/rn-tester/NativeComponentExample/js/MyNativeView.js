af4907025ef358c312d469118a52699dcf14507d
sarthak-d11 <sarthak.agarwal@dream11.com>
2024-10-10 08:59:25 -0700

    fix: change opacity logic from random to decrement by -0.20 (#46830)
    Summary:
    Part of this: https://github.com/facebook/react-native/issues/46757
    Solves:
    
    ME2E0003
    
    ## Changelog:
    
    <!-- Help reviewers and the release process by writing your own changelog entry.https://reactnative.dev/contributing/changelogs-in-pull-requests
    -->
    [ Internal ] [ Changed ] - The opacity logic is changed from random to decrement the opacity by -0.2
    For more details, see:
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/46830
    
    Test Plan:
    yarn e2e-test-ios
    yarn e2e-test-android
    
    Reviewed By: rshest
    
    Differential Revision: D64173519
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: 69a09b64e7e4f098d3bfd72529401dadb49b93d7

--- packages/rn-tester/NativeComponentExample/js/MyNativeView.js
+++ packages/rn-tester/NativeComponentExample/js/MyNativeView.js
@@ -85,6 +85,14 @@ function getTextFor(measureStruct: MeasureStruct): string {
     measureStruct.height,
   )}`;
 }
+const opacityDecrementCounter = 0.2;
+
+function computeNextOpacity(opacity: number): number {
+  if (parseFloat(opacity.toFixed(1)) > 0.0) {
+    return opacity - opacityDecrementCounter;
+  }
+  return 1.0;
+}
 
 // This is an example component that migrates to use the new architecture.
 export default function MyNativeView(props: {}): React.Node {
@@ -201,7 +209,7 @@ export default function MyNativeView(props: {}): React.Node {
       <Button
         title="Set Opacity"
         onPress={() => {
-          setOpacity(Math.random());
+          setOpacity(computeNextOpacity(opacity));
           setArrayValues([
             Math.floor(Math.random() * 100),
             Math.floor(Math.random() * 100),
