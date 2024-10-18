f9d05ed3c650bdc3cd753bf9140d89bcb91d4dde
Nick Gerleman <ngerlem@meta.com>
2024-10-17 18:19:57 -0700

    Remove legacy absolute positioning path (#46984)
    Summary:
    
    X-link: https://github.com/facebook/yoga/pull/1725
    
    The legacy (wrong) absolute positioning path positions in two places, including work that is definitely always overwritten in the new absolute layout path.
    
    This came up before for position: static, but we didn't clean this up at the time. This code is also now leading display: contents impl being more annoying.
    
    This diff tries to converge to the more spec correct implementation of positioning here, that also only happens in one place. 
    
    Previous path would potentially also incorrectly justify when `justify-content` was non-default, but not handled in the previous few cases? We don't have access to the flexLine at this point later, and apart from the existing tests now passing I reused the new correct logic for justification (spec says we should position child as if its the only child in the container https://www.w3.org/TR/css-flexbox-1/#abspos-items).
    
    I added a new, more scoped errata `AbsolutePositionWithoutInsetsExcludesPadding` to preserve some of the legacy behavior that showed as very breaking. 
    
    I also did not try removing `AbsolutePercentAgainstInnerSize` which I suspect would be more breaking than this change.
    
    Changelog: 
    [General][Breaking] - More spec compliant absolute positioning
    
    Reviewed By: joevilches
    
    Differential Revision: D64244949

--- packages/react-native/ReactCommon/yoga/yoga/YGEnums.h
+++ packages/react-native/ReactCommon/yoga/yoga/YGEnums.h
@@ -61,7 +61,7 @@ YG_ENUM_DECL(
     YGErrata,
     YGErrataNone = 0,
     YGErrataStretchFlexBasis = 1,
-    YGErrataAbsolutePositioningIncorrect = 2,
+    YGErrataAbsolutePositionWithoutInsetsExcludesPadding = 2,
     YGErrataAbsolutePercentAgainstInnerSize = 4,
     YGErrataAll = 2147483647,
     YGErrataClassic = 2147483646)
