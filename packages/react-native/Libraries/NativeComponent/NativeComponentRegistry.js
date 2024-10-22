71e9039e9ff196598992b73d6fe0ee0e27566a18
Sam Zhou <samzhou19815@meta.com>
2024-10-21 18:15:34 -0700

    Prepare typing changes for HostComponent (#47150)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/47150
    
    In https://github.com/facebook/react/pull/31314, I will change the host component type that will be synced to react-native. Notably, it will expose the issue where all the `HostComponent<mixed>` types are wrong, since it doesn't make sense to write `React.AbstractComponent<mixed>`. This diff fixes the existing usages first in prep for that typing change.
    
    Changelog: [Internal]
    
    Reviewed By: alexmckenley
    
    Differential Revision: D64722939
    
    fbshipit-source-id: 14e1477090128205d8be8fc7b135a8478f94b790

--- packages/react-native/Libraries/NativeComponent/NativeComponentRegistry.js
+++ packages/react-native/Libraries/NativeComponent/NativeComponentRegistry.js
@@ -48,7 +48,7 @@ export function setRuntimeConfigProvider(
  * The supplied `viewConfigProvider` may or may not be invoked and utilized,
  * depending on how `setRuntimeConfigProvider` is configured.
  */
-export function get<Config>(
+export function get<Config: {...}>(
   name: string,
   viewConfigProvider: () => PartialViewConfig,
 ): HostComponent<Config> {
@@ -121,7 +121,7 @@ export function get<Config>(
  * that the return value of this is not `HostComponent` because the returned
  * component instance is not guaranteed to have native methods.
  */
-export function getWithFallback_DEPRECATED<Config>(
+export function getWithFallback_DEPRECATED<Config: {...}>(
   name: string,
   viewConfigProvider: () => PartialViewConfig,
 ): React.ComponentType<Config> {
