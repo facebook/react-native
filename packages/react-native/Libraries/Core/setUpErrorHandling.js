0941b51e9d9fe8d0645e6b6e255eb86cc1dd3910
Ramanpreet Nara <ramanpreet@meta.com>
2024-10-25 15:12:40 -0700

    earlyjs: Make ExceptionsManager the js interface for c++ pipeline (#47165)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/47165
    
    The c++ pipeline needs a javascript interface.
    
    We could just re-use exceptions manager (for now).
    
    Changelog: [Internal]
    
    Reviewed By: javache
    
    Differential Revision: D64779068
    
    fbshipit-source-id: 4e300668a01fa22e194ea9f149ef1d936d5e0834

--- packages/react-native/Libraries/Core/setUpErrorHandling.js
+++ packages/react-native/Libraries/Core/setUpErrorHandling.js
@@ -21,13 +21,7 @@ ExceptionsManager.installConsoleErrorReporter();
 if (!global.__fbDisableExceptionsManager) {
   const handleError = (e: mixed, isFatal: boolean) => {
     try {
-      // TODO(T196834299): We should really use a c++ turbomodule for this
-      if (
-        !global.RN$handleException ||
-        !global.RN$handleException(e, isFatal)
-      ) {
-        ExceptionsManager.handleException(e, isFatal);
-      }
+      ExceptionsManager.handleException(e, isFatal);
     } catch (ee) {
       console.log('Failed to print error: ', ee.message);
       throw e;
