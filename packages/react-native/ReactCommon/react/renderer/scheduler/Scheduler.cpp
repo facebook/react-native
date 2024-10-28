63292b3f74d8b72bcfb9202f0ad0897e20e1b0c7
Samuel Susla <samuelsusla@meta.com>
2024-10-28 10:31:53 -0700

    batch sync events based on event beats second try
    Summary:
    changelog: [internal]
    
    Batching sync events into a single JavaScript task. The batching mechanism uses EventBeat's induce method.
    
    Reviewed By: javache
    
    Differential Revision: D64930580

--- packages/react-native/ReactCommon/react/renderer/scheduler/Scheduler.cpp
+++ packages/react-native/ReactCommon/react/renderer/scheduler/Scheduler.cpp
@@ -102,7 +102,6 @@ Scheduler::Scheduler(
       EventQueueProcessor(
           eventPipe, eventPipeConclusion, statePipe, eventPerformanceLogger_),
       std::move(eventBeat),
-      *runtimeScheduler,
       statePipe,
       eventPerformanceLogger_);
 
