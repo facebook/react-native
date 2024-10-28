f170634a0ab7850f82434c4ad5d5e6491f2dc0bf
Mateo Guzmán <info@mateoguzman.net>
2024-10-28 23:54:56 +0100

    fix(ios): adding missing border style in ios enum

--- packages/react-native/React/Fabric/Mounting/ComponentViews/View/RCTViewComponentView.mm
+++ packages/react-native/React/Fabric/Mounting/ComponentViews/View/RCTViewComponentView.mm
@@ -731,6 +731,8 @@ static CALayerCornerCurve CornerCurveFromBorderCurve(BorderCurve borderCurve)
 static RCTBorderStyle RCTBorderStyleFromBorderStyle(BorderStyle borderStyle)
 {
   switch (borderStyle) {
+    case BorderStyle::None:
+      return RCTBorderStyleUnset;
     case BorderStyle::Solid:
       return RCTBorderStyleSolid;
     case BorderStyle::Dotted:
