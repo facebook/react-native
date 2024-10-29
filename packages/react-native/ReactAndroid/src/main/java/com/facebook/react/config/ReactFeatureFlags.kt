4076dbfc8651fcd193b886bf63b23728fad466d7
David Vacca <dvacca@meta.com>
2024-10-28 18:49:19 -0700

    Convert ReactFeatureFlags to Kotlin (#47260)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/47260
    
    Convert ReactFeatureFlags to Kotlin
    
    changeLog: [Android][Changed] Migrate ReactFeatureFlags to Kotlin
    
    Reviewed By: shwanton, javache
    
    Differential Revision: D65070217
    
    fbshipit-source-id: 8939326c1e1e7cd634347a58123ad8c0795c309e

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/config/ReactFeatureFlags.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/config/ReactFeatureFlags.kt
@@ -5,22 +5,20 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.config;
+package com.facebook.react.config
 
-import com.facebook.infer.annotation.Nullsafe;
-import com.facebook.proguard.annotations.DoNotStripAny;
+import com.facebook.proguard.annotations.DoNotStripAny
+import kotlin.jvm.JvmField
 
 /**
  * Hi there, traveller! This configuration class is not meant to be used by end-users of RN. It
  * contains mainly flags for features that are either under active development and not ready for
  * public consumption, or for use in experiments.
  *
- * <p>These values are safe defaults and should not require manual changes.
+ * These values are safe defaults and should not require manual changes.
  */
-@Nullsafe(Nullsafe.Mode.LOCAL)
-@Deprecated(since = "Use com.facebook.react.internal.featureflags.ReactNativeFeatureFlags instead.")
+@Deprecated("Use com.facebook.react.internal.featureflags.ReactNativeFeatureFlags instead.")
 @DoNotStripAny
-public class ReactFeatureFlags {
-
-  public static boolean dispatchPointerEvents = false;
+public object ReactFeatureFlags {
+  @JvmField public var dispatchPointerEvents: Boolean = false
 }
