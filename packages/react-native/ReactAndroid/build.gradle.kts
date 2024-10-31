b9ce38249be8dc44449b61dc95baca0590e4e30b
Alan Lee <alanlee2@meta.com>
2024-10-31 13:45:24 -0700

    Add build flag to support 16KB page size (#47042)
    Summary:
    X-link: https://github.com/facebook/hermes/pull/1557
    
    
    Add native build flags to support 16KB page size
    
    - https://developer.android.com/guide/practices/page-sizes#compile-r27-higher
    
    Changelog:
    [Android][Added] add cmake arguments to support 16KB page size for native libraries
    
    Differential Revision: D64446876

--- packages/react-native/ReactAndroid/build.gradle.kts
+++ packages/react-native/ReactAndroid/build.gradle.kts
@@ -533,9 +533,7 @@ android {
             "-DREACT_BUILD_DIR=$buildDir",
             "-DANDROID_STL=c++_shared",
             "-DANDROID_TOOLCHAIN=clang",
-            // Due to https://github.com/android/ndk/issues/1693 we're losing Android
-            // specific compilation flags. This can be removed once we moved to NDK 25/26
-            "-DANDROID_USE_LEGACY_TOOLCHAIN_FILE=ON")
+            "-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON")
 
         targets(
             "reactnative",
