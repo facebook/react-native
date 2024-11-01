0c4b65d31f891c566dc3d7c54695bdfa31167417
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-11-01 13:15:19 +0100

    comment on cwd usage

--- packages/react-native/cli.js
+++ packages/react-native/cli.js
@@ -24,8 +24,14 @@ const deprecated = () => {
 };
 
 function findCommunityCli(startDir = process.cwd()) {
-  // In a pnpm, we won't be able to find `@react-native-community/cli` starting
-  // from the `react-native` directory. Instead, we must use the project's root.
+  // In monorepos, we cannot make any assumptions on where
+  // `@react-native-community/cli` gets installed. The safest way to find it
+  // (barring adding an optional peer dependency) is to start from the project
+  // root.
+  //
+  // Note that we're assuming that the current working directory is the project
+  // root. This is also what `@react-native-community/cli` assumes (see
+  // https://github.com/react-native-community/cli/blob/14.x/packages/cli-tools/src/findProjectRoot.ts).
   const options = { paths: [startDir] };
   const rncli = require.resolve('@react-native-community/cli', options);
   return require(rncli);
