9df8ac8186dd795e5dc5086b672e7df01078523a
Nicola Corti <ncor@meta.com>
2024-11-04 07:57:55 -0800

    Make ReactDebugOverlayTags and DebugOverlayTags internal (#47372)
    Summary:
    
    Those 2 classes are not supposed to be exposed externally, so I'm making them internal.
    The were never part of the public API so I'm not marking this commit as [BREAKING]
    
    Changelog:
    [Android] [Changed] - Make ReactDebugOverlayTags and DebugOverlayTags internal
    
    Reviewed By: fabriziocucci
    
    Differential Revision: D65420257

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/debug/tags/ReactDebugOverlayTags.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/debug/tags/ReactDebugOverlayTags.kt
@@ -11,35 +11,35 @@ import android.graphics.Color
 import com.facebook.debug.debugoverlay.model.DebugOverlayTag
 
 /** Category for debug overlays. */
-public object ReactDebugOverlayTags {
+internal object ReactDebugOverlayTags {
   @JvmField
-  public val PERFORMANCE: DebugOverlayTag =
+  val PERFORMANCE: DebugOverlayTag =
       DebugOverlayTag("Performance", "Markers for Performance", Color.GREEN)
   @JvmField
-  public val NAVIGATION: DebugOverlayTag =
+  val NAVIGATION: DebugOverlayTag =
       DebugOverlayTag("Navigation", "Tag for navigation", Color.rgb(0x9C, 0x27, 0xB0))
   @JvmField
-  public val RN_CORE: DebugOverlayTag =
+  val RN_CORE: DebugOverlayTag =
       DebugOverlayTag("RN Core", "Tag for React Native Core", Color.BLACK)
   @JvmField
-  public val BRIDGE_CALLS: DebugOverlayTag =
+  val BRIDGE_CALLS: DebugOverlayTag =
       DebugOverlayTag("Bridge Calls", "JS to Java calls (warning: this is spammy)", Color.MAGENTA)
   @JvmField
-  public val NATIVE_MODULE: DebugOverlayTag =
+  val NATIVE_MODULE: DebugOverlayTag =
       DebugOverlayTag("Native Module", "Native Module init", Color.rgb(0x80, 0x00, 0x80))
   @JvmField
-  public val UI_MANAGER: DebugOverlayTag =
+  val UI_MANAGER: DebugOverlayTag =
       DebugOverlayTag(
           "UI Manager",
           "UI Manager View Operations (requires restart\nwarning: this is spammy)",
           Color.CYAN)
   @JvmField
-  public val FABRIC_UI_MANAGER: DebugOverlayTag =
+  val FABRIC_UI_MANAGER: DebugOverlayTag =
       DebugOverlayTag("FabricUIManager", "Fabric UI Manager View Operations", Color.CYAN)
   @JvmField
-  public val FABRIC_RECONCILER: DebugOverlayTag =
+  val FABRIC_RECONCILER: DebugOverlayTag =
       DebugOverlayTag("FabricReconciler", "Reconciler for Fabric", Color.CYAN)
   @JvmField
-  public val RELAY: DebugOverlayTag =
+  val RELAY: DebugOverlayTag =
       DebugOverlayTag("Relay", "including prefetching", Color.rgb(0xFF, 0x99, 0x00))
 }
