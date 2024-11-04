bda15ffa66b1f7648d2b1cedaa1e4021fef8152c
Nicola Corti <ncor@meta.com>
2024-11-04 06:26:29 -0800

    Stable API - Make ReactDevToolsSettingsManagerModule and ReactDevToolsRuntimeSettingsModule internal
    Summary:
    Users should not depend on `ReactDevToolsSettingsManagerModule` directly hence I'm making it internal.
    I expect to breakages in OSS as there no usages at all:
    https://github.com/search?type=code&q=NOT+is%3Afork+NOT+org%3Afacebook+com.facebook.react.modules.reactdevtoolssettings.ReactDevToolsSettingsManagerModule
    https://github.com/search?type=code&q=NOT+is%3Afork+NOT+org%3Afacebook+com.facebook.react.modules.devtoolsruntimesettings.ReactDevToolsRuntimeSettingsModule
    
    Changelog:
    [Android] [Breaking] - Stable API - Make ReactDevToolsSettingsManagerModule and ReactDevToolsRuntimeSettingsModule internal
    
    Differential Revision: D65421030

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/reactdevtoolssettings/ReactDevToolsSettingsManagerModule.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/reactdevtoolssettings/ReactDevToolsSettingsManagerModule.kt
@@ -14,20 +14,20 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 
 @ReactModule(name = NativeReactDevToolsSettingsManagerSpec.NAME)
-public class ReactDevToolsSettingsManagerModule(reactContext: ReactApplicationContext) :
+internal class ReactDevToolsSettingsManagerModule(reactContext: ReactApplicationContext) :
     NativeReactDevToolsSettingsManagerSpec(reactContext) {
 
   private val sharedPreferences: SharedPreferences =
       reactContext.getSharedPreferences(SHARED_PREFERENCES_PREFIX, Context.MODE_PRIVATE)
 
-  public override fun setGlobalHookSettings(settings: String): Unit =
+  override fun setGlobalHookSettings(settings: String): Unit =
       sharedPreferences.edit().putString(KEY_HOOK_SETTINGS, settings).apply()
 
-  public override fun getGlobalHookSettings(): String? =
+  override fun getGlobalHookSettings(): String? =
       sharedPreferences.getString(KEY_HOOK_SETTINGS, null)
 
-  public companion object {
-    public const val NAME: String = NativeReactDevToolsSettingsManagerSpec.NAME
+  companion object {
+    const val NAME: String = NativeReactDevToolsSettingsManagerSpec.NAME
     private const val SHARED_PREFERENCES_PREFIX = "ReactNative__DevToolsSettings"
     private const val KEY_HOOK_SETTINGS = "HookSettings"
   }
