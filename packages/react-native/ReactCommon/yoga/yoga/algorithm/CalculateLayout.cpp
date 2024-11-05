027b8032c20b507c1149f526dc1c864f76ac4f3e
Joe Vilches <joevilches@meta.com>
2024-11-04 16:02:37 -0800

    Modify private apis to set, store, and get intrinsic sizing keywords (#46938)
    Summary:
    X-link: https://github.com/facebook/yoga/pull/1721
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/46938
    
    The private internals of how we store styles needed to change a bit to support 3 new keyword values. Right now the only other keyword that can be stored is `auto`. As a result there isn't much fancy logic to support storing this and its just stored as a specific type inside of `StyleValueHandle`. There are only 3 bits for types (8 values), so it is not sustainable to just stuff every keyword in there. So the change writes the keyword as a value with a new `keyword` `Type`.
    
    I chose not to put `auto` in there even though it is a keyword since it is a hot path, I did not want to regress perf when I did not need to.
    
    I also make a new `StyleSizeValue` class to store size values - so values for `width`, `height`, etc. This way these new keywords are kept specific to sizes and we will not be able to create, for example, a margin: `max-content`.
    
    Changelog: [Internal]
    
    Reviewed By: NickGerleman
    
    Differential Revision: D63927512
    
    fbshipit-source-id: 7285469d37ac4b05226183b56275c77f0c06996c

--- packages/react-native/ReactCommon/yoga/yoga/algorithm/CalculateLayout.cpp
+++ packages/react-native/ReactCommon/yoga/yoga/algorithm/CalculateLayout.cpp
@@ -749,7 +749,7 @@ static float distributeFreeSpaceSecondPass(
           marginCross;
       const bool isLoosePercentageMeasurement =
           currentLineChild->getProcessedDimension(dimension(crossAxis))
-                  .unit() == Unit::Percent &&
+              .isPercent() &&
           sizingModeCrossDim != SizingMode::StretchFit;
       childCrossSizingMode =
           yoga::isUndefined(childCrossSize) || isLoosePercentageMeasurement
