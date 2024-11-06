bf07a6efd5c14096ad701edf0d590d8a98241006
David Vacca <dvacca@meta.com>
2024-11-05 21:55:35 -0800

    Migrate ReactUnimplementedViewManager to use ViewManagerInterface (#47392)
    Summary:
    
    Migrate ReactUnimplementedViewManager to use ViewManagerInterface
    
    changelog: [internal] internal
    
    Reviewed By: cortinico
    
    Differential Revision: D65428644

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/unimplementedview/ReactUnimplementedViewManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/unimplementedview/ReactUnimplementedViewManager.kt
@@ -10,11 +10,21 @@ package com.facebook.react.views.unimplementedview
 import com.facebook.react.module.annotations.ReactModule
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.ViewGroupManager
+import com.facebook.react.uimanager.ViewManagerDelegate
 import com.facebook.react.uimanager.annotations.ReactProp
+import com.facebook.react.viewmanagers.UnimplementedNativeViewManagerDelegate
+import com.facebook.react.viewmanagers.UnimplementedNativeViewManagerInterface
 
 /** ViewManager for [ReactUnimplementedView] to represent a component that is not yet supported. */
 @ReactModule(name = ReactUnimplementedViewManager.REACT_CLASS)
-internal class ReactUnimplementedViewManager : ViewGroupManager<ReactUnimplementedView>() {
+internal class ReactUnimplementedViewManager :
+    ViewGroupManager<ReactUnimplementedView>(),
+    UnimplementedNativeViewManagerInterface<ReactUnimplementedView> {
+
+  private val delegate: ViewManagerDelegate<ReactUnimplementedView> =
+      UnimplementedNativeViewManagerDelegate(this)
+
+  public override fun getDelegate(): ViewManagerDelegate<ReactUnimplementedView> = delegate
 
   protected override fun createViewInstance(
       reactContext: ThemedReactContext
@@ -23,8 +33,8 @@ internal class ReactUnimplementedViewManager : ViewGroupManager<ReactUnimplement
   public override fun getName(): String = REACT_CLASS
 
   @ReactProp(name = "name")
-  public fun setName(view: ReactUnimplementedView, name: String) {
-    view.setName(name)
+  public override fun setName(view: ReactUnimplementedView, name: String?): Unit {
+    view.setName(name ?: "<null component name>")
   }
 
   internal companion object {
