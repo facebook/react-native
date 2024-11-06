b04f5fbfba61c5eb1808c4bcc795b03f6a950a05
Pieter De Baets <pieterdb@meta.com>
2024-11-06 06:38:47 -0800

    Add Binding.h header back for backwards compat (#47453)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/47453
    
    `react-native-screens` [depends on this header](https://github.com/software-mansion/react-native-screens/blob/main/android/src/main/cpp/NativeProxy.cpp#L2) and this is an unnecessary API breakage.
    
    Changelog: [Android][Fixed] Added back `<react/fabric/Binding.h>` header.
    
    Reviewed By: fabriziocucci
    
    Differential Revision: D65534176
    
    fbshipit-source-id: ce252f29be1c5322f44e20b6e36ab51db9c7a8ef

---
+++ packages/react-native/ReactAndroid/src/main/jni/react/fabric/Binding.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+// This header exists for backwards compatibility with libraries importing
+// <react/fabric/Binding.h>
+#include "FabricUIManagerBinding.h"
