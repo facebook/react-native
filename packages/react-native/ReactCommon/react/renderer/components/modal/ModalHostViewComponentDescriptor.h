8d53acdd2271ae95eacc218a476a6902fa4d6606
Dawid <dawid.malecki21@gmail.com>
2024-11-07 16:14:38 +0100

    Android only

--- packages/react-native/ReactCommon/react/renderer/components/modal/ModalHostViewComponentDescriptor.h
+++ packages/react-native/ReactCommon/react/renderer/components/modal/ModalHostViewComponentDescriptor.h
@@ -10,7 +10,10 @@
 #include <glog/logging.h>
 #include <react/renderer/components/modal/ModalHostViewShadowNode.h>
 #include <react/renderer/core/ConcreteComponentDescriptor.h>
+
+#ifdef ANDROID
 #include <react/jni/ReadableNativeMap.h>
+#endif
 
 namespace facebook::react {
 
@@ -31,22 +34,24 @@ class ModalHostViewComponentDescriptor final
             *shadowNode.getState())
             .getData();
 
-      const jni::global_ref<jobject>& fabricUIManager =
-              contextContainer_->at<jni::global_ref<jobject>>("FabricUIManager");
+#ifdef ANDROID
+    const jni::global_ref<jobject>& fabricUIManager =
+            contextContainer_->at<jni::global_ref<jobject>>("FabricUIManager");
 
-      static auto getDisplaySizes =
-              jni::findClassStatic("com/facebook/react/fabric/FabricUIManager")
-                      ->getMethod<NativeMap::javaobject(jint)>("getDisplaySizes");
+    static auto getDisplaySizes =
+            jni::findClassStatic("com/facebook/react/fabric/FabricUIManager")
+                    ->getMethod<NativeMap::javaobject(jint)>("getDisplaySizes");
 
-      auto sizes = getDisplaySizes(fabricUIManager, -1);
-      if (sizes != nullptr) {
-          auto dynamicMap = cthis(sizes)->consume();
-          auto height = static_cast<Float>(dynamicMap.getDefault("height", 0).getDouble());
-          auto width = static_cast<Float>(dynamicMap.getDefault("width", 0).getDouble());
+    auto sizes = getDisplaySizes(fabricUIManager, -1);
+    if (sizes != nullptr) {
+        auto dynamicMap = cthis(sizes)->consume();
+        auto height = static_cast<Float>(dynamicMap.getDefault("height", 0).getDouble());
+        auto width = static_cast<Float>(dynamicMap.getDefault("width", 0).getDouble());
 
-          auto &modalHostViewShadowNode = dynamic_cast<ModalHostViewShadowNode &>(shadowNode);
-          modalHostViewShadowNode.setScreenSize(width, height);
-      }
+        auto &modalHostViewShadowNode = dynamic_cast<ModalHostViewShadowNode &>(shadowNode);
+        modalHostViewShadowNode.setScreenSize(width, height);
+    }
+#endif
 
     layoutableShadowNode.setSize(
         Size{stateData.screenSize.width, stateData.screenSize.height});
