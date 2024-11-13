177bf4d043ce862911e477e0f0039214d8541cfd
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-11-13 12:20:16 -0800

    feat: handle negative values in enums (#47452)
    Summary:
    This PR adds support for negative values in enums.
    
    Currently when we try to use an enum with negative value:
    
    ```ts
    enum MyEnum {
      ZERO = 0,
      POSITIVE = 1,
      NEGATIVE = -1,
    }
    
    export interface Spec extends TurboModule {
      useArg(arg: MyEnum): void;
    }
    
    export default TurboModuleRegistry.get<Spec>('Foo');
    ```
    
    It will fail:
    
    ```
    Enum values can not be mixed. They all must be either blank, number, or string values.
    ```
    
    This is because negative values are parsed as `UnaryExpressions` which have `-` operator in front and value as argument.
    
    With the new approach codegen properly generates enums with negative values.
    
    ## Changelog:
    
    [GENERAL] [ADDED] - Codegen: Support negative values in enums
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/47452
    
    Test Plan: I've added tests to see if everything is working properly
    
    Reviewed By: vzaidman
    
    Differential Revision: D65887888
    
    Pulled By: elicwhite
    
    fbshipit-source-id: edb25f663dc58afa68c69cb84a47cfc67fc1f7e7

--- packages/react-native-codegen/src/parsers/flow/modules/__test_fixtures__/failures.js
+++ packages/react-native-codegen/src/parsers/flow/modules/__test_fixtures__/failures.js
@@ -265,6 +265,35 @@ export interface Spec extends TurboModule {
 export default TurboModuleRegistry.getEnforcing<Spec>('MixedValuesEnumNativeModule');
 `;
 
+const NUMERIC_VALUES_ENUM_NATIVE_MODULE = `
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+import type {TurboModule} from '../RCTExport';
+import * as TurboModuleRegistry from '../TurboModuleRegistry';
+
+export enum SomeEnum {
+  NUM = 1,
+  NEGATIVE = -1,
+  SUBFACTORIAL = !5,
+}
+
+export interface Spec extends TurboModule {
+  +getEnums: (a: SomeEnum) => string;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('NumericValuesEnumNativeModule');
+`;
+
 const MAP_WITH_EXTRA_KEYS_NATIVE_MODULE = `
 /**
  * Copyright (c) Meta Platforms, Inc. and affiliates.
@@ -304,5 +333,6 @@ module.exports = {
   TWO_NATIVE_EXTENDING_TURBO_MODULE,
   EMPTY_ENUM_NATIVE_MODULE,
   MIXED_VALUES_ENUM_NATIVE_MODULE,
+  NUMERIC_VALUES_ENUM_NATIVE_MODULE,
   MAP_WITH_EXTRA_KEYS_NATIVE_MODULE,
 };
