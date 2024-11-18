c18f678ba1c1921731ce5d5702695df21ad781da
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2024-11-18 02:48:02 -0800

    fix(iOS): unify make queryRootName in prefetchImageWithMetadata optional (#47532)
    Summary:
    in `prefetchImageWithMetadata`'s implementation in ObjC, the method's `queryRootName` is treated as being nullable. The image spec for it in JS (and the Codegened ObjC header that gets built on top of it) treat the field as not nullable. This change makes the field nullable in the spec to match up what we have in the implementation.
    
    I also noticed that the method is not defined in the [Image props](https://reactnative.dev/docs/image) on the RN website, so perhaps we should add this there as well.
    
    ## Changelog:
    
    [IOS] [CHANGED] - make `prefetchImageWithMetadata`'s `queryRootName` nullable in the spec
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/47532
    
    Test Plan:
    yarn test:
    <img width="1576" alt="Screenshot 2024-11-09 at 00 36 30" src="https://github.com/user-attachments/assets/4162ff79-1388-4f6f-9576-256fd9011fcf">
    It has no iOS specific tests, so nothing to run on that side
    
    Differential Revision: D65761208

--- packages/react-native/Libraries/Image/RCTImageLoader.mm
+++ packages/react-native/Libraries/Image/RCTImageLoader.mm
@@ -1227,7 +1227,7 @@ RCT_EXPORT_METHOD(prefetchImage
 
 RCT_EXPORT_METHOD(prefetchImageWithMetadata
                   : (NSString *)uri queryRootName
-                  : (NSString *)queryRootName rootTag
+                  : (nullable NSString *)queryRootName rootTag
                   : (double)rootTag resolve
                   : (RCTPromiseResolveBlock)resolve reject
                   : (RCTPromiseRejectBlock)reject)
