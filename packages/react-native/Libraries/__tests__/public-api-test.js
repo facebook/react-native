13462570bb5bd78b4fdcd1a2a3cefc32fccf7caa
Alan Lee <alanlee2@meta.com>
2024-11-19 05:46:54 -0800

    NewAppScreen from public API test
    Summary:
    NewAppScreen should be removed from public API test
    
    Changelog: [Internal]
    
    Differential Revision: D66165484

--- packages/react-native/Libraries/__tests__/public-api-test.js
+++ packages/react-native/Libraries/__tests__/public-api-test.js
@@ -26,6 +26,7 @@ const IGNORE_PATTERNS = [
   '**/*.fb.js',
   '**/*.macos.js',
   '**/*.windows.js',
+  'Libraries/NewAppScreen/**',
   // Non source files
   'Libraries/Renderer/implementations/**',
   'Libraries/Renderer/shims/**',
