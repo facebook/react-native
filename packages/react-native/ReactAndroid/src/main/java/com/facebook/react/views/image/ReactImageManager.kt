528a9425967d1523d748a373b7659d270f1a847a
Dmitry Rykun <dmitryrykun@meta.com>
2024-11-19 07:29:12 -0800

    Unify defaultSource and defaultSrc ImageProp (#47710)
    Summary:
    
    There is Android code that supports remapping `defaultSrc` -> `defaultSource` for `ImageNativeComponent`.
    The docs reference this prop as `defaultSource`: https://reactnative.dev/docs/next/image#defaultsource
    It is not referenced as `defaultSrc` anywhere. Let's unify it as `defaultSource` across both platforms.
    Changelog: [Internal]
    
    Differential Revision: D65819218

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/image/ReactImageManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/image/ReactImageManager.kt
@@ -124,8 +124,7 @@ public constructor(
     }
   }
 
-  // In JS this is Image.props.defaultSource
-  @ReactProp(name = "defaultSrc")
+  @ReactProp(name = "defaultSource")
   public fun setDefaultSource(view: ReactImageView, source: String?) {
     view.setDefaultSource(source)
   }
