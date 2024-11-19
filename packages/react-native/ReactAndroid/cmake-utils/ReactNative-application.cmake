9946838beddac549053a3ba3eb3b1b49c22a3fd9
Blake Friedman <blakef@meta.com>
2024-11-19 05:33:45 -0800

    CMake Windows path normalization (#47702)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/47702
    
    Use `file(TO_CMAKE_PATH` to normalize paths, and normalizing `input_SRC` as it's already a CMake path.
    
    Changelog: [Internal]
    
    Reviewed By: rshest
    
    Differential Revision: D66101321
    
    fbshipit-source-id: e81af40551d2777901f9c7cf9a4175f2bce76ec8

--- packages/react-native/ReactAndroid/cmake-utils/ReactNative-application.cmake
+++ packages/react-native/ReactAndroid/cmake-utils/ReactNative-application.cmake
@@ -31,19 +31,12 @@ if(CCACHE_FOUND)
 endif(CCACHE_FOUND)
 
 set(BUILD_DIR ${PROJECT_BUILD_DIR})
-if(CMAKE_HOST_WIN32)
-    string(REPLACE "\\" "/" BUILD_DIR ${BUILD_DIR})
-    string(REPLACE "\\" "/" REACT_ANDROID_DIR ${REACT_ANDROID_DIR})
-endif()
+file(TO_CMAKE_PATH "${BUILD_DIR}" BUILD_DIR)
+file(TO_CMAKE_PATH "${REACT_ANDROID_DIR}" REACT_ANDROID_DIR)
 
 file(GLOB input_SRC CONFIGURE_DEPENDS
         ${REACT_ANDROID_DIR}/cmake-utils/default-app-setup/*.cpp
         ${BUILD_DIR}/generated/autolinking/src/main/jni/*.cpp)
-
-# Ensure that `input_SRC` paths use forward slashes
-foreach(path IN LISTS input_SRC)
-    string(REPLACE "\\" "/" path "${path}")
-endforeach()
 
 add_library(${CMAKE_PROJECT_NAME} SHARED ${input_SRC})
 
