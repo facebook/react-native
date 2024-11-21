3de57c29338dea3c87fbd3bd632d9351fb9045fa
David Vacca <515103+mdvacca@users.noreply.github.com>
2024-11-20 16:22:52 -0800

    Migrate ReactCompoundView to kotlin
    Summary:
    Migrate ReactCompoundView to kotlin
    
    changelog: [internal] internal
    
    Differential Revision: D66217094

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ReactCompoundView.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ReactCompoundView.kt
@@ -5,13 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.uimanager;
-
-import android.view.View;
+package com.facebook.react.uimanager
 
 /**
  * This interface should be implemented be native {@link View} subclasses that can represent more
- * than a single react node (e.g. TextView). It is use by touch event emitter for determining the
+ * than a single react node (e.g. [TextView]). It is use by touch event emitter for determining the
  * react tag of the inner-view element that was touched.
  */
 public interface ReactCompoundView {
@@ -22,5 +20,5 @@ public interface ReactCompoundView {
    * @param touchX the X touch coordinate relative to the view
    * @param touchY the Y touch coordinate relative to the view
    */
-  int reactTagForTouch(float touchX, float touchY);
+  public fun reactTagForTouch(touchX: Float, touchY: Float): Int
 }
