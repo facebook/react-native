221d1eceda0e5ab870e96dcdd26e22ab17a3870c
Nishan <nishanbende@gmail.com>
2024-11-20 17:17:52 -0800

    fix: start and end points linear gradient algorithm (#47003)
    Summary:
    - Current implementation does not follow CSS spec for rectangle boxes with corner angles. It is using non spec compliant algorithm to calculate start and end points. This PR follows the spec compliant algorithm to implement and makes sure Web, iOS and Android gradients are identical with corner angles.
    - Also, currently it is using `CAGradientLayer` which does not support spec compliant start and end points i.e. start and end point can be outside of rectangle bounds. This leads to inconsistent gradients on iOS for corner angles compared to web and android. So this PR replaces it with `CGGradient`.
    - I have also moved some files to make it easier to add more background image types in future.
    
    ## Changelog:
    
    [GENERAL] [FIXED] - Linear gradient start and end point algorithm.
    
    <!-- Help reviewers and the release process by writing your own changelog entry.
    
    Pick one each for the category and type tags:
    
    For more details, see:
    https://reactnative.dev/contributing/changelogs-in-pull-requests
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/47003
    
    Test Plan:
    - Added multiple gradient example which should be identical in all platforms (Web, iOS and Android) and tested thoroughly on all platforms. I think some visual test cases can help here.
    - I have referred to [blink's](https://github.com/chromium/chromium/blob/main/third_party/blink/renderer/core/css/css_gradient_value.cc) implementation.
    
    ## Aside
    
    Took a while to understand the [spec](https://www.w3.org/TR/css-images-3/#corner-gradient-example), but felt great after getting it. Gradients should be 100% identical on all platforms now. Sorry i missed testing cornered angles + rectangles earlier and I found out it is inconsistent on platforms just this weekend  😅
    
    <img width="1389" alt="Screenshot 2024-10-14 at 12 24 45 AM" src="https://github.com/user-attachments/assets/2f61eb87-502b-4b8c-88f3-d8a3cca9a7a3">
    
    Reviewed By: joevilches
    
    Differential Revision: D64497127
    
    Pulled By: jorge-cab
    
    fbshipit-source-id: 2647176ae2ee74b6c71f9061465b07dccdabcfc1

--- packages/rn-tester/js/examples/LinearGradient/LinearGradientExample.js
+++ packages/rn-tester/js/examples/LinearGradient/LinearGradientExample.js
@@ -62,6 +62,43 @@ exports.examples = [
     },
   },
   {
+    title: 'Linear Gradient with corner angle',
+    description: 'Rectangular Linear gradient with corner angle',
+    render(): React.Node {
+      return (
+        <GradientBox
+          style={{
+            experimental_backgroundImage: 'linear-gradient(45deg, red, blue);',
+            height: 300,
+            width: 140,
+          }}
+          testID="linear-gradient-rectangular-with-corner-angle"
+        />
+      );
+    },
+  },
+  {
+    title: 'Multiple linear gradients',
+    render(): React.Node {
+      return (
+        <GradientBox
+          testID="linear-gradient-multiple"
+          style={{
+            experimental_backgroundImage: `
+                  linear-gradient(0deg, white, rgba(238, 64, 53, 0.8), rgba(238, 64, 53, 0) 70%), 
+    linear-gradient(45deg, white, rgba(243, 119, 54, 0.8), rgba(243, 119, 54, 0) 70%), 
+    linear-gradient(90deg, white, rgba(253, 244, 152, 0.8), rgba(253, 244, 152, 0) 70%), 
+    linear-gradient(135deg, white, rgba(123, 192, 67, 0.8), rgba(123, 192, 67, 0) 70%), 
+    linear-gradient(180deg, white, rgba(3, 146, 207, 0.8), rgba(3, 146, 207, 0) 70%);
+
+            `,
+            borderRadius: 16,
+          }}
+        />
+      );
+    },
+  },
+  {
     title: 'Diagonal Gradient',
     description: 'Linear gradient from top-left to bottom-right',
     render(): React.Node {
