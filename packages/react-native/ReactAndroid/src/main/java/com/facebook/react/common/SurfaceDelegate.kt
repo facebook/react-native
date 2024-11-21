33a8c22340ac766777297d41a482b527a7118432
David Vacca <515103+mdvacca@users.noreply.github.com>
2024-11-20 22:59:35 -0800

    Migrate SurfaceDelegate to Kotlin (#47687)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/47687
    
    Migrate SurfaceDelegate to Kotlin
    
    changelog: [internal] internal
    
    Reviewed By: javache
    
    Differential Revision: D66134455

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/common/SurfaceDelegate.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/common/SurfaceDelegate.kt
@@ -5,42 +5,39 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.common;
-
-import com.facebook.infer.annotation.Nullsafe;
+package com.facebook.react.common
 
 /**
  * Interface for handling a surface in React Native. In mobile platform a surface can be any
- * container that holds some {@link View}. For example, a Dialog can be a surface to wrap content
- * view object as needed. In VR platform, a surface is provided by Shell panel app sdk, which
- * requires custom logic to show/hide. NativeModules requires a surface will delegate interactions
- * with the surface to a SurfaceDelegate.
+ * container that holds some [View]. For example, a Dialog can be a surface to wrap content view
+ * object as needed. In VR platform, a surface is provided by Shell panel app sdk, which requires
+ * custom logic to show/hide. NativeModules requires a surface will delegate interactions with the
+ * surface to a SurfaceDelegate.
  */
-@Nullsafe(Nullsafe.Mode.LOCAL)
 public interface SurfaceDelegate {
   /**
    * Create the React content view that uses the appKey as the React application name
    *
    * @param appKey
    */
-  void createContentView(String appKey);
+  public fun createContentView(appKey: String): Unit
 
   /**
    * Check if the content view is created and ready to be shown
    *
    * @return true if the content view is ready to be shown
    */
-  boolean isContentViewReady();
+  public fun isContentViewReady(): Boolean
 
   /** Destroy the React content view to avoid memory leak */
-  void destroyContentView();
+  public fun destroyContentView(): Unit
 
   /** Show the surface containing the React content view */
-  void show();
+  public fun show(): Unit
 
   /** Hide the surface containing the React content view */
-  void hide();
+  public fun hide(): Unit
 
   /** Check if the surface is currently showing */
-  boolean isShowing();
+  public fun isShowing(): Boolean
 }
