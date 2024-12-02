91656c3c99c150f0f382e90c2a71b0d96977ddc7
Rubén Norte <rubennorte@meta.com>
2024-12-02 05:22:44 -0800

    Allow tests to specify opt/dev mode (#48022)
    Summary:
    
    Changelog: [internal]
    
    This adds support for Fantom options in tests to configure different aspects of the test execution.
    
    For now, it only supports specifying the mode (dev or opt) so we can try things without having to change the runner (watch mode still works if you change mode :D).
    
    Fantom options are specified as pragmas in the docblock of the test. E.g.:
    
    ```
    /**
     * flow strict-local
     * format
     * fantom_mode opt
     */
    ```
    
    We expect this is mostly going to be used for one-time tests and that regular tests won't specify the mode (they'll just run in dev mode).
    
    Maybe we can evolve this in the future to specify that you want a test to be executed in both modes, to ensure the behavior is consistent in dev/prod.
    
    Reviewed By: rshest
    
    Differential Revision: D66597626

--- jest/integration/runner/utils.js
+++ jest/integration/runner/utils.js
@@ -12,9 +12,59 @@
 import {spawnSync} from 'child_process';
 import crypto from 'crypto';
 import fs from 'fs';
+// $FlowExpectedError[untyped-import]
+import {extract, parse} from 'jest-docblock';
 import os from 'os';
 // $FlowExpectedError[untyped-import]
 import {SourceMapConsumer} from 'source-map';
+
+type DocblockPragmas = {[key: string]: string | string[]};
+type FantomTestMode = 'dev' | 'opt';
+type FantomTestConfig = {
+  mode: FantomTestMode,
+};
+
+const DEFAULT_MODE: FantomTestMode = 'dev';
+
+/**
+ * Extracts the Fantom configuration from the test file, specified as part of
+ * the docblock comment. E.g.:
+ *
+ * ```
+ * /**
+ *  * @flow strict-local
+ *  * @fantom mode:opt
+ *  *
+ * ```
+ *
+ * So far the only supported option is `mode`, which can be 'dev' or 'opt'.
+ */
+export function getFantomTestConfig(testPath: string): FantomTestConfig {
+  const docblock = extract(fs.readFileSync(testPath, 'utf8'));
+  const pragmas = parse(docblock) as DocblockPragmas;
+
+  const config = {
+    mode: DEFAULT_MODE,
+  };
+
+  const maybeMode = pragmas.fantom_mode;
+
+  if (maybeMode != null) {
+    if (Array.isArray(maybeMode)) {
+      throw new Error('Expected a single value for @fantom_mode');
+    }
+
+    const mode = maybeMode;
+
+    if (mode === 'dev' || mode === 'opt') {
+      config.mode = mode;
+    } else {
+      throw new Error(`Invalid Fantom mode: ${mode}`);
+    }
+  }
+
+  return config;
+}
 
 export function getBuckModeForPlatform(enableRelease: boolean = false): string {
   const mode = enableRelease ? 'opt' : 'dev';
