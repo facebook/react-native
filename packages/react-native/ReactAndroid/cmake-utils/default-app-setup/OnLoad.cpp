125b0f47d111b0f047558c99d17c12bfce8fdbc4
Nicola Corti <ncor@meta.com>
2024-12-06 16:34:27 +0000

    Revert "Include autolinkin.h in OnLoad.cpp only if it exists (#47875)"
    This reverts commit 5b2bbb84b14208905fc0dd7eade9ee6ce6e079b7.

--- packages/react-native/ReactAndroid/cmake-utils/default-app-setup/OnLoad.cpp
+++ packages/react-native/ReactAndroid/cmake-utils/default-app-setup/OnLoad.cpp
@@ -29,12 +29,7 @@
 
 #include <DefaultComponentsRegistry.h>
 #include <DefaultTurboModuleManagerDelegate.h>
-#if __has_include("<autolinking.h>")
-#define AUTOLINKING_AVAILABLE 1
 #include <autolinking.h>
-#else
-#define AUTOLINKING_AVAILABLE 0
-#endif
 #include <fbjni/fbjni.h>
 #include <react/renderer/componentregistry/ComponentDescriptorProviderRegistry.h>
 #include <rncore.h>
@@ -61,10 +56,8 @@ void registerComponents(
   REACT_NATIVE_APP_COMPONENT_REGISTRATION(registry);
 #endif
 
-#if AUTOLINKING_AVAILABLE
   // And we fallback to the components autolinked
   autolinking_registerProviders(registry);
-#endif
 }
 
 std::shared_ptr<TurboModule> cxxModuleProvider(
@@ -78,12 +71,8 @@ std::shared_ptr<TurboModule> cxxModuleProvider(
   //   return std::make_shared<NativeCxxModuleExample>(jsInvoker);
   // }
 
-#if AUTOLINKING_AVAILABLE
   // And we fallback to the CXX module providers autolinked
   return autolinking_cxxModuleProvider(name, jsInvoker);
-#endif
-
-  return nullptr;
 }
 
 std::shared_ptr<TurboModule> javaModuleProvider(
@@ -112,12 +101,10 @@ std::shared_ptr<TurboModule> javaModuleProvider(
     return module;
   }
 
-#if AUTOLINKING_AVAILABLE
   // And we fallback to the module providers autolinked
   if (auto module = autolinking_ModuleProvider(name, params)) {
     return module;
   }
-#endif
 
   return nullptr;
 }
