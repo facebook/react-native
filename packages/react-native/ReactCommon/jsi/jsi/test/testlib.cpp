c6f12254d16d87978383c08065a626d437e60450
Chi Tsai <chitsai@meta.com>
2024-12-06 01:35:18 -0800

    Add default getStringData/getPropNameIdData implementation (#47530)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/47530
    
    Adds the default implementation for `getStringData`/`getPropNameIdData`
    for VMs that do not provide their own implementation
    
    Changelog: [Internal]
    
    Reviewed By: neildhar
    
    Differential Revision: D65638889
    
    fbshipit-source-id: 0a97569433c09ffafbd08fec5d9c9fbf5639b778

--- packages/react-native/ReactCommon/jsi/jsi/test/testlib.cpp
+++ packages/react-native/ReactCommon/jsi/jsi/test/testlib.cpp
@@ -1635,6 +1635,35 @@ TEST_P(JSITest, UTF16Test) {
   EXPECT_EQ(str.utf16(rd), u"\uFFFD\u007A");
 }
 
+TEST_P(JSITest, GetStringDataTest) {
+  // This Runtime Decorator is used to test the default getStringData
+  // implementation for VMs that do not provide their own implementation
+  class RD : public RuntimeDecorator<Runtime, Runtime> {
+   public:
+    RD(Runtime& rt) : RuntimeDecorator(rt) {}
+
+    void getStringData(
+        const String& str,
+        void* ctx,
+        void (*cb)(void* ctx, bool ascii, const void* data, size_t num))
+        override {
+      Runtime::getStringData(str, ctx, cb);
+    }
+  };
+
+  RD rd = RD(rt);
+  String str = String::createFromUtf8(rd, "hello👋");
+
+  std::u16string buf;
+  auto cb = [&buf](bool ascii, const void* data, size_t num) {
+    assert(!ascii && "Default implementation is always utf16");
+    buf.append((const char16_t*)data, num);
+  };
+
+  str.getStringData(rd, cb);
+  EXPECT_EQ(buf, str.utf16(rd));
+}
+
 INSTANTIATE_TEST_CASE_P(
     Runtimes,
     JSITest,
