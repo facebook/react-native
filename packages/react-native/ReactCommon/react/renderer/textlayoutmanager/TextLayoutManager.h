ae90dd3aaa982a50339b294e9d17b30cb85dfcb2
Christoph Purrer <chpurrer@meta.com>
2024-12-13 16:12:40 -0800

    Share TextLayoutManager.h across all platforms (#48210)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/48210
    
    [Changelog] [Internal] - Share TextLayoutManager.h across all platforms
    
    The goal of this change is to enable to share `TextLayoutManager.h` across all platforms, the same we do for:
    
    https://github.com/facebook/react-native/blob/main/packages/react-native/ReactCommon/react/renderer/imagemanager/ImageManager.h
    
    Reviewed By: philIip
    
    Differential Revision: D67064488
    
    fbshipit-source-id: 15ee02f3c2351ad65590b5f0dee2f3dbbde4df32

--- packages/react-native/ReactCommon/react/renderer/textlayoutmanager/TextLayoutManager.h
+++ packages/react-native/ReactCommon/react/renderer/textlayoutmanager/TextLayoutManager.h
@@ -7,40 +7,64 @@
 
 #pragma once
 
-#include <memory>
-
 #include <react/renderer/attributedstring/AttributedStringBox.h>
 #include <react/renderer/attributedstring/ParagraphAttributes.h>
 #include <react/renderer/core/LayoutConstraints.h>
 #include <react/renderer/textlayoutmanager/TextLayoutContext.h>
 #include <react/renderer/textlayoutmanager/TextMeasureCache.h>
 #include <react/utils/ContextContainer.h>
+#include <memory>
 
 namespace facebook::react {
 
 class TextLayoutManager;
 
 /*
- * Cross platform facade for iOS-specific RCTTTextLayoutManager.
+ * Cross platform facade for text measurement (e.g. Android-specific
+ * TextLayoutManager)
  */
 class TextLayoutManager {
  public:
   TextLayoutManager(const ContextContainer::Shared& contextContainer);
+  virtual ~TextLayoutManager() = default;
+
+  /*
+   * Not copyable.
+   */
+  TextLayoutManager(const TextLayoutManager&) = delete;
+  TextLayoutManager& operator=(const TextLayoutManager&) = delete;
+
+  /*
+   * Not movable.
+   */
+  TextLayoutManager(TextLayoutManager&&) = delete;
+  TextLayoutManager& operator=(TextLayoutManager&&) = delete;
 
   /*
    * Measures `attributedString` using native text rendering infrastructure.
    */
-  TextMeasurement measure(
+  virtual TextMeasurement measure(
       const AttributedStringBox& attributedStringBox,
       const ParagraphAttributes& paragraphAttributes,
       const TextLayoutContext& layoutContext,
       const LayoutConstraints& layoutConstraints) const;
 
+#ifdef ANDROID
+  /**
+   * Measures an AttributedString on the platform, as identified by some
+   * opaque cache ID.
+   */
+  TextMeasurement measureCachedSpannableById(
+      int64_t cacheId,
+      const ParagraphAttributes& paragraphAttributes,
+      const LayoutConstraints& layoutConstraints) const;
+#endif
+
   /*
    * Measures lines of `attributedString` using native text rendering
    * infrastructure.
    */
-  LinesMeasurements measureLines(
+  virtual LinesMeasurements measureLines(
       const AttributedStringBox& attributedStringBox,
       const ParagraphAttributes& paragraphAttributes,
       const Size& size) const;
@@ -54,16 +78,21 @@ class TextLayoutManager {
       const ParagraphAttributes& paragraphAttributes,
       const Size& size) const;
 
+#ifdef __APPLE__
   /*
    * Returns an opaque pointer to platform-specific TextLayoutManager.
    * Is used on a native views layer to delegate text rendering to the manager.
    */
   std::shared_ptr<void> getNativeTextLayoutManager() const;
+#endif
 
- private:
+ protected:
+  std::shared_ptr<const ContextContainer> contextContainer_;
+#ifdef __APPLE__
   std::shared_ptr<void> nativeTextLayoutManager_;
-  TextMeasureCache textMeasureCache_{};
-  LineMeasureCache lineMeasureCache_{};
+#endif
+  TextMeasureCache textMeasureCache_;
+  LineMeasureCache lineMeasureCache_;
 };
 
 } // namespace facebook::react
