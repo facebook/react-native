081be01a5dd24d0a398c6aa8297575502a17d5ec
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-12-31 07:45:25 -0800

    feat: implement ReactNativeFactory (#46298)
    Summary:
    This PR implements ReactNativeFactory to encapsulate further the logic of creating an instance of React Native for iOS.
    
    This will remove the strong coupling on the RCTAppDelegate and allow us to support Scene Delegate in the future.
    
    The goal is to have a following API:
    
    ```objc
    self.reactNativeFactory = [[RCTReactNativeFactory alloc] initWithDelegate:self];
    
    UIView *rootView = [self.reactNativeFactory.rootViewFactory viewWithModuleName:self.moduleName
                                                                   initialProperties:self.initialProps
                                                                       launchOptions:launchOptions];
    
    // Standard iOS stuff here
    ```
    
    ## Changelog:
    
    [IOS] [ADDED] - implement ReactNativeFactory
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/46298
    
    Test Plan: Test out all the methods of AppDelegate
    
    Reviewed By: huntie
    
    Differential Revision: D67451403
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: 9e73cd996ffc27ca1e3e058b45fc899b1637bdba

--- packages/react-native/Libraries/AppDelegate/RCTAppDelegate.h
+++ packages/react-native/Libraries/AppDelegate/RCTAppDelegate.h
@@ -8,9 +8,9 @@
 #import <React/RCTBridgeDelegate.h>
 #import <React/RCTConvert.h>
 #import <UIKit/UIKit.h>
-#import "RCTArchConfiguratorProtocol.h"
+#import "RCTDefaultReactNativeFactoryDelegate.h"
+#import "RCTReactNativeFactory.h"
 #import "RCTRootViewFactory.h"
-#import "RCTUIConfiguratorProtocol.h"
 
 @class RCTBridge;
 @protocol RCTBridgeDelegate;
@@ -58,64 +58,22 @@ NS_ASSUME_NONNULL_BEGIN
                                                          (const facebook::react::ObjCTurboModule::InitParams &)params
  *   - (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
  */
-@interface RCTAppDelegate : UIResponder <
-                                UIApplicationDelegate,
-                                UISceneDelegate,
-                                RCTBridgeDelegate,
-                                RCTUIConfiguratorProtocol,
-                                RCTArchConfiguratorProtocol>
+@interface RCTAppDelegate : RCTDefaultReactNativeFactoryDelegate <UIApplicationDelegate, UISceneDelegate>
 
 /// The window object, used to render the UViewControllers
 @property (nonatomic, strong, nonnull) UIWindow *window;
+
 @property (nonatomic, nullable) RCTBridge *bridge;
 @property (nonatomic, strong, nullable) NSString *moduleName;
 @property (nonatomic, strong, nullable) NSDictionary *initialProps;
-@property (nonatomic, strong, nonnull) RCTRootViewFactory *rootViewFactory;
-@property (nonatomic, strong) id<RCTDependencyProvider> dependencyProvider;
+@property (nonatomic, strong) RCTReactNativeFactory *reactNativeFactory;
 
 /// If `automaticallyLoadReactNativeWindow` is set to `true`, the React Native window will be loaded automatically.
 @property (nonatomic, assign) BOOL automaticallyLoadReactNativeWindow;
 
 @property (nonatomic, nullable) RCTSurfacePresenterBridgeAdapter *bridgeAdapter;
 
-/**
- * It creates a `RCTBridge` using a delegate and some launch options.
- * By default, it is invoked passing `self` as a delegate.
- * You can override this function to customize the logic that creates the RCTBridge
- *
- * @parameter: delegate - an object that implements the `RCTBridgeDelegate` protocol.
- * @parameter: launchOptions - a dictionary with a set of options.
- *
- * @returns: a newly created instance of RCTBridge.
- */
-- (RCTBridge *)createBridgeWithDelegate:(id<RCTBridgeDelegate>)delegate launchOptions:(NSDictionary *)launchOptions;
-
-/**
- * It creates a `UIView` starting from a bridge, a module name and a set of initial properties.
- * By default, it is invoked using the bridge created by `createBridgeWithDelegate:launchOptions` and
- * the name in the `self.moduleName` variable.
- * You can override this function to customize the logic that creates the Root View.
- *
- * @parameter: bridge - an instance of the `RCTBridge` object.
- * @parameter: moduleName - the name of the app, used by Metro to resolve the module.
- * @parameter: initProps - a set of initial properties.
- *
- * @returns: a UIView properly configured with a bridge for React Native.
- */
-- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
-                          moduleName:(NSString *)moduleName
-                           initProps:(NSDictionary *)initProps;
-
-/// This method returns a map of Component Descriptors and Components classes that needs to be registered in the
-/// new renderer. The Component Descriptor is a string which represent the name used in JS to refer to the native
-/// component. The default implementation returns an empty dictionary. Subclasses can override this method to register
-/// the required components.
-///
-/// @return a dictionary that associate a component for the new renderer with his descriptor.
-- (NSDictionary<NSString *, Class<RCTComponentViewProtocol>> *)thirdPartyFabricComponents;
-
-/// Return the bundle URL for the main bundle.
-- (NSURL *__nullable)bundleURL;
+- (RCTRootViewFactory *)rootViewFactory;
 
 @end
 
