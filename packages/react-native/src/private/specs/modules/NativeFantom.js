381291938956cb0fe264bc1adf2bdbf374bc904c
Rubén Norte <rubennorte@fb.com>
2025-01-03 01:54:08 -0800

    Use codegen for Fantom native module
    Differential Revision: D67759729

--- packages/react-native/src/private/specs/modules/NativeFantom.js
+++ packages/react-native/src/private/specs/modules/NativeFantom.js
@@ -4,13 +4,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict-local
+ * @flow strict
  * @format
  */
 
-import type {TurboModule} from 'react-native/Libraries/TurboModule/RCTExport';
+import type {TurboModule} from '../../../../Libraries/TurboModule/RCTExport';
 
-import {TurboModuleRegistry} from 'react-native';
+import * as TurboModuleRegistry from '../../../../Libraries/TurboModule/TurboModuleRegistry';
 
 // match RenderFormatOptions.h
 export type RenderFormatOptions = {
