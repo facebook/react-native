5be4f843947372eb31ba23c604bc31c3c599e8e1
Ruslan Lesiutin <hoxy@meta.com>
2025-01-10 09:50:06 -0800

    Use Hermes Sampling Profiler API for JS Samples (#48315)
    Summary:
    
    # Changelog: [Internal]
    
    Use newly added API to Hermes.
    
    In terms of Trace Events, we are going to emit:
    - An event for Process metadata, which will be named "Hermes"
    - An event for Thread metadata, on which Hermes runs
    - Complete Trace Event for the same Thread, which is required track to appear in the Timeline UI. I believe duration field is the key for this
    - Trace Event for Profile
    - Trace Event for ProfileChunk, which contains information about nodes and the callstack tree representation
    
    > Important: current implementation emits single ProfileChunk event. I didn't notice any issues in terms of JSON bandwidth while recording 1 minute long session in CPU-heavy application. We should re-evaluate this decision in the future and split Profiles into multiple chunks.
    
    Differential Revision: D67353586

--- packages/react-native/ReactCommon/jsinspector-modern/tracing/PerformanceTracer.h
+++ packages/react-native/ReactCommon/jsinspector-modern/tracing/PerformanceTracer.h
@@ -10,6 +10,7 @@
 #include "CdpTracing.h"
 
 #include <folly/dynamic.h>
+#include <hermes/hermes.h>
 
 #include <functional>
 #include <optional>
@@ -115,9 +116,18 @@ class PerformanceTracer {
   PerformanceTracer& operator=(const PerformanceTracer&) = delete;
   ~PerformanceTracer() = default;
 
+  void populateTraceEventsForHermes(
+      const std::vector<
+          facebook::hermes::sampling_profiler::TraceEventCollectionDataSource>&
+          dataSources);
+  void processSingleHermesDataSource(
+      const facebook::hermes::sampling_profiler::TraceEventCollectionDataSource&
+          dataSource);
+
   folly::dynamic serializeTraceEvent(TraceEvent event) const;
 
   bool tracing_{false};
+  uint64_t tracingStartedTimestamp_{};
   std::unordered_map<std::string, uint64_t> customTrackIdMap_;
   std::vector<TraceEvent> buffer_;
   std::mutex mutex_;
