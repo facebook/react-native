e4d969a4ab71693f38837271356361a3d9df8c5d
Iwo Plaza <plazaiwo@meta.com>
2025-01-14 05:34:18 -0800

    Migrate StyleSheet/*.js to use export statements (#48609)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/48609
    
    # Motivation
    This is an attempt at modernizing the export syntax in some of the files in `Libraries/StyleSheet/`. It will allow these files to get properly ingested by modern Flow tooling.
    
    # This diff
    - Migrates the use of `module.exports` into `export default` for files located in `Libraries/StyleSheet/*.js`. Some files were omitted due to ballooning complexity, but will be addressed in other Diffs.
    - Updating internal *require*s to use ".default", no product code seems to be affected.
    - Migrating `require`s into `import`s where applicable, taking into account the performance implications (context: https://fb.workplace.com/groups/react.technologies.discussions/permalink/3638114866420225/)
    - Updates the current iteration of API snapshots (intended).
    - Updates `react-native-codegen`'s require of processColorArray, analogous to D42346452.
    
    Changelog:
    [General][Breaking] - Deep imports from some files in `StyleSheet/` can break when using the `require()` syntax, but can be easily fixed by appending `.default`
    
    Reviewed By: javache
    
    Differential Revision: D68017325
    
    fbshipit-source-id: 3c5b94742f101db0b2914c91efab6003dba2b61a

--- packages/react-native/Libraries/StyleSheet/__flowtests__/StyleSheet-flowtest.js
+++ packages/react-native/Libraries/StyleSheet/__flowtests__/StyleSheet-flowtest.js
@@ -12,7 +12,8 @@
 
 import type {ImageStyleProp, TextStyleProp} from '../StyleSheet';
 
-const StyleSheet = require('../StyleSheet');
+import * as StyleSheet from '../StyleSheet';
+
 const imageStyle = {tintColor: 'rgb(0, 0, 0)'};
 const textStyle = {color: 'rgb(0, 0, 0)'};
 
