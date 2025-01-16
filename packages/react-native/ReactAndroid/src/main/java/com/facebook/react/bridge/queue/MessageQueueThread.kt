166347ead93ac1128d7519d5211eb32e53659c50
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2025-01-16 07:34:03 -0800

    chore(Android): migrate MessageQueueThread and it's implementation to Kotlin (#48652)
    Summary:
    Continuing our usual journey, this time migrating MessageQueueThread. Was not expecting to see that many assertions in ReactContext.
    One important thing to note on this PR: I had to add an extra Throw RuntimeException to `startNewBackgroundThread`. It already had one from the`dataFuture.getOrThrow()`, but if your thread is not associated with a Looper, calling `myLooper` (Line 203 of MessageQueueThreadImpl) Can return null. Until now, this would have been a `NPE`, i just decided to make it a `RuntimeException` with the message `Looper not found for thread`. Let me know if you want me to make that function return a nullable, or throw another message or exception, or if you want me to treat Looper as Nullable in the whole class.
    
    ## Changelog:
    
    [INTERNAL] [FIXED] - Migrate MessageQueueThread and MessageQueueThreadImpl to Kotlin
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/48652
    
    Test Plan: <img width="1318" alt="Screenshot 2025-01-13 at 21 03 00" src="https://github.com/user-attachments/assets/462cc8af-4648-4437-9260-5ffa6c69e763" />
    
    Reviewed By: tdn120
    
    Differential Revision: D68155120
    
    Pulled By: rshest
    
    fbshipit-source-id: 1082a832df5b1d8ee64ca7be26e4b85e79152f88

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/queue/MessageQueueThread.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/queue/MessageQueueThread.kt
@@ -5,74 +5,67 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.bridge.queue;
+package com.facebook.react.bridge.queue
 
-import androidx.annotation.Nullable;
-import com.facebook.proguard.annotations.DoNotStrip;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
+import com.facebook.proguard.annotations.DoNotStripAny
+import com.facebook.react.bridge.AssertionException
+import java.util.concurrent.Callable
+import java.util.concurrent.Future
 
 /** Encapsulates a Thread that can accept Runnables. */
-@DoNotStrip
+@DoNotStripAny
 public interface MessageQueueThread {
   /**
    * Runs the given Runnable on this Thread. It will be submitted to the end of the event queue even
    * if it is being submitted from the same queue Thread.
    */
-  @DoNotStrip
-  boolean runOnQueue(Runnable runnable);
+  public fun runOnQueue(runnable: Runnable): Boolean
 
   /**
    * Runs the given Callable on this Thread. It will be submitted to the end of the event queue even
    * if it is being submitted from the same queue Thread.
    */
-  @DoNotStrip
-  <T> Future<T> callOnQueue(final Callable<T> callable);
+  public fun <T> callOnQueue(callable: Callable<T>): Future<T>
 
   /**
-   * @return whether the current Thread is also the Thread associated with this MessageQueueThread.
+   * Tells whether the current Thread is also the Thread associated with this MessageQueueThread.
    */
-  @DoNotStrip
-  boolean isOnThread();
+  public fun isOnThread(): Boolean
 
   /**
-   * Asserts {@link #isOnThread()}, throwing a {@link AssertionException} (NOT an {@link
-   * AssertionError}) if the assertion fails.
+   * Asserts [isOnThread], throwing a [AssertionException] (NOT an [AssertionError]) if the
+   * assertion fails.
    */
-  @DoNotStrip
-  void assertIsOnThread();
+  public fun assertIsOnThread()
 
   /**
-   * Asserts {@link #isOnThread()}, throwing a {@link AssertionException} (NOT an {@link
-   * AssertionError}) if the assertion fails. The given message is appended to the error.
+   * Asserts [isOnThread], throwing a [AssertionException] (NOT an [AssertionError]) if the
+   * assertion fails. The given message is appended to the error.
    */
-  @DoNotStrip
-  void assertIsOnThread(String message);
+  public fun assertIsOnThread(message: String)
 
   /**
    * Quits this MessageQueueThread. If called from this MessageQueueThread, this will be the last
    * thing the thread runs. If called from a separate thread, this will block until the thread can
    * be quit and joined.
    */
-  @DoNotStrip
-  void quitSynchronous();
+  public fun quitSynchronous()
 
   /**
    * Returns the perf counters taken when the framework was started. This method is intended to be
    * used for instrumentation purposes.
    */
-  @Nullable
-  @DoNotStrip
-  MessageQueueThreadPerfStats getPerfStats();
+  public fun getPerfStats(): MessageQueueThreadPerfStats?
 
   /**
    * Resets the perf counters. This is useful if the RN threads are being re-used. This method is
    * intended to be used for instrumentation purposes.
    */
-  @DoNotStrip
-  void resetPerfStats();
+  public fun resetPerfStats()
 
-  /** Returns true if the message queue is idle */
-  @DoNotStrip
-  boolean isIdle();
+  /**
+   * Resets the perf counters. This is useful if the RN threads are being re-used. This method is
+   * intended to be used for instrumentation purposes.
+   */
+  public fun isIdle(): Boolean
 }
