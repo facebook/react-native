7bf7ea71216b534b5a42ecd79e4dad372afcd3b3
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-01-20 04:15:25 -0800

    Add prepare-ios script (#48791)
    Summary:
    
    This change moves the same scripts we have to prepare the HelloWorld project to RNTester.
    
    This is something we forgot to do when we were decoupling the reacrt-native from the community CLI.
    
    This is also the base to start deprecating cocoapods and add more configuration steps for the project.
    
    ## Changelog:
    [Internal] - Copy cli.js script from HelloWorld to RNTester
    
    Reviewed By: cortinico
    
    Differential Revision: D68325581

---
+++ packages/rn-tester/cli.flow.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ * @oncall react_native
+ */
+
+import {run} from './scripts/utils';
+import {apple} from '@react-native/core-cli-utils';
+import {Option, program} from 'commander';
+import {readFileSync} from 'fs';
+
+program.version(JSON.parse(readFileSync('./package.json', 'utf8')).version);
+
+const bootstrap = program.command('bootstrap');
+
+type BootstrapOptions = {
+  arch: 'old' | 'new',
+  jsvm: 'hermes' | 'jsc',
+  frameworks?: 'static' | 'dynamic',
+};
+
+bootstrap
+  .command('ios')
+  .description('Bootstrap iOS')
+  .addOption(
+    new Option('--arch <arch>', "Choose React Native's architecture")
+      .choices(['new', 'old'])
+      .default('new'),
+  )
+  .addOption(
+    new Option(
+      '--frameworks <linkage>',
+      'Use frameworks instead of static libraries',
+    )
+      .choices(['static', 'dynamic'])
+      .default(undefined),
+  )
+  .addOption(
+    new Option('--jsvm <vm>', 'Choose VM used on device')
+      .choices(['jsc', 'hermes'])
+      .default('hermes'),
+  )
+  .action(async ({jsvm, arch, frameworks}: BootstrapOptions) => {
+    await run(
+      apple.bootstrap({
+        cwd: __dirname,
+        frameworks,
+        hermes: jsvm === 'hermes',
+        newArchitecture: arch === 'new',
+      }),
+    );
+  });
+
+if (require.main === module) {
+  program.parse();
+}
+
+export default program;
