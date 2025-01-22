947cecb1fc5d39cfb555f23ed79f7f5d7e4fe18b
zhongwuzw <zhongwuzw@qq.com>
2025-01-22 18:02:56 +0800

    [iOS] Bridge: Fixes HostTarget use after free when deallocated bridge

--- packages/react-native/React/Base/RCTBridge.mm
+++ packages/react-native/React/Base/RCTBridge.mm
@@ -313,6 +313,7 @@ RCT_NOT_IMPLEMENTED(-(instancetype)init)
 
 - (void)dealloc
 {
+  RCTBridge *batchedBridge = self.batchedBridge;
   /**
    * This runs only on the main thread, but crashes the subclass
    * RCTAssertMainQueue();
@@ -332,7 +333,15 @@ RCT_NOT_IMPLEMENTED(-(instancetype)init)
     RCTExecuteOnMainQueue(^{
       facebook::react::jsinspector_modern::getInspectorInstance().removePage(*inspectorPageId);
       inspectorPageId.reset();
-      inspectorTarget.reset();
+      if (batchedBridge) {
+        [batchedBridge
+            dispatchBlock:^{
+              inspectorTarget.reset();
+            }
+                    queue:RCTJSThread];
+      } else {
+        inspectorTarget.reset();
+      }
     });
   }
 }
