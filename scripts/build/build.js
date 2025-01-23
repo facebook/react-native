d4c1c7bfe316ca71de338defbf70a00af235a594
Alex Hunt <huntie@meta.com>
2025-01-23 02:39:32 -0800

    Move experimental type generation into dedicated script (#48867)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/48867
    
    Also:
    - Rename output dir to `types_generated/`.
    - Support source patterns outside of the `react-native` package.
    
    Changelog: [Internal]
    
    Reviewed By: j-piasecki
    
    Differential Revision: D68498211
    
    fbshipit-source-id: 7f9f540efd6d3d2c70b8b1721738f3fea569641d

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -10,7 +10,6 @@
  */
 
 const {PACKAGES_DIR, REPO_ROOT} = require('../consts');
-const buildRNTypes = require('./buildRNTypes');
 const {
   buildConfig,
   getBabelConfig,
@@ -37,7 +36,6 @@ const config = {
   allowPositionals: true,
   options: {
     check: {type: 'boolean'},
-    experimentalBuildRNTypes: {type: 'boolean'},
     help: {type: 'boolean'},
   },
 };
@@ -45,7 +43,7 @@ const config = {
 async function build() {
   const {
     positionals: packageNames,
-    values: {check, experimentalBuildRNTypes, help},
+    values: {check, help},
   } = parseArgs(config);
 
   if (help) {
@@ -60,9 +58,6 @@ async function build() {
   Options:
     --check           Validate that no build artifacts have been accidentally
                       committed.
-    --experimentalBuildRNTypes
-                      [Experimental] Enable source code -> type translation
-                      output for the react-native package.
     `);
     process.exitCode = 0;
     return;
@@ -72,25 +67,9 @@ async function build() {
     console.log('\n' + chalk.bold.inverse('Building packages') + '\n');
   }
 
-  let packagesToBuild = packageNames.length
+  const packagesToBuild = packageNames.length
     ? packageNames.filter(packageName => packageName in buildConfig.packages)
     : Object.keys(buildConfig.packages);
-
-  if (packagesToBuild.includes('react-native')) {
-    // Remove react-native from the list of packages to build, only type generation is implemented
-    packagesToBuild = packagesToBuild.filter(
-      packageName => packageName !== 'react-native',
-    );
-
-    if (experimentalBuildRNTypes) {
-      await emitReactNativeTypes();
-    } else if (packagesToBuild.length === 0) {
-      console.warn(
-        'Building the react-native package must be enabled using ' +
-          '--experimentalBuildRNTypes.',
-      );
-    }
-  }
 
   let ok = true;
   for (const packageName of packagesToBuild) {
@@ -102,10 +81,6 @@ async function build() {
   }
 
   process.exitCode = ok ? 0 : 1;
-}
-
-async function emitReactNativeTypes() {
-  await buildRNTypes();
 }
 
 async function checkPackage(packageName /*: string */) /*: Promise<boolean> */ {
