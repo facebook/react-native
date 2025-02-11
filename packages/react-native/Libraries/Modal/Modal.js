1cf4c84ba0f6f630d55ba0bebde122d742c419e8
Jakub Piasecki <piaseckijakub@meta.com>
2025-02-11 09:25:22 -0800

    Update files in `packages/virtualized-lists` to use `export` syntax (#49262)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/49262
    
    Changelog: [General][Breaking] Deep imports into `react-native/virtualized-lists` with require syntax may need to be appended with `.default`
    
    Reviewed By: huntie
    
    Differential Revision: D69308532
    
    fbshipit-source-id: 6de15d46e0931616bc9849edbccb7cf745e15dd5

--- packages/react-native/Libraries/Modal/Modal.js
+++ packages/react-native/Libraries/Modal/Modal.js
@@ -17,7 +17,7 @@ import {type EventSubscription} from '../vendor/emitter/EventEmitter';
 import ModalInjection from './ModalInjection';
 import NativeModalManager from './NativeModalManager';
 import RCTModalHostView from './RCTModalHostViewNativeComponent';
-import {VirtualizedListContextResetter} from '@react-native/virtualized-lists';
+import VirtualizedLists from '@react-native/virtualized-lists';
 import React from 'react';
 
 const ScrollView = require('../Components/ScrollView/ScrollView').default;
@@ -27,6 +27,9 @@ const I18nManager = require('../ReactNative/I18nManager').default;
 const {RootTagContext} = require('../ReactNative/RootTag');
 const StyleSheet = require('../StyleSheet/StyleSheet');
 const Platform = require('../Utilities/Platform');
+
+const VirtualizedListContextResetter =
+  VirtualizedLists.VirtualizedListContextResetter;
 
 type ModalEventDefinitions = {
   modalDismissed: [{modalID: number}],
