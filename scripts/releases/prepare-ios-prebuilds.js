c45876ad184159a8dd95dfab7400a71f27f53a23
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-02-12 09:36:59 -0800

    [skip ci] Add function to download dependencies (#49335)
    Summary:
    
    This change adds a function in the prebuild script to download a 3p dependency in the react native monorepo.
    
    ## Changelog:
    [Internal] - Add function to download 3rd party dependencies
    
    Reviewed By: cortinico
    
    Differential Revision: D69464429

--- scripts/releases/prepare-ios-prebuilds.js
+++ scripts/releases/prepare-ios-prebuilds.js
@@ -11,8 +11,43 @@
 
 require('../babel-register').registerForScript();
 
-function main() {
+const fs = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+/*::
+type Dependency = $ReadOnly<{
+  name: string,
+  version: string,
+  url: URL,
+}>;
+*/
+
+async function downloadDependency(
+  dependency /*: Dependency*/,
+) /*: Promise<void> */ {
+  const {name, version, url} = dependency;
+  const filename = `${name}-${version}.tgz`;
+  const archiveDestination = `/tmp/${filename}`;
+  const command = `curl -L ${url.toString()} --output ${archiveDestination}`;
+
+  console.log(`Downloading ${filename}...`);
+  await exec(command);
+
+  const destination = `packages/react-native/third-party/${name}`;
+  fs.mkdirSync(destination, {recursive: true});
+
+  console.log(`Extracting ${filename} to ${destination}...`);
+  await exec(`tar -xzf ${archiveDestination} -C ${destination}`);
+
+  console.log(`Cleaning up ${filename}...`);
+  await exec(`rm ${archiveDestination}`);
+}
+
+async function main() {
   console.log('Starting iOS prebuilds preparation...');
+
+  console.log('Done!');
 }
 
 if (require.main === module) {
