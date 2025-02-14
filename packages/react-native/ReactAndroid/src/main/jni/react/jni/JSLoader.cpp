74ca8db8769ae600c5415c6e64de98043934e151
Mateo Hrastnik <hrastnikmateo@gmail.com>
2025-02-14 13:38:41 +0100

    Wording tweaks
    Co-authored-by: Alex Hunt <hello@alexhunt.dev>

--- packages/react-native/ReactAndroid/src/main/jni/react/jni/JSLoader.cpp
+++ packages/react-native/ReactAndroid/src/main/jni/react/jni/JSLoader.cpp
@@ -89,10 +89,10 @@ loadScriptFromAssets(AAssetManager* manager, const std::string& assetName) {
 
   throw std::runtime_error(folly::to<std::string>(
       "Unable to load script.\n\n"
-      "Make sure you're running Metro ('npx react-native start') or that your "
+      "Make sure you're running Metro or that your "
       "bundle '", assetName, "' is packaged correctly for release.\n\n"
-      "The device must be on the same WiFi as your laptop to connect to Metro.\n\n"
-      "To use USB instead, shake the device to open the dev menu and set "
+      "The device must be on the same Wi-Fi network as your laptop to connect to Metro.\n\n"
+      "To use USB instead, shake the device to open the Dev Menu and set "
       "the bundler location to \"localhost:8081\" and run:\n"
       "  adb reverse tcp:8081 tcp:8081"));
 }
