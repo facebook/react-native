356a6d930453bcb176f5da8346a8500b73f4a70c
Liron Yahdav <lyahdav@meta.com>
2025-02-19 13:19:19 -0800

    Throw exception with name of TurboModule instead of deadlock during sync rendering (#49509)
    Summary:
    
    Currently if we hit a deadlock in sync rendering due to a TurboModule initialization that requires main queue setup we don't get any information about which TurboModule caused the issue.
    
    To help us know which TurboModules we need to fix, this instead will crash with the name of the TurboModule.
    
    Changelog: [Internal]
    
    Reviewed By: sammy-SC
    
    Differential Revision: D69805065

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/internal/featureflags/ReactNativeFeatureFlagsDefaults.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/internal/featureflags/ReactNativeFeatureFlagsDefaults.kt
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<5315bd93234b8c00b7d029081231eeeb>>
+ * @generated SignedSource<<94ffd31d788dae979b92c7a347f2ad40>>
  */
 
 /**
@@ -88,6 +88,8 @@ public open class ReactNativeFeatureFlagsDefaults : ReactNativeFeatureFlagsProvi
   override fun fuseboxNetworkInspectionEnabled(): Boolean = false
 
   override fun lazyAnimationCallbacks(): Boolean = false
+
+  override fun throwExceptionInsteadOfDeadlockOnTurboModuleSetupDuringSyncRenderIOS(): Boolean = false
 
   override fun traceTurboModulePromiseRejectionsOnAndroid(): Boolean = false
 
