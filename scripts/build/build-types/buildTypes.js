92e21688db632d72732b089d7436d2e19b09d3b6
Dawid Małecki <maleckidawid@meta.com>
2025-02-24 07:08:03 -0800

    Add RefreshControl to buildTypes and align Flow with TS types (#49564)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/49564
    
    Changelog:
    [Internal] - Added RefreshControl to buildTypes and aligned Flow with TS types
    
    Reviewed By: huntie
    
    Differential Revision: D69924368
    
    fbshipit-source-id: 1a01ede28f9d0729c483036f8d3f9c9eced58286

--- scripts/build/build-types/buildTypes.js
+++ scripts/build/build-types/buildTypes.js
@@ -75,6 +75,7 @@ const ENTRY_POINTS = [
   'packages/react-native/Libraries/Components/Touchable/TouchableHighlight.js',
   'packages/react-native/Libraries/Components/Switch/Switch.js',
   'packages/react-native/Libraries/Components/StatusBar/StatusBar.js',
+  'packages/react-native/Libraries/Components/RefreshControl/RefreshControl.js',
 ];
 
 /**
