a318c0dce5df0b031007f7757f93cc4baad57896
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-02-25 12:59:27 -0800

    Add example with generalized module (#49651)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/49651
    
    donotcommit
    
    This change adds an example with a Native TM and a Cxx turbomodule at the same time, registered using the new codegen mechanism.
    
    ## Problem
    As of today, it is not possible to create a pure C++ TM and to register it through a Swift AppDelegate
    
    ## Solution
    We can create a pod that can be imported in a Swift AppDelegate and that offer some pure Objective-C classes.
    
    These classes contains a provider that can be instantiated in Swift.
    
    The TurboModule manager delegate will ask the AppDelegate about the presence of some provider that can instantiate a pure C++ turbomodule with a given name.
    
    The provider has an empty interface, but the implementation contains a function that can actually instantiate the TM. The function is implemented in an Objective-C++ class that imports the pure C++ turbomodule and creates it.
    
    The TMManager extends the provider through a category to attaach the signature of the function that is implemented by the provider.
    
    The last diff in this stack contains an exaple on how to implement this.
    
    ## Changelog:
    [Internal] - Add example with a Cxx TM and a Native TM
    
    Differential Revision: D70121989

--- packages/helloworld/App.jsx
+++ packages/helloworld/App.jsx
@@ -21,6 +21,7 @@ import {
 } from 'react-native';
 import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 import SampleTurboModule from './specs/NativeSampleModule';
+import NativeLocalStorage from './specs/NativeLocalStorage';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -36,6 +37,30 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  const [value2, setValue2] = React.useState(null);
+
+  const [editingValue, setEditingValue] = React.useState(null);
+
+  React.useEffect(() => {
+    const storedValue = NativeLocalStorage?.getItem('myKey');
+    setValue2(storedValue ?? '');
+  }, []);
+
+  function saveValue() {
+    NativeLocalStorage?.setItem(editingValue ?? EMPTY, 'myKey');
+    setValue2(editingValue);
+  }
+
+  function clearAll() {
+    NativeLocalStorage?.clear();
+    setValue2('');
+  }
+
+  function deleteValue() {
+    NativeLocalStorage?.removeItem('myKey');
+    setValue2('');
+  }
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -61,6 +86,19 @@ function App(): React.JSX.Element {
           <Button title="Reverse" onPress={onPress} />
           <Text>Reversed text: {reversedValue}</Text>
         </View>
+        <View>
+          <Text style={styles.text}>
+            Current stored value is: {value2 ?? 'No Value'}
+          </Text>
+          <TextInput
+            placeholder="Enter the text you want to store"
+            style={styles.textInput}
+            onChangeText={setEditingValue}
+          />
+          <Button title="Save" onPress={saveValue} />
+          <Button title="Delete" onPress={deleteValue} />
+          <Button title="Clear" onPress={clearAll} />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -70,6 +108,19 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '600',
+  },
+  text: {
+    margin: 10,
+    fontSize: 20,
+  },
+  textInput: {
+    margin: 10,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 5,
   },
 });
 
