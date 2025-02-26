339b0b56532847809519cfcfc03a18e93ed9b607
Christian Falch <christian.falch@gmail.com>
2025-02-26 08:19:39 -0800

    Add utils: CLI arg validation and folders utils (#49668)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/49668
    
    This change refactors the script to prebuild ios dependencies by:
    - factoring out CLI validation in a separate file
    - factoring out the folder utilities in a separate file
    
    bypass-github-export-checks
    
    ## Changelog:
    
    [INTERNAL] - Factor out CLI and folder utilities
    
    Test Plan:
    Tested in this `AppDelegate.mm`:
    
    Imports:
    
    ```obj-c
    #import "AppDelegate.h"
    #import <glog/logging.h>
    #import <double-conversion.h>
    #include <fmt/core.h>
    #include <boost/assert.hpp>
    #include <fast_float/fast_float.h>
    #include <string>
    #import <SocketRocket/SRWebSocket.h>
    ```
    
    Code:
    ```obj-c
    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
      std::string input = "3.1416 xyz ";
      double_conversion::DoubleToStringConverter::EcmaScriptConverter();
      LOG(INFO) << "Hello from GLOG";
      fmt::print("Hello, world from FMT!\n");
      BOOST_ASSERT(100 == 100);
      double result;
      fast_float::from_chars(input.data(), input.data() + input.size(), result);
      LOG(INFO) << "Answer :" << result;
    
      NSArray *frameworks = [NSBundle allFrameworks];
    
      for (NSBundle *framework in frameworks) {
        NSString *frameworkName = framework.bundleURL.lastPathComponent;
        if ([frameworkName isEqualToString: @"ReactNativeDependencies.framework"]) {
          NSBundle *bundle = [NSBundle bundleWithURL:[framework bundleURL]];
          NSURL *bundleURL = [bundle URLForResource:@"ReactNativeDependencies_glog" withExtension:@"bundle"];
          NSBundle *resourceBundle = [NSBundle bundleWithURL:bundleURL];
          NSURL* url = [resourceBundle URLForResource:@"PrivacyInfo" withExtension:@"xcprivacy"];
          if (url == nil) {
            throw [NSException exceptionWithName:@"ResourceNotFoundException"
                                           reason:@"Could not find PrivacyInfo.xcprivacy in ReactNativeDependencies_glog bundle"
                                         userInfo:nil];
          }
          break;
        }
      }
      return YES;
    }
    ```
    
    Reviewed By: cortinico
    
    Differential Revision: D70175663
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: 5c6653aed7054658c5e265675766a28415da3918

---
+++ scripts/releases/prebuild/cli.js
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ * @oncall react_native
+ */
+
+const {dependencies, platforms} = require('./configuration');
+const yargs = require('yargs');
+
+/*::
+import type {Dependency, Platform} from './types';
+*/
+
+const arrayLike = (value /*: Array<any> */) /*: Array<any> */ =>
+  Array.isArray(value) ? value : [value];
+
+const cli = yargs
+  .usage(
+    'This script prepares iOS prebuilds for React Native. It downloads the dependencies, prepare them, builds them and creates the XCFrameworks.' +
+      'Calling the script with no options will build all the dependencies for all the slices and configurations.',
+  )
+  .option('setup', {
+    alias: 's',
+    type: 'boolean',
+    describe: 'Download and setup dependencies',
+  })
+  .option('swiftpackage', {
+    alias: 'w',
+    type: 'boolean',
+    describe: 'Creates the Package.swift file',
+  })
+  .option('build', {
+    alias: 'b',
+    type: 'boolean',
+    describe: 'Build dependencies/platforms',
+  })
+  .option('compose', {
+    alias: 'c',
+    type: 'boolean',
+    describe: 'Compose xcframework from built dependencies',
+  })
+  .option('platforms', {
+    alias: 'p',
+    type: 'array',
+    default: platforms,
+    describe: 'Specify one or more platforms to build for',
+  })
+  .option('dependencies', {
+    alias: 'd',
+    type: 'array',
+    default: dependencies.filter(d => !d.disabled).map(d => d.name),
+    describe: 'Specify one or more dependencies',
+  })
+  .option('configuration', {
+    alias: 'r',
+    type: 'string',
+    default: 'Debug',
+    describe: 'Specify the configuration to build, Release or Debug (default).',
+  })
+  .help();
+
+/**
+ * Returns the tasks that need to be run based on the command line arguments
+ */
+async function getCLIConfiguration() /*: Promise<?{|
+  tasks: {|
+    setup: boolean,
+    swiftpackage: boolean,
+    build: boolean,
+    compose: boolean,
+  |},
+  platforms: $ReadOnlyArray<Platform>,
+  dependencies: $ReadOnlyArray<Dependency>,
+  configuration: string,
+|}> */ {
+  // Run input parsing
+  const argv = await cli.argv;
+
+  // Verify that the platforms argument is valid
+  const invalidPlatforms = arrayLike(argv.platforms).filter(
+    rs => !platforms.includes(rs),
+  );
+  if (invalidPlatforms.length > 0) {
+    console.error(`Invalid platform specified: ${invalidPlatforms.join(', ')}`);
+    return undefined;
+  }
+
+  // Verify that the dependencies argument is valid
+  const invalidDependencies = arrayLike(argv.dependencies).filter(
+    rd => !dependencies.map(d => d.name).includes(rd),
+  );
+  if (invalidDependencies.length > 0) {
+    console.error(
+      `Invalid dependency specified: ${invalidDependencies.join(', ')}`,
+    );
+    return undefined;
+  }
+
+  // Prepare platforms and dependencies
+  const resolvedPlatforms = platforms.filter(p => argv.platforms.includes(p));
+  const resolvedDependencies = dependencies.filter(d =>
+    argv.dependencies.includes(d.name),
+  );
+
+  // Are we running all commands?
+  const runAllCommands =
+    argv.setup == null &&
+    argv.swiftpackage == null &&
+    argv.build == null &&
+    argv.compose == null;
+
+  return {
+    tasks: {
+      setup: runAllCommands || argv.setup != null,
+      swiftpackage: runAllCommands || argv.swiftpackage != null,
+      build: runAllCommands || argv.build != null,
+      compose: runAllCommands || argv.compose != null,
+    },
+    platforms: resolvedPlatforms,
+    dependencies: resolvedDependencies,
+    configuration: argv.configuration,
+  };
+}
+
+module.exports = {getCLIConfiguration};
