2cbb54e9566d393981f87efe14e9712c9a9a7fc0
Dawid Małecki <maleckidawid@meta.com>
2025-03-06 08:54:21 -0800

    Add TextInput to buildTypes and align Flow with TS types (#49720)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/49720
    
    Changelog:
    [Internal] - Added TextInput to buildTypes and aligned Flow with TS types
    
    Reviewed By: huntie
    
    Differential Revision: D70180918
    
    fbshipit-source-id: 2ea815153cbff47af534d80faec5db98e4a4a0e8

--- packages/react-native/types/__typetests__/index.tsx
+++ packages/react-native/types/__typetests__/index.tsx
@@ -1189,7 +1189,7 @@ const customEventEmitter = new CustomEventEmitter();
 customEventEmitter.addListener('event', () => {});
 
 class TextInputTest extends React.Component<{}, {username: string}> {
-  username: TextInput | null = null;
+  username: React.ElementRef<typeof TextInput> | null = null;
 
   handleUsernameChange = (text: string) => {
     console.log(`text: ${text}`);
@@ -1292,9 +1292,7 @@ class TextInputTest extends React.Component<{}, {username: string}> {
           onContentSizeChange={this.handleOnContentSizeChange}
         />
 
-        <TextInput contextMenuHidden={true} textAlignVertical="top" />
-
-        <TextInput textAlign="center" />
+        <TextInput contextMenuHidden={true} />
       </View>
     );
   }
