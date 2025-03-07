a01e07120d4f1792c04e920c12dd89733eddb1cc
Mateo Guzmán <info@mateoguzman.net>
2025-03-07 17:46:32 +0100

    Migrate JavaScriptContextHolder & JavaScriptExecutor

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/JavaScriptContextHolder.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/JavaScriptContextHolder.kt
@@ -5,29 +5,25 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.react.bridge;
+package com.facebook.react.bridge
 
-import androidx.annotation.GuardedBy;
+import androidx.annotation.GuardedBy
 
 /**
  * Wrapper for JavaScriptContext native pointer. CatalystInstanceImpl creates this on demand, and
  * will call clear() before destroying the VM. People who need the raw JavaScriptContext pointer can
  * synchronize on this wrapper object to guarantee that it will not be destroyed.
  */
-public class JavaScriptContextHolder {
+public class JavaScriptContextHolder public constructor(
+    @field:GuardedBy("this") private var context: Long
+) {
   @GuardedBy("this")
-  private long mContext;
-
-  public JavaScriptContextHolder(long context) {
-    mContext = context;
+  public fun get(): Long {
+    return context
   }
 
-  @GuardedBy("this")
-  public long get() {
-    return mContext;
-  }
-
-  public synchronized void clear() {
-    mContext = 0;
+  @Synchronized
+  public fun clear() {
+    context = 0
   }
 }
