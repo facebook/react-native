cd316b17498a8e26113592e05250f4c59338ed7e
Mateo Guzmán <info@mateoguzman.net>
2025-03-10 20:05:34 +0100

    Add back ReplaceWith

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/BaseActivityEventListener.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/BaseActivityEventListener.kt
@@ -12,7 +12,10 @@ import android.content.Intent
 
 /** An empty implementation of [ActivityEventListener]. */
 public class BaseActivityEventListener : ActivityEventListener {
-  @Deprecated("Use onActivityResult(Activity, Int, Int, Intent) instead.")
+  @Deprecated(
+    "Use onActivityResult(Activity, Int, Int, Intent) instead.",
+    ReplaceWith("onActivityResult(activity, requestCode, resultCode, data)")
+  )
   public fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {}
 
   override fun onActivityResult(activity: Activity, requestCode: Int, resultCode: Int, data: Intent?) {}
