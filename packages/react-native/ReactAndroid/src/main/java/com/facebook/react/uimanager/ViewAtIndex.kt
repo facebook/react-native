8c8a3d483819ac3fd1810bfa5193c3b62aafdbbb
Mateo Guzmán <info@mateoguzman.net>
2025-03-11 23:34:41 +0100

    Revert newArchEnabled

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ViewAtIndex.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ViewAtIndex.kt
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 package com.facebook.react.uimanager
 
 import com.facebook.react.common.annotations.internal.LegacyArchitecture
