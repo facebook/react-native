c068c599c641491e735caf79e4e60a0fb3c04f83
Jorge Cabiedes Acosta <jorgecab@meta.com>
2025-03-11 17:08:51 -0700

    Fix keyboard navigation for FlatList with `removeClippedSubviews` enabled (#49543)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/49543
    
    When using `ReactScrollView` or `ReactHorizontalScrollView` Views with `removeClippedSubviews` keyboard navigation didn't work.
    
    This is because keyboard navigation relies on Android's View hierarchy to find the next focusable element. With `removeClippedSubviews` the next View might've been removed from the hierarchy.
    
    With this change we delegate the job of figuring out the next focusable element to the Shadow Tree, which will always contain layout information of the next element of the ScrollView.
    
    Changelog: [Android][Fixed] - Fix keyboard navigation on lists with `removeClippedSubviews` enabled
    
    Reviewed By: joevilches
    
    Differential Revision: D69618406
    
    fbshipit-source-id: 1df7f90066dfc685e74a89b29222937777714b87

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManagerBinding.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManagerBinding.kt
@@ -55,6 +55,12 @@ internal class FabricUIManagerBinding : HybridClassBase() {
       isMountable: Boolean
   )
 
+  external fun findNextFocusableElementMetrics(
+      parentTag: Int,
+      focusedTag: Int,
+      direction: Int
+  ): FloatArray
+
   external fun stopSurface(surfaceId: Int)
 
   external fun stopSurfaceWithSurfaceHandler(surfaceHandler: SurfaceHandlerBinding)
