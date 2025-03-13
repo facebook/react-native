e876e4926fe31bb079e814c41ff8b0ff60aaf91e
Christian Falch <christian.falch@gmail.com>
2025-03-13 08:40:14 -0700

    Improve RCT-Folly so that other dependencies don't have to specify the folly flags (#49969)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/49969
    
    Improved RCT-Folly podspec by configuring it as a prepare step, so that we don't need to forward the folly flags to all the others pods and to 3rd party dependencies.
    
    ## Changelog:
    
    [INTERNAL] - Configure folly in a prepare step
    
    Test Plan:
    ✅ Run Rn-Tester and  verify that it works as expected both building deps from source and using prebuilt tarballs
    ✅ Add third-party library (react-native-reanimated) and perform the same tests to verify that it works with the changed podspec and utilities
    
    Reviewed By: javache
    
    Differential Revision: D71032640
    
    Pulled By: cipolleschi
    
    fbshipit-source-id: a34a9b38fa5d5b37a0a193a73659f0d45dd3d660

--- scripts/releases/ios-prebuild/compose-framework.js
+++ scripts/releases/ios-prebuild/compose-framework.js
@@ -88,14 +88,12 @@ function copyBundles(
   frameworkPaths /*:Array<string>*/,
 ) {
   console.log('Copying bundles to the framework...');
-
   // Let's precalculate the target folder. It is the xcframework's output folder with
   // all its targets.
   const targetArchFolders = fs
     .readdirSync(outputFolder)
     .map(p => path.join(outputFolder, p))
     .filter(p => fs.statSync(p).isDirectory());
-
   // For each framework (in frameworkPaths), copy the bundles from the source folder.
   // A bundle is the name of the framework + _ + target name + .bundle. We can
   // check if the target has a bundle by checking if it defines one or more resources.
@@ -105,7 +103,6 @@ function copyBundles(
       if (!resources || resources.length === 0) {
         return;
       }
-
       // Get bundle source folder
       const bundleName = `${scheme}_${dep.name}.bundle`;
       const sourceBundlePath = path.join(frameworkPath, bundleName);
@@ -125,7 +122,6 @@ function copyBundles(
           if (!fs.existsSync(path.dirname(sourceBundlePath))) {
             console.warn("Source bundle doesn't exist", sourceBundlePath);
           }
-
           // A bundle is a directory, so we need to copy the whole directory
           execSync(`cp -r ${sourceBundlePath} ${targetBundlePath}`);
         });
