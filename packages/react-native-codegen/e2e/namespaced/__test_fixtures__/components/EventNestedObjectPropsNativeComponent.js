4a2b3efd9779daa9f4b109b52557859432d584f6
Jakub Piasecki <piaseckijakub@meta.com>
2025-03-14 05:52:25 -0700

    Update codegen to support namespaced types (#49950)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/49950
    
    Changelog: [GENERAL][CHANGED] - Changed `react-native-codegen` to support types under `CodegenTypes` namespace
    
    ## Summary
    
    Currently, codegen relies on deep importing types under `react-native/Libraries/Types/CodegenTypes` to be properly type-checked. Updating codegen to support types under a single namespace will enable us to provide a single import from the package with access to all relevant types.
    
    Differential Revision: D70967809

---
+++ packages/react-native-codegen/e2e/namespaced/__test_fixtures__/components/EventNestedObjectPropsNativeComponent.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import type {CodegenTypes, HostComponent} from 'react-native';
+import type {ViewProps} from 'react-native/Libraries/Components/View/ViewPropTypes';
+
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+
+type OnChangeEvent = $ReadOnly<{
+  location: {
+    source: {url: string, ...},
+    x: CodegenTypes.Int32,
+    y: CodegenTypes.Int32,
+    arrayOfObjects: $ReadOnlyArray<{value: $ReadOnly<{str: string}>}>,
+    ...
+  },
+}>;
+
+type NativeProps = $ReadOnly<{
+  ...ViewProps,
+
+  // Props
+  disabled?: CodegenTypes.WithDefault<boolean, false>,
+
+  // Events
+  onChange?: ?CodegenTypes.BubblingEventHandler<OnChangeEvent>,
+}>;
+
+export default (codegenNativeComponent<NativeProps>(
+  'EventNestedObjectPropsNativeComponentView',
+): HostComponent<NativeProps>);
