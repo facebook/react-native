eb8c1c3e95c92b98a9c78832271562011831c4e0
Rubén Norte <rubennorte@meta.com>
2025-03-18 05:59:50 -0700

    Correctly batch reportMount calls (#50090)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50090
    
    Changelog: [internal]
    
    I refactored `FabricUIManager` in D54547194 / https://github.com/facebook/react-native/pull/43337 and accidentally removed setting this flag to avoid scheduling redundant tasks in the UI thread to report mount. This fixes it.
    
    Reviewed By: javache
    
    Differential Revision: D71387374
    
    fbshipit-source-id: cad8a3ead2434738325560902cbab817e5d5dde7

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
@@ -1256,6 +1256,8 @@ public class FabricUIManager
       }
 
       if (!mMountNotificationScheduled && !mMountedSurfaceIds.isEmpty()) {
+        mMountNotificationScheduled = true;
+
         // Notify mount when the effects are visible and prevent mount hooks to
         // delay paint.
         UiThreadUtil.getUiThreadHandler()
