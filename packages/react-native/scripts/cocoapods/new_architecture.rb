34df48b021c886b5fdca8f3c0869ffb7d91b0bff
Saad Najmi <sanajmi@microsoft.com>
2025-03-19 13:09:59 -0500

    chore(0.76): don't set the new architecture by default for macOS yet (#2425)
    ## Summary:
    
    Backport of #2423 
    This reverts commit b9f3186ee6314f56863c05272640d47643709a39.
    
    The new architecture isn't ready on macOS yet. Specifically, the Fabric
    renderer isn't ready. This is causing issue for new projects off the
    macOS template. Let's disable it until we're ready.
    ## Test Plan:
    
    CI should pass

--- packages/react-native/scripts/cocoapods/new_architecture.rb
+++ packages/react-native/scripts/cocoapods/new_architecture.rb
@@ -187,6 +187,6 @@ class NewArchitectureHelper
     end
 
     def self.new_arch_enabled
-        return ENV["RCT_NEW_ARCH_ENABLED"] == nil || ENV["RCT_NEW_ARCH_ENABLED"] == "1"
+        return ENV["RCT_NEW_ARCH_ENABLED"] == "1" # [macOS] Disable Fabric by default till macOS supports it
     end
 end
