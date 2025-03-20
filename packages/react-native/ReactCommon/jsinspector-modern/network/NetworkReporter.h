a8745324c6891b74333080a7b0b34640120774de
Alex Hunt <huntie@meta.com>
2025-03-20 06:46:13 -0700

    Scaffold NetworkReporter methods (#50131)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50131
    
    Outlines and stubs methods on the `NetworkReporter` class, and the `Network.getResponseBody` CDP request on `NetworkIOAgent`. Together, these form the APIs to implement for CDP network debugging.
    
    Also updates internal mutex use to `std::atomic<bool>`.
    
    Changelog: [Internal]
    
    Reviewed By: hoxyq
    
    Differential Revision: D70708526
    
    fbshipit-source-id: f44bd0d246a38883dd591752fb2d3ed4567de4a0

--- packages/react-native/ReactCommon/jsinspector-modern/network/NetworkReporter.h
+++ packages/react-native/ReactCommon/jsinspector-modern/network/NetworkReporter.h
@@ -7,7 +7,8 @@
 
 #pragma once
 
-#include <mutex>
+#include <atomic>
+#include <string>
 
 namespace facebook::react::jsinspector_modern {
 
@@ -23,7 +24,7 @@ class NetworkReporter {
    * Enable network tracking over CDP. Once enabled, network events will be
    * sent to the debugger client. Returns `false` if already enabled.
    *
-   * Corresponds to @cdp `Network.enable`.
+   * Corresponds to `Network.enable` in CDP.
    */
   bool enableDebugging();
 
@@ -31,9 +32,68 @@ class NetworkReporter {
    * Disable network tracking over CDP, preventing network events from being
    * sent to the debugger client. Returns `false` if not initially enabled.
    *
-   * Corresponds to @cdp `Network.disable`.
+   * Corresponds to `Network.disable` in CDP.
    */
   bool disableDebugging();
+
+  /**
+   * Report a network request that is about to be sent.
+   *
+   * - Corresponds to `Network.requestWillBeSent` in CDP.
+   * - Corresponds to `PerformanceResourceTiming.requestStart` (specifically,
+   *   marking when the native request was initiated).
+   *
+   * https://w3c.github.io/resource-timing/#dom-performanceresourcetiming-requeststart
+   */
+  void reportRequestStart(const std::string& requestId);
+
+  /**
+   * Report detailed timing info, such as DNS lookup, when a request has
+   * started.
+   *
+   * - Corresponds to `Network.requestWillBeSentExtraInfo` in CDP.
+   * - Corresponds to `PerformanceResourceTiming.domainLookupStart`,
+   * `PerformanceResourceTiming.connectStart`.
+   *
+   * https://w3c.github.io/resource-timing/#dom-performanceresourcetiming-connectstart
+   */
+  void reportConnectionTiming(const std::string& requestId);
+
+  /**
+   * Report when a network request has failed.
+   *
+   * Corresponds to `Network.loadingFailed` in CDP.
+   */
+  void reportRequestFailed(const std::string& requestId);
+
+  /**
+   * Report when HTTP response headers have been received, corresponding to
+   * when the first byte of the response is available.
+   *
+   * - Corresponds to `Network.responseReceived` in CDP.
+   * - Corresponds to `PerformanceResourceTiming.responseStart`.
+   *
+   * https://w3c.github.io/resource-timing/#dom-performanceresourcetiming-responsestart
+   */
+  void reportResponseStart(const std::string& requestId);
+
+  /**
+   * Report when additional chunks of the response body have been received.
+   *
+   * Corresponds to `Network.dataReceived` in CDP.
+   */
+  void reportDataReceived(const std::string& requestId);
+
+  /**
+   * Report when a network request is complete and we are no longer receiving
+   * response data.
+   *
+   * - Corresponds to `Network.loadingFinished` in CDP.
+   * - Corresponds to `PerformanceResourceTiming.responseEnd`.
+   *
+   * https://w3c.github.io/resource-timing/#dom-performanceresourcetiming-responseend
+   */
+  void reportResponseEnd(const std::string& requestId);
 
  private:
   NetworkReporter() = default;
@@ -41,8 +101,7 @@ class NetworkReporter {
   NetworkReporter& operator=(const NetworkReporter&) = delete;
   ~NetworkReporter() = default;
 
-  bool enabled_{false};
-  std::mutex mutex_;
+  std::atomic<bool> debuggingEnabled_{false};
 };
 
 } // namespace facebook::react::jsinspector_modern
