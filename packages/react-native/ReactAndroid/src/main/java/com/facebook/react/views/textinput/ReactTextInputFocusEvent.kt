e98c2a08332227b12f46a6ccb007419785c25e2f
Mateo Guzmán <info@mateoguzman.net>
2025-03-20 23:04:45 +0100

    Adjust comments

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputFocusEvent.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactTextInputFocusEvent.kt
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 package com.facebook.react.views.textinput
 
 import com.facebook.react.bridge.Arguments
@@ -11,7 +12,7 @@ import com.facebook.react.bridge.WritableMap
 import com.facebook.react.uimanager.common.ViewUtil
 import com.facebook.react.uimanager.events.Event
 
-/** Event emitted by EditText native view when it receives focus.  */ /* package */
+/** Event emitted by EditText native view when it receives focus. */
 internal class ReactTextInputFocusEvent(
     surfaceId: Int,
     viewId: Int
