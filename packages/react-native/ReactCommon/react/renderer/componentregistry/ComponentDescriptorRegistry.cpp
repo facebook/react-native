facebook/react-native

commit 62448925ce877fdabc4950f86d9aec4bdf757794

cleanup enabled_automatic_interop_android (#47321)
Summary:

Changelog: [Internal]

this seems to be the last callsite where we read the ReactNativeConfig from the contextContainer. i don't think this killswitch should block cleanup of the ReactNativeConfig, so maybe we can just remove this?

Reviewed By: cortinico

Differential Revision: D65192744

packages/react-native/ReactCommon/react/renderer/componentregistry/ComponentDescriptorRegistry.cpp
@@ -9,7 +9,6 @@
 
 #include "componentNameByReactViewName.h"
 
-#include <react/config/ReactNativeConfig.h>
 #include <react/debug/react_native_assert.h>
 #include <react/renderer/componentregistry/ComponentDescriptorProviderRegistry.h>
 #include <react/renderer/components/legacyviewmanagerinterop/UnstableLegacyViewManagerAutomaticComponentDescriptor.h>
@@ -84,23 +83,11 @@ const ComponentDescriptor& ComponentDescriptorRegistry::at(
   }
 
   if (it == _registryByName.end()) {
-    auto reactNativeConfig_ =
-        contextContainer_->at<std::shared_ptr<const ReactNativeConfig>>(
-            "ReactNativeConfig");
-    if (reactNativeConfig_->getBool(
-            "react_fabric:enabled_automatic_interop_android")) {
-      auto componentDescriptor = std::make_shared<
-          const UnstableLegacyViewManagerAutomaticComponentDescriptor>(
-          parameters_, unifiedComponentName);
-      registerComponentDescriptor(componentDescriptor);
-      return *_registryByName.find(unifiedComponentName)->second;
-    } else if (_fallbackComponentDescriptor == nullptr) {
-      throw std::invalid_argument(
-          ("Unable to find componentDescriptor for " + unifiedComponentName)
-              .c_str());
-    } else {
-      return *_fallbackComponentDescriptor.get();
-    }
+    auto componentDescriptor = std::make_shared<
+        const UnstableLegacyViewManagerAutomaticComponentDescriptor>(
+        parameters_, unifiedComponentName);
+    registerComponentDescriptor(componentDescriptor);
+    return *_registryByName.find(unifiedComponentName)->second;
   }
 
   return *it->second;

packages/react-native/ReactCommon/react/config/ReactNativeConfig.cpp
@@ -10,9 +10,6 @@
 namespace facebook::react {
 
 bool EmptyReactNativeConfig::getBool(const std::string& param) const {
-  if (param == "react_fabric:enabled_automatic_interop_android") {
-    return true;
-  }
   return false;
 }
 

commit 14c1fedbf0c93ff7dc6eaa68cc824a13aa357997

Fixing Fabric Interop layer bug defaulting to UnstableLegacyViewManagerAutomaticComponentDescriptor irrespective of feature flag (#50186)
Summary:

When `useFabricInterop` is disabled, deleting component descriptors in Component Registry is failing since  https://github.com/facebook/react-native/pull/47321 removed the Fabric Interop check from the ComponentDescriptorRegistry.cpp which was added earlier in https://github.com/facebook/react-native/pull/42294

Adding back the logic of Fallback component descriptor and error for the same.

Changelog:
[General][Fixed] Fixing Fabric Interop layer bug defaulting to UnstableLegacyViewManagerAutomaticComponentDescriptor irrespective of feature flags

Differential Revision: D71582553

packages/react-native/ReactCommon/react/renderer/componentregistry/ComponentDescriptorRegistry.cpp
@@ -10,6 +10,7 @@
 #include "componentNameByReactViewName.h"
 
 #include <react/debug/react_native_assert.h>
+#include <react/featureflags/ReactNativeFeatureFlags.h>
 #include <react/renderer/componentregistry/ComponentDescriptorProviderRegistry.h>
 #include <react/renderer/components/legacyviewmanagerinterop/UnstableLegacyViewManagerAutomaticComponentDescriptor.h>
 #include <react/renderer/components/legacyviewmanagerinterop/UnstableLegacyViewManagerAutomaticShadowNode.h>
@@ -83,11 +84,21 @@ const ComponentDescriptor& ComponentDescriptorRegistry::at(
   }
 
   if (it == _registryByName.end()) {
-    auto componentDescriptor = std::make_shared<
-        const UnstableLegacyViewManagerAutomaticComponentDescriptor>(
-        parameters_, unifiedComponentName);
-    registerComponentDescriptor(componentDescriptor);
-    return *_registryByName.find(unifiedComponentName)->second;
+    if (ReactNativeFeatureFlags::useFabricInterop()) {
+      auto componentDescriptor = std::make_shared<
+          const UnstableLegacyViewManagerAutomaticComponentDescriptor>(
+          parameters_, unifiedComponentName);
+      registerComponentDescriptor(componentDescriptor);
+      return *_registryByName.find(unifiedComponentName)->second;
+    } else {
+      if (_fallbackComponentDescriptor == nullptr) {
+        throw std::invalid_argument(
+            ("Unable to find componentDescriptor for " + unifiedComponentName)
+                .c_str());
+      } else {
+        return *_fallbackComponentDescriptor.get();
+      }
+    }
   }
 
   return *it->second;