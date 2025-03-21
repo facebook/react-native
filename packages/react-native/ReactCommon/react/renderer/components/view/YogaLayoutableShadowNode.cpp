2a2976dbefe975f9871bea20219151478a192f9b
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-03-21 15:42:41 +0100

    Remove redundant cast

--- packages/react-native/ReactCommon/react/renderer/components/view/YogaLayoutableShadowNode.cpp
+++ packages/react-native/ReactCommon/react/renderer/components/view/YogaLayoutableShadowNode.cpp
@@ -212,7 +212,7 @@ void YogaLayoutableShadowNode::adoptYogaChild(size_t index) {
   } else {
     // The child is owned by some other node, we need to clone that.
     // TODO: At this point, React has wrong reference to the node. (T138668036)
-    auto clonedChildNode = std::static_pointer_cast<YogaLayoutableShadowNode>(childNode.clone({}));
+    auto clonedChildNode = childNode.clone({});
 
     // Replace the child node with a newly cloned one in the children list.
     replaceChild(childNode, clonedChildNode, index);
