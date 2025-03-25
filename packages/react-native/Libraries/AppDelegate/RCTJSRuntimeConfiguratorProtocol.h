95c43ce94f30a4b9e663943121b5cd7b7e056a08
Kudo Chien <ckchien@gmail.com>
2025-03-26 00:44:39 +0800

    fix React-jsitooling build error for use_frameworks build

--- packages/react-native/Libraries/AppDelegate/RCTJSRuntimeConfiguratorProtocol.h
+++ packages/react-native/Libraries/AppDelegate/RCTJSRuntimeConfiguratorProtocol.h
@@ -5,10 +5,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#import <UIKit/UIKit.h>
-#import <react/runtime/JSRuntimeFactoryCAPI.h>
-
 #pragma once
+
+#if __has_include(<JSITooling/react/runtime/JSRuntimeFactoryCAPI.h>)
+// In a use_frameworks build and when any Objective-C files import the `React_RCTAppDelegate` framework,
+// Xcode will display an "include of non-modular header inside framework module" error.
+// The root cause is that the JSITooling CocoaPods-generated umbrella header does not correctly support submodules.
+// This workaround imports the header from outside the module.
+#import <JSITooling/react/runtime/JSRuntimeFactoryCAPI.h>
+#else
+#import <react/runtime/JSRuntimeFactoryCAPI.h>
+#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
