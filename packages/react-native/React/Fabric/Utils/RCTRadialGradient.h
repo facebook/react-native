e57fde294f1a244fe29d62468a810df020afc659
nishan (o^▽^o) <nishanbende@gmail.com>
2025-03-26 02:58:01 +0530

    radial gradient iOS changes

---
+++ packages/react-native/React/Fabric/Utils/RCTRadialGradient.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <UIKit/UIKit.h>
+#import <react/renderer/components/view/ViewProps.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTRadialGradient : NSObject
+
++ (CALayer *)gradientLayerWithSize:(CGSize)size gradient:(const facebook::react::RadialGradient &)gradient;
+
+@end
+
+NS_ASSUME_NONNULL_END
