64b8bb7f78583d8bb90b598144002043f46d130c
Alex Hunt <huntie@meta.com>
2025-03-26 08:24:27 -0700

    Refactor NetworkReporter internals (#50173)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50173
    
    Refactors the internals of `NetworkReporter` (and the `jsinspector_network` library) to better organise concepts before we scale to more Network CDP events.
    
    - Introduces `cdp::network` structs modelling CDP `Network` domain events and data types.
    - Moves implementation details in converting input data objects to CDP types into `CdpNetwork.cpp` and `HttpUtils.cpp`.
    
    Changelog: [Internal]
    
    Differential Revision: D71470039

--- packages/react-native/ReactCommon/jsinspector-modern/network/NetworkReporter.cpp
+++ packages/react-native/ReactCommon/jsinspector-modern/network/NetworkReporter.cpp
@@ -7,85 +7,17 @@
 
 #include "NetworkReporter.h"
 
-#include <glog/logging.h>
+#include "CdpNetwork.h"
+
+#include <folly/dynamic.h>
 #include <jsinspector-modern/cdp/CdpJson.h>
 
+#include <chrono>
 #include <stdexcept>
 
 namespace facebook::react::jsinspector_modern {
 
 namespace {
-
-/**
- * Get the CDP `ResourceType` for a given MIME type.
- *
- * https://chromedevtools.github.io/devtools-protocol/tot/Network/#type-ResourceType
- */
-std::string mimeTypeToResourceType(const std::string& mimeType) {
-  if (mimeType.find("image/") == 0) {
-    return "Image";
-  }
-
-  if (mimeType.find("video/") == 0 || mimeType.find("audio/") == 0) {
-    return "Media";
-  }
-
-  if (mimeType == "application/javascript" || mimeType == "text/javascript" ||
-      mimeType == "application/x-javascript") {
-    return "Script";
-  }
-
-  if (mimeType == "application/json" || mimeType.find("application/xml") == 0 ||
-      mimeType == "text/xml") {
-    // Assume XHR for JSON/XML types
-    return "XHR";
-  }
-
-  return "Other";
-}
-
-folly::dynamic headersToDynamic(const std::optional<Headers>& headers) {
-  folly::dynamic result = folly::dynamic::object;
-
-  if (headers) {
-    for (const auto& [key, value] : *headers) {
-      result[key] = value;
-    }
-  }
-
-  return result;
-}
-
-folly::dynamic requestToCdpParams(const RequestInfo& request) {
-  folly::dynamic result = folly::dynamic::object;
-  result["url"] = request.url;
-  result["method"] = request.httpMethod;
-  result["headers"] = headersToDynamic(request.headers);
-  result["postData"] = request.httpBody.value();
-
-  return result;
-}
-
-folly::dynamic responseToCdpParams(
-    const ResponseInfo& response,
-    int encodedDataLength) {
-  auto headers = response.headers.value_or(Headers());
-  std::string mimeType = "Other";
-
-  if (headers.find("Content-Type") != headers.end()) {
-    mimeType = mimeTypeToResourceType(headers.at("Content-Type"));
-  }
-
-  folly::dynamic result = folly::dynamic::object;
-  result["url"] = response.url;
-  result["status"] = response.statusCode;
-  result["statusText"] = "";
-  result["headers"] = headersToDynamic(response.headers);
-  result["mimeType"] = mimeType;
-  result["encodedDataLength"] = encodedDataLength;
-
-  return result;
-}
 
 /**
  * Get the current Unix timestamp in seconds (µs precision).
@@ -104,8 +36,8 @@ double getCurrentUnixTimestampSeconds() {
 } // namespace
 
 NetworkReporter& NetworkReporter::getInstance() {
-  static NetworkReporter tracer;
-  return tracer;
+  static NetworkReporter instance;
+  return instance;
 }
 
 void NetworkReporter::setFrontendChannel(FrontendChannel frontendChannel) {
@@ -118,7 +50,6 @@ bool NetworkReporter::enableDebugging() {
   }
 
   debuggingEnabled_.store(true, std::memory_order_release);
-  LOG(INFO) << "Network debugging enabled" << std::endl;
   return true;
 }
 
@@ -128,7 +59,6 @@ bool NetworkReporter::disableDebugging() {
   }
 
   debuggingEnabled_.store(false, std::memory_order_release);
-  LOG(INFO) << "Network debugging disabled" << std::endl;
   return true;
 }
 
@@ -136,34 +66,39 @@ void NetworkReporter::reportRequestStart(
     const std::string& requestId,
     const RequestInfo& requestInfo,
     int encodedDataLength,
-    const std::optional<ResponseInfo>& redirectResponse) {
-  if (!debuggingEnabled_.load(std::memory_order_relaxed)) {
+    const std::optional<ResponseInfo>& redirectResponse) const {
+  if (!isDebuggingEnabledNoSync()) {
     return;
   }
 
   double timestamp = getCurrentUnixTimestampSeconds();
+  auto request = cdp::network::Request::fromInputParams(requestInfo);
+  auto params = cdp::network::RequestWillBeSentParams{
+      .requestId = requestId,
+      .loaderId = "",
+      .documentURL = "mobile",
+      .request = std::move(request),
+      // NOTE: Both timestamp and wallTime use the same unit, however wallTime
+      // is relative to an "arbitrary epoch". In our implementation, use the
+      // Unix epoch for both.
+      .timestamp = timestamp,
+      .wallTime = timestamp,
+      .initiator = folly::dynamic::object("type", "script"),
+      .redirectHasExtraInfo = redirectResponse.has_value(),
+  };
 
-  folly::dynamic params = folly::dynamic::object;
-  params["requestId"] = requestId;
-  params["loaderId"] = "";
-  params["documentURL"] = "mobile";
-  params["request"] = requestToCdpParams(requestInfo);
-  // NOTE: timestamp and wallTime share the same time unit and precision,
-  // except wallTime is from an arbitrary epoch - use the Unix epoch for both.
-  params["timestamp"] = timestamp;
-  params["wallTime"] = timestamp;
-  params["initiator"] = folly::dynamic::object("type", "script");
-  params["redirectHasExtraInfo"] = redirectResponse.has_value();
   if (redirectResponse.has_value()) {
-    params["redirectResponse"] =
-        responseToCdpParams(redirectResponse.value(), encodedDataLength);
+    params.redirectResponse = cdp::network::Response::fromInputParams(
+        redirectResponse.value(), encodedDataLength);
   }
 
-  frontendChannel_(cdp::jsonNotification("Network.requestWillBeSent", params));
+  frontendChannel_(
+      cdp::jsonNotification("Network.requestWillBeSent", params.toDynamic()));
 }
 
-void NetworkReporter::reportConnectionTiming(const std::string& /*requestId*/) {
-  if (!debuggingEnabled_.load(std::memory_order_relaxed)) {
+void NetworkReporter::reportConnectionTiming(
+    const std::string& /*requestId*/) const {
+  if (!isDebuggingEnabledNoSync()) {
     return;
   }
 
@@ -171,8 +106,9 @@ void NetworkReporter::reportConnectionTiming(const std::string& /*requestId*/) {
   throw std::runtime_error("Not implemented");
 }
 
-void NetworkReporter::reportRequestFailed(const std::string& /*requestId*/) {
-  if (!debuggingEnabled_.load(std::memory_order_relaxed)) {
+void NetworkReporter::reportRequestFailed(
+    const std::string& /*requestId*/) const {
+  if (!isDebuggingEnabledNoSync()) {
     return;
   }
 
@@ -183,27 +119,29 @@ void NetworkReporter::reportRequestFailed(const std::string& /*requestId*/) {
 void NetworkReporter::reportResponseStart(
     const std::string& requestId,
     const ResponseInfo& responseInfo,
-    int encodedDataLength) {
-  if (!debuggingEnabled_.load(std::memory_order_relaxed)) {
+    int encodedDataLength) const {
+  if (!isDebuggingEnabledNoSync()) {
     return;
   }
 
-  folly::dynamic responseParams =
-      responseToCdpParams(responseInfo, encodedDataLength);
+  auto response =
+      cdp::network::Response::fromInputParams(responseInfo, encodedDataLength);
+  auto params = cdp::network::ResponseReceivedParams{
+      .requestId = requestId,
+      .loaderId = "",
+      .timestamp = getCurrentUnixTimestampSeconds(),
+      .type = cdp::network::resourceTypeFromMimeType(response.mimeType),
+      .response = response,
+      .hasExtraInfo = false,
+  };
 
-  folly::dynamic params = folly::dynamic::object;
-  params["requestId"] = requestId;
-  params["loaderId"] = "";
-  params["timestamp"] = getCurrentUnixTimestampSeconds();
-  params["type"] = responseParams["mimeType"];
-  params["response"] = responseParams;
-  params["hasExtraInfo"] = false;
-
-  frontendChannel_(cdp::jsonNotification("Network.responseReceived", params));
+  frontendChannel_(
+      cdp::jsonNotification("Network.responseReceived", params.toDynamic()));
 }
 
-void NetworkReporter::reportDataReceived(const std::string& /*requestId*/) {
-  if (!debuggingEnabled_.load(std::memory_order_relaxed)) {
+void NetworkReporter::reportDataReceived(
+    const std::string& /*requestId*/) const {
+  if (!isDebuggingEnabledNoSync()) {
     return;
   }
 
@@ -213,17 +151,19 @@ void NetworkReporter::reportDataReceived(const std::string& /*requestId*/) {
 
 void NetworkReporter::reportResponseEnd(
     const std::string& requestId,
-    int encodedDataLength) {
-  if (!debuggingEnabled_.load(std::memory_order_relaxed)) {
+    int encodedDataLength) const {
+  if (!isDebuggingEnabledNoSync()) {
     return;
   }
 
-  folly::dynamic params = folly::dynamic::object;
-  params["requestId"] = requestId;
-  params["timestamp"] = getCurrentUnixTimestampSeconds();
-  params["encodedDataLength"] = encodedDataLength;
+  auto params = cdp::network::LoadingFinishedParams{
+      .requestId = requestId,
+      .timestamp = getCurrentUnixTimestampSeconds(),
+      .encodedDataLength = encodedDataLength,
+  };
 
-  frontendChannel_(cdp::jsonNotification("Network.loadingFinished", params));
+  frontendChannel_(
+      cdp::jsonNotification("Network.loadingFinished", params.toDynamic()));
 }
 
 } // namespace facebook::react::jsinspector_modern
