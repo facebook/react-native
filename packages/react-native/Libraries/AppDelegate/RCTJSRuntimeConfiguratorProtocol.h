38cfd10d24a7ee627ff7e3bc14186f5e1b289fc9
Kudo Chien <ckchien@gmail.com>
2025-03-27 01:57:09 +0800

    forward declaration

--- packages/react-native/Libraries/AppDelegate/RCTJSRuntimeConfiguratorProtocol.h
+++ packages/react-native/Libraries/AppDelegate/RCTJSRuntimeConfiguratorProtocol.h
@@ -7,17 +7,11 @@
 
 #pragma once
 
-#if __has_include(<JSITooling/react/runtime/JSRuntimeFactoryCAPI.h>)
-// In a use_frameworks build and when any Objective-C files import the `React_RCTAppDelegate` framework,
-// Xcode will display an "include of non-modular header inside framework module" error.
-// The root cause is that the JSITooling CocoaPods-generated umbrella header does not correctly support submodules.
-// This workaround imports the header from outside the module.
-#import <JSITooling/react/runtime/JSRuntimeFactoryCAPI.h>
-#else
-#import <react/runtime/JSRuntimeFactoryCAPI.h>
-#endif
-
 NS_ASSUME_NONNULL_BEGIN
+
+// Forward declarations for umbrella headers.
+// In implementations, import `<react/runtime/JSRuntimeFactoryCAPI.h>` to obtain the actual type.
+typedef void* JSRuntimeFactoryRef;
 
 @protocol RCTJSRuntimeConfiguratorProtocol
 
