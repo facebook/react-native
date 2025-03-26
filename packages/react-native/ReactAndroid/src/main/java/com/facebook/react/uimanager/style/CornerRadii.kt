9170fe3be80db54c4215defa4e632ae42acbb2e8
Nick Gerleman <ngerlem@meta.com>
2025-03-26 15:02:26 -0700

    Move CornerRadii resolution logic out of LengthPercentage
    Summary:
    `LengthPercentage` is a class representing a length, or a percentage of a reference length.
    
    Having a function on it, specific to resolving asymetrical corner radius is the wrong organization. Let's move it to where it belongs.
    
    Very technically breaking, but I would be shocked if anyone is using this API.
    
    Changelog: [Internal]
    
    Reviewed By: jorge-cab
    
    Differential Revision: D71922893

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/style/CornerRadii.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/style/CornerRadii.kt
@@ -7,12 +7,20 @@
 
 package com.facebook.react.uimanager.style
 
+import com.facebook.react.uimanager.LengthPercentage
 import com.facebook.react.uimanager.PixelUtil
 
+/** Represents the resolved horizontal and vertical radii of the ellipse representing a corner. */
 public data class CornerRadii(
     val horizontal: Float = 0f,
     val vertical: Float = 0f,
 ) {
+  public constructor(
+      length: LengthPercentage,
+      referenceWidth: Float,
+      referenceHeight: Float
+  ) : this(horizontal = length.resolve(referenceWidth), vertical = length.resolve(referenceHeight))
+
   public fun toPixelFromDIP(): CornerRadii {
     return CornerRadii(PixelUtil.toPixelFromDIP(horizontal), PixelUtil.toPixelFromDIP(vertical))
   }
