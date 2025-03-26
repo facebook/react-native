c91cafae321c0cf6922260e059fef3ac14619aaa
Alex Hunt <huntie@meta.com>
2025-03-26 15:40:42 -0700

    Refactor NetworkReporter internals (#50173)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50173
    
    Refactors the internals of `NetworkReporter` (and the `jsinspector_network` library) to better organise concepts before we scale to more Network CDP events.
    
    - Introduces `cdp::network` structs modelling CDP `Network` domain events and data types.
    - Moves implementation details in converting input data objects to CDP types into `CdpNetwork.cpp` and `HttpUtils.cpp`.
    
    Changelog: [Internal]
    
    Reviewed By: hoxyq
    
    Differential Revision: D71470039
    
    fbshipit-source-id: 0c04ffb40efbbb6d6d9782959f5adb33c9097ccb

---
+++ packages/react-native/ReactCommon/jsinspector-modern/network/HttpUtils.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "NetworkTypes.h"
+
+#include <string>
+
+namespace facebook::react::jsinspector_modern {
+
+/**
+ * Get the MIME type for a response based on the 'Content-Type' header. If
+ * the header is not present, returns 'application/octet-stream'.
+ */
+std::string mimeTypeFromHeaders(const Headers& headers);
+
+} // namespace facebook::react::jsinspector_modern
