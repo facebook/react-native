4b82896390592f2b1678ac96aaa4b35cbb97f64c
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-03-26 08:26:01 -0700

    Automate checking for artifacts on Maven (#50275)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50275
    
    This change adds a check to automate a step in the release process: https://github.com/reactwg/react-native-releases/blob/main/docs/guide-release-process.md#verify-assets-have-been-uploaded-to-maven
    
    The script will poll maven for 90 minutes and return when the artifacts are available. If, after 90 minutes, artifacts are not available, it exits with code 1 that should fail the Release workflow. The Release Crew should have a look at what's happened.
    
    ## Changelog:
    [Internal] - Automate the check for artifacts being on Maven
    
    Reviewed By: fabriziocucci
    
    Differential Revision: D71825014
    
    fbshipit-source-id: 8879bf9c8fc4519e86b55ad8f9bd3ecf3f8ecfb7

---
+++ .github/workflow-scripts/__tests__/verifyArtifactsAreOnMaven-test.js
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+const {verifyArtifactsAreOnMaven} = require('../verifyArtifactsAreOnMaven');
+
+const mockSleep = jest.fn();
+const silence = () => {};
+const mockFetch = jest.fn();
+const mockExit = jest.fn();
+
+jest.mock('../utils.js', () => ({
+  log: silence,
+  sleep: mockSleep,
+}));
+
+process.exit = mockExit;
+global.fetch = mockFetch;
+
+describe('#verifyArtifactsAreOnMaven', () => {
+  beforeEach(jest.clearAllMocks);
+
+  it('waits for the packages to be published on maven when version has no v', async () => {
+    mockSleep.mockReturnValueOnce(Promise.resolve()).mockImplementation(() => {
+      throw new Error('Should not be called again!');
+    });
+    mockFetch
+      .mockReturnValueOnce(Promise.resolve({status: 404}))
+      .mockReturnValueOnce(Promise.resolve({status: 200}));
+
+    const version = '0.78.1';
+    await verifyArtifactsAreOnMaven(version);
+
+    expect(mockSleep).toHaveBeenCalledTimes(1);
+    expect(mockFetch).toHaveBeenCalledWith(
+      'https://repo1.maven.org/maven2/com/facebook/react/react-native-artifacts/0.78.1',
+    );
+  });
+
+  it('waits for the packages to be published on maven, when version starts with v', async () => {
+    mockSleep.mockReturnValueOnce(Promise.resolve()).mockImplementation(() => {
+      throw new Error('Should not be called again!');
+    });
+    mockFetch
+      .mockReturnValueOnce(Promise.resolve({status: 404}))
+      .mockReturnValueOnce(Promise.resolve({status: 200}));
+
+    const version = 'v0.78.1';
+    await verifyArtifactsAreOnMaven(version);
+
+    expect(mockSleep).toHaveBeenCalledTimes(1);
+    expect(mockFetch).toHaveBeenCalledWith(
+      'https://repo1.maven.org/maven2/com/facebook/react/react-native-artifacts/0.78.1',
+    );
+  });
+
+  it('passes immediately if packages are already on Maven', async () => {
+    mockFetch.mockReturnValueOnce(Promise.resolve({status: 200}));
+
+    const version = '0.78.1';
+    await verifyArtifactsAreOnMaven(version);
+
+    expect(mockSleep).toHaveBeenCalledTimes(0);
+    expect(mockFetch).toHaveBeenCalledWith(
+      'https://repo1.maven.org/maven2/com/facebook/react/react-native-artifacts/0.78.1',
+    );
+  });
+
+  it('tries 90 times and then exits', async () => {
+    mockSleep.mockReturnValue(Promise.resolve());
+    mockFetch.mockReturnValue(Promise.resolve({status: 404}));
+
+    const version = '0.78.1';
+    await verifyArtifactsAreOnMaven(version);
+
+    expect(mockSleep).toHaveBeenCalledTimes(90);
+    expect(mockExit).toHaveBeenCalledWith(1);
+    expect(mockFetch).toHaveBeenCalledWith(
+      'https://repo1.maven.org/maven2/com/facebook/react/react-native-artifacts/0.78.1',
+    );
+  });
+});
