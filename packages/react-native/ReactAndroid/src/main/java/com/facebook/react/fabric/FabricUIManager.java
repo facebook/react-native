5e5b4b71d4a1f139aa3a8b657ab99ce95e9ae284
Gijs Weterings <gweterings@meta.com>
2025-03-28 08:43:29 -0700

    fix nullsafe FIXMEs for SurfaceMountingManager.java and mark nullsafe
    Summary:
    Gone trough all the FIXMEs added in the previous diff by the nullsafe tool, marked the class as nullsafe and ensured no remaining violations.
    Changelog: [Android][Fixed] Made SurfaceMountingManager.java nullsafe
    
    Differential Revision: D71979577

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
@@ -728,7 +728,7 @@ public class FabricUIManager
       int rootTag,
       int reactTag,
       final String componentName,
-      @Nullable Object props,
+      ReadableMap props,
       @Nullable Object stateWrapper,
       boolean isLayoutable) {
     mMountItemDispatcher.addPreAllocateMountItem(
