e1758793c27b5d9458c642c3f4c3643d13d48d39
nishan (o^▽^o) <nishanbende@gmail.com>
2025-04-01 12:48:59 +0530

    add parse function to BackgroundImageLayer

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/style/BackgroundImageLayer.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/style/BackgroundImageLayer.kt
@@ -13,20 +13,31 @@ import android.graphics.Shader
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.bridge.ReadableType
 
-public class BackgroundImageLayer(gradientMap: ReadableMap?, context: Context) {
-  private val gradient: Gradient? = parseGradient(gradientMap, context)
+public class BackgroundImageLayer() {
+  private lateinit var gradient: Gradient
 
-  private fun parseGradient(gradientMap: ReadableMap?, context: Context): Gradient? {
-    if (gradientMap == null) return null
-    if (!gradientMap.hasKey("type") || gradientMap.getType("type") != ReadableType.String) return null
+  private constructor(gradient: Gradient) : this() {
+    this.gradient = gradient
+  }
 
-    return when (gradientMap.getString("type")) {
-      "linear-gradient" -> LinearGradient.parse(gradientMap, context)
-      "radial-gradient" -> RadialGradient.parse(gradientMap, context)
-      else -> null
+  public companion object {
+    public fun parse(gradientMap: ReadableMap?, context: Context): BackgroundImageLayer? {
+      if (gradientMap == null) return null
+      val gradient = parseGradient(gradientMap, context) ?: return null
+      return BackgroundImageLayer(gradient)
+    }
+
+    private fun parseGradient(gradientMap: ReadableMap, context: Context): Gradient? {
+      if (!gradientMap.hasKey("type") || gradientMap.getType("type") != ReadableType.String) return null
+
+      return when (gradientMap.getString("type")) {
+        "linear-gradient" -> LinearGradient.parse(gradientMap, context)
+        "radial-gradient" -> RadialGradient.parse(gradientMap, context)
+        else -> null
+      }
     }
   }
 
-  public fun getShader(bounds: Rect): Shader? =
-    gradient?.getShader(bounds.width().toFloat(), bounds.height().toFloat())
+  public fun getShader(bounds: Rect): Shader =
+    gradient.getShader(bounds.width().toFloat(), bounds.height().toFloat())
 }
