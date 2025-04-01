be8393c41b3d613385a2ee4632438030e80d6b2d
Iwo Plaza <plazaiwo@meta.com>
2025-04-01 03:26:25 -0700

    Limit @react-native/virtualized-lists subpath imports (#50392)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50392
    
    ## This diff
    Defines an exports field for the "react-native/virtualized-lists" package. Limits internal use of subpath
    imports to virtualized-lists package, and exports all currently used APIs one the root level.
    
    Changelog:
    [General][Breaking] - Subpath imports to the internal react-native/virtualized-lists package are not allowed.
    
    Reviewed By: huntie
    
    Differential Revision: D72162344
    
    fbshipit-source-id: 828dab8e569f019fc48084af475e152b898f5bb5

--- packages/virtualized-lists/index.js
+++ packages/virtualized-lists/index.js
@@ -10,12 +10,18 @@
 
 'use strict';
 
+import typeof ChildListCollection from './Lists/ChildListCollection';
 import typeof FillRateHelper from './Lists/FillRateHelper';
 import typeof ViewabilityHelper from './Lists/ViewabilityHelper';
 import typeof VirtualizedList from './Lists/VirtualizedList';
 import typeof VirtualizedSectionList from './Lists/VirtualizedSectionList';
 
-import {typeof VirtualizedListContextResetter} from './Lists/VirtualizedListContext';
+import {
+  typeof VirtualizedListCellContextProvider,
+  typeof VirtualizedListContext,
+  typeof VirtualizedListContextProvider,
+  typeof VirtualizedListContextResetter,
+} from './Lists/VirtualizedListContext';
 import {keyExtractor} from './Lists/VirtualizeUtils';
 
 export type {
@@ -49,13 +55,27 @@ export default {
     return require('./Lists/VirtualizedSectionList').default;
   },
   get VirtualizedListContextResetter(): VirtualizedListContextResetter {
-    const VirtualizedListContext = require('./Lists/VirtualizedListContext');
-    return VirtualizedListContext.VirtualizedListContextResetter;
+    return require('./Lists/VirtualizedListContext')
+      .VirtualizedListContextResetter;
+  },
+  get VirtualizedListContext(): VirtualizedListContext {
+    return require('./Lists/VirtualizedListContext').VirtualizedListContext;
+  },
+  get VirtualizedListContextProvider(): VirtualizedListContextProvider {
+    return require('./Lists/VirtualizedListContext')
+      .VirtualizedListContextProvider;
+  },
+  get VirtualizedListCellContextProvider(): VirtualizedListCellContextProvider {
+    return require('./Lists/VirtualizedListContext')
+      .VirtualizedListCellContextProvider;
   },
   get ViewabilityHelper(): ViewabilityHelper {
     return require('./Lists/ViewabilityHelper').default;
   },
   get FillRateHelper(): FillRateHelper {
     return require('./Lists/FillRateHelper').default;
+  },
+  get ChildListCollection(): ChildListCollection {
+    return require('./Lists/ChildListCollection').default;
   },
 };
