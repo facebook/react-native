5ec0a43c82943315d2b3a1f48d4a528e9ad6611d
Nicola Corti <ncor@meta.com>
2025-04-04 04:35:31 -0700

    Add a `legacyWarningsEnabled` property to enable Legacy Warnings on NewArch
    Summary:
    This diff introduces a new property called `legacyWarningsEnabled` for `gradle.properties` that
    toggles the Legacy Arch warnings for users.
    
    I've also introduced a new `ReactNativeApplicationEntryPoint` that is generated by RNGP. This class
    effectively wrap `DefaultNewArchitectureEntryPoint` by setting warnings and also invoking SoLoader.
    It will reduce the errors in the user space.
    
    As of now warnigns appear in Logcat, but I'm looking into adding some UI in a subsequent diff.
    
    Changelog:
    [Android] [Added] - Add a `legacyWarningsEnabled` property to enable Legacy Warnings on NewArch
    
    Differential Revision: D72383907

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/common/annotations/internal/LegacyArchitectureLogger.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/common/annotations/internal/LegacyArchitectureLogger.kt
@@ -25,6 +25,23 @@ public object LegacyArchitectureLogger {
   private val tag: String = "LegacyArchitectureLogger"
 
   /**
+   * Whether the OSS_LEGACY_WARNINGS_ENABLED should be enabled or not for OSS users. This is
+   * controlled through the `legacyWarningsEnabled` property in the gradle.properties file.
+   */
+  @JvmField public var OSS_LEGACY_WARNINGS_ENABLED: Boolean = false
+
+  private val exceptionMessage: String
+    get() =
+        if (OSS_LEGACY_WARNINGS_ENABLED) {
+          "is being executed when app is fully running on the NEW Architecture."
+        } else {
+          "is being executed when `legacyWarningsEnabled` is enabled. " +
+              "You're accessing a legacy API that will be removed in a future version of React Native. " +
+              "You should investigate the stacktrace you find below and understand which class is " +
+              "accessing this API and clean it up."
+        }
+
+  /**
    * Asserts and logs when legacy architecture components are being used in new architecture. This
    * method will throw an exception if the app is running on the new architecture and the logLevel
    * received by parameter is [LegacyArchitectureLogLevel.ERROR].
@@ -37,17 +54,15 @@ public object LegacyArchitectureLogger {
       name: String,
       logLevel: LegacyArchitectureLogLevel = LegacyArchitectureLogLevel.WARNING
   ) {
-    if (ReactBuildConfig.UNSTABLE_ENABLE_MINIFY_LEGACY_ARCHITECTURE) {
+    if (ReactBuildConfig.UNSTABLE_ENABLE_MINIFY_LEGACY_ARCHITECTURE ||
+        OSS_LEGACY_WARNINGS_ENABLED) {
       when (logLevel) {
         LegacyArchitectureLogLevel.ERROR -> {
-          throw AssertionException(
-              "$name is being executed when app is fully running on the NEW Architecture.")
+          throw AssertionException("$name $exceptionMessage")
         }
         LegacyArchitectureLogLevel.WARNING -> {
           ReactSoftExceptionLogger.logSoftException(
-              tag,
-              ReactNoCrashSoftException(
-                  "$name is being executed when app is fully running on the NEW Architecture."))
+              tag, ReactNoCrashSoftException("$name $exceptionMessage."))
         }
       }
     }
