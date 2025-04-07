7a5b9a3cb9e6191ede74485cefabf1f897293a4d
Nicola Corti <ncor@meta.com>
2025-04-07 08:46:55 -0700

    Migrate to Kotlin - DevSupportManagerFactory (#50516)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50516
    
    This diff migrates the following file to Kotlin - DevSupportManagerFactory
    as part of our ongoing effort of migrating the codebase to Kotlin
    
    Changelog:
    [Android] [Changed] - Migrate to Kotlin - DevSupportManagerFactory - We couldn't find any implementation of this class in OSS. Some Kotlin implementers might have to change the method signatures. However this interface is not supposed to be extended in OSS.
    
    Reviewed By: javache
    
    Differential Revision: D72556310

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/DevSupportManagerFactory.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.react.devsupport
+
+import android.content.Context
+import com.facebook.react.common.SurfaceDelegateFactory
+import com.facebook.react.devsupport.interfaces.DevBundleDownloadListener
+import com.facebook.react.devsupport.interfaces.DevLoadingViewManager
+import com.facebook.react.devsupport.interfaces.DevSupportManager
+import com.facebook.react.devsupport.interfaces.PausedInDebuggerOverlayManager
+import com.facebook.react.devsupport.interfaces.RedBoxHandler
+import com.facebook.react.packagerconnection.RequestHandler
+
+public interface DevSupportManagerFactory {
+
+  /**
+   * Factory used by the Old Architecture flow to create a [DevSupportManager] and a
+   * [BridgeDevSupportManager]
+   */
+  public fun create(
+      applicationContext: Context,
+      reactInstanceManagerHelper: ReactInstanceDevHelper,
+      packagerPathForJSBundleName: String?,
+      enableOnCreate: Boolean,
+      redBoxHandler: RedBoxHandler?,
+      devBundleDownloadListener: DevBundleDownloadListener?,
+      minNumShakes: Int,
+      customPackagerCommandHandlers: Map<String, RequestHandler>?,
+      surfaceDelegateFactory: SurfaceDelegateFactory?,
+      devLoadingViewManager: DevLoadingViewManager?,
+      pausedInDebuggerOverlayManager: PausedInDebuggerOverlayManager?
+  ): DevSupportManager
+
+  /**
+   * Factory used by the New Architecture/Bridgeless flow to create a [DevSupportManager] and a
+   * [BridgelessDevSupportManager]
+   */
+  public fun create(
+      applicationContext: Context,
+      reactInstanceManagerHelper: ReactInstanceDevHelper,
+      packagerPathForJSBundleName: String?,
+      enableOnCreate: Boolean,
+      redBoxHandler: RedBoxHandler?,
+      devBundleDownloadListener: DevBundleDownloadListener?,
+      minNumShakes: Int,
+      customPackagerCommandHandlers: Map<String, RequestHandler>?,
+      surfaceDelegateFactory: SurfaceDelegateFactory?,
+      devLoadingViewManager: DevLoadingViewManager?,
+      pausedInDebuggerOverlayManager: PausedInDebuggerOverlayManager?,
+      useDevSupport: Boolean
+  ): DevSupportManager
+}
