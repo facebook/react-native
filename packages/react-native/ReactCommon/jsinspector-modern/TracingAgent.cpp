af5074509ea59ed96cdbbe7f4535a567fd08954b
Ruslan Lesiutin <hoxy@meta.com>
2025-04-08 09:40:50 -0700

    Refactor RuntimeSamplingProfileTraceEventSerializer: use smart pointers, split code, add tests (#50531)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50531
    
    # Changelog: [Internal]
    
    This should not have any functional changes, mostly refactoring the previous code, splitting it into smaller methods, better documentation and added tests.
    
    Main changes are:
    1. We are going to use smart pointers for `ProfileTreeNode`
    2. Split single static method into multiple smaller methods of one class, less cognitive load and smaller context window
    
    Reviewed By: robhogan
    
    Differential Revision: D72401690
    
    fbshipit-source-id: a1eed5501349dcd811661d91f3bf126a0daaacf2

--- packages/react-native/ReactCommon/jsinspector-modern/TracingAgent.cpp
+++ packages/react-native/ReactCommon/jsinspector-modern/TracingAgent.cpp
@@ -72,7 +72,9 @@ bool TracingAgent::handleRequest(const cdp::PreparsedRequest& req) {
     }
 
     instanceAgent_->stopTracing();
-    bool correctlyStopped = PerformanceTracer::getInstance().stopTracing();
+
+    PerformanceTracer& performanceTracer = PerformanceTracer::getInstance();
+    bool correctlyStopped = performanceTracer.stopTracing();
     if (!correctlyStopped) {
       frontendChannel_(cdp::jsonError(
           req.id,
@@ -90,15 +92,16 @@ bool TracingAgent::handleRequest(const cdp::PreparsedRequest& req) {
           "Tracing.dataCollected",
           folly::dynamic::object("value", eventsChunk)));
     };
-    PerformanceTracer::getInstance().collectEvents(
+    performanceTracer.collectEvents(
         dataCollectedCallback, TRACE_EVENT_CHUNK_SIZE);
 
-    tracing::RuntimeSamplingProfileTraceEventSerializer::serializeAndNotify(
-        PerformanceTracer::getInstance(),
-        instanceAgent_->collectTracingProfile().getRuntimeSamplingProfile(),
-        instanceTracingStartTimestamp_,
+    tracing::RuntimeSamplingProfileTraceEventSerializer serializer(
+        performanceTracer,
         dataCollectedCallback,
         PROFILE_TRACE_EVENT_CHUNK_SIZE);
+    serializer.serializeAndNotify(
+        instanceAgent_->collectTracingProfile().getRuntimeSamplingProfile(),
+        instanceTracingStartTimestamp_);
 
     frontendChannel_(cdp::jsonNotification(
         "Tracing.tracingComplete",
