3265f8336dd1c8e935eb562d126bf3c2399cb5e4
Dawid Małecki <maleckidawid@meta.com>
2025-04-09 15:04:50 +0200

    new type

--- packages/react-native/types/rollup.d.ts
+++ packages/react-native/types/rollup.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable redundant-undefined/redundant-undefined */
 import type {EventSubscription as EventSubscription_2} from 'react-native/Libraries/vendor/emitter/EventEmitter';
 import type {ListRenderItem} from '@react-native/virtualized-lists';
 
@@ -16,9 +17,14 @@ export declare type AccessibilityAnnouncementFinishedEventHandler = (
 ) => void;
 export declare type AccessibilityChangeEvent = boolean;
 
-declare const stagger: (
+export declare const stagger: (
     time: number,
     animations: Array<CompositeAnimation>
   ) => CompositeAnimation;
 
-type Test = string;
+
+export declare type ____ViewStyle_InternalBase = Readonly<{
+  backfaceVisibility?: "visible" | "hidden";
+  backgroundColor?: ____ColorValue_Internal;
+  borderColor?: ____ColorValue_Internal;
+}>;
