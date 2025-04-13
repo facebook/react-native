340e9f63cb476c5593825479c74e16480f3022c8
nitin shukla <nitinshukla413@gmail.com>
2025-04-13 21:56:10 +0530

    migrating kotlin notation changed

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/text/frescosupport/FrescoBasedReactTextInlineImageShadowNode.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/text/frescosupport/FrescoBasedReactTextInlineImageShadowNode.kt
@@ -33,85 +33,85 @@
  @Nullsafe(Nullsafe.Mode.LOCAL)
  @LegacyArchitecture
  internal class FrescoBasedReactTextInlineImageShadowNode(
-     private val mDraweeControllerBuilder: AbstractDraweeControllerBuilder<*, *, *, *>,
-     @Nullable private val mCallerContext: Any?
+     private val draweeControllerBuilder: AbstractDraweeControllerBuilder<*, *, *, *>,
+     @Nullable private val callerContext: Any?
  ) : ReactTextInlineImageShadowNode() {
  
-     @Nullable private var mUri: Uri? = null
-     @Nullable private var mHeaders: ReadableMap? = null
-     private var mWidth = YogaConstants.UNDEFINED
-     @Nullable private var mResizeMode: String? = null
-     private var mHeight = YogaConstants.UNDEFINED
-     private var mTintColor = 0
+     @Nullable private var uri: Uri? = null
+     @Nullable private var headers: ReadableMap? = null
+     private var width = YogaConstants.UNDEFINED
+     @Nullable private var resizeMode: String? = null
+     private var height = YogaConstants.UNDEFINED
+     private var tintColor = 0
  
      @ReactProp(name = "src")
      public fun setSource(@Nullable sources: ReadableArray?) {
          val source =
-             if (sources == null || sources.size() == 0 || sources.getType(0) != ReadableType.Number)
+             if (sources == null || sources.size() == 0 || sources.getType(0) != ReadableType.Map)
                  null
              else
                  Preconditions.checkNotNull(sources.getMap(0)).getString("uri")
-         var uri: Uri? = null
+         var tempUri: Uri? = null
          if (source != null) {
              try {
-                 uri = Uri.parse(source)
+                 tempUri = Uri.parse(source)
                  // Verify scheme is set, so that relative uri (used by static resources) are not handled.
-                 if (uri.scheme == null) {
-                     uri = null
+                 if (tempUri.scheme == null) {
+                     tempUri = null
                  }
              } catch (e: Exception) {
                  // ignore malformed uri, then attempt to extract resource ID.
              }
-             if (uri == null) {
-                 uri = getResourceDrawableUri(getThemedContext(), source)
+             if (tempUri == null) {
+                 tempUri = getResourceDrawableUri(getThemedContext(), source)
              }
          }
-         if (uri != mUri) {
+         if (tempUri != uri) {
              markUpdated()
          }
-         mUri = uri
+         uri = tempUri
      }
  
      @ReactProp(name = "headers")
-     public fun setHeaders(@Nullable headers: ReadableMap?) {
-         mHeaders = headers
+     public fun setHeaders(@Nullable newHeaders: ReadableMap?) {
+         headers = newHeaders
      }
  
      @ReactProp(name = "tintColor", customType = "Color")
-     public fun setTintColor(tintColor: Int) {
-         mTintColor = tintColor
+     public fun setTintColor(newTintColor: Int) {
+         tintColor = newTintColor
      }
  
      /** Besides width/height, all other layout props on inline images are ignored */
-     override fun setWidth(width: Dynamic) {
-         if (width.type == ReadableType.Number) {
-             mWidth = width.asDouble().toFloat()
+     override fun setWidth(newWidth: Dynamic) {
+         if (newWidth.type == ReadableType.Number) {
+             width = newWidth.asDouble().toFloat()
          } else {
              FLog.w(ReactConstants.TAG, "Inline images must not have percentage based width")
-             mWidth = YogaConstants.UNDEFINED
+             width = YogaConstants.UNDEFINED
          }
      }
  
-     override fun setHeight(height: Dynamic) {
-         if (height.type == ReadableType.Number) {
-             mHeight = height.asDouble().toFloat()
+     override fun setHeight(newHeight: Dynamic) {
+         if (newHeight.type == ReadableType.Number) {
+             height = newHeight.asDouble().toFloat()
          } else {
              FLog.w(ReactConstants.TAG, "Inline images must not have percentage based height")
-             mHeight = YogaConstants.UNDEFINED
+             height = YogaConstants.UNDEFINED
          }
      }
  
      @ReactProp(name = ViewProps.RESIZE_MODE)
-     public fun setResizeMode(@Nullable resizeMode: String?) {
-         mResizeMode = resizeMode
+     public fun setResizeMode(@Nullable newResizeMode: String?) {
+         resizeMode = newResizeMode
      }
  
      public fun getUri(): Uri? {
-         return mUri
+         return uri
      }
  
      public fun getHeaders(): ReadableMap? {
-         return mHeaders
+         return headers
      }
  
      // TODO: t9053573 is tracking that this code should be shared
@@ -120,8 +120,8 @@
              if (name == null || name.isEmpty()) {
                  return null
              }
-             val name = name.toLowerCase(Locale.getDefault()).replace("-", "_")
-             val resId = context.resources.getIdentifier(name, "drawable", context.packageName)
+             val formattedName = name.toLowerCase(Locale.getDefault()).replace("-", "_")
+             val resId = context.resources.getIdentifier(formattedName, "drawable", context.packageName)
              return Uri.Builder()
                  .scheme(UriUtil.LOCAL_RESOURCE_SCHEME)
                  .path(resId.toString())
@@ -135,26 +135,26 @@
  
      override fun buildInlineImageSpan(): TextInlineImageSpan {
          val resources = getThemedContext().resources
-         val width = Math.ceil(mWidth.toDouble()).toInt()
-         val height = Math.ceil(mHeight.toDouble()).toInt()
+         val finalWidth = Math.ceil(width.toDouble()).toInt()
+         val finalHeight = Math.ceil(height.toDouble()).toInt()
          return FrescoBasedReactTextInlineImageSpan(
              resources,
-             height,
-             width,
-             mTintColor,
+             finalHeight,
+             finalWidth,
+             tintColor,
              getUri(),
              getHeaders(),
              getDraweeControllerBuilder(),
              getCallerContext(),
-             mResizeMode
+             resizeMode
          )
      }
  
      public fun getDraweeControllerBuilder(): AbstractDraweeControllerBuilder<*, *, *, *> {
-         return mDraweeControllerBuilder
+         return draweeControllerBuilder
      }
  
      public fun getCallerContext(): Any? {
-         return mCallerContext
+         return callerContext
      }
  }
