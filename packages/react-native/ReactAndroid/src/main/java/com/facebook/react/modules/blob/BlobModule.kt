a070d529fcddfabc7f9154bbe0d40badc1af5c7d
Alan Lee <alanlee2@meta.com>
2025-04-15 20:45:07 -0700

    Suppress deprecation error in OSS for BlobModule (#50748)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50748
    
    We have a different version of OkHttp internally and in OSS so we need to suppress these for now.
    
    Changelog: [Internal]
    
    Reviewed By: Abbondanzo
    
    Differential Revision: D73079163
    
    fbshipit-source-id: 1928b00acbba5bc6577a248bc46a091d5b970f10

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/blob/BlobModule.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/blob/BlobModule.kt
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+@file:Suppress("DEPRECATION_ERROR") // Conflicting okhttp versions
+
 package com.facebook.react.modules.blob
 
 import android.net.Uri
