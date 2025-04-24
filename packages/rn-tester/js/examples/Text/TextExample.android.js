128f29a39b090a28865a760760dd39ea2228d108
Eric Bailey <git@esb.lol>
2025-04-24 12:02:00 -0500

    Add example to Text examples

--- packages/rn-tester/js/examples/Text/TextExample.android.js
+++ packages/rn-tester/js/examples/Text/TextExample.android.js
@@ -1064,7 +1064,136 @@ function TextBaseLineLayoutExample(props: {}): React.Node {
   );
 }
 
+function TruncationIssue() {
+  const common = {
+    paddingTop: 8,
+    color: "black",
+    fontWeight: "700",
+    flexShrink: 1,
+    flexBasis: "auto",
+    /**
+     * Value of 1 helps with taking up as much space as it needs to, but then
+     * short names align to the left
+     */
+    flexGrow: 0,
+    /**
+     * Adjusting font size also arbitrarily affects the truncation of text
+     */
+    fontSize: 24,
+    borderWidth: 1,
+    borderColor: "red",
+  };
+  return (
+    <View>
+      <View
+        style={{
+          paddingVertical: 20,
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 8,
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            borderWidth: 1,
+            borderStyle: "dashed",
+            borderColor: "red",
+          }}
+        >
+          <Text
+            testID="profileHeaderDisplayName"
+            numberOfLines={1}
+            style={common}
+          >
+            Tom
+          </Text>
+          <View>
+            <View style={{ width: 40, height: 40, backgroundColor: "black" }} />
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          paddingVertical: 20,
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 8,
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            borderWidth: 1,
+            borderStyle: "dashed",
+            borderColor: "red",
+          }}
+        >
+          <Text
+            testID="profileHeaderDisplayName"
+            numberOfLines={1}
+            style={common}
+          >
+            Today's Tom Sawyeeeeeeeeeeeeee Mean Mean Stride
+          </Text>
+          <View>
+            <View style={{ width: 40, height: 40, backgroundColor: "black" }} />
+          </View>
+        </View>
+      </View>
+
+      {/**
+       * The content of the text clearly names a difference too
+       */}
+      <View
+        style={{
+          paddingVertical: 20,
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 8,
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            borderWidth: 1,
+            borderStyle: "dashed",
+            borderColor: "red",
+          }}
+        >
+          <Text
+            testID="profileHeaderDisplayName"
+            numberOfLines={1}
+            style={common}
+          >
+            Today Tom Sawyeeeeeeeeeeeeee Mean Mean Stride
+          </Text>
+          <View>
+            <View style={{ width: 40, height: 40, backgroundColor: "black" }} />
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
 const examples = [
+  {
+    title: 'Truncation Issue',
+    name: 'truncationIssue',
+    render(): React.Node {
+      return <TruncationIssue />
+    }
+  },
   {
     title: 'Dynamic Font Size Adjustment',
     name: 'ajustingFontSize',
