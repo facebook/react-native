ac8b01fb6f27353d0bb59fa94aca523bfaff1c71
Pieter De Baets <pieterdb@meta.com>
2025-04-24 11:41:11 -0700

    Fix OSS build error in JavaModuleWrapper (#50905)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50905
    
    Subtle compiler differences cause this to fail, but we can just `Class<*>` here instead.
    
    Changelog: [Internal]
    
    Reviewed By: cortinico, rshest
    
    Differential Revision: D73593304
    
    fbshipit-source-id: ea3996fc0641ae5d12a6923bd78645e21232afe7

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/JavaModuleWrapper.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/JavaModuleWrapper.kt
@@ -60,14 +60,13 @@ internal class JavaModuleWrapper(
   private fun findMethods() {
     Systrace.beginSection(TRACE_TAG_REACT, "findMethods")
 
-    var classForMethods: Class<out NativeModule> = moduleHolder.module.javaClass
+    var classForMethods: Class<*> = moduleHolder.module.javaClass
     val superClass = classForMethods.superclass
-    if (TurboModule::class.java.isAssignableFrom(superClass)) {
+    if (superClass != null && TurboModule::class.java.isAssignableFrom(superClass)) {
       // For java module that is based on generated flow-type spec, inspect the
       // spec abstract class instead, which is the super class of the given Java
       // module.
-      @Suppress("UNCHECKED_CAST")
-      classForMethods = superClass as Class<out NativeModule>
+      classForMethods = superClass
     }
 
     val targetMethods = classForMethods.declaredMethods
