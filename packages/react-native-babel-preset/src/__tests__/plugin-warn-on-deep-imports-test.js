5d367d460f2c6e5716d45169c79b9a3c23dbda7a
Dawid Małecki <maleckidawid@meta.com>
2025-04-24 04:25:19 -0700

    Add babel plugin to `@react-native/babel-preset` for `console.warn` injection under deep react native imports (#50802)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50802
    
    The plugin analyses the source of all `import`, `require`, and `export` statements and injects the `console.warn` statement for each path targeting deep react-native source code. It runs only on a dev mode so there is no need to keep that in the `if (__DEV__) ` block. It is possible to disable this plugin by setting `disableDeepImportWarnings: true` and **resetting** the Metro cache:
    
    ```js
    module.exports = {
      presets: [['module:react-native/babel-preset', {
        "disableDeepImportWarnings": true
      }]],
    };
    ```
    
    Changelog:
    [General][Internal] - Added plugin to react-native/babel-preset injecting `console.warn` for each react native deep import in dev mode.
    
    For a given code:
    ```js
    import { Image } from 'react-native';
    import View from 'react-native/Libraries/Components/View/View';
    const Text = require('react-native/Libraries/Text/Text');
    export { PressabilityDebugView } from 'react-native/Libraries/Pressability/PressabilityDebug';
    ```
    
    The transformed output should look like:
    
    ```js
    import { Image } from 'react-native';
    import View from 'react-native/Libraries/Components/View/View';
    const Text = require('react-native/Libraries/Text/Text');
    export { PressabilityDebugView } from 'react-native/Libraries/Pressability/PressabilityDebug';
    console.warn("Deep imports from the 'react-native' package are deprecated ('react-native/Libraries/Components/View/View').");
    console.warn("Deep imports from the 'react-native' package are deprecated ('react-native/Libraries/Text/Text').");
    console.warn("Deep imports from the 'react-native' package are deprecated ('react-native/Libraries/Pressability/PressabilityDebug').");
    ```
    
    For more information about why this plugin was needed, please check [RFC](https://github.com/react-native-community/discussions-and-proposals/pull/894).
    
    Reviewed By: huntie
    
    Differential Revision: D70783145

---
+++ packages/react-native-babel-preset/src/__tests__/plugin-warn-on-deep-imports-test.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @oncall react_native
+ */
+
+'use strict';
+
+const {transform} = require('../__mocks__/test-helpers');
+const rnDeepImportsWarningPlugin = require('../plugin-warn-on-deep-imports');
+
+jest.mock('path', () => ({
+  ...jest.requireActual('path'),
+  resolve: jest.fn((...args) => args.join('/')),
+}));
+
+test('deep esm import', () => {
+  const code = `
+    import View from 'react-native/Libraries/Components/View/View';
+    import {Text} from 'react-native';
+  `;
+
+  expect(transform(code, [rnDeepImportsWarningPlugin])).toMatchInlineSnapshot(`
+    "import View from 'react-native/Libraries/Components/View/View';
+    import { Text } from 'react-native';
+    console.warn(\\"Deep imports from the 'react-native' package are deprecated ('react-native/Libraries/Components/View/View'). Source: path/to/project/foo.js 2:4\\");"
+  `);
+});
+
+test('deep cjs import', () => {
+  const code = `
+    const View = require('react-native/Libraries/Components/View/View');
+    const {Text} = require('react-native');
+  `;
+
+  expect(transform(code, [rnDeepImportsWarningPlugin])).toMatchInlineSnapshot(`
+    "const View = require('react-native/Libraries/Components/View/View');
+    const {
+      Text
+    } = require('react-native');
+    console.warn(\\"Deep imports from the 'react-native' package are deprecated ('react-native/Libraries/Components/View/View'). Source: path/to/project/foo.js 2:17\\");"
+  `);
+});
+
+test('multiple deep imports', () => {
+  const code = `
+    import View from 'react-native/Libraries/Components/View/View';
+    import Text from 'react-native/Libraries/Text/Text';
+    import {Image} from 'react-native';
+  `;
+
+  expect(transform(code, [rnDeepImportsWarningPlugin])).toMatchInlineSnapshot(`
+    "import View from 'react-native/Libraries/Components/View/View';
+    import Text from 'react-native/Libraries/Text/Text';
+    import { Image } from 'react-native';
+    console.warn(\\"Deep imports from the 'react-native' package are deprecated ('react-native/Libraries/Components/View/View'). Source: path/to/project/foo.js 2:4\\");
+    console.warn(\\"Deep imports from the 'react-native' package are deprecated ('react-native/Libraries/Text/Text'). Source: path/to/project/foo.js 3:4\\");"
+  `);
+});
+
+test('deep reexport', () => {
+  const code = `
+    export { PressabilityDebugView } from 'react-native/Libraries/Pressability/PressabilityDebug';
+  `;
+
+  expect(transform(code, [rnDeepImportsWarningPlugin])).toMatchInlineSnapshot(`
+    "export { PressabilityDebugView } from 'react-native/Libraries/Pressability/PressabilityDebug';
+    console.warn(\\"Deep imports from the 'react-native' package are deprecated ('react-native/Libraries/Pressability/PressabilityDebug'). Source: path/to/project/foo.js 2:4\\");"
+  `);
+});
+
+test('import from other package', () => {
+  const code = `
+    import {foo} from 'react-native-foo';
+  `;
+
+  expect(transform(code, [rnDeepImportsWarningPlugin])).toMatchInlineSnapshot(
+    `"import { foo } from 'react-native-foo';"`,
+  );
+});
