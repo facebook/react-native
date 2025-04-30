0abd5d63e1c0c4708f81bd698e6d011fa75f01e5
Nicola Corti <ncor@meta.com>
2025-04-29 21:50:25 -0700

    Back out "Migrate ReactActivity" (#50998)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/50998
    
    I'm reverting this as this change is too disruptive for the OSS ecosystem.
    It will break ALL the apps written in Kotlin and it's coming too close to the branch cut which is in less than one week.
    
    We need to re-do this migration in a non breaking manner after the branch cut as this is highly disruptive for little benefit at this point
    
    Changelog
    [Android][Changed] - Back out "[RN][Kotlin] Migrate ReactActivity"
    
    Original commit changeset: 936263100ca9
    
    Original Phabricator Diff: D73507044
    
    Reviewed By: mdvacca
    
    Differential Revision: D73864144
    
    fbshipit-source-id: 264921b1f1cd38301e66364de4b619807272bd27

--- packages/rn-tester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterActivity.kt
+++ packages/rn-tester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterActivity.kt
@@ -22,7 +22,7 @@ import java.io.FileDescriptor
 import java.io.PrintWriter
 
 internal class RNTesterActivity : ReactActivity() {
-  class RNTesterActivityDelegate(val activity: ReactActivity, mainComponentName: String?) :
+  class RNTesterActivityDelegate(val activity: ReactActivity, mainComponentName: String) :
       DefaultReactActivityDelegate(activity, mainComponentName, fabricEnabled) {
     private val PARAM_ROUTE = "route"
     private lateinit var initialProps: Bundle
@@ -43,9 +43,6 @@ internal class RNTesterActivity : ReactActivity() {
         if (this::initialProps.isInitialized) initialProps else Bundle()
   }
 
-  override val mainComponentName: String?
-    get() = "RNTesterApp"
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
@@ -55,7 +52,7 @@ internal class RNTesterActivity : ReactActivity() {
     this.window?.setBackgroundDrawable(ColorDrawable(Color.BLACK))
     // register insets listener to update margins on the ReactRootView to avoid overlap w/ system
     // bars
-    reactDelegate?.getReactRootView()?.let { rootView ->
+    getReactDelegate()?.getReactRootView()?.let { rootView ->
       val insetsType: Int =
           WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
 
@@ -73,6 +70,8 @@ internal class RNTesterActivity : ReactActivity() {
   }
 
   override fun createReactActivityDelegate() = RNTesterActivityDelegate(this, mainComponentName)
+
+  override fun getMainComponentName() = "RNTesterApp"
 
   override fun dump(
       prefix: String,
