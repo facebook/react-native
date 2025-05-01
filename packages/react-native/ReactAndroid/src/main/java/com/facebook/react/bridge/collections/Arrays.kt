c32c75bb31ac1f23462b0da26ac8d1a24e91f001
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-05-01 13:38:05 +0900

    [support-collections-dsl] Make Arrays, Maps collection builders

---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/collections/Arrays.kt
@@ -0,0 +1,47 @@
+package com.facebook.react.bridge.collections
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.WritableArray
+
+public inline fun buildReadableArray(builder: ReadableArrayBuilder.() -> Unit): ReadableArray {
+  val array: WritableArray = Arguments.createArray()
+  ReadableArrayBuilder(array).apply(builder)
+  return array
+}
+
+public class ReadableArrayBuilder(
+  private val array: WritableArray
+) {
+  public fun add(value: String?) {
+    array.pushString(value)
+  }
+
+  public fun add(value: Int) {
+    array.pushInt(value)
+  }
+
+  public fun add(value: Boolean) {
+    array.pushBoolean(value)
+  }
+
+  public fun add(value: Double) {
+    array.pushDouble(value)
+  }
+
+  public fun add(value: Long) {
+    array.pushDouble(value.toDouble())
+  }
+
+  public fun addNull() {
+    array.pushNull()
+  }
+
+  public fun add(builder: ReadableMapBuilder.() -> Unit) {
+    array.pushMap(buildReadableMap(builder))
+  }
+
+  public fun add(builder: ReadableArrayBuilder.() -> Unit) {
+    array.pushArray(buildReadableArray(builder))
+  }
+}
---
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/collections/Maps.kt
@@ -0,0 +1,48 @@
+package com.facebook.react.bridge.collections
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.WritableMap
+
+public inline fun buildReadableMap(builder: ReadableMapBuilder.() -> Unit): ReadableMap {
+  val map: WritableMap = Arguments.createMap()
+  ReadableMapBuilder(map).apply(builder)
+  return map
+}
+
+
+public class ReadableMapBuilder(
+  private val map: WritableMap
+) {
+  public fun put(key: String, value: String?) {
+    map.putString(key, value)
+  }
+
+  public fun put(key: String, value: Int) {
+    map.putInt(key, value)
+  }
+
+  public fun put(key: String, value: Boolean) {
+    map.putBoolean(key, value)
+  }
+
+  public fun put(key: String, value: Double) {
+    map.putDouble(key, value)
+  }
+
+  public fun put(key: String, value: Long) {
+    map.putDouble(key, value.toDouble())
+  }
+
+  public fun putNull(key: String) {
+    map.putNull(key)
+  }
+
+  public fun put(key: String, builder: ReadableMapBuilder.() -> Unit) {
+    map.putMap(key, buildReadableMap(builder))
+  }
+
+  public fun put(key: String, builder: ReadableArrayBuilder.() -> Unit) {
+    map.putArray(key, buildReadableArray(builder))
+  }
+}
