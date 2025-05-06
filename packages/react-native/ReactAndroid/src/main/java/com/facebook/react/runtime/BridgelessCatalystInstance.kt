a02052d37f44498db3bf75fb74ac85cba914c539
David Vacca <dvacca@meta.com>
2025-05-05 23:53:10 -0700

    Mark BridgelessCatalystInstance as LegacyArchitecture and remove DoNotStrip (#51111)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51111
    
    BridgelessCatalystInstance is unused in bridgeless, any usage will lead to an exception / crash.
    This diffs marks BridgelessCatalystInstance as LegacyArchitecture and removes DoNotStrip annotation to enable further optimizations
    
    changelog: [internal] internal
    
    Reviewed By: alanleedev
    
    Differential Revision: D74200097
    
    fbshipit-source-id: f99b6fa74c30bdf0e1cd6758e223601b3ce5826c

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/runtime/BridgelessCatalystInstance.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/runtime/BridgelessCatalystInstance.kt
@@ -12,7 +12,6 @@
 package com.facebook.react.runtime
 
 import android.content.res.AssetManager
-import com.facebook.proguard.annotations.DoNotStrip
 import com.facebook.react.bridge.CatalystInstance
 import com.facebook.react.bridge.JavaScriptContextHolder
 import com.facebook.react.bridge.JavaScriptModule
@@ -26,14 +25,16 @@ import com.facebook.react.bridge.RuntimeScheduler
 import com.facebook.react.bridge.UIManager
 import com.facebook.react.bridge.queue.ReactQueueConfiguration
 import com.facebook.react.common.annotations.VisibleForTesting
+import com.facebook.react.common.annotations.internal.LegacyArchitecture
+import com.facebook.react.common.annotations.internal.LegacyArchitectureLogger
 import com.facebook.react.internal.turbomodule.core.interfaces.TurboModuleRegistry
 import com.facebook.react.turbomodule.core.interfaces.CallInvokerHolder
 import com.facebook.react.turbomodule.core.interfaces.NativeMethodCallInvokerHolder
 
-@DoNotStrip
 @Deprecated(
     message =
         "This class is deprecated, please to migrate to new architecture using [com.facebook.react.defaults.DefaultReactHost] instead.")
+@LegacyArchitecture
 internal class BridgelessCatalystInstance(private val reactHost: ReactHostImpl) : CatalystInstance {
 
   override fun handleMemoryPressure(level: Int) {
@@ -68,7 +69,6 @@ internal class BridgelessCatalystInstance(private val reactHost: ReactHostImpl) 
     throw UnsupportedOperationException("Unimplemented method 'hasRunJSBundle'")
   }
 
-  @DoNotStrip
   override fun invokeCallback(callbackID: Int, arguments: NativeArrayInterface) {
     throw UnsupportedOperationException("Unimplemented method 'invokeCallback'")
   }
@@ -171,5 +171,11 @@ internal class BridgelessCatalystInstance(private val reactHost: ReactHostImpl) 
           "This class is deprecated, please to migrate to new architecture using [com.facebook.react.defaults.DefaultReactHost] instead.")
   override fun getFabricUIManager(): UIManager {
     throw UnsupportedOperationException("Unimplemented method 'getFabricUIManager'")
+  }
+
+  private companion object {
+    init {
+      LegacyArchitectureLogger.assertLegacyArchitecture("BridgelessCatalystInstance")
+    }
   }
 }
