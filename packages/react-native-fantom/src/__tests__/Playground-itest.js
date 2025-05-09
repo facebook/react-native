05e92b77f32de1b9a0ee8ad996c34766910db3a4
Rubén Norte <rubennorte@meta.com>
2025-05-09 05:48:53 -0700

    Add Playground test (#51220)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51220
    
    Changelog: [internal]
    
    Just a playground file to do quick tests in the Fantom environment.
    
    For example, I just used it to know if Hermes supports `AggregateError`, which does:
    
    ```
      it('...', () => {
        console.log('AggregateError', global.AggregateError);
      });
    ```
    
    {F1977824992}
    
    Differential Revision: D74474870

---
+++ packages/react-native-fantom/src/__tests__/Playground-itest.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ * @oncall react_native
+ */
+
+import '@react-native/fantom/src/setUpDefaultReactNativeEnvironment';
+
+// eslint-disable-next-line no-unused-vars
+import * as Fantom from '@react-native/fantom';
+
+describe('Playground', () => {
+  it('...', () => {
+    // Add your tests here, but do NOT commit them.
+  });
+});
