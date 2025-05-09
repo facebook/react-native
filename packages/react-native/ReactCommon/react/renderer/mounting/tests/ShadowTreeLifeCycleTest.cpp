7e7a35a2367a63c4f12ebaa9d978d57e09ad23d4
Pieter De Baets <pieterdb@meta.com>
2025-05-09 08:25:09 -0700

    Simplify StubViewTree creation (#51221)
    Summary:
    
    In the case of an empty root node, we don't need `buildStubViewTreeWithoutUsingDifferentiator` to create the initial `StubViewTree`
    
    When testing with Fantom, this caused an additional unnecessary clone of RootShadowNode.
    
    Changelog: [Internal]
    
    Reviewed By: lenaic
    
    Differential Revision: D74472766

--- packages/react-native/ReactCommon/react/renderer/mounting/tests/ShadowTreeLifeCycleTest.cpp
+++ packages/react-native/ReactCommon/react/renderer/mounting/tests/ShadowTreeLifeCycleTest.cpp
@@ -80,7 +80,7 @@ static void testShadowNodeTreeLifeCycle(
                 ShadowNode::ListOfShared{singleRootChildNode})}));
 
     // Building an initial view hierarchy.
-    auto viewTree = buildStubViewTreeWithoutUsingDifferentiator(*emptyRootNode);
+    auto viewTree = StubViewTree(ShadowView(*emptyRootNode));
     viewTree.mutate(
         calculateShadowViewMutations(*emptyRootNode, *currentRootNode));
 
