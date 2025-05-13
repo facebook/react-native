154b935ddc03494139f4c4d00a3bcd8ea9cab28b
Nick Gerleman <ngerlem@meta.com>
2025-05-13 16:54:46 -0700

    Send prepared layouts in Android ParagraphState updates
    Differential Revision: D74356128

--- packages/react-native/ReactCommon/react/renderer/components/text/platform/android/react/renderer/components/text/ParagraphState.h
+++ packages/react-native/ReactCommon/react/renderer/components/text/platform/android/react/renderer/components/text/ParagraphState.h
@@ -11,6 +11,7 @@
 #include <react/renderer/attributedstring/AttributedString.h>
 #include <react/renderer/attributedstring/ParagraphAttributes.h>
 #include <react/renderer/textlayoutmanager/TextLayoutManager.h>
+#include <react/renderer/textlayoutmanager/TextLayoutManagerExtended.h>
 
 #include <fbjni/fbjni.h>
 #include <folly/dynamic.h>
@@ -51,13 +52,31 @@ class ParagraphState final {
    */
   std::weak_ptr<const TextLayoutManager> layoutManager;
 
+  /**
+   * A fully prepared representation of a text layout to mount
+   */
+  MeasuredPreparedLayout measuredLayout;
+
   ParagraphState(
       AttributedString attributedString,
       ParagraphAttributes paragraphAttributes,
-      const std::weak_ptr<const TextLayoutManager>& layoutManager)
+      std::weak_ptr<const TextLayoutManager> layoutManager,
+      MeasuredPreparedLayout measuredLayout)
       : attributedString(std::move(attributedString)),
         paragraphAttributes(std::move(paragraphAttributes)),
-        layoutManager(layoutManager) {}
+        layoutManager(std::move(layoutManager)),
+        measuredLayout(std::move(measuredLayout)) {}
+
+  ParagraphState(
+      AttributedString attributedString,
+      ParagraphAttributes paragraphAttributes,
+      std::weak_ptr<const TextLayoutManager> layoutManager)
+      : ParagraphState(
+            std::move(attributedString),
+            std::move(paragraphAttributes),
+            std::move(layoutManager),
+            {}) {}
+
   ParagraphState() = default;
   ParagraphState(
       const ParagraphState& /*previousState*/,
@@ -67,6 +86,7 @@ class ParagraphState final {
 
   folly::dynamic getDynamic() const;
   MapBuffer getMapBuffer() const;
+  jni::local_ref<jobject> getJNIReference() const;
 };
 
 } // namespace facebook::react
