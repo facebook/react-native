ee9bd851acfc38150f434d676602865ba8cec591
Jakub Piasecki <piaseckijakub@meta.com>
2025-05-14 08:01:15 -0700

    Add a custom Jest resolver to opt out from handling "exports" in tests (#51307)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51307
    
    D72228547 added the `exports` field to the main `react-native` package, in which all imports from `./src/*` are explicitly disallowed. This was expected to be a breaking change and to limit the surface area of it, this diff will allow using all imports in jest tests.
    
    Changelog: [General][Added] Added a custom Jest resolver to opt out from handling "exports" in tests
    
    Reviewed By: huntie
    
    Differential Revision: D74708701
    
    fbshipit-source-id: 9a2714f4e6f78ffbad9e56b5bb92657c9ea908ef

---
+++ packages/react-native/jest/resolver.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+'use strict';
+
+module.exports = (path, options) => {
+  const originalPackageFilter = options.packageFilter;
+
+  return options.defaultResolver(path, {
+    ...options,
+    packageFilter: pkg => {
+      const filteredPkg = originalPackageFilter
+        ? originalPackageFilter(pkg)
+        : pkg;
+
+      // Temporarily allow any react-native subpaths to be resolved and
+      // mocked by Jest (backwards compatibility around RFC0894)
+      if (filteredPkg.name === 'react-native') {
+        delete filteredPkg.exports;
+      }
+
+      return filteredPkg;
+    },
+  });
+};
