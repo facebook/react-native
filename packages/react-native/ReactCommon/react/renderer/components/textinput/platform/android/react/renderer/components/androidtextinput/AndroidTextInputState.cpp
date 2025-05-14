88de631d055f3ee4fb5db32b6feb235fded08a50
Nick Gerleman <ngerlem@meta.com>
2025-05-14 16:10:08 -0700

    RN_SERIALIZABLE_STATE (#51185)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51185
    
    Was running into issues with split Android ParagraphState, because a lot of code is tied to `#ifdef ANDROID`, which also builds for react-native-cxx.
    
    This does that spliitting, and also introduces `RN_SERIALIZABLE_STATE` to replace previous ANDROID checks related to state serialization, so we can cofine that to just the Android platform. These changes are dependent on each other.
    
    Changelog: [Internal]
    
    Reviewed By: joevilches
    
    Differential Revision: D74374376
    
    fbshipit-source-id: ffb246ea2e16773f85de12209f2d88a95f8bb792

--- packages/react-native/ReactCommon/react/renderer/components/textinput/platform/android/react/renderer/components/androidtextinput/AndroidTextInputState.cpp
+++ packages/react-native/ReactCommon/react/renderer/components/textinput/platform/android/react/renderer/components/androidtextinput/AndroidTextInputState.cpp
@@ -5,46 +5,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include "TextInputState.h"
+#include "AndroidTextInputState.h"
 
-#ifdef ANDROID
 #include <react/renderer/attributedstring/conversions.h>
 #include <react/renderer/components/text/ParagraphState.h>
-#endif
 
 namespace facebook::react {
 
-TextInputState::TextInputState(
-    AttributedStringBox attributedStringBox,
-    AttributedString reactTreeAttributedString,
-    ParagraphAttributes paragraphAttributes,
-    int64_t mostRecentEventCount)
-    : attributedStringBox(std::move(attributedStringBox)),
-      reactTreeAttributedString(std::move(reactTreeAttributedString)),
-      paragraphAttributes(std::move(paragraphAttributes)),
-      mostRecentEventCount(mostRecentEventCount) {}
-
-#ifdef ANDROID
-TextInputState::TextInputState(
-    const TextInputState& previousState,
-    const folly::dynamic& data)
-    : attributedStringBox(previousState.attributedStringBox),
-      reactTreeAttributedString(previousState.reactTreeAttributedString),
-      paragraphAttributes(previousState.paragraphAttributes),
-      mostRecentEventCount(data.getDefault(
-                                   "mostRecentEventCount",
-                                   previousState.mostRecentEventCount)
-                               .getInt()),
-      cachedAttributedStringId(data.getDefault(
-                                       "opaqueCacheId",
-                                       previousState.cachedAttributedStringId)
-                                   .getInt()){};
-
-folly::dynamic TextInputState::getDynamic() const {
+folly::dynamic AndroidTextInputState::getDynamic() const {
   LOG(FATAL) << "TextInputState state should only be read using MapBuffer";
 }
 
-MapBuffer TextInputState::getMapBuffer() const {
+MapBuffer AndroidTextInputState::getMapBuffer() const {
   auto builder = MapBufferBuilder();
   // If we have a `cachedAttributedStringId` we know that we're (1) not trying
   // to set a new string, so we don't need to pass it along; (2) setState was
@@ -66,6 +38,5 @@ MapBuffer TextInputState::getMapBuffer() const {
   }
   return builder.build();
 }
-#endif
 
 } // namespace facebook::react
