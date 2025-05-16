e7d23f80239398b219fd019b57b4cabe895f3538
Tim Yung <yungsters@meta.com>
2025-05-16 13:29:56 -0700

    RN: Add Missing `@format` Annotations (#51415)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51415
    
    Adds the `format` annotation to all files that were missing them.
    
    Also, adds `noformat` to generated files, and removed it from files that no longer need them.
    
    Changelog:
    [Internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D74901034

--- packages/react-native-popup-menu-android/react-native.config.js
+++ packages/react-native-popup-menu-android/react-native.config.js
@@ -3,13 +3,15 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @format
  */
 
 module.exports = {
   dependency: {
     platforms: {
       android: {
-        'cmakeListsPath': '../android/src/main/jni/CMakeLists.txt',
+        cmakeListsPath: '../android/src/main/jni/CMakeLists.txt',
       },
     },
   },
