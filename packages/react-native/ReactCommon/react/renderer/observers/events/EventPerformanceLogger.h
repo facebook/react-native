f2f46213b8fb43111c34dcc274fd7bc369628bc8
Ruslan Lesiutin <hoxy@meta.com>
2025-05-16 10:18:51 -0700

    Make processingStartTime and processingEndTime optional (#51390)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51390
    
    # Changelong: [Internal]
    
    Ideally, these should not be optional, but these values are set at runtime.
    
    Assertions added to validate that these values are set before reporting to `PerformanceEntryReporter`.
    
    Reviewed By: rshest
    
    Differential Revision: D74811439

--- packages/react-native/ReactCommon/react/renderer/observers/events/EventPerformanceLogger.h
+++ packages/react-native/ReactCommon/react/renderer/observers/events/EventPerformanceLogger.h
@@ -11,6 +11,7 @@
 #include <react/renderer/core/EventLogger.h>
 #include <react/renderer/runtimescheduler/RuntimeSchedulerEventTimingDelegate.h>
 #include <react/renderer/uimanager/UIManagerMountHook.h>
+
 #include <memory>
 #include <mutex>
 #include <optional>
@@ -52,9 +53,9 @@ class EventPerformanceLogger : public EventLogger,
   struct EventEntry {
     std::string_view name;
     SharedEventTarget target{nullptr};
-    DOMHighResTimeStamp startTime{0.0};
-    DOMHighResTimeStamp processingStartTime{0.0};
-    DOMHighResTimeStamp processingEndTime{0.0};
+    DOMHighResTimeStamp startTime;
+    std::optional<DOMHighResTimeStamp> processingStartTime;
+    std::optional<DOMHighResTimeStamp> processingEndTime;
 
     bool isWaitingForMount{false};
 
@@ -63,7 +64,7 @@ class EventPerformanceLogger : public EventLogger,
     PerformanceEntryInteractionId interactionId{0};
 
     bool isWaitingForDispatch() {
-      return processingEndTime == 0.0;
+      return !processingEndTime.has_value();
     }
   };
 
