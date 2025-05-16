7ed442f0e8f9548ac060ae0b7cde99565ffebe02
Ruslan Lesiutin <hoxy@meta.com>
2025-05-16 04:33:35 -0700

    Define Bridging protocol for HighResTimeStamp and HighResTimeDuration (#51253)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51253
    
    # Changelog: [Internal]
    
    Defines bridging template for `HighResTimeStamp` and `HighResTimeDuration`.
    
    When these values are passed to JavaScript over bridge, they will be converted to [`DOMHighResTimeStamp`](https://developer.mozilla.org/en-US/docs/Web/API/DOMHighResTimeStamp).
    
    Also the other way around, when we list `HighResTimeStamp` or `HighResTimeDuration` as a type of TurboModule spec, it will expect number value from JavaScript.
    
    Differential Revision: D74506516

---
+++ packages/react-native/ReactCommon/react/bridging/HighResTimeStamp.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <react/bridging/Base.h>
+#include <react/timing/primitives.h>
+
+namespace facebook::react {
+
+template <>
+struct Bridging<HighResTimeStamp> {
+  static HighResTimeStamp fromJs(
+      jsi::Runtime& /*rt*/,
+      const jsi::Value& jsiValue) {
+    return HighResTimeStamp::fromDOMHighResTimeStamp(jsiValue.asNumber());
+  }
+
+  static double toJs(jsi::Runtime& /*rt*/, const HighResTimeStamp& value) {
+    return value.toDOMHighResTimeStamp();
+  }
+};
+
+template <>
+struct Bridging<HighResDuration> {
+  static HighResDuration fromJs(
+      jsi::Runtime& /*rt*/,
+      const jsi::Value& jsiValue) {
+    return HighResDuration::fromDOMHighResTimeStamp(jsiValue.asNumber());
+  }
+
+  static double toJs(jsi::Runtime& /*rt*/, const HighResDuration& value) {
+    return value.toDOMHighResTimeStamp();
+  }
+};
+
+} // namespace facebook::react
