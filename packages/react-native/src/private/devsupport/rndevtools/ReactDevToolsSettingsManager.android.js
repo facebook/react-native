cebcb982c6f5ce3b792aab6e48b26cbc336ea28b
Tim Yung <yungsters@meta.com>
2025-05-17 22:48:10 -0700

    RN: Migrate to ESM - ReactDevToolsSettingsManager
    Summary:
    Migrates files to use ESM to mitigate the existing lint warning.
    
    Changelog:
    [Internal]
    
    Differential Revision: D74949984

--- packages/react-native/src/private/devsupport/rndevtools/ReactDevToolsSettingsManager.android.js
+++ packages/react-native/src/private/devsupport/rndevtools/ReactDevToolsSettingsManager.android.js
@@ -4,17 +4,16 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict-local
+ * @flow strict
  * @format
  */
 
 import NativeReactDevToolsSettingsManager from './specs/NativeReactDevToolsSettingsManager';
 
-module.exports = {
-  setGlobalHookSettings(settings: string) {
-    NativeReactDevToolsSettingsManager?.setGlobalHookSettings(settings);
-  },
-  getGlobalHookSettings(): ?string {
-    return NativeReactDevToolsSettingsManager?.getGlobalHookSettings();
-  },
-};
+export function setGlobalHookSettings(settings: string) {
+  NativeReactDevToolsSettingsManager?.setGlobalHookSettings(settings);
+}
+
+export function getGlobalHookSettings(): ?string {
+  return NativeReactDevToolsSettingsManager?.getGlobalHookSettings();
+}
