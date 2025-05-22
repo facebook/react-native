c937439e875ab5072aa2605686d2f103b8f8ea0c
Alex Hunt <huntie@meta.com>
2025-05-22 09:44:15 -0700

    Add a basic Jest test to helloworld (#51534)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51534
    
    Aims to give us CI coverage of React Native's Jest preset, preventing future bugs like https://github.com/facebook/react-native/pull/51525.
    
    Changes:
    - Add a basic "it renders" Jest test to helloworld
    - Add "Run Helloworld tests" step to `test-ios-helloworld` job in CI
    - Also convert helloworld's `App.tsx` to TypeScript, as easiest way to unblock Jest JSX behaviour.
    
    Changelog: [Internal]
    
    Reviewed By: cortinico
    
    Differential Revision: D75218901
    
    fbshipit-source-id: 601155c59c4483696971df4c29d51549d97f49f2

--- packages/helloworld/App.tsx
+++ packages/helloworld/App.tsx
@@ -4,7 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict-local
  * @format
  */
 
@@ -19,7 +18,7 @@ import {
   useColorScheme,
 } from 'react-native';
 
-function App(): React.Node {
+function App(): React.ReactNode {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
