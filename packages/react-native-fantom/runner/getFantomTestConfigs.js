77a7c9ef2d989a9ec3b84a49606ba17cc345010d
Rubén Norte <rubennorte@fb.com>
2025-05-23 03:34:46 -0700

    Add support multi-config test runs
    Differential Revision: D75231299

--- packages/react-native-fantom/runner/getFantomTestConfigs.js
+++ packages/react-native-fantom/runner/getFantomTestConfigs.js
@@ -12,7 +12,6 @@ import type {FeatureFlagValue} from '../../../packages/react-native/scripts/feat
 
 import ReactNativeFeatureFlags from '../../../packages/react-native/scripts/featureflags/ReactNativeFeatureFlags.config';
 import {HermesVariant} from './utils';
-import fs from 'fs';
 // $FlowExpectedError[untyped-import]
 import {extract, parse} from 'jest-docblock';
 
@@ -51,6 +50,12 @@ export type FantomTestConfig = {
   flags: FantomTestConfigFeatureFlags,
 };
 
+export type PartialFantomTestConfig = {
+  mode?: FantomTestConfigMode,
+  hermesVariant?: HermesVariant,
+  flags?: Partial<FantomTestConfigFeatureFlags>,
+};
+
 export const FantomTestConfigHermesVariant = HermesVariant;
 
 export const DEFAULT_MODE: FantomTestConfigMode =
@@ -66,7 +71,7 @@ export const DEFAULT_FEATURE_FLAGS: FantomTestConfigFeatureFlags = {
   reactInternal: {},
 };
 
-const FANTOM_FLAG_FORMAT = /^(\w+):(\w+)$/;
+const FANTOM_FLAG_FORMAT = /^(\w+):((?:\w+)|\*)$/;
 
 const FANTOM_BENCHMARK_FILENAME_RE = /[Bb]enchmark-itest\./g;
 const FANTOM_BENCHMARK_SUITE_RE =
@@ -75,8 +80,10 @@ const FANTOM_BENCHMARK_SUITE_RE =
 const FANTOM_BENCHMARK_DEFAULT_MODE: FantomTestConfigMode =
   FantomTestConfigMode.Optimized;
 
+const MAX_FANTOM_CONFIGURATION_VARIATIONS = 12;
+
 /**
- * Extracts the Fantom configuration from the test file, specified as part of
+ * Extracts the Fantom configurations from the test file, specified as part of
  * the docblock comment. E.g.:
  *
  * ```
@@ -92,19 +99,22 @@ const FANTOM_BENCHMARK_DEFAULT_MODE: FantomTestConfigMode =
  *
  * The supported options are:
  * - `fantom_mode`: specifies the level of optimization to compile the test
- *  with. Valid values are `dev` and `opt`.
+ *  with. Valid values are `dev`, `dev-bytecode` and `opt`.
  * - `fantom_hermes_variant`: specifies the Hermes variant to use to run the
  *  test. Valid values are `hermes`, `static_hermes` and
  *  `static_hermes_experimental`.
  * - `fantom_flags`: specifies the configuration for common and JS-only feature
  *  flags. They can be specified in the same pragma or in different ones, and
  *  the format is `<flag_name>:<value>`.
+ *
+ * If a wildcard (`*`) is used for any given value, we return a list of
+ * configurations with all the combinations of values for those options (with
+ * a limit of 12 configurations).
  */
-export default function getFantomTestConfig(
+export default function getFantomTestConfigs(
   testPath: string,
-): FantomTestConfig {
-  const testContents = fs.readFileSync(testPath, 'utf8');
-
+  testContents: string,
+): Array<FantomTestConfig> {
   const docblock = extract(testContents);
   const pragmas = parse(docblock) as DocblockPragmas;
 
@@ -126,6 +136,8 @@ export default function getFantomTestConfig(
 
   const maybeMode = pragmas.fantom_mode;
 
+  const configVariations: Array<Array<PartialFantomTestConfig>> = [];
+
   if (maybeMode != null) {
     if (Array.isArray(maybeMode)) {
       throw new Error('Expected a single value for @fantom_mode');
@@ -142,6 +154,13 @@ export default function getFantomTestConfig(
         break;
       case 'opt':
         config.mode = FantomTestConfigMode.Optimized;
+        break;
+      case '*':
+        configVariations.push([
+          {mode: FantomTestConfigMode.DevelopmentWithSource},
+          {mode: FantomTestConfigMode.DevelopmentWithBytecode},
+          {mode: FantomTestConfigMode.Optimized},
+        ]);
         break;
       default:
         throw new Error(`Invalid Fantom mode: ${mode}`);
@@ -174,6 +193,13 @@ export default function getFantomTestConfig(
       case 'static_hermes_experimental':
         config.hermesVariant = HermesVariant.StaticHermesExperimental;
         break;
+      case '*':
+        configVariations.push([
+          {hermesVariant: HermesVariant.Hermes},
+          {hermesVariant: HermesVariant.StaticHermes},
+          {hermesVariant: HermesVariant.StaticHermesExperimental},
+        ]);
+        break;
       default:
         throw new Error(`Invalid Fantom Hermes mode: ${hermesVariant}`);
     }
@@ -200,12 +226,42 @@ export default function getFantomTestConfig(
 
       if (ReactNativeFeatureFlags.common[name]) {
         const flagConfig = ReactNativeFeatureFlags.common[name];
-        const value = parseFeatureFlagValue(flagConfig.defaultValue, rawValue);
-        config.flags.common[name] = value;
+        if (rawValue === '*') {
+          if (typeof flagConfig.defaultValue !== 'boolean') {
+            throw new Error(
+              `Invalid format for Fantom feature flag: ${rawFlagConfig}. Wildcards are not supported for non-boolean feature flags.`,
+            );
+          }
+          configVariations.push([
+            {flags: {common: {[name]: false}}},
+            {flags: {common: {[name]: true}}},
+          ]);
+        } else {
+          const value = parseFeatureFlagValue(
+            flagConfig.defaultValue,
+            rawValue,
+          );
+          config.flags.common[name] = value;
+        }
       } else if (ReactNativeFeatureFlags.jsOnly[name]) {
         const flagConfig = ReactNativeFeatureFlags.jsOnly[name];
-        const value = parseFeatureFlagValue(flagConfig.defaultValue, rawValue);
-        config.flags.jsOnly[name] = value;
+        if (rawValue === '*') {
+          if (typeof flagConfig.defaultValue !== 'boolean') {
+            throw new Error(
+              `Invalid format for Fantom feature flag: ${rawFlagConfig}. Wildcards are not supported for non-boolean feature flags.`,
+            );
+          }
+          configVariations.push([
+            {flags: {jsOnly: {[name]: false}}},
+            {flags: {jsOnly: {[name]: true}}},
+          ]);
+        } else {
+          const value = parseFeatureFlagValue(
+            flagConfig.defaultValue,
+            rawValue,
+          );
+          config.flags.jsOnly[name] = value;
+        }
       } else {
         const validKeys = Object.keys(ReactNativeFeatureFlags.common)
           .concat(Object.keys(ReactNativeFeatureFlags.jsOnly))
@@ -236,12 +292,78 @@ export default function getFantomTestConfig(
       }
 
       const [, name, rawValue] = matches;
-      const value = parseFeatureFlagValue(false, rawValue);
-      config.flags.reactInternal[name] = value;
+      if (rawValue === '*') {
+        configVariations.push([
+          {flags: {reactInternal: {[name]: false}}},
+          {flags: {reactInternal: {[name]: true}}},
+        ]);
+      } else {
+        const value = parseFeatureFlagValue(false, rawValue);
+        config.flags.reactInternal[name] = value;
+      }
     }
   }
 
-  return config;
+  const combinations = configVariations.reduce(
+    (total, current) => total * current.length,
+    1,
+  );
+  if (combinations > MAX_FANTOM_CONFIGURATION_VARIATIONS) {
+    throw new Error(
+      `Cannot define a test with more than ${MAX_FANTOM_CONFIGURATION_VARIATIONS} configuration variations. Please reduce the number of wildcard values (*) used in your test configuration.`,
+    );
+  }
+
+  return getConfigurationVariations(config, configVariations);
+}
+
+function getConfigurationVariations(
+  config: FantomTestConfig,
+  variations: $ReadOnlyArray<$ReadOnlyArray<PartialFantomTestConfig>>,
+): Array<FantomTestConfig> {
+  if (variations.length === 0) {
+    return [config];
+  }
+
+  const results: Array<FantomTestConfig> = [];
+  const [currentConfigVariations, ...remainingVariations] = variations;
+
+  for (const currentConfigVariation of currentConfigVariations) {
+    const currentConfigWithVariation = {
+      mode: currentConfigVariation.mode ?? config.mode,
+      hermesVariant:
+        currentConfigVariation.hermesVariant ?? config.hermesVariant,
+      flags: {
+        common: currentConfigVariation.flags?.common
+          ? {
+              ...config.flags.common,
+              ...currentConfigVariation.flags.common,
+            }
+          : config.flags.common,
+        jsOnly: currentConfigVariation.flags?.jsOnly
+          ? {
+              ...config.flags.jsOnly,
+              ...currentConfigVariation.flags.jsOnly,
+            }
+          : config.flags.jsOnly,
+        reactInternal: currentConfigVariation.flags?.reactInternal
+          ? {
+              ...config.flags.reactInternal,
+              ...currentConfigVariation.flags.reactInternal,
+            }
+          : config.flags.reactInternal,
+      },
+    };
+
+    results.push(
+      ...getConfigurationVariations(
+        currentConfigWithVariation,
+        remainingVariations,
+      ),
+    );
+  }
+
+  return results;
 }
 
 function parseFeatureFlagValue<T: boolean | number | string>(
