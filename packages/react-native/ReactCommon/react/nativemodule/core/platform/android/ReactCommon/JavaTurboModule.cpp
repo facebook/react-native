ad0589eb40a078432a2e577f95f9d6297271850f
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2025-05-30 15:53:33 +0300

    Fix emitting event from turbo module crashes on 32bit android

--- packages/react-native/ReactCommon/react/nativemodule/core/platform/android/ReactCommon/JavaTurboModule.cpp
+++ packages/react-native/ReactCommon/react/nativemodule/core/platform/android/ReactCommon/JavaTurboModule.cpp
@@ -982,15 +982,16 @@ void JavaTurboModule::configureEventEmitterCallback() {
     FACEBOOK_JNI_THROW_PENDING_EXCEPTION();
   }
 
-  jvalue arg;
-  arg.l =
-      JCxxCallbackImpl::newObjectCxxArgs([&](folly::dynamic args) {
-        auto eventName = args.at(0).asString();
-        auto& eventEmitter = static_cast<AsyncEventEmitter<folly::dynamic>&>(
-            *eventEmitterMap_[eventName].get());
-        eventEmitter.emit(args.size() > 1 ? std::move(args).at(1) : nullptr);
-      }).release();
-  env->CallVoidMethod(instance_.get(), cachedMethodId, arg);
+  auto callback = JCxxCallbackImpl::newObjectCxxArgs([&](folly::dynamic args) {
+    auto eventName = args.at(0).asString();
+    auto& eventEmitter = static_cast<AsyncEventEmitter<folly::dynamic>&>(
+        *eventEmitterMap_[eventName].get());
+    eventEmitter.emit(args.size() > 1 ? std::move(args).at(1) : nullptr);
+  });
+
+  jvalue args[1];
+  args[0].l = callback.release();
+  env->CallVoidMethodA(instance_.get(), cachedMethodId, args);
   FACEBOOK_JNI_THROW_PENDING_EXCEPTION();
 }
 
