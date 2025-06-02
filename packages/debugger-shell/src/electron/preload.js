d753c50088d2c492af678cb8aa68bb3ce3187819
Moti Zilberman <moti@meta.com>
2025-06-02 11:31:53 -0700

    Bring RNDT shell to front when paused on breakpoint
    Summary:
    Changelog: [Internal]
    
    Implements the first RNDT shell-specific feature based on https://github.com/facebook/react-native-devtools-frontend/pull/168 - namely, the ability for RNDT to foreground itself when certain events occur. This is most noticeable when pausing on a breakpoint.
    
    Differential Revision: D75795689

---
+++ packages/debugger-shell/src/electron/preload.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+const {contextBridge, ipcRenderer} = require('electron');
+
+contextBridge.executeInMainWorld({
+  func: ipcDevTools => {
+    let didDecorateInspectorFrontendHostInstance = false;
+    // reactNativeDecorateInspectorFrontendHostInstance was introduced in
+    // https://github.com/facebook/react-native-devtools-frontend/pull/168
+    globalThis.reactNativeDecorateInspectorFrontendHostInstance =
+      InspectorFrontendHostInstance => {
+        didDecorateInspectorFrontendHostInstance = true;
+        InspectorFrontendHostInstance.bringToFront = () => {
+          ipcDevTools.bringToFront();
+        };
+      };
+
+    document.addEventListener('DOMContentLoaded', () => {
+      if (!didDecorateInspectorFrontendHostInstance) {
+        console.error(
+          'reactNativeDecorateInspectorFrontendHostInstance was not called at startup. ' +
+            'This version of the DevTools frontend may not be compatible with @react-native/debugger-shell.',
+        );
+      }
+    });
+  },
+  args: [
+    {
+      bringToFront() {
+        ipcRenderer.send('bringToFront');
+      },
+    },
+  ],
+});
