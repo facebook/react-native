d80bdcf3a991818ea617629ba973f617c5c9b1b6
Krystof Woldrich <krystofwoldrich@gmail.com>
2025-06-02 09:12:07 +0200

    fix lint

--- packages/react-native/types/modules/Devtools.d.ts
+++ packages/react-native/types/modules/Devtools.d.ts
@@ -9,10 +9,10 @@
 
 declare module 'react-native/Libraries/Core/Devtools/parseErrorStack' {
   export type StackFrame = {
-    column: number | null | undefined,
-    file: string | null | undefined,
-    lineNumber: number | null | undefined,
-    methodName: string,
+    column: number | null | undefined;
+    file: string | null | undefined;
+    lineNumber: number | null | undefined;
+    methodName: string;
   };
 
   export default function parseErrorStack(errorStack?: string): StackFrame[];
