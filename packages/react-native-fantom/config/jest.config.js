aa681705d5a4c28c7d3143c0b58a0f26b7d7a99e
Tim Yung <yungsters@fb.com>
2025-06-04 11:08:05 -0700

    RN: Flowify `packages/react-native-fantom`
    Summary:
    Adds `flow` to the remaining files that are lacking it in the `packages/react-native-fantom` directory.
    
    This also adds any necessary type annotations (using comment syntax).
    
    Changelog:
    [Internal]
    
    Differential Revision: D75886119

--- packages/react-native-fantom/config/jest.config.js
+++ packages/react-native-fantom/config/jest.config.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict
  * @format
  */
 
@@ -12,26 +13,32 @@
 const baseConfig = require('../../../jest.config');
 const path = require('path');
 
-const isCI = Boolean(process.env.SANDCASTLE || process.env.GITHUB_ACTIONS);
+const isCI =
+  Boolean(process.env.SANDCASTLE) || Boolean(process.env.GITHUB_ACTIONS);
+
+// In CI, we want to prewarm the caches/builds before running the tests so
+// that time isn't attributed to the first test that runs.
+const globalSetup /*: ?string */ = isCI
+  ? '<rootDir>/packages/react-native-fantom/runner/warmup/index.js'
+  : null;
 
 module.exports = {
-  rootDir: path.resolve(__dirname, '../../..'),
+  rootDir: path.resolve(__dirname, '../../..') /*:: as string */,
   roots: [
     '<rootDir>/packages/react-native',
     '<rootDir>/packages/react-native-fantom',
     '<rootDir>/packages/polyfills',
   ],
-  moduleFileExtensions: [...baseConfig.moduleFileExtensions, 'cpp', 'h'],
+  moduleFileExtensions: [
+    ...baseConfig.moduleFileExtensions,
+    'cpp',
+    'h',
+  ] /*:: as $ReadOnlyArray<string> */,
   // This allows running Meta-internal tests with the `-test.fb.js` suffix.
   testRegex: '/__tests__/.*-itest(\\.fb)?\\.js$',
   testPathIgnorePatterns: baseConfig.testPathIgnorePatterns,
   transformIgnorePatterns: ['.*'],
   testRunner: '<rootDir>/packages/react-native-fantom/runner/index.js',
   watchPathIgnorePatterns: ['<rootDir>/packages/react-native-fantom/build/'],
-
-  // In CI, we want to prewarm the caches/builds before running the tests so
-  // that time isn't attributed to the first test that runs.
-  globalSetup: isCI
-    ? '<rootDir>/packages/react-native-fantom/runner/warmup/index.js'
-    : null,
+  globalSetup,
 };
