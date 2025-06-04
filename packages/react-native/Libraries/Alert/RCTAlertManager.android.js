9ea9ca2281fc10d1a8633517125dd9fb17bfe5b2
Tim Yung <yungsters@meta.com>
2025-06-04 11:16:38 -0700

    RN: Flowify `packages/react-native/Libraries/Alert` (#51792)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51792
    
    Adds `flow` to the remaining files that are lacking it in the `packages/react-native/Libraries/Alert` directory.
    
    Changelog:
    [Internal]
    
    Reviewed By: SamChou19815
    
    Differential Revision: D75921478

--- packages/react-native/Libraries/Alert/RCTAlertManager.android.js
+++ packages/react-native/Libraries/Alert/RCTAlertManager.android.js
@@ -4,24 +4,30 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict-local
  * @format
  */
+
+import type {Args} from './NativeAlertManager';
 
 import NativeDialogManagerAndroid from '../NativeModules/specs/NativeDialogManagerAndroid';
 
 function emptyCallback() {}
 
-export default {
-  alertWithArgs: function (args, callback) {
-    // TODO(5998984): Polyfill it correctly with DialogManagerAndroid
-    if (!NativeDialogManagerAndroid) {
-      return;
-    }
+export function alertWithArgs(
+  args: Args,
+  callback: (id: number, value: string) => void,
+) {
+  // TODO(5998984): Polyfill it correctly with DialogManagerAndroid
+  if (!NativeDialogManagerAndroid) {
+    return;
+  }
 
-    NativeDialogManagerAndroid.showAlert(
-      args,
-      emptyCallback,
-      callback || emptyCallback,
-    );
-  },
-};
+  NativeDialogManagerAndroid.showAlert(
+    // $FlowFixMe[prop-missing] - Mismatched platform interfaces.
+    args,
+    emptyCallback,
+    // $FlowFixMe[incompatible-call] - Mismatched platform interfaces.
+    callback || emptyCallback,
+  );
+}
