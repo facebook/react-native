690841a3de1800c6bb89441422eea49194e4b9bf
Tim Yung <yungsters@meta.com>
2025-06-04 11:15:10 -0700

    RN: Flowify `packages/react-native` Mocks & Tests (#51794)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51794
    
    Adds `flow` to the remaining mocks and tests that are lacking it in the `packages/react-native` directory.
    
    This required adding numerous type annotations and suppressions.
    
    Changelog:
    [Internal]
    
    Reviewed By: javache
    
    Differential Revision: D75930095

--- packages/react-native/Libraries/Blob/__tests__/FileReader-test.js
+++ packages/react-native/Libraries/Blob/__tests__/FileReader-test.js
@@ -4,10 +4,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict-local
  * @format
  */
 
 'use strict';
+
+import type Event from '../../../src/private/webapis/dom/events/Event';
 
 const Blob = require('../Blob').default;
 const FileReader = require('../FileReader').default;
@@ -22,34 +25,38 @@ jest.mock('../../BatchedBridge/NativeModules', () => ({
 
 describe('FileReader', function () {
   it('should read blob as text', async () => {
-    const e = await new Promise((resolve, reject) => {
+    const e = await new Promise<Event>((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = resolve;
       reader.onerror = reject;
       reader.readAsText(new Blob());
     });
-    expect(e.target.result).toBe('');
+    // $FlowFixMe[prop-missing]
+    expect(e.target?.result).toBe('');
   });
 
   it('should read blob as data URL', async () => {
-    const e = await new Promise((resolve, reject) => {
+    const e = await new Promise<Event>((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = resolve;
       reader.onerror = reject;
       reader.readAsDataURL(new Blob());
     });
-    expect(e.target.result).toBe('data:text/plain;base64,NDI=');
+    // $FlowFixMe[prop-missing]
+    expect(e.target?.result).toBe('data:text/plain;base64,NDI=');
   });
 
   it('should read blob as ArrayBuffer', async () => {
-    const e = await new Promise((resolve, reject) => {
+    const e = await new Promise<Event>((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = resolve;
       reader.onerror = reject;
       reader.readAsArrayBuffer(new Blob());
     });
-    const ab = e.target.result;
-    expect(ab.byteLength).toBe(2);
+    // $FlowFixMe[prop-missing]
+    const ab = e.target?.result;
+    expect(ab?.byteLength).toBe(2);
+    // $FlowFixMe[cannot-resolve-name]
     expect(new TextDecoder().decode(ab)).toBe('42');
   });
 });
--- packages/react-native/Libraries/StyleSheet/__tests__/normalizeColor-test.js
+++ packages/react-native/Libraries/StyleSheet/__tests__/normalizeColor-test.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict-local
  * @format
  */
 
@@ -22,8 +23,10 @@ it('forwards calls to @react-native/normalize-colors', () => {
 describe('iOS', () => {
   if (OS === 'ios') {
     const PlatformColor =
+      // $FlowFixMe[missing-platform-support]
       require('../PlatformColorValueTypes.ios').PlatformColor;
     const DynamicColorIOS =
+      // $FlowFixMe[missing-platform-support]
       require('../PlatformColorValueTypesIOS.ios').DynamicColorIOS;
 
     it('should normalize iOS PlatformColor colors', () => {
@@ -84,6 +87,7 @@ describe('iOS', () => {
 describe('Android', () => {
   if (OS === 'android') {
     const PlatformColor =
+      // $FlowFixMe[missing-platform-support]
       require('../PlatformColorValueTypes.android').PlatformColor;
 
     it('should normalize Android PlatformColor colors', () => {
