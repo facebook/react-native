e8e51ac1fb5731b9e1614fd826db4aa189dec211
Tim Yung <yungsters@meta.com>
2025-06-04 19:33:54 -0700

    RN: Flowify Remaining Files (#51822)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51822
    
    Adds `flow` to the remaining files without it, in React Native.
    
    After this, every `*.{flow,js}` file in React Native will have either `flow` or `noflow`.
    
    Changelog:
    [Internal]
    
    Reviewed By: NickGerleman
    
    Differential Revision: D75980238
    
    fbshipit-source-id: 84cd88e4eb0b0b1dc69df247de79a75c2119bf96

--- packages/debugger-shell/src/electron/preload.js
+++ packages/debugger-shell/src/electron/preload.js
@@ -4,9 +4,11 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @flow strict-local
  * @format
  */
 
+// $FlowFixMe[prop-missing]
 const {contextBridge, ipcRenderer} = require('electron');
 
 contextBridge.executeInMainWorld({
@@ -14,13 +16,15 @@ contextBridge.executeInMainWorld({
     let didDecorateInspectorFrontendHostInstance = false;
     // reactNativeDecorateInspectorFrontendHostInstance was introduced in
     // https://github.com/facebook/react-native-devtools-frontend/pull/168
-    globalThis.reactNativeDecorateInspectorFrontendHostInstance =
-      InspectorFrontendHostInstance => {
-        didDecorateInspectorFrontendHostInstance = true;
-        InspectorFrontendHostInstance.bringToFront = () => {
-          ipcDevTools.bringToFront();
-        };
+    // $FlowIgnore[prop-missing]
+    globalThis.reactNativeDecorateInspectorFrontendHostInstance = (
+      InspectorFrontendHostInstance: $FlowFixMe,
+    ) => {
+      didDecorateInspectorFrontendHostInstance = true;
+      InspectorFrontendHostInstance.bringToFront = () => {
+        ipcDevTools.bringToFront();
       };
+    };
 
     document.addEventListener('DOMContentLoaded', () => {
       if (!didDecorateInspectorFrontendHostInstance) {
