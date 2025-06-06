9d402bc289c98cb63ac61affd7dfae1fd8fce131
Tim Yung <yungsters@meta.com>
2025-06-05 18:12:01 -0700

    RN: Private Package for Monorepo ESLint Rules
    Summary:
    Creates a new private package for the ESLint rules that are defined in the `facebook/react-native` repository for linting the monorepo itself: `react-native/eslint-plugin-monorepo`
    
    This also eliminiates the need for the `eslint-plugin-lint` dependency in open source.
    
    Changelog:
    [Internal]
    
    Differential Revision: D76088698

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,16 +10,12 @@
 
 'use strict';
 
-const path = require('node:path');
-
-require('eslint-plugin-lint').load(path.join(__dirname, 'tools/eslint/rules'));
-
 module.exports = {
   root: true,
 
   extends: ['@react-native'],
 
-  plugins: ['@react-native/eslint-plugin-specs', 'lint'],
+  plugins: ['@react-native/monorepo', '@react-native/specs'],
 
   overrides: [
     // overriding the JS config from @react-native/eslint-config to ensure
@@ -28,13 +24,13 @@ module.exports = {
       files: ['*.js', '*.js.flow', '*.jsx'],
       parser: 'hermes-eslint',
       rules: {
+        '@react-native/monorepo/sort-imports': 1,
         'ft-flow/require-valid-file-annotation': [2, 'always'],
         'no-extra-boolean-cast': 0,
         'no-void': 0,
         // These rules are not required with hermes-eslint
         'ft-flow/define-flow-type': 0,
         'ft-flow/use-flow-type': 0,
-        'lint/sort-imports': 1,
         // Flow handles these checks for us, so they aren't required
         'no-undef': 0,
         'no-unreachable': 0,
@@ -54,7 +50,7 @@ module.exports = {
       ],
       parser: 'hermes-eslint',
       rules: {
-        'lint/no-commonjs-exports': 1,
+        '@react-native/monorepo/no-commonjs-exports': 1,
       },
     },
     {
@@ -64,14 +60,14 @@ module.exports = {
     {
       files: ['package.json'],
       rules: {
-        'lint/react-native-manifest': 2,
+        '@react-native/monorepo/react-native-manifest': 2,
       },
     },
     {
       files: ['flow-typed/**/*.js', 'packages/react-native/flow/**/*'],
       rules: {
+        '@react-native/monorepo/valid-flow-typed-signature': 2,
         'ft-flow/require-valid-file-annotation': 0,
-        'lint/valid-flow-typed-signature': 2,
         'no-shadow': 0,
         'no-unused-vars': 0,
         quotes: 0,
@@ -83,14 +79,14 @@ module.exports = {
         'packages/react-native/src/**/*.js',
       ],
       rules: {
+        '@react-native/monorepo/no-haste-imports': 2,
+        '@react-native/monorepo/no-react-default-imports': 2,
+        '@react-native/monorepo/no-react-named-type-imports': 2,
+        '@react-native/monorepo/no-react-native-imports': 2,
+        '@react-native/monorepo/no-react-node-imports': 2,
+        '@react-native/monorepo/require-extends-error': 2,
         '@react-native/platform-colors': 2,
         '@react-native/specs/react-native-modules': 2,
-        'lint/no-haste-imports': 2,
-        'lint/no-react-native-imports': 2,
-        'lint/require-extends-error': 2,
-        'lint/no-react-node-imports': 2,
-        'lint/no-react-default-imports': 2,
-        'lint/no-react-named-type-imports': 2,
       },
     },
     {
@@ -144,7 +140,7 @@ module.exports = {
     {
       files: ['**/__tests__/**'],
       rules: {
-        'lint/no-react-native-imports': 'off',
+        '@react-native/monorepo/no-react-native-imports': 'off',
       },
     },
   ],
