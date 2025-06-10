16140f366149baec7cd2266120e8b6997b16bfa3
Tomasz Żelawski <tzelawski@gmail.com>
2025-06-10 16:35:07 +0200

    chore: cleanup

--- packages/react-native/ReactCommon/react/nativemodule/core/platform/ios/ReactCommon/RCTTurboModuleManager.mm
+++ packages/react-native/ReactCommon/react/nativemodule/core/platform/ios/ReactCommon/RCTTurboModuleManager.mm
@@ -220,17 +220,6 @@ typedef struct {
   dispatch_queue_t _sharedModuleQueue;
 }
 
-- (NSMutableArray<id<RCTBridgeModule>> *)modulesRespondingToSelector:(SEL)selector{
-  id holders = [NSMutableArray new];
-  for(auto& [name, moduleHolder] : self->_moduleHolders){
-    id bridgeModule = moduleHolder.getModule();
-    if([moduleHolder.getModule() respondsToSelector:selector]) {
-      [holders addObject:bridgeModule];
-    }
-  }
-  return holders;
-}
-
 - (instancetype)initWithBridge:(RCTBridge *)bridge
                    bridgeProxy:(RCTBridgeProxy *)bridgeProxy
          bridgeModuleDecorator:(RCTBridgeModuleDecorator *)bridgeModuleDecorator
