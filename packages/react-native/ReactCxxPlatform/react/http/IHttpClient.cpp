c5bfd5be8c2121722978dec771ff317233657b5e
Christoph Purrer <chpurrer@meta.com>
2025-06-09 20:30:06 -0700

    Add http target to ReactCxxPlatform (#51899)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51899
    
    changelog: [internal]
    
    Reviewed By: andrewdacenko
    
    Differential Revision: D76240550
    
    fbshipit-source-id: 5817b80d8683d951ac686390ad74b0927a2d6d2a

---
+++ packages/react-native/ReactCxxPlatform/react/http/IHttpClient.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace facebook::react {
+
+// NOLINTNEXTLINE(modernize-avoid-c-arrays)
+extern const char HttpClientFactoryKey[] = "HttpClientFactory";
+
+} // namespace facebook::react
