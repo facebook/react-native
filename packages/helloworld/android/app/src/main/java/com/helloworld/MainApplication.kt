ff3cfff264cfd3dc39e2b4c2347ccb6025380bf7
Nicola Corti <ncor@meta.com>
2025-06-10 05:42:18 -0700

    Remove JSC build logic from helloworld (#51895)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/51895
    
    Cleaning up helloworld from all the JSC infrastructure.
    
    Changelog:
    [Internal] [Changed] -
    
    Reviewed By: mdvacca
    
    Differential Revision: D76265678
    
    fbshipit-source-id: 81fd363bf484b4c22bd278199646c7b18c59c7ff

--- packages/helloworld/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ packages/helloworld/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -32,7 +32,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        override val isHermesEnabled: Boolean = true
       }
 
   override val reactHost: ReactHost
