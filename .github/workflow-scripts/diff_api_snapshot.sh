0c4835796b897019ab349fc325c6dd52c4ce49bb
Dawid Małecki <maleckidawid@meta.com>
2025-06-13 11:27:24 +0200

    execute through diff_api_snapshot.sh

---
+++ .github/workflow-scripts/diff_api_snapshot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+export GITHUB_OWNER=-facebook
+export GITHUB_REPO=-react-native
+
+node private/react-native-bots/diff-api-snapshot-bot.js
