51f763510491c178bd47589c9ba48b9fa2a142a9
Dawid Małecki <maleckidawid@meta.com>
2025-06-16 12:08:14 +0200

    testing current snapshot

--- packages/react-native/ReactNativeApi.d.ts
+++ packages/react-native/ReactNativeApi.d.ts
@@ -2,5 +2,5 @@ export declare type AlertButton = {
     isPreferred?: boolean;
     onPress?: (((value?: string) => any) | undefined) | (Function | undefined);
     style?: AlertButtonStyle;
-    text?: string;
+    // text?: string;
   };
