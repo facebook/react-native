74b6acb1f07352aed5b122b3af6cd65fb5af340f
Andrew Datsenko <asd@meta.com>
2025-06-17 04:18:08 -0700

    Add gflags (#52015)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52015
    
    Changelog: [Internal]
    Add gflags to fantom_tester so we can pass in data like featureFlags
    
    Reviewed By: cortinico
    
    Differential Revision: D76618409
    
    fbshipit-source-id: a18e642a02c405eef972a7418a606a5980253b6a

--- private/react-native-fantom/tester/build.sh
+++ private/react-native-fantom/tester/build.sh
@@ -11,6 +11,7 @@ BUILD_DIR="$SCRIPT_DIR/build"
 REACT_NATIVE_ROOT_DIR=$(readlink -f "$SCRIPT_DIR/../../../packages/react-native")
 
 cmake -S "$SCRIPT_DIR" -B "$BUILD_DIR" \
+ -DFANTOM_THIRD_PARTY_DIR="${SCRIPT_DIR}/../build/third-party" \
  -DREACT_THIRD_PARTY_NDK_DIR="${REACT_NATIVE_ROOT_DIR}/ReactAndroid/build/third-party-ndk" \
  -DREACT_COMMON_DIR="${REACT_NATIVE_ROOT_DIR}/ReactCommon"
 
@@ -18,7 +19,22 @@ cmake --build "$BUILD_DIR" --target fantom_tester
 
 while getopts ":r" opt; do
   case $opt in
-    r) "$BUILD_DIR/fantom_tester" ;;
-    \?) echo "Invalid option: -$OPTARG"; exit 1;;
+    r) execute_tester=true ;;
+    \?) ;;
   esac
 done
+
+for arg in "$@"; do
+  if [ "$arg" = "--featureFlags" ]; then
+    feature_flags="--featureFlags=${@:$OPTIND:1}"
+    break
+  fi
+done
+
+if [ "$execute_tester" = true ]; then
+  if [ -n "$feature_flags" ]; then
+    "$BUILD_DIR/fantom_tester" "$feature_flags"
+  else
+    "$BUILD_DIR/fantom_tester"
+  fi
+fi
--- private/react-native-fantom/tester/src/main.cpp
+++ private/react-native-fantom/tester/src/main.cpp
@@ -5,14 +5,22 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <folly/json.h>
+#include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <react/featureflags/ReactNativeFeatureFlags.h>
 #include <react/featureflags/ReactNativeFeatureFlagsDynamicProvider.h>
 #include <yoga/YGEnums.h>
 #include <yoga/YGValue.h>
+
 #include <format>
 #include <iostream>
 #include <memory>
+
+DEFINE_string(
+    featureFlags,
+    "",
+    "JSON representation of the common feature flags to set for the app");
 
 using namespace facebook::react;
 
@@ -21,25 +29,40 @@ static void setUpLogging() {
   FLAGS_logtostderr = true;
 }
 
-static void setUpFeatureFlags() {
+static folly::dynamic setUpFeatureFlags() {
   folly::dynamic dynamicFeatureFlags = folly::dynamic::object();
 
   dynamicFeatureFlags["enableBridgelessArchitecture"] = true;
   dynamicFeatureFlags["cxxNativeAnimatedEnabled"] = true;
 
+  if (!FLAGS_featureFlags.empty()) {
+    dynamicFeatureFlags.update(folly::parseJson(FLAGS_featureFlags));
+  }
+
   ReactNativeFeatureFlags::override(
       std::make_unique<ReactNativeFeatureFlagsDynamicProvider>(
           dynamicFeatureFlags));
+
+  return dynamicFeatureFlags;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc > 0 && argv != nullptr) {
+    // Don't exit app on unknown flags, as some of those may be provided when
+    // debugging via XCode:
+    gflags::AllowCommandLineReparsing();
+    gflags::ParseCommandLineFlags(&argc, &argv, false);
+  }
+
   setUpLogging();
 
-  setUpFeatureFlags();
+  auto dynamicFeatureFlags = setUpFeatureFlags();
 
   LOG(INFO) << "Hello, I am fantom_tester using glog!";
   LOG(INFO) << std::format(
       "[Yoga] undefined == zero: {}", YGValueZero == YGValueUndefined);
+  LOG(INFO) << fmt::format(
+      "[FeatureFlags] overrides: {}", folly::toJson(dynamicFeatureFlags));
 
   return 0;
 }
