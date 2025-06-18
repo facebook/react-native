056c64e9ac93034e07d37ca6a991d62f1f565f98
Rubén Norte <rubennorte@fb.com>
2025-06-18 05:17:35 -0700

    Remove unnecessary feature flags from fantom tests
    Differential Revision: D76894319

--- packages/react-native/src/private/renderer/consistency/__tests__/UIConsistency-itest.js
+++ packages/react-native/src/private/renderer/consistency/__tests__/UIConsistency-itest.js
@@ -4,7 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @fantom_flags enableAccessToHostTreeInFabric:true
  * @fantom_flags enableSynchronousStateUpdates:true
  * @flow strict-local
  * @format
