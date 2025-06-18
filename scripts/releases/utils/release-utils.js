6f3948fc48956f2d640f178546d4dd84de4a3f89
Nicola Corti <ncor@meta.com>
2025-06-18 04:02:28 -0700

    Publish, Close and Release the Maven repository during build_npm_package
    Summary:
    Due to us moving to central.sonatype.com for publishing, we cannot publish and release the Maven repository in 2 distinct invocations.
    This consolidates all the publishing job to happen during build_npm_package
    
    Changelog:
    [Internal] [Changed] -
    
    Reviewed By: fabriziocucci
    
    Differential Revision: D76888543
    
    fbshipit-source-id: 3cb0db6176ed2221a12b4f3f1f575232aa006a6c

--- scripts/releases/utils/release-utils.js
+++ scripts/releases/utils/release-utils.js
@@ -68,7 +68,7 @@ function publishAndroidArtifactsToMaven(
     // -------- For stable releases, we also need to close and release the staging repository.
     if (
       exec(
-        './gradlew findSonatypeStagingRepository closeAndReleaseSonatypeStagingRepository',
+        './gradlew publishAndroidToSonatype closeAndReleaseSonatypeStagingRepository',
       ).code
     ) {
       echo(
