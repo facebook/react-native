a7b0e17af23dfe27bd65835908cba1f7586da2e6
Jakub Piasecki <piaseckijakub@meta.com>
2025-06-23 01:13:59 -0700

    Remove duplicate type import
    Summary: Changelog: [Internal]
    
    Differential Revision: D77135628

--- packages/react-native/Libraries/Lists/FlatList.js
+++ packages/react-native/Libraries/Lists/FlatList.js
@@ -21,9 +21,7 @@ import type {
 import * as ReactNativeFeatureFlags from '../../src/private/featureflags/ReactNativeFeatureFlags';
 import {type ScrollResponderType} from '../Components/ScrollView/ScrollView';
 import View from '../Components/View/View';
-import VirtualizedLists, {
-  type VirtualizedListProps,
-} from '@react-native/virtualized-lists';
+import VirtualizedLists from '@react-native/virtualized-lists';
 import memoizeOne from 'memoize-one';
 import * as React from 'react';
 
