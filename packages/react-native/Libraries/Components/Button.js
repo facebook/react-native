4f94028ca686cac8bf2b772de518d385879dbb06
Jakub Piasecki <piaseckijakub@meta.com>
2025-06-25 00:16:24 -0700

    Reduce symbol collisions in the API snapshot (#52213)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52213
    
    Changelog: [Internal]
    
    Reviewed By: huntie
    
    Differential Revision: D77207979
    
    fbshipit-source-id: 10d8b9c8f24ddb52423197f5aa300402b1e45ebe

--- packages/react-native/Libraries/Components/Button.js
+++ packages/react-native/Libraries/Components/Button.js
@@ -281,10 +281,12 @@ export type ButtonProps = $ReadOnly<{
   ```
  */
 
-const Touchable: typeof TouchableNativeFeedback | typeof TouchableOpacity =
+const NativeTouchable:
+  | typeof TouchableNativeFeedback
+  | typeof TouchableOpacity =
   Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
 
-type ButtonRef = React.ElementRef<typeof Touchable>;
+type ButtonRef = React.ElementRef<typeof NativeTouchable>;
 
 const Button: component(
   ref?: React.RefSetter<ButtonRef>,
@@ -362,7 +364,7 @@ const Button: component(
       : importantForAccessibility;
 
   return (
-    <Touchable
+    <NativeTouchable
       accessible={accessible}
       accessibilityActions={accessibilityActions}
       onAccessibilityAction={onAccessibilityAction}
@@ -391,7 +393,7 @@ const Button: component(
           {formattedTitle}
         </Text>
       </View>
-    </Touchable>
+    </NativeTouchable>
   );
 };
 
