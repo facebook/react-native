cca83e145f7bf2f972891d590b4786d644a07984
Ruslan Lesiutin <28902667+hoxyq@users.noreply.github.com>
2025-06-25 03:15:21 -0700

    @react-native/js-polyfills: polyfill console.timeStamp
    Summary:
    # Changelog: [Internal]
    
    The main reason for stubbing-out this is to make sure this method is always installed. The actual implementation will be part of the `jsinspector-modern` stack, which is fully initialized in production builds.
    
    Once there is a gurantee that RuntimeTarget globals are always installed in any environments, we can remove polyfills altogether.
    
    Differential Revision: D76987507

---
+++ packages/polyfills/__tests__/consoleTimeStamp-itest.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ * @fantom_mode *
+ * @fantom_flags fuseboxEnabledRelease:*
+ */
+
+describe('console.timeStamp()', () => {
+  it('installed', () => {
+    expect(typeof console.timeStamp).toBe('function');
+  });
+});
