1a7f84785573f530c179b4c56de49cba4278f677
nishan (o^▽^o) <nishanbende@gmail.com>
2025-06-26 12:44:17 +0530

    fix: ios build import errors

--- packages/react-native/React/Fabric/Utils/RCTBackgroundImageUtils.h
+++ packages/react-native/React/Fabric/Utils/RCTBackgroundImageUtils.h
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#import <Foundation/Foundation.h>
+#import <QuartzCore/QuartzCore.h>
 #import <react/renderer/graphics/BackgroundSize.h>
 #import <react/renderer/graphics/BackgroundPosition.h>
 #import <react/renderer/graphics/BackgroundRepeat.h>
