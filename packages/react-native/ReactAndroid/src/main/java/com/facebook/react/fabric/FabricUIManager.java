a6a2884d63717a42ac2bafd2054991ce8b32a2e9
Nick Gerleman <ngerlem@meta.com>
2025-06-26 12:09:54 -0700

    Fix onTextLayout metrics not incorporating ReactTextViewManagerCallback (#52276)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52276
    
    The line metrics reported do not process the Spannable, meaning their layout results may disagree with those used for measurement and display.
    
    Changelog:
    [Android][Fixed] - Fix onTextLayout metrics not incorporating ReactTextViewManagerCallback
    
    Reviewed By: lenaic
    
    Differential Revision: D77261839
    
    fbshipit-source-id: 87bdc86ce16a2ae9fa69532c5721c19567a53595

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/FabricUIManager.java
@@ -530,13 +530,18 @@ public class FabricUIManager
       ReadableMapBuffer paragraphAttributes,
       float width,
       float height) {
+    ViewManager textViewManager = mViewManagerRegistry.get(ReactTextViewManager.REACT_CLASS);
+
     return (NativeArray)
         TextLayoutManager.measureLines(
             mReactApplicationContext,
             attributedString,
             paragraphAttributes,
             PixelUtil.toPixelFromDIP(width),
-            PixelUtil.toPixelFromDIP(height));
+            PixelUtil.toPixelFromDIP(height),
+            textViewManager instanceof ReactTextViewManagerCallback
+                ? (ReactTextViewManagerCallback) textViewManager
+                : null);
   }
 
   public int getColor(int surfaceId, String[] resourcePaths) {
