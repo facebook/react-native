8b434fc66d09e5b7f5cfe2fd04b4393c1c67cd86
Christoph Purrer <chpurrer@meta.com>
2025-07-04 11:01:40 -0700

    Remove SampleTurboCxxModule example
    Summary:
    Changelog: [Internal]
    
    The sample is from an outdated approach of enabling C++ Modules in RN which is not recommended anymore.
    
    Prefer C++ Turbo Modules if you need to expose / access C or C++ APIs in RN apps:
    
    https://reactnative.dev/docs/the-new-architecture/pure-cxx-modules
    
    It is not included in any RNTester app at this time
    
    Differential Revision: D77771111

--- packages/react-native/ReactCommon/react/nativemodule/samples/ReactCommon/SampleTurboCxxModule.h
+++ packages/react-native/ReactCommon/react/nativemodule/samples/ReactCommon/SampleTurboCxxModule.h
@@ -5,39 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#pragma once
-
-#include <memory>
-
-#include "NativeSampleTurboCxxModuleSpecJSI.h"
-
-namespace facebook::react {
-
-/**
- * A sample implementation of the C++ spec. In practice, this class can just
- * extend jsi::HostObject directly, but using the spec provides build-time
- * type-safety.
- */
-class SampleTurboCxxModule : public NativeSampleTurboCxxModuleSpecJSI {
- public:
-  SampleTurboCxxModule(std::shared_ptr<CallInvoker> jsInvoker);
-
-  void voidFunc(jsi::Runtime& rt) override;
-  bool getBool(jsi::Runtime& rt, bool arg) override;
-  double getEnum(jsi::Runtime& rt, double arg) override;
-  double getNumber(jsi::Runtime& rt, double arg) override;
-  jsi::String getString(jsi::Runtime& rt, const jsi::String& arg) override;
-  jsi::Array getArray(jsi::Runtime& rt, const jsi::Array& arg) override;
-  jsi::Object getObject(jsi::Runtime& rt, const jsi::Object& arg) override;
-  jsi::Object getValue(
-      jsi::Runtime& rt,
-      double x,
-      const jsi::String& y,
-      const jsi::Object& z) override;
-  void getValueWithCallback(jsi::Runtime& rt, const jsi::Function& callback)
-      override;
-  jsi::Value getValueWithPromise(jsi::Runtime& rt, bool error) override;
-  jsi::Object getConstants(jsi::Runtime& rt) override;
-};
-
-} // namespace facebook::react
+// This header is left here for informational reasons.
+// Refer to packages/rn-tester/NativeCxxModuleExample/NativeCxxModuleExample.h
+// for a concrete example.
+// See: https://reactnative.dev/docs/the-new-architecture/pure-cxx-modules
