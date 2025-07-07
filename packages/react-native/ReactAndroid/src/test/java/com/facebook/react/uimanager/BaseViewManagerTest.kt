3a99e31e56a4134bf1c365478ad59e75f1cfe7ba
Nicola Corti <ncor@meta.com>
2025-07-07 10:20:43 -0700

    @DeprecatedInNewArchitecture -> @Deprecated (#52399)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52399
    
    I'm raising the deprecation warnings for those methods that are using legacy arch.
    Previously the `DeprecatedInNewArchitecture` was not generating warnings for user in their builds, while now the Kotlin's/Java's `DeprecatedInNewArchitecture` it will.
    
    Changelog:
    [Android] [Changed] - Introduce more deprecation warnings for Legacy Arch classes
    
    Reviewed By: mdvacca
    
    Differential Revision: D77736713
    
    fbshipit-source-id: bc21729ed8253d3ec6b6a40577bcd76622c3f8a6

--- packages/react-native/ReactAndroid/src/test/java/com/facebook/react/uimanager/BaseViewManagerTest.kt
+++ packages/react-native/ReactAndroid/src/test/java/com/facebook/react/uimanager/BaseViewManagerTest.kt
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+@file:Suppress("DEPRECATION")
+
 package com.facebook.react.uimanager
 
 import android.view.View.OnFocusChangeListener
