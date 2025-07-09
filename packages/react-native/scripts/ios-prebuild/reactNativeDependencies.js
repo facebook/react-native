31fdd536ca2d2d12eb3d62a334b83affdb65264a
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-07-09 10:14:41 -0700

    Add the `ENTERPRISE_REPOSITORY` env var to let user consume artifacts from their personal maven mirror (#52514)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52514
    
    As per title, this change add the `ENTERPRISE_REPOSITORY` env variable so that users can use their owm maven mirror to consume artifacts rather than the official url.
    
    This is helpful as:
    - we can reduce the traffic toward maven central
    - companies can speed up their builds by relying on local/closer replicas
    
    ## Changelog:
    [iOS][Added] - Add the `ENTERPRISE_REPOSITORY` env variable to cocopaods infra
    
    Reviewed By: cortinico
    
    Differential Revision: D78011424
    
    fbshipit-source-id: 24b83b4866095d7fe3318362afc5075b99b146e7

--- packages/react-native/scripts/ios-prebuild/reactNativeDependencies.js
+++ packages/react-native/scripts/ios-prebuild/reactNativeDependencies.js
@@ -179,7 +179,10 @@ function getTarballUrl(
   version /*: string */,
   buildType /*: BuildFlavor */,
 ) /*: string */ {
-  const mavenRepoUrl = 'https://repo1.maven.org/maven2';
+  // You can use the `ENTERPRISE_REPOSITORY` ariable to customise the base url from which artifacts will be downloaded.
+  // The mirror's structure must be the same of the Maven repo the react-native core team publishes on Maven Central.
+  const mavenRepoUrl =
+    process.env.ENTERPRISE_REPOSITORY ?? 'https://repo1.maven.org/maven2';
   const namespace = 'com/facebook/react';
   return `${mavenRepoUrl}/${namespace}/react-native-artifacts/${version}/react-native-artifacts-${version}-reactnative-dependencies-${buildType.toLowerCase()}.tar.gz`;
 }
