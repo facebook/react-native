c1ff3d0e9b5713c3b89ce192cda0df91426c8308
Alex Hunt <huntie@meta.com>
2025-07-14 07:16:48 -0700

    Support CDP response previews (#52487)
    Summary:
    
    Continues integration of `NetworkReporter` (jsinspector-modern) on Android, to enable the Network panel in React Native DevTools.
    
    NOTE: As with iOS, all changes are gated behind the `enableNetworkEventReporting` and `fuseboxNetworkInspectionEnabled` feature flags.
    
    **This diff**
    
    Integrates `Network.storeRequestBody` on Android (CDP: [`Network.getResponseBody`](https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-getResponseBody) CDP event) to populate the "Preview" and "Response" tabs in the React Native DevTools Network panel.
    
    This is integrated with `NetworkingModule.kt` to support synchronously received `text` or `blob` data types, with incremental response support added next in D77927896.
    
    Changelog: [Internal]
    
    Reviewed By: cortinico
    
    Differential Revision: D77799617

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/network/NetworkEventUtil.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/network/NetworkEventUtil.kt
@@ -10,6 +10,7 @@
 package com.facebook.react.modules.network
 
 import android.os.Bundle
+import android.util.Base64
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.WritableMap
@@ -92,7 +93,16 @@ internal object NetworkEventUtil {
   }
 
   @JvmStatic
-  fun onDataReceived(reactContext: ReactApplicationContext?, requestId: Int, data: String?) {
+  fun onDataReceived(
+      reactContext: ReactApplicationContext?,
+      requestId: Int,
+      data: String?,
+      responseType: String
+  ) {
+    if (ReactNativeFeatureFlags.enableNetworkEventReporting()) {
+      InspectorNetworkReporter.maybeStoreResponseBody(
+          requestId, data.orEmpty(), responseType == "base64")
+    }
     reactContext?.emitDeviceEvent(
         "didReceiveNetworkData",
         buildReadableArray {
@@ -102,7 +112,16 @@ internal object NetworkEventUtil {
   }
 
   @JvmStatic
-  fun onDataReceived(reactContext: ReactApplicationContext?, requestId: Int, data: WritableMap?) {
+  fun onDataReceived(
+      reactContext: ReactApplicationContext?,
+      requestId: Int,
+      data: WritableMap,
+      rawData: ByteArray
+  ) {
+    if (ReactNativeFeatureFlags.enableNetworkEventReporting()) {
+      InspectorNetworkReporter.maybeStoreResponseBody(
+          requestId, Base64.encodeToString(rawData, Base64.NO_WRAP), true)
+    }
     reactContext?.emitDeviceEvent(
         "didReceiveNetworkData",
         Arguments.createArray().apply {
