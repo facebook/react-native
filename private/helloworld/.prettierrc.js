b453adb0c6af6bcb8af040a79ee319c3c7780227
Sam Zhou <samzhou19815@meta.com>
2025-07-21 15:07:53 -0700

    Prepare react-native for prettier v3: 2/n
    Summary:
    Prettier v3 no longer loads plugin implicitly. This diff first configures the hermes-parser plugin explicitly to prepare for v3 rollout. D78590158 missed this config.
    
    Changelog: [Internal]
    
    Reviewed By: christophpurrer
    
    Differential Revision: D78673890

--- private/helloworld/.prettierrc.js
+++ private/helloworld/.prettierrc.js
@@ -13,4 +13,20 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  plugins: [
+    // Using module.parent and createRequire hack to simulate prettier v2 plugin resolution behavior.
+    // The hack allows us to resolve the plugin from the install location of prettier.
+    (module.parent
+      ? require('module').createRequire(module.parent.id)
+      : require
+    ).resolve('prettier-plugin-hermes-parser'),
+  ],
+  overrides: [
+    {
+      files: ['*.js', '*.js.flow'],
+      options: {
+        parser: 'hermes',
+      },
+    },
+  ],
 };
