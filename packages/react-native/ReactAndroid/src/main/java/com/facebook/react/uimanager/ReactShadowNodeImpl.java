52f2cbab41297d4e5689c112188f67d3c6150fe9
David Vacca <dvacca@meta.com>
2025-07-22 16:54:30 -0700

    Delete ReactYogaConfigProvider (#52772)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52772
    
    ReactYogaConfigProvider is an internal legacy unused class, I'm just deleting it
    
    There are no usages of this class
    
    changelog: [internal] internal
    
    Reviewed By: NickGerleman
    
    Differential Revision: D78516728
    
    fbshipit-source-id: f694e9cd66ebe6cf97b343ce971b61fbd42f956f

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ReactShadowNodeImpl.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ReactShadowNodeImpl.java
@@ -16,10 +16,12 @@ import com.facebook.react.uimanager.annotations.ReactPropertyHolder;
 import com.facebook.yoga.YogaAlign;
 import com.facebook.yoga.YogaBaselineFunction;
 import com.facebook.yoga.YogaConfig;
+import com.facebook.yoga.YogaConfigFactory;
 import com.facebook.yoga.YogaConstants;
 import com.facebook.yoga.YogaDirection;
 import com.facebook.yoga.YogaDisplay;
 import com.facebook.yoga.YogaEdge;
+import com.facebook.yoga.YogaErrata;
 import com.facebook.yoga.YogaFlexDirection;
 import com.facebook.yoga.YogaGutter;
 import com.facebook.yoga.YogaJustify;
@@ -63,7 +65,9 @@ public class ReactShadowNodeImpl implements ReactShadowNode<ReactShadowNodeImpl>
   private static final YogaConfig sYogaConfig;
 
   static {
-    sYogaConfig = ReactYogaConfigProvider.INSTANCE.getYogaConfig();
+    sYogaConfig = YogaConfigFactory.create();
+    sYogaConfig.setPointScaleFactor(0f);
+    sYogaConfig.setErrata(YogaErrata.ALL);
   }
 
   static {
