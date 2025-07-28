0c06ccc78fd1916621867fdaf397c3b3df3a43be
Ruslan Shestopalyuk <rshest@meta.com>
2025-07-28 06:49:32 -0700

    Add benchmark Fantom test for Image component (#52871)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52871
    
    ## Changelog:
    [Internal] -
    
    As in the title, this adds a basic Fantom benchmark test for the Image component.
    
    Reviewed By: rubennorte
    
    Differential Revision: D79085957
    
    fbshipit-source-id: 4098415353c8a04992c39e39d9a1dd20270f20d7

---
+++ packages/react-native/Libraries/Image/__tests__/Image-benchmark-itest.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+import '@react-native/fantom/src/setUpDefaultReactNativeEnvironment';
+
+import * as Fantom from '@react-native/fantom';
+import * as React from 'react';
+import {Image} from 'react-native';
+
+const IMAGE1 = require('./img/img1.png');
+const IMAGE2 = require('./img/img2.png');
+
+let root;
+let testElements: React.MixedElement;
+
+Fantom.unstable_benchmark.suite('Image').test.each(
+  [100, 1000],
+  n => `render ${n.toString()} image component instances`,
+  () => {
+    Fantom.runTask(() => root.render(testElements));
+  },
+  n => ({
+    beforeAll: () => {
+      testElements = (
+        <>
+          {Array.from({length: n}, (_, i) => (
+            <Image
+              id={String(i)}
+              nativeID={String(i)}
+              source={i % 2 === 0 ? IMAGE1 : IMAGE2}
+              style={{
+                width: i + 1,
+                height: i + 1,
+              }}
+            />
+          ))}
+        </>
+      );
+    },
+    beforeEach: () => {
+      root = Fantom.createRoot();
+    },
+    afterEach: () => {
+      root.destroy();
+    },
+  }),
+);
