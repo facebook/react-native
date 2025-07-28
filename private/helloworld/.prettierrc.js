209e1243404b39755f9a6a78f2af4b221f788936
Sam Zhou <samzhou19815@meta.com>
2025-07-28 07:28:05 -0700

    Apply fix of https://github.com/facebook/react-native/pull/52787 to an inner prettier config (#52881)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52881
    
    Changelog: [Internal]
    
    Reviewed By: cipolleschi
    
    Differential Revision: D79089990
    
    fbshipit-source-id: 12d5a7defabf877c62113e72099c965351896cf7

--- private/helloworld/.prettierrc.js
+++ private/helloworld/.prettierrc.js
@@ -7,20 +7,18 @@
  * @format
  */
 
+let plugins = ['prettier-plugin-hermes-parser'];
+try {
+  plugins = require('../../.prettier-plugins.fb.js');
+} catch {}
+
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
-  plugins: [
-    // Using module.parent and createRequire hack to simulate prettier v2 plugin resolution behavior.
-    // The hack allows us to resolve the plugin from the install location of prettier.
-    (module.parent
-      ? require('module').createRequire(module.parent.id)
-      : require
-    ).resolve('prettier-plugin-hermes-parser'),
-  ],
+  plugins,
   overrides: [
     {
       files: ['*.js', '*.js.flow'],
