a3412b03abb6bc706c301c96d368ad66de696139
David Vacca <515103+mdvacca@users.noreply.github.com>
2025-07-29 14:35:37 -0700

    Throw Exception if ReactApplication.reactNativeHost is not overriden (#52912)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/52912
    
    This diff throws an Exception if ReactApplication.reactNativeHost is not overriden. This field is deprecated and it will be deleted in the near future.
    The goal of this diff is to be able to remove usages of reactNativeHost for classes that implement ReactApplication
    
    changelog: [Android][Breaking] Throw Exception if ReactApplication.reactNativeHost is not overriden
    
    Reviewed By: mlord93
    
    Differential Revision: D79186336

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactApplication.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/ReactApplication.kt
@@ -15,6 +15,9 @@ public interface ReactApplication {
       "You should not use ReactNativeHost directly in the New Architecture. Use ReactHost instead.",
       ReplaceWith("reactHost"))
   public val reactNativeHost: ReactNativeHost
+    get() {
+      throw RuntimeException("You should not use ReactNativeHost directly in the New Architecture")
+    }
 
   /**
    * Get the default [ReactHost] for this app. This method will be used by the new architecture of
