809efffc2028ee1fcc75ff12b2741d22617805f0
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-07-29 20:21:09 +0100

    remove git folder

---
+++ packages/ios-test-app/RNTestApp/RNTestApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RNTestApp
+//
+//  Created by Riccardo Cipolleschi on 29/07/2025.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
