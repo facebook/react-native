ed06e97bb513d3309de490e69230940bdf2866a1
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-07-29 20:21:09 +0100

    remove git folder

---
+++ packages/ios-test-app/RNTestApp/RNTestApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RNTestApp
+//
+//  Created by Riccardo Cipolleschi on 29/07/2025.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+  self.view.backgroundColor = [UIColor redColor];
+}
+
+
+@end
