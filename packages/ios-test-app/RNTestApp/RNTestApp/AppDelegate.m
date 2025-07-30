e4f532bf106a69b2e47efa61e781ae54db1d2d39
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-07-30 13:48:34 +0100

    Fix search paths and push forward with the app

--- packages/ios-test-app/RNTestApp/RNTestApp/AppDelegate.m
+++ packages/ios-test-app/RNTestApp/RNTestApp/AppDelegate.m
@@ -7,34 +7,31 @@
 
 #import "AppDelegate.h"
 
+#import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
+
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // Override point for customization after application launch.
+  
+  self.reactNativeFactory = [[RCTReactNativeFactory alloc] initWithDelegate:self];
+  self.dependencyProvider = [RCTAppDependencyProvider new];
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  [self.reactNativeFactory startReactNativeWithModuleName:@"RNTesterApp"
+                                                   inWindow:self.window
+                                        initialProperties:@{}
+                                              launchOptions:launchOptions];
   return YES;
 }
 
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-  // Called when a new scene session is being created.
-  // Use this method to select a configuration to create the new scene with.
-  return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+- (NSURL *)bundleURL
+{
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"js/RNTesterApp.ios"];
 }
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-  // Called when the user discards a scene session.
-  // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-  // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
-
 
 @end
