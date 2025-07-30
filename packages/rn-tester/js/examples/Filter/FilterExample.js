2f7dee54c9d2ca540e6b584a69f7604b4d01e9a7
nishan (o^▽^o) <nishanbende@gmail.com>
2025-07-30 15:52:15 +0530

    lint fix

--- packages/rn-tester/js/examples/Filter/FilterExample.js
+++ packages/rn-tester/js/examples/Filter/FilterExample.js
@@ -15,7 +15,7 @@ import type {ViewStyleProp} from 'react-native/Libraries/StyleSheet/StyleSheet';
 
 import React from 'react';
 import {useState} from 'react';
-import {Animated, Image, StyleSheet, Text, View, Button} from 'react-native';
+import {Animated, Button, Image, StyleSheet, Text, View} from 'react-native';
 
 const alphaHotdog = require('../../assets/alpha-hotdog.png');
 const hotdog = require('../../assets/hotdog.jpg');
