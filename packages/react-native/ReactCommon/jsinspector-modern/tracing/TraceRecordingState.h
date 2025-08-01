4c9a8c157f280fe6652d20a7e49f2a7d4d392b58
Ruslan Lesiutin <hoxy@meta.com>
2025-08-01 03:56:36 -0700

    Create a local session for Tracing
    Summary:
    # Changelog: [Internal]
    
    The current design for Tracing is flawed. Right now the logic is mostly scattered around CDP Agents, lifetime of which is tied to CDP session.
    
    This diff introduces a new approach:
    - The HostTarget will be the only public entrypoints to startTracing as part of the jsinspector backend.
    - It will create and own TraceRecording that acts as a local session. We won't use wording session here, because it is already reserved for CDP case.
    - Every Target will implement TracingAgent, lifetime of which will be limited by lifetime of either Target or TraceRecording.
    - All these TracingAgent will have a reference to TraceRecordingState, which they can mutate
    
    This approach unblocks:
    - Recording traces without active CDP sessions, for example in a background.
    - Recording full instance reloads and multiple profiles for Instances and Runtimes.
    
    {F1980838032}
    
    Differential Revision: D79371359

---
+++ packages/react-native/ReactCommon/jsinspector-modern/tracing/TraceRecordingState.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+namespace facebook::react::jsinspector_modern::tracing {
+
+struct TraceRecordingState {
+  bool isRecording = false;
+};
+
+} // namespace facebook::react::jsinspector_modern::tracing
