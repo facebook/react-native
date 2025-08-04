c88f959cc8c628aab0fa0c4513c9c72aaf4f87d5
Rubén Norte <rubennorte@meta.com>
2025-08-04 05:13:50 -0700

    Refactor runner to use a single instance of Metro for each run (#52777)
    Summary:
    
    Changelog: [internal]
    
    This significantly speeds up test execution in Fantom (around 2x in OSS and 6x at Meta) by starting a Metro server before all tests runs and reusing it across all tests to build test bundles, instead of spinning up a new Metro instance every time we run each test.
    
    The architecture change (also considering the previous change in buck prebuilds) looks like this:
    {F1980689532} 
    
    This is how is impacts execution times (compared to the baseline):
    * OSS
      * Before: 62s {F1980564286}
      * After: 30s (**2x faster**) {F1980564265}
    
    Reviewed By: andrewdacenko
    
    Differential Revision: D78741903

--- private/react-native-fantom/runner/global-setup/globalSetup.js
+++ private/react-native-fantom/runner/global-setup/globalSetup.js
@@ -10,6 +10,8 @@
 
 import {isOSS, validateEnvironmentVariables} from '../EnvironmentOptions';
 import build from './build';
+import Metro from 'metro';
+import path from 'path';
 
 export default async function globalSetup(
   globalConfig: {...},
@@ -19,7 +21,38 @@ export default async function globalSetup(
 
   validateEnvironmentVariables();
 
+  await startMetroServer();
+
   if (!isOSS) {
     await build();
   }
+}
+
+async function startMetroServer() {
+  const metroConfig = await Metro.loadConfig({
+    config: path.resolve(__dirname, '..', '..', 'config', 'metro.config.js'),
+  });
+
+  // We need to reuse the same port across runs because can only set environment
+  // variables for workers in the first one.
+  // $FlowExpectedError[cannot-write]
+  metroConfig.server.port =
+    process.env.__FANTOM_METRO_PORT__ != null
+      ? Number(process.env.__FANTOM_METRO_PORT__)
+      : // Any available port
+        0;
+
+  const server = await Metro.runServer(metroConfig, {
+    waitForBundler: true,
+    watch: true,
+  });
+
+  if (process.env.__FANTOM_METRO_PORT__ == null) {
+    process.env.__FANTOM_METRO_PORT__ = String(
+      server.httpServer.address().port,
+    );
+  }
+
+  // $FlowExpectedError[prop-missing]
+  globalThis.__METRO_SERVER__ = server;
 }
