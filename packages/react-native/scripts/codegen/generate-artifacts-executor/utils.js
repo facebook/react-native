30bdabce989881dcec7bb5772e49ed34731d8aa5
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-08-04 16:42:14 +0100

    implement changes in codegen to put files in the right folders

--- packages/react-native/scripts/codegen/generate-artifacts-executor/utils.js
+++ packages/react-native/scripts/codegen/generate-artifacts-executor/utils.js
@@ -430,6 +430,14 @@ function findDisabledLibrariesByPlatform(
   );
 }
 
+function findReactNativeRootPath(projectRoot /* : string */) /* : string */  {
+  const reactNativePackageJsonPath = require.resolve(path.join('react-native', 'package.json'), {
+    paths: [projectRoot],
+  });
+
+  return path.dirname(reactNativePackageJsonPath);
+}
+
 module.exports = {
   buildCodegenIfNeeded,
   pkgJsonIncludesGeneratedCode,
@@ -443,4 +451,5 @@ module.exports = {
   parseiOSAnnotations,
   readReactNativeConfig,
   findDisabledLibrariesByPlatform,
+  findReactNativeRootPath,
 };
