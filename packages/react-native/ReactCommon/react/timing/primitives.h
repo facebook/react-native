f1cf4894ff288c087b79faebe5293559e68d0238
Rubén Norte <rubennorte@meta.com>
2025-08-04 11:21:03 -0700

    Allow mocking HighResTimeStamp in debug builds (#53020)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53020
    
    Changelog: [internal]
    
    This adds a new feature to `HighResTimeStamp` to set a custom timestamp provider for "now" that can be useful for testing, only in debug builds to avoid potentially regressing performance.
    
    Reviewed By: hoxyq, rshest
    
    Differential Revision: D79554725
    
    fbshipit-source-id: c325d05999b9e2d69f769b61f15c763446777a0a

--- packages/react-native/ReactCommon/react/timing/primitives.h
+++ packages/react-native/ReactCommon/react/timing/primitives.h
@@ -7,7 +7,9 @@
 
 #pragma once
 
+#include <react/debug/flags.h>
 #include <chrono>
+#include <functional>
 
 namespace facebook::react {
 
@@ -227,6 +229,14 @@ class HighResTimeStamp {
     return HighResTimeStamp(chronoTimePoint);
   }
 
+#ifdef REACT_NATIVE_DEBUG
+  static void setTimeStampProviderForTesting(
+      std::function<std::chrono::steady_clock::time_point()>&&
+          timeStampProvider) {
+    getTimeStampProvider() = std::move(timeStampProvider);
+  }
+#endif
+
   // This method is provided for convenience, if you need to convert
   // HighResTimeStamp to some common epoch with time stamps from other sources.
   constexpr std::chrono::steady_clock::time_point toChronoSteadyClockTimePoint()
@@ -275,9 +285,24 @@ class HighResTimeStamp {
 
   std::chrono::steady_clock::time_point chronoTimePoint_;
 
+#ifdef REACT_NATIVE_DEBUG
+  static std::function<std::chrono::steady_clock::time_point()>&
+  getTimeStampProvider() {
+    static std::function<std::chrono::steady_clock::time_point()>
+        timeStampProvider = nullptr;
+    return timeStampProvider;
+  }
+
+  static std::chrono::steady_clock::time_point chronoNow() {
+    auto& timeStampProvider = getTimeStampProvider();
+    return timeStampProvider != nullptr ? timeStampProvider()
+                                        : std::chrono::steady_clock::now();
+  }
+#else
   inline static std::chrono::steady_clock::time_point chronoNow() {
     return std::chrono::steady_clock::now();
   }
+#endif
 };
 
 inline constexpr HighResDuration operator-(
