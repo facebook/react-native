1e529cd875ef008b7889e422245ea777959a4601
Rubén Norte <rubennorte@meta.com>
2025-08-05 09:17:29 -0700

    Add test to ensure setUpDefaultReactNativeEnvironment does not access feature flags (#53057)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53057
    
    Changelog: [internal]
    
    This adds a Fantom test to ensure that `setUpDefaultReactNativeEnvironment` doesn't read any feature flags. This prevents catching this as a runtime issue when the feature flag system complains that feature flags were accessed before being overridden, which always would happen if this module read any flags (as it runs before any product code that sets overrides).
    
    Reviewed By: rshest
    
    Differential Revision: D79639890

---
+++ packages/react-native/src/private/setup/__tests__/setUpDefaultReactNativeEnvironment-FeatureFlags-itest.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+import * as ReactNativeFeatureFlags from '../../featureflags/ReactNativeFeatureFlags';
+import * as ReactNativeFeatureFlagsBase from '../../featureflags/ReactNativeFeatureFlagsBase';
+import setUpDefaultReactNativeEnvironment from 'react-native/src/private/setup/setUpDefaultReactNativeEnvironment';
+
+describe('setUpReactNativeEnvironment', () => {
+  it('should not read any feature flags', () => {
+    ReactNativeFeatureFlagsBase.dangerouslyResetForTesting();
+
+    setUpDefaultReactNativeEnvironment(false);
+
+    expect(() => {
+      // If any feature flags were read, this call would fail.
+      ReactNativeFeatureFlags.override({
+        jsOnlyTestFlag: () => true,
+      });
+    }).not.toThrow();
+  });
+});
