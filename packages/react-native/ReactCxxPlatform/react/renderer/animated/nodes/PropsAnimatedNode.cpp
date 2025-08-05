9bf34cb7535a4c329ca7baef0aa9b3fd5f5bc255
Zeya Peng <zeyap@meta.com>
2025-08-05 08:55:24 -0700

    Make sure props default value is restored when disconnected from animated (#53043)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53043
    
    ## Changelog:
    
    [Internal] [Changed] - Make sure props default value is restored when disconnected from animated
    
    Reviewed By: sammy-SC
    
    Differential Revision: D79566216

--- packages/react-native/ReactCxxPlatform/react/renderer/animated/nodes/PropsAnimatedNode.cpp
+++ packages/react-native/ReactCxxPlatform/react/renderer/animated/nodes/PropsAnimatedNode.cpp
@@ -69,8 +69,11 @@ void PropsAnimatedNode::disconnectFromView(Tag viewTag) {
 void PropsAnimatedNode::restoreDefaultValues() {
   // If node is already disconnected from View, we cannot restore default values
   if (connectedViewTag_ != animated::undefinedAnimatedNodeIdentifier) {
-    manager_->schedulePropsCommit(
-        connectedViewTag_, folly::dynamic::object(), false, false);
+    std::lock_guard<std::mutex> lock(propsMutex_);
+    for (auto& iter : props_.items()) {
+      iter.second = folly::dynamic(nullptr);
+    }
+    manager_->schedulePropsCommit(connectedViewTag_, props_, false, false);
   }
 }
 
