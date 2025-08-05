d5d21d061493ee973c789a7c6ab8cceebc1f04f9
Nicola Corti <ncor@meta.com>
2025-08-05 05:11:12 -0700

    Remove possibility to newArchEnabled=false in 0.82 (#53025)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53025
    
    It's now time to say goodbye to the Legacy Architecture :')
    
    This change hardcodes the `newArchEnabled` property to true, and warns the users
    if they're attempting to set it to false.
    
    Changelog:
    [Android] [Breaking] - Remove possibility to newArchEnabled=false in 0.82
    
    Reviewed By: cipolleschi
    
    Differential Revision: D78560296
    
    fbshipit-source-id: ccfc45d2f7f21cc20e063cb901d76be3d41458d6

--- private/helloworld/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ private/helloworld/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -30,8 +30,6 @@ class MainApplication : Application(), ReactApplication {
         override fun getJSMainModuleName(): String = "index"
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
       }
 
   override val reactHost: ReactHost
