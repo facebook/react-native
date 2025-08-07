9214bf91f704920cfa1f762bb8bab42b72b8804c
David Vacca <515103+mdvacca@users.noreply.github.com>
2025-08-07 09:49:27 -0700

    Deprecate LegacyArchitecture classes from package com.facebook.react.uimanager
    Summary:
    Deprecate com.facebook.react.uimanager classes
    
    chagelog: [Android][Breaking] Deprecate LegacyArchitecture classes from package com.facebook.react.uimanager
    
    Differential Revision: D79660036

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/NoSuchNativeViewException.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/NoSuchNativeViewException.kt
@@ -16,6 +16,9 @@ import com.facebook.react.common.annotations.internal.LegacyArchitectureLogger
  * associated with it.
  */
 @LegacyArchitecture(logLevel = LegacyArchitectureLogLevel.ERROR)
+@Deprecated(
+    message = "This class is part of Legacy Architecture and will be removed in a future release",
+    level = DeprecationLevel.WARNING)
 internal class NoSuchNativeViewException(detailMessage: String) :
     IllegalViewOperationException(detailMessage) {
 
