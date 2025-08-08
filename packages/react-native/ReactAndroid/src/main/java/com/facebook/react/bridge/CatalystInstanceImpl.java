9831b0386063fded709aebf7bef30902f872b5b2
David Vacca <dvacca@meta.com>
2025-08-08 01:20:42 -0700

    Rename BridgeSoLoader -> ReactNativeJNISoLoader (#53153)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53153
    
    In this diff I'm renaming BridgeSoLoader -> ReactNativeJNISoLoader and removing LegacyArchitecture becuase this class loads jni classes that are required in new architecture
    
    changelog: [internal] internal
    
    Reviewed By: RSNara
    
    Differential Revision: D79827295
    
    fbshipit-source-id: 2d02fa1de49b2e4ee838f14e976ae3ab2ca98aef

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/CatalystInstanceImpl.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/CatalystInstanceImpl.java
@@ -50,7 +50,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @LegacyArchitecture
 public class CatalystInstanceImpl implements CatalystInstance {
   static {
-    BridgeSoLoader.staticInit();
+    ReactNativeJNISoLoader.staticInit();
     LegacyArchitectureLogger.assertLegacyArchitecture(
         "CatalystInstanceImpl", LegacyArchitectureLogLevel.WARNING);
   }
