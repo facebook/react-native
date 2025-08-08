e547f466ee41415a75ec6b6f910171285ee7bfc3
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-08-08 06:47:23 -0700

    Improve codegen to add getDebugProps to components (#53135)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53135
    
    Our Codegenerated components are not generating code for `getDebugProps`. This change modifies Codegen to add those functions for all the codegen components.
    
    ## Changelog:
    [General][Added] - Added getDebugProps to codegen
    
    ## Facebook:
    `getDebugProps` are required by Fantom to write tests. However, we can't generate these function for third party components, because codegen can generate arbitrary structs and we don't have a generic `toString()` method that can be used or automatically generated by C++.
    
    By generating this function only for Core Components, we can ensure that we can write Fantom tests without breaking all the users of React Native.
    
    Reviewed By: rubennorte
    
    Differential Revision: D79805145
    
    fbshipit-source-id: 0e41c65fc30eaa886a05557ca233fb0a9cb18a71

--- packages/react-native-codegen/src/generators/components/GenerateComponentDescriptorCpp.js
+++ packages/react-native-codegen/src/generators/components/GenerateComponentDescriptorCpp.js
@@ -61,6 +61,7 @@ module.exports = {
     packageName?: string,
     assumeNonnull: boolean = false,
     headerPrefix?: string,
+    includeGetDebugPropsImplementation?: boolean = false,
   ): FilesOutput {
     const fileName = 'ComponentDescriptors.cpp';
 
