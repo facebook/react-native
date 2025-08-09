c1f7c5e3217a7e8a77a859652aadff2a41e3ea58
David Vacca <dvacca@meta.com>
2025-08-09 04:39:35 -0700

    Depreacte remaining LegacyArchitecture classes from the bridge package (#53191)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53191
    
    Depreacte remaining LegacyArchitecture classes from the bridge package
    
    changelog: [Android][Changed] Depreacte all LegacyArchitecture classes from the bridge package
    
    Reviewed By: mlord93
    
    Differential Revision: D79674635
    
    fbshipit-source-id: 6a873d05157e17ef0434821e1c8a77959d7f079a

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/OnBatchCompleteListener.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/bridge/OnBatchCompleteListener.kt
@@ -12,6 +12,9 @@ import com.facebook.react.common.annotations.internal.LegacyArchitectureLogLevel
 
 /** Interface for a module that will be notified when a batch of JS->Java calls has finished. */
 @LegacyArchitecture(logLevel = LegacyArchitectureLogLevel.ERROR)
+@Deprecated(
+    message = "This class is part of Legacy Architecture and will be removed in a future release",
+    level = DeprecationLevel.WARNING)
 public fun interface OnBatchCompleteListener {
   public fun onBatchComplete()
 }
