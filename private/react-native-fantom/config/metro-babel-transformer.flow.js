9c201c0f8fe68a89a737aa520e2cb195b1b68e82
Rubén Norte <rubennorte@meta.com>
2025-08-12 05:41:11 -0700

    Automatically inject debugger statements in tests in preparation for debug mode (#53205)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53205
    
    Changelog: [internal]
    
    This injects a custom Babel transform for Fantom tests that automatically injects `debugger` statements in the generated code. This simplifies debugging by providing a default interruption point in the test setup for the test author to decide what to debug.
    
    This has no effect unless the debugger is opened, which isn't happening yet.
    
    Reviewed By: rshest
    
    Differential Revision: D79996000
    
    fbshipit-source-id: 6153587264d293a067e359edba4f64f41898c506

---
+++ private/react-native-fantom/config/metro-babel-transformer.flow.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+import type {
+  BabelTransformer,
+  BabelTransformerArgs,
+} from 'metro-babel-transformer';
+
+import MetroBabelTransformer from '@react-native/metro-babel-transformer';
+import crypto from 'crypto';
+import fs from 'fs';
+
+const transform: BabelTransformer['transform'] = (
+  args: BabelTransformerArgs,
+) => {
+  const processedArgs = {
+    ...args,
+    plugins: [
+      ...(args.plugins ?? []),
+      // $FlowExpectedError[untyped-import]
+      require('./babel-plugins/inject-debugger-statements-in-tests'),
+    ],
+  };
+  return MetroBabelTransformer.transform(processedArgs);
+};
+
+module.exports = {
+  ...MetroBabelTransformer,
+  transform,
+  getCacheKey(): string {
+    const key = crypto.createHash('md5');
+    const cacheKeyParts = [
+      MetroBabelTransformer.getCacheKey?.() ?? '',
+      fs.readFileSync(__filename),
+      fs.readFileSync(
+        require.resolve('./babel-plugins/inject-debugger-statements-in-tests'),
+      ),
+    ];
+    cacheKeyParts.forEach(part => key.update(part));
+    return key.digest('hex');
+  },
+};
