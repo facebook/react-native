241c4ffa9e7afb09e95e24c88e6f0fd64f4ce552
David Vacca <dvacca@meta.com>
2025-08-14 14:57:34 -0700

    Align APIs ReactSurfaceImpl.view with ReactSurfaceImpl.attachView()
    Summary:
    This diff aligns the APIs ReactSurfaceImpl.view and ReactSurfaceImpl.attachView() to make sure they receive and return the same type
    
    changelog: [Android][Changed] Changed return type of ReactSurfaceImpl.view to ReactSurfaceView to align with parameter recived by ReactSurfaceImpl.attachView()
    
    Reviewed By: sammy-SC
    
    Differential Revision: D80289764

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/runtime/ReactSurfaceImpl.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/runtime/ReactSurfaceImpl.kt
@@ -11,7 +11,6 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.View.MeasureSpec
-import android.view.ViewGroup
 import androidx.annotation.UiThread
 import com.facebook.infer.annotation.ThreadSafe
 import com.facebook.react.ReactHost
@@ -116,7 +115,7 @@ internal constructor(
     reactHostRef.set(null)
   }
 
-  public override val view: ViewGroup?
+  public override val view: ReactSurfaceView?
     get() = surfaceViewRef.get()
 
   override fun prerender(): TaskInterface<Void> {
