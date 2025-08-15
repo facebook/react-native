be69a791a284e8a8f0160e809779fb50d811b18a
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-08-15 08:53:42 -0700

    Fix Switch layout with iOS26 (#53247)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53247
    
    Apple changed the sizes of the UISwitchComponent and now, if you build an iOs app using the <Switch> component, the layout of the app will be broken because of wrong layout measurements.
    This has been reported also by [https://github.com/facebook/react-native/issues/52823](https://github.com/facebook/react-native/issues/52823).
    
    The `<Switch>` component was using hardcoded values for its size.
    This change fixes the problem by:
    - Using codegen for interface only
    - Implementing a custom Sadow Node to ask the platform for the Switch measurements
    - Updating the JS layout to wrap the size around the native component.
    
    ## Changelog:
    
    [iOS][Fixed] - Fix Switch layout to work with iOS26
    
    Pull Request resolved: https://github.com/facebook/react-native/pull/53067
    
    Test Plan:
    Tested locally with RNTester.
    
    | iOS Version | Before | After |
    | --- | --- | --- |
    | < iOS 26 | ![Simulator Screen Recording - iPhone 16 Pro - 2025-08-05 at 17 53 06](https://github.com/user-attachments/assets/91d73ea3-30ba-4a5c-948e-ea5c63aa7c6d) | ![Simulator Screen Recording - NewSim - 2025-08-05 at 17 51 34](https://github.com/user-attachments/assets/76061bc8-0f14-412a-a8fb-d1c3951772e6) |
    | >=--sanitized--
    
    Rollback Plan:
    
    Reviewed By: sammy-SC
    
    Differential Revision: D79653120
    
    Pulled By: cipolleschi

---
+++ packages/react-native/ReactCommon/react/renderer/components/switch/iosswitch/react/renderer/components/switch/MacOSSwitchShadowNode.mm
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <AppKit/AppKit.h>
+#import <React/RCTUtils.h>
+#include "AppleSwitchShadowNode.h"
+
+namespace facebook::react {
+
+extern const char IOSSwitchComponentName[] = "Switch";
+
+#pragma mark - LayoutableShadowNode
+
+Size SwitchShadowNode::measureContent(
+    const LayoutContext & /*layoutContext*/,
+    const LayoutConstraints &layoutConstraints) const
+{
+  if (iosSwitchSize.width != 0) {
+    return iosSwitchSize;
+  }
+  // Let's cache the value of the SwitchSize the first time we compute it.
+  __block CGSize cgsize;
+  RCTUnsafeExecuteOnMainQueueSync(^{
+    NSSwitch *switchControl = [[NSSwitch alloc] init];
+    cgsize = [switchControl intrinsicContentSize];
+  });
+
+  // For macOS, use the intrinsic size as-is
+  iosSwitchSize = {.height = cgsize.height, .width = cgsize.width};
+
+  return iosSwitchSize;
+}
+
+} // namespace facebook::react
