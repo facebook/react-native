a9c5e9d1c14d8c6b063564398e7e02ff13ad3634
riteshshukla04 <riteshshukla2381@gmail.com>
2025-08-15 17:29:35 +0530

    Perf-1

--- packages/react-native/Libraries/Lists/FlatList.js
+++ packages/react-native/Libraries/Lists/FlatList.js
@@ -669,7 +669,7 @@ class FlatList<ItemT = any> extends React.PureComponent<FlatListProps<ItemT>> {
       : {renderItem: renderProp};
   };
 
-  _getStickyHeaderIndices: Array<number> =
+  _getStickyHeaderIndices: Array<number> | void =
     this._getItemCount(this.props.data) > 0
       ? this.props.stickyHeaderIndices
       : [];
