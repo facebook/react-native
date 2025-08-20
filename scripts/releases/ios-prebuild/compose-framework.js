1c471fc57c391d95162dea8279417a397dc256fd
Christian Falch <875252+chrfalch@users.noreply.github.com>
2025-08-20 17:36:59 +0200

    Apply suggestion from @cipolleschi
    Co-authored-by: Riccardo Cipolleschi <cipolleschi@meta.com>

--- scripts/releases/ios-prebuild/compose-framework.js
+++ scripts/releases/ios-prebuild/compose-framework.js
@@ -218,7 +218,7 @@ function copySymbols(
       scheme + '.framework.dSYM,
     );
     if (!fs.existsSync(sourceSymbolPath)) {
-      throw new Error(`dSym folder ${sourceSymbolPath} not found`);
+      throw new Error(`dSYM folder ${sourceSymbolPath} not found`);
     }
 
     const archName = path.basename(targetFolder);
