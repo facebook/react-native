dcf3a90c9bb4abcaa8519f437fa2200a9e3e9b77
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-08-20 14:16:20 +0200

    prettier

--- packages/react-native/Libraries/Image/Image.android.js
+++ packages/react-native/Libraries/Image/Image.android.js
@@ -406,7 +406,8 @@ if (ReactNativeFeatureFlags.reduceDefaultPropsInImage()) {
         expanded: props['aria-expanded'] ?? props.accessibilityState?.expanded,
         selected: props['aria-selected'] ?? props.accessibilityState?.selected,
       },
-      importantForAccessibility: props['aria-hidden'] === true ? 'no-hide-descendants' : undefined,
+      importantForAccessibility:
+        props['aria-hidden'] === true ? 'no-hide-descendants' : undefined,
     };
 
     const flattenedStyle = flattenStyle<ImageStyleProp>(style);
