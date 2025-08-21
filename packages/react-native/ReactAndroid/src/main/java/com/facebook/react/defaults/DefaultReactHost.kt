3c79d7c18d30b18993f804ca39da7779ac056e3e
Juan Penaloza <juanpenaloza@fb.com>
2025-08-21 06:26:35 -0700

    Revert D80623826: Remove deprecated DefaultReactHost.getDefaultReactHost() overload - part 2
    Differential Revision:
    D80623826
    
    Original commit changeset: f201e99f7cd4
    
    Original Phabricator Diff: D80623826
    
    fbshipit-source-id: 006bef0883f8cc82b333a68b9260b77afaae060f

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultReactHost.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultReactHost.kt
@@ -158,6 +158,72 @@ public object DefaultReactHost {
    * Util function to create a default [ReactHost] to be used in your application. This method is
    * used by the New App template.
    *
+   * @param context the Android [Context] to use for creating the [ReactHost]
+   * @param packageList the list of [ReactPackage]s to use for creating the [ReactHost]
+   * @param jsMainModulePath the path to your app's main module on Metro. Usually `index` or
+   *   `index.<platform>`
+   * @param jsBundleAssetPath the path to the JS bundle relative to the assets directory. Will be
+   *   composed in a `asset://...` URL
+   * @param jsBundleFilePath the path to the JS bundle on the filesystem. Will be composed in a
+   *   `file://...` URL
+   * @param isHermesEnabled whether to use Hermes as the JS engine, default to true.
+   * @param useDevSupport whether to enable dev support, default to ReactBuildConfig.DEBUG.
+   * @param cxxReactPackageProviders a list of cxxreactpackage providers (to register c++ turbo
+   *   modules)
+   * @param exceptionHandler Callback that can be used by React Native host applications to react to
+   *   exceptions thrown by the internals of React Native.
+   * @param bindingsInstaller that can be used for installing bindings.
+   */
+  @Deprecated(
+      message = "Use `getDefaultReactHost`  with `jsRuntimeFactory` instead",
+      replaceWith =
+          ReplaceWith(
+              """
+      fun getDefaultReactHost(
+        context: Context,
+        packageList: List<ReactPackage>,
+        jsMainModulePath: String,
+        jsBundleAssetPath: String,
+        jsBundleFilePath: String?,
+        jsRuntimeFactory: JSRuntimeFactory?,
+        useDevSupport: Boolean,
+        cxxReactPackageProviders: List<(ReactContext) -> CxxReactPackage>,
+        exceptionHandler: (Exception) -> Unit,
+        bindingsInstaller: BindingsInstaller?,
+      ): ReactHost
+    """
+          ),
+  )
+  @JvmStatic
+  public fun getDefaultReactHost(
+      context: Context,
+      packageList: List<ReactPackage>,
+      jsMainModulePath: String = "index",
+      jsBundleAssetPath: String = "index",
+      jsBundleFilePath: String? = null,
+      isHermesEnabled: Boolean = true,
+      useDevSupport: Boolean = ReactBuildConfig.DEBUG,
+      cxxReactPackageProviders: List<(ReactContext) -> CxxReactPackage> = emptyList(),
+      exceptionHandler: (Exception) -> Unit = { throw it },
+      bindingsInstaller: BindingsInstaller? = null,
+  ): ReactHost =
+      getDefaultReactHost(
+          context,
+          packageList,
+          jsMainModulePath,
+          jsBundleAssetPath,
+          jsBundleFilePath,
+          HermesInstance(),
+          useDevSupport,
+          cxxReactPackageProviders,
+          exceptionHandler,
+          bindingsInstaller,
+      )
+
+  /**
+   * Util function to create a default [ReactHost] to be used in your application. This method is
+   * used by the New App template.
+   *
    * This method takes in input a [ReactNativeHost] (bridge-mode) and uses its configuration to
    * create an equivalent [ReactHost] (bridgeless-mode).
    *
