4a9da33afeac6d13b7489ac96cd5b0ccc372d25a
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-08-27 05:47:09 -0700

    Add deprecation for legacy arch APIs (#53368)
    Summary:
    
    This change introduces deprecation messages for several APIs used by the legacy arch in the RCTAppDelegate Library
    
    ## Changelog:
    [iOS][Added] - Add deprecation message for RCTAppdelegate APIs
    
    Reviewed By: cortinico
    
    Differential Revision: D80618102

--- packages/react-native/Libraries/AppDelegate/RCTArchConfiguratorProtocol.h
+++ packages/react-native/Libraries/AppDelegate/RCTArchConfiguratorProtocol.h
@@ -10,27 +10,29 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+__attribute__((deprecated(
+    "RCTArchConfiguratorProtocol is deprecated and will be removed when we remove the legacy architecture.")));
 @protocol RCTArchConfiguratorProtocol
 /// This method controls whether the `turboModules` feature of the New Architecture is turned on or off.
 ///
 /// @note: This is required to be rendering on Fabric (i.e. on the New Architecture).
 /// @return: `true` if the Turbo Native Module are enabled. Otherwise, it returns `false`.
-- (BOOL)turboModuleEnabled __attribute__((deprecated("Use RCTIsNewArchEnabled instead")));
+- (BOOL)turboModuleEnabled __attribute__((deprecated("This will be removed in a future version of React Native")));
 
 /// This method controls whether the App will use the Fabric renderer of the New Architecture or not.
 ///
 /// @return: `true` if the Fabric Renderer is enabled. Otherwise, it returns `false`.
-- (BOOL)fabricEnabled __attribute__((deprecated("Use RCTIsNewArchEnabled instead")));
+- (BOOL)fabricEnabled __attribute__((deprecated("This will be removed in a future version of React Native")));
 
 /// This method controls whether React Native's new initialization layer is enabled.
 ///
 /// @return: `true` if the new initialization layer is enabled. Otherwise returns `false`.
-- (BOOL)bridgelessEnabled __attribute__((deprecated("Use RCTIsNewArchEnabled instead")));
+- (BOOL)bridgelessEnabled __attribute__((deprecated("This will be removed in a future version of React Native")));
 
 /// This method controls whether React Native uses new Architecture.
 ///
 /// @return: `true` if the new architecture is enabled. Otherwise returns `false`.
-- (BOOL)newArchEnabled __attribute__((deprecated("Use RCTIsNewArchEnabled instead")));
+- (BOOL)newArchEnabled __attribute__((deprecated("This will be removed in a future version of React Native")));
 @end
 
 NS_ASSUME_NONNULL_END
