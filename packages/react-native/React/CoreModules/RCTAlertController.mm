dc066b1a70d3322a3e4f0ae9a69697f8adae3988
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-08-28 14:25:34 +0200

    fix: simplify alert controller on iOS

--- packages/react-native/React/CoreModules/RCTAlertController.mm
+++ packages/react-native/React/CoreModules/RCTAlertController.mm
@@ -9,49 +9,26 @@
 
 #import <React/RCTAlertController.h>
 
-@interface RCTAlertController ()
-
-@property (nonatomic, strong) UIWindow *alertWindow;
-
-@end
-
 @implementation RCTAlertController
-
-- (UIWindow *)alertWindow
-{
-  if (_alertWindow == nil) {
-    _alertWindow = [[UIWindow alloc] initWithWindowScene:RCTKeyWindow().windowScene];
-
-    if (_alertWindow) {
-      _alertWindow.rootViewController = [UIViewController new];
-      _alertWindow.windowLevel = UIWindowLevelAlert + 1;
-    }
-  }
-
-  return _alertWindow;
-}
 
 - (void)show:(BOOL)animated completion:(void (^)(void))completion
 {
+  UIWindow *keyWindow = RCTKeyWindow();
+  
   UIUserInterfaceStyle style = self.overrideUserInterfaceStyle;
   if (style == UIUserInterfaceStyleUnspecified) {
-    UIUserInterfaceStyle overriddenStyle = RCTKeyWindow().overrideUserInterfaceStyle;
+    UIUserInterfaceStyle overriddenStyle = keyWindow.overrideUserInterfaceStyle;
     style = overriddenStyle ? overriddenStyle : UIUserInterfaceStyleUnspecified;
   }
 
   self.overrideUserInterfaceStyle = style;
 
-  [self.alertWindow makeKeyAndVisible];
-  [self.alertWindow.rootViewController presentViewController:self animated:animated completion:completion];
+  [keyWindow.rootViewController presentViewController:self animated:animated completion:completion];
 }
 
 - (void)hide
 {
-  [_alertWindow setHidden:YES];
-
-  _alertWindow.windowScene = nil;
-
-  _alertWindow = nil;
+  [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
