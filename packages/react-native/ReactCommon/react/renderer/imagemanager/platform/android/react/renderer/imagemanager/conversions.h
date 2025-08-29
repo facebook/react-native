a2c6a3e24699c9ba07967b2e9502baa1a4d54e08
Christoph Purrer <chpurrer@meta.com>
2025-08-28 21:03:56 -0700

    Preparation to prefetch image requests in batch mode (#53490)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53490
    
    Changelog: [Internal]
    
    Sending individual image prefetch request on Android over JNI is causing measurable performance regression. The idea here is batch all image prefetch request for a given shadowNodeTree and then flush it all at once - **DONE** in the next change.
    
    Another optimization we should consider is to execute the batch on `n imagePrefetchRequest` off the JavaScript thread `mqt_v_js` and instead on e.g. the UiThread (on which currently Android Image UI initiates image resource downloads)
    
    Reviewed By: lenaic
    
    Differential Revision: D81186916
    
    fbshipit-source-id: f8b24e70f2ded237be96bdb973b72acbbb8b1c20

--- packages/react-native/ReactCommon/react/renderer/imagemanager/platform/android/react/renderer/imagemanager/conversions.h
+++ packages/react-native/ReactCommon/react/renderer/imagemanager/platform/android/react/renderer/imagemanager/conversions.h
@@ -7,13 +7,13 @@
 
 #pragma once
 
-#include <string>
-
 #include <react/renderer/core/graphicsConversions.h>
 #include <react/renderer/imagemanager/ImageRequestParams.h>
 #include <react/renderer/imagemanager/primitives.h>
 #include <react/renderer/mapbuffer/MapBuffer.h>
 #include <react/renderer/mapbuffer/MapBufferBuilder.h>
+#include <string>
+#include <vector>
 
 namespace facebook::react {
 
@@ -51,6 +51,8 @@ constexpr MapBuffer::Key IS_KEY_FADE_DURATION = 11;
 constexpr MapBuffer::Key IS_KEY_PROGRESSIVE_RENDERING_ENABLED = 12;
 constexpr MapBuffer::Key IS_KEY_LOADING_INDICATOR_SRC = 13;
 constexpr MapBuffer::Key IS_KEY_ANALYTIC_TAG = 14;
+constexpr MapBuffer::Key IS_KEY_SURFACE_ID = 15;
+constexpr MapBuffer::Key IS_KEY_TAG = 16;
 
 inline void serializeImageSource(
     MapBufferBuilder& builder,
@@ -91,14 +93,26 @@ inline void serializeImageRequestParams(
   builder.putString(IS_KEY_ANALYTIC_TAG, imageRequestParams.analyticTag);
 }
 
+inline MapBuffer serializeImageRequest(const ImageRequestItem& item) {
+  auto builder = MapBufferBuilder();
+  serializeImageSource(builder, item.imageSource);
+  serializeImageRequestParams(builder, item.imageRequestParams);
+  builder.putInt(IS_KEY_SURFACE_ID, item.surfaceId);
+  builder.putInt(IS_KEY_TAG, item.tag);
+  return builder.build();
+}
+
 } // namespace
 
-inline MapBuffer serializeImageRequest(
-    const ImageSource& imageSource,
-    const ImageRequestParams& imageRequestParams) {
-  auto builder = MapBufferBuilder();
-  serializeImageSource(builder, imageSource);
-  serializeImageRequestParams(builder, imageRequestParams);
+inline MapBuffer serializeImageRequests(
+    const std::vector<ImageRequestItem>& items) {
+  std::vector<MapBuffer> mapBufferList;
+  mapBufferList.reserve(items.size());
+  for (const auto& item : items) {
+    mapBufferList.emplace_back(serializeImageRequest(item));
+  }
+  MapBufferBuilder builder;
+  builder.putMapBufferList(0, mapBufferList);
   return builder.build();
 }
 
