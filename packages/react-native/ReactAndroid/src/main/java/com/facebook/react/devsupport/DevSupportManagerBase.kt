4c1012e578d4866df393f9e62921c006a595accd
Vitali Zaidman <vzaidman@meta.com>
2025-09-01 03:21:18 -0700

    fixed packager not reloading bundle on reconnect (#53370)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53370
    
    Changelog: [Android][Fixed] hot reload stops working if the app loses and then re-establishes connection to metro.
    
    Differential Revision: D80550226

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/DevSupportManagerBase.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/DevSupportManagerBase.kt
@@ -172,6 +172,7 @@ public abstract class DevSupportManagerBase(
   private var isShakeDetectorStarted = false
   private var isDevSupportEnabled = false
   private var isPackagerConnected = false
+  private var wasPackagerConnectedBefore = false
   private val errorCustomizers: MutableList<ErrorCustomizer> = mutableListOf()
   private var packagerLocationCustomizer: PackagerLocationCustomizer? = null
   private val jSExecutorDescription: String?
@@ -877,6 +878,11 @@ public abstract class DevSupportManagerBase(
           object : PackagerCommandListener {
             override fun onPackagerConnected() {
               isPackagerConnected = true
+              if (wasPackagerConnectedBefore) {
+                onPackagerReloadCommand()
+              } else {
+                wasPackagerConnectedBefore = true
+              }
               perfMonitorOverlayManager?.enable()
             }
 
