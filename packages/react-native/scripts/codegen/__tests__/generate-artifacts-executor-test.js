2a21c82e2dfbfb8a63fa69b5c6b0beeef22686b1
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-09-04 17:16:15 +0200

    remove package.swift as it is not stable

--- packages/react-native/scripts/codegen/__tests__/generate-artifacts-executor-test.js
+++ packages/react-native/scripts/codegen/__tests__/generate-artifacts-executor-test.js
@@ -54,7 +54,6 @@ const packageJson = JSON.stringify({
       'ReactCodegen/ReactCodegen.podspec',
       'ReactCodegen/RCTUnstableModulesRequiringMainQueueSetupProvider.h',
       'ReactCodegen/RCTUnstableModulesRequiringMainQueueSetupProvider.mm',
-      'Package.swift'
     ].forEach(file => {
       it(`"${file}" should match snapshot`, () => {
         const generatedFileDir = path.join(outputDir, 'build/generated/ios');
