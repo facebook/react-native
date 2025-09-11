7ffa113d8b43a89eb830764a2c9e4c91249e0222
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-09-11 09:14:17 -0700

    Add Function to deintegrate local SwiftPM packages from Xcode project (#53646)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53646
    
    ## Context
    
    Apple does not have a tool from CLI to add a package to
    a project, therefore we need to manually modify the Xcodeproject to integrate some Swift packages into it.
    
    ## Changed
    
    We are adding a function that deintegrate local Swift Packages. This helps to make sure we can have an idempotent script and that we don't add the same package twice.
    
    ## Changelog:
    [Internal] -
    
    Reviewed By: cortinico
    
    Differential Revision: D81778458

--- packages/react-native/scripts/swiftpm/xcodeproj-utils.js
+++ packages/react-native/scripts/swiftpm/xcodeproj-utils.js
@@ -41,7 +41,105 @@ function convertXcodeProjectToJSON(
   return JSON.parse(jsonOutput);
 }
 
+/**
+ * Remove all existing local SwiftPM package references and dependencies from Xcode project
+ * @param {Object} xcodeProject - The xcode project converted in JSON format
+ */
+function deintegrateSwiftPM(xcodeProject /*: XcodeProject */) /*: void */ {
+  const objects = xcodeProject.objects;
+  const objectsToRemove = [];
+
+  // Step 1: Find all PBXNativeTarget objects and clean up their SwiftPM dependencies
+  for (const objectId in objects) {
+    const object = objects[objectId];
+    if (object.isa !== 'PBXNativeTarget') continue;
+
+    // Find PBXFrameworksBuildPhase
+    for (const buildPhaseId of object.buildPhases || []) {
+      const buildPhaseObject = objects[buildPhaseId];
+      if (
+        !buildPhaseObject ||
+        buildPhaseObject.isa !== 'PBXFrameworksBuildPhase'
+      )
+        continue;
+
+      const filesToRemove /*: Array<string> */ = [];
+
+      // Check each file in the build phase
+      for (const fileId of buildPhaseObject.files || []) {
+        const buildFileObject = objects[fileId];
+        if (
+          !buildFileObject ||
+          buildFileObject.isa !== 'PBXBuildFile' ||
+          !buildFileObject.productRef
+        )
+          continue;
+
+        const productRefObject = objects[buildFileObject.productRef];
+        if (
+          !productRefObject ||
+          productRefObject.isa !== 'XCSwiftPackageProductDependency'
+        )
+          continue;
+
+        // Mark for removal: the product dependency, the build file, and remove from files list
+        objectsToRemove.push(buildFileObject.productRef);
+        objectsToRemove.push(fileId);
+        filesToRemove.push(fileId);
+      }
+
+      // Remove files from the build phase
+      if (filesToRemove.length > 0) {
+        buildPhaseObject.files = (buildPhaseObject.files || []).filter(
+          fileId => !filesToRemove.includes(fileId),
+        );
+      }
+    }
+  }
+
+  // Step 2: Find PBXProject and clean up packageReferences
+  for (const objectId in objects) {
+    const object = objects[objectId];
+    if (object.isa !== 'PBXProject') continue;
+
+    const packageReferencesToRemove = [];
+
+    // Check each package reference
+    for (const packageRefId of object.packageReferences || []) {
+      const packageRefObject = objects[packageRefId];
+      if (
+        !packageRefObject ||
+        packageRefObject.isa !== 'XCLocalSwiftPackageReference'
+      )
+        continue;
+
+      // Mark for removal
+      objectsToRemove.push(packageRefId);
+      packageReferencesToRemove.push(packageRefId);
+    }
+
+    // Remove package references from the project
+    if (packageReferencesToRemove.length > 0) {
+      object.packageReferences = (object.packageReferences || []).filter(
+        refId => !packageReferencesToRemove.includes(refId),
+      );
+    }
+
+    break;
+  }
+
+  // Step 3: Remove all marked objects
+  for (const objectId of objectsToRemove) {
+    delete objects[objectId];
+  }
+
+  console.log(
+    `✓ Removed ${objectsToRemove.length} SwiftPM-related objects from Xcode project`,
+  );
+}
+
 module.exports = {
   generateXcodeObjectId,
   convertXcodeProjectToJSON,
+  deintegrateSwiftPM,
 };
