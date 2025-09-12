97876545ac149866d9ce078c63f0e956a398adf9
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-09-12 06:35:00 -0700

    Create script to create hard links for headers from a source path to a destination path (#53620)
    Summary:
    
    ## Context
    
    One of the quirk of SwiftPM is that the packages has to have access to the headers they need. Usually this is solved by properly setting the header_search_path. However, in  SwiftPM, we are not allowed to use headers search path that escape the package itself (basically, header search path can't start with `../`).
    
    To work around this limitation we are recreating the correct Header structure by using hardlinks to the actual headers.
    
    ## Changed
    
    In this change we are adding an helper function that creates links between a source folder and a destination folder.
    
    ## Changelog:
    [Internal] -
    
    Reviewed By: cortinico
    
    Differential Revision: D81778469

---
+++ packages/react-native/scripts/swiftpm/headers-utils.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+const {listHeadersInFolder, setupSymlink} = require('./utils');
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Helper function to create symlinks from a source path
+ * @param {string} sourcePath - Source directory to search for headers
+ * @param {string} outputPath - Default output directory for symlinks
+ * @param {boolean} preserveStructure - Whether to preserve directory structure
+ * @param {Array<string>} excludeFolders - Folder names to exclude
+ * @param {Object} customMappings - Custom path mappings (prefix -> output path)
+ * @returns {number} Number of symlinks created
+ */
+function symlinkHeadersFromPath(
+  sourcePath /*: string */,
+  outputPath /*: string */,
+  preserveStructure /*: boolean */,
+  excludeFolders /*: Array<string> */,
+  customMappings /*: {[string]: string} */ = {},
+) /*: number */ {
+  let linkedCount = 0;
+
+  try {
+    // Build find command with exclusions using -prune
+    const headerFiles = listHeadersInFolder(sourcePath, excludeFolders);
+    headerFiles.forEach(sourceHeaderPath => {
+      if (fs.existsSync(sourceHeaderPath)) {
+        const relativePath = path.relative(sourcePath, sourceHeaderPath);
+        let destPath = '';
+        let mappedOutputPath = outputPath;
+
+        // Check for custom mappings first
+        for (const [prefix, customOutput] of Object.entries(customMappings)) {
+          if (relativePath.startsWith(prefix)) {
+            mappedOutputPath = customOutput;
+            console.log(`  Custom mapping: ${prefix} -> ${customOutput}`);
+            break;
+          }
+        }
+
+        if (preserveStructure) {
+          // Preserve directory structure
+          destPath = path.join(mappedOutputPath, relativePath);
+        } else {
+          // Flatten structure - just use the header filename
+          const headerName = path.basename(sourceHeaderPath);
+          destPath = path.join(mappedOutputPath, headerName);
+        }
+
+        // Create destination directory if it doesn't exist
+        setupSymlink(sourceHeaderPath, destPath);
+        linkedCount++;
+      }
+    });
+  } catch (error) {
+    console.warn(
+      `Failed to process headers from ${sourcePath}:`,
+      error.message,
+    );
+  }
+
+  return linkedCount;
+}
+
+module.exports = {
+  symlinkHeadersFromPath,
+};
