14458d8699f49fec8ff7ad38c541da61b1ef491e
Devan Buggay <devanb@meta.com>
2025-09-18 00:36:55 -0700

    Add focus/blur commands to View (#53828)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53828
    
    Adds focus and blur commands to Android views.
    
    Changelog: [Internal]
    
    Reviewed By: rozele
    
    Differential Revision: D82644681
    
    fbshipit-source-id: 96fa6370e177c91742d285b0b158b77447d105f6

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewManager.kt
@@ -404,6 +404,8 @@ public open class ReactViewManager : ReactClippingViewManager<ReactViewGroup>() 
     when (commandId) {
       HOTSPOT_UPDATE_KEY -> handleHotspotUpdate(root, args)
       "setPressed" -> handleSetPressed(root, args)
+      "focus" -> handleFocus(root)
+      "blur" -> handleBlur(root)
       else -> {}
     }
   }
@@ -427,5 +429,17 @@ public open class ReactViewManager : ReactClippingViewManager<ReactViewGroup>() 
     val x = args.getDouble(0).dpToPx()
     val y = args.getDouble(1).dpToPx()
     root.drawableHotspotChanged(x, y)
+  }
+
+  private fun handleFocus(root: ReactViewGroup) {
+    if (ReactNativeFeatureFlags.enableImperativeFocus()) {
+      root.requestFocusFromJS()
+    }
+  }
+
+  private fun handleBlur(root: ReactViewGroup) {
+    if (ReactNativeFeatureFlags.enableImperativeFocus()) {
+      root.clearFocusFromJS()
+    }
   }
 }
