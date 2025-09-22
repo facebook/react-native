8dddde49b2f3170afceedf988ce05d1dae895d81
Nicola Corti <ncor@meta.com>
2025-09-22 14:24:33 +0100

    [0.82] Ensure legacy architecture can't be initialized (#53843)
    * Ensure legacy architecture can't be initialized in Android (#53806)
    
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53806
    
    The Legacy architecture of React Native is not supported anymore, let's ensure nobody can initialize it anymore
    
    changelog: [internal] internal
    
    Reviewed By: cortinico
    
    Differential Revision: D82465004
    
    fbshipit-source-id: 099764fb59d906b11cf6ad0cc6208f56df029d19
    
    * Runtime check that NewArchitecture is enabled in DefaultNewArchitectureEntryPoint (#53780)
    
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53780
    
    This is a commit we're going to pick in 0.82 as we want to make sure users cannot invoke `load()`
    from `DefaultNewArchitectureEntryPoint` with flags that are not true,true,true.
    
    Changelog:
    [Android] [Changed] - Runtime check that NewArchitecture is enabled in DefaultNewArchitectureEntryPoint
    
    Reviewed By: mdvacca
    
    Differential Revision: D82456975
    
    fbshipit-source-id: 749996a3491913cfe400173608218077c3ffbc10
    
    * Fix DefaultNewArchitectureEntryPoint config validation (#53807)
    
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53807
    
    We need to read the values off feature flags before checking them.
    
    ## Changelog
    [Internal]
    
    Reviewed By: javache
    
    Differential Revision: D82572898
    
    fbshipit-source-id: 09fbc09570c78b41d9c25fb03c5557e390da7cae
    
    * Fix incorrect validation of feature flags in DefaultNewArchitectureEntryPoint.loadWithFeatureFlags() method (#53863)
    
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53863
    
    In this diff I'm fixing an incorrect validation of feature flags in DefaultNewArchitectureEntryPoint.loadWithFeatureFlags() method.
    
    changelog: [internal] internal
    
    Reviewed By: cortinico
    
    Differential Revision: D82841006
    
    fbshipit-source-id: 6f50f2475255d7b841d9224bc8808119dcc68aec
    
    ---------
    
    Co-authored-by: David Vacca <dvacca@meta.com>
    Co-authored-by: Eric Rozell <ericroz@meta.com>

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultNewArchitectureEntryPoint.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/defaults/DefaultNewArchitectureEntryPoint.kt
@@ -125,6 +125,16 @@ public object DefaultNewArchitectureEntryPoint {
     privateConcurrentReactEnabled = featureFlags.enableFabricRenderer()
     privateBridgelessEnabled = featureFlags.enableBridgelessArchitecture()
 
+    val (isValid, errorMessage) =
+        isConfigurationValid(
+            privateTurboModulesEnabled,
+            privateFabricEnabled,
+            privateBridgelessEnabled,
+        )
+    if (!isValid) {
+      error(errorMessage)
+    }
+
     DefaultSoLoader.maybeLoadSoLibrary()
   }
 
@@ -158,13 +168,13 @@ public object DefaultNewArchitectureEntryPoint {
       fabricEnabled: Boolean,
       bridgelessEnabled: Boolean,
   ): Pair<Boolean, String> =
-      when {
-        fabricEnabled && !turboModulesEnabled ->
-            false to
-                "fabricEnabled=true requires turboModulesEnabled=true (is now false) - Please update your DefaultNewArchitectureEntryPoint.load() parameters."
-        bridgelessEnabled && (!turboModulesEnabled || !fabricEnabled) ->
-            false to
-                "bridgelessEnabled=true requires (turboModulesEnabled=true AND fabricEnabled=true) - Please update your DefaultNewArchitectureEntryPoint.load() parameters."
-        else -> true to ""
+      if (!turboModulesEnabled || !fabricEnabled || !bridgelessEnabled) {
+        false to
+            "You cannot load React Native with the New Architecture disabled. " +
+                "Please use DefaultNewArchitectureEntryPoint.load() instead of " +
+                "DefaultNewArchitectureEntryPoint.load(turboModulesEnabled=$turboModulesEnabled, " +
+                "fabricEnabled=$fabricEnabled, bridgelessEnabled=$bridgelessEnabled)"
+      } else {
+        true to ""
       }
 }
