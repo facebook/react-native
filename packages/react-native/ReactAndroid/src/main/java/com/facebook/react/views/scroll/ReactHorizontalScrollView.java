ed048eb112f8122af47808c4f3843e0b07b1f8ed
Calix Tang <calixtang@meta.com>
2025-09-23 13:13:38 -0700

    VirtualView v2 support in HorizontalScrollView native component (Android) (#53890)
    Summary:
    
    
    Changelog: [Internal] -  Adds support for VirtualView v2 in Android HorizontalScrollView native component by implementing VirtualViewContainer interface. Only on Android because iOS equivalent native components are already set up to support virtualview v2 on horizontal scrollviews.
    
    ## Changes in Detail
    
    Adds necessary changes to Android `HorizontalScrollView` native component to support the experimental version of VirtualView.
    
    Reviewed By: lunaleaps
    
    Differential Revision: D82783403

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollView.java
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactHorizontalScrollView.java
@@ -79,7 +79,8 @@ public class ReactHorizontalScrollView extends HorizontalScrollView
         HasStateWrapper,
         HasFlingAnimator,
         HasScrollEventThrottle,
-        HasSmoothScroll {
+        HasSmoothScroll,
+        VirtualViewContainer {
 
   private static boolean DEBUG_MODE = false && ReactBuildConfig.DEBUG;
   private static String TAG = ReactHorizontalScrollView.class.getSimpleName();
@@ -100,6 +101,7 @@ public class ReactHorizontalScrollView extends HorizontalScrollView
   private final ValueAnimator DEFAULT_FLING_ANIMATOR = ObjectAnimator.ofInt(this, "scrollX", 0, 0);
 
   private Rect mOverflowInset = new Rect();
+  private @Nullable VirtualViewContainerState mVirtualViewContainerState;
   private boolean mActivelyScrolling;
   private @Nullable Rect mClippingRect;
   private Overflow mOverflow = Overflow.SCROLL;
@@ -156,6 +158,7 @@ public class ReactHorizontalScrollView extends HorizontalScrollView
    */
   private void initView() {
     mOverflowInset = new Rect();
+    mVirtualViewContainerState = null;
     mActivelyScrolling = false;
     mClippingRect = null;
     // The default value for `overflow` is set to `Visible` in the Yoga style props.
@@ -208,6 +211,15 @@ public class ReactHorizontalScrollView extends HorizontalScrollView
   }
 
   private void updateView() {}
+
+  @Override
+  public VirtualViewContainerState getVirtualViewContainerState() {
+    if (mVirtualViewContainerState == null) {
+      mVirtualViewContainerState = new VirtualViewContainerState(this);
+    }
+
+    return mVirtualViewContainerState;
+  }
 
   @Override
   public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info) {
@@ -512,6 +524,9 @@ public class ReactHorizontalScrollView extends HorizontalScrollView
     }
 
     ReactScrollViewHelper.emitLayoutEvent(this);
+    if (mVirtualViewContainerState != null) {
+      mVirtualViewContainerState.updateState();
+    }
   }
 
   /**
@@ -605,7 +620,11 @@ public class ReactHorizontalScrollView extends HorizontalScrollView
             this,
             mOnScrollDispatchHelper.getXFlingVelocity(),
             mOnScrollDispatchHelper.getYFlingVelocity());
+        if (mVirtualViewContainerState != null) {
+          mVirtualViewContainerState.updateState();
+        }
       }
+
     } finally {
       Systrace.endSection(Systrace.TRACE_TAG_REACT);
     }
@@ -856,6 +875,9 @@ public class ReactHorizontalScrollView extends HorizontalScrollView
     if (mRemoveClippedSubviews) {
       updateClippingRect();
     }
+    if (mVirtualViewContainerState != null) {
+      mVirtualViewContainerState.updateState();
+    }
   }
 
   @Override
@@ -874,6 +896,9 @@ public class ReactHorizontalScrollView extends HorizontalScrollView
     super.onDetachedFromWindow();
     if (mMaintainVisibleContentPositionHelper != null) {
       mMaintainVisibleContentPositionHelper.stop();
+    }
+    if (mVirtualViewContainerState != null) {
+      mVirtualViewContainerState.cleanup();
     }
   }
 
