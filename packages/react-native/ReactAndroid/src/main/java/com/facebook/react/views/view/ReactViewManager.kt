09441d8691f1e0423002a90321f248028e1e52c3
Pieter De Baets <pieterdb@meta.com>
2025-09-25 10:04:48 -0700

    Fix ReactViewManager subclasses triggering view recycling (#53933)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/53933
    
    Changelog: [Internal]
    
    Reviewed By: lenaic
    
    Differential Revision: D83238077
    
    fbshipit-source-id: d3dc4f96b9934fcbcd9a80354f976d5eb0bc9f0c

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactViewManager.kt
@@ -65,7 +65,10 @@ public open class ReactViewManager : ReactClippingViewManager<ReactViewGroup>() 
   }
 
   init {
-    if (ReactNativeFeatureFlags.enableViewRecyclingForView()) {
+    if (
+        ReactNativeFeatureFlags.enableViewRecyclingForView() &&
+            this.javaClass == ReactViewManager::class.java
+    ) {
       setupViewRecycling()
     }
   }
