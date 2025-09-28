54bfd5c98e5b0dcd6f633d5947bcea90209a252e
Nick Lefever <lefever@meta.com>
2025-09-28 13:15:57 -0700

    Add support for global view culling outset ratio setting (#53845)
    Summary:
    
    Adding a feature flag controlling the outset ratio set on the culling context frame. This feature flag value will add an outset on the width and height calculated by multiplying these with the provided ratio value. This will offset each side of the frame, maintaining the center of the frame at the same location.
    
    This outset ratio helps mitigate async scroll blanking and focus handling on host platforms.
    
    Changelog: [Internal]
    
    Reviewed By: mdvacca
    
    Differential Revision: D82684027

--- packages/react-native/ReactCommon/react/featureflags/ReactNativeFeatureFlagsProvider.h
+++ packages/react-native/ReactCommon/react/featureflags/ReactNativeFeatureFlagsProvider.h
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<4d141c52bc66225656194eeb8786f475>>
+ * @generated SignedSource<<fde4b531c2b0f19bef7bc69aaf3c5639>>
  */
 
 /**
@@ -98,6 +98,7 @@ class ReactNativeFeatureFlagsProvider {
   virtual bool useShadowNodeStateOnClone() = 0;
   virtual bool useTurboModuleInterop() = 0;
   virtual bool useTurboModules() = 0;
+  virtual double viewCullingOutsetRatio() = 0;
   virtual double virtualViewHysteresisRatio() = 0;
   virtual double virtualViewPrerenderRatio() = 0;
 };
