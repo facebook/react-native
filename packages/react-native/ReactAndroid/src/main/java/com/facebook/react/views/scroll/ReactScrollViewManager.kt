a137cebd36943cdb85688c4e0c398a20f492c914
Nick Lefever <lefever@meta.com>
2025-10-02 18:58:55 -0700

    Restore scroll state of scroll view on mount (#54000)
    Summary:
    
    Update the scroll view manager to load the fabric state and restore the scroll position and scroll away top padding on mount. Restoring the scroll view happens in the react scroll view state setter, which gets called by the `ReactScrollViewHelper` with the deserialized state provided on mount.
    
    The state loading will happen only until the scroll view itself submits a new state to Fabric (due to scrolling). This guarantees that we only restore the initial state on mount.
    
    This diff also updates the scroll away top padding setter to support setting a new value without triggering a fabric state update.
    
    Changelog: [Internal]
    
    Reviewed By: rshest
    
    Differential Revision: D83247016

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollViewManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollViewManager.kt
@@ -384,6 +384,9 @@ constructor(private val fpsListener: FpsListener? = null) :
       stateWrapper: StateWrapper,
   ): Any? {
     view.setStateWrapper(stateWrapper)
+    if (ReactNativeFeatureFlags.enableViewCulling()) {
+      ReactScrollViewHelper.loadFabricScrollState(view, stateWrapper)
+    }
     return null
   }
 
