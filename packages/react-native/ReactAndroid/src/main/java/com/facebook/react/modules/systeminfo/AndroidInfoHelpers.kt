e4da3437d93fcff883dbae2e37336f2eab377fd2
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-10-09 14:41:55 -0700

    xplat/js/react-native-github/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/AndroidInfoHelpers.kt (#54106)
    Summary: Pull Request resolved: https://github.com/facebook/react-native/pull/54106
    
    Reviewed By: javache
    
    Differential Revision: D84130472
    
    fbshipit-source-id: a1bfff89e0b6d36d2bcb3045a35ca15c6599f9d8

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/AndroidInfoHelpers.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/AndroidInfoHelpers.kt
@@ -85,7 +85,7 @@ public object AndroidInfoHelpers {
   private fun getReactNativeVersionString(): String {
     val version = ReactNativeVersion.VERSION
     return "${version["major"]}.${version["minor"]}.${version["patch"]}" +
-        (version["prerelease"]?.let { "-$it" } ?: "")
+        (version["prerelease"]?.let { "-$it" }.orEmpty())
   }
 
   private fun getDevServerPort(context: Context): Int =
@@ -123,7 +123,7 @@ public object AndroidInfoHelpers {
       var lastLine = ""
       var line: String?
       while (reader.readLine().also { line = it } != null) {
-        lastLine = line ?: ""
+        lastLine = line.orEmpty()
       }
       metroHostPropValue = lastLine
     } catch (e: Exception) {
@@ -133,6 +133,6 @@ public object AndroidInfoHelpers {
       reader?.close()
       process?.destroy()
     }
-    return metroHostPropValue ?: ""
+    return metroHostPropValue.orEmpty()
   }
 }
