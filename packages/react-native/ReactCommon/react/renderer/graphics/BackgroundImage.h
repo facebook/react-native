4d9a881e09eacfd3df37eadeb4f96f5997ca5785
Nick Lefever <lefever@meta.com>
2025-10-10 10:20:25 -0700

    Fix binary size regression for BackgroundImage
    Summary:
    The binary size for RN builds increased by ~180KiB due to the implementation of the `experimental_backgroundImage` property diffing.
    
    This diff removes default inlining for the `toDynamic` serialization functions of the structs used by the linear and radial gradient styles.
    
    Combined with the binary size improvements done previously for `ColorStop` and `ValueUnit`, the binary size has been reduced by 176.0KiB
    
    Changelog: [Internal]
    
    Differential Revision: D84214151

--- packages/react-native/ReactCommon/react/renderer/graphics/BackgroundImage.h
+++ packages/react-native/ReactCommon/react/renderer/graphics/BackgroundImage.h
@@ -10,21 +10,13 @@
 #include <react/renderer/graphics/ColorComponents.h>
 #include <react/renderer/graphics/LinearGradient.h>
 #include <react/renderer/graphics/RadialGradient.h>
-#include <vector>
 
 namespace facebook::react {
 
 using BackgroundImage = std::variant<LinearGradient, RadialGradient>;
 
 #ifdef RN_SERIALIZABLE_STATE
-inline folly::dynamic toDynamic(const BackgroundImage& backgroundImage) {
-  if (std::holds_alternative<LinearGradient>(backgroundImage)) {
-    return std::get<LinearGradient>(backgroundImage).toDynamic();
-  } else if (std::holds_alternative<RadialGradient>(backgroundImage)) {
-    return std::get<RadialGradient>(backgroundImage).toDynamic();
-  }
-  return folly::dynamic(nullptr);
-}
+folly::dynamic toDynamic(const BackgroundImage& backgroundImage);
 #endif
 
 }; // namespace facebook::react
--- packages/react-native/ReactCommon/react/renderer/graphics/LinearGradient.h
+++ packages/react-native/ReactCommon/react/renderer/graphics/LinearGradient.h
@@ -10,6 +10,7 @@
 #include <react/renderer/graphics/ColorStop.h>
 #include <react/renderer/graphics/Float.h>
 #include <react/renderer/graphics/ValueUnit.h>
+#include <stdexcept>
 #include <string>
 #include <variant>
 #include <vector>
@@ -18,39 +19,12 @@ namespace facebook::react {
 
 enum class GradientDirectionType { Angle, Keyword };
 
-inline std::string toString(
-    const GradientDirectionType& gradientDirectionType) {
-  switch (gradientDirectionType) {
-    case GradientDirectionType::Angle:
-      return "angle";
-    case GradientDirectionType::Keyword:
-      return "keyword";
-  }
-
-  return "";
-}
-
 enum class GradientKeyword {
   ToTopRight,
   ToBottomRight,
   ToTopLeft,
   ToBottomLeft,
 };
-
-inline std::string toString(const GradientKeyword& gradientKeyword) {
-  switch (gradientKeyword) {
-    case GradientKeyword::ToTopRight:
-      return "to top right";
-    case GradientKeyword::ToBottomRight:
-      return "to bottom right";
-    case GradientKeyword::ToTopLeft:
-      return "to top left";
-    case GradientKeyword::ToBottomLeft:
-      return "to bottom left";
-  }
-
-  return "";
-}
 
 struct GradientDirection {
   GradientDirectionType type;
@@ -61,17 +35,7 @@ struct GradientDirection {
   }
 
 #ifdef RN_SERIALIZABLE_STATE
-  folly::dynamic toDynamic() const {
-    folly::dynamic result = folly::dynamic::object();
-    result["type"] = toString(type);
-
-    if (std::holds_alternative<Float>(value)) {
-      result["value"] = std::get<Float>(value);
-    } else if (std::holds_alternative<GradientKeyword>(value)) {
-      result["value"] = toString(std::get<GradientKeyword>(value));
-    }
-    return result;
-  }
+  folly::dynamic toDynamic() const;
 #endif
 };
 
@@ -84,18 +48,7 @@ struct LinearGradient {
   }
 
 #ifdef RN_SERIALIZABLE_STATE
-  folly::dynamic toDynamic() const {
-    folly::dynamic result = folly::dynamic::object();
-    result["type"] = "linear-gradient";
-    result["direction"] = direction.toDynamic();
-
-    folly::dynamic colorStopsArray = folly::dynamic::array();
-    for (const auto& colorStop : colorStops) {
-      colorStopsArray.push_back(colorStop.toDynamic());
-    }
-    result["colorStops"] = colorStopsArray;
-    return result;
-  }
+  folly::dynamic toDynamic() const;
 #endif
 };
 
