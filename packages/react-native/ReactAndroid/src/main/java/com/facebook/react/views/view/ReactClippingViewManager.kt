c41d453d4f0cc2a3b8e68e585aae97fb8e235b9c
Hanno J. Gödecke <hanno@margelo.com>
2025-10-13 18:44:49 +0200

    fix(android): fix for addView with views in view transition (#109)

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactClippingViewManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/view/ReactClippingViewManager.kt
@@ -9,10 +9,14 @@ package com.facebook.react.views.view
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.doOnDetach
+import com.facebook.common.logging.FLog
 import com.facebook.react.bridge.UiThreadUtil
 import com.facebook.react.uimanager.ReactClippingViewGroupHelper
 import com.facebook.react.uimanager.ViewGroupManager
 import com.facebook.react.uimanager.annotations.ReactProp
+import java.util.HashMap
+import java.util.WeakHashMap
 
 /**
  * View manager which handles clipped subviews. Useful for custom views which extends from
@@ -27,14 +31,36 @@ public abstract class ReactClippingViewManager<T : ReactViewGroup> : ViewGroupMa
     view.removeClippedSubviews = removeClippedSubviews
   }
 
+  // parent: childIndex[] - Used when we can't immediately add a view
+  private val operationsMap = WeakHashMap<T, MutableMap<Int, Boolean>>()
+
   override fun addView(parent: T, child: View, index: Int) {
     UiThreadUtil.assertOnUiThread()
 
     if (child.parent != null) {
-      (child.parent as? ViewGroup)?.removeView(child)
-      child.post {
-        addViewInternal(parent, child, index)
+      operationsMap.getOrPut(parent) {
+        mutableMapOf()
+      }[index] = true
+
+      // When the child-parent relation is removed, onDetachedFromWindow will be called.
+      // Its important to wait for detaching as the view might be in a transition, and isn't removed immediately.
+      child.doOnDetach {
+        // Looking at how endViewTransition is implemented, dispatchDetachedFromWindow
+        // gets called _before_ the parent relation is removed, so we need to post this to the end of the frame:
+        child.post {
+          if(operationsMap.remove(parent) == null) {
+            // The addView operation was already countered by a removeView operation while we were waiting
+            FLog.w("ReactClippingViewManager", "Tried to add a view to a parent after the child was detached, but a remove operation was already enqueued")
+            return@post
+          }
+          FLog.w("ReactClippingViewManager", "addView(): ${child::class.java.simpleName} had a parent, removed from previous parent and after onDetach adding to new parent $parent")
+          addViewInternal(parent, child, index)
+        }
       }
+
+      // With the detach listener in place, we can now remove the view from the previous parent:
+      // Note: This call here is potentially redundant, as SurfaceMountingManager.kt is already removing it
+      (child.parent as? ViewGroup)?.removeView(child)
     } else {
       addViewInternal(parent, child, index)
     }
@@ -79,6 +105,7 @@ public abstract class ReactClippingViewManager<T : ReactViewGroup> : ViewGroupMa
     } else {
       parent.removeViewAt(index)
     }
+    operationsMap[parent]?.remove(index)
   }
 
   override fun removeAllViews(parent: T) {
@@ -89,6 +116,7 @@ public abstract class ReactClippingViewManager<T : ReactViewGroup> : ViewGroupMa
       parent.removeAllViewsWithSubviewClippingEnabled()
     } else {
       parent.removeAllViews()
+      operationsMap.remove(parent)
     }
   }
 }
