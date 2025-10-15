094794ac929ce15c111afaa2bc9fa748cefa1e59
Alex Hunt <huntie@meta.com>
2025-10-14 17:51:13 -0700

    Add performance marker for Fast Refresh update (#54154)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/54154
    
    Adds a prominent vertical "Fast Refresh ⚛︎" marker entry in the performance timeline when a Fast Refresh update is complete.
    
    This is available in apps with `performance.mark()` enabled (Canary feature).
    
    Changelog: [Internal]
    
    Reviewed By: hoxyq
    
    Differential Revision: D84624705
    
    fbshipit-source-id: 6c0ae0b8ae0acaaaba559a1854f64218bf5feabb

--- packages/react-native/Libraries/Utilities/HMRClient.js
+++ packages/react-native/Libraries/Utilities/HMRClient.js
@@ -233,6 +233,15 @@ Error: ${e.message}`;
       pendingUpdatesCount--;
       if (pendingUpdatesCount === 0) {
         DevLoadingView.hide();
+        performance.mark('Fast Refresh - Update done', {
+          detail: {
+            devtools: {
+              dataType: 'marker',
+              color: 'primary',
+              tooltipText: 'Fast Refresh \u269b',
+            },
+          },
+        });
       }
     });
 
