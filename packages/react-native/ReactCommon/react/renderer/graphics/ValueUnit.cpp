486236794c9b710da2255fb716a01226d11ea22d
Nick Lefever <lefever@meta.com>
2025-10-16 11:46:41 -0700

    Extract ValueUnit percent formatting to separate file (#54170)
    Summary:
    
    This diff extracts the `toString(double, char)` function used to format percent values from `ValueUnit` into a separate `DoubleConversions` file.
    
    This was added to graphics directly instead of moving it to core to avoid introducting cyclic dependencies since core depends on graphics.
    
    Changelog: [Internal]
    
    Reviewed By: mdvacca
    
    Differential Revision: D84714535

--- packages/react-native/ReactCommon/react/renderer/graphics/ValueUnit.cpp
+++ packages/react-native/ReactCommon/react/renderer/graphics/ValueUnit.cpp
@@ -8,36 +8,12 @@
 #include "ValueUnit.h"
 
 #ifdef RN_SERIALIZABLE_STATE
-#include <double-conversion/double-conversion.h>
-#include <array>
-#include <string>
+#include "DoubleConversions.h"
 #endif
 
 namespace facebook::react {
 
 #ifdef RN_SERIALIZABLE_STATE
-
-std::string toString(double doubleValue, char suffix) {
-  // Format taken from folly's toString
-  static double_conversion::DoubleToStringConverter conv(
-      0,
-      NULL,
-      NULL,
-      'E',
-      -6, // detail::kConvMaxDecimalInShortestLow,
-      21, // detail::kConvMaxDecimalInShortestHigh,
-      6, // max leading padding zeros
-      1); // max trailing padding zeros
-  std::array<char, 256> buffer{};
-  double_conversion::StringBuilder builder(buffer.data(), buffer.size());
-  if (!conv.ToShortest(doubleValue, &builder)) {
-    // Serialize infinite and NaN as 0%
-    builder.AddCharacter('0');
-    builder.AddCharacter('%');
-  }
-  builder.AddCharacter(suffix);
-  return builder.Finalize();
-}
 
 folly::dynamic ValueUnit::toDynamic() const {
   switch (unit) {
