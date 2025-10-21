6d735ba11d071f35d9a55e830d89aaa014e3ba20
Hanno J. Gödecke <die.drei99@yahoo.de>
2025-10-21 10:28:44 +0200

    remove logging

--- packages/react-native/src/private/webapis/dom/nodes/ReactNativeElement.js
+++ packages/react-native/src/private/webapis/dom/nodes/ReactNativeElement.js
@@ -167,7 +167,6 @@ class ReactNativeElement extends ReadOnlyElement implements NativeMethods {
   measureAsyncOnUI(callback: MeasureOnSuccessCallback) {
     const node = getNativeElementReference(this);
     if (node != null) {
-      console.log("ReactNativeElement node.measureAsyncOnUI()");
       // TODO: this is only available on native mobile platforms - when using web, do we need to fallback to measure()?
       NativeDOM.measureAsyncOnUI(node, callback);
     }
