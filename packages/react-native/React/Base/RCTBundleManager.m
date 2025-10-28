fcecf72b5d8e1401a2cd5196fb45b37e435d57b0
Dawid Malecki <dawid.malecki21@gmail.com>
2025-10-28 03:07:29 -0700

    iOS: Add new `RCTCustomBundleConfiguration` for modifying bundle URL (#54006)
    Summary:
    ## Summary:
    
    Following the [RFC](https://github.com/react-native-community/discussions-and-proposals/pull/933), this PR introduces a new `RCTBundleConfiguration` interface for modifying the bundle URL and exposes a new API for setting its instance in the `RCTReactNativeFactory`. The configuration object includes:
    
    - bundleFilePath - the URL of the bundle to load from the file system,
    - packagerServerScheme - the server scheme (e.g. http or https) to use when loading from the packager,
    - packagerServerHost - the server host (e.g. localhost) to use when loading from the packager.
    
    The `RCTBundleConfiguration` allows only for either `bundleFilePath` or `(packagerServerScheme, packagerServerHost)` to be set by defining appropriate initializers. 
    
    The logic for creating bundle URL query items is extracted to a separate `createJSBundleURLQuery` method and is used by `RCTBundleManager` to set the configured `packagerServerHost` and `packagerServerScheme`. If the configuration is not defined, the `getBundleURL` method returns the result of the passed `fallbackURLProvider`.
    
    The `bundleFilePath` should be created with `[NSURL fileURLWithPath:<path>]`, as otherwise the HMR client is created and fails ungracefully. The check is added in the `getBundle` method to log the error beforehand:
    
    <img width="306" height="822" alt="Simulator Screenshot - iPhone 16 Pro - 2025-10-15 at 17 09 58" src="https://github.com/user-attachments/assets/869eed16-c5d8-4204-81d7-bd9cd42b2223" />
    
    When the `bundleFilePath` is set in the `RCTBundleConfiguration` the `Connect to Metro...` message shouldn't be suggested.
    
    ## Changelog:
    
    [IOS][ADDED] - Add new `RCTBundleConfiguration` for modifying bundle URL on `RCTReactNativeFactory`.
    
    
    Test Plan: Test plan included in the last diff in the stack.
    
    Differential Revision: D84058022
    
    Pulled By: coado

--- packages/react-native/React/Base/RCTBundleManager.m
+++ packages/react-native/React/Base/RCTBundleManager.m
@@ -6,9 +6,99 @@
  */
 
 #import "RCTBundleManager.h"
+#import <React/RCTBundleURLProvider.h>
 #import "RCTAssert.h"
 #import "RCTBridge+Private.h"
 #import "RCTBridge.h"
+#import "RCTLog.h"
+
+@implementation RCTBundleConfiguration
+
++ (instancetype)defaultConfiguration
+{
+  return [[self alloc] initWithBundleFilePath:nil packagerServerScheme:nil packagerServerHost:nil bundlePath:nil];
+}
+
+- (instancetype)initWithBundleFilePath:(NSURL *)bundleFilePath
+{
+  return [self initWithBundleFilePath:bundleFilePath packagerServerScheme:nil packagerServerHost:nil bundlePath:nil];
+}
+
+- (instancetype)initWithPackagerServerScheme:(NSString *)packagerServerScheme
+                          packagerServerHost:(NSString *)packagerServerHost
+                                  bundlePath:(NSString *)bundlePath
+{
+  return [self initWithBundleFilePath:nil
+                 packagerServerScheme:packagerServerScheme
+                   packagerServerHost:packagerServerHost
+                           bundlePath:bundlePath];
+}
+
+- (instancetype)initWithBundleFilePath:(NSURL *)bundleFilePath
+                  packagerServerScheme:(NSString *)packagerServerScheme
+                    packagerServerHost:(NSString *)packagerServerHost
+                            bundlePath:(NSString *)bundlePath
+{
+  if (self = [super init]) {
+    _bundleFilePath = bundleFilePath;
+    _packagerServerScheme = packagerServerScheme;
+    _packagerServerHost = packagerServerHost;
+    _bundlePath = bundlePath;
+    _packagerOptionsUpdater = ^NSMutableArray<NSURLQueryItem *> *(NSMutableArray<NSURLQueryItem *> *options)
+    {
+      return options;
+    };
+  }
+
+  return self;
+}
+
+- (NSString *)getPackagerServerScheme
+{
+  if (!_packagerServerScheme) {
+    return [[RCTBundleURLProvider sharedSettings] packagerScheme];
+  }
+
+  return _packagerServerScheme;
+}
+
+- (NSString *)getPackagerServerHost
+{
+  if (!_packagerServerHost) {
+    return [[RCTBundleURLProvider sharedSettings] packagerServerHostPort];
+  }
+
+  return _packagerServerHost;
+}
+
+- (NSURL *)getBundleURL:(NSURL * (^)(void))fallbackURLProvider
+{
+  if (_packagerServerScheme && _packagerServerHost) {
+    NSArray<NSURLQueryItem *> *jsBundleURLQuery =
+        [[RCTBundleURLProvider sharedSettings] createJSBundleURLQuery:_packagerServerHost
+                                                       packagerScheme:_packagerServerScheme];
+
+    NSArray<NSURLQueryItem *> *updatedBundleURLQuery = _packagerOptionsUpdater((NSMutableArray *)jsBundleURLQuery);
+    NSString *path = [NSString stringWithFormat:@"/%@.bundle", _bundlePath];
+    return [[RCTBundleURLProvider class] resourceURLForResourcePath:path
+                                                       packagerHost:_packagerServerHost
+                                                             scheme:_packagerServerScheme
+                                                         queryItems:updatedBundleURLQuery];
+  }
+
+  if (_bundleFilePath) {
+    if (!_bundleFilePath.fileURL) {
+      RCTLogError(@"Bundle file path must be a file URL");
+      return nil;
+    }
+
+    return _bundleFilePath;
+  }
+
+  return fallbackURLProvider();
+}
+
+@end
 
 @implementation RCTBundleManager {
 #ifndef RCT_REMOVE_LEGACY_ARCH
@@ -17,6 +107,20 @@
   RCTBridgelessBundleURLGetter _bridgelessBundleURLGetter;
   RCTBridgelessBundleURLSetter _bridgelessBundleURLSetter;
   RCTBridgelessBundleURLGetter _bridgelessBundleURLDefaultGetter;
+}
+
+- (instancetype)initWithBundleConfig:(RCTBundleConfiguration *)bundleConfig
+{
+  if (self = [super init]) {
+    self.bundleConfig = bundleConfig ? bundleConfig : [RCTBundleConfiguration defaultConfiguration];
+  }
+
+  return self;
+}
+
+- (instancetype)init
+{
+  return [self initWithBundleConfig:[RCTBundleConfiguration defaultConfiguration]];
 }
 
 #ifndef RCT_REMOVE_LEGACY_ARCH
