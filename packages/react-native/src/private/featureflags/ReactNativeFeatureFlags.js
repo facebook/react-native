beccee21649eb0353e2828c65f0045053f04c6db
Rubén Norte <rubennorte@meta.com>
2025-10-30 08:34:57 -0700

    Promote Web Performance APIs to stable (#54324)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/54324
    
    Changelog: [General][Added] - Enable Web Performance APIs
    
    This promotes the Web Performance APIs from canary to stable.
    
    Reviewed By: cipolleschi
    
    Differential Revision: D85783108
    
    fbshipit-source-id: 8d8b4565f28dc20d63e677c01366f4f37d5c46e4

--- packages/react-native/src/private/featureflags/ReactNativeFeatureFlags.js
+++ packages/react-native/src/private/featureflags/ReactNativeFeatureFlags.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<25169f72feeaa596faaeb8fc3fffac06>>
+ * @generated SignedSource<<7edda733c47c5e6db0287b8b6325cdfb>>
  * @flow strict
  * @noformat
  */
@@ -425,7 +425,7 @@ export const enableVirtualViewWindowFocusDetection: Getter<boolean> = createNati
 /**
  * Enable Web Performance APIs (Performance Timeline, User Timings, etc.) by default.
  */
-export const enableWebPerformanceAPIsByDefault: Getter<boolean> = createNativeFlagGetter('enableWebPerformanceAPIsByDefault', false);
+export const enableWebPerformanceAPIsByDefault: Getter<boolean> = createNativeFlagGetter('enableWebPerformanceAPIsByDefault', true);
 /**
  * Uses the default event priority instead of the discreet event priority by default when dispatching events from Fabric to React.
  */
