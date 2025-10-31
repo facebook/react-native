b8f9d982f51de4a3fa296166dc5e77352356f48d
David Vacca <dvacca@meta.com>
2025-10-31 14:42:30 -0700

    Add KDoc documentation to ComponentNameResolverBinding (#54349)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/54349
    
    Added comprehensive KDoc documentation to the ComponentNameResolverBinding object and its JNI method for installing component name resolvers into the JavaScript runtime.
    
    changelog: [internal] internal
    
    Reviewed By: lenaic
    
    Differential Revision: D85926888
    
    fbshipit-source-id: 253577d4c5d48bee22acb5666680becd4e632879

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ComponentNameResolverBinding.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/uimanager/ComponentNameResolverBinding.kt
@@ -12,11 +12,23 @@ import com.facebook.react.bridge.RuntimeExecutor
 import com.facebook.soloader.SoLoader
 import kotlin.jvm.JvmStatic
 
+/**
+ * JNI binding for installing a ComponentNameResolver into the JavaScript runtime.
+ *
+ * This object provides a native binding to install a [ComponentNameResolver] into the React Native
+ * JavaScript runtime, making component name information available to the native side.
+ */
 @DoNotStripAny
 internal object ComponentNameResolverBinding {
   init {
     SoLoader.loadLibrary("uimanagerjni")
   }
 
+  /**
+   * Installs a ComponentNameResolver into the JavaScript runtime.
+   *
+   * @param runtimeExecutor The runtime executor for the JavaScript runtime
+   * @param componentNameResolver The component name resolver to install
+   */
   @JvmStatic external fun install(runtimeExecutor: RuntimeExecutor, componentNameResolver: Any)
 }
