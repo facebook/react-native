d9671959336db28dd728b51f50c45f27ff0a8cb7
Riccardo Cipolleschi <cipolleschi@meta.com>
2025-11-17 03:04:28 -0800

    Fix crash in RCTExceptionManager (#54550)
    Summary:
    
    # Summary
    
    This diff resolves a crash in RCTExceptionManager by modifying error handling. It fixes the issue by checking if `RCTJSExtraDataKey` and `RCTJSStackTraceKey` are null or not in a dictionary initializer.
    
    Also, `RCTJSExtraDataKey` is marked as nullable but its nullability is not checked.
    
    ## Changelog:
    [iOS][Fixed] - A Crash when some error information are nil
    
    Reviewed By: javache
    
    Differential Revision: D87140823

--- packages/react-native/React/CoreModules/RCTExceptionsManager.mm
+++ packages/react-native/React/CoreModules/RCTExceptionsManager.mm
@@ -77,8 +77,11 @@ RCT_EXPORT_MODULE()
     RCTTriggerReloadCommandListeners(@"JS Crash Reload");
   } else if (!RCT_DEV) {
     NSString *description = [@"Unhandled JS Exception: " stringByAppendingString:message];
-    NSDictionary *errorInfo =
-        @{NSLocalizedDescriptionKey : description, RCTJSStackTraceKey : stack, RCTJSExtraDataKey : extraDataAsJSON};
+    NSDictionary *errorInfo = @{
+      NSLocalizedDescriptionKey : description,
+      RCTJSStackTraceKey : stack == nil ? (id)kCFNull : stack,
+      RCTJSExtraDataKey : extraDataAsJSON == nil ? (id)kCFNull : extraDataAsJSON
+    };
     RCTFatal([NSError errorWithDomain:RCTErrorDomain code:0 userInfo:errorInfo]);
   }
 }
