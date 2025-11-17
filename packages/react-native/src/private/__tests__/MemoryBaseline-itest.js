0ebfa3e70d80cdf02b887306215954815c950b78
Gang Zhao <zhaogang@meta.com>
2025-11-17 10:15:15 -0800

    Add DEFAULT_OSS_HERMES_VARIANT and change DEFAULT_HERMES_VARIANT to use SH internally (#54543)
    Summary:
    
    Changelog: [Internal]
    
    We are switching to SH as default, so internally, we need to switch
    DEFAULT_HERMES_VARIANT to use SH stable. To keep the tests working in
    OSS, we add DEFAULT_OSS_HERMES_VARIANT to use legacy Hermes.
    
    Reviewed By: cipolleschi
    
    Differential Revision: D87082037

--- packages/react-native/src/private/__tests__/MemoryBaseline-itest.js
+++ packages/react-native/src/private/__tests__/MemoryBaseline-itest.js
@@ -29,12 +29,12 @@ declare var performance: Performance;
  */
 const MEMORY_LIMITS_KB = {
   environmentSetup: {
-    dev: 1000,
-    opt: 500,
+    dev: 1300,
+    opt: 700,
   },
   basicSurfaceRender: {
-    dev: 1200,
-    opt: 700,
+    dev: 1700,
+    opt: 900,
   },
 };
 
