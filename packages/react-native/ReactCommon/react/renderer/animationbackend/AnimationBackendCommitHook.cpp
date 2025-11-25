ec51fc08027f8faf6d331163259e65739ce7bc1d
Bartlomiej Bloniarz <blonibartlomiej@meta.com>
2025-11-25 01:34:31 -0800

    Introduce `AnimationBackendCommitHook` (#54138)
    Summary:
    
    This diff introduces `AnimationBackendCommitHook`. It is responsible for keeping animation updates in sync with React. When React does its updates, it doesn't look at the last mounted tree. Instead it hold its own references to ShadowNodes, and commits them whenever there is a (JS) rendering update. Any props that were commited from outside of React (e.g. by Reanimated) are lost.
    
    To work around this we have the Commit Hook. It enables us to override any rendering updates that react does, just before the ShadowTree enters the layout phase. 
    
    In this implementation we utilize RNSRU to push those updates back to React (whenever there is a commit originating from React). So after a render React will now hold references to ShadowNodes with the current animation state (in props). This is a new approach, in Reanimated's current implementation this is disabled, and we reapply the changes via the Commit Hook on each React commit.
    
    Differential Revision: D84250600

---
+++ packages/react-native/ReactCommon/react/renderer/animationbackend/AnimationBackendCommitHook.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <react/renderer/animationbackend/AnimationBackendCommitHook.h>
+
+namespace facebook::react {
+
+AnimationBackendCommitHook::AnimationBackendCommitHook(
+    UIManager* uiManager,
+    std::shared_ptr<AnimatedPropsRegistry> animatedPropsRegistry)
+    : uiManager_(uiManager),
+      animatedPropsRegistry_(std::move(animatedPropsRegistry)) {
+  uiManager_->registerCommitHook(*this);
+}
+
+RootShadowNode::Unshared AnimationBackendCommitHook::shadowTreeWillCommit(
+    const ShadowTree& shadowTree,
+    const RootShadowNode::Shared& oldRootShadowNode,
+    const RootShadowNode::Unshared& newRootShadowNode,
+    const ShadowTreeCommitOptions& commitOptions) noexcept {
+  if (commitOptions.source != ShadowTreeCommitSource::React) {
+    return newRootShadowNode;
+  }
+
+  const auto& res = animatedPropsRegistry_->getMap(shadowTree.getSurfaceId());
+  auto& surfaceFamilies = res.first;
+  auto& updates = res.second;
+
+  if (surfaceFamilies.empty()) {
+    return newRootShadowNode;
+  }
+  return std::static_pointer_cast<RootShadowNode>(
+      newRootShadowNode->cloneMultiple(
+          surfaceFamilies,
+          [&surfaceFamilies, &updates](
+              const ShadowNode& shadowNode,
+              const ShadowNodeFragment& fragment) {
+            auto newProps = ShadowNodeFragment::propsPlaceholder();
+            std::shared_ptr<BaseViewProps> viewProps = nullptr;
+            if (surfaceFamilies.contains(&shadowNode.getFamily()) &&
+                updates.contains(shadowNode.getTag())) {
+              auto& snapshot = updates.at(shadowNode.getTag());
+              if (!snapshot->propNames.empty()) {
+                PropsParserContext propsParserContext{
+                    shadowNode.getSurfaceId(),
+                    *shadowNode.getContextContainer()};
+
+                newProps = shadowNode.getComponentDescriptor().cloneProps(
+                    propsParserContext, shadowNode.getProps(), {});
+                viewProps = std::const_pointer_cast<BaseViewProps>(
+                    std::static_pointer_cast<const BaseViewProps>(newProps));
+              }
+
+              for (const auto& propName : snapshot->propNames) {
+                updateProp(propName, *viewProps, *snapshot);
+              }
+            }
+            return shadowNode.clone(
+                {.props = newProps,
+                 .children = fragment.children,
+                 .state = shadowNode.getState(),
+                 .runtimeShadowNodeReference = true});
+          }));
+}
+
+AnimationBackendCommitHook::~AnimationBackendCommitHook() {
+  // TODO: fix
+  // uiManager get's deallocated first, so we can't call unregisterCommitHook
+  // here (or need to get a shared_ptr) uiManager_->unregisterCommitHook(*this);
+}
+
+} // namespace facebook::react
