9ba4ce6d6aab72982d282f96f25123d88bb9f5ca
Ruslan Lesiutin <hoxy@meta.com>
2025-11-26 03:55:56 -0800

    Cleanup no longer used jni layer for frame timings (#54678)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/54678
    
    # Changelog: [Internal]
    
    This is not a breaking change, since it removes something that wasn't yet rolled out to OSS - it is unused.
    
    Reviewed By: sbuggay
    
    Differential Revision: D87378393
    
    fbshipit-source-id: 3d560276d029cdab942e76fe34111d63e453a26c

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/soloader/OpenSourceMergedSoMapping.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/soloader/OpenSourceMergedSoMapping.kt
@@ -28,7 +28,6 @@ public object OpenSourceMergedSoMapping : ExternalSoMapping {
         "react_devsupportjni",
         "react_featureflagsjni",
         "react_newarchdefaults",
-        "react_performancetracerjni",
         "reactnativeblob",
         "reactnativejni",
         "reactnativejni_common",
@@ -58,7 +57,6 @@ public object OpenSourceMergedSoMapping : ExternalSoMapping {
       "react_devsupportjni" -> libreact_devsupportjni_so()
       "react_featureflagsjni" -> libreact_featureflagsjni_so()
       "react_newarchdefaults" -> libreact_newarchdefaults_so()
-      "react_performancetracerjni" -> libreact_performancetracerjni_so()
       "reactnative" -> libreactnative_so()
       "reactnativeblob" -> libreactnativeblob_so()
       "reactnativejni" -> libreactnativejni_so()
@@ -89,8 +87,6 @@ public object OpenSourceMergedSoMapping : ExternalSoMapping {
   public external fun libreact_featureflagsjni_so(): Int
 
   public external fun libreact_newarchdefaults_so(): Int
-
-  public external fun libreact_performancetracerjni_so(): Int
 
   public external fun libreactnative_so(): Int
 
