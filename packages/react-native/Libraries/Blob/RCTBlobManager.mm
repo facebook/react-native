5da64cfe7fa70774b53885792b15b6b173c3644f
samuelscheit <github@samuelscheit.com>
2025-11-26 17:11:27 +0100

    fix: RCTBlobManager crash on nil response

--- packages/react-native/Libraries/Blob/RCTBlobManager.mm
+++ packages/react-native/Libraries/Blob/RCTBlobManager.mm
@@ -299,12 +299,21 @@ RCT_EXPORT_METHOD(release : (NSString *)blobId)
   // An empty body will have nil for data, in this case we need to return
   // an empty blob as per the XMLHttpRequest spec.
   data = data ?: [NSData new];
+
+  NSString *fileName = nil;
+  NSString *contentType = nil;
+
+  if (response) {
+    fileName = [response suggestedFilename];
+    contentType = [response MIMEType];
+  }
+    
   return @{
     @"blobId" : [self store:data],
     @"offset" : @0,
     @"size" : @(data.length),
-    @"name" : RCTNullIfNil([response suggestedFilename]),
-    @"type" : RCTNullIfNil([response MIMEType]),
+    @"name" : RCTNullIfNil(fileName),
+    @"type" : RCTNullIfNil(contentType),
   };
 }
 
