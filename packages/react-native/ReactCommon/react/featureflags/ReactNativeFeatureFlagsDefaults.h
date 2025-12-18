44ac50adcf4203ec23ab515b9d27260487db0e0f
Jakub Piasecki <piaseckijakub@fb.com>
2025-12-18 01:34:05 -0800

    Add feature flag for Fabric commit branching
    Summary:
    Changelog: [Internal]
    
    Adds a feature flag for the Fabric commit branching mechanism.
    
    Differential Revision: D88151491

--- packages/react-native/ReactCommon/react/featureflags/ReactNativeFeatureFlagsDefaults.h
+++ packages/react-native/ReactCommon/react/featureflags/ReactNativeFeatureFlagsDefaults.h
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<33cacaaeb6994f78f5a2d9379618e2a0>>
+ * @generated SignedSource<<06b786f98c25c4d63129083757577538>>
  */
 
 /**
@@ -116,6 +116,10 @@ class ReactNativeFeatureFlagsDefaults : public ReactNativeFeatureFlagsProvider {
   }
 
   bool enableExclusivePropsUpdateAndroid() override {
+    return false;
+  }
+
+  bool enableFabricCommitBranching() override {
     return false;
   }
 
