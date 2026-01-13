369cd8d1b8ffacc79a18619426d2003ea09f393f
Devan Buggay <devanb@meta.com>
2026-01-12 21:14:25 -0800

    RCTView compiling for AppleTV (#55130)
    Summary:
    
    Compile out unsupported UIKit APIs used by RCTView and it's manager.
    
    Changelog: [internal]
    
    Differential Revision: D90513795

--- packages/react-native/React/Views/RCTView.m
+++ packages/react-native/React/Views/RCTView.m
@@ -910,7 +910,8 @@ static void RCTUpdateShadowPathForView(RCTView *view)
 
 static void RCTUpdateHoverStyleForView(RCTView *view)
 {
-#if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 170000 /* __IPHONE_17_0 */
+#if !TARGET_OS_TV && defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && \
+    __IPHONE_OS_VERSION_MAX_ALLOWED >= 170000 /* __IPHONE_17_0 */
   if (@available(iOS 17.0, *)) {
     UIHoverStyle *hoverStyle = nil;
     if ([view cursor] == RCTCursorPointer) {
