bcb141b74d445f380a235cf28027e8fc0f9f005f
Devan Buggay <devanb@meta.com>
2026-01-12 21:14:28 -0800

    Compile out various unsupported TextInput APIs for AppleTV (#55129)
    Summary:
    
    TextInput uses a lot of UIKit APIs that are unavailable on AppleTV, compiling them out.
    
    Changelog: [internal]
    
    Reviewed By: shwanton
    
    Differential Revision: D90514227

--- packages/react-native/React/Fabric/Mounting/ComponentViews/TextInput/RCTTextInputUtils.mm
+++ packages/react-native/React/Fabric/Mounting/ComponentViews/TextInput/RCTTextInputUtils.mm
@@ -225,7 +225,7 @@ UITextContentType RCTUITextContentTypeFromString(const std::string &contentType)
     }
 
 #if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 170000 /* __IPHONE_17_0 */
-    if (@available(iOS 17.0, *)) {
+    if (@available(iOS 17.0, tvOS 17.0, *)) {
       [mutableContentTypeMap addEntriesFromDictionary:@{
         @"creditCardExpiration" : UITextContentTypeCreditCardExpiration,
         @"creditCardExpirationMonth" : UITextContentTypeCreditCardExpirationMonth,
@@ -271,6 +271,7 @@ UITextSmartInsertDeleteType RCTUITextSmartInsertDeleteTypeFromOptionalBool(std::
       : UITextSmartInsertDeleteTypeDefault;
 }
 
+#if !TARGET_OS_TV
 UIDataDetectorTypes RCTUITextViewDataDetectorTypesFromStringVector(const std::vector<std::string> &dataDetectorTypes)
 {
   static dispatch_once_t onceToken;
@@ -298,3 +299,4 @@ UIDataDetectorTypes RCTUITextViewDataDetectorTypesFromStringVector(const std::ve
   }
   return ret;
 }
+#endif
