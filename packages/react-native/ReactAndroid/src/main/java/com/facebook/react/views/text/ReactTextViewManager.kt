26c0f87d7b1360ae7843a609314997977adb455d
Nick Gerleman <ngerlem@meta.com>
2026-02-09 15:38:44 -0800

    (AI Generated) [RN][Android] Merge ReactTextAnchorViewManager into ReactTextViewManager (#55466)
    Summary:
    
    ReactTextAnchorViewManager was only extended by ReactTextViewManager, and its
    methods directly assumed the instantiated class was a ReactTextViewManager.
    This change eliminates the unnecessary inheritance hierarchy by merging
    ReactTextAnchorViewManager's functionality directly into ReactTextViewManager.
    
    ReactTextViewManager now extends BaseViewManager directly and contains all the
    prop setters (border, text, selection, etc.) that were previously in
    ReactTextAnchorViewManager.
    
    Changelog: [Internal]
    
    Reviewed By: mdvacca
    
    Differential Revision: D92635681

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/text/ReactTextViewManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/text/ReactTextViewManager.kt
@@ -8,25 +8,39 @@
 package com.facebook.react.views.text
 
 import android.os.Build
+import android.text.Layout
 import android.text.Spannable
 import android.text.Spanned
+import android.text.TextUtils
+import android.text.util.Linkify
+import android.view.Gravity
+import com.facebook.common.logging.FLog
 import com.facebook.react.R
+import com.facebook.react.common.ReactConstants
 import com.facebook.react.common.annotations.UnstableReactNativeAPI
 import com.facebook.react.common.mapbuffer.MapBuffer
 import com.facebook.react.internal.SystraceSection
 import com.facebook.react.internal.featureflags.ReactNativeFeatureFlags
 import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.uimanager.BackgroundStyleApplicator
+import com.facebook.react.uimanager.BaseViewManager
 import com.facebook.react.uimanager.IViewManagerWithChildren
+import com.facebook.react.uimanager.LengthPercentage
+import com.facebook.react.uimanager.LengthPercentageType
 import com.facebook.react.uimanager.ReactStylesDiffMap
 import com.facebook.react.uimanager.StateWrapper
 import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.ViewDefaults
+import com.facebook.react.uimanager.ViewProps
 import com.facebook.react.uimanager.annotations.ReactProp
+import com.facebook.react.uimanager.annotations.ReactPropGroup
+import com.facebook.react.uimanager.style.BorderRadiusProp
+import com.facebook.react.uimanager.style.BorderStyle.Companion.fromString
+import com.facebook.react.uimanager.style.LogicalEdge
+import com.facebook.react.views.text.DefaultStyleValuesUtil.getDefaultTextColorHighlight
 import java.util.HashMap
 
-/**
- * Concrete class for [ReactTextAnchorViewManager] which represents view managers of anchor `<Text>`
- * nodes.
- */
+/** View manager for `<Text>` nodes. */
 @Suppress("DEPRECATION")
 @ReactModule(name = ReactTextViewManager.REACT_CLASS)
 @OptIn(UnstableReactNativeAPI::class)
@@ -35,7 +49,7 @@ public class ReactTextViewManager
 public constructor(
     protected var reactTextViewManagerCallback: ReactTextViewManagerCallback? = null
 ) :
-    ReactTextAnchorViewManager<ReactTextShadowNode>(),
+    BaseViewManager<ReactTextView, ReactTextShadowNode>(),
     IViewManagerWithChildren,
     ReactTextViewManagerCallback {
   init {
@@ -53,7 +67,7 @@ public constructor(
     if (preparedView != null) {
       // Resets background and borders
       preparedView.recycleView()
-      // Defaults from ReactTextAnchorViewManager
+      // Reset selection color to default
       setSelectionColor(preparedView, null)
     }
     return preparedView
@@ -179,6 +193,180 @@ public constructor(
   @ReactProp(name = "overflow")
   public fun setOverflow(view: ReactTextView, overflow: String?) {
     view.setOverflow(overflow)
+  }
+
+  @ReactProp(name = "accessible")
+  public fun setAccessible(view: ReactTextView, accessible: Boolean) {
+    view.isFocusable = accessible
+  }
+
+  // maxLines can only be set in master view (block), doesn't really make sense to set in a span
+  @ReactProp(name = ViewProps.NUMBER_OF_LINES, defaultInt = ViewDefaults.NUMBER_OF_LINES)
+  public fun setNumberOfLines(view: ReactTextView, numberOfLines: Int) {
+    view.setNumberOfLines(numberOfLines)
+  }
+
+  @ReactProp(name = ViewProps.ELLIPSIZE_MODE)
+  public fun setEllipsizeMode(view: ReactTextView, ellipsizeMode: String?) {
+    when (ellipsizeMode) {
+      null,
+      "tail" -> view.setEllipsizeLocation(TextUtils.TruncateAt.END)
+      "head" -> view.setEllipsizeLocation(TextUtils.TruncateAt.START)
+      "middle" -> view.setEllipsizeLocation(TextUtils.TruncateAt.MIDDLE)
+      "clip" -> view.setEllipsizeLocation(null)
+      else -> {
+        FLog.w(ReactConstants.TAG, "Invalid ellipsizeMode: $ellipsizeMode")
+        view.setEllipsizeLocation(TextUtils.TruncateAt.END)
+      }
+    }
+  }
+
+  @ReactProp(name = ViewProps.ADJUSTS_FONT_SIZE_TO_FIT)
+  public fun setAdjustFontSizeToFit(view: ReactTextView, adjustsFontSizeToFit: Boolean) {
+    view.setAdjustFontSizeToFit(adjustsFontSizeToFit)
+  }
+
+  @ReactProp(name = ViewProps.FONT_SIZE)
+  public fun setFontSize(view: ReactTextView, fontSize: Float) {
+    view.setFontSize(fontSize)
+  }
+
+  @ReactProp(name = ViewProps.LETTER_SPACING, defaultFloat = 0f)
+  public fun setLetterSpacing(view: ReactTextView, letterSpacing: Float) {
+    view.letterSpacing = letterSpacing
+  }
+
+  @ReactProp(name = ViewProps.TEXT_ALIGN_VERTICAL)
+  public fun setTextAlignVertical(view: ReactTextView, textAlignVertical: String?) {
+    when (textAlignVertical) {
+      null,
+      "auto" -> view.setGravityVertical(Gravity.NO_GRAVITY)
+      "top" -> view.setGravityVertical(Gravity.TOP)
+      "bottom" -> view.setGravityVertical(Gravity.BOTTOM)
+      "center" -> view.setGravityVertical(Gravity.CENTER_VERTICAL)
+      else -> {
+        FLog.w(ReactConstants.TAG, "Invalid textAlignVertical: $textAlignVertical")
+        view.setGravityVertical(Gravity.NO_GRAVITY)
+      }
+    }
+  }
+
+  @ReactProp(name = "selectable")
+  public fun setSelectable(view: ReactTextView, isSelectable: Boolean) {
+    view.setTextIsSelectable(isSelectable)
+  }
+
+  @ReactProp(name = "selectionColor", customType = "Color")
+  public fun setSelectionColor(view: ReactTextView, color: Int?) {
+    view.highlightColor = color ?: getDefaultTextColorHighlight(view.context)
+  }
+
+  @ReactProp(name = "android_hyphenationFrequency")
+  public fun setAndroidHyphenationFrequency(view: ReactTextView, frequency: String?) {
+    when (frequency) {
+      null,
+      "none" -> view.hyphenationFrequency = Layout.HYPHENATION_FREQUENCY_NONE
+      "full" -> view.hyphenationFrequency = Layout.HYPHENATION_FREQUENCY_FULL
+      "normal" -> view.hyphenationFrequency = Layout.HYPHENATION_FREQUENCY_NORMAL
+      else -> {
+        FLog.w(ReactConstants.TAG, "Invalid android_hyphenationFrequency: $frequency")
+        view.hyphenationFrequency = Layout.HYPHENATION_FREQUENCY_NONE
+      }
+    }
+  }
+
+  @ReactPropGroup(
+      names =
+          [
+              ViewProps.BORDER_RADIUS,
+              ViewProps.BORDER_TOP_LEFT_RADIUS,
+              ViewProps.BORDER_TOP_RIGHT_RADIUS,
+              ViewProps.BORDER_BOTTOM_RIGHT_RADIUS,
+              ViewProps.BORDER_BOTTOM_LEFT_RADIUS,
+          ],
+      defaultFloat = Float.NaN,
+  )
+  public fun setBorderRadius(view: ReactTextView, index: Int, borderRadius: Float) {
+    val radius =
+        if (borderRadius.isNaN()) {
+          null
+        } else {
+          LengthPercentage(borderRadius, LengthPercentageType.POINT)
+        }
+    BackgroundStyleApplicator.setBorderRadius(view, BorderRadiusProp.values()[index], radius)
+  }
+
+  @ReactProp(name = "borderStyle")
+  public fun setBorderStyle(view: ReactTextView, borderStyle: String?) {
+    val parsedBorderStyle = if (borderStyle == null) null else fromString(borderStyle)
+    BackgroundStyleApplicator.setBorderStyle(view, parsedBorderStyle)
+  }
+
+  @ReactPropGroup(
+      names =
+          [
+              ViewProps.BORDER_WIDTH,
+              ViewProps.BORDER_LEFT_WIDTH,
+              ViewProps.BORDER_RIGHT_WIDTH,
+              ViewProps.BORDER_TOP_WIDTH,
+              ViewProps.BORDER_BOTTOM_WIDTH,
+              ViewProps.BORDER_START_WIDTH,
+              ViewProps.BORDER_END_WIDTH,
+          ],
+      defaultFloat = Float.NaN,
+  )
+  public fun setBorderWidth(view: ReactTextView, index: Int, width: Float) {
+    BackgroundStyleApplicator.setBorderWidth(view, LogicalEdge.values()[index], width)
+  }
+
+  @ReactPropGroup(
+      names =
+          [
+              "borderColor",
+              "borderLeftColor",
+              "borderRightColor",
+              "borderTopColor",
+              "borderBottomColor",
+          ],
+      customType = "Color",
+  )
+  public fun setBorderColor(view: ReactTextView, index: Int, color: Int?) {
+    BackgroundStyleApplicator.setBorderColor(view, LogicalEdge.values()[index], color)
+  }
+
+  @ReactProp(name = ViewProps.INCLUDE_FONT_PADDING, defaultBoolean = true)
+  public fun setIncludeFontPadding(view: ReactTextView, includepad: Boolean) {
+    view.includeFontPadding = includepad
+  }
+
+  @ReactProp(name = "disabled", defaultBoolean = false)
+  public fun setDisabled(view: ReactTextView, disabled: Boolean) {
+    view.isEnabled = !disabled
+  }
+
+  @ReactProp(name = "dataDetectorType")
+  public fun setDataDetectorType(view: ReactTextView, type: String?) {
+    when (type) {
+      "phoneNumber" -> {
+        view.setLinkifyMask(Linkify.PHONE_NUMBERS)
+        return
+      }
+      "link" -> {
+        view.setLinkifyMask(Linkify.WEB_URLS)
+        return
+      }
+      "email" -> {
+        view.setLinkifyMask(Linkify.EMAIL_ADDRESSES)
+        return
+      }
+      "all" -> {
+        @Suppress("DEPRECATION") view.setLinkifyMask(Linkify.ALL)
+        return
+      }
+    }
+
+    // "none" case, default, and null type are equivalent.
+    view.setLinkifyMask(0)
   }
 
   public companion object {
