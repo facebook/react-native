ba5c5af0609be2130e0b4fb14e230036970948be
Patrick Mick <patrick.mick@discordapp.com>
2026-02-12 09:19:29 -0800

    Hacky patch for new arch opt builds (#148)
    * Conditionally register for profiling
    
    * Adding a comment

--- packages/react-native/ReactCommon/react/runtime/hermes/HermesInstance.cpp
+++ packages/react-native/ReactCommon/react/runtime/hermes/HermesInstance.cpp
@@ -110,7 +110,11 @@ class HermesJSRuntime : public JSRuntime {
   }
 
   void unstable_initializeOnJsThread() override {
+    // On new arch, we're seeing a crash in release optimized builds here. Adding this as a stopgap measure to prevent
+    // the crash to allow us to more thoroughly test new arch while this is investigated.
+#ifdef HERMES_ENABLE_PROFILING
     runtime_->registerForProfiling();
+#endif
   }
 
  private:
