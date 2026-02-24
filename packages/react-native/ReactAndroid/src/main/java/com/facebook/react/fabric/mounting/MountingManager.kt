8da32caf2e9087bd5e2849b8cc489937c9906ff9
Pieter De Baets <pieterdb@meta.com>
2026-02-24 06:00:12 -0800

    SurfaceMountingManager kotlin nits
    Summary:
    Some follow-up to D90111698 to add more signal to issues reported and fix nits.
    
    Changelog: [Internal]
    
    Differential Revision: D94210145

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/MountingManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/fabric/mounting/MountingManager.kt
@@ -101,12 +101,6 @@ internal class MountingManager(
   @AnyThread
   fun attachRootView(surfaceId: Int, rootView: View, themedReactContext: ThemedReactContext) {
     val surfaceMountingManager = getSurfaceManagerEnforced(surfaceId, "attachView")
-
-    if (surfaceMountingManager.isStopped) {
-      logSoftException(TAG, IllegalStateException("Trying to attach a view to a stopped surface"))
-      return
-    }
-
     surfaceMountingManager.attachRootView(rootView, themedReactContext)
   }
 
