25ac11b03dda1464e4feccbd5554f550f7ae57df
Jakub Piasecki <piaseckijakub@meta.com>
2026-03-03 01:00:16 -0800

    Ignore anonymous namespaces in the snapshot (#55844)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/55844
    
    Changelog: [Internal]
    
    Updates the C++ API snapshot generator not to include anonymous namespaces in the snapshot since those have internal linkage.
    
    Reviewed By: cipolleschi
    
    Differential Revision: D94901508
    
    fbshipit-source-id: f025f69e4d9cc6cc42fa99974d596eb223e8da1c

---
+++ scripts/cxx-api/tests/snapshots/should_ignore_anonymous_namespace/test.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+namespace {
+enum class InternalEnum {
+  A,
+  B,
+};
+} // namespace
+
+namespace test {
+
+class Clss {
+ public:
+  void hello();
+};
+
+} // namespace test
