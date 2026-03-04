e450009a1ff2f3ee43b942bacbcaf236ff3820d2
Zeya Peng <zeyap@meta.com>
2026-03-04 08:26:54 -0800

    move DefaultTurboModules::get to end of TurboModuleManager (#55852)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/55852
    
    ## Changelog:
    
    [General] [Changed] - move DefaultTurboModules::get to end of TurboModuleManager
    
    To align with android/ios; so that the logic before it can override default behavior
    
    Reviewed By: christophpurrer
    
    Differential Revision: D94907092

--- packages/react-native/ReactCxxPlatform/react/runtime/TurboModuleManager.cpp
+++ packages/react-native/ReactCxxPlatform/react/runtime/TurboModuleManager.cpp
@@ -67,14 +67,7 @@ std::shared_ptr<TurboModule> TurboModuleManager::operator()(
     // when animatedNodesManagerProvider_ is null, defer to default
     return std::make_shared<AnimatedModule>(
         jsInvoker_, animatedNodesManagerProvider_);
-  }
-
-  if (auto turboModule =
-          DefaultTurboModules::getTurboModule(name, jsInvoker_)) {
-    return turboModule;
-  }
-
-  if (name == AppStateModule::kModuleName) {
+  } else if (name == AppStateModule::kModuleName) {
     return std::make_shared<AppStateModule>(jsInvoker_);
   } else if (name == DeviceInfoModule::kModuleName) {
     return std::make_shared<DeviceInfoModule>(jsInvoker_);
@@ -95,8 +88,6 @@ std::shared_ptr<TurboModule> TurboModuleManager::operator()(
         jsInvoker_, webSocketClientFactory_);
   } else if (name == NativeExceptionsManager::kModuleName) {
     return std::make_shared<NativeExceptionsManager>(onJsError_, jsInvoker_);
-  } else if (name == NativePerformance::kModuleName) {
-    return std::make_shared<NativePerformance>(jsInvoker_);
   } else if (name == NativeIntersectionObserver::kModuleName) {
     return std::make_shared<NativeIntersectionObserver>(jsInvoker_);
   } else if (name == NativeMutationObserver::kModuleName) {
@@ -107,6 +98,11 @@ std::shared_ptr<TurboModule> TurboModuleManager::operator()(
     if (logBoxSurfaceDelegate_) {
       return std::make_shared<LogBoxModule>(jsInvoker_, logBoxSurfaceDelegate_);
     }
+  }
+
+  if (auto turboModule =
+          DefaultTurboModules::getTurboModule(name, jsInvoker_)) {
+    return turboModule;
   }
 
   LOG(WARNING) << "Failed to load TurboModule: " << name;
