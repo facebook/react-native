e1caf7cbf98a46d899c1e69f462296fa71c5f252
nishan (o^▽^o) <nishanbende@gmail.com>
2026-03-04 18:27:32 +0530

    add cover and contain back to stylesheet types

--- packages/react-native/Libraries/StyleSheet/StyleSheetTypes.d.ts
+++ packages/react-native/Libraries/StyleSheet/StyleSheetTypes.d.ts
@@ -423,10 +423,13 @@ type RadialGradientValue = {
 
 export type BackgroundImageValue = LinearGradientValue | RadialGradientValue;
 
-export type BackgroundSizeValue = {
-  x: string | number;
-  y: string | number;
-};
+export type BackgroundSizeValue =
+  | {
+      x: string | number;
+      y: string | number;
+    }
+  | 'cover'
+  | 'contain';
 
 export type BackgroundRepeatKeyword =
   | 'repeat'
