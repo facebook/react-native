851e5ae546ea27db5ba657630e236f2945a8c929
Dawid Małecki <maleckidawid@meta.com>
2026-03-04 01:55:35 -0800

    Parser refactor (#55772)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/55772
    
    The `main.py` increases rapidly in size. This diff refactors that into the `utils.py` and `builder.py` to form more modularized and easier to read structure.
    
    Changelog:
    [Internal]
    
    Reviewed By: cipolleschi
    
    Differential Revision: D94072940
    
    fbshipit-source-id: f4b7b3c3bf4e2cf387829ced039d5645e2ad4b3e

--- scripts/cxx-api/parser/utils/__init__.py
+++ scripts/cxx-api/parser/utils/__init__.py
@@ -14,14 +14,24 @@ from .argument_parsing import (
     parse_type_with_argstrings,
 )
 from .qualified_path import parse_qualified_path
+from .text_resolution import (
+    extract_namespace_from_refid,
+    InitializerType,
+    normalize_angle_brackets,
+    resolve_linked_text_name,
+    resolve_ref_text_name,
+)
 from .type_qualification import qualify_arguments, qualify_parsed_type, qualify_type_str
 
 __all__ = [
     "Argument",
+    "extract_namespace_from_refid",
     "extract_qualifiers",
     "format_arguments",
     "format_parsed_type",
     "FunctionModifiers",
+    "InitializerType",
+    "normalize_angle_brackets",
     "parse_arg_string",
     "parse_function_pointer_argstring",
     "parse_qualified_path",
@@ -29,4 +39,6 @@ __all__ = [
     "qualify_arguments",
     "qualify_parsed_type",
     "qualify_type_str",
+    "resolve_linked_text_name",
+    "resolve_ref_text_name",
 ]
