88ca6b9677b706b31b84963920f15f887b98c6b3
Zeya Peng <zeyap@fb.com>
2026-03-04 08:12:07 -0800

    add animatedModule to defaults
    Differential Revision: D94244698

--- packages/react-native/ReactCommon/react/nativemodule/defaults/DefaultTurboModules.cpp
+++ packages/react-native/ReactCommon/react/nativemodule/defaults/DefaultTurboModules.cpp
@@ -13,6 +13,7 @@
 #include <react/nativemodule/intersectionobserver/NativeIntersectionObserver.h>
 #include <react/nativemodule/microtasks/NativeMicrotasks.h>
 #include <react/nativemodule/webperformance/NativePerformance.h>
+#include <react/renderer/animated/AnimatedModule.h>
 
 #ifdef REACT_NATIVE_DEBUGGER_ENABLED_DEVONLY
 #include <react/nativemodule/devtoolsruntimesettings/DevToolsRuntimeSettingsModule.h>
@@ -47,6 +48,13 @@ namespace facebook::react {
     if (name == NativeIntersectionObserver::kModuleName) {
       return std::make_shared<NativeIntersectionObserver>(jsInvoker);
     }
+  }
+
+  if (ReactNativeFeatureFlags::cxxNativeAnimatedEnabled() &&
+      ReactNativeFeatureFlags::useSharedAnimatedBackend() &&
+      name == AnimatedModule::kModuleName) {
+    return std::make_shared<AnimatedModule>(
+        jsInvoker, std::make_shared<NativeAnimatedNodesManagerProvider>());
   }
 
 #ifdef REACT_NATIVE_DEBUGGER_ENABLED_DEVONLY
