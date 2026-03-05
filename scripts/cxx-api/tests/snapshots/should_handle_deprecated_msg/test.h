6a53369bf817cb1234179e0444b79ee0d522bc60
Dawid Małecki <maleckidawid@meta.com>
2026-03-05 02:24:50 -0800

    Remove `__deprecated_msg` from the snapshot (#55780)
    Summary:
    
    Set predefined `__deprecated_msg` to empty string in the `.doxygen.config.template` as doxygen has problem with parsing and produces malformed xml.
    
    Changelog:
    [Internal]
    
    Reviewed By: cortinico
    
    Differential Revision: D94517576

---
+++ scripts/cxx-api/tests/snapshots/should_handle_deprecated_msg/test.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+@interface RCTTestInterface
+
+- (void)normalMethod:(NSString *)name;
+
+- (NSArray *)deprecatedMethod:(id)param __deprecated_msg("Use newMethod instead.");
+
+@end
+
+@interface RCTDeprecatedInterface
+
+- (void)normalMethod;
+
+- (void)oldMethod __deprecated_msg("Use newMethod instead.");
+
+- (void)customBubblingEventTypes __deprecated_msg("Use RCTBubblingEventBlock props instead.");
+
+- (void)legacyTitle __deprecated_msg("This API will be removed along with the legacy architecture.");
+
+@end
+
+@protocol RCTDeprecatedProtocol
+
+- (void)normalProtocolMethod;
+
+- (void)deprecatedProtocolMethod __deprecated_msg("Protocol method is deprecated.");
+
+@end
