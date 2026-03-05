1af6f3fbcdd3cabf6d969700caaf01c40f5de6e3
Dawid Małecki <maleckidawid@meta.com>
2026-03-05 02:24:50 -0800

    Add doxygen input filter to strip block comments (#55888)
    Summary:
    
    Adds a doxygen input filter (`input_filters/doxygen_strip_comments.py`) that strips block comments from source files before doxygen parses them.
    
    This prevents doxygen from incorrectly parsing Objective-C code examples (like `interface`, `protocol`) within documentation comments as actual code declarations. For example, a doc comment containing:
    
    ```
    /**
     * Example:
     *   interface RCT_EXTERN_MODULE(MyModule, NSObject)
     *   end
     */
    ```
    
    Was being parsed by doxygen as an actual interface declaration, resulting in malformed output like `interface RCT_EXTERN_MODULE {}` in the API snapshot.
    
    The filter preserves line numbers by replacing comment content with the equivalent number of newlines, ensuring error messages remain accurate.
    
    Changelog:
    [Internal]
    
    Reviewed By: cortinico
    
    Differential Revision: D94538938

---
+++ scripts/cxx-api/tests/snapshots/should_not_parse_interface_from_doc_comment/test.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Example of how to use this module:
+ *
+ *   @interface RCT_EXTERN_MODULE(MyModule, NSObject)
+ *   @end
+ */
+@interface RCTRealInterface
+
+- (void)realMethod;
+
+@end
