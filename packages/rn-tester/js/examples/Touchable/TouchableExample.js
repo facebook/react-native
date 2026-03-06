81c3a57206a270d387c03a964290adc4089edfcd
Nick Gerleman <ngerlem@meta.com>
2026-03-05 18:12:08 -0800

    Add Touchable E2E tests (#55956)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/55956
    
    Add name fields to 4 Touchable examples in TouchableExample.js and create 4 corresponding E2E interaction tests covering highlight, without-feedback, feedback-events, and hit-slop. Tests verify click interactions and text state changes.
    
    Reviewed By: cortinico
    
    Differential Revision: D95311373
    
    fbshipit-source-id: 82cd84f3cba55778beccb7aadc89f9c2b7a442bf

--- packages/rn-tester/js/examples/Touchable/TouchableExample.js
+++ packages/rn-tester/js/examples/Touchable/TouchableExample.js
@@ -672,6 +672,7 @@ exports.documentationURL = 'https://reactnative.dev/docs/touchablehighlight';
 exports.examples = [
   {
     title: '<TouchableHighlight>',
+    name: 'highlight',
     description: ('TouchableHighlight works by adding an extra view with a ' +
       'black background under the single child view.  This works best when the ' +
       'child view is fully opaque, although it can be made to work as a simple ' +
@@ -683,6 +684,7 @@ exports.examples = [
   },
   {
     title: '<TouchableWithoutFeedback>',
+    name: 'without-feedback',
     render(): React.Node {
       return <TouchableWithoutFeedbackBox />;
     },
@@ -742,6 +744,7 @@ exports.examples = [
   },
   {
     title: 'Touchable feedback events',
+    name: 'feedback-events',
     description: ('<Touchable*> components accept onPress, onPressIn, ' +
       'onPressOut, and onLongPress as props.': string),
     render(): React.MixedElement {
@@ -768,6 +771,7 @@ exports.examples = [
   },
   {
     title: 'Touchable Hit Slop',
+    name: 'hit-slop',
     description:
       ('<Touchable*> components accept hitSlop prop which extends the touch area ' +
         'without changing the view bounds.': string),
