9ffbd212a5f0cfabb118c0fca3c7cc58a29477b7
Mathieu Acthernoene <zoontek@gmail.com>
2026-03-11 15:28:40 +0100

    Replace getWindowDisplayMetrics (only used for density here)

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/text/TextLayoutManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/text/TextLayoutManager.kt
@@ -1377,7 +1377,7 @@ internal object TextLayoutManager {
     return FontMetricsUtil.getFontMetrics(
         layout.text,
         layout,
-        DisplayMetricsHolder.getWindowDisplayMetrics(),
+        DisplayMetricsHolder.getScreenDisplayMetrics(),
     )
   }
 
