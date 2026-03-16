3abcbe949969a92cc08f15967ff17e2ba1dd976f
Pieter De Baets <pieterdb@meta.com>
2026-03-16 07:59:52 -0700

    Cleanup fixImageSrcDimensionPropagation and remove duplicate setting of src and source
    Summary:
    Feature flag was just added for gating while rolling out, no regressions found.
    
    Also removing duplicate setting of `src` prop, which was introduced in D54000899.
    
    Changelog: [Internal]
    
    Differential Revision: D96727336

--- packages/react-native/scripts/featureflags/ReactNativeFeatureFlags.config.js
+++ packages/react-native/scripts/featureflags/ReactNativeFeatureFlags.config.js
@@ -1076,16 +1076,6 @@ const definitions: FeatureFlagDefinitions = {
       },
       ossReleaseStage: 'none',
     },
-    fixImageSrcDimensionPropagation: {
-      defaultValue: true,
-      metadata: {
-        description:
-          'Fix image dimensions not being passed through when src is used',
-        expectedReleaseValue: true,
-        purpose: 'release',
-      },
-      ossReleaseStage: 'none',
-    },
     fixVirtualizeListCollapseWindowSize: {
       defaultValue: false,
       metadata: {
