24dca6267917ca3d235b6e0bcadcd5331bd9b441
Dawid Małecki <maleckidawid@meta.com>
2026-03-18 05:40:16 -0700

    Move scope string generation logic to the base class (#56085)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/56085
    
    Changelog: [Internal]
    
    Moves the logic responsible for generating a string view of the scope's body to the base class to reduce duplication.
    
    Similarly, moves the logic responsible for qualifying template arguments to the `Extensible` class.
    
    Reviewed By: cipolleschi
    
    Differential Revision: D96455746

--- scripts/cxx-api/parser/scope/enum_scope_kind.py
+++ scripts/cxx-api/parser/scope/enum_scope_kind.py
@@ -7,8 +7,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from natsort import natsorted
-
 from .base_scope_kind import ScopeKind
 
 if TYPE_CHECKING:
@@ -21,22 +19,6 @@ class EnumScopeKind(ScopeKind):
         self.type: str | None = None
 
     def to_string(self, scope: Scope) -> str:
-        result = ""
         inheritance_string = f" : {self.type}" if self.type else ""
-
-        result += (
-            "\n" + f"{self.name} {scope.get_qualified_name()}{inheritance_string} {{"
-        )
-
-        stringified_members = []
-        for member in scope.get_members():
-            stringified_members.append(member.to_string(2) + ",")
-
-        stringified_members = natsorted(stringified_members)
-        result += ("\n" if len(stringified_members) > 0 else "") + "\n".join(
-            stringified_members
-        )
-
-        result += "\n}"
-
-        return result
+        header = f"\n{self.name} {scope.get_qualified_name()}{inheritance_string} "
+        return header + self._format_scope_body(scope, member_suffix=",")
