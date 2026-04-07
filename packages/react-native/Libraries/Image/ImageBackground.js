eccbb5533905123f4322b69da7b88d101fd15179
D N <4661784+retyui@users.noreply.github.com>
2026-04-07 17:42:22 +0200

    Convert `ImageBackground` to function component

--- packages/react-native/Libraries/Image/ImageBackground.js
+++ packages/react-native/Libraries/Image/ImageBackground.js
@@ -8,7 +8,6 @@
  * @format
  */
 
-import type {HostInstance} from '../../src/private/types/HostInstance';
 import type {ImageBackgroundProps} from './ImageProps';
 
 import View from '../Components/View/View';
@@ -43,66 +42,58 @@ export type {ImageBackgroundProps} from './ImageProps';
  * AppRegistry.registerComponent('DisplayAnImageBackground', () => DisplayAnImageBackground);
  * ```
  */
-class ImageBackground extends React.Component<ImageBackgroundProps> {
-  setNativeProps(props: {...}) {
-    // Work-around flow
-    const viewRef = this._viewRef;
-    if (viewRef) {
-      viewRef.setNativeProps(props);
-    }
-  }
 
-  _viewRef: ?React.ElementRef<typeof View> = null;
-
-  _captureRef = (ref: null | HostInstance) => {
-    this._viewRef = ref;
-  };
-
-  render(): React.Node {
-    const {
-      children,
-      style,
-      imageStyle,
-      imageRef,
-      importantForAccessibility,
-      ...props
-    } = this.props;
-
-    // $FlowFixMe[underconstrained-implicit-instantiation]
-    const flattenedStyle = flattenStyle(style);
-    return (
-      <View
-        accessibilityIgnoresInvertColors={true}
+const ImageBackground: component(
+  ref?: React.RefSetter<React.ElementRef<typeof View>>,
+  ...props: ImageBackgroundProps
+) = ({
+  ref,
+  children,
+  style,
+  imageStyle,
+  imageRef,
+  importantForAccessibility,
+  ...props
+}: {
+  ref?: React.RefSetter<React.ElementRef<typeof View>>,
+  ...ImageBackgroundProps,
+}): React.Node => {
+  // $FlowFixMe[underconstrained-implicit-instantiation]
+  const flattenedStyle = flattenStyle(style);
+  return (
+    <View
+      accessibilityIgnoresInvertColors={true}
+      importantForAccessibility={importantForAccessibility}
+      style={style}
+      ref={ref}>
+      {/* $FlowFixMe[incompatible-use] */}
+      <Image
+        {...props}
         importantForAccessibility={importantForAccessibility}
-        style={style}
-        ref={this._captureRef}>
-        {/* $FlowFixMe[incompatible-use] */}
-        <Image
-          {...props}
-          importantForAccessibility={importantForAccessibility}
-          style={[
-            StyleSheet.absoluteFill,
-            {
-              // Temporary Workaround:
-              // Current (imperfect yet) implementation of <Image> overwrites width and height styles
-              // (which is not quite correct), and these styles conflict with explicitly set styles
-              // of <ImageBackground> and with our internal layout model here.
-              // So, we have to proxy/reapply these styles explicitly for actual <Image> component.
-              // This workaround should be removed after implementing proper support of
-              // intrinsic content size of the <Image>.
-              // $FlowFixMe[prop-missing]
-              width: flattenedStyle?.width,
-              // $FlowFixMe[prop-missing]
-              height: flattenedStyle?.height,
-            },
-            imageStyle,
-          ]}
-          ref={imageRef}
-        />
-        {children}
-      </View>
-    );
-  }
-}
+        style={[
+          StyleSheet.absoluteFill,
+          {
+            // Temporary Workaround:
+            // Current (imperfect yet) implementation of <Image> overwrites width and height styles
+            // (which is not quite correct), and these styles conflict with explicitly set styles
+            // of <ImageBackground> and with our internal layout model here.
+            // So, we have to proxy/reapply these styles explicitly for actual <Image> component.
+            // This workaround should be removed after implementing proper support of
+            // intrinsic content size of the <Image>.
+            // $FlowFixMe[prop-missing]
+            width: flattenedStyle?.width,
+            // $FlowFixMe[prop-missing]
+            height: flattenedStyle?.height,
+          },
+          imageStyle,
+        ]}
+        ref={imageRef}
+      />
+      {children}
+    </View>
+  );
+};
+
+ImageBackground.displayName = 'ImageBackground';
 
 export default ImageBackground;
