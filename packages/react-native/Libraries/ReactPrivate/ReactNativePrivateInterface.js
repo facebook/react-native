4677ddf930465a59b8a44437e8763ff85c7d0c89
Rubén Norte <rubennorte@meta.com>
2026-04-11 08:39:15 -0700

    Expose dispatchTrustedEvent and setEventInitTimeStamp in ReactNativePrivateInterface
    Summary:
    Changelog: [internal]
    
    The React Native renderer needs access to these internals to dispatch events under the new EventTarget-based event dispatching pipeline.
    
    Differential Revision: D100462549

--- packages/react-native/Libraries/ReactPrivate/ReactNativePrivateInterface.js
+++ packages/react-native/Libraries/ReactPrivate/ReactNativePrivateInterface.js
@@ -9,6 +9,8 @@
  */
 
 import typeof CustomEvent from '../../src/private/webapis/dom/events/CustomEvent';
+import typeof {setEventInitTimeStamp} from '../../src/private/webapis/dom/events/internals/EventInternals';
+import typeof {dispatchTrustedEvent} from '../../src/private/webapis/dom/events/internals/EventTargetInternals';
 import typeof BatchedBridge from '../BatchedBridge/BatchedBridge';
 import typeof legacySendAccessibilityEvent from '../Components/AccessibilityInfo/legacySendAccessibilityEvent';
 import typeof TextInputState from '../Components/TextInput/TextInputState';
@@ -132,5 +134,13 @@ module.exports = {
   get getInternalInstanceHandleFromPublicInstance(): getInternalInstanceHandleFromPublicInstance {
     return require('../ReactNative/ReactFabricPublicInstance/ReactFabricPublicInstance')
       .getInternalInstanceHandleFromPublicInstance;
+  },
+  get dispatchTrustedEvent(): dispatchTrustedEvent {
+    return require('../../src/private/webapis/dom/events/internals/EventTargetInternals')
+      .dispatchTrustedEvent;
+  },
+  get setEventInitTimeStamp(): setEventInitTimeStamp {
+    return require('../../src/private/webapis/dom/events/internals/EventInternals')
+      .setEventInitTimeStamp;
   },
 };
