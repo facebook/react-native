0a6a92fb5ba78149d92e2a71a4abe9cad5ed3336
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-04-20 22:00:15 -0700

    Fix node ownership when `display: contents` is used (#56422)
    Summary:
    
    Changelog: [GENERAL][FIXED] Fixed Yoga node ownership when `display: contents` is used in absolutely positioned subtrees
    
    Fixes an edge case where nodes with `display: contents` weren't cloned properly inside absolutely positioned subtrees.
    
    Adds a test case covering this scenario.
    
    X-link: https://github.com/facebook/yoga/pull/1924
    
    Reviewed By: NickGerleman
    
    Differential Revision: D100581579
    
    Pulled By: j-piasecki

--- packages/react-native/ReactCommon/yoga/yoga/algorithm/AbsoluteLayout.cpp
+++ packages/react-native/ReactCommon/yoga/yoga/algorithm/AbsoluteLayout.cpp
@@ -558,6 +558,7 @@ bool layoutAbsoluteDescendants(
       // we need to mutate these descendents. Make sure the path of
       // nodes to them is mutable before positioning.
       child->cloneChildrenIfNeeded();
+      cleanupContentsNodesRecursively(child);
       const Direction childDirection =
           child->resolveDirection(currentNodeDirection);
       // By now all descendants of the containing block that are not absolute
