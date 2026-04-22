cc0f6b54aad29e442219e57626a5a3815a944775
Moti Zilberman <moti@meta.com>
2026-04-22 07:15:23 -0700

    Add LogBox-styled error overlay (#56550)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/56550
    
    Replace the legacy red-on-black RedBox design with a LogBox-inspired design for RedBox 2.0: charcoal background, salmon header bar, structured call stack, 3-button footer, full-screen view with no animated transition. Gated behind `redBoxV2IOS`.
    
    This diff is just for the low-hanging fruit - setting up the split implementation and borrowing the broad visual style of LogBox. Further up this stack we will port more functionality and improve on this baseline.
    
    Changelog: [Internal]
    
    Reviewed By: cipolleschi
    
    Differential Revision: D98115368

--- packages/react-native/Package.swift
+++ packages/react-native/Package.swift
@@ -392,7 +392,7 @@ let reactCoreModules = RNTarget(
   name: .reactCoreModules,
   path: "React/CoreModules",
   excludedPaths: ["PlatformStubs/RCTStatusBarManager.mm"],
-  dependencies: [.reactNativeDependencies, .jsi, .yoga, .reactTurboModuleCore]
+  dependencies: [.reactNativeDependencies, .jsi, .yoga, .reactTurboModuleCore, .reactFeatureFlags]
 )
 
 /// React-runtimeCore.podspec
