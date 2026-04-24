facebook/react-native

commit 45b4817414928c9bfa0b055bb09533ac177cada6

Implement Fabric commit branching (behind a flag) (#54835)
Summary:
Pull Request resolved: https://github.com/facebook/react-native/pull/54835

Changelog: [General][Changed] - Changed how React changes are commited to the Shadow Tree (behind a feature flag)

Introduces two branches for the Shadow Tree commits - React always commits to the JS revision, while other sources should commit to the main revision. Commits to the JS revision schedule a "merge" commit at the end of the event loop pass to upstream the React changes to the main revision.

"Merge" means taking the JS revision and promoting it to become the new main, losing changes that happened between fork and merge. Preservation of these changes is the responsibility of the party that created them, and they should be reapplied using a commit hook.

The main synchronization points here are:
- react revision promotion/promoted revision merge
  - During merge, the promoted revision is read and reset (UI thread), while promotion itself updated the promoted revision (JS thread)
- react commit/promoted revision merge
  - In case the promoted revision being merged is the same as the current React revision, the latter needs to be reset (UI thread), so that during the next JS commit it forks from the "main" branch (JS thread).
- react commit/react revision promotion
  - Both access the `currentReactRevision`, in case of a main thread render, those might happen to run concurrently on JS/UI thread

Reviewed By: javache, rubennorte

Differential Revision: D88151489

fbshipit-source-id: c70898ad25fbe5362c991be237c3fd0c1cf6ddcf

packages/react-native/ReactCxxPlatform/react/runtime/ReactHost.cpp
@@ -311,6 +311,9 @@ void ReactHost::destroyReactInstance() {
 
   reactInstanceData_->contextContainer->erase(RuntimeSchedulerKey);
   reactInstanceData_->mountingManager->setSchedulerTaskExecutor(nullptr);
+  reactInstanceData_->mountingManager = nullptr;
+  reactInstanceData_->contextContainer = nullptr;
+  reactInstanceData_->turboModuleProviders.clear();
   reactInstance_ = nullptr;
   reactInstanceData_->messageQueueThread = nullptr;
 }

packages/react-native/Libraries/ReactNative/__tests__/SyncOnCommit-itest.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @fantom_flags updateRuntimeShadowNodeReferencesOnCommit:true
+ * @fantom_flags updateRuntimeShadowNodeReferencesOnCommit:true enableFabricCommitBranching:*
  * @fantom_react_fb_flags passChildrenWhenCloningPersistedNodes:true
  * @flow strict-local
  * @format

packages/react-native/ReactCommon/react/nativemodule/fantomtestspecificmethods/NativeFantomTestSpecificMethods.cpp
@@ -49,4 +49,36 @@ void NativeFantomTestSpecificMethods::takeFunctionAndNoop(
     jsi::Runtime& runtime,
     jsi::Function function) {}
 
+void NativeFantomTestSpecificMethods::setRootNodeSize(
+    jsi::Runtime& runtime,
+    int surfaceId,
+    float width,
+    float height) {
+  auto& uiManager = getUIManagerFromRuntime(runtime);
+
+  uiManager.getShadowTreeRegistry().visit(
+      surfaceId, [&](const ShadowTree& shadowTree) {
+        shadowTree.commit(
+            [&](const RootShadowNode& oldRootShadowNode)
+                -> RootShadowNode::Unshared {
+              PropsParserContext propsParserContext{
+                  surfaceId, *uiManager.getContextContainer()};
+
+              const auto& mainBranchRootProps =
+                  oldRootShadowNode.getConcreteProps();
+              const auto layoutContext = mainBranchRootProps.layoutContext;
+              auto layoutConstraints = mainBranchRootProps.layoutConstraints;
+
+              layoutConstraints.maximumSize.width = width;
+              layoutConstraints.maximumSize.height = height;
+              layoutConstraints.minimumSize.width = width;
+              layoutConstraints.minimumSize.height = height;
+
+              return oldRootShadowNode.clone(
+                  propsParserContext, layoutConstraints, layoutContext);
+            },
+            {.source = ShadowTreeCommitSource::Unknown});
+      });
+}
+
 } // namespace facebook::react

packages/react-native/ReactCommon/react/nativemodule/fantomtestspecificmethods/NativeFantomTestSpecificMethods.h
@@ -21,6 +21,8 @@ class NativeFantomTestSpecificMethods : public NativeFantomTestSpecificMethodsCx
 
   void takeFunctionAndNoop(jsi::Runtime &runtime, jsi::Function callback);
 
+  void setRootNodeSize(jsi::Runtime &runtime, int surfaceId, float width, float height);
+
  private:
   std::shared_ptr<FantomForcedCloneCommitHook> fantomForcedCloneCommitHook_{};
 };

packages/react-native/ReactCommon/react/renderer/mounting/ShadowTree.cpp
@@ -220,7 +220,7 @@ void ShadowTree::setCommitMode(CommitMode commitMode) const {
   auto revision = ShadowTreeRevision{};
 
   {
-    ShadowTree::UniqueLock lock = uniqueCommitLock();
+    ShadowTree::UniqueLock lock = uniqueRevisionLock();
 
     if (commitMode_ == commitMode) {
       return;
@@ -238,7 +238,7 @@ void ShadowTree::setCommitMode(CommitMode commitMode) const {
 }
 
 CommitMode ShadowTree::getCommitMode() const {
-  SharedLock lock = sharedCommitLock();
+  SharedLock lock = sharedRevisionLock();
   return commitMode_;
 }
 
@@ -262,7 +262,7 @@ CommitStatus ShadowTree::commit(
     }
 
     {
-      std::unique_lock lock(commitMutexRecursive_);
+      std::unique_lock lock(revisionMutexRecursive_);
       return tryCommit(transaction, commitOptions);
     }
   } else {
@@ -286,18 +286,32 @@ CommitStatus ShadowTree::tryCommit(
     const CommitOptions& commitOptions) const {
   TraceSection s("ShadowTree::commit");
 
+  auto isReactBranch = ReactNativeFeatureFlags::enableFabricCommitBranching() &&
+      commitOptions.source == CommitSource::React;
+
+  // Commits on the JS branch are never synchronous.
+  react_native_assert(!isReactBranch || !commitOptions.mountSynchronously);
+
   auto telemetry = TransactionTelemetry{};
   telemetry.willCommit();
 
   CommitMode commitMode;
   auto oldRevision = ShadowTreeRevision{};
+  auto oldRevisionForStateProgression = ShadowTreeRevision{};
   auto newRevision = ShadowTreeRevision{};
 
   {
     // Reading `currentRevision_` in shared manner.
-    SharedLock lock = sharedCommitLock();
+    SharedLock lock = sharedRevisionLock();
     commitMode = commitMode_;
-    oldRevision = currentRevision_;
+
+    if (isReactBranch && currentReactRevision_.has_value()) {
+      oldRevision = currentReactRevision_.value();
+    } else {
+      oldRevision = currentRevision_;
+    }
+
+    oldRevisionForStateProgression = currentRevision_;
   }
 
   const auto& oldRootShadowNode = oldRevision.rootShadowNode;
@@ -308,8 +322,8 @@ CommitStatus ShadowTree::tryCommit(
   }
 
   if (commitOptions.enableStateReconciliation) {
-    auto updatedNewRootShadowNode =
-        progressState(*newRootShadowNode, *oldRootShadowNode);
+    auto updatedNewRootShadowNode = progressState(
+        *newRootShadowNode, *oldRevisionForStateProgression.rootShadowNode);
     if (updatedNewRootShadowNode) {
       newRootShadowNode =
           std::static_pointer_cast<RootShadowNode>(updatedNewRootShadowNode);
@@ -336,9 +350,10 @@ CommitStatus ShadowTree::tryCommit(
 
   {
     // Updating `currentRevision_` in unique manner if it hasn't changed.
-    UniqueLock lock = uniqueCommitLock();
+    UniqueLock lock = uniqueRevisionLock(
+        /*defer*/ isReactBranch);
 
-    if (currentRevision_.number != oldRevision.number) {
+    if (!isReactBranch && currentRevision_.number != oldRevision.number) {
       return CommitStatus::Failed;
     }
 
@@ -364,12 +379,21 @@ CommitStatus ShadowTree::tryCommit(
         .number = newRevisionNumber,
         .telemetry = telemetry};
 
-    currentRevision_ = newRevision;
+    if (isReactBranch) {
+      // Lock the deferred lock to ensure that the new React revision is not
+      // reset during a scheduled merge.
+      std::visit([](auto& concreteLock) { concreteLock.lock(); }, lock);
+      currentReactRevision_ = newRevision;
+    } else {
+      currentRevision_ = newRevision;
+    }
   }
 
   emitLayoutEvents(affectedLayoutableNodes);
 
-  if (commitMode == CommitMode::Normal) {
+  if (isReactBranch) {
+    scheduleReactRevisionPromotion();
+  } else if (commitMode == CommitMode::Normal) {
     mount(std::move(newRevision), commitOptions.mountSynchronously);
   }
 
@@ -377,8 +401,13 @@ CommitStatus ShadowTree::tryCommit(
 }
 
 ShadowTreeRevision ShadowTree::getCurrentRevision() const {
-  SharedLock lock = sharedCommitLock();
+  SharedLock lock = sharedRevisionLock();
   return currentRevision_;
+}
+
+std::optional<ShadowTreeRevision> ShadowTree::getCurrentReactRevision() const {
+  SharedLock lock = sharedRevisionLock();
+  return currentReactRevision_;
 }
 
 void ShadowTree::mount(ShadowTreeRevision revision, bool mountSynchronously)
@@ -386,6 +415,72 @@ void ShadowTree::mount(ShadowTreeRevision revision, bool mountSynchronously)
   mountingCoordinator_->push(std::move(revision));
   delegate_.shadowTreeDidFinishTransaction(
       mountingCoordinator_, mountSynchronously);
+}
+
+void ShadowTree::mergeReactRevision() const {
+  ShadowTreeRevision promotedRevision;
+
+  {
+    UniqueLock lock = uniqueRevisionLock(false);
+
+    if (!reactRevisionToBePromoted_.has_value()) {
+      return;
+    }
+
+    promotedRevision =
+        std::exchange(reactRevisionToBePromoted_, std::nullopt).value();
+  }
+
+  const auto mergedRevisionNumber = promotedRevision.number;
+
+  this->commit(
+      [revision = std::move(promotedRevision)](
+          const RootShadowNode& /*oldRootShadowNode*/) {
+        return std::make_shared<RootShadowNode>(
+            *revision.rootShadowNode, ShadowNodeFragment{});
+      },
+      {
+          .enableStateReconciliation = true,
+          .mountSynchronously = true,
+          .source = CommitSource::ReactRevisionMerge,
+      });
+
+  {
+    UniqueLock commitLock = uniqueRevisionLock(
+        /*defer*/ false);
+
+    // If the current react revision is the same as the one that was just
+    // merged, clear it.
+    if (currentReactRevision_.has_value() &&
+        mergedRevisionNumber == currentReactRevision_.value().number) {
+      currentReactRevision_.reset();
+    }
+  }
+}
+
+void ShadowTree::promoteReactRevision() const {
+  ShadowTreeRevision currentReactRevision;
+  {
+    SharedLock lock = sharedRevisionLock();
+    // Promotion happens at the end of the event loop tick, it's possible to
+    // have more than one promotion in a row. In this case, all but the first
+    // one should no-op.
+    if (!currentReactRevision_.has_value()) {
+      return;
+    }
+    currentReactRevision = currentReactRevision_.value();
+  }
+
+  {
+    UniqueLock lock = uniqueRevisionLock(false);
+    reactRevisionToBePromoted_ = std::move(currentReactRevision);
+  }
+
+  delegate_.shadowTreeDidPromoteReactRevision(*this);
+}
+
+void ShadowTree::scheduleReactRevisionPromotion() const {
+  delegate_.shadowTreeDidFinishReactCommit(*this);
 }
 
 void ShadowTree::commitEmptyTree() const {
@@ -426,19 +521,21 @@ void ShadowTree::notifyDelegatesOfUpdates() const {
   delegate_.shadowTreeDidFinishTransaction(mountingCoordinator_, true);
 }
 
-inline ShadowTree::UniqueLock ShadowTree::uniqueCommitLock() const {
+inline ShadowTree::UniqueLock ShadowTree::uniqueRevisionLock(bool defer) const {
   if (ReactNativeFeatureFlags::preventShadowTreeCommitExhaustion()) {
-    return std::unique_lock{commitMutexRecursive_};
+    return defer ? std::unique_lock{revisionMutexRecursive_, std::defer_lock}
+                 : std::unique_lock{revisionMutexRecursive_};
   } else {
-    return std::unique_lock{commitMutex_};
+    return defer ? std::unique_lock{revisionMutex_, std::defer_lock}
+                 : std::unique_lock{revisionMutex_};
   }
 }
 
-inline ShadowTree::SharedLock ShadowTree::sharedCommitLock() const {
+inline ShadowTree::SharedLock ShadowTree::sharedRevisionLock() const {
   if (ReactNativeFeatureFlags::preventShadowTreeCommitExhaustion()) {
-    return std::unique_lock{commitMutexRecursive_};
+    return std::unique_lock{revisionMutexRecursive_};
   } else {
-    return std::shared_lock{commitMutex_};
+    return std::shared_lock{revisionMutex_};
   }
 }
 

packages/react-native/ReactCommon/react/renderer/mounting/ShadowTree.h
@@ -51,6 +51,7 @@ enum class ShadowTreeCommitSource {
   Unknown,
   React,
   AnimationEndSync,
+  ReactRevisionMerge,
 };
 
 struct ShadowTreeCommitOptions {
@@ -125,6 +126,12 @@ class ShadowTree final {
   ShadowTreeRevision getCurrentRevision() const;
 
   /*
+   * Returns a `ShadowTreeRevision` representing the momentary state of
+   * the `ShadowTree` in the JS thread.
+   */
+  std::optional<ShadowTreeRevision> getCurrentReactRevision() const;
+
+  /*
    * Commit an empty tree (a new `RootShadowNode` with no children).
    */
   void commitEmptyTree() const;
@@ -137,6 +144,18 @@ class ShadowTree final {
 
   std::shared_ptr<const MountingCoordinator> getMountingCoordinator() const;
 
+  /**
+   * Promotes the current React revision to be merged into the main branch of the
+   * ShadowTree.
+   */
+  void promoteReactRevision() const;
+
+  /**
+   * Commits the currently promoted React revision to the "main" branch of the
+   * ShadowTree. No-op if the promoted React revision doesn't exist.
+   */
+  void mergeReactRevision() const;
+
  private:
   constexpr static ShadowTreeRevision::Number INITIAL_REVISION{0};
 
@@ -144,19 +163,23 @@ class ShadowTree final {
 
   void emitLayoutEvents(std::vector<const LayoutableShadowNode *> &affectedLayoutableNodes) const;
 
+  void scheduleReactRevisionPromotion() const;
+
   const SurfaceId surfaceId_;
   const ShadowTreeDelegate &delegate_;
-  mutable std::shared_mutex commitMutex_;
-  mutable std::recursive_mutex commitMutexRecursive_;
-  mutable CommitMode commitMode_{CommitMode::Normal}; // Protected by `commitMutex_`.
-  mutable ShadowTreeRevision currentRevision_; // Protected by `commitMutex_`.
+  mutable std::shared_mutex revisionMutex_;
+  mutable std::recursive_mutex revisionMutexRecursive_;
+  mutable CommitMode commitMode_{CommitMode::Normal}; // Protected by `revisionMutex_`.
+  mutable ShadowTreeRevision currentRevision_; // Protected by `revisionMutex_`.
+  mutable std::optional<ShadowTreeRevision> currentReactRevision_; // Protected by `revisionMutex_`.
+  mutable std::optional<ShadowTreeRevision> reactRevisionToBePromoted_; // Protected by `revisionMutex_`.
   std::shared_ptr<const MountingCoordinator> mountingCoordinator_;
 
   using UniqueLock = std::variant<std::unique_lock<std::shared_mutex>, std::unique_lock<std::recursive_mutex>>;
   using SharedLock = std::variant<std::shared_lock<std::shared_mutex>, std::unique_lock<std::recursive_mutex>>;
 
-  inline UniqueLock uniqueCommitLock() const;
-  inline SharedLock sharedCommitLock() const;
+  inline UniqueLock uniqueRevisionLock(bool defer = false) const;
+  inline SharedLock sharedRevisionLock() const;
 };
 
 } // namespace facebook::react

packages/react-native/ReactCommon/react/renderer/mounting/ShadowTreeDelegate.h
@@ -38,6 +38,17 @@ class ShadowTreeDelegate {
       std::shared_ptr<const MountingCoordinator> mountingCoordinator,
       bool mountSynchronously) const = 0;
 
+  /*
+   * Called right after Shadow Tree commits a new React revision of the tree.
+   */
+  virtual void shadowTreeDidFinishReactCommit(const ShadowTree &shadowTree) const = 0;
+
+  /*
+   * Called right after Shadow Tree promotes a React revision of the tree to
+   * be merged.
+   */
+  virtual void shadowTreeDidPromoteReactRevision(const ShadowTree &shadowTree) const = 0;
+
   virtual ~ShadowTreeDelegate() noexcept = default;
 };
 

packages/react-native/ReactCommon/react/renderer/mounting/tests/StateReconciliationTest.cpp
@@ -37,6 +37,12 @@ class DummyShadowTreeDelegate : public ShadowTreeDelegate {
   void shadowTreeDidFinishTransaction(
       std::shared_ptr<const MountingCoordinator> mountingCoordinator,
       bool /*mountSynchronously*/) const override {}
+
+  void shadowTreeDidFinishReactCommit(
+      const ShadowTree& /*shadowTree*/) const override {}
+
+  void shadowTreeDidPromoteReactRevision(
+      const ShadowTree& /*shadowTree*/) const override {}
 };
 
 namespace {

commit 61004fec7a3fb8e38110d233bfc8926002cbc879

Fix crash on reload
Revert crash on reload caused by https://github.com/facebook/react-native/commit/45b4817414928c9bfa0b055bb09533ac177cada6#diff-99e9fee4dbe9d329e2f7df16035fb871fa583c92028a48fefb1c48e405638944R314-R316

packages/react-native/ReactCxxPlatform/react/runtime/ReactHost.cpp
@@ -313,9 +313,6 @@ void ReactHost::destroyReactInstance() {
 
   reactInstanceData_->contextContainer->erase(RuntimeSchedulerKey);
   reactInstanceData_->mountingManager->setSchedulerTaskExecutor(nullptr);
-  reactInstanceData_->mountingManager = nullptr;
-  reactInstanceData_->contextContainer = nullptr;
-  reactInstanceData_->turboModuleProviders.clear();
   reactInstance_ = nullptr;
   reactInstanceData_->messageQueueThread = nullptr;
 }