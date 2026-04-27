1090fd9cc7ba32b0c7abac0056988cd71ced3f32
Rubén Norte <rubennorte@meta.com>
2026-04-27 03:39:00 -0700

    Back out "Revert D102166537: [RN] Hoist `PerformanceLogger` primitives out of `react-native`"
    Summary:
    Changelog: [General][Deprecated] Deprecated second argument for hook function passed to AppRegistry.setComponentProviderInstrumentationHook as it's not usable by apps. Passed stub that warns when used.
    
    Re-applying https://github.com/facebook/react-native/pull/56580 that was reverted.
    
    Differential Revision: D102593753

--- packages/react-native/Libraries/ReactNative/renderApplication.js
+++ packages/react-native/Libraries/ReactNative/renderApplication.js
@@ -9,10 +9,7 @@
  */
 
 import type {ViewStyleProp} from '../StyleSheet/StyleSheet';
-import type {IPerformanceLogger} from '../Utilities/createPerformanceLogger';
 
-import GlobalPerformanceLogger from '../Utilities/GlobalPerformanceLogger';
-import PerformanceLoggerContext from '../Utilities/PerformanceLoggerContext';
 import AppContainer from './AppContainer';
 import DisplayMode, {type DisplayModeType} from './DisplayMode';
 import getCachedComponentWithDebugName from './getCachedComponentWithDebugName';
@@ -39,7 +36,10 @@ export default function renderApplication<Props extends Object>(
   // Keep this parameter for backwards compatibility only. It is always treated as
   // true internally.
   fabric?: true | void,
-  scopedPerformanceLogger?: IPerformanceLogger,
+  // Reserved positional slot (formerly `scopedPerformanceLogger`). Preserved so
+  // existing callers that supply this argument continue to type-check; the
+  // value is ignored.
+  _unused?: void,
   isLogBox?: boolean,
   debugName?: string,
   displayMode?: ?DisplayModeType,
@@ -47,19 +47,15 @@ export default function renderApplication<Props extends Object>(
 ) {
   invariant(rootTag, 'Expect to have a valid rootTag, instead got ', rootTag);
 
-  const performanceLogger = scopedPerformanceLogger ?? GlobalPerformanceLogger;
-
   let renderable: React.MixedElement = (
-    <PerformanceLoggerContext.Provider value={performanceLogger}>
-      <AppContainer
-        rootTag={rootTag}
-        WrapperComponent={WrapperComponent}
-        rootViewStyle={rootViewStyle}
-        initialProps={initialProps ?? Object.freeze({})}
-        internal_excludeLogBox={isLogBox}>
-        <RootComponent {...initialProps} rootTag={rootTag} />
-      </AppContainer>
-    </PerformanceLoggerContext.Provider>
+    <AppContainer
+      rootTag={rootTag}
+      WrapperComponent={WrapperComponent}
+      rootViewStyle={rootViewStyle}
+      initialProps={initialProps ?? Object.freeze({})}
+      internal_excludeLogBox={isLogBox}>
+      <RootComponent {...initialProps} rootTag={rootTag} />
+    </AppContainer>
   );
 
   if (__DEV__ && debugName) {
@@ -75,8 +71,8 @@ export default function renderApplication<Props extends Object>(
 
   if (useOffscreen && displayMode != null) {
     // $FlowFixMe[incompatible-type]
-    // $FlowFixMe[prop-missing]
     // $FlowFixMe[missing-export]
+    // $FlowFixMe[prop-missing] `unstable_Activity` is not yet in the React types.
     const Activity: ActivityType = React.unstable_Activity;
 
     renderable = (
@@ -87,16 +83,8 @@ export default function renderApplication<Props extends Object>(
     );
   }
 
-  performanceLogger.startTimespan('renderApplication_React_render');
-  performanceLogger.setExtra('usedReactConcurrentRoot', '1');
-  performanceLogger.setExtra('usedReactFabric', '1');
-  performanceLogger.setExtra(
-    'usedReactProfiler',
-    Renderer.isProfilingRenderer(),
-  );
   Renderer.renderElement({
     element: renderable,
     rootTag,
   });
-  performanceLogger.stopTimespan('renderApplication_React_render');
 }
